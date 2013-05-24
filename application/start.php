cf715dd0fcd6deee90750e660b99294d95b44ae8
Shashank Kumar <shashankkumar.me@gmail.com>
2013-05-24 00:59:23 -0700

    Refs #3: BasicAuth class for handling authentication
    Desc:
    *	Adds entry for auto-loading of the class in start.php
    *	Enable error loggin in laravel.

--- application/start.php
+++ application/start.php
@@ -60,6 +60,7 @@ Laravel\Autoloader::$aliases = $aliases;
 
 Autoloader::map(array(
 	'Base_Controller' => path('app').'controllers/base.php',
+	'BasicAuth'		  => path('app').'libraries/basic_auth.php'
 ));
 
 /*
