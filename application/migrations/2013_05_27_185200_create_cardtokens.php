5a67de0c24c69d25dfefb2a387811cad70c80abd
Abhishek Das <das.abhshk@gmail.com>
2013-05-29 04:00:35 +0530

    Adds migration script for `cardtokens` table; Refs #4

---
+++ application/migrations/2013_05_27_185200_create_cardtokens.php
@@ -0,0 +1,41 @@
+<?php
+
+class Create_Cardtokens {
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('cardtokens', function($table){
+			$table->engine = 'InnoDB';
+
+			$table->increments('id');
+
+			$table->integer('card_id')
+				  ->unsigned()
+				  ->nullable();
+
+			$table->string('token', 32);
+
+			$table->boolean('expired');
+
+			$table->timestamps();
+
+			$table->foreign('card_id')->references('id')->on('cards')->on_delete('SET NULL');
+		});
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('cardtokens');
+	}
+
+}
