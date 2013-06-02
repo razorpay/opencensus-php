razorpay/opencensus-php

commit c8d380b7bb2f78afa94cc928c748239a00a4051f

Refs #5, #3, #4, BasicAuth class is now functional.
Desc:
*	Enhancements to BasicAuth class and bug fixes - #3
*	Addition of Key model class - #5
*	Schema changes to Key table - #4.

application/controllers/transaction.php
@@ -5,11 +5,6 @@ class Transaction_Controller extends Base_Controller
 
 	public $restful = true;
 
-	private function get_authenticated_merchant() {
-		//TODO: get the merchant's `id` authenticated via key.
-		return 1;
-	}
-
 	/**
 	* To retrieve transaction details by `id`
 	* Lists previous transactions if `id` not provided
@@ -17,31 +12,31 @@ class Transaction_Controller extends Base_Controller
 	* @param id (optional)
 	*
 	*/
-	public function get_index ( $token = NULL )
+	public function get_index ($token = NULL)
 	{
-		$merchant_id = $this->get_authenticated_merchant();
+		$merchant_id = BasicAuth::Merchant()->id;
 
-		$m = Merchant::find($merchant_id);
-		if ($m === NULL)
-			return Response::error('401');
-
-		if ($token === NULL) {
+		$m = BasicAuth::Merchant();
+		
+		if (is_null($token)) {
 			$t = $m->transactions;
-			if ( empty($t) )
+			if (empty($t))
 				return Response::error('404');
 			else
 				return Response::eloquent($t);
 		}
-
 		else {
-			$t = Transaction::where('token','=',$token)->first();
-			if ( $t !== NULL )
-				if ( $t->get_merchant() === $merchant_id )
+			$t = Transaction::where('token','=', $token)->first();
+		
+			if (is_null($token))
+				return Response::error('404');
+			else
+			{
+				if ($t->get_merchant() === $merchant_id)
 					return Response::eloquent($t);
 				else
 					return Response::error('401');
-			else
-				return Response::error('404');
+			}
 		}
 
 	}
@@ -51,14 +46,11 @@ class Transaction_Controller extends Base_Controller
 	*/
 	public function post_index ()
 	{
-		$merchant_id = $this->get_authenticated_merchant();
-
-		$m = Merchant::find($merchant_id);
-		if ($m === NULL)
-			return Response::error('401');
+		$merchant_id = BasicAuth::Merchant()->id;
 
 		$t = new Transaction();
-		$e = $t->build_transaction(Input::get(), $merchant_id);
+		
+		$e = $t->build(Input::get(), $merchant_id);
 		
 		if ($e !== NULL)
 			return $e;

application/models/key.php
@@ -2,62 +2,42 @@
 
 class Key extends Eloquent {
 
-	
+	private $key_generated = NULL;
 
-	public function __construct()
+	private static $key_row_rules = array(
+		'live' => 'required|size:1|in:0,1',
+		'secret' => 'required|size:1|in:0,1');
+
+	public function generate($data, &$error)
 	{
-		$key = openssl_random_pseudo_bytes(32);
-		$generated = true;
-	}
-
-	public function __construct($key)
-	{
-
-	}
-
-	public function __construct($key, $live, $secret)
-	{
-		this->$key = $key;
-		this->$live = $live;
-		this->$secret = $secret;
-		this->$generated = false;
-	}
-
-	public static function http_basic_auth_key()
-	{
-		if (!isset($_SERVER['PHP_AUTH_USER']))
+		$validation = Validator::make($data, self::$key_row_rules);
+		
+		if ($validation->fails())
 		{
+			$error = $validation->errors;
 			return false;
 		}
 
-		$key = $_SERVER['PHP_AUTH_USER'];
-		
-		Key::where('public', '=', $key)
-			->or_where('private' '=', $key);
+		$this->keys = bin2hex(openssl_random_pseudo_bytes(16));
+		$this->live = $data['live'];
+		$this->secret = $data['secret'];
+		$this->merchant_id = $data['merchant_id'];
+		$this->active = 1;
+		return $this->save();
 	}
 
-	public function is_secret()
+	public function merchant()
 	{
-		return (($generated) ? -1 : $secret);
+		return $this->belongs_to('Merchant');
 	}
 
-	public function is_public()
+	public static function find_by_key($key)
 	{
-		return !(this->is_secret());
+		return Key::where('keys', '=', $key)->first();
 	}
 
-	public function is_live()
+	private static function geernate_key()
 	{
-		return (($generated) ? -1 : $live);
-	}
-
-	public function is_test()
-	{
-		return !(this->is_live());
-	}
-
-	public function is_generated()
-	{
-		return $generated;
+		return bin2hex(openssl_random_pseudo_bytes(16));
 	}
 }

application/models/transaction.php
@@ -7,7 +7,7 @@
 		public static $hidden = array('id','merchant_id','card_id');
 
 		private static $rules = array(
-			'amount' => 'required|numeric',
+			'amount' => 'required|integer',
 			'card' => 'required|match:/crd_[0-9a-z]{28}/'
 		);
 
@@ -18,7 +18,7 @@
 			return $this->belongs_to('Card');
 		}
 
-		public function validate_attributes ($input)
+		public function validate_attributes($input)
 		{
 			$validation = Validator::make($input, static::$rules);
 			if ($validation->fails()) {
@@ -26,14 +26,14 @@
 			}
 		}
 
-		public function build_transaction ($input, $merchant)
+		public function build($input, $merchant)
 		{
 			$e = $this->validate_attributes ($input);
-			if ($e !== NULL)
+			if (is_null($e))
 				return $e;
 
 			$card = CardToken::where('token','=',$input['card'])->first();
-			if ($card === NULL) {
+			if (is_null($card)) {
 				return Response::error('404');
 			}
 			else {

application/routes.php
@@ -32,28 +32,35 @@
 |
 */
 
-Route::post('cards', 'card@index');
+Route::controller('transactions');
 
-Route::get('transactions', 'transaction@index');
+Route::group(array('before' => 'auth.public'), function()
+{
+	Route::post('transactions', 'transaction@index');
+});
 
-Route::get('transactions/(:any)', 'transaction@index');
+Route::group(array('before' => 'auth'), function()
+{
+	Route::post('cards', 'card@index');
 
-Route::post('transactions', 'transaction@index');
+	Route::get('transactions', 'transaction@index');
 
-Route::post('transactions/(:any)/refund', 'transaction@refund');
+	Route::get('transactions/(:any)', 'transaction@index');
 
-Route::get('transactions/refund', 'transaction@refund');
+	Route::post('transactions/(:any)/refund', 'transaction@refund');
 
-Route::post('transactions/(:any)/process', 'transaction@process');
+	Route::get('transactions/refund', 'transaction@refund');
 
-Route::get('transactions/success', 'transaction@process');
+	Route::post('transactions/(:any)/process', 'transaction@process');
+
+	Route::get('transactions/success', 'transaction@process');
+});
 
 Route::get('/', function()
 {
 	return View::make('home.index');
 });
 
-Route::controller('transactions');
 /*
 |--------------------------------------------------------------------------
 | Application 404 & 500 Error Handlers
@@ -123,8 +130,25 @@ Route::filter('csrf', function()
 	if (Request::forged()) return Response::error('500');
 });
 
+/**
+ * Only allows requests with secret keys to get through.
+ */
 Route::filter('auth', function()
 {
 	BasicAuth::verify_key();
-	if (!BasicAuth::check()) return Response::error('401');
+	if (!BasicAuth::check() and
+		BasicAuth::secret())
+		return Response::error('401');
+});
+
+/**
+ * Only allows requests with public keys to get through.
+ */
+Route::filter('auth.public', function()
+{
+	BasicAuth::verify_key();
+	if (!BasicAuth::check() and
+		BasicAuth::secret())
+		return Response::error('401');
+
 });

application/libraries/key.php
@@ -1,63 +1,0 @@
-<?php 
-
-class Key {
-	
-	/**
-     * Key is test/live
-     */
-	private $live;
-
-	/**
-	 * Key is published/secret.
-	 */
-	private $secret;
-
-	/**
-	 * Key is generated/provided.
-	 */
-	private $generated;
-
-	/**
-	 * Key
-	 */
-	private $key;
-
-	public function __construct()
-	{
-		if (isset($_SERVER['PHP_AUTH_USER']))
-		{
-			$key = $_SERVER['PHP_AUTH_USER'];
-			$generated = false;
-		}
-		else
-		{
-			$key = openssl_random_pseudo_bytes(32);
-			$generated = true;
-		}
-	}
-
-	public function is_secret()
-	{
-		return $secret;
-	}
-
-	public function is_public()
-	{
-		return !$secret;
-	}
-
-	public function is_live()
-	{
-		return $live;
-	}
-
-	public function is_test()
-	{
-		return !$live;
-	}
-
-	public function is_generated()
-	{
-		return $generated;
-	}
-}

application/libraries/basicauth.php
@@ -8,24 +8,20 @@ class BasicAuth {
 
 	public static function verify_key()
 	{
-		if(isset($_SERVER['PHP_USER_AUTH']))
+		if(isset($_SERVER['PHP_AUTH_USER']))
 		{
-			$key = $_SERVER['PHP_USER_AUTH'];
+			$key = $_SERVER['PHP_AUTH_USER'];
+			
+			self::$Key = Key::find_by_key($key);
 
-
-			if (!is_null($Key))
+			if(is_null(self::$Key or self::$Key->active == 0))
+				return;
+			
+			$merchant_id = self::$Key->merchant_id;
+			$Merchant = Merchant::find($merchant_id);
+			if(!is_null($Merchant))
 			{
-				self::$Key = Key::find($key);
-
-				if(self::$Key->active == 0)
-					return;
-				
-				$merchant_id = self::$Key->merchant_id;
-				$Merchant = Merchant::find($merchant_id);
-				if(!is_null($Merchant))
-				{
-					self::$Merchant = $Merchant;
-				}
+				self::$Merchant = $Merchant;
 			}
 		}
 	}
@@ -33,7 +29,7 @@ class BasicAuth {
 	public static function check()
 	{
 		if ((!is_null(self::$Key)) &&
-			(!is_null($Merchant)))
+			(!is_null(self::$Merchant)))
 			return true;
 		else
 			return false;	
@@ -47,6 +43,16 @@ class BasicAuth {
 	public static function Merchant()
 	{
 		return self::$Merchant;
+	}
+
+	public static function live()
+	{
+		return self::$Key->live;
+	}
+
+	public static function secret()
+	{
+		return self::$Key->secret;
 	}
 
 }

application/migrations/2013_04_23_221828_create_keys.php
@@ -12,8 +12,11 @@ class Create_Keys {
 		Schema::create('keys', function($table){
 			$table->engine = 'InnoDB';
 
-			$table->string('key', 32)
-				  ->primary();
+			$table->increments('id');
+
+			$table->string('keys', 32)
+				  ->unique();
+			
 			$table->integer('merchant_id')
 				  ->unsigned()
 				  ->nullable();

application/models/merchant.php
@@ -9,6 +9,11 @@
 			return $this->has_many('Transaction');
 		}
 
+		public function keys()
+		{
+			return $this->has_many('Keys');
+		}
+
 	}
 
 ?>

commit 02c4c1eeb7f4ea4aef0776ebaede4c2eb3557dd0

Fixes a few bugs introduced by c8d380b7bb2f78afa94cc928c748239a00a4051f;
Desc:
*   `merchant_id` always has to be of type `int`
*   No need to register controller in `routes.php`

application/controllers/transaction.php
@@ -14,7 +14,7 @@ class Transaction_Controller extends Base_Controller
 	*/
 	public function get_index ($token = NULL)
 	{
-		$merchant_id = BasicAuth::Merchant()->id;
+		$merchant_id = (int)BasicAuth::Merchant()->id;
 
 		$m = BasicAuth::Merchant();
 		
@@ -28,7 +28,7 @@ class Transaction_Controller extends Base_Controller
 		else {
 			$t = Transaction::where('token','=', $token)->first();
 		
-			if (is_null($token))
+			if (is_null($t))
 				return Response::error('404');
 			else
 			{
@@ -46,7 +46,7 @@ class Transaction_Controller extends Base_Controller
 	*/
 	public function post_index ()
 	{
-		$merchant_id = BasicAuth::Merchant()->id;
+		$merchant_id = (int)BasicAuth::Merchant()->id;
 
 		$t = new Transaction();
 		

application/models/key.php
@@ -36,7 +36,7 @@ class Key extends Eloquent {
 		return Key::where('keys', '=', $key)->first();
 	}
 
-	private static function geernate_key()
+	private static function generate_key()
 	{
 		return bin2hex(openssl_random_pseudo_bytes(16));
 	}

application/models/transaction.php
@@ -29,7 +29,7 @@
 		public function build($input, $merchant)
 		{
 			$e = $this->validate_attributes ($input);
-			if (is_null($e))
+			if (!is_null($e))
 				return $e;
 
 			$card = CardToken::where('token','=',$input['card'])->first();

application/routes.php
@@ -32,8 +32,6 @@
 |
 */
 
-Route::controller('transactions');
-
 Route::group(array('before' => 'auth.public'), function()
 {
 	Route::post('transactions', 'transaction@index');