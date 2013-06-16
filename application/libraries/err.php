fec5fc08def2b83d3960dab1445d0d3445c05faa
Shashank Kumar <shashankkumar.me@gmail.com>
2013-06-16 16:50:13 -0700

    Refs #8: Develop error passing and logging infrastructure
    Desc:
    *	Lays the foundation for the above issue via ERR class.

---
+++ application/libraries/err.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Defines the constant error for the application to use.
+ */
+class ERR
+{
+
+    /**
+     * We don't want the class to have objects.
+     */
+    private function __construct() {}
+    
+    const NO_ERROR = NULL;
+
+    const SUCCESS = 0x0;
+
+    const INVALID_PARAMETERS = 0x1;
+    const MSG_1 = "The parameters provided are invalid.";
+
+    const TOKEN_ALREADY_USED = 0x2;
+    const MSG_2 = "This token has alreay been used and hence expired.";
+
+    const CVV_ALREADY_VERIFIED = 0x3;
+    const MSG_3 = "The CVV of the card has already been verified previously.";
+
+}
+
