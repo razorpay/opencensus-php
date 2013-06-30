razorpay/opencensus-php

commit c598de588e71ad509ae3a0648b0e13e72f79716c

Refs #4: DB Schema and laravel migration files
Desc:
*	Makes card_ as foreign key for transactions table.
*	Corrects indentation format for the migration files.
*	Converts amount in transaction table to unsigned integer format
	and adds additional fields.

application/migrations/2013_03_30_204855_create_cards.php
@@ -2,77 +2,80 @@
 
 class Create_Cards {
 
-  /**
-   * Make changes to the database.
-   *
-   * @return void
-   */
-  public function up()
-  {
-    Schema::create('cards', function($table){
-      $table->engine = 'InnoDB';
-      
-      $table->increments('id');
+    /**
+     * Make changes to the database.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cards', function($table){
+                
+            $table->engine = 'InnoDB';
+    
+            $table->increments('id');
 
-      $table->string('number');
+            $table->string('number');
 
-      $table->string('expiry_month')
-            ->nullable();
+            $table->string('expire_month')
+                  ->nullable();
 
-      $table->string('expiry_year')
-            ->nullable();
+            $table->string('expire_year')
+                  ->nullable();
 
-      $table->string('cvv')
-            ->nullable();
+            $table->string('cvv')
+                  ->nullable();
 
-      $table->integer('cardtype_id')
-            ->unsigned()
-            ->nullable();
+            $table->string('type');
 
-      $table->string('name');
+            $table->string('name');
 
-      $table->string('address_line1')
-            ->nullable();
+            /**
+             * Two letter ISO codes representing the country of the card.
+             */
+            $table->string('country', 2);
 
-      $table->string('address_line2')
-            ->nullable();
+            $table->string('address_line1')
+                  ->nullable();
 
-      $table->string('address_state')
-            ->nullable();
+            $table->string('address_line2')
+                  ->nullable();
 
-      $table->integer('address_zip')
-            ->unsigned()
-            ->nullable();
+            $table->string('address_city')
+                  ->nullable();
+            
+            $table->string('address_state')
+                  ->nullable();
 
-      $table->string('address_country')
-            ->nullable();
+            $table->integer('address_zip')
+                  ->unsigned()
+                  ->nullable();
 
-      $table->integer('user_id')
-            ->unsigned()
-            ->nullable();
+            $table->string('address_country')
+                  ->nullable();
 
-      $table->timestamps();
+            $table->boolean('cvv_check')
+                  ->nullable();
 
-      $table->foreign('cardtype_id')
-            ->references('id')
-            ->on('cardtypes')
-            ->on_delete('SET NULL');
+            $table->boolean('address_line1_check')
+                  ->nullable();
 
-      $table->foreign('user_id')
-            ->references('id')
-            ->on('users')
-            ->on_delete('SET NULL');
-    });
-  }
+            $table->boolean('addres_zip_check')
+                  ->nullable();
 
-  /**
-   * Revert the changes to the database.
-   *
-   * @return void
-   */
-  public function down()
-  {
-    Schema::drop('cards');
-  }
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Revert the changes to the database.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('cards');
+    }
 
 }

application/migrations/2013_05_21_060208_create_transactions_foreign_keys.php
@@ -16,8 +16,8 @@ class Create_Transactions_Foreign_Keys {
 	        	  ->on('merchants')
 	        	  ->on_delete('restrict');
 	        
-	        $table->foreign('card_id')
-	        	  ->references('id')
+	        $table->foreign('card_token')
+	        	  ->references('token')
 	        	  ->on('cards')
 	        	  ->on_delete('SET NULL');
 	        
@@ -37,7 +37,7 @@ class Create_Transactions_Foreign_Keys {
 	{
 		Schema::table('transactions', function($table){
 			$table->drop_foreign('transactions_merchant_id_foreign');
-			$table->drop_foreign('transactions_card_id_foreign');
+			$table->drop_foreign('transactions_card_token_foreign');
 			$table->drop_foreign('transactions_status_code_foerign');
 		});
 	}

application/migrations/2013_03_30_204840_create_users.php
@@ -1,37 +1,0 @@
-<?php
-
-class Create_Users {
-
-  /**
-   * Make changes to the database.
-   *
-   * @return void
-   */
-  public function up()
-  {
-    Schema::create('users', function($table){
-      $table->engine = 'InnoDB';
-      
-      $table->increments('id');
-
-      $table->string('name', 50)
-            ->nullable();
-
-      $table->string('email', 256)
-            ->unique();
-
-      $table->string('password');
-    });
-  }
-
-  /**
-   * Revert the changes to the database.
-   *
-   * @return void
-   */
-  public function down()
-  {
-    Schema::drop('users');
-  }
-
-}

application/migrations/2013_03_30_204909_create_transactions.php
@@ -19,23 +19,29 @@ class Create_Transactions {
             $table->integer('merchant_id')
                   ->unsigned();
 
-            $table->float('amount')
+            $table->integer('amount')
                   ->unsigned();
             
-            $table->integer('card_id')
+            $table->string('currency', 3)
+                  ->default('INR');
+
+            $table->string('description');
+
+            $table->boolean('livemode');
+
+            $table->integer('card_token')
                   ->unsigned()
                   ->nullable();
             
+            $table->boolean('processed')
+                  ->default('1');
+
             $table->integer('status_code')
                   ->unsigned()
                   ->nullable();
             
             $table->string('bankresponse')
                   ->nullable();
-            
-            $table->string('currency')
-                  ->nullable()
-                  ->default('INR');
             
             $table->timestamps();        // Adds created_at and updated_at columns to the table
 

application/migrations/2013_05_27_185200_create_cardtokens.php
@@ -18,7 +18,11 @@ class Create_Cardtokens {
 				  ->unsigned()
 				  ->nullable();
 
-			$table->string('token', 32);
+			$table->integer('merchant_id')
+				  ->unsigned()
+				  ->nullable();
+
+			$table->string('token', 16);
 
 			$table->boolean('expired');
 
@@ -28,6 +32,10 @@ class Create_Cardtokens {
 				  ->references('id')
 				  ->on('cards')
 				  ->on_delete('SET NULL');
+
+			$table->foreign('merchant_id')
+				  ->references('id')
+				  ->on('merchants');
 		});
 	}
 

commit a841ce19b3b3f993dce072bd14ecb91f2f5c711a

Refs #4: DB Schema and laravel migration files *	Rectify bugs introduced in c598de5

application/migrations/2013_03_30_204855_create_cards.php
@@ -17,10 +17,10 @@ class Create_Cards {
 
             $table->string('number');
 
-            $table->string('expire_month')
+            $table->string('expiry_month')
                   ->nullable();
 
-            $table->string('expire_year')
+            $table->string('expiry_year')
                   ->nullable();
 
             $table->string('cvv')
@@ -60,7 +60,7 @@ class Create_Cards {
             $table->boolean('address_line1_check')
                   ->nullable();
 
-            $table->boolean('addres_zip_check')
+            $table->boolean('address_zip_check')
                   ->nullable();
 
             $table->timestamps();

application/migrations/2013_05_21_060208_create_transactions_foreign_keys.php
@@ -18,7 +18,7 @@ class Create_Transactions_Foreign_Keys {
 	        
 	        $table->foreign('card_token')
 	        	  ->references('token')
-	        	  ->on('cards')
+	        	  ->on('cardtokens')
 	        	  ->on_delete('SET NULL');
 	        
 	        $table->foreign('status_code')
@@ -38,7 +38,7 @@ class Create_Transactions_Foreign_Keys {
 		Schema::table('transactions', function($table){
 			$table->drop_foreign('transactions_merchant_id_foreign');
 			$table->drop_foreign('transactions_card_token_foreign');
-			$table->drop_foreign('transactions_status_code_foerign');
+			$table->drop_foreign('transactions_status_code_foreign');
 		});
 	}
 