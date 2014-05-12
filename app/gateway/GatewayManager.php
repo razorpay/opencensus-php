90cdf2d9fbfc22bb0a1d59fa3f7648b4dd5fd492
Abhishek Das <das.abhshk@gmail.com>
2014-05-12 18:26:38 +0530

    Style changes in gateway module

--- app/gateway/GatewayManager.php
+++ app/gateway/GatewayManager.php
@@ -4,54 +4,54 @@ namespace Gateway;
 
 class GatewayManager
 {
-	protected $gateways = array();
+    protected $gateways = array();
 
-	protected function createGatewayProvider()
-	{
-		$this->provider = new HDFC();
-	}
+    protected function createGatewayProvider()
+    {
+        $this->provider = new HDFC();
+    }
 
-	protected function gateway($gateway = null)
-	{
-		$gateway = $gateway ?: $this->getDefaultGateway();
+    protected function gateway($gateway = null)
+    {
+        $gateway = $gateway ?: $this->getDefaultGateway();
 
-		if ( ! isset($this->gateways[$gateway]))
-		{
-			$this->gateways[$gateway] = $this->createGateway($gateway);
-		}
+        if ( ! isset($this->gateways[$gateway]))
+        {
+            $this->gateways[$gateway] = $this->createGateway($gateway);
+        }
 
-		return $this->gateways[$gateway];
-	}
+        return $this->gateways[$gateway];
+    }
 
-	protected function createGateway($gateway)
-	{
-		$method = 'create'.ucfirst($gateway).'Gateway';
+    protected function createGateway($gateway)
+    {
+        $method = 'create'.ucfirst($gateway).'Gateway';
 
-		if (method_exists($this, $method))
-		{
-			return $this->$method();
-		}
+        if (method_exists($this, $method))
+        {
+            return $this->$method();
+        }
 
-		throw new \InvalidArgumentException("Gateway $gateway not supported");
-	}
+        throw new \InvalidArgumentException("Gateway $gateway not supported");
+    }
 
-	public function getDefaultGateway()
-	{
-		return 'Hdfc';
-	}
+    public function getDefaultGateway()
+    {
+        return 'Hdfc';
+    }
 
-	public function getGateways()
-	{
-		return $gateways;
-	}
+    public function getGateways()
+    {
+        return $gateways;
+    }
 
-	public function createHdfcGateway()
-	{
-		return new HdfcGateway\HdfcGateway();
-	}
+    public function createHdfcGateway()
+    {
+        return new HdfcGateway\HdfcGateway();
+    }
 
-	public function __call($method, $parameters)
-	{
-		return call_user_func_array(array($this->gateway(), $method), $parameters);
-	}
+    public function __call($method, $parameters)
+    {
+        return call_user_func_array(array($this->gateway(), $method), $parameters);
+    }
 }
--- app/gateway/hdfc/HdfcGatewayResult.php
+++ app/gateway/hdfc/HdfcGatewayResult.php
@@ -4,21 +4,21 @@ namespace Gateway\HdfcGateway;
 
 final class HdfcGatewayResult
 {
-	/**
-	 * Result codes received in response for card enrollment
-	 */
-	const ENROLLED = 'ENROLLED';
-	const NOT_ENROLLED = 'NOT ENROLLED';
-	const FSS001 = 'FSS001';
+    /**
+     * Result codes received in response for card enrollment
+     */
+    const ENROLLED = 'ENROLLED';
+    const NOT_ENROLLED = 'NOT ENROLLED';
+    const FSS001 = 'FSS001';
 
-	/**
-	 * Result codes received in response for transaction
-	 * authorization
-	 */
-	const CAPUTRED = 'CAPTURED';
-	const APPROVED = 'APPROVED';
-	const NOT_CAPTURED = 'NOT CAPTURED';
-	const NOT_APPROVED = 'NOT APPROVED';
-	const DENIED_BY_RISK = 'DENIED BY RISK';
-	const HOST_TIMEOUT = 'HOST TIMEOUT';
+    /**
+     * Result codes received in response for transaction
+     * authorization
+     */
+    const CAPTURED = 'CAPTURED';
+    const APPROVED = 'APPROVED';
+    const NOT_CAPTURED = 'NOT CAPTURED';
+    const NOT_APPROVED = 'NOT APPROVED';
+    const DENIED_BY_RISK = 'DENIED BY RISK';
+    const HOST_TIMEOUT = 'HOST TIMEOUT';
 }
