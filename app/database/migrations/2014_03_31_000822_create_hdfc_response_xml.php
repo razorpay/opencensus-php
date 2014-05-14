8f4dedeacd1f1e03993201d9b1f7ad5febab2b1b
Shashank Kumar <shashankkumar.me@gmail.com>
2014-05-14 07:54:16 -0700

    Adds timestamps to hdfc_response_xml table

--- app/database/migrations/2014_03_31_000822_create_hdfc_response_xml.php
+++ app/database/migrations/2014_03_31_000822_create_hdfc_response_xml.php
@@ -5,39 +5,45 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateHdfcResponseXml extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('hdfc_response_xml', function(Blueprint $table)
-		{
-			$table->engine = 'InnoDB';
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hdfc_response_xml', function(Blueprint $table)
+        {
+            $table->engine = 'InnoDB';
 
-			$table->string('id', 32)
-				  ->primary();
+            $table->string('id', 32)
+                  ->primary();
 
-			$table->text('enroll_xml');
+            $table->text('enroll');
 
-			$table->text('auth_xml_enrolled')
-				  ->nullable();
+            $table->text('auth_enrolled')
+                  ->nullable();
 
-			$table->text('auth_xml_not_enrolled')
-				  ->nullable();
+            $table->text('auth_not_enrolled')
+                  ->nullable();
 
-		});
-	}
+            $table->timestamps();
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		//
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('hdfc_response_xml', function($table){
+
+            ;
+        });
+
+        Schema::drop('hdfc_response_xml');
+    }
 
 }
