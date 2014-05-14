0f1534c640a78288efe58874df30206f64186648
Abhishek Das <das.abhshk@gmail.com>
2014-05-14 21:02:57 +0530

    Changes '0x256' to corresponding constant name

--- app/models/service/transaction.php
+++ app/models/service/transaction.php
@@ -127,7 +127,7 @@ class Transaction extends Service
 
     public function refund($txn_data = NULL)
     {
-        $data = array('txn' => $txn_data->toArrayEx(0x256));
+        $data = array('txn' => $txn_data->toArrayEx(DAL\Transaction::WITH_CARD));
 
         $gateway = new GatewayManager();
 
