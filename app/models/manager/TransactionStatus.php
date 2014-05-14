79acc9dbedfb171af48c4b2f8d6a66eaafc31d4e
Shashank Kumar <shashankkumar.me@gmail.com>
2014-05-14 08:02:18 -0700

    Adds transaction status codes

--- app/models/manager/TransactionStatus.php
+++ app/models/manager/TransactionStatus.php
@@ -8,6 +8,7 @@ class TransactionStatus
 	const PENDING = 'pending';
 	const HOLD = 'hold';
 	const CAPTURED = 'captured';
+	const ENROLLED = 'enrolled';
 	const FAILED = 'failed';
 	const REFUNDED = 'refunded';
 	const PARTIALLY_REFUNDED = 'prefunded';
