a4ca60dd06f5dfaa814fc42d4736ed844c3bdc7d
Harshil Mathur <harshilmathur@gmail.com>
2014-05-15 16:01:03 +0530

    Added wercker conf

--- app/config/testing/database.sample.php
+++ app/config/testing/database.sample.php
@@ -54,10 +54,11 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'apitesting',
-			'username'  => 'shippable',
-			'password'  => '',
+			'host'      => $_ENV['WERCKER_MYSQL_HOST'],
+			'port'		=> $_ENV['WERCKER_MYSQL_PORT'],
+			'database'  => $_ENV['WERCKER_MYSQL_DATABASE'],
+			'username'  => $_ENV['WERCKER_MYSQL_USERNAME'],
+			'password'  => $_ENV['WERCKER_MYSQL_PASSWORD'],
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
