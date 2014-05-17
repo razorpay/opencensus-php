a485373a7864bf2fee137d06da7085ea789d530b
Harshil Mathur <harshilmathur@gmail.com>
2014-05-17 19:13:14 +0530

    Added unit test for retrieving transaction by id

--- app/tests/TransactionRetrieveTest.php
+++ app/tests/TransactionRetrieveTest.php
@@ -18,8 +18,9 @@ class TransactionRetrieveTest extends TestCase {
 
         //Seed the db with required data
         Eloquent::unguard();
-        $key = Factory::create('Models\DAL\Key');
-        $transaction=Factory::create('Models\DAL\Transaction');
+        $merchant=Factory::create('Models\DAL\Merchant', ['id' => 1]);
+        $key = Factory::create('Models\DAL\Key', ['merchant_id'=>1]);
+        $transaction=Factory::create('Models\DAL\Transaction', ['merchant_id'=>1]);
         Eloquent::reguard();
     }
 
@@ -30,12 +31,16 @@ class TransactionRetrieveTest extends TestCase {
     }
 
 	/**
-	* Tests the /transactions route. Should return valid json with list of transactions
+	* Tests the /transactions & /transactions/$id route. 
+	* Should return valid json with list of all transactions in case 1
+	* Should return valid json with details of transaction specified by id in case 2
 	*/
 
 	public function testRetrieveTransaction()
     {	
-    	//GIVEN
+
+    	//Testing retrieval of all transactions with /transactions
+    	//GIVEN - Nothing
 
     	//WHEN
         $response = $this->call('GET', '/transactions');
@@ -43,8 +48,21 @@ class TransactionRetrieveTest extends TestCase {
 
         //THEN
         $this->assertJson($content);
-    }
+        $transactions=json_decode($content);
 
+        //Testing retrieval of specific transactions with /transactions/$id
+        //GIVEN
+        $id=$transactions[0]->id;
+
+        //WHEN
+        $response = $this->call('GET', "/transactions/$id");
+        $content = $response->getContent();
+
+        //THEN
+        $this->assertJson($content);
+        $transaction = json_decode($content, true);
+        $this->assertEquals($transaction['id'], $id);
+    }
     
 }
 
