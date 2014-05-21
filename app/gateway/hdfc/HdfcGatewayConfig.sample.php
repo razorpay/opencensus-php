61c2da48ed770f13068377828e0a977553ad60f1
Harshil Mathur <harshilmathur@gmail.com>
2014-05-21 16:52:34 +0530

    Modified for wercker, removed shippable.

---
+++ app/gateway/hdfc/HdfcGatewayConfig.sample.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Gateway\HdfcGateway;
+
+final class HdfcGatewayConfig
+{
+    public static function getCreds(){
+    	//modify and add actual ID, password
+        $id = 'HDFC_ID';
+        $password = 'HDFC_PASSWORD';
+        return array($id, $password);
+    } 
+}
