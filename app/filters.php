6a0f2902ed9692ed314e06288a3f428a4c3aee59
Harshil Mathur <harshilmathur@gmail.com>
2014-05-27 19:39:53 +0530

    Removed test domain

--- app/filters.php
+++ app/filters.php
@@ -40,11 +40,6 @@ App::after(function($request, $response)
  */
 Route::filter('auth', function($route, $request)
 {
-	if(isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] == 'test.razorpay.com')
-	{
-		$_SERVER['PHP_AUTH_PW']= 'd9c6bf091a1a64cb5678d8c1d5e7360f';
-	}
-
 	if (!isset($_SERVER['PHP_AUTH_PW']) && !isset($_SERVER['PHP_AUTH_USER']))
 	{
 		//Used by first request from browser thaty checks if HTTP AUTH is expected
