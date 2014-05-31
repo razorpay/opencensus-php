91780d1714d8da9852f77a6178305088bdc3cf70
Abhishek Kandoi <abhikandoi2000@gmail.com>
2014-05-31 15:37:22 +0530

    Changes column size for `iins` table

--- app/database/migrations/2014_05_28_091717_create_iins_table.php
+++ app/database/migrations/2014_05_28_091717_create_iins_table.php
@@ -15,7 +15,7 @@ class CreateIinsTable extends Migration {
 		Schema::create('iins', function(Blueprint $table)
 		{
 			$table->integer('iin')->primary();
-			$table->string('card_category', 10);
+			$table->string('card_category', 26);
 			$table->string('brand', 10);
 			$table->string('card_type', 6);
 			$table->string('country_code', 2);
