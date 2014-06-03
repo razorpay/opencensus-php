9c56b6ffcc851ac22fd7f234b1a783131c64321a
Abhishek Kandoi <abhikandoi2000@gmail.com>
2014-06-03 15:58:19 +0530

    Using Card service to generate card details
    Manual check for card network

--- app/models/manager/card.php
+++ app/models/manager/card.php
@@ -4,6 +4,7 @@ namespace Models\Manager;
 
 use \Validator;
 use Models\DAL;
+use Models\Service;
 
 class Card extends EntityManager
 {
@@ -12,6 +13,9 @@ class Card extends EntityManager
         'expiry_month'  => 'required|month',
         'expiry_year'   => 'required|expiry_year',
         'cvv'           => 'required|numeric|digits:3',
+        'type'          => 'card_type',
+        'bank'          => 'regex:/[a-zA-Z,0-9.&\- ]*/|max:100',
+        'country'       => 'alpha|max:2',
         'name'          => 'required|alpha_space|max:100',
         'address_line1'     => 'regex:/[a-zA-Z,1-9. ]*/|max:100',
         'address_line2'     => 'regex:/[a-zA-Z,1-9. ]*/|max:100',
@@ -31,7 +35,30 @@ class Card extends EntityManager
 
     protected static $createValidators = array('address');
 
-    protected static $generators = array('last4', 'country', 'network', 'type', 'bank');
+    protected static $generators = array('last4', 'network', 'type', 'bank', 'country');
+
+    public function build(array $input)
+    {
+        // retrieve card details
+        $iin = intval(substr($input['number'], 0, 6));
+
+        $cardDetails = Service\Card::getNewInstance()->retrieveDetails($iin);
+
+        if($cardDetails)
+        {
+            $input['type'] = $cardDetails['card_type'];
+            $input['bank'] = $cardDetails['bank'];
+            $input['country'] = $cardDetails['brand'];
+        }
+        else
+        {
+            $input['type'] = '';
+            $input['bank'] = '';
+            $input['country'] = '';
+        }
+
+        parent::build($input);
+    }
 
     protected function validateAddress($input)
     {
@@ -73,15 +100,25 @@ class Card extends EntityManager
 
     public function generateNetwork($input)
     {
-        $network = "";
+        // shift this code to a common place in future
+        $network = false;
 
-        $iin = intval(substr($input['number'], 0, 6));
+        $maestroFirstFour = array('5018', '5020', '5038', '5612', '5893', '6304', '6759', '6761', '6762', '6763', '0604', '6390');
 
-        $cardDetails = DAL\CardDetail::find($iin);
-
-        if(null !== $cardDetails)
+        if(substr($input['number'], 0, 1) == '4')
         {
-            $network = $cardDetails->brand;
+            $network = 'VISA';
+        }
+        elseif(intval(substr($input['number'], 0, 2)) >= 51
+                and
+                intval(substr($input['number'], 0, 2)) <= 55
+                )
+        {
+            $network = 'MASTERCARD';
+        }
+        elseif(in_array(substr($input['number'], 0, 4), $maestroFirstFour))
+        {
+            $network = 'MAESTRO';
         }
 
         $this->setField('network', $network);
@@ -89,48 +126,21 @@ class Card extends EntityManager
 
     public function generateType($input)
     {
-        $type = "";
-
-        $iin = intval(substr($input['number'], 0, 6));
-
-        $cardDetails = DAL\CardDetail::find($iin);
-
-        if(null !== $cardDetails)
-        {
-            $type = $cardDetails->card_type;
-        }
+        $type = $input['type'];
 
         $this->setField('type', $type);
     }
 
     public function generateBank($input)
     {
-        $bank = "";
-
-        $iin = intval(substr($input['number'], 0, 6));
-
-        $cardDetails = DAL\CardDetail::find($iin);
-
-        if(null !== $cardDetails)
-        {
-            $bank = $cardDetails->bank;
-        }
+        $bank = $input['bank'];
 
         $this->setField('bank', $bank);
     }
 
     public function generateCountry($input)
     {
-        $country = "";
-
-        $iin = intval(substr($input['number'], 0, 6));
-
-        $cardDetails = DAL\CardDetail::find($iin);
-
-        if(null !== $cardDetails)
-        {
-            $country = $cardDetails->country_code;
-        }
+        $country = $input['country'];
 
         $this->setField('country', $country);
     }
