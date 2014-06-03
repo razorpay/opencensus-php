a78fe07978a3357f130b5be403fd5d13baabd9d7
Shashank Kumar <shashankkumar.me@gmail.com>
2014-06-03 16:17:28 +0530

    Adds environment support

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,11 +24,14 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(array(
+$env = $app->detectEnvironment( function () {
 
-	'local' => array('your-machine-name'),
-
-));
+    // Look for ./environment.php
+    if (file_exists(__DIR__ . '/environment.php' )) 
+    {
+    return require __DIR__ . '/environment.php';
+    }
+});
 
 /*
 |--------------------------------------------------------------------------
