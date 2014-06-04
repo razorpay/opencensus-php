1c712bb27c16b948e008e9a7b2bfd46a1d412550
Shashank Kumar <shashankkumar.me@gmail.com>
2014-06-04 17:15:37 +0530

    Changes tabs to spaces

--- app/database/migrations/2014_05_28_091717_create_iins_table.php
+++ app/database/migrations/2014_05_28_091717_create_iins_table.php
@@ -5,32 +5,32 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateIinsTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('iins', function(Blueprint $table)
-		{
-			$table->integer('iin')->primary();
-			$table->string('card_category', 26);
-			$table->string('brand', 10);
-			$table->string('card_type', 6);
-			$table->string('country_code', 2);
-			$table->string('bank', 100);
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('iins', function(Blueprint $table)
+        {
+            $table->integer('iin')->primary();
+            $table->string('card_category', 26);
+            $table->string('brand', 10);
+            $table->string('card_type', 6);
+            $table->string('country_code', 2);
+            $table->string('bank', 100);
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('iins');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('iins');
+    }
 
 }
