b46326ba2f36edafeb08e86f1e23ddaa0026b5c3
Abhishek Das <das.abhshk@gmail.com>
2014-06-04 20:18:52 +0530

    Creates login page

--- app/views/merchants/getLogin.blade.php
+++ app/views/merchants/getLogin.blade.php
@@ -1,1 +1,31 @@
 @extends('layout')
+
+@section('content')
+	<div class="grid grid-pad">
+		<div class="centered" id="login-wrapper">
+			<div class="content">
+				<h2>Sign In</h2>
+				<form method="POST" action="/login">
+					<div class="text">
+						<input type="text" name="email" placeholder="Email" autofocus>
+						<input type="password" placeholder="Password" name="password">
+					</div>
+					<div class="row">
+						<div class="remember col-1-2">
+							<input type="checkbox" id="remember-input"><label for="remember-input">Remember Me</label>
+						</div>
+						<div class="forgot-wrap col-1-2">
+							<a class="forgot" href="./reset">Forgot Password?</a>
+						</div>
+					</div>
+					<button id="login-button">Sign In</button>
+				</form>
+				<div class="form-footer">
+					<div class="footer-text">
+						Don't have a Razorpay account? <a href="./register">Sign Up</a> today.
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+@stop
