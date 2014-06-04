b513f889e2533edcb23e5d6ee515f3faa0ccb26b
Shashank Kumar <shashankkumar.me@gmail.com>
2014-06-04 12:50:44 +0530

    Adds retrieveDetails function to CardDetail DAL

--- app/models/dal/carddetail.php
+++ app/models/dal/carddetail.php
@@ -13,4 +13,17 @@ class CardDetail extends DAL
     public $timestamps = false;
 
     protected $guarded = array('*');
+
+    public static function retrieveDetails($iin)
+    {
+        if (strlen($iin) > 6)
+        {
+            $iin = intval(substr($iin, 0, 6)); 
+        }
+
+        // retrieve card details
+        $cardDetails = self::find($iin);
+
+        return $cardDetails;
+    }
 }
