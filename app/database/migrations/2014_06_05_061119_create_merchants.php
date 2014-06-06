ee015fb8ab30cccbffa97868081ac15b2db39e44
Abhishek Das <das.abhshk@gmail.com>
2014-06-06 10:30:04 +0530

    Adds migration for `merchants` table

---
+++ app/database/migrations/2014_06_05_061119_create_merchants.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMerchants extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('merchants', function(Blueprint $table)
+        {
+            $table->increments('id');
+
+            $table->string('name', 200);
+
+            $table->string('email', 255)->unique();
+
+            $table->string('password', 100);
+
+            $table->string('remember_token', 100)->nullable();
+
+            $table->integer('created_at');
+            $table->integer('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('merchants'); 
+    }
+
+}
