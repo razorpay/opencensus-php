264226caa6271cab1fbf29fe6175934739b1c23e
Shashank Kumar <shashankkumar.me@gmail.com>
2014-06-06 19:19:57 +0530

    Adds tracing configuration file

--- app/config/trace.php
+++ app/config/trace.php
@@ -2,12 +2,30 @@
 
 return array(
 
-  /*
-  |--------------------------------------------------------------------------
-  | Path for trace logs
-  |--------------------------------------------------------------------------
-  */
+    /**
+     * The following debug options come
+     * into play only 'debug' is true.
+     *
+     * They define the places where the
+     * logs will be written.
+     */
+    'debug_options' => array(
+        'screen' => true,
+        'browser' => true,
+        'chrome' => true),
 
-  'logpath' => storage_path().'/logs/trace.log',
+    /**
+     * Displays line/file/class/method from which the log call originated 
+     */
+    'introspection' => true,
 
+    'queue' => false,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Path for trace logs
+    |--------------------------------------------------------------------------
+    */
+
+    'logpath' => storage_path().'/logs/trace.log'
 );
