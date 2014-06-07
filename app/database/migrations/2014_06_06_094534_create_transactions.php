12eb6a22b20f419b5d498d3f2e635af340a29706
Abhishek Das <das.abhshk@gmail.com>
2014-06-07 13:03:55 +0530

    Adds migration file for `transactions` table

---
+++ app/database/migrations/2014_06_06_094534_create_transactions.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactions extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function(Blueprint $table)
+        {
+            $table->engine = 'InnoDB';
+
+            $table->increments('id');
+
+            $table->integer('merchant_id')
+                  ->unsigned();
+
+            $table->enum('type', array(
+                                      'day',
+                                      'week',
+                                      'month',
+                                      'year'
+                                      ));
+
+            $table->integer('amount')
+                  ->unsigned();
+
+            $table->integer('count')
+                  ->unsigned()
+                  ->default(1);
+
+            $table->integer('created_at');
+            $table->integer('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('transactions'); 
+    }
+
+}
