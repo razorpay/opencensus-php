14449fa34d839b0db2b80ee10c5b12e02f685e2b
Abhishek Das <das.abhshk@gmail.com>
2014-06-07 13:24:52 +0530

    Adds functions in transaction service to get analytics, process incoming transactions

--- app/models/service/transaction.php
+++ app/models/service/transaction.php
@@ -2,11 +2,19 @@
 
 namespace Models\Service;
 
+use Models\DAL;
 use Models\Manager;
 
 class Transaction extends Service
 {
-    public function fetch(array $input)
+    protected static $timeIntervals = array(
+        'day'   =>  86400, //24 * 60 * 60
+        'week'  =>  604800, //7 * 24 * 60 * 60
+        'month' =>  2678400, //31 * 24 * 60 * 60
+        'year'  =>  31536000 //365 * 24 * 60 * 60
+    );
+
+    public function fetchFromApi(array $input)
     {
         list($error,$options) = Manager\Transaction::createValidate($input, 'fetch')->getData();
 
@@ -21,6 +29,116 @@ class Transaction extends Service
             $data = $error;
         }
 
+        return $data;
+    }
+
+    public function process(array $input)
+    {
+        list($error, $data) = Manager\Transaction::createValidate($input, 'process')->getData();
+
+        if (empty($error))
+        {
+            foreach (static::$timeIntervals as $type => $interval)
+            {
+                $obj = DAL\Transaction::retrieveLastByType($data['merchant_id'], $type);
+
+                if (NULL === $obj || strtotime($obj->created_at) < ($data['created_at'] - $interval))
+                    $this->create($data, $type);
+                else
+                    $this->update($data, $obj);
+            }
+            return $data;
+        }
+        else
+            return $error;
+    }
+
+    protected function create($data, $type)
+    {
+        switch($type)
+        {
+            case 'day':
+                $data['created_at'] = strtotime(date('j F Y', $data['created_at']));
+                break;
+            case 'week':
+                $data['created_at'] = strtotime(date('o-\\WW', $data['created_at']));
+                break;
+            case 'month':
+                $data['created_at'] = strtotime(date('M Y', $data['created_at']));
+                break;
+            case 'year':
+                $data['created_at'] = strtotime("1 Jan " . date('Y', $data['created_at']));
+                break;
+        }
+        $data['type'] = $type;
+        DAL\Transaction::createOrFail($data);
+    }
+
+    protected function update($data, $obj)
+    {
+        $obj->updateAmount($data['amount']);
+        $obj->updateCount(1);
+
+        $obj->save();
+    }
+
+    public function getAnalytics($input)
+    {
+        list($error, $input) = Manager\Transaction::createValidate($input, 'analytics')->getData();
+
+        if (empty($error))
+        {
+            $data = DAL\Transaction::where('merchant_id','=',$input['merchant_id'])
+                            ->where('type','=',$input['type'])
+                            ->where('created_at','>=',$input['from'])
+                            ->where('created_at','<=',$input['to'])->get();
+
+            $data = $this->fillMissing($input, $data);
+
+            return $data;
+        }
+        else
+        {
+            return [];
+        }
+    }
+
+    //@todo: see if this function can be improved.
+    protected function fillMissing($input, $array)
+    {
+        for ($i = $input['from']; $i <= $input['to']; $i = strtotime('+1 ' . $input['type'], $i) )
+        {
+            $flag = false;
+
+            switch($input['type'])
+            {
+                case 'day':
+                    $i = strtotime(date('j F Y', $i));
+                    break;
+                case 'week':
+                    $i = strtotime(date('o-\\WW', $i));
+                    break;
+                case 'month':
+                    $i = strtotime(date('M Y', $i));
+                    break;
+                case 'year':
+                    $i = strtotime('1 Jan ' . date('Y', $i));
+                    break;
+            }
+
+            foreach ($array as $obj)
+            {
+                if ((strtotime($obj->created_at)) == $i)
+                {
+                    $data[] = $obj->toArray();
+                    $flag = true;
+                    break;
+                }
+            }
+
+            if ($flag == false)
+                $data[] = ['amount' => 0, 'count' => 0, 'created_at' => $i];
+        }
         return $data;
     }
 }
