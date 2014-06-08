e847028566d8246e95609652e08150629e4d783a
Shashank Kumar <shashankkumar.me@gmail.com>
2014-06-09 03:54:15 +0530

    Convert string routes to constants

--- app/routes.php
+++ app/routes.php
@@ -31,13 +31,14 @@
 |		});
 |
 */
+
 Route::group(array('after' => 'sameorigin'), function()
 {
 	Route::group(array('before' => 'auth.public'), function()
 	{
-		Route::post('transactions', 'TransactionController@postIndex');
+		Route::post(Constants\URL::TXN_CREATE, 'TransactionController@postIndex');
 
-		Route::get('transactions/jsonp', 'TransactionController@getJSONP');
+		Route::get(Constants\URL::TXN_JSONP, 'TransactionController@getJSONP');
 		
 	});
 
@@ -47,22 +48,19 @@ Route::group(array('after' => 'sameorigin'), function()
 
 		Route::get('tokens/{token}', 'CardController@getRetrieve');
 
-		Route::get('transactions/{param}', 'TransactionController@getIndex');
+		Route::get(Constants\URL::TXN_RETRIEVE, 'TransactionController@getIndex');
 
-		Route::get('transactions', 'TransactionController@getIndex');
+		Route::post(Constants\URL::TXN_REFUND, 'TransactionController@postRefund');
 
-		Route::post('transactions/{id}/refund', 'TransactionController@postRefund');
-
-		Route::post('transactions/{id}/capture', 'TransactionController@postCapture');
+		Route::post(Constants\URL::TXN_CAPTURE, 'TransactionController@postCapture');
 
 		//@todo: temporary
 		//create an artisan command and get rid of this
 		Route::get('capture', 'TransactionController@capture');
-
 	});
 });
 
-Route::post('transactions/callback', 'TransactionController@postCallback');
+Route::post(Constants\URL::TXN_CALLBACK, 'TransactionController@postCallback');
 
 Route::get('/', function()
 {
