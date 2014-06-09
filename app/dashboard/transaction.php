b070a0384d399e2530627227419064453a64e4de
Abhishek Das <das.abhshk@gmail.com>
2014-06-09 19:50:28 +0530

    Requeues records if status is false;

--- app/dashboard/transaction.php
+++ app/dashboard/transaction.php
@@ -6,13 +6,11 @@ class Transaction extends Dashboard
 {
     protected static $resource = 'transactions';
 
-    /**
-     * Mappings from API to Dashboard
-     */
     protected static $fields = array(
-        'merchant_id'   => 'merchant_id',
-        'amount'        => 'amount',
-        'status'        => 'status',
-        'updated_at'    => 'created_at'
+        'merchant_id',
+        'amount',
+        'status',
+        'updated_at',
+        'created_at'
     );
 }
