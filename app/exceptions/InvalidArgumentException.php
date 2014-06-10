ee506d428cc04de8345813ac4072ec545be057ba
Shashank Kumar <shashankkumar.me@gmail.com>
2014-06-10 17:19:20 +0530

    Adds support for passing message array in InvalidArgumentException

--- app/exceptions/InvalidArgumentException.php
+++ app/exceptions/InvalidArgumentException.php
@@ -4,8 +4,19 @@ namespace Exceptions;
 
 class InvalidArgumentException extends RazorpayException
 {
+    protected $messageArray = array();
+
     public function __construct($message, $code = 0 , Exception $previous = null)
     {
+        if (is_array($message))
+        {
+            $this->messageArray = $message;
+
+            $messages = implode('\n ', $message);
+            
+            parent::__construct($message, $code, $previous);
+        }
+
         parent::__construct('Invalid Arguments', $message, $code, $previous);
     }
 }
