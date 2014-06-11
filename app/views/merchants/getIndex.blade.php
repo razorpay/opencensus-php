4187f6a4aed39ecbf904e36d08eacc24862f9d80
Abhishek Das <das.abhshk@gmail.com>
2014-06-11 15:47:59 +0530

    Changes urls from relative to absolute

--- app/views/merchants/getIndex.blade.php
+++ app/views/merchants/getIndex.blade.php
@@ -18,16 +18,16 @@
 				</h2>
 				<ul>
 					<li class="active" data-tab="dashboard">
-						<a href="./#!/">Dashboard</a>
+						<a href="/#!/">Dashboard</a>
 					</li>
 					<li data-tab="transactions">
-						<a href="./#!/transactions">Transactions</a>
+						<a href="/#!/transactions">Transactions</a>
 					</li>
 					<li data-tab="refunds">
-						<a href="./#!/refunds">Refunds</a>
+						<a href="/#!/refunds">Refunds</a>
 					</li>
 					<li data-tab="settlements">
-						<a href="./#!/settlements">Settlements</a>
+						<a href="/#!/settlements">Settlements</a>
 					</li>
 				</ul>
 				<h2 class="lined">
@@ -35,13 +35,13 @@
 				</h2>
 				<ul>
 					<li data-tab="keys">
-						<a href="./#!/keys">API Keys</a>
+						<a href="/#!/keys">API Keys</a>
 					</li>
 					<li data-tab="activation">
-						<a href="./#!/activation">Activation</a>
+						<a href="/#!/activation">Activation</a>
 					</li>
 					<li data-tab="account">
-						<a href="./#!/account">Account</a>
+						<a href="/#!/account">Account</a>
 					</li>
 				</ul>
 			</div>
