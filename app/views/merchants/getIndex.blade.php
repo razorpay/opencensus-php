726119f819b28a5d513e3942f3610057b28a71e7
Abhishek Das <das.abhshk@gmail.com>
2014-06-12 19:36:28 +0530

    Creates separate blade template for sidebar

--- app/views/merchants/getIndex.blade.php
+++ app/views/merchants/getIndex.blade.php
@@ -12,39 +12,7 @@
 				</div>
 				<div class="button-desc">Live</div>
 			</div> -->
-			<div class="content boxed" id="sidebar">
-				<h2 class="lined">
-					<div class="title">Features</div>
-				</h2>
-				<ul>
-					<li class="active" data-tab="dashboard">
-						<a href="/#!/">Dashboard</a>
-					</li>
-					<li data-tab="transactions">
-						<a href="/#!/transactions">Transactions</a>
-					</li>
-					<li data-tab="refunds">
-						<a href="/#!/refunds">Refunds</a>
-					</li>
-					<li data-tab="settlements">
-						<a href="/#!/settlements">Settlements</a>
-					</li>
-				</ul>
-				<h2 class="lined">
-					<div class="title">Settings</div>
-				</h2>
-				<ul>
-					<li data-tab="keys">
-						<a href="/#!/keys">API Keys</a>
-					</li>
-					<li data-tab="activation">
-						<a href="/#!/activation">Activation</a>
-					</li>
-					<li data-tab="account">
-						<a href="/#!/account">Account</a>
-					</li>
-				</ul>
-			</div>
+			@include('sidebar')
 		</div>
 		<div class="col-9-12 spaced-right-box">
 			<div class="content boxed" id="dashboard-wrapper">
