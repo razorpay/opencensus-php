6f9dc6671bf5746698b6247768f7614e2ad7ce50
Abhishek Das <das.abhshk@gmail.com>
2014-06-14 14:39:56 +0530

    Creates db seeder for `merchants` and `aggregations` table

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,28 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->seed();
+	}
+
+	private function seed()
+	{
+		DB::table('merchants')->insert(array(
+			'id'			=>	1,
+			'name'			=>	'Abhishek Das',
+			'email'			=>	'das@razorpay.com',
+			'password'		=>	Hash::make('123456'),
+			'created_at'	=>	time(),
+			'updated_at'	=>	time()
+			)
+		);
+
+		DB::table('aggregations')->insert(array(
+			'merchant_id'	=>	1,
+			'txn_count'		=>	0,
+			'created_at'	=>	time(),
+			'updated_at'	=>	time()
+			)
+		);
 	}
 
 }
