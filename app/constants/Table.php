60ceb9ed40fefdfd38b665d34c89f0f1198c6cc1
Shashank Kumar <shashankkumar.me@gmail.com>
2014-06-15 14:39:35 +0530

    Adds constants for table names

---
+++ app/constants/Table.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Constants;
+
+class Table
+{
+    const TRANSACTIONS = 'transactions';
+
+    const CARDS = 'cards';
+
+    const MERCHANTS = 'merchants';
+
+    const TOKENS = 'cardtokens';
+
+    const LEDGER = 'ledger';
+
+    const KEYS = 'keys';
+
+    const IINS = 'iins';
+}
