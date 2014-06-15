fa4a437182310d6834ca35c8cdacb4e640fa6029
Shashank Kumar <shashankkumar.me@gmail.com>
2014-06-15 09:58:43 +0530

    Adds spaces to action constants

--- app/gateway/hdfc/HdfcGatewayAction.php
+++ app/gateway/hdfc/HdfcGatewayAction.php
@@ -5,8 +5,12 @@ namespace Gateway\HdfcGateway;
 class HdfcGatewayAction
 {
     const PURCHASE = 1;
+
     const REFUND = 2;
+
     const AUTH = 4;
+
     const CAPTURE = 5;
+
     const INQUIRY = 8;
 }
