4f3d99d2c02fe5ba7d745a823f44b697f4a23a7c
Shashank Kumar <shashankkumar.me@gmail.com>
2014-06-15 09:55:26 +0530

    Adds hdfc gateway error codes as constants

---
+++ app/gateway/hdfc/HdfcGatewayErrorCode.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Gateway\HdfcGateway;
+
+class HdfcGatewayErrorCode
+{
+    const FSS0001   = 'FSS0001';
+    const FSS00002  = 'FSS00002';
+    const GW00150   = 'GW00150';
+    const GW00151   = 'GW00151';
+    const GW00152   = 'GW00152';
+    const GW00153   = 'GW00153';
+    const GW00154   = 'GW00154';
+    const GW00159   = 'GW00159';
+    const GW00181   = 'GW00181';
+    const GW00205   = 'GW00205';
+    const GW00157   = 'GW00157';
+    const GW00165   = 'GW00165';
+    const GW00166   = 'GW00166';
+    const GW00167   = 'GW00167';
+    const GW00170   = 'GW00170';
+    const GW00171   = 'GW00171';
+    const GW00160   = 'GW00160';
+    const GW00161   = 'GW00161';
+    const GW00162   = 'GW00162';
+    const GW00163   = 'GW00163';
+    const GW00164   = 'GW00164';
+    const GW00177   = 'GW00177';
+    const GW00183   = 'GW00183';
+    const GW00201   = 'GW00201';
+    const GW00258   = 'GW00258';
+    const GW00259   = 'GW00259';
+    const GV00004   = 'GV00004';
+    const GV00005   = 'GV00005';
+    const GV00006   = 'GV00006';
+    const GV00007   = 'GV00007';
+    const GV00008   = 'GV00008';
+    const GV00011   = 'GV00011';
+    const PY20006   = 'PY20006';
+    const PY20001   = 'PY20001';
+    const PY20002   = 'PY20002';
+    const RP00001   = 'RP00001';
+    const RP00002   = 'RP00002';
+    const RP00003   = 'RP00003';
+
+    // The error codes starting with 'RP' are our custom ones 
+    // to handle unknow error cases returned from bank.
+
+}
