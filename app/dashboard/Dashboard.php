cd40cf1a5315aab151f37399cc61d1d57af82bce
Abhishek Das <das.abhshk@gmail.com>
2014-06-18 12:29:18 +0530

    Revert "Comments out dsahboard post request"
    This reverts commit f9597c179ae57cc773d8ee4c8c670c9bfd580cc0.

--- app/dashboard/Dashboard.php
+++ app/dashboard/Dashboard.php
@@ -63,15 +63,14 @@ class Dashboard
 
     public function postRequest($job, $data)
     {
-        // $response = Requests::post(
-        //     $this->getUrl() . $data['resource'],
-        //     array(),
-        //     $data['message']
-        // );
+        $response = Requests::post(
+            $this->getUrl() . $data['resource'],
+            array(),
+            $data['message']
+        );
 
-        //  For debugging purposes, persist failed requests
-        // if (json_decode($response->body)->status === FALSE);
-        //     DashboardDal::persistAfterFail($data['message']);
+        /* For debugging purposes, persist requests */
+        DashboardDal::persistAfterFail($data['message']);
 
         $job->delete();
     }
