0b214ad9691a21d5e0635fe589218ad657037312
Shashank Kumar <shashankkumar.me@gmail.com>
2014-06-20 11:38:23 +0530

    Adds support for MessageBag instance of messages for exception thrown

--- app/exceptions/InvalidArgumentException.php
+++ app/exceptions/InvalidArgumentException.php
@@ -2,17 +2,25 @@
 
 namespace Exceptions;
 
+use Illuminate\Support\MessageBag;
+
 class InvalidArgumentException extends RazorpayException
 {
-    protected $messageArray = array();
+    protected $messagebag = null;
 
     public function __construct($message, $code = 0 , Exception $previous = null)
     {
         if (is_array($message))
         {
-            $this->messageArray = $message;
+            $this->messagebag = $message;
 
             $message = implode('\n ', $message);
+        }
+        else if ($message instanceof MessageBag)
+        {
+            $this->messagebag = $message;
+
+            $message = implode('\n', join("\n",$validation->messages()->all()));
         }
 
         parent::__construct('Invalid Arguments', $message, $code, $previous);
