c57c9285be4dfc0288a19f8ebce55986d9167270
Shashank Kumar <shashankkumar.me@gmail.com>
2014-06-22 16:11:15 +0530

    Adds getters and setters and fixes typos for PublicError

--- app/ee/Error/PublicError.php
+++ app/ee/Error/PublicError.php
@@ -5,12 +5,11 @@ namespace EE\Error;
 class PublicError
 {
     protected $fields = array(
-        'class',
         'description',
         'code',
         'field');
 
-    protected $attributes = array();
+    protected $error = array();
 
     protected $httpStatusCode = 200;
 
@@ -24,9 +23,14 @@ class PublicError
         $this->httpStatusCode = $statusCode;
     }
 
+    public function getHttpStatusCode()
+    {
+        return $this->httpStatusCode;
+    }
+
     public function setGatewayTimeout()
     {
-        $this->setErrorClass(ErrorClass::GATEWAY_ERROR);
+        $this->setErrorCode(PublicErrorCode::GATEWAY_ERROR);
 
         $this->setDescription('Request to gateway timed out.');
 
@@ -35,7 +39,7 @@ class PublicError
 
     public function setGatewayError($httpStatusCode = 502)
     {
-        $this->attributes['class'] = ErrorClass::GATEWAY_ERROR;
+        $this->error['class'] = ErrorClass::GATEWAY_ERROR;
 
         $this->setDescription('Some wizardry happened on gateway side causing the txn/request to fail');
 
@@ -53,28 +57,36 @@ class PublicError
 
     public function setCardError($code, $desc)
     {
-        $this->setErrorClass(ErrorClass::$code);
+        $this->setErrorCode($code);
 
-        $this->setErrorDesc($desc);
+        $this->setErrorDescription($desc);
+
+        $this->setHttpStatusCode(400);
     }
 
-    public function setErrorClass($class)
+    public function setErrorCode($code)
     {
-        $this->attributes['class'] = $class;
+        $this->error['code'] = $code;
     }
 
     public function setErrorDescription($description)
     {
-        $this->attributes['description'] = $description;
+        $this->error['description'] = $description;
     }
 
     public function setDescription($desc)
     {
-        $this->attributes['description'] = $desc;
+        $this->error['description'] = $desc;
     }
 
-    public function getAttributes()
+    public function getErrorArray()
     {
-        return $this->attributes;
+        return $this->error;
+    }
+
+    public function toArray()
+    {
+        return array(
+            'error' => $this->getErrorArray());
     }
 }
