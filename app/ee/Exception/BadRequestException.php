0a1dc3598eac77b5cd19dfcc762a27b9190d59d7
Shashank Kumar <shashankkumar.me@gmail.com>
2014-06-23 02:04:43 +0530

    Refactors bad request and card error exceptions to use common functions via message formats trait

--- app/ee/Exception/BadRequestException.php
+++ app/ee/Exception/BadRequestException.php
@@ -6,78 +6,16 @@ use Illuminate\Support\messageBag;
 
 class BadRequestException extends RazorpayException
 {
-    protected $messageBag = null;
-
-    protected $messageArray = null;
-
-    protected $first = null;
+    use MessageFormats;
 
     public function __construct($message, $code = 0, \Exception $previous = null)
     {
-        if ($message instanceof messageBag)
-        {
-            $this->messageBag = $message;
+        $message = $this->constructStringMessage($message);
 
-            $this->messageArray = $message->getMessages();
+        $intcode = 0;
 
-            $message = implode('\n', $message->all());
+        parent::__construct($message, $intcode, $previous);
 
-            $this->setFirstPair();
-        }
-        else if (is_array($message))
-        {
-            $this->messageArray = $message;
-
-            $message = $this->implodeMessagesArray();
-
-            $this->setFirstPair();
-        }
-
-        parent::__construct($message, $code, $previous);
-    }
-
-    public function getMessageBag()
-    {
-        return $this->messageBag;
-    }
-
-    protected function setFirstPair()
-    {
-        $array = $this->messageArray;
-
-        if ($array !== null)
-        {
-            $pair = each($array);
-
-            $firstKey = $pair['key'];
-
-            $firstValue = (is_array($pair['value'])) ? $pair['value'][0] : $pair['value'];
-
-            $this->first = array($firstKey => $firstValue);
-        }
-    }
-
-    protected function implodeMessagesArray()
-    {
-        $messages = array();
-
-        foreach ($messageArray as $field => $values)
-        {
-            if (is_array($values))
-                array_push($messages, implode('\n', $messages));
-            else
-                array_push($messages, $values);
-        }
-
-        $message = implode('\n', $messages);
-
-        return $message;
-    }
-
-    public function getFirstPair()
-    {
-        $pair = each($this->first);
-
-        return array($pair['key'], $pair['value']);
+        $this->constructError($message, $code);
     }
 }
--- app/ee/Exception/CardErrorException.php
+++ app/ee/Exception/CardErrorException.php
@@ -4,8 +4,9 @@ namespace EE\Exception;
 
 use Illuminate\Support\MessageBag;
 
-class CardErrorException extends BadRequestException
+class CardErrorException extends RazorpayException
 {
+    use MessageFormats;
 
     /**
      * Card field for which the public error will be shown
@@ -20,36 +21,10 @@ class CardErrorException extends BadRequestException
     {
         $intcode = 0;
 
+        $message = $this->constructStringMessage($messsage);
+
         parent::__construct($message, $intcode, $previous);
 
-        if (($message !== null) and
-            (is_string($message) === false))
-        {
-            list($field, $desc) = $this->getFirstPair();
-
-            $errorCode = '\EE\Error\ErrorCode::CARD_ERROR_INVALID_'.strtoupper($field);
-
-            if (defined($errorCode) === false)
-            {
-                throw new \InvalidArgumentException($field . ' error not defined');
-            }
-
-            $errorCode = constant($errorCode);
-
-            $this->error = new \EE\Error\Error($errorCode, $desc);
-        }
-        else if ($code !== 0)
-        {
-            if (defined('\EE\Error\ErrorCode::'.$code))
-            {
-                $error = new \EE\Error\Error($code, $message);
-
-                $this->setError($error);
-            }
-            else
-            {
-                throw new \InvalidArgumentException($code . ' is not defined');
-            }
-        }
+        $this->constructError($message, $code);
     }
 }
