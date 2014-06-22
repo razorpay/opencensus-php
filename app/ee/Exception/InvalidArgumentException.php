1dad63767d5f5ce7022643b5e829a483b6f47d58
Shashank Kumar <shashankkumar.me@gmail.com>
2014-06-22 15:38:20 +0530

    Adds code in InvalidArgumentException to catch all bad request error exceptions

--- app/ee/Exception/InvalidArgumentException.php
+++ app/ee/Exception/InvalidArgumentException.php
@@ -2,27 +2,82 @@
 
 namespace EE\Exception;
 
-use Illuminate\Support\MessageBag;
+use Illuminate\Support\messageBag;
 
-class InvalidArgumentException extends RazorpayException
+class BadRequestException extends RazorpayException
 {
-    protected $messagebag = null;
+    protected $messageBag = null;
 
-    public function __construct($message, $code = 0 , Exception $previous = null)
+    protected $messageArray = null;
+
+    protected $first = null;
+
+    public function __construct($message, $code = 0, \Exception $previous = null)
     {
-        if (is_array($message))
+        if ($message instanceof messageBag)
         {
-            $this->messagebag = $message;
+            $this->messageBag = $message;
 
-            $message = implode('\n ', $message);
+            $this->messageArray = $message->getMessages();
+
+            $message = implode('\n', $message->all());
+
+            $this->setFirstPair();
         }
-        else if ($message instanceof MessageBag)
+        else if (is_array($message))
         {
-            $this->messagebag = $message;
+            $this->messageArray = $message;
 
-            $message = implode('\n', join("\n",$validation->messages()->all()));
+            $message = $this->implodeMessagesArray();
+
+            $this->setFirstPair();
         }
 
-        parent::__construct('Invalid Arguments', $message, $code, $previous);
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function getMessageBag()
+    {
+        return $this->messageBag;
+    }
+
+    protected function setFirstPair()
+    {
+        $array = $this->messageArray;
+
+        if ($array !== null)
+        {
+            $pair = each($array);
+
+            $firstKey = $pair['key'];
+
+            $firstValue = (is_array($pair['value'])) ? $pair['value'][0] : $pair['value'];
+
+            $this->first = array($firstKey => $firstValue);
+        }
+    }
+
+    protected function implodeMessagesArray()
+    {
+        $messages = array();
+
+        foreach ($messageArray as $field => $values)
+        {
+            if (is_array($values))
+                array_push($messages, implode('\n', $messages));
+            else
+                array_push($messages, $values);
+        }
+
+        $message = implode('\n', $messages);
+
+        return $message;
+    }
+
+    public function getFirstPair()
+    {
+        $pair = each($this->first);
+
+        return array($pair['key'], $pair['value']);
     }
 }
