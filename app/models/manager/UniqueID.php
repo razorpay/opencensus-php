e2d1c3e57ed6a1bf61cd868ce0a1014dceb27b01
Shashank Kumar <shashankkumar.me@gmail.com>
2014-06-23 02:04:11 +0530

    Utilize UniqueId for uid generation and verification in future

---
+++ app/models/manager/UniqueID.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Models\Manager;
+
+class UniqueID
+{
+    const ID_LENGTH = \Constnats\Fields::ID_LENGTH;
+
+    const ID_KEY = \Constants\Field\Common::ID;
+
+    //const UNIQUE_ID_CHECK_REGEX = '/^[0-9a-f]{'.self::ID_LENGTH.'}$/i';
+
+    public static function verify($id)
+    {
+        if (is_array($id))
+        {
+            self::verifyArrayUid($id);
+        }
+        else if (is_string($id))
+        {
+            self::verifyStringUid($id);
+        }
+        else
+        {
+            throw new \InvalidArgumentException('invalid uid: ' . $id);
+        }
+    }
+
+    public static function verifyArrayUid($id, $key = self::ID_KEY)
+    {
+        Assert(is_array($id) === true);
+
+        if (! isset($id[$key]))
+        {
+            throw new \InvalidArgumentException('id key not set');
+        }
+
+        return self::verifyUid($id[$key]);
+    }
+
+    public static function verifyUid($id, $throw = false)
+    {
+        $uniqueIdCheckRegex = '/^[0-9a-f]{'.self::ID_LENGTH.'}$/i';
+        return preg_match($uniqueIdCheckRegex, $id);
+
+        if ($throw)
+        {
+            throw new \InvalidArgumentException($id . ' is not a valid id');
+        }
+    }
+}
