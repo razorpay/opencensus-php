cc96d6f2c46e86b79dbe8afd477e446b9b103762
Shashank Kumar <shashankkumar.me@gmail.com>
2014-06-25 16:24:42 +0530

    Adds dummy functions to mock gateway

--- app/gateway/mock/MockGateway.php
+++ app/gateway/mock/MockGateway.php
@@ -6,5 +6,23 @@ use Gateway\BaseGateway;
 
 class MockGateway extends BaseGateway
 {
-    ;
+    public function process(array $input)
+    {
+        ;
+    }
+
+    public function refund(array $input)
+    {
+        ;
+    }
+
+    public function capture(array $input)
+    {
+        ;
+    }
+
+    public function auth()
+    {
+        ;
+    }
 }
