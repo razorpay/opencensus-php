5c73bc1c069345c132ef161743d27df365aed572
Shashank Kumar <shashankkumar.me@gmail.com>
2014-06-26 15:09:19 +0530

    Adds TransactionAction class

---
+++ app/models/manager/TransactionAction.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Models\Manager;
+
+class TransactionAction
+{
+    const AUTH = 'auth';
+    const CALLBACK = 'callback';
+    const CAPTURE = 'capture';
+    const REFUND = 'refund';
+}
