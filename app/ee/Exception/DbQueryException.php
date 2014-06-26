3bda211d10fac1faca294801da173361eedfe50e
Shashank Kumar <shashankkumar.me@gmail.com>
2014-06-27 00:43:37 +0530

    Makes ServerError and DbQuery exceptions work properly

--- app/ee/Exception/DbQueryException.php
+++ app/ee/Exception/DbQueryException.php
@@ -2,66 +2,27 @@
 
 namespace EE\Exception;
 
+use EE\Error\Error;
+use EE\Error\ErrorCode;
+
 class DbQueryException extends ServerErrorException
 {
-	protected $fields = array(
-		'operation',
-		'table',
-		'model',
-		'attributes');
+    /**
+     * Aim should be to fill the value of these attributes.
+     * @var array
+     */
+    protected $fields = array(
+        'operation',
+        'model',
+        'attributes',
+        'query');
 
-	protected $data = array();
+    protected $data = array();
 
     public function __construct(array $data, Exception $previous = null)
     {
-    	$this->set($data);
+        $code = ErrorCode::DB_QUERY_FATAL_ERROR;
 
-		$message = $this->createMessage();
-
-    	$message = 'Failed '. $this->data['operation'].' operation on '.$table.' table with attributes '.
-    			   implode_assoc_array($attributes);
-
-        parent::__construct($message, Status::DB_ERROR, $previous);
-    }
-
-    public function set(array $array)
-    {
-    	foreach ($array as $key => $value)
-    	{
-    		if (in_array($key, $this->fields))
-    		{
-    			$this->data['key'] = $value;
-    		}
-    	}
-    }
-
-    public function createMessage()
-    {
-    	$msg = 'Failed db';
-    	$data = $this->data;
-
-    	if (isset($data['operation']))
-    	{
-    		$msg .= ' ' . $data['operation'];
-    	}
-
-    	$msg .= ' operation';
-
-    	if (isset($data['table']))
-    	{
-    		$msg .= ' on ' . $table . ' table';
-    	}
-
-    	if (isset($data['model']))
-    	{
-    		$msg .= ' on ' . $model . ' model';
-    	}
-
-    	if (isset($data['attributes']))
-    	{
-    		$msg .= ' with attributes ' . implode_assoc_array($data['attributes']);
-    	}
-
-    	return $msg;
+        parent::__construct($code, $data, $previous);
     }
 }
--- app/ee/Exception/ServerErrorException.php
+++ app/ee/Exception/ServerErrorException.php
@@ -2,22 +2,30 @@
 
 namespace EE\Exception;
 
+use EE\Error\Error;
+
 class ServerErrorException extends BaseException
 {
-	protected $publicMessage;
+    protected $data = null;
 
-    public function __construct($message, $code = 0 , Exception $previous = null)
+    protected $code = null;
+
+    public function __construct($code, $data = null, Exception $previous = null)
     {
-        parent::__construct('Server Error', $message, $code, $previous);
+        $this->data = $data;
+
+        $this->code = $code;
+
+        $this->previous = $previous;
     }
 
-    public function setPublicMessage($publicMessage)
+    public function generateJson()
     {
-    	$this->publicMessage = $publicMessage;
+        $error = new \EE\Error\Error($code, null, null, $data);
     }
 
-    public function getPublicMessage()
+    public function getData()
     {
-    	return $this->publicMessage;
+        return $this->data;
     }
 }
