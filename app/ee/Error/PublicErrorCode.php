ba6c4ce24a7794408e6c2957f526ae2b7eba9e8e
Shashank Kumar <shashankkumar.me@gmail.com>
2014-06-28 12:23:33 +0530

    Adds card error codes to public error code file as well

--- app/ee/Error/PublicErrorCode.php
+++ app/ee/Error/PublicErrorCode.php
@@ -10,6 +10,20 @@ class PublicErrorCode
 
     const SERVER_ERROR = 'server_error';
 
+    /**
+     * public card errors
+     */
+    const CARD_ERROR_INVALID_NAME                                   = 'CARD_ERROR_INVALID_NAME';
+    const CARD_ERROR_INVALID_EXPIRY_MONTH                           = 'CARD_ERROR_INVALID_EXPIRY_MONTH';
+    const CARD_ERROR_INVALID_EXPIRY_YEAR                            = 'CARD_ERROR_INVALID_EXPIRY_YEAR';
+    const CARD_ERROR_INVALID_CVV                                    = 'CARD_ERROR_INVALID_CVV';
+    const CARD_ERROR_INVALID_BRAND                                  = 'CARD_ERROR_INVALID_BRAND';
+    const CARD_ERROR_INVALID_AMOUNT                                 = 'CARD_ERROR_INVALID_AMOUNT';
+    const CARD_ERROR_INVALID_NUMBER                                 = 'CARD_ERROR_INVALID_NUMBER';
+    const CARD_ERROR_INVALID_EXPIRY_DATE                            = 'CARD_ERROR_INVALID_EXPIRY_DATE';
+    const CARD_ERROR_CARD_DECLINED                                  = 'CARD_ERROR_CARD_DECLINED';
+    const CARD_ERROR_INSUFFICIENT_BALANCE                           = 'CARD_ERROR_INSUFFICIENT_BALANCE';
+
     public static function verify($errorClass)
     {
         if (! is_string($class))
