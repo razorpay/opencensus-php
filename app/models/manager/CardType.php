e6a05b704dd6d12f96b01dd49ecda425102b5f90
Shashank Kumar <shashankkumar.me@gmail.com>
2014-07-06 22:27:03 +0530

    Adds card types for debit, credit, etc.

---
+++ app/models/manager/CardType.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Models\Manager;
+
+use EE\Error\ErrorCode;
+use EE\Exception;
+
+class CardType
+{
+   const CREDIT = 'credit';
+
+   const DEBIT = 'debit';
+
+   const PREPAID = 'prepaid';
+}
