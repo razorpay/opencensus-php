76e3e56c7e1e5f1d611992b57e5e74751e934d4b
Abhishek Das <das.abhshk@gmail.com>
2014-07-09 16:37:06 +0530

    Updates txn attributes in dashboard to use names from `Transaction\Entity`;

--- app/dashboard/Transaction.php
+++ app/dashboard/Transaction.php
@@ -2,14 +2,19 @@
 
 namespace Dashboard;
 
+use Models\Transaction\Entity as TransactionEntity;
+
 class Transaction extends Dashboard
 {
     protected static $resource = 'transactions';
 
+    //Any changes to this array
+    //has to be reflected in dashboard also.
+    //Dashboard: Manager\Transaction
     protected static $fields = array(
-        'merchant_id',
-        'amount',
-        'status',
+        TransactionEntity::MERCHANT_ID,
+        TransactionEntity::AMOUNT,
+        TransactionEntity::STATUS,
         'updated_at',
         'created_at'
     );
