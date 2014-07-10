a57b0ba377aa9e2f51a534a45c7212cdee8708f6
Shashank Kumar <shashankkumar.me@gmail.com>
2014-07-10 17:30:53 +0530

    Adds comment explaining url in sample.api.js

--- app/config/api.sample.php
+++ app/config/api.sample.php
@@ -2,6 +2,11 @@
 
 return array(
 
+    /**
+     * This url is used when sending requests directly to rzp backend
+     * without using rzp-php api.
+     * Mostly used for endpoints not exposed by rzp-php
+     */
     'url'       =>  'https://api.razorpay.com/',
     'ip'        =>  '10.0.0.140',
     'auth_pass' =>  'DASHBOARD_AUTH_PASS',
