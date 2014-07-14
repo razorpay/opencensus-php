0925b58c650fb5639d0821f2daa17a325d9c7742
Shashank Kumar <shashankkumar.me@gmail.com>
2014-07-14 16:21:54 +0530

    Adds data for merchant balance in db seeder

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use Constants\Table;
+
 class DatabaseSeeder extends Seeder {
 
     /**
@@ -20,7 +22,7 @@ class DatabaseSeeder extends Seeder {
     {
         DB::transaction(function()
         {
-            DB::table('merchants')->insert(
+            DB::table(Table::MERCHANT)->insert(
                 array(
                     'id'            =>  1,
                     'created_at'    =>  time(),
@@ -28,7 +30,7 @@ class DatabaseSeeder extends Seeder {
                     )
                 );
 
-            DB::table('merchants')->insert(
+            DB::table(Table::MERCHANT)->insert(
                 array(
                     'id'            =>  2,
                     'created_at'    =>  time(),
@@ -36,7 +38,23 @@ class DatabaseSeeder extends Seeder {
                     )
                 );
 
-            DB::table('cards')->insert(
+            DB::table(Table::BALANCE)->insert(
+                array(
+                    'id'          =>  1,
+                    'created_at'    =>  time(),
+                    'updated_at'    =>  time(),
+                    )
+                );
+
+            DB::table(Table::BALANCE)->insert(
+                array(
+                    'id'          =>  2,
+                    'created_at'    =>  time(),
+                    'updated_at'    =>  time(),
+                    )
+                );
+
+            DB::table(Table::CARD)->insert(
                 array(
                     'id'            =>  '174bdd3e456c8f6f174bdd3e',
                     'name'          =>  'shk',
@@ -45,10 +63,12 @@ class DatabaseSeeder extends Seeder {
                     'network'       =>  'visa',
                     'country'       =>  'IN',
                     'last4'         =>  '7890',
+                    'created_at'    =>  time(),
+                    'updated_at'    =>  time(),
                     )
                 );
 
-            DB::table('cards')->insert(
+            DB::table(Table::CARD)->insert(
                 array(
                     'id'            =>  '274bdd3e456c8f6f174bdd3f',
                     'name'          =>  'shk',
@@ -57,18 +77,22 @@ class DatabaseSeeder extends Seeder {
                     'network'       =>  'visa',
                     'country'       =>  'IN',
                     'last4'         =>  '7891',
+                    'created_at'    =>  time(),
+                    'updated_at'    =>  time(),
                     )
                 );
 
-            DB::table('tokens')->insert(
+            DB::table(Table::TOKEN)->insert(
                 array(
                     'id'            =>  '174bdd3e456c8f6f174bdd3e',
                     'card_id'       =>  '274bdd3e456c8f6f174bdd3f',
                     'merchant_id'   =>  1,
+                    'created_at'    =>  time(),
+                    'updated_at'    =>  time(),
                     )
                 );
 
-            DB::table('keys')->insert(
+            DB::table(Table::KEY)->insert(
                 array(
                     'id'            =>  'd9c6bf091a1a64cb5678d8c1',
                     'merchant_id'   =>  1,
@@ -80,7 +104,7 @@ class DatabaseSeeder extends Seeder {
                     )
                 );
 
-            DB::table('keys')->insert(
+            DB::table(Table::KEY)->insert(
                 array(
                     'id'            =>  'd9c6bf091a1a64cb5678d8c2',
                     'merchant_id'   =>  1,
@@ -92,7 +116,7 @@ class DatabaseSeeder extends Seeder {
                     )
                 );
 
-            DB::table('keys')->insert(
+            DB::table(Table::KEY)->insert(
                 array(
                     'id'            =>  'd9c6bf091a1a64cb5678d8c3',
                     'merchant_id'   =>  2,
@@ -104,7 +128,7 @@ class DatabaseSeeder extends Seeder {
                     )
                 );
 
-            DB::table('keys')->insert(
+            DB::table(Table::KEY)->insert(
                 array(
                     'id'            =>  'd9c6bf091a1a64cb5678d8c4',
                     'merchant_id'   =>  2,
