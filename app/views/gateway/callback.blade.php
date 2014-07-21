da385647387be6065cca325345b77fe2c913ecc7
Shashank Kumar <shashankkumar.me@gmail.com>
2014-07-22 00:14:00 +0530

    Adds spaces and makes comments multi-line

--- app/views/gateway/callback.blade.php
+++ app/views/gateway/callback.blade.php
@@ -4,40 +4,60 @@
 </head>
 <body>
 <script>
+//
 // everything is wrapped in the XD function to reduce namespace collisions
-var XD = function(){
+//
+var XD = function() {
 
     var interval_id,
-    last_hash,
-    cache_bust = 1,
-    attached_callback,
-    window = this;
+        last_hash,
+        cache_bust = 1,
+        attached_callback,
+        window = this;
 
     return {
         postMessage : function(message, target_url, target) {
             if (!target_url) {
                 return;
             }
-            target = target || parent;  // default to parent
+            //
+            // default to parent
+            //
+            target = target || parent;
+
             if (window['postMessage']) {
-                // the browser supports window.postMessage, so call it with a targetOrigin
-                // set appropriately, based on the target_url parameter.
+                //
+                // the browser supports window.postMessage, so call it with a
+                // targetOrigin set appropriately, based on the target_url
+                // parameter.
+                //
                 target['postMessage'](message, target_url.replace( /([^:]+:\/\/[^\/]+).*/, '$1'));
             } else if (target_url) {
-                // the browser does not support window.postMessage, so use the window.location.hash fragment hack
-                target.location = target_url.replace(/#.*$/, '') + '#' + (+new Date) + (cache_bust++) + '&' + message;
+                //
+                // the browser does not support window.postMessage,
+                // so use the window.location.hash fragment hack
+                //
+                target.location = target_url.replace(/#.*$/, '') + '#' +
+                                  (+new Date) + (cache_bust++) + '&' + message;
             }
         },
         receiveMessage : function(callback, source_origin) {
+            //
             // browser supports window.postMessage
+            //
             if (window['postMessage']) {
-                // bind the callback to the actual event associated with window.postMessage
+                //
+                // bind the callback to the actual event associated with
+                // window.postMessage
+                //
                 if (callback) {
                     attached_callback = function(e) {
                         if ((typeof source_origin === 'string' && e.origin !== source_origin)
-                        || (Object.prototype.toString.call(source_origin) === "[object Function]" && source_origin(e.origin) === !1)) {
+                        || ((Object.prototype.toString.call(source_origin) === "[object Function]") &&
+                            (source_origin(e.origin) === !1))) {
                              return !1;
                          }
+
                          callback(e);
                      };
                  }
@@ -47,9 +67,13 @@ var XD = function(){
                      window[callback ? 'attachEvent' : 'detachEvent']('onmessage', attached_callback);
                  }
              } else {
-                 // a polling loop is started & callback is called whenever the location.hash changes
+                 //
+                 // a polling loop is started & callback is called whenever
+                 // the location.hash changes
+                 //
                  interval_id && clearInterval(interval_id);
                  interval_id = null;
+
                  if (callback) {
                      interval_id = setInterval(function() {
                          var hash = document.location.hash,
