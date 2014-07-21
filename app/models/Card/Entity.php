4fbfa4d4361e2d845124fa37e8e954e15d84c84c
Shashank Kumar <shashankkumar.me@gmail.com>
2014-07-22 00:40:46 +0530

    Improves card entity fields alignment

--- app/models/Card/Entity.php
+++ app/models/Card/Entity.php
@@ -8,35 +8,21 @@ use Models\Base;
 
 class Entity extends Base\UniqueIdEntity
 {
-    const ID = 'id';
-
-    const NAME = 'name';
-
-    const EXPIRY_MONTH = 'expiry_month';
-
-    const EXPIRY_YEAR = 'expiry_year';
-
-    const LAST4 = 'last4';
-
-    const NETWORK = 'network';
-
-    const TYPE = 'type';
-
-    const BANK = 'bank';
-
-    const COUNTRY = 'country';
-
-    const ADDRESS_LINE1 = 'address_line1';
-
-    const ADDRESS_LINE2 = 'address_line2';
-
-    const ADDRESS_CITY = 'address_city';
-
-    const ADDRESS_STATE = 'address_state';
-
-    const ADDRESS_ZIP = 'address_zip';
-
-    const ADDRESS_COUNTRY = 'address_country';
+    const ID                = 'id';
+    const NAME              = 'name';
+    const EXPIRY_MONTH      = 'expiry_month';
+    const EXPIRY_YEAR       = 'expiry_year';
+    const LAST4             = 'last4';
+    const NETWORK           = 'network';
+    const TYPE              = 'type';
+    const BANK              = 'bank';
+    const COUNTRY           = 'country';
+    const ADDRESS_LINE1     = 'address_line1';
+    const ADDRESS_LINE2     = 'address_line2';
+    const ADDRESS_CITY      = 'address_city';
+    const ADDRESS_STATE     = 'address_state';
+    const ADDRESS_ZIP       = 'address_zip';
+    const ADDRESS_COUNTRY   = 'address_country';
 
     const COUNTRY_LENGTH = 2;
 
