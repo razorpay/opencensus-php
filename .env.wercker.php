f152b92d7dc8b0d757ec4e950796f2c028ceeb02
Shashank Kumar <shashankkumar.me@gmail.com>
2014-07-24 12:41:27 +0530

    Environment file specific to wercker for build and testing purposes

---
+++ .env.wercker.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+    'DB_MYSQL_DATABASE'         => $_ENV['WERCKER_MYSQL_HOST'],
+    'DB_MYSQL_USERNAME'         => 'user',
+    'DB_MYSQL_PASSWORD'         => 'password',
+
+    'APP_DASHBOARD_SECRET'      => 'secret',
+
+    'HDFC_ID'                   => 'hdfc_id',
+    'HDFC_PASSWORD'             => 'hdfc_password',
+);
