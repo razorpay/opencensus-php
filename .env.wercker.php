2ff455d943db5c11093aa8ec454b84df369cd26d
Shashank Kumar <shashankkumar.me@gmail.com>
2014-07-24 22:57:13 +0530

    Fixes wercker env var bugs

--- .env.wercker.php
+++ .env.wercker.php
@@ -1,9 +1,9 @@
 <?php
 
 return array(
-    'DB_MYSQL_PORT'             => getenv('WERCKER_MYSQL_HOST'),
-    'DB_MYSQL_HOST'             => getenv('WERCKER_MYSQL_PORT'),
-    'DB_MYSQL_DATABASE'         => getenv('WERCKER_MYSQL_HOST'),
+    'DB_MYSQL_PORT'             => getenv('WERCKER_MYSQL_PORT'),
+    'DB_MYSQL_HOST'             => getenv('WERCKER_MYSQL_HOST'),
+    'DB_MYSQL_DATABASE'         => getenv('WERCKER_MYSQL_DATABASE'),
     'DB_MYSQL_USERNAME'         => getenv('WERCKER_MYSQL_USERNAME'),
     'DB_MYSQL_PASSWORD'         => getenv('WERCKER_MYSQL_PASSWORD'),
 
