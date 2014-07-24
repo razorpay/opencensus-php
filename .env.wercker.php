4e2dbc831b8fb29f37190fc01a3e8f88d2b93c7c
Harshil Mathur <harshilmathur@gmail.com>
2014-07-24 21:53:23 +0530

    Using getenv instead of $_ENV for getting wercker system variables

--- .env.wercker.php
+++ .env.wercker.php
@@ -1,11 +1,11 @@
 <?php
 
 return array(
-    'DB_MYSQL_PORT'             => $_ENV['WERCKER_MYSQL_HOST'],
-    'DB_MYSQL_HOST'             => $_ENV['WERCKER_MYSQL_PORT'],
-    'DB_MYSQL_DATABASE'         => $_ENV['WERCKER_MYSQL_HOST'],
-    'DB_MYSQL_USERNAME'         => $_ENV['WERCKER_MYSQL_USERNAME'],
-    'DB_MYSQL_PASSWORD'         => $_ENV['WERCKER_MYSQL_PASSWORD'],
+    'DB_MYSQL_PORT'             => getenv('WERCKER_MYSQL_HOST'),
+    'DB_MYSQL_HOST'             => getenv('WERCKER_MYSQL_PORT'),
+    'DB_MYSQL_DATABASE'         => getenv('WERCKER_MYSQL_HOST'),
+    'DB_MYSQL_USERNAME'         => getenv('WERCKER_MYSQL_USERNAME'),
+    'DB_MYSQL_PASSWORD'         => getenv('WERCKER_MYSQL_PASSWORD'),
 
     'APP_DASHBOARD_URL'     	=> 'http://dashboard.razorpay.com/',
     'APP_DASHBOARD_SECRET'      => 'secret',
