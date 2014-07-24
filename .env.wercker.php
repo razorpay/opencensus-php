cfab691daf471cb8b1e71e79f4d239fa69e2e719
Shashank Kumar <shashankkumar.me@gmail.com>
2014-07-24 12:55:22 +0530

    Adds vars to wercker env file

--- .env.wercker.php
+++ .env.wercker.php
@@ -1,9 +1,11 @@
 <?php
 
 return array(
+    'DB_MYSQL_PORT'             => $_ENV['WERCKER_MYSQL_HOST'],
+    'DB_MYSQL_HOST'             => $_ENV['WERCKER_MYSQL_PORT'],
     'DB_MYSQL_DATABASE'         => $_ENV['WERCKER_MYSQL_HOST'],
-    'DB_MYSQL_USERNAME'         => 'user',
-    'DB_MYSQL_PASSWORD'         => 'password',
+    'DB_MYSQL_USERNAME'         => $_ENV['WERCKER_MYSQL_USERNAME'],
+    'DB_MYSQL_PASSWORD'         => $_ENV['WERCKER_MYSQL_PASSWORD'],
 
     'APP_DASHBOARD_SECRET'      => 'secret',
 
