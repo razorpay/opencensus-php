184050738781f330ac320ca9a88f5ed324f7bb56
Shashank Kumar <shashankkumar.me@gmail.com>
2014-07-24 12:10:50 +0530

    Adds sample envrionment vars file

---
+++ .env.sample.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+    'DB_MYSQL_PORT'             => '3306',
+    'DB_MYSQL_HOST'             => 'localhost',
+    'DB_MYSQL_DATABASE'         => 'db',
+    'DB_MYSQL_USERNAME'         => 'user',
+    'DB_MYSQL_PASSWORD'         => 'password',
+
+    'APP_DASHBOARD_SECRET'      => 'secret',
+
+    'HDFC_ID'                   => 'hdfc_id',
+    'HDFC_PASSWORD'             => 'hdfc_password',
+);
