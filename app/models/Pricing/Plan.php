e894195a47d8e9e81f7b24718f7bfcb9a18ec051
Shashank Kumar <shashankkumar.me@gmail.com>
2014-07-27 17:49:37 +0530

    Adds pricing plan class

---
+++ app/models/Pricing/Plan.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Models\Pricing;
+
+class Plan extends Illuminate\Database\Eloquent\Collection
+{
+    const ID = 'id';
+    const COUNT = 'count';
+    const RULES = 'rules';
+
+    /**
+     * Get the collection of items as a plain array.
+     *
+     * @return array
+     */
+    public function toArrayPublic()
+    {
+        $plan = array();
+        $rules = array();
+
+        foreach ($this->items as $item)
+        {
+            array_push($rules, $item->toArray());
+        }
+
+        $this->setPlanAttributes(
+            $plan,
+            $this->items[0],
+            $rules,
+            count($this->items));
+
+        return $plan;
+    }
+
+    public function toArrayMultiplePlans()
+    {
+        $plans = array();
+        $plans[self::COUNT] = 0;
+
+        $plan = array();
+        $this->setPlanAttributes($plan, $this->items[0]);
+        $rules = & $plan[self::RULES];
+
+        foreach ($this->items as $item)
+        {
+            if ($plan[self::ID] === $item->getPlanId())
+            {
+                $plan[self::COUNT]++;
+                array_push($rules, $item->toArray());
+            }
+            else
+            {
+                array_push($plans, $plan);
+                $plans[self::COUNT]++;
+
+                $plan = array();
+                $this->setPlanAttributes($plan, $item);
+                $rules = & $plan[self::RULES];
+
+                array_push($rules, $item->toArray());
+            }
+        }
+
+        return $plans;
+    }
+
+    protected function setPlanAttributes(& $plan, $item, $rules = array(), $count = 0)
+    {
+        $plan[self::ID] = $item->getPlanId();
+        $plan[self::ENTITY] = 'plan';
+        $plan[self::RULES] = $rules;
+        $plan[self::COUNT] = $count;
+    }
+}
