3832fdfa9e7e195725455077e24a24c802afeaed
Shashank Kumar <shashankkumar.me@gmail.com>
2014-07-30 17:24:17 +0530

    Shifts to using Spine DbQueryExceptionTrait in DbQueryException class

--- app/ee/Exception/DbQueryException.php
+++ app/ee/Exception/DbQueryException.php
@@ -5,18 +5,11 @@ namespace EE\Exception;
 use DB;
 use EE\Error\Error;
 use EE\Error\ErrorCode;
+use Razorpay\Spine\Exception\DbQueryExceptionTrait;
 
 class DbQueryException extends ServerErrorException
 {
-    /**
-     * Aim should be to fill the value of these attributes.
-     * @var array
-     */
-    protected $fields = array(
-        'operation',
-        'model',
-        'attributes',
-        'query');
+    use DbQueryExceptionTrait;
 
     public function __construct(array $data, Exception $previous = null)
     {
@@ -25,37 +18,5 @@ class DbQueryException extends ServerErrorException
         $message = $this->constructMessage($data);
 
         parent::__construct($message, $code, $data, $previous);
-    }
-
-    protected function constructMessage(array $data)
-    {
-        $message = 'DB query failed to execute successfully';
-
-        foreach ($this->fields as $field)
-        {
-            if (isset($data[$field]))
-            {
-                $message .= PHP_EOL . ucfirst($field) . ': ' . $data[$field];
-            }
-        }
-
-        $lastQuery = $this->getLastQuery();
-
-        $message .= PHP_EOL . 'Last query: ' . $lastQuery;
-
-        return $message;
-    }
-
-    protected function getLastQuery()
-    {
-        $queries = DB::getQueryLog();
-
-        $lastQuery = end($queries);
-
-        ob_start();
-        print_r($lastQuery);
-        $query = ob_get_clean();
-
-        return $query;
     }
 }
