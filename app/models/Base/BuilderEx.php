1cab041eb6fc13b42a58c876650cacd066711e0b
Shashank Kumar <shashankkumar.me@gmail.com>
2014-07-30 17:20:50 +0530

    Inherits BuilderEx, EloquentEx, Entity from corresponding Spine classes.

--- app/models/Base/BuilderEx.php
+++ app/models/Base/BuilderEx.php
@@ -4,34 +4,9 @@ namespace Models\Base;
 
 use EE\Error\ErrorCode;
 use EE\Exception;
-use Illuminate\Database\Eloquent\Builder;
 
-class BuilderEx extends Builder
+class BuilderEx extends \Razorpay\Spine\BuilderEx
 {
-    public static function createOrFail(array $attributes)
-    {
-        if ( ! (NULL === $model = static::create($attributes))) return $model;
-
-        $e = array(
-                'model' => get_called_class(),
-                'attributes' => $attributes,
-                'operation' => 'create');
-
-        $this->throwException($e);
-    }
-
-    public function findOrFail($id, $columns = array('*'))
-    {
-        if ( ! is_null($model = $this->find($id, $columns))) return $model;
-
-        $e = array(
-                'model' => get_class($this->model),
-                'attributes' => $id,
-                'operation' => 'find');
-
-        $this->throwException($e);
-    }
-
     public function findOrFailPublic($id, $columns = array('*'))
     {
         if ( ! is_null($model = $this->find($id, $columns))) return $model;
@@ -44,45 +19,5 @@ class BuilderEx extends Builder
         throw new Exception\BadRequestException(
             null,
             ErrorCode::BAD_REQUEST_INVALID_ID);
-    }
-
-    /**
-     * Save the model to the database.
-     *
-     * @param  array  $options
-     */
-    public function saveOrFail(array $options = array())
-    {
-        $saved = parent::save($options);
-
-        if ($saved === true)
-            return;
-
-        $e = array(
-                'model' => get_called_class(),
-                'attributes' => $this->attributes,
-                'operation' => 'save');
-
-        $this->throwException($e);
-    }
-
-    public function pushOrFail()
-    {
-        $pushed = parent::push();
-
-        if ($pushed === true)
-            return;
-
-        $e = array(
-                'model' => get_called_class(),
-                'attributes' => $this->attributes,
-                'operation' => 'push');
-
-        $this->throwException($e);
-    }
-
-    protected function throwException($e)
-    {
-        throw new Exception\DbQueryException($e);
     }
 }
--- app/models/Base/EloquentEx.php
+++ app/models/Base/EloquentEx.php
@@ -4,40 +4,8 @@ namespace Models\Base;
 
 use EE\Exception;
 
-class EloquentEx extends \Eloquent
+class EloquentEx extends \Razorpay\Spine\Entity
 {
-    public static function createOrFail(array $attributes)
-    {
-        if ( ! (NULL === $model = static::create($attributes))) return $model;
-
-        (new static)->throwException('create', $attributes);
-    }
-
-    /**
-     * Save the model to the database.
-     *
-     * @param  array  $options
-     */
-    public function saveOrFail(array $options = array())
-    {
-        $saved = parent::save($options);
-
-        if ($saved === true)
-            return;
-
-        $this->throwException('save');
-    }
-
-    public function pushOrFail()
-    {
-        $pushed = parent::push();
-
-        if ($pushed === true)
-            return;
-
-        $this->throwException('push');
-    }
-
     /**
      * Create a new Eloquent query builder for the model.
      *
@@ -54,18 +22,5 @@ class EloquentEx extends \Eloquent
         $e = $this->getExceptionDataArray($operation, $attributes);
 
         throw new Exception\DbQueryException($e);
-    }
-
-    protected function getExceptionDataArray($operation, $attributes = null)
-    {
-        if ($attributes === null)
-            $attributes = $this->attributes;
-
-        $e = array(
-                'model' => get_called_class(),
-                'attributes' => $attributes,
-                'operation' => $operation);
-
-        return $e;
     }
 }
