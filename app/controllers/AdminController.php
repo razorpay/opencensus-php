9350e2ca228360f7ae4b5ae2fdc442c69ef2584c
Harshil Mathur <harshilmathur@gmail.com>
2014-08-02 23:08:19 +0530

    Added admin controller, manger and service

---
+++ app/controllers/AdminController.php
@@ -0,0 +1,277 @@
+<?php
+
+use Models\Service;
+
+class AdminController extends BaseController
+{
+
+    /*
+    |--------------------------------------------------------------------------
+    | Admin Controller
+    |--------------------------------------------------------------------------
+    |
+    | Defines the actions for an Admin on the dashboard
+    |
+    */
+    // private $_laravelDuo;
+
+    // function __construct(LaravelDuo\LaravelDuo $laravelDuo)
+    // {
+    //     $this->_laravelDuo = $laravelDuo;
+    // }
+
+    /**
+     * Stage One - The Login form
+     * @return  Login View
+     */
+    public function getLogin()
+    {
+        return View::make('admin.getLogin');
+    }
+
+    /**
+     * Stage Two - The Duo Auth form
+     * @return Duo Login View or Redirect on error
+     */
+    public function postLogin()
+    {
+        $user = array(
+            'username' => Input::get('username'),
+            'password' => Input::get('password')
+        );
+
+        // /**
+        //  * Validate the user details, but don't log the user in
+        //  */
+        // if (Auth::admin()->validate($user))
+        // {
+        //     $username = Input::get('username');
+
+        //     $duoinfo = array(
+        //         'HOST' => $this->_laravelDuo->get_host(),
+        //         'POST' => URL::to('/') . '/admin/duologin',
+        //         'USER' => $username,
+        //         'SIG'  => $this->_laravelDuo->signRequest(
+        //                             $this->_laravelDuo->get_ikey(),
+        //                             $this->_laravelDuo->get_skey(),
+        //                             $this->_laravelDuo->get_akey(),
+        //                             $username)
+        //     );
+
+        //     return View::make('admin.duologin')
+        //                 ->with(compact('duoinfo'))
+        //                 ->with('title', 'Duo Authentication');
+        // }
+        // else
+        // {
+        //     return View::make('admin.getLogin')
+        //                 ->with('error', 'Invalid Username/Password')
+        //                 ->with('title', 'Login');
+        // }
+        
+        if(Auth::admin()->attempt($user))
+        {
+            return Redirect::to('/admin');
+        }
+        else
+        {
+            return Redirect::action('AdminController@getLogin')
+                                ->with('error', array("Invalid Username/password"));
+        }
+    }
+
+    /**
+     * Stage Three - After Duo Auth Form
+     * @return Redirect to home
+     */
+    // public function postDuologin()
+    // {
+    //     /**
+    //      * Sent back from Duo
+    //      */
+    //     $response = Input::get('sig_response');
+
+    //     $U = $this->_laravelDuo->verifyResponse(
+    //                     $this->_laravelDuo->get_ikey(),
+    //                     $this->_laravelDuo->get_skey(),
+    //                     $this->_laravelDuo->get_akey(),
+    //                     $response
+    //     );
+
+    //     /**
+    //      * Duo response returns USER field from Stage Two
+    //      */
+    //     if ($U){
+
+    //         /**
+    //          * Get the id of the authenticated user from their email address
+    //          */
+    //         $id = Admin::getIdFromUsername($U);
+
+    //         /**
+    //          * Log the user in by their ID
+    //          */
+    //         Auth::admin()->loginUsingId($id);
+
+    //         /**
+    //          * Check Auth worked, redirect to homepage if so
+    //          */
+    //         if (Auth::admin()->check())
+    //         {
+    //             return Redirect::to('/admin');
+    //         }
+    //     }
+
+    //     /**
+    //      * Otherwise, Auth failed, redirect to homepage with message
+    //      */
+    //     return View::make('admin.getLogin')
+    //                 ->with('error', 'Authentication Failed!')
+    //                 ->with('title', 'Login');
+
+    // }
+
+
+
+    public function getLogout()
+    {
+        Auth::admin()->logout();
+
+        return Redirect::action('AdminController@getLogin');
+    }
+
+    public function getPassword()
+    {
+        return View::make('admin.getPassword');
+    }
+
+    public function postPassword()
+    {
+        $input = Input::all();
+
+        list($error, $data) = Service\Admin::getInstance()->changePassword($input, Auth::admin()->get());
+
+        $view = Redirect::action('AdminController@getPassword');
+
+        if (empty($error))
+        {
+            $view->with('error', array('Passsword changed successfully!'));
+        }
+        else
+        {
+            $view->with('data', $data)->with('error', $error);
+        }
+
+        return $view;
+    }
+
+    public function getIndex()
+    {
+        return View::make('admin.getIndex');
+    }
+
+    public function getMerchants()
+    {
+        $merchants = Service\Admin::getInstance()->listMerchants();
+        
+        return View::make('admin.getMerchants')
+                   ->with('data', $merchants);
+    }
+
+    public function getMerchantLogin($id)
+    {
+        $merchant = Service\Merchant::getInstance()->fetch($id);
+
+        Auth::merchant()->loginUsingId($merchant['id']);
+
+        return Redirect::to('/');
+    }
+
+    public function getMerchantStatus($id)
+    {
+        $details = Service\Admin::getInstance()->fetchMerchantStatus($id);
+
+        return View::make('admin.getMerchantStatus')
+                   ->with('details', $details);
+    }
+
+    public function postStepsFinished($id)
+    {
+        $input = Input::all();
+
+        $error = Service\Admin::getInstance()->changeStepsFinished($id, $input);
+
+        $response= Redirect::to('/admin/merchant/'.$id);
+
+        if(empty($error))
+        {
+            return $response->with('success', true);
+        }
+        else
+        {
+            return $response->with('error', $error);
+        }
+    }
+    public function getMerchantDetails($id)
+    {
+        $details = Service\Admin::getInstance()->fetchMerchantDetails($id);
+
+        return View::make('admin.getMerchantDetails')
+                   ->with('data', json_encode($details));
+    }
+
+    public function postValidate($id, $step)
+    {
+        $error = Service\MerchantDetails::getInstance()->validateStep($id, $step);  
+        
+        if (empty($error))
+        {
+            return Response::json(array('success' => true));
+        }
+        else
+        {
+            return Response::json(array('success' => false, 'status' => $error));
+        }
+    }
+
+    public function getAdmins()
+    {
+        $admins = Service\Admin::getInstance()->getAdmins();
+
+        return View::make('admin.getAdmins')
+                   ->with('admins', $admins);
+    }
+
+    public function getDeleteAdmin($id)
+    {
+        $response = Service\Admin::getInstance()->deleteAdmin($id);
+
+        return Redirect::action('AdminController@getAdmins')
+                        ->with('success', $response);
+    }
+
+    public function getAddAdmin()
+    {
+        return View::make('admin.getAddAdmin');
+    }
+
+    public function postAddAdmin()
+    {
+        $input = Input::all();
+
+        list($error, $data) = Service\Admin::getInstance()->add($input, Auth::admin()->get());
+
+        $view = Redirect::action('AdminController@getAddAdmin');
+
+        if (empty($error))
+        {
+            $view->with('error', array('Admin added successfully!'));
+        }
+        else
+        {
+            $view->with('data', $data)->with('error', $error);
+        }
+
+        return $view;
+    }
+}
---
+++ app/models/service/admin.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace Models\Service;
+
+use Models\DAL;
+use Models\Manager;
+
+class Admin extends Service
+{
+
+    /**
+     * Changes password oflogged in admin
+     *
+     * @param  $input input array
+     * @param  $admin DAL\Admin Object
+     * @return  Status
+     */
+    public function changePassword($input, DAL\Admin $admin)
+    {
+        list($error, $data) = Manager\Admin::createValidate($input, 'password')->getData();
+
+        if(empty($error) === false)
+        {
+            return [$error, $data];
+        }
+
+        $old_password = $data['old_password'];
+
+        unset($data['old_password']);
+
+        if (\Hash::check($old_password, $admin->password) == false)
+        {
+            $error = array("Invalid Password");
+
+            return [$error, $data];
+        }
+
+        $admin = $admin->update($data);
+
+        return [$error, $data];
+    }
+
+    public function listMerchants()
+    {
+        return DAL\Merchant::get()->toArray();
+    }
+
+    public function getAdmins()
+    {
+        return DAL\Admin::get()->toArray();
+    }
+
+    public function deleteAdmin($id)
+    {
+        if($id === \Auth::admin()->id()) return false;
+        try
+        {
+            $admin = DAL\Admin::findorfail($id);
+            $admin->delete();
+        }
+        catch(\Exception $e)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    /* Adds a new admin
+     * @param $data input array
+     * @return Status
+     */
+    public function add($input, DAL\Admin $admin)
+    {
+        list($error, $data) = Manager\Admin::createValidate($input, 'register')->getData();
+
+        if (empty($error))
+        {   
+            $admin = DAL\Admin::createOrFail($data);
+
+            $data = $admin->toArray();
+        }
+
+        return [$error, $data];
+    }
+
+    public function fetchMerchantDetails($id)
+    {
+        $merchant_details =  DAL\MerchantDetails::findorfail($id);
+
+        $response = DAL\MerchantDetails::filterDetails($merchant_details);
+
+        foreach($response['files'] as $key => &$file)
+        {
+            $extension_position = strrpos($file, '.', -1);
+            $extension  = substr($file, $extension_position + 1);
+
+            $s3 =  \AWS::get('s3');
+
+            try
+            {
+                $result = $s3->getObjectUrl(
+                            $_ENV['AWS_ACTIVATION_BUCKET'],
+                            $id.'/'.$key.'.'.$extension,
+                            '+10 minutes'
+                );
+
+                $file = $result;
+            }
+            catch(\Exception $e)
+            {
+                $file = 'ERROR';
+            }
+        }
+        
+        return $response;
+    }
+
+    public function fetchMerchantStatus($id)
+    {   
+        $merchant = DAL\Merchant::with('MerchantDetails')->findorfail($id);
+        
+        $merchant_details =  $merchant->MerchantDetails;
+
+        $response = array(
+            'steps_finished'    => json_decode($merchant_details['steps_finished'], true),
+            'locked'            => $merchant_details['locked'],
+            'merchant'          => $merchant->toArray()
+        );
+        
+        return $response;
+    }
+
+    public function changeStepsFinished($input, $details)
+    {   
+        $merchant_details = DAL\MerchantDetails::findorfail($id);
+        
+        if(count(array_intersect($merchant_details['steps_finished'], $all)) == count($search_this));
+    }
+}
