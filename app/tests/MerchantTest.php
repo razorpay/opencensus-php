1b5d0240bb21dfc66613d4deac049a3bdc9482fd
Harshil Mathur <harshilmathur@gmail.com>
2014-08-04 03:15:55 +0530

    Added tests for display of all panels

--- app/tests/MerchantTest.php
+++ app/tests/MerchantTest.php
@@ -54,7 +54,7 @@ class MerchantTest extends IntegrationTestCase
         $this->browser
             ->open(URL::action('MerchantController@getIndex'))    // Visits the 'stuff' index
             ->click(l::linkContaining('Sign Out'))                 // Click in the button
-            ->waitForPageToLoad(1000);                      // Wait for page to load
+            ->waitForPageToLoad(2000);                      // Wait for page to load
         
         // Asserts if at the end the user is at the stuff index again
         $this->assertEquals(URL::action('MerchantController@getLogin'),$this->browser->getLocation());
@@ -67,17 +67,7 @@ class MerchantTest extends IntegrationTestCase
             ->type(l::IdOrName('email'), $this->merchant->email)   // Fill name
             ->type(l::IdOrName('password'), '123456')   // Fill slug
             ->click(l::css('#form-button'))                 // Click in the button
-            ->waitForPageToLoad(1000);                      // Wait for page to load
-
-        // Asserts if at the end the user is at the stuff index again
-        $this->assertEquals(URL::action('MerchantController@getIndex').'/#!/',$this->browser->getLocation());
-    }
-
-
-    public function testIndex()
-    {
-         $this->browser
-            ->open(URL::action('MerchantController@getIndex'));
+            ->waitForPageToLoad(2000);                      // Wait for page to load
 
         // Asserts if at the end the user is at the stuff index again
         $this->assertEquals(URL::action('MerchantController@getIndex').'/#!/',$this->browser->getLocation());
@@ -88,6 +78,59 @@ class MerchantTest extends IntegrationTestCase
 
         $this->assertBodyHasText("Successful Transactions");
     }
+
+    public function testTransactionsPanel()
+    {
+         $this->browser
+            ->open(URL::action('MerchantController@getIndex'))
+            ->click(l::linkContaining('Transactions'))   
+            ->waitForCondition("selenium.browserbot.getCurrentWindow().$('#transaction-count-line-chart-full > div').length > 0", 2000);;  
+
+        $this->assertEquals(URL::action('MerchantController@getIndex').'/#!/transactions',$this->browser->getLocation());
+
+        $this->assertBodyHasText("Successful Transactions");
+
+        $this->assertBodyHasText("Recent Transactions");
+
+    }
+
+    public function testRefundsPanel()
+    {
+         $this->browser
+            ->open(URL::action('MerchantController@getIndex'))
+            ->click(l::linkContaining('Refunds'))   
+            ->waitForCondition("selenium.browserbot.getCurrentWindow().$('#refund-list > div').length > 0", 2000);;  
+
+        $this->assertEquals(URL::action('MerchantController@getIndex').'/#!/refunds',$this->browser->getLocation());
+
+        $this->assertBodyHasText("Recent Refunds");
+    }
+
+    public function testSettlementsPanel()
+    {
+         $this->browser
+            ->open(URL::action('MerchantController@getIndex'))
+            ->click(l::linkContaining('Settlements'))   
+            ->waitForCondition("selenium.browserbot.getCurrentWindow().$('#settle-list > div').length > 0", 2000);;  
+
+        $this->assertEquals(URL::action('MerchantController@getIndex').'/#!/settlements',$this->browser->getLocation());
+
+        $this->assertBodyHasText("Recent Settlements");
+    }
+
+    public function testAccountPanel()
+    {
+         $this->browser
+            ->open(URL::action('MerchantController@getIndex'))
+            ->click(l::linkContaining('Account'))   
+            ->waitForCondition("selenium.browserbot.getCurrentWindow().$('#account-name').html().length > 0", 2000);;  
+
+        $this->assertEquals(URL::action('MerchantController@getIndex').'/#!/account',$this->browser->getLocation());
+
+        $this->assertBodyHasText($this->merchant->name);
+    }
+
+
 
     
 
