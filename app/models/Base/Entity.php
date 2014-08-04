8bcf5c0dd68619c3a1d5649b79d245e385a4fe10
Shashank Kumar <shashankkumar.me@gmail.com>
2014-08-04 15:21:43 +0530

    Overrides asDateTime function of laravel to always return timestamps in well timestamps format

--- app/models/Base/Entity.php
+++ app/models/Base/Entity.php
@@ -7,5 +7,17 @@ use EE\Exception;
 
 class Entity extends EloquentEx
 {
-
+    protected function asDateTime($value)
+    {
+        // If this value is an integer, we will assume it is a UNIX timestamp's value
+        // and return as it is. Otherwise we will call the parent function to handle it.
+        if (is_numeric($value))
+        {
+            return $value;
+        }
+        else
+        {
+            return parent::asDateTime($value);
+        }
+    }
 }
