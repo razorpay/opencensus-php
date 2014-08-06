6a6302a4ae059d11e1f05fe46579ee85e74b2238
Harshil Mathur <harshilmathur@gmail.com>
2014-08-06 23:45:54 +0530

    Displaying exception when debug off on api, removed unused view

--- app/models/service/request.php
+++ app/models/service/request.php
@@ -22,7 +22,11 @@ class Request extends Service
         $response = \Requests::post(\Config::get('api.url') . $url, array(), $data, $options);
         
         $array = json_decode($response->body, true);
-        if($array === NULL) $array['error']['description'] = 'API Exception';
+        
+        if(isset($array['error']['message'])) 
+        {
+            echo $response->body; die();
+        }
 
         return $array;
     }
@@ -33,7 +37,11 @@ class Request extends Service
         $response = \Requests::put(\Config::get('api.url') . $url, array(), $data, $options);
         
         $array = json_decode($response->body, true);
-        if($array === NULL) $array['error']['description'] = 'API Exception';
+        
+        if(isset($array['error']['message'])) 
+        {
+            echo $response->body; die();
+        }
 
         return $array;
     }
@@ -43,9 +51,13 @@ class Request extends Service
         $qs = http_build_query($data);
         $options = ['auth' => [$this->ID,$this->PASSWORD]];
         $response = \Requests::get(\Config::get('api.url') . $url . '?' . $qs, array(), $options);
-        
+
         $array = json_decode($response->body, true);
-        if($array === NULL) $array['error']['description'] = 'API Exception';
+        
+        if(isset($array['error']['message'])) 
+        {
+            echo $response->body; die();
+        }
 
         return $array;
     }
