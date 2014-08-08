db124e3181862f45d2971103bb21de9a4597ab07
Harshil Mathur <harshilmathur@gmail.com>
2014-08-09 04:35:13 +0530

    Added debug bar

--- app/config/app.php
+++ app/config/app.php
@@ -120,6 +120,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'Aws\Laravel\AwsServiceProvider',
+		'Barryvdh\Debugbar\ServiceProvider'
 	),
 
 	/*
