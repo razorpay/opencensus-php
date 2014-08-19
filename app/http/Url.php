64f6c38e3ecf935ed6d9caa887d4f9381b37afa9
Shashank Kumar <shashankkumar.me@gmail.com>
2014-08-19 17:41:36 +0530

    Restructures api routes definition
    Desc:
    * Defines api routes in Url file separated in categories
      public, private, internal.
    * All routes are added to laravel's internal route lib
      utilizing the above categories
    * Defines names for all routes

--- app/http/Url.php
+++ app/http/Url.php
@@ -2,52 +2,55 @@
 
 namespace Http;
 
+use Route;
+
 final class URL
 {
-    const TXN_CREATE_URL = 'transactions';
-    const TXN_CREATE_METHOD = 'post';
+    public static $public = array(
+        'transaction_create'        => ['post', 'transactions', 'TransactionController@postCreateTransaction'],
+        'transaction_jsonp'         => ['post', 'transactions/jsonp', 'TransactionController@postCallback'],
+        'transaction_callback'      => ['post', 'transactions/callback/{id}', 'TransactionController@getJSONP'],
+        );
 
-    const TXN_REFUND_URL = 'transactions/{id}/refund';
-    const TXN_REFUND_METHOD = 'post';
+    public static $private = array(
+        'transaction_refund'            => ['post', 'transactions/{id}/refund', 'TransactionController@postRefund'],
+        'transaction_capture'           => ['post', 'transactions/{id}/capture', 'TransactionController@postCapture'],
+        'transaction_fetch_by_id'       => ['get',  'transactions/{id}', 'TransactionController@getTransaction'],
+        'transaction_fetch_multiple'    => ['get',  'transactions/{param?}', 'TransactionController@getTransactions'],
+        );
 
-    const TXN_CAPTURE_URL = 'transactions/{id}/capture';
-    const TXN_CAPTURE_METHOD = 'post';
-
-    const TXN_JSONP_URL = 'transactions/jsonp';
-    const TXN_JSONP_METHOD = 'get';
-
-    const TXN_RETRIEVE_BY_ID_URL = 'transactions/{id}';
-    const TXN_RETRIEVE_MULTIPLE_URL = 'transactions';
-
-    const TXN_RETRIEVE_URL = 'transactions/{param?}';
-    const TXN_RETRIEVE_METHOD = 'get';
-
-    const TXN_CALLBACK_URL = 'transactions/callback/{id}';
-    const TXN_CALLBACK_METHOD = 'post';
-
-    const MERCHANT_REGISTER = 'merchants';
-    const MERCHANT_REGISTER_METHOD = 'post';
-
-    protected $url = array(
-        'transaction_create' => array(
-            'method' => 'post',
-            'url' => 'transactions',
-            'action' => 'TransactionController@postIndex'));
-
-    protected static $doNotLogUrls = array(
-        self::TXN_JSONP_URL);
-
-    protected static $jsonpUrls = array(
-        self::TXN_JSONP_URL);
+    public static $internal = array(
+        'merchant_create'           => ['post', 'merchants',                'MerchantController@postCreateMerchant'],
+        'merchant_fetch_keys'       => ['get',  'merchants/{id}/keys',      'MerchantController@getKeys'],
+        'merchant_replace_key'      => ['put',  'merchants/{merchantId}/keys/{keyId}', 'MerchantController@putKeys'],
+        'merchant_assign_pricing'   => ['post', 'merchants/{id}/pricing',   'MerchantController@postAssignPricingPlan'],
+        'merchant_get_pricing'      => ['get',  'merchants/{id}/pricing',   'MerchantController@getPricingPlan'],
+        'merchant_create_terminal'  => ['post', 'merchants/{id}/terminal',  'MerchantController@postCreateTerminal'],
+        'merchant_get_terminal'     => ['get',  'merchants/{id}/terminal',  'MerchantController@getTerminals'],
+        'merchant_activate'         => ['post', 'merchants/{id}/activate',  'MerchantController@postActivate'],
+        'pricing_create_plan'       => ['post', 'pricing',                  'PricingController@postCreatePricingPlan'],
+        'pricing_get_plans'         => ['get',  'pricing',                  'PricingController@getPricingPlans'],
+        'pricing_get_merchant_plans'=> ['get',  'pricing/merchants',        'PricingController@getMerchantPricingPlans'],
+        'pricing_get_gateway_plans' => ['get',  'pricing/gateways',         'PricingController@getGatewayPricingPlans'],
+        'pricing_add_plan_rule'     => ['post', 'pricing/{id}/rule',        'PricingController@postAddPricingPlanRule'],
+        'pricing_get_by_id'         => ['get',  'pricing/{id}',             'PricingController@getPricingPlan'],
+        'pricing_get_plan_rule'     => ['get',  'pricing/{planId}/rule/{ruleId}', 'PricingController@getPricingPlanRule'],
+        );
 
     public static function getDoNotLogURLs()
     {
-        return self::$doNotLogUrls;
+        $doNotLogUrls = array(
+            self::$public['transaction_jsonp'][1]);
+
+        return $doNotLogUrls;
     }
 
     public static function isJsonpUrl($url)
     {
-        return in_array($url, self::$jsonpUrls);
+        $jsonpUrls = array(
+            self::$public['transaction_jsonp'][1]);
+
+        return in_array($url, $jsonpUrls);
     }
 
     public static function callback($id)
@@ -80,6 +83,18 @@ final class URL
             $urlSegment .= '/' . $txn->getPublicId();
 
 
+        }
+    }
+
+    public static function addRoutes($type)
+    {
+        foreach (self::$$type as $routeName => $routeInfo)
+        {
+            $method = $routeInfo[0];
+            $url = $routeInfo[1];
+            $action = $routeInfo[2];
+
+            Route::$method($url, array('as' => $routeName, 'uses' => $action));
         }
     }
 }
--- app/routes.php
+++ app/routes.php
@@ -18,79 +18,22 @@ use Http\URL;
  | make sure to run the full test suite
  */
 
+//s(Route::getCurrentRequest());
 Route::group(array('prefix' => 'v1'), function()
 {
     Route::group(array('before' => 'auth.public'), function()
     {
-        $method = URL::TXN_CALLBACK_METHOD;
-        Route::$method(
-            URL::TXN_CALLBACK_URL,
-            'TransactionController@postCallback');
-
-        $method = URL::TXN_CREATE_METHOD;
-        Route::$method(
-            URL::TXN_CREATE_URL,
-            'TransactionController@postCreateTransaction');
-
-        $method = URL::TXN_JSONP_METHOD;
-        Route::$method(
-            URL::TXN_JSONP_URL,
-            'TransactionController@getJSONP');
+        URL::addRoutes('public');
     });
 
     Route::group(array('before' => 'auth.private'), function()
     {
-        $method = URL::TXN_RETRIEVE_METHOD;
-        Route::$method(
-            URL::TXN_RETRIEVE_BY_ID_URL,
-            'TransactionController@getTransaction');
-
-        Route::$method(
-            URL::TXN_RETRIEVE_MULTIPLE_URL,
-            'TransactionController@getTransactions');
-
-        $method = URL::TXN_REFUND_METHOD;
-        Route::$method(
-            URL::TXN_REFUND_URL,
-            'TransactionController@postRefund');
-
-        $method = URL::TXN_CAPTURE_METHOD;
-        Route::$method(
-            URL::TXN_CAPTURE_URL,
-            'TransactionController@postCapture');
+        URL::addRoutes('private');
     });
 
     Route::group(array('before' => 'auth.app'), function()
     {
-        Route::post('merchants', 'MerchantController@postCreateMerchant');
-
-        Route::get('merchants/{id}/keys', 'MerchantController@getKeys');
-
-        Route::put('merchants/{merchantId}/keys/{keyId}', 'MerchantController@putKeys');
-
-        Route::post('merchants/{id}/pricing', 'MerchantController@postAssignPricingPlan');
-
-        Route::get('merchants/{id}/pricing', 'MerchantController@getPricingPlan');
-
-        Route::post('merchants/{id}/terminal', 'MerchantController@postCreateTerminal');
-
-        Route::get('merchants/{id}/terminal', 'MerchantController@getTerminals');
-
-        Route::post('merchants/{id}/activate', 'MerchantController@postActivate');
-
-        Route::post('pricing', 'PricingController@postCreatePricingPlan');
-
-        Route::get('pricing', 'PricingController@getPricingPlans');
-
-        Route::get('pricing/merchants', 'PricingController@getMerchantPricingPlans');
-
-        Route::get('pricing/gateways', 'PricingController@getGatewayPricingPlans');
-
-        Route::post('pricing/{id}/rule', 'PricingController@postAddPricingPlanRule');
-
-        Route::get('pricing/{id}', 'PricingController@getPricingPlan');
-
-        Route::get('pricing/{planId}/rule/{ruleId}', 'PricingController@getPricingPlanRule');
+        URL::addRoutes('internal');
     });
 });
 
