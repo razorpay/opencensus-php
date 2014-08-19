25736ef1603b7658dd443914412355cb544446c3
Shashank Kumar <shashankkumar.me@gmail.com>
2014-08-19 19:37:49 +0530

    Places api route definitions in Http/Route class
    Desc:
    * Adds api route definitions in Route class.
    * Combines different route categories into one apiRoutes array
    * The route categories only contain route names

--- app/http/Route.php
+++ app/http/Route.php
@@ -2,62 +2,164 @@
 
 namespace Http;
 
-use Route;
-
-final class URL
+final class Route
 {
+    protected static $router;
+
+    /*
+     | The order in which routes are defined is very important.
+     | Whenever the order of routes is changed,
+     | make sure to run the full test suite
+     */
+
+    protected static $apiRoutes = array(
+        'transaction_create'        => ['post', 'transactions',                 'TransactionController@postCreateTransaction'],
+        'transaction_jsonp'         => ['post', 'transactions/jsonp',           'TransactionController@getJSONP'],
+        'transaction_callback'      => ['post', 'transactions/callback/{id}',   'TransactionController@postCallback'],
+        'transaction_refund'        => ['post', 'transactions/{id}/refund',     'TransactionController@postRefund'],
+        'transaction_capture'       => ['post', 'transactions/{id}/capture',    'TransactionController@postCapture'],
+        'transaction_fetch_by_id'   => ['get',  'transactions/{id}',            'TransactionController@getTransaction'],
+        'transaction_fetch_multiple'=> ['get',  'transactions/{param?}',        'TransactionController@getTransactions'],
+        'merchant_create'           => ['post', 'merchants',                    'MerchantController@postCreateMerchant'],
+        'merchant_fetch_keys'       => ['get',  'merchants/{id}/keys',          'MerchantController@getKeys'],
+        'merchant_replace_key'      => ['put',  'merchants/{merchantId}/keys/{keyId}', 'MerchantController@putKeys'],
+        'merchant_assign_pricing'   => ['post', 'merchants/{id}/pricing',       'MerchantController@postAssignPricingPlan'],
+        'merchant_get_pricing'      => ['get',  'merchants/{id}/pricing',       'MerchantController@getPricingPlan'],
+        'merchant_create_terminal'  => ['post', 'merchants/{id}/terminal',      'MerchantController@postCreateTerminal'],
+        'merchant_get_terminal'     => ['get',  'merchants/{id}/terminal',      'MerchantController@getTerminals'],
+        'merchant_activate'         => ['post', 'merchants/{id}/activate',      'MerchantController@postActivate'],
+        'pricing_create_plan'       => ['post', 'pricing',                      'PricingController@postCreatePricingPlan'],
+        'pricing_get_plans'         => ['get',  'pricing',                      'PricingController@getPricingPlans'],
+        'pricing_get_merchant_plans'=> ['get',  'pricing/merchants',            'PricingController@getMerchantPricingPlans'],
+        'pricing_get_gateway_plans' => ['get',  'pricing/gateways',             'PricingController@getGatewayPricingPlans'],
+        'pricing_add_plan_rule'     => ['post', 'pricing/{id}/rule',            'PricingController@postAddPricingPlanRule'],
+        'pricing_get_by_id'         => ['get',  'pricing/{id}',                 'PricingController@getPricingPlan'],
+        'pricing_get_plan_rule'     => ['get',  'pricing/{planId}/rule/{ruleId}', 'PricingController@getPricingPlanRule'],
+        );
+
     public static $public = array(
-        'transaction_create'        => ['post', 'transactions', 'TransactionController@postCreateTransaction'],
-        'transaction_jsonp'         => ['post', 'transactions/jsonp', 'TransactionController@postCallback'],
-        'transaction_callback'      => ['post', 'transactions/callback/{id}', 'TransactionController@getJSONP'],
+        'transaction_create',
+        'transaction_jsonp',
+        'transaction_callback',
         );
 
     public static $private = array(
-        'transaction_refund'            => ['post', 'transactions/{id}/refund', 'TransactionController@postRefund'],
-        'transaction_capture'           => ['post', 'transactions/{id}/capture', 'TransactionController@postCapture'],
-        'transaction_fetch_by_id'       => ['get',  'transactions/{id}', 'TransactionController@getTransaction'],
-        'transaction_fetch_multiple'    => ['get',  'transactions/{param?}', 'TransactionController@getTransactions'],
+        'transaction_refund',
+        'transaction_capture',
+        'transaction_fetch_by_id',
+        'transaction_fetch_multiple',
         );
 
     public static $internal = array(
-        'merchant_create'           => ['post', 'merchants',                'MerchantController@postCreateMerchant'],
-        'merchant_fetch_keys'       => ['get',  'merchants/{id}/keys',      'MerchantController@getKeys'],
-        'merchant_replace_key'      => ['put',  'merchants/{merchantId}/keys/{keyId}', 'MerchantController@putKeys'],
-        'merchant_assign_pricing'   => ['post', 'merchants/{id}/pricing',   'MerchantController@postAssignPricingPlan'],
-        'merchant_get_pricing'      => ['get',  'merchants/{id}/pricing',   'MerchantController@getPricingPlan'],
-        'merchant_create_terminal'  => ['post', 'merchants/{id}/terminal',  'MerchantController@postCreateTerminal'],
-        'merchant_get_terminal'     => ['get',  'merchants/{id}/terminal',  'MerchantController@getTerminals'],
-        'merchant_activate'         => ['post', 'merchants/{id}/activate',  'MerchantController@postActivate'],
-        'pricing_create_plan'       => ['post', 'pricing',                  'PricingController@postCreatePricingPlan'],
-        'pricing_get_plans'         => ['get',  'pricing',                  'PricingController@getPricingPlans'],
-        'pricing_get_merchant_plans'=> ['get',  'pricing/merchants',        'PricingController@getMerchantPricingPlans'],
-        'pricing_get_gateway_plans' => ['get',  'pricing/gateways',         'PricingController@getGatewayPricingPlans'],
-        'pricing_add_plan_rule'     => ['post', 'pricing/{id}/rule',        'PricingController@postAddPricingPlanRule'],
-        'pricing_get_by_id'         => ['get',  'pricing/{id}',             'PricingController@getPricingPlan'],
-        'pricing_get_plan_rule'     => ['get',  'pricing/{planId}/rule/{ruleId}', 'PricingController@getPricingPlanRule'],
+        'merchant_create',
+        'merchant_fetch_keys',
+        'merchant_replace_key',
+        'merchant_assign_pricing',
+        'merchant_get_pricing',
+        'merchant_create_terminal',
+        'merchant_get_terminal',
+        'merchant_activate',
+        'pricing_create_plan',
+        'pricing_get_plans',
+        'pricing_get_merchant_plans',
+        'pricing_get_gateway_plans',
+        'pricing_add_plan_rule',
+        'pricing_get_by_id',
+        'pricing_get_plan_rule',
         );
+
+    public static function setRouter($router)
+    {
+        self::$router = $router;
+    }
 
     public static function getDoNotLogURLs()
     {
         $doNotLogUrls = array(
-            self::$public['transaction_jsonp'][1]);
+            self::$apiRoutes['transaction_jsonp'][1]);
 
         return $doNotLogUrls;
     }
 
-    public static function isJsonpUrl($url)
+    public static function isJsonpRoute($route)
     {
-        $jsonpUrls = array(
-            self::$public['transaction_jsonp'][1]);
+        $jsonpRoute = array(
+            self::$apiRoutes['transaction_jsonp'][1]);
 
-        return in_array($url, $jsonpUrls);
+        return in_array($route, $jsonpRoute);
+    }
+
+    protected static function addRoutes($type)
+    {
+        foreach (self::$$type as $routeName)
+        {
+            $routeInfo = self::$apiRoutes[$routeName];
+
+            $method = $routeInfo[0];
+            $uri = $routeInfo[1];
+            $action = $routeInfo[2];
+
+            $router = self::$router;
+
+            $router->$method($uri, array('as' => $routeName, 'uses' => $action));
+        }
+    }
+
+    public static function defineApiRoutes()
+    {
+        $router = self::$router;
+
+        $router->group(array('prefix' => 'v1'), function() use ($router)
+        {
+            //
+            // First define internal routes and then private and finally public
+            // If by mistake a route is defined twice in say internal and public,
+            // then it will go into internal app auth and will not expose the route.
+            // This must not happen though.
+            //
+            $router->group(array('before' => 'auth.app'), function()
+            {
+                self::addRoutes('internal');
+            });
+
+            $router->group(array('before' => 'auth.private'), function()
+            {
+                self::addRoutes('private');
+            });
+
+            $router->group(array('before' => 'auth.public'), function()
+            {
+                self::addRoutes('public');
+            });
+        });
+
+        $router->get('/', function()
+        {
+            $response['message'] = "Welcome to Razorpay API.";
+            return Http\ApiResponse::json($response);
+        });
+
+        $router->any('{all}', function($uri)
+        {
+            return Http\ApiResponse::routeNotFound();
+        })->where('all', '.*');
+    }
+
+    public static function getApiRoutes()
+    {
+        return self::$apiRoutes;
+    }
+
+    public static function getApiRoute($name)
+    {
+        return self::$apiRoutes[$name];
     }
 
     public static function callback($id)
     {
-        $urlSegment = self::TXN_CALLBACK_URL;
+        $route = 'transaction_callback';
 
-        $pos = strrpos($urlSegment, '/');
+        $pos = strrpos($route, '/');
 
         $urlSegment = substr($urlSegment, 0, $pos);
 
@@ -70,31 +172,5 @@ final class URL
         $callbackUrl = $scheme . $key . '@' . $host . '/' . $urlSegment;
 
         $callbackData['callbackUrl'] = $callbackUrl;
-    }
-
-    public static function buildUrl($segment, $arg = null, $auth = null)
-    {
-        if (isset($arg))
-        {
-            $pos = strrpos($urlSegment, '/');
-
-            $urlSegment = substr($urlSegment, 0, $pos);
-
-            $urlSegment .= '/' . $txn->getPublicId();
-
-
-        }
-    }
-
-    public static function addRoutes($type)
-    {
-        foreach (self::$$type as $routeName => $routeInfo)
-        {
-            $method = $routeInfo[0];
-            $url = $routeInfo[1];
-            $action = $routeInfo[2];
-
-            Route::$method($url, array('as' => $routeName, 'uses' => $action));
-        }
     }
 }
--- app/routes.php
+++ app/routes.php
@@ -1,49 +1,5 @@
 <?php
 
-use Http\URL;
+Http\Route::setRouter($app['router']);
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Simply tell Laravel the HTTP verbs and URIs it should respond to. It is a
-| breeze to setup your application using Laravel's RESTful routing and it
-| is perfectly suited for building large applications and simple APIs.
-*/
-
-/*
- | The order in which routes are defined is very important.
- | Whenever the order of routes is changed,
- | make sure to run the full test suite
- */
-
-//s(Route::getCurrentRequest());
-Route::group(array('prefix' => 'v1'), function()
-{
-    Route::group(array('before' => 'auth.public'), function()
-    {
-        URL::addRoutes('public');
-    });
-
-    Route::group(array('before' => 'auth.private'), function()
-    {
-        URL::addRoutes('private');
-    });
-
-    Route::group(array('before' => 'auth.app'), function()
-    {
-        URL::addRoutes('internal');
-    });
-});
-
-Route::get('/', function()
-{
-    $response['message'] = "Welcome to Razorpay API.";
-    return Http\ApiResponse::json($response);
-});
-
-Route::any('{all}', function($uri)
-{
-    return Http\ApiResponse::routeNotFound();
-})->where('all', '.*');
+Http\Route::defineApiRoutes();
