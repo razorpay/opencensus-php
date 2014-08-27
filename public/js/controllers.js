7185a9353280c06e8fef54fc381305b4631966a6
Harshil Mathur <harshilmathur@gmail.com>
2014-08-28 01:41:45 +0530

    Adds graphs on dashboard with date and stat type chooser

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -143,10 +143,6 @@ angular.module('app.controllers', ['pascalprecht.translate', 'ngCookies'])
       isopen: false
     };
 
-    $scope.toggled = function(open) {
-      //console.log('Dropdown is now: ', open);
-    };
-
     $scope.toggleDropdown = function($event) {
       $event.preventDefault();
       $event.stopPropagation();
@@ -297,11 +293,6 @@ angular.module('app.controllers', ['pascalprecht.translate', 'ngCookies'])
       $scope.dt = null;
     };
 
-    // Disable weekend selection
-    $scope.disabled = function(date, mode) {
-      return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
-    };
-
     $scope.toggleMin = function() {
       $scope.minDate = $scope.minDate ? null : new Date();
     };
@@ -321,8 +312,6 @@ angular.module('app.controllers', ['pascalprecht.translate', 'ngCookies'])
     };
 
     $scope.initDate = new Date('2016-15-20');
-    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-    $scope.format = $scope.formats[0];
   }])
   .controller('TimepickerDemoCtrl', ['$scope', function($scope) {
     $scope.mytime = new Date();
@@ -740,32 +729,135 @@ angular.module('app.controllers', ['pascalprecht.translate', 'ngCookies'])
               });
       };
   }])
-  .controller('AggregationsCtrl', ['$scope', '$http', 'modeFactory',
-    function($scope, $http, modeFactory) {
-        $scope.data = {
-                        success: 0,
-                        amount: 0,
-                        txns: 0
-                    };
-
-        var request = $http.get("/"+modeFactory.getMode()+"/analytics/aggregations");
-
-        request.success(function(result){
-          
-          if (result.data !== null) {
-              if (parseInt(result.data.txn_count) !== 0)
-                  $scope.data.success = parseInt(result.data.successful_txn_count * 100/result.data.txn_count);
-              $scope.data.amount = result.data.total_amount;
-              $scope.data.txns = result.data.txn_count;
-          }
-        });
-
-  }])
   .controller('modeCtrl', ['$scope', 'modeFactory',
     function($scope, modeFactory){
       $scope.modes = modeFactory.getModes;
       $scope.mode = modeFactory.getMode;
       $scope.selectMode = modeFactory.selectMode;
   }])
+  .controller('AggregationsCtrl', ['$scope', '$http', 'modeFactory',
+    function($scope, $http, modeFactory) {
 
+    //Aggreagates
+    $scope.aggregations = {};
+
+    $scope.aggregations.data = {
+                        success: 0,
+                        amount: 0,
+                        txns: 0
+                    };
+
+    var request = $http.get("/"+modeFactory.getMode()+"/analytics/aggregations");
+
+    request.success(function(result){
+      
+      if (result.data !== null) {
+          if (parseInt(result.data.txn_count) !== 0)
+              $scope.aggregations.data.success = parseInt(result.data.successful_txn_count * 100/result.data.txn_count);
+          $scope.aggregations.data.amount = result.data.total_amount;
+          $scope.aggregations.data.txns = result.data.txn_count;
+      }
+    });
+  }])
+  .controller('DashboardCtrl', ['$scope', '$http', 'modeFactory', 'dateFactory',
+    function($scope, $http, modeFactory, dateFactory) {
+
+      
+      initialiseStatType();
+      initialiseGraphs();
+      
+      //Watches changes in parameters and trigger regeneration fo graph if any changes
+      $scope.$watch('showSpline', function() {
+         $scope.refreshGraph = !$scope.refreshGraph;
+      });
+    
+      //@todo once angular 1.3 is stable switch to watchgroup
+      $scope.$watch('statType + date.startDate + date.endDate',function() {
+         generateGraphs();
+      });
+
+      function initialiseStatType(){
+        //Stat Type handlers
+        $scope.statType = 'day';
+
+        $scope.stats = {day: "Daily", week: "Weekly", month: "Monthly", year: "Yearly"};
+
+        $scope.setStat = function(type) {
+          $scope.statType = type;
+        };
+      };
+
+      function initialiseGraphs() {
+        //Date Handlers
+        $scope.date = dateFactory.getHandler($scope);
+        
+        //Graphs Initalisers
+        $scope.showSpline = true;
+
+        $scope.refreshGraph = false; //Variable that is toggled whenever we want the graph to be refreshed
+
+        var graphData = {
+          data: [ [0,0] ],
+          options: {
+            colors: [$scope.app.color.info, $scope.app.color.primary],
+            series: { shadowSize: 3 },
+            xaxis: {mode: 'time', timezone: "browser"},
+            yaxis:{ font: { color: '#a1a7ac' }},
+            grid: { hoverable: true, clickable: true, borderWidth: 0, color: '#dce5ec' },
+            tooltip: true,
+            tooltipOpts: {
+              defaultTheme: false, 
+              shifts: { x: 10, y: -25 }
+            }
+          }
+        };
+
+        $scope.successfull = graphData;
+        $scope.transactions = graphData;
+        
+        $scope.successfull.options.tooltipOpts.content = 'Date: %x <br/> Count: %y';
+        $scope.successfull.options.tooltipOpts.content = 'Date: %x <br/> Amount: %y';
+      };
+
+      function generateGraphs() {
+        if(!$scope.date.startDate || !$scope.date.endDate) return;
+
+        var from = parseInt(($scope.date.startDate.getTime())/1000) - 1;
+        var to = parseInt(($scope.date.endDate.getTime())/1000) + 1;
+
+        var request = $http.get("/" + modeFactory.getMode() + "/analytics/transactions?type="+$scope.statType+"&from=" + from + "&to="+ to);
+
+
+        request.success(function(data){
+          $scope.successfull.data = [];
+          $scope.transactions.data = [];
+
+          angular.forEach(data.data , function(value, key){
+            $scope.successfull.data.push([
+              parseInt(value.created_at)*1000,
+              parseInt(value.count)
+            ]);
+
+            $scope.transactions.data.push([
+              parseInt(value.created_at)*1000,
+              parseInt(value.amount)
+            ]);
+          });
+
+          $scope.successfull.options.xaxis.minTickSize = getMinTickSize($scope.statType);
+          $scope.transactions.options.xaxis.minTickSize = getMinTickSize($scope.statType);
+
+          $scope.refreshGraph = !$scope.refreshGraph;
+        });
+      };
+
+      function getMinTickSize(statType){
+        if(statType === 'week'){
+          return [7, 'day'];
+        }
+        else{
+          return [1, statType];
+        }
+      };
+  }])
   ;
