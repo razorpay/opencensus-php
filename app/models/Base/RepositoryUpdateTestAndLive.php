aafec1f638a84c74609d79d821e6f4c3aa8dc218
Shashank Kumar <shashankkumar.me@gmail.com>
2014-08-27 12:46:42 +0530

    fixes minor bug in dual db update function

--- app/models/Base/RepositoryUpdateTestAndLive.php
+++ app/models/Base/RepositoryUpdateTestAndLive.php
@@ -31,9 +31,11 @@ trait RepositoryUpdateTestAndLive
         {
             if ($exists)
             {
+                //
                 // The entity already exists in db
                 // Fetch it from both live and test databases
                 // and lock for update
+                //
 
                 $repo = $this->repo;
                 $testEntity = $repo::on('test')->lockForUpdate()->findOrFail($id);
@@ -50,15 +52,25 @@ trait RepositoryUpdateTestAndLive
                 {
                     throw new Exception\LogicException('A row in test and live database do not match');
                 }
+
+                // Update the test and live entities
+                $attributes = $entity->getAttributes();
+                $testEntity->setRawAttributes($attributes);
+                $liveEntity->setRawAttributes($attributes);
             }
             else
             {
+                //
+                // The entity hasn't been persisted yet.
+                // Create it's copies for live and test database
+                //
                 $testEntity = clone $entity;
                 $liveEntity = clone $entity;
             }
 
-            $testEntity->setConnection('test')->saveOrFail($options);
+            // Persist then entity in both live and test databases.
             $liveEntity->setConnection('live')->saveOrFail($options);
+            $testEntity->setConnection('test')->saveOrFail($options);
         }
         catch (\Exception $e)
         {
@@ -71,7 +83,10 @@ trait RepositoryUpdateTestAndLive
         DB::connection('live')->commit();
         DB::connection('test')->commit();
 
+        // Now that the entity has been update in both live and test databases,
+        // update the one passed as argument in this function
+        $attributes = $liveEntity->getAttributes();
+        $entity->setRawAttributes($attributes, true);
         $entity->exists = true;
     }
-
 }
