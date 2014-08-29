6852f8f06147a049da6d7474c205f8662a8b118c
Harshil Mathur <harshilmathur@gmail.com>
2014-08-29 22:32:38 +0530

    Added transactionlist controller

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -860,4 +860,117 @@ angular.module('app.controllers', ['pascalprecht.translate', 'ngCookies'])
         }
       };
   }])
+  .controller('TransactionsListCtrl', ['$scope', '$http', 'modeFactory', 'alertsFactory', '$state',
+    function($scope, $http, modeFactory, alertsFactory, $state){
+      //Intialise alerts and scope functions
+      $scope.alerts = alertsFactory.initialise();
+
+      $scope.transactions = {
+          data: {},
+          id: '',
+          count: 0,
+          countStart: 0,
+          countEnd: 0,
+          skip: 0,
+          from: 0,
+          to: 4102444800,
+          status: ''
+      };
+
+      generateTable();
+
+      $scope.$watch('transactions.status', regenerate);
+
+      //@todo seperate refund controller later
+      //Used for refunds list display route
+      if($state.current.data.status) $scope.transactions.status = $state.current.data.status;
+
+      $scope.next= function() {
+        clear('id');
+        $scope.transactions.skip += 10;
+        generateTable();
+      }
+
+      $scope.prev= function() {
+        clear('id');
+        $scope.transactions.skip -= 10;
+        generateTable();
+      }
+
+      $scope.search= function() {
+        clear('skip');
+        generateTable();
+      }
+
+      $scope.getStatusClass = function(status) {
+        var mapper = {
+          open: "bg-light",
+          authorized: "bg-info",
+          captured: "bg-success",
+          refunded: "bg-warning",
+          failed: "bg-danger"
+        }
+        return mapper[status];
+      }
+
+      function clear(field){
+        if(field === 'id')
+          $scope.transactions.id = '';
+        if(field === 'skip')
+          $scope.transactions.skip = 0;
+      }
+
+      function regenerate(){
+        clear('skip');
+        clear('id');
+        generateTable();
+      }
+
+      function generateTable() {
+        alertsFactory.resetAlerts();
+        alertsFactory.addAlert('info', "Processing... ");
+        var query =
+          "count=10" +
+          "&skip="+ $scope.transactions.skip +
+          "&from="+ $scope.transactions.from +
+          "&to="+ $scope.transactions.to;
+
+        if($scope.transactions.status !== '')
+          query += "&status=" + $scope.transactions.status;
+
+        if($scope.transactions.id === '')
+          var request = $http.get("/" + modeFactory.getMode() +  "/transactions?" + query);
+        else
+          var request = $http.get("/" + modeFactory.getMode() +  "/transactions/" + $scope.transactions.id);
+        
+        request.success(function(data){
+          alertsFactory.resetAlerts();
+
+          if(data.success) {
+            $scope.transactions.data = data.data;
+
+            $scope.transactions.count = data.count;
+
+            $scope.transactions.countStart = $scope.transactions.skip + 1;
+
+            if(data.count === 0) $scope.transactions.countEnd = $scope.transactions.countStart;
+
+            else $scope.transactions.countEnd = $scope.transactions.countStart + $scope.transactions.count -1;
+
+            $scope.allowPrev = $scope.transactions.countStart !== 1;
+            
+            $scope.allowNext=$scope.transactions.count >= 10;
+          }
+          else {
+            angular.forEach(data.errors, function(value, key){
+              alertsFactory.addAlert('danger', value);
+            });            
+          }          
+        })
+        .error(function(){
+          alertsFactory.resetAlerts();
+          alertsFactory.addAlert('danger');
+        });
+      }
+  }])
   ;
