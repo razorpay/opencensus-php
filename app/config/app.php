ccc756616796f99a58b81f9a27aa12ab06ea251b
Shashank Kumar <shashankkumar.me@gmail.com>
2014-08-29 11:47:40 +0530

    Adds maatwebsite/excel package with it's alias and ServiceProvider

--- app/config/app.php
+++ app/config/app.php
@@ -114,6 +114,7 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
+        'Maatwebsite\Excel\ExcelServiceProvider',
         'Http\BasicAuth\ServiceProvider'
 	),
 
@@ -180,6 +181,7 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
+        'Excel'           => 'Maatwebsite\Excel\Facades\Excel',
         'BasicAuth'       => 'Http\BasicAuth\Facade'
 	),
 
