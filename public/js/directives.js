248d6af408aeee5a7248dd34f1e07b596ed0c8ce
Harshil Mathur <harshilmathur@gmail.com>
2014-08-30 19:06:00 +0530

    Adds confirm click directive for sending confirmation alert before executing ng-click

--- public/js/directives.js
+++ public/js/directives.js
@@ -191,4 +191,21 @@ angular.module('app.directives', ['ui.load'])
       }
      };
   }])
+  .directive('ngConfirmClick', [
+  function(){
+    return {
+      priority: -1,
+      restrict: 'A',
+      link: function(scope, element, attrs){
+        element.bind('click', function(e){
+          var message = attrs.ngConfirmClick;
+          if(message && !confirm(message)){
+            e.stopImmediatePropagation();
+            e.preventDefault();
+          }
+        });
+      }
+    }
+  }
+]);
 ;
