ad9ad6a55aaf4407f283cc5d7391cfdcf3e90ada
Shashank Kumar <shashankkumar.me@gmail.com>
2014-08-30 12:57:57 +0530

    Adds mock hdfc gateway urls

---
+++ app/gateway/MockHdfc/Urls.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Gateway\MockHdfc;
+
+class Urls
+{
+    const TEST_ENROLL_URL = 'gateway/mockhdfc/enroll';
+
+    const TEST_AUTH_NOT_ENROLLED_URL = 'gateway/mockhdfc/transaction';
+
+    const TEST_AUTH_ENROLLED_URL = 'gateway/mockhdfc/auth_enrolled';
+
+    const TEST_SUPPORT_TXN_URL = 'gateway/mockhdfc/transaction';
+}
