260423aa26ddb53faf5e88accdbbf8acce3ff56d
Shashank Kumar <shashankkumar.me@gmail.com>
2014-08-31 12:59:47 +0530

    Adds mode and app instance to base service class

--- app/models/Base/Service.php
+++ app/models/Base/Service.php
@@ -2,11 +2,31 @@
 
 namespace Models\Base;
 
+use App;
+
 class Service
 {
+    /**
+     * The application instance.
+     *
+     * @var Illuminate\Foundation\Application
+     */
+    protected $app;
+
+    /**
+     * Test/Live mode
+     *
+     * @var string
+     */
+    protected $mode;
+
 	public function __construct()
 	{
-		;
+		$this->app = App::getFacadeRoot();
+
+        $this->mode = $this->app['basicauth']->getMode();
+
+        $this->trace = $this->app['trace'];
 	}
 
     public static function getNewInstance()
