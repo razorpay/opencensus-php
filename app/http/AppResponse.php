b6c2f9191de80cb13f1a323e306992ee1b63eb34
Harshil Mathur <harshilmathur@gmail.com>
2014-09-06 02:42:02 +0530

    Adds appresponse for standardising JSON reponses

---
+++ app/http/AppResponse.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Http;
+
+use Response;
+
+class AppResponse
+{
+    public static function jsonResponse($errors, $data = null)
+    {
+        if(empty($errors)){
+            $response = array('success' => true);
+
+            if($data != null){
+                $response = $response + array('data' => $data);
+            }
+        }
+        else {
+            $response = array('success' => false, 'errors' => $errors);
+        }
+
+        return Response::json($response);
+    }
+}
