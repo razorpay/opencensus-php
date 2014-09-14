d261555e83fad3d8cd59ed9de1a6fa103af057fb
Harshil Mathur <harshilmathur@gmail.com>
2014-09-14 16:11:17 +0530

    Adds rzp logo to confirmation mail

--- app/views/emails/confirmation.blade.php
+++ app/views/emails/confirmation.blade.php
@@ -12,5 +12,12 @@
 			<p>Alternatively, open the following link in your browser:<br/>
 			<a href = "{{ URL::to('/#/access/confirm/'.$merchant['confirm_token']) }}" >{{ URL::to('/#/access/confirm/'.$merchant['confirm_token']) }}</a>
 		</div>
+
+		<div>
+			<p>--<br/>
+			The Razorpay Team <br/>
+			contact@razorpay.com </br/>
+			<img src="<?php echo $message->embed('public/img/logo_black.png'); ?>">
+			
 	</body>
 </html>
