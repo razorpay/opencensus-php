41abb95bffff5522eb0d4bdc2257313e42927b28
Harshil Mathur <harshilmathur@gmail.com>
2014-09-17 22:29:58 +0530

    Adds mock property to RZP Api

--- app/rzp/Api.php
+++ app/rzp/Api.php
@@ -6,12 +6,14 @@ use Razorpay;
 
 class Api extends Razorpay\Api\Api
 {
+    public static $mock = false;
     /**
      * @param string $api_key
      */
     function __construct($key, $secret)
     {
         self::$baseUrl = $_ENV['API_URL'];
+        self::$mock = isset($_ENV['API_MOCK']) ? $_ENV['API_MOCK'] : false;
         parent::__construct($key, $secret);
     }
 
@@ -21,7 +23,16 @@ class Api extends Razorpay\Api\Api
      */
     function __get($name)
     {
-        $className = __NAMESPACE__.'\\'.ucwords($name);
+        if(self::$mock === true)
+        {
+            //Delay the response by 0.5 secs (avoids issue with non loading of js before calls)
+            usleep(500000);
+            $className = __NAMESPACE__.'\\Mock\\'.ucwords($name);
+        }
+        else
+        {
+            $className = __NAMESPACE__.'\\'.ucwords($name);
+        }
 
         if(class_exists($className) === true)
         {
