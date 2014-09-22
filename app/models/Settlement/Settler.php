89494b1c3b0f469f4a6d875557bb5f020330130a
Shashank Kumar <shashankkumar.me@gmail.com>
2014-09-22 17:39:14 +0530

    Removes mock hdfc mpr generation code. Had shifted that part to hdfc gateway sometime bakc

--- app/models/Settlement/Settler.php
+++ app/models/Settlement/Settler.php
@@ -144,35 +144,4 @@ class Settler
             self::$settlementTimestamp = $timestamp;
         }
     }
-
-    protected function queueSettlementFailureSlackNotification($e)
-    {
-        $message = 'Failed to send out settlements. ' . PHP_EOL;
-
-        $message .= 'Exception class: ' . get_class($e) . ', ' .
-                    'Exception message: ' . $e->getMessage();
-
-        $func = __CLASS__ . '@sendSlackNotification';
-
-        $this->queue->push($func, $message);
-    }
-
-    protected function queueSettlementSuccessSlackNotification($count)
-    {
-        $message = 'Settlements sent out for ' . $count . ' merchants';
-
-        $func = __CLASS__ . '@sendSlackNotification';
-
-        $this->queue->push($func, $message);
-    }
-
-    public function sendSlackNotification($job, $message)
-    {
-        $channel = '#settlements';
-        $username = 'settlements';
-
-        $job->delete();
-
-        (new \Services\Slack)->send($message, $channel, $username);
-    }
 }
