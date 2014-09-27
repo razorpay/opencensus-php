debe1bb53d42a033c2def93a145c547438a0614d
Harshil Mathur <harshilmathur@gmail.com>
2014-09-28 00:55:26 +0530

    Fixes dashboard class to support refund

--- app/dashboard/Dashboard.php
+++ app/dashboard/Dashboard.php
@@ -40,6 +40,11 @@ class Dashboard
         return $this->config['url'];
     }
 
+    protected function getAuthSecret()
+    {
+        return $this->config['secret'];
+    }
+
     public static function getInstance()
     {
         return new static;
@@ -70,10 +75,13 @@ class Dashboard
     {
         $mode = \BasicAuth::getMode();
 
+        $options = array('auth'=> array('rzp_api', $this->getAuthSecret()));
+
         $response = Requests::post(
-            $this->getUrl() . $mode . '/' . $data['resource'],
+            $this->getUrl() . $mode . '/transactions/' . $data['resource'],
             array(),
-            $data['message']
+            $data['message'],
+            $options
         );
 
         //
@@ -82,7 +90,11 @@ class Dashboard
         //
         if (!is_object(json_decode($response->body)) ||
             json_decode($response->body)->status === FALSE)
-                (new Repository)->persistAfterFail($data['message']);
+        {
+            (new Repository)->persistAfterFail($data['message']);
+            
+            return;
+        }
 
         $job->delete();
     }
