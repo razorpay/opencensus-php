8575e1d55941e43b8e555b88cd3c1bcf227c060e
Shashank Kumar <shashankkumar.me@gmail.com>
2014-10-22 15:51:42 +0530

    Adds bank codes

---
+++ app/models/Payment/Bank.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Models\Payment;
+
+class Method
+{
+    const ALLA = 'ALLA';
+    const AXIS = 'AXIS';
+    const CITI = 'CITI';
+    const HDFC = 'HDFC';
+    const IBKL = 'IBKL';
+    const ICIC = 'ICIC';
+    const KKBK = 'KKBK';
+    const PUNB = 'PUNB';
+    const SBIN = 'SBIN';
+
+    public static $banks = array(
+        self::ALLA => 'Allahabad Bank',
+        self::CITI => 'Citi Bank',
+        self::HDFC => 'HDFC Bank',
+        self::IBKL => 'IDBI Bank',
+        self::ICIC => 'ICICI Bank',
+        self::KKBK => 'Kotak Mahindra Bank',
+        self::PUNB => 'Punjab National Bank',
+        self::SBIN => 'State Bank of India',
+    );
+
+    public static function checkValidBank($bank)
+    {
+        if (defined(__CLASS__.'::'.$bank) === false)
+        {
+            throw new Exception\BadRequestException(
+                ErrorCode::BAD_REQUEST_PAYMENT_INVALID_BANK_CODE);
+        }
+    }
+}
