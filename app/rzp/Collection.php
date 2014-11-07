4a75c9bdcb7aff7dff8afc2666eefbe59ddfed12
Shashank Kumar <shashankkumar.me@gmail.com>
2014-11-08 01:18:37 +0530

    Adds mock for Collection and Terminal entities

---
+++ app/rzp/Collection.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace RZP;
+
+class Collection extends Entity
+{
+}
---
+++ app/rzp/Terminal.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace RZP;
+
+class Terminal extends Entity
+{
+    /**
+     * @param $id Merchant id
+     */
+    public function fetch($id)
+    {
+        return parent::fetch($id);
+    }
+
+    public function all($options = array())
+    {
+        return parent::all();
+    }
+}
