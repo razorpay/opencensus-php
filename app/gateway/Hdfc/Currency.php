aa98a35b359cb4d0511c2ab94553d680cfb8da1a
Shashank Kumar <shashankkumar.me@gmail.com>
2014-12-13 23:37:17 +0530

    Adds currency codes for hdfc gateway

---
+++ app/gateway/Hdfc/Currency.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Gateway\Hdfc;
+
+class Currency
+{
+    const AED = "784";
+    const AUD = "036";
+    const CAD = "124";
+    const EUR = "978";
+    const GBP = "826";
+    const INR = "356";
+    const OMR = "512";
+    const QAR = "634";
+    const SGD = "702";
+    const USD = "840";
+}
