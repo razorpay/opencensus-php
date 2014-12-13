e78382331b05c1b10551e9d3173ff67b3f332d46
Shashank Kumar <shashankkumar.me@gmail.com>
2014-12-13 17:24:12 +0530

    Adds layout.php for more attractive demo

---
+++ public/test/layout.php
@@ -0,0 +1,106 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors','1');
+
+$checkout = 'https://checkout.razorpay.com';
+$protocol = 'https';
+$hostname = 'api.razorpay.com';
+
+if (file_exists('config.php'))
+{
+    require('config.php');
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="utf-8">
+    <link href="<?= $checkout ?>/v1/css/style.css" type="text/css" rel="stylesheet">
+
+    <script src="<?= $checkout ?>/v1/checkout.js" ></script>
+</head>
+<body>
+    <button class="btn" id="rzp-button1">Tron Legacy</button>
+    <button class="btn" id="rzp-button2">Alive (2007)</button>
+
+    <script>
+        var options = {
+            'key': 'rzp_test_1DP5mmOlF5G5ag',
+            'amount': '5100',
+            'name': 'Daft Punk',
+            'description': 'Tron Legacy',
+            'image': 'https://i.imgur.com/3g7nmJC.png',
+            'handler': function (transaction) {
+                alert("You have successfully purchased " + rzp1.options.description);
+            },
+            'protocol': 'http',
+            'hostname': 'rzp',
+            'prefill': {
+                'name': 'Harshil Mathur',
+                'email': 'harshil@razorpay.com',
+                'contact': '9999999999'
+            },
+            udf: {
+                'address': 'Hello World'
+            },
+            netbanking: true
+        }
+        var rzp1 = new Razorpay(options);
+
+        $('#rzp-button1').click(function(e) {
+            rzp1.open();
+            e.preventDefault();
+        });
+
+        $.extend(true, options, {
+            amount : 5200,
+            description : 'Alive (2007)',
+            image : 'https://i.imgur.com/GXalrU0.png',
+            handler : function (transaction){
+                alert("You have successfully purchased "+rzp2.options.description);
+            },
+            udf: {
+                'shipping': "Bye World"
+            }
+        });
+
+        var rzp2 = new Razorpay(options);
+
+        $('#rzp-button2').click(function(e){
+            rzp2.open();
+            e.preventDefault();
+        })
+    </script>
+
+    <style>
+
+    html {
+        background-image: url('http://www.youredm.com/wp-content/uploads/2014/03/Daft-Punk-Wallpaper-2560x1024-1.jpg');
+        background-size: cover cover;
+        /**background-position: center center fixed;*/
+        background-repeat: no-repeat;
+        background-position: -300px 0px;
+    }
+    .btn {
+        width: 20px;
+        background-color: #28B3D2;
+        color: white;
+        width: 200px;
+        border-radius: 3px;
+        height: 40px;
+        font-size: 20px;
+        text-align: center;
+        border: none;
+        position: absolute;
+        left: 45%;
+    }
+    #rzp-button2 {
+        top: 400px;
+    }
+    #rzp-button1 {
+        top: 350px;
+    }
+    </style>
+</body>
+</html>
