eb0af7041c3ec0dc2ed3b84101ac9cd6aa34e106
Shashank Kumar <shashankkumar.me@gmail.com>
2014-12-25 00:06:29 +0530

    Enables pricing rule addition for net-banking

--- app/models/Pricing/Validator.php
+++ app/models/Pricing/Validator.php
@@ -5,25 +5,54 @@ namespace Models\Pricing;
 use EE\Exception;
 use EE\Error\ErrorCode;
 use Models\Base;
+use Models\Payment;
 
 class Validator extends Base\Validator
 {
     protected static $addPlanRuleRules = array(
         Entity::GATEWAY             => 'sometimes|in:hdfc',
-        Entity::PAYMENT_MODE        => 'required|alpha|in:card',
+        Entity::PAYMENT_MODE        => 'required|alpha|in:card,net banking',
         Entity::PAYMENT_MODE_TYPE   => 'sometimes|in:debit,credit',
         Entity::PAYMENT_NETWORK     => 'sometimes|alpha|in:VISA,MC,DICL,RP,MAES',
         Entity::PAYMENT_ISSUER      => 'sometimes|alpha|max:10',
         Entity::PERCENT_RATE        => 'sometimes|numeric|max:10000',
         Entity::FIXED_RATE          => 'sometimes|numeric|max:100000');
 
-    protected static $addPlanRuleValidators = array('addPlanRuleExtras');
+    protected static $addPlanRuleValidators = array(
+        'addPlanRuleRate',
+        'addPlanRuleNB');
 
     protected static $createPlanRules = array(
         Entity::PLAN_NAME => 'required|alpha_num|max:20');
 
-    protected function validateAddPlanRuleExtras($input)
+    protected function validateAddPlanRuleNB($input)
     {
+        // Check that payment_mode_type is not defined when mode is net-banking
+        if ($input[Entity::PAYMENT_MODE] === Payment\Method::NET_BANKING)
+        {
+            $fields = array(
+                Entity::PAYMENT_MODE_TYPE,
+                Entity::PAYMENT_NETWORK,
+                Entity::PAYMENT_ISSUER);
+
+            foreach ($fields as $field)
+            {
+                if (isset($input[$field]) and
+                    $input[$field] !== null)
+                {
+                    throw new Exception\BadRequestException(
+                        ErrorCode::BAD_REQUEST_PRICING_FIELD_NOT_REQUIRED_FOR_NB,
+                        $field);
+                }
+            }
+        }
+    }
+
+    protected function validateAddPlanRuleRate($input)
+    {
+        //
+        // At least one of percent_rate and fixed_rate has to be specified
+        //
         if ((isset($input[Entity::PERCENT_RATE]) === false) and
             (isset($input[Entity::FIXED_RATE]) === false))
         {
@@ -34,6 +63,7 @@ class Validator extends Base\Validator
 
     public function createPlanValidate($input)
     {
+        // If no plan name, then set it to null
         $planInput[Entity::PLAN_NAME] =
             (isset($input[Entity::PLAN_NAME])) ? $input[Entity::PLAN_NAME] : null;
 
@@ -48,6 +78,7 @@ class Validator extends Base\Validator
     {
         $this->validateInput('addPlanRule', $input);
 
+        // The plan should already have at leats one rule
         if ($plan->count() === 0)
         {
             throw new Exception\LogicException(
@@ -56,8 +87,10 @@ class Validator extends Base\Validator
 
         $rule = $plan->first();
 
+        // The input and pricing rule (any) gateway should match
         $this->matchGateway($rule, $input);
 
+        // The input should not match any existing rule
         $this->matchPaymentRules($plan, $input);
     }
 
@@ -76,6 +109,9 @@ class Validator extends Base\Validator
         }
     }
 
+    /**
+     * Check whether this new rule already exists
+     */
     public function matchPaymentRules($plan, $input)
     {
         $rules = $plan->toArray();
