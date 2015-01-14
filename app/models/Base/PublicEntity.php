3f8c658b23cbd7bc8e6e5f9ea264698088483d5e
Shashank Kumar <shashankkumar.me@gmail.com>
2015-01-13 18:12:35 -0800

    Changes delimiter to _ from -

--- app/models/Base/PublicEntity.php
+++ app/models/Base/PublicEntity.php
@@ -13,7 +13,7 @@ class PublicEntity extends UniqueIdEntity
 
     protected static $sign = '';
 
-    protected static $delimiter = '-';
+    protected static $delimiter = '_';
 
     /**
      * For an entity which is being exposed outside,
