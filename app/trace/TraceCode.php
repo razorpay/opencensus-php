07d01f0536dc36c8acf184c7a3b0a76e041c7cad
Shashank Kumar <shashankkumar.me@gmail.com>
2015-01-18 09:15:48 -0800

    Aligns trace codes indentation

--- app/trace/TraceCode.php
+++ app/trace/TraceCode.php
@@ -10,49 +10,49 @@ class TraceCode
      * Payment component error messages
      */
 
-    const PAYMENT_NEW_REQUEST           = 'PAYMENT_NEW_REQUEST';
-    const PAYMENT_CREATED               = 'PAYMENT_CREATED';
-    const PAYMENT_CREATE_FAILED         = 'PAYMENT_CREATE_FAILED';
-    const PAYMENT_AUTH_SUCCESS          = 'PAYMENT_AUTH_SUCCESS';
-    const PAYMENT_AUTH_FAILURE          = 'PAYMENT_AUTH_FAILURE';
-    const PAYMENT_REFUND_SUCCESS        = 'PAYMENT_REFUND_SUCCESS';
-    const PAYMENT_REFUND_FAILURE        = 'PAYMENT_REFUND_FAILURE';
-    const PAYMENT_CAPTURE_SUCCESS       = 'PAYMENT_CAPTURE_SUCCESS';
-    const PAYMENT_CAPTURE_FAILURE       = 'PAYMENT_CAPTURE_FAILURE';
+    const PAYMENT_NEW_REQUEST                       = 'PAYMENT_NEW_REQUEST';
+    const PAYMENT_CREATED                           = 'PAYMENT_CREATED';
+    const PAYMENT_CREATE_FAILED                     = 'PAYMENT_CREATE_FAILED';
+    const PAYMENT_AUTH_SUCCESS                      = 'PAYMENT_AUTH_SUCCESS';
+    const PAYMENT_AUTH_FAILURE                      = 'PAYMENT_AUTH_FAILURE';
+    const PAYMENT_REFUND_SUCCESS                    = 'PAYMENT_REFUND_SUCCESS';
+    const PAYMENT_REFUND_FAILURE                    = 'PAYMENT_REFUND_FAILURE';
+    const PAYMENT_CAPTURE_SUCCESS                   = 'PAYMENT_CAPTURE_SUCCESS';
+    const PAYMENT_CAPTURE_FAILURE                   = 'PAYMENT_CAPTURE_FAILURE';
+    const PAYMENT_FAILED                            = 'PAYMENT_FAILED';
 
-    const PAYMENT_FAILED                = 'PAYMENT_FAILED';
     /*
      * Gateway component error messages
      */
 
-    const GATEWAY_ENROLL_REQUEST            = 'GATEWAY_ENROLL_REQUEST';
-    const GATEWAY_ENROLL_RESPONSE           = 'GATEWAY_ENROLL_RESPONSE';
-    const GATEWAY_ENROLL_ERROR              = 'GATEWAY_ENROLL_ERROR';
-    const GATEWAY_NOT_ENROLLED_REQUEST      = 'GATEWAY_NOT_ENROLLED_REQUEST';
-    const GATEWAY_NOT_ENROLLED_RESPONSE     = 'GATEWAY_NOT_ENROLLED_RESPONSE';
-    const GATEWAY_NOT_ENROLLED_ERROR        = 'GATEWAY_NOT_ENROLLED_ERROR';
-    const GATEWAY_ENROLLED_AUTH_REQUEST     = 'GATEWAY_ENROLLED_AUTH_REQUEST';
-    const GATEWAY_ENROLLED_AUTH_RESPONSE    = 'GATEWAY_ENROLLED_AUTH_RESPONSE';
-    const GATEWAY_ENROLLED_AUTH_ERROR       = 'GATEWAY_ENROLLED_AUTH_ERROR';
-    const GATEWAY_SUPPORT_REQUEST           = 'GATEWAY_SUPPORT_REQUEST';
-    const GATEWAY_SUPPORT_RESPONSE          = 'GATEWAY_SUPPORT_RESPONSE';
-    const GATEWAY_SUPPORT_ERROR             = 'GATEWAY_SUPPORT_ERROR';
-    const GATEWAY_UNKNOWN_ERROR             = 'GATEWAY_UNKNOWN_ERROR';
+    const GATEWAY_ENROLL_REQUEST                    = 'GATEWAY_ENROLL_REQUEST';
+    const GATEWAY_ENROLL_RESPONSE                   = 'GATEWAY_ENROLL_RESPONSE';
+    const GATEWAY_ENROLL_ERROR                      = 'GATEWAY_ENROLL_ERROR';
+    const GATEWAY_NOT_ENROLLED_REQUEST              = 'GATEWAY_NOT_ENROLLED_REQUEST';
+    const GATEWAY_NOT_ENROLLED_RESPONSE             = 'GATEWAY_NOT_ENROLLED_RESPONSE';
+    const GATEWAY_NOT_ENROLLED_ERROR                = 'GATEWAY_NOT_ENROLLED_ERROR';
+    const GATEWAY_ENROLLED_AUTH_REQUEST             = 'GATEWAY_ENROLLED_AUTH_REQUEST';
+    const GATEWAY_ENROLLED_AUTH_RESPONSE            = 'GATEWAY_ENROLLED_AUTH_RESPONSE';
+    const GATEWAY_ENROLLED_AUTH_ERROR               = 'GATEWAY_ENROLLED_AUTH_ERROR';
+    const GATEWAY_SUPPORT_REQUEST                   = 'GATEWAY_SUPPORT_REQUEST';
+    const GATEWAY_SUPPORT_RESPONSE                  = 'GATEWAY_SUPPORT_RESPONSE';
+    const GATEWAY_SUPPORT_ERROR                     = 'GATEWAY_SUPPORT_ERROR';
+    const GATEWAY_UNKNOWN_ERROR                     = 'GATEWAY_UNKNOWN_ERROR';
 
-    const ERROR_EXCEPTION                   = 'ERROR_EXCEPTION';
+    const ERROR_EXCEPTION                           = 'ERROR_EXCEPTION';
 
     protected static $messages = array(
-        self::PAYMENT_NEW_REQUEST               => 'Request for new payment received',
-        self::PAYMENT_CREATED                   => 'New payment created',
-        self::PAYMENT_CREATE_FAILED             => 'Payment creation failed',
-        self::PAYMENT_AUTH_SUCCESS              => 'Payment authenticated successfully',
-        self::PAYMENT_AUTH_FAILURE              => 'Payment auth failed',
-        self::PAYMENT_FAILED                    => 'Payment failed',
-        self::PAYMENT_REFUND_SUCCESS            => 'Payment refunded successfully',
-        self::PAYMENT_REFUND_FAILURE            => 'Payment refund failed',
-        self::PAYMENT_CAPTURE_SUCCESS           => 'Payment captured successfully',
-        self::PAYMENT_CAPTURE_FAILURE           => 'Payment capture failed',
-        self::PAYMENT_FAILED                    => 'Payment failed',
+        self::PAYMENT_NEW_REQUEST                   => 'Request for new payment received',
+        self::PAYMENT_CREATED                       => 'New payment created',
+        self::PAYMENT_CREATE_FAILED                 => 'Payment creation failed',
+        self::PAYMENT_AUTH_SUCCESS                  => 'Payment authenticated successfully',
+        self::PAYMENT_AUTH_FAILURE                  => 'Payment auth failed',
+        self::PAYMENT_FAILED                        => 'Payment failed',
+        self::PAYMENT_REFUND_SUCCESS                => 'Payment refunded successfully',
+        self::PAYMENT_REFUND_FAILURE                => 'Payment refund failed',
+        self::PAYMENT_CAPTURE_SUCCESS               => 'Payment captured successfully',
+        self::PAYMENT_CAPTURE_FAILURE               => 'Payment capture failed',
+        self::PAYMENT_FAILED                        => 'Payment failed',
 
         self::GATEWAY_ENROLL_REQUEST                => 'Request for enrollment sent',
         self::GATEWAY_ENROLL_RESPONSE               => 'Enrollment response received',
