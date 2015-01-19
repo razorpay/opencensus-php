01634932add56b2422420ef23417080b2aa19ce5
Harshil Mathur <harshilmathur@gmail.com>
2015-01-18 18:08:22 -0800

    Adds addfund ctrl and tmpl

---
+++ public/js/merchant/controllers/addfundsCtrl.js
@@ -0,0 +1,116 @@
+//Add Funds Controller
+app.controller('AddfundsCtrl', ['$scope', '$http', 'alertsFactory','user', 'modeFactory', 'uiLoad', 'transformRequestAsFormPost',
+  function($scope, $http, alertsFactory, user, modeFactory, uiLoad, transformRequestAsFormPost){
+    $scope.mode = modeFactory.getMode();
+
+    $scope.alerts = alertsFactory.getHandler();
+
+
+    $scope.options = {
+        'key': '',
+        'amount': '50000',
+        'name': '',
+        'description': 'Add Funds to Account',
+        'image': '',
+        'handler': function(transaction){
+          $scope.transactionHandler(transaction);
+        },
+        'prefill': {
+            'name': '',
+            'email': '',
+            'contact': ''
+        },
+        notes: {
+            'dashboard': 'true'
+        },
+        netbanking: true
+    }
+    
+    user.identity().then(function(data){
+      $scope.options.name = data.name;
+      $scope.options.prefill.email = data.email;
+    });
+
+    if(window.location.hostname == "betadashboard.razorpay.com") {
+      uiLoad.loadScript("https://betacheckout.razorpay.com/v1/checkout.js");
+      $scope.options.hostname = "beta.razorpay.com";
+    }
+    else if(window.location.hostname == "dashboard.razorpay.com") {
+      uiLoad.loadScript("https://checkout.razorpay.com/v1/checkout.js");
+    }
+    else {
+      uiLoad.loadScript("https://checkout.razorpay.com/v1/checkout.js");
+      
+      $http.get('/apihost').success(function(data){
+        var host = data.data.split( '/' );
+        $scope.options.protocol = host[0].substring(0, host[0].length - 1);
+        $scope.options.hostname = host[2];
+      });
+
+    }
+    
+
+    $scope.addFunds = function() {
+      console.log($scope.options);
+      try{
+        var rzp1 = new Razorpay($scope.options);
+        rzp1.open();
+      }
+      catch(e)
+      {
+        $scope.alerts.addAlert("danger", "An error occured - " + e.message, true);
+      }
+    }
+
+    fetchKey();
+
+    function fetchKey(){
+      var request = $http.get('/'+$scope.mode+'/keys');
+
+      request
+      .success(function(data){     
+        if(data.success) {
+          if(data.data.count > 0) {
+            $scope.options.key = data.data.items[0].id;
+          }
+          else {
+            $scope.alerts.addAlert('danger', "No valid api keys found, check Api Keys page.", true);
+          }
+        }
+        else {
+          $scope.alerts.addAlert('danger', null, true);
+        }
+      })
+      .error(function(){
+        $scope.alerts.addAlert('danger', null, true);
+      })
+    }
+
+    $scope.transactionHandler = function(transaction)
+    {
+      transaction.amount = $scope.options.amount;
+
+      var request = $http({
+                    method: "post",
+                    url: "/" + modeFactory.getMode() + "/addfunds",
+                    transformRequest: transformRequestAsFormPost,
+                    data: transaction
+      });
+
+      request
+      .success(function(data){
+        if(data.success) {
+          $scope.alerts.addAlert('success', "Funds added successfully", true);
+        }
+        else {
+          $scope.alerts.resetAlerts();
+          angular.foreach(data.errors, function(value, key){
+            $scope.alerts.addAlert('danger', value);
+          });
+        }
+      })
+      .error(function(){
+        $scope.alerts.addAlert('danger', null, true);
+      });
+    };
+}]);
