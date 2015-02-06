c81753422aa6be3b41f1526ff68bebf185f5d8c0
Shashank Kumar <shashankkumar.me@gmail.com>
2015-02-05 23:21:11 -0800

    Improves alignment of public error code descriptions

--- app/ee/Error/PublicErrorDescription.php
+++ app/ee/Error/PublicErrorDescription.php
@@ -4,206 +4,71 @@ namespace EE\Error;
 
 class PublicErrorDescription
 {
-    const GATEWAY_ERROR =
-        'There is a problem with the gateway causing the payment to fail';
-
-    const SERVER_ERROR =
-        'The server encountered an error. The incident has been reported to admins';
-
-    const GATEWAY_ERROR_REQUEST_TIMEOUT =
-        'The gateway request to submit payment information timed out. Please submit your details again';
-
-    const GATEWAY_ERROR_PROCESSING_DECLINED =
-        'Payment failed due to processing error on gateway';
-
-    const BAD_REQUEST_PAYMENT_FAILED =
-        'Payment failed';
-
-    const BAD_REQUEST_PAYMENT_ALREADY_PROCCESSED =
-        'The payment has already been processed. Did you press the back button in browser?';
-
-    const BAD_REQUEST_PAYMENT_ALREADY_CAPTURED =
-        'This payment has already been captured';
-
-    const BAD_REQUEST_PAYMENT_CURRENCY_NOT_SUPPORTED =
-        'Invalid currency. Currently only INR is supported.';
-
-    const BAD_REQUEST_PAYMENT_STATUS_NOT_CAPTURED =
-        'The payment status should be captured for refund action to be taken';
-
-    const BAD_REQUEST_PAYMENT_FULLY_REFUNDED =
-        'The payment has been fully refunded already';
-
-    const BAD_REQUEST_PAYMENT_REFUND_AMOUNT_GREATER_THAN_CAPTURED =
-        'The refund amount proivded is greater than amount captured';
-
-    const BAD_REQUEST_PAYMENT_REFUND_AMOUNT_GREATER_THAN_UNREFUNDED =
-        'The refund amount provided is greater than the unrefunded amount';
-
-    const BAD_REQUEST_PAYMENT_AMOUNT_LESS_THAN_MIN_AMOUNT =
-        'Minimum transaction amount allowed is Rs 50';
-
-    const BAD_REQUEST_PAYMENT_ATOM_NET_BANKING_MIN_AMOUNT_FIFTY =
-        'Minimum amount allowed for net banking transaction for the merchant is INR 50';
-
-    const BAD_REQUEST_PAYMENT_CARD_IS_NOT_ARRAY =
-        'Card provided is not a dictionary';
-
-    const BAD_REQUEST_PAYMENT_CARD_NOT_PROVIDED =
-        'Payment Exception: Card not provided';
-
-    const BAD_REQUEST_PAYMENT_NET_BANKING_NOT_ENABLED =
-        'Net banking is not enabled for the merchant';
-
-    const BAD_REQUEST_PAYMENT_BANK_NOT_PROVIDED =
-        'Bank not provided for net banking payment';
-
-    const BAD_REQUEST_PAYMENT_INVALID_BANK_CODE =
-        'Bank code provided for net banking payment is invalid';
-
-    const BAD_REQUEST_PAYMENT_CONTACT_NOT_DIGITS =
-        'Contact number can only contain digits and + symbol';
-
-    const BAD_REQUEST_PAYMENT_CONTACT_MIN_TEN_DIGITS =
-        'Contact number should be at least 10 digits';
-
-    const BAD_REQUEST_PAYMENT_CONTACT_MAX_TWELVE_DIGITS =
-        'Contact number should not be greater than 12 digits, including country code';
-
-    const BAD_REQUEST_PAYMENT_CARD_NETWORK_NOT_SUPPORTED =
-        'Card network not supported curently';
-
-    const BAD_REQUEST_PAYMENT_CARD_DECLINED =
-        'Card decline by bank';
-
-    const BAD_REQUEST_PAYMENT_CARD_INVALID_EXPIRY_DATE =
-        'Expiry date is not valid';
-
-    const BAD_REQUEST_PAYMENT_TIMED_OUT =
-        'Paymend was not completed on time.';
-
-    const BAD_REQUEST_INVALID_ID =
-        'The id provided does not exist';
-
-    const BAD_REQUEST_PAYMENT_CAPTURE_AMOUNT_NOT_EQUAL_TO_AUTH =
-        'Capture amount must be equal to the amount authorized';
-
-    const BAD_REQUEST_KEY_EXPIRED =
-        'Key is expired';
-
-    const BAD_REQUEST_KEY_EXPIRING_SOON =
-        'Key is already set to expire soon';
-
-    CONST BAD_REQUEST_KEY_OF_DEMO_ACCOUNT =
-        'Operation failed for the key because it\'s of a demo account';
-
-    const BAD_REQUEST_URL_NOT_FOUND =
-        'The requested URL was not found on the server.';
-
-    const BAD_REQUEST_PAYMENT_CAPTURE_ONLY_AUTHORIZED =
-        'Only payments which have been authorized and not yet captured can be captured';
-
-    const BAD_REQUEST_NOTES_TOO_MANY_KEYS =
-        'Number of fields in notes should be less than or equal to 15';
-
-    const BAD_REQUEST_NOTES_VALUE_CANNOT_BE_ARRAY =
-        'Notes values themselves should not be an array';
-
-    const BAD_REQUEST_NOTES_KEY_TOO_LARGE =
-        'Notes key cannot be greater 255 characters';
-
-    const BAD_REQUEST_NOTES_VALUE_TOO_LARGE =
-        'Notes value cannot be greater 255 characters';
-
-    const BAD_REQUEST_DESCRIPTION_SHOULD_BE_STRING =
-        'Description provides should be string';
-
-    const BAD_REQUEST_DESCRIPTION_TOO_LARGE =
-        'Description provides should be max 1000 characters';
-
-    const BAD_REQUEST_NOTES_SHOULD_BE_ARRAY =
-        'Notes should be provided as a dictionary';
-
-    const BAD_REQUEST_ONLY_HTTPS_ALLOWED =
-        'Razorpay API is only available over HTTPS.';
-
-    const BAD_REQUEST_UNAUTHORIZED_BASICAUTH_EXPECTED =
-        'Please provide your api key for authentication purposes.';
-
-    const BAD_REQUEST_UNAUTHORIZED_INVALID_API_KEY =
-        'The api key provided is invalid';
-
-    const BAD_REQUEST_UNAUTHORIZED_INVALID_API_SECRET =
-        'The api secret provided is invalid';
-
-    const BAD_REQUEST_UNAUTHORIZED_SECRET_NOT_PROVIDED =
-        'Please provide api secret';
-
-    const BAD_REQUEST_UNAUTHORIZED_SECRET_SENT_ON_PUBLIC_ROUTE =
-        'Please do not provide your secret on public sided requests';
-
-    const BAD_REQUEST_UNAUTHORIZED_API_KEY_NOT_PROVIDED =
-        'Please provide your Razorpay Api Key Id';
-
-    const BAD_REQUEST_PRICING_ID_REQURED =
-        'Pricing plan id is required';
-
-    const BAD_REQUEST_PRICING_PLAN_ALREADY_EXISTS =
-        'Pricing plan name already exists. Are you trying a pricing plan rule instead?';
-
-    const BAD_REQUEST_PRICING_RATE_NOT_DEFINED =
-        'One of percent_rate and fixed_rate must be present';
-
-    const BAD_REQUEST_PRICING_GATEWAY_REQUIRED =
-        'This plan has a gateway set. Please provide it in input';
-
-    const BAD_REQUEST_PRICING_RULE_ALREADY_DEFINED =
-        'The new rule matches with an active existing rule';
-
-    const BAD_REQUEST_PRICING_PLAN_WITH_SAME_NAME_EXISTS =
-        'Pricing plan name already exists. Are you trying a pricing plan rule instead?';
-
-    const BAD_REQUEST_PRICING_NOT_DEFINED_FOR_MERCHANT =
-        'The merchant does not have pricing assigned';
-
-    const BAD_REQUEST_PRICING_FIELD_NOT_REQUIRED_FOR_NB =
-        'The field should be null for net-banking';
-
-    const BAD_REQUEST_MERCHANT_ALREADY_ACTIVATED =
-        'The merchant has already been activated';
-
-    const BAD_REQUEST_MERCHANT_NOT_ACTIVATED =
-        'The merchant has not been activated. This action can only be taken for activated merchants';
-
-    const BAD_REQUEST_MERCHANT_ALREADY_LIVE =
-        'The merchant is already live';
-
-    const BAD_REQUEST_MERCHANT_NOT_LIVE =
-        'The merchant is not live currently';
-
-    const BAD_REQUEST_MERCHANT_NOT_LIVE_ACTION_DENIED =
-        'There is a temporary block placed on the account currently because of which new payment operations are on put on hold.
-         If you are seeing this message unexpectedly, please drop a mail to contact@razorpay.com with your email-id and
-         we will look into the issue immediately.';
-
-    const BAD_REQUEST_MERCHANT_NO_TERMINAL_ASSIGNED =
-        'The merchant has no pricing assigned';
-
-    const BAD_REQUEST_MERCHANT_KEY_ALREADY_CREATED =
-        'The merchant keys have already been created';
-
-    const BAD_REQUEST_MERCHANT_NO_BANK_ACCOUNT_FOUND =
-        'The merchant has not yet provided his bank account details';
-
-    const BAD_REQUEST_MERCHANT_BANK_ACCOUNT_ALREADY_PROVIDED =
-        'The merchant already has provided a bank account';
-
-    const BAD_REQUEST_GATEWAY_TERMINAL_ONLY_TWO_ALLOWED =
-        'Currently only two terminals are allowed per merchant';
-
-    const BAD_REQUEST_GATEWAY_MERCHANT_ID_EXISTS =
-        'A record with same gateway merchant id (mid) exists';
-
-    const BAD_REQUEST_MERCHANT_TERMINAL_EXISTS_FOR_GATEWAY =
-        'A terminal for this gateway for this merchant already exists';
-}
+    const GATEWAY_ERROR                                                 = 'There is a problem with the gateway causing the payment to fail';
+    const SERVER_ERROR                                                  = 'The server encountered an error. The incident has been reported to admins';
+    const GATEWAY_ERROR_REQUEST_TIMEOUT                                 = 'The gateway request to submit payment information timed out. Please submit your details again';
+    const GATEWAY_ERROR_PROCESSING_DECLINED                             = 'Payment failed due to processing error on gateway';
+
+    const BAD_REQUEST_URL_NOT_FOUND                                     = 'The requested URL was not found on the server.';
+    const BAD_REQUEST_ONLY_HTTPS_ALLOWED                                = 'Razorpay API is only available over HTTPS.';
+    const BAD_REQUEST_INVALID_ID                                        = 'The id provided does not exist';
+    const BAD_REQUEST_PAYMENT_FAILED                                    = 'Payment failed';
+    const BAD_REQUEST_PAYMENT_ALREADY_PROCCESSED                        = 'The payment has already been processed. Did you press the back button in browser?';
+    const BAD_REQUEST_PAYMENT_ALREADY_CAPTURED                          = 'This payment has already been captured';
+    const BAD_REQUEST_PAYMENT_CURRENCY_NOT_SUPPORTED                    = 'Invalid currency. Currently only INR is supported.';
+    const BAD_REQUEST_PAYMENT_STATUS_NOT_CAPTURED                       = 'The payment status should be captured for refund action to be taken';
+    const BAD_REQUEST_PAYMENT_FULLY_REFUNDED                            = 'The payment has been fully refunded already';
+    const BAD_REQUEST_PAYMENT_REFUND_AMOUNT_GREATER_THAN_CAPTURED       = 'The refund amount proivded is greater than amount captured';
+    const BAD_REQUEST_PAYMENT_REFUND_AMOUNT_GREATER_THAN_UNREFUNDED     = 'The refund amount provided is greater than the unrefunded amount';
+    const BAD_REQUEST_PAYMENT_AMOUNT_LESS_THAN_MIN_AMOUNT               = 'Minimum transaction amount allowed is Rs 50';
+    const BAD_REQUEST_PAYMENT_ATOM_NET_BANKING_MIN_AMOUNT_FIFTY         = 'Minimum amount allowed for net banking transaction for the merchant is INR 50';
+    const BAD_REQUEST_PAYMENT_CARD_IS_NOT_ARRAY                         = 'Card provided is not a dictionary';
+    const BAD_REQUEST_PAYMENT_CARD_NOT_PROVIDED                         = 'Payment Exception: Card not provided';
+    const BAD_REQUEST_PAYMENT_NET_BANKING_NOT_ENABLED                   = 'Net banking is not enabled for the merchant';
+    const BAD_REQUEST_PAYMENT_BANK_NOT_PROVIDED                         = 'Bank not provided for net banking payment';
+    const BAD_REQUEST_PAYMENT_INVALID_BANK_CODE                         = 'Bank code provided for net banking payment is invalid';
+    const BAD_REQUEST_PAYMENT_CONTACT_NOT_DIGITS                        = 'Contact number can only contain digits and + symbol';
+    const BAD_REQUEST_PAYMENT_CONTACT_MIN_TEN_DIGITS                    = 'Contact number should be at least 10 digits';
+    const BAD_REQUEST_PAYMENT_CONTACT_MAX_TWELVE_DIGITS                 = 'Contact number should not be greater than 12 digits, including country code';
+    const BAD_REQUEST_PAYMENT_CARD_NETWORK_NOT_SUPPORTED                = 'Card network not supported curently';
+    const BAD_REQUEST_PAYMENT_CARD_DECLINED                             = 'Card decline by bank';
+    const BAD_REQUEST_PAYMENT_CARD_INVALID_EXPIRY_DATE                  = 'Expiry date is not valid';
+    const BAD_REQUEST_PAYMENT_TIMED_OUT                                 = 'Paymend was not completed on time.';
+    const BAD_REQUEST_PAYMENT_CAPTURE_AMOUNT_NOT_EQUAL_TO_AUTH          = 'Capture amount must be equal to the amount authorized';
+    const BAD_REQUEST_KEY_EXPIRED                                       = 'Key is expired';
+    const BAD_REQUEST_KEY_EXPIRING_SOON                                 = 'Key is already set to expire soon';
+    const BAD_REQUEST_KEY_OF_DEMO_ACCOUNT                               = 'Operation failed for the key because it\'s of a demo account';
+    const BAD_REQUEST_PAYMENT_CAPTURE_ONLY_AUTHORIZED                   = 'Only payments which have been authorized and not yet captured can be captured';
+    const BAD_REQUEST_NOTES_TOO_MANY_KEYS                               = 'Number of fields in notes should be less than or equal to 15';
+    const BAD_REQUEST_NOTES_VALUE_CANNOT_BE_ARRAY                       = 'Notes values themselves should not be an array';
+    const BAD_REQUEST_NOTES_KEY_TOO_LARGE                               = 'Notes key cannot be greater 255 characters';
+    const BAD_REQUEST_NOTES_VALUE_TOO_LARGE                             = 'Notes value cannot be greater 255 characters';
+    const BAD_REQUEST_DESCRIPTION_SHOULD_BE_STRING                      = 'Description provides should be string';
+    const BAD_REQUEST_DESCRIPTION_TOO_LARGE                             = 'Description provides should be max 1000 characters';
+    const BAD_REQUEST_NOTES_SHOULD_BE_ARRAY                             = 'Notes should be provided as a dictionary';
+    const BAD_REQUEST_UNAUTHORIZED_BASICAUTH_EXPECTED                   = 'Please provide your api key for authentication purposes.';
+    const BAD_REQUEST_UNAUTHORIZED_INVALID_API_KEY                      = 'The api key provided is invalid';
+    const BAD_REQUEST_UNAUTHORIZED_INVALID_API_SECRET                   = 'The api secret provided is invalid';
+    const BAD_REQUEST_UNAUTHORIZED_SECRET_NOT_PROVIDED                  = 'Please provide api secret';
+    const BAD_REQUEST_UNAUTHORIZED_SECRET_SENT_ON_PUBLIC_ROUTE          = 'Please do not provide your secret on public sided requests';
+    const BAD_REQUEST_UNAUTHORIZED_API_KEY_NOT_PROVIDED                 = 'Please provide your Razorpay Api Key Id';
+    const BAD_REQUEST_PRICING_ID_REQURED                                = 'Pricing plan id is required';
+    const BAD_REQUEST_PRICING_PLAN_ALREADY_EXISTS                       = 'Pricing plan name already exists. Are you trying a pricing plan rule instead?';
+    const BAD_REQUEST_PRICING_RATE_NOT_DEFINED                          = 'One of percent_rate and fixed_rate must be present';
+    const BAD_REQUEST_PRICING_GATEWAY_REQUIRED                          = 'This plan has a gateway set. Please provide it in input';
+    const BAD_REQUEST_PRICING_RULE_ALREADY_DEFINED                      = 'The new rule matches with an active existing rule';
+    const BAD_REQUEST_PRICING_PLAN_WITH_SAME_NAME_EXISTS                = 'Pricing plan name already exists. Are you trying a pricing plan rule instead?';
+    const BAD_REQUEST_PRICING_NOT_DEFINED_FOR_MERCHANT                  = 'The merchant does not have pricing assigned';
+    const BAD_REQUEST_PRICING_FIELD_NOT_REQUIRED_FOR_NB                 = 'The field should be null for net-banking';
+    const BAD_REQUEST_MERCHANT_ALREADY_ACTIVATED                        = 'The merchant has already been activated';
+    const BAD_REQUEST_MERCHANT_NOT_ACTIVATED                            = 'The merchant has not been activated. This action can only be taken for activated merchants';
+    const BAD_REQUEST_MERCHANT_ALREADY_LIVE                             = 'The merchant is already live';
+    const BAD_REQUEST_MERCHANT_NOT_LIVE                                 = 'The merchant is not live currently';
+    const BAD_REQUEST_MERCHANT_NOT_LIVE_ACTION_DENIED                   = 'There is a temporary block placed on the account currently because of which new payment operations are on put on hold.          If you are seeing this message unexpectedly, please drop a mail to contact@razorpay.com with your email-id and we will look into the issue immediately.';
+    const BAD_REQUEST_MERCHANT_NO_TERMINAL_ASSIGNED                     = 'The merchant has no pricing assigned';
+    const BAD_REQUEST_MERCHANT_KEY_ALREADY_CREATED                      = 'The merchant keys have already been created';
+    const BAD_REQUEST_MERCHANT_NO_BANK_ACCOUNT_FOUND                    = 'The merchant has not yet provided his bank account details';
+    const BAD_REQUEST_MERCHANT_BANK_ACCOUNT_ALREADY_PROVIDED            = 'The merchant already has provided a bank account';
+    const BAD_REQUEST_GATEWAY_TERMINAL_ONLY_TWO_ALLOWED                 = 'Currently only two terminals are allowed per merchant';
+    const BAD_REQUEST_GATEWAY_MERCHANT_ID_EXISTS                        = 'A record with same gateway merchant id (mid) exists';
+    const BAD_REQUEST_MERCHANT_TERMINAL_EXISTS_FOR_GATEWAY              = 'A terminal for this gateway for this merchant already exists'; }
