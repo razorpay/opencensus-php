adab57294c720ff7ca5c161bc4ea3fb9842e5127
Shashank Kumar <shashankkumar.me@gmail.com>
2015-02-18 01:34:12 -0800

    Fixes typo bug

--- app/models/Payment/Processor/TerminalPicker.php
+++ app/models/Payment/Processor/TerminalPicker.php
@@ -88,7 +88,7 @@ class TerminalPicker
         {
             if (($hdfcTerm !== null) and
                 ($hdfcTerm->isCardEnabled()))
-                $terminal = $atomTerm;
+                $terminal = $hdfcTerm;
             else if (($atomTerm !== null) and
                      ($atomTerm->isCardEnabled()))
                 $terminal = $atomTerm;
