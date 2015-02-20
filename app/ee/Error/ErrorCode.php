84faa6c700bca10eb41a8b8721a8e39844aa8104
Shashank Kumar <shashankkumar.me@gmail.com>
2015-02-19 18:01:58 -0800

    Fixes MethodNotAllowedHttpException errors that have been occuring.

--- app/ee/Error/ErrorCode.php
+++ app/ee/Error/ErrorCode.php
@@ -66,6 +66,7 @@ class ErrorCode
 // Generic bad requests
     const BAD_REQUEST_INVALID_ID                                            = 'BAD_REQUEST_INVALID_ID';
     const BAD_REQUEST_ONLY_HTTPS_ALLOWED                                    = 'BAD_REQUEST_ONLY_HTTPS_ALLOWED';
+    const BAD_REQUEST_HTTP_METHOD_NOT_ALLOWED                               = 'BAD_REQUEST_HTTP_METHOD_NOT_ALLOWED';
     const BAD_REQUEST_ERROR                                                 = 'BAD_REQUEST_ERROR';
     const BAD_REQUEST_EXTRA_FIELDS_PROVIDED                                 = 'BAD_REQUEST_EXTRA_FIELDS_PROVIDED';
     const BAD_REQUEST_VALIDATION_FAILURE                                    = 'BAD_REQUEST_VALIDATION_FAILURE';
