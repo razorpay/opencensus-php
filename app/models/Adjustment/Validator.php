f9df929a00fc6018fbfc1502e489a85a739c6bc0
Shashank Kumar <shashankkumar.me@gmail.com>
2015-03-20 02:16:27 -0700

    Makes currency optional for adding adjustment. By default, it's INR

--- app/models/Adjustment/Validator.php
+++ app/models/Adjustment/Validator.php
@@ -8,7 +8,7 @@ class Validator extends Base\Validator
 {
     protected static $createRules = array(
         Entity::AMOUNT        =>  'required|integer',
-        Entity::CURRENCY      =>  'required|in:INR',
+        Entity::CURRENCY      =>  'sometimes|in:INR',
         Entity::DESCRIPTION   =>  'required|min:10',
     );
 }
