d357b6e07f4b90d72c8e3af59f8972c0c2bff9af
Shashank Kumar <shashankkumar.me@gmail.com>
2015-04-23 21:46:45 -0700

    Fixing atom payment verify test

--- app/gateway/Atom/Gateway.php
+++ app/gateway/Atom/Gateway.php
@@ -142,7 +142,7 @@ class Gateway extends BaseGateway
         }
         else
         {
-            $status = ($payment['success'] === '1') ? true : false;
+            $status = ((int) $payment['success'] === 1) ? true : false;
 
             if (($status === false) and
                 ($atomStatus === false))
@@ -153,7 +153,7 @@ class Gateway extends BaseGateway
             {
                 if (($status === true) and
                     ($atomStatus === true) and
-                    ($content['BID'] === $payment['bank_payment_id']))
+                    ($content['BID'] === (string) $payment['bank_payment_id']))
                 {
                     ;
                 }
