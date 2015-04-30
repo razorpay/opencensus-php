988adb8d6900379daf4b45d8980e3eb97731b3f1
Shashank Kumar <shashankkumar.me@gmail.com>
2015-04-30 07:41:33 +0530

    Adds geniuspay querydr url

--- app/gateway/Genius/Url.php
+++ app/gateway/Genius/Url.php
@@ -4,9 +4,10 @@ namespace Gateway\Genius;
 
 class Url
 {
-    const TEST_DOMAIN  = 'https://uat-geniusepay.in';
-    const LIVE_DOMAIN  = 'https://geniuspay.in';
+    const TEST_DOMAIN   = 'https://uat-geniusepay.in';
+    const LIVE_DOMAIN   = 'https://geniuspay.in';
 
-    const PAY          = '/VAS/DCC/do.action';
-    const AMA          = '/VAS/rac';
+    const PAY           = '/VAS/DCC/do.action';
+    const AMA           = '/VAS/rac';
+    const QUERY         = '/VAS/QueryDR';
 }
