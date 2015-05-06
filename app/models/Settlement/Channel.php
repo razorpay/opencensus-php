251f3c5ddb403c0186ea68b5bed7c633f161f54d
Shashank Kumar <shashankkumar.me@gmail.com>
2015-05-06 18:46:35 +0530

    Corrects setl channel, fixes typo errors

--- app/models/Settlement/Channel.php
+++ app/models/Settlement/Channel.php
@@ -12,8 +12,9 @@ class Channel
     public static $gateways = array(
         self::KOTAK => array(
             Payment\Gateway::HDFC,
-            Payment\Gateway::AXIS,
-            Payment\Gateway::GENIUS,
+            Payment\Gateway::AXIS_MIGS,
+            Payment\Gateway::AXIS_GENIUS,
+            Payment\Gateway::KOTAK
         ),
 
         self::ATOM => array(
