25091575c782bac41db3aad399d186e89e08d695
Shashank Kumar <shashankkumar.me@gmail.com>
2015-05-13 10:13:01 +0530

    Adds kotak gateway mock classes

--- app/gateway/Kotak/Mock/Gateway.php
+++ app/gateway/Kotak/Mock/Gateway.php
@@ -11,5 +11,20 @@ use Models\Card;
 
 class Gateway extends Kotak\Gateway
 {
-    ;
+    use Base\Mock\GatewayTrait;
+
+    public function authorize(array $input)
+    {
+        $request = parent::authorize($input);
+
+        // The key thing now is to replace redirectUrl from kotak's to ours!
+        $parts = parse_url($request['url']);
+
+        $url = \Http\Route::getUrlWithPublicAuth('mock_kotak_payment');
+        $url = $url . '&' .$parts['query'];
+
+        $request['url'] = $url;
+
+        return $request;
+    }
 }
