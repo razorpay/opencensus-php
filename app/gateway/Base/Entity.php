9698aaf7f05ed3fe7a33442252c00cf987ac200c
Shashank Kumar <shashankkumar.me@gmail.com>
2015-05-22 20:54:37 +0530

    Adds gateway base entity class

---
+++ app/gateway/Base/Entity.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Gateway\Base;
+
+class Entity extends \Models\Base\Entity
+{
+    public function setPaymentId($paymentId)
+    {
+        $this->attributes['payment_id'] = $paymentId;
+    }
+
+    public function setAction($action)
+    {
+        $this->setAttribute('action', $action);
+    }
+}
