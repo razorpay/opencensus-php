e0285c6ea28efaabf1bc10dd5b65a97991bed844
Shashank Kumar <shashankkumar.me@gmail.com>
2015-05-27 11:15:05 +0530

    Adds mock gateway class for hdfc netbanking

---
+++ app/gateway/Netbanking/Hdfc/Mock/Validator.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Gateway\Netbanking\Hdfc\Mock;
+
+use Models\Base;
+
+class Validator extends Base\Validator
+{
+    protected static $authRules = array(
+        'ClientCode'            => 'required|email',
+        'MerchantCode'          => 'required|alpha',
+        'TxnCurrency'           => 'required|in:INR',
+        'TxnAmount'             => 'required|numeric',
+        'TxnScAmount'           => 'required|in:0',
+        'MerchantRefNo'         => 'required|alpha_num|size:14',
+        'SuccessStaticFlag'     => 'required|in:N',
+        'FailureStaticFlag'     => 'required|in:N',
+        'Date'                  => 'required',
+        'DynamicUrl'            => 'required|url',
+        'CheckSum'              => 'required',
+    );
+}
