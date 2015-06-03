f0155e5de9dac86169044cfdfb4171be8eebdaaa
Shashank Kumar <shashankkumar.me@gmail.com>
2015-06-03 07:12:58 +0530

    Fixes minor bug

--- app/tests/TestDummy/Factory.php
+++ app/tests/TestDummy/Factory.php
@@ -63,7 +63,7 @@ class Factory extends Base\Factory
      * @param  IsPersistable $provider
      * @return void
      */
-    public static function setDatabaseProvider($provider = null)
+    protected function setDatabaseProvider($provider = null)
     {
         if ( ! static::$databaseProvider) {
             static::$databaseProvider = $provider ?: new Base\EloquentModel;
