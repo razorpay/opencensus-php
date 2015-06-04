bc7085eb418ac43d62602207becce7f8d02b23d8
Shashank Kumar <shashankkumar.me@gmail.com>
2015-06-04 19:52:45 +0530

    Remove Rs 50 limit for netbankign transactions

--- app/models/Payment/Validator.php
+++ app/models/Payment/Validator.php
@@ -61,14 +61,7 @@ class Validator extends Base\Validator
 
     protected function validateAmount($input)
     {
-        if (($input['method'] === Payment\Method::NETBANKING) and
-            (((int) $input['amount']) < 1000))
-        {
-            throw new Exception\BadRequestException(
-                ErrorCode::BAD_REQUEST_PAYMENT_ATOM_NET_BANKING_MIN_AMOUNT_FIFTY,
-                'amount');
-        }
-        else if ($input['amount'] < 100)
+        if ($input['amount'] < 100)
         {
             throw new Exception\BadRequestException(
                 ErrorCode::BAD_REQUEST_PAYMENT_AMOUNT_LESS_THAN_MIN_AMOUNT,
