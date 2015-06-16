d71aa5675d5ba0eb5614486b3bd7455a84412d4e
Shashank Kumar <shashankkumar.me@gmail.com>
2015-06-17 03:48:01 +0530

    Increase max terminals count to 9 to account for newer sharp gateway

--- app/models/Terminal/Entity.php
+++ app/models/Terminal/Entity.php
@@ -28,7 +28,7 @@ class Entity extends Base\PublicEntity
 
     const DELETED_AT                    = 'deleted_at';
 
-    const MAX_TERMINALS_COUNT           = 8;
+    const MAX_TERMINALS_COUNT           = 9;
 
     protected $fillable = array(
         self::MERCHANT_ID,
