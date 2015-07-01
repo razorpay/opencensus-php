f2873afbf6d8ac7f4300b942cd24fdbbe6987d8b
Abhay Rana <nemo@razorpay.com>
2015-07-01 15:52:07 +0530

    Fixes indentation issues in migrations

--- app/database/migrations/2015_06_30_195614_AddSubmittedAtToMerchantDetail.php
+++ app/database/migrations/2015_06_30_195614_AddSubmittedAtToMerchantDetail.php
@@ -5,30 +5,30 @@ use Illuminate\Database\Migrations\Migration;
 
 class AddSubmittedAtToMerchantDetail extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::table('merchant_details', function($table)
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('merchant_details', function($table)
         {
             $table->integer('submitted_at')->nullable();
         });
-	}
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::table('merchant_details', function($table)
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('merchant_details', function($table)
         {
             $table->dropColumn('submitted_at');
         });
-	}
+    }
 
 }
--- app/database/migrations/2015_06_30_231735_AddTransactionReportEmailToMerchantDetail.php
+++ app/database/migrations/2015_06_30_231735_AddTransactionReportEmailToMerchantDetail.php
@@ -5,30 +5,30 @@ use Illuminate\Database\Migrations\Migration;
 
 class AddTransactionReportEmailToMerchantDetail extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::table('merchant_details', function($table)
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('merchant_details', function($table)
         {
             $table->integer('transaction_report_email')->nullable();
         });
-	}
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::table('merchant_details', function($table)
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('merchant_details', function($table)
         {
             $table->dropColumn('transaction_report_email');
         });
-	}
+    }
 
 }
