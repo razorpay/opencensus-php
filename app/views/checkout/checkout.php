e5ebe85381aa86edb6a7ddac3e8f83104bb22ce0
Pranav Gupta <pranavguptarulz@gmail.com>
2015-07-10 05:46:15 +0530

    added woff2 to checkout.php

--- app/views/checkout/checkout.php
+++ app/views/checkout/checkout.php
@@ -10,7 +10,7 @@ $fonts = 'https://s3.amazonaws.com/checkout-live/lato';
     <meta http-equiv="pragma" content="no-cache">
     <meta http-equiv="cache-control" content="no-cache">
     <meta name="viewport" content="user-scalable=no,width=device-width,initial-scale=1,maximum-scale=1">
-    <style>@font-face{font-family:'lato';src:url("<?= $fonts ?>.eot?#iefix") format('embedded-opentype'),url("<?= $fonts ?>.woff") format('woff'),url("<?= $fonts ?>.ttf") format('truetype'),url("<?= $fonts ?>.svg#lato") format('svg');font-weight:normal;font-style:normal}</style>
+    <style>@font-face{font-family:'lato';src:url("<?= $fonts ?>.eot?#iefix") format('embedded-opentype'),url("<?= $fonts ?>.woff2") format('woff2'),url("<?= $fonts ?>.woff") format('woff'),url("<?= $fonts ?>.ttf") format('truetype'),url("<?= $fonts ?>.svg#lato") format('svg');font-weight:normal;font-style:normal}</style>
     <link rel="stylesheet" href="<?= $checkout ?>/v1/css/checkout.css">
  </head>
   <body>
