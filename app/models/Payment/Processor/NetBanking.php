609da97af85d3f2be21e789e4e2039a516b52e3b
Shashank Kumar <shashankkumar.me@gmail.com>
2015-07-14 04:16:28 +0530

    Adds bank codes where banks have corporate and retail net-banking

--- app/models/Payment/Processor/NetBanking.php
+++ app/models/Payment/Processor/NetBanking.php
@@ -3,9 +3,17 @@
 namespace Models\Payment\Processor;
 
 use Models\Bank\IFSC;
+use Models\Bank\Name;
 
-class NetBanking
+class Netbanking
 {
+    const BARB_C = 'BARB_C';
+    const BARB_R = 'BARB_R';
+    const PUNB_C = 'PUNB_C';
+    const PUNB_R = 'PUNB_R';
+    const LAVB_C = 'PUNB_C';
+    const LAVB_R = 'PUNB_R';
+
     protected static $enabled = array(
 //        IFSC::BARB,
         IFSC::CITI,
@@ -26,6 +34,15 @@ class NetBanking
         IFSC::VIJB,
         IFSC::VYSA,
         IFSC::YESB,
+    );
+
+    protected static $names = array(
+        self::BARB_C => 'Bank of Baroda - Corporate Banking',
+        self::BARB_R => 'Bank of Baroda - Retail Banking',
+        self::PUNB_C => 'Punjab National Bank - Corporate Banking',
+        self::PUNB_R => 'Punjab National Bank - Retail Banking',
+        self::LAVB_C => 'Lakshmi Vilas Bank - Corporate Banking',
+        self::LAVB_R => 'Lakshmi Vilas Bank - Retail Banking',
     );
 
     protected static $processing = array(
@@ -56,5 +73,14 @@ class NetBanking
     public static function getEnabledBanks()
     {
         return self::$enabled;
+    }
+
+    public static function getNames($codes)
+    {
+        $names = array_intersect_key(self::$names, array_flip($codes));
+
+        $names = array_merge($names, Name::getNames($codes));
+
+        return $names;
     }
 }
