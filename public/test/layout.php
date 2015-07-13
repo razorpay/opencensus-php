e944018151b9f66fb5de12069c208eaa1d438533
Pranav Gupta <pranavguptarulz@gmail.com>
2015-07-13 12:51:48 +0530

    layout.php modification

--- public/test/layout.php
+++ public/test/layout.php
@@ -3,99 +3,91 @@
 require('vars.php');
 
 ?>
-<!DOCTYPE html>
+<!doctype html>
 <html>
-<head lang="en">
-    <meta charset="utf-8">
-    <link href="<?= $checkout ?>/v1/css/checkout.css" type="text/css" rel="stylesheet">
-
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="<?= $checkout ?>/v1/checkout.js" ></script>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Razorpay - Automatic Checkout</title>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link href='//fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
+  <script src="<?= $checkout ?>/v1/checkout.js" type="text/javascript"></script>
 </head>
 <body>
-    <button class="btn" id="rzp-button1">Tron Legacy</button>
-    <button class="btn" id="rzp-button2">Alive (2007)</button>
+  <div class="all-container">
+    <div class="header">
+      <div class="container">
+        <a href="https://www.razorpay.com" target="_blank" class="logo"></a>
+        <a class="nav active">Products</a>
+        <a class="nav">Delivery</a>
+        <a class="nav">Contact Us</a>
+        <div class="pull-right">
+          <a class="nav">Search</a>
+          <a class="nav">Checkout</a>
+        </div>
+      </div>
+    </div>
+    <div class="content">
+      <div class="container">
+        <div class="tab">
+          <div class="leftcontent">
+            <div class="product"></div>
+            <div class="thumbnails">
+              <div class="thumbnail"></div>
+              <div class="thumbnail"></div>
+              <div class="thumbnail"></div>
+              <div class="thumbnail "></div>
+            </div>
+          </div>
+          <div class="clear"></div>
+        </div>
+        <div class="tab">
+          <div class="rightcontent">
+            <h3>Fine Tshirt</h3>
+              <div class="rating"><img src="images/rating.png"></div>
+              <p class="review">5 Reviews</p>
+            <div class="price">₹5</div>
 
-    <script>
-        var options = {
-            'key': 'rzp_test_1DP5mmOlF5G5ag',
-            'amount': '5100',
-            'name': 'Daft Punk',
-            'description': 'Tron Legacy',
-            'image': 'https://i.imgur.com/3g7nmJC.png',
-            'handler': function (transaction) {
-                alert("You have successfully purchased Tron Legacy \n transaction id: " + transaction.razorpay_payment_id);
-            },
-            'protocol': '<?= $protocol ?>',
-            'hostname': '<?= $hostname ?>',
-            'prefill': {
-                'name': 'Harshil Mathur',
-                'email': 'harshil@razorpay.com',
-                'contact': '9999999999'
-            },
-            notes: {
-                'address': 'Hello World'
-            },
-            netbanking: true,
-            handler: function (transaction){
-                alert('You have successfully purchased Fine tshirt\ntransaction id: ' + transaction.razorpay_payment_id);
-            }
-        }
-        var rzp1 = new Razorpay(options);
-
-        $('#rzp-button1').click(function(e) {
-            rzp1.open();
-            e.preventDefault();
-        });
-
-        $.extend(true, options, {
-            amount : 5200,
-            description : 'Alive (2007)',
-            image : 'https://i.imgur.com/GXalrU0.png',
-            handler : function (transaction){
-                alert("You have successfully purchased Alive (2007)");
-            },
-            notes: {
-                'shipping': "Bye World"
-            }
-        });
-
-        var rzp2 = new Razorpay(options);
-
-        $('#rzp-button2').click(function(e){
-            rzp2.open();
-            e.preventDefault();
-        })
-    </script>
-
-    <style>
-
-    html {
-        background-image: url('https://i.imgur.com/zx7XGPJ.jpg');
-        background-size: cover cover;
-        /**background-position: center center fixed;*/
-        background-repeat: no-repeat;
-        background-position: -300px 0px;
-    }
-    .btn {
-        width: 20px;
-        background-color: #28B3D2;
-        color: white;
-        width: 200px;
-        border-radius: 3px;
-        height: 40px;
-        font-size: 20px;
-        text-align: center;
-        border: none;
-        position: absolute;
-        left: 45%;
-    }
-    #rzp-button2 {
-        top: 400px;
-    }
-    #rzp-button1 {
-        top: 350px;
-    }
-    </style>
+            <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc fermentum tincidunt libero nec imperdiet. Etiam sed malesuada dolor. Integer id ante ut urna pretium aliquet et et quam. Fusce tempor ultricies velit non finibus. Nullam lacus nulla, vehicula vitae pharetra nec, vestibulum id odio. Nunc quis sapien vestibulum, vulputate ipsum nec, consequat erat. Nunc interdum pharetra commodo. Nullam blandit id neque id ultrices. Proin quis efficitur mauris.</p>
+            <p class="wishlist"><img src="images/wishlist.png"></p>
+            <div class="pay">
+              <input type="button" value="" class="razorpay-payment-button" id="paybtn">
+              <script>
+              document.getElementById('paybtn').onclick = function(){
+                new Razorpay({
+                  key: 'rzp_test_1DP5mmOlF5G5ag',
+                  protocol: '<?= $protocol ?>',
+                  hostname: '<?= $hostname ?>',
+                  amount: '500',
+                  name: 'Merchant Name',
+                  description: 'Purchase Description',
+                  image: 'https://i.imgur.com/3g7nmJC.png',
+                  prefill: {
+                    name: 'Harshil Mathur',
+                    email: 'harshil@razorpay.com',
+                    contact: '9999999999'
+                  },
+                  handler: function (transaction){
+                    alert('You have successfully purchased Fine tshirt\ntransaction id: ' + transaction.razorpay_payment_id);
+                  }
+                }).open()
+              }
+              </script>
+            </div>
+          </div>
+        </div>
+        <div class="clear"></div>
+      </div>
+    </div>
+    <div class="footer">
+      <div class="container">
+        <a href="https://www.razorpay.com/" target="_blank" class="nav" >Home</a>
+        <a href="https://www.razorpay.com/features/" target="_blank" class="nav">Features</a>
+        <a href="https://www.razorpay.com/pricing/" target="_blank" class="nav">Pricing</a>
+        <a href="https://docs.razorpay.com/" target="_blank" class="nav">Documentation</a>
+        <a href="https://www.razorpay.com/contact/" target="_blank" class="nav">Contact Us</a>
+      </div>
+    </div>
+  </div>
 </body>
 </html>
