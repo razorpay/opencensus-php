1f523a0be14e883b158ad3e34b5c8da37c28b561
Shashank Kumar <shashankkumar.me@gmail.com>
2015-07-21 20:56:18 +0530

    Adds receipt_email_enabled property to merchant entity

--- app/models/Payment/Processor/Authorize.php
+++ app/models/Payment/Processor/Authorize.php
@@ -172,9 +172,12 @@ trait Authorize
             return $this->getReturnRequestDataForMerchant($payment);
         }
 
-        // Send email to the customer
-        // Can be extended later for SMS as well
-        $this->notifyCustomer($payment);
+        if ($payment->merchant->isReceiptEmailsEnabled())
+        {
+            // Send email to the customer
+            // Can be extended later for SMS as well
+            $this->notifyCustomer($payment);
+        }
 
         return ['razorpay_payment_id' => $payment->getPublicId()];
     }
@@ -184,9 +187,11 @@ trait Authorize
         $app = App::getFacadeRoot();
 
         // Dont send mails in test mode
-        if($this->mode === Mode::TEST and !$app->environment('dev'))
+        // @todo: remove this somehow
+        if (($this->mode === Mode::TEST) and
+            ($app->environment('dev') === false))
         {
-            return true;
+            return;
         }
 
         $templateData = [
