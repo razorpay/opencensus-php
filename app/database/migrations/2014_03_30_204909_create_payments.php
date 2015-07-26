47a2bf9c4802a92af60874f4dcdb1ea620309bab
Shashank Kumar <shashankkumar.me@gmail.com>
2015-07-27 02:08:56 +0530

    Change payment callback_url column to text type

--- app/database/migrations/2014_03_30_204909_create_payments.php
+++ app/database/migrations/2014_03_30_204909_create_payments.php
@@ -96,7 +96,7 @@ class CreatePayments  extends Migration
             $table->boolean(Payment::VERIFIED)
                   ->nullable();
 
-            $table->string(Payment::CALLBACK_URL)
+            $table->text(Payment::CALLBACK_URL)
                   ->nullable();
 
             // Adds created_at and updated_at columns to the table
