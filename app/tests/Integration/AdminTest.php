9ad442073a687e2ee1bdbe30480893c6d9140b63
Abhay Rana <nemo@razorpay.com>
2015-08-04 00:31:58 +0530

    [tests] Make sure butterbar is hidden before clicking on go

--- app/tests/Integration/AdminTest.php
+++ app/tests/Integration/AdminTest.php
@@ -120,8 +120,8 @@ class AdminTest extends TestCase
             ->waitForCondition("selenium.browserbot.getCurrentWindow().$('.merchant_type').length > 0", 20000)
             ->select(l::IdOrName('merchant_type'), 'label=All')
             ->waitForCondition("selenium.browserbot.getCurrentWindow().$('.merchant_go').length > 0", 20000)
-            ->click(l::css('.merchant_go'))
             ->waitForCondition("selenium.browserbot.getCurrentWindow().$('.butterbar.hide').length > 1", 20000)
+            ->click(l::css('.merchant_go'))
             ->waitForCondition("selenium.browserbot.getCurrentWindow().$('.merchants-table-body').length > 0", 20000);
 
         $this->assertBodyHasText($this->merchant->id);
@@ -138,8 +138,8 @@ class AdminTest extends TestCase
             ->waitForCondition("selenium.browserbot.getCurrentWindow().$('.merchant_type').length > 0", 20000)
             ->select(l::IdOrName('merchant_type'), 'label=All')
             ->waitForCondition("selenium.browserbot.getCurrentWindow().$('.merchant_go').length > 0", 20000)
-            ->click(l::css('.merchant_go'))
             ->waitForCondition("selenium.browserbot.getCurrentWindow().$('.butterbar.hide').length > 1", 20000)
+            ->click(l::css('.merchant_go'))
             ->waitForCondition("selenium.browserbot.getCurrentWindow().$('.merchants-table-body').length > 0", 20000)
             ->click(l::linkContaining($this->merchant->id))
             ->waitForCondition("selenium.browserbot.getCurrentWindow().$('.merchant-wrapper').length > 0", 20000);
