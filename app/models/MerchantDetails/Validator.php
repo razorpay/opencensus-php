93a5f2b5c8b8c1197b0db50e932823100ebd1d95
Abhay Rana <nemo@razorpay.com>
2015-08-04 16:31:57 +0530

    Fixes #20

--- app/models/MerchantDetails/Validator.php
+++ app/models/MerchantDetails/Validator.php
@@ -53,7 +53,7 @@ class Validator extends Base\Validator
     protected static $step4Rules = array(
         'bank_name'             => 'required|alpha_space|max:255',
         'bank_account_number'   => 'required|numeric|digits_between:1,20',
-        'bank_account_name'     => 'required|alpha_space|max:40',
+        'bank_account_name'     => 'required|alpha_space_num|max:40',
         'bank_account_type'     => 'required|alpha_space|max:20',
         'bank_branch'           => 'required|max:255',
         'bank_branch_ifsc'      => 'required|alpha_num|max:11',
