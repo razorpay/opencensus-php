3f95bd593db2f31942a1202197dc95cb3d7e13e0
Shashank Kumar <shashankkumar.me@gmail.com>
2015-08-05 23:26:32 +0530

    Fixes typo

--- app/models/Terminal/Validator.php
+++ app/models/Terminal/Validator.php
@@ -48,7 +48,7 @@ class Validator extends Base\Validator
         Entity::GATEWAY                     => 'required|in:axis_migs',
         Entity::GATEWAY_MERCHANT_ID         => 'required|alpha_num|size:15',
         Entity::GATEWAY_SECURE_SECRET       => 'required|alpha_num|size:32',
-        Entity::GATEWAY_ACCESS_CODE         => 'required|alhpa_num|size:8',
+        Entity::GATEWAY_ACCESS_CODE         => 'required|alpha_num|size:8',
         Entity::GATEWAY_TERMINAL_ID         => 'required',
         Entity::GATEWAY_TERMINAL_PASSWORD   => 'required',
     );
