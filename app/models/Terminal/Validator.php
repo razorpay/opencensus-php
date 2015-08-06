b310f544d10e791db9669061bf603bd51762ed61
Shashank Kumar <shashankkumar.me@gmail.com>
2015-08-06 10:40:53 +0530

    Reduce axis migs terminal merchant id requisite length

--- app/models/Terminal/Validator.php
+++ app/models/Terminal/Validator.php
@@ -46,7 +46,7 @@ class Validator extends Base\Validator
 
     protected static $axisMigsTerminalRules = array(
         Entity::GATEWAY                     => 'required|in:axis_migs',
-        Entity::GATEWAY_MERCHANT_ID         => 'required|alpha_num|size:15',
+        Entity::GATEWAY_MERCHANT_ID         => 'required|alpha_num|min:8',
         Entity::GATEWAY_SECURE_SECRET       => 'required|alpha_num|size:32',
         Entity::GATEWAY_ACCESS_CODE         => 'required|alpha_num|size:8',
         Entity::GATEWAY_TERMINAL_ID         => 'required',
