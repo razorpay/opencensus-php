299ae2bc35489fdb905e86f640c92dd3c9efc49c
Shashank Kumar <shashankkumar.me@gmail.com>
2015-08-17 14:52:26 +0530

    Gives preference to hdfc tid over axis ones

--- app/models/Payment/Processor/TerminalPicker.php
+++ app/models/Payment/Processor/TerminalPicker.php
@@ -129,6 +129,11 @@ class TerminalPicker
 
         $international = $payment->merchant->isInternational();
 
+        if (isset($gatewayTerms[Gateway::HDFC]))
+        {
+            return $gatewayTerms[Gateway::HDFC];
+        }
+
         if (isset($gatewayTerms[Gateway::AXIS_MIGS]))
         {
             return $gatewayTerms[Gateway::AXIS_MIGS];
@@ -137,11 +142,6 @@ class TerminalPicker
         if (isset($gatewayTerms[Gateway::AXIS_GENIUS]))
         {
             return $gatewayTerms[Gateway::AXIS_GENIUS];
-        }
-
-        if (isset($gatewayTerms[Gateway::HDFC]))
-        {
-            return $gatewayTerms[Gateway::HDFC];
         }
 
         if ($this->mode === Mode::TEST)
