76782616680155fc3353e9300dcc8cf5247e52bd
Abhay Rana <nemo@razorpay.com>
2015-09-02 17:21:21 +0530

    Adds special case for merchant id

--- public/js/admin/controllers/entitiesCtrl.js
+++ public/js/admin/controllers/entitiesCtrl.js
@@ -279,10 +279,12 @@ app.controller('EntitiesCtrl', ['$scope', '$http', 'alertsFactory', '$state', '$
       }
 
       $scope.getType = function(key, value) {
+        var entity = key.substr(0, key.length - 3);
 
         var isTimestamp = function(key) {
           return key.substr(-3) === '_at';
         }
+
         var isId = function(key) {
           var validEntities = ["adjustment", "atom", "axis_genius",
             "axis_migs", "bank_account", "bank_account", "billdesk",
@@ -291,21 +293,30 @@ app.controller('EntitiesCtrl', ['$scope', '$http', 'alertsFactory', '$state', '$
             "settlement", "terminal", "transaction"
           ];
 
-          var entity = key.substr(0, key.length - 3);
-
           // It needs to be suffixed with _id
           // and be a valid entity name for this to work
+
           return (key.substr(-3) === '_id')
             && (validEntities.indexOf(entity) > -1);
         }
 
+        // Timestamps could be blank, which is why
+        // we consider its value as well
         if(value && isTimestamp(key)) {
           return 'timestamp';
         }
 
+        // We have a separate view for merchant entity
+        else if(entity === 'merchant') {
+          return 'merchant'
+        }
+
+        // All other entity links are considered here
         else if(isId(key)) {
           return 'id';
         }
+
+        // Unknown type is entity specific things, like amount
         else {
           return 'unknown';
         }
