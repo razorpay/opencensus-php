40e06028d46c49e01b2b5d874bd0fa329487a3d6
Ayush Ghosh <ayush@ayush.me>
2015-09-02 12:44:30 +0530

    Added Sbiepay migration

--- app/database/migrations/2015_08_28_065646_create_sbiepay.php
+++ app/database/migrations/2015_08_28_065646_create_sbiepay.php
@@ -23,6 +23,39 @@ class CreateSbiepay extends Migration {
             $table->string('action');
             $table->string('method');
             $table->boolean('received')->default(0);
+
+            $table->string('MerchantId', 20);
+            $table->string('OperatingMode', 10);
+            $table->string('MerchantCountry', 10);
+            $table->string('MerchantCurrency', 10);
+            $table->string('PostingAmount', 19);
+            $table->string('OtherDetails', 1000);
+            $table->string('AggregatorId', 10);
+            $table->string('MerchantOrderNo', 100);
+            $table->string('MerchantCustomerID', 100);
+            $table->string('Paymode', 10);
+            $table->string('Accesmedium', 10);
+            $table->string('TransactionSource', 10);
+
+            $table->string('SBIePayReferenceID', 20)->nullable();
+            $table->string('Status', 20)->nullable();
+            $table->string('Reason', 200)->nullable();
+            $table->string('BankCode', 10)->nullable();
+            $table->string('BankReferenceNumber', 40)->nullable();
+            $table->dateTime('TransactionDate')->nullable();
+            $table->string('CIN',20)->nullable();
+
+
+            $table->char('refund_id', UniqueIdEntity::ID_LENGTH)->nullable();
+            $table->integer('created_at');
+            $table->integer('updated_at');
+
+            $table->foreign('payment_id')
+                ->references('id')
+                ->on('payments')
+                ->on_delete('restrict');
+
+            $table->index('received');
         });
 	}
 
