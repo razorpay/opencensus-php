5474922add5017808f8b5cf92b4c19b2918b2794
Ayush Ghosh <ayush@ayush.me>
2015-09-07 15:53:30 +0530

    Added Kotak Shared Terminal

--- app/models/Terminal/Shared.php
+++ app/models/Terminal/Shared.php
@@ -7,16 +7,17 @@ use Models\Terminal;
 
 class Shared
 {
-    const ATOM_RAZORPAY_TERMINAL        = '1000AtomShared';
-    const AXIS_MIGS_RAZORPAY_TERMINAL   = '1000AxisMigsTl';
+    const ATOM_RAZORPAY_TERMINAL = '1000AtomShared';
+    const AXIS_MIGS_RAZORPAY_TERMINAL = '1000AxisMigsTl';
     const AXIS_GENIUS_RAZORPAY_TERMINAL = '1000AxisGenius';
-    const BILLDESK_RAZORPAY_TERMINAL    = '1000BdeskTrmnl';
-    const HDFC_RAZORPAY_TERMINAL        = '1000HdfcShared';
-    const KOTAK_RAZORPAY_TERMINAL       = '1000KotakTrmnl';
-    const MOBIKWIK_RAZORPAY_TERMINAL    = '1000MobiKwikTl';
-    const PAYTM_RAZORPAY_TERMINAL       = '1000PaytmTrmnl';
-    const NETBANKING_HDFC_TERMINAL      = '100NbHdfcTrmnl';
-    const SHARP_RAZORPAY_TERMINAL       = '1000SharpTrmnl';
+    const BILLDESK_RAZORPAY_TERMINAL = '1000BdeskTrmnl';
+    const HDFC_RAZORPAY_TERMINAL = '1000HdfcShared';
+    const KOTAK_RAZORPAY_TERMINAL = '1000KotakTrmnl';
+    const MOBIKWIK_RAZORPAY_TERMINAL = '1000MobiKwikTl';
+    const PAYTM_RAZORPAY_TERMINAL = '1000PaytmTrmnl';
+    const NETBANKING_HDFC_TERMINAL = '100NbHdfcTrmnl';
+    const NETBANKING_KOTAK_TERMINAL = '100NbKotakTmnl';
+    const SHARP_RAZORPAY_TERMINAL = '1000SharpTrmnl';
 
     protected static $shared = array(
         self::ATOM_RAZORPAY_TERMINAL,
@@ -28,20 +29,22 @@ class Shared
         self::MOBIKWIK_RAZORPAY_TERMINAL,
         self::PAYTM_RAZORPAY_TERMINAL,
         self::NETBANKING_HDFC_TERMINAL,
+        self::NETBANKING_KOTAK_TERMINAL,
         self::SHARP_RAZORPAY_TERMINAL,
     );
 
     protected static $map = array(
-        self::ATOM_RAZORPAY_TERMINAL         =>  Gateway::ATOM,
-        self::AXIS_GENIUS_RAZORPAY_TERMINAL  =>  Gateway::AXIS_GENIUS,
-        self::AXIS_MIGS_RAZORPAY_TERMINAL    =>  Gateway::AXIS_MIGS,
-        self::BILLDESK_RAZORPAY_TERMINAL     =>  Gateway::BILLDESK,
-        self::HDFC_RAZORPAY_TERMINAL         =>  Gateway::HDFC,
-        self::KOTAK_RAZORPAY_TERMINAL        =>  Gateway::KOTAK,
-        self::MOBIKWIK_RAZORPAY_TERMINAL     =>  Gateway::MOBIKWIK,
-        self::PAYTM_RAZORPAY_TERMINAL        =>  Gateway::PAYTM,
-        self::NETBANKING_HDFC_TERMINAL       =>  Gateway::NETBANKING_HDFC,
-        self::SHARP_RAZORPAY_TERMINAL        =>  Gateway::SHARP,
+        self::ATOM_RAZORPAY_TERMINAL        => Gateway::ATOM,
+        self::AXIS_GENIUS_RAZORPAY_TERMINAL => Gateway::AXIS_GENIUS,
+        self::AXIS_MIGS_RAZORPAY_TERMINAL   => Gateway::AXIS_MIGS,
+        self::BILLDESK_RAZORPAY_TERMINAL    => Gateway::BILLDESK,
+        self::HDFC_RAZORPAY_TERMINAL        => Gateway::HDFC,
+        self::KOTAK_RAZORPAY_TERMINAL       => Gateway::KOTAK,
+        self::MOBIKWIK_RAZORPAY_TERMINAL    => Gateway::MOBIKWIK,
+        self::PAYTM_RAZORPAY_TERMINAL       => Gateway::PAYTM,
+        self::NETBANKING_HDFC_TERMINAL      => Gateway::NETBANKING_HDFC,
+        self::NETBANKING_KOTAK_TERMINAL     => Gateway::NETBANKING_KOTAK,
+        self::SHARP_RAZORPAY_TERMINAL       => Gateway::SHARP,
     );
 
     public static function isSharedTerminal($terminal)
