6e5fcea16965960288bbda598dcd186ca7984b05
Ayush Ghosh <ayush@ayush.me>
2015-09-07 15:56:07 +0530

    Added Kotak NB to Payment Gateway

--- app/models/Payment/Gateway.php
+++ app/models/Payment/Gateway.php
@@ -16,6 +16,7 @@ class Gateway
     const MOBIKWIK          = 'mobikwik';
     const PAYTM             = 'paytm';
     const NETBANKING_HDFC   = 'netbanking_hdfc';
+    const NETBANKING_KOTAK   = 'netbanking_kotak';
     const SHARP             = 'sharp';
 
     public static $channels = array(
@@ -28,6 +29,7 @@ class Gateway
         self::MOBIKWIK          => Settlement\Channel::KOTAK,
         self::PAYTM             => Settlement\Channel::KOTAK,
         self::NETBANKING_HDFC   => Settlement\Channel::KOTAK,
+        self::NETBANKING_KOTAK  => Settlement\Channel::KOTAK,
         self::SHARP             => Settlement\Channel::KOTAK,
     );
 
@@ -45,6 +47,7 @@ class Gateway
             self::PAYTM,
             self::BILLDESK,
             self::NETBANKING_HDFC,
+            self::NETBANKING_KOTAK,
         ),
 
         Method::WALLET => array(
