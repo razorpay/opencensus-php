124c4c9808b62654a4ad15ce5469335a2664d0a7
Ayush Ghosh <ayush@ayush.me>
2015-09-08 17:22:33 +0530

    SBIEPAY Gateway Test Added

---
+++ app/tests/Functional/Gateway/Sbiepay/SbiepayGatewayTest.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Tests\Functional\Gateway\Sbiepaypay;
+
+use Tests\Functional\Helpers\Payment\PaymentTrait;
+use Tests\Functional\TestCase;
+
+class SbiepayGatewayTest extends TestCase
+{
+    use PaymentTrait;
+
+    public function setUp()
+    {
+        $this->testDataFilePath = __DIR__.'/SbiepayGatewayTestData.php';
+
+        parent::setUp();
+
+        $this->sharedTerminal = $this->fixtures->create('terminal:shared_sbiepay_terminal');
+
+        $this->fixtures->create('terminal:disable_default_hdfc_terminal');
+
+        $this->gateway = 'sbiepay';
+
+        $this->setMockGatewayTrue();
+    }
+
+    public function testPayment()
+    {
+        $payment = $this->getDefaultNetbankingPaymentArray();
+        $payment['bank'] = 'SBIN';
+        $payment = $this->doAuthAndCapturePayment($payment);
+        $payment = $this->getLastEntity('payment', true);
+        $this->assertTestResponse($payment);
+
+        $payment = $this->getLastEntity('sbiepay', true);
+
+        $this->assertArraySelectiveEquals(
+            $this->testData['testPaymentSbiepayEntity'], $payment);
+    }
+
+//    public function testPaymentFailed()
+//    {
+//        $this->markTestIncomplete();
+//
+//        $this->failPaymentOnBankPage = true;
+//    }
+//
+//    public function testPaymentVerify()
+//    {
+//        $payment = $this->getDefaultNetbankingPaymentArray();
+//        $payment = $this->doAuthAndCapturePayment($payment);
+//
+//        $this->verifyPayment($payment['id']);
+//    }
+//
+//    public function testPaymentRefund()
+//    {
+//        $payment = $this->getDefaultNetbankingPaymentArray();
+//        $payment = $this->doAuthAndCapturePayment($payment);
+//
+//        $this->refundPayment($payment['id']);
+//
+//        $refund = $this->getLastEntity('billdesk', true);
+//        $this->assertTestResponse($refund);
+//    }
+//
+//    public function testGetPaymentMethodsRoute()
+//    {
+//        $this->ba->publicLiveAuth();
+//
+//        $this->fixtures->links['merchant']->activate('10000000000000');
+//
+//        $attributes = array(
+//            'merchant_id'               => '10000000000000',
+//            'gateway'                   => 'billdesk',
+//            'card'                      => 0,
+//            'gateway_merchant_id'       => 'razorpay billdesk',
+//            'gateway_terminal_id'       => 'nodal account billdesk',
+//            'gateway_terminal_password' => 'razorpay_password',
+//        );
+//
+//        $terminal = $this->fixtures->on('live')->create('terminal', $attributes);
+//
+//        $content = $this->startTest();
+//
+//        $count = count($content['netbanking']);
+//        $this->assertEquals(48, $count);
+//    }
+}
