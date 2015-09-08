5e0c5614662db39717f6a8a7eac8257ad4fd43a9
Ayush Ghosh <ayush@ayush.me>
2015-09-08 13:50:59 +0530

    Updated ResponseFieldTrait

--- app/gateway/Netbanking/Kotak/ResponseFieldsTrait.php
+++ app/gateway/Netbanking/Kotak/ResponseFieldsTrait.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace Gateway\Billdesk;
+namespace Gateway\Netbanking\Kotak;
 
 use Constants\Mode;
 use EE\Error\ErrorCode;
 use EE\Exception;
 use Gateway\Base;
 use Gateway\Base\Action;
-use Gateway\Billdesk;
+use Gateway\Netbanking\Kotak;
 use Requests;
 use Trace\Trace;
 use Trace\TraceCode;
@@ -15,134 +15,44 @@ use Trace\TraceCode;
 trait ResponseFieldsTrait
 {
     protected static $authorizeRequestFields = array(
-        'MerchantID',
-        'CustomerID',
-        'Unknown1',
-        'TxnAmount',
-        'BankID',
-        'Unknown2',
-        'Unknown3',
-        'CurrencyType',
-        'ItemCode',
-        'TypeField1',
-        'SecurityID',
-        'Unknown4',
-        'Unknown5',
-        'TypeField2',
-        'AdditionalInfo1',
-        'Unknown6',
-        'Unknown7',
-        'Unknown8',
-        'Unknown9',
-        'Unknown10',
-        'Unknown11',
-        'RU',
+        'MessageCode',
+        'DateTimeInGMT',
+        'MerchantId',
+        'TraceNumber',
+        'Amount',
+        'TransactionDescription',
         'Checksum',
     );
 
     protected static $callbackResponseFields = array(
-        'MercantID',
-        'CustomerID',
-        'TxnReferenceNo',
-        'BankReferenceNo',
-        'TxnAmount',
-        'BankID',
-        'BankMerchantID',
-        'TxnType',
-        'CurrencyName',
-        'ItemCode',
-        'SecurityType',
-        'SecurityID',
-        'SecurityPassword',
-        'TxnDate',
-        'AuthStatus',
-        'SettlementType',
-        'AdditionalInfo1',
-        'AdditionalInfo2',
-        'AdditionalInfo3',
-        'AdditionalInfo4',
-        'AdditionalInfo5',
-        'AdditionalInfo6',
-        'AdditionalInfo7',
-        'ErrorStatus',
-        'ErrorDescription',
-        'Checksum',
-    );
-
-    protected static $refundRequestFields = array(
-        'RequestType',
-        'MerchantID',
-        'TxnReferenceNo',
-        'TxnDate',
-        'CustomerID',
-        'TxnAmount',
-        'RefAmount',
-        'RefDateTime',
-        'MerchantRefNo',
-        'Filler1',
-        'Filler2',
-        'Filler3',
-        'Checksum',
-    );
-
-    protected static $refundResponseFields = array(
-        'RequestType',
-        'MerchantID',
-        'TxnReferenceNo',
-        'TxnDate',
-        'CustomerID',
-        'TxnAmount',
-        'RefAmount',
-        'RefDateTime',
-        'RefStatus',
-        'RefundId',
-        'ErrorCode',
-        'ErrorReason',
-        'ProcessStatus',
+        'MessageCode',
+        'DateTimeInGMT',
+        'MerchantId',
+        'TraceNumber',
+        'Amount',
+        'AuthorizationStatus',
+        'BankReference',
         'Checksum',
     );
 
     protected static $verifyRequestFields = array(
-        'RequestType',
-        'Merchant ID',
-        'Customer ID',
-        'Current Date/ Timestamp',
+        'MessageCode',
+        'DateTimeInGMT',
+        'MerchantId',
+        'TraceNumber',
+        'Future1',
+        'Future2',
         'Checksum',
     );
 
     protected static $verifyResponseFields = array(
-        'RequestType',
-        'MerchantID',
-        'CustomerID',
-        'TxnReferenceNo',
-        'BankReferenceNo',
-        'TxnAmount',
-        'BankID',
-        'BankMerchantID',
-        'TxnType',
-        'CurrencyName',
-        'ItemCode',
-        'SecurityType',
-        'SecurityID',
-        'SecurityPassword',
-        'TxnDate',
-        'AuthStatus',
-        'SettlementType',
-        'AdditionalInfo1',
-        'AdditionalInfo2',
-        'AdditionalInfo3',
-        'AdditionalInfo4',
-        'AdditionalInfo5',
-        'AdditionalInfo6',
-        'AdditionalInfo7',
-        'ErrorStatus',
-        'ErrorDescription',
-        'Filler1',
-        'RefundStatus',
-        'TotalRefundAmount',
-        'LastRefundDate',
-        'LastRefundRefNo',
-        'QueryStatus',
+        'MessageCode',
+        'DateTimeInGMT',
+        'MerchantId',
+        'TraceNumber',
+        'Amount',
+        'AuthorizationStatus',
+        'BankReference',
         'Checksum',
     );
 
