51e90a3c85dfd62ea1885202455f71eb194f8d35
Ayush Ghosh <ayush@ayush.me>
2015-09-08 13:50:05 +0530

    Updated Netbanking table schema, added status

--- app/database/migrations/2015_05_19_181536_create_netbanking.php
+++ app/database/migrations/2015_05_19_181536_create_netbanking.php
@@ -29,6 +29,7 @@ class CreateNetbanking extends Migration
             $table->string('client_code')->nullable();
             $table->string('merchant_code')->nullable();
             $table->string('bank_payment_id')->nullable();
+            $table->string('status')->nullable();
             $table->string('error_message')->nullable();
             $table->string('reference1')->nullable();
             $table->string('date')->nullable();
