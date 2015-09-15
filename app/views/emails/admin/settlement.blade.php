bfad480cd891f7bb1b6eb81a2f010d6f4e4d2b9b
Abhay Rana <nemo@razorpay.com>
2015-09-15 12:54:09 +0530

    Switches email to html format with a table.

---
+++ app/views/emails/admin/settlement.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 100%; line-height: 1.6em; margin: 0; padding: 0;">
+<head>
+<meta name="viewport" content="width=device-width">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>{{{$subject}}}</title>
+</head>
+<body bgcolor="#f6f6f6" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 100%; line-height: 1.6em; -webkit-font-smoothing: antialiased; height: 100%; -webkit-text-size-adjust: none; width: 100% !important; margin: 0; padding: 0;">
+
+<!-- body -->
+<table class="body-wrap" bgcolor="#f6f6f6" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 100%; line-height: 1.6em; width: 100%; margin: 0; padding: 20px;"><tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 100%; line-height: 1.6em; margin: 0; padding: 0;">
+<td style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 100%; line-height: 1.6em; margin: 0; padding: 0;"></td>
+    <td class="container" bgcolor="#FFFFFF" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 100%; line-height: 1.6em; clear: both !important; display: block !important; max-width: 600px !important; Margin: 0 auto; padding: 20px; border: 1px solid #f0f0f0;">
+
+      <!-- content -->
+      <div class="content" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 100%; line-height: 1.6em; display: block; max-width: 600px; margin: 0 auto; padding: 0;">
+      <table style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 100%; line-height: 1.6em; width: 100%; margin: 0; padding: 0;"><tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 100%; line-height: 1.6em; margin: 0; padding: 0;">
+<td style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 100%; line-height: 1.6em; margin: 0; padding: 0;">
+<p>PFA Settlement files.</p>
+            <table>
+                <thead>
+                    <th>Description</th>
+                    <th>Amount</th>
+                    <th>Count</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>NEFT</td>
+                        <td>{{{$amounts['neft']}}}</td>
+                        <td>{{{$count['neft']}}}</td>
+                    </tr>
+                    <tr>
+                        <td>IFT</td>
+                        <td>{{{$amounts['ift']}}}</td>
+                        <td>{{{$count['ift']}}}</td>
+                    </tr>
+                    <tr>
+                        <td>Total</td>
+                        <td>{{{$amounts['total']}}}</td>
+                        <td>{{{$count['total']}}}</td>
+                    </tr>
+                </tbody>
+            </table>
+          </td>
+        </tr></table>
+</div>
+      <!-- /content -->
+
+    </td>
+    <td style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 100%; line-height: 1.6em; margin: 0; padding: 0;"></td>
+  </tr></table>
+<!-- /body -->
+</body>
+</html>
