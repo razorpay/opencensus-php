f529d262318a361ab67d26abbf0f1885abce6456
Ayush Ghosh <ayush@ayush.me>
2015-09-23 17:32:18 +0530

    Added Kotak Callback response handler to Gateway Controller

--- app/controllers/GatewayController.php
+++ app/controllers/GatewayController.php
@@ -15,4 +15,23 @@ class GatewayController extends BaseController
         $app = \App::getFacadeRoot();
         $app['slack']->send($input, 'transactions', '#transactions');
     }
+
+    public function callbackKotak()
+    {
+        $input_msg = Input::get('msg');
+        $input = explode('|', $input_msg);
+        //check mode before search
+        $nb = (new \Gateway\Netbanking\Base\Repository)->findByTraceIdAndAction(
+            $input[3], \Gateway\Base\Action::AUTHORIZE);
+
+        $payment_id_public = 'pay_' . $nb->payment_id;
+        $secret = \App::make('config')->get('app.key');
+
+        $hash = hash_hmac('sha1', $payment_id_public, $secret);
+
+        $url = \Http\Route::getUrlWithPublicCallbackAuth(['id' => $payment_id_public, 'hash' => $hash]);
+        $url = $url . '?msg=' . $input_msg;
+
+        return Redirect::to($url);
+    }
 }
