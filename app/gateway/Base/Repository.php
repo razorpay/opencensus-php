7c35ce156f35bb9b08b3a1113945451acb76ab73
Ayush Ghosh <ayush@ayush.me>
2015-09-24 11:55:41 +0530

    Updated Base repository for trace_id

--- app/gateway/Base/Repository.php
+++ app/gateway/Base/Repository.php
@@ -33,7 +33,7 @@ class Repository extends Base\Repository
     {
         $repo = $this->repo;
 
-        return $repo::where('client_code', '=', $paymentId)
+        return $repo::where('trace_id', '=', $paymentId)
             ->where('action', '=', $action)
             ->first();
     }
