8f65a00d08f7201b48fffedcd25893da2b38dae8
Shashank Kumar <shashankkumar.me@gmail.com>
2015-09-25 18:02:09 +0530

    Removes and replaces test code portions with common functions

--- app/tests/Functional/Helpers/Payment/PaymentTrait.php
+++ app/tests/Functional/Helpers/Payment/PaymentTrait.php
@@ -244,6 +244,23 @@ trait PaymentTrait
         return $content;
     }
 
+    protected function doAuthPaymentViaCheckoutRoute($payment)
+    {
+        if ($payment === null)
+        {
+            $payment = $this->getDefaultPaymentArray();
+        }
+
+        $request = array(
+            'content' => $payment,
+            'url' => '/payments/create/checkout',
+            'method' => 'post');
+
+        $this->ba->publicAuth();
+
+        return $this->makeRequestAndGetContent($request);
+    }
+
     protected function capturePayment($id, $amount)
     {
         $request = array(
@@ -345,6 +362,15 @@ trait PaymentTrait
         $content = $this->makeRequestAndGetContent($request);
 
         return $content;
+    }
+
+    protected function timeoutOldPayment()
+    {
+        $this->ba->appAuth();
+
+        $request = array('url' => '/payments/timeout');
+
+        return $this->makeRequestAndGetContent($request);
     }
 
     protected function deleteTerminal($mid, $tid)
