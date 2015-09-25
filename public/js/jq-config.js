ee4fdaa7fcea87824a39318140341ea8ef2866f4
Abhay Rana <nemo@razorpay.com>
2015-09-25 14:14:20 +0530

    Adds application level js syntax fixes

--- public/js/jq-config.js
+++ public/js/jq-config.js
@@ -4,35 +4,52 @@
  * value: array of the css js file located
  */
 app.constant('JQ_CONFIG', {
-    easyPieChart:   ['//cdnjs.cloudflare.com/ajax/libs/easy-pie-chart/2.1.4/jquery.easypiechart.min.js'],
-    sparkline:      ['//cdnjs.cloudflare.com/ajax/libs/jquery-sparklines/2.1.2/jquery.sparkline.min.js'],
-    plot:           ['//cdnjs.cloudflare.com/ajax/libs/flot/0.8.2/jquery.flot.min.js', 
-                        '//cdnjs.cloudflare.com/ajax/libs/flot/0.8.2/jquery.flot.time.min.js',
-                        '//cdnjs.cloudflare.com/ajax/libs/flot/0.8.2/jquery.flot.resize.min.js',
-                        'js/jquery/charts/flot/jquery.flot.tooltip.min.js',
-                        'js/jquery/charts/flot/jquery.flot.spline.js',
-                        'js/jquery/charts/flot/jquery.flot.orderBars.js',
-                        '//cdnjs.cloudflare.com/ajax/libs/flot/0.8.2/jquery.flot.pie.min.js'],
-    sortable:       ['//cdnjs.cloudflare.com/ajax/libs/html5sortable/0.1.1/html.sortable.min.js'],
-    nestable:       ['js/jquery/nestable/jquery.nestable.js',
-                        'js/jquery/nestable/nestable.css'],
-    filestyle:      ['js/jquery/file/bootstrap-filestyle.min.js'],
-    slider:         ['js/jquery/slider/bootstrap-slider.js',
-                        'js/jquery/slider/slider.css'],
-    chosen:         ['//cdnjs.cloudflare.com/ajax/libs/chosen/1.1.0/chosen.jquery.min.js',
-                        '//cdnjs.cloudflare.com/ajax/libs/chosen/1.1.0/chosen.min.css'],
-    TouchSpin:      ['js/jquery/spinner/jquery.bootstrap-touchspin.min.js',
-                        'js/jquery/spinner/jquery.bootstrap-touchspin.css'],
-    wysiwyg:        ['js/jquery/wysiwyg/bootstrap-wysiwyg.js',
-                        'js/jquery/wysiwyg/jquery.hotkeys.js'],
-    dataTable:      ['//cdnjs.cloudflare.com/ajax/libs/datatables/1.10.1/js/jquery.dataTables.min.js',
-                        'js/jquery/datatables/dataTables.bootstrap.js',
-                        'js/jquery/datatables/dataTables.bootstrap.css'],
-    vectorMap:      ['js/jquery/jvectormap/jquery-jvectormap.min.js', 
-                        'js/jquery/jvectormap/jquery-jvectormap-world-mill-en.js',
-                        'js/jquery/jvectormap/jquery-jvectormap-us-aea-en.js',
-                        'js/jquery/jvectormap/jquery-jvectormap.css'],
-    footable:       ['js/jquery/footable/footable.all.min.js',
-                        'js/jquery/footable/footable.core.css']
-    }
-);
+  easyPieChart: ['//cdnjs.cloudflare.com/ajax/libs/easy-pie-chart/2.1.4/jquery.easypiechart.min.js'],
+  sparkline: ['//cdnjs.cloudflare.com/ajax/libs/jquery-sparklines/2.1.2/jquery.sparkline.min.js'],
+  plot: [
+    '//cdnjs.cloudflare.com/ajax/libs/flot/0.8.2/jquery.flot.min.js',
+    '//cdnjs.cloudflare.com/ajax/libs/flot/0.8.2/jquery.flot.time.min.js',
+    '//cdnjs.cloudflare.com/ajax/libs/flot/0.8.2/jquery.flot.resize.min.js',
+    'js/jquery/charts/flot/jquery.flot.tooltip.min.js',
+    'js/jquery/charts/flot/jquery.flot.spline.js',
+    'js/jquery/charts/flot/jquery.flot.orderBars.js',
+    '//cdnjs.cloudflare.com/ajax/libs/flot/0.8.2/jquery.flot.pie.min.js'
+  ],
+  sortable: ['//cdnjs.cloudflare.com/ajax/libs/html5sortable/0.1.1/html.sortable.min.js'],
+  nestable: [
+    'js/jquery/nestable/jquery.nestable.js',
+    'js/jquery/nestable/nestable.css'
+  ],
+  filestyle: ['js/jquery/file/bootstrap-filestyle.min.js'],
+  slider: [
+    'js/jquery/slider/bootstrap-slider.js',
+    'js/jquery/slider/slider.css'
+  ],
+  chosen: [
+    '//cdnjs.cloudflare.com/ajax/libs/chosen/1.1.0/chosen.jquery.min.js',
+    '//cdnjs.cloudflare.com/ajax/libs/chosen/1.1.0/chosen.min.css'
+  ],
+  TouchSpin: [
+    'js/jquery/spinner/jquery.bootstrap-touchspin.min.js',
+    'js/jquery/spinner/jquery.bootstrap-touchspin.css'
+  ],
+  wysiwyg: [
+    'js/jquery/wysiwyg/bootstrap-wysiwyg.js',
+    'js/jquery/wysiwyg/jquery.hotkeys.js'
+  ],
+  dataTable: [
+    '//cdnjs.cloudflare.com/ajax/libs/datatables/1.10.1/js/jquery.dataTables.min.js',
+    'js/jquery/datatables/dataTables.bootstrap.js',
+    'js/jquery/datatables/dataTables.bootstrap.css'
+  ],
+  vectorMap: [
+    'js/jquery/jvectormap/jquery-jvectormap.min.js',
+    'js/jquery/jvectormap/jquery-jvectormap-world-mill-en.js',
+    'js/jquery/jvectormap/jquery-jvectormap-us-aea-en.js',
+    'js/jquery/jvectormap/jquery-jvectormap.css'
+  ],
+  footable: [
+    'js/jquery/footable/footable.all.min.js',
+    'js/jquery/footable/footable.core.css'
+  ]
+});
--- public/js/services.js
+++ public/js/services.js
@@ -1,307 +1,270 @@
 'use strict';
-
 /* Services */
-
-angular.module('app.services', [])
-			// User Service
-			// Fetches & stores details of currently logged in user
-			.factory('user', ['$q', '$http', '$timeout', '$idle',
-			  function($q, $http, $timeout, $idle) {
-			    var _identity = undefined,
-			      _authenticated = false;
-
-			    return {
-			      isIdentityResolved: function() {
-			        return angular.isDefined(_identity);
-			      },
-			      isAuthenticated: function() {
-			        return _authenticated;
-			      },
-			      authenticate: function(identity) {
-			        _identity = identity;
-			        _authenticated = identity != null;
-			      },
-			      identity: function(force) {
-			        var deferred = $q.defer();
-
-			        if (force === true) _identity = undefined;
-
-			        // check and see if we have retrieved the identity data from the server. if we have, reuse it by immediately resolving
-			        if (angular.isDefined(_identity)) {
-			          deferred.resolve(_identity);
-
-			          return deferred.promise;
-			        }
-
-					$http.get('/user', { ignoreErrors: true })
-						.success(function(data) {
-							_identity = data.data;
-							if(data.data.steps_finished) {
-                                _identity.activation_progress = parseInt(((data.data.steps_finished).length * 100)/ 5);
-							}
-					   		_authenticated = data.success === true;
-					   		if(_authenticated) $idle.watch();
-					   		else $idle.unwatch();
-							deferred.resolve(_identity);
-						})
-						.error(function () {
-						   _identity = null;
-						   _authenticated = false;
-						   deferred.resolve(_identity);
-					});
-
-			        return deferred.promise;
-			      }
-			    };
-			  }
-			])
-			//Authorisation service
-			//Checks if the logged in user is allowed to browse to the requested url, redirects him otherwise.
-			.factory('authorization', ['$rootScope', '$state', 'user', '$location',
-			  function($rootScope, $state, user, $location) {
-			    return {
-			      authorize: function() {
-			      	return user.identity()
-				          .then(function() {
-				          	if($rootScope.toState.data.role === 'auth') {
-				            	if (user.isAuthenticated() === false) $state.go('access.signin'); // user is signed in but not authorized for desired state
-				        	}
-				        	else if($rootScope.toState.data.role === 'guest') {
-				        		if (user.isAuthenticated() === true) $state.go('app.dashboard'); // user is signed in but not authorized for desired state
-				        	}
-				        });
-				    }
-			    };
-			  }
-			])
-			.factory("modeFactory",['$state', '$localStorage', '$rootScope', 'user',
-			 function($state, $localStorage, $rootScope, user){
-				var modes = {test: "test", live: "live"};
-
-				var currentMode = "test";
-
-
-				if(angular.isDefined($localStorage.rzp_mode) ) {
-			        currentMode = $localStorage.rzp_mode;
-					
-					user.identity().then(function(data){
-					  if(currentMode == "live" && parseInt(data.activated) !== 1) {
-					    currentMode = "test";
-					  }
-					});
-
-			      } else {
-			        $localStorage.rzp_mode = currentMode;
-			      }
-
-				$rootScope.$watch(function() {
-				  return currentMode;
-				}, function watchCallback(newValue, oldValue) {
-				  $localStorage.rzp_mode = newValue;
-				},
-				true);
-
-				return {
-					getMode: function(){
-						return currentMode;
-					},
-					selectMode: function(mode){
-						currentMode = modes[mode];
-						$state.go($state.$current, null, { reload: true });
-						return currentMode;
-					},
-					getModes: function(){
-						return modes;
-					}
-				};
-			}])
-			//Transforms json array to form post fields, also modifies content type of submission
-			.factory("transformRequestAsFormPost",
-            function() {
-
-                // I prepare the request data for the form post.
-                function transformRequest( data, getHeaders ) {
-
-                    var headers = getHeaders();
-
-                    headers[ "Content-Type" ] = "application/x-www-form-urlencoded; charset=utf-8";
-
-                    return( serializeData( data ) );
-
-                }
-
-                // Return the factory value.
-                return( transformRequest );
-
-                function serializeData( data ) {
-
-                    // If this is not an object, defer to native stringification.
-                    if ( ! angular.isObject( data ) ) {
-
-                        return( ( data == null ) ? "" : data.toString() );
-
-                    }
-
-                    var buffer = [];
-
-                    // Serialize each key in the object.
-                    for ( var name in data ) {
-
-                        if ( ! data.hasOwnProperty( name ) ) {
-
-                            continue;
-
-                        }
-
-                        var value = data[ name ];
-
-                        buffer.push(
-                            encodeURIComponent( name ) +
-                            "=" +
-                            encodeURIComponent( ( value == null ) ? "" : value )
-                        );
-
-                    }
-
-                    // Serialize the buffer and clean it up for transportation.
-                    var source = buffer
-                        .join( "&" )
-                        .replace( /%20/g, "+" )
-                    ;
-
-                    return( source );
-
-                }
-
-            })
-			//Alerts factory.
-			//Used for creating/removing alerts for display in a page.
-			.factory('alertsFactory', function() {
-				function handler(){
-					this.alerts = [];
-					this.getAlerts = function() {
-						return this.alerts;
-					},
-					this.closeAlert = function(index) {
-						this.alerts.splice(index, 1);
-					},
-					this.addAlert = function($type, $message, reset) {
-						$message = $message || "An error occured.";
-						if(reset) this.alerts = [];
-						this.alerts.push({type: $type, msg: $message});
-					},
-					this.resetAlerts = function(last) {
-						if(!last)
-							this.alerts = [];
-						else
-							this.alerts.pop();
-					}
-				};
-
-				return {
-					getHandler: function(){
-						return new handler();
-					}
-				};
-			})
-			.factory('dateFactory', function(){
-				function handler($scope) {
-					this.endDate = new Date();
-					this.startDate = new Date(new Date().setMonth(new Date().getMonth()-1)),
-					this.opened = {};
-					this.dateOptions= {
-						formatYear: 'yy',
-						startingDay: 1,
-						class: 'datepicker'
-					};
-					this.clear= function () {
-						$scope.date.endDate = null;
-						$scope.date.startDate = null;
-					};
-					this.open = function($event, key) {
-						$event.preventDefault();
-						$event.stopPropagation();
-						$scope.date.opened = {};
-						$scope.date.opened[key] = true;
-					};
-				};
-
-				return {
-					getHandler: function($scopeVar){
-						return new handler($scopeVar);
-					}
-				};
-			})
-			// Fetches & stores details of currently logged in user
-			.factory('admin', ['$q', '$http', '$timeout', '$idle',
-			  function($q, $http, $timeout, $idle) {
-			    var _identity = undefined,
-			      _authenticated = false;
-
-			    return {
-			      isIdentityResolved: function() {
-			        return angular.isDefined(_identity);
-			      },
-			      isAuthenticated: function() {
-			        return _authenticated;
-			      },
-			      authenticate: function(identity) {
-			        _identity = identity;
-			        _authenticated = identity != null;
-			      },
-			      identity: function(force) {
-			        var deferred = $q.defer();
-
-			        if (force === true) _identity = undefined;
-
-			        // check and see if we have retrieved the identity data from the server. if we have, reuse it by immediately resolving
-			        if (angular.isDefined(_identity)) {
-			          deferred.resolve(_identity);
-
-			          return deferred.promise;
-			        }
-
-					$http.get('/admin/user', { ignoreErrors: true })
-						.success(function(data) {
-							_identity = data.data;
-					   		_authenticated = data.success === true;
-					   		if(_authenticated) $idle.watch();
-					   		else $idle.unwatch();
-							deferred.resolve(_identity);
-						})
-						.error(function () {
-						   _identity = null;
-						   _authenticated = false;
-						   deferred.resolve(_identity);
-					});
-
-			        return deferred.promise;
-			      }
-			    };
-			  }
-			])
-			//Authorisation service
-			//Checks if the logged in user is allowed to browse to the requested url, redirects him otherwise.
-			.factory('adminAuthorization', ['$rootScope', '$state', 'admin', '$location',
-			  function($rootScope, $state, admin, $location) {
-			    return {
-			      authorize: function() {
-			      	return admin.identity()
-				          .then(function() {
-				          	if($rootScope.toState.data.role === 'auth') {
-				            	if (admin.isAuthenticated() === false) $state.go('access.signin'); // user is signed in but not authorized for desired state
-				        		if($rootScope.toState.data.superadmin) {
-	 				        		admin.identity().then(function(data){
-					        			if(data.superadmin != 1) $state.go('app.dashboard');
-					        		});
-				        		}
-				        	}
-				        	else if($rootScope.toState.data.role === 'guest') {
-				        		if (admin.isAuthenticated() === true) $state.go('app.dashboard'); // user is signed in but not authorized for desired state
-				        	}
-				        });
-				    }
-			    };
-			  }
-			])
-;
+angular.module('app.services', [])  // User Service
+                                    // Fetches & stores details of currently logged in user
+.factory('user', [
+  '$q',
+  '$http',
+  '$timeout',
+  '$idle',
+  function ($q, $http, $timeout, $idle) {
+    var _identity, _authenticated = false;
+    return {
+      isIdentityResolved: function () {
+        return angular.isDefined(_identity);
+      },
+      isAuthenticated: function () {
+        return _authenticated;
+      },
+      authenticate: function (identity) {
+        _identity = identity;
+        _authenticated = identity != null;
+      },
+      identity: function (force) {
+        var deferred = $q.defer();
+        if (force === true)
+          _identity = undefined;
+        // check and see if we have retrieved the identity data from the server. if we have, reuse it by immediately resolving
+        if (angular.isDefined(_identity)) {
+          deferred.resolve(_identity);
+          return deferred.promise;
+        }
+        $http.get('/user', { ignoreErrors: true }).success(function (data) {
+          _identity = data.data;
+          if (data.data.steps_finished) {
+            _identity.activation_progress = parseInt(data.data.steps_finished.length * 100 / 5);
+          }
+          _authenticated = data.success === true;
+          if (_authenticated)
+            $idle.watch();
+          else
+            $idle.unwatch();
+          deferred.resolve(_identity);
+        }).error(function () {
+          _identity = null;
+          _authenticated = false;
+          deferred.resolve(_identity);
+        });
+        return deferred.promise;
+      }
+    };
+  }
+])  //Authorisation service
+    //Checks if the logged in user is allowed to browse to the requested url, redirects him otherwise.
+.factory('authorization', [
+  '$rootScope',
+  '$state',
+  'user',
+  '$location',
+  function ($rootScope, $state, user, $location) {
+    return {
+      authorize: function () {
+        return user.identity().then(function () {
+          if ($rootScope.toState.data.role === 'auth') {
+            if (user.isAuthenticated() === false)
+              $state.go('access.signin');  // user is signed in but not authorized for desired state
+          } else if ($rootScope.toState.data.role === 'guest') {
+            if (user.isAuthenticated() === true)
+              $state.go('app.dashboard');  // user is signed in but not authorized for desired state
+          }
+        });
+      }
+    };
+  }
+]).factory('modeFactory', [
+  '$state',
+  '$localStorage',
+  '$rootScope',
+  'user',
+  function ($state, $localStorage, $rootScope, user) {
+    var modes = {
+      test: 'test',
+      live: 'live'
+    };
+    var currentMode = 'test';
+    if (angular.isDefined($localStorage.rzp_mode)) {
+      currentMode = $localStorage.rzp_mode;
+      user.identity().then(function (data) {
+        if (currentMode == 'live' && parseInt(data.activated) !== 1) {
+          currentMode = 'test';
+        }
+      });
+    } else {
+      $localStorage.rzp_mode = currentMode;
+    }
+    $rootScope.$watch(function () {
+      return currentMode;
+    }, function watchCallback(newValue, oldValue) {
+      $localStorage.rzp_mode = newValue;
+    }, true);
+    return {
+      getMode: function () {
+        return currentMode;
+      },
+      selectMode: function (mode) {
+        currentMode = modes[mode];
+        $state.go($state.$current, null, { reload: true });
+        return currentMode;
+      },
+      getModes: function () {
+        return modes;
+      }
+    };
+  }
+])  //Transforms json array to form post fields, also modifies content type of submission
+.factory('transformRequestAsFormPost', function () {
+  // I prepare the request data for the form post.
+  function transformRequest(data, getHeaders) {
+    var headers = getHeaders();
+    headers['Content-Type'] = 'application/x-www-form-urlencoded; charset=utf-8';
+    return serializeData(data);
+  }
+  // Return the factory value.
+  return transformRequest;
+  function serializeData(data) {
+    // If this is not an object, defer to native stringification.
+    if (!angular.isObject(data)) {
+      return data == null ? '' : data.toString();
+    }
+    var buffer = [];
+    // Serialize each key in the object.
+    for (var name in data) {
+      if (!data.hasOwnProperty(name)) {
+        continue;
+      }
+      var value = data[name];
+      buffer.push(encodeURIComponent(name) + '=' + encodeURIComponent(value == null ? '' : value));
+    }
+    // Serialize the buffer and clean it up for transportation.
+    var source = buffer.join('&').replace(/%20/g, '+');
+    return source;
+  }
+})  //Alerts factory.
+    //Used for creating/removing alerts for display in a page.
+.factory('alertsFactory', function () {
+  function handler() {
+    this.alerts = [];
+    this.getAlerts = function () {
+      return this.alerts;
+    }, this.closeAlert = function (index) {
+      this.alerts.splice(index, 1);
+    }, this.addAlert = function ($type, $message, reset) {
+      $message = $message || 'An error occured.';
+      if (reset)
+        this.alerts = [];
+      this.alerts.push({
+        type: $type,
+        msg: $message
+      });
+    }, this.resetAlerts = function (last) {
+      if (!last)
+        this.alerts = [];
+      else
+        this.alerts.pop();
+    };
+  }
+  return {
+    getHandler: function () {
+      return new handler();
+    }
+  };
+}).factory('dateFactory', function () {
+  function handler($scope) {
+    this.endDate = new Date();
+    this.startDate = new Date(new Date().setMonth(new Date().getMonth() - 1)), this.opened = {};
+    this.dateOptions = {
+      formatYear: 'yy',
+      startingDay: 1,
+      class: 'datepicker'
+    };
+    this.clear = function () {
+      $scope.date.endDate = null;
+      $scope.date.startDate = null;
+    };
+    this.open = function ($event, key) {
+      $event.preventDefault();
+      $event.stopPropagation();
+      $scope.date.opened = {};
+      $scope.date.opened[key] = true;
+    };
+  }
+  return {
+    getHandler: function ($scopeVar) {
+      return new handler($scopeVar);
+    }
+  };
+})  // Fetches & stores details of currently logged in user
+.factory('admin', [
+  '$q',
+  '$http',
+  '$timeout',
+  '$idle',
+  function ($q, $http, $timeout, $idle) {
+    var _identity, _authenticated = false;
+    return {
+      isIdentityResolved: function () {
+        return angular.isDefined(_identity);
+      },
+      isAuthenticated: function () {
+        return _authenticated;
+      },
+      authenticate: function (identity) {
+        _identity = identity;
+        _authenticated = identity != null;
+      },
+      identity: function (force) {
+        var deferred = $q.defer();
+        if (force === true)
+          _identity = undefined;
+        // check and see if we have retrieved the identity data from the server. if we have, reuse it by immediately resolving
+        if (angular.isDefined(_identity)) {
+          deferred.resolve(_identity);
+          return deferred.promise;
+        }
+        $http.get('/admin/user', { ignoreErrors: true }).success(function (data) {
+          _identity = data.data;
+          _authenticated = data.success === true;
+          if (_authenticated)
+            $idle.watch();
+          else
+            $idle.unwatch();
+          deferred.resolve(_identity);
+        }).error(function () {
+          _identity = null;
+          _authenticated = false;
+          deferred.resolve(_identity);
+        });
+        return deferred.promise;
+      }
+    };
+  }
+])  //Authorisation service
+    //Checks if the logged in user is allowed to browse to the requested url, redirects him otherwise.
+.factory('adminAuthorization', [
+  '$rootScope',
+  '$state',
+  'admin',
+  '$location',
+  function ($rootScope, $state, admin, $location) {
+    return {
+      authorize: function () {
+        return admin.identity().then(function () {
+          if ($rootScope.toState.data.role === 'auth') {
+            if (admin.isAuthenticated() === false)
+              $state.go('access.signin');
+            // user is signed in but not authorized for desired state
+            if ($rootScope.toState.data.superadmin) {
+              admin.identity().then(function (data) {
+                if (data.superadmin != 1)
+                  $state.go('app.dashboard');
+              });
+            }
+          } else if ($rootScope.toState.data.role === 'guest') {
+            if (admin.isAuthenticated() === true)
+              $state.go('app.dashboard');  // user is signed in but not authorized for desired state
+          }
+        });
+      }
+    };
+  }
+]);
