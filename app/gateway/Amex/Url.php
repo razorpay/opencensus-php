41b17e03e922d2ec3572234d7c0678b5f10cebda
Shashank Kumar <shashankkumar.me@gmail.com>
2015-09-29 18:29:29 +0530

    Adds amex gateway urls

---
+++ app/gateway/Amex/Url.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Gateway\Ame;
+
+class Url
+{
+    const LIVE_DOMAIN           = 'https://vpos.amxvpos.com/vpcpay';
+    const TEST_DOMAIN           = 'https://vpos.amxvpos.com/vpcdps';
+
+    const PAY                   = '/vpcpay';
+    const AMA                   = '/vpcdps';
+}
