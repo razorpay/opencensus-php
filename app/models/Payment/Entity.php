e76ee9d05aa3de645364328e41085f6623061c64
Shashank Kumar <shashankkumar.me@gmail.com>
2015-09-30 14:00:25 +0530

    Adds missing getErrorCode function in payment entity

--- app/models/Payment/Entity.php
+++ app/models/Payment/Entity.php
@@ -491,6 +491,11 @@ class Entity extends Base\PublicEntity
         return $this->getAttribute(self::TRANSACTION_ID);
     }
 
+    public function getErrorCode()
+    {
+        return $this->getAttribute(self::ERROR_CODE);
+    }
+
     public function getInternalErrorCode()
     {
         return $this->getAttribute(self::INTERNAL_ERROR_CODE);
