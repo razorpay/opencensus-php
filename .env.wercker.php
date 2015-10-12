41aa574714e74aaec91204915b16e1710eb01ec7
Shashank Kumar <shashankkumar.me@gmail.com>
2015-10-12 22:33:42 +0530

    Adds missing env vars to env wercker config file

--- .env.wercker.php
+++ .env.wercker.php
@@ -51,12 +51,12 @@ return array(
     'HDFC_GATEWAY_TEST_TERMINAL_ID'                 => getenv('HDFC_GATEWAY_TEST_TERMINAL_ID'),
     'HDFC_GATEWAY_TEST_TERMINAL_PASSWORD'           => getenv('HDFC_GATEWAY_TEST_TERMINAL_PASSWORD'),
 
-    'MOBIKWIK_GATEWAY_TEST_HASH_SECRET'             => 'randomkuchbhi',
-
     'KOTAK_GATEWAY_TEST_HASH_SECRET'                => getenv('KOTAK_GATEWAY_TEST_HASH_SECRET'),
     'KOTAK_GATEWAY_TEST_MERCHANT_ID'                => getenv('KOTAK_GATEWAY_TEST_MERCHANT_ID'),
     'KOTAK_GATEWAY_TEST_ACCESS_CODE'                => getenv('KOTAK_GATEWAY_TEST_ACCESS_CODE'),
     'KOTAK_GATEWAY_TEST_TERMINAL_ID'                => getenv('KOTAK_GATEWAY_TEST_TERMINAL_ID'),
+
+    'MOBIKWIK_GATEWAY_TEST_HASH_SECRET'             => 'randomkuchbhi',
 
     'PAYTM_GATEWAY_TEST_MERCHANT_ID'                => getenv('PAYTM_GATEWAY_TEST_MERCHANT_ID'),
     'PAYTM_GATEWAY_TEST_HASH_SECRET'                => getenv('PAYTM_GATEWAY_TEST_HASH_SECRET'),
@@ -69,7 +69,8 @@ return array(
 
     'NETBANKING_HDFC_GATEWAY_LIVE_HASH_SECRET'      => '000000',
 
-    'MOCK_GATEWAY_SECRET'                           => 'wercker_random_password',
+    'NETBANKING_KOTAK_GATEWAY_TEST_HASH_SECRET'     => '',
+    'NETBANKING_KOTAK_GATEWAY_LIVE_HASH_SECRET'     => '',
 
     'AMEX_MOCK'                                     => true,
     'ATOM_MOCK'                                     => true,
@@ -82,6 +83,8 @@ return array(
     'PAYTM_MOCK'                                    => true,
     'PAYZAPP_MOCK'                                  => true,
     'NETBANKING_HDFC_MOCK'                          => true,
+
+    'MOCK_GATEWAY_SECRET'                           => 'wercker_random_password',
 
     'EMAIL_MOCK'                                    => true,
 
