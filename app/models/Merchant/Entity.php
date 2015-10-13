af106f7fc3ce24d1e2613225c8b17547681d1242
Abhay Rana <nemo@razorpay.com>
2015-10-13 10:54:03 +0530

    Revert "Disables accounts after 90 days of inactivity"
    This reverts commit aa35bcb973bcaa464f6a696fa32519e2f57f0d69.

--- app/models/Merchant/Entity.php
+++ app/models/Merchant/Entity.php
@@ -108,11 +108,6 @@ class Entity extends Base\Entity implements UserInterface, RemindableInterface
         return $this->edit($input, 'changePassword');
     }
 
-    public static function findByEmail($email)
-    {
-        return self::where('email', $email)->first();
-    }
-
     public static function getAggregations($data, $mode)
     {
         $data = \DB::table('aggregations')
@@ -364,13 +359,5 @@ class Entity extends Base\Entity implements UserInterface, RemindableInterface
         } while ($num);
 
         return $res;
-    }
-
-    /**
-     * TODO: Fix this with team support
-     */
-    public function getLastActivity()
-    {
-        return time();
     }
 }
--- app/models/Merchant/Service.php
+++ app/models/Merchant/Service.php
@@ -11,9 +11,6 @@ use Requests;
 
 class Service extends Base\Service
 {
-    // 60 * 60 * 24 * 90 = 90 days
-    protected static $INACTIVE_PERIOD = 7776000;
-
     public function register(array $input)
     {
         $merchant = new Merchant\Entity;
@@ -150,19 +147,14 @@ class Service extends Base\Service
             'password'  => $input['password']
         );
 
-        $merchantAuth = \Auth::merchant();
-        $merchant = Entity::findByEmail($input['email']);
+        $merchant = \Auth::merchant();
 
-        if ($merchantAuth->validate($credentials) === false or $merchant === null)
+        if ($merchant->validate($credentials) === false)
         {
             // Checks credentials but doesn't login the merchant, throws error if invalid
             $error = ['Email or password is invalid.'];
         }
-        else if (time() - $merchant->getLastActivity() > self::$INACTIVE_PERIOD)
-        {
-            $error = ['Account has been inactive for 90 days. Please contact support@razorpay.com'];
-        }
-        else if ($merchantAuth->attempt($credentials + array('confirm_token' => null)) === false)
+        else if ($merchant->attempt($credentials + array('confirm_token' => null)) === false)
         {
             // Tries to login merchant if confirmed, throws error if merchant is not confirmed
             $error = ['not activated'];
