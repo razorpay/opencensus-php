3b087b4b12ccfe7b91e86a8701ff829798377195
Abhay Rana <nemo@razorpay.com>
2015-10-21 14:01:06 +0530

    Wraps Notify calls in Authorize inside a try/catch
    - This way the critical pathway is not affected by errors in Notify

--- app/trace/TraceCode.php
+++ app/trace/TraceCode.php
@@ -26,6 +26,7 @@ class TraceCode
     const PAYMENT_FAILED_TO_AUTHORIZED              = 'PAYMENT_FAILED_TO_AUTHORIZED';
     const PAYMENT_CALLBACK_FAILURE                  = 'PAYMENT_CALLBACK_FAILURE';
     const PAYMENT_AUTHORIZE_FAILED                  = 'PAYMENT_AUTHORIZE_FAILED';
+    const PAYMENT_NOTIFY_FAILED                     = 'PAYMENT_NOTIFY_FAILED';
 
     const TERMINAL_EDIT                             = 'TERMINAL_EDIT';
     const MERCHANT_EDIT                             = 'MERCHANT_EDIT';
