21467cfc023a67beb4011fdff76549fccdc48946
Shashank Kumar <shashankkumar.me@gmail.com>
2015-11-05 21:49:01 +0530

    Adds column gratis in transaction table along with it's index.

--- app/database/migrations/2014_03_22_204890_create_transactions.php
+++ app/database/migrations/2014_03_22_204890_create_transactions.php
@@ -60,11 +60,11 @@ class CreateTransactions extends Migration
             $table->integer(Transaction::API_FEE)
                   ->nullable();
 
-            $table->bigInteger(Transaction::ESCROW_BALANCE)
-                  ->nullable();
-
             $table->boolean(Transaction::GRATIS)
                   ->default(0);
+
+            $table->bigInteger(Transaction::ESCROW_BALANCE)
+                  ->nullable();
 
             $table->string(Transaction::CHANNEL, 8);
 
@@ -97,6 +97,8 @@ class CreateTransactions extends Migration
             $table->index(Transaction::RECONCILED_AT);
 
             $table->index(Transaction::CHANNEL);
+
+            $table->index(Transaction::GRATIS);
 
             $table->foreign(Transaction::MERCHANT_ID)
                   ->references(Merchant\Entity::ID)
