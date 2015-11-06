2dafadc870cfdda955bd6660a5850f397e0767ca
Shashank Kumar <shashankkumar.me@gmail.com>
2015-11-06 10:54:41 +0530

    Put authorized payments route in cron allowed routes

--- app/http/Route.php
+++ app/http/Route.php
@@ -289,6 +289,7 @@ final class Route
                 'payment_auto_capture',
                 'payment_verify_all',
                 'refund_netbanking_generate_excel',
+                'payments_capture_reminder',
                 'payment_verify_all',
             ),
 
