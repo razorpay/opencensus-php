60311398267a21689bba8ed28df5fa151bf9c87b
Shashank Kumar <shashankkumar.me@gmail.com>
2015-11-22 10:18:21 +0530

    Fixes hdfc netbanking payment verify edge case.

--- app/gateway/Netbanking/Hdfc/Gateway.php
+++ app/gateway/Netbanking/Hdfc/Gateway.php
@@ -359,6 +359,21 @@ class Gateway extends Base\Gateway
         return $this->getHashOfString($str);
     }
 
+    protected function sendGatewayRequest($request)
+    {
+        $response = parent::sendGatewayRequest($request);
+
+        $body = $response->body;
+
+        $msg = 'Unable to reach destination.';
+
+        if (strpos($body, $msg) !== false)
+        {
+            throw new Exception\GatewayTimeoutException(
+                'Hdfc netbanking gateway could not be reached');
+        }
+    }
+
     protected function getHashOfString($str)
     {
         $secret = $this->getSecret();
