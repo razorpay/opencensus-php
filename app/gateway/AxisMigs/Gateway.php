1a4ba59fa7a00ee623eb88ed852d996354360316
Shashank Kumar <shashankkumar.me@gmail.com>
2015-11-24 05:08:44 +0530

    Fixes migs payment verification bug.

--- app/gateway/AxisMigs/Gateway.php
+++ app/gateway/AxisMigs/Gateway.php
@@ -254,8 +254,7 @@ class Gateway extends Base\Gateway
 
         $verify->match = ($status === VerifyResult::STATUS_MATCH) ? true : false;
 
-        if (($verify->match === true) and
-            ($payment['received'] === false))
+        if ($payment['received'] === false)
         {
             unset($content['vpc_Command']);
 
