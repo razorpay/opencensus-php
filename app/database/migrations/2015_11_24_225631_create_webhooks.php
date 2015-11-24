df4174c03c87f98fd4954549b1edd7c58a19e469
Shashank Kumar <shashankkumar.me@gmail.com>
2015-11-25 04:31:01 +0530

    Adds webhook table migration.

---
+++ app/database/migrations/2015_11_24_225631_create_webhooks.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+use Constants\Table;
+use Models\Merchant\Webhook\Entity as Webhook;
+use Models\Merchant;
+
+class CreateWebhooks extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(Table::WEBHOOK, function(Blueprint $table)
+        {
+            $table->engine = 'InnoDB';
+
+            $table->char(Webhook::ID, Webhook::ID_LENGTH)
+                  ->primary();
+
+            $table->char(Webhook::MERCHANT_ID, Webhook::ID_LENGTH);
+
+            $table->boolean(Webhook::ACTIVE);
+
+            $table->string(Webhook::URL);
+
+            $table->integer(Webhook::EVENTS);
+
+            $table->integer(Webhook::FAILURE_COUNT);
+
+            $table->integer(Webhook::CREATED_AT);
+            $table->integer(Webhook::UPDATED_AT);
+
+            $table->foreign(Webhook::MERCHANT_ID)
+                  ->references(Merchant\Entity::ID)
+                  ->on(Table::MERCHANT)
+                  ->on_delete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table(Table::WEBHOOK, function($table)
+        {
+            $table->dropForeign(
+                TABLE::WEBHOOK.'_'.Webhook::MERCHANT_ID.'_foreign');
+        });
+
+        Schema::drop(Table::WEBHOOK);
+    }
+}
