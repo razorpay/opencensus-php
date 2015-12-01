54900713fa15a2a11f6ab48b540136514c785d4e
Abhay Rana <nemo@razorpay.com>
2015-12-01 17:37:33 +0530

    [testing] Card expiry date resetted

--- public/test/obscure.php
+++ public/test/obscure.php
@@ -85,7 +85,7 @@ $callback_url = 'http://'.$baseurl.'/return/callback?key_id='.$key_id;
     <tr>
         <td colspan ='40'>Exp Date:</td>
         <td><input type="text" name="card[expiry_month]" value="11"></td>
-        <td><input type="text" name="card[expiry_year]" value="2015"></td>
+        <td><input type="text" name="card[expiry_year]" value="2020"></td>
         <tr>
             <td colspan='40'>Amount:</td>
             <td><input type="text" name="amount" size="25" value="500"></td>
