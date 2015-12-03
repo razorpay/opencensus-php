ec2326ab848529fc5d5770e25329a5b8372cbc46
Abhay Rana <nemo@razorpay.com>
2015-12-03 14:53:57 +0530

    [admin] Show more fields as null in payment details page

--- public/js/admin/controllers/paymentDetailCtrl.js
+++ public/js/admin/controllers/paymentDetailCtrl.js
@@ -21,7 +21,7 @@ app.controller('PaymentDetailCtrl', [
       return mapper[status];
     };
     $scope.displayClass = function (value) {
-      if (value === null) {
+      if (value == null) {
         return 'label label-warning col-lg-1';
       } else if (value === '') {
         return 'label label-info';
@@ -29,6 +29,17 @@ app.controller('PaymentDetailCtrl', [
         return '';
       }
     };
+
+    $scope.timestamp = function(value) {
+      moment().zone(5.5);
+      // If not null and not zero
+      if (value) {
+        return moment(value * 1000).format('D MMM YYYY h:mm:ss a (ddd) ') + 'IST'
+      }
+      else {
+        return 'null';
+      }
+    }
     $scope.displayValue = function (value) {
       if (value === null) {
         return 'null';
