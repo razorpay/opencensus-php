78c98a6c1a6fb0e6c9479af2f7f080bbc74b5b9a
Abhay Rana <nemo@razorpay.com>
2015-12-05 22:34:19 +0530

    [admin] Slight refactor of the admin export feature
    - Now all rows are collected before the headings are generated
    - This means that non-uniform fields such as notes don't screw up things
    - The generated csv is sorted by the array keys

--- app/http/AppResponse.php
+++ app/http/AppResponse.php
@@ -27,31 +27,81 @@ class AppResponse
         return Response::json($response);
     }
 
-    public static function csvResponse(array $data)
+    /**
+     * Flattens an array recursively
+     * Concatenating keys using periods
+     * @param  array $array  input array
+     * @param  string $prefix prefix used to concat keys
+     * @return array flat version of input array
+     */
+    protected static function flatten(array $array, $prefix = '')
     {
-        foreach ($data['items'] as &$item)
-        {
-            foreach ($item as $key => $value)
-            {
-                if (is_array($value))
-                {
-                    unset($item[$key]);
+        $result = array();
 
-                    if(($key = array_search($key, $data['headings'])) !== false)
-                    {
-                        unset($data['headings'][$key]);
-                    }
-                }
+        foreach ($array as $key => $value)
+        {
+            if (is_array($value))
+            {
+                $result = $result + self::flatten($value, $prefix . $key . '_');
+            }
+            else
+            {
+                $result[$prefix . $key] = $value;
             }
         }
+
+        return $result;
+    }
+
+    public static function csvResponse(array $data)
+    {
+        // Flatten all the inner keys
+        // So internal arrays (like notes)
+        // are converted properly
+        //
+        // The csvMerge Object contains a key/value pair that keeps the first
+        // value of every time the key was seen
+        $headings = $csvMergeObject = [];
+
+        foreach ($data as &$row)
+        {
+            $row = self::flatten($row);
+            $csvMergeObject = $csvMergeObject + $row;
+        }
+
+        $headings = array_keys($csvMergeObject);
+        sort($headings);
 
         $file = new SplTempFileObject();
 
         $csv = Writer::createFromFileObject($file);
 
-        $csv->insertOne($data['headings']);
+        // This makes sure that we have uniform array keys for all rows
+        // and that no key is missing for any row
+        $csv->addFormatter(function ($row) use($headings) {
+            // Add extra keys to the array
+            $keysToAdd = array_diff($headings, array_keys($row));
 
-        $csv->insertAll($data['items']);
+            foreach ($keysToAdd as $key)
+            {
+                $row[$key] = null;
+            }
+
+            // Sort the array by key
+            ksort($row);
+            return $row;
+        });
+
+        // We will force headings to become a key/pair so our formatter
+        // doesn't screw with them
+        $newHeadings = [];
+        foreach ($headings as $heading) {
+            $newHeadings[$heading] = $heading;
+        }
+
+        $csv->insertOne($newHeadings);
+
+        $csv->insertAll($data);
 
         $response = Response::make($csv);
 
