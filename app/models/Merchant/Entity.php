357d09613a5d75bc03df284604835bf51ad25ccb
Raj Abishek <rajabishek@hotmail.com>
2015-12-10 15:55:59 +0530

    Remove the generateUuid method and use the Uuid facade

--- app/models/Merchant/Entity.php
+++ app/models/Merchant/Entity.php
@@ -2,6 +2,7 @@
 
 namespace Models\Merchant;
 
+use Uuid;
 use Models\Base;
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableInterface;
@@ -69,7 +70,7 @@ class Entity extends Base\Entity implements UserInterface, RemindableInterface
      */
     public function generateId()
     {
-        $this->setAttribute('id', self::generateUniqueId());
+        $this->setAttribute('id', Uuid::generate());
     }
 
     /**
@@ -306,58 +307,5 @@ class Entity extends Base\Entity implements UserInterface, RemindableInterface
     public function setPasswordAttribute($password)
     {
         $this->attributes['password'] = \Hash::make($password);
-    }
-
-    public static function generateUniqueId()
-    {
-        // Timestmap of 1st Jan 2014!!
-        // 1388534400
-        $ts1stJan2014 = 1388534400;
-
-        // Get current nanotime from 1st Jan 1970
-        $nanotime = self::getNanotimeInteger();
-
-        // Subtract nanotime of 1st Jan 2014
-        $nanotime -= $ts1stJan2014*1000*1000*1000;
-
-        // Convert to base 62
-        $b62 = self::base62($nanotime);
-
-        // Generate 3 random bytes, convert to hex and then to dec
-        $dec = hexdec(bin2hex(openssl_random_pseudo_bytes(5)));
-
-        // Convert the random decimal generated to base 62
-        $rand = self::base62($dec);
-
-        // Only 4 base 62 digits are needed, so cutoff any more.
-        if (strlen($rand) > 4)
-            $rand = substr($rand, 0, 4);
-
-        // Combine the base 62 nanotime with 4 base 62 digits
-        // and create a unique identifier
-        $id = $b62 . $rand;
-
-        assert(strlen($id) === 14);
-
-        return $id;
-    }
-
-    protected static function getNanotimeInteger()
-    {
-        exec('date +%s%N', $nanotime, $status);
-        return $nanotime[0];
-    }
-
-    protected static function base62($num)
-    {
-        $index = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-
-        $res = '';
-        do {
-            $res = $index[$num % 62] . $res;
-            $num = intval($num / 62);
-        } while ($num);
-
-        return $res;
     }
 }
