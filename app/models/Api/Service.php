ba725546f3ba1734c7c40fccffdd61d2799c78d2
Raj Abishek <rajabishek@hotmail.com>
2015-12-13 02:14:18 +0530

    Add the year data also and merchant_id set as null

--- app/models/Api/Service.php
+++ app/models/Api/Service.php
@@ -154,7 +154,7 @@ class Service extends Base\Service
         return $error;
     }
 
-    public function generateReportForMonth($month, $mode)
+    public function generateReportForMonth($month, $year, $mode)
     {
         $data = array();
         $error = (new Validator)->validateInput('generateReport', compact('month'), '')
@@ -166,17 +166,19 @@ class Service extends Base\Service
             {
                 $params = array(
                     'merchant_id' => $this->merchantId,
-                    'month' => $month
+                    'month' => (int)$month,
+                    'year' => (int)$year
                 );
-                $this->setApiCredentials($this->merchantId, $mode);
+
+                $this->setApiCredentials(null, $mode);
                 $data = $this->api
                              ->transaction
                              ->generateReport($params)
                              ->toArray();
                 
-                //Sample endpoint for testing
-                //$response = \Requests::get('http://jsonplaceholder.typicode.com/posts');
-                //$data = json_decode($response->body, true);
+                // Sample endpoint for testing
+                // $response = \Requests::get('http://jsonplaceholder.typicode.com/posts');
+                // $data = json_decode($response->body, true);
 
                 $file = $this->generateTransactionReportAsExcelFromDataForMonth($data, $month);
                 
@@ -194,14 +196,13 @@ class Service extends Base\Service
 
     protected function generateTransactionReportAsExcelFromDataForMonth($data, $month)
     {
-        $file = \Excel::create('transaction_report_' + $month, function($excel) use ($data, $month)
+        $file = \Excel::create('transaction_report', function($excel) use ($data, $month)
         {
             // Set the title
             $excel->setTitle("Transaction Report - $month");
 
             // Chain the setters
-            $excel->setCreator('Razorpay')
-                  ->setCompany('Razorpay');
+            $excel->setCreator('Razorpay')->setCompany('Razorpay');
 
             // Call them separately
             $excel->setDescription("Transaction report for the $month month");
