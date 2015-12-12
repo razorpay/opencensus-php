123f948cbb141f1ee36bb5334f5c238523c4a6c5
Raj Abishek <rajabishek@hotmail.com>
2015-12-13 02:34:06 +0530

    Pass the year data along with the month

--- app/models/Api/Service.php
+++ app/models/Api/Service.php
@@ -157,7 +157,7 @@ class Service extends Base\Service
     public function generateReportForMonth($month, $year, $mode)
     {
         $data = array();
-        $error = (new Validator)->validateInput('generateReport', compact('month'), '')
+        $error = (new Validator)->validateInput('generateReport', compact('month','year'), '')
                                 ->messages();
 
         if (empty($error))
@@ -180,7 +180,7 @@ class Service extends Base\Service
                 // $response = \Requests::get('http://jsonplaceholder.typicode.com/posts');
                 // $data = json_decode($response->body, true);
 
-                $file = $this->generateTransactionReportAsExcelFromDataForMonth($data, $month);
+                $file = $this->generateTransactionReportAsExcelFromDataForMonth($data, $month, $year);
                 
                 return array($error, $file);
             }
@@ -194,18 +194,18 @@ class Service extends Base\Service
         return array($error, null);
     }
 
-    protected function generateTransactionReportAsExcelFromDataForMonth($data, $month)
+    protected function generateTransactionReportAsExcelFromDataForMonth($data, $month, $year)
     {
-        $file = \Excel::create('transaction_report', function($excel) use ($data, $month)
+        $file = \Excel::create('transaction_report', function($excel) use ($data, $month, $year)
         {
             // Set the title
-            $excel->setTitle("Transaction Report - $month");
+            $excel->setTitle("Transaction Report - $month/$year");
 
             // Chain the setters
             $excel->setCreator('Razorpay')->setCompany('Razorpay');
 
             // Call them separately
-            $excel->setDescription("Transaction report for the $month month");
+            $excel->setDescription("Transaction report for $month/$year");
 
             // Our first sheet
             $excel->sheet($month, function($sheet) use ($data)
