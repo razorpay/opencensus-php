4f6e25fc3595733b36dbedd872aaca99b5aada6d
Raj Abishek <rajabishek@hotmail.com>
2015-12-13 01:11:27 +0530

    Add the functionality to get the transaction data from api

--- app/models/Api/Service.php
+++ app/models/Api/Service.php
@@ -154,6 +154,69 @@ class Service extends Base\Service
         return $error;
     }
 
+    public function generateReportForMonth($month, $mode)
+    {
+        $data = array();
+        $error = (new Validator)->validateInput('generateReport', compact('month'), '')
+                                ->messages();
+
+        if (empty($error))
+        {
+            try
+            {
+                $params = array(
+                    'merchant_id' => $this->merchantId,
+                    'month' => $month
+                );
+                $this->setApiCredentials($this->merchantId, $mode);
+                $data = $this->api
+                             ->transaction
+                             ->generateReport($params)
+                             ->toArray();
+                
+                //Sample endpoint for testing
+                //$response = \Requests::get('http://jsonplaceholder.typicode.com/posts');
+                //$data = json_decode($response->body, true);
+
+                $file = $this->generateTransactionReportAsExcelFromDataForMonth($data, $month);
+                
+                return array($error, $file);
+            }
+            catch(\Razorpay\Api\Errors\BadRequestError $e)
+            {
+                $error[] = $e->getMessage();
+                return array($error, null);
+            }
+        }
+
+        return array($error, null);
+    }
+
+    protected function generateTransactionReportAsExcelFromDataForMonth($data, $month)
+    {
+        $file = \Excel::create('transaction_report_' + $month, function($excel) use ($data, $month)
+        {
+            // Set the title
+            $excel->setTitle("Transaction Report - $month");
+
+            // Chain the setters
+            $excel->setCreator('Razorpay')
+                  ->setCompany('Razorpay');
+
+            // Call them separately
+            $excel->setDescription("Transaction report for the $month month");
+
+            // Our first sheet
+            $excel->sheet($month, function($sheet) use ($data)
+            {
+                $sheet->fromArray($data);
+            });
+
+        });
+
+        return $file;
+    }
+
     public function refundPayment($id, $amount, $mode)
     {
         $error = array();
