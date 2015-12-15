372be61ce4aead084c39048fd69e8d1dbfc8ab4b
Raj Abishek <rajabishek@hotmail.com>
2015-12-15 16:06:05 +0530

    Adds the failed_jobs table from laravel to persist failed jobs after specific no of tries

---
+++ app/database/migrations/2015_12_15_102354_create_failed_jobs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFailedJobsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('failed_jobs', function(Blueprint $table)
+		{
+			$table->engine = 'InnoDB';
+
+			$table->increments('id');
+			$table->text('connection');
+			$table->text('queue');
+			$table->text('payload');
+			
+			$table->timestamp('failed_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('failed_jobs');
+	}
+
+}
