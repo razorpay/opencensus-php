61d63890b2cde5a752b9439212e86294b89ef1ae
Shashank Kumar <shashankkumar.me@gmail.com>
2015-12-16 13:42:11 +0530

    Fixes bank account id problem.

--- app/models/Merchant/BankAccount/Entity.php
+++ app/models/Merchant/BankAccount/Entity.php
@@ -96,6 +96,7 @@ class Entity extends Base\PublicEntity
     protected $guarded = array(self::ID);
 
     protected static $generators = array(
+        self::ID,
         self::BENEFICIARY_CODE,
         self::BENEFICIARY_COUNTRY,
     );
