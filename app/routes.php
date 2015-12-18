d8a287e84729b61f3f894cd3169032ba55a406a4
Raj Abishek <rajabishek@hotmail.com>
2015-12-18 23:31:04 +0530

    Add the route to sync the beta features of a merchant

--- app/routes.php
+++ app/routes.php
@@ -163,6 +163,8 @@ Route::group(array('before' => 'auth.admin'), function()
 
         Route::post('/admin/merchant/{id}/tags', 'AdminController@postTagMerchant');
 
+        Route::post('/admin/merchant/{id}/features', 'AdminController@postFeatureMerchant');
+
         Route::post('/admin/merchant/{id}/comment/edit', 'AdminController@postEditMerchantComment');
 
         Route::post('/admin/merchant/{id}/banks', 'AdminController@postMerchantBanks');
