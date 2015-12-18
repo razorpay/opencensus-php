d1adc6dec18f197b334631f2acb59e0543fab303
Asutosh Palai <asupalai@gmail.com>
2015-12-18 10:19:42 +0000

    Increased the benificiary mobile number limit to 11

--- app/database/migrations/2014_12_16_082034_create_bank_accounts.php
+++ app/database/migrations/2014_12_16_082034_create_bank_accounts.php
@@ -52,7 +52,7 @@ class CreateBankAccounts extends Migration
 
             $table->string(BankAccount::BENEFICIARY_EMAIL, 255);
 
-            $table->char(BankAccount::BENEFICIARY_MOBILE, 10);
+            $table->char(BankAccount::BENEFICIARY_MOBILE, 11);
 
             $table->integer(BankAccount::CREATED_AT);
             $table->integer(BankAccount::UPDATED_AT);
