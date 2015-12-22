f6e60866e5fc771ecc468518ec85415ec0dc58ea
Raj Abishek <rajabishek@hotmail.com>
2015-12-22 16:03:48 +0530

    Removes the unused validator methods on the merchant model

--- app/models/Merchant/Validator.php
+++ app/models/Merchant/Validator.php
@@ -6,20 +6,6 @@ use Models\Base;
 
 class Validator extends Base\Validator
 {
-    protected static $createRules = array(
-        'name'                  => 'required|alpha_space|max:200',
-        'email'                 => 'required|email|unique:merchants',
-        'password'              => 'required|between:7,50|confirmed|numbers|letters',
-        'password_confirmation' => 'required|between:7,50',
-        'captcha'               => 'required'
-    );
-
-    protected static $createValidators = array('captcha');
-
-    protected static $unsetCreateInput = array(
-        'captcha'
-    );
-
     protected static $terminalRules = array(
         'mode'                                      => 'required|in:test,live',
         'gateway'                                   => '',
@@ -66,31 +52,6 @@ class Validator extends Base\Validator
             {
                 throw new \Exception(
                     'Merchant data mismatch with api for '.$merchant['id'].' at '.$key);
-            }
-        }
-    }
-
-    protected function validateCaptcha($input)
-    {
-        if($_SERVER['HTTP_HOST'] === 'dashboard.razorpay.com' OR $_SERVER['HTTP_HOST'] === 'betadashboard.razorpay.com')
-        {
-            $captchaResponse = $input['captcha'];
-
-            if (isset($_SERVER['HTTP_X_FORWARDED_FOR']) && $_SERVER['HTTP_X_FORWARDED_FOR']) {
-                $clientIpAddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-            } else {
-                $clientIpAddress = $_SERVER['REMOTE_ADDR'];
-            }
-
-            $url = "https://www.google.com/recaptcha/api/siteverify?secret=".$_ENV['NOCAPTCHA_SECRET']."&response=".$captchaResponse."&remoteip=".$clientIpAddress;
-
-            $response = \Requests::get($url);
-
-            $output = json_decode($response->body);
-
-            if($output->success !== true)
-            {
-                $this->addError('captcha', 'Captcha Failed');
             }
         }
     }
