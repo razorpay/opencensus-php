6273037b5857669901389c3f92a25a0d94480c97
Raj Abishek <rajabishek@hotmail.com>
2015-12-22 17:13:07 +0530

    Use the merchant mailer class to send out merchant related emails

--- app/models/MerchantDetails/Service.php
+++ app/models/MerchantDetails/Service.php
@@ -7,7 +7,7 @@ use AWS;
 use Mail;
 use Models\Base;
 use Queue;
-use Razorpay\Mailers\UserMailer;
+use Razorpay\Mailers\MerchantMailer;
 
 class Service extends Base\Service
 {
@@ -204,7 +204,8 @@ class Service extends Base\Service
             'website' => $merchantDetails->getAttribute('business_website')
         );
 
-        $mailer = new UserMailer(\Auth::merchant()->user());
+        $user = Auth::user()->user();
+        $mailer = new MerchantMailer($user->currentMerchant);
 
         $mailer->confirmActivationSubmission()->queueAndDeliver();
 
