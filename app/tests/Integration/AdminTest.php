625a51f0a0aa8b55b57e2892b048a495fe45df33
Raj Abishek <rajabishek@hotmail.com>
2015-12-23 16:38:40 +0530

    Generate te merchant entity from the user entity and business name

--- app/tests/Integration/AdminTest.php
+++ app/tests/Integration/AdminTest.php
@@ -41,10 +41,13 @@ class AdminTest extends TestCase
         catch(Exception $e)
         {
             $this->admin = $this->createEntity('admin');
-            $this->merchant = $this->createEntity('merchant', array('id'=> Uuid::generate(), 'email' =>static::generateMerchantEmail(), 'confirm_token' => static::generateRandomString(24)));
-            $this->merchant_details = $this->createEntity('merchant_details', array('merchant_id'=>$this->merchant->id));
-            $user = Models\User\Entity::createFromMerchant($this->merchant);
+            $user = $this->createEntity('user');
             $user->saveOrFail();
+            
+            $this->merchant = Models\Merchant\Entity::createFromUserWithBusinessName($user,'Razorpay');
+            $this->merchant->saveOrFail();
+            $this->merchant_details = $this->createEntity('merchant_details', array('merchant_id' => $this->merchant->id));
+            
             $user->merchants()->attach($this->merchant, ['role' => 'owner']);
             $error = (new Models\Merchant\Service)->confirm($this->merchant->confirm_token);
 
