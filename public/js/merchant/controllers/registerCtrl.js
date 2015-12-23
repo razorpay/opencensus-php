fddae052b97e9427007d2f4f1c769e13ea4917c0
Raj Abishek <rajabishek@hotmail.com>
2015-12-23 11:06:18 +0530

    Inject the  dependency in the controller to log the user in to the dashboard

--- public/js/merchant/controllers/registerCtrl.js
+++ public/js/merchant/controllers/registerCtrl.js
@@ -2,11 +2,13 @@
 app.controller('RegisterCtrl', [
   '$scope',
   '$http',
+  '$state',
   'alertsFactory',
+  'user',
   'transformRequestAsFormPost',
   '$analytics',
   '$location',
-  function ($scope, $http, alertsFactory, transformRequestAsFormPost, $analytics, $location) {
+  function ($scope, $http, $state, alertsFactory, user, transformRequestAsFormPost, $analytics, $location) {
     
     $scope.data = {};
     if($location.search().invitation)
@@ -41,7 +43,12 @@ app.controller('RegisterCtrl', [
             name: data.data.name,
             email: data.data.email
           });
-          $scope.alerts.addAlert('success', 'Registration Successful. Please check your inbox for confirmation email from Razorpay.', true);
+          if(data.data.login) {
+            user.identity(true);
+            $state.go('app.dashboard');
+          }
+          else
+            $scope.alerts.addAlert('success', 'Registration Successful. Please check your inbox for confirmation email from Razorpay.', true);
         } else {
           $scope.alerts.resetAlerts();
           angular.forEach(data.errors, function (error, key) {
