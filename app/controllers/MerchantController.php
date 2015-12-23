ebcf3f23f2da0ba2637fc37c57e7dd178a0fbcf8
Asutosh Palai <asupalai@gmail.com>
2015-12-23 09:19:45 +0000

    Returning null

--- app/controllers/MerchantController.php
+++ app/controllers/MerchantController.php
@@ -198,11 +198,6 @@ class MerchantController extends BaseController
     {
         $data = (new Merchant\Service)->getBankAccount($id);
 
-        if ($data === null)
-        {
-            return ApiResponse::generateErrorResponse(404);
-        }
-
         return ApiResponse::json($data);
     }
 
