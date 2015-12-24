8b66d4fb59313e9c1f6dab407ba225c24c75e6d8
Raj Abishek <rajabishek@hotmail.com>
2015-12-24 20:56:18 +0530

    Adds the validation rules to update the invitation

--- app/models/Invitation/Validator.php
+++ app/models/Invitation/Validator.php
@@ -6,8 +6,20 @@ use Models\Base;
 
 class Validator extends Base\Validator
 {
+    /**
+     * The validation rules for sending an invitation.
+     * @var array $roles
+     */
     protected static $sendInvitationRules = array(
         'email' => 'required|max:255|email',
+        'role' => 'required|in:owner,manager,operations,finance,developer',
+    );
+
+    /**
+     * The validation rules for updating an invitation.
+     * @var array $roles
+     */
+    protected static $updateInvitationRules = array(
         'role' => 'required|in:owner,manager,operations,finance,developer',
     );
 }
