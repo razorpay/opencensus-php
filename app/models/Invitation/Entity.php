f41d4238b9f020d6dca69729150a4222c94d6c5c
Abhay Rana <nemo@razorpay.com>
2016-01-07 20:15:01 +0530

    [invite] Redundant default declarations

--- app/models/Invitation/Entity.php
+++ app/models/Invitation/Entity.php
@@ -15,20 +15,6 @@ class Entity extends Base\Entity
     protected $table = 'invitations';
 
     /**
-     * The guarded attributes on the model.
-     *
-     * @var array
-     */
-    protected $guarded = [];
-
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
-    protected $hidden = [];
-
-    /**
      * Get the merchant that owns the invitation.
      */
     public function merchant()
