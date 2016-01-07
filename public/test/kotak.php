56b176e38628e28bf1bed6f1c212b44257f2913c
Giridaran M <giri@razorpay.com>
2016-01-08 00:45:53 +0530

    Removed capture and refund from kotak.php

--- public/test/kotak.php
+++ public/test/kotak.php
@@ -91,22 +91,6 @@ $callback_url = 'http://'.$baseurl.'/return/callback?key_id='.$key_id;
 </form>
 </table>
 <br><br>
-<div style="text-align:center">
-<h3>Test Capture/Refund</h3>
-
-<form name ="capture" method="post" action="//<?=$private_url?>/payments/">
-<input type="text" id="capture_id" placeholder = "Enter payment id to capture"/>
-<input type = "text" id = "amount" name = "amount" placeholder = "Enter amount to capture" value="500" />
-<input type="submit" value="Capture" onClick="javascript:document.capture.action = document.capture.action + document.getElementById('capture_id').value +'/capture'; document.capture.submit(); return false;"/>
-</form>
-
-<form name ="refund" method="post" action="//<?=$private_url?>/payments/">
-<input type="text" id="refund_id" placeholder="Enter payment id to refund"/>
-<input type="text" id="amount" name="amount" value="500"/>
-<input type="submit" value="Refund" onClick="javascript:document.refund.action = document.refund.action + document.getElementById('refund_id').value +'/refund'; document.refund.submit(); return false;"/>
-</form>
-
-</div>
 
 <table width="96%" border="0" cellspacing="0" cellpadding="0">
 <tr>
