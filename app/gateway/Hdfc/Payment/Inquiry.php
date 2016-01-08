5ce66eb5cfdaf52c819a183fb3f3486034fd4ac0
Shashank Kumar <shashankkumar.me@gmail.com>
2016-01-08 21:50:35 +0530

    Fixes hdfc payment verification bug.

--- app/gateway/Hdfc/Payment/Inquiry.php
+++ app/gateway/Hdfc/Payment/Inquiry.php
@@ -15,28 +15,6 @@ trait Inquiry
 {
     use Base\AuthorizeFailed;
 
-    protected function inquire($input)
-    {
-        $payment = $input['payment'];
-
-        $this->id = $input['payment']['id'];
-
-        $txn = $this->repo->findByPaymentId($this->id);
-
-        $txn = $txn[0];
-
-        $data = &$this->inquiryRequest['data'];
-
-        $data['action'] = Payment\Action::INQUIRY;
-        $data['transid'] = $txn['gateway_transaction_id'];
-
-        $this->runRequestResponseFlow(
-            $this->inquiryRequest,
-            $this->inquiryResponse);
-
-//        sd($input['payment'], $this->inquiryResponse['xml']);
-    }
-
     protected function getPaymentToVerify($input, $verify)
     {
         $payment = $this->repo->findByPaymentIdToVerify($input['payment']['id']);
@@ -57,6 +35,8 @@ trait Inquiry
 
         if ($this->wasEnrollSuccessful($payment) === false)
         {
+            $verify->match = true;
+
             return $verify->status;
         }
 
