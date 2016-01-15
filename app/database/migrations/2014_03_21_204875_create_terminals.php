b09ded53bb8b7e9e703846faea114c870be3cc08
Sunny Aggrawal <sunnyagg@outlook.com>
2016-01-15 23:56:27 +0530

    fixes for emi related changes

--- app/database/migrations/2014_03_21_204875_create_terminals.php
+++ app/database/migrations/2014_03_21_204875_create_terminals.php
@@ -54,6 +54,12 @@ class CreateTerminals extends Migration
             $table->boolean(Terminal::NETBANKING)
                   ->default(0);
 
+            $table->boolean(Terminal::EMI)
+                  ->default(0);
+
+            $table->boolean(Terminal::EMI_DURATION)
+                  ->nullable();
+
             $table->boolean(Terminal::SHARED)
                   ->default(0);
 
