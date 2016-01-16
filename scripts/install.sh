33876fd0e486075f0c92658a6a9622bc18c1252a
Shashank Kumar <shashankkumar.me@gmail.com>
2016-01-17 01:21:43 +0530

    Comment out app down command for now in install script.

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,7 +5,7 @@ BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )
 
 # Take the app down
 echo "Take the app down"
-cd /home/ubuntu/api/ && php artisan down
+# cd /home/ubuntu/api/ && php artisan down
 
 # Install new version
 echo  "Install new version"
@@ -14,9 +14,6 @@ cd $BASEDIR && rsync -avz --force --delete --progress --exclude-from=./.rsyncign
 # Fix permissions
 echo  "Fix permissions"
 cd /home/ubuntu/api/ && sudo chmod 777 -R app/storage
-
-echo "Listing vendor dir"
-ls vendor
 
 # DB Migrate
 echo  "DB Migrate"
