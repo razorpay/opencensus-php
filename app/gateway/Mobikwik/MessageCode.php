ff34a9f4faabbd206ee5b735ac8d5aa077aa0021
Shashank Kumar <shashankkumar.me@gmail.com>
2016-01-27 16:26:32 +0530

    [mobikwik] Adds mobikwik message code constants.

---
+++ app/gateway/Mobikwik/MessageCode.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Gateway\Mobikwik;
+
+use EE\Error;
+use EE\Error\ErrorCode;
+
+class MessageCode
+{
+	const OTP_SUBMIT 			= '503';
+	const OTP_GENERATE 			= '504';
+	const CHECK_EXISTING_USER 	= '500';
+}
