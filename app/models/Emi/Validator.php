30efd048750c4cfd80c7d463082e5bf8e5f943b3
Sunny Aggrawal <sunnyagg@outlook.com>
2016-01-28 00:31:13 +0530

    adding emi fields to public array and validator updates for emi duration

--- app/models/Emi/Validator.php
+++ app/models/Emi/Validator.php
@@ -9,7 +9,7 @@ class Validator extends Base\Validator
 {
     protected static $createRules = array(
         'bank'                  => 'required|size:4|in:HDFC,UTIB',
-        'duration'              => 'required|integer|min:1|max:12',
+        'duration'              => 'required|integer|in:3,6,9,12,15',
         'rate'                  => 'required|integer',
         'methods'               => 'sometimes|in:card,wallet,netbanking',
     );
--- app/models/Payment/Validator.php
+++ app/models/Payment/Validator.php
@@ -16,7 +16,7 @@ class Validator extends Base\Validator
         'card'          =>  'sometimes',
         'bank'          =>  'required_if:method,netbanking',
         'wallet'        =>  'required_if:method,wallet|in:paytm,mobikwik,payzapp',
-        'emi_duration'  =>  'required_if:method,emi',
+        'emi_duration'  =>  'required_with:emi|integer|in:3,6,9,12,15',
         'description'   =>  'sometimes',
         'email'         =>  'required|email',
         'contact'       =>  'required',
