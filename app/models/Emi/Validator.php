b952efcbb05899b8bd392cf2e47e4341a17898e5
Sunny Aggrawal <sunnyagg@outlook.com>
2016-01-29 18:18:36 +0530

    adds kotak in emi supported banks

--- app/models/Emi/Validator.php
+++ app/models/Emi/Validator.php
@@ -8,7 +8,7 @@ use Models\Base;
 class Validator extends Base\Validator
 {
     protected static $createRules = array(
-        'bank'                  => 'required|size:4|in:HDFC,UTIB',
+        'bank'                  => 'required|size:4|in:HDFC,UTIB,KKBK',
         'duration'              => 'required|integer|in:3,6,9,12,15',
         'rate'                  => 'required|integer',
         'methods'               => 'sometimes|in:card,wallet,netbanking',
