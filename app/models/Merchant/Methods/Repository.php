f94d69e452bb7864d7d28a5faa736bad603b9101
Shashank Kumar <shashankkumar.me@gmail.com>
2016-02-06 15:44:21 +0530

    [methods] Adds different app fetch filters to methods repository.

--- app/models/Merchant/Methods/Repository.php
+++ app/models/Merchant/Methods/Repository.php
@@ -14,6 +14,16 @@ class Repository extends Base\Repository
 
     protected $entity = 'Methods';
 
+    protected $appFetchParamRules = array(
+        Entity::CARD        => 'sometimes|in:0,1',    
+        Entity::AMEX        => 'sometimes|in:0,1',    
+        Entity::BANKS       => 'sometimes|in:0,1',    
+        Entity::PAYTM       => 'sometimes|in:0,1',    
+        Entity::MOBIKWIK    => 'sometimes|in:0,1',        
+        Entity::PAYZAPP     => 'sometimes|in:0,1',    
+        Entity::EMI         => 'sometimes|in:0,1',
+    );
+
     public function getMerchantBanks($id)
     {
         $repo = $this->repo;
