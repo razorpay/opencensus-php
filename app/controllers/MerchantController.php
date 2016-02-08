3a389ccf929d2813a56094fb457cc53da7ba037f
Shashank Kumar <shashankkumar.me@gmail.com>
2016-02-08 14:33:27 +0530

    Allows loading checkout files from a different domain based on input paramterer.

--- app/controllers/MerchantController.php
+++ app/controllers/MerchantController.php
@@ -341,6 +341,8 @@ class MerchantController extends BaseController
 
     public function getCheckout()
     {
+        $input = Input::all();
+
         $methods = (new Merchant\Service)->getPaymentMethods();
 
         $app = \App::getFacadeRoot();
@@ -356,6 +358,10 @@ class MerchantController extends BaseController
         if (in_array($context, array_keys($urlMap)))
         {
             $url = $urlMap[$context];
+        }
+        else if (isset($input['checkout']))
+        {
+            $url = $input['checkout'];
         }
 
         $data['checkout'] = $url;
