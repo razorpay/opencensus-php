a1d3ad14b67dd4b75be9f41efadb06f094893d5e
Giridaran M <giri@razorpay.com>
2016-02-09 01:43:29 +0530

    Initial Commit. Added Migration to create orders.

---
+++ app/models/Order/Entity.php
@@ -0,0 +1,25 @@
+<?php
+
+namespaace Models\Order;
+
+use Models\Base;
+
+class Entity extends Base\PublicEntity
+{
+    const ID          = 'id';
+    const MERCHANT_ID = 'merchant_id';
+    const AMOUNT      = 'amount';
+    const CURRENCY    = 'currency';
+    const ATTEMPTS    = 'attempts';
+    const STATUS      = 'status';
+    const RECEIPT     = 'receipt';
+    const METHOD      = 'method';
+    const ACCOUNT_ID  = 'account_id';
+    const CREATED_AT  = 'created_at';
+    const VALIDITY    = 'validity';
+    const VALID_TILL  = 'valid_till';
+
+    // Auto capture if set
+    // const CAPTURE     = 'capture';
+
+}
