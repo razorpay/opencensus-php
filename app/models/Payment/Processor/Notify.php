e53a26bede0f4184abc98588c7361764f2d44050
Abhay Rana <nemo@razorpay.com>
2016-02-08 18:20:11 +0530

    [emails] Custom email support for authorized events.
    - Slight refactor in code, gets it to work

--- app/models/Payment/Processor/Notify.php
+++ app/models/Payment/Processor/Notify.php
@@ -13,6 +13,7 @@ use Trace\TraceCode;
 class Notify
 {
     use SlackPoster;
+
     const AUTHORIZED = 'authorized';
     const CAPTURED   = 'captured';
     const REFUNDED   = 'refunded';
@@ -38,7 +39,7 @@ class Notify
     protected $mailViews = [
         self::AUTHORIZED    =>  [
             'customer'  => [
-                'from' => 'receipts@razorpay.com',
+                'from' => 'care',
                 'view' => [
                     'html'=> 'emails.payment.customer',
                     'text'=> 'emails.payment.customer_text'
@@ -93,6 +94,8 @@ class Notify
         $this->mode = $this->app['rzp.mode'];
 
         $this->trace = $this->app['trace'];
+
+        $this->domain = $this->app['config']->get('applications.mailgun.url');
     }
 
     /**
@@ -122,14 +125,16 @@ class Notify
      * @param  string $to      Email address to send to
      * @return null
      */
-    protected function sendMail($view, $subject, $to, $from = 'reports@razorpay.com')
+    protected function sendMail($view, $subject, $to, $from = 'reports')
     {
+        $from    = $this->getCompleteEmail($from);
+        $replyTo = $this->getCompleteEmail('support');
+
         Mail::queue(
             $view,
             $this->template,
-            function ($message) use ($subject, $to)
+            function ($message) use ($subject, $to, $from, $replyTo)
             {
-
                 // to might be an array
                 if (is_array($to))
                 {
@@ -147,10 +152,20 @@ class Notify
 
                 $message->from($from);
                 $message->subject($subject);
-                $message->from('reports@razorpay.com');
-                $message->replyTo('support@razorpay.com');
+                $message->replyTo($replyTo);
             }
         );
+    }
+
+
+    /**
+     * Returns a complete email address
+     * @param  string $user (reports)
+     * @return string (reports@razorpay.com)
+     */
+    protected function getCompleteEmail($user)
+    {
+        return "$user@{$this->domain}";
     }
 
     /**
@@ -170,14 +185,13 @@ class Notify
             $to = $this->template[$type]['email'];
 
             $view = $struct['view'];
-            $from = null;
 
             $from = (isset($struct['from'])) ? $struct['from'] : null;
 
             // This finally sends the mail
             if ($this->isMailEnabled($event, $isMerchant))
             {
-                $this->sendMail($view, $subject, $to);
+                $this->sendMail($view, $subject, $to, $from);
             }
         }
     }
