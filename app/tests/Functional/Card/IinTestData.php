75b571358fd39d2f2ad9d66e4221b483e0d6641e
Sunny Aggrawal <sunnyagg@outlook.com>
2016-02-09 20:32:54 +0530

    test case fix

--- app/tests/Functional/Card/IinTestData.php
+++ app/tests/Functional/Card/IinTestData.php
@@ -79,7 +79,7 @@ return [
         'response' => [
             'content' => [
                 'entity' => 'collection',
-                'count' => 7,
+                'count' => 8,
                 'items' => [
                     [
                     ]
