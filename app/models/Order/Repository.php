b73e4d0b548dc1e2ad55ce22a07dfbfe96a43233
Shashank Kumar <shashankkumar.me@gmail.com>
2016-02-16 23:29:10 +0530

    [orders] Adds filters for fetching order entity via admin auth.

--- app/models/Order/Repository.php
+++ app/models/Order/Repository.php
@@ -9,4 +9,10 @@ class Repository extends Base\Repository
     use Base\RepositoryFetch;
 
     protected $entity = 'order';
+
+    protected $appFetchParamRules = array(
+        Entity::MERCHANT_ID     => 'sometimes|alpha_num',
+        Entity::STATUS 			=> 'sometimes|in:created,attempted,paid',
+        Entity::AUTHORIZED 		=> 'sometimes|in:0,1',
+    );
 }
