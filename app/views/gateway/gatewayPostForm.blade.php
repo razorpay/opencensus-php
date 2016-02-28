8ed365e91e31dd966496b23d4de13b57c3b30dc6
Pranav Gupta <pranavguptarulz@gmail.com>
2016-02-29 02:59:49 +0530

    loading message added to gateway post form

--- app/views/gateway/gatewayPostForm.blade.php
+++ app/views/gateway/gatewayPostForm.blade.php
@@ -1,7 +1,6 @@
 <!doctype html>
-<html lang="en">
+<html lang="en" style="height: 100%; overflow: hidden;font-family:ubuntu,helvetica,sans-serif;">
     <script>
-
         try{
 
             var payment_id = "{{$data['payment_id']}}";
@@ -13,13 +12,26 @@
             }
 
         } catch(e){}
-
-        function sub() {
-            document.form1.submit();
-        }
     </script>
 
-    <body onload="sub();">
+    <body onload="document.form1.submit()" style="text-align: center; height: 100%">
+    <div style="position: relative; top: 50%; margin-top: -100px;">
+        <div>Please wait while your transaction is processed...</div>
+        <div id="powered" style="font-size: 13px; position: absolute; right: 50%; bottom: 35px; letter-spacing: 0.25px; color: #888; margin-right: -96px;">powered by</div>
+        <script>
+            try{
+                var parent = document.currentScript.parentNode;
+                var src = localStorage.getItem('rzppowered');
+                var img = document.createElement('img');
+                img.src = src;
+                img.width = 192;
+                img.style.marginTop = '60px';
+                parent.appendChild(img);
+                document.querySelector('powered').style.display = 'block';
+            }
+            catch(e){}
+        </script>
+    </div>
     <form id="form1" name="form1" action="{{$data['request']['url']}}" method="post" onsubmit="return true;">
 
 @foreach ($data['request']['content'] as $key => $value)
