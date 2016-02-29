37bdf915e9d8fda9a356dc4aae546c698257e4e2
Abhay Rana <nemo@razorpay.com>
2016-02-29 15:16:59 +0530

    [bank-account] Allows alphabets in bank account numbers

--- app/tests/Integration/MerchantTest.php
+++ app/tests/Integration/MerchantTest.php
@@ -318,7 +318,7 @@ class MerchantTest extends TestCase
             ->click(l::css('form[name="step3"] > fieldset > .prev-next > .btn-next'))
             ->waitForVisible('form[name="step4"]')
             ->type(l::IdOrName('bank_name'), 'BankName')   // Fill name
-            ->type(l::IdOrName('bank_account_number'), '123443')   // Fill slug
+            ->type(l::IdOrName('bank_account_number'), 'RZP123443')   // Fill slug, alphanumeric
             ->type(l::IdOrName('bank_account_name'), 'Tester')   // Fill slug
             ->type(l::IdOrName('bank_account_type'), 'savings')   // Fill slug
             ->type(l::IdOrName('bank_branch_address'), 'Abcd')   // Fill slug
