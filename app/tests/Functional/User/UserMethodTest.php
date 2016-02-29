3330533cbca270d09095e660af12bc5e67f41e07
Sunny Aggrawal <sunnyagg@outlook.com>
2016-02-29 15:25:03 +0530

    moving user methods test to a new files

---
+++ app/tests/Functional/User/UserMethodTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Tests\Functional\UserMethod;
+
+use Tests\Functional\TestCase;
+use Tests\Functional\RequestResponseFlowTrait;
+
+class UserMethodTest extends TestCase
+{
+    use RequestResponseFlowTrait;
+
+    public function setUp()
+    {
+        $this->testDataFilePath = __DIR__.'/UserTestData.php';
+
+        parent::setUp();
+
+        $user = $this->fixtures->create('user:default_users');
+
+        $userMethods = $this->fixtures->create('user_method:default_user_methods');
+    }
+    
+/*
+    public function testAddUserMethodCard()
+    {
+        $this->ba->privateAuth();
+
+        $this->startTest();
+    }
+
+    public function testAddUserMethodWallet()
+    {
+        $this->ba->privateAuth();
+
+        $this->startTest();
+    }
+
+    public function testAddUserMethodNetbanking()
+    {
+        $this->ba->privateAuth();
+
+        $this->startTest();
+    }
+
+    public function testUpdateUserMethodCard()
+    {
+        $this->ba->privateAuth();
+
+        $this->startTest();
+    }
+*/
+    public function testGetUserMethods()
+    {
+        $this->ba->privateAuth();
+
+        $this->startTest();
+    }
+
+    public function testGetUserMethod()
+    {
+        $this->ba->privateAuth();
+
+        return $this->startTest();
+    }
+
+    public function testDeleteUserMethod()
+    {
+        $this->ba->privateAuth();
+
+        $this->startTest();
+    }
+}
--- app/tests/Functional/User/UserTest.php
+++ app/tests/Functional/User/UserTest.php
@@ -16,8 +16,6 @@ class UserTest extends TestCase
         parent::setUp();
 
         $user = $this->fixtures->create('user:default_users');
-
-        $userMethods = $this->fixtures->create('user_method:default_user_methods');
     }
 
     public function testCreateUser()
@@ -46,55 +44,6 @@ class UserTest extends TestCase
     }
 
     public function testDeleteUser()
-    {
-        $this->ba->privateAuth();
-
-        $this->startTest();
-    }
-/*
-    public function testAddUserMethodCard()
-    {
-        $this->ba->privateAuth();
-
-        $this->startTest();
-    }
-
-    public function testAddUserMethodWallet()
-    {
-        $this->ba->privateAuth();
-
-        $this->startTest();
-    }
-
-    public function testAddUserMethodNetbanking()
-    {
-        $this->ba->privateAuth();
-
-        $this->startTest();
-    }
-
-    public function testUpdateUserMethodCard()
-    {
-        $this->ba->privateAuth();
-
-        $this->startTest();
-    }
-*/
-    public function testGetUserMethods()
-    {
-        $this->ba->privateAuth();
-
-        $this->startTest();
-    }
-
-    public function testGetUserMethod()
-    {
-        $this->ba->privateAuth();
-
-        return $this->startTest();
-    }
-
-    public function testDeleteUserMethod()
     {
         $this->ba->privateAuth();
 
