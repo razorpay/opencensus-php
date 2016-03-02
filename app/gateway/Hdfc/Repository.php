4559ac98c96dff1e92a1d8c61f3470152800f476
Shashank Kumar <shashankkumar.me@gmail.com>
2016-03-02 13:19:09 +0530

    Minor fix for auth filter in hdfc repo.

--- app/gateway/Hdfc/Repository.php
+++ app/gateway/Hdfc/Repository.php
@@ -14,7 +14,7 @@ class Repository extends Base\Repository
 
     protected $appFetchParamRules = array(
         Entity::PAYMENT_ID              => 'sometimes|string|size:14',
-        'auth'                          => 'sometimes|integer',
+        'auth'                          => 'sometimes|string|max:6',
         'gateway_transaction_id'        => 'sometimes|numeric|digits:16',
         'ref'                           => 'sometimes|numeric|digits:12');
 
