f4745b5864660154ee1d8f6642e6a3c8e7c6d1a7
Abhay Rana <nemo@razorpay.com>
2016-03-02 16:10:44 +0530

    [timeout] Fixes timeouts on the Daily Report generation

--- app/models/Merchant/Service.php
+++ app/models/Merchant/Service.php
@@ -511,6 +511,9 @@ class Service extends Base\Service
      */
     public function sendDailyReportForAllMerchants()
     {
+        ini_set('memory_limit', '1024M');
+        set_time_limit(300);
+
         $filter = [];
 
         // In test, none of the merchants are activated
