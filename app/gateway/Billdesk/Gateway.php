2d99850815f0b509e090cdd51553553d07bf8ce4
Shashank Kumar <shashankkumar.me@gmail.com>
2016-03-04 02:55:18 +0530

    Couple of billdesk payment verification fixes.

--- app/gateway/Billdesk/Gateway.php
+++ app/gateway/Billdesk/Gateway.php
@@ -46,7 +46,7 @@ class Gateway extends Base\Gateway
         // We should ensure once that AuthStatus is 0300 and
         // RefundStatus is null.
 
-        assert ($payment['RefundStatus'] === null);
+        assert ($payment['RefStatus'] === null);
         assert ($payment['AuthStatus'] === AuthStatus::SUCCESS);
     }
 
@@ -111,8 +111,8 @@ class Gateway extends Base\Gateway
             // In that case, we need to let the refund go ahead.
 
             if (($content['ErrorCode'] === 'ERR_REF009') and
-                ($payment['RefundStatus'] === RefundStatus::CANCELLED) and
-                ((int) $payment['TotalRefundAmount'] * 100 === $input['payment']['amount']))
+                ($payment['RefStatus'] === RefundStatus::CANCELLED) and
+                ((int) $payment['RefAmount'] * 100 === $input['payment']['amount']))
             {
                 return;
             }
@@ -141,7 +141,7 @@ class Gateway extends Base\Gateway
         $content = $verify->verifyResponseContent;
         $input = $verify->input;
 
-        $amountRefunded = (int) ($content['TotalRefundAmount'] * 100);
+        $amountRefunded = (int) ($content['RefAmount'] * 100);
 
         $status = VerifyResult::STATUS_MATCH;
 
