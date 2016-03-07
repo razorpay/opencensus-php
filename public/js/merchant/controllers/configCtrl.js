db99a25e23162d44e69289a8b122fd90707c7a38
Abhay Rana <nemo@razorpay.com>
2016-03-07 15:44:18 +0530

    Adds a basic controller with a fetch method to fetch configuration

--- public/js/merchant/controllers/configCtrl.js
+++ public/js/merchant/controllers/configCtrl.js
@@ -1,7 +1,7 @@
 /**
  * Webhooks Ctrl
  */
-app.controller('WebhooksCtrl', [
+app.controller('ConfigCtrl', [
   '$scope',
   '$http',
   'alertsFactory',
@@ -10,8 +10,30 @@ app.controller('WebhooksCtrl', [
   function ($scope, $http, alertsFactory, transformRequestAsFormPost, $modal) {
     //Intialise alerts and scope functions
     $scope.alerts = alertsFactory.getHandler();
-    $scope.config = {
+    $scope.config = {};
 
-    };
+    $scope.fetchConfig = function() {
+
+      var request = $http({
+        method: 'get',
+        url: '/merchants/config'
+      });
+
+      request.success(function (data) {
+        if (data.success) {
+          $scope.config = data.data;
+        } else {
+          $scope.alerts.resetAlerts();
+          angular.forEach(data.errors, function (value, key) {
+            $scope.alerts.addAlert('danger', value);
+          });
+        }
+      }).error(function () {
+        $scope.alerts.addAlert('danger', null, true);
+      });
+    }
+
+    // Fetch the config on load
+    $scope.fetchConfig();
   }
 ]);
