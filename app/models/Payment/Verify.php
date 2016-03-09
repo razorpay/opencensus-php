c7e8f19b1bc64129eb076bc81a5d37d721b9d80e
Shashank Kumar <shashankkumar.me@gmail.com>
2016-03-10 00:45:33 +0530

    Reduces time before verifying a payment in created state.

--- app/models/Payment/Verify.php
+++ app/models/Payment/Verify.php
@@ -67,7 +67,7 @@ class Verify
 
     public function verifyPaymentsWithCreatedStatus()
     {
-        $ts = time() - 4 * 60;
+        $ts = time() - 3 * 60;
 
         $payments = $this->repo->getPaymentsWithCreatedStatusForVerification($ts);
 
