3c5546219106559bf7f770b605f8f4bc7cdfa9a6
Shashank Kumar <shashankkumar.me@gmail.com>
2016-03-10 14:33:28 +0530

    Fixes fixtures.

--- app/tests/Functional/Fixtures/Factory/factory.php
+++ app/tests/Functional/Fixtures/Factory/factory.php
@@ -14,7 +14,7 @@ $factory(Models\Merchant\Entity::class, [
     'transaction_report_email' => 'test@razorpay.com',
     'receipt_email_enabled' => true,
     'settlement_schedule' => 3,
-    'fee_bearer' => Models\Merchant\FeeBearer::getValueForBearerString(Models\Merchant\FeeBearer::PLATFORM),
+    'fee_bearer' => Models\Merchant\FeeBearer::PLATFORM,
     'risk_rating' => 3,
 ]);
 
