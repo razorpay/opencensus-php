cc06c3b82b89da169d27bf30fc688d4af358b980
Pranav Gupta <pranavguptarulz@gmail.com>
2016-03-14 18:33:06 +0530

    no-transform header on checkout.php

--- public/communicator.php
+++ public/communicator.php
@@ -1,7 +1,7 @@
 <?php
 header('P3P: CP="We dont have any P3P Policy"');
 header('Expires: Sat, 01 Jan 2000 00:00:00 GMT');
-header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: no-transform, no-store, no-cache, must-revalidate');
 header('Cache-Control: post-check=0, pre-check=0', FALSE);
 header('Pragma: no-cache');
 ?>
