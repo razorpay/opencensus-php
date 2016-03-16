572aa61319e46b625de7b35918326551f9806352
Giridaran M <giri@razorpay.com>
2016-03-16 19:12:41 +0530

    Added Netbanking kotak live relative url.

--- app/gateway/Netbanking/Kotak/Url.php
+++ app/gateway/Netbanking/Kotak/Url.php
@@ -4,9 +4,10 @@ namespace Gateway\Netbanking\Kotak;
 
 class Url
 {
-    const LIVE_DOMAIN   = 'https://www.kotak.com';
-    const TEST_DOMAIN   = 'https://203.196.200.42';
+    const LIVE_DOMAIN    = 'https://www.kotak.com';
+    const TEST_DOMAIN    = 'https://203.196.200.42';
 
-    const AUTHORIZE     = '/pgx/ksecLogin.jsp';
-    const VERIFY        = '/pg/kbsegquery.jsp';
+    const TEST_AUTHORIZE = '/pgx/ksecLogin.jsp';
+    const LIVE_AUTHORIZE = '/pg/ksecLogin.jsp';
+    const VERIFY         = '/pg/kbsegquery.jsp';
 }
