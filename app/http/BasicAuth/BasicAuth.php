992271dfbaca061ea8005ad4424643558e750e6c
Abhay Rana <nemo@razorpay.com>
2016-04-12 14:40:18 +0530

    [ssl] Don't parse the hostname, but whitelist

--- app/http/BasicAuth/BasicAuth.php
+++ app/http/BasicAuth/BasicAuth.php
@@ -49,6 +49,12 @@ class BasicAuth
         'public_key' => '',
         'secret' => '');
 
+    const PRODUCTION_HOSTS = [
+        'alpha.razorpay.com',
+        'beta.razorpay.com',
+        'api.razorpay.com'
+    ];
+
     /**
      * Key used for authentication
      * @var Key\Entity
@@ -397,12 +403,17 @@ class BasicAuth
     }
 
 
+    /**
+     * Ensures that HTTPS is forced on all
+     * production environments
+     */
     public function verifyHttps()
     {
         $host = $this->request->getHttpHost();
-        $rootDomain = substr($host, -12);
 
-        if (( $rootDomain=== 'razorpay.com') and
+        // If we are on a production host and HTTP is being used
+        // Throw an error
+        if ( in_array($host, self::PRODUCTION_HOSTS) and
             ($this->request->secure() === false))
         {
             return ApiResponse::generateErrorResponse(
