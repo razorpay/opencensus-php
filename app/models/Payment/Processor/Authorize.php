63b347fbfb4e65b80a2ebb750c2386d8c8ceaf01
Sunny Aggrawal <sunnyagg@outlook.com>
2016-04-13 12:19:48 +0530

    fix for exception in saving duplicate token

--- app/models/Payment/Processor/Authorize.php
+++ app/models/Payment/Processor/Authorize.php
@@ -437,10 +437,13 @@ trait Authorize
         {
             (new Token\Core)->create($customer, $saveMethodInput);
         }
-        catch (Exception\BaseException $e)
+        catch (Exception\RecoverableException $e)
+        {
+            // Ignore the exception, can be an already saved method
+        }
+        catch (\Exception $e)
         {
             $this->trace->traceException($e);
-            // Ignore the exception, can be an already saved method
         }
     }
 
