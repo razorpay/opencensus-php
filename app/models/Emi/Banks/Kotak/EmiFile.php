f0d4dd5d40e77799d7bd9cf7d0a599d27213571f
Sunny Aggrawal <sunnyagg@outlook.com>
2016-04-15 17:23:27 +0530

    fix for emi file generation from global saved card

--- app/models/Emi/Banks/Kotak/EmiFile.php
+++ app/models/Emi/Banks/Kotak/EmiFile.php
@@ -113,6 +113,11 @@ class EmiFile extends Base\EmiFile
 
     protected function getCardNumber($card)
     {
+        if ($card->globalCard !== null)
+        {
+            $card = $card->globalCard;
+        }
+
         $cardToken = $card->getVaultToken();
 
         $app = \App::getFacadeRoot();
