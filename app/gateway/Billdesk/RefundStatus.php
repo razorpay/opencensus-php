c55ddc5acf9d72fb1b374864c00e3411aa4b890f
Shashank Kumar <shashankkumar.me@gmail.com>
2016-04-16 22:51:06 +0530

    [billdesk] Adds billdesk payment status for support team's help.

--- app/gateway/Billdesk/RefundStatus.php
+++ app/gateway/Billdesk/RefundStatus.php
@@ -6,4 +6,10 @@ class RefundStatus
 {
     const CANCELLED = '0699';
     const REFUNDED  = '0799';
+    const NA        = 'NA';
+
+    public static $statusMap = array(
+        self::CANCELLED => 'cancelled',
+        self::REFUNDED  => 'refunded',
+        self::NA        => null);
 }
