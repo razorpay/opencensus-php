3bae6280c80c6ec52b9a8d9d07d3a983c3e61dee
Shashank Kumar <shashankkumar.me@gmail.com>
2016-04-16 13:24:36 +0530

    Capitlizes ifsc code when generating beneficiary file.

--- app/models/Merchant/BankAccount/BeneficiaryFile2.php
+++ app/models/Merchant/BankAccount/BeneficiaryFile2.php
@@ -50,6 +50,9 @@ class BeneficiaryFile2
 
         foreach ($list as $ba)
         {
+            $ifsc = $ba->getAttribute(BankAccount\Entity::IFSC_CODE);
+            $ifsc = strtoupper($ifsc);
+
             $array = array(
                 'Client_Code'           => $ba->getAttribute(BankAccount\Entity::BENEFICIARY_CODE),
                 'Bene_Code'             => '',
@@ -67,7 +70,7 @@ class BeneficiaryFile2
                 'Bene_Mobile'           => $ba->getAttribute(BankAccount\Entity::BENEFICIARY_MOBILE),
                 'Bene_Tel'              => '',
                 'Bene_Fax'              => '',
-                'IFSC'                  => $ba->getAttribute(BankAccount\Entity::IFSC_CODE),
+                'IFSC'                  => $ifsc,
                 'Bene_A/c No.'          => "'".$ba->getAttribute(BankAccount\Entity::ACCOUNT_NUMBER),
             );
 
