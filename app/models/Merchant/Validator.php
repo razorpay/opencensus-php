84da3896b9982fdc6cc64513431e8955fab3bdb4
Abhay Rana <nemo@razorpay.com>
2016-04-26 17:25:00 +0530

    [fix] Submerchant creation should not check email uniqueness

--- app/models/Merchant/Validator.php
+++ app/models/Merchant/Validator.php
@@ -13,7 +13,7 @@ class Validator extends Base\Validator
 
     protected static $createSubmerchantRules = array(
         'name'                  => 'required|alpha_space_num|max:200',
-        'email'                 => 'sometimes|email|unique:merchants',
+        'email'                 => 'sometimes|email',
     );
 
     protected static $unsetCreateInput = array(
