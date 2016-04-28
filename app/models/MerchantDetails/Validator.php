bf79133bbc30a5a756aa2901d312eaa6d7442a79
Abhay Rana <nemo@razorpay.com>
2016-04-28 20:25:49 +0530

    [activation] Drops the bank name and the bank address

--- app/models/MerchantDetails/Validator.php
+++ app/models/MerchantDetails/Validator.php
@@ -56,11 +56,11 @@ class Validator extends Base\Validator
     );
 
     protected static $step4Rules = array(
-        'bank_name'             => 'required|alpha_space|max:255',
+        'bank_name'             => 'sometimes|alpha_space|max:255',
         'bank_account_number'   => 'required|alpha_num|between:5,20',
         'bank_account_name'     => 'required|alpha_space_num|max:40',
         'bank_account_type'     => 'required|alpha_space|max:20',
-        'bank_branch'           => 'required|max:255',
+        'bank_branch'           => 'sometimes|max:255',
         'bank_branch_ifsc'      => 'required|alpha_num|max:11',
         'bank_beneficiary_address1' => 'required|max:30',
         'bank_beneficiary_address2' => 'max:30',
