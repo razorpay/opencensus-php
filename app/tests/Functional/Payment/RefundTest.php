57469e5b3912a4993c31697fa9bad78ea2e29144
Giridaran M <giri@razorpay.com>
2016-04-29 14:56:55 +0530

    [refund] Add test for buggy case.

--- app/tests/Functional/Payment/RefundTest.php
+++ app/tests/Functional/Payment/RefundTest.php
@@ -169,6 +169,35 @@ class RefundTest extends TestCase
 
     }
 
+    // Testing Buggy Case where a payment is captured in hdfc gateway
+    // But is in authorised state in RZP db.
+    // This will also be picked up for a refund and refunded.
+    public function testRefundOnHdfcCapturedPaymentAuthorized()
+    {
+        $authorizedAt = Carbon::today('Asia/Kolkata')->subDays(10)->timestamp;
+
+        $payment = $this->fixtures->create(
+            'payment:captured',
+            ['authorized_at' => $authorizedAt, 'created_at' => $authorizedAt]);
+
+        $this->fixtures->payment->edit($payment->getId(), ['status' => 'authorized']);
+
+        $content = $this->refundOldAuthorizedPayments();
+
+        $this->assertArrayHasKey('refunded', $content);
+        $this->assertEquals(1, $content['refunded']);
+        $this->assertArrayHasKey('authorized', $content);
+        $this->assertEquals(1, $content['authorized']);
+
+        $refundedEntities = $this->getEntities('hdfc', ['count' => 1], true);
+
+        foreach ($refundedEntities['items'] as $entity)
+        {
+            $this->assertEquals('refunded', $entity['status']);
+        }
+
+    }
+
     public function testFetchRefundById()
     {
         $payment = $this->fixtures->create('payment:captured');
