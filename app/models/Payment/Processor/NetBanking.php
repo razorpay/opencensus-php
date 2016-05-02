29f937e55f261a801833c6eb16c77058b41263dd
Giridaran M <giri@razorpay.com>
2016-05-02 19:50:10 +0530

    [netbanking] Fix tests and disable ICICI temporarily

--- app/models/Payment/Processor/NetBanking.php
+++ app/models/Payment/Processor/NetBanking.php
@@ -74,7 +74,7 @@ class Netbanking
         IFSC::DLXB,
         IFSC::FDRL,
         IFSC::IBKL,
-        // IFSC::ICIC, Temporarily disabling this while billdesk issue is resolved
+        IFSC::ICIC, //Temporarily disabling this while billdesk issue is resolved
         IFSC::IDIB,
         IFSC::INDB,
         IFSC::IOBA,
