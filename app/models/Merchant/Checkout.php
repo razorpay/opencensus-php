dc3e62eb7474c7b3e3d831672a00c1833dda1098
Prashanth Y.V <prashanth.yv@razorpay.com>
2016-05-10 13:57:12 +0530

    naming checkout logo image option to just image

--- app/models/Merchant/Checkout.php
+++ app/models/Merchant/Checkout.php
@@ -50,7 +50,7 @@ class Checkout
 
         $data['methods'] = $methodsArray;
         $data['options']['theme']['color'] = $merchant->getBrandColor();
-        $data['options']['logo'] = $merchant->getLogoUrl(self::CHECKOUT_LOGO_SIZE);
+        $data['options']['image'] = $merchant->getLogoUrl(self::CHECKOUT_LOGO_SIZE);
         $data['fee_bearer'] = false;
         $data['version'] = 1;
 
