8e390db7743dcd477874d365ca5ab346e805c403
Prashanth Y.V <prashanth.yv@razorpay.com>
2016-05-25 04:22:07 +0530

    [hdfc gateway refactor] Minor refactoring and added a couple of comments

--- app/gateway/Hdfc/Payment/Inquiry.php
+++ app/gateway/Hdfc/Payment/Inquiry.php
@@ -6,7 +6,7 @@ use EE\Exception;
 use Gateway\Base;
 use Gateway\Base\VerifyResult;
 use Gateway\Hdfc;
-use Gateway\Hdfc\Payment;
+use Gateway\Hdfc\Payment as GatewayPayment;
 use Trace\Trace;
 use Trace\TraceCode;
 
@@ -184,82 +184,79 @@ trait Inquiry
 
     protected function sendPaymentVerifyRequest($verify)
     {
+        // Gets the payment entity that has to be verified
         $payment = $verify->payment;
-        $content = $this->getPaymentVerifyRequestContentArray($verify);
 
-        $this->trace->info(
-            TraceCode::GATEWAY_PAYMENT_VERIFY,
-            [$content]);
+        // Gets the request array for verify from gateway
+        $requestContent = $this->getPaymentVerifyRequestContentArray($verify);
 
-        $inquiryRequest = &$this->inquiryRequest;
-        $inquiryRequest['url'] = Hdfc\Urls::SUPPORT_PAYMENT_URL;
+        // Sets the gateway URL for the inquiry (verifying payment status)
+        $this->inquiryRequest['url'] = Hdfc\Urls::SUPPORT_PAYMENT_URL;
 
-        $data = &$this->inquiryRequest['data'];
-        $data = [];
-        $data = $content;
+        // Sets the request body for the inquiry (verifying payment status)
+        $this->inquiryRequest['data'] = $requestContent;
 
+        // TO NOTE: This is just initializing RESPONSE from the inquiry.
         $this->inquiryResponse['data'] = [];
 
         $this->trace->info(
             TraceCode::GATEWAY_PAYMENT_VERIFY_REQUEST,
-            $inquiryRequest);
+            $this->inquiryRequest);
 
+        // This sets the response received from the inquiry
         $this->runRequestResponseFlow(
             $this->inquiryRequest,
             $this->inquiryResponse);
 
-        $data = & $this->inquiryResponse['data'];
+        $responseData = & $this->inquiryResponse['data'];
 
-        if ((isset($data['result'])) and
-            ($data['result'] === Result::SUCCESS))
+        if ((isset($responseData['result'])) and
+            ($responseData['result'] === Result::SUCCESS))
         {
-            $payment = $verify->payment;
+            // TODO: Add a comment on what is `$payment['action']`
+            $paymentAction = $payment['action'];
 
-            $action = $payment['action'];
-
-            if ($payment['action'] === Payment\Action::AUTHORIZE)
+            if ($paymentAction === GatewayPayment\Action::AUTHORIZE)
             {
                 $result = Result::APPROVED;
             }
-            else if ($payment['action'] === Payment\Action::PURCHASE)
+            // TODO: Check if this should be `GatewayPayment\Action::CAPTURE`
+            else if ($paymentAction === GatewayPayment\Action::PURCHASE)
             {
                 $result = Result::CAPTURED;
             }
             else
             {
                 throw new Exception\LogicException(
-                    'Unexpected action: ' . $payment['action']);
+                    'Unexpected action: ' . $paymentAction);
             }
 
-            $data['result'] = $result;
+            $responseData['result'] = $result;
         }
 
         $inquiryResponse = $this->inquiryResponse;
-        $content = $this->inquiryResponse['data'];
+        $responseContent = $this->inquiryResponse['data'];
 
         $this->trace->info(
             TraceCode::GATEWAY_PAYMENT_VERIFY_RESPONSE,
             [
                 'payment_id' => $payment->getPaymentId(),
-                'xml' => $inquiryResponse['xml']
+                'xml' => $inquiryResponse['xml'],
+                'response_content' => $responseContent
             ]);
-
-        $this->trace->info(
-            TraceCode::GATEWAY_PAYMENT_VERIFY_RESPONSE,
-            $content);
 
         $verify->verifyResponse = $inquiryResponse;
         $verify->verifyResponseBody = $inquiryResponse['xml'];
-        $verify->verifyResponseContent = $content;
+        $verify->verifyResponseContent = $responseContent;
 
-        return $content;
+        return $responseContent;
     }
 
     protected function getPaymentVerifyRequestContentArray($verify)
     {
         $payment = $verify->payment;
 
-        $content['action'] = Payment\Action::INQUIRY;
+        $content['action'] = GatewayPayment\Action::INQUIRY;
         $content['transid'] = $payment['gateway_transaction_id'];
         $content['udf5'] = 'PaymentID';
 
