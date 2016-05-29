2441f428d3ac03fc9e5a625163cbd1e111f6f527
Prashanth Y.V <prashanth.yv@razorpay.com>
2016-05-29 12:08:26 +0530

    [invoice report] doesn't send cess components if not applicable

--- app/models/Base/Report.php
+++ app/models/Base/Report.php
@@ -181,8 +181,8 @@ class Report extends Service
         $totalTax = $data[self::TAX];
 
         // Sets the default amounts for the cess-es, in case they are not applicable in the time period.
-        $taxes[self::SWACH_BHARAT_CESS] = 0;
-        $taxes[self::KRISHI_KALYAN_CESS] = 0;
+        $taxes[self::SWACH_BHARAT_CESS] = null;
+        $taxes[self::KRISHI_KALYAN_CESS] = null;
 
         // This is all in Paise
         // so we can round to the nearest integer
@@ -199,7 +199,8 @@ class Report extends Service
         $taxes[self::SERVICE_TAX] = $totalTax - $taxes[self::SWACH_BHARAT_CESS] - $taxes[self::KRISHI_KALYAN_CESS];
         $taxes[self::SERVICE_TAX] = round($taxes[self::SERVICE_TAX]);
 
-        $data[self::TAXES] = $taxes;
+        // array_filter is being used here to remove the cess keys if they are null or 0.
+        $data[self::TAXES] = array_filter($taxes);
     }
 
     /**
