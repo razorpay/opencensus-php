ba9acc2257918e34ac942d02153894f71e4b7c01
Shashank Kumar <shashankkumar.me@gmail.com>
2016-06-02 21:24:55 +0530

    Fixes sharp bug.

--- app/gateway/Sharp/Gateway.php
+++ app/gateway/Sharp/Gateway.php
@@ -136,8 +136,11 @@ class Gateway extends Base\Gateway
 
         $method = 'post';
 
-        $content['card_number'] = $this->encryptCardNumber($input['card']['number']);
-        $content['encrypt'] = '1';
+        if ($input['payment']['method'] === 'card')
+        {
+            $content['card_number'] = $this->encryptCardNumber($input['card']['number']);
+            $content['encrypt'] = '1';
+        }
 
         if (($input['payment']['method'] === 'card') and
             ($input['card']['number'] === '4111111111111111'))
