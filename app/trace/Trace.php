5e09175e101c746a0d5bd63c44af32a3d1c38984
Shashank Kumar <shashankkumar.me@gmail.com>
2016-06-02 13:31:46 +0530

    Adds support for simple email notification in case tracing fails due to any issue.

--- app/trace/Trace.php
+++ app/trace/Trace.php
@@ -15,7 +15,37 @@ class Trace extends TraceWriter
 
         $context = $this->getContext($traceCode, $context);
 
-        return parent::addRecord($level, $traceCode, $context);
+        try
+        {
+            return parent::addRecord($level, $traceCode, $context);
+        }
+        catch (\Exception $e)
+        {
+            $data = array(
+                'type' => get_class($exception),
+                'message' => $exception->getMessage(),
+                'code' => $exception->getCode(),
+                'file' => $exception->getFile(),
+                'line' => $exception->getLine(),
+                'trace' => $exception->getTraceAsString()
+            );
+
+            $msg = '';
+
+            foreach ($data as $key => $value)
+            {
+                $msg .= "$key => $value" . PHP_EOL;
+            }
+
+            $subject = self::CHANNEL . ' - Critical error occurred';
+
+            // No point checking it's return value at this point because have
+            // already experienced a critical failure upstream and this is
+            // just a mechanism for out-of-band notification.
+            // Just pray that it's working actually _/\_
+
+            mail('developers@razorpay.com', $subject, $msg);
+        }
     }
 
     /**
