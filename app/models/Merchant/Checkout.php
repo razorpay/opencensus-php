e7096d25808b321752e5f16e92f8a736308e8fca
Shashank Kumar <shashankkumar.me@gmail.com>
2016-06-17 19:10:47 +0530

    [identity] Minor improvements.

--- app/models/Merchant/Checkout.php
+++ app/models/Merchant/Checkout.php
@@ -42,34 +42,9 @@ class Checkout
             $input[App\Entity::DEVICE_TOKEN] = $deviceToken;
         }
 
-        $methodsArray = array(
-            'entity'        => 'methods',
-            'card'          => true,
-            'netbanking'    => [],
-            'wallet'        => [],
-            'emi'           => false
-        );
+        $methods = $this->getMethods($merchant, $input);
 
-        $methods = (new Methods\Core)->getMethods($merchant);
-
-        if ($methods !== null)
-        {
-            $methodsArray['card'] = $methods->isCardEnabled();
-            $netbankingEnabled = $methods->isNetbankingEnabled();
-            if ($netbankingEnabled === true)
-            {
-                $methodsArray['netbanking'] = $methods->toArrayWithBankNames();
-            }
-            $methodsArray['wallet'] = $methods->getEnabledWallets();
-            $methodsArray['emi'] = $methods->isEmiEnabled();
-        }
-
-        if ($mode === Mode::TEST)
-        {
-            $methods['card'] = true;
-        }
-
-        $data['methods'] = $methodsArray;
+        $data['methods'] = $methods;
         $data['options']['theme']['color'] = $merchant->getBrandColor();
         $data['options']['image'] = $merchant->getLogoUrl(self::CHECKOUT_LOGO_SIZE);
         $data['fee_bearer'] = false;
@@ -80,54 +55,9 @@ class Checkout
             $data['fee_bearer'] = true;
         }
 
-        //fetch customer data and saved cards data
-        if ((isset($input[Payment\Entity::CUSTOMER_ID])) or
-            (isset($input[Payment\Entity::APP_TOKEN])))
-        {
-            $custData = $this->fetchCustomerData($input, $merchant);
+        $this->checkAndFillSavedTokens($input, $merchant, $data);
 
-            if ($custData !== null)
-            {
-                $data['customer'] = $custData;
-            }
-        }
-        elseif ((isset($input[App\Entity::DEVICE_TOKEN])) and
-                (isset($input['contact'])))
-        {
-            $response = (new Customer\Service)->validateDeviceToken(
-                $input[App\Entity::DEVICE_TOKEN],
-                $input);
-
-            $data['customer'] = array(
-                'contact'   => $input['contact'],
-                'valid'     => $response['valid']);
-
-            if ($response['valid'] === true)
-            {
-                $data['customer'][Payment\Entity::APP_TOKEN] = $response[Payment\Entity::APP_TOKEN];
-            }
-        }
-        elseif (isset($input['contact']))
-        {
-            $response = (new Customer\Service)->fetchCustomerStatus($input['contact']);
-
-            $data['customer'] = array(
-                'contact'   => $input['contact'],
-                'saved'     => $response['saved']);
-        }
-
-        // If merchant is TPV enabled pass details for
-        // current order as part of preferences
-        if (($merchant->isTPVRequired()) and
-            (isset($input[Payment\Entity::ORDER_ID])))
-        {
-            $orderData = $this->fetchTPVOrderInfo($input, $merchant);
-
-            if ($orderData !== null)
-            {
-                $data['order'] = $orderData;
-            }
-        }
+        $this->checkAndAddOrderForTpv($merchant, $input, $data);
 
         return $data;
     }
@@ -183,5 +113,87 @@ class Checkout
         }
 
         return $custData;
+    }
+
+    protected function getMethods($merchant, $input)
+    {
+        $methodsArray = array(
+            'entity'        => 'methods',
+            'card'          => true,
+            'netbanking'    => [],
+            'wallet'        => [],
+            'emi'           => false
+        );
+
+        $methods = (new Methods\Core)->getMethods($merchant);
+
+        if ($methods !== null)
+        {
+            $methodsArray['card'] = $methods->isCardEnabled();
+            $netbankingEnabled = $methods->isNetbankingEnabled();
+            if ($netbankingEnabled === true)
+            {
+                $methodsArray['netbanking'] = $methods->toArrayWithBankNames();
+            }
+            $methodsArray['wallet'] = $methods->getEnabledWallets();
+            $methodsArray['emi'] = $methods->isEmiEnabled();
+        }
+
+        return $methodsArray;
+    }
+
+    protected function checkAndAddOrderForTpv($merchant, $inpu, & $data)
+    {
+        // If merchant is TPV enabled pass details for
+        // current order as part of preferences
+        if (($merchant->isTPVRequired()) and
+            (isset($input[Payment\Entity::ORDER_ID])))
+        {
+            $orderData = $this->fetchTPVOrderInfo($input, $merchant);
+
+            if ($orderData !== null)
+            {
+                $data['order'] = $orderData;
+            }
+        }
+    }
+
+    protected function checkAndFillSavedTokens($input, $merchant, & $data)
+    {
+        // fetch customer data and saved cards data
+        if ((isset($input[Payment\Entity::CUSTOMER_ID])) or
+            (isset($input[Payment\Entity::APP_TOKEN])))
+        {
+            $custData = $this->fetchCustomerData($input, $merchant);
+
+            if ($custData !== null)
+            {
+                $data['customer'] = $custData;
+            }
+        }
+        else if ((isset($input[App\Entity::DEVICE_TOKEN])) and
+                (isset($input['contact'])))
+        {
+            $response = (new Customer\Service)->validateDeviceToken(
+                $input[App\Entity::DEVICE_TOKEN],
+                $input);
+
+            $data['customer'] = array(
+                'contact'   => $input['contact'],
+                'valid'     => $response['valid']);
+
+            if ($response['valid'] === true)
+            {
+                $data['customer'][Payment\Entity::APP_TOKEN] = $response[Payment\Entity::APP_TOKEN];
+            }
+        }
+        else if (isset($input['contact']))
+        {
+            $response = (new Customer\Service)->fetchCustomerStatus($input['contact']);
+
+            $data['customer'] = array(
+                'contact'   => $input['contact'],
+                'saved'     => $response['saved']);
+        }
     }
 }
