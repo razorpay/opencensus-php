797f2290289ad095b13d11ec75df50ecdb084961
Harman Singh <harman28@gmail.com>
2016-06-24 17:22:04 +0530

    Set pwd in config files; refactored common method getZippedFile

--- .env.wercker.php
+++ .env.wercker.php
@@ -138,6 +138,8 @@ return array(
     'AWS_S3_SETTLEMENT_BUCKET'                      => '',
     'AWS_S3_LOGO_BUCKET'                            => '',
 
+    'EMI_FILE_PASSWORD'                             => getenv('EMI_FILE_PASSWORD'),
+
     'CRON_PASSWORD'                                 => 'a923r8u98uwaf98uw9w8fu',
 
     'RUN_FIXTURES'                                  => true,
--- app/models/Emi/Banks/Base/EmiFile.php
+++ app/models/Emi/Banks/Base/EmiFile.php
@@ -2,6 +2,7 @@
 
 namespace Models\Emi\Banks\Base;
 
+use ZipArchive;
 use Carbon\Carbon;
 use Models\Card;
 use Models\Settlement\Kotak\FileHandlerTrait;
@@ -34,4 +35,19 @@ class EmiFile
         return $cardNumber;
     }
 
+    protected function getZippedFile()
+    {
+        $zippath = $this->getZipFullFilePath();
+        $fullpath = $this->getExcelFullFilePath();
+
+        $password = \Config::get('applications.emi')['password'];
+
+        $zip = new ZipArchive();
+        $zip->open($zippath, ZipArchive::CREATE);
+        $zip->addFile($fullpath);
+        $zip->setPassword($password);
+        $zip->close();
+
+        return $zippath;
+    }
 }
