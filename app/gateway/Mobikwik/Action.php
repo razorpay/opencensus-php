04d583f0a2b4b9fccbab07f395a0657fce910bb2
Priyanshu Chhazed <priyanshu.chhazed@razorpay.com>
2016-06-28 11:32:08 +0530

    fixed alignment in mobikwik gateway

--- app/gateway/Mobikwik/Action.php
+++ app/gateway/Mobikwik/Action.php
@@ -9,5 +9,5 @@ class Action extends Base\Action
     const CHECK_USER    = 'check_user';
     const OTP_GENERATE  = 'otp_generate';
     const OTP_SUBMIT    = 'otp_submit';
-    const CREATE_USER = 'create_user';
+    const CREATE_USER   = 'create_user';
 }
--- app/gateway/Mobikwik/Url.php
+++ app/gateway/Mobikwik/Url.php
@@ -4,16 +4,16 @@ namespace Gateway\Mobikwik;
 
 class Url
 {
-    const LIVE_DOMAIN 	= 'https://walletapi.mobikwik.com';
+    const LIVE_DOMAIN   = 'https://walletapi.mobikwik.com';
     // const TEST_DOMAIN   = 'https://mobikwik.com';
     const TEST_DOMAIN   = 'https://test.mobikwik.com';
     const API_DOMAIN    = 'https://walletapi.mobikwik.com';
 
-    const AUTHORIZE 	= '/wallet';
-    const REFUND 		= '/walletrefund';
-    const VERIFY 		= '/checkstatus';
-    const CHECK_USER 	= '/querywallet';
-    const OTP_GENERATE 	= '/otpgenerate';
-    const OTP_SUBMIT 	= '/debitwallet';
-    const CREATE_USER = '/createwalletuser';
+    const AUTHORIZE     = '/wallet';
+    const REFUND        = '/walletrefund';
+    const VERIFY        = '/checkstatus';
+    const CHECK_USER    = '/querywallet';
+    const OTP_GENERATE  = '/otpgenerate';
+    const OTP_SUBMIT    = '/debitwallet';
+    const CREATE_USER   = '/createwalletuser';
 }
