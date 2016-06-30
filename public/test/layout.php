9f44be00b287a6b2fba3d1a490ed0c1ff139d3dd
Pranav Gupta <pranavguptarulz@gmail.com>
2016-06-30 19:35:31 +0530

    local api + checkout test page

--- public/test/layout.php
+++ public/test/layout.php
@@ -1,94 +1,54 @@
-<?php
-
-require('vars.php');
-
-?>
 <!doctype html>
-<html>
+<html style="background: #f6f6f6;">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Razorpay - Automatic Checkout</title>
-  <link rel="stylesheet" type="text/css" href="css/style.css">
-  <link href='//fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
-  <script src="<?= $checkout ?>/v1/checkout.js" type="text/javascript"></script>
+  <title>Razorpay - Checkout Testing page</title>
 </head>
-<body>
-  <div class="all-container">
-    <div class="header">
-      <div class="container">
-        <a href="https://www.razorpay.com" target="_blank" class="logo"></a>
-        <a class="nav active">Products</a>
-        <a class="nav">Delivery</a>
-        <a class="nav">Contact Us</a>
-        <div class="pull-right">
-          <a class="nav">Search</a>
-          <a class="nav">Checkout</a>
-        </div>
-      </div>
-    </div>
-    <div class="content">
-      <div class="container">
-        <div class="tab">
-          <div class="leftcontent">
-            <div class="product"></div>
-            <div class="thumbnails">
-              <div class="thumbnail"></div>
-              <div class="thumbnail"></div>
-              <div class="thumbnail"></div>
-              <div class="thumbnail "></div>
-            </div>
-          </div>
-          <div class="clear"></div>
-        </div>
-        <div class="tab">
-          <div class="rightcontent">
-            <h3>Fine Tshirt</h3>
-              <div class="rating"><img src="images/rating.png"></div>
-              <p class="review">5 Reviews</p>
-            <div class="price">₹5</div>
-
-            <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc fermentum tincidunt libero nec imperdiet. Etiam sed malesuada dolor. Integer id ante ut urna pretium aliquet et et quam. Fusce tempor ultricies velit non finibus. Nullam lacus nulla, vehicula vitae pharetra nec, vestibulum id odio. Nunc quis sapien vestibulum, vulputate ipsum nec, consequat erat. Nunc interdum pharetra commodo. Nullam blandit id neque id ultrices. Proin quis efficitur mauris.</p>
-            <p class="wishlist"><img src="images/wishlist.png"></p>
-            <div class="pay">
-              <input type="button" value="" class="razorpay-payment-button" id="paybtn">
-              <script>
-              window.r = new Razorpay({
-                key: 'rzp_test_1DP5mmOlF5G5ag',
-                protocol: '<?= $protocol ?>',
-                hostname: '<?= $hostname ?>',
-                amount: '500',
-                name: 'Merchant Name',
-                description: 'Fine tshirt',
-                image: 'https://i.imgur.com/3g7nmJC.png',
-                prefill: {
-                  name: 'Harshil Mathur',
-                  email: 'harshil@razorpay.com',
-                  contact: '9999999999'
-                },
-                handler: function (transaction){
-                  alert('You have successfully purchased Fine tshirt\ntransaction id: ' + transaction.razorpay_payment_id);
-                }
-              })
-              document.getElementById('paybtn').onclick = function(){
-                r.open()
-              }
-              </script>
-            </div>
-          </div>
-        </div>
-        <div class="clear"></div>
-      </div>
-    </div>
-    <div class="footer">
-      <div class="container">
-        <a href="https://www.razorpay.com/" target="_blank" class="nav" >Home</a>
-        <a href="https://www.razorpay.com/features/" target="_blank" class="nav">Features</a>
-        <a href="https://www.razorpay.com/pricing/" target="_blank" class="nav">Pricing</a>
-        <a href="https://docs.razorpay.com/" target="_blank" class="nav">Documentation</a>
-        <a href="https://www.razorpay.com/contact/" target="_blank" class="nav">Contact Us</a>
-      </div>
-    </div>
-  </div>
+<body style="width: 80%; max-width: 800px; margin: 30px auto; font-family: ubuntu, helvetica">
+<script>
+var options;
+<?php
+if ($baseurl !== "https://api.razorpay.com") {
+?>
+var Razorpay = {
+  config: {
+    api: "/"
+  }
+}
+<?php
+}
+?>
+</script>
+<script src="https://checkout.razorpay.com/v1/checkout.js"></script>
+<textarea style="border-radius: 3px; width: 100%; display: block; height: 580px; font-family: mono; font-color: #444; resize: none;">
+{
+  "key": "rzp_test_1DP5mmOlF5G5ag",
+  "amount": 600000,
+  "name": "Innerchef",
+  "description": "JEE Main & Advanced",
+  "image": "http://i.imgur.com/n5tjHFD.png",
+  "method": {
+    "netbanking": true,
+    "card": true
+  },
+  "remember_customer": true,
+  "prefill": {
+    "name": "Harshil \"Mathur",
+    "email": "harshil@razorpay.com",
+    "contact": "+918879524924",
+    "card[number]": "4111 1111 1111 1111",
+    "card[expiry]": "11 / 23"
+  },
+  "theme": {
+    "color": "#1abc9c"
+  },
+  "handler": function(resp){
+    alert(resp.razorpay_payment_id);
+  }
+}
+</textarea>
+<button style="font-size: 16px; font-family: inherit; padding: .5em 1em; color: #444;
+  border: 1px solid #999; background-color: #E6E6E6; text-decoration: none;
+  display: block; margin: 20px auto; border-radius: 2px;"
+  onclick="eval('options = ' + document.querySelector('textarea').value); Razorpay.open(options); return false;">Open</button>
 </body>
 </html>
