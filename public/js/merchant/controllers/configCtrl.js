e92202a335431c2ab294e498de254ec174c2fa28
Nemo <nemo@razorpay.com>
2016-06-30 14:53:39 +0530

    [merchant] Allows easier re-upload in the config/logo

--- public/js/merchant/controllers/configCtrl.js
+++ public/js/merchant/controllers/configCtrl.js
@@ -1,3 +1,4 @@
+"use strict";
 /**
  * Webhooks Ctrl
  */
@@ -14,14 +15,18 @@ app.controller('ConfigCtrl', [
     $scope.config = {};
 
     $scope.showColorPicker = false;
+    $scope.showImageSelector = false;
 
     $scope.onFileSelect = saveFile;
 
     $scope.setConfig = function(config) {
       $scope.config.brand_color = config.brand_color ? config.brand_color : null;
+
       // This always stays as a string, except when we send it back
       $scope.config.transaction_report_email = config.transaction_report_email.join(',');
-    }
+
+      $scope.config.logo_url = config.logo_url;
+    };
 
     $scope.fetchConfig = function() {
 
@@ -36,16 +41,20 @@ app.controller('ConfigCtrl', [
           if ($scope.config.brand_color !== null) {
             $scope.showColorPicker = true;
           }
+
+          if ($scope.config.logo_url === null) {
+            $scope.showImageSelector = true;
+          }
         } else {
           $scope.alerts.resetAlerts();
-          angular.forEach(data.errors, function (value, key) {
+          angular.forEach(data.errors, function (value) {
             $scope.alerts.addAlert('danger', value);
           });
         }
       }).error(function () {
         $scope.alerts.addAlert('danger', null, true);
       });
-    }
+    };
 
     // Fetch the config on load
     $scope.fetchConfig();
@@ -54,7 +63,7 @@ app.controller('ConfigCtrl', [
       var data = {
         brand_color: config.brand_color ? config.brand_color.substr(1).toUpperCase() : null,
         transaction_report_email: config.transaction_report_email ? config.transaction_report_email.split(',') : null
-      }
+      };
       var request = $http({
         "method": 'PUT',
         "url": '/config',
@@ -67,14 +76,14 @@ app.controller('ConfigCtrl', [
           $scope.alerts.addAlert('success', 'Configuration Updated', true);
         } else {
           $scope.alerts.resetAlerts();
-          angular.forEach(data.errors, function (value, key) {
+          angular.forEach(data.errors, function (value) {
             $scope.alerts.addAlert('danger', value);
           });
         }
       }).error(function () {
         $scope.alerts.addAlert('danger', null, true);
       });
-    }
+    };
 
     function saveFile($files, fieldname) {
       var file = $files[0];
@@ -107,13 +116,12 @@ app.controller('ConfigCtrl', [
           }
         }
       });
-      request.success(function (data, status, headers, config) {
+      request.success(function (data) {
         if (data.success) {
           $scope.alerts.addAlert('success', 'File Uploaded Successfully', true);
         } else {
-          console.log(data);
           $scope.alerts.resetAlerts();
-          angular.forEach(data.errors, function (value, key) {
+          angular.forEach(data.errors, function (value) {
             $scope.alerts.addAlert('danger', value);
           });
         }
