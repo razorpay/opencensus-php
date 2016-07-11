9ccfc9f3ca10b1d8fed264015f8630b6dafa487f
Nemo <nemo@razorpay.com>
2016-07-11 17:27:39 +0530

    Fixes #207

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,26 +2,39 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider {
+class AppServiceProvider extends ServiceProvider
+{
+    protected $defer = true;
 
-	/**
-	 * Bootstrap any application services.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		//
-	}
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
 
-	/**
-	 * Register any application services.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		//
-	}
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerValidatorResolver();
+    }
+
+    protected function registerValidatorResolver()
+    {
+        $this->app['validator']->resolver(function($translator, $data, $rules, $messages, $customAttributes)
+        {
+
+            return new Razorpay\Spine\Validation\LaravelValidatorEx(
+                $translator, $data, $rules, $messages, $customAttributes
+            );
+        });
+    }
 
 }
