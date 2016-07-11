643b682d45f03b4239ed4c3d2bbfd7b3d03c0d40
gaurav <gaurav.d@razorpay.com>
2016-07-11 19:29:01 +0530

    changed columns mapping

--- app/Models/Card/IIN/Import/Formatter.php
+++ app/Models/Card/IIN/Import/Formatter.php
@@ -3,6 +3,7 @@
 namespace RZP\Models\Card\IIN\Import;
 
 use RZP\Models\Card\IIN\Entity as IIN;
+use RZP\Models\Card\Network;
 use RZP\Models\Base;
 use RZP\Exception;
 
@@ -84,45 +85,56 @@ class Formatter
      *
      * @return collection of arrays with keys from iin entity.
      */
-    public function formatDataNew($columns, $data)
+    public function formatDataNew($columns, $data, $networkMapping, $FieldToDel)
     {
-        $iins = new Base\PublicCollection;
+        $iins = array();
 
         foreach ($data as $row)
         {
             $input = array();
             $index = 0;
-            foreach ($columns as $column)
-            {
-                switch (strtolower($column))
-                {
-                    case 'iin':
-                        $input[IIN::IIN] = $row[$index];
-                        break;
-                    case 'network':
-                        $input[IIN::NETWORK] = $row[$index];
-                        break;
-                    case 'issuer_name':
-                        $input[IIN::ISSUER_NAME] = $row[$index];
-                        break;
-                    case 'type':
-                        $input[IIN::TYPE] = $row[$index];
-                        break;
-                    case 'category':
-                        $input[IIN::CATEGORY] = $row[$index];
-                        break;
-                    case 'country':
-                        $input[IIN::COUNTRY] = $row[$index];
-                        break;
-                    default:
-                        //ignore extra columns
-                        break;
-                }
-                $index++;
-            }
 
-            $iins[] = $input;
+            $input = array_combine($columns, $row);
+            foreach($FieldToDel as $field){
+                unset($input[$field]);
+            }
+            $input[IIN::NETWORK] = $this->formatNetwork(
+                $input[IIN::NETWORK],
+                $networkMapping
+            );
+
+            $input[IIN::TYPE] = $this->formatType(
+                $input[IIN::TYPE]
+            );
+
+            $iins[$input[IIN::IIN]] = $input;
         }
+
         return $iins;
     }
+
+    private function formatNetwork($value, $networkMapping){
+        if (array_key_exists($value, $networkMapping))
+        {
+            return Network::$fullName[$networkMapping[$value]];
+        }
+
+        return Network::$fullName[$networkMapping['unknown']];
+    }
+
+    private function formatType($value){
+
+        if ($value == 'CREDIT' or $value == 'DEBIT')
+        {
+            $value = strtolower($value);
+        }
+
+        if ($value == 'credit' or $value == 'debit')
+        {
+            return $value;
+        }
+
+        return '';
+    }
+
 }
