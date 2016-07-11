81f9be4b5c9fdb795a5374585180065be2eb7243
Vivek Kumar <viveklakhera@gmail.com>
2016-07-11 11:24:40 +0530

    [AWS] Add trace to logo upload

--- app/Models/Merchant/Logo.php
+++ app/Models/Merchant/Logo.php
@@ -7,6 +7,7 @@ use RZP\Models\Merchant;
 use RZP\Exception;
 use RZP\Error\ErrorCode;
 use Config;
+use Trace;
 use AWS;
 
 class Logo
@@ -203,6 +204,8 @@ class Logo
         // Since all the file names have sizes appended to them.
         $url = '/' . $awsFileName;
 
+        Trace::info(TraceCode::AWS_S3_LOGO_UPLOAD);
+
         foreach ($logoDimensions as $size => $_)
         {
             // Gets the location of the file w.r.t. the size.
@@ -220,7 +223,6 @@ class Logo
                     'ContentType'   => $mimeType,
                     'SourceFile'    => $filePath,
                 ];
-
                 // The method which will upload to s3.
                 $result = $s3->putObject($s3Obj);
             }
@@ -230,9 +232,17 @@ class Logo
                     'Failed to upload file: ' . $awsFileName,
                     ErrorCode::SERVER_ERROR_AWS_FAILURE, null, $e);
             }
+            catch (\Aws\S3\Exception\S3Exception $e)
+            {
+                throw new Exception\ServerErrorException(
+                    'Failed to upload file: ' . $awsFileName,
+                    ErrorCode::SERVER_ERROR_AWS_FAILURE, null, $e);
+            }
 
+            Trace::info(TraceCode::AWS_S3_LOGO_UPLOAD, ['File Uploaded']);
             //$s3Url = $result['ObjectURL'];
         }
+
         return $url;
     }
 }
