ff86ddd41088023d3a5f0fa68cb3ea372b34914a
Vivek Kumar <viveklakhera@gmail.com>
2016-07-14 16:10:25 +0530

    Add redis config

--- config/database.php
+++ config/database.php
@@ -132,9 +132,9 @@ return array(
         'cluster' => false,
 
         'default' => array(
-            'host'     => '127.0.0.1',
-            'port'     => 6379,
-            'database' => 0,
+            'host'     => env('REDIS_HOST'),
+            'port'     => env('REDIS_PORT'),
+            'database' => env('REDIS_DB'),
         ),
 
     ),
