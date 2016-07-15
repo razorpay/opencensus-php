c567add054e11dd0f94f57bc973b131171860bd8
Vivek Kumar <viveklakhera@gmail.com>
2016-07-15 15:58:12 +0530

    Revert "[mailgun] Add temporary test code change for beta"
    This reverts commit 7ac2a2ea843a7c3b3ebc6c2967ef71b6472b9372.

--- app/Models/Payment/Processor/Notify.php
+++ app/Models/Payment/Processor/Notify.php
@@ -634,12 +634,6 @@ class Notify
      */
     protected function isMailEnabled($event, $isMerchant = false)
     {
-        // Hack for beta for now
-        if (env('CONTEXT') === 'beta')
-        {
-            return true;
-        }
-
         // If the merchant has disabled customer emails
         // And this was a customer receipt email
         if (($this->payment->merchant->isReceiptEmailsEnabled() === false) and
