d71be702665a7ce910541d7135b3b18c51faefcf
Sunny Aggrawal <sunnyagg@outlook.com>
2016-07-19 22:30:38 +0530

    [card] add id,entity to card public

--- tests/Functional/Payment/helpers/CardTestData.php
+++ tests/Functional/Payment/helpers/CardTestData.php
@@ -14,6 +14,7 @@ return [
         ],
         'response' => [
             'content' => [
+                'entity'        => 'card',
                 'name'          => 'Harshil',
                 'network'       => 'Visa',
                 'last4'         => '3335',
