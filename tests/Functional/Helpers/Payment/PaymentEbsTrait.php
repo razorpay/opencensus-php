61db40fb5c40ad9c2f2272d2fa326d94e6b020e9
gaurav <gaurav.d@razorpay.com>
2016-07-20 17:29:55 +0530

    added 1st test for ebs

---
+++ tests/Functional/Helpers/Payment/PaymentEbsTrait.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace RZP\Tests\Functional\Helpers\Payment;
+
+use Config;
+use Requests;
+use Symfony\Component\DomCrawler\Crawler;
+use RZP\Tests\Functional\TestCase;
+
+trait PaymentEbsTrait
+{
+    protected function runPaymentCallbackFlowEbs($response, &$callback = null)
+    {
+        $mock = $this->isGatewayMocked();
+
+        list ($url, $method, $content) = $this->getDataForGatewayRequest($response, $callback);
+        if ($mock)
+        {
+            $request = $this->makeFirstGatewayPaymentMockRequest(
+                                                    $url, $method, $content);
+        }
+        else
+        {
+            ;
+        }
+
+        return $this->submitPaymentCallbackRequest($request);
+    }
+}
