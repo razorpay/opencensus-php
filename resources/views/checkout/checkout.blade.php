d7b5f66c2bb9f99283eb381d56d09ff440369df8
Pranav Gupta <pranavguptarulz@gmail.com>
2016-07-21 21:39:02 +0530

    checkout blade template conditions

--- resources/views/checkout/checkout.blade.php
+++ resources/views/checkout/checkout.blade.php
@@ -7,20 +7,17 @@
     <meta http-equiv="pragma" content="no-cache">
     <meta http-equiv="cache-control" content="no-cache">
     <meta name="viewport" content="user-scalable=no,width=device-width,initial-scale=1,maximum-scale=1">
-    <style>@font-face{font-family:'lato';src:url("<?= $font ?>.eot?#iefix") format('embedded-opentype'),url("<?= $font ?>.woff2") format('woff2'),url("<?= $font ?>.woff") format('woff'),url("<?= $font ?>.ttf") format('truetype'),url("<?= $font ?>.svg#lato") format('svg');font-weight:normal;font-style:normal}</style>
-    <link rel="stylesheet" href="<?= $css ?>">
+    @if (isset($font))
+      <style>@font-face{font-family:'lato';src:url("{{ $font }}.eot?#iefix") format('embedded-opentype'),url("{{ $font }}.woff2") format('woff2'),url("{{ $font }}.woff") format('woff'),url("{{ $font }}.ttf") format('truetype'),url("{{ $font }}.svg#lato") format('svg');font-weight:normal;font-style:normal}</style>
+    @endif
+    @if (isset($css))
+      <link rel="stylesheet" href="{{ $css }}">
+    @endif
   </head>
   <body></body>
-  <script>
-    function appendScript(){
-      var script = document.createElement('script');
-      script.src = "<?= $framejs ?>";
-      document.body.appendChild(script);
-    }
-  </script>
-  <script>
-    @if (isset($error))
-      var error = <?= json_encode($error);?>;
+  @if (isset($error))
+    <script>
+      var error = {!! json_encode($error) !!};
 
       function sendMessage(message){
         if(typeof window.CheckoutBridge == 'object'){
@@ -38,14 +35,18 @@
           sendMessage({event: 'hidden'});
         })
       }
-    @else
-      var preferences = <?= json_encode($preferences) ?>;
+    </script>
+  @else
+    <script>
       function appendScript(){
         var script = document.createElement('script');
-        script.src = "<?= $framejs ?>";
+        script.src = "{{ $framejs }}";
         document.body.appendChild(script);
       }
-    @endif
-  </script>
-  <script src="<?= $framejs ?>" crossorigin onerror="appendScript()"></script>
+      @if (isset($preferences))
+        var preferences = {!! json_encode($preferences) !!};
+      @endif
+    </script>
+    <script src="{{ $framejs }}" crossorigin onerror="appendScript()"></script>
+  @endif
 </html>
