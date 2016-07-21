b7e9829cc8d504874ec400e1a0f4ddeb788d38e7
Pranav Gupta <pranavguptarulz@gmail.com>
2016-07-21 22:02:02 +0530

    removed redundant condition

--- resources/views/checkout/checkout.blade.php
+++ resources/views/checkout/checkout.blade.php
@@ -7,12 +7,6 @@
     <meta http-equiv="pragma" content="no-cache">
     <meta http-equiv="cache-control" content="no-cache">
     <meta name="viewport" content="user-scalable=no,width=device-width,initial-scale=1,maximum-scale=1">
-    @if (isset($font))
-      <style>@font-face{font-family:'lato';src:url("{{ $font }}.eot?#iefix") format('embedded-opentype'),url("{{ $font }}.woff2") format('woff2'),url("{{ $font }}.woff") format('woff'),url("{{ $font }}.ttf") format('truetype'),url("{{ $font }}.svg#lato") format('svg');font-weight:normal;font-style:normal}</style>
-    @endif
-    @if (isset($css))
-      <link rel="stylesheet" href="{{ $css }}">
-    @endif
   </head>
   <body></body>
   @if (isset($error))
@@ -37,6 +31,8 @@
       }
     </script>
   @else
+    <style>@font-face{font-family:'lato';src:url("{{ $font }}.eot?#iefix") format('embedded-opentype'),url("{{ $font }}.woff2") format('woff2'),url("{{ $font }}.woff") format('woff'),url("{{ $font }}.ttf") format('truetype'),url("{{ $font }}.svg#lato") format('svg');font-weight:normal;font-style:normal}</style>
+    <link rel="stylesheet" href="{{ $css }}">
     <script>
       function appendScript(element){
         var script = document.createElement('script');
