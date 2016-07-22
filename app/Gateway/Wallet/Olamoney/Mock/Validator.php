50f51a24d6407172eb92d1232aeb5da5b124a794
Priyanshu Chhazed <priyanshu.chhazed@razorpay.com>
2016-07-22 15:19:15 +0530

    Renamed validator rules to authorizeRules

--- app/Gateway/Wallet/Olamoney/Mock/Validator.php
+++ app/Gateway/Wallet/Olamoney/Mock/Validator.php
@@ -6,7 +6,7 @@ use RZP\Models\Base;
 
 class Validator extends Base\Validator
 {
-    protected static $debitRules   = array(
+    protected static $authorizeRules   = array(
         'paymentId' => 'required|string',
         'bill'      => 'required|regex:"^[a-zA-Z0-9+/=]"',
         'phone'     => 'required|integer'
