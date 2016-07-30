a6a70aea5827adafec15490b8de9410b4878d794
Vivek Kumar <viveklakhera@gmail.com>
2016-07-30 15:24:35 +0530

    [error_code] Add test coverage for customer error codes

--- app/Error/CustomerErrorDescription.php
+++ app/Error/CustomerErrorDescription.php
@@ -4,5 +4,5 @@ namespace RZP\Error;
 
 class CustomerErrorDescription
 {
-    const BAD_REQUEST_PAYMENT_DECLINED_3DSECURE_AUTH_FAILED                     = 'Payment processing failed due to 3dsecure failure';
+    const BAD_REQUEST_PAYMENT_DECLINED_3DSECURE_AUTH_FAILED = 'Payment processing failed due to 3dsecure failure';
 }
