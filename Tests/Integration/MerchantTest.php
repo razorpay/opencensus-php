134499f1ee813f102177373dfb055ee4be2edd7b
Sumeeti Lalwani <sumeeti@razorpay.com>
2016-07-30 16:41:02 +0530

    [tests] Keys roll confirm modal

--- Tests/Integration/MerchantTest.php
+++ Tests/Integration/MerchantTest.php
@@ -138,6 +138,7 @@ class MerchantTest extends TestCase
     /**
      * Tests payments panel display
      */
+
     public function testPaymentsList()
     {
         $this->url('#/access/signin');
@@ -196,7 +197,7 @@ class MerchantTest extends TestCase
         $this->clickByClassName('confirm-ok');
         $this->waitAndClickByClassName('roll_key');
         $this->clickByClassName('modal-ok');
-        $this->waitUntilDisplayedByClassName('modal-content');
+        $this->waitUntilDisplayedByClassName('new-key-modal');
         $this->clickByClassName('modal-ok');
         $this->waitUntilDisplayedByClassName('confirm-modal');
         $this->clickByClassName('confirm-ok');
