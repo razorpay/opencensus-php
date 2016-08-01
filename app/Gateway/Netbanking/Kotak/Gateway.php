a633783a7a9124dc5d8648e1ac31ec4524015173
Giridaran M <giri@razorpay.com>
2016-08-01 12:40:40 +0530

    [nb_kotak] Add verifyPayment.

--- app/Gateway/Netbanking/Kotak/Gateway.php
+++ app/Gateway/Netbanking/Kotak/Gateway.php
@@ -122,6 +122,41 @@ class Gateway extends Base\Gateway
         return $this->runPaymentVerifyFlow($verify);
     }
 
+    public function verifyPayment($verify)
+    {
+        $payment = $verify->payment;
+        $content = $verify->verifyResponseContent;
+
+        $status = VerifyResult::STATUS_MATCH;
+
+        $verify->apiSuccess = true;
+        $verify->gatewaySuccess = false;
+
+        if ($content['AuthorizationStatus'] === 'Y')
+        {
+            $verify->gatewaySuccess = true;
+        }
+
+        $input = $verify->input;
+
+        // If payment status is either failed or created,
+        // this is an api failure
+        if (($input['payment']['status'] === 'failed') or
+            ($input['payment']['status'] === 'created'))
+        {
+            $verify->apiSuccess = false;
+        }
+
+        // If both don't match we have a status mis match
+        if ($verify->gatewaySuccess !== $verify->apiSuccess)
+        {
+            $status = VerifyResult::STATUS_MISMATCH;
+        }
+
+        $verify->match = ($status === VerifyResult::STATUS_MATCH) ? true : false;
+
+        return $status;
+    }
     protected function validateCallbackChecksum($content)
     {
         $expectedHash = $content['Checksum'];
@@ -193,6 +228,10 @@ class Gateway extends Base\Gateway
         $response = $this->sendGatewayRequest($request);
         $content = $response->body;
         $content = $this->getDataFromResponse($content);
+
+        $this->trace->info(
+            TraceCode::GATEWAY_PAYMENT_VERIFY,
+            ['responseContent' => $content]);
 
         $verify->verifyResponse = $response;
         $verify->verifyResponseBody = $response->body;
