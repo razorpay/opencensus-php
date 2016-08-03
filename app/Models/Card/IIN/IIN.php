fdd0e82760ccd18c8c28778ddb353c054cd11d42
Shashank Kumar <shashankkumar.me@gmail.com>
2016-08-03 18:07:15 +0530

    [cards] Adds NetworkName class to resolve full network names quickly.

--- app/Models/Card/IIN/IIN.php
+++ app/Models/Card/IIN/IIN.php
@@ -3,6 +3,8 @@
 namespace RZP\Models\Card\IIN;
 
 use RZP\Models\Bank;
+use RZP\Exception;
+use RZP\Error\ErrorCode;
 
 class IIN
 {
@@ -75,6 +77,15 @@ class IIN
         return true;
     }
 
+    public static function validateEmiAvailableForCard($iin, $cardNumber)
+    {
+        if (self::isEmiAvailableForCard($iin, $cardNumber) === false)
+        {
+            throw new Exception\BadRequestException(
+                ErrorCode::BAD_REQUEST_PAYMENT_EMI_NOT_AVAILABLE_ON_CARD);
+        }
+    }
+
     /**
      * This checks for the special case of Axis Bank which works on first 8 digits
      * of the card instea of the first 6.
@@ -86,5 +97,4 @@ class IIN
 
         return $emi;
     }
-
 }
