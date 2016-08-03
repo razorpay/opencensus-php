1c5297a6bca99ca2d173c9635838da0ab1191091
Sunny Aggrawal <sunnyagg@outlook.com>
2016-08-03 14:40:55 +0530

    [emi] updates test case for emi

--- tests/Functional/EMI/EMITestData.php
+++ tests/Functional/EMI/EMITestData.php
@@ -13,6 +13,7 @@ return [
                 'duration' => 3,
                 'rate' => 1045,
                 'methods' => 'card',
+                'min_amount' => 400000
             ],
             'method' => 'POST',
             'url' => '/emi',
@@ -23,6 +24,7 @@ return [
                 'duration' => 3,
                 'rate' => 1045,
                 'methods' => 'card',
+                'min_amount' => 400000
             ],
         ],
     ],
