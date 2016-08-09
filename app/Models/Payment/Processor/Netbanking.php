d98b3f8b63358f68531a3f62945a017848c1cd9f
Prashanth Y.V <prashanth.yv@razorpay.com>
2016-08-09 11:24:28 +0530

    [ebs] minor improvements

--- app/Models/Payment/Processor/Netbanking.php
+++ app/Models/Payment/Processor/Netbanking.php
@@ -271,8 +271,7 @@ class Netbanking
         // Merge paytm and billdesk supported banks and remove
         // duplicate values
         //
-        return array_unique(array_merge(self::$paytm, self::$billdesk,[IFSC::KKBK]));
-//        return array_unique(array_merge(self::$paytm, self::$billdesk, self::$sbiepay));
+        return array_unique(array_merge(self::$paytm, self::$billdesk, [IFSC::KKBK]));
     }
 
     public static function getDisabledBanks($banks)
