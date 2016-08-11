2b1296973c09f7c13ed2d930b413de8bf1a5f65c
Venkatesan <venkat.v@razorpay.com>
2016-08-11 16:24:47 +0530

    [terminal performance selection] - add verbose for terminal selection

--- app/Models/Terminal/Selector.php
+++ app/Models/Terminal/Selector.php
@@ -182,7 +182,7 @@ class Selector
     {
         $options = new Terminal\Options();
 
-        $terminalsSelected = $this->select($options);
+        $terminalsSelected = $this->select($options, true);
 
         if ($options->getMultiple() === false)
         {
