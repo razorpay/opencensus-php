ec7c9095b19124c54d494260e6cc75faefbac4d7
gaurav <gaurav.d@razorpay.com>
2016-08-17 19:20:26 +0530

    added sorter to give pref to direc terminal

---
+++ app/Models/Terminal/Sorters/ExclusivitySorter.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace RZP\Models\Terminal\Sorters;
+
+use RZP\Models\Terminal;
+
+class ExclusivitySorter extends Terminal\Sorter
+{
+    protected $properties = [
+        'shared',
+    ];
+
+    /**
+     * Direct terminals should be placed above the shared terminals.
+     * Place the direct terminals above shared terminals.
+     *
+     * @param $terminals
+     * @param array $input
+     * @return array
+     */
+    public function sharedSorter($terminals, array $input)
+    {
+        $specificCategoryTerminals = [];
+        $genericCategoryTerminals = [];
+
+        // As the terminals are from the priority list
+        // append to the terminal
+        foreach ($terminals as $terminal)
+        {
+            if ($terminal->isShared() === true)
+            {
+                $specificCategoryTerminals[] = $terminal;
+            }
+            else
+            {
+                $genericCategoryTerminals[] = $terminal;
+            }
+        }
+
+        $sortedTerminals = array_merge($specificCategoryTerminals, $genericCategoryTerminals);
+
+        return $sortedTerminals;
+    }
+}
