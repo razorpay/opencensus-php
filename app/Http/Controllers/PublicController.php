b655b6a07b83e92762b7683b43abd611eb02c237
Ankit <ankitstarski@gmail.com>
2016-08-17 19:45:43 +0530

    Account (#880)
    * account route stub
    
    * account rooute stub
    
    * add static view for cards
    
    * add static view for cards
    
    * add deletion overlay
    
    * add deletion overlay
    
    * add payments table
    
    * add payments table
    
    * add card and transaction to templates
    
    * add card and transaction to templates
    
    * add login screen
    
    * add login screen
    
    * login functionality
    
    * login functionality
    
    * OTP resend, logout and card deletion
    
    * account: sanitize code
    
    * [account] Minor changes in discussion with @ankitstarski

---
+++ app/Http/Controllers/PublicController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace RZP\Http\Controllers;
+
+use View;
+
+class PublicController extends Controller
+{
+    public function getAccount()
+    {
+        $data = [
+            'static'    => 'https://cdn.razorpay.com/account',
+            'checkout'  => 'https://checkout.razorpay.com',
+            'api'       => 'https://api.razorpay.com/',
+        ];
+
+        return View::make('public.account', $data);
+    }
+}
