8c498a802a7abd00f990a47b2251702c5ce63733
Nemo <nemo@razorpay.com>
2016-08-22 18:07:35 +0530

    [report] Increases timeout for request to 120 seconds

--- app/RZP/Transaction.php
+++ app/RZP/Transaction.php
@@ -37,7 +37,7 @@ class Transaction extends Entity
         // This is an instance of App\RZP\Request now
         // which includes the setOption method
         $request = new Request();
-        $request->setOption('timeout', 60);
+        $request->setOption('timeout', 120);
 
         $response = $request->request($method, $relativeUrl, $data);
 
