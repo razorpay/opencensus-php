886cb789d8a4bdca45bff96947d2aca024ebc282
Shashank Kumar <shashankkumar.me@gmail.com>
2016-08-24 00:20:57 +0530

    Adds tracing for billdesk response when failing.

--- app/Gateway/Billdesk/Gateway.php
+++ app/Gateway/Billdesk/Gateway.php
@@ -388,6 +388,8 @@ class Gateway extends Base\Gateway
 
         if ($formCrawler->count() === 0)
         {
+            $this->trace->info(TraceCode::GATEWAY_PAYMENT_ERROR, ['response' => $response->body]);
+
             throw new Exception\GatewayTimeoutException('Gateway Timed Out', null, true);
         }
 
