a03d58be298e3190c82e357077e29ee4414b5ab1
gaurav <gaurav.d@razorpay.com>
2016-08-30 20:54:19 +0530

    read correct config and changed variable nam e

--- app/Gateway/Ebs/Gateway.php
+++ app/Gateway/Ebs/Gateway.php
@@ -225,7 +225,7 @@ class Gateway extends Base\Gateway
     {
         $request['options']['follow_redirects'] = false;
 
-        $request['headers']['Referer'] = $this->config['url'];
+        $request['headers']['Referer'] = $this->app['config']->get('app.url');
     }
 
     protected function sendFirstGatewayRequestForEbsAuthorize($request)
@@ -249,7 +249,7 @@ class Gateway extends Base\Gateway
 
         try
         {
-            $failureRequestNumber = 'first';
+            $RequestNumber = 'first';
 
             // This is the first redirect (302). We receive headers and cookies in this response
             // which needs to be sent to the second redirect request.
@@ -257,14 +257,14 @@ class Gateway extends Base\Gateway
 
             $secondRedirectRequest = $this->getRequestFromResponse302($response302);
 
-            $failureRequestNumber = 'second';
+            $RequestNumber = 'second';
 
             // This is the second redirect (form post). The response of this is passed on to the third redirect request.
             $secondRedirectResponse = $this->sendSecondGatewayRequestForEbsAuthorize($secondRedirectRequest);
 
             $lastRedirectRequest = $this->getRequestFromFormPostResponse($secondRedirectRequest, $secondRedirectResponse);
 
-            $failureRequestNumber = 'third';
+            $RequestNumber = 'third';
 
             // Makes the last redirect request before the request to bank's ACS url is made by the checkout.
             $lastRedirectResponse = $this->sendThirdGatewayRequestForEbsAuthorize($lastRedirectRequest);
@@ -277,7 +277,7 @@ class Gateway extends Base\Gateway
             $this->trace->warning(
                 TraceCode::GATEWAY_REQUEST_TIMEOUT,
                 ['payment_id' => $input['payment'][Payment\Entity::ID],
-                'message'    => 'Payment Authorization failed after '.$failureRequestNumber.' Authorization Request']);
+                'message'    => 'Payment Authorization failed after '.$RequestNumber.' Authorization Request']);
 
             throw $e;
         }
