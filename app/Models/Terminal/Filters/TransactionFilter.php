6bf18e49b047a5dbe4cd49247dc3c200b97874dc
gaurav <gaurav.d@razorpay.com>
2016-08-31 16:37:02 +0530

    convert wallet name to lower

--- app/Models/Terminal/Filters/TransactionFilter.php
+++ app/Models/Terminal/Filters/TransactionFilter.php
@@ -43,7 +43,7 @@ class TransactionFilter extends Terminal\Filter
 
             // Pick the right terminal only
             case Method::WALLET:
-                $wallet = $input['payment']->getWallet();
+                $wallet = strtolower($input['payment']->getWallet());
 
                 $gateway = Gateway::getGatewayForWallet($wallet);
 
