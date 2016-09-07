2e042e91a4cf3c0176c53d56e33b8203c736dd53
Giridaran <giri@razorpay.com>
2016-09-07 22:42:30 +0530

    [terminals] Removed status.

--- app/Models/Terminal/Validator.php
+++ app/Models/Terminal/Validator.php
@@ -28,7 +28,6 @@ class Validator extends Base\Validator
         Entity::EMI_DURATION                => 'required_only_if:emi,1|integer|in:3,6,9,12,18,24',
         Entity::SHARED                      => 'sometimes|boolean',
         Entity::GATEWAY_ACQUIRER            => 'sometimes|string',
-        Entity::STATUS                      => 'sometimes|enum',
         Entity::TERMINAL_CATEGORY           => 'sometimes',
     );
 
