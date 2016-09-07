8b441d30f41371c57c5f7d397164689a3a9d7a56
Shashank Kumar <shashankkumar.me@gmail.com>
2016-09-08 01:15:27 +0530

    [payments] Fixes space indentation in CancelTest.

--- tests/Functional/Payment/PaymentCancelTest.php
+++ tests/Functional/Payment/PaymentCancelTest.php
@@ -23,21 +23,21 @@ class PaymentCancelTest extends TestCase
 
     public function testCancelPayment()
     {
-    	$data = [
-	        'response' => [
-	            'content' => [
-	                'error' => [
-	                    'code' => PublicErrorCode::BAD_REQUEST_ERROR,
-	                ],
+        $data = [
+            'response' => [
+                'content' => [
+                    'error' => [
+                        'code' => PublicErrorCode::BAD_REQUEST_ERROR,
+                    ],
                     'http_status_code' => 400,
-	            ],
-	            'status_code' => 200,
-	        ],
-	        'exception' => [
-	            'class' => 'RZP\Exception\BadRequestException',
-	            'internal_error_code' => ErrorCode::BAD_REQUEST_PAYMENT_CANCELLED_BY_USER
-	        ],
-	    ];
+                ],
+                'status_code' => 200,
+            ],
+            'exception' => [
+                'class' => 'RZP\Exception\BadRequestException',
+                'internal_error_code' => ErrorCode::BAD_REQUEST_PAYMENT_CANCELLED_BY_USER
+            ],
+        ];
 
         $payment = $this->fixtures->create(
             'payment',
@@ -51,27 +51,27 @@ class PaymentCancelTest extends TestCase
 
     public function testCancelPaymentAfterRecentlyProcessed()
     {
-    	$content = $this->doAuthPayment();
-    	$pid = $content['razorpay_payment_id'];
+        $content = $this->doAuthPayment();
+        $pid = $content['razorpay_payment_id'];
 
-    	$content2 = $this->cancelPayment($pid);
+        $content2 = $this->cancelPayment($pid);
         unset($content2['http_status_code']);
 
-    	$this->assertEquals($content, $content2);
+        $this->assertEquals($content, $content2);
     }
 
     public function testCancelPaymentAfterAutoCaptureAndRecentlyProcessed()
     {
-    	$order = $this->createOrder(['payment_capture' => '1']);
+        $order = $this->createOrder(['payment_capture' => '1']);
 
-    	$payment = $this->getDefaultPaymentArray();
-    	$payment['order_id'] = $order['id'];
-    	$content = $this->doAuthPayment($payment);
+        $payment = $this->getDefaultPaymentArray();
+        $payment['order_id'] = $order['id'];
+        $content = $this->doAuthPayment($payment);
 
-    	$pid = $content['razorpay_payment_id'];
+        $pid = $content['razorpay_payment_id'];
 
-    	$content2 = $this->cancelPayment($pid);
+        $content2 = $this->cancelPayment($pid);
         unset($content2['http_status_code']);
-    	$this->assertEquals($content, $content2);
+        $this->assertEquals($content, $content2);
     }
 }
