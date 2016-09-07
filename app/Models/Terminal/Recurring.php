7556d54845b634e6b7e4e08556e9dcd9083332b8
Sunny Aggrawal <sunnyagg@outlook.com>
2016-09-07 19:12:59 +0530

    [terminal] adds comments for terminal recurring values

--- app/Models/Terminal/Recurring.php
+++ app/Models/Terminal/Recurring.php
@@ -4,7 +4,12 @@ namespace RZP\Models\Terminal;
 
 class Recurring
 {
+    // Terminal to be used for non recurring payments
     const NON_RECURRING     = 0;
+
+    // Recurring terminal for first transaction
     const RECURRING_3DS     = 1;
+
+    // Recurring terminal for non 3ds transaction after first succesfull payment
     const RECURRING_N3DS    = 2;
 }
