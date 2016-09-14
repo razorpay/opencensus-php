5b382ad3c3fb61676734e6062396660219f5885c
Venkatesan <venkat.v@razorpay.com>
2016-09-14 22:50:17 +0530

    remove unnecessary code

--- app/Models/Payment/TerminalAnalytics/Entity.php
+++ app/Models/Payment/TerminalAnalytics/Entity.php
@@ -47,21 +47,4 @@ class Entity extends Base\PublicEntity
     protected $casts = array(
         self::TERMINAL_RESPONSE_TIME => 'int',
     );
-
-    public static function microtime_diff($start, $end = null)
-    {
-        if (!$end)
-        {
-            $end = microtime();
-        }
-        list($start_usec, $start_sec) = explode(" ", $start);
-
-        list($end_usec, $end_sec) = explode(" ", $end);
-
-        $diff_sec = intval($end_sec) - intval($start_sec);
-
-        $diff_usec = floatval($end_usec) - floatval($start_usec);
-
-        return floatval($diff_sec) + $diff_usec;
-    }
 }
