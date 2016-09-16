27f73ec0e8f8c1d11c3e0fb3581445b32ee48428
Priyanshu Chhazed <priyanshu.chhazed@razorpay.com>
2016-09-16 11:36:56 +0530

    Added value for international in payment fixture

--- tests/Functional/Fixtures/Entity/Payment.php
+++ tests/Functional/Fixtures/Entity/Payment.php
@@ -149,6 +149,7 @@ class Payment extends Base
             'status' => 'authorized',
             'terminal_id' => '1n25f6uN5S1Z5a',
             'card_id' => $card['id'],
+            'international' => false,
         );
 
         $attributes = array_merge($defaultValues, $attributes);
