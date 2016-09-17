dbd528c86155e220a98a392f2dda1a2b0e98b09e
Vivek Kumar <73sl4@users.noreply.github.com>
2016-09-17 19:05:46 +0530

    Revert "[trace] Remove auth data from referer" (#1090)

--- app/Trace/WebProcessor.php
+++ app/Trace/WebProcessor.php
@@ -71,8 +71,6 @@ class WebProcessor extends \Monolog\Processor\WebProcessor
 
         $this->unsetUrlForSensitiveUrls($serverData);
 
-        $this->scrapeSensitiveDataFromUrls($serverData);
-
         return $serverData;
     }
 
@@ -83,19 +81,6 @@ class WebProcessor extends \Monolog\Processor\WebProcessor
         if (in_array($serverData['uri'], $sensitiveUrls))
         {
             unset($serverData['url']);
-        }
-    }
-
-    protected function scrapeSensitiveDataFromUrls(& $serverData)
-    {
-        $sensitiveKeys = ['referer'];
-
-        foreach ($sensitiveKeys as $key)
-        {
-            if (empty($serverData[$key]) === false)
-            {
-                $serverData[$key] = http_build_url($serverData[$key], [], HTTP_URL_STRIP_PASS);
-            }
         }
     }
 }
