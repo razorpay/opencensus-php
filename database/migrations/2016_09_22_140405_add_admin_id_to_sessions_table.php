65b4f19cb4b2be4a6fd244a3ab69e23a7697cb73
Sumeeti Lalwani <sumeeti@razorpay.com>
2016-09-22 22:02:56 +0530

    class name error

--- database/migrations/2016_09_22_140405_add_admin_id_to_sessions_table.php
+++ database/migrations/2016_09_22_140405_add_admin_id_to_sessions_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddAdminToSessionsTable extends Migration
+class AddAdminIdToSessionsTable extends Migration
 {
     /**
      * Run the migrations.
