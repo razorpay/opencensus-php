e3a4fb303c11be361ae340f6d12f3bfbf4ae32f4
Shashank Kumar <shashankkumar.me@gmail.com>
2016-09-24 21:18:32 +0530

    Removes checking condition for existence of assertTrue.

--- app/lib/utility.php
+++ app/lib/utility.php
@@ -190,3 +190,25 @@ if(! function_exists('assertTrue'))
         }
     }
 }
+
+
+/**
+ * We do not check for whether this function is defined already
+ * If it is defined already by some other library (like phpunit)
+ * then we want this definition to be the correct one.
+ */
+function assertTrue($assertion, $message = null)
+{
+    if (version_compare(phpversion(), '7.0.0', '<'))
+    {
+        $message = $message ?: '';
+
+        assert($assertion, $message);
+    }
+    else
+    {
+        $e = new RZP\Exception\AssertionException($message);
+
+        assert($assertion, $e);
+    }
+}
