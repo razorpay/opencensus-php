dbd87243e9e5539ecaffd062a328c4bca96370a3
Sreeram Boyapati <sreeram.boyapati2011@gmail.com>
2016-09-27 15:07:11 +0530

    [AirtelMoney] Cast the arg to bool in getAirtelMoneyAttribute (#1182)

--- app/Models/Merchant/Methods/Entity.php
+++ app/Models/Merchant/Methods/Entity.php
@@ -99,11 +99,6 @@ class Entity extends Base\PublicEntity
         self::AIRTELMONEY,
     );
 
-    // Casts the attributes to native types
-    protected $casts = [
-        self::AIRTELMONEY => 'boolean',
-    ];
-
     public function setMethods(array $input = array())
     {
         $this->edit($input, 'setMethods');
@@ -403,7 +398,7 @@ class Entity extends Base\PublicEntity
 
     protected function getAirtelmoneyAttribute()
     {
-        return $this->attributes[self::AIRTELMONEY];
+        return (bool) $this->attributes[self::AIRTELMONEY];
     }
 
     protected function getPayzappAttribute()
