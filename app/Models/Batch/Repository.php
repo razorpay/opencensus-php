4911a439aca24d3474c9155a3d4cfe7a941de046
Shashank Kumar <shashankkumar.me@gmail.com>
2016-09-28 03:24:13 +0530

    [batch] Removes unused query.

--- app/Models/Batch/Repository.php
+++ app/Models/Batch/Repository.php
@@ -14,28 +14,14 @@ class Repository extends Base\Repository
 
     protected $entity = 'batch';
 
-    protected $proxyFetchParamRules = [
-        Entity::MERCHANT_ID     => 'sometimes|alpha_num'
-    ];
-
     public function findUnprocessedEntries($limit = 10)
     {
         $status = array(Status::CREATED, Status::PROCESSING);
+
         return $this->newQuery()
                     ->whereIn(Batch::STATUS, $status)
                     ->oldest()
                     ->limit($limit)
-                    ->get();
-    }
-
-    public function getBatches($merchantId, $type, $skip, $take = 10)
-    {
-        return $this->newQuery()
-                    ->latest()
-                    ->where(Entity::MERCHANT_ID, '=', $merchantId)
-                    ->where(Entity::TYPE, '=', $type)
-                    ->skip($skip)
-                    ->take($take)
                     ->get();
     }
 }
