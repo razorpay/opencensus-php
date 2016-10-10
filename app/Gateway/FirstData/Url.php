9c1e9276ef62f064c0661b50da09e96e13fdd472
Harman Singh <harman28@gmail.com>
2016-10-10 13:54:50 +0530

    Corrected live domain

--- app/Gateway/FirstData/Url.php
+++ app/Gateway/FirstData/Url.php
@@ -5,7 +5,7 @@ namespace RZP\Gateway\FirstData;
 class Url
 {
     const TEST_DOMAIN   = 'https://test.ipg-online.com';
-    const LIVE_DOMAIN   = 'https://ipg-online.com';
+    const LIVE_DOMAIN   = 'https://www4.ipg-online.com';
 
     const PROCESSING    = '/connect/gateway/processing';
     const SERVICES      = '/ipgapi/services';
