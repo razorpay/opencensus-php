7c88c7be72d56e8d5fa9bc93688c0639863037a2
Vivek Kumar <viveklakhera@gmail.com>
2016-10-17 12:24:21 +0530

    [cybersource] Fix usage of route class

--- app/Gateway/Cybersource/Mock/Server.php
+++ app/Gateway/Cybersource/Mock/Server.php
@@ -267,7 +267,7 @@ class Server extends Base\Mock\Server
                 $response['reasonCode'] = Cybersource\Result::ENROLLED;
 
                 $params = array('gateway' => 'cybersource');
-                $response['payerAuthEnrollReply']['acsURL'] = Http\Route::getUrl('mock_cybersource_acs', $params);
+                $response['payerAuthEnrollReply']['acsURL'] = $this->route->getUrl('mock_cybersource_acs', $params);
                 $response['payerAuthEnrollReply']['paReq'] = 'eNpVUttygjAQfc9XMP0AkiAw';
                 $response['payerAuthEnrollReply']['xid'] = 'cGdKQXF5STA1TFl3OUtueHJnWDA';
                 $response['payerAuthEnrollReply']['veresEnrolled'] = 'Y';
