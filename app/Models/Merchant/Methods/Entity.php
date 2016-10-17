fed2f97c073517f65631db4ab57128213482e653
Sumeeti Lalwani <sumeeti@razorpay.com>
2016-10-17 12:41:14 +0530

    Adding setCard definition back

--- app/Models/Merchant/Methods/Entity.php
+++ app/Models/Merchant/Methods/Entity.php
@@ -366,6 +366,11 @@ class Entity extends Base\PublicEntity
         $this->setAttribute(self::FREECHARGE, $value);
     }
 
+    public function setCard($card)
+    {
+        $this->setAttribute(self::CARD, $card);
+    }
+
     public function setCreditCard($card)
     {
         $this->setAttribute(self::CREDIT_CARD, $card);
