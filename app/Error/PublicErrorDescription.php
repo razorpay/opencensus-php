b6ad23a98e93a60b0a575dac63186ac697aa578d
Vivek <73sl4@users.noreply.github.com>
2016-10-18 06:49:55 +0530

    [orders] Add validation for minimum amount (#1355)
    * [orders] Add validation for minimum amount
    
    * [order] Fix validation and amount data
    
    * [orders] Fix validator and add native validation with lang
    
    * Fix description for BAD_REQUEST_PAYMENT_AMOUNT_LESS_THAN_MIN_AMOUNT

--- app/Error/PublicErrorDescription.php
+++ app/Error/PublicErrorDescription.php
@@ -33,7 +33,7 @@ class PublicErrorDescription
     const BAD_REQUEST_PAYMENT_FULLY_REFUNDED                                    = 'The payment has been fully refunded already';
     const BAD_REQUEST_PAYMENT_REFUND_AMOUNT_GREATER_THAN_CAPTURED               = 'The refund amount provided is greater than amount captured';
     const BAD_REQUEST_PAYMENT_REFUND_AMOUNT_GREATER_THAN_UNREFUNDED             = 'The refund amount provided is greater than the unrefunded amount';
-    const BAD_REQUEST_PAYMENT_AMOUNT_LESS_THAN_MIN_AMOUNT                       = 'Minimum transaction amount allowed is Rs 1';
+    const BAD_REQUEST_PAYMENT_AMOUNT_LESS_THAN_MIN_AMOUNT                       = 'Minimum transaction amount allowed is Re. 1';
     const BAD_REQUEST_PAYMENT_AMOUNT_LESS_THAN_10_MIN_AMOUNT                    = 'Minimum transaction amount allowed is Rs 10';
     const BAD_REQUEST_PAYMENT_AMOUNT_TOO_HIGH                                   = 'The amount may not be greater than 50000000.';
     const BAD_REQUEST_PAYMENT_ATOM_NET_BANKING_MIN_AMOUNT_FIFTY                 = 'Minimum amount allowed for net banking transaction for the merchant is INR 50';
--- app/Models/Order/Validator.php
+++ app/Models/Order/Validator.php
@@ -11,7 +11,7 @@ use RZP\Error\ErrorCode;
 class Validator extends Base\Validator
 {
     protected static $createRules = array(
-        Entity::AMOUNT          =>  'required|integer|max:50000000',
+        Entity::AMOUNT          =>  'required|integer|min:100|max:50000000',
         Entity::CURRENCY        =>  'required|size:3|in:INR',
         Entity::RECEIPT         =>  'required|string|max:40',
         Entity::PAYMENT_CAPTURE =>  'sometimes|boolean',
