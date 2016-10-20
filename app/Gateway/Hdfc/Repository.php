8ea70e80eaf8466cb345027393c0a8138f096eb8
Harman Singh <harman28@gmail.com>
2016-10-20 14:25:46 +0530

    Reverting to findCapturedPaymentByIdOrFail (#1379)

--- app/Gateway/Hdfc/Repository.php
+++ app/Gateway/Hdfc/Repository.php
@@ -26,6 +26,15 @@ class Repository extends Base\Repository
                     ->first();
     }
 
+    public function findCapturedPaymentByIdOrFail($paymentId)
+    {
+        $repo = $this->repo;
+
+        return $repo::where(Entity::PAYMENT_ID, '=', $paymentId)
+                    ->where(Entity::ACTION, '=', Action::CAPTURE)
+                    ->firstOrFail();
+    }
+
     public function persistAfterEnroll($request, $response)
     {
         $result = $response['enroll_result'];
