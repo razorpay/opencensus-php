881bce078f81a0745daecf3701f480fe132b90ed
Prashanth Y.V <prashanth.yv@razorpay.com>
2016-10-25 02:11:08 +0530

    [recon] saves the txn association with payment after creating a new txn

--- app/Reconciliator/Base/SubReconciliator/PaymentReconciliate.php
+++ app/Reconciliator/Base/SubReconciliator/PaymentReconciliate.php
@@ -821,6 +821,8 @@ class PaymentReconciliate extends Foundation\SubReconciliate
         $txn = (new Transaction\Core)->createFromPaymentAuthorized($this->payment);
 
         $this->repo->saveOrFail($txn);
+        // This is required to save the association of the transaction with the payment.
+        $this->repo->saveOrFail($this->payment);
     }
 
     protected function recordGatewayFee($reconGatewayFee, $currentGatewayFee)
