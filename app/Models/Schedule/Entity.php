99091750e16445a1fc346283f698cda3d3b8161c
Harman Singh <harman28@gmail.com>
2016-10-26 18:47:55 +0530

    Changed Default value for next_run to an IST hour mark (#1429)
    * Changed default value for next_run; further extended settlement time
    
    * Changed default value for next_run to an IST hour mark
    
    * Saved the updated_value for next_run
    
    * Added tracing
    
    * More tracing

--- app/Models/Schedule/Entity.php
+++ app/Models/Schedule/Entity.php
@@ -64,6 +64,8 @@ class Entity extends Base\PublicEntity
         $nextRun = Library::getNextApplicableTime($lastRun, $this);
 
         $this->setNextRun($nextRun);
+
+        $this->saveOrFail();
     }
 
     // ----------------------- Associations ----------------------------------------
@@ -96,7 +98,11 @@ class Entity extends Base\PublicEntity
     {
         if (isset($input[self::NEXT_RUN]) === false)
         {
-            $input[self::NEXT_RUN] = 0;
+            $format = 'Y-m-d H:i:s';
+
+            $istStart = '2000-01-01 00:00:00';
+
+            $input[self::NEXT_RUN] = Carbon::createFromFormat($format, $istStart, 'Asia/Kolkata')->timestamp;
         }
     }
 
--- app/Models/Settlement/Processor.php
+++ app/Models/Settlement/Processor.php
@@ -86,11 +86,11 @@ class Processor extends Base\Core
         // be uploaded anytime
 
         $sevenAm = Carbon::today('Asia/Kolkata')->hour(7)->timestamp;
-        $sixPm = Carbon::today('Asia/Kolkata')->hour(18)->timestamp;
+        $sevenPm = Carbon::today('Asia/Kolkata')->hour(19)->timestamp;
 
         if (($this->mode === Mode::LIVE) and
             ($this->setlTime >= $sevenAm) and
-            ($this->setlTime <= $sixPm))
+            ($this->setlTime <= $sevenPm))
         {
             return true;
         }
@@ -149,11 +149,13 @@ class Processor extends Base\Core
             list($txns, $schedules) = $this->repo->transaction->fetchUnsettledTxnsAndSchedules($this->setlTime);
 
             $schedules->callOnEveryItem('updateNextRun');
+
+            $this->trace->info(TraceCode::SCHEDULE_NEXT_RUN_UPDATED, $schedules->toArray());
         }
 
-        $txns = $this->filterTransactionsForSettlement($txns, $channel, $schedule);
+        $this->trace->info(TraceCode::SCHEDULE_UNSETTLED_TXNS, [$txns]);
 
-        $this->trace->info(TraceCode::FORCE_AUTHORIZE_TIMEOUT_PAYMENTS_RESPONSE, [$txns]);
+        $txns = $this->filterTransactionsForSettlement($txns, $channel, $schedule);
 
         return $this->repo->transaction(function() use ($txns, $channel)
         {
