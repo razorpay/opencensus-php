dc6aafa60fd5853807507a514b03de0184e517bd
gaurav <gaurav.d@razorpay.com>
2016-10-27 22:08:50 +0530

    [verify] fixed test cases
    remove extra dot

--- tests/Functional/Payment/VerifyTest.php
+++ tests/Functional/Payment/VerifyTest.php
@@ -325,13 +325,19 @@ class VerifyTest extends TestCase
         unset($content['authorize_time']);
 
         $defaultParams = [
-            'success'    => 0,
-            'authorized' => 0,
-            'timeout'    => 0,
-            'error'      => 0,
+            'success'       => 0,
+            'authorized'    => 0,
+            'timeout'       => 0,
+            'error'         => 0,
         ];
 
+        $total = array_sum($defaultParams);
+
+
         $defaultParams = array_merge($defaultParams, $param);
+
+        $defaultParams['total_payments'] = $defaultParams['success'] +
+            $defaultParams['authorized'] + $defaultParams['timeout'] + $defaultParams['error'];
 
         $this->assertEquals($defaultParams, $content);
     }
@@ -557,6 +563,7 @@ class VerifyTest extends TestCase
                 'error'          => 0,
                 'authorize_time' => 0,
                 'total_time'     => '0 secs',
+                'total_payments' => 1,
             ],
             $content);
     }
