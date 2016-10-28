28a90cedf6dc031c79833775a1d8cdb205c8745f
Venkatesan <venkat.v@razorpay.com>
2016-10-28 13:18:43 +0530

    remmove unnecessary repository method

--- app/Models/Payment/TerminalAnalytics/Repository.php
+++ app/Models/Payment/TerminalAnalytics/Repository.php
@@ -15,14 +15,7 @@ class Repository extends Base\Repository
         Entity::PAYMENT_ID      => 'sometimes|alpha_num',
         Entity::TERMINAL_ID     => 'sometimes|alpha_num',
     );
-
-    public function fetchTerminalAnalyticsForPaymentIds($paymentIds = [])
-    {
-        return $this->newQuery()
-            ->whereIn(Entity::PAYMENT_ID, $paymentIds)
-            ->get();
-    }
-
+    
     public function fetchFailedTerminalAnalyticsForPaymentIds($paymentIds = [])
     {
         return $this->newQuery()
