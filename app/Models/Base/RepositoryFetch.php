0b148e3a5d02cbb17c9add41caeee645d2e6e90c
Shashank Kumar <shashankkumar.me@gmail.com>
2016-10-31 22:27:36 +0530

    [repository] Fixes minor function overload signature difference bug.

--- app/Models/Base/RepositoryFetch.php
+++ app/Models/Base/RepositoryFetch.php
@@ -246,7 +246,7 @@ trait RepositoryFetch
         return $newParams;
     }
 
-    protected function validateAdditional($params)
+    protected function validateAdditional(array $params)
     {
         ;
     }
