6bafcc7f70c0e18dfd9a14cbfc82673f11e6bd30
Shashank Kumar <shashankkumar.me@gmail.com>
2016-10-31 22:35:04 +0530

    [validator] Uses JitValidator for input validation in Report.

--- app/Base/Validator.php
+++ app/Base/Validator.php
@@ -7,12 +7,6 @@ use RZP\Exception;
 
 class Validator extends \Razorpay\Spine\Validation\Validator
 {
-    protected static $reportRules = [
-        'year'  =>  'required|digits:4',
-        'month' =>  'required|digits_between:1,2',
-        'day'   =>  'sometimes|digits_between:1,2',
-    ];
-
     protected function throwExtraFieldsException($extraFields)
     {
         throw new Exception\ExtraFieldsException($extraFields);
