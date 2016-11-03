0b1913712c6b2ba519787b1412f52839298ea4fc
selvagsz <selvaganeshbsg@gmail.com>
2016-11-04 01:08:58 +0530

    Transform $.ajax into true promise

---
+++ public/react/rzp/utils/cookies.js
@@ -0,0 +1,4 @@
+export const getCookie = (key) => {
+  if (!key) return null
+  return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(key).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
+}
