122783a4a4fd7b3f9fff20bcb7fac28a52a9b2d8
Shashank Kumar <shashankkumar.me@gmail.com>
2016-11-05 13:44:37 +0530

    [repository] Validates id generation if entity is synced in test and live.

---
+++ tests/Unit/Models/Base/RepositoryTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace RZP\Tests\Unit\Models\Base;
+
+use RZP\Tests\Functional\TestCase;
+use RZP\Models\Merchant;
+
+class RepositoryTest extends TestCase
+{
+    public function testVerifyIdGenerationWhenTestAndLiveEntityAreInSync()
+    {
+        $repo = (new Merchant\Repository);
+
+        $entity = (new Merchant\Entity);
+
+        $this->setExpectedException(
+            'RZP\Exception\LogicException',
+            'Unique id not generated for the entity');
+
+        $repo->saveOrFail($entity);
+    }
+}
