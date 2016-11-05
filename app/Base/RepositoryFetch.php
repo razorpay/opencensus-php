b15e867a2fad47d18ceb1663604799eaa525c4c5
Vivek Kumar <viveklakhera@gmail.com>
2016-11-05 19:55:41 +0530

    [jit_Validator] Fix custom validator

--- app/Base/RepositoryFetch.php
+++ app/Base/RepositoryFetch.php
@@ -302,7 +302,7 @@ trait RepositoryFetch
     {
         // Function name should start from 'validator'
 
-        assert (strpos($func, 'validator') === 0);
+        assert (strpos($func, 'validate') === 0);
 
         $this->$func($attribute, $value, $parameters);
     }
