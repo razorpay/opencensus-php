7ae8104b0647535cc3645204646749927e096349
Shashank Kumar <shashankkumar.me@gmail.com>
2016-11-07 14:03:46 +0530

    [segment] Disable sending segment events on 'test' mode.

--- app/Services/SegmentClient.php
+++ app/Services/SegmentClient.php
@@ -215,7 +215,8 @@ class SegmentClient extends Base\Core
 
     protected function sendLumberjackRequest($headers, $url, $events)
     {
-        if ($this->mock)
+        if (($this->mock) or
+            ($this->mode === Mode::TEST))
         {
             return;
         }
