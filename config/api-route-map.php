519d34e7d4794ce38bb1882d7c051503735a9bfb
Rishabh <rishabh.pugalia@gmail.com>
2016-11-10 00:28:12 +0530

    role view (list) and add

--- config/api-route-map.php
+++ config/api-route-map.php
@@ -8,4 +8,8 @@ return [
     'org_get_multiple' 					=> 'orgs',
     'org_get' 							=> 'orgs/{id}',
     'org_edit' 							=> 'orgs/{id}',
+
+    // Roles
+    'role_get_multiple'                 => 'orgs/{id}/roles',
+    'role_create'                       => 'orgs/{id}/roles'
 ];
