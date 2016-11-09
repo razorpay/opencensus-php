34e1f179c7f1be8497423cd6a2721e59c4059da9
Rishabh <rishabh.pugalia@gmail.com>
2016-11-09 14:34:28 +0530

    migration change

--- database/migrations/2016_11_03_172347_create_merchant_map.php
+++ database/migrations/2016_11_03_172347_create_merchant_map.php
@@ -39,11 +39,6 @@ class CreateMerchantMap extends Migration
      */
     public function down()
     {
-        Schema::table(Table::MERCHANT_MAP, function($table)
-        {
-            $table->dropForeign(Table::MERCHANT_MAP.'_merchant_id_foreign');
-        });
-
         Schema::drop(Table::MERCHANT_MAP);
     }
 }
