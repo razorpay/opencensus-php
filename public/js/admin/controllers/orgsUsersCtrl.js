12bfb83ab82f30e80ddf500a5a9406cd575e7307
Ankit <ankitstarski@gmail.com>
2016-11-10 17:11:05 +0530

    add orgs user list & edit modal

--- public/js/admin/controllers/orgsUsersCtrl.js
+++ public/js/admin/controllers/orgsUsersCtrl.js
@@ -6,21 +6,55 @@ app.controller('OrgsUsersCtrl', [
   'alertsFactory',
   'transformRequestAsFormPost',
   function ($scope, $http, $modal, alertsFactory, transformRequestAsFormPost) {
-    $scope.users = {};
+    $scope.users = [];
+    $scope.count = 0;
     $scope.alerts = alertsFactory.getHandler();
 
-    $scope.listUsers = function() {
+    /**
+     *  Modals
+    **/
+
+    $scope.openEditOrgUser = function (id) {
+      $scope.selected = $scope.users.filter(function(x) { return x['id'] === id; });
+      var modalInstance = $modal.open({
+        templateUrl: 'editOrgUserModalContent.html',
+        controller: 'editOrgUserModalCtrl',
+        resolve: {
+          current: function () {
+            return jQuery.extend({}, $scope.selected[0]);
+          }
+        }
+      });
+      modalInstance.result.then(function (admin) {
+        $scope.editAdmin(admin);
+      }, $.noop);
+    };
+
+    /**
+     *  Actions
+    **/
+
+    $scope.listUsers = function(id) {
       var request = $http.get('/admin/generic', {
         params: {
-          route_name: 'admin_get_multiple'
+          route_name: 'admin_get_multiple',
+
+          url_params: {
+            '{id}': id
+          }
         }
       });
       request.success(function (data) {
         if (data.success) {
-          $scope.users = data.data;
+          $scope.users = data.data.items;
+          $scope.count = data.data.count;
         }
       });
     }
+
+    $scope.listUsers('org_6dLbNSpv5XbCOG');
+
+
 
   }
 ]).controller('newAdminModalCtrl', [
@@ -34,15 +68,15 @@ app.controller('OrgsUsersCtrl', [
       $modalInstance.dismiss('cancel');
     };
   }
-]).controller('editAdminModalCtrl', [
+]).controller('editOrgUserModalCtrl', [
   '$scope',
   '$modalInstance',
   'current',
   function ($scope, $modalInstance, current) {
 
-    $scope.current = current;
-    $scope.ok = function (admin) {
-      $modalInstance.close(admin);
+    $scope.user = current;
+    $scope.ok = function (user) {
+      $modalInstance.close(user);
     };
     $scope.cancel = function () {
       $modalInstance.dismiss('cancel');
