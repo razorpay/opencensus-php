a44302a8b724ef831b41655bf29fa090e8156ce5
Pranav Gupta <pranavguptarulz@gmail.com>
2016-11-11 18:24:24 +0530

    success view

--- resources/views/invoice/index.blade.php
+++ resources/views/invoice/index.blade.php
@@ -15,10 +15,34 @@
     </script>
     @endif
     <script src="https://checkout.razorpay.com/v1/checkout.js"></script>
+    <style>
+    body {
+      text-align: center;
+      font-family: -apple-system, ubuntu, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif;
+      color: #414141;
+      background: #ecf0f1;
+    }
+    path {
+      fill: #6DCA00;
+    }
+    h3 {
+      font-weight: normal;
+    }
+    #success {
+      background: #fff;
+      border-radius: 2px;
+      box-shadow: 0 2px 9px rgba(0, 0, 0, 0.1);
+      padding: 30px;
+      margin: 30px auto;
+      width: 80%;
+      max-width: 300px;
+    }
+    </style>
   </head>
 <body>
-  <div id='success' style='display: none'>
-    <h1>Your Payment has been received</h1>
+  <div id='success'>
+    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-1.959 17l-4.5-4.319 1.395-1.435 3.08 2.937 7.021-7.183 1.422 1.409-8.418 8.591z"/></svg>
+    <h3>Your Payment has been received</h3>
   </div>
 </body>
   <script>
@@ -28,7 +52,7 @@
       amount: data.amount,
       description: 'Invoice #' + data.invoice_id,
       handler: function(response) {
-        document.querySelector('success').style.display = 'block';
+        document.querySelector('#success').style.display = 'block';
       },
       prefill: {
         contact: data.customer_contact,
