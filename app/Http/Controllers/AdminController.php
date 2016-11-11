629f0c2cc4a68c1a59cce551037c5d2964a37b4c
Giridaran <giri@razorpay.com>
2016-11-11 12:47:39 +0530

    [heimdall] Add the beta wl dashboard for default org.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -25,8 +25,9 @@ class AdminController extends Controller
     // Org domain => Org ID
     // We'll hardcode this for now
     const ORG_CHART = [
-        'dashboard.razorpay.dev' => 'org_6dLbNSpv5XbCOG',
-        'heimdall.razorpay.dev'=> 'org_6dLbNSpv5XbCOG',
+        'dashboard.razorpay.dev'         => 'org_6dLbNSpv5XbCOG',
+        'heimdall.razorpay.dev'          => 'org_6dLbNSpv5XbCOG',
+        'beta-wl-dashboard.razorpay.com' => 'org_6dLbNSpv5XbCOG',
     ];
 
     /*
