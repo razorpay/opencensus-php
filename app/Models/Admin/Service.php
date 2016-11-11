720e1f1dbb43789867262a7ae522f7d29e066f97
Vivek Kumar <viveklakhera@gmail.com>
2016-11-11 09:25:19 +0530

    [entity] Fix fetch for entities with incrementing id

--- app/Models/Admin/Service.php
+++ app/Models/Admin/Service.php
@@ -15,7 +15,12 @@ class Service extends Base\Service
 
         $entityClass = Entity::getEntityClass($entity);
 
-        $id = $entityClass::verifyIdAndSilentlyStripSign($id);
+        $entityObject = new $entityClass;
+
+        if ($entityObject->getGenerateIdOnCreate() === true)
+        {
+            $id = $entityClass::verifyIdAndSilentlyStripSign($id);
+        }
 
         $entity = $this->repo->$entity->findOrFailPublic($id);
 
