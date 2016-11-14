ef1429cf987ec6bae4dbd553cdf45a893e94d1c0
Sumeeti Lalwani <sumeeti@razorpay.com>
2016-11-14 22:37:23 +0530

    Password confirmation missing in validator and compoer update

--- app/Models/Admin/Admin/Entity.php
+++ app/Models/Admin/Admin/Entity.php
@@ -14,6 +14,7 @@ class Entity extends Base\PublicEntity
     const NAME                      = 'name';
     const USERNAME                  = 'username';
     const PASSWORD                  = 'password';
+    const PASSWORD_CONFIRMATION     = 'password_confirmation';
     const EMAIL                     = 'email';
     const REMEMBER_TOKEN            = 'remember_token';
     const OAUTH_ACCESS_TOKEN        = 'oauth_access_token';
