7d79f9ca739613e6e7cbb649f9d9c7383436c578
Ankit Agarwal <ankit.agarwal@razorpay.com>
2016-11-16 20:12:51 +0530

    [fee breakup] - minor fix

--- app/Models/Transaction/DataMigration.php
+++ app/Models/Transaction/DataMigration.php
@@ -115,7 +115,7 @@ class DataMigration extends Base\Service
 
             $fee = $this->feeCalculator->calculateRzpFee($pricing, $amount);
 
-            $isValidPricingPlan = $this->isValidPricingPlan($transaction, $fee);
+            $isValidPricingPlan = $this->isValidFees($transaction, $fee);
 
             if ($isValidPricingPlan === true)
             {
@@ -131,7 +131,7 @@ class DataMigration extends Base\Service
 
             $fee = $this->feeCalculator->calculateRzpFee($pricing, $amount);
 
-            $isValidPricingPlan = $this->isValidPricingPlan($transaction, $fee);
+            $isValidPricingPlan = $this->isValidFees($transaction, $fee);
 
             if ($isValidPricingPlan === true)
             {
