791555126a254492debab146f9d14cf26d80c93c
Amit Mahbubani <amit.mahbubani@razorpay.com>
2016-11-16 13:11:03 +0530

    Change webhook route

--- app/Http/Route.php
+++ app/Http/Route.php
@@ -291,7 +291,7 @@ final class Route
         'feature_get_multiple'                    => ['get',      'features/{entityId}',                            'FeatureController@getFeatures'                                     ],
         'feature_bulk_assign'                     => ['post',     'features/assign',                                'FeatureController@multiAssignFeature'                              ],
         'feature_bulk_remove'                     => ['post',     'features/remove',                                'FeatureController@multiRemoveFeature'                              ],
-        'mailgun_webhook'                         => ['post',     'admin/email_hook/{type}',                        'AdminController@postMailgunCallback'                               ],
+        'mailgun_webhook'                         => ['post',     'mailgun_callback/{type}',                        'AdminController@postMailgunCallback'                               ],
     );
 
     public static $public = array(
