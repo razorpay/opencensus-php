312a92026a836fb5edf186e42106429cf2c8552d
Prashanth Y.V <prashanth.yv@razorpay.com>
2016-11-18 13:36:29 +0530

    [invoices] removes fail safe for now.

--- app/Services/Bitly.php
+++ app/Services/Bitly.php
@@ -51,12 +51,12 @@ class Bitly
 
         if (isset($formattedResponse['data']['url']) === false)
         {
-            $this->trace->error(
+            $this->trace->warning(
                 TraceCode::INVOICE_BITLY_FAIL,
                 $traceData
             );
 
-            return $longUrl;
+            // return $longUrl;
         }
 
         $this->trace->info(
