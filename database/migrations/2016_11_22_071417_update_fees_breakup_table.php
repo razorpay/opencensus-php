7dfee987c533c28df43a9a40b01a4bb1cba245b6
Ankit Agarwal <ankit.agarwal@razorpay.com>
2016-11-22 12:58:19 +0530

    [fee breakup] - Adding migration for unique key constraints for name and transaction id.

---
+++ database/migrations/2016_11_22_071417_update_fees_breakup_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+use RZP\Constants\Table;
+use RZP\Models\Transaction\FeeBreakup\Entity as FeeBreakup;
+
+class UpdateFeesBreakupTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table(Table::FEE_BREAKUP, function($table)
+        {
+            $table->unique([FeeBreakup::NAME, FeeBreakup::TRANSACTION_ID]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table(Table::FEE_BREAKUP, function($table)
+        {
+            $table->dropUnique([FeeBreakup::NAME, FeeBreakup::TRANSACTION_ID]);
+        });
+    }
+}
