787694b633de44c01700948e62b6c4589b83f046
Shashank Kumar <shashankkumar.me@gmail.com>
2016-11-25 01:04:23 +0530

    [exception] Standardize exception messages for GatewayTimeoutException and GatewayRequestException.

--- app/Exception/BaseException.php
+++ app/Exception/BaseException.php
@@ -9,7 +9,7 @@ class BaseException extends Exception
 {
     protected $error = null;
 
-    protected $data = null;
+    protected $data = []];
 
     /**
      * Constructor for base exception of the
