01eff3e9640e0d827155981c682f6aeb78f64a5c
Shashank Kumar <shashankkumar.me@gmail.com>
2016-11-26 12:28:54 +0530

    [cards] Fixes test for rule selection when card type is unknown.

--- tests/Unit/Models/Card/MerchantFeeTest.php
+++ tests/Unit/Models/Card/MerchantFeeTest.php
@@ -403,13 +403,13 @@ class MerchantFeeTest extends TestCase
 
         // Credit Card rule not available in plan,
         // Card type unknown will be treated as
-        // debit card and their rules will be applied
+        // credit card and their rules will be applied
 
-        $this->runMerchantFeeTest("100", "Visa", "4pmbgtgNVVDd7x", Card\Type::UNKNOWN);
+        $this->runMerchantFeeTest("100", "Visa", "1nvp2XPMmaRLxy", Card\Type::UNKNOWN);
 
-        $this->runMerchantFeeTest("200000", "Visa", "4pmbgtgNVVDd7x", Card\Type::UNKNOWN);
+        $this->runMerchantFeeTest("200000", "Visa", "1nvp2XPMmaRLxy", Card\Type::UNKNOWN);
 
-        $this->runMerchantFeeTest("200100", "Visa", "4pmdaEzu3jmDTx", Card\Type::UNKNOWN);
+        $this->runMerchantFeeTest("200100", "Visa", "1nvp2XPMmaRLxy", Card\Type::UNKNOWN);
 
         $this->runMerchantFeeTest("100", "Visa", "1nvp2XPMmaRLxy", Card\Type::CREDIT);
 
@@ -422,14 +422,14 @@ class MerchantFeeTest extends TestCase
         $this->fee->setPricingRepo($this->getMockPricingRepo($useCreditCardRule));
 
         // Credit Card rule not available in plan,
-        // Unknown Cards will be treated as debit card
+        // Unknown Cards will be treated as credit card
         // and subsequent rules will be applied.
 
-        $this->runMerchantFeeTest("100", "Visa", "4pmbgtgNVVDd7x", Card\Type::UNKNOWN);
+        $this->runMerchantFeeTest("100", "Visa", "1nvp2XPMmaRLxx", Card\Type::UNKNOWN);
 
-        $this->runMerchantFeeTest("200000", "Visa", "4pmbgtgNVVDd7x", Card\Type::UNKNOWN);
+        $this->runMerchantFeeTest("200000", "Visa", "1nvp2XPMmaRLxx", Card\Type::UNKNOWN);
 
-        $this->runMerchantFeeTest("200100", "Visa", "4pmdaEzu3jmDTx", Card\Type::UNKNOWN);
+        $this->runMerchantFeeTest("200100", "Visa", "1nvp2XPMmaRLxx", Card\Type::UNKNOWN);
 
         $this->runMerchantFeeTest("100", "Visa", "1nvp2XPMmaRLxx", Card\Type::CREDIT);
 
