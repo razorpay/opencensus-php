c880fa94b276130bd6f5c2760bc200dfbfc4906d
Shashank Kumar <shashankkumar.me@gmail.com>
2016-11-26 12:44:26 +0530

    [trace] Changes traceError back to traceException.

--- app/Models/Payment/Verify/Verify.php
+++ app/Models/Payment/Verify/Verify.php
@@ -464,7 +464,7 @@ class Verify extends Base\Core
             // @note: If payment verification fails due to any reason
             // other than expected ones, we should log it as an error
             // exception.
-            $this->trace->traceError($e);
+            $this->trace->traceException($e);
 
             // Just continue
             $result = Result::ERROR;
