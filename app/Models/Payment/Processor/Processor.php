348d6dc046b8e6058231f2881d0d480da2ad0452
Prashanth Y.V <prashanth.yv@razorpay.com>
2016-11-28 19:18:52 +0530

    [invoices] adds invoice id to payment entity

--- app/Models/Payment/Processor/Processor.php
+++ app/Models/Payment/Processor/Processor.php
@@ -572,6 +572,8 @@ class Processor
 
         $this->setOrderDetails($payment, $input);
 
+        $this->setInvoiceDetails($payment);
+
         $metadata = isset($input['_']) ? $input['_'] : null;
 
         $payment->setMetadata($metadata);
@@ -693,8 +695,7 @@ class Processor
 
         $validator->validateOrderNotPaid($this->order);
 
-        $validator->validateMerchantSpecificData($this->order,
-                                                 $payment);
+        $validator->validateMerchantSpecificData($this->order, $payment);
 
         $this->order->setStatus(Order\Status::ATTEMPTED);
 
@@ -710,6 +711,18 @@ class Processor
         $this->order->saveOrFail();
 
         $payment->order()->associate($this->order);
+    }
+
+    protected function setInvoiceDetails(Payment\Entity $payment, Order\Entity $order)
+    {
+        if ($order->invoice === null)
+        {
+            return;
+        }
+
+        $invoice = $order->invoice;
+
+        $payment->invoice()->associate($invoice);
     }
 
     protected function tracePaymentFailed($error, $traceCode)
