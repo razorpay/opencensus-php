6cb41add2ffad92eb0ae6e38c8c6798d3f689670
Sunny Aggrawal <sunnyagg@outlook.com>
2016-11-29 11:38:16 +0530

    [b2bwallet] adds feature contant for b2bwallet

--- app/Http/Route.php
+++ app/Http/Route.php
@@ -689,6 +689,7 @@ final class Route
         // 'item_fetch_multiple'           => 'invoice',
         // 'item_update'                   => 'invoice',
         // 'item_delete'                   => 'invoice',
+        'customer_get_wallet_balance'   => 'b2bwallet',
     );
 
     const RAZORPAYJS_ROUTES = array(
