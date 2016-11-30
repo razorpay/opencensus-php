27caf3e300c3698e8750970d9bb8e36e8105681a
Amit Mahbubani <amit.mahbubani@razorpay.com>
2016-11-30 13:10:09 +0530

    [b2bwallet] migration for customer_transactions

---
+++ database/migrations/2016_11_30_072411_create_customer_transactions.php
@@ -0,0 +1,70 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+
+use RZP\Models\Customer\Transactions\Entity;
+use RZP\Constants\Table;
+
+class CreateCustomerTransactions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(Table::CUSTOMER_TRANSACTION, function(Blueprint $table)
+        {
+            $table->engine = 'InnoDB';
+
+            $table->char(Entity::ID, Entity::ID_LENGTH)
+                  ->primary();
+
+            $table->char(Entity::ENTITY_ID, Entity::ID_LENGTH);
+
+            $table->string(Entity::ENTITY_TYPE, 50);
+
+            $table->string(Entity::STATUS, 50);
+
+            $table->bigInteger(Entity::AMOUNT)
+                  ->unsigned();
+
+            $table->char(Entity::CURRENCY, 10);
+
+            $table->bigInteger(Entity::DEBIT)
+                  ->unsigned();
+
+            $table->bigInteger(Entity::CREDIT)
+                  ->unsigned();
+
+            $table->bigInteger(Entity::BALANCE)
+                  ->nullable();
+
+            $table->string(Entity::DESCRIPTION, 255)
+                  ->nullable();
+
+            $table->integer(Entity::CREATED_AT);
+
+            $table->integer(Entity::UPDATED_AT);
+
+            $table->index(Entity::CREATED_AT);
+
+            $table->index(Entity::UPDATED_AT);
+
+            $table->index(Entity::ENTITY_ID);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop(Table::CUSTOMER_TRANSACTION);
+    }
+}
