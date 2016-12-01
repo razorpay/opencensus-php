061554d405c29a2355d2d3bcc7202b904164d8f1
Ankit Agarwal <ankit.agarwal@razorpay.com>
2016-12-01 15:44:05 +0530

    [Merchant Onboarding] - Using settlement bucket for testing in beta env

--- config/aws.php
+++ config/aws.php
@@ -27,7 +27,11 @@ return [
     'settlement_bucket'     => env('AWS_S3_SETTLEMENT_BUCKET'),
     'logo_bucket'           => env('AWS_S3_LOGO_BUCKET'),
     'h2h_bucket'            => env('AWS_H2H_BUCKET'),
-    'activation_bucket'     => env('AWS_ACTIVATION_BUCKET'),
+
+    // TODO: Change this for production after bucket is created
+    'activation_bucket'     => env('AWS_S3_SETTLEMENT_BUCKET'),
+
+    //'activation_bucket'     => env('AWS_ACTIVATION_BUCKET'),
 
     'mock' => env('AWS_S3_MOCK')
 ];
