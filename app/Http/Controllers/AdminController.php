d6391daaeedb41019bfd50a8596bfa649eb6483d
Sumeeti Lalwani <sumeeti@razorpay.com>
2016-12-05 12:48:38 +0530

    icici and bo addition

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -26,10 +26,12 @@ class AdminController extends Controller
     // Org domain => Org ID
     // We'll hardcode this for now
     const ORG_CHART = [
-        'dashboard.razorpay.dev'         => 'org_6dLbNSpv5XbCOG',
-        'heimdall.razorpay.dev'          => 'org_6dLbNSpv5XbCOG',
-        'beta-wl-dashboard.razorpay.com' => 'org_6dLbNSpv5XbCOG',
-        'dashboard.codeankit.me'         => 'org_6dLbNSpv5XbCOG',
+        'dashboard.razorpay.dev'            => 'org_6dLbNSpv5XbCOG',
+        'heimdall.razorpay.dev'             => 'org_6dLbNSpv5XbCOG',
+        'beta-wl-dashboard.razorpay.com'    => 'org_6dLbNSpv5XbCOG',
+        'dashboard.codeankit.me'            => 'org_6dLbNSpv5XbCOG',
+        'beta-icici-dashboard.razorpay.com' => 'org_6lFupOxpf36BY3',
+        'beta-bob-dashboard.razorpay.com'   => 'org_6dLbNSpv5XbCOI',
     ];
 
     /*
