3c4899bbf80c97c68bb35aefc86e9cfaba96730b
Vivek Kumar <viveklakhera@gmail.com>
2016-12-08 16:53:35 +0530

    [reversal] Remove reversal feature and add try-catch block

--- app/Models/Payment/Processor/Refund.php
+++ app/Models/Payment/Processor/Refund.php
@@ -345,8 +345,6 @@ trait Refund
             $this->tracePaymentFailed(
                     $e->getError(),
                     TraceCode::PAYMENT_REVERSE_FAILURE);
-
-            throw $e;
         }
     }
 
@@ -416,8 +414,7 @@ trait Refund
             {
                 $this->refundOnGateway($data);
             }
-            else if (($this->gatewaySupportsReverse($payment) === true) and
-                     ($payment->merchant->isFeatureEnabled(Feature::REVERSE) === true))
+            else if ($this->gatewaySupportsReverse($payment) === true)
             {
                 $this->reverseOnGateway($data);
             }
