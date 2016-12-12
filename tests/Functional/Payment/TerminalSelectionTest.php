16eea5d67891759b4b1e980b9a43bd445a963401
gaurav <gaurav.d@razorpay.com>
2016-12-12 21:18:16 +0530

    [terminal] test cases minor optimization

--- tests/Functional/Payment/TerminalSelectionTest.php
+++ tests/Functional/Payment/TerminalSelectionTest.php
@@ -12,13 +12,6 @@ class TerminalSelectionTest extends TestCase
 {
     use PaymentTrait;
 
-    public function setUp()
-    {
-        $this->testDataFilePath = __DIR__.'/helpers/TerminalSelectionTestData.php';
-
-        parent::setUp();
-    }
-
     public function testChooseGatewayWithSharedTerminals()
     {
         $this->fixtures->create('terminal:multiple_netbanking_terminals');
@@ -32,6 +25,7 @@ class TerminalSelectionTest extends TestCase
         $this->assertEquals('billdesk', $payment['gateway']);
         $this->assertEquals('1000BdeskTrmnl', $payment['terminal_id']);
     }
+
     public function testChooseGatewayWithDirectTerminals()
     {
         $this->fixtures->create('terminal:multiple_netbanking_terminals');
@@ -46,22 +40,35 @@ class TerminalSelectionTest extends TestCase
         $this->assertEquals('10BillDirTrmnl', $payment['terminal_id']);
     }
 
-    public function testAnotherMerchantDirectTerminalAssignedToTestAccountAndTestUnassign()
+    /**
+     * Assign Direct Terminal To Another Merchant
+     * Assign That Direct Terminal to Test Merchant also
+     * Unassign that terminal from test merchant and test if payment fails
+     */
+    public function testMultipleMerchantForTerminal()
     {
         $this->ba->appAuth();
 
-        $data = $this->testData['testAnotherMerchantDirectTerminalAssignedToTestAccountAndTestUnassign'];
+        $this->fixtures->create('terminal:multiple_netbanking_terminals');
 
         $this->fixtures->create('terminal:direct_terminal_for_non_test_merchant');
 
-        $mid = '10000000000000';
+        $mid = Merchant\Account::TEST_ACCOUNT;
 
         $tid = '10BillDirTrmn2';
 
-        $url = '/terminals/'.$tid.'/merchants/'.$mid;
+        $payment = $this->getDefaultNetbankingPaymentArray();
+
+        $payment = $this->doAuthAndCapturePayment($payment);
+
+        $payment = $this->getLastEntity('payment', true);
+
+        $this->assertEquals('1000BdeskTrmnl', $payment['terminal_id']);
+
+        $url = '/terminals/' . $tid . '/merchants/' . $mid;
 
         $request = [
-            'url'   => $url,
+            'url'    => $url,
             'method' => 'PUT'
         ];
 
@@ -76,19 +83,19 @@ class TerminalSelectionTest extends TestCase
         $this->assertEquals($tid, $payment['terminal_id']);
 
         $request = [
-            'url'   => $url,
+            'url'    => $url,
             'method' => 'DELETE'
         ];
 
         $content = $this->makeRequestAndGetContent($request);
 
         $payment = $this->getDefaultNetbankingPaymentArray();
-        $this->runRequestResponseFlow(
-            $data,
-            function () use ($payment)
-            {
-                $payment = $this->doAuthAndCapturePayment($payment);
-            });
+
+        $payment = $this->doAuthAndCapturePayment($payment);
+
+        $payment = $this->getLastEntity('payment', true);
+
+        $this->assertEquals('1000BdeskTrmnl', $payment['terminal_id']);
     }
 
     public function testChooseTerminalWithCategory()
