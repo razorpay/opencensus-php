c65da517a4abf2fef118e19d65527fb656129367
Saurav Chowdhury <saurav1991@gmail.com>
2016-12-13 18:13:47 +0530

    [feature] Exposes feature modification action to merchant (#1782)
    * [feature] Exposes feature add api to merchant dashboard so that merchants can modify selected features
    
    * [feature] Adds default feature value map
    
    * [feature] Adds tests
    
    * fixes failing test
    
    * [feature] Temporarily adds toggle_lock to merchants with cardsaving feature
    
    * [feature] Adds trace log on merchant opt out
    
    * [noflash] make flashcheckout routes public and route name changes
    
    * [flashcheckout] minor changes on get features
    
    * [noflash] changes for the udpate merchant features and validations
    
    * [flashcheckout] adds review changes and test fixes
    
    * [flashcheckout] Test fix
    
    * [noflashcheckout] Makes cardsaving feature visible to merchant
    
    * [noflashcheckout] Code review change

--- app/Http/Controllers/MerchantController.php
+++ app/Http/Controllers/MerchantController.php
@@ -520,6 +520,22 @@ class MerchantController extends Controller
         return ApiResponse::json($data);
     }
 
+    public function updateMerchantFeatures($id)
+    {
+        $input = Request::all();
+
+        $data = (new Merchant\Service)->addOrRemoveMerchantFeatures($input);
+
+        return ApiResponse::json($data);
+    }
+
+    public function getMerchantFeatures($id)
+    {
+        $data = (new Merchant\Service)->getMerchantFeatures();
+
+        return ApiResponse::json($data);
+    }
+
     // --------------------- Credits API Handlers -----------------------------------------
 
     public function postCreateCreditsLog(Credits\Service $service, $id)
