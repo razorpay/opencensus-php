e29db1dd7f4cf64be972a0645a5b49f956d2ad48
Ankit Agarwal <ankit.agarwal@razorpay.com>
2016-12-16 16:50:28 +0530

    [Merchant Onboarding]: Fix Submit issue - Fixed the required field logic - Added test case for submit call

--- app/Models/Merchant/Detail/Service.php
+++ app/Models/Merchant/Detail/Service.php
@@ -38,7 +38,7 @@ class Service extends Base\Service
             $this->markSubmitted($merchantDetails);
         }
 
-        return $response;
+        return $this->createResponse($merchantDetails);
     }
 
     public function uploadActivationFile(array $input)
@@ -122,7 +122,7 @@ class Service extends Base\Service
     {
         return (($response['can_submit'] === true) and
                 (isset($input[Detail\Entity::SUBMIT]) === true) and
-                ($input[Detail\Entity::SUBMIT] === true));
+                    ($input[Detail\Entity::SUBMIT] === '1'));
     }
 
     protected function markSubmitted($merchantDetails)
@@ -168,7 +168,17 @@ class Service extends Base\Service
 
         // List of all the required fields which are not set
         $detailsKeys = array_keys($merchantDetailsArr);
-        $requiredFields = array_diff($detailsKeys, ValidationFields::DASHBOARD_FIELDS);
+
+        $requiredFields = [];
+
+        foreach (ValidationFields::DASHBOARD_FIELDS as $key)
+        {
+            if ((array_key_exists($key, $merchantDetailsArr) === false) or
+                (is_null($merchantDetailsArr[$key]) === true))
+            {
+                $requiredFields[] = $key;
+            }
+        }
 
         if (count($requiredFields) > 0)
         {
