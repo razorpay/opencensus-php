b7f018468b5028e8e54e2bfbd82fbbedbcc86b21
Sumeeti Lalwani <sumeeti@razorpay.com>
2016-12-17 16:29:08 +0530

    Custom code for orgs

--- app/Models/Admin/Org/Validator.php
+++ app/Models/Admin/Org/Validator.php
@@ -20,6 +20,7 @@ class Validator extends Base\Validator
         Entity::MAIN_LOGO_URL       => 'sometimes|url',
         Entity::INVOICE_LOGO_URL    => 'sometimes|url',
         'admin'                     => 'required|array',
+        'custom_code'               => 'required',
     ];
 
     protected static $editRules = [
@@ -33,6 +34,7 @@ class Validator extends Base\Validator
         Entity::LOGIN_LOGO_URL      => 'sometimes|url',
         Entity::MAIN_LOGO_URL       => 'sometimes|url',
         Entity::INVOICE_LOGO_URL    => 'sometimes|url',
+        'custom_code'               => 'required',
     ];
 
     protected function validateEmailDomains($attribute, $domains)
