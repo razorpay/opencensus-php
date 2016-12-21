d75e72f66e0fcfdf51c8ad9154d1dedfdc065b2b
Vivek Kumar <viveklakhera@gmail.com>
2016-12-21 20:09:11 +0530

    [migs] Add strict checking in verify refund

--- app/Models/Payment/Processor/Refund.php
+++ app/Models/Payment/Processor/Refund.php
@@ -41,14 +41,14 @@ trait Refund
 
         $this->setPaymentAndRefundInfo($refund, $payment);
 
-        $enabledGateways = [
+        $verifyRefundGateways = [
             Payment\Gateway::HDFC,
             Payment\Gateway::AXIS_MIGS
         ];
 
         // Currently doing it for only HDFC. In case when other gateways start
         // getting similar issues, we will start supporting for them too.
-        if (in_array($payment->getGateway(), $enabledGateways) === false)
+        if (in_array($payment->getGateway(), $verifyRefundGateways, true) === false)
         {
             throw new Exception\BadRequestException(ErrorCode::BAD_REQUEST_INVALID_GATEWAY);
         }
