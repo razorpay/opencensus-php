3ae4ccc4fc9a3c0dbd653bd2448c920b52e62c09
Shashank Kumar <shashankkumar.me@gmail.com>
2016-12-23 03:44:06 +0530

    [report] Adds a test case for broking report generation.

--- tests/Functional/Merchant/EntityReportTest.php
+++ tests/Functional/Merchant/EntityReportTest.php
@@ -55,4 +55,54 @@ class EntityReportTest extends TestCase
         $this->assertEquals($invoice['tax'], '300');
         $this->assertEquals($invoice['razorpay_fee'], 2000);
     }
+
+    public function testBrokingReport()
+    {
+        $this->sharedTerminal = $this->fixtures->create('terminal:shared_billdesk_terminal');
+
+        $payment = $this->getDefaultNetbankingPaymentArray();
+        $this->doAuthAndCapturePayment($payment);
+        $this->doAuthCaptureAndRefundPayment($payment);
+
+        $dt = Carbon::today('Asia/Kolkata');
+
+        $input = array(
+            'year' => $dt->year,
+            'month' => $dt->month,
+            'day' => $dt->day);
+
+        $combinedReport = $this->fetchReport('broking', $input);
+
+        $this->assertEquals(count($combinedReport), 3);
+
+        $expectedContent = [
+            'Merchant Name' => 'ut',
+            'Merchant ID' => '10000000000000',
+            // 'Txn Id' => 'pay_6w6bmFIqLiOGVj',
+            'Txn State' => 'Sale',
+            // 'Txn Date' => '2016-12-23 03:28',
+            'Client Code' => null,
+            'Merchant Txn Id' => null,
+            'Product' => 'NSE',
+            'Discriminator' => 'NB',
+            'Bank Name' => 'ICICI Bank Ltd',
+            'Card Type' => null,
+            'Card No' => null,
+            'Card Issuing Bank' => null,
+            // 'Bank Ref No' => 'GJZMBHNV9O',
+            'Gross Txn Amount' => 500,
+            'Txn Charges' => 12.5,
+            'Service Tax' => 1.76,
+            'SB Cess' => 0.06,
+            'Krishi Kalyan Cess' => 0.06,
+            'Total Chargeable' => 14.38,
+            'Net Amount' => 485.62,
+            'Payment Status' => null,
+            'Settlement Date' => null,
+            'Refund Reference' => null,
+            'Refund Status' => null,
+        ];
+
+        $this->assertArraySelectiveEquals($expectedContent, $combinedReport[0]);
+    }
 }
