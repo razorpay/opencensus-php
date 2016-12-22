988a7321caff7eac245130b67e3facdba9f3faac
Sreeram Boyapati <sreeram.boyapati@razorpay.com>
2016-12-22 17:42:28 +0530

    [Admin] perform checks if admin auth

--- app/Models/Admin/Admin/Service.php
+++ app/Models/Admin/Admin/Service.php
@@ -346,11 +346,15 @@ class Service extends Base\Service
 
     public function editAdmin(string $orgId, string $adminId, array $input)
     {
-        // $authAdmin = $this->app['basicauth']->getAdmin();
-
         $admin = $this->repo->admin->findByPublicIdAndOrgId($adminId, $orgId);
 
-        // $admin->getValidator()->validateSelfEditForbidden($authAdmin, $admin);
+        // making impromptu changes to make editAdmin work on appAuth
+        if ($this->app['basicauth']->isAdminAuth() === true)
+        {
+            $authAdmin = $this->app['basicauth']->getAdmin();
+
+            $admin->getValidator()->validateSelfEditForbidden($authAdmin, $admin);
+        }
 
         $admin = $this->core()->edit($admin, $input);
 
