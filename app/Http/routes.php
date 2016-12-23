3ee603e9c2576dbc87b9d04ddadca2c5c819d38f
Ankit Agarwal <ankit.agarwal@razorpay.com>
2016-12-23 17:49:11 +0530

    [Signup Flow]: Minor Fix

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -33,7 +33,7 @@ Route::group([], function()
     {
         Route::post('/user/signin', 'UserController@postSignin');
         Route::post('/user/register', 'UserController@postRegister');
-        Route::post('/user/pre_signup', 'MerchantController@postSignup');\
+        Route::post('/user/pre_signup', 'MerchantController@postSignup');
         Route::get('/user/pre_signup', 'MerchantController@getSignup');
         Route::post('/user/resend', 'MerchantController@postResendConfirmation');
         Route::post('/user/password/reset', 'PasswordController@postRemind');
