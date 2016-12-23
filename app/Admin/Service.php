4bc34a4178c2817f9518d0c99715b1dfbcb9a116
Sreeram Boyapati <sreeram.boyapati@razorpay.com>
2016-12-23 16:04:47 +0530

    [Admin] Fix alignment issues

--- app/Admin/Service.php
+++ app/Admin/Service.php
@@ -139,7 +139,7 @@ class Service extends Base\Service
         // TODO: can throw exception
         $admin = $this->api
                       ->admin
-                      ->getByEmail($orgId, ['email' => 'harshil@razorpay.com'])
+                      ->getByEmail($orgId, ['email' => $result->email])
                       ->toArray();
 
         if ($admin)
