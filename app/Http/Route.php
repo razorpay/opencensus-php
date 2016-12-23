03a756d96821fa4c305bb4f10fb50dc948b97859
Nemo <nemo@razorpay.com>
2016-12-23 19:50:15 +0530

    [security] Hides oauth access token and old passwords from responses

--- app/Http/Route.php
+++ app/Http/Route.php
@@ -669,7 +669,6 @@ final class Route
         'group_get',
         'group_get_multiple',
         'group_get_allowed_groups',
-        'admin_get',
         'org_create',
         'org_edit',
         'org_delete',
