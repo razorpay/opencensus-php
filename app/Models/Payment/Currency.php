52b7be5dbe2369184b21710a3534b369785317f0
Sunny Aggrawal <sunnyagg@outlook.com>
2016-12-26 15:35:44 +0530

    [international] updates currency list

--- app/Models/Payment/Currency.php
+++ app/Models/Payment/Currency.php
@@ -4,12 +4,106 @@ namespace RZP\Models\Payment;
 
 class Currency
 {
+    //Brazilian Real
+    const BRL = 'BRL';
+    //Euro
+    const EUR = 'EUR';
+    //Indian Rupee
     const INR = 'INR';
+    //Pound Sterling
+    const GBP = 'GBP';
+    //US Dollar
     const USD = 'USD';
+    //South African Rand
+    const ZAR = 'ZAR';
+    //Swiss Franc
+    const CHF = 'CHF';
+    //Australian Dollar
+    const AUD = 'AUD';
+    //Bahrain Dinar
+    const BHD = 'BHD';
+    //Canadian Dollar
+    const CAD = 'CAD';
+    //Chinese Renmibi
+    const CNY = 'CNY';
+    //Croatian Kuna
+    const HRK = 'HRK';
+    //Czech Koruna
+    const CZK = 'CZK';
+    //Danish Krone
+    const DKK = 'DKK';
+    //Hong Kong Dollar
+    const HKD = 'HKD';
+    //Hungarian Forint
+    const HUF = 'HUF';
+    //Israeli New Shekel
+    const ISL = 'ISL';
+    //Japanese Yen
+    const JPY = 'JPY';
+    //Kuwaiti Dinar
+    const KWD = 'KWD';
+    //Lithuanian Litas
+    const LTL = 'LTL';
+    //Mexican Peso
+    const MXN = 'MXN';
+    //New Zealand Dollar
+    const NZD = 'NZD';
+    //Norwegian Krone
+    const NOK = 'NOK';
+    //Polish Zloty
+    const PLN = 'PLN';
+    //Romanian New Leu
+    const RON = 'RON';
+    //Saudi Rihal
+    const SAR = 'SAR';
+    //Singapore Dollar
+    const SGD = 'SGD';
+    //South Korean Won
+    const KRW = 'KRW';
+    //Swedish Krona
+    const SEK = 'SEK';
+    //Turkish Lira
+    const TRY = 'TRY';
+    //UAE Dirham
+    const AED = 'AED';
 
     const SUPPORTED_CURRENCIES = [
         self::INR,
         self::USD,
+    ];
+
+    const ISO_NUMERIC_CODES = [
+        self::BRL => '986',
+        self::EUR => '978',
+        self::INR => '356',
+        self::GBP => '826',
+        self::USD => '840',
+        self::ZAR => '710',
+        self::CHF => '756',
+        self::AUD => '036',
+        self::BHD => '048',
+        self::CAD => '124',
+        self::CNY => '156',
+        self::HRK => '191',
+        self::CZK => '203',
+        self::DKK => '208',
+        self::HKD => '344',
+        self::HUF => '348',
+        self::ISL => '376',
+        self::JPY => '392',
+        self::KWD => '414',
+        self::LTL => '440',
+        self::MXN => '484',
+        self::NZD => '554',
+        self::NOK => '578',
+        self::PLN => '985',
+        self::RON => '946',
+        self::SAR => '682',
+        self::SGD => '702',
+        self::KRW => '410',
+        self::SEK => '752',
+        self::TRY => '949',
+        self::AED => '784',
     ];
 }
 
