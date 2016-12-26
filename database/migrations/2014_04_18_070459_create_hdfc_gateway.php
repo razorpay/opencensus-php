18fc5466e7ef461c52f449c98547b91fabf3e1c1
Sunny Aggrawal <sunnyagg@outlook.com>
2016-12-26 12:26:32 +0530

    [international] removes currency from gateway entity for now

--- database/migrations/2014_04_18_070459_create_hdfc_gateway.php
+++ database/migrations/2014_04_18_070459_create_hdfc_gateway.php
@@ -37,8 +37,6 @@ class CreateHdfcGateway extends Migration
 
             $table->string('amount', 10);
 
-            $table->string('currency', 3);
-
             $table->string('enroll_result', Hdfc\Constants::ENROLL_RESULT_LENGTH)
                   ->nullable();
 
