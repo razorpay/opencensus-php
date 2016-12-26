439e39b09ce3d997537ba935e8d6e9ecb4ec4216
Pranav Gupta <pranavguptarulz@gmail.com>
2016-12-26 15:17:54 +0530

    initial markup in otp view

--- resources/views/gateway/gatewayOtpPostForm.blade.php
+++ resources/views/gateway/gatewayOtpPostForm.blade.php
@@ -7,6 +7,13 @@
     $basepath = 'http://static.razorpay.dev/payment_redirect';
     $basepath = 'https://cdn.razorpay.com/static/payment_redirect';
   ?>
+  <style>
+.loader{height:24px;width:24px;border-radius:50%;display:inline-block;opacity:0;
+animation:lo .8s infinite linear;-webkit-animation:lo .8s infinite linear;
+transition:0.3s;-webkit-transition:0.3s;
+border:2px solid #29B7D6;border-top-color:transparent}
+@keyframes lo{to{transform:rotate(360deg)}}@-webkit-keyframes lo{to{-webkit-transform:rotate(360deg)}}
+  </style>
   <link href="{{$basepath}}/bundle.css" rel="stylesheet"></link>
 </head>
 <body>
