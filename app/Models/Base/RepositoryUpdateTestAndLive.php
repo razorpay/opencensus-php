a8a43dbb64079a78da7753ba211006024fb7a16e
Shashank Kumar <shashankkumar.me@gmail.com>
2016-12-28 16:23:32 +0530

    [repository] Refactors sync code for repo live and test. Fixes bugs in forceDelete function.

--- app/Models/Base/RepositoryUpdateTestAndLive.php
+++ app/Models/Base/RepositoryUpdateTestAndLive.php
@@ -17,54 +17,34 @@ trait RepositoryUpdateTestAndLive
         $this->validateInstanceIsOfCurrentEntity($entity);
         $this->validateIdGenerated($entity);
 
-        $exists = $entity->exists;
-
-        $this->db->connection('test')->beginTransaction();
-        $this->db->connection('live')->beginTransaction();
-
-        try
-        {
-            if ($exists)
+        $liveEntity = $this->manager->transactionOnLiveAndTest(
+            function () use ($entity, $options)
             {
-                //
-                // The entity already exists in db
-                // Fetch it from both live and test databases
-                // and lock for update
-                //
+                $exists = $entity->exists;
 
-                list($testEntity, $liveEntity) = $this->dualUpdateFetchEntities($entity);
-            }
-            else
-            {
-                //
-                // The entity hasn't been persisted yet.
-                // Create it's copies for live and test database
-                //
-                $testEntity = clone $entity;
-                // we do not care about audit actions for test entities at this point
-                $testEntity->resetAuditAction();
+                if ($exists)
+                {
+                    //
+                    // The entity already exists in db
+                    // Fetch it from both live and test databases
+                    // and lock for update
+                    //
 
-                $liveEntity = clone $entity;
-            }
+                    list($testEntity, $liveEntity) = $this->dualUpdateFetchEntities($entity);
+                }
+                else
+                {
+                    list($liveEntity, $testEntity) = $this->cloneEntity($entity);
+                }
 
-            // Persist the entity in both live and test databases.
-            $liveEntity->setConnection('live')->saveOrFail($options);
-            $testEntity->setConnection('test')->saveOrFail($options);
-        }
-        catch (\Exception $e)
-        {
-            //
-            // Some error occurred, rollback now.
-            //
-            $this->db->connection('live')->rollBack();
-            $this->db->connection('test')->rollBack();
+                // Persist the entity in both live and test databases.
+                $liveEntity->saveOrFail($options);
+                $testEntity->saveOrFail($options);
 
-            throw $e;
-        }
+                $this->validateEntitiesMatch($liveEntity, $testEntity);
 
-        // Update finished successfully, commit now.
-        $this->db->connection('live')->commit();
-        $this->db->connection('test')->commit();
+                return $liveEntity;
+            });
 
         // Now that the entity has been updated in both live and test databases,
         // update the entity (in-memory) passed as argument in this function
@@ -75,126 +55,64 @@ trait RepositoryUpdateTestAndLive
 
     public function sync($entity, $relation, $ids = array())
     {
-        $this->db->connection('test')->beginTransaction();
-        $this->db->connection('live')->beginTransaction();
+        return $this->manager->transactionOnLiveAndTest(
+            function () use ($entity, $relation, $ids)
+            {
+                $changes = [];
 
-        $changes = [];
+                //
+                // The relationship hasn't been synced yet.
+                // Create it's copies for live and test database
+                //
+                list($liveEntity, $testEntity) = $this->cloneEntity($entity);
 
-        try
-        {
-            //
-            // The relationship hasn't been synced yet.
-            // Create it's copies for live and test database
-            //
-            $testEntity = clone $entity;
-            $testEntity->resetAuditAction();
-            $liveEntity = clone $entity;
+                // Sync the relationship in both live and test databases.
+                // In laravel 5.2 there is no way to use the parent
+                // model connection in relations because of which this
+                // hack is used.
+                // This has been fixed in Laravel 5.4 by #16103.
+                // We'll use the parent connection once we update to
+                // L5.4
+                Config::set('database.default', 'live');
+                $changes = $liveEntity->$relation()->sync($ids);
 
-            $defaultConnection = Config::get('database.default');
+                Config::set('database.default', 'test');
+                $testEntity->$relation()->sync($ids);
 
-            // Sync the relationship in both live and test databases.
-            // In laravel 5.2 there is no way to use the parent
-            // model connection in relations because of which this
-            // hack is used.
-            // This has been fixed in Laravel 5.4 by #16103.
-            // We'll use the parent connection once we update to
-            // L5.4
-            Config::set('database.default', 'live');
-            $changes = $liveEntity->$relation()->sync($ids);
-
-            Config::set('database.default', 'test');
-            $testEntity->$relation()->sync($ids);
-        }
-        catch (\Exception $e)
-        {
-            //
-            // Some error occurred, rollback now.
-            //
-            $this->db->connection('live')->rollBack();
-            $this->db->connection('test')->rollBack();
-
-            throw $e;
-        }
-        finally
-        {
-            // Revert back the database connection to default.
-            Config::set('database.default', $defaultConnection);
-        }
-
-        // Update finished successfully, commit now.
-        $this->db->connection('live')->commit();
-        $this->db->connection('test')->commit();
-
-        $entity = $liveEntity;
-
-        return $changes;
+                return $changes;
+            });
     }
 
     public function attach($entity, $relation, $id, array $attributes = [], $touch = true)
     {
-        $this->db->connection('test')->beginTransaction();
-        $this->db->connection('live')->beginTransaction();
+        return $this->manager->transactionOnLiveAndTest(
+            function () use ($entity, $relation, $ids, $touch)
+            {
+                //
+                // The relationship hasn't been attached yet.
+                // Create it's copies for live and test database
+                //
+                list($liveEntity, $testEntity) = $this->cloneEntity($entity);
 
-        try
-        {
-            //
-            // The relationship hasn't been attached yet.
-            // Create it's copies for live and test database
-            //
-            $testEntity = clone $entity;
-            $testEntity->resetAuditAction();
-            $liveEntity = clone $entity;
+                // Attach the relationship in both live and test databases.
+                Config::set('database.default', 'live');
+                $liveEntity->$relation()->attach($id);
 
-            $defaultConnection = Config::set('database.default');
-
-            // Attach the relationship in both live and test databases.
-            Config::set('database.default', 'live');
-            $liveEntity->$relation()->attach($id);
-
-            Config::set('database.default', 'test');
-            $testEntity->$relation()->attach($id);
-        }
-        catch (\Exception $e)
-        {
-            //
-            // Some error occurred, rollback now.
-            //
-            $this->db->connection('live')->rollBack();
-            $this->db->connection('test')->rollBack();
-
-            throw $e;
-        }
-        finally
-        {
-            Config::set('database.default', $defaultConnection);
-        }
-
-        // Update finished successfully, commit now.
-        $this->db->connection('live')->commit();
-        $this->db->connection('test')->commit();
-
-        $entity = $liveEntity;
+                Config::set('database.default', 'test');
+                $testEntity->$relation()->attach($id);
+            });
     }
 
     public function delete($entity)
     {
         return $this->manager->transactionOnLiveAndTest(function () use ($entity)
         {
-            $testEntity = clone $entity;
-            $testEntity->resetAuditAction();
-            $liveEntity = clone $entity;
+            list($liveEntity, $testEntity) = $this->cloneEntity($entity);
 
-            $res1 = $liveEntity->setConnection('live')->delete();
-            $res2 = $testEntity->setConnection('test')->delete();
+            $res1 = $liveEntity->delete();
+            $res2 = $testEntity->delete();
 
-            if ($res1 !== $res2)
-            {
-                $this->db->connection('live')->rollBack();
-                $this->db->connection('test')->rollBack();
-                throw new Exception\RuntimeException(
-                    'Delete query on live and test did not give same results. ' .
-                    'Live: ' . $res1 . ' Test: ' . $res2);
-            }
+            $this->validateEntitiesMatch($liveEntity, $testEntity);
 
             return $res1;
         });
@@ -204,19 +122,12 @@ trait RepositoryUpdateTestAndLive
     {
         return $this->manager->transactionOnLiveAndTest(function () use ($entity)
         {
-            $testEntity = clone $entity;
-            $testEntity->resetAuditAction();
-            $liveEntity = clone $entity;
+            list($liveEntity, $testEntity) = $this->cloneEntity($entity);
 
             $res1 = $testEntity->forceDelete();
             $res2 = $liveEntity->forceDelete();
 
-            if ($res1 !== $res2)
-            {
-                throw new Exception\RuntimeException(
-                    'Force delete query on live and test did not give same results. ' .
-                    'Live: ' . $res1 . ' Test: ' . $res2);
-            }
+            $this->validateEntitiesMatch($liveEntity, $testEntity);
 
             return $res1;
         });
@@ -232,12 +143,40 @@ trait RepositoryUpdateTestAndLive
         $testEntity = $this->newQueryWithConnection('test')->lockForUpdate()->findOrFail($id);
         $liveEntity = $this->newQueryWithConnection('live')->lockForUpdate()->findOrFail($id);
 
-        $testAttributes = $testEntity->getAttributes();
-        $liveAttributes = $liveEntity->getAttributes();
-
         // reset the current entity's audit action with the older one
         $liveEntity->setAuditAction($auditAction);
         $testEntity->resetAuditAction();
+
+        $this->validateEntitiesMatch($liveEntity, $testEntity);
+
+        // Update the test and live entities
+        $attributes = $entity->getAttributes();
+
+        $testEntity->setRawAttributes($attributes);
+        $liveEntity->setRawAttributes($attributes);
+
+        $testEntity->setConnection('test');
+        $liveEntity->setConnection('live');
+
+        return array($testEntity, $liveEntity);
+    }
+
+    protected function cloneEntity($entity)
+    {
+        $testEntity = clone $entity;
+        $testEntity->resetAuditAction();
+        $liveEntity = clone $entity;
+
+        $liveEntity->setConnection('live');
+        $testEntity->setConnection('test');
+
+        return [$liveEntity, $testEntity];
+    }
+
+    protected function validateEntitiesMatch($liveEntity, $testEntity)
+    {
+        $testAttributes = $testEntity->getAttributes();
+        $liveAttributes = $liveEntity->getAttributes();
 
         // Timestamps are allowed to be different
         // Ignore timestamps for similarity.
@@ -255,28 +194,21 @@ trait RepositoryUpdateTestAndLive
 
         if (count($diff1) > 0)
         {
-            $msg .= print_r($diff1, true) . PHP_EOL;
+            $msg .= json_encode($diff1) . PHP_EOL;
             $diff = true;
         }
         if (count($diff2) > 0)
         {
-            $msg .= print_r($diff2, true) . PHP_EOL;
+            $msg .= json_encode($diff2) . PHP_EOL;
             $diff = true;
         }
 
         if ($diff)
         {
             $msg = 'Entity: ' . $this->entity . PHP_EOL . $msg;
-            $msg = 'A row in test and live database do not match' . PHP_EOL . $msg;
+            $msg = 'A row in test and live database do not match' . PHP_EOL;
+
             throw new Exception\LogicException($msg);
         }
-
-        // Update the test and live entities
-        $attributes = $entity->getAttributes();
-
-        $testEntity->setRawAttributes($attributes);
-        $liveEntity->setRawAttributes($attributes);
-
-        return array($testEntity, $liveEntity);
     }
 }
