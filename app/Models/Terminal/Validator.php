89caab9018008d7ff3646619a5531cbf4a3e53c4
mayankamencherla <mayank.amencherla@razorpay.com>
2016-12-30 12:42:05 +0530

    Added Terminal Create Rules

--- app/Models/Terminal/Validator.php
+++ app/Models/Terminal/Validator.php
@@ -177,6 +177,12 @@ class Validator extends Base\Validator
         Entity::GATEWAY_SECURE_SECRET       => 'required|string',
     ];
 
+    protected static $netbankingAirtelTerminalRules = [
+        Entity::GATEWAY                     => 'required|in:netbanking_airtel',
+        Entity::GATEWAY_MERCHANT_ID         => 'required|string',
+        Entity::GATEWAY_TERMINAL_PASSWORD   => 'required|string',
+    ];
+
     protected function validateGateway($input)
     {
         if (Payment\Gateway::isValidGateway($input['gateway']) === false)
