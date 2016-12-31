3f2984bfa63db4f98d054771d4b1a47954a5601f
Ankit <ankitstarski@gmail.com>
2016-12-31 15:37:49 +0530

    signup: do not lock email on prefill (#491)

--- public/js/merchant/controllers/registerCtrl.js
+++ public/js/merchant/controllers/registerCtrl.js
@@ -21,10 +21,6 @@ app.controller('RegisterCtrl', [
     $scope.invite = $location.search().merchant_invitation;
 
     if ($location.search().email) {
-
-      // Used to make the email field in the form readonly
-      $scope.query_email = $location.search().email;
-
       $scope.data.email = $location.search().email;
 
       // XHR to save this email in a generic table
