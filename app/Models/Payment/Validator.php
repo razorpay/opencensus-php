14bff29d1a2b47878c2e67d3af73e5804af790b6
gaurav <gaurav.d@razorpay.com>
2017-01-02 14:01:33 +0530

    [payments] make contacts and email non-mandatory for payment creation

--- app/Models/Payment/Validator.php
+++ app/Models/Payment/Validator.php
@@ -13,7 +13,7 @@ use RZP\Models\Payment\Processor\Wallet;
 
 class Validator extends Base\Validator
 {
-    protected static $createRules = array(
+    protected static $createRules = [
         'amount'                  =>  'required|integer',
         'currency'                =>  'required|size:3',
         'method'                  =>  'custom',
@@ -23,8 +23,8 @@ class Validator extends Base\Validator
         'wallet'                  =>  'required_if:method,wallet|custom',
         'emi_duration'            =>  'required_if:method,emi|integer|in:3,6,9,12,18,24',
         'description'             =>  'sometimes',
-        'email'                   =>  'required|email',
-        'contact'                 =>  'required|contact_syntax',
+        'email'                   =>  'sometimes|email',
+        'contact'                 =>  'sometimes|contact_syntax',
         'signature'               =>  'sometimes',
         'notes'                   =>  'sometimes|notes',
         'notes.merchant_order_id' =>  'required_with:signature',
@@ -37,26 +37,28 @@ class Validator extends Base\Validator
         'recurring'               =>  'sometimes_if:method,card|in:0,1',
         'fee'                     =>  'sometimes|integer|max:50000000',
         'service_tax'             =>  'sometimes|integer|max:50000000',
-        '_'                       =>  'sometimes');
+        '_'                       =>  'sometimes'
+    ];
 
     protected static $captureRules = [
         'amount'        => 'required|integer',
         'currency'      => 'required|in:INR,USD',
     ];
 
-    protected static $refundRules = array(
+    protected static $refundRules = [
         'amount'        => 'sometimes|integer',
         'notes'         => 'sometimes|notes'
-    );
+    ];
 
-    protected static $createValidators = array(
+    protected static $createValidators = [
         'card_key',
         'amount',
         'bank',
         'currency',
         'description',
         'fee',
-        'contact');
+        'contact'
+    ];
 
     protected function validateMethod($attribute, $method)
     {
