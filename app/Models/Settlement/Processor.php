8d550364437a5648d2aec189de343c511021dac7
Prashanth Y.V <prashanth.yv@razorpay.com>
2017-01-04 22:14:26 +0530

    minor improvements

--- app/Models/Settlement/Processor.php
+++ app/Models/Settlement/Processor.php
@@ -155,7 +155,7 @@ class Processor extends Base\Core
 
     protected function createSettlements($channel, $schedule)
     {
-        $txns = new Base\PublicCollection;
+        // $txns = new Base\PublicCollection;
 
         if ($schedule === false)
         {
@@ -226,9 +226,9 @@ class Processor extends Base\Core
         $txnsSettled = new Base\PublicCollection;
 
         $i = 0;
-        $count = $txns->count();
+        $txnsCount = $txns->count();
 
-        while ($i < $count)
+        while ($i < $txnsCount)
         {
             // Settlement amount
             $setlAmount = $setlGatewayFee = $setlApiFee = 0;
@@ -238,10 +238,15 @@ class Processor extends Base\Core
 
             // Get merchant
             assert($txns[$i]->merchant !== null);
-            $merchant = $txns[$i]->merchant;
-            $merchantId = $txns[$i]->getMerchantId();
 
-            while (($i < $count) and
+            $merchant = $txns[$i]->merchant;
+            $merchantId = $merchant->getId();
+
+            //
+            // Since transactions are ordered by the merchant id,
+            // we can do the following operation in O(n) instead of O(n^2)
+            //
+            while (($i < $txnsCount) and
                    ($txns[$i]->getMerchantId() === $merchantId))
             {
                 $txn = $txns[$i];
@@ -259,7 +264,6 @@ class Processor extends Base\Core
             //settle only if settlement amount is more than INR 1
             if ($setlAmount <= 100)
             {
-                $setlAmount = 0;
                 continue;
             }
 
