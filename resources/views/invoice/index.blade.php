0734bef38438381ee1e35559458c6cd27f7124ee
Pranav Gupta <pranavguptarulz@gmail.com>
2017-01-06 17:05:23 +0530

    [invoice] failure card css

--- resources/views/invoice/index.blade.php
+++ resources/views/invoice/index.blade.php
@@ -16,6 +16,10 @@
     </script>
     @endif
 
+    <?php
+      $error_icon = '<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 16.538l-4.592-4.548 4.546-4.587-1.416-1.403-4.545 4.589-4.588-4.543-1.405 1.405 4.593 4.552-4.547 4.592 1.405 1.405 4.555-4.596 4.591 4.55 1.403-1.416z"/></svg>';
+    ?>
+
     <script src="https://checkout.razorpay.com/v1/checkout.js"></script>
     <style>
     body {
@@ -27,10 +31,13 @@
     path {
       fill: #6DCA00;
     }
+    #failure path {
+      fill: #e74c3c;
+    }
     h3 {
       font-weight: normal;
     }
-    #success {
+    .card {
       background: #fff;
       border-radius: 2px;
       box-shadow: 0 2px 9px rgba(0, 0, 0, 0.1);
@@ -48,16 +55,6 @@
       text-align: left;
       line-height: 24px;
     }
-    #failure {
-      color: #fff;
-      background: #EF6050;
-      border-radius: 2px;
-      box-shadow: 0 2px 9px rgba(0, 0, 0, 0.1);
-      padding: 30px;
-      margin: 30px auto;
-      width: 80%;
-      max-width: 300px;
-    }
     span {
       float: right;
     }
@@ -67,10 +64,24 @@
     .paid #success {
       display: block;
     }
+    button {
+      background-color: #4994E6;
+      color: #fff;
+      border: 0;
+      outline: none;
+      cursor: pointer;
+      font: inherit;
+      margin-top: 10px;
+      padding: 10px 20px;
+      border-radius: 2px;
+    }
+    button:active {
+      box-shadow: 0 0 0 1px rgba(0,0,0,.15) inset, 0 0 6px rgba(0,0,0,.2) inset;
+    }
     </style>
   </head>
   <body class="{{$data['status']}}">
-    <div id='success'>
+    <div id="success" class="card">
       <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-1.959 17l-4.5-4.319 1.395-1.435 3.08 2.937 7.021-7.183 1.422 1.409-8.418 8.591z"/></svg>
       <h3>Your Payment has been received</h3>
       <div id='break'>
@@ -82,9 +93,10 @@
     @if ($data['view_less'] === true)
       @if ($data['status'] !== 'paid')
         @if (isset($data['error']))
-          <div id='failure'>
-            <h2>Error</h2>
-            <p>{{ $data['error']['description'] }}</p>
+          <div id="failure" class="card">
+            {!! $error_icon !!}
+            <h2>Payment Failed</h2>
+            <p>{{ $POST['error']['description'] }}</p>
             <button>Retry</button>
           </div>
         @endif
@@ -144,7 +156,8 @@
         </script>
       @endif
     @else
-      <div id='failure'>
+      <div id="failure" class="card">
+        {!! $error_icon !!}
         <h2>Error</h2>
         <p>This invoice cannot be displayed. Please contact the merchant for assistance.</p>
       </div>
