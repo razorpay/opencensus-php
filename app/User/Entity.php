22c2e7af8c34175668976d94394ed6478618bdbe
Sumeeti Lalwani <sumeeti.lalwani@razorpay.com>
2017-01-06 20:21:45 +0530

    Adding virtual attribute confirmed to user entity and hiding confirm_… (#511)
    * Adding virtual attribute confirmed to user entity and hiding confirm_token
    
    * square brackets

--- app/User/Entity.php
+++ app/User/Entity.php
@@ -41,7 +41,7 @@ class Entity extends Base\Entity implements AuthenticatableContract, CanResetPas
      *
      * @var array
      */
-    protected $hidden = ['password', 'remember_token'];
+    protected $hidden = ['password', 'remember_token', 'confirm_token'];
 
     /**
      * The attributes that are mass assignable.
@@ -56,6 +56,8 @@ class Entity extends Base\Entity implements AuthenticatableContract, CanResetPas
      * @var array
      */
     protected static $generators = array('id','confirm_token');
+
+    protected $appends = ['confirmed'];
 
     /**
      * Generates Uuid ID
@@ -351,5 +353,10 @@ class Entity extends Base\Entity implements AuthenticatableContract, CanResetPas
     public static function getUserForConfirmation($token)
     {
         return self::where('confirm_token', '=', $token)->first();
+    }
+
+    public function getConfirmedAttribute()
+    {
+        return ($this->confirm_token === null);
     }
 }
