632e9490c4d5f38593b9cba5d99ae38ad2c91fd6
Pranav Gupta <pranavguptarulz@gmail.com>
2017-01-06 19:01:05 +0530

    loader css as partial blade view

--- resources/views/public/callback_params.blade.php
+++ resources/views/public/callback_params.blade.php
@@ -4,8 +4,10 @@
     <title>Processing, Please wait...</title>
     <meta name="viewport" content="width=device-width">
     <meta charset="utf-8">
+    @include('partials.loader')
   </head>
-  <body onload="document.forms[0].action=location.hash.slice(1);document.forms[0].submit()">
+  <body onload="document.forms[0].action=location.hash.slice(1);document.forms[0].submit">
+    <div class="loader vis" style="position:absolute;top:115px;left:50%;margin-left:-12px"></div>
     <form method="post">
       @foreach ($params as $key => $value)
         <input type="hidden" name="{{{ $key }}}" value="{{{ $value }}}">
