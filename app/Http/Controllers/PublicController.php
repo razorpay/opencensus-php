000813921c564a72d7742c351331d3eafeb27a07
Pranav Gupta <pranavguptarulz@gmail.com>
2017-01-09 18:02:53 +0530

    handle error case for automatic checkout redirection

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -20,15 +20,24 @@ class PublicController extends Controller
     public function getCallbackUrlWithParams() {
         $allParams = Request::all();
 
-        $data = array(
+        $data = [
+                    'back' => $allParams['back'],
                     'params' => json_decode($allParams['params']),
                     'url' => $allParams['url']
-                );
+                ];
 
         if (isset($allParams['razorpay_payment_id'])) {
             $data['payment_id'] = $allParams['razorpay_payment_id'];
         }
         else {
+            // fill basic error codes if it does not exist.
+            if (!isset($allParams['error']) || !isset($allParams['error']['description'])) {
+                $allParams['error'] = [
+                    'description' => 'Something went wrong'
+                ];
+            }
+            $data['error'] = $allParams['error']['description'];
+
             // passing as json encoded string
             $data['options'] = $allParams['options'];
         }
