2bd683a8e3d1d59b7c1625945df823f3d8319226
Vivek Kumar <viveklakhera@gmail.com>
2017-01-10 11:27:23 +0530

    [terminal] Fix validation logging

--- app/Models/Terminal/Validator.php
+++ app/Models/Terminal/Validator.php
@@ -277,7 +277,7 @@ class Validator extends Base\Validator
             throw new Exception\BadRequestValidationFailureException(
                 'Category provided invalid for gateway',
                 Entity::NETWORK_CATEGORY,
-                [$input]);
+                $input[Entity::NETWORK_CATEGORY]);
             }
     }
 
