ea2c47b1fd932ad0d96e842a6288714ed7ed86be
Pranav Gupta <pranavguptarulz@gmail.com>
2017-01-10 12:40:58 +0530

    added some comments

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -21,8 +21,15 @@ class PublicController extends Controller
         $allParams = Request::all();
 
         $data = [
+                    // merchant site url, to create "go back to merchant website" link
+                    // this is required because otherwise there is no escape for customer until payment is successful
+                    // this is automatically picked from previous page.
                     'back' => $allParams['back'],
+
+                    // parameters to be converted into POST. Reason we're going through this maneuver
                     'params' => json_decode($allParams['params']),
+
+                    // actual callback_url, picked from previous page form action
                     'url' => $allParams['url']
                 ];
 
@@ -30,15 +37,17 @@ class PublicController extends Controller
             $data['payment_id'] = $allParams['razorpay_payment_id'];
         }
         else {
-            // fill basic error codes if it does not exist.
+            // fill basic error codes if it error parameter does not exist.
             if (!isset($allParams['error']) || !isset($allParams['error']['description'])) {
                 $allParams['error'] = [
                     'description' => 'Something went wrong'
                 ];
             }
+
+            // just pass in printable error.
             $data['error'] = $allParams['error']['description'];
 
-            // passing as json encoded string
+            // Razorpay frontend initialization object as json string
             $data['options'] = $allParams['options'];
         }
 
