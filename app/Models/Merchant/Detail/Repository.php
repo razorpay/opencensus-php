1fdf87bcb6caacbfc0e3a2e11d375af46a361808
Ankit Agarwal <ankit.agarwal@razorpay.com>
2017-01-12 15:06:35 +0530

    [Merchant Onboarding]: Fix for merchant activation file migration error.

--- app/Models/Merchant/Detail/Repository.php
+++ app/Models/Merchant/Detail/Repository.php
@@ -28,8 +28,14 @@ class Repository extends Base\Repository
                         Entity::PROMOTER_PROOF_URL,
                         Entity::PROMOTER_PAN_URL,
                         Entity::PROMOTER_ADDRESS_URL)
-                    ->whereNotNull(Entity::BUSINESS_PROOF_URL)
-                    ->whereRaw('length(business_proof_url) > 19')
+                    ->whereRaw(
+                        'length(business_proof_url) > 19 or
+                        length(business_operation_proof_url) > 19 or
+                        length(business_pan_url) > 19 or
+                        length(address_proof_url) > 19 or
+                        length(promoter_proof_url) > 19 or
+                        length(promoter_pan_url) > 19 or
+                        length(promoter_address_url) > 19')
                     ->orderBy(Entity::MERCHANT_ID)
                     ->skip($skip)
                     ->take($count)
