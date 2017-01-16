69dc4fc26d8afddaddcd8610d47eca0f8673b6e9
Sunny Aggrawal <sunnyagg@outlook.com>
2017-01-17 02:16:36 +0530

    [icici] adds icici binary file to supported formats

--- app/Models/FileStore/Storage/Base/Bucket.php
+++ app/Models/FileStore/Storage/Base/Bucket.php
@@ -14,6 +14,7 @@ class Bucket
         Type::AIRTELMONEY_WALLET_REFUND => 'settlement_bucket',
         Type::PAYUMONEY_WALLET_REFUND   => 'settlement_bucket',
         Type::ICICI_UPI_REFUND          => 'settlement_bucket',
+        Type::ICICI_NODAL_SETTLEMENT    => 'h2h_bucket',
         Type::BATCH_INPUT               => 'batch_bucket',
         Type::BATCH_OUTPUT              => 'batch_bucket',
     ];
