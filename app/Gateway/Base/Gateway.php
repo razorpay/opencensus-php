7d6c4b1ab17f8fddba174942a1517db27681aef9
Nemo <nemo@razorpay.com>
2017-01-17 13:02:53 +0530

    [upi/icici] razorpay@eazypay is the handle for UAT

--- app/Gateway/Base/Gateway.php
+++ app/Gateway/Base/Gateway.php
@@ -489,7 +489,7 @@ class Gateway
         return $verify->getDataToTrace();
     }
 
-    public function preProcessS2SResponse($input): array
+    public function preProcessServerCallback($input): array
     {
         return $input;
     }
