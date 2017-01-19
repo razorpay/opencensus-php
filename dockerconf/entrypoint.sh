8cd0865fb475a098e72a8241dcf11be008ab0428
Joy Bhattacherjee <joy.bhattacherjee@razorpay.com>
2017-01-19 14:05:58 +0530

    docker: docker config

---
+++ dockerconf/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# wait for db to be provisioned
+sleep 60
+
+# Fix permissions
+echo  "Fix permissions"
+cd /app/ && chmod 777 -R storage
+
+# Copy config
+cp dockerconf/api.docker.conf /etc/apache2/conf.d/api.conf && \
+cp environment/.env.docker environment/.env.testing && \
+cp environment/env.sample.php environment/env.php && \
+sed -i 's/dev/testing/g' ./environment/env.php
+
+# DB Migrate
+echo  "DB Migrate"
+cd /app/ && php artisan migrate --force
+
+# start httpd
+mkdir /tmp/run
+chown 0775 /tmp/run/
+/usr/sbin/httpd -D FOREGROUND
