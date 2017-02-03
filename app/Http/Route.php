027c6da69f39a96ca5a6ec1dd5f683cadb3c078d
Sunny Aggrawal <sunnyagg@outlook.com>
2017-02-03 17:52:00 +0530

    [report] keeps only file route on slave db for ow

--- app/Http/Route.php
+++ app/Http/Route.php
@@ -936,7 +936,6 @@ final class Route
     public static $slaveRoutes = [
         // TODO: Uncomment this when slave variables issue is fixed.
         //'es_migrate_entity',
-        'reports_public_entity',
         'reports_public_entity_file',
     ];
 
