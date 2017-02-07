825fd31f0d93a1fefb9f104852858970458955b7
mayankamencherla <mayank.amencherla@razorpay.com>
2017-02-07 18:13:26 +0530

    [airtel nb] Added request into the refund request array

--- app/Gateway/Netbanking/Airtel/Gateway.php
+++ app/Gateway/Netbanking/Airtel/Gateway.php
@@ -29,9 +29,11 @@ class Gateway extends Base\Gateway
         AuthFields::AMOUNT => Base\Entity::AMOUNT
     ];
 
-    const TIME_FORMAT            = 'dmYhis';
+    const REQUEST      = 'ECOMM_REVERSAL';
 
-    const ACTION_ERROR           = 'Action not set correctly';
+    const TIME_FORMAT  = 'dmYhis';
+
+    const ACTION_ERROR = 'Action not set correctly';
 
     public function authorize(array $input)
     {
@@ -339,6 +341,7 @@ class Gateway extends Base\Gateway
             RefundFields::TRANSACTION_ID    => $gatewayPayment[Base\Entity::BANK_PAYMENT_ID],
             RefundFields::TRANSACTION_DATE  => $this->getFormattedDate($input),
             RefundFields::MERCHANT_ID       => $this->getMerchantId(),
+            RefundFields::REQUEST           => self::REQUEST,
             RefundFields::AMOUNT            => (string) $this->getFormattedAmount($input),
         ];
 
