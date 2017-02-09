0534f0b44c79f722a7ffd0257a728561d7277baa
Giridaran <giri@razorpay.com>
2017-02-09 20:50:44 +0530

    [gateway] Adds callback id checks for Netbanking Kotak and Axis.

--- app/Gateway/Netbanking/Base/Entity.php
+++ app/Gateway/Netbanking/Base/Entity.php
@@ -84,6 +84,12 @@ class Entity extends Base\Entity
 
     public function getBankPaymentId()
     {
-        return $this->getAttribute('bank_payment_id');
+        return $this->getAttribute('BANK_PAYMENT_ID');
     }
+
+    public function getIntPaymentId()
+    {
+        return $this->getAttribute('INT_PAYMENT_ID');
+    }
+
 }
--- app/Gateway/Netbanking/Kotak/Gateway.php
+++ app/Gateway/Netbanking/Kotak/Gateway.php
@@ -96,6 +96,8 @@ class Gateway extends Base\Gateway
         $gatewayPayment = $this->repo->findByPaymentIdAndActionOrFail(
             $input['payment']['id'], Action::AUTHORIZE);
 
+        $this->assertPaymentId($gatewayPayment->getIntPaymentId(), $content['TraceNumber']);
+
         $attrs['received'] = true;
         $attrs['status'] = $content['AuthorizationStatus'];
         $attrs['bank_payment_id'] = $content['BankReference'];
