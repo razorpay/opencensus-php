c51038a1c7e44e1feb2eb4757b903d76ef511276
mayankamencherla <mayank.amencherla@razorpay.com>
2017-02-09 16:08:32 +0530

    [airtel nb] Added a comment to explain hash failure test

--- tests/Functional/Gateway/Netbanking/Airtel/NetbankingAirtelGatewayTest.php
+++ tests/Functional/Gateway/Netbanking/Airtel/NetbankingAirtelGatewayTest.php
@@ -246,6 +246,7 @@ class NetbankingAirtelGatewayTest extends TestCase
         {
             if($action === 'hash')
             {
+                // Shuffling the hash so that has verification fails
                 $content['HASH'] = str_shuffle($content['HASH']);
             }
         });
