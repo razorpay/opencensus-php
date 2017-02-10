83cf553ba126b0ea957d1a39e7cb5660d9f6b4be
Harman Singh <harman28@gmail.com>
2017-02-10 19:37:17 +0530

    Removes airtel from method list for now

--- tests/Functional/Gateway/Billdesk/BilldeskGatewayTest.php
+++ tests/Functional/Gateway/Billdesk/BilldeskGatewayTest.php
@@ -186,7 +186,7 @@ class BilldeskGatewayTest extends TestCase
 
         $count = count($content['netbanking']);
 
-        $this->assertEquals(60, $count);
+        $this->assertEquals(59, $count);
     }
 
     public function testServerToServerCallback()
