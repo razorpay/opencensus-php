575f3fa8f8cd123ed9bec4d067894617f81e11f1
Harman Singh <harman28@gmail.com>
2017-02-17 12:38:52 +0530

    Adds basic tests for E-collect

---
+++ tests/Functional/Ecollect/EcollectTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace RZP\Tests\Functional\Ecollect;
+
+use RZP\Tests\Functional\TestCase;
+use RZP\Tests\Functional\RequestResponseFlowTrait;
+
+class EcollectTest extends TestCase
+{
+    use RequestResponseFlowTrait;
+
+    public function setUp()
+    {
+        $this->testDataFilePath = __DIR__.'/EcollectTestData.php';
+
+        parent::setUp();
+
+        $this->ba->appAuth();
+    }
+
+    public function testEcollectValidate()
+    {
+        $this->startTest();
+    }
+
+    public function testEcollectValidateFalse()
+    {
+        $this->startTest();
+    }
+
+    public function testEcollectValidateFailure()
+    {
+        $this->startTest();
+    }
+
+    public function testEcollectPay()
+    {
+        $this->startTest();
+    }
+
+    public function testEcollectPayFailure()
+    {
+        $this->startTest();
+    }
+}
