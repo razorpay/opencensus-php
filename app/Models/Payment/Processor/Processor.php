4fb6a9c12825aeae800940c5a5300bcf50071162
Prashanth Y.V <yvprashant@gmail.com>
2017-02-17 21:31:56 +0530

    [payments] traces all card details except card number and cvv (#2399)
    * [payments] traces all card details except card number and cvv
    
    * [trace] handles card having a string input
    
    * [cards] allows hyphens, dots and spaces in card name
    
    * [cards] adds apostrophe for name validation

--- app/Models/Payment/Processor/Processor.php
+++ app/Models/Payment/Processor/Processor.php
@@ -21,6 +21,7 @@ use RZP\Error\ErrorCode;
 use RZP\Trace\Trace;
 use RZP\Trace\TraceCode;
 use RZP\Models\Customer;
+use RZP\Models\Card;
 use RZP\Models\Transaction;
 use RZP\Models\Feature\Constants as Feature;
 
@@ -880,10 +881,19 @@ class Processor
 
     protected function tracePaymentNewRequest(array $input)
     {
-        // @note: please keep this line here. It unsets card input in case
-        // it's present
-        unset($input['card']);
+        $this->unsetSensitiveCardDetails($input);
+
         $this->trace->debug(TraceCode::PAYMENT_NEW_REQUEST, $input);
+    }
+
+    protected function unsetSensitiveCardDetails(array & $input)
+    {
+        if ((isset($input['card'])) and
+            (is_array($input['card'])))
+        {
+            unset($input['card'][Card\Entity::CVV]);
+            unset($input['card'][Card\Entity::NUMBER]);
+        }
     }
 
     protected function notifyDashboard($type, $entity)
