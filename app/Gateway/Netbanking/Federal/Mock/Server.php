a2763a2413acf884f2bb55bf27fd55fcde3ff63f
mayankamencherla <mayank.amencherla@razorpay.com>
2017-02-20 14:24:23 +0530

    [federal nb] Initial work for mock server done

--- app/Gateway/Netbanking/Federal/Mock/Server.php
+++ app/Gateway/Netbanking/Federal/Mock/Server.php
@@ -6,5 +6,38 @@ use RZP\Gateway\Netbanking\Base;
 
 class Server extends Base\Mock\Server
 {
+    public function authorize($input)
+    {
+        parent::authorize($input);
 
+        $response = $this->getCallbackResponseData($input);
+
+        $this->content($response);
+
+        $callbackUrl = $input[RequestFields::RETURN_URL] . '?' .
+                        http_build_query($response);
+
+        return $callbackUrl;
+    }
+
+    public function verify($input)
+    {
+        parent::verify($input);
+
+        $response = $this->getVerifyResponseData($request);
+
+        $this->content($response);
+
+        return $this->makeResponse($response);
+    }
+
+    protected function getCallbackResponseData($input)
+    {
+
+    }
+
+    protected function getVerifyResponseData($input)
+    {
+
+    }
 }
