653dab93ad86546b0a56e11499b9df135914920e
Sreeram Boyapati <sreeram.boyapati@razorpay.com>
2017-02-21 21:59:00 +0530

    [Freecharge] remove environment checks for now

--- app/Gateway/Wallet/Freecharge/Gateway.php
+++ app/Gateway/Wallet/Freecharge/Gateway.php
@@ -677,8 +677,7 @@ class Gateway extends Base\Gateway
 
     protected function getMerchantId($terminal)
     {
-        if (($this->mode === Mode::TEST) and
-            ($this->app['env'] === 'dev'))
+        if ($this->mode === Mode::TEST)
         {
             return $this->config['test_merchant_id'];
         }
@@ -688,22 +687,12 @@ class Gateway extends Base\Gateway
 
     protected function getDealerId($terminal)
     {
-        //
-        // We use shared terminals in testing mode
-        // So we need not pick from config
-        // In case of dev, Terminal may not be created for gateway
-        // pick dealerId from config
-        //
-        if (($this->mode === Mode::TEST) and
-            ($this->app['env'] === 'dev'))
+        if ($this->mode === Mode::TEST)
         {
             return $this->config['test_dealer_id'];
         }
 
-        if ((empty($terminal['gateway_merchant_id2']) === false))
-        {
-            return $terminal['gateway_merchant_id2'];
-        }
+        return $terminal['gateway_merchant_id2'];
     }
 
     protected function getUrlDomain()
