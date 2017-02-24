8c86c5c3ccfbba3ca504497fd6d3779fc39a380e
Amit Mahbubani <amit.mahbubani@razorpay.com>
2017-02-24 12:29:41 +0530

    Add filter on destination_id

--- public/js/admin/controllers/entitiesCtrl.js
+++ public/js/admin/controllers/entitiesCtrl.js
@@ -441,6 +441,7 @@ app.controller('EntitiesCtrl', [
       payout: {
         merchant_id: ['Merchant Id'],
         customer_id: ['Customer Id'],
+        destination_id: ['Bank Account Id'],
         method: [
           'all',
           'fund_transfer'
