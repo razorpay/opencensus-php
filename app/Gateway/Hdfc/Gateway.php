0bb752854175792a3b797f01825b83b8b552f3b3
Shashank Kumar <shashankkumar.me@gmail.com>
2017-02-24 13:19:26 +0530

    [hdfc] Merges exception handling of GatewayRequestException and GatewayTimeoutException in hdfc gateway.

--- app/Gateway/Hdfc/Gateway.php
+++ app/Gateway/Hdfc/Gateway.php
@@ -510,7 +510,7 @@ class Gateway extends Base\Gateway
             // send the request and get response
             $response['response'] = $this->postRequest($request);
         }
-        catch (Exception\GatewayTimeoutException $e)
+        catch (Exception\GatewayRequestException $e)
         {
             // For verify we should throw exception as is.
             if ($this->action === BaseAction::VERIFY)
@@ -524,19 +524,14 @@ class Gateway extends Base\Gateway
 
             $curlErrorMessage = strtolower($e->getData()['message']);
 
-            Hdfc\ErrorHandler::setTimeoutError($response, $curlErrorMessage);
-
-            return;
-        }
-        catch (Exception\GatewayRequestException $e)
-        {
-            $this->error = true;
-
-            $response['content'] = '';
-
-            $curlErrorMessage = strtolower($e->getData()['message']);
-
-            Hdfc\ErrorHandler::setRequestError($response, $curlErrorMessage);
+            if ($e instanceof Exception\GatewayTimeoutException)
+            {
+                Hdfc\ErrorHandler::setTimeoutError($response, $curlErrorMessage);
+            }
+            else
+            {
+                Hdfc\ErrorHandler::setRequestError($response, $curlErrorMessage);
+            }
 
             return;
         }
