26cfdccc306773a2b2a9d42aefb853c82f667288
Vivek <73sl4@users.noreply.github.com>
2017-02-27 13:18:48 +0530

    [maxmind] Hardcode maxmind id in the config (#2470)

--- config/applications.php
+++ config/applications.php
@@ -58,7 +58,7 @@ return array(
 
     'maxmind' => array(
         'mock'      => env('MAXMIND_MOCK', false),
-        'id'        => env('MAXMIND_ID', '113707'),
+        'id'        => '113707',
         'secret'    => env('MAXMIND_SECRET'),
         'secretv2'  => env('MAXMIND_V2_SECRET')
     ),
