ad08b2dd789d86f0d9e385b1af8225e028b0d637
Ashmeet Sidhu <ashsidhu@users.noreply.github.com>
2017-02-28 18:17:44 +0530

    Update pre sign up date check (#628)

--- public/js/services.js
+++ public/js/services.js
@@ -52,7 +52,7 @@ angular.module('app.services', [])
           }
           // if any of the fields is missing, isPreSignupDone will be false
           _isPreSignupDone = _identity.pre_signup
-            && ((_identity.created_at < 1488240000) // pre-signup is only for signup on/after 28 Feb 2017
+            && ((_identity.created_at < 1488306600) // pre-signup is only for signup on/after 01 March 2017
               || !!Object.keys(_identity.pre_signup)
                 // get all values
                 .map(function (key) {return _identity.pre_signup[key]})
