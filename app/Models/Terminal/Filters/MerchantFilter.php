d984f3ddf8a9a4dccb2394c516b3d49a083753e9
Vivek Kumar <viveklakhera@gmail.com>
2017-03-01 14:20:40 +0530

    [filter] Add check for aala first data terminal to allow rupay and maestro

--- app/Models/Terminal/Filters/MerchantFilter.php
+++ app/Models/Terminal/Filters/MerchantFilter.php
@@ -246,16 +246,28 @@ class MerchantFilter extends Terminal\Filter
 
         if (($terminal->isShared() === true) and
             ($category2 === Category::PHARMA) and
-            ($acquirer === Gateway::ACQUIRER_HDFC) and
             ($input['payment']->isMethodCardOrEmi()))
         {
-            // This check is for all the card networks which are
-            // supported by gateways from other acquirers that also
-            // have a shared terminal.
-            // Currently, we don't have a shared terminal RuPay and
-            // Maestro. We are doing a workaround using the
-            // merchant descriptor feature of FirstData
-            return false;
+            if ($acquirer === Gateway::ACQUIRER_HDFC)
+            {
+                // This check is for all the card networks which are
+                // supported by gateways from other acquirers that also
+                // have a shared terminal.
+                // Currently, we don't have a shared terminal RuPay and
+                // Maestro. We are doing a workaround using the
+                // merchant descriptor feature of FirstData
+                return false;
+            }
+            // Terminal ID for Aala first data terminal is 76lEBqibDvhOzY
+            else if ($terminal->getId() === '76lEBqibDvhOzY')
+            {
+                 $network = $input['payment']->card->getNetworkCode();
+
+                 if (in_array($network, [Network::RUPAY, Network::MESTRO], true) === false)
+                 {
+                    return false;
+                 }
+            }
         }
 
         return true;
