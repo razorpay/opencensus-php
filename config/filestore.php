5617eeafb1d382b62557f4c1d7fffa9a77545344
gaurav <gaurav.d@razorpay.com>
2017-03-02 20:22:12 +0530

    [UFH] changes in configs for filestore

--- config/filestore.php
+++ config/filestore.php
@@ -4,33 +4,31 @@ use Aws\Laravel\AwsServiceProvider;
 
 return [
     'aws' => [
-        'region'        => env('AWS_REGION', 'us-east-1'),
         'bucket_region' => env('AWS_BUCKET_REGION', 'us-east-1'),
-        'mock'          => env('AWS_S3_MOCK'),
 
         'settlement_bucket_config' => [
             'name'   => env('AWS_S3_SETTLEMENT_BUCKET'),
-            'region' => env('AWS_S3_SETTLEMENT_BUCKET_REGION', 'us-east-1')
+            'region' => env('AWS_BUCKET_REGION', 'us-east-1')
         ],
         'logo_bucket_config' => [
             'name'   => env('AWS_S3_LOGO_BUCKET'),
-            'region' => env('AWS_S3_LOGO_BUCKET_REGION', 'us-east-1')
+            'region' => env('AWS_BUCKET_REGION', 'us-east-1')
         ],
         'h2h_bucket_config' => [
             'name'   => env('AWS_H2H_BUCKET'),
-            'region' => env('AWS_H2H_BUCKET_REGION', 'us-east-1')
+            'region' => env('AWS_BUCKET_REGION', 'us-east-1')
         ],
         'activation_bucket_config' => [
             'name'   => env('AWS_ACTIVATION_BUCKET'),
-            'region' => env('AWS_ACTIVATION_BUCKET_REGION', 'us-east-1')
+            'region' => env('AWS_BUCKET_REGION', 'us-east-1')
         ],
         'invoice_bucket_config' => [
             'name'   => env('AWS_S3_INVOICES_BUCKET'),
-            'region' => env('AWS_S3_INVOICES_BUCKET_REGION', 'us-east-1')
+            'region' => env('AWS_BUCKET_REGION', 'us-east-1')
         ],
         'test_bucket_config' => [
             'name'   => env('AWS_S3_TEST_BUCKET'),
-            'region' => env('AWS_S3_TEST_BUCKET_REGION', 'us-east-1')
+            'region' => env('AWS_BUCKET_REGION', 'us-east-1')
         ],
     ],
 
