3c48c8d9bb88c1d75650a9676db379368294f313
Sunny Aggrawal <sunnyagg@outlook.com>
2017-03-04 13:28:29 +0530

    [schedule] adds merchant_schedule entity

---
+++ app/Models/Merchant/Schedule/Entity.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace RZP\Models\Merchant\Schedule;
+
+use RZP\Models\Base;
+
+class Entity extends Base\PublicEntity
+{
+    const ID                = 'id';
+    const MERCHANT_ID       = 'merchant_id';
+    const TYPE              = 'type';
+    const METHOD            = 'method';
+    const SCHEDULE_ID       = 'schedule_id';
+
+    protected $primaryKey = self::MERCHANT_ID;
+
+    protected $entity = 'merchant_schedule';
+
+    protected $fillable = [
+        self::TYPE,
+        self::METHOD,
+    ];
+
+    protected $visible = [
+        self::ID,
+        self::MERCHANT_ID,
+        self::TYPE,
+        self::METHOD,
+        self::SCHEDULE_ID,
+    ];
+
+    protected $public = [
+        self::ID,
+        self::MERCHANT_ID,
+        self::TYPE,
+        self::METHOD,
+        self::SCHEDULE_ID
+    ];
+
+    protected $defaults = [
+        self::METHOD    => null,
+    ];
+
+    public function merchant()
+    {
+        return $this->belongsTo('RZP\Models\Merchant\Entity');
+    }
+
+    public function schedule()
+    {
+        return $this->belongsTo('RZP\Models\Schedule\Entity');
+    }
+
+    public function getMethod()
+    {
+        return $this->getAttribute(self::METHOD);
+    }
+
+    public function getType()
+    {
+        return $this->getAttribute(self::TYPE);
+    }
+}
