753da41707fe0f7525c136854fda34dc29a7d774
Sunny Aggrawal <sunnyagg@outlook.com>
2017-03-12 13:45:55 +0530

    [merchant_schedule] fixes validator

--- app/Models/Merchant/Schedule/Validator.php
+++ app/Models/Merchant/Schedule/Validator.php
@@ -11,7 +11,7 @@ use RZP\Error\ErrorCode;
 class Validator extends Base\Validator
 {
     protected static $createRules = [
-        MerchantSchedule::METHOD            => 'sometimes|filled|string|max:20|custom',
+        MerchantSchedule::METHOD            => 'sometimes|string|max:20|custom',
         MerchantSchedule::SCHEDULE_ID       => 'required|alpha_dash|max:20'
     ];
 
