15de041fc9625869c044d87b887e4f46488610c6
Sunny Aggrawal <sunnyagg@outlook.com>
2017-03-14 16:55:23 +0530

    [entities] updates batch_settlement to batch_fund_transfer

--- public/js/admin/controllers/entitiesCtrl.js
+++ public/js/admin/controllers/entitiesCtrl.js
@@ -265,7 +265,14 @@ app.controller('EntitiesCtrl', [
         gateway_payment_id: ['Gateway Payment ID'],
         tdate: ['Tdate'],
       },
-      batch_settlement: {},
+      batch_fund_transfer: {
+        type: [
+          'all',
+          'settlement',
+          'payout'
+        ],
+        date: ['Date'],
+      },
       emi_plan: {
         bank: ['Bank'],
         network: ['Network']
