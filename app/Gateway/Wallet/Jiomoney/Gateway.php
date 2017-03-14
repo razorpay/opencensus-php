2b51584a6692124d9376712840a3a5741a9383df
Saurav Chowdhury <saurav1991@gmail.com>
2017-03-14 15:19:22 +0530

    [jiomoney] handles the case when Jiomoney verify returns an array of objects in response (#2612)

--- app/Gateway/Wallet/Jiomoney/Gateway.php
+++ app/Gateway/Wallet/Jiomoney/Gateway.php
@@ -582,8 +582,9 @@ class Gateway extends Base\Gateway
         }
         else if ($this->verifiedUsingCheckPaymentStatus === true)
         {
-            return $content[ResponseFields::RESPONSE][ResponseFields::CHECKPAYMENTSTATUS]
-                [ResponseFields::TXN_STATUS];
+            $gatewayData = $this->getGatewayDataFromVerifyResponse($content);
+
+            return $gatewayData[ResponseFields::TXN_STATUS];
         }
 
         return null;
@@ -603,10 +604,9 @@ class Gateway extends Base\Gateway
     {
         if ($this->verifiedUsingCheckPaymentStatus === true)
         {
-            $date = $content[ResponseFields::RESPONSE][ResponseFields::CHECKPAYMENTSTATUS]
-                        [ResponseFields::TXN_TIME_STAMP];
+            $gatewayData = $this->getGatewayDataFromVerifyResponse($content);
 
-            return $date;
+            return $gatewayData[ResponseFields::TXN_TIME_STAMP];
         }
 
         return null;
@@ -620,11 +620,30 @@ class Gateway extends Base\Gateway
         }
         else if ($this->verifiedUsingCheckPaymentStatus === true)
         {
-            return $content[ResponseFields::RESPONSE][ResponseFields::CHECKPAYMENTSTATUS]
-                [ResponseFields::JM_TRAN_REF_NO];
+            $gatewayData = $this->getGatewayDataFromVerifyResponse($content);
+
+            return $gatewayData[ResponseFields::JM_TRAN_REF_NO];
         }
 
         return null;
+    }
+
+    /**
+     * We sometimes get an array of nested JSON objects from the Jiomoney verify API
+     * This method returns the first object from the array
+     * @param  array  $content verify response content
+     * @return array
+     */
+    protected function getGatewayDataFromVerifyResponse(array $content)
+    {
+        $checkPaymentStatusData = $content[ResponseFields::RESPONSE][ResponseFields::CHECKPAYMENTSTATUS];
+
+        if (is_associative_array($checkPaymentStatusData) === false)
+        {
+            $checkPaymentStatusData = array_shift($checkPaymentStatusData);
+        }
+
+        return $checkPaymentStatusData;
     }
 
     protected function getCheckPaymentStatusRequest(array $input)
