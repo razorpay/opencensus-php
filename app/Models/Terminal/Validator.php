30fc0926b4eeae14f9d20d6981a4c3562c1df4bd
mayankamencherla <mayank.amencherla@razorpay.com>
2017-03-17 14:10:23 +0530

    [iciic prod] Changed SPID back to required

--- app/Models/Terminal/Validator.php
+++ app/Models/Terminal/Validator.php
@@ -196,7 +196,7 @@ class Validator extends Base\Validator
     protected static $netbankingIciciTerminalRules = [
         Entity::GATEWAY              => 'required|in:netbanking_icici',
         Entity::GATEWAY_MERCHANT_ID  => 'required|string',
-        Entity::GATEWAY_MERCHANT_ID2 => 'sometimes|string',
+        Entity::GATEWAY_MERCHANT_ID2 => 'required|string',
     ];
 
     protected static $walletJiomoneyTerminalRules = [
