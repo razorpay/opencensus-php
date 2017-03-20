e04e1792d24d4476cf444c097838cf437df77584
gappan <azhagappan.sivaraman@razorpay.com>
2017-03-20 16:00:56 +0530

    docker: selenium layer added for dev

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 if [ -z selenium-server.jar ]
 then curl https://selenium-release.storage.googleapis.com/2.53/selenium-server-standalone-2.53.1.jar -o selenium-server.jar
 fi
-docker build -t pronav/dash .
+docker-compose -f docker-compose.dev.yml up --build 
