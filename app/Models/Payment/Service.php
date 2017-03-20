32be0969436e9e0f651d429b4298a226901b0a0a
Sunny Aggrawal <sunnyagg@outlook.com>
2017-03-20 13:22:11 +0530

    [transactions] fixes transaction fetch route for payment

--- app/Models/Payment/Service.php
+++ app/Models/Payment/Service.php
@@ -369,7 +369,7 @@ class Service extends Base\Service
     {
         $payment = $this->repo->payment->findByPublicIdAndMerchant($id, $this->merchant);
 
-        $transaction = $this->repo->transaction->findByEntityId($id, $this->merchant, true);
+        $transaction = $this->repo->transaction->findByEntityId($payment->getId(), $this->merchant, true);
 
         return $transaction->toArrayPublic();
     }
