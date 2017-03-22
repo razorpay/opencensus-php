15de54a5587e947f1c5634c4642b461e2e15b6b6
Rishabh <rishabh.pugalia@gmail.com>
2017-03-22 19:55:34 +0530

    Revert "Allow activation form edits from admin side"

--- gulpfile.js
+++ gulpfile.js
@@ -131,8 +131,6 @@ const concatJs = lazypipe()
     'js/generated/admin.js': [
       'public/js/admin/**/*.js',
       'public/js/*.js',
-      // We share the merchant activation controller code
-      'public/js/merchant/controllers/activationCtrl.js',
       'node_modules/moment/min/moment.min.js'
     ]
   });
