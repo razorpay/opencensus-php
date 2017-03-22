d3b6502391dd97f28aca5265cbbbc48de24d348f
mayankamencherla <mayank.amencherla@razorpay.com>
2017-03-22 17:05:56 +0530

    [federal nb] Minor bug fix

--- app/Constants/MailTags.php
+++ app/Constants/MailTags.php
@@ -19,6 +19,7 @@ class MailTags
     const HDFC_NETBANKING_REFUNDS_MAIL    = 'hdfc_netbanking_refunds_mail';
     const AXIS_NETBANKING_REFUNDS_MAIL    = 'axis_netbanking_refunds_mail';
     const FEDERAL_NETBANKING_REFUNDS_MAIL = 'axis_netbanking_refunds_mail';
+    const ICICI_NETBANKING_REFUNDS_MAIL   = 'icici_netbanking_refunds_mail';
 
     const AIRTEL_MONEY_REFUNDS_MAIL       = 'airtel_money_refunds_mail';
     const PAYU_MONEY_REFUNDS_MAIL         = 'payu_money_refunds_mail';
