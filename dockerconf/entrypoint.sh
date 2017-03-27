7d872e805e10c8b363861240d233a34de786a106
Giridaran <giri@razorpay.com>
2017-03-27 19:30:15 +0530

    [f_containerization] Adds db migrate step to entrypoint. Fixes docker port for docker-compose. Adds v1 to .env.example.

--- dockerconf/entrypoint.sh
+++ dockerconf/entrypoint.sh
@@ -20,6 +20,10 @@ echo "$date Memory Limit"
 echo 'memory_limit = 128M' | sed -E 's/memory_limit\s*=\s*\d*M/memory_limit = 3048M/g' /etc/php7/php.ini > /tmp/php.ini
 mv /tmp/php.ini /etc/php7/php.ini
 
+echo "$(date) DB Migrate"
+echo "$(date) Seeding live db"
+cd /app && \
+php artisan migrate --seed
 
 echo "$date Starting Nginx"
 export PATH=$PATH:/app/
@@ -29,5 +33,3 @@ mkdir /tmp/run
 chown 0775 /tmp/run/
 /usr/sbin/php-fpm7
 /usr/sbin/nginx -g 'daemon off;'
-
-
