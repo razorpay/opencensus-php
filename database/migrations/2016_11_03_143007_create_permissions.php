0256aa86b747dda265f235622ba54720a22c1dc7
Rishabh <rishabh.pugalia@gmail.com>
2017-03-27 18:05:48 +0530

    permission description should be nullable

--- database/migrations/2016_11_03_143007_create_permissions.php
+++ database/migrations/2016_11_03_143007_create_permissions.php
@@ -26,7 +26,8 @@ class CreatePermissions extends Migration
             $table->string(Permission::CATEGORY, 250)
                   ->nullable();
 
-            $table->string(Permission::DESCRIPTION, 250);
+            $table->string(Permission::DESCRIPTION, 250)
+                  ->nullable();
 
             $table->integer(Permission::CREATED_AT);
             $table->integer(Permission::UPDATED_AT);
