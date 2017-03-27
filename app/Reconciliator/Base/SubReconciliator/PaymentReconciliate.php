0b3f98ae5ea46a4e490914c8441bab7b70b757f6
mayankamencherla <mayank.amencherla@razorpay.com>
2017-03-27 18:35:47 +0530

    [icici recon] Fixed minor bug in constant name

--- app/Reconciliator/Base/SubReconciliator/PaymentReconciliate.php
+++ app/Reconciliator/Base/SubReconciliator/PaymentReconciliate.php
@@ -25,7 +25,7 @@ class PaymentReconciliate extends Foundation\SubReconciliate
     const GATEWAY_FEES_ABSENT_GATEWAYS = [
         Orchestrator::KOTAK,
         Orchestrator::NETBANKING_AXIS,
-        Orchestrator::NETBANKING_ICIC,
+        Orchestrator::NETBANKING_ICICI,
         Orchestrator::JIOMONEY
     ];
 
