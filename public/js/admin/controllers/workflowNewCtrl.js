81ccc592f076db4377023db34b288137edc504b5
Ashmeet Sidhu <ashmeet.sidhu7@gmail.com>
2017-03-27 19:34:22 +0530

    add select2 in workflows

--- public/js/admin/controllers/workflowNewCtrl.js
+++ public/js/admin/controllers/workflowNewCtrl.js
@@ -10,6 +10,59 @@ app.controller('WorkflowNewCtrl', [
   'admin',
   function ($scope, $http, alertsFactory, $state, $modal, $stateParams, admin) {
     $scope.new_checker = null;
+    $scope.selectedWorkflows = {};
+    $scope.workflows = [{
+      id: 0,
+      name: "Initiate Settlement"
+    }, {
+      id: 1,
+      name: "Upload Settlement Reconciliation (UTR)"
+    }, {
+      id: 2,
+      name: "Upload Reconciliation File (Payment/Refund)"
+    }, {
+      id: 3,
+      name: "Add IIN Rule"
+    }, {
+      id: 4,
+      name: "Add EMI Plan"
+    }, {
+      id: 5,
+      name: "Verify Payment"
+    }, {
+      id: 6,
+      name: "Archive Merchant"
+    }, {
+      id: 7,
+      name: "Confirm User"
+    }, {
+      id: 8,
+      name: "Verify All Payments"
+    }, {
+      id: 9,
+      name: "Generate Refunds Excel (Netbanking)"
+    }, {
+      id: 10,
+      name: "Generate Beneficiary File"
+    }, {
+      id: 11,
+      name: "Download Beneficiary File"
+    }, {
+      id: 12,
+      name: "Authorize Failed Payment"
+    }, {
+      id: 13,
+      name: "Send Merchant Newsletter"
+    }, {
+      id: 14,
+      name: "Create Schedule"
+    }, {
+      id: 15,
+      name: "Trigger Dummy Error"
+    }, {
+      id: 16,
+      name: "Make API Call"
+    }];
     $scope.checkers = [
     {
       id: 0,
@@ -21,7 +74,13 @@ app.controller('WorkflowNewCtrl', [
       id: 2,
       name: 'Chetty'
     }];
-    $('.select2').select2({
+    var $workflowSelect = $('.workflow-select2').select2({
+    });
+    $workflowSelect.on("select2:select", function (e) {
+      $scope.selectedWorkflows[e.params.data.id] = (e.params.data.text);
+      console.log($scope.selectedWorkflows);
+      $workflowSelect.val(null).trigger("change");
+      console.log(e)
     });
   }
 ]);
