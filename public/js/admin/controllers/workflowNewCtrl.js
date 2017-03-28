00301bd6b1568aafd01ee7a042ac99d99f56d4f8
Ashmeet Sidhu <ashmeet.sidhu7@gmail.com>
2017-03-28 18:53:52 +0530

    select2 data

--- public/js/admin/controllers/workflowNewCtrl.js
+++ public/js/admin/controllers/workflowNewCtrl.js
@@ -10,59 +10,27 @@ app.controller('WorkflowNewCtrl', [
   'admin',
   function ($scope, $http, alertsFactory, $state, $modal, $stateParams, admin) {
     $scope.new_checker = null;
-    $scope.selectedWorkflows = {};
-    $scope.workflows = [{
-      id: 0,
-      name: "Initiate Settlement"
-    }, {
-      id: 1,
-      name: "Upload Settlement Reconciliation (UTR)"
-    }, {
-      id: 2,
-      name: "Upload Reconciliation File (Payment/Refund)"
-    }, {
-      id: 3,
-      name: "Add IIN Rule"
-    }, {
-      id: 4,
-      name: "Add EMI Plan"
-    }, {
-      id: 5,
-      name: "Verify Payment"
-    }, {
-      id: 6,
-      name: "Archive Merchant"
-    }, {
-      id: 7,
-      name: "Confirm User"
-    }, {
-      id: 8,
-      name: "Verify All Payments"
-    }, {
-      id: 9,
-      name: "Generate Refunds Excel (Netbanking)"
-    }, {
-      id: 10,
-      name: "Generate Beneficiary File"
-    }, {
-      id: 11,
-      name: "Download Beneficiary File"
-    }, {
-      id: 12,
-      name: "Authorize Failed Payment"
-    }, {
-      id: 13,
-      name: "Send Merchant Newsletter"
-    }, {
-      id: 14,
-      name: "Create Schedule"
-    }, {
-      id: 15,
-      name: "Trigger Dummy Error"
-    }, {
-      id: 16,
-      name: "Make API Call"
-    }];
+    $scope.workflows = {
+      0: "Initiate Settlement",
+      1: "Upload Settlement Reconciliation (UTR)",
+      2: "Upload Reconciliation File (Payment/Refund)",
+      3: "Add IIN Rule",
+      4: "Add EMI Plan",
+      5: "Verify Payment",
+      6: "Archive Merchant",
+      7: "Confirm User",
+      8: "Verify All Payments",
+      9: "Generate Refunds Excel (Netbanking)",
+      10: "Generate Beneficiary File",
+      11: "Download Beneficiary File",
+      12: "Authorize Failed Payment",
+      13: "Send Merchant Newsletter",
+      14: "Create Schedule",
+      15: "Trigger Dummy Error",
+      16: "Make API Call",
+    }
+    $scope.workflowOptions = Object.keys($scope.workflows);
+    $scope.selectedWorkflows = [];
     $scope.checkers = [
     {
       id: 0,
@@ -75,12 +43,25 @@ app.controller('WorkflowNewCtrl', [
       name: 'Chetty'
     }];
     var $workflowSelect = $('.workflow-select2').select2({
+      theme: 'classic',  
+      placeholder: 'Select an action',
+
     });
     $workflowSelect.on("select2:select", function (e) {
-      $scope.selectedWorkflows[e.params.data.id] = (e.params.data.text);
-      console.log($scope.selectedWorkflows);
+      var id = e.params.data.id;
+      $scope.selectedWorkflows.push(id);
+      var index = $scope.workflowOptions.indexOf(id);
+      if (index > -1) {
+        $scope.workflowOptions.splice(index, 1);
+      }
       $workflowSelect.val(null).trigger("change");
-      console.log(e)
+      console.log(e);
     });
+    $scope.deselectWorkflow = function (id) {
+      var workflow = $scope.selectedWorkflows[id];
+      $scope.workflows.push(workflow);
+      console.log($scope.workflows)
+      delete $scope.selectedWorkflows[id];
+    }
   }
 ]);
