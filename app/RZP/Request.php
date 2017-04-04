24c1719c12e20b6b4034c441ee069211b75041b8
Nemo <me@captnemo.in>
2017-04-04 23:49:19 +0530

    Downgrade razorpay php (#753)
    * Revert "[rzp] Fixes the baseUrl which was made protected (#750)"
    
    This reverts commit f7b51a14f7921e1fa0aba228fd0f6275628f5688.
    
    * [deps] Downgrades razorpay-php to 1.2.8

--- app/RZP/Request.php
+++ app/RZP/Request.php
@@ -20,7 +20,7 @@ class Request extends BaseRequest
 
     public function request($method, $url, $data = null)
     {
-        $url = Api::getFullUrl($url);
+        $url = Api::$baseUrl . $url;
 
         if ($data === null)
             $data = array();
@@ -36,7 +36,7 @@ class Request extends BaseRequest
 
     public function rawRequest($method, $url, $data = null)
     {
-        $url = Api::getFullUrl($url);
+        $url = Api::$baseUrl . $url;
 
         if ($data === null)
             $data = array();
