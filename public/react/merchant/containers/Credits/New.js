c69a689aa7126b8a7919de74fd65a7f4d2695740
Pranav Gupta <pranavguptarulz@gmail.com>
2017-04-04 17:49:53 +0530

    minor css changes

--- public/react/merchant/containers/Credits/New.js
+++ public/react/merchant/containers/Credits/New.js
@@ -171,7 +171,7 @@ export default class CreditsList extends Component {
         </div>
 
         <div class="wrapper-md profile-wrapper">
-          <div class="panel panel-default panel-form col-sm-10 col-sm-offset-1">
+          <div class="panel panel-default panel-form col-sm-8 col-sm-offset-2">
             <div class="panel-heading m-t m-b">
               Your Credits
 
