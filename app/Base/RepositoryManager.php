facde6f7864a9365d92b9f4012ebf67dec4e80e5
Sreeram Boyapati <sreeram.boyapati@razorpay.com>
2017-04-05 16:26:29 +0530

    [Repository] add detach API to repo manager

--- app/Base/RepositoryManager.php
+++ app/Base/RepositoryManager.php
@@ -54,6 +54,13 @@ class RepositoryManager extends \Illuminate\Support\Manager
         return $repo->sync($entity, $relation, $ids);
     }
 
+    public function detach($entity, $relation, $ids = [])
+    {
+        $repo = $this->getRepositoryClassFromObject($entity);
+
+        return $repo->detach($entity, $relation, $ids);
+    }
+
     public function attach($entity, $relation, $id, array $attributes = [], $touch = true)
     {
         $repo = $this->getRepositoryClassFromObject($entity);
