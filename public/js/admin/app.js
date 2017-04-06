85bae3c969b65abb547c8df7bfff11b91d68415f
Rishabh <rishabh.pugalia@gmail.com>
2017-04-06 21:16:21 +0530

    refac workflows

--- public/js/admin/app.js
+++ public/js/admin/app.js
@@ -228,8 +228,14 @@ var app = angular.module('app', [
     }).state('app.workflows.feed.detail', {
       url: '/:action_id',
       templateUrl: 'tpl/admin/app_workflow_feed.html'
+    }).state('app.workflows.list', {
+      url: '/list',
+      templateUrl: 'tpl/admin/app_workflow_list.html'
     }).state('app.workflows.new', {
       url: '/new',
+      templateUrl: 'tpl/admin/app_workflow_new.html'
+    }).state('app.workflows.edit', {
+      url: '/:id/edit',
       templateUrl: 'tpl/admin/app_workflow_new.html'
     })
 
