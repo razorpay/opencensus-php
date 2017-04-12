313395182cb4536446642175c22f692f020cf869
Ria Garg <ria.garg@razorpay.com>
2017-04-12 15:27:10 +0530

    corrected live  refund url

--- app/Gateway/Upi/Icici/Url.php
+++ app/Gateway/Upi/Icici/Url.php
@@ -15,7 +15,7 @@ class Url
      */
     const LIVE_AUTHORIZE    = '/api/MerchantAPI/UPI/v1/CollectPay';
     const LIVE_VERIFY       = '/api/MerchantAPI/UPI/v1/TransactionStatus';
-    const LIVE_REFUND       = '/api/MerchantAPI/UPI/v1/refund';
+    const LIVE_REFUND       = '/api/MerchantAPI/UPI/v1/Refund/%s';
 
     const TEST_AUTHORIZE    = '/newCollectPay';
     const TEST_VERIFY       = '/newTransactionStatus';
