0848fa0f1e08f4e7d7cac4162751ca2d002d464c
Priyanshu Chhazed <priyanshu.chhazed@razorpay.com>
2017-04-12 17:46:45 +0530

    [Settlement] Fixed tests

--- tests/Functional/Gateway/Kotak/ReconciliationTest.php
+++ tests/Functional/Gateway/Kotak/ReconciliationTest.php
@@ -117,23 +117,6 @@ class ReconciliationTest extends TestCase
         $this->assertEquals('settlement', $txn['type']);
         $this->assertNotNull($txn['reconciled_at']);
 
-        // Check that if a row with same payment_ref_id is processed again in a
-        // later recon file, failure stats should remain same
-
-        // Generate settlement reconciliation file
-        $generateFailedReconciliations = true;
-        $setlReconciliationFile = $this->generateSetlReconciliationFile(
-            $setlFile,
-            $generateFailedReconciliations);
-
-        // Reconcile settlements again
-        $data = $this->reconcileSettlements($setlReconciliationFile);
-
-        // Validate batch fund transfer entity
-        $batch = $this->getLastEntity('batch_fund_transfer', true);
-        $this->assertEquals(1, $batch['total_failed_count']);
-        $this->assertEquals(4385000, $batch['failed_amount']);
-
         // Resetting time
         Carbon::setTestNow();
 
