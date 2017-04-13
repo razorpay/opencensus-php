799280d8637b816490011a8fdf3fe24d26eaf72e
gaurav <gaurav.d@razorpay.com>
2017-04-13 23:21:20 +0530

    [Queue] move test mode queue to non-default queue (#2896)
    * [Queue] move test mode queue to non-default queue
    
    * [Queue] fixed mode bug
    
    * [Queue] changed config to nested array for enabling reading via dot
    
    * [Queue] fixed env variable in config/queue.php
    
    * [Queue] move common code to trait
    
    * [Queue] removed unused code
    
    * [Queue] Enable non-default queue for test mode only
    
    * [Queue] move dispatch to central place
    
    * [Queue] minor fix
    
    * [Queue] added type hinting

--- app/Listeners/ApiEventSubscriber.php
+++ app/Listeners/ApiEventSubscriber.php
@@ -2,7 +2,6 @@
 
 namespace RZP\Listeners;
 
-use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Events\Dispatcher;
 
 use App;
@@ -12,12 +11,13 @@ use RZP\Models\Base;
 use RZP\Models\Event;
 use RZP\Models\Payment;
 use RZP\Models\Invoice;
+use RZP\Jobs\DispatchRouter;
 use RZP\Models\Merchant\Webhook\Event as WebhookEvent;
 
 class ApiEventSubscriber extends Base\Core
 {
     // Used to push jobs to queues
-    use DispatchesJobs;
+    use DispatchRouter;
 
     /**
      * Event being fired
@@ -261,7 +261,11 @@ class ApiEventSubscriber extends Base\Core
     {
         $data = $this->getWebhookData($payload);
 
-        $this->dispatch(new Webhook($data));
+        $job = new Webhook($data);
+
+        $queueConfig = [Constants\Jobs::WEBHOOK, $this->getMode(), $this->event];
+
+        $this->dispatchOn($job, $queueConfig);
     }
 
     protected function getWebhookData($payload)
