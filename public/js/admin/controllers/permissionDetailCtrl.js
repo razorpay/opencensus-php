03b4f018ae74bbfb62df1a14cc81f82a6f29a01b
aseemgupta <aseemgupta2625@gmail.com>
2017-04-17 16:14:36 +0530

    Add org list. Allow user to add/edit org list in premissions.

--- public/js/admin/controllers/permissionDetailCtrl.js
+++ public/js/admin/controllers/permissionDetailCtrl.js
@@ -5,7 +5,41 @@ app.controller('PermissionDetailCtrl', [
   '$stateParams',
   '$state',
   'transformRequestAsFormPost',
-  function ($scope, $http, alertsFactory, $stateParams, $state, transformRequestAsFormPost) {
+  'utils',
+  function ($scope, $http, alertsFactory, $stateParams, $state, transformRequestAsFormPost, utils) {
+    $scope.localOrgs = {};
+    $scope.RZid = 'org_100000razorpay';
+
+    // Map and merge the org data to local org structure
+    function mapOrgsToLocal(selOrgList) {
+      var tmpAllOrgs = {};
+      var tmpSelOrgs = {};
+
+      // Prepare all org list
+      if ($scope.organizations && Object.keys($scope.organizations).length) {
+        tmpAllOrgs = $scope.organizations.reduce(function(result, item) {
+          result[item.id] = false;
+          return result;
+        }, {});
+      }
+
+      // Prepare previously selected org list
+      if (selOrgList && Object.keys(selOrgList).length) {
+        tmpSelOrgs = selOrgList.reduce(function(result, item) {
+
+          result[item.id] = true;
+          return result;
+        }, {});
+
+        // True only if previously selected
+        if (tmpSelOrgs.hasOwnProperty($scope.RZid)) {
+          $scope.freezeRZ = true;
+        }
+      }
+
+      $scope.localOrgs = utils.concatObj(tmpAllOrgs, tmpSelOrgs, $scope.localOrgs); // Don't change order for tmpAllOrgs, tmpSelOrgs
+    }
+
     $scope.fetchPermission = function (id) {
       var data = {
         route_name: 'permission_get',
@@ -22,15 +56,68 @@ app.controller('PermissionDetailCtrl', [
         if (data.success) {
           var permission = data.data;
           $scope.permission = permission;
+          mapOrgsToLocal($scope.permission.orgs); // Update the list view with previously selected organizations
         }
       });
     };
+
+    // (De)Select all oraganizations
+    $scope.toggleSelAll = function() {
+      $scope.select_all = !$scope.select_all;
+      if (!$scope.select_all) {
+        $scope.organizations.map(function(perm) {
+          if (perm.id === $scope.RZid && $scope.freezeRZ) {
+            return;
+          }
+          $scope.localOrgs[perm.id] = false;
+        });
+      } else {
+        $scope.organizations.map(function(perm) {
+          $scope.localOrgs[perm.id] = true;
+        });
+      }
+    };
+
+    // Deselect in view
+    $scope.updateSelAllTag = function(id)   {
+      if (!$scope.localOrgs[id]) {
+        $scope.select_all = false;
+      }
+    };
+
+    // Fetch entire list of org
+    $scope.fetchOrgs = function () {
+      var request = $http.get('/admin/generic', {
+        params: {
+          route_name: 'org_get_multiple'
+        }
+      });
+
+      request.success(function (data) {
+        if (data.success) {
+          $scope.organizations = data.data.items;
+          mapOrgsToLocal(); // add all orgs in list view
+        }
+      });
+    };
+
+    $scope.fetchOrgs();
 
     if ($stateParams.id) {
       $scope.fetchPermission($stateParams.id)
     }
 
     $scope.save = function (permission) {
+      // dont trust user check
+      if($scope.freezeRZ) {
+        $scope.localOrgs[$scope.RZid] = true;
+      }
+
+      // Remove keys with false value
+      permission.orgs = Object.keys($scope.localOrgs).filter(function(ele){
+        return $scope.localOrgs[ele];
+      });
+
       // edit
       if (permission.id) {
         var data = {
