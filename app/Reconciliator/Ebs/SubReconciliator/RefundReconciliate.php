29efd7f2d3dd2991b0f60eec5b7fcb82c5c05f2a
namrata <namrata.chaudhary@razorpay.com>
2017-04-21 12:36:29 +0530

    [ebs_recon] Refund recon file changes

--- app/Reconciliator/Ebs/SubReconciliator/RefundReconciliate.php
+++ app/Reconciliator/Ebs/SubReconciliator/RefundReconciliate.php
@@ -9,12 +9,25 @@ class RefundReconciliate extends Base\RefundReconciliate
     // ----- Row header names -----
     const COLUMN_REFUND_ID          = 'merchant_ref_no';
     const COLUMN_REFUND_AMOUNT      = 'refunded';
-    const COLUMN_GATEWAY_PAYMENT_ID = 'paymentid';
 
     protected function getRefundId(array $row)
     {
         $refundId = $row[self::COLUMN_REFUND_ID];
 
         return $refundId;
+    }
+
+    protected function getPaymentId(array $row)
+    {
+        $paymentId = $this->getRefundId($row);
+
+        return $paymentId;
+    }
+
+    protected function getRefundAmount(array $row)
+    {
+        $paymentAmount = floatval($row[self::COLUMN_REFUND_AMOUNT]) * 100;
+
+        return abs($paymentAmount);
     }
 }
