c9e398872e9645a2d9deaa0ad7a90f7383c5aeb9
Vivek <ccrims0n@users.noreply.github.com>
2017-04-21 15:07:49 +0530

    [cybersource] Change auth error from gateway error to bad request (#2983)
    * [cybersource] Change auth error from gateway error to bad request
    
    * [cybersource] Fix test cases

--- app/Error/PublicErrorDescription.php
+++ app/Error/PublicErrorDescription.php
@@ -54,6 +54,7 @@ class PublicErrorDescription
     const BAD_REQUEST_PAYMENT_CARD_NOT_PROVIDED                                 = 'Payment Exception: Card not provided';
     const BAD_REQUEST_PAYMENT_CARD_CVV_NOT_PROVIDED                             = 'Payment Exception: Card cvv not provided';
     const BAD_REQUEST_PAYMENT_CARD_INVALID_CVV                                  = 'Payment failed due to incorrect card CVV';
+    const BAD_REQUEST_PAYMENT_CARD_HOLDER_AUTHENTICATION_FAILED                 = 'Payment failed because cardholder couldn\'t be authenticated';
     const BAD_REQUEST_PAYMENT_NET_BANKING_NOT_ENABLED                           = 'Net banking is not enabled for the merchant';
     const BAD_REQUEST_PAYMENT_WALLET_NOT_SUPPORTED                              = 'Wallet is not supported';
     const BAD_REQUEST_PAYMENT_WALLET_NOT_PROVIDED                               = 'Wallet is not provided';
