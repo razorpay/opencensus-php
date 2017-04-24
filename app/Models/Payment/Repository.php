ac63125862c5d002334ddfea5e945fc3397de3dd
Harman Singh <harman28@gmail.com>
2017-04-24 08:55:57 +0530

    [verify] Updates sample verify query

--- app/Models/Payment/Repository.php
+++ app/Models/Payment/Repository.php
@@ -323,11 +323,7 @@ class Repository extends Base\Repository
         // Sample Query
         // SELECT *
         // FROM   `payments`
-        // WHERE  `gateway` IN ( 'axis_migs', 'billdesk', 'ebs', 'mobikwik',
-        //                      'paytm', 'hdfc', 'amex', 'netbanking_hdfc',
-        //                      'netbanking_kotak', 'wallet_payzapp', 'first_data',
-        //                      'cybersource', 'wallet_payumoney', 'wallet_airtelmoney',
-        //                      'wallet_olamoney', 'wallet_freecharge' )
+        // WHERE  `gateway` NOT IN ( 'wallet_openwallet' )
         //        AND `status` = 'failed'
         //        AND ( ( `verify_bucket` = '0' AND `created_at` < '1478023148' )
         //              OR ( `verify_bucket` = '1' AND `created_at` < '1478022368' )
