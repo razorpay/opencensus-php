23bdfb8569910036a706486bdb14de7aa1c3699a
Ankit <ankitstarski@gmail.com>
2017-04-24 20:14:36 +0530

    use single quotes

--- public/react/merchant/models/Base.js
+++ public/react/merchant/models/Base.js
@@ -1,4 +1,4 @@
-import { objectDiff, isBlank } from "rzp/utils/rzp-utils";
+import { objectDiff, isBlank } from 'rzp/utils/rzp-utils';
 
 /*
   - Model-based approach makes it more handy to handle the serialization/deserialization of crud calls.
@@ -15,7 +15,7 @@ import { objectDiff, isBlank } from "rzp/utils/rzp-utils";
 */
 
 export default class BaseModel {
-  resourceIdField = "id";
+  resourceIdField = 'id';
 
   constructor(props = {}) {
     Object.assign(this, props);
@@ -34,7 +34,7 @@ export default class BaseModel {
   }
 
   getResourceMethod() {
-    return this.isNew ? "post" : "patch";
+    return this.isNew ? 'post' : 'patch';
   }
 
   getResourceUrlAndMethod() {
@@ -45,7 +45,7 @@ export default class BaseModel {
     Returns JSON payload with only properties specified in `resourceFields`
   */
   serialize() {
-    let fields = typeof this.resourceFields === "function"
+    let fields = typeof this.resourceFields === 'function'
       ? this.resourceFields()
       : this.resourceFields;
     let serializedModel = {};
@@ -55,7 +55,7 @@ export default class BaseModel {
       serializedModel[prop] = this.serializeProperty(prop);
     }
 
-    if (this.getResourceMethod() === "put") {
+    if (this.getResourceMethod() === 'put') {
       return serializedModel;
     }
 
@@ -107,6 +107,6 @@ export default class BaseModel {
   didDeserialize() {}
 
   toString() {
-    return "model";
+    return 'model';
   }
 }
--- public/react/rzp/ui/CheckIcon.js
+++ public/react/rzp/ui/CheckIcon.js
@@ -1,7 +1,7 @@
 export default ({ value }) => {
-	var className = value
-		? ' fa fa-check text-success'
-		: ' fa fa-times text-danger';
+  var className = value
+    ? ' fa fa-check text-success'
+    : ' fa fa-times text-danger';
 
-	return <i class={className} />;
+  return <i class={className} />;
 };
