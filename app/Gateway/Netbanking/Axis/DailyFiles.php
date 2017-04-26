943cde42f5ecca412c262ebd692b837f8d1562c8
gaurav <gaurav.d@razorpay.com>
2017-04-26 19:47:57 +0530

    [DailyFile] set filename also when sending e-mail

--- app/Gateway/Netbanking/Axis/DailyFiles.php
+++ app/Gateway/Netbanking/Axis/DailyFiles.php
@@ -28,20 +28,30 @@ class DailyFiles extends Base\DailyFiles
             'total'   => $claimData['count'] + $refundData['count'],
         ];
 
+        $claimsFile = [
+            'url'  => $claimData['signed_url'],
+            'name' => basename($claimData['local_file_path']),
+        ];
+
+        $refundsFile = [
+            'url'  => $refundData['signed_url'],
+            'name' => basename($refundData['local_file_path']),
+        ];
+
         // Send the mail only when there is at least 1 claim or refund
         if ($amount['claims'] + $amount['refunds'] > 0)
         {
             $this->sendMail(
                 $amount,
-                $claimData['signed_url'],
-                $refundData['signed_url'],
+                $claimsFile,
+                $refundsFile,
                 $count,
                 $email);
         }
 
         return [
             'refunds' => $refundData['local_file_path'],
-            'claims' => $claimData['local_file_path']
+            'claims'  => $claimData['local_file_path']
         ];
     }
 
@@ -76,12 +86,14 @@ class DailyFiles extends Base\DailyFiles
 
             if (empty($data['claimsFile']) === false)
             {
-                $message->attach($data['claimsFile']);
+                $message->attach($data['claimsFile']['url'])
+                        ->setFilename($data['claimsFile']['name']);;
             }
 
             if (empty($data['refundsFile']) === false)
             {
-                $message->attach($data['refundsFile']);
+                $message->attach($data['refundsFile']['url'])
+                        ->setFilename($data['refundsFile']['name']);;
             }
 
             $headers = $message->getHeaders();
