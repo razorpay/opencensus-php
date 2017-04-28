3914e3be02593396b51cdf9e6a8dbaecbee09b4d
aseemgupta <aseemgupta2625@gmail.com>
2017-04-28 18:38:38 +0530

    [Feature]: Workflow enable checkbox for all orgs in permissions view

--- public/js/admin/controllers/permissionDetailCtrl.js
+++ public/js/admin/controllers/permissionDetailCtrl.js
@@ -8,6 +8,7 @@ app.controller('PermissionDetailCtrl', [
   function($scope, $http, alertsFactory, $stateParams, $state, utils) {
     $scope.select_all = false;
     $scope.selected_organizations = {};
+    $scope.workflow_orgs = {}; // Workflow enabled/disabled field corresponding to each org
 
     // Fetch orgs having permissions
     $scope.fetchPermission = function(id) {
@@ -30,6 +31,11 @@ app.controller('PermissionDetailCtrl', [
           permission.orgs.forEach(function(org) {
             $scope.selected_organizations[org.id] = true;
           });
+
+          // auto-select workflow_orgs from the response payload
+          permission.workflow_orgs.forEach(function(org) {
+            $scope.workflow_orgs[org.id] = true;
+          });
         }
       });
     };
@@ -40,6 +46,7 @@ app.controller('PermissionDetailCtrl', [
       $scope.selected_organizations = {};
 
       if (!$scope.select_all) {
+        $scope.workflow_orgs = {}; // Empty workflow_orgs if toggleAll checkbox is turned off
         return;
       } else {
         $scope.organizations.map(function(perm) {
@@ -48,10 +55,16 @@ app.controller('PermissionDetailCtrl', [
       }
     };
 
-    // Deselect in view
-    $scope.updateSelAllTag = function(id) {
+    // If an org is (un)selected, perform actions
+    $scope.updateIfOrgsChanged = function(id) {
+      // If org id is removed from selected_organizations then set select_all tag to false
       if (!$scope.selected_organizations[id]) {
         $scope.select_all = false;
+      }
+
+      // Auto unselect workflow enable tag for corresponding org if this org is (un)selected
+      if ($scope.workflow_orgs[id]) {
+        $scope.workflow_orgs[id] = false;
       }
     };
 
@@ -103,6 +116,11 @@ app.controller('PermissionDetailCtrl', [
         $scope.selected_organizations
       ).filter(function(key) {
         return $scope.selected_organizations[key];
+      });
+
+      // Safe check way to include only those workflow_org which have corresponding org id in selected_organizations.
+      permission.workflow_orgs = permission.orgs.filter(function(id) {
+        return $scope.workflow_orgs[id];
       });
 
       // edit
