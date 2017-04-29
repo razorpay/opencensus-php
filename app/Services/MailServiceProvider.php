52181af2cbf54fb4b8c09dbd635a391b2c579aec
Shashank Kumar <shashankkumar.me@gmail.com>
2017-04-29 19:36:47 +0530

    [laravel5.4] Updates MailServiceProvider to cater to new version.

--- app/Services/MailServiceProvider.php
+++ app/Services/MailServiceProvider.php
@@ -12,38 +12,27 @@ class MailServiceProvider extends LaravelMailServiceProvider
      *
      * @return void
      */
-    public function register()
+    protected function registerIlluminateMailer()
     {
-        $this->registerSwiftMailer();
+        $this->app->singleton('mailer', function ($app) {
+            $config = $app->make('config')->get('mail');
 
-        $this->app->singleton('mailer', function ($app)
-        {
             // Once we have create the mailer instance, we will set a container instance
             // on the mailer. This allows us to resolve mailer classes via containers
             // for maximum testability on said classes instead of passing Closures.
             $mailer = new Mailer(
-                $app['view'],
-                $app['swift.mailer'],
-                $app['events']
+                $app['view'], $app['swift.mailer'], $app['events']
             );
 
-            $this->setMailerDependencies($mailer, $app);
-
-            // If a "from" address is set, we will set it on the mailer so that all mail
-            // messages sent by the applications will utilize the same "from" address
-            // on each one, which makes the developer's life a lot more convenient.
-            $from = $app['config']['mail.from'];
-
-            if ((is_array($from)) and (isset($from['address'])))
-            {
-                $mailer->alwaysFrom($from['address'], $from['name']);
+            if ($app->bound('queue')) {
+                $mailer->setQueue($app['queue']);
             }
 
-            $to = $app['config']['mail.to'];
-
-            if ((is_array($to) and (isset($to['address']))))
-            {
-                $mailer->alwaysTo($to['address'], $to['name']);
+            // Next we will set all of the global addresses on this mailer, which allows
+            // for easy unification of all "from" addresses as well as easy debugging
+            // of sent messages since they get be sent into a single email address.
+            foreach (['from', 'reply_to', 'to'] as $type) {
+                $this->setGlobalAddress($mailer, $config, $type);
             }
 
             return $mailer;
