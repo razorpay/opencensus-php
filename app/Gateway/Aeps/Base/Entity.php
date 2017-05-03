daeb3856e5c1f5cfe87bcce8146afa0612cbfadd
gaurav <gaurav.d@razorpay.com>
2017-05-03 19:17:15 +0530

    [AEPS] added aeps table

---
+++ app/Gateway/Aeps/Base/Entity.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace RZP\Gateway\Aeps\Base;
+
+use RZP\Constants;
+use RZP\Gateway\Base;
+
+class Entity extends Base\Entity
+{
+    const ID                    = 'id';
+    const AMOUNT                = 'amount';
+    const RECEIVED              = 'received';
+    const REVERSED              = 'reversed';
+    const ERROR_CODE            = 'error_code';
+    const ERROR_DESCRIPTION     = 'error_description';
+    const RRN                   = 'rrn';
+    const PAYMENT_ID            = 'payment_id';
+
+    protected $fields = [
+        self::AMOUNT,
+        self::RECEIVED,
+        self::ERROR_CODE,
+        self::PAYMENT_ID,
+        self::REVERSED,
+        self::RRN,
+        self::ERROR_DESCRIPTION,
+    ];
+
+    protected $fillable = [
+        self::RRN,
+        self::AMOUNT,
+        self::RECEIVED,
+        self::REVERSED,
+        self::ERROR_CODE,
+        self::PAYMENT_ID,
+        self::ERROR_DESCRIPTION,
+    ];
+
+    protected $casts = [
+        self::AMOUNT        => 'int',
+    ];
+
+    protected $entity = Constants\Entity::EBS;
+}
---
+++ database/migrations/2017_05_03_133124_create_aeps.php
@@ -0,0 +1,72 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+use RZP\Constants\Table;
+use RZP\Gateway\Aeps\Base\Entity as Aeps;
+use RZP\Models\Base\UniqueIdEntity;
+use RZP\Models\Payment;
+
+class CreateAeps extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(Table::AEPS, function(Blueprint $table)
+        {
+            $table->engine = 'InnoDB';
+
+            $table->increments(Aeps::ID);
+
+            $table->string(Aeps::PAYMENT_ID, UniqueIdEntity::ID_LENGTH);
+
+            $table->integer(Aeps::AMOUNT);
+
+            $table->tinyInteger(Aeps::RECEIVED)->default(0);
+
+            $table->tinyInteger(Aeps::REVERSED)->default(0);
+
+            $table->string(Aeps::ERROR_CODE)->nullable();
+
+            $table->string(Aeps::ERROR_DESCRIPTION)->nullable();
+
+            $table->string(Aeps::RRN)->nullable();
+
+            $table->integer(Aeps::CREATED_AT);
+
+            $table->integer(Aeps::UPDATED_AT);
+
+            $table->foreign(Aeps::PAYMENT_ID)
+                  ->references(Payment\Entity::ID)
+                  ->on(Table::PAYMENT)
+                  ->on_delete('restrict');
+
+            $table->index(Aeps::PAYMENT_ID);
+
+            $table->index(Aeps::CREATED_AT);
+
+            $table->index(Aeps::UPDATED_AT);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table(Table::AEPS, function($table)
+        {
+            $table->dropForeign('aeps_payment_id_foreign');
+        });
+
+        Schema::drop(Table::AEPS);
+    }
+}
