3d21452588df940eb000595a2a3bc6b9c7845c7c
gaurav <gaurav.d@razorpay.com>
2017-05-03 17:00:23 +0530

    [AEPS] added url for prod environment

--- app/Gateway/Aeps/Icici/Socket.php
+++ app/Gateway/Aeps/Icici/Socket.php
@@ -9,7 +9,7 @@ class Socket extends Base\Core
 {
     protected $sock;
 
-    public function __construct($address, $port)
+    public function __construct()
     {
         $this->sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 
@@ -25,6 +25,8 @@ class Socket extends Base\Core
                 'Socket Creation Failed',
                 ErrorCode::SERVER_ERROR_SOCKET_FAILURE);
         }
+
+        list($address, $port) = $this->getAddressAndPort();
 
         $result = socket_connect($sock, $address, $port);
 
@@ -55,5 +57,15 @@ class Socket extends Base\Core
     public function closeSocket()
     {
         socket_close($this->sock);
+    }
+
+    protected function getAddressAndPort()
+    {
+        if ($this->mode === Mode::TEST)
+        {
+            return [Url::TEST_DOMAIN_URL, Url::TEST_DOMAIN_PORT];
+        }
+
+        return [Url::LIVE_DOMAIN_URL, Url::LIVE_DOMAIN_PORT];
     }
 }
