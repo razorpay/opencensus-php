15f894b2785dff2f014fb072ba410c6ab6b06674
gaurav <gaurav.d@razorpay.com>
2017-05-05 00:47:53 +0530

    [AEPS] remove commented prod urls

--- app/Gateway/Aeps/Icici/Url.php
+++ app/Gateway/Aeps/Icici/Url.php
@@ -4,11 +4,8 @@ namespace RZP\Gateway\Aeps\Icici;
 
 class Url
 {
-    //const TEST_DOMAIN_URL   = '203.199.32.86';
-    //const LIVE_DOMAIN_URL   = '203.189.92.113';
-
-    const TEST_DOMAIN_URL   = '127.0.0.1';
-    const LIVE_DOMAIN_URL   = '127.0.0.1';
+    const TEST_DOMAIN_URL   = '203.199.32.86';
+    const LIVE_DOMAIN_URL   = '203.189.92.113';
 
     const TEST_DOMAIN_PORT  = '4443';
     const LIVE_DOMAIN_PORT  = '4404';
