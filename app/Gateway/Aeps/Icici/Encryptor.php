4f95cf5cd9f54b006664a944cedc1eff7c388769
gaurav <gaurav.d@razorpay.com>
2017-05-04 20:49:03 +0530

    [AEPS] update encrypter to generate skey

--- app/Gateway/Aeps/Icici/Encryptor.php
+++ app/Gateway/Aeps/Icici/Encryptor.php
@@ -4,21 +4,53 @@ namespace RZP\Gateway\Aeps\Icici;
 
 class Encryptor
 {
-    public function encryptUsingSessionKey()
+    const CERT_PATH  = 'certs\public.cer';
+    const CERT_EXPIRY = '20191230';
+
+    public function encryptUsingSessionKey($fpData, $sKey)
     {
     }
 
-    public function encryptSessionKey()
+    public function encryptSessionKey($skey)
+    {
+        $publicKey = file_get_contents(self::CERT_PATH);
+
+        openssl_public_encrypt($skey, $encrypted, $publicKey);
+
+        $encoded = base64_encode($encrypted);
+
+        return $encoded;
+    }
+
+    public function generateHmac($fpData, $sKey)
     {
     }
 
     public function encryptInput(array & $input)
     {
-        $this->encryptUsingSessionKey();
-        $this->encryptSessionKey();
+        $skey = $this->generateSkey();
 
-        $input['aadhaar_hmac'] = 'test';
-        $input['aadhaar_session_key'] = 'test';
-        $input['aadhaar_cert_expiry'] = 'test';
+        $this->encryptUsingSessionKey();
+
+        $input['aadhaar_hmac'] = $this->generateHmac($input['aadhaar_fingerprint'], $skey);
+
+        $input['aadhaar_fingerprint'] = $this->encryptSessionKey($input['aadhaar_fingerprint'], $skey);
+
+        $input['aadhaar_session_key'] = $this->encryptSessionKey($skey);
+
+        $input['aadhaar_cert_expiry'] = self::CERT_EXPIRY;
+    }
+
+    function generateSkey($length = 16)
+    {
+        //TODO : make it better
+        $result = '';
+
+        foreach (range(0, $length - 1) as $index)
+        {
+            $result .= mt_rand(0, 9);
+        }
+
+        return $result;
     }
 }
