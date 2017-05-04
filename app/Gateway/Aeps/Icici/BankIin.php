dc5dc18dd68ba4f8adf58b921645669d1d535347
gaurav <gaurav.d@razorpay.com>
2017-05-05 00:44:19 +0530

    [AEPS] updated bank iin

--- app/Gateway/Aeps/Icici/BankIin.php
+++ app/Gateway/Aeps/Icici/BankIin.php
@@ -7,93 +7,91 @@ use RZP\Models\Bank\IFSC;
 class BankIin
 {
     public static $map = [
+        IFSC::AUGX => '607091',
+        IFSC::ANDB => '607076',
+        IFSC::APGX => '607198',
+        IFSC::APGB => '607121',
+        IFSC::AGVX => '607064',
+        IFSC::UTIB => '607153',
+        IFSC::BGVX => '607063',
+        IFSC::BARB => '606985',
+        IFSC::BKID => '508505',
+        IFSC::MAHB => '607387',
+        IFSC::BGGX => '606995',
+        IFSC::BRGX => '607280',
+        IFSC::BUGX => '606993',
+        IFSC::BGBX => '607377',
         IFSC::CNRB => '607396',
+        IFSC::CBIN => '607264',
+        IFSC::CMPX => '607071',
+        IFSC::CGGX => '607080',
+        IFSC::CGBX => '607214',
+        IFSC::CORP => '607184',
+        IFSC::BKDN => '508547',
+        IFSC::DEGX => '607099',
+        IFSC::EDBX => '607218',
+        IFSC::ARYX => '607024',
+        IFSC::HDFC => '607152',
         IFSC::ICIC => '508534',
+        IFSC::IBKL => '607095',
+        IFSC::IDFB => '608117',
+        IFSC::IDIB => '607105',
+        IFSC::IOBA => '607126',
+        IFSC::INDB => '607189',
+        IFSC::JAKA => '607440',
+        IFSC::KVGB => '607122',
+        IFSC::KVBL => '508662',
+        IFSC::KLGB => '607399',
+        IFSC::LDRX => '607202',
+        IFSC::MADX => '607232',
+        IFSC::MGRB => '607241',
+        IFSC::MRBX => '607062',
+        IFSC::MYAX => '607206',
+        IFSC::NJGX => '607022',
+        IFSC::ODGB => '607060',
+        IFSC::ORBC => '607085',
+        IFSC::PABX => '607052',
+        IFSC::PDNX => '607059',
+        IFSC::PGBX => '607400',
+        IFSC::PRTH => '607124',
+        IFSC::PSIB => '607087',
+        IFSC::PUGX => '607138',
+        IFSC::PUNB => '607027',
+        IFSC::PURX => '607212',
+        IFSC::MDGX => '607509',
+        IFSC::SPBX => '607053',
+        IFSC::HGBX => '607139',
+        IFSC::SUBX => '607135',
+        IFSC::TSUX => '607200',
+        IFSC::SIBL => '607475',
+        IFSC::STBP => '606990',
+        IFSC::SBBJ => '606987',
+        IFSC::SBHY => '607901',
+        IFSC::SBIN => '607094',
+        IFSC::SBMY => '606989',
+        IFSC::SBTR => '606991',
+        IFSC::SYNB => '607580',
+        IFSC::SJGX => '607310',
+        IFSC::VIJB => '607075',
+        IFSC::UTBI => '607646',
+        IFSC::UBIN => '607161',
+        IFSC::UCBA => '607066',
+        IFSC::DGBX => '607195',
+        IFSC::TGBX => '607065',
+        IFSC::UGBX => '607234',
+        IFSC::UBGX => '607069',
+        IFSC::UTGX => '607197',
+        IFSC::VGBX => '607210',
     ];
 
     //TODO update mapping for below banks also
     /*
-        Allahabad UP Gramin Bank    607091
-        Andhra Bank    607076
-        Andhra Pradesh Gramin Vikash Bank    607198
-        Andhra Pragathi Grameena Bank    607121
-        Assam Gramin Vikash Bank    607064
-        Axis Bank    607153
-        Bangiya Gramin Vikash Bank    607063
-        Bank Of Baroda    606985
-        Bank of India    508505
-        Bank of Maharashtra    607387
-        Baroda Gujarat Gramin Bank     606995/607046/608144
-        Baroda Rajasthan Kshetriya Gramin Bank    607280
-        Baroda Uttar Pradesh Gramin Bank     606993
-        Bhartiya Mahila Bank    608114
-        Bihar Gramin Bank    607377
-        Canara Bank    607396
-        Central Bank of India    607264
-        Central Madya Pradesh Gramin Bank    607071
-        Chaitanya Godavari Gramin Bank    607080
-        Chhattisgharh Rajya Gramin Bank    607214
-        Corporation Bank    607184
-        Dena Bank    508547
-        Dena Gujarat Gramin Bank    607099
-        Ellaquai Dehati Bank    607218
-        Gramin bank of Aryavart    607024
-        HDFC Bank    607152
-        Himachal Pradesh Gramin Bank    607140
-        ICICI Bank    508534
-        IDBI Bank    607095
-        IDFC Bank    608117/608118/608116
-        Indian bank    607105
-        Indian Overseas Bank    607126
-        IndusInd Bank    607189
-        Jammu & Kashmir Bank     607440
-        Jharkhand Gramin Bank    607021
-        Karnataka Vikas Grameena Bank     607122
-        Karur Vysya Bank    508662
-        Kerala Gramin Bank    607399
-        Langpi Dehangi Rural Bank    607202
-        Madhya Bihar Gramin Bank    607136
-        Madhyanchal Gramin Bank    607232
-        Malwa Gramin Bank    607241
-        Manipur Rural Bank     607062
-        Meghalaya Rural Bank    607206
-        Narmada Jhabua Gramin Bank    607022
-        Odisha Gramya Bank     607060
-        Oriental Bank of Commerce    607085/607041
-        Pallavan Grama Bank    607052
-        Pandyan GB    607059
-        Pragathi Krishna Gramin Bank    607400
-        Prathama Bank    607124
-        Punjab & Sind Bank    607087
-        Punjab Gramin Bank    607138
-        Punjab National Bank    607027
-        Purvanchal Gramin Bank    607212
-        Rajasthan Marudhara Gramin Bank    607509
-        Ratnakar Bank    607393
-        Saptagiri Grameena Bank    607053
-        Sarva Haryana Gramin Bank    607139
-        Sarva UP Gramin Bank     607135
-        Saurashtra Gramin Bank    607200
-        South Indian Bank    607475
-        State  bank of Bikaner and Jaipur    606987
-        State bank of Hyderabad    607901
-        State Bank of India    607094
-        State Bank of Mysore    606989
-        State Bank of Patiala    606990
-        State Bank of Travancore    606991
-        Sutlej Gramin Bank    607310
-        Syndicate Bank    607580
-        Telangana Grameena Bank    607195
-        Tripura Gramin Bank    607065
-        UCO Bank    607066
-        Union Bank of India    607161/508500
-        United Bank Of India    607646
-        Utkal Gramin Bank    607234
-        Uttar Banga Kshetriya Gramin Bank     607073
-        Uttar Bihar Grameen Bank     607069
-        Uttarakhand Gramin Bank    607197
-        Vananchal Gramin Bank    607210
         Vidarbha Konkan Gramin Bank    607020
-        Vijaya Bank    607075
+        Uttar Banga Kshetriya Gramin Bank     607073
+        Bhartiya Mahila Bank    608114
+        Himachal Pradesh Gramin Bank    607140
+        Jharkhand Gramin Bank    607021
+        Madhya Bihar Gramin Bank    607136
+        Ratnakar Bank    607393
      */
 }
