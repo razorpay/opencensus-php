63acd1c2cc44a26fef0c58d20428f716f2ddbe38
Vivek Kumar <viveklakhera@gmail.com>
2017-05-05 11:59:39 +0530

    [terminal] Fix aeps icici validator

--- app/Models/Terminal/Validator.php
+++ app/Models/Terminal/Validator.php
@@ -73,7 +73,6 @@ class Validator extends Base\Validator
 
     protected static $aepsIciciTerminalRules = [
         Entity::GATEWAY                     => 'required|in:aeps_icici',
-        Entity::GATEWAY_ACQUIRER            => 'required',
         Entity::GATEWAY_MERCHANT_ID         => 'required|alpha_num',
     ];
 
