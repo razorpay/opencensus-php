650f3da46b0ff25ce3a42cb28865ac4ef598d20d
surya15t <surya.teja@razorpay.com>
2017-05-08 19:39:57 +0530

    [merchant] remove rollbar, analytics from userCtrl

--- public/js/merchant/controllers/userCtrl.js
+++ public/js/merchant/controllers/userCtrl.js
@@ -164,17 +164,6 @@ app
             }
           }
 
-          Rollbar.configure({
-            payload: {
-              person: {
-                id: data.id,
-                name: data.name,
-                email: data.email,
-                role: 'merchant',
-              },
-            },
-          });
-
           if (window.smoochScript) {
             smoochScript.then(function() {
               var sk_user = function() {
@@ -210,13 +199,6 @@ app
               }
             });
           }
-
-          analytics.identify(data.id, {
-            name: data.name,
-            email: data.email,
-            activated: data.activated,
-            submitted: data.submitted,
-          });
         });
       };
       $scope.refreshUser();
