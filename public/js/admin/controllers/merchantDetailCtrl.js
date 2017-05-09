7841259c5000f5f7d6b79afb55997b8aab8aaa48
Rishabh <rishabh.pugalia@gmail.com>
2017-05-09 20:22:45 +0530

    workflow detail changes

--- public/js/admin/controllers/merchantDetailCtrl.js
+++ public/js/admin/controllers/merchantDetailCtrl.js
@@ -13,6 +13,8 @@ app
     '$upload',
     'organization',
     'displayValue',
+    'utils',
+    '$state',
     function(
       $scope,
       $http,
@@ -24,7 +26,9 @@ app
       admin,
       $upload,
       organization,
-      displayValue
+      displayValue,
+      utils,
+      $state
     ) {
       admin.identity().then(function(data) {
         $scope.admin = data;
@@ -740,6 +744,13 @@ app
                 'Merchant archived successfully',
                 true
               );
+
+              if (utils.isWorkflow(data.data)) {
+                $state.go('app.workflows.actions.detail', {
+                  action_id: data.data.id,
+                });
+              }
+
               $scope.merchant.details.archived_at = Date.now() / 1000;
             } else {
               $scope.alerts.resetAlerts();
