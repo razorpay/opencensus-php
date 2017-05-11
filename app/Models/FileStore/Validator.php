b4f1894d09fc9ae78258e0e30a7b49a79af31ff1
namrata <namrata.chaudhary@razorpay.com>
2017-05-11 17:27:30 +0530

    [reports] fixing validator for entity_fetch

--- app/Models/FileStore/Validator.php
+++ app/Models/FileStore/Validator.php
@@ -8,34 +8,12 @@ use RZP\Constants\Entity as E;
 
 class Validator extends Base\Validator
 {
+    /*
+     * entity_id => can be a pulic id, hence 20 chars
+     * entity    => should be one of the allowed entities
+     */
     protected static $entityFetchRules = [
-        'entity_id'     => 'required|alpha_num|max:14',
-        'entity'        => 'required|string'
+        'entity_id'     => 'required|alpha_dash|max:20',
+        'entity'        => 'required|string|in:report'
     ];
-
-    /**
-     * Entities for which file_store entity exists
-     * Used to validate input from 'file_get_signed_url'
-     *
-     */
-    protected $allowed = [
-        E::REPORT,
-    ];
-
-    /**
-     * Throws Exception if entity is not allowed
-     *
-     * @param string $entity
-     *
-     * @return void
-     * @throws Exception\BadRequestValidationFailureException
-     */
-    protected function validateRequestEntity(string $entity)
-    {
-        if (in_array($entity, $this->allowed, true) === false)
-        {
-            throw new Exception\BadRequestValidationFailureException(
-                'Cannot get file for the given entity type');
-        }
-    }
 }
