6736a813a842009243d5b5b87851155c48bc8cb1
selvagsz <selvaganeshbsg@gmail.com>
2017-05-15 19:36:55 +0530

    Push missed file

---
+++ public/react/merchant/containers/Invoicing.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Route, NavLink, withRouter } from 'react-router-dom';
+
+import Invoices from 'merchant/containers/Invoices/List';
+import Customers from 'merchant/containers/Customers/List';
+import Items from 'merchant/containers/Items/List';
+
+@withRouter
+export default class InvoicingContainer extends Component {
+  render() {
+    return (
+      <tabbed-container>
+        <header>
+          <NavLink to="/app/invoices">Invoices</NavLink>
+          <NavLink to="/app/customers">Customers</NavLink>
+          <NavLink to="/app/items">Items</NavLink>
+        </header>
+
+        <Route path="/app/invoices" component={Invoices} />
+        <Route path="/app/customers" component={Customers} />
+        <Route path="/app/items" component={Items} />
+      </tabbed-container>
+    );
+  }
+}
