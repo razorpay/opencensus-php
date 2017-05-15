309c4679332487a94b1c550f1709974f98856976
Harman Singh <harman28@gmail.com>
2017-05-15 16:56:41 +0530

    [first_data] Adds callback verb check

--- app/Gateway/FirstData/Gateway.php
+++ app/Gateway/FirstData/Gateway.php
@@ -5,19 +5,21 @@ namespace RZP\Gateway\FirstData;
 use Carbon\Carbon;
 use Requests_Hooks;
 use SimpleXMLElement;
-use RZP\Constants;
-use RZP\Constants\HashAlgo;
-use RZP\Constants\Mode;
+
 use RZP\Error;
+use RZP\Constants;
 use RZP\Exception;
-use RZP\Gateway\Base;
-use RZP\Gateway\Base\Action;
-use RZP\Gateway\Base\VerifyResult;
 use RZP\Models\Card;
-use RZP\Models\Currency\Currency;
+use RZP\Gateway\Base;
 use RZP\Models\Payment;
-use RZP\Models\Terminal;
+use RZP\Constants\Mode;
+use RZP\Error\ErrorCode;
 use RZP\Trace\TraceCode;
+use RZP\Models\Terminal;
+use RZP\Constants\HashAlgo;
+use RZP\Gateway\Base\Action;
+use RZP\Models\Currency\Currency;
+use RZP\Gateway\Base\VerifyResult;
 
 class Gateway extends Base\Gateway
 {
@@ -92,6 +94,15 @@ class Gateway extends Base\Gateway
         parent::callback($input);
 
         $this->traceGatewayCallback($input['gateway']);
+
+        if (empty($input['gateway']) === true)
+        {
+            // If the callback body is empty, then it's likely because the customer has accidentally
+            // sent us a GET request from his browser during redirection. In this case we can treat
+            // the payment as failed (effectively a timeout), and let verify handle it like a boss.
+            throw new Exception\GatewayErrorException(
+                ErrorCode::BAD_REQUEST_PAYMENT_MISSING_DATA);
+        }
 
         $this->assertPaymentId($input['payment']['id'], $input['gateway'][ConnectResponseFields::ORDER_ID]);
 
