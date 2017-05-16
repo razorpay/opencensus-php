0cab6a78f201d0dbe30191d361629924d83ca3a5
Sreeram Boyapati <sreeram.boyapati@razorpay.com>
2017-05-16 15:37:22 +0530

    [Workflow] Restrict check for duplicate workflows within the same org

--- app/Models/Workflow/Repository.php
+++ app/Models/Workflow/Repository.php
@@ -82,12 +82,17 @@ class Repository extends Base\Repository
         return $workflow;
     }
 
-    public function getWorkflowIdsForPermissions(array $permissionIds)
+    public function getWorkflowIdsForPermissionsAndOrgId(
+        string $orgId, array $permissionIds)
     {
         $pid = 'workflow_permissions.permission_id';
 
+        $wOrgId = $this->dbColumn(Entity::ORG_ID);
+        $wid = $this->dbColumn(Entity::ID);
+
         return $this->newQuery()
-                    ->join(Table::WORKFLOW_PERMISSION, Entity::ID, '=', 'workflow_permissions.workflow_id')
+                    ->join(Table::WORKFLOW_PERMISSION, $wid, '=', 'workflow_permissions.workflow_id')
+                    ->where($wOrgId, '=', $orgId)
                     ->whereIn($pid, $permissionIds)
                     ->whereNull(Entity::DELETED_AT)
                     ->pluck(Entity::ID);
