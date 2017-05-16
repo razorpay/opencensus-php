8c2e6fc62bc48eb2746a89d99a13c9354e74b00b
Ankit <ankitstarski@gmail.com>
2017-05-16 21:02:21 +0530

    use StatusLabel on home page

--- public/react/merchant/components/Home/EntityTable.js
+++ public/react/merchant/components/Home/EntityTable.js
@@ -1,36 +1,48 @@
-import Amount from "rzp/ui/Amount";
+import Amount from 'rzp/ui/Amount';
 import {
-  titleCase,
-  paymentStausColor,
-  formatFromNow,
-} from "rzp/utils/rzp-utils";
+  PaymentStatusLabel,
+  SettlementStatusLabel,
+} from 'merchant/components/StatusLabel';
+import { titleCase, formatFromNow } from 'rzp/utils/rzp-utils';
+
+const StatusLabel = ({ status, entity, children, ...otherProps }) => {
+  let Label = entity === 'settlement'
+    ? SettlementStatusLabel
+    : PaymentStatusLabel;
+  status = status || 'refunded';
+
+  return (
+    <Label status={status} {...otherProps}>
+      {children}
+    </Label>
+  );
+};
 
 export default ({ entity, data }) => {
   return (
-    <div className="col-md-4 b-r b-light no-border-xs">
+    <div className="col-md-4 b-light no-border-xs">
       <a
         data-tip="See All Payments"
         className="pull-right"
-        href="#/app/payments/list"
+        href={`#/app/${entity}s/list`}
       >
         <i className="icon-arrow-right" />
       </a>
-      <h4 style={{ margin: "0 0 10px" }}>Recent {titleCase(entity)}s</h4>
+      <h4 style={{ margin: '0 0 10px' }}>Recent {titleCase(entity)}s</h4>
       {data.count
         ? data.items.slice(0, 5).map((item, index) => {
             return (
-              <div key={index} className="row" style={{ margin: "10px" }}>
-                <a href={`#/app/payments/${item.id}`}>
-                  <div
-                    className={
-                      "col-xs-4 col-md-3 label bg-" +
-                        (paymentStausColor[item.status] || "light")
-                    }
-                    data-tip={titleCase(item.status)}
+              <div key={index} className="row" style={{ margin: '10px' }}>
+                <a href={`#/app/${entity}/${item.id}`}>
+                  <StatusLabel
+                    entity={entity}
+                    status={item.status}
+                    data-tip={titleCase(item.status) || null}
                     data-place="right"
                   >
                     <Amount value={item.amount} />
-                  </div>
+                  </StatusLabel>
+
                   <div className="col-xs-8 col-md-9">
                     <code className="hidden-xs">{item.id}</code>
                     <span className="pull-right">
