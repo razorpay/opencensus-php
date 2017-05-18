b57457bafcce4e366488d80d833530b1f42f7592
selvagsz <selvaganeshbsg@gmail.com>
2017-05-18 13:26:44 +0530

    Port Profile to react-router

--- public/react/merchant/containers/MyAccount.js
+++ public/react/merchant/containers/MyAccount.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
 import { Route, NavLink, withRouter } from 'react-router-dom';
 
+import Profile from 'merchant/containers/Profile';
 import Activation from 'merchant/containers/Activation';
 import AddFunds from 'merchant/containers/AddFunds';
 import Credits from 'merchant/containers/Credits/List';
 
-@withRouter
 export default class MyAccount extends Component {
   render() {
     return (
       <tabbed-container>
         <header>
+          <NavLink to="/app/profile">Profile</NavLink>
           <NavLink to="/app/activation">Activation</NavLink>
           <NavLink to="/app/credits">Credits</NavLink>
           <NavLink to="/app/addfunds">Add Funds</NavLink>
         </header>
 
+        <Route path="/app/profile" component={Profile} />
         <Route path="/app/activation" component={Activation} />
         <Route path="/app/credits" component={Credits} />
         <Route path="/app/addfunds" component={AddFunds} />
