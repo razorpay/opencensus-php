52643458ab07a2be80cb55f1eaf01468da104529
Ankit Agarwal <ankit.agarwal@razorpay.com>
2017-05-22 20:13:52 +0530

    [Fee]: Reverse commit

--- app/Models/Pricing/FeeCalculator.php
+++ app/Models/Pricing/FeeCalculator.php
@@ -640,7 +640,7 @@ class FeeCalculator
             $payment = $this->entity;
 
             if (($payment->isMethodCardOrEmi() === true) and
-                ($this->amount <= 200000))
+                ($payment->getBaseAmount() <= 200000))
             {
                 $totalTaxes = 0;
             }
