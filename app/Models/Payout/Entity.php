1cfeac5f4be64180cd95976f54b65b75e33a92b4
Priyanshu Chhazed <priyanshu.chhazed@razorpay.com>
2017-05-22 21:15:37 +0530

    Fixed bug

--- app/Models/Payout/Entity.php
+++ app/Models/Payout/Entity.php
@@ -315,6 +315,6 @@ class Entity extends Base\PublicEntity
     {
         $processedAt = $this->attributes[self::PROCESSED_AT];
 
-        return $this->getDateInFormatDMY($processedAt);
+        return date('d/m/y', $processedAt);
     }
 }
