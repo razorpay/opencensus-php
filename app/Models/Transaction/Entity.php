bd71a06bd311404681a9cff70faa3dd186286af3
Priyanshu Chhazed <priyanshu.chhazed@razorpay.com>
2017-05-23 10:11:12 +0530

    Fixed bug

--- app/Models/Transaction/Entity.php
+++ app/Models/Transaction/Entity.php
@@ -654,7 +654,7 @@ class Entity extends Base\PublicEntity
 
             $reportTxn['settlement_utr'] = $settlement->getUtr();
             $reportTxn[self::SETTLED] = null;
-            $reportTxn[self::PROCESSED_AT] = $settlement->getProcessedAt();
+            $reportTxn['processed_at'] = $settlement->getProcessedAt();
         }
         else if ($this->isTypeAdjustment())
         {
