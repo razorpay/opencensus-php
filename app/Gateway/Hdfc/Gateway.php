68307fe5979990aadd241c2ab6a1a819737ce708
Vivek <ccrims0n@users.noreply.github.com>
2017-05-26 15:48:02 +0530

    [fss] Add verify refund (#3473)
    Adds verify refund flow for hdfc fss based on track Id.

--- app/Gateway/Hdfc/Gateway.php
+++ app/Gateway/Hdfc/Gateway.php
@@ -380,22 +380,6 @@ class Gateway extends Base\Gateway
         return $this->runPaymentVerifyFlow($verify);
     }
 
-    public function verifyRefund(array $input)
-    {
-        // FSS returns an error when refund amount exceeds the remaining amount
-        // on FSS's end. We take advantage of this error and initiate refunds
-        // for all the pending refunds whose amount is either equal to payment, i.e,
-        // they are full refund or twice of refund amount is less than payment amount
-        if (($input['refund']['amount'] !== $input['payment']['amount']) and
-            ((2 * $input['refund']['amount']) <= $input['payment']['amount']))
-        {
-            throw new Exception\LogicException(
-                'Verify refund is only supported for full refundsa and specific partial refunds');
-        }
-
-        return false;
-    }
-
     /**
      * HDFC gateway does not provide void
      */
