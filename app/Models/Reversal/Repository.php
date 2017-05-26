dac05fd340991ed2de53fef79a9d075f57b733b3
Amit Mahbubani <amit.mahbubani@razorpay.com>
2017-05-26 14:51:30 +0530

    [reversals] Add fetch service functions

--- app/Models/Reversal/Repository.php
+++ app/Models/Reversal/Repository.php
@@ -8,6 +8,10 @@ class Repository extends Base\Repository
 {
     protected $entity = 'reversal';
 
+    protected $entityFetchParamRules = [
+        Entity::TRANSFER_ID         => 'sometimes|string|size:18|public_id',
+    ];
+
     protected $appFetchParamRules = [
         Entity::TRANSACTION_ID      => 'sometimes|alpha_num|size:14',
         Entity::MERCHANT_ID         => 'sometimes|alpha_num|size:14',
