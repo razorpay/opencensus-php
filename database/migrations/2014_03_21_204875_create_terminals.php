96414f82a7542023e1c25bfcabd7519e1bd299a9
Vivek Kumar <viveklakhera@gmail.com>
2017-05-26 15:08:27 +0530

    [migration] Fix default value of type attribute in terminal

--- database/migrations/2014_03_21_204875_create_terminals.php
+++ database/migrations/2014_03_21_204875_create_terminals.php
@@ -7,7 +7,6 @@ use RZP\Constants\Table;
 use RZP\Models\Merchant;
 use RZP\Models\Terminal\Entity as Terminal;
 use RZP\Models\Terminal\Mode;
-use RZP\Models\Terminal\Type;
 
 class CreateTerminals extends Migration
 {
@@ -99,7 +98,7 @@ class CreateTerminals extends Migration
                   ->default(0);
 
             $table->tinyInteger(Terminal::TYPE)
-                  ->default(Type::NON_RECURRING);
+                  ->default(1);
 
             $table->tinyInteger(Terminal::MODE)
                   ->default(Mode::DUAL);
