827310fea1338daa59aa17faab834a1720d56ef7
Nemo <nemo@razorpay.com>
2017-05-30 20:05:26 +0530

    [http] Adds the CORS middleware file

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Http\Middleware;
+use Closure;
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        // ALLOW OPTIONS METHOD
+
+        $headers = [
+            'Access-Control-Allow-Origin'       => env('AUTH_SERVICE_URL'),
+            'Access-Control-Allow-Methods'      => 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Credentials'  => true,
+        ];
+
+        if ($request->getMethod() === 'OPTIONS')
+        {
+            return \Response::make('OK', 200, $headers);
+        }
+
+        $response = $next($request);
+        $response->header(key($headers), current($headers));
+        return $response;
+    }
+}
