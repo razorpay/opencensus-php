29b299d592dceafe0412790779bf4c8fe1b68a26
Ankit Agarwal <ankit.agarwal@razorpay.com>
2017-05-31 18:03:19 +0530

    [User]: Minor fix - User is not able to switch merchant when he approves an invite

--- public/react/merchant/components/Profile/Invitations.js
+++ public/react/merchant/components/Profile/Invitations.js
@@ -11,7 +11,7 @@ export default ({ invitations, onAcceptClick, onRejectClick }) => {
           key={index}
           label={() => (
             <span>
-              Invitation to join <strong>${invite.merchant.name}</strong>
+              Invitation to join <strong>{invite.merchant.name}</strong>
             </span>
           )}
           value={() => (
