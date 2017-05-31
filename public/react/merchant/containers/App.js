c67813b4e790dd30c5a5c401865c920dacfa74b3
selvagsz <selvaganeshbsg@gmail.com>
2017-05-31 16:07:28 +0530

    Bring back splash loader

--- public/react/merchant/containers/App.js
+++ public/react/merchant/containers/App.js
@@ -55,6 +55,9 @@ export default class App extends Component {
         }
       }),
     ]).then(() => {
+      let $splash = document.getElementById('splash');
+      $splash.parentElement.removeChild($splash);
+
       this.setState({ isLoading: false });
     });
   }
