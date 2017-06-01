89702b237144b79143866cd07c88c6c3d1783371
Ria Garg <ria.garg@razorpay.com>
2017-06-01 22:26:41 +0530

    added comment for merchant id

--- app/Gateway/Upi/Icici/Url.php
+++ app/Gateway/Upi/Icici/Url.php
@@ -7,6 +7,7 @@ class Url
     const TEST_DOMAIN  = 'https://apigwuat.icicibank.com:8443';
     const LIVE_DOMAIN  = 'https://api.icicibank.com:8443';
 
+    //here %s is for merchant id
     const AUTHORIZE    = '/api/MerchantAPI/UPI/v2/CollectPay/%s';
     const VERIFY       = '/api/MerchantAPI/UPI/v2/TransactionStatus/%s';
     const REFUND       = '/api/MerchantAPI/UPI/v1/Refund/%s';
