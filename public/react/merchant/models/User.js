1ba4f3b7b4fe63fa986140f4e62dff711941c689
selvagsz <selvaganeshbsg@gmail.com>
2017-06-05 17:21:19 +0530

    Remove unused isPreSignupDone from user model

--- public/react/merchant/models/User.js
+++ public/react/merchant/models/User.js
@@ -24,29 +24,6 @@ export default class User {
     return this.user.confirmed;
   }
 
-  get isPreSignupDone() {
-    if (
-      !this.user.merchants.length ||
-      this.pre_signup.length === 0 ||
-      this.created_at < 1488306600 // pre-signup is only for signup on/after 01 March 2017
-    ) {
-      return true;
-    }
-
-    return (
-      this.pre_signup &&
-      Object.keys(this.pre_signup)
-        // get all values
-        .map(key => {
-          return this.pre_signup[key];
-        })
-        // reduce all values using '&&'
-        .reduce((x, y) => {
-          return x && y;
-        })
-    );
-  }
-
   get isActivated() {
     return !!parseInt(this.user.activated);
   }
