0bd7d888fa86db677a774db16a8a0eaa711ad982
selvagsz <selvaganeshbsg@gmail.com>
2017-06-05 18:05:36 +0530

    Wrap form elements with .form-group for better UI in mobile view

--- public/react/merchant/containers/Reports/index.js
+++ public/react/merchant/containers/Reports/index.js
@@ -113,65 +113,75 @@ export default class ReportsContainer extends Component {
         <div class="content-wrapper content-sm text-center">
           <div class="row">
             <div class="col-sm-3">
-              <Field name="entity" component="select" class="form-control">
-                <option value="payment">Payment</option>
-                <option value="refund">Refund</option>
-                <option value="order">Order</option>
-                <option value="settlement">Settlement</option>
-                <option value="transaction">Combined</option>
-                {user.tags.indexOf('Broking_Report') === -1 ||
-                  <option value="broking">Broking Report</option>}
-                <option value="invoice">Monthly Invoice</option>
-                {user.tags.indexOf('Marketplace') === -1 ||
-                  <optgroup label="Marketplace">
-                    <option value="transfer">Transfer</option>
-                    <option value="reversal">Reversal</option>
-                  </optgroup>}
-              </Field>
+              <div class="form-group">
+                <Field name="entity" component="select" class="form-control">
+                  <option value="payment">Payment</option>
+                  <option value="refund">Refund</option>
+                  <option value="order">Order</option>
+                  <option value="settlement">Settlement</option>
+                  <option value="transaction">Combined</option>
+                  {user.tags.indexOf('Broking_Report') === -1 ||
+                    <option value="broking">Broking Report</option>}
+                  <option value="invoice">Monthly Invoice</option>
+                  {user.tags.indexOf('Marketplace') === -1 ||
+                    <optgroup label="Marketplace">
+                      <option value="transfer">Transfer</option>
+                      <option value="reversal">Reversal</option>
+                    </optgroup>}
+                </Field>
+              </div>
             </div>
 
             {entity === 'invoice' ||
               <div class="col-sm-2">
-                <Field name="type" class="form-control" component="select">
-                  <option value="daily">Daily</option>
-                  <option value="monthly">Monthly</option>
-                </Field>
+                <div class="form-group">
+                  <Field name="type" class="form-control" component="select">
+                    <option value="daily">Daily</option>
+                    <option value="monthly">Monthly</option>
+                  </Field>
+                </div>
               </div>}
 
             <div class="col-sm-2">
-              <Field name="year" class="form-control" component="select">
-                <option value="2017">2017</option>
-                <option value="2016">2016</option>
-                <option value="2015">2015</option>
-              </Field>
+              <div class="form-group">
+                <Field name="year" class="form-control" component="select">
+                  <option value="2017">2017</option>
+                  <option value="2016">2016</option>
+                  <option value="2015">2015</option>
+                </Field>
+              </div>
             </div>
 
             <div class="col-sm-3">
-              <Field name="month" class="form-control" component="select">
-                {moment.months().map((name, index) => {
-                  return (
-                    <option value={index + 1} key={index}>
-                      {name}
-                    </option>
-                  );
-                })}
-              </Field>
+              <div class="form-group">
+                <Field name="month" class="form-control" component="select">
+                  {moment.months().map((name, index) => {
+                    return (
+                      <option value={index + 1} key={index}>
+                        {name}
+                      </option>
+                    );
+                  })}
+                </Field>
+              </div>
             </div>
 
             {type === 'daily' &&
               <div class="col-sm-2">
-                <Field name="day" class="form-control" component="select">
-                  {Array.from(
-                    Array(numberOfDays(month, year)),
-                    (undef, index) => {
-                      return (
-                        <option value={index + 1} key={index}>
-                          {index + 1}
-                        </option>
-                      );
-                    }
-                  )}
-                </Field>
+                <div class="form-group">
+                  <Field name="day" class="form-control" component="select">
+                    {Array.from(
+                      Array(numberOfDays(month, year)),
+                      (undef, index) => {
+                        return (
+                          <option value={index + 1} key={index}>
+                            {index + 1}
+                          </option>
+                        );
+                      }
+                    )}
+                  </Field>
+                </div>
               </div>}
 
           </div>
