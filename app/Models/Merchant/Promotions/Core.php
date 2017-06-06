a1bba3523bab0634d37f06890405aac6839c66db
gaurav <gaurav.d@razorpay.com>
2017-06-06 13:06:45 +0530

    [Coupon] added apply/expire credit

--- app/Models/Merchant/Promotions/Core.php
+++ app/Models/Merchant/Promotions/Core.php
@@ -23,4 +23,37 @@ class Core extends Base\Core
 
         return $merchantPromotion;
     }
+
+    public function updateCredits(Entity $merchantPromotion)
+    {
+        //TODO fill me
+    }
+
+    public function applyCredits($merchant, $promotion)
+    {
+        $creditInput = [
+            'campaign' => $promotion->getName(),
+            'value'    => $promotion->getAmount(),
+            'type'     => $promotion->getType(),
+        ];
+
+        (new Credits\Core)->create($merchant, $creditInput);
+    }
+
+    public function expireCredits($merchant, $promotion)
+    {
+        $creditInput = [
+            'campaign' => $promotion->getName(),
+            'value'    => $this->calculateCreditToExpire() * -1,
+            'type'     => $promotion->getType(),
+        ];
+
+        (new Credits\Core)->create($merchant, $creditInput);
+    }
+
+    protected function calculateCreditToExpire($merchant, $promotion)
+    {
+        //TODO implement this
+        return 0;
+    }
 }
