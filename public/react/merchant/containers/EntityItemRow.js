d38f394e5ab18000fcb6c4e7562ebc924ba5a06a
selvagsz <selvaganeshbsg@gmail.com>
2017-06-13 17:28:14 +0530

    Minor tweaks in detailRow & entityItemRow components

--- public/react/merchant/containers/EntityItemRow.js
+++ public/react/merchant/containers/EntityItemRow.js
@@ -4,9 +4,11 @@ import { connect } from 'react-redux';
 @connect(state => state.app)
 export default class EnityItemRow extends Component {
   render() {
-    let { id, luminateRowId } = this.props;
+    let { id, luminateRowId, activeEntityId } = this.props;
     return (
-      <tr class={luminateRowId === id ? 'luminate' : null}>
+      <tr
+        class={`${luminateRowId === id ? 'luminate' : null} ${activeEntityId === id ? 'active' : null}`}
+      >
         {this.props.children}
       </tr>
     );
