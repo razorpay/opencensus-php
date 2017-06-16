407badd870f195f45ffea6837bc2ce1ce9cecd46
Ankit Agarwal <ankit.agarwal@razorpay.com>
2017-06-16 14:27:37 +0530

    [Invitation]: Adding merchant name in the user invitations

--- app/Models/Invitation/Entity.php
+++ app/Models/Invitation/Entity.php
@@ -19,8 +19,9 @@ class Entity extends Base\PublicEntity
     const DELETED_AT   = 'deleted_at';
 
     // Other constants
-    const ACTION       = 'action';
-    const SENDER_NAME  = 'sender_name';
+    const ACTION        = 'action';
+    const SENDER_NAME   = 'sender_name';
+    const MERCHANT_NAME = 'merchant_name';
 
     const TOKEN_LENGTH = 40;
 
@@ -76,5 +77,19 @@ class Entity extends Base\PublicEntity
     public function getToken()
     {
         return $this->getAttribute(self::TOKEN);
+    }
+
+    public function toArrayUser()
+    {
+        $attributes = [
+            self::ID            => $this->getAttribute(self::ID),
+            self::EMAIL         => $this->getAttribute(self::EMAIL),
+            self::ROLE          => $this->getAttribute(self::ROLE),
+            self::USER_ID       => $this->getAttribute(self::USER_ID),
+            self::MERCHANT_ID   => $this->getAttribute(self::MERCHANT_ID),
+            self::MERCHANT_NAME => $this->merchant->getName(),
+        ];
+
+        return $attributes;
     }
 }
