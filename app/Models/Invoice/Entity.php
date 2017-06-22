4ab8d7ab48728f3fc1c9029e4871c30e62e0e6c9
jitendra-1217 <jitendraojha1217@gmail.com>
2017-06-22 16:00:12 +0530

    [invoice] Exposes partial_payment attribute publicly, editable in issued state

--- app/Models/Invoice/Entity.php
+++ app/Models/Invoice/Entity.php
@@ -279,7 +279,7 @@ class Entity extends Base\PublicEntity
         self::EMAIL_STATUS,
         self::DATE,
         self::TERMS,
-        // self::PARTIAL_PAYMENT,
+        self::PARTIAL_PAYMENT,
         self::GROSS_AMOUNT,
         self::TAX_AMOUNT,
         self::AMOUNT,
--- app/Models/Invoice/Validator.php
+++ app/Models/Invoice/Validator.php
@@ -147,6 +147,7 @@ class Validator extends Base\Validator
         Entity::NOTES               => 'sometimes|notes',
         Entity::COMMENT             => 'sometimes|string|max:2048',
         Entity::RECEIPT             => 'sometimes|string|min:1|max:40',
+        Entity::PARTIAL_PAYMENT     => 'sometimes|boolean',
     ];
 
     protected static $issueBatchRules = [
