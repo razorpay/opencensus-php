18085fbc6986eed495ebdff862dc790ce009c2a3
jitendra-1217 <jitendraojha1217@gmail.com>
2017-06-23 15:11:28 +0530

    [invoices] Usage mutator for expire_by to set empty string to null

--- app/Models/Invoice/Entity.php
+++ app/Models/Invoice/Entity.php
@@ -334,16 +334,27 @@ class Entity extends Base\PublicEntity
 
     // -------------------------------------- Mutators ---------------
 
+    // Following 2 mutators are for converting '' (empty strings)
+    // input to null.
+
     public function setDateAttribute($date)
     {
-        // To convert '' (empty strings coming from url encoded form data)
-        // to null
         if (empty($date))
         {
             $date = null;
         }
 
         $this->attributes[self::DATE] = $date;
+    }
+
+    public function setExpireByAttribute($expireBy)
+    {
+        if (empty($expireBy) === true)
+        {
+            $expireBy = null;
+        }
+
+        $this->attributes[self::EXPIRE_BY] = $expireBy;
     }
 
     // -------------------------------------- End Mutators -----------
