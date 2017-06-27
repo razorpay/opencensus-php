41bd20aca2de99c7b67371f44059fbb3ebbf4131
Nemo <nemo@razorpay.com>
2017-06-27 13:42:46 +0530

    [upi/mindgate] Adds back mindgate tests

---
+++ tests/Functional/Gateway/Upi/Mindgate/MindgateGatewayTestData.php
@@ -0,0 +1,79 @@
+<?php
+
+use RZP\Error\ErrorCode;
+use RZP\Error\PublicErrorCode;
+use RZP\Error\PublicErrorDescription;
+
+return [
+    'testPayment' => [
+        'merchant_id' => '10000000000000',
+        'amount' => 50000,
+        'method' => 'upi',
+        'status' => 'captured',
+        'amount_authorized' => 50000,
+        'amount_refunded' => 0,
+        'refund_status' => null,
+        'currency' => 'INR',
+        'description' => 'random description',
+        'bank' => null,
+        'error_code' => null,
+        'error_description' => null,
+        'email' => 'a@b.com',
+        'contact' => '9918899029',
+        'notes' => [
+            'merchant_order_id' => 'random order id',
+        ],
+        'gateway' => 'upi_mindgate',
+        'signed' => false,
+        'verified' => null,
+        'entity' => 'payment',
+    ],
+
+    'testFailedCollect' => [
+        'response'  => [
+            'content'     => [
+                'error' => [
+                    'code'        => PublicErrorCode::BAD_REQUEST_ERROR,
+                    'description' => PublicErrorDescription::BAD_REQUEST_PAYMENT_FAILED,
+                ],
+            ],
+            'status_code' => 400,
+        ],
+        'exception' => [
+            'class'               => RZP\Exception\GatewayErrorException::class,
+            'internal_error_code' => ErrorCode::BAD_REQUEST_PAYMENT_FAILED
+        ],
+    ],
+
+    'testVerificationFailure' => [
+        'response'  => [
+            'content'   => [
+                'error' => [
+                    'code'        => PublicErrorCode::BAD_REQUEST_ERROR,
+                    'description' => PublicErrorDescription::BAD_REQUEST_PAYMENT_VERIFICATION_FAILED,
+                ],
+            ],
+            'status_code' => 400,
+        ],
+        'exception' => [
+            'class'                 => RZP\Exception\PaymentVerificationException::class,
+            'internal_error_code'   => ErrorCode::BAD_REQUEST_PAYMENT_VERIFICATION_FAILED
+        ]
+    ],
+
+    'testCollectRejectedFailure' => [
+        'response'  =>  [
+            'content'   =>  [
+                'error' =>  [
+                    'code'          => PublicErrorCode::BAD_REQUEST_ERROR,
+                    'description'   => PublicErrorDescription::BAD_REQUEST_PAYMENT_FAILED
+                ]
+            ],
+            'status_code'           => 400
+        ],
+        'exception' => [
+            'class'                 => RZP\Exception\GatewayErrorException::class,
+            'internal_error_code'   => ErrorCode::BAD_REQUEST_PAYMENT_FAILED
+        ]
+    ],
+];
