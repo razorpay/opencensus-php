980275e8435bb4be41df0877ecbe9a5a38d54418
Vivek <viveklakhera@gmail.com>
2017-06-29 01:10:38 +0530

    [terminal] Add mpesa validator (#3871)

--- app/Models/Terminal/Validator.php
+++ app/Models/Terminal/Validator.php
@@ -232,6 +232,12 @@ class Validator extends Base\Validator
         Entity::GATEWAY_TERMINAL_PASSWORD => 'required|string',
     ];
 
+    protected static $walletMpesaTerminalRules = [
+        Entity::GATEWAY                     => 'required|in:wallet_mpesa',
+        Entity::GATEWAY_MERCHANT_ID         => 'required|string',
+        Entity::GATEWAY_SECURE_SECRET       => 'required|string',
+    ];
+
     protected static $netbankingAirtelTerminalRules = [
         Entity::GATEWAY                     => 'required|in:netbanking_airtel',
         Entity::GATEWAY_MERCHANT_ID         => 'required|string',
