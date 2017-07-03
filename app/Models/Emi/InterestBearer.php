0d2d3db393676d715d6c65b3b63a7152d8ea11fd
Ria Garg <ria.garg@razorpay.com>
2017-07-04 01:23:34 +0530

    removed redundant code

--- app/Models/Emi/InterestBearer.php
+++ app/Models/Emi/InterestBearer.php
@@ -6,21 +6,4 @@ class InterestBearer
 {
     const MERCHANT = 'merchant';
     const CUSTOMER = 'customer';
-
-    protected static $values = [
-        self::PLATFORM => 0,
-        self::CUSTOMER => 1,
-    ];
-
-    public static function getValueForBearerString($bearer)
-    {
-        return self::$values[$bearer];
-    }
-
-    public static function getBearerStringForValue($bearerValue)
-    {
-        $values = array_flip(self::$values);
-
-        return $values[$bearerValue];
-    }
 }
