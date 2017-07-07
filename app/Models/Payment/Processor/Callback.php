e2e1194b38c39da79fdbee1a8ef9011ef7220a2f
Giridaran <giri@razorpay.com>
2017-07-07 14:42:57 +0530

    [allow_billdesk_corporate] Allows pending authorization on s2s callback as well.

--- app/Models/Payment/Processor/Callback.php
+++ app/Models/Payment/Processor/Callback.php
@@ -98,6 +98,22 @@ trait Callback
                         ErrorCode::BAD_REQUEST_PAYMENT_ALREADY_PROCESSED);
                 }
 
+                try {
+
+                    $this->processPaymentCallback($payment, $gatewayInput);
+
+                } catch (Exception\GatewayErrorException $ex)
+                {
+                    if ($ex->getError()->getInternalErrorCode() === ErrorCode::BAD_REQUEST_PAYMENT_PENDING_AUTHORIZATION)
+                    {
+                        return $this->processPendingResponse($payment);
+                    }
+                    else
+                    {
+                        throw $ex;
+                    }
+                }
+
                 $this->processPaymentCallback($payment, $gatewayInput);
 
                 $this->autoCapturePaymentIfApplicable($payment);
