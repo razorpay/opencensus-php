a957d2213e1a1c4f37e347de6f1ba6ffac4fc0a1
Nemo <me@captnemo.in>
2017-07-11 01:25:13 +0530

    Rate limiting (#3829)
    * [deps] Adds laravel-throttle package
    
    * [security] Adds rate-limit middleware
    
    * [rate-limit] Switches to per-minute rate limits
    
    * [rate-limit] Increase Direct Auth rate-limit in live mode
    
    - In discussion with @sunnyagg
    
    * [rate-limit] Disable rate-limits in testing mode
    
    * [rate-limit] Disable in testing mode
    
    * [throttle] Add test case for throttle
    
    * [rate_limiter] Update error description and throttle logic
    
    * [rate_limiter] Fix error description for rate limiter
    
    * [rate_limiter] Fix test

---
+++ tests/Functional/Payment/RateLimitingTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace RZP\Tests\Functional\Payment;
+
+use RZP\Error\ErrorCode;
+use RZP\Tests\Functional\TestCase;
+use RZP\Error\PublicErrorDescription;
+use RZP\Tests\Functional\Helpers\Payment\PaymentTrait;
+
+class RateLimitingTest extends TestCase
+{
+    use PaymentTrait;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->ba->publicAuth();
+
+        $this->app['config']->set('throttle.skip', false);
+    }
+
+    public function tearDown()
+    {
+        $this->app['config']->set('throttle.skip', true);
+
+        parent::tearDown();
+    }
+
+    public function testThrottle()
+    {
+        $payment = $this->getDefaultPaymentArray();
+        $payment['card']['number'] = '5217294025032720';
+
+        $this->app['config']->set('throttle.limits.test.public', 0);
+
+        $response = $this->doAuthPayment($payment);
+
+        $this->assertArrayHasKey('error', $response);
+        $this->assertArrayNotHasKey('razorpay_payment_id', $response);
+
+        $this->assertEquals('Request failed. Please try after sometime.', $response['error']['description']);
+    }
+
+    public function testThrottleWithCallback()
+    {
+        $payment = $this->getDefaultPaymentArray();
+        $payment['card']['number'] = '4111111111111111';
+
+        // For enrolled cards we need the limit to be more than double
+        // of actual values needed for public auth
+        $this->app['config']->set('throttle.limits.test.public', 1);
+
+        $response = $this->doAuthPayment($payment);
+
+        $this->assertArrayHasKey('error', $response);
+        $this->assertArrayNotHasKey('razorpay_payment_id', $response);
+
+        $this->assertEquals('Request failed. Please try after sometime.', $response['error']['description']);
+    }
+
+    public function testThrottleWithMultipleLimits()
+    {
+        $payment = $this->getDefaultPaymentArray();
+        $payment['card']['number'] = '5217294025032720';
+
+        $this->app['config']->set('throttle.limits.test.public', 10);
+
+        $response = $this->doAuthPayment($payment);
+
+        $this->assertArrayHasKey('razorpay_payment_id', $response);
+
+        $paymentId = $response['razorpay_payment_id'];
+
+        $this->app['config']->set('throttle.limits.test.private', 0);
+
+        $response = $this->getEntityById('payment', $paymentId);
+
+        $this->assertArrayHasKey('error', $response);
+        $this->assertEquals('Request failed. Please try after sometime.', $response['error']['description']);
+    }
+}
