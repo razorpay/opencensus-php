e06218f1217fc88abad857dd4fb149db5a6642a3
Patlola <praveen.patlola@razorpay.com>
2017-07-10 08:34:32 +0000

    adding merchant activation details to admin

--- app/Http/Route.php
+++ app/Http/Route.php
@@ -952,6 +952,7 @@ final class Route
         'invitation_action',
         'migrate_tokens_to_gateway_tokens',
         'mock_generate_reconciliation',
+        'merchant_activation_details'
     ];
 
     public static $proxy = [
@@ -1207,6 +1208,7 @@ final class Route
         'merchant_fetch'                   => '*',
         'merchant_get_terminals'           => '*',
         'merchant_get_pricing'             => '*',
+        'merchant_activation_details'      => '*',
     ];
 
     public static $direct = [
