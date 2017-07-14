b65b752a86c3816c9f9be27de2bf163cc981800d
selvagsz <selvaganeshbsg@gmail.com>
2017-07-14 14:43:06 +0530

    Commit missed file

---
+++ public/react/merchant/components/NestedEntityDetailRow.js
@@ -0,0 +1,17 @@
+import EntityDetailRow from 'merchant/components/EntityDetailRow';
+import ListGroupToggler from 'rzp/ui/ListGroupToggler';
+
+export default ({ label, value = {} }) => {
+  if (Object.keys(value).length) {
+    return (
+      <div class="detail-notes">
+        <ListGroupToggler label={label} show={true}>
+          {Object.keys(value).map(key => (
+            <EntityDetailRow label={key} value={value[key]} />
+          ))}
+        </ListGroupToggler>
+      </div>
+    );
+  }
+  return <EntityDetailRow label={label} value="--" />;
+};
