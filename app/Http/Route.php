2d0059fb54b770eae5465cbb1bbdd9d13f830a95
surya15t <surya.teja@razorpay.com>
2017-07-24 18:05:38 +0530

    [admin] moving admin_get_file route to admin auth

--- app/Http/Route.php
+++ app/Http/Route.php
@@ -932,7 +932,6 @@ final class Route
         'merchant_activation_migrate',
         'transaction_create_fees_breakup',
         'billdesk_create_cancelled_refunds',
-        'admin_get_file',
         'offer_deactivate',
         'merchant_patch_beneficiary_code',
         'schedule_fetch',
@@ -1081,6 +1080,7 @@ final class Route
         'schedule_fetch_multiple',
         'feature_delete',
         'admin_dummy_account_test',
+        'admin_get_file',
         // workflows
         'workflow_create',
         'workflow_get',
