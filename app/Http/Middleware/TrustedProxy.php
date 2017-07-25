86e0c7edb9f40d721191e9e52e4bc1e9960bd0bb
Nemo <nemo@razorpay.com>
2017-07-25 14:25:39 +0530

    [http] Drops getRealClientIp
    - We are now setting trusted proxies correctly, so shifts to ip()
      everywhere
    - Also fixed indentation in TrustedProxy middleware

--- app/Http/Middleware/TrustedProxy.php
+++ app/Http/Middleware/TrustedProxy.php
@@ -1,61 +1,68 @@
 <?php
 
- namespace RZP\Http\Middleware;
+namespace RZP\Http\Middleware;
 
- use Closure;
- use Config;
+use Closure;
+use Config;
 
- class TrustedProxy
- {
-     /**
-      * Handle an incoming request.
-      *
-      * @param \Illuminate\Http\Request $request
-      * @param \Closure                 $next
-      *
-      * @throws \Symfony\Component\HttpKernel\Exception\HttpException
-      *
-      * @return mixed
-      */
-     public function handle($request, Closure $next)
-     {
-         // Set trusted header names
-         foreach ($this->getTrustedHeaders() as $headerKey => $headerName)
-         {
-             $request->setTrustedHeaderName($headerKey, $headerName);
-         }
-         $request->setTrustedProxies($this->getTrustedProxies($request->getClientIps()));
+class TrustedProxy
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     *
+     * @throws \Symfony\Component\HttpKernel\Exception\HttpException
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        // Set trusted header names
+        foreach ($this->getTrustedHeaders() as $headerKey => $headerName)
+        {
+            $request->setTrustedHeaderName($headerKey, $headerName);
+        }
 
-         return $next($request);
-     }
-     /**
-      * Return an array of trusted proxy IP addresses.
-      *
-      * @param array $clientIpAddresses Array of client IP addresses retrieved
-      *                                 *prior* to setting trusted proxy
-      *
-      * @return array
-      */
-     protected function getTrustedProxies(array $clientIpAddresses = [])
-     {
-         $trustedProxies = Config::get('trustedproxy.proxies');
+        $proxies = $this->getTrustedProxies($request->getClientIps());
+        $request->setTrustedProxies($proxies);
 
-         return (array) $trustedProxies;
-     }
-     /**
-      * Get trusted header names.
-      *
-      * @return array
-      */
-     protected function getTrustedHeaders()
-     {
-         $trustedHeaderNames = Config::get('trustedproxy.headers');
-         /*
-          * In case the user does not pass an array of header names we
-          * will default to an empty array. This will force defaults from
-          * class \Symfony\Component\HttpFoundation\Request::$trustedHeaders
-          */
-         $trustedHeaderNames = is_array($trustedHeaderNames) ? $trustedHeaderNames : [];
-         return $trustedHeaderNames;
-     }
- }
+        return $next($request);
+    }
+
+    /**
+     * Return an array of trusted proxy IP addresses.
+     *
+     * @param array $clientIpAddresses Array of client IP addresses retrieved
+     *                                  *prior* to setting trusted proxy
+     *
+     * @return array
+     */
+    protected function getTrustedProxies(array $clientIpAddresses = [])
+    {
+        $trustedProxies = Config::get('trustedproxy.proxies');
+
+        return (array) $trustedProxies;
+    }
+
+    /**
+     * Get trusted header names.
+     *
+     * @return array
+     */
+    protected function getTrustedHeaders()
+    {
+        $trustedHeaderNames = Config::get('trustedproxy.headers');
+
+        /*
+         * In case the user does not pass an array of header names we
+         * will default to an empty array. This will force defaults from
+         * class \Symfony\Component\HttpFoundation\Request::$trustedHeaders
+         */
+
+        $trustedHeaderNames = is_array($trustedHeaderNames) ? $trustedHeaderNames : [];
+
+        return $trustedHeaderNames;
+    }
+}
