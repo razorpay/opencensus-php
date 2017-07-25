8e41fa20b91b5ae15d7ca9df6e782966e5466a60
Giridaran <giri@razorpay.com>
2017-07-25 18:08:14 +0530

    [stanc_emi] Adds standard chartered emi.

---
+++ app/Models/Emi/Scbl/EmiFile.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace RZP\Models\Emi\Banks\Scbl;
+
+use Carbon\Carbon;
+use RZP\Models\FileStore;
+use RZP\Models\Emi\Banks\Base;
+
+class EmiFile extends Base\EmiFile
+{
+    protected static $fileToWriteName = 'Scbl_Emi_File';
+
+    protected $emailIdsToSendTo = ['scbl.emi@razorpay.com'];
+
+    protected $bankName  = 'Scbl';
+
+    const EXTENSION = FileStore\Format::XLSX;
+
+    const TYPE = FileStore\Type::SCBL_EMI_FILE;
+
+    protected static $headers = [
+        'CARD_NUMBER',
+        'MID',
+        'MERCHANT_NAM',
+        'TRXN_AMOUNT',
+        'TRXN_DATE',
+        'SETTLEMENT_DATE',
+        'AUTH_CODE',
+        'TENOR',
+        'REDUCING_INTEREST_RATE_P_A',
+        'PROCESSING_FEE',
+        'FORCLOSURE_FEE',
+        'MIN_AMT',
+        'MAX_AMT',
+        'REDUCING_INTEREST_RATE_P_A_1',
+    ];
+
+    protected function getEmiData($input)
+    {
+        $data = [];
+
+        foreach ($input as $emiPayment)
+        {
+            $emiTenure = $emiPayment->emiPlan['duration'];
+
+            $merchant = $this->repo->merchant->fetchMerchantFromEntity($emiPayment);
+
+            $txn = $this->repo->transaction->fetchForPayment($emiPayment);
+
+            $emiRate = $emiPayment->emiPlan['rate']/100;
+
+            $data[] = [
+                'CARD_NUMBER'                  => $this->getCardNumber($emiPayment->card),
+                'MID'                          => $emiPayment->getId(),
+                'MERCHANT_NAME'                => 'Razorpay Payments',
+                'TRXN_AMOUNT'                  => number_format($emiPayment->getAmount()/100, 2),
+                'TRXN_DATE'                    => $this->formattedDateFromTimestamp($emiPayment->getCaptureTimestamp()),
+                'SETTLEMENT_DATE'              => $this->formattedDateFromTimestamp($txn->getSettledAt()),
+                'AUTH_CODE'                    => $this->getAuthCode($emiPayment),
+                'TENOR'                        => $emiTenure,
+                'REDUCING_INTEREST_RATE_P_A'   => '0.' . $emiRate,
+                'PROCESSING_FEE'               => '',
+                'FORCLOSURE_FEE'               => '',
+                'MIN_AMT'                      => '',
+                'MAX_AMT'                      => '',
+                'REDUCING_INTEREST_RATE_P_A_1' => '',
+            ];
+        }
+
+        return $data;
+    }
+
+    private function formattedDateFromTimestamp($timestamp)
+    {
+        return Carbon::createFromTimestamp($timestamp, 'Asia/Kolkata')->format('d/m/Y');
+    }
+}
--- tests/Functional/Fixtures/Entity/EmiPlan.php
+++ tests/Functional/Fixtures/Entity/EmiPlan.php
@@ -55,6 +55,15 @@ class EmiPlan extends Base
             'min_amount'        => '300000',
             'issuer_plan_id'    => '85009'
         ],
+        [
+            'id'                => '10101010101111',
+            'duration'          => '9',
+            'rate'              => '1300',
+            'methods'           => 'debitcard',
+            'bank'              => 'SCBL',
+            'min_amount'        => '300000',
+            'issuer_plan_id'    => '850092'
+        ],
     ];
 
     public function createDefaultEmiPlans()
