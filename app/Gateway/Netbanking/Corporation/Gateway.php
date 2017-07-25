44e4c503601a6a4a0a177a011307d7ce0c333e13
Albin George <albinin0002@gmail.com>
2017-07-25 20:06:41 +0530

    feat(corporation_netbanking): Add initial Gateway

---
+++ app/Gateway/Netbanking/Corporation/Gateway.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace RZP\Gateway\Netbanking\Corporation;
+
+use RZP\Gateway\Netbanking\Base\Entity as NetbankingEntity;
+
+class Gateway extends Base\Gateway
+{
+
+    protected $gateway = 'netbanking_corporation';
+
+    protected $bank = 'corporation';
+
+    protected $tpv;
+
+    protected $map = [
+        'CustID'    => NetbankingEntity::CUSTOMER_ID,
+        'MerCD'     => NetbankingEntity::MERCHANT_CODE,
+        'AMT'       => NetbankingEntity::AMOUNT,
+        'OTC'       => NetbankingEntity::INT_PAYMENT_ID,
+    ];
+
+    public function authorize(array $input)
+    {
+        parent::authorize($input);
+
+        $content = $this->getPaymentRequestData($input);
+
+        $gatewayPayment = $this->createGatewayPaymentEntity($content);
+
+        $request = array(
+            'url' => $this->getUrl('pay'),
+            'method' => 'post',
+            'content' => $content);
+
+        $this->traceGatewayPaymentRequest($request, $input);
+
+        return $request;
+    }
+
+    protected function getPaymentRequestData($input)
+    {
+        $clientCode = $this->stripEmailSpecialChars($input['payment']['email']);
+
+        $data = array(
+            'CustID'            => $clientCode,
+            'MerCD'             => $input['terminal']['gateway_merchant_id'],
+            'AMT'               => $input['payment']['amount'] / 100,
+            'OTC'               => $input['payment']['id'],
+            'MD'                => 'P',
+            'TT'                => 'T',
+        );
+
+        if ($this->mode === Mode::TEST)
+        {
+            $data['MerchantCode'] = 'RAZORPAY';
+        }
+
+        if ($input['merchant']->isTPVRequired())
+        {
+            $data['AcctNo'] = $input['order']['account_number'];
+
+            if ($this->mode === Mode::TEST)
+            {
+                $data['MerchantCode'] = 'RAZORPAY1';
+            }
+        }
+
+        return $data;
+    }
+
+}
---
+++ app/Gateway/Netbanking/Corporation/Url.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace RZP\Gateway\Netbanking\Corporation;
+
+class Url
+{
+    const LIVE_DOMAIN   = 'http://210.212.195.74:7005';
+    const TEST_DOMAIN   = 'http://210.212.195.74:7005';
+
+    const PAY           = '/RetailBankUAT/OLT';
+    const VERIFY        = '/RetailBank/OLTDVER';
+}
