2b22a607fa36497b1844370f2bb7e9d2b6925722
aseemgupta <aseemgupta2625@gmail.com>
2017-07-25 17:13:20 +0530

    Review changes in style

--- public/react/rzp/ui/Toggler/ListToggler.js
+++ public/react/rzp/ui/Toggler/ListToggler.js
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 import AsyncButton from 'react-async-button';
-import BaseToggler from 'rzp/ui/BaseToggler';
+import BaseToggler from 'rzp/ui/Toggler/BaseToggler';
 
 /*
  // USAGE: Check PaymentDetails
--- public/react/rzp/ui/Toggler/NestedEntityDetailToggler.js
+++ public/react/rzp/ui/Toggler/NestedEntityDetailToggler.js
@@ -1,7 +1,7 @@
 import { Component } from 'react';
 import AsyncButton from 'react-async-button';
 import EntityDetailRow from 'merchant/components/EntityDetailRow';
-import BaseToggler from 'rzp/ui/BaseToggler';
+import BaseToggler from 'rzp/ui/Toggler/BaseToggler';
 /*
  // Description: To be used where Key has array of objects and each object has key-value pair
  // USAGE: Check NestedEntityDetailRow
