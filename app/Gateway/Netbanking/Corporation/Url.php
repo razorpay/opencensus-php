a5988caa9853258eb85bf5a66a6d3ba00eb36d3d
Albin George <albinin0002@gmail.com>
2017-07-28 19:07:40 +0530

    feat(corporation_netbanking): Added URLs for test and live

--- app/Gateway/Netbanking/Corporation/Url.php
+++ app/Gateway/Netbanking/Corporation/Url.php
@@ -4,9 +4,9 @@ namespace RZP\Gateway\Netbanking\Corporation;
 
 class Url
 {
-    const LIVE_DOMAIN   = 'http://210.212.195.74:7005';
-    const TEST_DOMAIN   = 'http://210.212.195.74:7005';
+    const LIVE_DOMAIN   = 'http://210.212.195.74:7777';
+    const TEST_DOMAIN   = 'http://210.212.195.74:7777';
 
-    const PAY           = '/RetailBankUAT/OLT';
+    const PAY           = '/RetailBank/OLT';
     const VERIFY        = '/RetailBank/OLTDVER';
 }
