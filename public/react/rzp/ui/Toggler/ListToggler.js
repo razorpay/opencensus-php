e7344835aa3e433b04b6867a7dcc99e02c176669
aseemgupta <aseemgupta2625@gmail.com>
2017-08-01 16:42:30 +0530

    Show no results if no reversals found

--- public/react/rzp/ui/Toggler/ListToggler.js
+++ public/react/rzp/ui/Toggler/ListToggler.js
@@ -12,25 +12,27 @@ export default class ListToggler extends BaseToggler {
     return (
       <div class="list-table">
         <span class="list-label">{this.props.label} • </span>
-        <AsyncButton
-          class="primary-link"
-          text="View all"
-          pendingText="Fetching..."
-          onClick={this.toggle}
-        >
-          {({ buttonText, isPending }) => (
-            <span>
-              {isPending && 'Fetching...'}
-              {!isPending &&
+        {!this.props.totalItems
+          ? <span>No Results</span>
+          : <AsyncButton
+              class="primary-link"
+              text="View all"
+              pendingText="Fetching..."
+              onClick={this.toggle}
+            >
+              {({ buttonText, isPending }) => (
                 <span>
-                  {this.state.show ? 'Hide' : 'View'} {' '}
-                  {this.props.totalItems
-                    ? <span>all <b>{this.props.totalItems} &gt;</b></span>
-                    : null}
-                </span>}
-            </span>
-          )}
-        </AsyncButton>
+                  {isPending && 'Fetching...'}
+                  {!isPending &&
+                    <span>
+                      {this.state.show ? 'Hide' : 'View'} {' '}
+                      {this.props.totalItems
+                        ? <span>all <b>{this.props.totalItems} &gt;</b></span>
+                        : null}
+                    </span>}
+                </span>
+              )}
+            </AsyncButton>}
         {this.state.show
           ? <div class="panel-body" style={{ padding: '15px 0' }}>
               <div class="list-group detail-row-container">
