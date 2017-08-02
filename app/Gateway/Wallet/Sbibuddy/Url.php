8fc0c0927db55a05076803ff977c90e3a3994938
Albin George <albinin0002@gmail.com>
2017-08-02 15:21:58 +0530

    [buddy] Added URLs for test and live

---
+++ app/Gateway/Wallet/Sbibuddy/Url.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace RZP\Gateway\Wallet\Sbibuddy;
+
+class Url
+{
+    const TEST_DOMAIN                = 'https://example.sbibuddy.com/';
+    const LIVE_DOMAIN                = 'https://example.sbibuddy.com/';
+
+    const AUTHORIZE                  = 'mmgw-tls/merchant/page/paynow';
+
+
+    const REFUND                     = 'mmgw-tls/merchant/api/refund';
+    const VERIFY                     = 'mmgw-tls/merchant/api/status';
+    const PAYMENT_STATUS             = 'mmgw-tls/merchant/api/status';
+}
