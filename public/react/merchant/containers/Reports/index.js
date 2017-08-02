aaf751de15505da8658ba59fead290245c300566
aseemgupta <aseemgupta2625@gmail.com>
2017-08-02 15:10:02 +0530

    Style changes

--- public/react/merchant/containers/Reports/index.js
+++ public/react/merchant/containers/Reports/index.js
@@ -48,10 +48,19 @@ export default class ReportsContainer extends Component {
     this.prepareEntityOptions();
   }
 
+  getEntityLabel(value) {
+    let label = null;
+    try {
+      label = this.entityOptions.filter(item => item.value === value)[0].label;
+    } catch (e) {
+      label = 'Some Error';
+    }
+
+    return label;
+  }
+
   prepareEntityOptions() {
     const { user } = this.props;
-
-    console.log(user.tags);
 
     this.entityOptions = [
       {
@@ -214,197 +223,203 @@ export default class ReportsContainer extends Component {
         <header>
           <NavLink to="/reports">Download Reports</NavLink>
         </header>
-        <div class="report-wrapper col-lg-8 col-sm-10 col-xs-11">
-          {/*Report Type Selection*/}
-          <div
-            class={`col-lg-4 col-md-4 col-sm-12 col-xs-12 report-list-panel report-list-panel${this.isMobileDevice ? '--mobile' : '--desktop'}`}
-          >
-            <div class="title">SELECT REPORT TYPE</div>
-            {this.isMobileDevice
-              ? <PowerSelect
-                  options={this.entityOptions}
-                  searchEnabled={false}
-                  selected={this.state.entity}
-                  onChange={({ option }) => {
-                    this.setState({ entity: option }); // only for powerselect view otherwise not consumed elsewhere
-                    this.props.change('entity', option.value); // programmatically set redux-form 'entity' otherwise, powerselect closes before redux-form is updated
-                  }}
-                  optionComponent={({ option }) => (
-                    <div class="reports-entity-options">
-                      <Field
-                        name="entity"
-                        value={option.value}
-                        id={option.id}
-                        component="input"
-                        type="radio"
-                        class="report-type form-control"
-                      />
-                      <label for={option.id}>{option.label}</label>
-                    </div>
-                  )}
-                  selectedOptionComponent={({ option }) => (
-                    <div>{option.label}</div>
-                  )}
-                />
-              : <div>
-                  {this.entityOptions.map((option, index) => (
-                    <div class="reports-entity-options" key={index}>
-                      <Field
-                        name="entity"
-                        value={option.value}
-                        id={option.id}
-                        component="input"
-                        type="radio"
-                        class="report-type form-control"
-                      />
-                      <label for={option.id}>{option.label}</label>
-                    </div>
-                  ))}
+        <content>
+          <div class="report-wrapper col-lg-8 col-sm-10 col-xs-11">
+            {/*Report Type Selection*/}
+            <div
+              class={`col-lg-4 col-md-4 col-sm-12 col-xs-12 report-list-panel report-list-panel${this.isMobileDevice ? '--mobile' : '--desktop'}`}
+            >
+              <div class="title">SELECT REPORT TYPE</div>
+              {this.isMobileDevice
+                ? <PowerSelect
+                    options={this.entityOptions}
+                    searchEnabled={false}
+                    selected={this.state.entity}
+                    onChange={({ option }) => {
+                      this.setState({ entity: option }); // only for powerselect view otherwise not consumed elsewhere
+                      this.props.change('entity', option.value); // programmatically set redux-form 'entity' otherwise, powerselect closes before redux-form is updated
+                    }}
+                    optionComponent={({ option }) => (
+                      <div class="reports-entity-options">
+                        <Field
+                          name="entity"
+                          value={option.value}
+                          id={option.id}
+                          component="input"
+                          type="radio"
+                          class="report-type form-control"
+                        />
+                        <label for={option.id}>{option.label}</label>
+                      </div>
+                    )}
+                    selectedOptionComponent={({ option }) => (
+                      <div>{option.label}</div>
+                    )}
+                  />
+                : <div>
+                    {this.entityOptions.map((option, index) => (
+                      <div class="reports-entity-options" key={index}>
+                        <Field
+                          name="entity"
+                          value={option.value}
+                          id={option.id}
+                          component="input"
+                          type="radio"
+                          class="report-type form-control"
+                        />
+                        <label for={option.id}>{option.label}</label>
+                      </div>
+                    ))}
+                  </div>}
+            </div>
+            {/*Report Generate Panel*/}
+            <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12 report-generate-panel">
+              {!this.isMobileDevice &&
+                <div class="form-heading">
+                  {this.props.entity && this.getEntityLabel(this.props.entity)}
                 </div>}
-          </div>
-          {/*Report Generate Panel*/}
-          <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12 report-generate-panel">
-            <div class="form-element">
-              <div class="title">
+              <div class="form-element">
+                <div class="title">
+                  {isMarketplace &&
+                    ['transaction', 'payment', 'refund', 'settlement'].indexOf(
+                      this.props.entity
+                    ) > -1
+                    ? 'SELECT '
+                    : ''}
+                  ACCOUNT
+                </div>
                 {isMarketplace &&
                   ['transaction', 'payment', 'refund', 'settlement'].indexOf(
                     this.props.entity
                   ) > -1
-                  ? 'SELECT '
-                  : ''}
-                ACCOUNT
-              </div>
-              {isMarketplace &&
-                ['transaction', 'payment', 'refund', 'settlement'].indexOf(
-                  this.props.entity
-                ) > -1
-                ? <div class="custom-select" style={{ position: 'relative' }}>
-                    <i class="icon icon-search custom-icon" />
-                    <div
-                      class="typeAheadSkin"
-                      ref={typeAheadSkin => {
-                        this.typeAheadSkin = typeAheadSkin;
-                      }}
-                    >
-                      {this.state.merchantSelected
-                        ? <div>
-                            <b style={{ marginRight: '5' }}>
-                              {this.state.merchantSelected.name}
-                            </b>
-                            <span>- {this.state.merchantSelected.id}</span>
+                  ? <div class="custom-select" style={{ position: 'relative' }}>
+                      <i class="icon icon-search custom-icon" />
+                      <div
+                        class="typeAheadSkin"
+                        ref={typeAheadSkin => {
+                          this.typeAheadSkin = typeAheadSkin;
+                        }}
+                      >
+                        {this.state.merchantSelected
+                          ? <div>
+                              <b style={{ marginRight: '5' }}>
+                                {this.state.merchantSelected.name}
+                              </b>
+                              <span>- {this.state.merchantSelected.id}</span>
+                              <span
+                                class={`${this.isMobileDevice ? this.state.merchantSelected.tagIcon + ' icon' : ''} custom-tag`}
+                              >
+                                {this.isMobileDevice
+                                  ? ''
+                                  : this.state.merchantSelected.tag}
+                              </span>
+                            </div>
+                          : null}
+                      </div>
+
+                      <TypeAhead
+                        options={merchantAccounts}
+                        placeholder="Search for merchant Name/Email/Merchant ID"
+                        optionLabelPath="name"
+                        onClick={() => {
+                          this.typeAheadSkin.classList.add('hide');
+                        }}
+                        onBlur={() => {
+                          this.typeAheadSkin.classList.remove('hide');
+                        }}
+                        searchIndices={['name', 'id', 'email']}
+                        selected={this.state.merchantSelected}
+                        optionComponent={({ option }) => (
+                          <div style={{ padding: 5 }}>
+                            <b style={{ marginRight: '5' }}>{option.name}</b>
+                            <span>- {option.id}</span>
                             <span
-                              class={`${this.isMobileDevice ? this.state.merchantSelected.tagIcon + ' icon' : ''} custom-tag`}
+                              class={`${this.isMobileDevice ? option.tagIcon + ' icon' : ''} custom-tag`}
                             >
-                              {this.isMobileDevice
-                                ? ''
-                                : this.state.merchantSelected.tag}
+                              {this.isMobileDevice ? '' : option.tag}
                             </span>
                           </div>
-                        : null}
+                        )}
+                        selectedOptionComponent={({ option }) => (
+                          <div>
+                            <b style={{ marginRight: '5' }}>{option.name}</b>
+                            <span>- {option.id}</span>
+                          </div>
+                        )}
+                        onChange={({ option }) => {
+                          if (option) {
+                            this.setState({ merchantSelected: option });
+                          }
+                        }}
+                      />
                     </div>
-
-                    <TypeAhead
-                      options={merchantAccounts}
-                      placeholder="Search for merchant Name/Email/Merchant ID"
-                      optionLabelPath="name"
-                      onClick={() => {
-                        this.typeAheadSkin.classList.add('hide');
-                      }}
-                      onBlur={() => {
-                        this.typeAheadSkin.classList.remove('hide');
-                      }}
-                      searchIndices={['name', 'id', 'email']}
-                      selected={this.state.merchantSelected}
-                      optionComponent={({ option }) => (
-                        <div style={{ padding: 5 }}>
-                          <b style={{ marginRight: '5' }}>{option.name}</b>
-                          <span>- {option.id}</span>
-                          <span
-                            class={`${this.isMobileDevice ? option.tagIcon + ' icon' : ''} custom-tag`}
-                          >
-                            {this.isMobileDevice ? '' : option.tag}
-                          </span>
-                        </div>
-                      )}
-                      selectedOptionComponent={({ option }) => (
-                        <div>
-                          <b style={{ marginRight: '5' }}>{option.name}</b>
-                          <span>- {option.id}</span>
-                        </div>
-                      )}
-                      onChange={({ option }) => {
-                        if (option) {
-                          this.setState({ merchantSelected: option });
-                        }
-                      }}
-                    />
-                  </div>
-                : <div class="account">
-                    <strong>{user.name || user.user.name}</strong>
-                  </div>}
-            </div>
-
-            <div class="form-element">
-              <div class="title">
-                DATE RANGE
+                  : <div class="account">
+                      <strong>{user.name || user.user.name}</strong>
+                    </div>}
               </div>
-              {entity === 'invoice' ||
-                <div class="col-sm-3">
-                  <div class="form-group">
-                    <Field name="type" class="form-control" component="select">
-                      <option value="daily">Daily</option>
-                      <option value="monthly">Monthly</option>
-                    </Field>
-                  </div>
-                </div>}
 
-              {(type === 'monthly' || entity === 'invoice') &&
-                <div class="col-sm-4">
-                  <div class="form-group">
-                    <Field
-                      name="date"
-                      component={ReduxDatetime}
-                      dateFormat="MMM, YYYY"
-                      closeOnSelect={true}
-                      isValidDate={validYear}
-                      placeholder="Select Year-Month"
-                      timeFormat={false}
-                    />
-                  </div>
-                </div>}
+              <div class="form-element">
+                <div class="title">
+                  DATE RANGE
+                </div>
+                {entity === 'invoice' ||
+                  <div class="col-sm-3 col-xs-12">
+                    <div class="form-group form-control">
+                      <Field name="type" class="fix-select" component="select">
+                        <option value="daily">Daily</option>
+                        <option value="monthly">Monthly</option>
+                      </Field>
+                    </div>
+                  </div>}
 
-              {type === 'daily' &&
-                entity !== 'invoice' &&
-                <div class="col-sm-4">
-                  <div class="form-group">
-                    <Field
-                      name="date"
-                      dateFormat="DD MMM, YYYY"
-                      closeOnSelect={true}
-                      component={ReduxDatetime}
-                      placeholder="Select Date-Month-Year"
-                      isValidDate={validYear}
-                      timeFormat={false}
-                    />
-                  </div>
-                </div>}
-            </div>
+                {(type === 'monthly' || entity === 'invoice') &&
+                  <div class="col-sm-4 col-xs-12">
+                    <div class="form-group">
+                      <Field
+                        name="date"
+                        component={ReduxDatetime}
+                        dateFormat="MMM, YYYY"
+                        closeOnSelect={true}
+                        isValidDate={validYear}
+                        placeholder="Select Year-Month"
+                        timeFormat={false}
+                      />
+                    </div>
+                  </div>}
 
-            <div class="form-element">
-              <AsyncButton
-                class="btn btn-primary"
-                onClick={handleSubmit(this.prepareGenerateReport)}
-                text="Generate and Download Report"
-                pendingText="Generating..."
-              />
+                {type === 'daily' &&
+                  entity !== 'invoice' &&
+                  <div class="col-sm-4 col-xs-12">
+                    <div class="form-group">
+                      <Field
+                        name="date"
+                        dateFormat="DD MMM, YYYY"
+                        closeOnSelect={true}
+                        component={ReduxDatetime}
+                        placeholder="Select Date-Month-Year"
+                        isValidDate={validYear}
+                        timeFormat={false}
+                      />
+                    </div>
+                  </div>}
+              </div>
 
-              <footer>
-                Combined reports will include transactions on the given date, as well as payments
-                settled on that given date.
-              </footer>
+              <div class="form-element">
+                <AsyncButton
+                  class="btn btn-primary"
+                  onClick={handleSubmit(this.prepareGenerateReport)}
+                  text="Generate and Download Report"
+                  pendingText="Generating..."
+                />
+
+                <footer>
+                  Combined reports will include transactions on the given date, as well as payments
+                  settled on that given date.
+                </footer>
+              </div>
             </div>
           </div>
-        </div>
+        </content>
       </tabbed-container>
     );
   }
