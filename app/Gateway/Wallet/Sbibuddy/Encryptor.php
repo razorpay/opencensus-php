cca8e309b4704fccd1a83d549a8d5acf898ab62e
Albin George <albinin0002@gmail.com>
2017-08-03 20:03:36 +0530

    [buddy] Correct Encryptor to use the base cryptor

--- app/Gateway/Wallet/Sbibuddy/Encryptor.php
+++ app/Gateway/Wallet/Sbibuddy/Encryptor.php
@@ -2,74 +2,26 @@
 
 namespace RZP\Gateway\Wallet\Sbibuddy;
 
-use phpseclib\Crypt\AES;
+use RZP\Gateway\Netbanking\Base;
 
-class Encryptor
+class Encryptor extends Base\AESCrypto
 {
-    protected $masterKey;
 
-    protected $blockSize = 8;
-
-    protected $method = 'aes-128-ecb';
-
-    public function __construct($masterKey)
-    {
-        $this->aes = new AES(MCRYPT_MODE_ECB);
-
-        $this->aes->setKey($masterKey);
-
-        $this->aes->setKeyLength(128);
-    }
-
-    public function setBlockSize($blockSize)
-    {
-        $this->blockSize = $blockSize;
-    }
-
-    public function setMethod($method)
-    {
-        $this->method = $method;
-    }
-
-    public function encrypt($data)
+    public function encryptString(string $data)
     {
         $encoded = utf8_encode($data);
 
-        $encryptedData = $this->aes->encrypt($encoded);
+        $encryptedData = parent::encryptString($encoded);
 
         return base64_encode($encryptedData);
     }
 
-    public function decrypt($data)
+    public function decryptString(string $data)
     {
         $decoded = base64_decode($data);
 
-        $decryptedData = $this->aes->decrypt($decoded);
+        $decryptedData = parent::decryptString($decoded);
 
         return utf8_decode($decryptedData);
-    }
-
-    public static function pkcs5Pad($text, $blocksize)
-    {
-        $pad = $blocksize - (strlen($text) % $blocksize);
-
-        return $text . str_repeat(chr($pad), $pad);
-    }
-
-    public static function pkcs5Unpad($text)
-    {
-        $pad = ord($text{strlen($text) - 1});
-
-        if ($pad > strlen($text))
-        {
-            return false;
-        }
-
-        if (strspn($text, chr($pad), strlen($text) - $pad) != $pad)
-        {
-            return false;
-        }
-
-        return substr($text, 0, -1 * $pad);
     }
 }
