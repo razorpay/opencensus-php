46c9d057da24bb13b759db17058a2cada59eae44
Ria Garg <ria.garg@razorpay.com>
2017-08-04 17:17:21 +0530

    Adding Force Authorization in Indusind (#4520)
    * Adding Force Authorization in Indusind
    
    * added to list of force authorization gateways

--- app/Gateway/Netbanking/Indusind/Gateway.php
+++ app/Gateway/Netbanking/Indusind/Gateway.php
@@ -264,6 +264,26 @@ class Gateway extends Base\Gateway
         }
     }
 
+    public function forceAuthorizeFailed($input)
+    {
+        $gatewayPayment = $this->repo->findByPaymentIdAndAction(
+                                    $input['payment']['id'],
+                                    Payment\Action::AUTHORIZE);
+
+        // If it's already authorized on gateway side, We just return back.
+        if (($gatewayPayment->getReceived() === true) and
+            ($gatewayPayment->getStatus() === Constants::YES))
+        {
+            return true;
+        }
+
+        $gatewayPayment->setStatus(Constants::YES);
+
+        $this->repo->saveOrFail($gatewayPayment);
+
+        return true;
+    }
+
     public function getPid(): string
     {
         $pId = $this->getLiveMerchantId();
