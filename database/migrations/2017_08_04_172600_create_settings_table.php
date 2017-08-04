162074e6084fb53712ceb6b8b6f834aef9f2cd32
Amit Mahbubani <amit.mahbubani@razorpay.com>
2017-08-04 14:58:50 +0530

    [settings] Add config and migration

---
+++ database/migrations/2017_08_04_172600_create_settings_table.php
@@ -0,0 +1,64 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Config;
+
+use RZP\Models\Base\PublicEntity;
+
+class CreateSettingsTable extends Migration
+{
+    /**
+     * @var string
+     */
+    protected $tableName;
+
+    /**
+     * @var string
+     */
+    protected $keyColumn;
+
+    /**
+     * @var string
+     */
+    protected $valueColumn;
+
+	public function __construct()
+	{
+        $this->tableName   = Config::get('settings.table');
+        $this->keyColumn   = Config::get('settings.keyColumn');
+        $this->valueColumn = Config::get('settings.valueColumn');
+	}
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create($this->tableName, function(Blueprint $table)
+		{
+			$table->increments('id');
+
+			$table->string('entity_type', 100);
+
+			$table->char('entity_id', PublicEntity::ID_LENGTH);
+
+			$table->string($this->keyColumn)
+                  ->index();
+
+			$table->text($this->valueColumn);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop($this->tableName);
+	}
+}
