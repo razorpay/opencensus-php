b0e284d52bab090954830eb89b4605ce67efbb84
Amit Mahbubani <amit.mahbubani@razorpay.com>
2017-08-06 23:07:08 +0530

    [settings] Add Settings service methods

--- app/Models/Settings/Service.php
+++ app/Models/Settings/Service.php
@@ -1,7 +1,53 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: amitmahbubani
- * Date: 05/08/17
- * Time: 3:05 PM
- */
+
+namespace RZP\Models\Settings;
+
+use RZP\Models\Base;
+
+class Service extends Base\Service
+{
+    public function get(string $entity, string $id, string $key): array
+    {
+        $entity = $this->fetchEntity($entity, $id);
+
+        $settings = Core::for($entity)->get($key);
+
+        return ['settings' => $settings];
+    }
+
+    public function getAll(string $entity, string $id): array
+    {
+        $entity = $this->fetchEntity($entity, $id);
+
+        $settings = Core::for($entity)->all();
+
+        return ['settings' => $settings];
+    }
+
+    public function upsert(string $entity, string $id, array $input)
+    {
+        // Validate input
+
+        $entity = $this->fetchEntity($entity, $id);
+
+        Core::for($entity)->create($input);
+    }
+
+    public function delete(string $entity, string $id, string $key)
+    {
+        $entity = $this->fetchEntity($entity, $id);
+
+        Core::for($entity)->delete($key);
+    }
+
+    protected function fetchEntity(string $entity, string $id): Base\PublicEntity
+    {
+        // Validate Entity whitelisted
+
+        $entity = $this->repo
+                       ->$entity
+                       ->findOrFailPublic($id);
+
+        return $entity;
+    }
+}
