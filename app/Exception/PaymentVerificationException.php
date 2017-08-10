f3c703b97f07ac8ed1a58d95e960a10df22fc51b
gaurav <gaurav.d@razorpay.com>
2017-08-10 16:27:19 +0530

    take default param instead of checking for null

--- app/Exception/PaymentVerificationException.php
+++ app/Exception/PaymentVerificationException.php
@@ -23,14 +23,9 @@ class PaymentVerificationException extends RecoverableException
     public function __construct(
         $data,
         $verify,
-        $code = null,
+        $code = ErrorCode::BAD_REQUEST_PAYMENT_VERIFICATION_FAILED,
         \Exception $previous = null)
     {
-        if ($code === null)
-        {
-            $code = ErrorCode::BAD_REQUEST_PAYMENT_VERIFICATION_FAILED;
-        }
-
         $this->error = new Error($code, null, null, $data);
 
         $this->data = $data;
