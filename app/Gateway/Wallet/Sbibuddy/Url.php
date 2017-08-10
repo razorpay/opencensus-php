be9bc4fe6db2a3d7f04e8691c4752b45120f03e0
Albin George <albinin0002@gmail.com>
2017-08-10 13:08:10 +0530

    [buddy] Alignment fix

--- app/Gateway/Wallet/Sbibuddy/Url.php
+++ app/Gateway/Wallet/Sbibuddy/Url.php
@@ -4,13 +4,13 @@ namespace RZP\Gateway\Wallet\Sbibuddy;
 
 class Url
 {
-    const TEST_DOMAIN                = 'https://buddyuat.sbi.co.in/';
-    const LIVE_DOMAIN                = 'https://example.sbibuddy.com/';
+    const TEST_DOMAIN    = 'https://buddyuat.sbi.co.in/';
+    const LIVE_DOMAIN    = 'https://example.sbibuddy.com/';
 
-    const AUTHORIZE                  = 'mmgw-tls/merchant/page/paynow';
+    const AUTHORIZE      = 'mmgw-tls/merchant/page/paynow';
 
 
-    const REFUND                     = 'mmgw-tls/merchant/api/refund';
-    const VERIFY                     = 'mmgw-tls/merchant/api/status';
-    const PAYMENT_STATUS             = 'mmgw-tls/merchant/api/status';
+    const REFUND         = 'mmgw-tls/merchant/api/refund';
+    const VERIFY         = 'mmgw-tls/merchant/api/status';
+    const PAYMENT_STATUS = 'mmgw-tls/merchant/api/status';
 }
