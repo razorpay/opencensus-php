f992231a6a199ffcbe0beaf72f4733a1d0180cac
Albin George <albinin0002@gmail.com>
2017-08-11 12:40:03 +0530

    [corp_nb] Added request and response fields

---
+++ app/Gateway/Netbanking/Corporation/RequestFields.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace RZP\Gateway\Netbanking\Corporation;
+
+class RequestFields
+{
+    const CUSTOMER_ID          = 'CustID';
+    const MERCHANT_CODE        = 'MerCD';
+    const AMOUNT               = 'AMT';
+    const PAYMENT_ID           = 'OTC';
+    const MODE_OF_TRANSACTION  = 'MD';
+    const FUND_TRANSFER        = 'TT';
+}
---
+++ app/Gateway/Netbanking/Corporation/ResponseFields.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace RZP\Gateway\Netbanking\Corporation;
+
+class ResponseFields
+{
+    const MODE_OF_TRANSACTION   = 'MD';
+    const MERCHANT_CODE         = 'MerCD';
+    const PAYMENT_ID            = 'OTC';
+    const CUSTOMER_ID           = 'CustID';
+    const AMOUNT                = 'AMT';
+    const FUND_TRANSFER         = 'TT';
+    const BANK_REF_NUMBER       = 'BRN';
+    const STATUS                = 'Status';
+}
