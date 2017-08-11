3fbe75d8a8460b9cd2fa725ae13507bc657a428c
Harman Singh <harman28@gmail.com>
2017-08-11 19:05:03 +0530

    [recon] Removes unused constants

--- app/Reconciliator/VirtualAccKotak/Reconciliate.php
+++ app/Reconciliator/VirtualAccKotak/Reconciliate.php
@@ -6,42 +6,22 @@ use RZP\Reconciliator\Base;
 
 class Reconciliate extends Base\Reconciliate
 {
-    /*******************
-     * Row Header Names
-     ******************/
-
-    const TXN_DATE                = 'txn_date';
-    const TXN_REF_NO              = 'txn_ref_no';
-    const E_COLL_AC_NO            = 'e_coll_ac_no';
-    const DEALER_NAME             = 'dealer_name';
-    const MASTER_AC_NO            = 'master_ac_no';
-    const AMOUNT                  = 'amount';
-    const BENE_CUST_ACNAME        = 'bene_cust_acname';
-    const SEND_CUST_ACNAME        = 'send_cust_acname';
-    const SEND_CUST_AC_NO         = 'send_cust_ac_no';
-    const REMITT_INFO             = 'remitt_info';
-    const SND_BRN_IFSC            = 'snd_brn_ifsc';
-    const CUSTOMER_CODE           = 'customer_code';
-    const REF2                    = 'ref2';
-    const REF3                    = 'ref3';
-    const CREDIT_TIME             = 'credit_time';
-
     const HEADERS = [
-        self::TXN_DATE,
-        self::TXN_REF_NO,
-        self::E_COLL_AC_NO,
-        self::DEALER_NAME,
-        self::MASTER_AC_NO,
-        self::AMOUNT,
-        self::BENE_CUST_ACNAME,
-        self::SEND_CUST_ACNAME,
-        self::SEND_CUST_AC_NO,
-        self::REMITT_INFO,
-        self::SND_BRN_IFSC,
-        self::CUSTOMER_CODE,
-        self::REF2,
-        self::REF3,
-        self::CREDIT_TIME,
+        'txn_date',
+        'txn_ref_no',
+        'e_coll_ac_no',
+        'dealer_name',
+        'master_ac_no',
+        'amount',
+        'bene_cust_acname',
+        'send_cust_acname',
+        'send_cust_ac_no',
+        'remitt_info',
+        'snd_brn_ifsc',
+        'customer_code',
+        'ref2',
+        'ref3',
+        'credit_time',
     ];
 
     /**
--- app/Reconciliator/VirtualAccKotak/SubReconcilator/PaymentReconciliate.php
+++ app/Reconciliator/VirtualAccKotak/SubReconcilator/PaymentReconciliate.php
@@ -7,6 +7,10 @@ use RZP\Reconciliator\Base;
 
 class PaymentReconciliate extends Base\PaymentReconciliate
 {
+    const TXN_REF_NO              = 'txn_ref_no';
+    const AMOUNT                  = 'amount';
+    const SEND_CUST_ACNAME        = 'send_cust_acname';
+
     /**
      * Identify the bank transfer using UTR, and thus find payment
      *
@@ -15,9 +19,9 @@ class PaymentReconciliate extends Base\PaymentReconciliate
      */
     protected function getPaymentId($row)
     {
-        if (isset($row[Reconciliate::TXN_REF_NO]) === true)
+        if (isset($row[self::TXN_REF_NO]) === true)
         {
-            $utr = $row[Reconciliate::TXN_REF_NO];
+            $utr = $row[self::TXN_REF_NO];
         }
         else
         {
@@ -47,7 +51,7 @@ class PaymentReconciliate extends Base\PaymentReconciliate
      */
     protected function getGatewayPaymentAmount($row)
     {
-        $paymentAmount = floatval($row[Reconciliate::AMOUNT]) * 100;
+        $paymentAmount = floatval($row[self::AMOUNT]) * 100;
 
         // We are converting to int after casting to string as PHP randomly
         // returns wrong int values due to differing floating point precisions
@@ -108,15 +112,15 @@ class PaymentReconciliate extends Base\PaymentReconciliate
     protected function getNbCustomerDetails($row)
     {
         return [
-            Base\Reconciliate::CUSTOMER_NAME => $this->getCustomerName($row),
+            Base\self::CUSTOMER_NAME => $this->getCustomerName($row),
         ];
     }
 
     protected function getCustomerName($row)
     {
-        if (empty($row[Reconciliate::SEND_CUST_ACNAME]) === false)
+        if (empty($row[self::SEND_CUST_ACNAME]) === false)
         {
-            return $row[Reconciliate::SEND_CUST_ACNAME];
+            return $row[self::SEND_CUST_ACNAME];
         }
 
         return null;
