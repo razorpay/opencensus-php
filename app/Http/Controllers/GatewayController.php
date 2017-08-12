4a203277e1b3d9a849b2e8d93a9ed59f66015cfa
Albin George <albinin0002@gmail.com>
2017-08-12 20:30:33 +0530

    [corp_nb] Correct gateway controller

--- app/Http/Controllers/GatewayController.php
+++ app/Http/Controllers/GatewayController.php
@@ -181,38 +181,9 @@ class GatewayController extends Controller
             [ 'input' => $input ]
         );
 
-        $paymentId = $input[Corporation\ResponseFields::PAYMENT_ID];
+        $data = $this->processServerCallback($input, 'netbanking_corporation');
 
-        $mode = $this->app['repo']->determineLiveOrTestModeForEntity($paymentId, 'payment');
-
-        $this->app['config']->set('database.default', $mode);
-
-        $netbanking = $this->app['repo']->netbanking->findByPaymentIdAndAction(
-            $paymentId,
-            \RZP\Gateway\Base\Action::AUTHORIZE
-        );
-
-        if ($netbanking === null)
-        {
-            throw new Exception\BadRequestValidationFailureException(
-                'Failed to find requisite payment id: ' . $paymentId);
-        }
-
-        $publicPaymentId = $netbanking->getPublicPaymentId();
-
-        $payment = $this->repo->payment->findOrFailPublic($paymentId);
-
-        $keys = $this->repo->key->getKeysForMerchant($payment->getMerchantId());
-
-        $publicKey = $keys->first()->getPublicKey($mode);
-
-        $url = $this->route->getPublicCallbackUrlWithHash($publicPaymentId, $publicKey);
-
-        $inputMsg = http_build_query($input);
-
-        $url = $url . '?' . $inputMsg;
-
-        return Redirect::to($url);
+        return ApiResponse::json($data);
     }
 
     protected function getNetbankingEntityAndModeByTraceId($traceId)
