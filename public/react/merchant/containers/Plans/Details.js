9f68581e9ffedba9aba5bd1120c69b3b927b5e79
aseemgupta <aseemgupta2625@gmail.com>
2017-08-14 17:59:26 +0530

    Fetch subscriptions for planid independent of subscriptions model

--- public/react/merchant/containers/Plans/Details.js
+++ public/react/merchant/containers/Plans/Details.js
@@ -2,27 +2,28 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PlanDetails from 'merchant/components/Plans/Details';
 import { fetchPlan as fetchItem } from 'merchant/modules/plans';
-import { fetchSubscriptions } from 'merchant/modules/subscriptions';
+import {
+  fetchSubscriptionsByPlanId as fetchSubscriptions,
+} from 'merchant/modules/plans';
 
 @connect(
   state => ({
     ...state.plan,
-    subscriptions: state.subscriptions,
   }),
   { fetchItem, fetchSubscriptions }
 )
 export default class PlanDetailsContainer extends Component {
   componentWillMount() {
     this.props.fetchItem(this.props.id).then(() => {
-      this.props.fetchSubscriptions({
-        plan_id: this.props.id,
-      });
+      this.props.fetchSubscriptions(this.props.entity);
     });
   }
 
   componentWillReceiveProps(nextProps) {
     if (this.props.id !== nextProps.id) {
-      this.props.fetchItem(nextProps.id);
+      this.props.fetchItem(nextProps.id).then(() => {
+        this.props.fetchSubscriptions(nextProps.entity);
+      });
     }
   }
 
--- public/react/merchant/modules/plans.js
+++ public/react/merchant/modules/plans.js
@@ -1,12 +1,14 @@
 import Plan from 'merchant/models/Plan';
 import { makeActionCollectionReducer, fetchAll } from 'rzp/modules/collection';
 import { makeEntityReducer, updateEntity } from 'rzp/modules/entity';
+import { set } from 'rzp/utils/immutable';
 
 export const PLANS_FETCH = 'PLANS_FETCH';
 export const PLAN_CREATE = 'PLAN_CREATE';
 export const PLAN_EDIT = 'PLAN_EDIT';
 export const PLAN_DELETE = 'PLAN_DELETE';
 export const PLAN_FETCH = 'PLAN_FETCH';
+export const PLAN_FETCH_SUBSCRIPTIONS = 'PLAN_FETCH_SUBSRIPTIONS';
 
 export const fetchPlans = params => fetchAll(params, Plan, 'PLANS');
 
@@ -15,6 +17,13 @@ export const fetchPlan = id => {
   return {
     type: PLAN_FETCH,
     payload: plan.fetch(id),
+  };
+};
+
+export const fetchSubscriptionsByPlanId = plan => {
+  return {
+    type: PLAN_FETCH_SUBSCRIPTIONS,
+    payload: plan.fetchSubscriptions(),
   };
 };
 
@@ -36,8 +45,37 @@ export const deletePlan = params => {
   };
 };
 
+const updateSubscriptions = isPending => (state, action) => {
+  if (isPending) {
+    return set(state, 'subscriptions', {
+      loading: true,
+      items: [],
+    });
+  }
+
+  return set(state, 'subscriptions', {
+    loading: false,
+    items: action.payload.data.items,
+  });
+};
+
 // List Reducer
 export const plansReducer = makeActionCollectionReducer('PLANS');
 
+// Plan Details Initial State
+let planInitialState = {
+  subscriptions: {
+    loading: false,
+    items: [],
+  },
+};
+
 // Details Reducer
-export const planReducer = makeEntityReducer(PLAN_FETCH);
+export const planReducer = makeEntityReducer(
+  PLAN_FETCH,
+  {
+    [`${PLAN_FETCH_SUBSCRIPTIONS}::PENDING`]: updateSubscriptions(true),
+    [`${PLAN_FETCH_SUBSCRIPTIONS}::SUCCESS`]: updateSubscriptions(false),
+  },
+  planInitialState
+);
