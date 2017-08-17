93ec8d95e1e62de0d368611402697b9d12e34702
Ria Garg <ria.garg@razorpay.com>
2017-08-17 21:04:52 +0530

    Added Settlement Flow for IRCTC (#4718)

---
+++ app/Models/Merchant/FileProcessor/Irctc/Settlement.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace RZP\Models\Merchant\FileProcessor\Irctc;
+
+use RZP\Models\Merchant\FileProcessor\Base;
+use RZP\Models\Payment\Processor\Processor as PaymentProcessor;
+
+class Settlement extends Base
+{
+    const PAYMENT_ID         = 'payment_id';
+    const PAYMENT_AMOUNT     = 'payment_amount';
+    const TRANSACTION_DATE   = 'transaction_date';
+    const MERCHANT_REFERENCE = 'merchant_reference';
+
+    const HEADERS = [
+        self::PAYMENT_ID,
+        self::PAYMENT_AMOUNT,
+        self::TRANSACTION_DATE,
+        self::MERCHANT_REFERENCE,
+    ];
+
+    protected function processEntry(array $row)
+    {
+        $paymentId = $row[self::PAYMENT_ID];
+
+        $payment = $this->repo->payment->findByPublicId($paymentId);
+
+        $paymentProcessor = (new PaymentProcessor($payment->merchant));
+
+        $paymentProcessor->capture($paymentId);
+
+        return true;
+    }
+
+    public function getHeaders()
+    {
+        return self::HEADERS;
+    }
+
+    public function getDelimiter()
+    {
+        return '|';
+    }
+}
