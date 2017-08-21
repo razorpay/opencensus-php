ba4ac5d2172a8b16742d8ed482722fdf7cf93263
aseemgupta <aseemgupta2625@gmail.com>
2017-08-21 19:41:51 +0530

    Add pop up for edit/create add-on

---
+++ public/react/merchant/containers/AddOns/New.js
@@ -0,0 +1,137 @@
+import { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { Field, FieldArray, reduxForm } from 'redux-form';
+import AsyncButton from 'react-async-button';
+import InputField from 'rzp/ui/Forms/InputField';
+import InputGroupField from 'rzp/ui/Forms/InputField/InputGroupField';
+import Alert from 'rzp/ui/Forms/Alert';
+import ModalHeader from 'rzp/ui/ModalHeader';
+import { required } from 'rzp/utils/validators';
+import { savePlan } from 'merchant/modules/plans';
+import * as ModalActions from 'rzp/modules/modals';
+import { showNotification } from 'rzp/modules/notifications';
+
+@connect(null, {
+  savePlan,
+  showNotification,
+  ...ModalActions,
+})
+@reduxForm({
+  form: 'newAddOn',
+  initialValues: {
+    item: {
+      currency: 'INR',
+    },
+  },
+})
+export default class CreateAddOn extends Component {
+  state = {};
+
+  componentWillMount() {
+    if (this.props.addon) {
+      this.props.initialize(this.props.addon);
+    }
+  }
+
+  save = props => {
+    return this.props
+      .savePlan(props)
+      .then(addon => {
+        this.props.onSave(addon);
+        this.props.showNotification({
+          type: 'success',
+          message: 'Add-on details successfully created',
+        });
+      })
+      .catch(err => {
+        this.setState({
+          errors: err.errors,
+        });
+      });
+  };
+
+  render() {
+    const { handleSubmit, invalid, addon } = this.props;
+
+    return (
+      <div class="addon-create">
+        <ModalHeader
+          title={addon && addon.id ? 'Edit Add-on' : 'Create Add-on'}
+          onCloseClick={this.props.closeModal}
+        />
+
+        <div class="modal-body">
+          <Alert type="error" message={this.state.errors} />
+
+          <form onSubmit={handleSubmit(this.save)}>
+            <div class="form-group">
+              <label class="colcontrol-label label-required">Name</label>
+              <Field
+                name="item[name]"
+                component={InputField}
+                class="form-control"
+                autoFocus={true}
+                validate={required()}
+              />
+            </div>
+
+            <div class="form-group">
+              <label class="control-label">Description</label>
+              <Field
+                name="item[description]"
+                placeholder="Enter optional  description here"
+                component="textarea"
+                class="form-control"
+              />
+            </div>
+
+            <div class="form-group">
+              <div style={{ display: 'inline-block', width: '62%' }}>
+                <label class="control-label label-required">
+                  Price per unit (in ₹)
+                </label>
+                <Field
+                  name="item[amount]"
+                  placeholder="0.00"
+                  component={InputGroupField}
+                  class="form-control"
+                  validate={required()}
+                />
+              </div>
+              <div
+                style={{
+                  display: 'inline-block',
+                  width: '28%',
+                  float: 'right',
+                }}
+              >
+                <label class="control-label label-required">No. of Units</label>
+                <Field
+                  name="item[units]"
+                  component={InputGroupField}
+                  class="form-control"
+                  validate={required()}
+                />
+              </div>
+            </div>
+
+            <div class="Modal__actions clearfix" style={{ paddingTop: '16px' }}>
+              <AsyncButton
+                type="submit"
+                class="btn btn-primary col-lg-12"
+                text="Create and Include"
+                disabled={invalid}
+                pendingText="Creating..."
+                onClick={handleSubmit(this.save)}
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+CreateAddOn.defaultProps = {
+  onSave: () => {},
+};
