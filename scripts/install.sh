10a219571ef131d36fe4c3c3ecabe25ef8cbb8fd
Albin George <albinin0002@gmail.com>
2017-08-22 19:24:08 +0530

    [sbibuddy] Revert changes in install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -35,6 +35,3 @@ cd "$API_INSTALL_DIR" && php artisan queue:restart
 # Clear and Re-cache Routes
 echo "Route Cache"
 cd "$API_INSTALL_DIR" && php artisan route:cache
-
-# TODO: Remove this before merging
-sudo service apache2 restart
