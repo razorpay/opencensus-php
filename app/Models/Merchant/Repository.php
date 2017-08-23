548538695e0df1523a7a7792bbd008f46f692af0
Patlola <praveen.patlola@razorpay.com>
2017-08-23 10:33:11 +0000

    removed master merge conflict function

--- app/Models/Merchant/Repository.php
+++ app/Models/Merchant/Repository.php
@@ -43,23 +43,6 @@ class Repository extends Base\Repository
         Entity::EMAIL                   => Permission::VIEW_MERCHANT_EMAIL,
     ];
 
-    public function fetchActivatedMerchantsBeforeTimestamp(int $limit, int $skip, int $end, array $merchantIds = [])
-    {
-        $query = $this->newQuery()
-                    ->where(Entity::ACTIVATED, '=', 1)
-                    ->where(Entity::ACTIVATED_AT, '<=', $end)
-                    ->take($limit)
-                    ->skip($skip)
-                    ->with('merchantDetail');
-
-        if (empty($merchantIds) === false)
-        {
-            $query = $query->whereIn(Entity::ID, $merchantIds);
-        }
-
-        return $query->get();
-    }
-
     public function getSharedAccount()
     {
         if ($this->sharedMerchant === null)
