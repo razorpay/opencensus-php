b36d04be8d5e2bc97cf79fd6f520e03a08790b24
Amit Mahbubani <amit.mahbubani@razorpay.com>
2017-08-23 16:51:05 +0530

    CS fixes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -201,7 +201,6 @@ class UserController extends Controller
     /**
      * Fetch data for the currently active user session
      *
-     *
      * @return mixed
      */
     public function getSessionData()
