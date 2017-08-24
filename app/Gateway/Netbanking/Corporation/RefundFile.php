31e371b719af4e20092c91ea74385f10eefec7cb
Albin George <albinin0002@gmail.com>
2017-08-24 14:38:40 +0530

    [corp_nb] Add refund constants and generate refund file

---
+++ app/Gateway/Netbanking/Corporation/RefundFile.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace RZP\Gateway\Netbanking\Corporation;
+
+use Carbon\Carbon;
+use RZP\Constants\Timezone;
+use RZP\Gateway\Base;
+use RZP\Constants\Mode;
+use RZP\Models\FileStore;
+use RZP\Mail\Gateway\RefundFile\Base as RefundFileMail;
+
+class RefundFile extends Base\RefundFile
+{
+    protected static $fileToWriteName = 'CORPORATION_Netbanking_Refunds';
+
+    // TODO: Remove the below data and use env to store them
+    const DEBIT_ACCOUNT             = '12313123123132123';
+    const POOLING_ACCOUNT_BR_CODE   = '1234';
+    const CUSTOMER_ACCOUNT_BR_CODE  = '4321';
+    const SELF_ACCOUNT_NUMBER       = '234567';
+    const OTHERS_ACCOUNT_NUMBER     = '987654';
+
+    const FIXED_VALUE = '824603';
+
+    const FIXED_STRING_REAR = '000000000000000000000000000000000000000000   0000000   00000000000000Refunds';
+
+    const FIXED_VALUE_REAR = '26645097';
+
+    public function generate($input)
+    {
+        $text = $this->getRefundData($input);
+
+        $fileName = $this->getFileToWriteNameWithoutExt();
+
+        $creator = $this->createFile(
+            FileStore\Format::TXT,
+            $text,
+            $fileName,
+            FileStore\Type::CORPORATION_NETBANKING_REFUND
+        );
+
+        $file = $creator->get();
+
+        $signedFileUrl = $creator->getSignedUrl(self::SIGNED_URL_DURATION)['url'];
+
+        $fileData = [
+            'file_path'  => $file['local_file_path'],
+            'file_name'  => basename($file['local_file_path']),
+            'signed_url' => $signedFileUrl,
+        ];
+
+        sd($input['email']);
+
+        $this->sendRefundEmail($fileData, $input['email']);
+        // TODO: Create the text file
+    }
+
+    protected function sendRefundEmail($fileData = [], $email = null)
+    {
+        $refundFileMail = new RefundFileMail($fileData, Gateway::NETBANKING_HDFC, $email);
+
+        Mail::queue($refundFileMail);
+    }
+
+    protected function getRefundData(array $input)
+    {
+        $totalAmount = 0;
+
+        $count = 0;
+
+        $data = [];
+
+        // For first line
+        array_push($data, $this->getDataForRow(
+                self::POOLING_ACCOUNT_BR_CODE,
+                Carbon::now(Timezone::IST)->timestamp,
+                Constants::REFUND_FILE_DEBIT,
+                '00000000120000',
+                Constants::REFUND_FILE_ACCOUNT_TYPE_1,
+                Constants::REFUND_FILE_ACCOUNT_SUB_TYPE,
+                self::SELF_ACCOUNT_NUMBER,
+                true
+            )
+        );
+
+        foreach ($input['data'] as $row)
+        {
+            array_push($data, $this->getDataForRow(
+                    self::CUSTOMER_ACCOUNT_BR_CODE,
+                    $row['payment']['created_at'],
+                    Constants::REFUND_FILE_CREDIT,
+                    '00000000040000',
+                    Constants::REFUND_FILE_ACCOUNT_TYPE_2,
+                    Constants::REFUND_FILE_ACCOUNT_SUB_TYPE,
+                    self::OTHERS_ACCOUNT_NUMBER
+                )
+            );
+
+            $totalAmount += $row['refund']['amount'] / 100;
+
+            $count++;
+        }
+
+        return $this->generateText($data, '', true);
+    }
+
+    protected function getDataForRow(
+        $accountBrCode,
+        $date,
+        $mode,
+        $amount,
+        $accountType,
+        $accountSubType,
+        $accountNumber,
+        $firstLine = false
+    )
+    {
+        $date = Carbon::createFromTimestamp(
+                $date, Timezone::IST
+            )
+            ->format('Ymd');
+
+        $data = [
+            $accountBrCode,
+            $date,
+            $date,
+            $mode,
+            self::FIXED_VALUE,
+            $amount,
+            $accountType,
+            $accountSubType,
+            $accountNumber,
+            $this->getRearString($firstLine, $date)
+        ];
+
+        return $data;
+    }
+
+    protected function getRearString($firstLine, $date = null)
+    {
+        $lastString = self::FIXED_VALUE_REAR;
+
+        if($firstLine === true)
+        {
+            $date = Carbon::createFromTimestamp(
+                $date
+            )
+            ->format('d.m.Y');
+
+            $lastString = ' Dt: ' . $date;
+        }
+
+        return self::FIXED_STRING_REAR . $lastString;
+    }
+}
