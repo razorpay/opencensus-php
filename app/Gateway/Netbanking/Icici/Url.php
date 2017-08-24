0ece59aea91f4c57227348dfb0570e50c0ae869a
Giridaran <giri@razorpay.com>
2017-08-24 18:15:21 +0530

    [nb_icici] Fix nb_icici_corp url.

--- app/Gateway/Netbanking/Icici/Url.php
+++ app/Gateway/Netbanking/Icici/Url.php
@@ -8,7 +8,7 @@ class Url
     const RETAIL_LIVE_DOMAIN    = 'https://shopping.icicibank.com/corp/BANKAWAY?';
     const RETAIL_TEST_DOMAIN    = 'https://shopping.icicibank.com/corp/BANKAWAY?';
 
-    const CORPORATE_LIVE_DOMAIN = 'https://cib.icicibank.com/corp/BANKAWAY?';
+    const CORPORATE_LIVE_DOMAIN = 'https://cibnext.icicibank.com/corp/BANKAWAY?';
     const CORPORATE_TEST_DOMAIN = 'https://cibnextcug.icicibank.com/corp/BANKAWAY?';
 
     const RETAIL_QUERY          = 'IWQRYTASKOBJNAME=bay_mc_login&BAY_BANKID=ICI';
