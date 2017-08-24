b9b26988c04ab33789293b6d01fcc294033ef482
rzpamidi <prashanth.pamidi@razorpay.com>
2017-08-24 13:54:47 +0530

    Changes the layout of invoice

--- public/react/merchant/components/Invoices/InvoiceDetail.js
+++ public/react/merchant/components/Invoices/InvoiceDetail.js
@@ -7,13 +7,44 @@ import CopyLink from 'merchant/components/Invoices/CopyLink';
 import ShowWhen from 'merchant/components/ShowWhen';
 import LineItemReadOnlyTable from './LineItemReadOnlyTable';
 import { InvoiceStatusLabel } from 'merchant/components/StatusLabel';
-import DetailRow from 'merchant/components/DetailRow';
+import EntityDetailRow from 'merchant/components/EntityDetailRow';
+import NestedEntityDetailRow from 'merchant/components/NestedEntityDetailRow';
 import NestedDetailRow from 'merchant/components/NestedDetailRow';
 
 const notificationClassMap = {
   sent: 'text-success',
   pending: 'text-warning',
 };
+
+const getCustomerDetail = invoice => ({
+  Name: invoice.customer_details.customer_name,
+  Email: (
+    <span>
+      {invoice.customer_details.customer_email}
+      {invoice.email_status
+        ? <span
+            style={{ marginLeft: '10px' }}
+            class={`${notificationClassMap[invoice.email_status]}`}
+          >
+            ({invoice.email_status} mail)
+          </span>
+        : null}
+    </span>
+  ),
+  Phone: (
+    <span>
+      {invoice.customer_details.customer_contact}
+      {invoice.sms_status
+        ? <span
+            style={{ marginLeft: '10px' }}
+            class={`${notificationClassMap[invoice.sms_status]}`}
+          >
+            ({invoice.sms_status} sms)
+          </span>
+        : null}
+    </span>
+  ),
+});
 
 export default props => {
   let { invoice, isLoading, statusMsg } = props;
@@ -59,21 +90,44 @@ export default props => {
             <div class="SliderPanel__Body">
               <div class="panel-body">
                 <div class="list-group details-row-container">
-                  <DetailRow
+                  <EntityDetailRow
                     label="Amount"
                     value={() => <Amount value={invoice.amount} />}
                   />
-                  <DetailRow
+                  <EntityDetailRow
+                    label="Status"
+                    value={() => <InvoiceStatusLabel status={invoice.status} />}
+                  />
+                  <EntityDetailRow
                     label="Amount Paid"
                     value={() => <Amount value={invoice.amount_paid} />}
                   />
-                  <DetailRow label="Summary" value={invoice.description} />
-                  <DetailRow
-                    label="Invoice Date"
+                  <EntityDetailRow
+                    label="Payment Link"
+                    value={() => <CopyLink url={invoice.short_url} />}
+                  />
+                  <EntityDetailRow
+                    label="Summary"
+                    value={invoice.description || '-'}
+                  />
+                  <EntityDetailRow label="Receipt" value={invoice.receipt} />
+                  <NestedEntityDetailRow
+                    label="Customer Details"
+                    value={getCustomerDetail(invoice)}
+                  />
+                  <EntityDetailRow
+                    label="Created at"
                     value={() => <Time value={invoice.date} />}
                   />
-
-                  <DetailRow
+                  <EntityDetailRow
+                    label="Paid At"
+                    value={() =>
+                      <Time
+                        value={invoice.paid_at}
+                        format="DD MMM YYYY, hh:mm:ss a"
+                      />}
+                  />
+                  <EntityDetailRow
                     label={isExpired ? 'Expired on' : 'Expires on'}
                     value={() =>
                       <Time
@@ -82,114 +136,7 @@ export default props => {
                       />}
                   />
 
-                  <DetailRow label="Receipt" value={invoice.receipt} />
-                  <DetailRow
-                    label="Payment Link"
-                    value={() => <CopyLink url={invoice.short_url} />}
-                  />
-                  <DetailRow
-                    label="Status"
-                    value={() => <InvoiceStatusLabel status={invoice.status} />}
-                  />
-                  <DetailRow label="Type" value={titleCase(invoice.type)} />
-                  <DetailRow
-                    label="Payment Id"
-                    value={() => {
-                      if (!invoice.payment_id) {
-                        return '--';
-                      }
-                      return (
-                        <NavLink to={`/payments/${invoice.payment_id}`}>
-                          <code>
-                            {invoice.payment_id}
-                          </code>
-                        </NavLink>
-                      );
-                    }}
-                  />
-                  <DetailRow
-                    label="Paid At"
-                    value={() =>
-                      <Time
-                        value={invoice.paid_at}
-                        format="DD MMM YYYY, hh:mm:ss a"
-                      />}
-                  />
-                  <DetailRow label="Terms & Conditions" value={invoice.terms} />
-                  <NestedDetailRow label="Notes" value={invoice.notes} />
-                </div>
-
-                <div class="panel panel-default">
-                  <div class="panel-heading panel-heading-sm">
-                    Customer Details
-                  </div>
-                  <div class="list-group details-row-container">
-                    <DetailRow
-                      label="Name"
-                      value={invoice.customer_details.customer_name}
-                    />
-                    <DetailRow
-                      label="Email"
-                      value={() =>
-                        <span>
-                          {invoice.customer_details.customer_email}
-                          {invoice.email_status
-                            ? <span
-                                style={{ marginLeft: '10px' }}
-                                class={`${notificationClassMap[
-                                  invoice.email_status
-                                ]}`}
-                              >
-                                ({invoice.email_status})
-                              </span>
-                            : null}
-                        </span>}
-                    />
-                    <DetailRow
-                      label="Phone"
-                      value={() =>
-                        <span>
-                          {invoice.customer_details.customer_contact}
-                          {invoice.sms_status
-                            ? <span
-                                style={{ marginLeft: '10px' }}
-                                class={`${notificationClassMap[
-                                  invoice.sms_status
-                                ]}`}
-                              >
-                                ({invoice.sms_status})
-                              </span>
-                            : null}
-                        </span>}
-                    />
-                  </div>
-                </div>
-
-                {invoice.line_items.length
-                  ? <div class="panel panel-default">
-                      <div class="panel-heading">Item Details</div>
-
-                      <div class="panel-body">
-                        <LineItemReadOnlyTable
-                          line_items={invoice.line_items}
-                        />
-                      </div>
-                    </div>
-                  : ''}
-
-                <div class="panel panel-default">
-                  <div class="panel-heading panel-heading-sm">Updates</div>
-
-                  <div class="list-group details-row-container">
-                    <DetailRow
-                      label="Created At"
-                      value={() =>
-                        <Time
-                          value={invoice.created_at}
-                          format="DD MMM YYYY, hh:mm:ss a"
-                        />}
-                    />
-                  </div>
+                  <NestedEntityDetailRow label="Notes" value={invoice.notes} />
                 </div>
               </div>
             </div>
--- public/react/merchant/components/NestedEntityDetailRow.js
+++ public/react/merchant/components/NestedEntityDetailRow.js
@@ -1,24 +1,27 @@
 import EntityDetailRow from 'merchant/components/EntityDetailRow';
-import NestedEntityDetailToggler
-  from 'rzp/ui/Toggler/NestedEntityDetailToggler';
+import NestedEntityDetailToggler from 'rzp/ui/Toggler/NestedEntityDetailToggler';
 
-export default ({ label, value = {} }) => {
+export default ({ label, value = {}, placeholder = '--' }) => {
   if (Object.keys(value).length) {
     return (
       <NestedEntityDetailToggler label={label} show={false}>
         {Object.keys(value).length
           ? <div class="table-responsive">
-              {Object.keys(value).map(key => (
+              {Object.keys(value).map(key =>
                 <div key={key} class="pair-list-item">
-                  <div class="item-label">{key}</div>
-                  <div class="items-value">{value[key]}</div>
+                  <div class="item-label">
+                    {key}
+                  </div>
+                  <div class="items-value">
+                    {value[key] || placeholder}
+                  </div>
                 </div>
-              ))}
+              )}
             </div>
           : null}
       </NestedEntityDetailToggler>
     );
   }
 
-  return <EntityDetailRow label={label} value="--" />;
+  return <EntityDetailRow label={label} value={placeholder} />;
 };
