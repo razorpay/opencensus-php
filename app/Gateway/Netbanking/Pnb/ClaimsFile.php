7b8ebcae57251f47f47bb967c20169c4a23a3aac
namrata <namrata.chaudhary@razorpay.com>
2017-08-24 19:38:10 +0530

    [pnb] fixes string padding

--- app/Gateway/Netbanking/Pnb/ClaimsFile.php
+++ app/Gateway/Netbanking/Pnb/ClaimsFile.php
@@ -8,7 +8,6 @@ use RZP\Gateway\Base;
 use RZP\Constants\Mode;
 use RZP\Models\FileStore;
 use RZP\Constants\Timezone;
-use RZP\Models\Payment\RefundStatus;
 
 class ClaimsFile extends Base\RefundFile
 {
@@ -80,8 +79,8 @@ class ClaimsFile extends Base\RefundFile
                 str_pad($row['payment'][Constants::CLAIM_TYPE], 2, ' ', STR_PAD_LEFT),
                 str_pad($amount, 17, ' ', STR_PAD_LEFT),
                 $row['payment'][Constants::TXN_DETAIL],
-                str_pad($row['payment']['id'], 2, ' ', STR_PAD_LEFT),
-                str_pad($date, 2, ' ', STR_PAD_LEFT),
+                str_pad($row['payment']['id'], 16, ' ', STR_PAD_LEFT),
+                str_pad($date, 12, ' ', STR_PAD_LEFT),
             ];
 
             $totalAmount += $amount;
