fe9a4c03ddf5708783a3e39788ddcb371599d426
Patlola <praveen.patlola@razorpay.com>
2017-08-25 16:24:25 +0000

    terminals in admin auth

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -252,15 +252,6 @@ class AdminController extends Controller
         return AppResponse::jsonResponse($error, $data);
     }
 
-    public function getMerchantTerminal($id)
-    {
-        $input = Input::all();
-
-        list($error, $data) = (new Admin\Service)->postMerchantTerminal($id, $input);
-
-        return AppResponse::jsonResponse($error, $data);
-    }
-
     public function postMerchantTerminal($id)
     {
         $input = Input::all();
