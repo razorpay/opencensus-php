1618ed14cc0d8abd8df9ff7ed6acf0bc360f2a8c
Vivek Kumar <viveklakhera@gmail.com>
2017-08-30 16:01:28 +0530

    [blade] Update the directory server URL

--- app/Gateway/Blade/Url.php
+++ app/Gateway/Blade/Url.php
@@ -4,6 +4,13 @@ namespace RZP\Gateway\Blade;
 
 class Url
 {
+    // VISA Directory Server
+    const LIVE_VISA_DS = 'https://dcwc.visa3dsecure.com/DSMsgServlet';
+
+    // MasterCard Directory Server
+    const LIVE_MC_DS   = 'https://mcdirectory.securecode.com';
+
+    // For PIT testing and CTH testing
     const PIT_DS = 'https://dropit.3dsecure.net:9443/PIT/DS';
     const CTH_DS = 'https://3dsecuretestfacility.com:9660/cth/md/Razorpay+Blade+v1.0';
 }
