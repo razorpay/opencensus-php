3fa7f132d1eb509d9b361b6d98397ea5332b637c
aseemgupta <aseemgupta2625@gmail.com>
2017-08-30 19:08:17 +0530

    Fix date selection on end of month

--- public/react/merchant/containers/Reports/index.js
+++ public/react/merchant/containers/Reports/index.js
@@ -100,6 +100,13 @@ export default class ReportsContainer extends Component {
     });
   }
 
+  componentWillReceiveProps(nextProps) {
+    // set the date to 1st of current month otherwise e.g, if 30 Aug changes to Feb then date becomes 30, making it select March!
+    if (this.props.type === 'daily' && nextProps.type === 'monthly') {
+      this.props.change('date', this.props.date.startOf('month'));
+    }
+  }
+
   getEntityLabel(value) {
     let label = null;
     label = this.entityOptions.find(item => item.value === value).label;
