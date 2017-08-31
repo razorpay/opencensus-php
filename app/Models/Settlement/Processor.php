17f53e510bba0bb753342b0861b8c6064b9a3ae3
Pratik Kapasi <pratik.kapasi@razorpay.com>
2017-08-31 19:24:51 +0530

    [beta-api] changes for beta testing

--- app/Models/Settlement/Processor.php
+++ app/Models/Settlement/Processor.php
@@ -284,6 +284,7 @@ class Processor extends Base\Core
 
     protected function shouldProcessSettlements()
     {
+        return [true, null];
         if (($this->mode === Mode::TEST) and
             ($this->env === 'testing'))
         {
--- tests/Functional/Address/AddressTest.php
+++ tests/Functional/Address/AddressTest.php
@@ -13,6 +13,7 @@ class AddressTest extends TestCase
 
     public function setUp()
     {
+        sd("beta");
         $this->testDataFilePath = __DIR__ . '/Helpers/AddressTestData.php';
 
         parent::setUp();
