e71eadf3172b34da737843529e82b8206e8ba661
rzpamidi <prashanth.pamidi@razorpay.com>
2017-09-01 19:18:29 +0530

    Converting paise to rupees

--- resources/views/components/currency.blade.php
+++ resources/views/components/currency.blade.php
@@ -1,4 +1,4 @@
 <?php $absValue = abs($value); ?>
 @if($value < 0)-@endif
-@if(isset($currency)){{{ $currency/(isSet($isRupees) ? 1 : 100) }}}@else₹@endif
-{{$absValue}}
+@if(isset($currency)){{{ $currency }}}@else₹@endif
+{{$absValue/(isSet($isRupees) ? 1 : 100)}}
