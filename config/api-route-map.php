fad147c272c5cc1ed93c14412a978acbba711300
Rishabh <rishabh.pugalia@gmail.com>
2017-09-01 16:42:43 +0530

    changing duty and type params for workflow actions

--- config/api-route-map.php
+++ config/api-route-map.php
@@ -70,7 +70,7 @@ return [
         'admin_get_file'                    => 'files/{fileId}/signed-url',
 
         // Workflows
-        'workflow_get_multiple'             => 'orgs/{orgId}/workflows',
+        'workflow_get_multiple'             => 'workflows',
         'workflow_create'                   => 'workflows',
         'workflow_get'                      => 'workflows/{id}',
         'workflow_update'                   => 'workflows/{id}',
