597cd6e2bfd40ea3d5dc81c9b9c40a99d675715a
Ria Garg <ria.garg@razorpay.com>
2017-09-04 18:16:30 +0530

    [irctc_refunds] refactoring refund and settlement processors

---
+++ app/Models/Batch/Processor/IrctcRefund.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace RZP\Models\Batch\Processor;
+
+use RZP\Models\Batch;
+use RZP\Models\Payment\Processor\Processor as PaymentProcessor;
+
+class IrctcRefund extends Base
+{
+    protected function processEntry(array & $entry)
+    {
+        $paymentId = trim($entry[Batch\Header::PAYMENT_ID]);
+
+        $payment = $this->repo->payment->findByPublicId($paymentId);
+
+        $type = $entry[Batch\Header::REFUND_TYPE];
+
+        $processor = 'process' . studly_case($type) .'TypeRefunds';
+
+        $this->$processor($row, $payment);
+
+        $entry[Batch\Header::STATUS]       = Batch\Status::SUCCESS;
+        $entry[Batch\Header::REFUND_ID]    = $refund->getPublicId();
+        $entry[Batch\Header::REFUND_DATE]  = $refund->getCreatedAt();
+        $enrty[Batch\Header::PAYMENT_DATE] = $payment->getCreatedAt();
+    }
+
+    protected function processRTypeRefunds(array $row, Payment\Entity $payment)
+    {
+        $paymentProcessor = (new PaymentProcessor($payment->merchant));
+
+        $input = [
+            Entity::RECEIPT => $row[self::CANCELLATION_ID . '_' . self::MERCHANT_REFERENCE],
+            Entity::NOTES   => [
+                'reservation_id'  => $row[self::MERCHANT_REFERENCE],
+                'cancellation_id' => $row[self::CANCELLATION_ID],
+                'refund_type'     => $row[self::REFUND_TYPE],
+            ],
+        ];
+
+        $paymentProcessor->createRefundFromMerchantFile($payment, $input);
+    }
+
+    protected function processCTypeRefunds(array $row, Payment\Entity $payment)
+    {
+        $paymentProcessor = (new PaymentProcessor($payment->merchant));
+
+        if ($payment->getStatus() !== Payment\Status::CAPTURED) {
+            throw new Exception\BadRequestException(
+                'Payment is not Captured for C type refund',
+                $row
+            );
+        }
+
+        $input = [
+            Entity::AMOUNT  => intval($row[self::REFUND_AMOUNT] * 100),
+            Entity::RECEIPT => $row[self::CANCELLATION_ID . '_' . self::MERCHANT_REFERENCE],
+            Entity::NOTES   => [
+                'reservation_id'  => $row[self::MERCHANT_REFERENCE],
+                'cancellation_id' => $row[self::CANCELLATION_ID],
+                'refund_type'     => $row[self::REFUND_TYPE],
+            ],
+        ];
+
+        $paymentProcessor->createRefundFromMerchantFile($payment, $input);
+    }
+}
---
+++ app/Models/Batch/Processor/IrctcSettlement.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace RZP\Models\Batch\Processor;
+
+use RZP\Models\Batch;
+use RZP\Models\Payment\Processor\Processor as PaymentProcessor;
+
+class IrctcSettlement extends Base
+{
+    protected function processEntry(array & $entry)
+    {
+
+        $paymentId = trim($entry[Batch\Header::PAYMENT_ID]);
+
+        $payment = $this->repo->payment->findByPublicId($paymentId);
+
+        $paymentProcessor = (new PaymentProcessor($payment->merchant));
+
+        $amount = $payment->getAmount();
+
+        // We do not capture the payment if its already refunded
+        if ($payment->isPartiallyOrFullyRefunded() === false)
+        {
+            $paymentProcessor->capture($paymentId, ['amount' => $amount]);
+        }
+
+        $entry[Batch\Header::STATUS]       = Batch\Status::SUCCESS;
+    }
+}
