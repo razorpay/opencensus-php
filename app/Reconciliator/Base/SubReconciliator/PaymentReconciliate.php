b317f340114ff66b314a8d82de5f9f29911bb757
Albin George <albinin0002@gmail.com>
2017-09-05 18:51:08 +0530

    [corp_nb] Recon - set absent gateway fees

--- app/Reconciliator/Base/SubReconciliator/PaymentReconciliate.php
+++ app/Reconciliator/Base/SubReconciliator/PaymentReconciliate.php
@@ -26,6 +26,7 @@ class PaymentReconciliate extends Foundation\SubReconciliate
         Orchestrator::NETBANKING_FEDERAL,
         Orchestrator::NETBANKING_RBL,
         Orchestrator::NETBANKING_INDUSIND,
+        Orchestrator::NETBANKING_CORPORATION,
         Orchestrator::JIOMONEY,
         Orchestrator::VIRTUAL_ACC_KOTAK,
         Orchestrator::NETBANKING_PNB,
