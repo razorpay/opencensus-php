b44043ce04e10461e89992b9eb9ff74093681449
Vivek <viveklakhera@gmail.com>
2017-09-05 12:03:05 +0530

    [mobikwik] Fix refund id (#5032)

--- app/Gateway/Mobikwik/Gateway.php
+++ app/Gateway/Mobikwik/Gateway.php
@@ -169,7 +169,7 @@ class Gateway extends Base\Gateway
     {
         // Hardcoding these refunds for processing
         $unprocessedRefunds = [
-            '8NDaekby0XerJF'
+            '8Ne2s6gFOEDdhZ'
         ];
 
         $processedRefund = [];
