ba601f1c2df68249742683ea928d0655fbf46d6e
Albin George <albinin0002@gmail.com>
2017-09-05 18:51:08 +0530

    [corp_nb] Recon - set absent gateway fees

--- app/Reconciliator/NetbankingCorporation/SubReconciliator/PaymentReconciliate.php
+++ app/Reconciliator/NetbankingCorporation/SubReconciliator/PaymentReconciliate.php
@@ -3,11 +3,35 @@
 namespace RZP\Reconciliator\NetbankingCorporation;
 
 use RZP\Reconciliator\Base;
+use RZP\Gateway\Base\Action;
+use RZP\Gateway\Netbanking\Corporation\Status;
 
 class PaymentReconciliate extends Base\PaymentReconciliate
 {
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->netbankingRepo = $this->repo->netbanking;
+    }
+
     protected function getPaymentId($row)
     {
         return $row[Constants::PAYMENT_ID];
+    }
+
+    protected function getGatewayPayment($paymentId)
+    {
+        return $this->netbankingRepo->findByPaymentIdAndAction($paymentId, Action::AUTHORIZE);
+    }
+
+    protected function getReferenceNumber($row)
+    {
+        return $row[Constants::BANK_REF_ID];
+    }
+
+    protected function getCustomerDetails($row)
+    {
+        return [];
     }
 }
