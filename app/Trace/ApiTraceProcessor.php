e06a76ea6409ee84d3868833ed5bc7e55a680881
Sumeeti Lalwani <sumeeti.lalwani@razorpay.com>
2017-09-06 18:51:47 +0530

    Fixing trace for oauth token and client id (#5049)

--- app/Trace/ApiTraceProcessor.php
+++ app/Trace/ApiTraceProcessor.php
@@ -19,11 +19,20 @@ class ApiTraceProcessor
     {
         $this->addMode($record);
 
+        $this->addOAuthAttributes($record);
+
         return $record;
     }
 
     protected function addMode(&$record)
     {
         $record['mode'] = $this->app['basicauth']->getMode();
+    }
+
+    protected function addOAuthAttributes(&$record)
+    {
+        $record['access_token_id'] = $this->app['basicauth']->getAccessTokenId();
+
+        $record['oauth_client_id'] = $this->app['basicauth']->getOAuthClientId();
     }
 }
