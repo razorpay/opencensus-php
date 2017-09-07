0c66a40983529b2ebbd9b3ca8b48618e54d63eca
jitendra-1217 <jitendraojha1217@gmail.com>
2017-09-07 19:26:18 +0000

    [es] merchants: Use standard_custom instead of standard

--- config/es_mappings.php
+++ config/es_mappings.php
@@ -213,22 +213,26 @@ return [
             'name' => [
                 'type'            => 'text',
                 'analyzer'        => 'edge_ngram_analyzer',
-                'search_analyzer' => 'standard',
+                'search_analyzer' => 'standard_custom',
                 'index_options'   => 'offsets',
             ],
             'email' => [
-                'type' => 'text',
+                'type'            => 'text',
+                'analyzer'        => 'edge_ngram_analyzer',
+                'search_analyzer' => 'standard_custom',
+                'index_options'   => 'offsets',
             ],
             'billing_label' => [
                 'type'            => 'text',
                 'analyzer'        => 'edge_ngram_analyzer',
-                'search_analyzer' => 'standard',
+                'search_analyzer' => 'standard_custom',
                 'index_options'   => 'offsets',
             ],
             'website' => [
                 'type'            => 'text',
-                'analyzer'        => 'standard',
-                'search_analyzer' => 'standard',
+                'analyzer'        => 'edge_ngram_analyzer',
+                'search_analyzer' => 'standard_custom',
+                'index_options'   => 'offsets',
             ],
             'tag_list' => [
                 'type'            => 'text',
