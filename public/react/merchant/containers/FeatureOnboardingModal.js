a21423318f1c79d5cdf67069680f23aa7e9aa81e
Amit Mahbubani <amit.mahbubani@razorpay.com>
2017-09-07 16:59:02 +0530

    [onboard] Add Modal container

---
+++ public/react/merchant/containers/FeatureOnboardingModal.js
@@ -0,0 +1,105 @@
+import { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+
+import AsyncButton from 'react-async-button';
+import { Field, reduxForm, formValueSelector } from 'redux-form';
+import ModalHeader from 'rzp/ui/ModalHeader';
+import * as ModalActions from 'rzp/modules/modals';
+import { showNotification } from 'rzp/modules/notifications';
+
+const selector = formValueSelector('uploadBatch');
+@withRouter
+@connect(
+  state => ({
+    sms_notify: selector(state, 'sms_notify'),
+    email_notify: selector(state, 'email_notify'),
+  }),
+  { ...ModalActions, showNotification }
+)
+@reduxForm({
+  form: 'featureOnboardingModal',
+})
+export default class FeatureOnboardingModal extends Component {
+  // Form submit handler
+  onSubmitClick = props => {
+    const prom = new Promise(() => {
+      let additionalFormFields = {};
+
+      this.props
+        // do something
+        .then(() => {
+          this.props.closeModal();
+
+          this.props.showNotification({
+            type: 'success',
+            message: 'Successful',
+          });
+          this.props.history.push(this.props.closeUrl);
+        })
+        .catch(({ errors }) => {
+          this.props.showNotification({
+            type: 'error',
+            message: errors,
+          });
+        });
+    });
+
+    return prom;
+  };
+
+  // View render
+  render() {
+    const { handleSubmit } = this.props;
+
+    return (
+      <div class="proceed-upload-modal">
+        <ModalHeader
+          title="Batch Upload"
+          onCloseClick={this.props.closeModal}
+        />
+        <form class="form-horizontal">
+          <div class="modal-body">
+            <div>
+              <p>Send link and payment instructions to...</p>
+
+              <div class="rzpCheckbox">
+                <Field
+                  name="sms_notify"
+                  id="sms_notify"
+                  component="input"
+                  type="checkbox"
+                />
+                <label for="sms_notify">Sms Notify</label>
+              </div>
+
+              <div class="rzpCheckbox">
+                <Field
+                  name="email_notify"
+                  id="email_notify"
+                  component="input"
+                  type="checkbox"
+                />
+                <label for="email_notify">Email Notify</label>
+              </div>
+            </div>
+
+            <div>
+              A <b>payment link</b> will also be created.
+            </div>
+
+            <div class="Modal__actions">
+              <AsyncButton
+                type="submit"
+                class="btn btn-primary btn-block btn-lg"
+                text="Submit"
+                pendingText="Submitting..."
+                onClick={handleSubmit(this.onSubmitClick)}
+              />
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
