b2bcdb16b2f2d9fbfb4ad8f8c20b5c94de20793f
Vivek Kumar <viveklakhera@gmail.com>
2017-09-08 11:48:42 +0530

    [maxmind] Add validation for email tld

--- tests/Functional/Payment/helpers/FraudDetectionTestData.php
+++ tests/Functional/Payment/helpers/FraudDetectionTestData.php
@@ -35,5 +35,20 @@ return [
             'internal_error_code' => ErrorCode::BAD_REQUEST_PAYMENT_POSSIBLE_FRAUD,
             'public_error_description' => ''
         ],
+    ],
+
+    'testFraudDetectedWithInvalidEmailTld' => [
+        'response' => [
+            'content' => [
+                'error' => [
+                    'code' => PublicErrorCode::BAD_REQUEST_ERROR,
+                ],
+            ],
+            'status_code' => 400,
+        ],
+        'exception' => [
+            'class' => RZP\Exception\BadRequestValidationFailureException::class,
+            'internal_error_code' => ErrorCode::BAD_REQUEST_VALIDATION_FAILURE,
+        ],
     ]
 ];
