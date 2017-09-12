961009f74f154b4467ee5ae4f46e3f339d268d3f
Patlola <praveen.patlola@razorpay.com>
2017-09-12 12:51:15 +0000

    admin token decryption and validation

--- app/Models/Admin/Admin/Token/Repository.php
+++ app/Models/Admin/Admin/Token/Repository.php
@@ -2,18 +2,48 @@
 
 namespace RZP\Models\Admin\Admin\Token;
 
+use Hash;
 use RZP\Models\Admin\Base;
+use RZP\Exception;
 use Carbon\Carbon;
+use RZP\Models\Base\UniqueIdEntity;
 
 class Repository extends Base\Repository
 {
     protected $entity = 'admin_token';
 
+    /**
+     * Since token is encrypted we need to check with principal
+     * and validate before fetching token instance.
+     * @param $token
+     * @return mixed
+     */
     public function findOrFailToken($token)
     {
-        return $this->newQuery()
+        // Token = bearertoken + TokenId(principal)(14chars).
+        $principalLength = UniqueIdEntity::ID_LENGTH;
+
+        $principal = substr($token, -$principalLength);
+
+        $bearerToken = substr($token, 0, -$principalLength);
+
+        // Current Timestamp
+        $currentTimestamp = Carbon::now()->getTimestamp();
+
+        $token =  $this->newQuery()
                     ->with('admin')
-                    ->where(Entity::TOKEN, '=', $token)
+                    ->where(Entity::ID, '=', $principal)
+                    ->where(Entity::EXPIRES_AT, '>', $currentTimestamp)
                     ->firstOrFailPublic();
+
+        if (Hash::check($bearerToken, $token->getToken()) === true)
+        {
+            return $token;
+        }
+        else
+        {
+            throw new Exception\BadRequestException(
+                ErrorCode::BAD_REQUEST_NO_RECORDS_FOUND, null, $data);
+        }
     }
 }
