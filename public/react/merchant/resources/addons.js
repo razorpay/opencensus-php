a439cb685e8faa18a8388de2d2f6d594ed50a376
aseemgupta <aseemgupta2625@gmail.com>
2017-09-12 21:29:45 +0530

    Resource file for addons

---
+++ public/react/merchant/resources/addons.js
@@ -0,0 +1,47 @@
+/*
+  Description: Filter out extra values on basis of `resourceFields`
+  Input:
+    1) fields = string / array
+    2) data = value / object
+*/
+export function formatFields(fields, data) {
+  if (!Array.isArray(fields)) {
+    return _serializeProperty(fields, data);
+  }
+
+  const formattedData = {};
+
+  // Parsing over fields(array) is better than parsing over data(object)
+  // Parsing over data(object) will lead to iterating over not to be formatted keys
+  fields.forEach(key => {
+    let value = data[key];
+    if (value) {
+      formattedData[key] = _serializeProperty(key, value);
+    }
+  });
+
+  return { ...data, ...formattedData }; // This will send formatted data back along with other values in data which are not to be processed
+}
+
+/* ================= Util functions ===================== */
+
+// Process any data before sending in this fn.
+function _serializeProperty(key, value) {
+  switch (key) {
+    case 'description':
+    case 'name':
+      return value && value.trim();
+    case 'amount':
+      return Number(value) * 100; // Ideally should be multiplied by unit price. Currently assuming INR
+    case 'quantity':
+      return Number(value);
+  }
+}
+
+// Process any data on receiving in this fn.
+function _deserializeProperty(key, value) {
+  switch (key) {
+    case 'amount':
+      return Number(value) / 100; // Ideally should be divided by unit price. Currently assuming INR
+  }
+}
