af7f5ca321852ff7d49166339ea98af1e9c8de15
rzpamidi <prashanth.pamidi@razorpay.com>
2017-09-13 18:09:06 +0530

    Added Definition Component

---
+++ public/react/rzp/ui/Definition.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+/*
+ * Description:
+ * Displays children in form of Title and Content, The first child
+ * will be title and rest of the children will be the body, Title will
+ * be bigger in font size than rest of the content, by default it will
+ * also make sure that the heading is not empty.
+ *
+ * Usage:
+ * <Definiton>
+ *   <span>Contact Details</span>
+ *   <span>Anthony Gonsalves</span>
+ *   <span>anthony@gonsalves.com</span>
+ *   <span>9988998899</span>
+ * </Definiton>
+ */
+export default class Definition extends Component {
+  render() {
+    let children = Array.isArray(this.props.children)
+      ? [...this.props.children]
+      : [this.props.children];
+
+    const allowEmptyTitle = this.props.allowEmptyTitle;
+
+    if (!allowEmptyTitle) {
+      while (children.length > 0 && !children[0]) {
+        children.shift();
+      }
+    }
+
+    if (children.length === 0) {
+      return null;
+    }
+
+    const heading = children[0],
+      body = children.slice(1);
+
+    return (
+      <dl class="rzp-definition">
+        {heading &&
+          <dt>
+            {heading}
+          </dt>}
+        {body.map((item, key) =>
+          <dd key={key}>
+            {item}
+          </dd>
+        )}
+      </dl>
+    );
+  }
+}
