556dee98edb2b457f93c9d81f266c7759bd3fd79
Nemo <me@captnemo.in>
2017-09-15 12:10:52 +0530

    [ifsc] Whitelists a SBIN IFSC code as a special code (#5184)
    * [ifsc] Adds a live whitelist of IFSC codes
    
    * [ifsc] Whitelist SBIN ifsc code
    
    * [ifsc] use same constant in both places

--- app/Models/BankAccount/Validator.php
+++ app/Models/BankAccount/Validator.php
@@ -6,6 +6,8 @@ use Razorpay\IFSC\IFSC;
 use RZP\Base;
 use RZP\Constants\Mode;
 use RZP\Exception;
+use RZP\Models\VirtualAccount\Provider;
+use RZP\Models\Merchant\Detail\Validator as MerchantDetailValidator;
 
 class Validator extends Base\Validator
 {
@@ -82,8 +84,26 @@ class Validator extends Base\Validator
      */
     protected function isSpecialIfscCode($ifsc, $mode)
     {
-        return ((($mode === Mode::TEST) or ($mode === null)) and
-                (($ifsc === Entity::SPECIAL_IFSC_CODE) or
-                 ($ifsc === 'RAZR0000001')));
+        // https://razorpay.zendesk.com/agent/tickets/94340
+        $liveWhitelist = MerchantDetailValidator::IFSC_WHITELIST;
+
+        $testWhitelist = array_merge(
+            Provider::DEFAULT_DETAILS[Provider::DASHBOARD],
+            [Entity::SPECIAL_IFSC_CODE]
+        );
+
+        switch ($mode)
+        {
+            case Mode::TEST:
+            case null:
+                $list = $testWhitelist;
+                break;
+
+            case Mode::LIVE:
+                $list = $liveWhitelist;
+                break;
+        }
+
+        return in_array($ifsc, $list, true);
     }
 }
