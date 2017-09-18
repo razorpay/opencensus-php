d96b15c0efd3127268387c5284863d599befe085
gaurav <gaurav.d@razorpay.com>
2017-09-19 02:06:20 +0530

    [Hitachi] added terminal and added gateway

--- app/Models/Payment/Gateway.php
+++ app/Models/Payment/Gateway.php
@@ -22,6 +22,7 @@ class Gateway
     const BILLDESK               = 'billdesk';
     const BLADE                  = 'blade';
     const CYBERSOURCE            = 'cybersource';
+    const HITACHI                = 'hitachi';
     const EBS                    = 'ebs';
     const FIRST_DATA             = 'first_data';
     const HDFC                   = 'hdfc';
@@ -177,6 +178,7 @@ class Gateway
         self::UPI_ICICI           => Settlement\Channel::KOTAK,
         self::AEPS_ICICI          => Settlement\Channel::KOTAK,
         self::CYBERSOURCE         => Settlement\Channel::KOTAK,
+        self::HITACHI             => Settlement\Channel::KOTAK,
     ];
 
     /**
@@ -196,6 +198,7 @@ class Gateway
             self::CYBERSOURCE,
             self::FIRST_DATA,
             self::BLADE,
+            self::HITACHI,
         ],
 
         Method::NETBANKING => [
@@ -274,6 +277,7 @@ class Gateway
             self::NOT_SUPPORTED => [Network::MAES, Network::RUPAY]
         ],
         self::WALLET_OPENWALLET     => [],
+        self::HITACHI               => [],
     ];
 
     /**
@@ -287,6 +291,7 @@ class Gateway
         self::AXIS_MIGS,
         self::AMEX,
         self::WALLET_OPENWALLET,
+        self::HITACHI,
     ];
 
 
@@ -352,6 +357,10 @@ class Gateway
             Network::UNKNOWN
         ],
         self::CYBERSOURCE => [
+            Network::MC,
+            Network::VISA
+        ],
+        self::HITACHI => [
             Network::MC,
             Network::VISA
         ],
