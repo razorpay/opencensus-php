88927c2c658c71fae8b717a969ad2e9f8edfdd1b
Vivek <viveklakhera@gmail.com>
2017-09-18 12:34:45 +0530

    [sharp] Add validation for the actions (#5037)
    * [sharp] Add validation for the actions
    
    * Added action constants, and validation for action in gateway manager
    
    * Fixed exception message
    
    * Fixed exception message
    
    * Fixed action for gatewayManager
    
    * Removed debug statements
    
    * Fixed gateway sharp server
    
    * Thrown RuntimeException when action is invalid
    
    * Added more actions
    
    * Fixed exception type and message
    
    * [Merchant Invoice] Update gstin - handle errors (#5139)
    
    * [Merchant Invoice] Update gstin rewrite
    
    * Fixed exception data
    
    * Changes for beta testing
    
    * Fixed exception data
    
    * Removed beta test changes
    
    * Minor changes
    
    * Revert "[Merchant Invoice] Update gstin - handle errors (#5139)"
    
    This reverts commit 55142e1f802597cedfc6f36ad44b99cf8c36ba25.

--- app/Gateway/GatewayManager.php
+++ app/Gateway/GatewayManager.php
@@ -36,6 +36,9 @@ class GatewayManager extends \Illuminate\Support\Manager
 
         $gateway->setGatewayParams($input, $mode, $terminal);
 
+        // Validate action
+        Payment\Action::validateAction($action);
+
         // Laravel helper function converts snake case to camel case
         $action = camel_case($action);
 
