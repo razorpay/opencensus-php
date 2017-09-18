d0d56290ab40c34b76e44e1b78fece25870652da
Kunal Kushwah <kunal.kushwah@razorpay.com>
2017-09-18 18:40:08 +0530

    fixing fees const

--- app/Models/Adjustment/Core.php
+++ app/Models/Adjustment/Core.php
@@ -31,20 +31,20 @@ class Core extends Base\Core
 
         $tax =  $adjInput[MerchantInvoice\Entity::TAX] ?? 0;
 
-        $fees = $adjInput[Entity::FEES] ?? 0;
+        $fees = $adjInput[Settlement\Entity::FEES] ?? 0;
 
         $adjInput[Entity::AMOUNT] = $amount + $tax + $fees;
 
         if (isset($adjInput[Entity::AMOUNT]) === true and
             isset($adjInput[MerchantInvoice\Entity::TAX]) === false and
-            isset($adjInput[Entity::FEES]) === false)
+            isset($adjInput[Settlement\Entity::FEES]) === false)
         {
             $adj = $this->createAdjEntityAndSetWorkflow($adjInput, $merchant);
 
             return $this->transaction([$this, 'createAdjInTransaction'], $adj, $merchant);
         }
         elseif ((isset($adjInput[Entity::AMOUNT]) === false) and
-            (isset($adjInput[MerchantInvoice\Entity::TAX]) or isset($adjInput[Entity::FEES])) === true)
+            (isset($adjInput[MerchantInvoice\Entity::TAX]) or isset($adjInput[Settlement\Entity::FEES])) === true)
         {
             $adj = $this->createAdjEntityAndSetWorkflow($adjInput, $merchant);
 
