78f638cdadbc116ed3e59fd3b9b84f820521a9da
hobbes09 <sourin.sutradhar@gmail.com>
2017-09-19 13:53:58 +0530

    [merchant, harvester] merchant analytics feature

--- app/Models/Merchant/Service.php
+++ app/Models/Merchant/Service.php
@@ -29,6 +29,7 @@ use RZP\Models\Admin\Group;
 use RZP\Models\BankAccount;
 use RZP\Base\RuntimeManager;
 use RZP\Models\Merchant\Webhook;
+use Requests_Response as Response;
 use RZP\Models\Settlement\Holidays;
 use RZP\Models\Schedule\Task as ScheduleTask;
 use RZP\Models\Merchant\SlackActions as SlackActions;
@@ -1352,4 +1353,12 @@ class Service extends Base\Service
 
         return $mailer;
     }
+
+    public function fetchAnalytics($input)
+    {
+        (new Core())->validateFilterAttributesAndAddMerchantId($this->merchant->getId(), $input);
+
+        return $this->app['eventManager']->query($input);
+    }
+
 }
