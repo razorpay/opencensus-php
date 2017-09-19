b276513b2a19e76b9fc97d7b052b5f067c129d6a
Ria Garg <ria.garg@razorpay.com>
2017-09-19 22:06:55 +0530

    [irctc] added route for irctc refund report

--- app/Http/Route.php
+++ app/Http/Route.php
@@ -312,6 +312,7 @@ final class Route
         'reports_monthly_invoice'                 => ['get',      'reports/invoice',                                'MerchantController@getInvoiceReport'                               ],
         'reports_public_entity'                   => ['get',      'reports/{entity}',                               'MerchantController@getPublicEntityReport'                          ],
         'reports_public_entity_file'              => ['get',      'reports/{entity}/file',                          'MerchantController@getPublicEntityReportUrl'                       ],
+        'reports_refund_irctc'                    => ['get',       'reports/refund/irctc',                          'MerchantController@getIrctcRefundReport'                           ],
         'customer_create'                         => ['post',     'customers',                                      'CustomerController@createLocalCustomer'                            ],
         'customer_update'                         => ['put',      'customers/{id}',                                 'CustomerController@updateCustomer'                                 ],
         'customer_fetch_by_id'                    => ['get',      'customers/{id}',                                 'CustomerController@getCustomer'                                    ],
@@ -1020,6 +1021,7 @@ final class Route
         'settings_fetch',
         'settings_upsert',
         'settings_delete',
+        'reports_refund_irctc',
     ];
 
     public static $proxy = [
@@ -1392,6 +1394,7 @@ final class Route
             'merchant_create_invoice_entities',
             'merchant_payout',
             'gateway_file_create',
+            'reports_refund_irctc',
         ],
 
         'kotak' => [
