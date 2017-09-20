cd5f3ffd7c633f513952ee1158d47a4acb7e3a11
rzpamidi <prashanth.pamidi@razorpay.com>
2017-09-20 19:06:33 +0530

    Checks for old user

--- public/react/merchant/containers/Home/OnboardingCard/index.js
+++ public/react/merchant/containers/Home/OnboardingCard/index.js
@@ -23,6 +23,7 @@ export default class OnboardingCard extends Component {
     this.setState({
       showOnboarding: LocalStorageService.getItem('show_onboarding_card'),
       isFirstStep: LocalStorageService.getItem('onboarding_first_step'),
+      isOldUser: !LocalStorageService.getItem('ngStorage-new_user_signup'),
     });
   }
 
@@ -38,7 +39,7 @@ export default class OnboardingCard extends Component {
 
   render() {
     let { user, mode, modeFormatted, payments = [] } = this.props;
-    let { isFirstStep, showOnboarding } = this.state;
+    let { isFirstStep, showOnboarding, isOldUser } = this.state;
 
     if (!showOnboarding || user.isOldUIEnabled) {
       return null;
@@ -52,92 +53,92 @@ export default class OnboardingCard extends Component {
           <img class="media-object" src={OnboardingIllustrationPNG} />
         </div>
 
-        <div className="media-body">
-          <div className="media-heading">Explore Our Product Stack</div>
-          <p>
-            Presenting India’s first holistic converged payment solution for
-            you. Check our brand new products.
-          </p>
-          <div className="new-products-row">
-            {newProducts.map((product, key) =>
-              <div
-                key={key}
-                className={`product-item`}
-                style={productItemStyle}
-              >
-                <MediaCard title={product.name} symbol={product.symbol}>
-                  <div className="text-small m-b">
-                    {product.description}
-                  </div>
-                  <Link to={product.link}>Activate</Link>
-                </MediaCard>
-              </div>
-            )}
-          </div>
-        </div>
-
-        {isFirstStep
-          ? <div class="media-body hide">
-              <div class="media-heading">
-                Welcome to Razorpay. Let's get started.
-              </div>
+        {isOldUser
+          ? <div className="media-body">
+              <div className="media-heading">Explore Our Product Stack</div>
               <p>
-                Dashboard is your one stop for all your payments. Using
-                dashboard, here are some of the things you can do:
+                Presenting India’s first holistic converged payment solution for
+                you. Check our brand new products.
               </p>
-              <ul class="row">
-                <li class="col-sm-4">Complete Activation Process</li>
-                <li class="col-sm-4">Generate Financial Reports</li>
-                <li class="col-sm-4">Issue Refunds</li>
-                <li class="col-sm-4">Check Transaction History</li>
-                <li class="col-sm-4">Access Razorpay Products</li>
-                <li class="col-sm-4">Check Settlements</li>
-              </ul>
-
-              <button
-                class="btn btn-lg btn-primary"
-                onClick={this.gotoNextStep}
-              >
-                <span>Got it! So, what's next?</span>
-                <i class="icon icon-chevron-right" />
-              </button>
+              <div className="new-products-row">
+                {newProducts.map((product, key) =>
+                  <div
+                    key={key}
+                    className={`product-item`}
+                    style={productItemStyle}
+                  >
+                    <MediaCard title={product.name} symbol={product.symbol}>
+                      <div className="text-small m-b">
+                        {product.description}
+                      </div>
+                      <Link to={product.link}>Activate</Link>
+                    </MediaCard>
+                  </div>
+                )}
+              </div>
             </div>
-          : <div class="media-body hide">
-              {user.isActivated
-                ? <button class="close" onClick={this.closeOnboarding}>
-                    <i class="icon icon-close" />
-                  </button>
-                : null}
-              <div class="media-heading">Your Next Steps...</div>
-              <p>
-                Your Razorpay account is created. Now, you can browse through
-                the dashboard or do the following:
-              </p>
-              <div class="row">
-                <div class="col-sm-6" style={{ paddingRight: 0 }}>
-                  <ActivationStep user={user} />
+          : isFirstStep
+            ? <div class="media-body hide">
+                <div class="media-heading">
+                  Welcome to Razorpay. Let's get started.
                 </div>
+                <p>
+                  Dashboard is your one stop for all your payments. Using
+                  dashboard, here are some of the things you can do:
+                </p>
+                <ul class="row">
+                  <li class="col-sm-4">Complete Activation Process</li>
+                  <li class="col-sm-4">Generate Financial Reports</li>
+                  <li class="col-sm-4">Issue Refunds</li>
+                  <li class="col-sm-4">Check Transaction History</li>
+                  <li class="col-sm-4">Access Razorpay Products</li>
+                  <li class="col-sm-4">Check Settlements</li>
+                </ul>
 
-                <div class="col-sm-6">
-                  {payments.length
-                    ? <PaymentsReceivedStep />
-                    : <KeyGenerationStep
-                        user={user}
-                        mode={mode}
-                        modeFormatted={modeFormatted}
-                      />}
-                </div>
+                <button
+                  class="btn btn-lg btn-primary"
+                  onClick={this.gotoNextStep}
+                >
+                  <span>Got it! So, what's next?</span>
+                  <i class="icon icon-chevron-right" />
+                </button>
               </div>
-
-              {user.isActivated
-                ? <div style={{ marginTop: '12px' }}>
-                    You may now{' '}
-                    <a onClick={this.closeOnboarding}>close this card</a>
-                    . You can access the <a>documentation</a> from topbar, if
-                    needed.
+            : <div class="media-body hide">
+                {user.isActivated
+                  ? <button class="close" onClick={this.closeOnboarding}>
+                      <i class="icon icon-close" />
+                    </button>
+                  : null}
+                <div class="media-heading">Your Next Steps...</div>
+                <p>
+                  Your Razorpay account is created. Now, you can browse through
+                  the dashboard or do the following:
+                </p>
+                <div class="row">
+                  <div class="col-sm-6" style={{ paddingRight: 0 }}>
+                    <ActivationStep user={user} />
                   </div>
-                : null}
-            </div>}
+
+                  <div class="col-sm-6">
+                    {payments.length
+                      ? <PaymentsReceivedStep />
+                      : <KeyGenerationStep
+                          user={user}
+                          mode={mode}
+                          modeFormatted={modeFormatted}
+                        />}
+                  </div>
+                </div>
+
+                {user.isActivated
+                  ? <div style={{ marginTop: '12px' }}>
+                      You may now{' '}
+                      <a onClick={this.closeOnboarding}>close this card</a>
+                      . You can access the <a>documentation</a> from topbar, if
+                      needed.
+                    </div>
+                  : null}
+              </div>}
       </div>
     );
   }
