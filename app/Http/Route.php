a5a67dd6a88db779f9b8d8fac7e2cd5ea0b9e812
Pratik Kapasi <pratik.kapasi@razorpay.com>
2017-09-20 20:13:15 +0530

    [feature-onboarding] routes updated

--- app/Http/Route.php
+++ app/Http/Route.php
@@ -1112,6 +1112,7 @@ final class Route
         'merchant_analytics',
         'feature_onboarding_fetch_questions',
         'feature_onboarding_create',
+        'feature_onboarding_fetch_responses',
         'feature_onboarding_fetch_all_responses',
     ];
 
