5987364d00ad97577521b5b34665f3eab55462ec
rzpamidi <prashanth.pamidi@razorpay.com>
2017-09-21 04:22:50 +0530

    Minor Changes

--- public/react/merchant/components/ActivationBanner.js
+++ public/react/merchant/components/ActivationBanner.js
@@ -9,11 +9,16 @@ export default ({ productName, symbol, onActivate }) => {
         symbol={symbol}
         title={`Get Started with ${productName} in live mode`}
       >
-        <p>
-          You are currently in test mode. To integrate in test mode, you can go
-          through the documentation. To activate {productName} in live mode, you
-          can request for activation and we will get back to you in 24 hours.
-        </p>
+        <div className="m-b">
+          <div>
+            You are currently in test mode. To integrate in test mode, you can
+            go through the documentation.
+          </div>
+          <div>
+            To activate {productName} in live mode, you can request for
+            activation and we will get back to you in 24 hours.
+          </div>
+        </div>
         <button className="btn btn-default" onClick={onActivate}>
           Activate Now
         </button>
