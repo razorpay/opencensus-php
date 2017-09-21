f185d63649aa684a294104040eb8bfa9f7d963b1
Amit Mahbubani <amit.mahbubani@razorpay.com>
2017-09-21 09:33:19 +0530

    [merchant] Add linked_account_kyc to edit

--- app/Models/Merchant/Validator.php
+++ app/Models/Merchant/Validator.php
@@ -41,6 +41,7 @@ class Validator extends Base\Validator
         Entity::BILLING_LABEL               => 'sometimes|max:255',
         Entity::TRANSACTION_REPORT_EMAIL    => 'sometimes|array',
         Entity::RECEIPT_EMAIL_ENABLED       => 'sometimes|boolean',
+        Entity::LINKED_ACCOUNT_KYC          => 'sometimes|boolean',
         Entity::SETTLEMENT_SCHEDULE         => 'sometimes|integer|min:1|max:30',
         Entity::NAME                        => 'sometimes|alpha_space_num|max:200',
         Entity::RISK_RATING                 => 'sometimes|min:0|max:5',
