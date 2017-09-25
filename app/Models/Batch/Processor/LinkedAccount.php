4910730bd2f206aa2d141908fe272099f7ccb961
Amit Mahbubani <amit.mahbubani@razorpay.com>
2017-09-25 18:16:21 +0530

    [batch] Add linked_account processor; refactor

---
+++ app/Models/Batch/Processor/LinkedAccount.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace RZP\Models\Batch\Processor;
+
+class LinkedAccount extends Base
+{
+    protected function processEntries(array & $entries)
+    {
+        // Get input - account name
+
+        // Create merchant
+
+        // Get Merchant Details input
+
+        // Submit
+
+        // Append merchant ID to response report
+    }
+}
