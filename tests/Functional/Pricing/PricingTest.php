fdfd41d03ddd17c06ee6d2a225b2e06938b901a2
hobbes09 <sourin.sutradhar@gmail.com>
2017-09-25 22:56:50 +0530

    [pricing] moved bulk create api to admin auth

--- tests/Functional/Pricing/PricingTest.php
+++ tests/Functional/Pricing/PricingTest.php
@@ -32,26 +32,36 @@ class PricingTest extends TestCase
 
     public function testBulkPricingPlan()
     {
+        $this->ba->adminAuth('test', null, 'org_' . Org::RZP_ORG);
+
         $this->startTest();
     }
 
     public function testEmptyBulkPricingPlan()
     {
+        $this->ba->adminAuth('test', null, 'org_' . Org::RZP_ORG);
+
         $this->startTest();
     }
 
     public function testDuplicateBulkPricingPlan()
     {
+        $this->ba->adminAuth('test', null, 'org_' . Org::RZP_ORG);
+
         $this->startTest();
     }
 
     public function testCreatePricingPlanWithMinAndMaxFee()
     {
+        $this->ba->adminAuth('test', null, 'org_' . Org::RZP_ORG);
+
         $this->startTest();
     }
 
     public function testCreatePricingPlanWithInvalidMinAndMaxFee()
     {
+        $this->ba->adminAuth('test', null, 'org_' . Org::RZP_ORG);
+
         $this->startTest();
     }
 
