d24432db3a89fc77e3400c2bc9a2f7e5df067d7c
Pranav Gupta <pranavguptarulz@gmail.com>
2017-09-26 11:41:24 +0530

    replaced utf-8 charaters with html code

--- resources/views/gateway/gatewayPostForm.blade.php
+++ resources/views/gateway/gatewayPostForm.blade.php
@@ -203,6 +203,7 @@ body{
 </head>
 <body onload="document.form1.submit()">
   <div id='bg'></div>
+  <div style="display:inline-block;vertical-align:middle;height:100%"></div>
   <div id='cntnt'>
     <div id="hdr">
       @if (isset($data['image']))
@@ -225,7 +226,7 @@ body{
     <div id="ldr"></div>
     <div id="txt">
       <div style="display:inline-block;vertical-align:middle;white-space:normal;">
-        <h2 id='title'>Loading Bank page…</h2>
+        <h2 id='title'>Loading Bank page&#x2026;</h2>
         <p id='msg'>Please wait while we redirect you to your Bank page</p>
       </div>
       <div style="display:inline-block;vertical-align:middle;height:100%"></div>
@@ -235,7 +236,6 @@ body{
       <div style="display:inline-block;vertical-align:middle;height:100%"></div>
     </div>
   </div>
-  <div style="display:inline-block;vertical-align:middle;height:100%"></div>
   <form id="form1" name="form1" action="{{$data['request']['url']}}" method="post" onsubmit="return true;">
   @foreach ($data['request']['content'] as $key => $value)
     <input type="hidden" name="{{$key}}" value="{{$value}}">
