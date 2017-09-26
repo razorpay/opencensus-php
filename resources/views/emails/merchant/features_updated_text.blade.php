bac4a435e30671b632a988aee23f1bc00f117b47
Pratik Kapasi <pratik.kapasi@razorpay.com>
2017-09-26 16:43:01 +0530

    [features] added email views

---
+++ resources/views/emails/merchant/features_updated_text.blade.php
@@ -0,0 +1,10 @@
+Hi,
+
+We are writing to let you know that the following features have been enabled on your account for the live mode -
+{{explode(',', $features)}}.
+
+
+You can contact us at contact@razorpay.com in case of any discrepancy.
+
+--
+Team Razorpay
