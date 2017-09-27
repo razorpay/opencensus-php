610914b0eac2fccecbdc5ef78db74cbd684231a3
Patlola <praveen.patlola@razorpay.com>
2017-09-27 09:18:19 +0000

    not running authpolicy for superadmin

--- app/Models/Admin/Org/AuthPolicy/Validator.php
+++ app/Models/Admin/Org/AuthPolicy/Validator.php
@@ -36,26 +36,29 @@ class Validator extends Base\Validator
 
     public function validate(Admin\Entity $admin, array $data, string $op)
     {
-        $prop = $op . 'PolicyRules';
-
-        $attributes = $this->entity->toArray();
-
-        foreach ($this->{$prop} as $rule)
+        if ($admin->isSuperAdmin() === false)
         {
-            $class = 'RZP\Models\Admin\Org\AuthPolicy\Rules\\' . studly_case($rule) . 'Rule';
+            $prop = $op . 'PolicyRules';
 
-            // In some cases like Locked Account Rule
-            // we won't need a value from the Policy Entity
-            if (isset($attributes[$rule]))
-            {
-                $classOb = new $class($attributes[$rule]);
-            }
-            else
-            {
-                $classOb = new $class();
-            }
+            $attributes = $this->entity->toArray();
 
-            $classOb->validate($admin, $data);
+            foreach ($this->{$prop} as $rule)
+            {
+                $class = 'RZP\Models\Admin\Org\AuthPolicy\Rules\\' . studly_case($rule) . 'Rule';
+
+                // In some cases like Locked Account Rule
+                // we won't need a value from the Policy Entity
+                if (isset($attributes[$rule]))
+                {
+                    $classOb = new $class($attributes[$rule]);
+                }
+                else
+                {
+                    $classOb = new $class();
+                }
+
+                $classOb->validate($admin, $data);
+            }
         }
     }
 }
