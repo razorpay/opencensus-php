0358026ea882cf362e1b65f4a9517d60149c3af6
Albin George <albinin0002@gmail.com>
2017-09-27 15:13:15 +0530

    [bob_nb] Recon test

---
+++ app/Gateway/Netbanking/Bob/Mock/Reconciliator.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace RZP\Gateway\Netbanking\Bob\Mock;
+
+use Carbon\Carbon;
+use RZP\Constants\Timezone;
+
+use RZP\Gateway\Base;
+use RZP\Models\FileStore;
+use RZP\Models\Payment;
+
+class Reconciliator extends Base\RefundFile
+{
+    const PAYMENT_ENTITY = 'payment';
+    const GATEWAY_ENTITY = 'gateway';
+
+    const HEADER_1 = 'fileName.txt|Num of Txn|Total Amount of Txns';
+    const HEADER_2 = 'Sr.No|fldMerchCode|TransDate|fldMerchRefNbr|Transaction Amount|fldBankRefNbr|AccountNo.';
+
+    const BANK_REF_NUMBER = 'AB1234';
+    const BANK_ACC_NUMBER = '309002069863';
+
+    protected static $fileToWriteName = 'Bob_Netbanking_Reconciliation';
+
+    protected $header1 = ['fileName.txt', 'Num of Txn', 'Total Amount of Txns'];
+    protected $header2 = [
+        'Sr.No',
+        'fldMerchCode',
+        'TransDate',
+        'fldMerchRefNbr',
+        'Transaction Amount',
+        'fldBankRefNbr',
+        'AccountNo.'
+    ];
+
+    public function generateReconciliation($input = null)
+    {
+        $input = [
+            'gateway' => 'netbanking_bob'
+        ];
+
+        $payments = $this->repo->payment->fetch($input, '10000000000000');
+
+        $inputData = [];
+
+        foreach ($payments as $payment)
+        {
+            $data[self::PAYMENT_ENTITY] = $payment->toArray();
+
+            $gatewayInput['payment_id'] = $payment[Payment\Entity::ID];
+
+            $gatewayPayment = $this->repo->netbanking->fetch($gatewayInput);
+
+            $data[self::GATEWAY_ENTITY] = $gatewayPayment[0]->toArray();
+
+            $inputData[] = $data;
+        }
+
+        return $this->generate($inputData);
+    }
+
+    public function generate($input)
+    {
+        list($totalAmount, $data) = $this->getReconciliationData($input);
+
+        $fileName = $this->getFileToWriteNameWithoutExt();
+
+        $txt = $this->generateText($data, '|');
+
+        $creator = $this->createFile(
+            FileStore\Format::TXT,
+            $txt,
+            $fileName,
+            FileStore\Type::BOB_NETBANKING_REFUND
+        );
+
+        $file = $creator->get();
+
+        return [
+            'local_file_path' => $file['local_file_path'],
+            'count'           => count($data),
+            'file_name'       => basename($file['local_file_path']),
+            'total_amount'    => $totalAmount,
+        ];
+    }
+
+    protected function getReconciliationData(array $input)
+    {
+        $data = [];
+
+        $index = 1;
+
+        $totalAmount = 0.0;
+
+        foreach ($input as $row)
+        {
+            $date = Carbon::createFromTimestamp(
+                        $row[self::PAYMENT_ENTITY][Payment\Entity::CREATED_AT],
+                        Timezone::IST)
+                        ->format('d-M-y');
+
+            $amount = $this->getFormattedAmount($row['payment']['amount']);
+
+            $data[] = [
+                'sr_no' => $index++,
+                'merchant_code' => 'razorpay',
+                'date' => $date,
+                'payment_id' => $row['payment']['id'],
+                'amount' => $amount,
+                'bank_ref_number' => self::BANK_REF_NUMBER,
+                'bank_acc_number' => self::BANK_ACC_NUMBER,
+            ];
+
+            $totalAmount += floatval($amount);
+        }
+
+        $output = array_merge(
+            $this->generateHeaders($data, $totalAmount),
+            $data
+        );
+
+        return [$totalAmount, $output];
+    }
+
+    protected function generateHeaders($data, $totalAmount)
+    {
+        $output = [$this->header1];
+
+        $output[] = [
+            self::$fileToWriteName . ".txt",
+            count($data),
+            $totalAmount
+        ];
+
+        $output[] = $this->header2;
+
+        return $output;
+    }
+}
