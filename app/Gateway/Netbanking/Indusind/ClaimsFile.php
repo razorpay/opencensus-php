ed7a1bdfd4c0a95d641b1627e9ea97a643560326
Ria Garg <ria.garg@razorpay.com>
2017-09-27 12:35:09 +0530

    [indusind] Providing dump of successful transactions

--- app/Gateway/Netbanking/Indusind/ClaimsFile.php
+++ app/Gateway/Netbanking/Indusind/ClaimsFile.php
@@ -2,17 +2,49 @@
 
 namespace RZP\Gateway\Netbanking\Indusind;
 
+use Carbon\Carbon;
+use RZP\Constants\Timezone;
+
+use RZP\Constants\Mode;
 use RZP\Gateway\Base;
+use RZP\Models\FileStore;
 
 class ClaimsFile extends Base\RefundFile
 {
+    protected static $fileToWriteName = 'PGClaimRazorpay';
+
     /**
      * Not generating a claims file because Indusind
      * Only needs total count and total amount to be sent across
      */
     public function generate($input)
     {
-        return $this->getClaimsData($input);
+        list($totalAmount , $count, $data) =  $this->getClaimsData($input);
+
+        $fileText = $this->getTextData($data);
+
+        $fileName = $this->getFileToWriteNameWithoutExt();
+
+        // Creating a file with txt format
+        $creator = $this->createFile(
+            FileStore\Format::TXT,
+            $fileText,
+            $fileName,
+            FileStore\Type::INDUSIND_NETBANKING_CLAIM);
+
+        $file = $creator->get();
+
+        $today = Carbon::now(Timezone::IST)->format('d_m_Y');
+
+        $signedFileUrl = $creator->getSignedUrl(self::SIGNED_URL_DURATION)['url'];
+
+
+        return [
+            'total_amount'    => $totalAmount,
+            'count'           => $count,
+            'signed_url'      => $signedFileUrl,
+            'local_file_path' => $file['local_file_path'],
+        ];
     }
 
     protected function getClaimsData(array $input)
@@ -20,18 +52,48 @@ class ClaimsFile extends Base\RefundFile
         $totalAmount = 0;
         $count = 0;
 
-        foreach ($input['data'] as $row)
+        $data = [];
+
+         foreach ($input['data'] as $index => $row)
         {
+            $data[] = [
+                RefundFileFields::SERIAL_NO          => $index + 1,
+                RefundFileFields::TRANSACTION_ID     => $row['payment']['id'],
+                RefundFileFields::BANK_REFERENCE_ID  => $row['gateway']['bank_payment_id']
+            ];
+
             $totalAmount += $row['payment']['amount'] / 100;
 
             $count++;
         }
 
-        return [
-            'total_amount'   => $totalAmount,
-            'count'          => $count,
-            'signed_url'     => '',
-            'local_file_path' => '',
-        ];
+
+        return [$totalAmount , $count, $data];
+    }
+
+    protected function getTextData(array $data, string $prependLine = '')
+    {
+        $ignoreLastNewline = true;
+
+        $txt = $this->generateText($data, '|', $ignoreLastNewline);
+
+        $txt = $prependLine . $txt;
+
+        return $txt;
+    }
+
+     /*
+     * @override parent class's method
+     */
+    protected function getFileToWriteNameWithoutExt()
+    {
+        $time = Carbon::now(Timezone::IST)->format('dmY');
+
+        if ($this->mode === Mode::TEST)
+        {
+            return static::$fileToWriteName . $time . $this->mode;
+        }
+
+        return static::$fileToWriteName . $time;
     }
 }
