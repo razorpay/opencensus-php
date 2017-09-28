855b551c181609294b0c23c882ca1c6051804177
Nemo <nemo@razorpay.com>
2017-09-28 16:22:12 +0530

    [tests] Mock all Dns methods

--- tests/Functional/Merchant/WebhookTest.php
+++ tests/Functional/Merchant/WebhookTest.php
@@ -9,6 +9,7 @@ use Mail;
 use RZP\Mail\Merchant\Webhook as WebhookMail;
 use Http\Mock\Client;
 use RZP\Jobs\WebHook;
+use Symfony\Bridge\PhpUnit\DnsMock;
 use RZP\Tests\Functional\TestCase;
 use Http\Discovery\MessageFactoryDiscovery;
 use RZP\Tests\Functional\Helpers\Payment\PaymentTrait;
@@ -19,6 +20,9 @@ use Http\Discovery\HttpClientDiscovery;
 use Http\Discovery\Strategy\MockClientStrategy;
 use Http\Client\Common\Exception\ClientErrorException;
 
+/**
+ * @group dns-sensitive
+ */
 class WebhookTest extends TestCase
 {
     use PaymentTrait;
@@ -30,6 +34,32 @@ class WebhookTest extends TestCase
         parent::setUp();
 
         $this->ba->proxyAuth();
+
+        $this->setupMockDns();
+    }
+
+    private function setupMockDns()
+    {
+        DnsMock::withMockedHosts(array(
+            'example.com' => [
+                [
+                    'type' => 'A',
+                    'ip' => '1.2.3.4',
+                ],
+            ],
+            '10.0.0.1.xip.io' => [
+                [
+                    'type'  => 'A',
+                    'ip'    => '10.0.0.1',
+                ],
+            ],
+            '169.254.169.254.xip.io'    => [
+                [
+                    'type'  =>  'A',
+                    'ip'    =>  '169.254.169.254',
+                ]
+            ],
+        ));
     }
 
     public function testCreateWebhook()
