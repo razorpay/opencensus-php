69ead5c71e6ea51bb5775c480716c00fff46b0e9
Pranav Gupta <pranavguptarulz@gmail.com>
2017-10-05 15:19:50 +0530

    admin view base

--- web/modules/admin/App.js
+++ web/modules/admin/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import ModalContainer, { openModal, closeModal } from 'store/modal';
+import { NavLink, Route } from 'react-router-dom';
+import ModalContainer from 'store/modal';
+
+import MerchantList from 'admin/merchants';
 
 export default class App extends Component {
   state = {
@@ -10,9 +13,22 @@ export default class App extends Component {
   render() {
     return (
       <div id="app-container">
-        <main />
+        <main>
+          <Route path="/merchants" component={MerchantList} />
+        </main>
         <header />
-        <aside />
+        <aside>
+          <a href="/admin">
+            <img src="https://cdn.razorpay.com/logo_invert.svg" width="146" />
+          </a>
+          <label>Management</label>
+          <NavLink to="/merchants">Merchants</NavLink>
+          <NavLink to="/stats">Merchant Stats</NavLink>
+          <NavLink to="/plans">Pricing Plans</NavLink>
+          <NavLink to="/entities">Entities</NavLink>
+          <NavLink to="/actions">Actions</NavLink>
+          <NavLink to="/email-logs">Email Logs</NavLink>
+        </aside>
         <ModalContainer />
       </div>
     );
