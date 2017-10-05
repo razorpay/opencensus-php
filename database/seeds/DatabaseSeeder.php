3895ffbd6df5b399e1e7ef88e39cb31215ad13f7
Albin George <albinin0002@gmail.com>
2017-10-05 15:55:28 +0530

    Enable emandate for default merchant in seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -204,6 +204,17 @@ class DatabaseSeeder extends Seeder
                 )
             );
 
+            DB::table(Table::FEATURE)->insert(
+                array(
+                    'id'            => 'feature_303030',
+                    'name'          => 'e_mandate',
+                    'entity_id'     => '10000000000000',
+                    'entity_type'   => 'merchant',
+                    'created_at'    => $currentTime,
+                    'updated_at'    => $currentTime
+                )
+            );
+
             DB::table(Table::MERCHANT_DETAIL)->insert(
                 array(
                     'merchant_id'   => Account::NODAL_ACCOUNT,
