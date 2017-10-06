e9459998e6d4d709369bb6088b4e0c9545fa348a
Patlola <praveen.patlola@razorpay.com>
2017-10-06 09:25:19 +0000

    added pre_signup route to save in route map

--- config/api-route-map.php
+++ config/api-route-map.php
@@ -538,7 +538,8 @@ return [
             'url'       => 'feature/onboarding/{feature}/responses',
             'routeName' => 'feature_onboarding_fetch_responses'
         ],
-        'merchant_pre_signup_details' => 'pre_signup',
+        'merchant_pre_signup_details'           => 'pre_signup',
+        'merchant_edit_pre_signup_details'      => 'pre_signup',
     ],
 
     // auth
