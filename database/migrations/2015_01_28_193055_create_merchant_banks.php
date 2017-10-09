16170aa39dc78d4402c3448bbfc67a68b55036b0
Ria Garg <ria.garg@razorpay.com>
2017-10-09 13:53:16 +0530

    [bharat qr] added new merchant method bharat qr

--- database/migrations/2015_01_28_193055_create_merchant_banks.php
+++ database/migrations/2015_01_28_193055_create_merchant_banks.php
@@ -86,6 +86,9 @@ class CreateMerchantBanks extends Migration
             $table->tinyInteger(Methods::AEPS)
                   ->default(0);
 
+            $table->tinyInteger(Methods::BHARAT_QR)
+                  ->default(0);
+
             $table->integer(Methods::CREATED_AT);
 
             $table->integer(Methods::UPDATED_AT);
