64aee7944155650696d53ac2b83545f93e58429b
Albin George <albinin0002@gmail.com>
2017-10-10 15:07:15 +0530

    [axis_em] Handle s2s in mock gateway

--- app/Gateway/Netbanking/Axis/Mock/Gateway.php
+++ app/Gateway/Netbanking/Axis/Mock/Gateway.php
@@ -14,6 +14,12 @@ class Gateway extends Axis\Gateway
     {
         $request = parent::authorize($input);
 
+        // Second recurring payment is via a server to server call
+        if ($this->isSecondRecurringPaymentRequest($input) === true)
+        {
+            return $request;
+        }
+
         $url = $this->route->getUrlWithPublicAuth('mock_netbanking_payment',
                                                  ['bank' => $this->bank]);
 
