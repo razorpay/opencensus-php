29798dafa8d3e83c504809f41d89d241a6802ef5
Patlola <praveen.patlola@razorpay.com>
2017-10-10 07:19:18 +0000

    mailchimp composer file

--- config/app.php
+++ config/app.php
@@ -285,5 +285,11 @@ return array(
     'sorting_hat' => [
         'token' => env('SORTING_HAT_TOKEN', ''),
         'url'   => env('SORTING_HAT_URL', 'https://sorting-hat-slack.herokuapp.com/')
+    ],
+
+    'mailchimp' => [
+        'list_id'   => env('MAILCHIMP_LIST_ID', 'random_id'),
+        'api_key'   => env('MAILCHIMP_API_TOKEN', 'mailchimp_token'),
+        'mock'      => env('MAILCHIMP_MOCK', false),
     ]
 );
