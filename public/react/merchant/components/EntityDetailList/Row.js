4d0c4f9f95eb416c470cc9129f2c658ba3d0e892
aseemgupta <aseemgupta2625@gmail.com>
2017-10-12 22:38:05 +0530

    [Mod]: Change label for halted status invoice

--- public/react/merchant/components/EntityDetailList/Row.js
+++ public/react/merchant/components/EntityDetailList/Row.js
@@ -49,13 +49,22 @@ export default props => {
     if (item.subscription_status === 'halted') {
       if (isInvoiceWithAttemptsFailed === 2) {
         // To handle situation where such invoices were created in halted state but now invoice is pending (,active, complete, etc)
-        chargeAttemptsFailedText = 'No auto-charge attempted. ';
+        /*
+          chargeAttemptsFailedText = 'No auto-charge attempted. ';
+        */
+
+        // Temp change until api supports properly
+        chargeAttemptsFailedText = <span>Invoice unpaid. </span>;
       } else if (isInvoiceWithAttemptsFailed === 1) {
         // retryingInfo = 'Not retrying automatically';
         // To handle situation where such invoices were created in halted state but now invoice is pending (,active, complete, etc)
-        chargeAttemptsFailedText = (
-          <span>All auto-charge attempts failed. </span>
-        );
+        /*
+          chargeAttemptsFailedText = (
+            <span>All auto-charge attempts failed. </span>
+          );
+        */
+        // Temp change until api supports properly
+        chargeAttemptsFailedText = <span>Invoice unpaid. </span>;
       }
     }
   }
@@ -90,54 +99,62 @@ export default props => {
       <div class="row-item content">
         <div class="detail-row">
           <div class="row-element left">
-            {loading
-              ? <PlaceholderLoader style={{ width: '70%' }} />
-              : item.billing_start
-                ? <span class="label--primary">
-                    <Time value={item.billing_start} format="MMM DD, YYYY" />
-                    {timeDiff > 0 &&
-                      <span>
-                        {' '}(due in {Math.ceil(timeDiff / (3600 * 24))} days)
-                      </span>}
+            {loading ? (
+              <PlaceholderLoader style={{ width: '70%' }} />
+            ) : item.billing_start ? (
+              <span class="label--primary">
+                <Time value={item.billing_start} format="MMM DD, YYYY" />
+                {timeDiff > 0 && (
+                  <span>
+                    {' '}
+                    (due in {Math.ceil(timeDiff / (3600 * 24))} days)
                   </span>
-                : 'Upcoming Payment'}
+                )}
+              </span>
+            ) : (
+              'Upcoming Payment'
+            )}
           </div>
           <div class="row-element right">
-            {loading
-              ? <PlaceholderLoader style={{ width: '45%' }} />
-              : <Amount currency={item.currency} value={item.amount} />}
+            {loading ? (
+              <PlaceholderLoader style={{ width: '45%' }} />
+            ) : (
+              <Amount currency={item.currency} value={item.amount} />
+            )}
           </div>
         </div>
 
         <div class="detail-row">
           <div class="row-element left">
-            {loading
-              ? <PlaceholderLoader style={{ width: '60%', height: '10px' }} />
-              : <span class="label--secondary">
-                  {index ? `Recurring payment # ${index}` : ''}
-                  {index && isUpfront ? ', ' : ''}
-                  {isUpfront ? 'Upfront Amount' : ''}
-                </span>}
+            {loading ? (
+              <PlaceholderLoader style={{ width: '60%', height: '10px' }} />
+            ) : (
+              <span class="label--secondary">
+                {index ? `Recurring payment # ${index}` : ''}
+                {index && isUpfront ? ', ' : ''}
+                {isUpfront ? 'Upfront Amount' : ''}
+              </span>
+            )}
           </div>
           <div class="row-element right">
-            {loading
-              ? <PlaceholderLoader style={{ width: '30%' }} />
-              : <span class="tag">
-                  <InvoiceStatusLabel status={item.status} />
-                </span>}
+            {loading ? (
+              <PlaceholderLoader style={{ width: '30%' }} />
+            ) : (
+              <span class="tag">
+                <InvoiceStatusLabel status={item.status} />
+              </span>
+            )}
           </div>
         </div>
 
         <div class="detail-row">
           {item.id &&
-          (chargeAttemptsFailedText || retryingInfo) && [
-            <span key="retrying-attempts" class="text-danger">
-              <i class="icon icon-info-circle" /> {chargeAttemptsFailedText}
-            </span>,
-            <span key="retrying-info">
-              {' '}{retryingInfo}
-            </span>,
-          ]}
+            (chargeAttemptsFailedText || retryingInfo) && [
+              <span key="retrying-attempts" class="text-danger">
+                <i class="icon icon-info-circle" /> {chargeAttemptsFailedText}
+              </span>,
+              <span key="retrying-info"> {retryingInfo}</span>,
+            ]}
           {
             do {
               if (
