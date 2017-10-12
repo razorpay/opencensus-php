d136ef3ecb0a29a5dfc68f4c57b6b7a3a4cda42f
aseemgupta <aseemgupta2625@gmail.com>
2017-10-12 16:44:34 +0530

    [Mod]: Add entity row and styles

---
+++ web/js/ui/EntityRow.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default ({ label, value, children, ...otherProps }) => {
+  if (value === null || value === undefined || value === '') {
+    value = '--';
+  }
+
+  return (
+    <div class="row-item clearfix" {...otherProps}>
+      {typeof label === 'function' ? (
+        label()
+      ) : (
+        <div class="row-label">{label}</div>
+      )}
+      <div class="row-value">
+        {children ? (
+          children
+        ) : typeof value === 'function' ? (
+          value()
+        ) : (
+          <span>{value + ''}</span>
+        )}
+      </div>
+    </div>
+  );
+};
