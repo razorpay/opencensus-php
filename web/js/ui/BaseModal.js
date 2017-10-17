6b53fe5cd62c2424820bbfb6944fa2ce51b7a23c
aseemgupta <aseemgupta2625@gmail.com>
2017-10-17 14:51:14 +0530

    [Mod]: Remove size props. Take custom class instead

--- web/js/ui/BaseModal.js
+++ web/js/ui/BaseModal.js
@@ -6,10 +6,11 @@ import React, { Component } from 'react';
   Props:
     title: Header of modal. Pass string or custom component or divs
     children: contents of the modal
+    customClass: optional string props
 */
-export default ({ title, children, size = 'small' }) => [
-  <div class={`base-modal modal-${size}`}>
-    <div class="modal-header">{title}</div>
+export default ({ header, children, customClass = '' }) => (
+  <div class={`base-modal ${customClass}`}>
+    <div class="modal-header">{header}</div>
     <div class="modal-body">{children}</div>
-  </div>,
-];
+  </div>
+);
