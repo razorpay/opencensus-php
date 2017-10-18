dfb9efb206480a8b2bf59a0550a9c9f9e4e21f8a
Patlola <praveen.patlola@razorpay.com>
2017-10-18 09:03:58 +0000

    sending params in body

--- app/Admin/Service.php
+++ app/Admin/Service.php
@@ -1159,9 +1159,9 @@ class Service extends Base\Service
             ];
 
             $requestConfig = [
-                'route_name'    => 'admin_get_app_auth',
-                'query_params'  => $params,
-                'mode'          => 'live'
+                'route_name' => 'admin_get_app_auth',
+                'body'       => $params,
+                'mode'       => 'live',
             ];
 
             $genericService = new Generic\Service;
