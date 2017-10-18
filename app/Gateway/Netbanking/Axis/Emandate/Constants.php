6a5c986cef9a78810deb3bd74ace4e51dbd0d8ec
Albin George <albinin0002@gmail.com>
2017-10-18 17:33:54 +0530

    [axis_em] Remove frequency constants

--- app/Gateway/Netbanking/Axis/Emandate/Constants.php
+++ app/Gateway/Netbanking/Axis/Emandate/Constants.php
@@ -6,10 +6,4 @@ class Constants
 {
     const VERSION = '1.0';
     const PMD     = 'AIB';
-
-    const FREQUENCY_WEEKLY    = 'weekly';
-    const FREQUENCY_MONTHLY   = 'monthly';
-    const FREQUENCY_QUARTERLY = 'quarterly';
-    const FREQUENCY_YEARLY    = 'yearly';
-    const FREQUENCY_ADHOC     = 'adhoc';
 }
