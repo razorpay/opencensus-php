e2d41b2b08384302b0dd5f12e3af825631cda107
Amit Mahbubani <amit.mahbubani@razorpay.com>
2017-10-23 17:41:44 +0530

    [reversal] Fix associate order

--- app/Models/Reversal/Core.php
+++ app/Models/Reversal/Core.php
@@ -52,13 +52,13 @@ class Core extends Base\Core
 
         $reversal->merchant()->associate($merchant);
 
+        $reversal->source()->associate($transfer);
+
         $txn = (new Transaction\Core)->createFromTransferReversal($reversal);
 
         $this->repo->saveOrFail($txn);
 
         $reversal->transaction()->associate($txn);
-
-        $reversal->source()->associate($transfer);
 
         $this->repo->saveOrFail($reversal);
 
