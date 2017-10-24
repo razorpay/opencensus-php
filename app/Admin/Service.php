54f76b99e678910807246f87967ac2f6d9576431
Sumeeti Lalwani <sumeeti.lalwani@razorpay.com>
2017-10-24 17:35:16 +0530

    Adding terminal mode to assign terminal flow on merchant detail page (#1765)

--- app/Admin/Service.php
+++ app/Admin/Service.php
@@ -519,6 +519,10 @@ class Service extends Base\Service
 
         $mode = $input['mode'];
 
+        $input['mode'] = $input['terminal_mode'];
+
+        unset($input['terminal_mode']);
+
         if (empty($error))
         {
             $this->dropFields($input, [
