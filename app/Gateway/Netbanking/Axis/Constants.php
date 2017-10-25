b4357293f781d0dd142247d82a411e32498eb392
Albin George <albinin0002@gmail.com>
2017-10-25 16:58:16 +0530

    [axis_em] Remove no modification const from axis constant

--- app/Gateway/Netbanking/Axis/Constants.php
+++ app/Gateway/Netbanking/Axis/Constants.php
@@ -4,12 +4,11 @@ namespace RZP\Gateway\Netbanking\Axis;
 
 class Constants
 {
-    const PAY             = 'P';
-    const RESPONSE        = 'AUTO';
-    const PAYEE_NAME      = 'RAZORPAY';
-    const AUTH_MENU_ID    = 'CIMSHP';
-    const AUTH_CALL_MODE  = '2';
-    const CATEGORY_ID     = 'IRCSM';
-    const MODE_CBC        = 2;
-    const NO_MODIFICATION = 'MN';
+    const PAY            = 'P';
+    const RESPONSE       = 'AUTO';
+    const PAYEE_NAME     = 'RAZORPAY';
+    const AUTH_MENU_ID   = 'CIMSHP';
+    const AUTH_CALL_MODE = '2';
+    const CATEGORY_ID    = 'IRCSM';
+    const MODE_CBC       = 2;
 }
