5b3bba039e8d7bc6e1281cb81a59339e12d74291
surya15t <surya.teja@razorpay.com>
2017-10-25 17:57:22 +0530

    [merchant] remove unset business_name

--- app/User/Service.php
+++ app/User/Service.php
@@ -63,8 +63,6 @@ class Service extends Base\Service
      */
     public function register($input)
     {
-        unset($input['business_name']);
-
         $registerUser = [
             'route_name' => 'user_register',
             'body'       => $input
