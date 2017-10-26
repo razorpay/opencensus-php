969bb1b75cfd9d89da9733d8c28de11c9251bac8
Priyanshu Chhazed <priyanshu.chhazed@razorpay.com>
2017-10-26 12:19:13 +0530

    Fixed comment-style

--- app/Gateway/Netbanking/Axis/Emandate/Constants.php
+++ app/Gateway/Netbanking/Axis/Emandate/Constants.php
@@ -6,30 +6,30 @@ class Constants
 {
     const VERSION         = '1.0';
 
-    /**
-     * "PMD" represents payment mode. Can contain "AIB" or "CD"
-     * AIB = Axis Internet Banking
-     * CD  = Credit / Debit Cards
-     */
+    //
+    // "PMD" represents payment mode. Can contain "AIB" or "CD"
+    // AIB = Axis Internet Banking
+    // CD  = Credit / Debit Cards
+    //
     const PMD             = 'AIB';
 
     // This value was set to TEST in the documentation
     const TYPE            = 'TEST';
 
-    /**
-     * From the document:
-     *
-     * Bank system will consider modification/Data entry is there at bank end.
-     * After clicking on submit from corporate site to bank site it will take
-     * to intermediate page where all details received from corporate site in
-     * request ( In PPI field) will display for modification/Data entry and
-     * user will submit request to payment gateway page.
-     */
+    //
+    // From the document:
+    //
+    // Bank system will consider modification/Data entry is there at bank end.
+    // After clicking on submit from corporate site to bank site it will take
+    // to intermediate page where all details received from corporate site in
+    // request ( In PPI field) will display for modification/Data entry and
+    // user will submit request to payment gateway page.
+    //
     const NO_MODIFICATION = 'MN';
 
-    /**
-     * We can pass any value to this field, since they're not using it as confirmed in the mail.
-     * @see PRE_POP_INFO
-     */
+    //
+    //We can pass any value to this field, since they're not using it as confirmed in the mail.
+    //@see PRE_POP_INFO
+    //
     const PPI_AMOUNT_TYPE = 'max';
 }
