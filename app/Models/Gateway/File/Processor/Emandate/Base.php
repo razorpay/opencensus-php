5f4f001f087525c03e5692249aed728b5e769a9b
Priyanshu Chhazed <priyanshu.chhazed@razorpay.com>
2017-10-26 14:06:57 +0530

    Removed throw exception

--- app/Models/Gateway/File/Processor/Emandate/Base.php
+++ app/Models/Gateway/File/Processor/Emandate/Base.php
@@ -61,7 +61,6 @@ abstract class Base extends BaseProcessor
         }
         catch (\Throwable $e)
         {
-            throw $e;
             $this->trace->traceException(
                             $e,
                             Trace::INFO,
