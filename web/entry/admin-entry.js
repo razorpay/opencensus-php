e2393d99275b4fea5c2542079eb6fa091289f0ea
rchaubey <rohit.chaubey@razorpay.com>
2017-10-30 14:20:41 +0530

    admin-entry changes

---
+++ web/entry/admin-entry.js
@@ -0,0 +1,15 @@
+function prefixCdn(url) {
+  prefix = '';
+  var match = location.hostname.match(/(.*)dashboard\.razorpay\.com$/);
+  if (location.protocol === 'https:' && match) {
+    prefix =
+      'https://' + (match[1] ? 'beta' : '') + 'cdn.razorpay.com/dashboard';
+  }
+  return prefix + url;
+}
+
+document.write('<script src="' + prefixCdn('vendor.js') + '"></script>');
+document.write('<script src="' + prefixCdn('admin/admin.js') + '"></script>');
+document.write(
+  '<link href="' + prefixCdn('css/admin.css') + '" rel="stylesheet"></link>'
+);
