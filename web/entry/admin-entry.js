f82fbf1a7ebe41ecfb66b23549a6080073223ed8
Pranav Gupta <pranavguptarulz@gmail.com>
2017-10-30 12:25:24 +0530

    entry file cdn url

--- web/entry/admin-entry.js
+++ web/entry/admin-entry.js
@@ -1,3 +1,15 @@
-document.write('<script src="vendor.js"></script>');
-document.write('<script src="admin/admin.js"></script>');
-document.write('<link href="css/admin.css" rel="stylesheet"></link>');
+function prefixCdn(url) {
+  prefix = '';
+  var match = location.hostname.match(/(.*)dashboard\.razorpay\.com$/);
+  if (location.protocol === 'https' && match) {
+    prefix =
+      'https://' + (match[1] ? 'beta' : '') + 'cdn.razorpay.com/dashboard';
+  }
+  return prefix + url;
+}
+
+document.write('<script src="' + prefixCdn('vendor.js') + '"></script>');
+document.write('<script src="' + prefixCdn('admin/admin.js') + '"></script>');
+document.write(
+  '<link href="' + prefixCdn('css/admin.css') + '" rel="stylesheet"></link>'
+);
