c9ea8bd64570c6d1fe32e97596fd390119f23fbd
bush-niel <bushniel@gmail.com>
2017-10-31 16:28:21 +0530

    K8s modifications for Dashboard

--- dockerconf/entrypoint.sh
+++ dockerconf/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 set -euo pipefail
 
 if [[ -n "${GIT_COMMIT_HASH}" ]]; then
@@ -22,22 +21,11 @@ else
   $ALOHOMORA_BIN cast --region ap-south-1 --env $APP_MODE --app dashboard "environment/.env.vault.j2"
 fi
 
-# Copy config
-#cp environment/.env.example environment/.env.dev_docker && \
-#cp environment/.env.example environment/.env.testing_docker && \
-#cp environment/env.sample.php environment/env.php && \
-#sed -i "s/dev/dev_docker/g" environment/env.php
-
-# php.ini changes
-sed -i 's/display_errors = Off/display_errors = On/' /etc/php7/php.ini
-sed -i 's/display_startup_errors = Off/display_startup_errors = On/' /etc/php7/php.ini
-
-
-#echo "$date Memory Limit"
+# echo "$date Memory Limit"
 ## This is a bad workaround for increasing php's memory to to 3G enable running tests locally
 ## Mac's sed idiosyncrasies :(
-#echo 'memory_limit = 128M' | sed -E 's/memory_limit\s*=\s*\d*M/memory_limit = 3048M/g' /etc/php7/php.ini > /tmp/php.ini
-#mv /tmp/php.ini /etc/php7/php.ini
+# echo 'memory_limit = 128M' | sed -E 's/memory_limit\s*=\s*\d*M/memory_limit = 3048M/g' /etc/php7/php.ini > /tmp/php.ini
+# mv /tmp/php.ini /etc/php7/php.ini
 
 # Fix permissions
 echo  "$date Fix permissions"
@@ -46,7 +34,6 @@ echo "$date Configuring App"
 
 echo "$(date) DB Migrate"
 echo "$(date) Seeding live db"
-cd /app && \
 php artisan migrate --seed
 
 echo "$date Starting Nginx"
