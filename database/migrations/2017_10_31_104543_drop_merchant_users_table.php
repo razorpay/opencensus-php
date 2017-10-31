0e81b1850f1069b3c784641e0c8cf1c57a97c796
surya15t <surya.teja@razorpay.com>
2017-10-31 17:24:18 +0530

    [user][merchant] drop unused tables

---
+++ database/migrations/2017_10_31_104543_drop_merchant_users_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DropMerchantUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('merchant_users', function(Blueprint $table)
+        {
+            $table->dropForeign('merchant_users_merchant_id_foreign');
+
+            $table->dropForeign('merchant_users_user_id_foreign');
+        });
+
+        Schema::dropIfExists('merchant_users');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::create('merchant_users', function(Blueprint $table)
+        {
+            $table->engine = 'InnoDB';
+
+            $table->char('merchant_id', 14);
+
+            $table->char('user_id', 14);
+
+            $table->string('role');
+
+            $table->foreign('merchant_id')
+                  ->references('id')
+                  ->on('merchants');
+
+            $table->foreign('user_id')
+                  ->references('id')
+                  ->on('users');
+        });
+    }
+}
