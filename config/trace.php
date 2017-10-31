2e2dea3d8d8503d4558e4d78b563ae3ed4d6f0ec
hashfyre <joy.bhattacherjee@razorpay.com>
2017-10-31 17:06:42 +0530

    trace: fix hostname error

--- config/trace.php
+++ config/trace.php
@@ -1,7 +1,6 @@
 <?php
 
-$hostname = 'localhost';
-
+$hostname = "localhost";
 if (getenv('HOSTNAME') !== false)
 {
     $hostname = getenv('HOSTNAME');
@@ -46,7 +45,7 @@ return array(
     'rotate'  => true,
 
     'log_max_files' => 5,
-    
+
     'logpath' => storage_path().'/logs/'.$hostname.'-trace.log',
 
     'logpath' => storage_path() . '/logs/' . $hostname . '-trace.log',
