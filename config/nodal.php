dfd2fca38c491d43bcac626bb8e3cbdf729ae636
gaurav <gaurav.d@razorpay.com>
2017-11-02 15:07:42 +0530

    added configs

--- config/nodal.php
+++ config/nodal.php
@@ -7,10 +7,15 @@ return [
     ],
 
     'rbl' => [
-        'url'             => env('RBL_NODAL_URL'),
-        'username'        => env('RBL_NODAL_USERNAME'),
-        'password'        => env('RBL_NODAL_PASSWORD'),
-        'client_id'       => env('RBL_NODAL_CLIENT_ID'),
-        'client_password' => env('RBL_NODAL_CLIENT_PASSWORD'),
+        'url'                    => env('RBL_NODAL_URL'),
+        'username'               => env('RBL_NODAL_USERNAME'),
+        'password'               => env('RBL_NODAL_PASSWORD'),
+        'client_id'              => env('RBL_NODAL_CLIENT_ID'),
+        'client_password'        => env('RBL_NODAL_CLIENT_PASSWORD'),
+        'client_certificate'     => env('RBL_NODAL_CLIENT_CERTIFICATE'),
+        'client_certificate_key' => env('RBL_NODAL_CLIENT_CERTIFICATE_KEY'),
+        'certificate_name'       => env('RBL_NODAL_CERTIFICATE_NAME'),
+        'certificate_path'       => env('RBL_NODAL_CERTIFICATE_PATH'),
+        'certificate_key_name'   => env('RBL_NODAL_CERTIFICATE_KEY_NAME'),
     ],
 ];
