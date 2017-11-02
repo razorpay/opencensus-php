62fa8d0776cbd0181a0367b6d77ceae058f2b1aa
Saurav Chowdhury <saurav1991@gmail.com>
2017-11-03 01:22:17 +0530

    [terminal] fixes terminal selection logging everywhere (#5822)

--- app/Models/Terminal/Filter.php
+++ app/Models/Terminal/Filter.php
@@ -92,7 +92,7 @@ class Filter extends Base\Core
 
         if (($verbose === true) and (empty($terminals) === false))
         {
-            $terminalData = array_pluck($terminals, 'id', 'gateway');
+            $terminalData = array_pluck($terminals, 'gateway', 'id');
 
             $traceData = ['count' => count($terminals), 'terminals' => $terminalData, 'msg' => $msg];
 
