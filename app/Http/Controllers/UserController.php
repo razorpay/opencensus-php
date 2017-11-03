a4ad386fcfd72178ad9d28c4acad9ce807b02ed2
surya15t <surya.teja@razorpay.com>
2017-11-03 15:15:55 +0530

    [merchant] removing newui tag during signup

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -84,8 +84,6 @@ class UserController extends Controller
                 ];
 
                 Auth::attempt($credentials, false, true);
-
-                (new Merchant\Service)->tagMerchant(['newui' => 'true']);
             }
         }
         catch (User\RecoverableException $e)
