d6b21328a8d9fd4e4ef730d602ae0e505645591e
rzpamidi <prashanth.pamidi@razorpay.com>
2017-11-13 15:09:48 +0530

    Adds missing space in Refund modal message

--- public/react/merchant/containers/Payments/RefundModal.js
+++ public/react/merchant/containers/Payments/RefundModal.js
@@ -61,11 +61,7 @@ const RefundType = ({ partial, isTitleCase = false }) => {
     text = titleCase(text);
   }
 
-  return (
-    <span>
-      {text}
-    </span>
-  );
+  return <span>{text}</span>;
 };
 
 const selector = formValueSelector('refundModal');
@@ -222,20 +218,23 @@ export default class RefundModal extends Component {
                   placeholder="Enter the refund amount"
                 />
               </div>
-              {!!amountError
-                ? <div class="InputField__ErrorText text-danger">
-                    {amountError}
-                  </div>
-                : <small class="help-block">
-                    This will be a{' '}
-                    <b>
-                      <RefundType partial={partial} /> refund
-                    </b>.
-                    {!partial &&
-                      <span>Change amount for a partial refund.</span>}
-                  </small>}
+              {!!amountError ? (
+                <div class="InputField__ErrorText text-danger">
+                  {amountError}
+                </div>
+              ) : (
+                <small class="help-block">
+                  This will be a{' '}
+                  <b>
+                    <RefundType partial={partial} /> refund
+                  </b>.
+                  {!partial && (
+                    <span> Change amount for a partial refund.</span>
+                  )}
+                </small>
+              )}
             </div>
-            {transfers.items.length > 0 &&
+            {transfers.items.length > 0 && (
               <div class="form-group">
                 <div class="checkbox rzpCheckbox">
                   <Field
@@ -256,7 +255,8 @@ export default class RefundModal extends Component {
                     as well
                   </label>
                 </div>
-              </div>}
+              </div>
+            )}
             <div class="form-group">
               <label>Comments (Optional)</label>
               <Field
