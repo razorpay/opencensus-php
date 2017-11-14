333c6ddb0a4f3ead74718cc5151ff1412c692649
aseemgupta <aseemgupta2625@gmail.com>
2017-11-14 19:31:05 +0530

    [Fea]: Add website details form

---
+++ web/js/admin/merchants/entity/MerchantActivationForms/WebsiteDetails.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+
+import Form from 'ui/Form';
+import Field from 'ui/Field';
+
+export default ({ merchant_details: merchantDetails }) => (
+  <div>
+    <header>Website Details</header>
+    {!merchantDetails ? (
+      <div class="spinner" />
+    ) : (
+      <Form>
+        <Field
+          label="Website Address"
+          name="business_website"
+          defaultValue={merchantDetails.business_website}
+          placeholder="http://www.website.com"
+        />
+
+        <Field
+          label="About Us URL"
+          name="website_about"
+          defaultValue={merchantDetails.website_about}
+          placeholder="http://www.website.com/aboutus.html"
+        />
+
+        <Field
+          label="Contact Us URL"
+          name="website_contact"
+          defaultValue={merchantDetails.website_contact}
+          placeholder="http://www.website.com/contact.html"
+          infoMsg="Must contain the operational address"
+        />
+
+        <Field
+          label="Privacy Policy URL"
+          name="website_privacy"
+          defaultValue={merchantDetails.website_privacy}
+          placeholder="http://www.website.com/privacy.html"
+        />
+
+        <Field
+          label="Terms & Conditions URL"
+          name="website_terms"
+          defaultValue={merchantDetails.website_terms}
+          placeholder="http://www.example.com/terms.html"
+        />
+
+        <Field
+          label="Refund/Cancellation Policy URL"
+          name="website_refund"
+          defaultValue={merchantDetails.website_refund}
+          placeholder="http://www.example.com/refund.html"
+        />
+
+        <Field
+          label="URL displaying Product Pricing"
+          name="website_pricing"
+          defaultValue={merchantDetails.website_pricing}
+          placeholder="http://www.example.com/pricing.html"
+          infoMsg="Any page with product prices. May display a range of prices if not actual price"
+        />
+
+        <Field
+          label="Login Window URL"
+          name="website_login"
+          defaultValue={merchantDetails.website_login}
+          infoMsg="If no login required, give us a page where you ask for customer details"
+          placeholder="http://www.website.com/login.html"
+        />
+      </Form>
+    )}
+  </div>
+);
