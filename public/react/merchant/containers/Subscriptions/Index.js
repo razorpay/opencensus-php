f74c82be90c34759ea89e58ac8dafabc83ad6084
rzpamidi <prashanth.pamidi@razorpay.com>
2017-11-20 13:51:37 +0530

    Fixes showing addons tab

--- public/react/merchant/containers/Subscriptions/Index.js
+++ public/react/merchant/containers/Subscriptions/Index.js
@@ -97,14 +97,14 @@ export default class SubscriptionsController extends Component {
           <header id="subscriptions-header">
             <NavLink to="/subscriptions">Subscriptions</NavLink>
             <NavLink to="/plans">Plans</NavLink>
-            <NavLink to="/addons">Add Ons</NavLink>
+            {/* <NavLink to="/addons">Add Ons</NavLink> */}
           </header>
           <TestModeBanner />
           <content>
             <Switch>
               <Route path="/subscriptions" component={SubscriptionsList} />
               <Route path="/plans" component={PlansList} />
-              <Route path="/addons" component={AddOnsList} />
+              {/* <Route path="/addons" component={AddOnsList} /> */}
             </Switch>
           </content>
         </tabbed-container>
