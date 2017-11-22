f91a196f4f1a468e1bed25f1b4209cf794bf894e
Vivek <viveklakhera@gmail.com>
2017-11-22 17:47:22 +0530

    [hitachi] Fix merchant reference id in capture (#6213)

--- app/Gateway/Hitachi/Gateway.php
+++ app/Gateway/Hitachi/Gateway.php
@@ -370,6 +370,11 @@ class Gateway extends Base\Gateway
             RequestFields::MERCHANT_REF_NUMBER => $input['payment']['id']
         ];
 
+        if ($input['merchant']['id'] === '6ZJzxyLFWrGs74')
+        {
+            $content[RequestFields::MERCHANT_REF_NUMBER] = substr($input['payment']['id'], 0, 10);
+        }
+
         return $this->getStandardRequestArray($content);
     }
 
@@ -380,6 +385,11 @@ class Gateway extends Base\Gateway
         $content[RequestFields::TRANSACTION_TYPE] = TransactionType::REFUND;
         $content[RequestFields::REQUEST_ID]       = $input['refund']['id'];
 
+        if ($input['merchant']['id'] === '6ZJzxyLFWrGs74')
+        {
+            $content[RequestFields::REQUEST_ID] = substr($input['refund']['id'], 0, 10);
+        }
+
         return $this->getStandardRequestArray($content);
     }
 
@@ -388,6 +398,11 @@ class Gateway extends Base\Gateway
         $content = $this->getDefaultRefundReverseArray($input, $gatewayPayment);
 
         $content[RequestFields::TRANSACTION_TYPE] = TransactionType::VOID;
+
+        if ($input['merchant']['id'] === '6ZJzxyLFWrGs74')
+        {
+            $content[RequestFields::MERCHANT_REF_NUMBER] = substr($input['refund']['id'], 0, 10);
+        }
 
         return $this->getStandardRequestArray($content);
     }
@@ -405,8 +420,13 @@ class Gateway extends Base\Gateway
             RequestFields::TRANSACTION_DATE    => $date,
             RequestFields::RETRIEVAL_REF_NUM   => $gatewayPayment->getRrn(),
             RequestFields::MERCHANT_ID         => $this->getMerchantId(),
-            RequestFields::MERCHANT_REF_NUMBER => $input['refund']['id']
+            RequestFields::MERCHANT_REF_NUMBER => $input['payment']['id']
         ];
+
+        if ($input['merchant']['id'] === '6ZJzxyLFWrGs74')
+        {
+            $content[RequestFields::MERCHANT_REF_NUMBER] = substr($input['payment']['id'], 0, 10);
+        }
 
         return $content;
     }
