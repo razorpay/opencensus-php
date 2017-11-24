a4838f229dd1e1be6b08dabc414643b651696f5e
Pranav Gupta <pranavguptarulz@gmail.com>
2017-11-24 10:30:46 +0530

    s3sync script updated

--- scripts/s3sync.js
+++ scripts/s3sync.js
@@ -1,78 +1,58 @@
-var fs = require('fs');
-var path = require('path');
-var s3 = require('s3');
-var aws = require('aws-sdk');
-var glob = require('glob');
-var ENV = process.env;
+#!/usr/bin/env node
 
-var s3sdk = new aws.S3({
-  signatureVersion: 'v2',
-  accessKeyId: ENV.AWS_KEY,
-  secretAccessKey: ENV.AWS_SECRET,
-  region: ENV.AWS_REGION,
-});
+const { readFileSync } = require('fs');
+const path = require('path');
+const glob = require('glob');
+const zlib = require('zlib');
+const AWS = require('./aws');
+const s3 = new AWS.S3();
+const ENV = process.env;
+const AWS_DIR = ENV.AWS_DIR || ENV.TARGET_DIR;
 
-var client = s3.createClient({
-  s3Client: s3sdk,
-  maxAsyncS3: 20, // this is the default
-  s3RetryCount: 3, // this is the default
-  s3RetryDelay: 1000, // this is the default
-  multipartUploadThreshold: 20971520, // this is the default (20 MB)
-  multipartUploadSize: 15728640, // this is the default (15 MB)
-  s3Options: {
-    accessKeyId: ENV.AWS_KEY,
-    secretAccessKey: ENV.AWS_SECRET,
-    region: ENV.AWS_REGION,
-    // any other options are passed to new AWS.S3()
-    // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property
-  },
-});
-
+// http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html
 var params = {
-  // deleteRemoved: true, // default false, whether to remove s3 objects that have no corresponding local file.
-  s3Params: {
-    Bucket: ENV.AWS_CDN_BUCKET,
-    ACL: 'public-read',
-    CacheControl: 'max-age=31536000, public',
-    // other options supported by putObject, except Body and ContentLength.
-    // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property
-  },
+  Bucket: ENV.AWS_CDN_BUCKET || ENV.AWS_BUCKET || ENV.AWS_S3_BUCKET,
+  ACL: 'public-read',
+  CacheControl: 'max-age=2700, must-revalidate',
 };
 
-const prefixLen = ENV.TARGET_DIR.length + 1;
+// textual file types
+const ContentType = {
+  js: 'application/javascript',
+  html: 'text/html',
+  css: 'text/css',
+  svg: 'image/svg+xml',
+};
 
-glob(ENV.TARGET_DIR + '/**', {}, function(error, files) {
-  files.forEach(function(f) {
-    if (fs.lstatSync(f).isFile()) {
-      var ext = /[^\.]+$/.exec(f);
-      ext = ext[0];
-      if (ext && ext !== 'map') {
-        // ignore mapfiles
-        var fileParams = JSON.parse(JSON.stringify(params));
+// to remove it from begining of file path before uploading to s3
+const prefixLen = AWS_DIR.length + 1;
 
-        if (
-          f.endsWith('-entry.js') ||
-          f.endsWith('.js.map') ||
-          ext.startsWith('woff')
-        ) {
-          fileParams.s3Params.CacheControl = 'no-store,must-revalidate';
-        }
+glob(AWS_DIR + '/**', { nodir: true }, (error, files) => {
+  files.forEach(file => {
+    var fileParams = {
+      ...params,
+      Key: file.slice(prefixLen),
+      Body: readFileSync(file),
+    };
+    var ext = path.extname(file).slice(1);
 
-        fileParams.localFile = f;
-        if (ext == 'css' || ext == 'js' || ext === 'html') {
-          fileParams.s3Params.ContentEncoding = 'gzip';
-        }
-        fileParams.s3Params.Key = f;
-        console.log(fileParams);
-        var uploader = client.uploadFile(fileParams);
-        uploader.on('error', function(err) {
-          console.error('unable to sync:', err.stack);
-          throw err;
-        });
-        uploader.on('end', function(data) {
-          console.log(f);
-        });
-      }
+    if (
+      file.endsWith('-entry.js') ||
+      file.endsWith('.js.map') ||
+      ext.startsWith('woff')
+    ) {
+      fileParams.CacheControl = 'no-store,must-revalidate';
     }
+
+    var type = ContentType[ext];
+    if (type) {
+      fileParams.ContentType = type;
+      fileParams.ContentEncoding = 'gzip';
+      fileParams.Body = zlib.gzipSync(fileParams.Body, {
+        level: zlib.Z_BEST_COMPRESSION,
+      });
+    }
+
+    s3.putObject(fileParams, err => console.log(err || fileParams.Key));
   });
 });
