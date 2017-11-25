c3f7807d3534ee66953c2b9ab971e5c1c8531f43
Amit Mahbubani <amit.mahbubani@razorpay.com>
2017-11-25 16:27:59 +0530

    [db_cache] Skip cache logs for non DB cache

--- app/Listeners/LogCache.php
+++ app/Listeners/LogCache.php
@@ -7,8 +7,26 @@ use RZP\Trace\TraceCode;
 
 class LogCache
 {
+    const REMEMBERABLE_KEY = 'rememberable';
+
     public function handle($event)
     {
+        if (property_exists($event, 'key') === false)
+        {
+            return;
+        }
+
+        $isRememberableEvent = strpos($event->key, self::REMEMBERABLE_KEY);
+
+        //
+        // Only trace rememberable events
+        // https://github.com/razorpay/api/pull/6256
+        //
+        if ($isRememberableEvent === false)
+        {
+            return;
+        }
+
         $trace = App::getFacadeRoot()['trace'];
 
         $trace->info(TraceCode::CACHE_EVENT, [
