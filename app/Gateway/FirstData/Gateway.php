029396af352f354763b3e32d471dbe5b79d20f60
Harman Singh <harman28@gmail.com>
2017-11-26 20:03:15 +0530

    [first_data] Allows refunds

--- app/Gateway/FirstData/Gateway.php
+++ app/Gateway/FirstData/Gateway.php
@@ -211,9 +211,6 @@ class Gateway extends Base\Gateway
     {
         parent::refund($input);
 
-        // All refunds temporarily blocked, due to FirstData issues
-        $this->failRefund();
-
         $requestContent = $this->getRefundRequestArray($input, TxnType::REFUND);
 
         $this->trace->info(TraceCode::GATEWAY_REFUND_REQUEST,
@@ -243,9 +240,6 @@ class Gateway extends Base\Gateway
     {
         parent::reverse($input);
 
-        // All refunds temporarily blocked, due to FirstData issues
-        $this->failRefund();
-
         $requestContent = $this->getReverseRequestArray($input);
 
         $this->trace->info(
@@ -269,24 +263,6 @@ class Gateway extends Base\Gateway
         $reverseEntity = $this->createGatewayPaymentEntity($reverseFields, $input);
 
         $this->checkApprovalCode($reverseEntity);
-    }
-
-    /**
-     * Failing all FirstData refunds for now
-     * Will be retried later via cron
-     * @throws Exception\GatewayErrorException
-     */
-    protected function failRefund()
-    {
-        $this->trace->info(
-            TraceCode::GATEWAY_FIRST_DATA_REFUND_BLOCKED,
-            [
-                'payment_id' => $this->input['payment']['id'],
-                'refund_id'  => $this->input['refund']['id'],
-                'action'     => $this->action,
-            ]);
-
-        throw new Exception\GatewayErrorException(ErrorCode::GATEWAY_ERROR_REQUEST_ERROR);
     }
 
     public function verify(array $input)
