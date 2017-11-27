07ab2430932bb9564f8e3648191c97ca653e81c2
Pranav Gupta <pranavguptarulz@gmail.com>
2017-11-27 12:37:13 +0530

    separated admin and merchant headers

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,8 +1,19 @@
-@include('partials/header')
-<script>
-  var org = {!! json_encode($org) !!};
-  var user = {!! json_encode($user) !!};
-</script>
+<!DOCTYPE html>
+<html lang="en" data-ng-app="app">
+<head>
+  <meta charset="utf-8">
+  <meta name="google" value="notranslate" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Razorpay Merchant Dashboard">
+  <meta name="author" content="Razorpay">
+  <link rel="shortcut icon" href="/img/favicon.png">
+  <title>Razorpay - Admin Panel</title>
+  <meta name="description" content="Online payment gateway for India with the best in class API, integration procedure, robust security and powerful dashboard" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+  <script>
+    var org = {!! json_encode($org) !!};
+    var user = {!! json_encode($user) !!};
+  </script>
 </head>
 <body>
 <div id="react-root" class="react-root"></div>
