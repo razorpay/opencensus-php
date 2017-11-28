2cd1794da905392908ee8bbb71df21a76aed03fb
Sunny Aggrawal <sunnyagg@users.noreply.github.com>
2017-11-28 20:45:53 +0530

    [refunds] updates failed refunds list for ebs (#6297)

--- app/Gateway/Ebs/Gateway.php
+++ app/Gateway/Ebs/Gateway.php
@@ -428,8 +428,6 @@ class Gateway extends Base\Gateway
 
         // Hardcoding these refunds for processing
         $unprocessedRefunds = [
-            '8SSbtyrGAntTkL',
-            '8SShzxcY41dwVX',
             '8isATbpjCjzemn',
             '8nmxz1O7qMMrbC',
             '8nnx2FXXZBKoRq',
@@ -442,6 +440,9 @@ class Gateway extends Base\Gateway
             '8p4NUngn9iGaaI',
             '8p4QHner55GN0n',
             '8qyNyJzcArBg99',
+            '8SSbtyrGAntTkL',
+            '8SShzxcY41dwVX',
+            '8u6XaEd7xKoL2l'
         ];
 
         $processedRefund = [];
