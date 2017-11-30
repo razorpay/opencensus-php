5d83922176aba84dae9163ddfc22fb58d4123c4a
Harman Singh <harman28@gmail.com>
2017-11-30 19:00:09 +0530

    [virtual acc] Removes whitespaces from payee account number

--- tests/Functional/BankTransfer/BankTransferTestData.php
+++ tests/Functional/BankTransfer/BankTransferTestData.php
@@ -198,6 +198,23 @@ return [
         ],
     ],
 
+    'testBankTransferRemoveSpaces' => [
+        'url'     => '/ecollect/validate',
+        'method'  => 'post',
+        'content' => [
+            'payee_account'  => 'R A Z O R P A Y 1 2 3',
+            'payee_ifsc'     => null,
+            'payer_name'     => 'Name of account holder',
+            'payer_account'  => '9876543210123456789',
+            'payer_ifsc'     => 'XYZ9876543210',
+            'mode'           => 'imps',
+            'transaction_id' => strtoupper(random_alphanum_string(22)),
+            'time'           => 148415544000,
+            'amount'         => 50000,
+            'description'    => 'IMPS payment of 50,000 rupees, with a stupid bank code',
+        ],
+    ],
+
     'testBankTransferImpsFromRogueBankNullAccount' => [
         'url'     => '/ecollect/validate',
         'method'  => 'post',
