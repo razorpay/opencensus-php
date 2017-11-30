6389c6c7e5f3bc067847c274ca840b7600972000
Pratik Kapasi <pratik.kapasi@razorpay.com>
2017-11-30 12:46:03 +0530

    [accounts] added custom validation functions

--- app/Models/Merchant/Detail/Validator.php
+++ app/Models/Merchant/Detail/Validator.php
@@ -143,6 +143,14 @@ class Validator extends Base\Validator
         Entity::SUBMIT                          => 'sometimes|boolean',
     ];
 
+    /**
+     * Validate the transaction report email
+     *
+     * @param $attribute
+     * @param $value
+     *
+     * @throws Exception\BadRequestValidationFailureException
+     */
     public function validateTransactionReportEmail($attribute, $value)
     {
         $emails = explode(',', $value);
@@ -154,6 +162,54 @@ class Validator extends Base\Validator
                 throw new Exception\BadRequestValidationFailureException(
                     "The provided transaction report email is invalid: $email",
                     Entity::TRANSACTION_REPORT_EMAIL
+                );
+            }
+        }
+    }
+
+    /**
+     * Validate the technical spoc email
+     *
+     * @param $attribute
+     * @param $value
+     *
+     * @throws Exception\BadRequestValidationFailureException
+     */
+    public function validateTechnicalSpocEmail($attribute, $value)
+    {
+        $emails = explode(',', $value);
+
+        foreach ($emails as $email)
+        {
+            if (filter_var($email, FILTER_VALIDATE_EMAIL) === false)
+            {
+                throw new Exception\BadRequestValidationFailureException(
+                    "The provided technical spoc email is invalid: $email",
+                    Entity::TECHNICAL_SPOC_EMAIL
+                );
+            }
+        }
+    }
+
+    /**
+     * Validate the business spoc email
+     *
+     * @param $attribute
+     * @param $value
+     *
+     * @throws Exception\BadRequestValidationFailureException
+     */
+    public function validateBusinessSpocEmail($attribute, $value)
+    {
+        $emails = explode(',', $value);
+
+        foreach ($emails as $email)
+        {
+            if (filter_var($email, FILTER_VALIDATE_EMAIL) === false)
+            {
+                throw new Exception\BadRequestValidationFailureException(
+                    "The provided business spoc email is invalid: $email",
+                    Entity::BUSINESS_SPOC_EMAIL
                 );
             }
         }
