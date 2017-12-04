652e155092a3d60b74523bb24edc28c986785e9a
jitendra-1217 <jitendraojha1217@gmail.com>
2017-12-05 01:23:20 +0530

    [invoice] Adds unit tests to cover view data
    There are class and helpers(e.g. ViewDataSerializer which
    gets used to get serialized invoice entity which is not just
    toArray but includes few other details (e.g. merchant details including logo,
    brand color, subscription details if applies, payments list, environment and other
    attributes etc) which our blade file usage (on hosted page).
    
    Adding unit tests to know that we continue to return those properly formatted
    and structured response with changes happening in Invoice, Subscription and other
    related entity or around these helper classes itself.

---
+++ tests/Unit/Models/Invoice/Traits/CreatesInvoice.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace RZP\Tests\Unit\Models\Invoice\Traits;
+
+use RZP\Models\Invoice;
+
+/**
+ * Contains helper methods which get used in Invoice related unit tests.
+ */
+trait CreatesInvoice
+{
+    public function createInvoice(
+        array $invoiceAttributes = [],
+        array $orderAttributes = []): Invoice\Entity
+    {
+        $defaultOrderAttributes = [
+            'id'              => '100000000order',
+            'amount'          => 100000,
+            'payment_capture' => true,
+        ];
+        $orderAttributes = array_merge($defaultOrderAttributes, $orderAttributes);
+
+        $order = $this->fixtures->create('order', $orderAttributes);
+
+        return $this->fixtures->create('invoice', $invoiceAttributes);
+    }
+
+    public function createInvoiceWithPayment(): Invoice\Entity
+    {
+        $invoiceAttributes = [
+            'status'  => 'paid',
+            'paid_at' => time(),
+        ];
+        $orderAttributes = [
+            'status'      => 'paid',
+            'amount_paid' => 100000,
+            'attempts'    => 1,
+        ];
+
+        $invoice = $this->createInvoice($invoiceAttributes, $orderAttributes);
+
+        $paymentAttributes = [
+            'invoice_id' => '1000000invoice',
+            'order_id'   => '100000000order',
+        ];
+
+        $this->fixtures->create('payment', $paymentAttributes);
+
+        return $invoice;
+    }
+
+    public function createSubscriptionInvoice(): Invoice\Entity
+    {
+        $subscriptionAttributes = [
+            'id'          => '10subscription',
+            'plan_id'     => '1000000000plan',
+            'schedule_id' => '100000schedule',
+        ];
+
+        $schedule     = $this->fixtures->create('schedule', ['id' => '100000schedule']);
+        $plan         = $this->fixtures->plan->create();
+        $subscription = $this->fixtures->create('subscription', $subscriptionAttributes);
+        $invoice      = $this->createInvoice();
+
+        $invoice->subscription()->associate($subscription);
+
+        return $invoice;
+    }
+}
