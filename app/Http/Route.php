942165803ca7df99796633f21585d13c31b4da9d
Prashanth Y.V <prashanth.yv@razorpay.com>
2017-12-04 16:59:34 +0530

    [slave] moves payment fetch multiple to slave

--- app/Http/Route.php
+++ app/Http/Route.php
@@ -1560,6 +1560,7 @@ final class Route
 
     public static $slaveRoutes = [
         'payment_fetch_transaction',
+        'payment_fetch_multiple',
     ];
 
     protected static $jsonpRoutes = [
