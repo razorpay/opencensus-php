0b6fc7983b2b43a6ea6170e4a8658c4239bfc61a
mayankamencherla <mayank.amencherla@razorpay.com>
2017-12-05 14:08:00 +0530

    [sbi recon] Pulling out payment recon data from recon file

--- app/Reconciliator/NetbankingRbl/SubReconciliator/PaymentReconciliate.php
+++ app/Reconciliator/NetbankingRbl/SubReconciliator/PaymentReconciliate.php
@@ -22,6 +22,7 @@ class PaymentReconciliate extends Base\PaymentReconciliate
 
     protected function getPaymentId($row)
     {
+        sd($row);
         if (empty($row[ClaimFields::BANK_REFERENCE]) === false)
         {
             return $row[ClaimFields::BANK_REFERENCE];
