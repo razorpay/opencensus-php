2b8ab20647e4934dd7fbad269ec13f96f23d21ca
tessie <tessyjoseph1992@gmail.com>
2017-12-05 11:50:15 +0530

    Names with underscore  should be valid

--- tests/Functional/Customer/CustomerTest.php
+++ tests/Functional/Customer/CustomerTest.php
@@ -48,6 +48,7 @@ class CustomerTest extends TestCase
             'A-B pvt Ltd'        => 'test6@test.razorpay.com',
             'A-B pvt (test) Ltd' => 'test7@test.razorpay.com',
             'M-dash–Name'        => 'test8@test.razorpay.com',                 //Names with m-dash should be valid (–)
+            'Underscore_ABC'     => 'test9@test.razorpay.com',
         ];
 
         $testData = & $this->testData[__FUNCTION__];
