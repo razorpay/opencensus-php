bf1cc1c0c743c5dec77fe4d8ce4150a954623c84
hobbes09 <sourin.sutradhar@gmail.com>
2017-12-07 04:32:54 +0530

    [dispute] clubbed dispute type and reason and header changes for mail

--- resources/views/emails/dispute/creation.blade.php
+++ resources/views/emails/dispute/creation.blade.php
@@ -13,14 +13,18 @@
         $createdDate = date('jS F Y', $dispute['created_at']);
         $expiryDate = date('jS F Y', $dispute['expires_on']);
 
+        $remainingDays = floor(( $dispute['expires_on'] - time() ) / 86400);
+
         $amount = sprintf('%0.2f', ($dispute['amount'] / 100));
         $amount = floatval($amount);
 
-        $note = 'Kindly provide the requested documents in the required format by the deadline, failing which the case will be lost and the corresponding amount will be debited from the current balance. ';
+        $paymentLink = 'https://dashboard.razorpay.com/#/app/payments/pay_' . $dispute['payment_id'];
+
+        $noteResult = 'failing which the case will be lost and the corresponding amount will be debited from the current balance. ';
 
         if (($dispute['phase'] === 'retrieval') or ($dispute['phase'] === 'fraud'))
         {
-            $note = 'Kindly provide the requested documents in the required format by the deadline, failing which the corresponding amount might be debited from the current balance. ';
+            $noteResult = 'failing which the corresponding amount might be debited from the current balance. ';
         }
 
     @endphp
@@ -31,12 +35,12 @@
         <div style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; line-height: 20px; text-align: center; background-color: {{ $brand_color }}; color: {{ $brand_text_color }}; padding: 30px 0 135px !important;">
 
             <h2 style="font-family: -apple-system,'.SFNSDisplay','Oxygen','Ubuntu','Roboto','Segoe UI','Helvetica Neue','Lucida Grande',sans-serif; font-weight: normal; padding: 0; word-break: normal; margin: 0; font-size: 20px; line-height: 24px; text-align: center; color: {{ $brand_text_color }};">
-                  Dispute Alert
+                  Dispute raised for Rs. {{ $amount }}
               </h2>
 
             <div style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; line-height: 20px; margin-top: 12px; color: {{ $brand_text_color }};">
                 <div style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; line-height: 20px; color: {{ $brand_text_color }};">
-                    A dispute has been raised for Rs. {{ $amount }}
+                    Please respond by {{ $expiryDate }}
                 </div>
               </div>
         </div>
@@ -46,7 +50,7 @@
         <table class="table" border="0" cellpadding="0" cellspacing="0" style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; line-height: 20px; color: #212121; border-spacing: 0; border-collapse: collapse; padding: 0; vertical-align: top; text-align: left; width: 100%; background-color: #fafafa; margin-top: -100px !important; height: 100%; wdith: 80%; max-width: 600px; margin: 0 auto;"><tbody style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; line-height: 20px; color: #212121;">
 <tr style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; line-height: 20px; color: #212121; padding: 0; vertical-align: top; text-align: left;">
 <td class="content" style="word-break: break-word; -webkit-hyphens: auto; -moz-hyphens: auto; hyphens: auto; border-collapse: collapse !important; vertical-align: top; font-family: -apple-system,'.SFNSDisplay','Oxygen','Ubuntu','Roboto','Segoe UI','Helvetica Neue','Lucida Grande',sans-serif; font-weight: normal; margin: 0; text-align: left; font-size: 14px; line-height: 19px; padding: 24px 4%; background-color: #fff; border-left: 1px solid #f2f2f2; border-right: 1px solid #f2f2f2; color: #000000; padding-bottom: 24px;">
-                    <strong class="text-uppercase lh-26" style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; color: #535353; text-transform: uppercase; line-height: 26px;">Payment ID</strong> <br style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; line-height: 20px; color: #212121;"> pay_{{ $dispute['payment_id'] }}
+                    <strong class="text-uppercase lh-26" style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; color: #535353; text-transform: uppercase; line-height: 26px;">Payment ID</strong> <br style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; line-height: 20px; color: #212121;"><a href="{{ $paymentLink }}" style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; line-height: 20px; text-decoration: none; color: #39ACE5;"> pay_{{ $dispute['payment_id'] }} </a>
                 </td>
             </tr>
 <tr style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; line-height: 20px; color: #212121; padding: 0; vertical-align: top; text-align: left;">
@@ -66,7 +70,7 @@
 <tr style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; line-height: 20px; color: #212121; padding: 0; vertical-align: top; text-align: left;">
 <td class="content" style="word-break: break-word; -webkit-hyphens: auto; -moz-hyphens: auto; hyphens: auto; border-collapse: collapse !important; vertical-align: top; font-family: -apple-system,'.SFNSDisplay','Oxygen','Ubuntu','Roboto','Segoe UI','Helvetica Neue','Lucida Grande',sans-serif; font-weight: normal; margin: 0; text-align: left; font-size: 14px; line-height: 19px; padding: 24px 4%; background-color: #fff; border-left: 1px solid #f2f2f2; border-right: 1px solid #f2f2f2; color: #000000; padding-bottom: 0px; padding-top: 0px;">
                     <div style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; line-height: 20px; color: #212121;">
-                        <strong class="text-uppercase lh-26" style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; color: #535353; text-transform: uppercase; line-height: 26px;">Dispute Type</strong> <br style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; line-height: 20px; color: #212121;"><span style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; line-height: 20px; color: #212121; text-transform: capitalize;">{{ $dispute['phase'] }}</span>
+                        <strong class="text-uppercase lh-26" style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; color: #535353; text-transform: uppercase; line-height: 26px;">Dispute Type and Reason</strong> <br style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; line-height: 20px; color: #212121;"><span style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; line-height: 20px; color: #212121; text-transform: capitalize;"> <b style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; line-height: 20px; color: #212121;"> {{ $dispute['phase'] }} </b> : {{ $dispute['reason_description'] }} </span>
                     </div>
                 </td>
             </tr>
@@ -78,16 +82,9 @@
                 </td>
             </tr>
 <tr style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; line-height: 20px; color: #212121; padding: 0; vertical-align: top; text-align: left;">
-<td class="content" style="word-break: break-word; -webkit-hyphens: auto; -moz-hyphens: auto; hyphens: auto; border-collapse: collapse !important; vertical-align: top; font-family: -apple-system,'.SFNSDisplay','Oxygen','Ubuntu','Roboto','Segoe UI','Helvetica Neue','Lucida Grande',sans-serif; font-weight: normal; margin: 0; text-align: left; font-size: 14px; line-height: 19px; padding: 24px 4%; padding-bottom: 0; background-color: #fff; border-left: 1px solid #f2f2f2; border-right: 1px solid #f2f2f2; color: #000000;">
-                    <div style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; line-height: 20px; color: #212121;">
-                        <strong class="text-uppercase lh-26" style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; color: #535353; text-transform: uppercase; line-height: 26px;">Dispute Reason</strong> <br style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; line-height: 20px; color: #212121;"> {{ $dispute['reason_description'] }}
-                    </div>
-                </td>
-            </tr>
-<tr style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; line-height: 20px; color: #212121; padding: 0; vertical-align: top; text-align: left;">
 <td class="content" style="word-break: break-word; -webkit-hyphens: auto; -moz-hyphens: auto; hyphens: auto; border-collapse: collapse !important; vertical-align: top; font-family: -apple-system,'.SFNSDisplay','Oxygen','Ubuntu','Roboto','Segoe UI','Helvetica Neue','Lucida Grande',sans-serif; font-weight: normal; margin: 0; text-align: left; font-size: 14px; line-height: 19px; padding: 24px 4%; background-color: #fff; border-left: 1px solid #f2f2f2; border-right: 1px solid #f2f2f2; color: #000000; padding-bottom: 24px !important;">
                     <div style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; line-height: 20px; color: #212121;">
-                        <strong class="text-uppercase lh-26" style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; color: #535353; text-transform: uppercase; line-height: 26px;">Deadline for resolution</strong> <br style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; line-height: 20px; color: #212121;"> {{ $expiryDate }}
+                        <strong class="text-uppercase lh-26" style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; color: #535353; text-transform: uppercase; line-height: 26px;">Deadline for resolution</strong> <br style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; line-height: 20px; color: #212121;"><b style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; line-height: 20px; color: #212121;"> {{ $expiryDate }} </b> ( in {{ $remainingDays }} days )
                     </div>
                 </td>
             </tr>
@@ -99,7 +96,7 @@
                         <br style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; line-height: 20px; color: #212121;"><br style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; line-height: 20px; color: #212121;">
                         We have received a dispute for Rs. {{ $amount }} on pay_{{ $dispute['payment_id'] }} against {{ $merchant['name'] }}. Please share all proofs like invoices, proof of delivery of product/service and any relevant screenshots pertaining to the transaction in a consolidated ZIP archive named as the Payment ID.
                         <br style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; line-height: 20px; color: #212121;"><br style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; line-height: 20px; color: #212121;">
-                        Note: {{ $note }}
+                        Note: Kindly <b style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; line-height: 20px; color: #212121;"> reply to this email </b> with the requested documents in the required format by the deadline, {{ $noteResult }}
                         <br style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; line-height: 20px; color: #212121;"><br style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; line-height: 20px; color: #212121;">
 </div>
                 </td>
@@ -115,7 +112,7 @@
                                 </td>
                                 <td class="content" style="word-break: break-word; -webkit-hyphens: auto; -moz-hyphens: auto; hyphens: auto; border-collapse: collapse !important; vertical-align: top; color: #222222; font-family: -apple-system,'.SFNSDisplay','Oxygen','Ubuntu','Roboto','Segoe UI','Helvetica Neue','Lucida Grande',sans-serif; font-weight: normal; padding: 0; margin: 0; text-align: left; font-size: 14px; line-height: 19px;">
                                     <div class="footerRZP" style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; line-height: 20px; text-align: right; padding-left: 10%; padding-bottom: 24px; font-size: 10px; color: #757575;">
-                                        For any queries, reply directly to this <br style="font-family: -apple-system, BlinkMacSystemFont, Arial, sans-serif; line-height: 20px; color: #212121;"> email and we will help you out.
+                                        For any queries, please reply to this email.
                                     </div>
                                 </td>
                             </tr></tbody></table>
