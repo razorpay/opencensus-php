4c0409fcc79f50991fd8f6b65f53ec16b1906f4c
aseemgupta <aseemgupta2625@gmail.com>
2017-12-08 16:14:09 +0530

    [Fix]: Fix case senstive file path issue

--- public/react/merchant/containers/Subscriptions/Details.js
+++ public/react/merchant/containers/Subscriptions/Details.js
@@ -16,7 +16,7 @@ import { fetchSubscriptionAddOns } from 'merchant/modules/addons';
 import { deleteAddOn } from 'merchant/modules/addons';
 import { showNotification } from 'rzp/modules/notifications';
 import { expandSlider, compactSlider } from 'rzp/modules/slider';
-import fetchKeysAndCheckout from 'merchant/utils/fetchkeysandcheckout';
+import fetchKeysAndCheckout from 'merchant/utils/fetchKeysAndCheckout';
 
 import { openModal, closeModal } from 'rzp/modules/modals';
 import CancellationModal from './CancellationModal';
