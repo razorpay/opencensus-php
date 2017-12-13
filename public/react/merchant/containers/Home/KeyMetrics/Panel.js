e1b2139bdea0073d7a02b52ef201cd265165cffb
Chart.js <anshul.sahni@razorpay.com>
2017-12-13 18:28:05 +0530

    custom tooltip designed and rendered

--- public/react/merchant/containers/Home/KeyMetrics/Panel.js
+++ public/react/merchant/containers/Home/KeyMetrics/Panel.js
@@ -10,12 +10,78 @@ import { timeScale } from 'rzp/utils/chart/index.js';
 import { tabsMeta, breakdownVals } from './data';
 import Legend from 'merchant/components/Home/Legend';
 
+/* function for custom tooltip */
+const customToolTip = function(tooltipModel) {
+  // Tooltip Element
+  var tooltipDOM = document.getElementById('chartjs-tooltip');
+
+  // Create element on first render
+  if (!tooltipDOM) {
+    tooltipDOM = document.createElement('div');
+    tooltipDOM.id = 'chartjs-tooltip';
+    tooltipDOM.innerHTML = '<div class="custom-tooltip-inner"></div>';
+    document.body.appendChild(tooltipDOM);
+  }
+
+  // Hide if no tooltip
+  if (!tooltipModel.opacity) {
+    tooltipDOM.style.opacity = 0;
+    return;
+  }
+
+  // Setting the body and title of tooltip
+  if (tooltipModel.body) {
+    var innerHtml = '';
+    const dataPoints = tooltipModel.dataPoints;
+    const sumOfAllDataPoints = dataPoints.reduce(
+      (tempSum, { yLabel }) => tempSum + yLabel,
+      0
+    );
+
+    innerHtml += `<div class="title">₹ ${sumOfAllDataPoints}</div>`;
+
+    var bodyLines = tooltipModel.body.map(({ lines }) => lines[0]);
+    let rows = '';
+    bodyLines.forEach((body, index) => {
+      // var colors = tooltipModel.labelColors[i];
+      const labelColors = tooltipModel.labelColors;
+      const bodyItems = body.split(':');
+
+      const labelIcon = `<span class="label-icon" style="background-color: ${labelColors[
+        index
+      ].backgroundColor}"></span>`;
+      const label = `<span class="label">${bodyItems[0]}</span>`;
+      const labelValue = `<span class="label-value">${bodyItems[1].trim()}</span>`;
+      rows += `<div class="tooltip-row">${labelIcon}${label}${labelValue}</div>`;
+    });
+
+    innerHtml += `<div class="tooltip-body">${rows}</div>`;
+
+    var innerTooltip = tooltipDOM.querySelector('.custom-tooltip-inner');
+    innerTooltip.innerHTML = innerHtml;
+  }
+
+  var { offsetTop /*offsetLeft*/ } = this._chart.canvas;
+
+  tooltipDOM.style.opacity = 1;
+  tooltipDOM.style.left = `${tooltipModel.caretX +
+    this._chart.width / this._data.labels.length}px`;
+  tooltipDOM.style.top = `${offsetTop + tooltipModel.caretY}px`;
+  tooltipDOM.style.padding = `${tooltipModel.yPadding}px ${tooltipModel.xPadding}px`;
+};
+
 const chartOptions = {
   ...timeScale({}),
   layout: {
     padding: {
       top: 50,
     },
+  },
+  tooltips: {
+    enabled: false,
+    position: 'nearest',
+    /* custom tooltip */
+    custom: customToolTip,
   },
 };
 
