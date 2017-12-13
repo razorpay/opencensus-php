dcdddcc6141db16da7410921e302d4e040e23010
Amit Mahbubani <amit.mahbubani@razorpay.com>
2017-12-14 00:02:43 +0530

    [email] Add custom authorized email text/reply-to

--- resources/views/emails/payment/customer_text.blade.php
+++ resources/views/emails/payment/customer_text.blade.php
@@ -22,7 +22,7 @@ Customer Details:
 
 If this is correct, you don't need to take any further action.
 
-You can contact us at contact@razorpay.com in case of any discrepancy.
+{{$data['support_text_plain']}}
 
 --
 Team Razorpay
