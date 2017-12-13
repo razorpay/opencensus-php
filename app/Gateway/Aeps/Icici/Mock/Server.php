a7502aa69bd7fb51b14a12e64a08e272aead8c0d
Albin George <albinin0002@gmail.com>
2017-12-13 20:25:27 +0530

    [aeps] Add mock response for auth request

--- app/Gateway/Aeps/Icici/Mock/Server.php
+++ app/Gateway/Aeps/Icici/Mock/Server.php
@@ -9,7 +9,39 @@ class Server extends Base\Mock\Server
 {
     public function authorize($input)
     {
-        sd($input);
+        $response = $this->getAuthResponse($input);
+
+        return $this->makePostResponse($response);
+    }
+
+    protected function getAuthResponse(string $input): string
+    {
+        $data = [
+            Icici\ResponseConstants::AUTH_MESSAGE_TYPE_INDICATOR                => '0110"/>',
+            Icici\ResponseConstants::AUTH_PAN                                   => '5085350123456789003"/>',
+            Icici\ResponseConstants::AUTH_PROC_CODE                             => '421000"/>',
+            Icici\ResponseConstants::AUTH_AMOUNT                                => '000000010000"/>',
+            Icici\ResponseConstants::AUTH_SYSTEM_TRACE_AUDIT_NO                 => '005810"/>',
+            Icici\ResponseConstants::AUTH_TIME                                  => '182938"/>',
+            Icici\ResponseConstants::AUTH_DATE                                  => '0321"/>',
+            Icici\ResponseConstants::AUTH_NETWORK_INTERNATIONAL_IDENTIFIER      => '001"/>',
+            Icici\ResponseConstants::AUTH_POINT_OF_SERVICE_CONDITION_CODE       => '05"/>',
+            Icici\ResponseConstants::AUTH_RRN                                   => '708018541465"/>',
+            Icici\ResponseConstants::AUTH_AUTHORIZATION_IDENTIFICATION_RESPONSE => '541465"/>',
+            Icici\ResponseConstants::AUTH_RESPONSE_CODE                         => '00"/>',
+            Icici\ResponseConstants::AUTH_CARD_ACCEPTOR_TERMINAL_IDENTIFICATION => '13016403"/>',
+            Icici\ResponseConstants::AUTH_CARD_ACCEPTOR_NAME                    => 'Pune - Check PUNE',
+            Icici\ResponseConstants::AUTH_ADDITIONAL_AMOUNTS                    => '1001356C0000002548081002356C000000254737"/>',
+            Icici\ResponseConstants::AUTH_AUTHENTICATION_CODE                   => 'da433aa16bd84a479300a8f75e329eae"/>',
+            Icici\ResponseConstants::AUTH_BENEF_ACCOUNT_NUMBER                  => '1111111111111"/>',
+            Icici\ResponseConstants::AUTH_REMITTER                              => 'null|ANIL V KAPOOR|cwduid| | |Card not present.| |n/a||da433aa16bd84a479300a8f75e329eae',
+        ];
+
+        $this->content($data, 'auth');
+
+        $xml = $this->generateXml($data);
+
+        return $xml;
     }
 
     public function refund($input)
@@ -25,7 +57,7 @@ class Server extends Base\Mock\Server
             $sessionKey
         );
 
-        $data = json_decode($decryptedData);
+        $data = json_decode($decryptedData, true);
 
         $responseData = $this->getRefundResponse($data);
 
@@ -57,5 +89,30 @@ class Server extends Base\Mock\Server
         $response->headers->set('Content-Type', 'application/json');
 
         return $response;
+    }
+
+    protected function makePostResponse($content)
+    {
+
+        $response = \Response::make($content);
+
+        $response->headers->set('Content-Type', 'text/xml; charset=UTF-8');
+        $response->headers->set('Cache-Control', 'no-cache');
+
+        return $response;
+    }
+
+    protected function generateXml(array $data): string
+    {
+        $content = '<isomsg><header>00000000</header>' . "\n";
+
+        foreach ($data as $key => $value)
+        {
+            $content .= '<field id="' . $key . '" value="' . $value . '"/>' . "\n";
+        }
+
+        $content .= '</isomsg>';
+
+        return $content;
     }
 }
