38eca72e76d291df793b945f66d9611aca03985f
Patlola <praveen.patlola@razorpay.com>
2017-12-19 13:10:39 +0000

    move simple xml func to util

--- app/Gateway/Fss/Utility.php
+++ app/Gateway/Fss/Utility.php
@@ -20,4 +20,24 @@ class Utility
 
         return $xml;
     }
+
+    /**
+     * By default decrypted comes with only fields instead of nested, to let simple xml understand the data.
+     * We wrap around response.
+     * Example: response with out a parent node = <result>SUCCESS</result><auth>0123</auth><payid>12314241</payid>
+     * Respoonse should be with a parent node for xml to array conversion
+     * <response><result>SUCCESS</result><auth>0123</auth><payid>12314241</payid></response>
+     *
+     * @param string $string
+     *
+     * @return array
+     */
+    public static function createResponseArray($string)
+    {
+        $string = "<response>" . $string . "</response>";
+
+        $result = (array) simplexml_load_string($string);
+
+        return $result;
+    }
 }
