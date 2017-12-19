69bd925a556c52777aabe791ec638cc5677ce623
Chart.js <anshul.sahni@razorpay.com>
2017-12-19 14:00:02 +0530

    [FIX]: humanReadableIndian method bugs

--- public/react/rzp/utils/numerals.js
+++ public/react/rzp/utils/numerals.js
@@ -1,15 +1,18 @@
 const suffixes = ['k', 'L', 'Cr'];
 
-export const humanReadableIndian = num => {
-  if (num < 1000) return num.toString();
-  else num /= 1000;
+export const formatNumberWithCommas = value =>
+  Number(value).toLocaleString('en-IN', {
+    currency: 'INR',
+  });
 
-  const stdDivisor = 100;
-  const powerRaised = Math.min(
-    2,
-    Math.floor(Math.log(num) / Math.log(stdDivisor))
-  );
-  return `${Math.round(num / Math.pow(stdDivisor, powerRaised))}${suffixes[
-    powerRaised
-  ]}`;
+export const humanReadableIndian = (num, noOfVisibleDigits = 3) => {
+  const maxWithoutReadable = Math.pow(10, Math.max(noOfVisibleDigits, 3));
+  if (num < Math.pow(10, Math.max(noOfVisibleDigits, 3)))
+    return formatNumberWithCommas(num);
+
+  const formattedNumberArr = formatNumberWithCommas(num).split(',');
+  const suffix = suffixes[Math.min(2, formattedNumberArr.length - 2)] || '';
+  return `${Number(`${formattedNumberArr[0]}.${formattedNumberArr[1]}`).toFixed(
+    2
+  )}${suffix}`;
 };
