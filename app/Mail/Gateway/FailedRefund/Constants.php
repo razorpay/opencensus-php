14cbbd3dac3f825c62e3ccb2ebf37e6e4482c228
tessie <tessyjoseph1992@gmail.com>
2017-12-20 20:10:58 +0530

    Add mail subjects header for airtel money failed Refund

--- app/Mail/Gateway/FailedRefund/Constants.php
+++ app/Mail/Gateway/FailedRefund/Constants.php
@@ -9,18 +9,22 @@ use RZP\Models\Payment\Gateway;
 class Constants extends Base\Constants
 {
     const HEADER_MAP = [
-        Gateway::UPI_ICICI              => 'UPI Icici Failed Refunds',
+        Gateway::UPI_ICICI     => 'UPI Icici Failed Refunds',
+        Gateway::AIRTEL_MONEY  => 'Airtel Money Failed Refunds',
     ];
 
     const SUBJECT_MAP = [
-        Gateway::UPI_ICICI              => 'UPI Icici Failed refunds file for ',
+        Gateway::UPI_ICICI      => 'UPI Icici Failed refunds file for ',
+        Gateway::AIRTEL_MONEY   => 'Airtel Money Failed refunds file for ',
     ];
 
     const BODY_MAP = [
-        Gateway::UPI_ICICI              => 'Please find attached failed refunds information for  ICICI UPI',
+        Gateway::UPI_ICICI    => 'Please find attached failed refunds information for  ICICI UPI',
+        Gateway::AIRTEL_MONEY => 'Please find attached failed refunds information for  Airtel Money',
     ];
 
     const MAIL_TEMPLATE_MAP = [
-        Gateway::UPI_ICICI              => 'emails.message',
+        Gateway::UPI_ICICI     => 'emails.message',
+        Gateway::AIRTEL_MONEY   => 'emails.message',
     ];
 }
