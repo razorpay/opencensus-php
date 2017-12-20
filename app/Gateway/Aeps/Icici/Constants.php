92755b1f0102dcd150570af00363a293fc65e9b4
Albin George <albinin0002@gmail.com>
2017-12-20 22:41:52 +0530

    [aeps] Use constants for refund request

---
+++ app/Gateway/Aeps/Icici/Constants.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace RZP\Gateway\Aeps\Icici;
+
+class Constants
+{
+    REFUND_DATA_ACCOUNT_TYPE        = 'Saving';
+    REFUND_DATA_PRE_APPROVED        = 'A';
+    REFUND_DATA_USE_DEFAULT_ACC     = 'D';
+    REFUND_DATA_DEFAULT_DEBIT       = 'N';
+    REFUND_DATA_DEFAULT_CREDIT      = 'N';
+    REFUND_DATA_GLOBAL_ADDRESS_TYPE = 'AADHAR';
+    REFUND_DATA_PAYEE_NAME          = 'Razorpay';
+    REFUND_DATA_MCC                 = '5411';
+    REFUND_DATA_MERCHANT_TYPE       = 'ENTITY';
+}
