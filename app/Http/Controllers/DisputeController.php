2c0784c1c69f8e881d9fa2545befed24f618f704
hobbes09 <sourin.sutradhar@gmail.com>
2017-12-21 14:12:40 +0530

    [dispute] directing request to different services as per auth type

--- app/Http/Controllers/DisputeController.php
+++ app/Http/Controllers/DisputeController.php
@@ -21,19 +21,19 @@ class DisputeController extends Controller
     {
         $input = Request::all();
 
-        // For edits by merchant either from API or dashboard
+        $response = [];
 
-        if (($this->ba->isPrivateAuth() === true) or
-            ($this->ba->isProxyAuth() === true))
+        if ($this->ba->isAdminAuth())
+        {
+            $response = $this->service()->update($id, $input);
+        }
+        else if (($this->ba->isPrivateAuth() === true) or
+                ($this->ba->isProxyAuth() === true))
         {
             $response = $this->service()->updateForMerchant($id, $input);
-
-            return ApiResponse::json($response);
         }
 
-        $entity = $this->service()->update($id, $input);
-
-        return ApiResponse::json($entity);
+        return ApiResponse::json($response);
     }
 
     public function migrateOldAdjustments()
