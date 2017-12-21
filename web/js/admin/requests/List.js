3e012e8a50b04aaad60d2e92b5596575067c6df0
aseemgupta <aseemgupta2625@gmail.com>
2017-12-21 18:08:25 +0530

    [Bug] Fix request list

--- web/js/admin/requests/List.js
+++ web/js/admin/requests/List.js
@@ -74,7 +74,7 @@ export default class RequestList extends Component {
 const fields = [
   ['Action', item => item.permission_description],
   ['Title', item => item.title],
-  ['Created By', item => item.admin.name],
+  ['Created By', item => (item.admin ? item.admin.name : '--')],
   ['Created At', item => formatDate(item.created_at)],
   [
     'State',
