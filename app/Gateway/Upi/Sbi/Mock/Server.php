ab8e3f3b972dc07f43079a6430058cf5fdc0923a
mayankamencherla <mayank.amencherla@razorpay.com>
2017-12-25 15:51:04 +0530

    [sbi recon] Setting defaults to 9's in mock server response for sbi upi

--- app/Gateway/Upi/Sbi/Mock/Server.php
+++ app/Gateway/Upi/Sbi/Mock/Server.php
@@ -153,9 +153,9 @@ class Server extends Base\Mock\Server
 
         $response = [
             ResponseFields::PSP_REFERENCE_NO       => $paymentId,
-            ResponseFields::UPI_TRANS_REFERENCE_NO => $gatewayPayment->getNpciReferenceId(),
+            ResponseFields::UPI_TRANS_REFERENCE_NO => 99999,
             ResponseFields::NPCI_TRANSACTION_ID    => 99999999999,
-            ResponseFields::CUSTOMER_REFERENCE_NO  => $gatewayPayment->getGatewayPaymentId(),
+            ResponseFields::CUSTOMER_REFERENCE_NO  => 99999999999,
             ResponseFields::AMOUNT                 => $gatewayPayment->getAmount() / 100,
             ResponseFields::TRANSACTION_AUTH_DATE  => Carbon::now(Timezone::IST)->toDateTimeString(),
             ResponseFields::RESPONSE_CODE          => '00',
@@ -174,9 +174,9 @@ class Server extends Base\Mock\Server
     {
         $response = [
             ResponseFields::PSP_REFERENCE_NO       => $upiEntity[Entity::PAYMENT_ID],
-            ResponseFields::UPI_TRANS_REFERENCE_NO => $upiEntity[Entity::NPCI_REFERENCE_ID],
+            ResponseFields::UPI_TRANS_REFERENCE_NO => 99999,
             ResponseFields::NPCI_TRANSACTION_ID    => 99999999999,
-            ResponseFields::CUSTOMER_REFERENCE_NO  => $upiEntity[Entity::GATEWAY_PAYMENT_ID],
+            ResponseFields::CUSTOMER_REFERENCE_NO  => 99999999999,
             ResponseFields::AMOUNT                 => $upiEntity[Entity::AMOUNT] / 100,
             ResponseFields::TRANSACTION_AUTH_DATE  => Carbon::now(Timezone::IST)->toDateTimeString(),
             ResponseFields::RESPONSE_CODE          => '00',
