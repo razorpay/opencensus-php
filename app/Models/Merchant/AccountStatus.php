216a5eec7229d091f2f609993adb95f43cdaa35b
Jitendra Ojha <jitendra-1217@users.noreply.github.com>
2017-12-27 21:08:39 +0530

    [es][merchant] Modify archived filter; Keep old logic for bc; (#6731)

--- app/Models/Merchant/AccountStatus.php
+++ app/Models/Merchant/AccountStatus.php
@@ -13,7 +13,8 @@ final class AccountStatus
     const SUSPENDED                   = 'suspended';
     const ARCHIVED                    = 'archived';
     const ACTIVATED                   = 'activated';
-    const PENDING_OLD                 = 'pending_old'; // To be removed, for bc.
+    const PENDING_OLD                 = 'pending_old';  // To be removed; for bc
+    const ARCHIVED_OLD                = 'archived_old'; // To be removed; for bc
     const PENDING                     = 'pending';
     const PENDING_UNDER_REVIEW        = 'pending_under_review';
     const PENDING_NEEDS_CLARIFICATION = 'pending_needs_clarification';
