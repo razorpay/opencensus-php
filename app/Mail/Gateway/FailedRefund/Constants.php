f13a9874bca43cafb904a75385cdde2de22314da
tessie <tessyjoseph1992@gmail.com>
2017-12-29 14:49:45 +0530

    Fix constant undefined for wallet_airtelmoney

--- app/Mail/Gateway/FailedRefund/Constants.php
+++ app/Mail/Gateway/FailedRefund/Constants.php
@@ -10,23 +10,23 @@ use RZP\Mail\Gateway\RefundFile;
 class Constants extends RefundFile\Constants
 {
     const HEADER_MAP = [
-        Gateway::UPI_ICICI     => 'UPI Icici Failed Refunds',
-        Gateway::AIRTEL_MONEY  => 'Airtel Money Failed Refunds',
+        Gateway::UPI_ICICI          => 'UPI Icici Failed Refunds',
+        Gateway::WALLET_AIRTELMONEY => 'Airtel Money Failed Refunds',
     ];
 
     const SUBJECT_MAP = [
-        Gateway::UPI_ICICI      => 'UPI Icici Failed refunds file for ',
-        Gateway::AIRTEL_MONEY   => 'Airtel Money Failed refunds file for ',
+        Gateway::UPI_ICICI          => 'UPI Icici Failed refunds file for ',
+        Gateway::WALLET_AIRTELMONEY => 'Airtel Money Failed refunds file for ',
     ];
 
     const BODY_MAP = [
-        Gateway::UPI_ICICI    => 'Please find attached failed refunds information for  ICICI UPI',
-        Gateway::AIRTEL_MONEY => 'Please find attached failed refunds information for  Airtel Money',
+        Gateway::UPI_ICICI          => 'Please find attached failed refunds information for  ICICI UPI',
+        Gateway::WALLET_AIRTELMONEY => 'Please find attached failed refunds information for  Airtel Money',
     ];
 
     const MAIL_TEMPLATE_MAP = [
-        Gateway::UPI_ICICI     => 'emails.message',
-        Gateway::AIRTEL_MONEY   => 'emails.message',
+        Gateway::UPI_ICICI            => 'emails.message',
+        Gateway::WALLET_AIRTELMONEY   => 'emails.message',
     ];
 
     const MAILTAG_MAP = [
