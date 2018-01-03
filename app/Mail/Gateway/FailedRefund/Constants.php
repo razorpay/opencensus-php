b91064202f33220e7b9b399ebf7d02a3cf1c1e7f
tessie <tessyjoseph1992@gmail.com>
2018-01-03 18:25:22 +0530

    Fix space issues in FaieldRefund Mail constants

--- app/Mail/Gateway/FailedRefund/Constants.php
+++ app/Mail/Gateway/FailedRefund/Constants.php
@@ -29,27 +29,27 @@ class Constants extends Base\Constants
     const BODY_MAP = [
         Gateway::UPI_ICICI          => 'Please find attached failed refunds information for ICICI UPI',
         Gateway::WALLET_AIRTELMONEY => 'Please find attached failed refunds information for Airtel Money',
-        Gateway::AXIS_MIGS           => 'Please find attached failed refunds information for Axis Migs',
+        Gateway::AXIS_MIGS          => 'Please find attached failed refunds information for Axis Migs',
         Gateway::FIRST_DATA         => 'Please find attached failed refunds information for FirstData',
         Gateway::CYBERSOURCE        => 'Please find attached failed refunds information for Cybersource',
         Gateway::HDFC               => 'Please find attached failed refunds information for Hdfc',
     ];
 
     const MAIL_TEMPLATE_MAP = [
-        Gateway::UPI_ICICI            => 'emails.message',
-        Gateway::WALLET_AIRTELMONEY   => 'emails.message',
-        Gateway::AXIS_MIGS            => 'emails.message',
-        Gateway::FIRST_DATA           => 'emails.message',
-        Gateway::CYBERSOURCE          => 'emails.message',
-        Gateway::HDFC                 => 'emails.message',
+        Gateway::UPI_ICICI           => 'emails.message',
+        Gateway::WALLET_AIRTELMONEY  => 'emails.message',
+        Gateway::AXIS_MIGS           => 'emails.message',
+        Gateway::FIRST_DATA          => 'emails.message',
+        Gateway::CYBERSOURCE         => 'emails.message',
+        Gateway::HDFC                => 'emails.message',
     ];
 
     const MAILTAG_MAP = [
-        Gateway::UPI_ICICI            => MailTags::ICICI_UPI_FAILED_REFUNDS_MAIL,
-        Gateway::WALLET_AIRTELMONEY   => MailTags::AIRTEL_MONEY_FAILED_REFUNDS_MAIL,
-        Gateway::AXIS_MIGS            => MailTags::AXIS_MIGS_FAILED_REFUNDS_MAIL,
-        Gateway::FIRST_DATA           => MailTags::FIRST_DATA_FAILED_REFUNDS_MAIL,
-        Gateway::CYBERSOURCE          => MailTags::CYBERSOURCE_FAILED_REFUNDS_MAIL,
-        Gateway::HDFC                 => MailTags::HDFC_FAILED_REFUNDS_MAIL,
+        Gateway::UPI_ICICI           => MailTags::ICICI_UPI_FAILED_REFUNDS_MAIL,
+        Gateway::WALLET_AIRTELMONEY  => MailTags::AIRTEL_MONEY_FAILED_REFUNDS_MAIL,
+        Gateway::AXIS_MIGS           => MailTags::AXIS_MIGS_FAILED_REFUNDS_MAIL,
+        Gateway::FIRST_DATA          => MailTags::FIRST_DATA_FAILED_REFUNDS_MAIL,
+        Gateway::CYBERSOURCE         => MailTags::CYBERSOURCE_FAILED_REFUNDS_MAIL,
+        Gateway::HDFC                => MailTags::HDFC_FAILED_REFUNDS_MAIL,
     ];
 }
