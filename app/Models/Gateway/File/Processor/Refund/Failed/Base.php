2445f2b53b75fd2242df7694c9b0da75a40b669a
tessie <tessyjoseph1992@gmail.com>
2018-01-03 14:25:08 +0530

    Avoid Unneccesary inheritance

--- app/Models/Gateway/File/Processor/Refund/Failed/Base.php
+++ app/Models/Gateway/File/Processor/Refund/Failed/Base.php
@@ -3,15 +3,21 @@
 namespace RZP\Models\Gateway\File\Processor\Refund\Failed;
 
 use Mail;
+use Carbon\Carbon;
 
+use RZP\Error\ErrorCode;
 use RZP\Models\FileStore;
+use RZP\Constants\Timezone;
+use RZP\Gateway\Base\Action;
 use RZP\Models\Payment\Method;
 use RZP\Models\Payment\Gateway;
+use RZP\Models\Gateway\File\Status;
 use RZP\Models\Base\PublicCollection;
-use RZP\Models\Gateway\File\Processor\Refund;
+use RZP\Exception\GatewayFileException;
 use RZP\Mail\Gateway\FailedRefund\Base as FailedRefundFileMail;
+use RZP\Models\Gateway\File\Processor\Base as BaseProcessor;
 
-class Base extends Refund\Base
+class Base extends BaseProcessor
 {
     public function fetchEntities(): PublicCollection
     {
@@ -39,7 +45,125 @@ class Base extends Refund\Base
         return $refunds;
     }
 
-    protected function formatDataForMail()
+    public function checkIfValidDataAvailable(PublicCollection $refunds)
+    {
+        if ($refunds->isEmpty() === true)
+        {
+            throw new GatewayFileException(
+                    ErrorCode::SERVER_ERROR_GATEWAY_FILE_NO_DATA_FOUND);
+        }
+    }
+
+    public function generateData(PublicCollection $refunds)
+    {
+        $gateway = static::GATEWAY;
+
+        $data = [];
+
+        foreach ($refunds as $refund)
+        {
+            $payment = $refund->payment;
+
+            $terminal = $payment->terminal;
+
+            $col['refund'] = $refund->toArray();
+            $col['payment'] = $payment->toArray();
+            $col['terminal'] = $terminal->toArray();
+
+            $data[] = $col;
+        }
+
+        $paymentIds = $refunds->pluck('payment_id')->toArray();
+
+        $gatewayEntities = $this->repo->$gateway->fetchByPaymentIdsAndAction(
+                            $paymentIds, Action::AUTHORIZE);
+
+        $gatewayEntities = $gatewayEntities->keyBy('payment_id');
+
+        $data = array_map(function($row) use ($gatewayEntities)
+        {
+            $paymentId = $row['payment']['id'];
+
+            if (isset($gatewayEntities[$paymentId]))
+            {
+                $row['gateway'] = $gatewayEntities[$paymentId]->toArray();
+            }
+
+            return $row;
+        }, $data);
+
+        return $data;
+    }
+
+    public function createFile($data)
+    {
+        // Don't process further if file is already generated
+        if ($this->isFileGenerated() === true)
+        {
+            return;
+        }
+
+        try
+        {
+            $fileData = $this->formatDataForFile($data);
+
+            $fileName = $this->getFileToWriteNameWithoutExt();
+
+            $creator = new FileStore\Creator;
+
+            $creator->extension(static::EXTENSION)
+                    ->content($fileData)
+                    ->name($fileName)
+                    ->store(FileStore\Store::S3)
+                    ->type(static::FILE_TYPE)
+                    ->entity($this->gatewayFile)
+                    ->save();
+
+            $file = $creator->getFileInstance();
+
+            $this->gatewayFile->setFileGeneratedAt($file->getCreatedAt());
+
+            $this->gatewayFile->setStatus(Status::FILE_GENERATED);
+        }
+        catch (\Throwable $e)
+        {
+            throw new GatewayFileException(
+                ErrorCode::SERVER_ERROR_GATEWAY_FILE_ERROR_GENERATING_FILE,
+                [
+                    'id'        => $this->gatewayFile->getId(),
+                ],
+                $e);
+        }
+    }
+
+    public function sendFile($data)
+    {
+        try
+        {
+            $recipients = $this->gatewayFile->getRecipients();
+
+            $mailData = $this->formatDataForMail($data);
+
+            $refundFileMail = new FailedRefundFileMail($mailData, static::GATEWAY, $recipients);
+
+            Mail::queue($refundFileMail);
+
+            $this->gatewayFile->setFileSentAt(time());
+
+            $this->gatewayFile->setStatus(Status::FILE_SENT);
+        }
+        catch (\Throwable $e)
+        {
+            throw new GatewayFileException(
+                ErrorCode::SERVER_ERROR_GATEWAY_FILE_ERROR_SENDING_FILE,
+                [
+                    'id'        => $this->gatewayFile->getId(),
+                ],
+                $e);
+        }
+    }
+
+    protected function formatDataForMail(array $data)
     {
         $file = $this->gatewayFile
                      ->files()
@@ -56,10 +180,15 @@ class Base extends Refund\Base
         return $mailData;
     }
 
-    protected function refundMail($mailData, $recipients)
+    protected function shouldNotReportFailure(string $code): bool
     {
-        $refundFileMail = new FailedRefundFileMail($mailData, static::GATEWAY, $recipients);
+        return ($code === ErrorCode::SERVER_ERROR_GATEWAY_FILE_NO_DATA_FOUND);
+    }
 
-        return $refundFileMail;
+    protected function getFileToWriteNameWithoutExt()
+    {
+        $time = Carbon::now(Timezone::IST)->format('d-m-Y');
+
+        return static::FILE_NAME . '_' . $this->mode . '_' . $time;
     }
 }
