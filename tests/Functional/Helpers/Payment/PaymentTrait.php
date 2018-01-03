17dc2cdc840e5e0bcdbc8e7ac2d660d0d12973f2
tessie <tessyjoseph1992@gmail.com>
2018-01-03 19:11:17 +0530

    Revert "year should be set as current year using carbon in default payment array"
    This reverts commit 706623a7017d4ad9ba8228b7b6a796f4f016d314.

--- tests/Functional/Helpers/Payment/PaymentTrait.php
+++ tests/Functional/Helpers/Payment/PaymentTrait.php
@@ -5,7 +5,6 @@ namespace RZP\Tests\Functional\Helpers\Payment;
 use Mockery;
 use Requests;
 use RZP\Exception;
-use Carbon\Carbon;
 use RZP\Models\Merchant\Account;
 use RZP\Http\BasicAuth\BasicAuth;
 use RZP\Models\Payment\Verify\Action;
@@ -1003,7 +1002,7 @@ trait PaymentTrait
             'number'            => '4012001038443335',
             'name'              => 'Harshil',
             'expiry_month'      => '12',
-            'expiry_year'       =>  Carbon::now()->year,
+            'expiry_year'       => '2017',
             'cvv'               => '566',
         );
 
