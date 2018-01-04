ac45562e303e587a560e1196dd83b7db3974f3d9
Patlola <praveen.patlola@razorpay.com>
2018-01-04 08:26:07 +0000

    remove this after testing

--- config/zapier.php
+++ config/zapier.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'signups'     => 'https://zapier.com/hooks/catch/1088429/2e1xtg/',
-    'submissions' => 'https://hooks.zapier.com/hooks/catch/1088429/46x8fa/',
+    'signups'     => 'https://hooks.zapier.com/hooks/catch/2687172/so2q0a/',
+    'submissions' => 'https://hooks.zapier.com/hooks/catch/2687172/so2q0a/',
     'activations' => 'https://hooks.zapier.com/hooks/catch/1088429/4twqyo/',
-    'mock'        => env('ZAPIER_MOCK', false),
+    'mock'        => false,
 ];
