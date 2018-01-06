6b02c418e29be50068e7c47180c1124b1cc9e86b
Prashanth Y.V <prashanth.yv@razorpay.com>
2018-01-06 16:08:19 +0530

    [recurring] fixes getters of isRecurring in payment and token entities

--- app/Models/Payment/Entity.php
+++ app/Models/Payment/Entity.php
@@ -1465,7 +1465,7 @@ class Entity extends Base\PublicEntity
 
     public function isRecurring()
     {
-        return $this->getAttribute(self::RECURRING);
+        return ($this->getAttribute(self::RECURRING) === true);
     }
 
     public function getCardId()
