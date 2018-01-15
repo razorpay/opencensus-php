29e28f2dc7077d2e55367c9df759d73b57250bd8
tessie <tessyjoseph1992@gmail.com>
2018-01-15 14:43:25 +0530

    Adding unique field vpc_transaction_no in failed refund file for migs

--- app/Models/Gateway/File/Processor/Refund/Failed/AxisMigs.php
+++ app/Models/Gateway/File/Processor/Refund/Failed/AxisMigs.php
@@ -16,15 +16,14 @@ class AxisMigs extends Base
     const FILE_TYPE          = FileStore\Type::AXIS_MIGS_FAILED_REFUND;
 
     const SR_NO                  = 'Sr No';
+    const VPC_TRANSACTION_NO     = 'vpc_TransactionNo';
+    const REFUND_ID              = 'refund_id';
     const REFUND_DATE            = 'refund Date';
     const TRANSACTION_DATE       = 'Transaction date';
     const REFUND_AMOUNT          = 'Refund Amount';
     const PAYMENT_AMOUNT         = 'Payment Amount';
     const REFUND_TYPE            = 'Refund Type';
     const MERCHANT_CODE          = 'Merchant Code';
-    const VPC_MERCHANT_TXN_REF   = 'VPC Merchant Txn Reference';
-    const VPC_RRN                = 'VPC rrn';
-    const REFUND_ID              = 'refund_id';
     const PAYMENT_ID             = 'Payment ID';
 
     protected function formatDataForFile(array $data)
@@ -37,19 +36,18 @@ class AxisMigs extends Base
                 $row['payment']['created_at'], Timezone::IST)->format('Y/m/d');
 
             $refundDate = Carbon::createFromTimestamp(
-                $row['refund']['last_attempted_at'], Timezone::IST)->format('Y/m/d');
+                 $row['refund']['last_attempted_at'], Timezone::IST)->format('Y/m/d');
 
             $formattedData[] = [
                 self::SR_NO                   => $index + 1,
+                self::VPC_TRANSACTION_NO      => $row['gateway']['vpc_TransactionNo'],
+                self::REFUND_ID               => $row['refund']['id'],
                 self::REFUND_DATE             => $refundDate,
                 self::TRANSACTION_DATE        => $transactionDate,
                 self::REFUND_AMOUNT           => $this->getFormattedAmount($row['refund']['amount']),
                 self::PAYMENT_AMOUNT          => $this->getFormattedAmount($row['payment']['amount']),
                 self::REFUND_TYPE             => $row['payment']['refund_status'],
                 self::MERCHANT_CODE           => $row['terminal']['gateway_merchant_id'],
-                self::VPC_MERCHANT_TXN_REF    => $row['gateway']['vpc_MerchTxnRef'],
-                self::VPC_RRN                 => $row['gateway']['vpc_ReceiptNo'],
-                self::REFUND_ID               => $row['refund']['id'],
                 self::PAYMENT_ID              => $row['payment']['id'],
             ];
         }
