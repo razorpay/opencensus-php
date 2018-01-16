309d35e02973a2dcd39639d97f79f079a9b51a7f
surya15t <surya.teja@razorpay.com>
2018-01-16 18:10:01 +0530

    [merchant][epos] making keys routes changes

--- config/api-route-map.php
+++ config/api-route-map.php
@@ -137,7 +137,7 @@ return [
         'merchant_activation_status'        => 'merchant/activation/{id}/activation_status',
         'merchant_get_rejection_reasons'    => 'merchant/activation/rejection_reasons',
         'merchant_activation_status_change_log' => 'merchant/activation/{id}/status_change_log',
-        
+
         // Add new dispute reason
         'dispute_reason_create'             => 'disputes/reasons',
 
@@ -586,6 +586,20 @@ return [
             'routeName' => 'subuser_register'
         ],
 
+        // Keys
+        'merchant_fetch_keys'               => [
+            'url'       => 'merchant/keys',
+            'routeName' => 'get_keys'
+        ],
+        'merchant_create_key'               => [
+            'url'       => 'merchant/keys',
+            'routeName' => 'keys_setup'
+        ],
+        'merchant_replace_key'              => [
+            'url'       => 'merchant/keys/{keyId}',
+            'routeName' => 'post_keys'
+        ],
+
         'merchant_pre_signup_details'       => 'pre_signup',
         'merchant_edit_pre_signup_details'  => 'pre_signup',
     ],
@@ -629,20 +643,6 @@ return [
 
     // auth
     'internal' => [
-        // Keys
-        'merchant_fetch_keys'               => [
-            'url'       => 'merchants/{id}/keys',
-            'routeName' => 'get_keys'
-        ],
-        'merchant_create_key'               => [
-            'url'       => 'merchants/{id}/keys',
-            'routeName' => 'keys_setup'
-        ],
-        'merchant_replace_key'              => [
-            'url'       => 'merchants/{merchantId}/keys/{keyId}',
-            'routeName' => 'post_keys'
-        ],
-
         // User Signup
         'user_register'                     => 'users/register',
         // User Login
