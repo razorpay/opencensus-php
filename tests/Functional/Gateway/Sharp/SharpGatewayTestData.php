4265c547d746b14ceddb4de8643bf48538f10aab
shubhambadkur12 <34709246+shubhambadkur12@users.noreply.github.com>
2018-01-18 20:50:58 +0530

    add soft and hard decline in sharp for recurring payments (#7049)
    * add soft and hard decline in sharp for recurring payments
    
    * optimize if condition
    
    * add mock tokenex
    
    * fixed card number and amount
    
    * add exception s2s payment

--- tests/Functional/Gateway/Sharp/SharpGatewayTestData.php
+++ tests/Functional/Gateway/Sharp/SharpGatewayTestData.php
@@ -5,6 +5,38 @@ use RZP\Error\PublicErrorCode;
 use RZP\Error\PublicErrorDescription;
 
 return [
+    'testRecurringHardDeclinePaymentAuthenticateCard' => [
+        'response'  => [
+            'content'     => [
+                'error' => [
+                    'code'          => PublicErrorCode::BAD_REQUEST_ERROR,
+                    'description'   => PublicErrorDescription::BAD_REQUEST_CARD_STOLEN_OR_LOST,
+                ],
+            ],
+            'status_code' => 400,
+        ],
+        'exception' => [
+            'class'                 => RZP\Exception\GatewayErrorException::class,
+            'internal_error_code'   => ErrorCode::BAD_REQUEST_CARD_STOLEN_OR_LOST,
+        ],
+    ],
+
+    'testRecurringSoftDeclinePaymentAuthenticateCard' => [
+        'response'  => [
+            'content'     => [
+                'error' => [
+                    'code'          => PublicErrorCode::BAD_REQUEST_ERROR,
+                    'description'   => PublicErrorDescription::BAD_REQUEST_PAYMENT_CARD_INSUFFICIENT_BALANCE,
+                ],
+            ],
+            'status_code' => 400,
+        ],
+        'exception' => [
+            'class'                 => RZP\Exception\GatewayErrorException::class,
+            'internal_error_code'   => ErrorCode::BAD_REQUEST_PAYMENT_CARD_INSUFFICIENT_BALANCE,
+        ],
+    ],
+
     'testPaymentFailed' => [
         'request' => [
             'method' => 'POST',
