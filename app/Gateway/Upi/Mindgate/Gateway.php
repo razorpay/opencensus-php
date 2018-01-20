632498448a53d98c458ff60559c015bb4e1189aa
Vivek <viveklakhera@gmail.com>
2018-01-20 16:56:22 +0530

    Fix upi mindgate key (#7116)

--- app/Gateway/Upi/Mindgate/Gateway.php
+++ app/Gateway/Upi/Mindgate/Gateway.php
@@ -261,12 +261,7 @@ class Gateway extends Base\Gateway
      */
     protected function getEncryptionKey()
     {
-        if ($this->mode === Mode::LIVE)
-        {
-            return $this->config['live_merchant_key'];
-        }
-
-        return $this->config['test_merchant_key'];
+        return $this->config['gateway_encryption_key'];
     }
 
     /**
--- config/gateway.php
+++ config/gateway.php
@@ -246,8 +246,7 @@ return [
 
     'upi_mindgate' => [
         'test_merchant_id'       => env('UPI_MINDGATE_TEST_MERCHANT_ID'),
-        'test_merchant_key'      => env('UPI_MINDGATE_TEST_MERCHANT_KEY'),
-        'live_merchant_key'      => env('UPI_MINDGATE_GATEWAY_SECURE_SECRET'),
+        'gateway_encryption_key' => env('UPI_MINDGATE_GATEWAY_SECURE_SECRET'),
     ],
 
     'upi_sbi' => [
