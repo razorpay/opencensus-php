29fc29c4b71ca59e3ce5347e9f0ae716737e64f9
tessie <tessyjoseph1992@gmail.com>
2018-01-22 16:41:08 +0530

    Changed Axis Migs format

--- app/Models/Gateway/File/Processor/Refund/Failed/AxisMigs.php
+++ app/Models/Gateway/File/Processor/Refund/Failed/AxisMigs.php
@@ -7,22 +7,21 @@ use RZP\Models\FileStore;
 
 class AxisMigs extends Base
 {
-    const GATEWAY            = Payment\Gateway::AXIS_MIGS;
-    const EXTENSION          = FileStore\Format::XLSX;
-    const FILE_NAME          = 'Axis_Migs_Failed_Refunds';
-    const FILE_TYPE          = FileStore\Type::AXIS_MIGS_FAILED_REFUND;
+    const GATEWAY   = Payment\Gateway::AXIS_MIGS;
+    const EXTENSION = FileStore\Format::XLSX;
+    const FILE_NAME = 'Axis_Migs_Failed_Refunds';
+    const FILE_TYPE = FileStore\Type::AXIS_MIGS_FAILED_REFUND;
+    const ACQUIRER  = Payment\GATEWAY::ACQUIRER_AXIS;
 
-    const SR_NO                  = 'Sr No';
-    const VPC_TRANSACTION_NO     = 'vpc_TransactionNo';
-    const REFUND_ID              = 'refund_id';
-    const REFUND_DATE            = 'refund Date';
-    const TRANSACTION_DATE       = 'Transaction date';
-    const REFUND_AMOUNT          = 'Refund Amount';
-    const PAYMENT_AMOUNT         = 'Payment Amount';
-    const REFUND_TYPE            = 'Refund Type';
-    const MERCHANT_CODE          = 'Merchant Code';
-    const PAYMENT_ID             = 'Payment ID';
-    const ACQUIRER               =  Payment\Gateway::ACQUIRER_AXIS;
+    const SR_NO                   = 'Sr No';
+    const CARD_NUMBER             = 'Card Number';
+    const TRANSACTION_AMOUNT      = 'Transaction Amount';
+    const MERCHANT_TRAN_REF       = 'Merchant Transaction Reference No.';
+    const MID                     = 'MID';
+    const REFUND_AMOUNT           = 'Amount to be refunded';
+    const TRANSACTION_DATE        = 'Transaction Date and Time';
+    const RAZORPAY_TRANSACTION_ID = 'Razorpay Payment ID';
+    const RAZORPAY_REFUND_ID      = 'Razorpay Refund ID';
 
     protected function formatDataForFile(array $data)
     {
@@ -32,15 +31,13 @@ class AxisMigs extends Base
         {
             $formattedData[] = [
                 self::SR_NO                   => $index + 1,
-                self::VPC_TRANSACTION_NO      => $row['gateway']['vpc_TransactionNo'],
-                self::REFUND_ID               => $row['refund']['id'],
-                self::REFUND_DATE             => $this->getFormattedDate($row['refund']['last_attempted_at'], 'Y/m/d'),
-                self::TRANSACTION_DATE        => $this->getFormattedDate($row['payment']['created_at'], 'Y/m/d'),
-                self::REFUND_AMOUNT           => $this->getFormattedAmount($row['refund']['amount']),
-                self::PAYMENT_AMOUNT          => $this->getFormattedAmount($row['payment']['amount']),
-                self::REFUND_TYPE             => $row['payment']['refund_status'],
-                self::MERCHANT_CODE           => $row['terminal']['gateway_merchant_id'],
-                self::PAYMENT_ID              => $row['payment']['id'],
+                self::CARD_NUMBER             => $this->getCardNumber($row['card']['iin'], $row['card']['last4']),
+                self::MERCHANT_TRAN_REF       => $row['gateway']['vpc_MerchTxnRef'],
+                self::MID                     => $row['terminal']['gateway_terminal_id'],
+                self::REFUND_AMOUNT           => $this->getFormattedAmount($row['payment']['amount']),
+                self::TRANSACTION_DATE        => $this->getFormattedDate($row['payment']['created_at'], 'd/m/y H:m'),
+                self::RAZORPAY_REFUND_ID      => $row['refund']['id'],
+                self::RAZORPAY_TRANSACTION_ID => $row['payment']['id'],
             ];
         }
 
