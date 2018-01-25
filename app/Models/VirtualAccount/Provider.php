f10240056a50662e0ea25b5c574d9642d96f5038
Ria Garg <ria.garg@razorpay.com>
2018-01-25 19:30:46 +0530

    [upi/icici] remove dynamic tlv

--- app/Models/VirtualAccount/Provider.php
+++ app/Models/VirtualAccount/Provider.php
@@ -253,10 +253,9 @@ class Provider
             $visaTlv,
             $masterCardTlv,
             $rupayCardTlv,
-            // Removing this as we will be sending VA reference always.
-            // Will support static QR code in another PR
-            // $this->getBharatQrUpiTlv(),
-            $this->getBharatQrDynamicUpiTlv($qrCode),
+            $this->getBharatQrUpiTlv(),
+            // This tag is not supported by UPI ICICI
+            // $this->getBharatQrDynamicUpiTlv($qrCode),
             Tags::MERCHANT_CATEGORY .$this->getLengthAndValue(Constants::MERCHANT_CATEGORY),
             Tags::CURRENCY_CODE . $this->getLengthAndValue(Constants::CURRENCY_CODE),
             $this->getBharatQrAmountTlv($qrCode),
