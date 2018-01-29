acd60848675ecb4e3840710621aad435be71ffea
mayankamencherla <mayank.amencherla@razorpay.com>
2018-01-29 14:22:43 +0530

    [nb oriental] Added ORBC to obscure

--- public/test/obscure.php
+++ public/test/obscure.php
@@ -96,6 +96,7 @@ function disableEmptyInputs(form) {
                 <option value="SBIN">SBI Bank</option>
                 <option value="ICIC">ICICI Bank</option>
                 <option value="AIRP">Airtel Payments Bank</option>
+                <option value="ORBC">Oriental Bank</option>
                 <option value="FDRL">Federal Bank</option>
                 <option value="RATN">RBL Bank</option>
                 <option value="CITI">CITI Bank</option>
