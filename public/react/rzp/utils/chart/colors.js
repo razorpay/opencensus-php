f7cb6b2549c3063b0dffb233af9131a8b5b50161
rzpamidi <prashanth.pamidi@razorpay.com>
2018-01-29 14:28:14 +0530

    Redirection logic and aesthetics

--- public/react/rzp/utils/chart/colors.js
+++ public/react/rzp/utils/chart/colors.js
@@ -1,12 +1,12 @@
 const colors = [
   '#4B5471', // very dark blue
-  '#5F7FB9', // dark blue
-  '#89C2ED', // light blue
-  '#C0C0F2', // violet
-  '#97D4E4', // teal
-  '#F4BEDF', // pink
-  '#EDD8BE', // biscuit
-  '#F3AFAF', // red
+  '#6E8EEE', // dark blue
+  '#97E2FE', // light blue
+  '#D3CFFF', // violet
+  '#93F5E9', // teal
+  '#F9BCE1', // pink
+  '#F6D8B4', // biscuit
+  '#FFABAB', // red
   'rgb(230, 126, 34)',
   'rgb(241, 196, 15)',
   'rgb(155, 89, 182)',
