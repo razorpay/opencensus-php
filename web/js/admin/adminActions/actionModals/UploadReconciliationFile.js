48fcf626977def00a924c31d713ddf8850a8501f
rzpamidi <prashanth.pamidi@razorpay.com>
2018-02-05 17:26:27 +0530

    Fixes option value for Virtual Accounts Kotak

--- web/js/admin/adminActions/actionModals/UploadReconciliationFile.js
+++ web/js/admin/adminActions/actionModals/UploadReconciliationFile.js
@@ -32,6 +32,10 @@ const gateWayOptions = [
   'Virtual Accounts Kotak',
 ];
 
+const optionValueMap = {
+  'Virtual Accounts Kotak': 'VirtualAccKotak',
+};
+
 UploadReconciliationFile.permission = 'add_reconciliation_file';
 UploadReconciliationFile.title = 'Upload Reconciliation File (Payment/Refund)';
 export default function UploadReconciliationFile() {
@@ -40,7 +44,10 @@ export default function UploadReconciliationFile() {
       <SelectMode />
       <SelectField label="Gateway" name="gateway">
         {gateWayOptions.map((opt, idx) => (
-          <option key={idx} value={opt.replace(/\s/g, '')}>
+          <option
+            key={idx}
+            value={optionValueMap[opt] || opt.replace(/\s/g, '')}
+          >
             {opt}
           </option>
         ))}
