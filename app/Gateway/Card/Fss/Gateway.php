615cf668269eb10d3bc8695e754531f1aae7c6c0
Patlola <praveen.patlola@razorpay.com>
2018-02-06 20:03:22 +0000

    test cases fix for bank code mapping

--- app/Gateway/Card/Fss/Gateway.php
+++ app/Gateway/Card/Fss/Gateway.php
@@ -272,15 +272,17 @@ class Gateway extends Base\Gateway
         switch ($gatewayAquirer)
         {
             case Acquirer::FSS:
-                $requestContent[Fields::BANK_CODE] = BankCodes::getBankCodeByIfsc($input[E::CARD][Card\Entity::ISSUER]);
-                $requestContent[Fields::UDF5]      = strtolower(Constants::TRACK_ID);
-                $requestContent[Fields::UDF3]      = $input[E::TERMINAL][Terminal\Entity::GATEWAY_TERMINAL_ID];
-
                 if ($this->mode === Mode::TEST)
                 {
                     $requestContent[Fields::ID]         = $this->config['fss']['terminal_id'];
                     $requestContent[Fields::BANK_CODE]  = $this->config['fss']['bank_code'];
                     $requestContent[Fields::UDF3]       = $this->config['fss']['terminal_id'];
+                }
+                else
+                {
+                    $requestContent[Fields::BANK_CODE] = BankCodes::getBankCodeByIfsc($input[E::CARD][Card\Entity::ISSUER]);
+                    $requestContent[Fields::UDF5]      = strtolower(Constants::TRACK_ID);
+                    $requestContent[Fields::UDF3]      = $input[E::TERMINAL][Terminal\Entity::GATEWAY_TERMINAL_ID];
                 }
 
                 break;
