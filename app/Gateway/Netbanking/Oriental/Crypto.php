567e51ba4dfe4ad79378f5f2385e71c3eb1a0f9f
mayankamencherla <mayank.amencherla@razorpay.com>
2018-02-06 14:46:58 +0530

    [nb oriental] Padding key string to length 16

--- app/Gateway/Netbanking/Oriental/Crypto.php
+++ app/Gateway/Netbanking/Oriental/Crypto.php
@@ -6,6 +6,13 @@ use RZP\Gateway\Base\AESCrypto;
 
 class Crypto extends AESCrypto
 {
+    public function __construct(int $mode, string $masterKey, string $initializationVector = '')
+    {
+        $masterKey = str_pad($masterKey, 16, $masterKey);
+
+        parent::__construct($mode, $masterKey, $initializationVector);
+    }
+
     public function encryptString(string $string)
     {
         return base64_encode(parent::encryptString($string));
