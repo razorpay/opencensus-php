70fcfe85199384dc950935eb56a972835e4b4563
kajol22 <kajolnigam@gmail.com>
2018-02-06 21:00:41 +0530

    Hitachi reconciliation (#6751)
    * adds reconciliation for hitachi
    
    * tests added
    
    * test cases fixed
    
    * adds hitachi gateway in base file
    
    * adds issettles in test case
    
    * test cases fixed
    
    * test cases modified
    
    * renames auth_id to auth_code
    
    * adds checks for card fields
    
    * makes getPaymentId consistent to parent
    
    * use trace instead of app trace
    
    * [recon] Setting hitachi as GATEWAY_FEES_ABSENT_GATEWAYS
    
    * [recon][hitachi] Adds a check of 200 for payment recon
    
    * [recon] Adds Refund reconciliator for hitachi
    
    * [recon] Fix test case for hitachi
    
    * [recon] Move findByRefundIdAndAction to base repository
    
    * [recon] Returns the result from base repo
    
    * [recon] Raises recon alert in case refund not found
    
    * [recon][tests] Adds hitachi refund recon test

--- app/Gateway/Hitachi/Repository.php
+++ app/Gateway/Hitachi/Repository.php
@@ -2,6 +2,8 @@
 
 namespace RZP\Gateway\Hitachi;
 
+use RZP\Error;
+use RZP\Exception;
 use RZP\Gateway\Base;
 
 class Repository extends Base\Repository
@@ -17,4 +19,27 @@ class Repository extends Base\Repository
                     ->where(Entity::ACTION, '=', Base\Action::AUTHORIZE)
                     ->firstOrFail();
     }
+
+    public function findSuccessfulRefundByRefundId($refundId)
+    {
+        $refundEntities = $this->findByRefundIdAndAction($refundId, Base\Action::REFUND);
+
+        //
+        // There should never be more than one successful gateway refund entity
+        // for a given refund_id
+        //
+        if ($refundEntities->count() > 1)
+        {
+            throw new Exception\LogicException(
+                'Multiple successful refund entities found for a refund ID',
+                Error\ErrorCode::SERVER_ERROR_MULTIPLE_REFUNDS_FOUND,
+                [
+                    'refund_id' => $refundId,
+                    'refund_entities' => $refundEntities->toArray()
+                ]);
+        }
+
+        return $refundEntities;
+    }
+
 }
