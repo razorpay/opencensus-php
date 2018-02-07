651a24d8c6a929e96d3110108d6c02bb18ba28a2
Albin George <albinin0002@gmail.com>
2018-02-07 15:01:25 +0530

    [bob_nb] Test case for user cancelled payments (#7406)
    * [bob_nb] Callback URL quickfix
    
    * [bob_nb] Test case for use cancelled payment

--- tests/Functional/Gateway/Netbanking/Bob/NetbankingBobGatewayTest.php
+++ tests/Functional/Gateway/Netbanking/Bob/NetbankingBobGatewayTest.php
@@ -72,6 +72,22 @@ class NetbankingBobGatewayTest extends TestCase
         $this->assertTestResponse($gatewayPayment, 'testPaymentFailedNetbankingEntity');
     }
 
+    public function testUserCancelledPayments()
+    {
+        $data = $this->testData[__FUNCTION__];
+
+        $this->mockFailedCallbackResponse();
+
+        $this->mockCancelledPaymentResponse();
+
+        $this->runRequestResponseFlow(
+            $data,
+            function()
+            {
+                $this->doAuthAndCapturePayment($this->payment);
+            });
+    }
+
     public function testPaymentAmountMismatch()
     {
         $data = $this->testData[__FUNCTION__];
@@ -167,6 +183,19 @@ class NetbankingBobGatewayTest extends TestCase
             {
                 $content[ResponseFields::STATUS] = Status::FAILURE;
                 unset($content[ResponseFields::BANK_REF_NUMBER]);
+            }
+        });
+    }
+
+    protected function mockCancelledPaymentResponse()
+    {
+        $this->mockServerContentFunction(function(& $content, $action = null)
+        {
+            if ($action === 'cancelPayment')
+            {
+                $content['request']['method'] = 'get';
+                $content['request']['url'] = $content['request']['url'] . http_build_query($content['content']);
+                unset($content['request']['content']);
             }
         });
     }
