7692bd4a51e6cdc107c6be4ab9dbea325d024e05
mayankamencherla <mayank.amencherla@razorpay.com>
2018-02-12 18:18:39 +0530

    [upi icici recon] Setting refund status as processed

--- app/Reconciliator/UpiIcici/SubReconciliator/RefundReconciliate.php
+++ app/Reconciliator/UpiIcici/SubReconciliator/RefundReconciliate.php
@@ -42,6 +42,13 @@ class RefundReconciliate extends Base\RefundReconciliate
         return Carbon::createFromFormat('d-m-Y h:i a', $refundSettledAt, Timezone::IST)->getTimestamp();
     }
 
+    /**
+     * This method is called when refundId is not found in our DB. Therefore,
+     * we would need to find it based on the UPI entity instead.
+     *
+     * @param array $row
+     * @return null
+     */
     protected function getPaymentId(array $row)
     {
         $rrn = $row[self::ORIGINAL_BANK_RRN] ?? null;
@@ -76,6 +83,23 @@ class RefundReconciliate extends Base\RefundReconciliate
 
             return false;
         }
+
+        return true;
+    }
+
+    protected function persistReconciliationData(array $rowDetails)
+    {
+        $retVal = parent::persistReconciliationData($rowDetails);
+
+        if ($retVal === false)
+        {
+            return $retVal;
+        }
+
+        // We mark the refund status as processed if retval is true
+        $this->refund->setStatusProcessed();
+
+        $this->repo->saveOrFail($this->refund);
 
         return true;
     }
