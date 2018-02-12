48c07c64db46978d59ac0600bd0cf0c1ba567725
Saurav Chowdhury <saurav1991@gmail.com>
2018-02-12 20:20:03 +0530

    adds correct order by clause to all entities (#7471)

--- app/Base/RepositoryFetch.php
+++ app/Base/RepositoryFetch.php
@@ -2,11 +2,9 @@
 
 namespace RZP\Base;
 
-use RZP\Constants;
 use RZP\Constants\Es;
 use RZP\Trace\TraceCode;
 use RZP\Models\Merchant;
-use RZP\Models\Customer;
 use RZP\Constants\Entity as E;
 use RZP\Models\Base\EsRepository;
 use RZP\Models\Base\PublicEntity;
@@ -780,7 +778,8 @@ trait RepositoryFetch
 
     protected function addQueryOrder($query)
     {
-        $query->orderBy(Common::ID, 'desc');
+        $query->orderBy(Common::CREATED_AT, 'desc')
+              ->orderBy(Common::ID, 'desc');
     }
 
     protected function addForceIndexForNestaway($query)
