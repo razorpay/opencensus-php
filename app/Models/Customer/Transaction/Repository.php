43aae895552bd08b1a12ece19b9011d5d11a38c7
Amit Mahbubani <amit.mahbubani@razorpay.com>
2018-02-15 00:30:22 +0530

    [openwallet] Support for force authorize payment

--- app/Models/Customer/Transaction/Repository.php
+++ app/Models/Customer/Transaction/Repository.php
@@ -3,8 +3,8 @@
 namespace RZP\Models\Customer\Transaction;
 
 use RZP\Models\Base;
-use RZP\Models\Merchant;
-use RZP\Models\Customer;
+use RZP\Models\Payment;
+use RZP\Constants\Entity as E;
 
 class Repository extends Base\Repository
 {
@@ -18,4 +18,21 @@ class Repository extends Base\Repository
         Entity::CUSTOMER_ID   => 'sometimes|string|size:14',
         Entity::MERCHANT_ID   => 'sometimes|string|size:14',
     ];
+
+    public function findByPaymentIdAndAmountForVerify(string $paymentId, int $amount, string $merchantId)
+    {
+        Payment\Entity::verifyIdAndSilentlyStripSign($paymentId);
+
+        $customerTxn = $this->newQuery()
+                            ->where(Entity::TYPE, Type::TRANSFER)
+                            ->where(Entity::ENTITY_TYPE, E::PAYMENT)
+                            ->where(Entity::ENTITY_ID, $paymentId)
+                            ->where(Entity::AMOUNT, $amount)
+                            ->where(Entity::DEBIT, $amount)
+                            ->where(Entity::STATUS, 'complete')
+                            ->merchantId($merchantId)
+                            ->get();
+
+        return $customerTxn;
+    }
 }
