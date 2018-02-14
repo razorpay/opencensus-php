f22411a0216821a2986e96d71192963464e37148
Miroojin Bakshi <mbakshi1094@gmail.com>
2018-02-14 19:01:15 +0530

    [shield] Add test for Shield client

--- app/Services/Mock/ShieldClient.php
+++ app/Services/Mock/ShieldClient.php
@@ -4,44 +4,14 @@ namespace RZP\Services\Mock;
 
 class ShieldClient
 {
-    const TEST_RESPONSE = [
-        "id"         => "12345678",
-        "expression" => "amount > 10000",
-        "created_at" => 1518608813,
-        "updated_at" => 1518608813,
-        "action"     => "",
-    ];
-
     public function createRule(array $input)
     {
-        return TEST_RESPONSE;
-    }
-
-    public function getRules()
-    {
-        return [TEST_RESPONSE];
-    }
-
-    public function getRuleById(string $id)
-    {
-        return TEST_RESPONSE;
-    }
-
-    public function deleteRuleById(string $id)
-    {
-        return null;
-    }
-
-    public function updateRuleById(string $id, array $input)
-    {
-        return TEST_RESPONSE;
-    }
-
-    public function evaluateRules($input)
-    {
         return [
-            "result" => false,
-            "triggered_rules" => [],
+            'id'         => '12345678',
+            'expression' => $input['expression'],
+            'is_active'  => true,
+            'updated_at' => 1518608813,
+            'created_at' => 1518608813,
         ];
     }
 }
