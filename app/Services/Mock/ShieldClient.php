258d71e06b872c72e8c32d5593c63d927d4e62bc
Miroojin Bakshi <mbakshi1094@gmail.com>
2018-02-14 17:29:24 +0530

    [shield] Add mock client implementation

--- app/Services/Mock/ShieldClient.php
+++ app/Services/Mock/ShieldClient.php
@@ -2,8 +2,46 @@
 
 namespace RZP\Services\Mock;
 
-
 class ShieldClient
 {
+    const TEST_RESPONSE = [
+        "id"         => "12345678",
+        "expression" => "amount > 10000",
+        "created_at" => 1518608813,
+        "updated_at" => 1518608813,
+        "action"     => "",
+    ];
 
+    public function createRule(array $input)
+    {
+        return TEST_RESPONSE;
+    }
+
+    public function getRules()
+    {
+        return [TEST_RESPONSE];
+    }
+
+    public function getRuleById(string $id)
+    {
+        return TEST_RESPONSE;
+    }
+
+    public function deleteRuleById(string $id)
+    {
+        return null;
+    }
+
+    public function updateRuleById(string $id, array $input)
+    {
+        return TEST_RESPONSE;
+    }
+
+    public function evaluateRules($input)
+    {
+        return [
+            "result" => false,
+            "triggered_rules" => [],
+        ];
+    }
 }
