e98bcb79d9df5226700d09e90d3ddb737332b001
Pranav Gupta <pranavguptarulz@gmail.com>
2018-02-15 00:24:32 +0530

    fix onboarding fileupload

--- web/js/merchant/modules/onboarding.js
+++ web/js/merchant/modules/onboarding.js
@@ -10,13 +10,12 @@ export const saveOnboarding = (feature, fields, file, fileName) => {
   let formData = new FormData();
 
   if (file) {
-    formData.append('file', file);
-    formData.append('file_name', fileName);
+    formData.append(fileName, file);
   }
 
   for (let key in fields) {
     if (fields.hasOwnProperty(key)) {
-      formData.append(`body[${key}]`, fields[key]);
+      formData.append(key, fields[key]);
     }
   }
 
