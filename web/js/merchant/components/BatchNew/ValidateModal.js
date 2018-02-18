dbaac2ef243c9e6f94b39605dad0b6d235d4cdc3
rchaubey <dev.rohit.chaubey@gmail.com>
2018-02-19 04:30:52 +0530

    [Fea] batch upload success modal

--- web/js/merchant/components/BatchNew/ValidateModal.js
+++ web/js/merchant/components/BatchNew/ValidateModal.js
@@ -19,87 +19,84 @@ export default function BatchValidateModal({
   closeModal,
 }) {
   return (
-    <div class="batch-upload-modal">
-      <ModalHeader title="Batch Upload" onCloseClick={closeModal} />
-      <div class="modal-body">
-        <h4 class="modal-heading">Upload File</h4>
-        <div class="modal-file">
-          <FileUploadInputButton
-            accept="xlsx,csv"
-            uploadedFileName="Upload File here"
-            maxSize="1000000"
-            onChange={onFileChange}
-          />
-          {notifyMsg && (
-            <h5 class={`notification ${status}`}>
-              <i class="i i-info-circle m-r" />
-              {notifyMsg}
-            </h5>
+    <div class="modal-body">
+      <h4 class="modal-heading">Upload File</h4>
+      <div class="modal-file">
+        <FileUploadInputButton
+          accept="xlsx,csv"
+          uploadedFileName="Upload File here"
+          maxSize="1000000"
+          onChange={onFileChange}
+        />
+        {notifyMsg && (
+          <h5 class={`notification ${status}`}>
+            <i class="i i-info-circle m-r" />
+            {notifyMsg}
+          </h5>
+        )}
+      </div>
+
+      {/* Show batch upload modal info when no file uploaded */}
+      {!status && (
+        <div class="modal-info">
+          <h5>
+            Getting Started with Batch Uploads?{' '}
+            <a class="btn btn-link m-l" href={docUrl} target="_blank">
+              View Documentation <i class="i i-external-link" />
+            </a>
+          </h5>
+          <p>
+            Upload a batch file to continue. Please note the following things
+            before proceeding further:{' '}
+            {!shouldLoadMore && (
+              <span class="btn-link clickable" onClick={onLoadMore}>
+                Load More
+              </span>
+            )}
+          </p>
+          {shouldLoadMore && (
+            <Fragment>
+              <ul>
+                <li>1. The Amount mentioned should be in Paise.</li>
+                <li>
+                  2. The receipt id for all {titleCase(batchType)}s should be
+                  unique.
+                </li>
+                <li>3. The number of rows should not exceed 5000.</li>
+              </ul>
+              <p>
+                In case of any issues, please{' '}
+                <a class="btn-link" href={sampleUrl}>
+                  download sample file
+                </a>
+              </p>
+            </Fragment>
           )}
         </div>
+      )}
 
-        {/* Show batch upload modal info when no file uploaded */}
-        {!status && (
-          <div class="modal-info">
-            <h5>
-              Getting Started with Batch Uploads?{' '}
-              <a class="btn btn-link m-l" href={docUrl} target="_blank">
-                View Documentation <i class="i i-external-link" />
+      {/* Show batch modal error-info when file upload */}
+      {fileUrl ? (
+        <div class="modal-info error stretch">
+          <h4 class="m-b">How to fix an error?</h4>
+          <div class="row m-t">
+            <div class="col-sm-9">
+              <p>
+                The errors are marked in a the same file in a separate column.
+                <br />
+                Download the error file, fix the errors and upload again to
+                proceed.
+              </p>
+            </div>
+            <div class="col-sm-3">
+              <a class="btn btn-primary" href={fileUrl}>
+                {' '}
+                <i class="i i-download m-r" /> Download File
               </a>
-            </h5>
-            <p>
-              Upload a batch file to continue. Please note the following things
-              before proceeding further:{' '}
-              {!shouldLoadMore && (
-                <span class="btn-link clickable" onClick={onLoadMore}>
-                  Load More
-                </span>
-              )}
-            </p>
-            {shouldLoadMore && (
-              <Fragment>
-                <ul>
-                  <li>1. The Amount mentioned should be in Paise.</li>
-                  <li>
-                    2. The receipt id for all {titleCase(batchType)}s should be
-                    unique.
-                  </li>
-                  <li>3. The number of rows should not exceed 5000.</li>
-                </ul>
-                <p>
-                  In case of any issues, please{' '}
-                  <a class="btn-link" href={sampleUrl}>
-                    download sample file
-                  </a>
-                </p>
-              </Fragment>
-            )}
-          </div>
-        )}
-
-        {/* Show batch modal error-info when file upload */}
-        {fileUrl ? (
-          <div class="modal-info error stretch">
-            <h4 class="m-b">How to fix an error?</h4>
-            <div class="row m-t">
-              <div class="col-sm-9">
-                <p>
-                  The errors are marked in a the same file in a separate column.
-                  <br />
-                  Download the error file, fix the errors and upload again to
-                  proceed.
-                </p>
-              </div>
-              <div class="col-sm-3">
-                <a class="btn btn-primary" href={fileUrl}>
-                  {' '}
-                  <i class="i i-download m-r" /> Download File
-                </a>
-              </div>
             </div>
           </div>
-        ) : null}
-      </div>
+        </div>
+      ) : null}
     </div>
   );
 }
--- web/js/merchant/containers/BatchNew/Upload.js
+++ web/js/merchant/containers/BatchNew/Upload.js
@@ -1,8 +1,12 @@
 import { Component } from 'react';
 import { connect } from 'react-redux';
+
+import ModalHeader from 'rzp/ui/ModalHeader';
+import { closeModal, openModal } from 'rzp/modules/modals';
+
 import BatchValidate from './Validate';
 import BatchCreate from './Create';
-import { closeModal, openModal } from 'rzp/modules/modals';
+
 /**
  * Container:  Switches between validation or creation of batch.
  */
@@ -10,7 +14,7 @@ import { closeModal, openModal } from 'rzp/modules/modals';
 @connect(null, { closeModal, openModal })
 export default class BatchUpload extends Component {
   state = {
-    currentState: 'validate',
+    currentStatus: 'validate',
     batch: null,
   };
 
@@ -19,34 +23,71 @@ export default class BatchUpload extends Component {
   handleValidation = batch => {
     this.setState({
       batch,
-      currentState: 'create',
+      currentStatus: 'create',
     });
   };
 
   handleCreation = batch => {
-    this.props.closeModal();
-
     this.setState({
-      batch,
-      currentState: 'success',
+      batch: { ...this.state.batch, ...batch },
+      currentStatus: 'success',
     });
   };
 
   render() {
-    //TODO: create batch.
-    return this.state.currentState === 'validate' ? (
-      <BatchValidate
-        onValidation={this.handleValidation}
-        batchType={this.props.batchType}
-        sampleUrl={this.props.sampleUrl}
-        docUrl={this.props.docUrl}
-      />
-    ) : (
-      <BatchCreate
-        onCreation={this.handleCreation}
-        batch={this.state.batch}
-        batchType={this.props.batchType}
-      />
+    return (
+      <div class={`batch-upload-modal ${this.state.currentStatus}`}>
+        <ModalHeader
+          title="Batch Upload"
+          onCloseClick={this.props.closeModal}
+        />
+        {(() => {
+          switch (this.state.currentStatus) {
+            case 'validate':
+              return (
+                <BatchValidate
+                  onValidation={this.handleValidation}
+                  batchType={this.props.batchType}
+                  sampleUrl={this.props.sampleUrl}
+                  docUrl={this.props.docUrl}
+                />
+              );
+            case 'create':
+              return (
+                <BatchCreate
+                  onCreation={this.handleCreation}
+                  batch={this.state.batch}
+                  batchType={this.props.batchType}
+                />
+              );
+            case 'success':
+              return (
+                <BatchSuccess
+                  name={this.state.batch.name}
+                  success_count={this.state.batch.success_count}
+                  total_count={this.state.batch.total_count}
+                />
+              );
+            case 'default':
+              return null;
+          }
+        })()}
+      </div>
     );
   }
 }
+
+const BatchSuccess = ({ success_count, total_count, name }) => (
+  //TODO: remove inline styles
+  <div class="modal-body">
+    The batch{' '}
+    {name ? (
+      <span>
+        titled <strong>{name}</strong>{' '}
+      </span>
+    ) : null}{' '}
+    is created succesfully. A total of {success_count} rows were processed out
+    of {total_count}. You can try processing the remaining rows by uploading a
+    new batch file.
+  </div>
+);
