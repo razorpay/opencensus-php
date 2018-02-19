755d5256e74ff46f547f62e331d15bf94db6259d
Rishabh <rishabh.pugalia@gmail.com>
2018-02-20 00:30:13 +0530

    generic service removal

--- app/Http/Controllers/GenericController.php
+++ app/Http/Controllers/GenericController.php
@@ -11,30 +11,6 @@ use App\Http\AppResponse;
 
 class GenericController extends Controller
 {
-    protected $guard = 'admin';
-
-    /*
-    |--------------------------------------------------------------------------
-    | Admin Controller
-    |--------------------------------------------------------------------------
-    |
-    | Defines the actions for an Admin on the dashboard
-    |
-    */
-
-    public function handle()
-    {
-        $method = Request::method();
-
-        $input = Input::all();
-
-        $genericService = new Generic\Service;
-
-        list($error, $data) = $genericService->call($method, $input);
-
-        return AppResponse::jsonResponse($error, $data);
-    }
-
     public function handleAny($mode, $path)
     {
         $request = new App\Admin\ApiRequestAny([
@@ -46,15 +22,5 @@ class GenericController extends Controller
         list($error, $data) = $request->send($path, $method);
 
         return AppResponse::jsonResponse($error, $data);
-    }
-
-    public function handleGuest($path = '/')
-    {
-        return $this->handleAny(null, $path);
-    }
-
-    public function handleUser($path)
-    {
-        return $this->handleAny('live', $path);
     }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -101,9 +101,7 @@ Route::group(['middleware' => ['web']], function () {
 
     Route::group(['middleware'  =>  ['admin', 'admin_access']], function()
     {
-        Route::any('/admin/generic', 'GenericController@handle');
         Route::any('/admin/stats/{id}', 'AdminController@getMerchantStats');
-        // Route::any('/admin/api/{mode}/{path}', 'GenericController@handleAny')->where(['path' => '.*'])->name('admin');
         Route::get('/admin/user', 'AdminController@getAdmin');
         Route::get('/admin/user/logout', 'AdminController@getLogout');
         Route::get('/admin/user/keepalive', 'AdminController@getKeepAlive');
