5efb3b379dbbd93d4b816d040055a61f59b01ff7
Vivek <viveklakhera@gmail.com>
2018-02-20 16:09:06 +0530

    Fix bank gateway mapping (#7566)

--- app/Models/Terminal/Filters/TransactionFilter.php
+++ app/Models/Terminal/Filters/TransactionFilter.php
@@ -94,7 +94,8 @@ class TransactionFilter extends Terminal\Filter
 
     public function bankFilter($terminal)
     {
-        if ($this->input['payment']->isNetbanking())
+        if (($this->input['payment']->isNetbanking() === true) or
+            ($this->input['payment']->isEmandate() === true))
         {
             $bank = $this->input['payment']->getBank();
 
@@ -364,7 +365,7 @@ class TransactionFilter extends Terminal\Filter
         }
 
         $emiDuration = $this->input['payment']->emiPlan->getDuration();
-        
+
         return $terminal->isValidEmiTerminal($gateway, $emiDuration);
     }
 
