bab1bb77f632f56fe4ca3ad80c50459c1bc805f1
Vivek Kumar <viveklakhera@gmail.com>
2018-02-20 15:08:07 +0530

    Feature based whitelisting for debit pin

--- app/Models/Payment/Validator.php
+++ app/Models/Payment/Validator.php
@@ -208,6 +208,15 @@ class Validator extends Base\Validator
         }
 
         AuthType::validateAuthType($input[Entity::AUTH_TYPE], $input[Entity::METHOD]);
+
+        // Feature based whitelisting for debit pin
+        if (($input[Entity::METHOD] === Method::CARD) and
+            ($input[Entity::AUTH_TYPE] === AuthType::DEBIT_PIN) and
+            ($merchant->isFeatureEnabled(Feature\Constants::DEBIT_PIN) === false))
+        {
+            throw new Exception\BadRequestValidationFailureException(
+                'The auth_type should be only present when method is E-Mandate');
+        }
     }
 
     protected function validateUpiExpiryTime(array $input)
