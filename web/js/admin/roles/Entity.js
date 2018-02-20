27b52d67ad896d35ea64e1c02487542b2ffd3296
aseemgupta <aseemgupta2625@gmail.com>
2018-02-20 23:45:55 +0530

    [Mod]: Changes in api, step 6

--- web/js/admin/roles/Entity.js
+++ web/js/admin/roles/Entity.js
@@ -135,7 +135,7 @@ export function showEntity(collection) {
 export function removeEntity(e) {
   prevent(e);
 
-  return adminDelete(`roles/${this.id}`).then(response => {
+  return adminDelete(`live/roles/${this.id}`).then(response => {
     if (response) {
       this.collection.items.remove(this);
       notifyDone();
