ab4c4633b172d85123c0ced00f7211adb70c8f96
aseemgupta <aseemgupta2625@gmail.com>
2018-02-21 01:16:00 +0530

    [Mod]: Changes in api, step 8

--- web/js/admin/merchants/entity/entityModals/EditMerchant.js
+++ web/js/admin/merchants/entity/entityModals/EditMerchant.js
@@ -3,7 +3,7 @@ import BaseModal from 'ui/BaseModal';
 import { toJS, observable } from 'mobx';
 import { observer } from 'mobx-react';
 import { closeModal, notifyError, notifySuccess } from 'common/modal';
-import { adminFetch, adminPost } from 'common/fetch';
+import fetch, { adminFetch, adminPost } from 'common/fetch';
 import { getRiskRating } from '../entity-resources';
 import ShowWhen from 'admin/components/ShowWhen';
 import Form from 'ui/Form';
@@ -97,7 +97,11 @@ export default class EditMerchant extends Component {
       return;
     }
 
-    return adminPost(body, '/admin/merchant/' + this.props.merchantId + '/edit')
+    return fetch({
+      url: '/admin/merchant/' + this.props.merchantId + '/edit',
+      method: 'post',
+      data: body,
+    })
       .then(data => {
         if (data) {
           closeModal();
