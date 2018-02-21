ea52b256b3157326895af24e6d7ce3fdc6b01dc3
Patlola <praveen.patlola@razorpay.com>
2018-02-21 11:05:51 +0000

    adding feature_onboarding_fetch_all_responses to admin proxy

--- app/Http/Route.php
+++ app/Http/Route.php
@@ -1669,6 +1669,7 @@ final class Route
         'shield_rules_delete'                    => Permission::DELETE_SHIELD_RULES,
         'shield_rules_evaluate'                  => Permission::EVALUATE_SHIELD_RULES,
         'user_fetch_admin'                       => '*',
+        'feature_onboarding_fetch_all_responses' => '*',
     ];
 
     public static $direct = [
