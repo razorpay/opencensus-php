76f28524b38f10c7a7bece8c506b7678c93cf39e
rchaubey <dev.rohit.chaubey@gmail.com>
2018-02-22 16:02:23 +0530

    [Mod] css changes

--- web/js/merchant/containers/Activation/SubmitForm.js
+++ web/js/merchant/containers/Activation/SubmitForm.js
@@ -16,7 +16,7 @@ export default class SubmitForm extends Component {
         <Fieldset disabled={locked}>
           <div class="form-group">
             <div class="col-md-offset-3 col-md-9">
-              <div class="checkbox rzpCheckbox submit-form">
+              <div class="checkbox rzpCheckbox next submit-form">
                 <Field
                   name="agree_terms"
                   id="agree_terms"
