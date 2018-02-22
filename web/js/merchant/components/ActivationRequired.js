a2d703d5ad127d3c03d18a8d03d93473530b89df
rchaubey <dev.rohit.chaubey@gmail.com>
2018-02-22 05:54:04 +0530

    [Css] activation required changes

--- web/js/merchant/components/ActivationRequired.js
+++ web/js/merchant/components/ActivationRequired.js
@@ -8,14 +8,17 @@ export default ({ onCloseClick }) => {
       <div class="modal-body">
         You can only use Razorpay in test mode until your account is activated.{' '}
         <br />
-        Please fill and submit the{' '}
-        <NavLink to="/activation" onClick={onCloseClick}>
-          <u>activation form.</u>
-        </NavLink>
+        Please fill and submit the activation form to access live mode.
         <div class="Modal__actions text-right">
-          <button class="btn btn-primary" onClick={onCloseClick}>
-            OK
-          </button>
+          <NavLink to="/activation" onClick={onCloseClick}>
+            <button class="btn btn-primary btn-block">
+              Fill Activation Form
+              <i
+                class="i i-arrow-forward pull-right"
+                style={{ paddingTop: '4px' }}
+              />
+            </button>
+          </NavLink>
         </div>
       </div>
     </div>
