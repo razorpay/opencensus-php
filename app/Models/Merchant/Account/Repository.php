10b16eb0f4d4efc02aa20904acbf75cd9509da33
Pratik Kapasi <pratik.kapasi@razorpay.com>
2018-02-23 12:25:31 +0530

    [accounts] fetches LAs

--- app/Models/Merchant/Account/Repository.php
+++ app/Models/Merchant/Account/Repository.php
@@ -2,6 +2,7 @@
 
 namespace RZP\Models\Merchant\Account;
 
+use RZP\Models\Base;
 use RZP\Models\Merchant;
 
 class Repository extends Merchant\Repository
@@ -11,4 +12,21 @@ class Repository extends Merchant\Repository
     protected $entityFetchParamRules = [
         Entity::PARENT_ID => 'sometimes|string|size:14',
     ];
+
+    public function getAccounts(string $parentId, array $input): Base\PublicCollection
+    {
+        // Send all the linked accounts. Dashboard applies a local filter.
+        $limit = 500;
+
+        $query = $this->newQuery()
+                      ->where(Entity::PARENT_ID, $parentId);
+
+        foreach ($input as $attribute => $value)
+        {
+            $query = $query->where($attribute, $value);
+        }
+
+        return $query->take($limit)
+                     ->get();
+    }
 }
