f2c89d750cb81005b2f33eafd311fb16d502876b
aseemgupta <aseemgupta2625@gmail.com>
2018-02-23 16:58:05 +0530

    [Bug]: Remove mode field if terminal mode empty

--- web/js/admin/merchants/entity/entityModals/AssignTerminal.js
+++ web/js/admin/merchants/entity/entityModals/AssignTerminal.js
@@ -92,7 +92,10 @@ export default class TerminalForm extends Component {
     let mode = body.mode;
     delete body.mode;
 
-    body.mode = body.terminal_mode || undefined;
+    if (!body.terminal_mode) {
+      delete body.mode;
+    }
+
     delete body.terminal_mode;
 
     return adminFormUpload2(
