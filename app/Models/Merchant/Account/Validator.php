c7ca88e8944c8e3fcc3b85fe8c1c75c860546f83
Pratik Kapasi <pratik.kapasi@razorpay.com>
2018-02-24 19:56:27 +0530

    [accounts] adds skip count in validator

--- app/Models/Merchant/Account/Validator.php
+++ app/Models/Merchant/Account/Validator.php
@@ -2,11 +2,12 @@
 
 namespace RZP\Models\Merchant\Account;
 
+use RZP\Base\Fetch;
 use RZP\Models\Merchant;
 
 class Validator extends Merchant\Validator
 {
-     protected static $createRules = [
+    protected static $createRules = [
          Entity::NAME                                          => 'required|alpha_space_num|max:200',
          Entity::EMAIL                                         => 'required|email',
          Entity::TNC_ACCEPTED                                  => 'required|boolean|in:1',
@@ -22,5 +23,7 @@ class Validator extends Merchant\Validator
 
     protected static $fetchRules = [
         Entity::EMAIL => 'sometimes|email',
+        Fetch::SKIP   => 'sometimes|integer',
+        Fetch::COUNT  => 'sometimes|integer',
     ];
 }
