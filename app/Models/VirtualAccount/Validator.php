cad6beeeee4b5468808244361811156ea847567c
Harman Singh <harman28@gmail.com>
2018-02-27 12:22:04 +0530

    [virtual acc] Test for receivers validator

--- app/Models/VirtualAccount/Validator.php
+++ app/Models/VirtualAccount/Validator.php
@@ -16,7 +16,7 @@ class Validator extends Base\Validator
         Entity::AMOUNT_EXPECTED                 => 'filled|integer|min:0',
         Entity::DESCRIPTION                     => 'sometimes|nullable|string|max:2048',
         Entity::CUSTOMER_ID                     => 'filled|public_id|size:19',
-        Entity::RECEIVERS                       => 'required|array',
+        Entity::RECEIVERS                       => 'required|array|custom',
         Entity::RECEIVERS . '.' . Entity::TYPES => 'present|array',
         Entity::NOTES                           => 'sometimes|notes',
     ];
@@ -26,10 +26,6 @@ class Validator extends Base\Validator
         Entity::STATUS          => 'sometimes|in:closed',
         Entity::DESCRIPTION     => 'sometimes|nullable|string|max:2048',
         Entity::NOTES           => 'sometimes|notes',
-    ];
-
-    protected static $createValidators = [
-        Entity::RECEIVERS
     ];
 
     protected static $bankAccountReceiverOptionRules = [
@@ -63,14 +59,15 @@ class Validator extends Base\Validator
         }
     }
 
-    protected function validateReceivers(array $input)
+    protected function validateReceivers(string $key, array $value, array $data)
     {
-        if (Receiver::areTypesValid($input[Entity::RECEIVERS][Entity::TYPES]) === false)
+        if ((isset($value[Entity::TYPES]) === true) and
+            (Receiver::areTypesValid($value[Entity::TYPES]) === false))
         {
             throw new Exception\BadRequestException(
                 ErrorCode::BAD_REQUEST_VIRTUAL_ACCOUNT_INVALID_RECEIVER_TYPES,
                 'receiver_type',
-                $input);
+                $data);
         }
     }
 }
