6b25867bb5bfd556321350f73462ba1877dc0240
hobbes09 <sourin.sutradhar@gmail.com>
2018-03-01 12:57:41 +0530

    [Batch] need testing before deletion - temp

--- app/Models/Batch/Processor/Base.php
+++ app/Models/Batch/Processor/Base.php
@@ -176,10 +176,6 @@ class Base extends BaseModel\Core
 
         $response += $this->getFileIdAndSignedUrl($ufh);
 
-        $this->deleteFile($this->validatedFileLocalPath);
-
-        $this->deleteFile($this->inputFileLocalPath);
-
         return $response;
     }
 
