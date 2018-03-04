9c716d588396014df11ccd527d1a32761e293089
Amit Mahbubani <amit.mahbubani@razorpay.com>
2018-03-04 22:50:55 +0530

    [gst] Fix GST state code; add name map

--- app/lib/GSTIN.php
+++ app/lib/GSTIN.php
@@ -14,43 +14,125 @@ class GSTIN
      */
     const GSTIN_REGEX = '/^(\d{2})[A-Z]{5}\d{4}[A-Z]{1}\d[Z]{1}[A-Z\d]{1}$/';
 
-    protected static $stateCodes = [
-        '01' => State::JK,
-        '02' => State::HP,
-        '03' => State::PB,
-        '04' => State::CH,
-        '05' => State::UT,
-        '06' => State::HA,
-        '07' => State::DL,
-        '08' => State::RJ,
-        '09' => State::UP,
-        '10' => State::BI,
-        '11' => State::SK,
-        '12' => State::AR,
-        '13' => State::NA,
-        '14' => State::MA,
-        '15' => State::MI,
-        '16' => State::TR,
-        '17' => State::ME,
-        '19' => State::WB,
-        '20' => State::JH,
-        '21' => State::OR,
-        '22' => State::CT,
-        '23' => State::MP,
-        '24' => State::GJ,
-        '25' => State::DD,
-        '26' => State::DN,
-        '27' => State::MH,
-        '28' => State::AP,
-        '29' => State::KA,
-        '30' => State::GO,
-        '31' => State::LD,
-        '32' => State::KE,
-        '33' => State::TN,
-        '34' => State::PO,
-        '35' => State::AN,
-        '36' => State::TG,
-        '37' => State::AD,
+    //
+    // State codes from http://www.ddvat.gov.in/docs/List%20of%20State%20Code.pdf
+    //
+    const AN = 'AN';
+    const AP = 'AP';
+    const AD = 'AD';
+    const AR = 'AR';
+    const AS = 'AS';
+    const BH = 'BH';
+    const CH = 'CH';
+    const CT = 'CT';
+    const DN = 'DN';
+    const DD = 'DD';
+    const DL = 'DL';
+    const GA = 'GA';
+    const GJ = 'GJ';
+    const HR = 'HR';
+    const HP = 'HP';
+    const JK = 'JK';
+    const JH = 'JH';
+    const KA = 'KA';
+    const KL = 'KL';
+    const LD = 'LD';
+    const MP = 'MP';
+    const MH = 'MH';
+    const MN = 'MN';
+    const ME = 'ME';
+    const MI = 'MI';
+    const NL = 'NL';
+    const OR = 'OR';
+    const PY = 'PY';
+    const PB = 'PB';
+    const RJ = 'RJ';
+    const SK = 'SK';
+    const TN = 'TN';
+    const TS = 'TS';
+    const TR = 'TR';
+    const UP = 'UP';
+    const UT = 'UT';
+    const WB = 'WB';
+
+    protected static $stateTinIdMap = [
+        '35' => self::AN,
+        '28' => self::AP,
+        '37' => self::AD,
+        '12' => self::AR,
+        '18' => self::AS,
+        '10' => self::BH,
+        '04' => self::CH,
+        '22' => self::CT,
+        '26' => self::DN,
+        '25' => self::DD,
+        '07' => self::DL,
+        '30' => self::GA,
+        '24' => self::GJ,
+        '06' => self::HR,
+        '02' => self::HP,
+        '01' => self::JK,
+        '20' => self::JH,
+        '29' => self::KA,
+        '32' => self::KL,
+        '31' => self::LD,
+        '23' => self::MP,
+        '27' => self::MH,
+        '14' => self::MN,
+        '17' => self::ME,
+        '15' => self::MI,
+        '13' => self::NL,
+        '21' => self::OR,
+        '34' => self::PY,
+        '03' => self::PB,
+        '08' => self::RJ,
+        '11' => self::SK,
+        '33' => self::TN,
+        '36' => self::TS,
+        '16' => self::TR,
+        '09' => self::UP,
+        '05' => self::UT,
+        '19' => self::WB,
+    ];
+
+    protected static $stateNames = [
+        'Andaman and Nicobar Islands' => self::AN,
+        'Andhra Pradesh'              => self::AP,
+        'Andhra Pradesh (New)'        => self::AD,
+        'Arunachal Pradesh'           => self::AR,
+        'Assam'                       => self::AS,
+        'Bihar'                       => self::BH,
+        'Chandigarh'                  => self::CH,
+        'Chattisgarh'                 => self::CT,
+        'Dadra and Nagar Haveli'      => self::DN,
+        'Daman and Diu'               => self::DD,
+        'Delhi'                       => self::DL,
+        'Goa'                         => self::GA,
+        'Gujarat'                     => self::GJ,
+        'Haryana'                     => self::HR,
+        'Himachal Pradesh'            => self::HP,
+        'Jammu and Kashmir'           => self::JK,
+        'Jharkhand'                   => self::JH,
+        'Karnataka'                   => self::KA,
+        'Kerala'                      => self::KL,
+        'Lakshadweep Islands'         => self::LD,
+        'Madhya Pradesh'              => self::MP,
+        'Maharashtra'                 => self::MH,
+        'Manipur'                     => self::MN,
+        'Meghalaya'                   => self::ME,
+        'Mizoram'                     => self::MI,
+        'Nagaland'                    => self::NL,
+        'Odisha'                      => self::OR,
+        'Pondicherry'                 => self::PY,
+        'Punjab'                      => self::PB,
+        'Rajasthan'                   => self::RJ,
+        'Sikkim'                      => self::SK,
+        'Tamil Nadu'                  => self::TN,
+        'Telangana'                   => self::TS,
+        'Tripura'                     => self::TR,
+        'Uttar Pradesh'               => self::UP,
+        'Uttarakhand'                 => self::UT,
+        'West Bengal'                 => self::WB,
     ];
 
     public static function isValid(string $gstin): bool
@@ -64,7 +146,7 @@ class GSTIN
         // state code is valid as per list in `$stateCodes`
         //
         return (($valid === 1) and
-                (array_key_exists($stateCode, static::$stateCodes) === true));
+                (array_key_exists($stateCode, static::$stateTinIdMap) === true));
     }
 
     /**
@@ -83,5 +165,19 @@ class GSTIN
                 null,
                 ['gstin' => $gstin]);
         }
+    }
+
+    public static function getStatesToTinIdMap(): array
+    {
+        $tinMap = array_flip(self::$stateTinIdMap);
+
+        $states = self::$stateNames;
+
+        array_walk($states, function(& $code, $name) use ($tinMap)
+        {
+            $code = (string) $tinMap[$code];
+        });
+
+        return $states;
     }
 }
