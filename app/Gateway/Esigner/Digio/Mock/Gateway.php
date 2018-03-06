16a075ed61a4604d797f12fef5c609cd1e2ceb5c
Vivek Kumar <viveklakhera@gmail.com>
2018-03-06 06:57:56 +0530

    Fix test cases

--- app/Gateway/Esigner/Digio/Mock/Gateway.php
+++ app/Gateway/Esigner/Digio/Mock/Gateway.php
@@ -13,4 +13,24 @@ class Gateway extends Digio\Gateway
     {
         return $this->authorizeMock($input, 'mock_esigner_payment');
     }
+
+    protected function putMockPaymentGatewayUrl(array & $request, $route)
+    {
+        $route = 'mock_esigner_payment';
+
+        $url = $this->route->getUrlWithPublicAuth($route, ['signer' => 'digio']);
+
+        // The key thing now is to replace the url from gateway to our mock one!
+        $parts = parse_url($request['url']);
+
+        // To handle the URL fragment
+        if (isset($parts['fragment']) === true)
+        {
+            $fragmentParts = explode('?', $parts['fragment']);
+
+            $url .= '&' . $fragmentParts['1'];
+        }
+
+        $request['url'] = $url;
+    }
 }
