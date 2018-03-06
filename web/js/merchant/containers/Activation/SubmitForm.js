b1b0f6f100c6631fa292869b6104f3e888a0b4d8
rchaubey <dev.rohit.chaubey@gmail.com>
2018-03-06 14:48:08 +0530

    [Mod] allow url validation to be case sensitive

--- web/js/merchant/containers/Activation/SubmitForm.js
+++ web/js/merchant/containers/Activation/SubmitForm.js
@@ -22,7 +22,7 @@ export default class SubmitForm extends Component {
                   id="agree_terms"
                   component="input"
                   type="checkbox"
-                  disabled={!!submitted || invalid}
+                  disabled={!!submitted}
                   validate={value => {
                     if (!value) {
                       return 'Required';
--- web/js/rzp/utils/rzp-utils.js
+++ web/js/rzp/utils/rzp-utils.js
@@ -409,7 +409,7 @@ export const arrayToCsvDataUrl = array => {
 };
 
 /**
- * 
+ *
  * @param {*} url
  * Add 'http' to the URL is not available
  */
@@ -423,9 +423,9 @@ export const autoPrefixUrls = url => {
   tempUrl = url.toLowerCase();
 
   if (!regex.test(tempUrl)) {
-    tempUrl = 'http://' + tempUrl;
+    url = 'http://' + url;
   }
-  return tempUrl;
+  return url;
 };
 
 export { acronyms, shortenText };
