cb09919ae06d884b095abf003e4f1c2e10ad120a
hobbes09 <sourin.sutradhar@gmail.com>
2018-03-07 20:35:17 +0530

    [Batch] batch stats endpoint changed

--- tests/Functional/Invoice/Helpers/InvoiceTestData.php
+++ tests/Functional/Invoice/Helpers/InvoiceTestData.php
@@ -3631,7 +3631,7 @@ return [
 
     'testInvoiceStatsByBatch' => [
         'request' => [
-            'url'     => '/batch/batch_00000000000001/stats',
+            'url'     => '/batches/batch_00000000000001/stats',
             'method'  => 'get',
         ],
         'response' => [
