ba9dbb5242128b7e28b08f5e05d2e10a40d587cd
Harman Singh <harman28@gmail.com>
2018-03-08 18:28:42 +0530

    [virtual acc] VA credited webhook

--- app/Models/Payment/Processor/Capture.php
+++ app/Models/Payment/Processor/Capture.php
@@ -522,6 +522,8 @@ trait Capture
         $this->eventOrderPaid();
 
         $this->eventInvoicePaid();
+
+        $this->eventVirtualAccountCredited();
     }
 
     /**
@@ -597,6 +599,22 @@ trait Capture
         ];
 
         $this->app['events']->fire($event, $eventPayload);
+    }
+
+    protected function eventVirtualAccountCredited()
+    {
+        $payment = $this->payment;
+
+        if ($payment->isBankTransfer() === false)
+        {
+            return;
+        }
+
+        $eventPayload = [
+            ApiEventSubscriber::MAIN => $payment
+        ];
+
+        $this->app['events']->fire('api.virtual_account.credited', $eventPayload);
     }
 
     protected function eventPaymentCaptured()
