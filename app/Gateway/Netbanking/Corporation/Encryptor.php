6816535ef001cea30f9d3e1caf1464e58981cf57
Albin George <albinin0002@gmail.com>
2018-03-08 17:16:10 +0530

    [bob_nb] Set keylength to 256 for AES

--- app/Gateway/Netbanking/Corporation/Encryptor.php
+++ app/Gateway/Netbanking/Corporation/Encryptor.php
@@ -9,6 +9,13 @@ class Encryptor extends AESCrypto
     const KEY_VALUE_SEPARATOR = '~';
     const PAIRS_SEPARATOR     = '`';
 
+    public function __construct(int $mode, string $masterKey, string $initializationVector = '')
+    {
+        parent::__construct($mode, $masterKey, $initializationVector);
+
+        $this->aes->setKeyLength(256);
+    }
+
     public function encryptData(array $data)
     {
         $encoded = [];
