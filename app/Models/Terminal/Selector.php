0545f37f5f7c8860497a49ad6e37d4c090bb41f3
Prashanth Y.V <prashanth.yv@razorpay.com>
2018-03-11 16:27:24 +0530

    [terminals] returns empty extra set of terminals if no gateway tokens present

--- app/Models/Terminal/Selector.php
+++ app/Models/Terminal/Selector.php
@@ -225,6 +225,11 @@ class Selector extends Base\Core
     {
         $gatewayTokens = $this->input['gateway_tokens'];
 
+        if ($gatewayTokens->count() === 0)
+        {
+            return [];
+        }
+
         $merchantIdsForGatewayTokenTerminals = $gatewayTokens->pluck('terminal.merchant_id')
                                                              ->toArray();
 
