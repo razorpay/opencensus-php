f0a942203d2a79d99fe198d1323aeeef393e0405
rzpamidi <prashanth.pamidi@razorpay.com>
2018-03-13 18:55:56 +0530

    handled max

--- web/js/admin/plans/plan.js
+++ web/js/admin/plans/plan.js
@@ -112,7 +112,7 @@ export const options = {
     '0-200000': '0-200000',
     '200000-1000000000': '200000-1000000000',
     '0-10000000': '0 - 1 lac',
-    '10000000-1000000000': '1 lac+'
+    '10000000-': '1 lac+'
   },
   emi_duration: {
     '': 'All',
@@ -158,7 +158,8 @@ class Rule extends CollectionItem {
       let range = data.amount_range.split('-');
       data.amount_range_active = 1;
       data.amount_range_min = range[0];
-      data.amount_range_max = range[1];
+      // if the max not specified its 2cr
+      data.amount_range_max = range[1] || 2000000000;
     }
     delete data.amount_range;
 
