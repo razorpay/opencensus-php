c7b3fdd6042874575aefa0882b6a5a297d194c3b
surya15t <surya.teja@razorpay.com>
2018-03-16 16:45:51 +0530

    [merchant][admin] remove bank beneficiary address fields from activation form

--- app/Models/BankAccount/Core.php
+++ app/Models/BankAccount/Core.php
@@ -266,13 +266,6 @@ class Core extends Base\Core
             Entity::IFSC_CODE             => $details[DetailEntity::BANK_BRANCH_IFSC],
             Entity::BENEFICIARY_NAME      => $details[DetailEntity::BANK_ACCOUNT_NAME],
             Entity::ACCOUNT_NUMBER        => $details[DetailEntity::BANK_ACCOUNT_NUMBER],
-            Entity::BENEFICIARY_ADDRESS1  => $details[DetailEntity::BANK_BENEFICIARY_ADDRESS1],
-            Entity::BENEFICIARY_ADDRESS2  => $details[DetailEntity::BANK_BENEFICIARY_ADDRESS2],
-            Entity::BENEFICIARY_ADDRESS3  => $details[DetailEntity::BANK_BENEFICIARY_ADDRESS3],
-            Entity::BENEFICIARY_ADDRESS4  => '',
-            Entity::BENEFICIARY_PIN       => $details[DetailEntity::BANK_BENEFICIARY_PIN],
-            Entity::BENEFICIARY_CITY      => $details[DetailEntity::BANK_BENEFICIARY_CITY],
-            Entity::BENEFICIARY_STATE     => $details[DetailEntity::BANK_BENEFICIARY_STATE],
             Entity::BENEFICIARY_COUNTRY   => 'IN',
             Entity::BENEFICIARY_EMAIL     => $details[DetailEntity::CONTACT_EMAIL],
             Entity::BENEFICIARY_MOBILE    => $details[DetailEntity::CONTACT_MOBILE],
@@ -285,10 +278,6 @@ class Core extends Base\Core
         //
         if ($linkedAccount === true)
         {
-            $data[Entity::BENEFICIARY_ADDRESS1] = 'Bangalore';
-            $data[Entity::BENEFICIARY_CITY]     = 'Bangalore';
-            $data[Entity::BENEFICIARY_STATE]    = 'KA';
-            $data[Entity::BENEFICIARY_PIN]      = 560001;
             $data[Entity::BENEFICIARY_MOBILE]   = 9999999999;
         }
 
@@ -301,12 +290,6 @@ class Core extends Base\Core
             Detail\Entity::BANK_BRANCH_IFSC          => $input[Entity::IFSC_CODE],
             Detail\Entity::BANK_ACCOUNT_NUMBER       => $input[Entity::ACCOUNT_NUMBER],
             Detail\Entity::BANK_ACCOUNT_NAME         => $input[Entity::BENEFICIARY_NAME],
-            Detail\Entity::BANK_BENEFICIARY_ADDRESS1 => $input[Entity::BENEFICIARY_ADDRESS1],
-            Detail\Entity::BANK_BENEFICIARY_ADDRESS2 => $input[Entity::BENEFICIARY_ADDRESS2] ?? null,
-            Detail\Entity::BANK_BENEFICIARY_ADDRESS3 => $input[Entity::BENEFICIARY_ADDRESS3] ?? null,
-            Detail\Entity::BANK_BENEFICIARY_CITY     => $input[Entity::BENEFICIARY_CITY],
-            Detail\Entity::BANK_BENEFICIARY_STATE    => $input[Entity::BENEFICIARY_STATE],
-            Detail\Entity::BANK_BENEFICIARY_PIN      => $input[Entity::BENEFICIARY_PIN],
         ];
 
         return $detail;
