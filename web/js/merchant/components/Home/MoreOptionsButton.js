fa2267b5ae28560ecf8219ed6b6bbda15e6888fb
rzpamidi <prashanth.pamidi@razorpay.com>
2018-03-26 10:53:07 +0530

    Changes the download image icon

--- web/js/merchant/components/Home/MoreOptionsButton.js
+++ web/js/merchant/components/Home/MoreOptionsButton.js
@@ -13,9 +13,7 @@ export default ({
     <Dropdown className="dropdown-toggle">
       <DropdownTrigger>
         <button className="btn btn-default" onClick={handleClick}>
-          <svg xmlns="http://www.w3.org/2000/svg">
-            <path d="M2 0C.9 0 0 .9 0 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm12 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zM8 0C6.9 0 6 .9 6 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z" />
-          </svg>
+          <i className="i i-download" />
         </button>
       </DropdownTrigger>
       <DropdownContent>
