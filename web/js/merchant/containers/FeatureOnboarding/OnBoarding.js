12362916bebc9c97c132da3b23694ce3702a7224
rchaubey <dev.rohit.chaubey@gmail.com>
2018-03-27 19:04:56 +0530

    [mod] onboarding activation routes changes

--- web/js/merchant/containers/FeatureOnboarding/OnBoarding.js
+++ web/js/merchant/containers/FeatureOnboarding/OnBoarding.js
@@ -43,7 +43,7 @@ export default class OnBoarding extends Component {
     this.props
       .getOnboardingResponse(this.props.formType)
       .then(response => {
-        const submitted = response.data && !(response.data instanceof Array);
+        const submitted = !!response.data.id;
 
         this.setState({
           isLoading: false,
@@ -83,10 +83,10 @@ export default class OnBoarding extends Component {
         const { website_checkbox, ...rest } = props;
         props = rest;
       }
-      if(props.website_details) {
+      if (props.website_details) {
         props.website_details = autoPrefixUrls(props.website_details);
       }
-      
+
       return this.props
         .saveOnboarding(this.props.formType, props, file, fileName)
         .then(() => {
