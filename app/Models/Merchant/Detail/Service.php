e46c54e04b6d8caf7d7381b411d37d5704b4fa7d
Mohak Kataria <mohak.kataria@razorpay.com>
2018-03-29 16:32:41 +0530

    Fixes for Product Activations

--- app/Models/Merchant/Detail/Service.php
+++ app/Models/Merchant/Detail/Service.php
@@ -118,12 +118,7 @@ class Service extends Base\Service
             $merchantDetails->getValidator()->validateFileType($value);
 
             // Adding a prefix hash for filename to avoid overwrites to the same fileName on S3.
-            $partial = substr(
-                strtr(
-                    bin2hex(random_bytes(6)),
-                    ['+' => '', '/' => '', '=' => '']),
-                0,
-                3);
+            $partial = substr(bin2hex(random_bytes(6)), 0, 5);
 
             $fileName = 'api/' . $merchant->getId() .'/' . $partial . '/' . $key;
 
@@ -543,7 +538,7 @@ class Service extends Base\Service
 
         $permission = $this->repo
                             ->permission
-                            ->findByOrgIdAndPermission($orgId, Admin\Permission\Name::REVIEW_MERCHANT_ACTIVATION);
+                            ->findByOrgIdAndPermission($orgId, Admin\Permission\Name::EDIT_ACTIVATE_MERCHANT);
 
         if (empty($permission) === true)
         {
