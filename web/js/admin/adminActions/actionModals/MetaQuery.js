c5593b5104cdc9efe4140590dc8aa9875cdf25ce
rchaubey <dev.rohit.chaubey@gmail.com>
2018-04-03 20:22:54 +0530

    [mod] handle `create table` query requests

--- web/js/admin/adminActions/actionModals/MetaQuery.js
+++ web/js/admin/adminActions/actionModals/MetaQuery.js
@@ -1,79 +1,83 @@
-import React, { Component } from 'react';
+import React, { Fragment } from 'react';
 
 import Form from 'ui/Form';
+import BaseModal from 'ui/BaseModal';
 import AsyncButton from 'ui/AsyncButton';
 import { TextAreaField, SelectMode } from 'ui/Field';
-import { notifySuccess, closeModal } from 'common/modal';
+import { notifySuccess, closeModal, openModal } from 'common/modal';
 
 import { adminPost } from 'common/fetch';
 
-export default class MetaQuery extends Component {
-  static title = 'Meta Query';
-  static permission = 'db_meta_query';
+MetaQuery.title = 'DB Meta Query';
+MetaQuery.permission = 'db_meta_query';
 
-  state = {
-    queryDump: null,
-  };
+export default function MetaQuery() {
+  return (
+    <Form class="full-elements full-span">
+      <TextAreaField
+        label="Query"
+        name="query"
+        helpMsg={
+          <span>
+            Enter a valid db meta query, currently only the following queries
+            with prefixes are allowed
+            <ul style={listStyle}>
+              <li>explain select</li>
+              <li>show create table</li>
+              <li>show indexes from</li>
+            </ul>
+          </span>
+        }
+      />
+      <SelectMode />
+      <AsyncButton
+        text="Execute"
+        class="btn"
+        pendingClass="small spinner"
+        onSubmit={body => {
+          let mode = body.mode;
 
-  render() {
-    return (
-      <Form class="full-elements full-span">
-        <TextAreaField
-          label="Query"
-          name="query"
-          helpMsg={
-            <span>
-              Enter a valid db meta query, currently only the following queries
-              with prefixes are allowed
-              <ul style={listStyle}>
-                <li>explain select</li>
-                <li>show create table</li>
-                <li>show indexes from</li>
-              </ul>
-            </span>
-          }
-        />
-        <SelectMode />
-        <AsyncButton
-          text="Execute"
-          class="btn"
-          pendingClass="small spinner"
-          onSubmit={body => {
-            let mode = body.mode;
+          delete body.mode;
 
-            delete body.mode;
-
-            this.setState({
-              queryDump: null,
-            });
-
-            return adminPost({
-              url: `${mode}/db_meta_query`,
-              data: body,
-            }).then(response => {
-              if (response) {
-                this.setState({
-                  queryDump: response,
-                });
-                notifySuccess('Query executed successfully.');
-              }
-            });
-          }}
-        />
-        <br />
-        <br />
-        {this.state.queryDump && (
-          <div>
-            Raw Data:
-            <div class="code">
-              {JSON.stringify(this.state.queryDump, null, 4)}
-            </div>
-          </div>
-        )}
-      </Form>
-    );
-  }
+          return adminPost({
+            url: `${mode}/db_meta_query`,
+            data: body,
+          }).then(response => {
+            if (response) {
+              openQueryModal(response);
+              notifySuccess('Query executed successfully.');
+            }
+          });
+        }}
+      />
+    </Form>
+  );
 }
+
+const openQueryModal = queryDump => {
+  const isCreateTableQuery =
+    !!(queryDump && queryDump[0]['Create Table']) || false;
+
+  openModal(
+    <BaseModal header="Response">
+      <pre class="code" style={{ width: '650px' }}>
+        {isCreateTableQuery ? (
+          <Fragment>
+            {queryDump.map(query => (
+              <div>
+                Table: {query['Table']}
+                <br />
+                {query['Create Table']}
+              </div>
+            ))}
+          </Fragment>
+        ) : (
+          JSON.stringify(queryDump, null, 4)
+        )}
+      </pre>
+    </BaseModal>
+  );
+};
 
 const listStyle = {
   marginLeft: '25px',
