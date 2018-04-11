07267e2008eb43fb3a76509bac47f75d07bb3f28
Albin George <albinin0002@gmail.com>
2018-04-11 18:15:55 +0530

    [csb] Fix tests

--- tests/Functional/Gateway/Netbanking/Csb/NetbankingCsbGatewayTest.php
+++ tests/Functional/Gateway/Netbanking/Csb/NetbankingCsbGatewayTest.php
@@ -155,34 +155,6 @@ class NetbankingCsbGatewayTest extends TestCase
         $this->assertEquals(Payment\Status::CAPTURED, $payment[Payment\Entity::STATUS]);
     }
 
-    public function testPaymentFailedVerify()
-    {
-        $payment = $this->testPaymentFailed();
-
-        $data = $this->testData['testVerifyMismatch'];
-
-        $this->runRequestResponseFlow(
-            $data,
-            function() use ($payment)
-            {
-                $this->verifyPayment($payment[Payment\Entity::ID]);
-            });
-
-        $payment = $this->getLastEntity(ConstantsEntity::PAYMENT, true);
-
-        $this->assertEquals(Payment\Status::FAILED, $payment[Payment\Entity::STATUS]);
-        $this->assertEquals(VerifyStatus::FAILED, $payment[Payment\Entity::VERIFIED]);
-
-        $netbanking = $this->getLastEntity(ConstantsEntity::NETBANKING, true);
-
-        $testData = $this->testData['testPaymentFailedNetbankingEntity'];
-
-        // The status changes from 'N' to 'Y' after verification
-        $testData['status'] = Status::SUCCESS;
-
-        $this->assertArraySelectiveEquals($testData, $netbanking);
-    }
-
     public function testPaymentFailedVerifyFailed()
     {
         $payment = $this->testPaymentFailed();
@@ -238,8 +210,15 @@ class NetbankingCsbGatewayTest extends TestCase
         $this->mockServerContentFunction(
             function(& $content, $action = null)
             {
-                $content[ResponseFields::STATUS] = Status::FAILURE;
-                $content[ResponseFields::NARRATION] = 'Payment failed';
+                if ($action === 'authorize')
+                {
+                    $content[ResponseFields::STATUS] = Status::FAILURE;
+                    $content[ResponseFields::NARRATION] = 'Payment failed';
+                }
+                else if ($action === 'verify')
+                {
+                    $content[ResponseFields::VERIFICATION] = Status::FAILURE;
+                }
             });
     }
 
