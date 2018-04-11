3e27d5ceabefe571467688cc41e3aec14c149f42
Albin George <albinin0002@gmail.com>
2018-04-11 19:27:24 +0530

    [aeps] Add live URL

--- app/Gateway/Netbanking/Csb/Url.php
+++ app/Gateway/Netbanking/Csb/Url.php
@@ -5,7 +5,7 @@ namespace RZP\Gateway\Netbanking\Csb;
 class Url
 {
     const TEST_DOMAIN = 'http://203.197.151.38/newibanking';
-    const LIVE_DOMAIN = 'https://to-be-filled-url.com';
+    const LIVE_DOMAIN = 'https://www.csbnet.co.in';
 
     const AUTHORIZE   = '/BkPgEmVantageIntf.aspx';
     const VERIFY      = '/BkPgVrfyEmVantageIntf.aspx';
