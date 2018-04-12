f11cb5afd359da8170f586d44146f68f094ce7b7
tessie <tessyjoseph1992@gmail.com>
2018-04-12 12:22:22 +0530

    Minor code improvements

--- app/Gateway/Netbanking/Obc/Url.php
+++ app/Gateway/Netbanking/Obc/Url.php
@@ -5,6 +5,7 @@ namespace RZP\Gateway\Netbanking\Obc;
 final class Url
 {
     const TEST_DOMAIN = 'http://220.226.198.27/PreProdcorp/';
+
     const LIVE_DOMAIN = 'https://www.obconline.co.in/corp/';
 
     const AUTHORIZE   = '/AuthenticationController?FORMSGROUP_ID__=AuthenticationFG&__START_TRAN_FLAG__=Y&FG_BUTTONS__=LOAD&ACTION.LOAD=Y&AuthenticationFG.LOGIN_FLAG=1&BANK_ID=022&AuthenticationFG.USER_TYPE=1&AuthenticationFG.MENU_ID=CIMSHP&AuthenticationFG.CALL_MODE=2';
