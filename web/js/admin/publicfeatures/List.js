c3ac96fd543724017e4a66155a31d6858b20a8d7
rchaubey <dev.rohit.chaubey@gmail.com>
2018-04-13 14:20:18 +0530

    [mod] fields changes

--- web/js/admin/publicfeatures/List.js
+++ web/js/admin/publicfeatures/List.js
@@ -8,7 +8,7 @@ import Collection from 'model/collection';
 import { adminFetch } from 'common/fetch';
 import { showEntity } from './Entity';
 import { statusPill, publicFeature } from 'common/data';
-import { snakeToTitleCase } from 'common/util';
+import { snakeToTitleCase, prevent, formatDate } from 'common/util';
 
 const defaultFilters = {
   status: '',
@@ -41,8 +41,40 @@ export default class PublicFeaturesList extends Component {
     });
   };
 
+  fields = [
+    [
+      'Merchant ID',
+      item => (
+        <span
+          class="link"
+          onClick={this._openMerchantDetails}
+          data-merchantid={item.merchant.id}
+        >
+          {item.merchant.id}
+        </span>
+      ),
+    ],
+    ['Merchant Name', item => item.merchant.name],
+    ['Product', item => item.name],
+    [
+      'Account Activation Status',
+      item => (item.merchant.activated ? 'Activated' : 'Not Activated'),
+    ],
+    ['Product Activation Status', item => statusPill(item.status)],
+    ['Submitted At', item => formatDate(item.created_at)],
+  ];
+
   onSubmit = filters => {
     return this.collection.applyFilters(filters);
+  };
+
+  _openMerchantDetails = event => {
+    //prevent default row click for merchant id links
+    prevent(event);
+
+    const merchantId = event.target.dataset.merchantid;
+
+    window.open(`/admin/merchants/${merchantId}`, '_blank');
   };
 
   render() {
@@ -80,21 +112,10 @@ export default class PublicFeaturesList extends Component {
         </div>
         <PageTable
           model={this.collection}
-          fields={fields}
+          fields={this.fields}
           onClick={showEntity}
         />
       </div>
     );
   }
 }
-
-const fields = [
-  ['Merchant ID', item => item.merchant.id],
-  ['Merchant Name', item => item.merchant.name],
-  ['Product', item => item.name],
-  [
-    'Account Activation Status',
-    item => (item.merchant.activated ? 'Activated' : 'Not Activated'),
-  ],
-  ['Product Activation Status', item => statusPill(item.status)],
-];
