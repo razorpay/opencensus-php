85f428aa4b45cd71a50e9471d351b2bd774bc908
Sunny Aggrawal <sunnyagg@outlook.com>
2018-04-16 11:05:59 +0530

    [refunds] adds error code for refunds older than 6 months in fisrt data

--- app/Error/ErrorCode.php
+++ app/Error/ErrorCode.php
@@ -412,6 +412,7 @@ class ErrorCode
     const BAD_REQUEST_REFUND_FAILED                                                 = 'BAD_REQUEST_REFUND_FAILED';
     const BAD_REQUEST_REFUND_NOT_ENOUGH_BALANCE                                     = 'BAD_REQUEST_REFUND_NOT_ENOUGH_BALANCE';
     const BAD_REQUEST_REFUND_NOT_ENOUGH_CREDITS                                     = 'BAD_REQUEST_REFUND_NOT_ENOUGH_CREDITS';
+    const BAD_REQUEST_REFUND_PAYMENT_OLDER_THAN_SIX_MONTHS                          = 'BAD_REQUEST_REFUND_PAYMENT_OLDER_THAN_SIX_MONTHS';
 
     const BAD_REQUEST_PAYOUT_NOT_ENOUGH_BALANCE                                     = 'BAD_REQUEST_PAYOUT_NOT_ENOUGH_BALANCE';
     const BAD_REQUEST_MERCHANT_FUNDS_ON_HOLD                                        = 'BAD_REQUEST_MERCHANT_FUNDS_ON_HOLD';
