7361f8893cc5c982f6be26a85579c14ce400216d
Vivek <viveklakhera@gmail.com>
2018-04-18 07:12:52 +0530

    Remove PUNB from self list in favor of PUNB_R (#8041)
    * Remove PUNB from self list in favor of PUNB_R
    
    * Fix strict check
    
    * Fix test cases

--- tests/Functional/Merchant/MethodsTest.php
+++ tests/Functional/Merchant/MethodsTest.php
@@ -42,7 +42,7 @@ class MethodsTest extends TestCase
 
         $count = count($content['netbanking']);
 
-        $this->assertEquals(62, $count);
+        $this->assertEquals(61, $count);
 
         $this->assertArrayNotHasKey('recurring', $content);
     }
@@ -68,7 +68,7 @@ class MethodsTest extends TestCase
 
         $count = count($content['netbanking']);
 
-        $this->assertEquals(62, $count);
+        $this->assertEquals(61, $count);
     }
 
     public function testBulkMethodUpdate()
