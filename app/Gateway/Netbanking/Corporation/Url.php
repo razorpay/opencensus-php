ac9e642dc086196955b308f5defa66642879d813
Albin George <albinin0002@gmail.com>
2018-04-19 15:51:21 +0530

    [corporation] New UAT credentials

--- app/Gateway/Netbanking/Corporation/Url.php
+++ app/Gateway/Netbanking/Corporation/Url.php
@@ -6,8 +6,8 @@ class Url
 {
     // TODO: Add live domain later
     const LIVE_DOMAIN   = '';
-    const TEST_DOMAIN   = 'http://210.212.195.74:7777';
+    const TEST_DOMAIN   = 'http://172.22.2.11:7003';
 
-    const AUTHORIZE     = '/RetailBank/OLT';
-    const VERIFY        = '/RetailBank/OLTDVER';
+    const AUTHORIZE     = '/corp/OLT';
+    const VERIFY        = '/corp/OLTDVER';
 }
