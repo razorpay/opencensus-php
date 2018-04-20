ca3153a60cbd78fad5d143ad75e3a7ee31e802cc
Albin George <albinin0002@gmail.com>
2018-04-20 13:30:50 +0530

    [corp] Correct UAT URL

--- app/Gateway/Netbanking/Corporation/Url.php
+++ app/Gateway/Netbanking/Corporation/Url.php
@@ -6,7 +6,7 @@ class Url
 {
     // TODO: Add live domain later
     const LIVE_DOMAIN   = '';
-    const TEST_DOMAIN   = 'http://172.22.2.11:7003';
+    const TEST_DOMAIN   = 'http://202.62.247.116';
 
     const AUTHORIZE     = '/corp/OLT';
     const VERIFY        = '/corp/OLTDVER';
