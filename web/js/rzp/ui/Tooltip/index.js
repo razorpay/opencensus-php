855dd47f656e155b00ed4fd6af191d51ab7ab712
rzpamidi <prashanth.pamidi@razorpay.com>
2018-04-24 15:26:23 +0530

    Fixes auto alignment bug

--- web/js/rzp/ui/Tooltip/index.js
+++ web/js/rzp/ui/Tooltip/index.js
@@ -6,7 +6,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const gutter = 10,
+const DEFAULT_OFFSET = 10,
   TOOLTIP_DELAY = 200;
 
 class Tooltip extends Component {
@@ -26,38 +26,35 @@ class Tooltip extends Component {
     this.handleMouseMove = this.handleMouseMove.bind(this);
   }
 
-  showTooltip(data = {}) {
+  getDimensions(data) {
     let { screenX, screenY } = data;
 
     const node = this.node,
       parent = node.parentElement,
-      align = data.align || this.props.align;
+      align = data.align || this.props.align,
+      offset = this.props.offset;
 
-    let width = 0,
-      height = 0,
-      left = 0,
-      top = 0;
+    let parentWidth = 0,
+      parentHeight = 0,
+      parentLeft = 0,
+      parentTop = 0;
 
     const boundingRect = parent.getBoundingClientRect();
 
     if (!this.props.followPointer) {
-      width = boundingRect.width;
-      height = boundingRect.height;
-      left = boundingRect.left;
-      top = boundingRect.top;
+      parentWidth = boundingRect.width;
+      parentHeight = boundingRect.height;
+      parentLeft = boundingRect.left;
+      parentTop = boundingRect.top;
     } else {
-      left = screenX;
-      top = screenY;
+      parentLeft = screenX;
+      parentTop = screenY;
     }
 
     let tooltipWidth =
         this.nodeWidth || (this.nodeWidth = this.node.clientWidth),
       tooltipHeight =
-        this.nodeHeight || (this.nodeHeight = this.node.clientHeight),
-      screenTop = 0,
-      screenLeft = 0,
-      screenRight = window.innerWidth,
-      screenBottom = window.innerHeight;
+        this.nodeHeight || (this.nodeHeight = this.node.clientHeight);
 
     let tooltipLeft = 0,
       tooltipTop = 0,
@@ -67,31 +64,62 @@ class Tooltip extends Component {
       paddingRight = 0;
 
     if (align === 'bottom' || align === 'top') {
-      tooltipLeft = left + width / 2 - tooltipWidth / 2;
+      tooltipLeft = parentLeft + parentWidth / 2 - tooltipWidth / 2;
 
-      tooltipTop = top;
+      tooltipTop = parentTop;
 
       if (align === 'bottom') {
-        tooltipTop += height;
-        paddingTop = gutter;
+        tooltipTop += parentHeight;
+        paddingTop = offset;
       } else {
         tooltipTop -= tooltipHeight;
-        tooltipTop -= gutter;
-        paddingBottom = gutter;
+        paddingBottom = offset;
       }
     } else if (align === 'right' || align === 'left') {
-      tooltipLeft = left;
+      tooltipLeft = parentLeft;
 
-      tooltipTop = top + height / 2 - tooltipHeight / 2;
+      tooltipTop = parentTop + parentHeight / 2 - tooltipHeight / 2;
 
       if (align === 'right') {
-        tooltipLeft += width;
-        paddingLeft = gutter;
+        tooltipLeft += parentWidth;
+        paddingLeft = offset;
       } else {
         tooltipLeft -= tooltipWidth;
-        paddingRight = gutter;
+        paddingRight = offset;
       }
     }
+
+    return {
+      tooltipTop,
+      tooltipLeft,
+      tooltipWidth,
+      tooltipHeight,
+      paddingLeft,
+      paddingRight,
+      paddingTop,
+      paddingBottom,
+    };
+  }
+
+  showTooltip(data = {}) {
+    const node = this.node,
+      align = data.align || this.props.align;
+
+    const screenTop = 0,
+      screenLeft = 0,
+      screenRight = window.innerWidth,
+      screenBottom = window.innerHeight;
+
+    let {
+      tooltipTop,
+      tooltipLeft,
+      tooltipWidth,
+      tooltipHeight,
+      paddingLeft,
+      paddingRight,
+      paddingTop,
+      paddingBottom,
+    } = this.getDimensions(data);
 
     let horizontalAdjustment = 0,
       verticalAdjustment = 0;
@@ -117,18 +145,28 @@ class Tooltip extends Component {
         if (horizontalAdjustment) {
           const isLeftAdjustment = horizontalAdjustment > 0;
 
+          let { tooltipLeft: tooltipLeftOnRightAlignment } = this.getDimensions(
+            { ...data, align: 'right' }
+          );
+
           if (
             isLeftAdjustment &&
-            horizontalAdjustment + tooltipLeft + tooltipWidth <= screenRight
+            horizontalAdjustment + tooltipLeftOnRightAlignment + tooltipWidth <=
+              screenRight
           ) {
-            return this.changeAlignment('right');
+            return this.changeAlignment(data, 'right');
           }
+
+          let { tooltipLeft: tooltipLeftOnLeftAlignment } = this.getDimensions({
+            ...data,
+            align: 'left',
+          });
 
           if (
             !isLeftAdjustment &&
-            tooltipLeft + horizontalAdjustment >= screenLeft
+            tooltipLeftOnLeftAlignment + horizontalAdjustment >= screenLeft
           ) {
-            return this.changeAlignment('left');
+            return this.changeAlignment(data, 'left');
           }
         }
       } else if (align === 'top' || align === 'bottom') {
@@ -137,18 +175,29 @@ class Tooltip extends Component {
         if (verticalAdjustment) {
           const isTopAdjustment = verticalAdjustment > 0;
 
+          let { tooltipTop: tooltipTopOnBottomAlignment } = this.getDimensions({
+            ...data,
+            align: 'bottom',
+          });
+
           if (
             isTopAdjustment &&
-            verticalAdjustment + tooltipTop + tooltipHeight <= screenBottom
+            verticalAdjustment + tooltipTopOnBottomAlignment + tooltipHeight <=
+              screenBottom
           ) {
-            return this.changeAlignment('bottom');
+            return this.changeAlignment(data, 'bottom');
           }
+
+          let { tooltipTop: tooltipTopOnTopAlignment } = this.getDimensions({
+            ...data,
+            align: 'top',
+          });
 
           if (
             !isTopAdjustment &&
-            verticalAdjustment + tooltipTop >= screenTop
+            verticalAdjustment + tooltipTopOnTopAlignment >= screenTop
           ) {
-            return this.changeAlignment('top');
+            return this.changeAlignment(data, 'top');
           }
         }
       }
@@ -159,7 +208,9 @@ class Tooltip extends Component {
     }
 
     if (!this.props.followPointer) {
-      node.style.top = tooltipTop + 'px';
+      // when paddingBottom is present, top needs to be adjusted as
+      // the box grows down
+      node.style.top = tooltipTop - paddingBottom + 'px';
       node.style.left = tooltipLeft + 'px';
       node.style.paddingLeft = paddingLeft + 'px';
       node.style.paddingTop = paddingTop + 'px';
@@ -175,7 +226,7 @@ class Tooltip extends Component {
     });
   }
 
-  changeAlignment(resultantAlignment) {
+  changeAlignment(data, resultantAlignment) {
     this.showTooltip({ align: resultantAlignment });
 
     return (
@@ -284,6 +335,7 @@ class Tooltip extends Component {
         children,
         align,
         delay,
+        offset,
         followPointer,
         persistent,
         onAdjustment,
@@ -308,6 +360,7 @@ Tooltip.defaultProps = {
   delay: TOOLTIP_DELAY,
   followPointer: false,
   persistent: false,
+  offset: DEFAULT_OFFSET,
 };
 
 Tooltip.propTypes = {
@@ -315,6 +368,7 @@ Tooltip.propTypes = {
   followPointer: PropTypes.bool.isRequired,
   persistent: PropTypes.bool.isRequired,
   delay: PropTypes.number.isRequired,
+  offset: PropTypes.number.isRequired,
   onAdjustment: PropTypes.func,
   onAlignmentChange: PropTypes.func,
 };
