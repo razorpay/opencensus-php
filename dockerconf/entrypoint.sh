035c919169b93ec0bf9d25282ee909cf9a20c5c8
Aseem Gupta <aseem2625@users.noreply.github.com>
2018-04-25 19:27:55 +0530

    Update entrypoint.sh

--- dockerconf/entrypoint.sh
+++ dockerconf/entrypoint.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 cd /app/
 
 ALOHOMORA_BIN=$(which alohomora)
-$ALOHOMORA_BIN cast --region ap-south-1 --env $APP_MODE --app dashboard "environment/.env.vault.j2" "environment/env.php.j2" "dockerconf/dashboard.conf.j2" "dockerconf/newrelic.ini.j2"
+$ALOHOMORA_BIN cast --region ap-south-1 --env $APP_MODE --app dashboard "environment/.env.vault.j2" "environment/env.php.j2" "dockerconf/dashboard.conf.j2"
 
 echo "$(date) Add nginx host to dashboard."
 sed -i "s|NGINX_HOST|$HOSTNAME|g" dockerconf/dashboard.conf
