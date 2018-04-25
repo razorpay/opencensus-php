0906a69c4c7d48246fd6878abf0ebaf23ef8662b
Albin George <albinin0002@gmail.com>
2018-04-25 22:09:10 +0530

    [csb] Added edit terminal rules

--- app/Models/Terminal/Validator.php
+++ app/Models/Terminal/Validator.php
@@ -391,6 +391,12 @@ class Validator extends Base\Validator
         Entity::GATEWAY_SECURE_SECRET       => 'required|string',
     ];
 
+    protected static $netbankingCsbEditTerminalRules = [
+        Entity::GATEWAY_MERCHANT_ID         => 'required|string',
+        Entity::GATEWAY_MERCHANT_ID2        => 'required|string',
+        Entity::GATEWAY_SECURE_SECRET       => 'required|string',
+    ];
+
     protected static $cardFssTerminalRules = [
         Entity::GATEWAY                     => 'required|in:card_fss',
         Entity::GATEWAY_MERCHANT_ID         => 'required|string',
