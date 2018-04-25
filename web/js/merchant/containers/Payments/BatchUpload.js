95bb8580e6c1a4ad6e7fcafa1b1f7c73ba53ffaa
Anshul Sahni <anshul.sahni@razorpay.com>
2018-04-25 16:29:24 +0530

    [FEA]: added container for Payments BatchUpload

---
+++ web/js/merchant/containers/Payments/BatchUpload.js
@@ -0,0 +1,167 @@
+import { Component } from 'react';
+import { connect } from 'react-redux';
+
+import ValidateModal from 'merchant/components/BatchNew/ValidateModal';
+import CreateModal from 'merchant/components/BatchNew/CreateModal';
+import SuccessModal from 'merchant/components/BatchNew/SuccessModal';
+import { merchantFetch } from 'rzp/utils/ajax';
+import Spinner from 'rzp/ui/Spinner';
+import TableSlider from 'rzp/ui/TableSlider';
+import ModalHeader from 'rzp/ui/ModalHeader';
+import { closeModal } from 'rzp/modules/modals';
+import { createPaymentsBatch as createBatch } from 'merchant/modules/batches';
+import { showNotification } from 'rzp/modules/notifications';
+
+const iframeHost = 'http://api.razorpay.in';
+
+@connect(null, { closeModal, createBatch, showNotification })
+export default class BatchUploadContainer extends Component {
+  state = {
+    fileUploadProgress: 0,
+    mode: 'loading',
+  };
+
+  onWindowEvent = ({ data: message }) => {
+    if (event.origin === iframeHost) {
+      console.log(message.event);
+      switch (message.event) {
+        case 'load':
+          this.setState({ iFrameLoaded: true });
+          break;
+        case 'upload_init':
+          this.setState({
+            status: 'process',
+            uploadedFile: { ...message.data },
+          });
+          break;
+        case 'progress':
+          const { loaded, total } = message.data;
+          const fileUploadProgress = loaded / total * 100;
+          this.setState({ fileUploadProgress });
+          break;
+        case 'file_uploaded':
+          const { parsed_entries: parsedEntries, file_id: id } = message.data;
+          this.setState({
+            mode: 'create',
+            uploadedFile: { ...this.state.uploadedFile, id },
+            parsedEntries,
+          });
+          break;
+        case 'error':
+          this.setState({
+            fileUploadProgress: 0,
+            error: `${
+              message.data.error.description
+            }. Please upload file again`,
+            status: 'error',
+          });
+          break;
+        default:
+          break;
+      }
+    }
+  };
+
+  componentWillMount() {
+    merchantFetch({
+      url: 'merchant/token',
+      method: 'post',
+    }).then(({ data }) => {
+      this.setState({ mode: 'upload', ott: data.token });
+      window.addEventListener('message', this.onWindowEvent);
+    });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('message', this.onWindowEvent);
+  }
+
+  handleCreateBatch = ({ name }) => {
+    this.setState({ mode: 'loading' }, () => {
+      this.props
+        .createBatch({
+          file_id: this.state.uploadedFile.id,
+          name,
+        })
+        .then(() => {
+          this.setState({ mode: 'success' });
+        })
+        .catch(error => {
+          this.props.showNotification({
+            type: 'error',
+            message: error.errors,
+          });
+          this.props.closeModal();
+        });
+    });
+  };
+
+  render() {
+    const Header = () => (
+      <ModalHeader title="Batch Upload" onCloseClick={this.props.closeModal} />
+    );
+    const Loader = () => (
+      <div class="page-spinner-container">
+        <Spinner />
+      </div>
+    );
+
+    switch (this.state.mode) {
+      case 'loading':
+        return <Loader />;
+
+      case 'upload':
+        return (
+          <div class="batch-upload-modal">
+            {this.state.iFrameLoaded && <Header />}
+            <iframe
+              src={`${iframeHost}/v1/batches/upload?token=${this.state.ott}`}
+              class={`
+                batch-payments-iframe
+                ${this.state.status === 'process' ? 'disabled' : ''}
+                ${this.state.iFrameLoaded ? 'enabled' : ''}
+              `}
+            />
+            {this.state.iFrameLoaded ? (
+              <ValidateModal
+                batchType="direct_debit"
+                iframeHost={iframeHost}
+                showUpload={!this.state.uploadedFile}
+                showStaged={!!this.state.uploadedFile}
+                shouldLoadMore
+                status={this.state.status}
+                notifyMsg={this.state.error}
+                {...this.state}
+              />
+            ) : (
+              <Loader />
+            )}
+          </div>
+        );
+
+      case 'create':
+        const initialValues = {
+          name: this.state.uploadedFile.name,
+        };
+        return (
+          <div class="batch-upload-modal create">
+            <Header />
+            <CreateModal
+              parsedEntries={this.state.parsedEntries}
+              onCreateBatch={this.handleCreateBatch}
+              ctaText="Process Payments"
+              initialValues={initialValues}
+            />
+          </div>
+        );
+      case 'success':
+        return (
+          <div class="batch-upload-modal success">
+            <SuccessModal onModalClose={this.props.closeModal} />
+          </div>
+        );
+      default:
+        return null;
+    }
+  }
+}
