d25597bf3ed1d230dd98d1119f2988a7273096b7
Anshul Sahni <anshul.sahni@razorpay.com>
2018-04-25 16:27:04 +0530

    [FEA]: file/staged component supports direct filename and fileszie

--- web/js/merchant/components/File/Staged.js
+++ web/js/merchant/components/File/Staged.js
@@ -1,4 +1,5 @@
 import { readableFileSize } from 'rzp/utils/rzp-utils';
+import { titleCase } from 'common/util';
 
 const avlblFileTypeIcons = ['pdf', 'jpg', 'png', 'csv', 'xlsx'];
 
@@ -7,20 +8,27 @@ const getFileTypeIcon = fileName => {
   return avlblFileTypeIcons.indexOf(fileType) > -1 ? fileType : 'misc';
 };
 
+const getFileAttr = attr => ({
+  file = {},
+  [`file${titleCase(attr)}`]: attrVal,
+}) => file[attr] || attrVal;
+
 export default props => {
   const { file, progress = 0, currentStatus, onCloseClick = () => {} } = props;
+  const getFileName = getFileAttr('name');
+  const getFileSize = getFileAttr('size');
   return (
-    <div class={`staged-file ${currentStatus || ''}`} key={`${file.name}`}>
+    <div class={`staged-file ${currentStatus || ''}`}>
       <div class="file-icon">
         <div>
-          <span class={`file-type-${getFileTypeIcon(file.name)}`} />
+          <span class={`file-type-${getFileTypeIcon(getFileName(props))}`} />
         </div>
       </div>
       <div class="file-details">
         <div>
           <div>
             <strong class="text-muted">
-              {file.name} ({readableFileSize(file.size)})
+              {getFileName(props)} ({readableFileSize(getFileSize(props))})
             </strong>
           </div>
           <div class="text-muted">
@@ -34,7 +42,12 @@ export default props => {
           <span class="icon i-close" onClick={onCloseClick} />
         </div>
       </div>
-      {currentStatus === 'process' && <div class="loader" />}
+      {!!progress && (
+        <div class="upload-status-bar">
+          <div style={{ width: `${progress}%` }} class="status" />
+        </div>
+      )}
+      {currentStatus === 'process' && !progress && <div class="loader" />}
     </div>
   );
 };
