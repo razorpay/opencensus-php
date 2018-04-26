aedb25cd3ebd94e7d9925149831ffea99373fe7b
Albin George <albinin0002@gmail.com>
2018-04-26 13:57:58 +0530

    [corp NB] Fix test URL

--- app/Gateway/Netbanking/Corporation/Url.php
+++ app/Gateway/Netbanking/Corporation/Url.php
@@ -6,7 +6,7 @@ class Url
 {
     // TODO: Add live domain later
     const LIVE_DOMAIN   = '';
-    const TEST_DOMAIN   = 'http://202.62.247.116';
+    const TEST_DOMAIN   = 'https://202.62.247.116';
 
     const AUTHORIZE     = '/corp/OLT';
     const VERIFY        = '/corp/OLTDVER';
