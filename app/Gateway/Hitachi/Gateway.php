d2dc1a23f34dafd50b243be91384c5c4817b9a78
Ria Garg <ria.garg@razorpay.com>
2018-04-28 21:36:22 +0530

    [hitachi] fixes amount

--- app/Gateway/Hitachi/Gateway.php
+++ app/Gateway/Hitachi/Gateway.php
@@ -198,7 +198,7 @@ class Gateway extends Base\Gateway
         $this->compareHashes($actualChecksum, $expectedChecksum);
 
         $qrData = [
-            BharatQr\GatewayResponseParams::AMOUNT                => $this->getIntegerFormattedAmount($input[ResponseFields::AMOUNT]),
+            BharatQr\GatewayResponseParams::AMOUNT                => $this->getIntegerFormattedAmount($input[ResponseFields::AMOUNT])/100,
             BharatQr\GatewayResponseParams::CARD_FIRST6           => substr($input[ResponseFields::MASKED_CARD_NUMBER], 0, 6),
             BharatQr\GatewayResponseParams::CARD_LAST4            => substr($input[ResponseFields::MASKED_CARD_NUMBER], 12, 4),
             BharatQr\GatewayResponseParams::METHOD                => Payment\Method::CARD,
