5e227b2c07ccd2cf700f2104c7117d0c105d052a
Albin George <albinin0002@gmail.com>
2018-05-02 14:48:27 +0530

    [csb] Fix adding axis nodal account details to config

--- config/nodal.php
+++ config/nodal.php
@@ -2,8 +2,10 @@
 
 return [
     'axis' => [
-        'secret'    => env('AXIS_NODAL_AES_SECRET'),
-        'iv'        => env('AXIS_NODAL_AES_IV'),
+        'secret'         => env('AXIS_NODAL_AES_SECRET'),
+        'iv'             => env('AXIS_NODAL_AES_IV'),
+        'account_number' => env('AXIS_NODAL_ACCOUNT_NUMBER'),
+        'ifsc_code'      => env('AXIS_NODAL_IFSC')
     ],
 
     'rbl' => [
@@ -29,10 +31,5 @@ return [
         'src_app_cd'        => env('KOTAK_NODAL_BALANCE_APPLICATION_ID'),
         'crn'               => env('KOTAK_NODAL_CRN'),
         'account_number'    => env('KOTAK_NODAL_ACCOUNT_NUMBER')
-    ],
-
-    'axis' => [
-        'account_number' => env('AXIS_NODAL_ACCOUNT_NUMBER'),
-        'ifsc_code'      => env('AXIS_NODAL_IFSC')
     ]
 ];
