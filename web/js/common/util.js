8f4ceeb52f73e6af1db6ae6169088af991597142
aseemgupta <aseemgupta2625@gmail.com>
2018-05-04 15:10:00 +0530

    Add classlist utils

--- web/js/common/util.js
+++ web/js/common/util.js
@@ -64,3 +64,19 @@ export const isWorkflow = (response, history = null) => {
     return false;
   }
 };
+
+export function classList(...args) {
+  const classes = [];
+
+  for (var i = 0; i < args.length; i++) {
+    if (args[i]) {
+      if (args[i] instanceof Array) {
+        args[i] = args[i].join(' ');
+      }
+
+      classes.push(args[i]);
+    }
+  }
+
+  return classes.join(' ');
+}
