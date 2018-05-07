c3c2c241f59b63d7fc18cb77331173652916a462
Albin George <albinin0002@gmail.com>
2018-05-07 19:55:24 +0530

    [hdfc] Emandate - Correct register file headings

--- app/Models/Gateway/File/Processor/Emandate/Register/Hdfc.php
+++ app/Models/Gateway/File/Processor/Emandate/Register/Hdfc.php
@@ -65,17 +65,14 @@ class Hdfc extends Base
 
             $row = [
                 Headings::CLIENT_NAME                   => $data[Headings::CLIENT_NAME],
-                Headings::MERCHANT_UNIQUE_REFERENCE_NO  => $data[Headings::MERCHANT_UNIQUE_REFERENCE_NO],
                 Headings::CUSTOMER_NAME                 => $data[Headings::CUSTOMER_NAME],
                 Headings::CUSTOMER_ACCOUNT_NUMBER       => $data[Headings::CUSTOMER_ACCOUNT_NUMBER],
-                Headings::AMOUNT                        => $this->getFormattedAmount($payment->getAmount()),
+                Headings::AMOUNT                        => '1.00',
                 Headings::AMOUNT_TYPE                   => $data[Headings::AMOUNT_TYPE],
                 Headings::START_DATE                    => $startDate,
                 Headings::END_DATE                      => $endDate,
                 Headings::FREQUENCY                     => $data[Headings::FREQUENCY],
-                Headings::MANDATE_SERIAL_NUMBER         => $data[Headings::MANDATE_SERIAL_NUMBER],
                 Headings::MANDATE_ID                    => $data[Headings::MANDATE_ID],
-                Headings::MERCHANT_REQUEST_NO           => $data[Headings::MERCHANT_REQUEST_NO],
             ];
 
             $rows[] = $row;
