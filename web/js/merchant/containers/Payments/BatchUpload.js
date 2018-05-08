ecb25b2865676a809d5e8f85af294c9249d3cbc2
Anshul Sahni <anshul.sahni@razorpay.com>
2018-05-08 12:19:55 +0530

    update link to cdn url

--- web/js/merchant/containers/Payments/BatchUpload.js
+++ web/js/merchant/containers/Payments/BatchUpload.js
@@ -143,7 +143,7 @@ export default class BatchUploadContainer extends Component {
                 status={this.state.status}
                 notifyMsg={this.state.error}
                 docUrl="https://docs.razorpay.com/v1/page/batch-card-payments"
-                sampleUrl="files/sample_batch_payments.csv"
+                sampleUrl="https://cdn.razorpay.com/dashboard/sample_batch_payments.csv"
                 {...this.state}
               />
             ) : (
