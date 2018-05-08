02965da856c6ab1a7f3a601ea194c5fc666a0afb
Ratan Phayade <ratanphayade@users.noreply.github.com>
2018-05-08 15:49:00 +0530

    [rbl] purpose fix (#8216)
    * [rbl] purpose fix
    
    * MI trace added

--- app/Models/FundTransfer/Base/Initiator/NodalAccount.php
+++ app/Models/FundTransfer/Base/Initiator/NodalAccount.php
@@ -38,13 +38,6 @@ abstract class NodalAccount extends Base\Core
 
     protected $purpose           = null;
 
-    public function __construct(string $purpose)
-    {
-        parent::__construct();
-
-        $this->purpose = $purpose;
-    }
-
     protected function isRefund(): bool
     {
         return ($this->purpose === Attempt\Purpose::REFUND);
