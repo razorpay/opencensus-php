0d399bec8a6a347ebdb6d19890a5de47da4d5c08
Amit Mahbubani <amit.mahbubani@razorpay.com>
2018-05-08 13:47:44 +0530

    [gstin] Add state code validator + tests

--- tests/Unit/GSTIN/GstinTest.php
+++ tests/Unit/GSTIN/GstinTest.php
@@ -34,6 +34,30 @@ class GstinTest extends TestCase
         }
     }
 
+    public function testValidStateCode()
+    {
+        $list = $this->testData[__FUNCTION__];
+
+        foreach ($list as $code)
+        {
+            $this->assertTrue(
+                Gstin::isStateCodeValid($code),
+                'Failed validity test for GST State Code: ' . $code);
+        }
+    }
+
+    public function testInvalidStateCode()
+    {
+        $list = $this->testData[__FUNCTION__];
+
+        foreach ($list as $code)
+        {
+            $this->assertFalse(
+                Gstin::isStateCodeValid($code),
+                'Failed invalid test for GST State Code: ' . $code);
+        }
+    }
+
     /**
      * Asserts the state metadata returned by the lib function.
      *
