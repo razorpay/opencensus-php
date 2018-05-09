8f4832a01a70a5c99db03b0ac2602976f4e357b3
Albin George <albinin0002@gmail.com>
2018-05-09 16:52:53 +0530

    [hdfc] Emandate register alignment fix

--- app/Models/Batch/Processor/Emandate/Register/Hdfc.php
+++ app/Models/Batch/Processor/Emandate/Register/Hdfc.php
@@ -7,31 +7,30 @@ namespace RZP\Models\Batch\Processor\Emandate\Register;
  use RZP\Models\Payment;
  use RZP\Models\Customer\Token;
  use RZP\Models\Payment\Gateway;
- use RZP\Gateway\Base\Entity as GatewayEntity;
  use RZP\Gateway\Netbanking\Base\Entity as NetbankingEntity;
  use RZP\Gateway\Netbanking\Hdfc\EMandateRegisterFileHeadings as Headings;
 
- class Hdfc extends Base
- {
-     const GATEWAY   = Gateway::NETBANKING_HDFC;
+class Hdfc extends Base
+{
+    const GATEWAY   = Gateway::NETBANKING_HDFC;
 
-     const SUCCESS   = 'success';
-     const REJECT    = 'reject';
+    const SUCCESS   = 'success';
+    const REJECT    = 'reject';
 
-     protected  $gatewayPaymentMapping = [
-         Base::TOKEN_ID       => NetbankingEntity::SI_TOKEN,
-         Base::STATUS         => NetbankingEntity::SI_STATUS,
-         Base::REMARK         => NetbankingEntity::SI_MSG,
-         Base::ACCOUNT_NUMBER => NetbankingEntity::ACCOUNT_NUMBER,
-     ];
+    protected  $gatewayPaymentMapping = [
+        Base::TOKEN_ID       => NetbankingEntity::SI_TOKEN,
+        Base::STATUS         => NetbankingEntity::SI_STATUS,
+        Base::REMARK         => NetbankingEntity::SI_MSG,
+        Base::ACCOUNT_NUMBER => NetbankingEntity::ACCOUNT_NUMBER,
+    ];
 
-     protected static $statusMap = [
+    protected static $statusMap = [
         self::SUCCESS => Token\RecurringStatus::CONFIRMED,
         self::REJECT  => Token\RecurringStatus::REJECTED,
-     ];
+    ];
 
-     protected function getDataFromRow(array & $entry): array
-     {
+    protected function getDataFromRow(array & $entry): array
+    {
         $tokenId = $entry[Headings::MANDATE_ID];
 
         $status = $entry[Headings::STATUS];
@@ -48,22 +47,22 @@ namespace RZP\Models\Batch\Processor\Emandate\Register;
             Base::REMARK           => $remark,
             Base::ACCOUNT_NUMBER   => $accountNumber,
         ];
-     }
+    }
 
-     protected function getTokenStatus(string $gatewayTokenStatus): string
-     {
-         $gatewayTokenStatus = strtolower($gatewayTokenStatus);
+    protected function getTokenStatus(string $gatewayTokenStatus): string
+    {
+        $gatewayTokenStatus = strtolower($gatewayTokenStatus);
 
-         if (isset(self::$statusMap[$gatewayTokenStatus]) === false)
-         {
-             throw new Exception\LogicException('Unrecognized gateway status: ', $gatewayTokenStatus);
-         }
+        if (isset(self::$statusMap[$gatewayTokenStatus]) === false)
+        {
+            throw new Exception\LogicException('Unrecognized gateway status: ', $gatewayTokenStatus);
+        }
 
-         return self::$statusMap[$gatewayTokenStatus];
-     }
+        return self::$statusMap[$gatewayTokenStatus];
+    }
 
-     protected function getGatewayPayment(Payment\Entity $payment)
-     {
-         return $this->repo->netbanking->findByPaymentIdAndAction($payment['id'], Action::AUTHORIZE);
-     }
- }
+    protected function getGatewayPayment(Payment\Entity $payment)
+    {
+        return $this->repo->netbanking->findByPaymentIdAndAction($payment['id'], Action::AUTHORIZE);
+    }
+}
