1c314696c5887fa82e0a768a2825e6c6ff453142
Albin George <albinin0002@gmail.com>
2018-05-12 07:00:10 +0530

    [hdfc] Fixtures correction

--- tests/Functional/Fixtures/Entity/Payment.php
+++ tests/Functional/Fixtures/Entity/Payment.php
@@ -6,6 +6,16 @@ class Payment extends Base
 {
     use TransactionTrait;
 
+    protected $emandateRegistrationInitialPaymentDefaultAttributes = [
+        'amount'         => 0,
+        'method'         => 'emandate',
+        'customer_id'    => '100000customer',
+        'email'          => 'a@b.com',
+        'contact'        => '+919918899029',
+        'recurring_type' => 'initial',
+        'auth_type'      => 'netbanking',
+    ];
+
     public function createCaptured(array $attributes = array())
     {
         if (isset($attributes['method']))
@@ -413,21 +423,40 @@ class Payment extends Base
             $id, ['status' => 'failed', 'error_code' => 'BAD_REQUEST_PAYMENT_FAILED']);
     }
 
-    public function createEmandateRegistrationSuccess(array $attributes = [])
+    public function createEmandateRegistrationInitial(array $attributes = [])
     {
-        $defaults = [
-            'amount'         => 0,
-            'method'         => 'emandate',
-            'status'         => 'authorized',
-            'customer_id'    => '100000customer',
-            'email'          => 'a@b.com',
-            'contact'        => '+919918899029',
-            'recurring_type' => 'initial',
-            'auth_type'      => 'netbanking',
-        ];
+        $defaults = array_merge(
+            $this->emandateRegistrationInitialPaymentDefaultAttributes,
+            ['status'         => 'authorized']
+        );
 
         $attributes = array_merge($defaults, $attributes);
 
         return $this->create($attributes);
+    }
+
+    public function createEmandateRegistrationConfirmed(array $attributes = [])
+    {
+        $defaults = array_merge(
+            $this->emandateRegistrationInitialPaymentDefaultAttributes,
+            ['status'         => 'captured']
+        );
+
+        $attributes = array_merge($defaults, $attributes);
+
+        return $this->create($attributes);
+    }
+
+    public function createEmandateDebit(array $attributes = [])
+    {
+        $defaults = [
+            'method'         => 'emandate',
+            'customer_id'    => '100000customer',
+            'auto_captured'  => false,
+            'email'          => 'a@b.com',
+            'contact'        => '+919918899029',
+            'recurring_type' => 'auto',
+            'auth_type'      => 'netbanking',
+        ];
     }
 }
