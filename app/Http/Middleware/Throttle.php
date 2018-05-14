040ab8185b76d1945d328bd7dfa651dabf43ad9b
jitendra-1217 <jitendraojha1217@gmail.com>
2018-05-14 23:08:44 +0530

    [metric] Fixes metric dimensions

--- app/Http/Middleware/Throttle.php
+++ app/Http/Middleware/Throttle.php
@@ -11,22 +11,32 @@ use RZP\Http\Throttle\Throttler;
 
 final class Throttle
 {
+    /**
+     * Handles http request:
+     * - Inits request context
+     * - Attempts throttling
+     * - Pushes http related metrics
+     *
+     * We are intentionally not adding another middleares to init reqeust context
+     * & to push http metrics because middlewares in laravel have little overhead.
+     *
+     * @param  Request  $request
+     * @param  \Closure $next
+     * @return Response
+     */
     public function handle($request, \Closure $next)
     {
         $start = microtime(true);
 
-        // Throttle is the first middleware and request context is initialized here
         app('request.ctx')->init();
 
         (new Throttler)->throttle();
 
-        // TODO: Refactor here. Probably create another class in http/ to take care of all http metric crap
-
         $response = $next($request);
 
-        $end = microtime(true);
+        $duration = microtime(true) - $start; // For metric http_request_duration_microseconds
 
-        $this->pushHttpMetrics($request, $response, $start, $end);
+        $this->pushHttpMetrics($request, $response, $duration);
 
         return $response;
     }
@@ -35,16 +45,14 @@ final class Throttle
      * Pushes specific HTTP metrics
      * @param  Request  $request
      * @param  Response $response
-     * @param  int      $start
-     * @param  int      $end
+     * @param  int      $duration
      */
-    protected function pushHttpMetrics(Request $request, Response $response, int $start, int $end)
+    protected function pushHttpMetrics(Request $request, Response $response, int $duration)
     {
         $dimensions = $this->getMetricDimensions($request, $response);
 
         Metrics::count(Metric::HTTP_REQUESTS_TOTAL, 1, $dimensions);
-
-        Metrics::histogram(Metric::HTTP_REQUEST_DURATION_MICROSECONDS, $end - $start, $dimensions);
+        Metrics::histogram(Metric::HTTP_REQUEST_DURATION_MICROSECONDS, $duration, $dimensions);
     }
 
     /**
@@ -55,30 +63,18 @@ final class Throttle
      */
     protected function getMetricDimensions(Request $request, Response $response): array
     {
-        //
-        // Todo:
-        // Awaiting an ongoing refactoring effort to move request's context variables (e.g. merchant identifier, internal
-        // application name etc.) out of any existing tied class (e.g. BasicAuth) so can be used at various places. Post
-        // that should move this part of code elsewhere. Currently keeping here as this is the first one to get triggered.
-        //
-
-        //
-        // Enhancement:
-        // We would like to tag metrics for specific key merchants at leasts. For this need someway to know when to use
-        // the merchant id in label value and when to use a placeholder. TBD later. Also awaiting on above refactoring(
-        // todo ^) so this could be done in a clean way.
-        //
+        $requestCtx = app('request.ctx');
 
         return [
             Metric::LABEL_METHOD                => $request->getMethod(),
             Metric::LABEL_ROUTE                 => $request->route()->getName(),
             Metric::LABEL_STATUS                => $response->getStatusCode(),
-            Metric::LABEL_RZP_MODE              => Metric::LABEL_DEFAULT_VALUE,
-            Metric::LABEL_RZP_KEY_ID            => Metric::LABEL_DEFAULT_VALUE,
-            Metric::LABEL_RZP_MERCHANT_ID       => Metric::LABEL_DEFAULT_VALUE,
-            Metric::LABEL_RZP_OAUTH_CLIENT_ID   => Metric::LABEL_DEFAULT_VALUE,
-            Metric::LABEL_RZP_AUTH              => Metric::LABEL_DEFAULT_VALUE,
-            Metric::LABEL_RZP_INTERNAL_APP_NAME => Metric::LABEL_DEFAULT_VALUE,
+            Metric::LABEL_RZP_MODE              => $requestCtx->getMode(),
+            Metric::LABEL_RZP_KEY_ID            => $requestCtx->getKeyId(),
+            Metric::LABEL_RZP_MERCHANT_ID       => $requestCtx->getMid(),
+            Metric::LABEL_RZP_OAUTH_CLIENT_ID   => $requestCtx->getOauthClientId(),
+            Metric::LABEL_RZP_AUTH              => $requestCtx->getAuth(),
+            Metric::LABEL_RZP_INTERNAL_APP_NAME => $requestCtx->getInternalAppName(),
         ];
     }
 }
