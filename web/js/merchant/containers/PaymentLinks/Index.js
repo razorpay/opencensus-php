15666045e68a1502fb84601b8e92300fd42bdeed
rchaubey <dev.rohit.chaubey@gmail.com>
2018-05-15 04:29:00 +0530

    [mod] restore old batch sample file

--- web/js/merchant/containers/PaymentLinks/Index.js
+++ web/js/merchant/containers/PaymentLinks/Index.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Route, Switch, NavLink } from 'react-router-dom';
 import ShowWhen from 'merchant/components/ShowWhen';
