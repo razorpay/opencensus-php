75452b0074954c8eb61d03b48e082eb29d3fc505
Ria Garg <ria.garg@razorpay.com>
2018-05-16 14:59:43 +0530

    puts check on empty card holder name

--- app/Models/BharatQr/Processor.php
+++ app/Models/BharatQr/Processor.php
@@ -236,11 +236,13 @@ class Processor extends VirtualAccount\Processor
 
     protected function getDummyCardDetails()
     {
+        $cardHolderName = preg_replace("/[^ \w]+/", "", $this->gatewayInput[GatewayResponseParams::SENDER_NAME]);
+
         // TODO: Handle the null checks in card validation
         $card = [
             Card\Entity::NUMBER       => $this->getLuhnValidCardNumber(),
             Card\Entity::CVV          => Constants::CARD_CVV,
-            Card\Entity::NAME         => preg_replace("/[^ \w]+/", "", $this->gatewayInput[GatewayResponseParams::SENDER_NAME]),
+            Card\Entity::NAME         => $cardHolderName ?: Constants::CARD_NAME,
             Card\Entity::EXPIRY_MONTH => Constants::CARD_EXPIRY_MONTH,
             Card\Entity::EXPIRY_YEAR  => Constants::CARD_EXPIRY_YEAR,
         ];
