3deff9bc8dfc54b2e79355bcec1d0528b4be4aae
aseemgupta <aseemgupta2625@gmail.com>
2018-05-22 12:45:29 +0530

    Fix updated at in uplaoad documents response

--- app/Models/Merchant/Detail/Service.php
+++ app/Models/Merchant/Detail/Service.php
@@ -117,7 +117,7 @@ class Service extends Base\Service
 
         $this->repo->saveOrFail($merchantDetails);
 
-        $response = array_merge($response, $merchantDetails->toArrayPublic()); // verification key is only in $response. array_merge will also add some additional keys missing in $merchantDetails.
+        $response['updated_at'] = $merchantDetails->getUpdatedAt(); // To add the latest update_at value in $response.
 
         return $response;
     }
