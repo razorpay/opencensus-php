da3adbca9b2da3fe9b7c8befd01b704c066a9a35
aseemgupta <aseemgupta2625@gmail.com>
2018-05-24 05:13:38 +0530

    Empty biz name force to presignup page if sent null

--- app/Models/Merchant/Detail/Validator.php
+++ app/Models/Merchant/Detail/Validator.php
@@ -96,7 +96,7 @@ class Validator extends Base\Validator
         Entity::CONTACT_MOBILE                  => 'sometimes|numeric|digits_between:8,11',
         Entity::CONTACT_LANDLINE                => 'sometimes|numeric|digits_between:8,11',
         Entity::BUSINESS_TYPE                   => 'sometimes|numeric|digits_between:1,10',
-        Entity::BUSINESS_NAME                   => 'sometimes|max:255',
+        Entity::BUSINESS_NAME                   => 'filled|max:255',
         Entity::BUSINESS_DBA                    => 'sometimes|max:255',
         Entity::BUSINESS_WEBSITE                => 'sometimes|max:255|url',
         Entity::BUSINESS_INTERNATIONAL          => 'sometimes|in:0,1',
