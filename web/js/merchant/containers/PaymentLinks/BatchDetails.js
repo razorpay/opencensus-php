090d2262b1b048d34a02010c53b9cf2cb4d98500
Anshul Sahni <anshul.sahni@razorpay.com>
2018-05-23 19:47:46 +0530

    [BATCH][NEW]: all details in batch in payment Payment link batch details container  continuing with the changes done in commit cbb25dc9781b0c63870b63cb9791e62d5c38359a following more changes are implemented   - Using ListToggler and DataTable to render invoices   - temporarily disabled ga event implemented in batch detail   - rendering error message in case of failure of link genration re-factored

--- web/js/merchant/containers/PaymentLinks/BatchDetails.js
+++ web/js/merchant/containers/PaymentLinks/BatchDetails.js
@@ -1,9 +1,49 @@
-import { Component } from 'react';
+import { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
-import BatchDetails from 'merchant/containers/BatchNew/Details';
-import BatchStats from '../../components/BatchNew/Stats';
+import { NavLink } from 'react-router-dom';
 
+import BatchDetails from 'merchant/containers/BatchNew/Details';
 import { fetchPaymentLinkBatchesDetails as fetchBatchDetails } from 'merchant/modules/batches';
+import { pluralize } from 'rzp/utils/rzp-utils';
+
+import BatchStats from 'merchant/components/BatchNew/Stats';
+import EntityDetailRow from 'merchant/components/EntityDetailRow';
+import EmptyTableRow from 'rzp/ui/EmptyTableRow';
+import DataTable from 'rzp/ui/Table/DataTable';
+import ListToggler from 'rzp/ui/Toggler/ListToggler';
+import Time from 'rzp/ui/Time';
+import { amount, status } from 'rzp/ui/item/pair';
+import { BatchUploadStatusLabel } from 'merchant/components/StatusLabel';
+
+const renderBatchDetails = props => {
+  const { batch, stats, invoices } = props;
+  const statsTable = getStatsTable(stats);
+  return (
+    <Fragment>
+      <div class="equal-margin">
+        <BatchStats stats={statsTable} />
+      </div>
+      <div class="equal-margin">
+        <EntityDetailRow label="Status">
+          <BatchUploadStatusLabel status={batch.status} />
+        </EntityDetailRow>
+        <EntityDetailRow label="Status">
+          <Time value={batch.created_at} />
+        </EntityDetailRow>
+      </div>
+      <InvoicesTable invoices={invoices} batchId={batch.id} />
+      <hr />
+      {stats.batch_total > stats.issued_count &&
+        batch.status !== 'created' && (
+          <LinksErrMessage
+            issuedCount={stats.issued_count}
+            onDownload={props.onDownload}
+            batchId={batch.id}
+          />
+        )}
+    </Fragment>
+  );
+};
 
 @connect(null, {
   fetchBatchDetails,
@@ -20,9 +60,48 @@ export default class PaymentLinksBatchDetailsContainer extends Component {
   }
 }
 
-function renderBatchDetails(props) {
-  const stats = props.stats;
-  const statsTable = [
+function InvoicesTable({ invoices, batchId }) {
+  return (
+    <ListToggler
+      label={invoices.length ? pluralize('Payment Link', invoices.length) : ''}
+      subLabel={invoices.length ? 'created from this batch' : ''}
+      limit={4}
+      limitUrl={`/paymentlinks?batch_id=${batchId}`}
+      totalItems={invoices.length}
+    >
+      <DataTable
+        columns={[invoiceEmail, amount, status]}
+        customClass="invoice-list"
+        limit={4}
+        title="Invoices"
+        showHeaders={false}
+        items={invoices}
+      />
+    </ListToggler>
+  );
+}
+
+function LinksErrMessage({ issuedCount, onDownload, batchId }) {
+  return (
+    <small class="help-block m-l">
+      <i class="i i-info-circle" /> {issuedCount === 0 ? 'The payment' : 'Some'}{' '}
+      links related to this batch were not created due to errors. Please<span
+        class="btn-link"
+        onClick={onDownload.bind(this, batchId)}
+      >
+        {' '}
+        download{' '}
+      </span>the report containing all Payment Links data
+    </small>
+  );
+}
+
+var invoiceEmail = {
+  value: invoice => invoice.customer_details.email,
+};
+
+function getStatsTable(stats) {
+  return [
     { title: 'Total rows processed', value: stats.batch_total },
     { title: 'Payment links created', value: stats.issued_count },
     {
@@ -34,5 +113,4 @@ function renderBatchDetails(props) {
       value: <span class="text-danger">{stats.expired_count}</span>,
     },
   ];
-  return <BatchStats stats={statsTable} />;
 }
