37fd1aac6816e6b29344752580cc7507bbbfc46c
Patlola <praveen.patlola@razorpay.com>
2018-05-26 12:56:28 +0000

    single quotes on teh data

--- app/Gateway/Card/Fss/Gateway.php
+++ app/Gateway/Card/Fss/Gateway.php
@@ -423,9 +423,9 @@ class Gateway extends Base\Gateway
         // Trace payment decrypted data as string because if their
         // is any error in data format we can get to know about this.
         $this->traceGatewayData([
-                                    "gateway_data" => $decryptedString,
-                                    "payment_id"   => $input['payment']['id'],
-                                    "gateway"      => $this->gateway,
+                                    'gateway_data' => $decryptedString,
+                                    'payment_id'   => $input['payment']['id'],
+                                    'gateway'      => $this->gateway,
                                 ],
                                 TraceCode::GATEWAY_PAYMENT_CALLBACK);
 
