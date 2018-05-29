22334aad48b4ec6e0a9eb1afdddd5b644fa19123
Anshul Sahni <anshul.sahni@razorpay.com>
2018-05-29 16:20:51 +0530

    [BATCH][NEW]: change in struture of BatchNew/ga  Following changes are implemented:   - tracking methods are returned when default method is called with pageTitle as argument   - all other tracking methods passed as collection in a single object

--- web/js/merchant/containers/BatchNew/ga.js
+++ web/js/merchant/containers/BatchNew/ga.js
@@ -2,72 +2,74 @@ import { stringifyQueryParamsWithPipe } from 'rzp/utils/rzp-utils';
 
 import { setTrackData } from 'rzp/utils/googleAnalytics';
 
-const pageTitle = 'Dashboard - Payment Links';
+export default pageTitle => {
+  const track = setTrackData({ eventCategory: pageTitle });
 
-export const track = setTrackData({ eventCategory: pageTitle });
+  return {
+    trackGoToLinks: toText => {
+      track({
+        eventAction: `Go to - ${toText}`,
+      });
+    },
 
-export const trackGoToLinks = toText => {
-  track({
-    eventAction: `Go to - ${toText}`,
-  });
-};
+    trackSampleFileDownload: () => {
+      track({
+        eventAction: 'Download - Batch Sample File',
+      });
+    },
 
-export const trackSampleFileDownload = () => {
-  track({
-    eventAction: 'Download - Batch Sample File',
-  });
-};
+    trackDetails: (action, batchId) => {
+      track({
+        eventAction: `${action} Details - Batch`,
+        eventLabel: `batch_id=${batchId}`,
+      });
+    },
 
-export const trackDetails = (action, batchId) => {
-  track({
-    eventAction: `${action} Details - Batch`,
-    eventLabel: `batch_id=${batchId}`,
-  });
-};
+    trackSearchFilters: params => {
+      const label = stringifyQueryParamsWithPipe(params);
+      track({
+        eventAction: 'Search - Batches',
+        eventLabel: label,
+      });
+    },
 
-export const trackSearchFilters = params => {
-  const label = stringifyQueryParamsWithPipe(params);
-  track({
-    eventAction: 'Search - Batches',
-    eventLabel: label,
-  });
-};
+    trackUploadBatchFile: () => {
+      track({
+        eventAction: 'Upload - Batch File',
+      });
+    },
 
-export const trackUploadBatchFile = () => {
-  track({
-    eventAction: 'Upload - Batch File',
-  });
-};
+    trackSendAllLinks: params => {
+      const label = stringifyQueryParamsWithPipe(params);
+      track({
+        eventAction: 'Send - All Payment Links ',
+        eventLabel: label,
+      });
+    },
 
-export const trackSendAllLinks = params => {
-  const label = stringifyQueryParamsWithPipe(params);
-  track({
-    eventAction: 'Send - All Payment Links ',
-    eventLabel: label,
-  });
-};
+    trackSeeAllLinks: batchId => () => {
+      track({
+        eventAction: 'See - All Payment Links',
+        eventLabel: `batch_id=${batchId}`,
+      });
+    },
 
-export const trackSeeAllLinks = batchId => {
-  track({
-    eventAction: 'See - All Payment Links',
-    eventLabel: `batch_id=${batchId}`,
-  });
-};
+    trackDownloadErrorReport: () => {
+      track({
+        eventAction: 'Download - Validation Error Report',
+      });
+    },
 
-export const trackDownloadErrorReport = () => {
-  track({
-    eventAction: 'Download - Validation Error Report',
-  });
-};
+    trackDownloadProcessedBatchReport: () => {
+      track({
+        eventAction: 'Download - Processed Batch Report ',
+      });
+    },
 
-export const trackDownloadProcessedBatchReport = () => {
-  track({
-    eventAction: 'Download - Processed Batch Report ',
-  });
-};
-
-export const trackUploadBatch = action => {
-  track({
-    eventAction: `${action} Form - Create Batch`,
-  });
+    trackUploadBatch: action => {
+      track({
+        eventAction: `${action} Form - Create Batch`,
+      });
+    },
+  };
 };
