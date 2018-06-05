25ec76b8a29912916e1c2f118da5564b6c95bf53
Ria Garg <ria.garg@razorpay.com>
2018-06-05 15:55:55 +0530

    [bqr] adds test cases

--- tests/Functional/VirtualAccount/VirtualAccountTest.php
+++ tests/Functional/VirtualAccount/VirtualAccountTest.php
@@ -172,6 +172,34 @@ class VirtualAccountTest extends TestCase
         $this->assertEquals('428734', $masterCardAcquirerCode);
     }
 
+    public function testCreateVirtualAccountWithReference()
+    {
+        $this->ba->proxyAuthLive();
+
+        $this->fixtures->merchant->activate();
+
+        $input = [
+            'receivers'  => [
+                'types' => ['qr_code'],
+                'qr_code'       => [
+                    'reference' => 'abc'
+                ]
+            ]
+        ];
+
+        $request = [
+            'method'  => 'POST',
+            'url'     => '/virtual_accounts',
+            'content' => $input,
+        ];
+
+        $this->expectException(\Rzp\Exception\BadRequestException::class);
+
+        $this->expectExceptionMessage('The requested URL was not found on the server.');
+
+        $this->makeRequestAndGetContent($request);
+    }
+
     public function testCreateVirtualAccountWithBharatQrWithNoTerminal()
     {
         $this->fixtures->terminal->disableTerminal($this->t1['id']);
