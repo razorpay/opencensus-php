b093234cd98e6d60c609b9926ebcd667a72b53a0
hobbes09 <sourin.sutradhar@gmail.com>
2018-06-05 23:13:44 +0530

    [PaymentLink] tests for update, activate and deactivate api

--- tests/Functional/PaymentLink/Helpers/PaymentLinkTestData.php
+++ tests/Functional/PaymentLink/Helpers/PaymentLinkTestData.php
@@ -241,4 +241,137 @@ return [
             'status_reason'     => 'completed',
         ],
     ],
+
+    'testDeactivatePaymentLink' => [
+        'request' => [
+            'url'     => '/payment_links/pl_100000000000pl/deactivate',
+            'method'  => 'patch',
+            ],
+        'response' => [
+            'content' => [
+                'id'            => 'pl_100000000000pl',
+                'status'        => 'inactive',
+                'status_reason' => 'deactivated',
+            ],
+        ],
+    ],
+
+    'testDeactivateAlreadyDeactivatedPaymentLink' => [
+        'request' => [
+            'url'     => '/payment_links/pl_100000000000pl/deactivate',
+            'method'  => 'patch',
+        ],
+        'response' => [
+            'content' => [
+                'error' => [
+                    'code'        => PublicErrorCode::BAD_REQUEST_ERROR,
+                    'description' => 'Payment link cannot be deactivated as it is already inactive',
+                ],
+            ],
+            'status_code' => 400,
+        ],
+        'exception' => [
+            'class'               => 'RZP\Exception\BadRequestException',
+            'internal_error_code' => ErrorCode::BAD_REQUEST_PAYMENT_LINK_ALREADY_INACTIVE,
+        ],
+    ],
+
+    'testActivatePaymentLink' => [
+        'request' => [
+            'url'     => '/payment_links/pl_100000000000pl/activate',
+            'method'  => 'patch',
+        ],
+        'response' => [
+            'content' => [
+                'id'            => 'pl_100000000000pl',
+                'status'        => 'active',
+                'status_reason' => null,
+            ],
+        ],
+    ],
+
+    'testActivateLinkAlreadyActivated' => [
+        'request' => [
+            'url'     => '/payment_links/pl_100000000000pl/activate',
+            'method'  => 'patch',
+        ],
+        'response' => [
+            'content' => [
+                'error' => [
+                    'code'        => PublicErrorCode::BAD_REQUEST_ERROR,
+                    'description' => 'Payment link cannot be activated as it is already active',
+                ],
+            ],
+            'status_code' => 400,
+        ],
+        'exception' => [
+            'class'               => 'RZP\Exception\BadRequestException',
+            'internal_error_code' => ErrorCode::BAD_REQUEST_PAYMENT_LINK_ALREADY_ACTIVE,
+        ],
+    ],
+
+    'testActivateWithTimesPayableLessThanTimesPaid' => [
+        'request' => [
+            'url'     => '/payment_links/pl_100000000000pl/activate',
+            'method'  => 'patch',
+            'content' => [
+                'times_payable' => 1,
+            ],
+        ],
+        'response' => [
+            'response' => [
+                'content' => [
+                    'error' => [
+                        'code'        => PublicErrorCode::BAD_REQUEST_ERROR,
+                        'description' => 'Times payable cannot be less than the number of payments processed',
+                    ],
+                ],
+                'status_code' => 400,
+            ],
+            'exception' => [
+                'class'               => 'RZP\Exception\BadRequestValidationFailureException',
+            ],
+        ],
+    ],
+
+    'testMinExpiryTimeForActivation' => [
+        'request' => [
+            'url'     => '/payment_links/pl_100000000000pl/activate',
+            'method'  => 'patch',
+            'content' => [
+            ],
+        ],
+        'response' => [
+            'content' => [
+                'error' => [
+                    'code'        => PublicErrorCode::BAD_REQUEST_ERROR,
+                    'description' => 'expire_by should be at least 15 minutes after ahead of the current time.',
+                ],
+            ],
+            'status_code' => 400,
+        ],
+        'exception' => [
+            'class'               => 'RZP\Exception\BadRequestValidationFailureException',
+            'internal_error_code' => ErrorCode::BAD_REQUEST_VALIDATION_FAILURE,
+        ],
+    ],
+
+    'testEditPaymentLinkToComplete' => [
+        'request' => [
+            'url'     => '/payment_links/pl_100000000000pl',
+            'method'  => 'patch',
+            'content' => [
+                'times_payable' => 1,
+            ],
+        ],
+        'response' => [
+            'content' => [
+                'id'            => 'pl_100000000000pl',
+                'times_payable' => 1,
+                'status'        => 'inactive',
+                'status_reason' => 'completed',
+                'times_paid'    => 1
+            ],
+        ],
+    ],
 ];
--- tests/Functional/PaymentLink/PaymentLinkTest.php
+++ tests/Functional/PaymentLink/PaymentLinkTest.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\Queue;
 
 use RZP\Models\Payment;
 use RZP\Error\ErrorCode;
+use RZP\Models\PaymentLink;
+use RZP\Jobs\PaymentLinkRefund;
 use RZP\Tests\Functional\TestCase;
 use RZP\Error\PublicErrorDescription;
 use RZP\Exception\BadRequestException;
@@ -190,6 +192,139 @@ class PaymentLinkTest extends TestCase
         $paymentAuth = $this->fixtures->create('payment:authorized', $paymentAttributes);
 
         $this->doAutoCapture();
+    }
+
+    public function testDeactivatePaymentLink()
+    {
+        $this->fixtures->create('payment_link', ['id' => '100000000000pl']);
+
+        $this->ba->proxyAuth();
+
+        $this->startTest();
+    }
+
+    public function testDeactivateAlreadyDeactivatedPaymentLink()
+    {
+        $attributes = [
+            'id'            => '100000000000pl',
+            'status'        => 'inactive',
+            'status_reason' => 'deactivated',
+        ];
+
+        $this->fixtures->create('payment_link', $attributes);
+
+        $this->ba->proxyAuth();
+
+        $this->startTest();
+    }
+
+    public function testActivatePaymentLink()
+    {
+        $attributes = [
+            'id'            => '100000000000pl',
+            'status'        => 'inactive',
+            'status_reason' => 'deactivated',
+        ];
+
+        $this->fixtures->create('payment_link', $attributes);
+
+        $this->ba->proxyAuth();
+
+        $this->startTest();
+    }
+
+    public function testActivateLinkAlreadyActivated()
+    {
+        $attributes = [
+            'id'            => '100000000000pl',
+            'status'        => 'active',
+            'status_reason' => null,
+        ];
+
+        $this->fixtures->create('payment_link', $attributes);
+
+        $this->ba->proxyAuth();
+
+        $this->startTest();
+    }
+
+    public function testActivateWithTimesPayableLessThanTimesPaid()
+    {
+        $attributes = [
+            'id'                => '100000000000pl',
+            'status'            => 'inactive',
+            'status_reason'     => 'completed',
+            'times_paid'        => 2,
+            'times_payable'     => 2,
+            'amount'            => 100,
+            'total_amount_paid' => 200,
+        ];
+
+        $this->fixtures->create('payment_link', $attributes);
+
+        $this->ba->proxyAuth();
+
+        $this->startTest();
+    }
+
+    public function testMinExpiryTimeForActivation()
+    {
+        $now = Carbon::now(Timezone::IST);
+
+        $expireBy = $now->copy()->addSeconds(120)->getTimestamp();
+
+        $attributes = [
+            'id'                => '100000000000pl',
+            'status'            => 'inactive',
+            'status_reason'     => 'expired',
+        ];
+
+        $this->fixtures->create('payment_link', $attributes);
+
+        $this->ba->proxyAuth();
+
+        $this->testData[__FUNCTION__]['request']['content']['expire_by'] = $expireBy;
+
+        $this->startTest();
+    }
+
+    public function testEditPaymentLinkToComplete()
+    {
+        Queue::fake();
+
+        $attributes = [
+            'id'                => '100000000000pl',
+            'times_payable'     => 2,
+        ];
+
+        $paymentLink = $this->fixtures->create('payment_link', $attributes);
+
+        $this->makePaymentForPaymentLinkAndAssert($paymentLink->toArray());
+
+        $paymentAttributes = [
+            'payment_link_id' => $paymentLink['id'],
+        ];
+
+        //
+        // This is not a mandatory condition, but we added this to
+        // recreate the condition for which the test is required
+        //
+        $paymentAttributes['created_at'] = time() - ((24+1) * 60 * 60);;
+
+        $paymentAuth = $this->fixtures->create('payment:authorized', $paymentAttributes);
+
+        $this->ba->proxyAuth();
+
+        $this->startTest();
+
+        $this->doAutoCapture();
+
+        Queue::assertPushed(PaymentLinkRefund::class, function($job) use ($paymentAuth)
+        {
+            $this->assertEquals($paymentAuth['id'], $job->getPaymentId());
+
+            return true;
+        });
     }
 
     // -------------------- Protected methods --------------------
