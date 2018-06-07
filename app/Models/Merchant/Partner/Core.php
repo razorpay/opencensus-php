d19993ce7ee2a7dae2c625418f52eacc3ab62f4e
Pratik Kapasi <pratik.kapasi@razorpay.com>
2018-06-07 20:29:39 +0530

    [partners] removes unused code

--- app/Models/Merchant/Partner/Core.php
+++ app/Models/Merchant/Partner/Core.php
@@ -14,18 +14,4 @@ class Core extends Merchant\Core
 
         $this->repo->saveOrFail($merchant);
     }
-
-    /**
-     * Sets the Partner type attribute as null
-     *
-     * @param string $merchantId
-     */
-    public function unmarkAsPartner(string $merchantId)
-    {
-        $merchant =  $this->repo->merchant->findOrFail($merchantId);
-
-        $merchant->setPartnerType(null);
-
-        $this->repo->saveOrFail($merchant);
-    }
 }
