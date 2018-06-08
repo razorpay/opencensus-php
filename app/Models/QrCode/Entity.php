aff34f0adcead2778d56c24b555d8195b8b4abbc
Ria Garg <ria.garg@razorpay.com>
2018-06-08 16:52:09 +0530

    Enables Bharat Qr Webhooks

--- app/Models/QrCode/Entity.php
+++ app/Models/QrCode/Entity.php
@@ -43,6 +43,7 @@ class Entity extends Base\PublicEntity
 
     protected $public = [
         self::ID,
+        self::ENTITY,
         self::REFERENCE,
         self::SHORT_URL,
         self::CREATED_AT,
