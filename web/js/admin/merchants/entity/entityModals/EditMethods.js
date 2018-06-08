c82bf45c13fee11345037a7cf1f6aec23158d2d2
rchaubey <dev.rohit.chaubey@gmail.com>
2018-06-08 14:59:18 +0530

    [mod] change edit methods rendering list

--- web/js/admin/merchants/entity/entityModals/EditMethods.js
+++ web/js/admin/merchants/entity/entityModals/EditMethods.js
@@ -27,15 +27,11 @@ export default ({ props, merchantId }) => {
     const fields = [];
     const methods = {};
 
-    _getForceFields().map(method => {
-      // Assign a default of false and override if we have it
-      methods[method] = '0';
-
-      if (methods.hasOwnProperty(method)) {
-        methods[method] = props.merchant.details.methods[method] ? '1' : '0';
+    Object.keys(props.merchant.details.methods).map(method => {
+      if (exclusionFields.indexOf(method) < 0) {
+        methods[method] = props.merchant.details.methods[method] | 0;
       }
     });
-
     defaultMethods = { ...methods };
 
     for (let method in methods) {
@@ -58,6 +54,10 @@ export default ({ props, merchantId }) => {
   /* Submit button action */
   function onSubmit(body) {
     body = filterChangedMethods(body);
+
+    if (Object.keys(body).length === 0) {
+      return notifyError('Please change a method or methods to proceed.');
+    }
 
     for (let method in body) {
       if (body[method] === '1') {
@@ -107,28 +107,13 @@ export default ({ props, merchantId }) => {
   );
 };
 
-function _getForceFields() {
-  // List all the methods here
-  // This lets us display methods that are not returned by the API as false
-  return [
-    'aeps',
-    'bank_transfer',
-    'mobikwik',
-    'payzapp',
-    'payumoney',
-    'olamoney',
-    'mpesa',
-    'upi',
-    'airtelmoney',
-    'freecharge',
-    'emi',
-    'amex',
-    'netbanking',
-    'emandate',
-    'debit_card',
-    'credit_card',
-    'jiomoney',
-    'openwallet',
-    'sbibuddy',
-  ];
-}
+//exclude fields from `methods` which are not required
+const exclusionFields = [
+  //extra fields
+  'card',
+  'disabled_banks',
+  'entity',
+  'merchant_id',
+  //method fields
+  'paytm',
+];
