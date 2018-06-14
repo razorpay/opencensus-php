362c62ad5cd1b014955c59bf165f865f283e51c3
Ria Garg <ria.garg@razorpay.com>
2018-06-14 23:27:14 +0530

    fixes bad back merge

--- app/Http/Route.php
+++ app/Http/Route.php
@@ -1254,7 +1254,6 @@ final class Route
         'tax_group_delete',
         'tax_get_meta_states',
         'tax_get_meta_gst_taxes',
-        'bharat_qr_pay_test',
         'payment_link_get',
         'payment_link_list',
         'payment_link_create',
