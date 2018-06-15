62fdd4149999ac7e67de3dc122570b21af58aea3
Pratik Kapasi <pratik.kapasi@razorpay.com>
2018-06-15 17:20:03 +0530

    [partners] adds partner type into the public array

--- app/Models/Merchant/Entity.php
+++ app/Models/Merchant/Entity.php
@@ -243,6 +243,7 @@ class Entity extends Base\PublicEntity
         self::HANDLE,
         self::RISK_RATING,
         self::RISK_THRESHOLD,
+        self::PARTNER_TYPE,
         self::CREATED_AT,
         self::UPDATED_AT,
         self::SUSPENDED_AT,
