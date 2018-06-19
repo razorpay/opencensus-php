8b80ed31286d94619c45877eff2e82cf213fe57d
Anshul Sahni <anshul.sahni@razorpay.com>
2018-06-19 18:09:11 +0530

    [PARTNER]: added modal for adding sub-merchants  - this modal opens when a user clicks on add new merchant button in list view  - mocks the user object passed to isEmailMandatory method  - mocks method to be used to for api calls

---
+++ web/js/merchant/containers/PartnerDashboard/SubMerchant/AddMerchant.js
@@ -0,0 +1,89 @@
+import { Component } from 'react';
+import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
+import AsyncButton from 'react-async-button';
+
+import ModalHeader from 'rzp/ui/ModalHeader';
+import InputField from 'rzp/ui/Forms/InputField';
+
+import { required } from 'rzp/utils/validators';
+
+@connect(null)
+@reduxForm({
+  form: 'addMerchant',
+})
+export default class AddMerchant extends Component {
+  addNewMerchant = () => {};
+  render() {
+    const { handleSubmit, partnerType } = this.props;
+    const emailMandatory = isEmailMandatory({ partner_type: 'fully_managed' });
+    const emailValidators = emailMandatory ? [required()] : [];
+
+    return (
+      <div>
+        <ModalHeader
+          title="Add New Merchant"
+          onCloseClick={this.props.closeModal}
+        />
+        <div class="modal-body">
+          {/* Merchant Name */}
+          <div class="form-group">
+            <label class="label-required">Merchant Name</label>
+            <Field
+              name="name"
+              component={InputField}
+              class="form-control"
+              autoFocus
+              validate={required()}
+            />
+          </div>
+
+          {/* Merchant Email */}
+          <div class="form-group">
+            <label class={emailMandatory ? 'label-required' : ''}>
+              Email Address
+            </label>
+            <Field
+              name="email"
+              component={InputField}
+              validate={emailValidators}
+              placeholder={emailMandatory ? '' : 'Optional'}
+              class="form-control"
+            />
+            <span class="help-block">
+              The Razorpay sign-up link will be sent to this email.
+            </span>
+
+            {!emailMandatory && (
+              <span class="help-block">
+                If no email is provided, your email will be mapped as the
+                registered email ID of this merchant.
+              </span>
+            )}
+          </div>
+
+          <div class="Modal__Actions clearfix">
+            <AsyncButton
+              class="btn btn-primary btn-block"
+              text="Add New Merchant"
+              pendingText="Adding..."
+              onClick={handleSubmit(this.addNewMerchant)}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+function isEmailMandatory(user) {
+  // a feature check will also be added for partner_type as aggregator
+  return emailMandatoryMap[user.partner_type];
+}
+
+const emailMandatoryMap = {
+  bank: true,
+  reseller: true,
+  fully_managed: false,
+  aggregator: false,
+};
