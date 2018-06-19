ec450d959f2648e2815e0bf6656b81acc6a7cc75
aseemgupta <aseemgupta2625@gmail.com>
2018-06-19 14:10:25 +0530

    Remove notes from pp filter now

--- web/js/merchant/containers/PaymentPages/Pages/List.js
+++ web/js/merchant/containers/PaymentPages/Pages/List.js
@@ -139,15 +139,6 @@ export default class PaymentPagesContainer extends ListContainer {
             </Field>
           </div>
 
-          <div class="form-group list-filter-item">
-            <label>Notes</label>
-            <Field
-              name="notes"
-              component="input"
-              class="form-control input-sm"
-            />
-          </div>
-
           <div class="form-group list-filter-item count">
             <label>Count</label>
             <Field
