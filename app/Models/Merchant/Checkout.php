857bfeb641639315ae193571565d03631be2b4ba
Saurav Chowdhury <saurav1991@gmail.com>
2018-06-21 19:01:49 +0530

    [offer] adds emi method details in preferences response for emi offers (#8569)

--- app/Models/Merchant/Checkout.php
+++ app/Models/Merchant/Checkout.php
@@ -4,26 +4,25 @@ namespace RZP\Models\Merchant;
 
 use App;
 use Request;
-use RZP\Base\RepositoryManager;
 use Session;
+use Razorpay\Trace\Logger as Trace;
 
-use RZP\Error\ErrorCode;
 use RZP\Exception;
 use RZP\Models\Base;
-use RZP\Models\Customer;
 use RZP\Models\Emi;
-use RZP\Models\Feature;
-use RZP\Models\Gateway\Downtime;
-use RZP\Models\Invoice;
-use RZP\Models\Merchant;
 use RZP\Models\Offer;
 use RZP\Models\Order;
 use RZP\Models\Payment;
-use RZP\Models\Payment\Processor\Netbanking;
-use RZP\Models\Plan\Subscription;
-use Razorpay\Trace\Logger as Trace;
+use RZP\Models\Invoice;
+use RZP\Models\Feature;
+use RZP\Error\ErrorCode;
+use RZP\Models\Customer;
+use RZP\Models\Merchant;
 use RZP\Trace\TraceCode;
-use RZP\Models\Base\PublicCollection;
+use RZP\Base\RepositoryManager;
+use RZP\Models\Gateway\Downtime;
+use RZP\Models\Plan\Subscription;
+use RZP\Models\Payment\Processor\Netbanking;
 
 class Checkout
 {
@@ -552,7 +551,7 @@ class Checkout
         if (($order !== null) and
             ($order->hasOffers() === true))
         {
-            $this->checkAndFillOrderOffers($order, $data);
+            $this->checkAndFillOrderOffers($merchant, $order, $data);
         }
         else
         {
@@ -560,7 +559,7 @@ class Checkout
         }
     }
 
-    protected function checkAndFillOrderOffers(Order\Entity $order, array & $data)
+    protected function checkAndFillOrderOffers(Merchant\Entity $merchant, Order\Entity $order, array & $data)
     {
         $offers = $order->offers;
 
@@ -575,7 +574,7 @@ class Checkout
         {
             $offer = $offers->first();
 
-            $this->updateMethodsToEnableOnCheckout($offer, $data);
+            $this->updateMethodsToEnableOnCheckout($merchant, $offer, $data);
         }
 
         //
@@ -598,14 +597,14 @@ class Checkout
         }
     }
 
-    protected function updateMethodsToEnableOnCheckout(Offer\Entity $offer, array & $data)
+    protected function updateMethodsToEnableOnCheckout(Merchant\Entity $merchant, Offer\Entity $offer, array & $data)
     {
-        $method = $offer->getPaymentMethod();
+        $offerMethod = $offer->getPaymentMethod();
 
         // If offer method is empty we do not update methods in response
-        if (empty($method) === true)
+        if (empty($offerMethod) === true)
         {
-            return ;
+            return;
         }
 
         $enabledBanks = $data['methods'][Payment\Method::NETBANKING];
@@ -616,13 +615,20 @@ class Checkout
             'entity' => 'methods'
         ];
 
-        switch ($method)
+        switch ($offerMethod)
         {
             case Payment\Method::CARD:
             case Payment\Method::EMI:
 
-                // For card offers only set card method to true
-                $data['methods']['card'] = true;
+                $offerMethodType = $offer->getPaymentMethodType();
+
+                $emiSubvention = $merchant->getEmiSubvention();
+
+                $this->updateMethodsForCardOrEmiOffer(
+                    $data,
+                    $emiSubvention,
+                    $offerMethod,
+                    $offerMethodType);
 
                 break;
 
@@ -665,9 +671,49 @@ class Checkout
             // For other methods like UPI, we currently handle it here, by just
             // enabling the particular method.
             default:
-                $data['methods'][$method] = true;
+                $data['methods'][$offerMethod] = true;
 
                 break;
+        }
+    }
+
+    protected function updateMethodsForCardOrEmiOffer(
+        array & $data,
+        string $emiSubvention,
+        string $offerMethod,
+        string $offerMethodType = null)
+    {
+        $data['methods'][Payment\Method::CARD] = true;
+
+        if ($offerMethod === Payment\Method::EMI)
+        {
+            $emiService = new Emi\Service;
+
+            $data['methods'][Payment\Method::EMI] = true;
+
+            $data['methods']['emi_subvention']    = $emiSubvention;
+
+            $data['methods']['emi_plans']         = $emiService->all();
+
+            $data['methods']['emi_options']       = $emiService->getEmiOptions();
+        }
+
+        switch ($offerMethodType)
+        {
+            case 'credit':
+                $data['methods'][Methods\Entity::CREDIT_CARD]  = true;
+
+                break;
+
+            case 'debit':
+                $data['methods'][Methods\Entity::DEBIT_CARD]  = true;
+
+                break;
+
+            default:
+
+                $data['methods'][Methods\Entity::DEBIT_CARD]  = true;
+                $data['methods'][Methods\Entity::CREDIT_CARD] = true;
         }
     }
 
