e7d1e243a31968a12cdd57e891a43c8471febbcb
aseemgupta <aseemgupta2625@gmail.com>
2018-06-22 20:31:19 +0530

    Make 2 requests to show onboarding for first-time users

--- web/js/merchant/containers/PaymentPages/Pages/List.js
+++ web/js/merchant/containers/PaymentPages/Pages/List.js
@@ -4,6 +4,7 @@ import { Field } from 'redux-form';
 import { NavLink } from 'react-router-dom';
 import HeaderAction from 'rzp/ui/HeaderAction';
 import Pager from 'rzp/ui/Pager';
+import Spinner from 'rzp/ui/Spinner';
 import ShowWhen from 'merchant/components/ShowWhen';
 import ListContainer from 'merchant/containers/ListContainer';
 import ListFilter from 'merchant/components/ListFilter';
@@ -19,7 +20,7 @@ import { showNotification } from 'rzp/modules/notifications';
 
 import { populateRPLReduxList } from 'merchant/modules/invoices/list';
 
-import EarlyAccessRPL from './EarlyAccess';
+import OnboardingPP from './OnboardingPP';
 
 @connect(state => ({ ...state.invoices, ...state.session }), {
   showNotification,
@@ -28,7 +29,29 @@ import EarlyAccessRPL from './EarlyAccess';
 export default class PaymentPagesContainer extends ListContainer {
   state = {
     loading: true,
+    loadingAllList: true,
   };
+
+  componentDidMount() {
+    this.fetchAllEntityList();
+  }
+
+  /* Fetch all payment pages list to find whether first-time user */
+  fetchAllEntityList() {
+    fetchPaymentPagesList().then(resp => {
+      this.setState({
+        loadingAllList: false,
+      });
+
+      if (resp.data) {
+        this.setState({
+          totalPaymentPagesLength: resp.data.items.length,
+        });
+      }
+
+      return resp;
+    });
+  }
 
   fetchEntityList(params) {
     return fetchPaymentPagesList(params)
@@ -78,14 +101,164 @@ export default class PaymentPagesContainer extends ListContainer {
   };
 
   render() {
-    const { loading } = this.state;
+    const { loading, loadingAllList, totalPaymentPagesLength } = this.state;
     const { paymentPages } = this.props;
 
-    if (false) {
-      return (
-        <div class="content-wrapper">
-          <EarlyAccessRPL />
+    let content;
+
+    if (loadingAllList) {
+      content = (
+        <div class="page-spinner-container">
+          <Spinner />
         </div>
+      );
+    } else if (
+      !loadingAllList &&
+      !totalPaymentPagesLength &&
+      !paymentPages.length
+    ) {
+      // !paymentPages check is required so that while creation first time, the list would be updated while totalPaymentPagesLength still = 0
+      content = (
+        <div class="content-wrapper">
+          <OnboardingPP />
+        </div>
+      );
+    } else {
+      content = (
+        <React.Fragment>
+          <ListFilter
+            form="PaymentPagesPaymentLitFilter"
+            count={this.state.count}
+            onSearchAnalytics={this.onSearchAnalytics}
+            onClearAnalytics={this.onClearAnalytics}
+          >
+            <div class="form-group list-filter-item">
+              <label>Title</label>
+              <Field
+                name="title"
+                component="input"
+                class="form-control input-sm"
+              />
+            </div>
+
+            <div class="form-group list-filter-item">
+              <label>Receipt No.</label>
+              <Field
+                name="receipt"
+                component="input"
+                class="form-control input-sm"
+              />
+            </div>
+
+            <div class="form-group list-filter-item">
+              <label>Status</label>
+              <Field
+                name="status"
+                component="select"
+                class="form-control input-sm"
+              >
+                <option value="">All</option>
+                <option value="active">Active</option>
+                <option value="inactive">Inactive</option>
+              </Field>
+            </div>
+
+            <div class="form-group list-filter-item count">
+              <label>Count</label>
+              <Field
+                name="count"
+                component="input"
+                min={1}
+                max={100}
+                type="number"
+                class="form-control input-sm"
+              />
+            </div>
+          </ListFilter>
+
+          {loading ||
+            (!!paymentPages.length && (
+              <div class="table-responsive">
+                <table class="table table-hover table-striped">
+                  <thead>
+                    <tr>
+                      <th>Title</th>
+                      <th>Amount</th>
+                      <th>Payments Made</th>
+                      <th>Times Payable</th>
+                      <th>Total Sales</th>
+                      <th>Link Url</th>
+                      <th>Created At</th>
+                      <th>Status</th>
+                    </tr>
+                  </thead>
+                  <TableBody
+                    isLoading={loading}
+                    colSpan={8}
+                    rows={paymentPages}
+                    emptyTableMsg="No data found!"
+                  >
+                    {paymentPages.map(item => (
+                      <EntityItemRow id={item.id} key={item.id}>
+                        <td>
+                          <NavLink to={`/paymentpages/${item.id}`}>
+                            <code>{item.title}</code>
+                          </NavLink>
+                        </td>
+                        <td>
+                          <Amount
+                            value={item.amount}
+                            currency={item.currency}
+                          />
+                        </td>
+                        <td>{item.times_paid}</td>
+                        <td>{item.times_payable || '--'}</td>
+                        <td>
+                          <Amount
+                            value={item.total_amount_paid}
+                            currency={item.currency}
+                          />
+                        </td>
+                        <td>
+                          {item.short_url && (
+                            <span class="CopyLink">
+                              <span>{item.short_url}</span>
+                              <CustomClipboard
+                                value={item.short_url}
+                                onCopy={this.onCopy({
+                                  itemId: item.id,
+                                })}
+                              >
+                                <button class="btn btn-default btn-xs">
+                                  copy
+                                </button>
+                              </CustomClipboard>
+                            </span>
+                          )}
+                        </td>
+                        <td>
+                          <Time value={item.created_at} />
+                        </td>
+                        <td>
+                          <PaymentPagesStatusLabel status={item.status} />
+                        </td>
+                      </EntityItemRow>
+                    ))}
+                  </TableBody>
+                </table>
+              </div>
+            ))}
+
+          {!loading &&
+            !!paymentPages.length && (
+              <Pager
+                count={this.state.count}
+                skip={this.state.skip}
+                length={paymentPages.length}
+                onClick={this.paginate}
+              />
+            )}
+        </React.Fragment>
       );
     }
 
@@ -102,149 +275,7 @@ export default class PaymentPagesContainer extends ListContainer {
           </ShowWhen>
         </HeaderAction>
 
-        <ListFilter
-          form="PaymentPagesPaymentLitFilter"
-          count={this.state.count}
-          onSearchAnalytics={this.onSearchAnalytics}
-          onClearAnalytics={this.onClearAnalytics}
-        >
-          <div class="form-group list-filter-item">
-            <label>Title</label>
-            <Field
-              name="title"
-              component="input"
-              class="form-control input-sm"
-            />
-          </div>
-
-          <div class="form-group list-filter-item">
-            <label>Receipt No.</label>
-            <Field
-              name="receipt"
-              component="input"
-              class="form-control input-sm"
-            />
-          </div>
-
-          <div class="form-group list-filter-item">
-            <label>Status</label>
-            <Field
-              name="status"
-              component="select"
-              class="form-control input-sm"
-            >
-              <option value="">All</option>
-              <option value="active">Active</option>
-              <option value="inactive">Inactive</option>
-            </Field>
-          </div>
-
-          <div class="form-group list-filter-item count">
-            <label>Count</label>
-            <Field
-              name="count"
-              component="input"
-              min={1}
-              max={100}
-              type="number"
-              class="form-control input-sm"
-            />
-          </div>
-        </ListFilter>
-
-        {loading || paymentPages.length ? (
-          <div class="table-responsive">
-            <table class="table table-hover table-striped">
-              <thead>
-                <tr>
-                  <th>Title</th>
-                  <th>Amount</th>
-                  <th>Payments Made</th>
-                  <th>Times Payable</th>
-                  <th>Total Sales</th>
-                  <th>Link Url</th>
-                  <th>Created At</th>
-                  <th>Status</th>
-                </tr>
-              </thead>
-              <TableBody
-                isLoading={loading}
-                colSpan={8}
-                rows={paymentPages}
-                emptyTableMsg="No data found!"
-              >
-                {paymentPages.map(item => (
-                  <EntityItemRow id={item.id} key={item.id}>
-                    <td>
-                      <NavLink to={`/paymentpages/${item.id}`}>
-                        <code>{item.title}</code>
-                      </NavLink>
-                    </td>
-                    <td>
-                      <Amount value={item.amount} currency={item.currency} />
-                    </td>
-                    <td>{item.times_paid}</td>
-                    <td>{item.times_payable || '--'}</td>
-                    <td>
-                      <Amount
-                        value={item.total_amount_paid}
-                        currency={item.currency}
-                      />
-                    </td>
-                    <td>
-                      {item.short_url && (
-                        <span class="CopyLink">
-                          <span>{item.short_url}</span>
-                          <CustomClipboard
-                            value={item.short_url}
-                            onCopy={this.onCopy({
-                              itemId: item.id,
-                            })}
-                          >
-                            <button class="btn btn-default btn-xs">copy</button>
-                          </CustomClipboard>
-                        </span>
-                      )}
-                    </td>
-                    <td>
-                      <Time value={item.created_at} />
-                    </td>
-                    <td>
-                      <PaymentPagesStatusLabel status={item.status} />
-                    </td>
-                  </EntityItemRow>
-                ))}
-              </TableBody>
-            </table>
-          </div>
-        ) : (
-          <div class="Onboarding Onboarding--PaymentPages">
-            <div class="illustration" />
-            You haven't created any payment page yet.{' '}
-            <a
-              class="btn-link"
-              href="https://razorpay.com/docs/private/partial-payments/"
-              target="_blank"
-            >
-              Learn more
-            </a>
-            <br />
-            <NavLink class="btn btn-primary" to="/paymentpages/new">
-              <i class="i i-plus" />
-              <span>Create your first Payment Page</span>
-            </NavLink>
-          </div>
-        )}
-
-        {!loading &&
-          !!paymentPages.length && (
-            <Pager
-              count={this.state.count}
-              skip={this.state.skip}
-              length={paymentPages.length}
-              onClick={this.paginate}
-            />
-          )}
+        {content}
       </div>
     );
   }
---
+++ web/js/merchant/containers/PaymentPages/Pages/OnboardingPP.js
@@ -0,0 +1,16 @@
+export default () => {
+  return (
+    <div class="Onboarding Onboarding--PaymentPages">
+      <div class="illustration" />
+      You haven't created any payment page yet.{' '}
+      <a
+        class="btn-link"
+        href="https://razorpay.com/docs/private/partial-payments/"
+        target="_blank"
+      >
+        Learn more
+      </a>
+      <br />
+    </div>
+  );
+};
