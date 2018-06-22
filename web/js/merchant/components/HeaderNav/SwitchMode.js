2ef9600a82a73fc240d6355c4538f70784ada8d0
aseemgupta <aseemgupta2625@gmail.com>
2018-06-22 12:57:26 +0530

    Add style to selected mode

--- web/js/merchant/components/HeaderNav/SwitchMode.js
+++ web/js/merchant/components/HeaderNav/SwitchMode.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Dropdown, { DropdownTrigger, DropdownContent } from 'rzp/ui/Dropdown';
+import { classList } from 'common/util';
 
 const SwitchMode = ({ mode, modeFormatted, onSwitchMode }) => {
   return (
@@ -15,12 +16,18 @@ const SwitchMode = ({ mode, modeFormatted, onSwitchMode }) => {
       <DropdownContent>
         <ul class="dropdown-menu switch-modes-menu nav nav-stacked">
           <li>
-            <a onClick={() => onSwitchMode('test')}>
+            <a
+              onClick={() => onSwitchMode('test')}
+              class={classList(mode === 'test' && 'selected')}
+            >
               <span class="ModeIndicator ModeIndicator--test" /> Test Mode
             </a>
           </li>
           <li>
-            <a onClick={() => onSwitchMode('live')}>
+            <a
+              onClick={() => onSwitchMode('live')}
+              class={classList(mode === 'live' && 'selected')}
+            >
               <span class="ModeIndicator ModeIndicator--live" /> Live Mode
             </a>
           </li>
