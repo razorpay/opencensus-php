c2be54bdc6fe6ef40baae39bb3b3b3d0a5e20ce9
Albin George <albinin0002@gmail.com>
2018-06-25 16:56:55 +0530

    [hdfc][emandate] Add submerchant name to debit file

--- app/Models/Gateway/File/Processor/Emandate/Register/Hdfc.php
+++ app/Models/Gateway/File/Processor/Emandate/Register/Hdfc.php
@@ -65,6 +65,7 @@ class Hdfc extends Base
 
             $row = [
                 Headings::CLIENT_NAME                  => $data[Headings::CLIENT_NAME],
+                Headings::SUB_MERCHANT_NAME            => $data[Headings::SUB_MERCHANT_NAME],
                 Headings::CUSTOMER_NAME                => $data[Headings::CUSTOMER_NAME],
                 Headings::CUSTOMER_ACCOUNT_NUMBER      => $data[Headings::CUSTOMER_ACCOUNT_NUMBER],
                 Headings::AMOUNT                       => number_format($token->getMaxAmount() / 100, 2, '.', ''),
