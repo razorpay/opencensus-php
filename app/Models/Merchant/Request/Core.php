a56cdc382650d1dd3dcdc2dabc83a1717049db02
Pratik Kapasi <pratik.kapasi@razorpay.com>
2018-06-25 10:48:40 +0530

    [partners] final code changes

--- app/Models/Merchant/Request/Core.php
+++ app/Models/Merchant/Request/Core.php
@@ -200,7 +200,6 @@ class Core extends Base\Core
             switch ($status)
             {
                 case Status::REJECTED:
-                {
                     if (empty($rejectionReason) === false)
                     {
                         (new Reason\Core)->addRejectionReasons([$rejectionReason], $stateEntity);
@@ -209,24 +208,19 @@ class Core extends Base\Core
                     }
 
                     break;
-                }
 
                 case Status::ACTIVATED:
-                {
                     $this->activated($request);
 
                     break;
-                }
 
                 case Status::NEEDS_CLARIFICATION:
-                {
                     if (empty($needsClarificationText) === false)
                     {
                         $this->sendNeedsClarificationEmail($request, $needsClarificationText);
                     }
 
                     break;
-                }
             }
         });
 
@@ -245,28 +239,18 @@ class Core extends Base\Core
         switch (true)
         {
             case $request->isProductRequest():
-            {
                 $this->addFeatureIfNotEnabled($request);
-
                 break;
-            }
 
             case $request->isPartnerActivationRequest():
-            {
                 (new Merchant\Core)->markAsPartner($request);
-
                 break;
-            }
 
             case $request->isPartnerDeactivationRequest():
-            {
                 (new Merchant\Core)->unmarkAsPartner($request->merchant);
-
                 break;
-            }
 
             default:
-            {
                 throw new Exception\BadRequestException(
                     ErrorCode::BAD_REQUEST_MERCHANT_REQUEST_INVALID_NAME,
                     Entity::NAME,
@@ -274,7 +258,6 @@ class Core extends Base\Core
                         Entity::ID   => $request->getId(),
                         Entity::NAME => $request->getName(),
                     ]);
-            }
         }
     }
 
@@ -817,19 +800,13 @@ class Core extends Base\Core
         switch (true)
         {
             case $request->isProductRequest():
-            {
                 (new Feature\Core)->postOnboardingSubmissions($request->merchant, $submissions, $input[Entity::NAME]);
-
                 break;
-            }
 
             // Partner deactivation requests do not have any submissions to store
             case $request->isPartnerActivationRequest():
-            {
                 (new Merchant\Core)->postPartnerSubmissions($request, $submissions);
-
                 break;
-            }
         }
     }
 }
