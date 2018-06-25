a5c792b5494399623d0c0541306b7c2e5447e9d8
Albin George <albinin0002@gmail.com>
2018-06-25 17:46:43 +0530

    [hdfc][emandate] Throw an exception is the status does not match

--- app/Gateway/Netbanking/Hdfc/Status.php
+++ app/Gateway/Netbanking/Hdfc/Status.php
@@ -2,6 +2,8 @@
 
 namespace RZP\Gateway\Netbanking\Hdfc;
 
+use RZP\Exception\ReconciliationException;
+
 class Status
 {
     const DEBIT_SUCCESS = 'processed';
@@ -10,16 +12,46 @@ class Status
     const REGISTRATION_SUCCESS = 'success';
     const REGISTRATION_FAILURE = 'reject';
 
+    const REGISTRATION_FILE_STATUSES = [
+        self::REGISTRATION_SUCCESS,
+        self::REGISTRATION_FAILURE,
+    ];
+
+    const DEBIT_FILE_STATUSES = [
+        self::DEBIT_SUCCESS,
+        self::DEBIT_REJECT,
+    ];
+
+    /**
+     * @param $status
+     * @return bool
+     * @throws ReconciliationException
+     */
     public static function isRegistrationSuccess($status)
     {
         $status = strtolower($status);
 
+        if (in_array($status, self::REGISTRATION_FILE_STATUSES) === false)
+        {
+            throw new ReconciliationException('Unexpected status passed', ['status' => $status]);
+        }
+
         return ($status === self::REGISTRATION_SUCCESS);
     }
 
+    /**
+     * @param $status
+     * @return bool
+     * @throws ReconciliationException
+     */
     public static function isDebitSuccess($status)
     {
         $status = strtolower($status);
+
+        if (in_array($status, self::DEBIT_FILE_STATUSES) === false)
+        {
+            throw new ReconciliationException('Unexpected status passed', ['status' => $status]);
+        }
 
         return ($status === self::DEBIT_SUCCESS);
     }
