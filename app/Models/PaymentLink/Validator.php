ba2bb709830d1e19295d3579213f0add665d5b0a
Amit Mahbubani <amit.mahbubani@razorpay.com>
2018-06-27 21:32:19 +0530

    [ppv2] optional amount: validation

--- app/Models/PaymentLink/Validator.php
+++ app/Models/PaymentLink/Validator.php
@@ -11,11 +11,18 @@ use RZP\Constants\Timezone;
 use RZP\Exception\BadRequestException;
 use RZP\Exception\BadRequestValidationFailureException;
 
+/**
+ * Class Validator
+ *
+ * @package RZP\Models\PaymentLink
+ *
+ * @property Entity $entity
+ */
 class Validator extends Base\Validator
 {
     protected static $createRules = [
-        Entity::AMOUNT        => 'required|mysql_unsigned_int|min:100',
-        Entity::CURRENCY      => 'filled|in:INR',
+        Entity::AMOUNT        => 'sometimes|nullable|mysql_unsigned_int|min:100',
+        Entity::CURRENCY      => 'required_with:amount|nullable|in:INR',
         Entity::EXPIRE_BY     => 'sometimes|epoch|nullable|custom',
         Entity::TIMES_PAYABLE => 'sometimes|mysql_unsigned_int|min:1|nullable',
         Entity::RECEIPT       => 'sometimes|string|min:1|max:40|nullable',
@@ -25,12 +32,22 @@ class Validator extends Base\Validator
     ];
 
     protected static $editRules = [
+        Entity::AMOUNT        => 'sometimes|nullable|mysql_unsigned_int|min:100',
+        Entity::CURRENCY      => 'sometimes|nullable|in:INR',
         Entity::EXPIRE_BY     => 'sometimes|epoch|nullable|custom',
         Entity::TIMES_PAYABLE => 'sometimes|mysql_unsigned_int|min:1|nullable|custom',
         Entity::RECEIPT       => 'sometimes|string|min:1|max:40|nullable',
         Entity::TITLE         => 'filled|string|max:40',
         Entity::DESCRIPTION   => 'sometimes|string|max:2048|nullable',
         Entity::NOTES         => 'sometimes|notes',
+    ];
+
+    protected static $createValidators = [
+        'amount_currency'
+    ];
+
+    protected static $editValidators = [
+        'amount_currency'
     ];
 
     protected static $sendNotificationRules = [
@@ -42,7 +59,7 @@ class Validator extends Base\Validator
 
     public function validateExpireBy(string $attribute, int $value)
     {
-        $now = Carbon::now(Timezone::IST);
+        $now         = Carbon::now(Timezone::IST);
         $minExpireBy = $now->copy()->addSeconds(Entity::MIN_EXPIRY_SECS);
 
         if ($value < $minExpireBy->getTimestamp())
@@ -56,8 +73,11 @@ class Validator extends Base\Validator
     /**
      * Validates attribute for edit operation. Note that in edit we allow making of times_payable equal to number of
      * times_paid already and while doing so payment link goes to inactive status.
+     *
      * @param string   $attribute
      * @param int|null $value
+     *
+     * @throws BadRequestValidationFailureException
      */
     public function validateTimesPayable(string $attribute, int $value = null)
     {
@@ -76,7 +96,10 @@ class Validator extends Base\Validator
 
     /**
      * Validate times_payable attribute for activation. For activation(unlike edit), it must be greater than times_paid
+     *
      * @param int|null $value
+     *
+     * @throws BadRequestValidationFailureException
      */
     public function validateTimesPayableForActivation(int $value = null)
     {
@@ -119,6 +142,7 @@ class Validator extends Base\Validator
 
     /**
      * Validates that payment link's attributes are holding values that confirms to active state requirements.
+     *
      * @throws BadRequestValidationFailureException
      */
     public function validateShouldActivationBeAllowed()
@@ -150,7 +174,9 @@ class Validator extends Base\Validator
 
     /**
      * If amount is set for payment link, validates that amount of new payment request is same as expected
+     *
      * @param  Payment\Entity $payment
+     *
      * @throws BadRequestException
      */
     public function validatePaymentAmount(Payment\Entity $payment)
@@ -167,6 +193,20 @@ class Validator extends Base\Validator
                     'expected' => $paymentLinkAmount,
                     'actual'   => $paymentAmount,
                 ]);
+        }
+    }
+
+    public function validateAmountCurrency(array $input)
+    {
+        $amount   = array_key_exists(Entity::AMOUNT, $input) ?
+                     $input[Entity::AMOUNT] : $this->entity->getAmount();
+
+        $currency = array_key_exists(Entity::CURRENCY, $input) ?
+                     $input[Entity::CURRENCY] : $this->entity->getCurrency();
+
+        if ((empty($amount) === true) and (empty($currency) === false))
+        {
+            throw new BadRequestValidationFailureException('asd');
         }
     }
 }
