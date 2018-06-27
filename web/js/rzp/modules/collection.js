1cf6d10d89aae749c4435958c0f7160a7d2e4dd2
Anshul Sahni <anshul.sahni@razorpay.com>
2018-06-27 17:14:36 +0530

    [PARTNER]:Created reducer and fetchAll for Submerchant model

--- web/js/rzp/modules/collection.js
+++ web/js/rzp/modules/collection.js
@@ -10,6 +10,7 @@ import Settlement from 'merchant/models/Settlement';
 import Reversal from 'merchant/models/Reversal';
 import Transfer from 'merchant/models/Transfer';
 import Dispute from 'merchant/models/Dispute';
+import Submerchant from 'merchant/models/Submerchant';
 
 // useEntityReducer tells whether to use common reducer or entity-specific
 export const fetchAll = (params, Entity, namespace) => {
@@ -107,7 +108,7 @@ export const paymentsReducer = makeCollectionReducer(
   'PAYMENTS',
   {},
   // ignore( do not send to API ) "ref" param if seen present the url
-  {...defaultInitialState, blacklistQueryParams:["ref"]}
+  { ...defaultInitialState, blacklistQueryParams: ['ref'] }
 );
 
 export const fetchOrders = params => fetchAll(params, Order, 'ORDERS');
@@ -130,7 +131,7 @@ export const refundsReducer = makeCollectionReducer(
   'REFUNDS',
   {},
   // ignore( do not send to API ) "ref" param if seen present the url
-  {...defaultInitialState, blacklistQueryParams:["ref"]}
+  { ...defaultInitialState, blacklistQueryParams: ['ref'] }
 );
 
 export const fetchLinkBatches = params =>
@@ -143,3 +144,7 @@ export const settlementsReducer = makeCollectionReducer('SETTLEMENTS');
 
 export const fetchDisputes = params => fetchAll(params, Dispute, 'DISPUTES');
 export const disputesReducer = makeCollectionReducer('DISPUTES');
+
+export const fetchSubmerchants = params =>
+  fetchAll(params, Submerchant, 'SUB_MERCHANTS');
+export const submerchantsReducer = makeActionCollectionReducer('SUB_MERCHANTS');
