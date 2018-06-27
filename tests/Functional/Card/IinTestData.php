9278e02e7b20e9460c3346a27c9d095a62d96696
Sunny Aggrawal <sunnyagg@outlook.com>
2018-06-27 12:46:33 +0530

    [flows] fixes payment otp flow test

--- tests/Functional/Card/IinTestData.php
+++ tests/Functional/Card/IinTestData.php
@@ -83,7 +83,7 @@ return [
 
     'testGetPaymentOtpFlow' => [
         'request'  => [
-            'url'     => '/payment_flows',
+            'url'     => '/payment/flows',
             'content' => [
                 'iin' => '112333',
             ],
