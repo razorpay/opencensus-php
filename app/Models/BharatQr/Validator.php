c03e6e616bdc3890d95fde7992e74ac6d2e98079
Ria Garg <ria.garg@razorpay.com>
2018-06-29 13:59:29 +0530

    add min 0 in amount validation

--- app/Models/BharatQr/Validator.php
+++ app/Models/BharatQr/Validator.php
@@ -23,7 +23,7 @@ class Validator extends Base\Validator
         GatewayResponseParams::CARD_LAST4            => 'required_if:method,card',
         GatewayResponseParams::SENDER_NAME           => 'required_if:method,card',
         GatewayResponseParams::PROVIDER_REFERENCE_ID => 'required|string',
-        GatewayResponseParams::AMOUNT                => 'required|integer',
+        GatewayResponseParams::AMOUNT                => 'required|integer|min:0',
         GatewayResponseParams::GATEWAY_MERCHANT_ID   => 'required_without:gateway_mpan|string',
         GatewayResponseParams::MPAN                  => 'required_without:gateway_merchant_id|string',
     ];
