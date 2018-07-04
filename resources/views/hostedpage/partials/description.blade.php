e64be0c2ead213bfe1dd49254deb55c57f2c107c
aseemgupta <aseemgupta2625@gmail.com>
2018-07-04 12:54:07 +0530

    [Paymentpage]: Add instructions in Schinder hosted page

--- resources/views/hostedpage/partials/description.blade.php
+++ resources/views/hostedpage/partials/description.blade.php
@@ -1,3 +1,8 @@
+<?php
+    $intro_note = 'Welcome to the Schindler Group. Now, pay your Schindler service bill in 4 simple steps :';
+    $instructions = array('Enter the details for the service you availed.', 'Choose the method of payment. ', 'Pay the amount. ', 'Receive online confirmation and get a confirmation email.');
+    $end_note =  'In case of any doubts, please reach out to Schindler on the contact details mentioned above.';
+?>
 <div id="description-section">
     <div class="heading" style="font-size: 18px;">
         {{$payment_page_data['title']}}
@@ -7,18 +12,16 @@
     @if(isset($payment_page_data['description']) === true)
         <p id="payment-for"></p>
     @endif
+    <p>{{$intro_note}}</p>
 
     <ol>
-        <li>
-            Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-        </li>
-        <li>
-            Lorem Ipsum has been the industry standard dummy text ever since the 1500s.
-        </li>
-        <li>
-            Lorem Ipsum has been the industry standard dummy text ever since the 1500s.
-        </li>
+        @foreach ($instructions as $key => $ins)
+            <li>{{$ins}}</li>
+        @endforeach
     </ol>
+
+    <p>{{$end_note}}</p>
+
 
     <div class="footer description-footer">
         Powered by
