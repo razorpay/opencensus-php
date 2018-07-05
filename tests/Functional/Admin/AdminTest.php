294f21c8fc16c09ddd91800e46e3f2d1933c8f5b
ambar maini <ambar.maini@razorpay.com>
2018-07-05 13:18:27 +0530

    fixed failing test case

--- tests/Functional/Admin/AdminTest.php
+++ tests/Functional/Admin/AdminTest.php
@@ -874,7 +874,7 @@ class AdminTest extends TestCase
 
         $this->assertCount(10, $result['fields']);
 
-        $this->assertCount(147, $result['entities']);
+        $this->assertCount(148, $result['entities']);
     }
 
     public function testFetchSoftDeletedEntityForAdmin()
