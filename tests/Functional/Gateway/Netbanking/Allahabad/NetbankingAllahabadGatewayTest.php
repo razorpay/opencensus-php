4549bbf7b23adbd354fef3971d35d6a761728e4c
Aagosh <you@example.com>
2018-07-08 11:34:55 +0530

    Added tests files

---
+++ tests/Functional/Gateway/Netbanking/Allahabad/NetbankingAllahabadGatewayTest.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aagoshaggarwal
+ * Date: 06/07/18
+ * Time: 3:49 PM
+ */
---
+++ tests/Functional/Gateway/Netbanking/Allahabad/NetbankingAllahabadGatewayTestData.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aagoshaggarwal
+ * Date: 08/07/18
+ * Time: 11:33 AM
+ */
