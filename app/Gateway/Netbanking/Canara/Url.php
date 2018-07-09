72f9aa56b2562dbc7214986d3066d49431e0d786
Abhinav Venkatesh <abhinav.venkatesh@razorpay.com>
2018-07-09 12:42:34 +0530

    url file

---
+++ app/Gateway/Netbanking/Canara/Url.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace RZP\Gateway\Netbanking\Canara;
+
+class Url
+{
+
+    const LIVE_DOMAIN   = '';
+    const TEST_DOMAIN   = 'https://172.16.14.29';
+
+    const AUTHORIZE     = '/B001/merchantcorp';
+    const VERIFY        = '/B001/merchantcorp';
+}
+
