542baec05d74e40cb94240cbc71fb703765aca6d
Anshul Sahni <anshul.sahni@razorpay.com>
2018-07-10 17:28:47 +0530

    [PARTNER]:resourceUrl of submerchant model changed to /submerchants   - removed fetchAll method from submerchant entity, now it uses inherited GenericEntity's fetchAll

--- web/js/merchant/models/Submerchant.js
+++ web/js/merchant/models/Submerchant.js
@@ -2,17 +2,7 @@ import ajax from 'merchant/utils/ajax';
 import GenericEntity from './GenericEntity';
 
 export default class Submerchant extends GenericEntity {
-  resourceUrl = 'referrals';
-
-  fetchAll() {
-    const data = { mode: 'live' };
-    return this.makeGenericAjaxCall({ data }).then(response => {
-      // since /referrals api call do not return data in data.items,
-      // rather in data itself
-      response.data.items = response.data;
-      return response;
-    });
-  }
+  resourceUrl = 'submerchants';
 
   create(data) {
     return ajax({
