467ab8928ef0b4da102fe5c0b7eb54b5e600c806
Sai Neeraj Kanuri <neerajkanuri@MacBook-Pro-10.local>
2018-07-12 12:20:02 +0530

    Upi/Axis/Fields.php ready

---
+++ app/Gateway/Upi/Axis/Fields.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace RZP\Gateway\Upi\Axis;
+
+class Fields
+{
+    const MERCH_ID			= 'merchId';
+    const MERCH_CHAN_ID		= 'merchChanId';
+    const UNQ_TXN_ID		= 'unqTxnId';
+    const UNQ_CUST_ID		= 'unqCustId';
+    const AMOUNT			= 'amount';
+    const TXN_DTL			= 'txnDtl';
+    const CURRENCY			= 'currency';
+    const ORDER_ID			= 'orderId';
+    const CUSTOMER_VPA		= 'customerVpa';
+    const EXPIRY			= 'expiry';
+    const S_ID				= 'sId';
+    const TXN_REFUND_ID		= 'txnRefundId';
+    const MOB_NO			= 'mobNo';
+    const TXN_REFUND_AMOUNT	= 'txnRefundAmount';
+    const REFUND_REASON		= 'refundReason';
+    const CHECKSUM			= 'checkSum';
+}
