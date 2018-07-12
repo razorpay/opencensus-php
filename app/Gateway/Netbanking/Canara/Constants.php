0efff92f19037d1559125d99880da3021a5a0551
Abhinav Venkatesh <abhinav.venkatesh@razorpay.com>
2018-07-12 16:58:17 +0530

    constants file

---
+++ app/Gateway/Netbanking/Canara/Constants.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace RZP\Gateway\Netbanking\Canara;
+
+class Constants
+{
+    const MODE_OF_TRANSACTION_PURCHASE      = 'PUR';
+    const MODE_OF_TRANSACTION_VERIFY        = 'VRF';
+    const INDIAN_CURRENCY                   = 'INR';
+    const CLIENT_CODE                       = 'CLIENTCODE';
+    const MESSAGE                           = 'This field will contain message description';
+}
