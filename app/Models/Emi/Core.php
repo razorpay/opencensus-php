d29154f9d19bafffc6ee0617838a2aad9366ef95
Ria Garg <ria.garg@razorpay.com>
2018-07-13 12:37:08 +0530

    fixes minor comments

--- app/Models/Emi/Core.php
+++ app/Models/Emi/Core.php
@@ -21,10 +21,12 @@ class Core extends Base\Core
     {
         $emiPlans = $this->repo->emi_plan->fetchByDurationsAndBankOrNetwork($durations, $bank, $network);
 
-        $minAmounts =  $emiPlans->map(function($emiPlan, $key) {
+        $minAmounts =  $emiPlans->map(function($emiPlan) {
             return Calculator::calculateMinAmount($emiPlan->getMinAmount(), $emiPlan->getMerchantPayback());
         });
 
+        // Here we are doing max because we want maximum
+        // of the minimum amounts needed for applicabe emi plans
         return $minAmounts->max();
     }
 }
