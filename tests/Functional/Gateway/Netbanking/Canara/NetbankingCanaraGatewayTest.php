20931e453397bf37692656aea25c70966a33b097
Abhinav Venkatesh <abhinav.venkatesh@razorpay.com>
2018-07-13 17:29:56 +0530

    test case for nomarl verify flow

--- tests/Functional/Gateway/Netbanking/Canara/NetbankingCanaraGatewayTest.php
+++ tests/Functional/Gateway/Netbanking/Canara/NetbankingCanaraGatewayTest.php
@@ -2,6 +2,7 @@
 
 namespace RZP\Tests\Functional\Gateway\Netbanking\Canara;
 
+use RZP\Gateway\Netbanking\Canara;
 use RZP\Tests\Functional\Helpers\Payment\PaymentTrait;
 use RZP\Tests\Functional\TestCase;
 
@@ -38,6 +39,39 @@ class NetbankingCanaraGatewayTest extends TestCase
             $this->testData['testPaymentNetbankingEntity'], $netbankingentity);
     }
 
+    public function testPaymentVerify()
+    {
+        $payment = $this->doNetbankingCanaraAuthAndCapturePayment();
+
+        $verify = $this->verifyPayment($payment['id']);
+
+        assert($verify['payment']['verified'] === 1);
+
+        $gatewayPayment = $this->getLastEntity('netbanking', true);
+
+        $this->assertTestResponse($gatewayPayment, 'testPaymentVerifySuccessEntity');
+    }
+
+    public function testTamperedPayment()
+    {
+        $data = $this->testData[__FUNCTION__];
+        sd($data);
+
+        $this->mockFailedVerifyResponse();
+
+        $this->runRequestResponseFlow(
+            $data,
+            function()
+            {
+                $payment = $this->doNetbankingCanaraAuthAndCapturePayment();
+            });
+
+        // Assert that we don't save any information into the netbanking entity
+        $gatewayPayment = $this->getLastEntity('netbanking', true);
+
+        $this->assertTestResponse($gatewayPayment, 'testPaymentFailedNetbankingEntity');
+    }
+
     public function doNetbankingCanaraAuthAndCapturePayment()
     {
         $payment = $this->getDefaultNetbankingPaymentArray($this->bank);
@@ -45,6 +79,19 @@ class NetbankingCanaraGatewayTest extends TestCase
         $payment = $this->doAuthAndCapturePayment($payment);
 
         return $payment;
+    }
+
+    protected function mockFailedVerifyResponse()
+    {
+        $this->mockServerContentFunction(function(& $content, $action = null)
+        {
+            if ($action === 'verify')
+            {
+                $content = [
+                    Canara\ResponseFields::VERIFY_RESULT => Canara\ResponseCodeMap::RESULT_REJECTED
+                ];
+            }
+        });
     }
 
 }
