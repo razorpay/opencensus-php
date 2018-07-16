68249f170007a2860b40adac669c565c25641322
Aagosh <you@example.com>
2018-07-16 20:48:59 +0530

    added necessary fields

--- app/Models/Payment/Processor/Netbanking.php
+++ app/Models/Payment/Processor/Netbanking.php
@@ -64,6 +64,7 @@ class Netbanking
         IFSC::INDB,
         IFSC::ORBC,
         IFSC::CSBK,
+        IFSC::ALLA,
         self::PUNB_R,
         self::BARB_R,
     ];
@@ -438,7 +439,7 @@ class Netbanking
                                             self::$self,
                                             self::$selfCorp)));
     }
-
+    // @codingStandardsIgnoreLine
     public static function getSupportedBanksForTPV()
     {
         return array_values(array_unique(array_merge(self::$billdeskTPV, self::$selfTPV)));
@@ -455,7 +456,7 @@ class Netbanking
 
         return self::$functionName($bank);
     }
-
+    // @codingStandardsIgnoreLine
     public static function isBankSupportedByGatewayForTPV($bank, $gateway)
     {
         // Direct gateways are handled seperately
