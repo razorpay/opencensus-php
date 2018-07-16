ca0719942cd03d1bbafa2ecc8dd61a7b85fc04de
Abhinav Venkatesh <abhinav.venkatesh@razorpay.com>
2018-07-16 18:59:37 +0530

    refund implementation-still wip

---
+++ app/Gateway/Netbanking/Canara/RefundFile.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace RZP\Gateway\Netbanking\Canara;
+
+use Carbon\Carbon;
+use Mail;
+use Config;
+
+use RZP\Constants\Timezone;
+use RZP\Gateway\Base;
+use RZP\Gateway\Netbanking\Base\Entity as NetbankingEntity;
+use RZP\Mail\Gateway\RefundFile\Base as RefundFileMail;
+use RZP\Models\FileStore;
+use RZP\Models\Payment\Gateway;
+
+class RefundFile extends Base\RefundFile
+{
+    protected static $fileToWriteName = 'canara_Netbanking_Refunds';
+
+    public function generate($input)
+    {
+        throw new \Exception();
+        $text = $this->getRefundData($input);
+        sd($text);
+
+        $fileName = $this->getFileToWriteNameWithoutExt();
+
+        $creator = $this->createFile(
+            FileStore\Format::TXT,
+            $text,
+            $fileName,
+            FileStore\Type::CANARA_NETBANKING_REFUND
+        );
+
+        $file = $creator->get();
+
+        $signedFileUrl = $creator->getSignedUrl(self::SIGNED_URL_DURATION)['url'];
+
+        $fileData = [
+            'file_path'  => $file['local_file_path'],
+            'file_name'  => basename($file['local_file_path']),
+            'signed_url' => $signedFileUrl,
+        ];
+
+        $this->sendRefundEmail($fileData, (array) $input['email']);
+
+        return $file['local_file_path'];
+    }
+
+    protected function sendRefundEmail($fileData = [], array $email = [])
+    {
+        $refundFileMail = new RefundFileMail($fileData, Gateway::NETBANKING_CANARA, $email);
+
+        Mail::queue($refundFileMail);
+    }
+
+    protected function getRefundData(array $input)
+    {
+        $data = [];
+
+        // For first line
+        $data[]    = [
+                        RefundFileFields::TRANSACTION_DATE_TIME,
+                        RefundFileFields::REFUND_DATE,
+                        RefundFileFields::BANK_REF_NO,
+                        RefundFileFields::PG_REF_NUM,
+                        RefundFileFields::REFUND_REFERENCE,
+                        RefundFileFields::TRANSACTION_AMOUNT,
+                        RefundFileFields::REFUND_AMOUNT
+                    ];
+
+        foreach ($input['data'] as $row)
+        {
+            $data[] = $this->getDataForRow(
+                $row['payment']['created_at'],
+                $row['refund']['created_at'],
+                $row['gateway']['bank_payment_id'],
+                $row['payment']['id'],
+                $row['refund']['id'],
+                $row['payment']['amount'],
+                $row['refund']['amount']
+            );
+        }
+
+        return $this->generateText($data, '|', true);
+    }
+
+    protected function getDataForRow(
+        $transactionDate,
+        $refundDate,
+        $bankRefNo,
+        $pgRefNo,
+        $refundRef,
+        $transactionAmount,
+        $refundAmount
+    )
+    {
+        $transactionDate = Carbon::createFromTimestamp((int) $transactionDate, Timezone::IST)
+                                   ->format('d-m-Y H:i:s');
+
+        $refundDate = Carbon::createFromTimestamp((int) $refundDate, Timezone::IST)
+                              ->format('d-m-Y H:i:s');
+
+        $data = [
+            $transactionDate,
+            $refundDate,
+            $bankRefNo,
+            $pgRefNo,
+            $refundRef,
+            $transactionAmount,
+            $refundAmount,
+        ];
+
+        return $data;
+    }
+}
---
+++ app/Gateway/Netbanking/Canara/RefundFileFields.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace RZP\Gateway\Netbanking\Canara;
+
+class RefundFileFields
+{
+    const TRANSACTION_DATE_TIME       = 'TRANSACTION DATE AND TIME';
+    const REFUND_DATE                 = 'Refund Date';
+    const BANK_REF_NO                 = 'BANK_REF_NO';
+    const PG_REF_NUM                  = 'PG_REF_NUM';
+    const REFUND_REFERENCE            = 'Refund Reference';
+    const TRANSACTION_AMOUNT          = 'Transaction Amount';
+    const REFUND_AMOUNT               = 'Refund Amount';
+}
