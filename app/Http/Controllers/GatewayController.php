db79df297bde915c88d6e5bed4a68e03fe1e2878
Abhinav Venkatesh <abhinav.venkatesh@razorpay.com>
2018-07-16 21:24:33 +0530

    modified controller to include canara bank

--- app/Http/Controllers/GatewayController.php
+++ app/Http/Controllers/GatewayController.php
@@ -14,6 +14,7 @@ use RZP\Models\Payment\Gateway;
 use RZP\Models\Gateway\Downtime;
 use RZP\Gateway\Upi\Base\ProviderCode;
 use RZP\Gateway\Netbanking\Corporation;
+use RZP\Gateway\Netbanking\Canara;
 use RZP\Models\Gateway\Priority as GatewayPriority;
 use RZP\Gateway\Wallet\Amazonpay\ResponseFields as AmazonResponse;
 
@@ -210,6 +211,48 @@ class GatewayController extends Controller
         );
 
         $paymentId = $input[Corporation\ResponseFields::PAYMENT_ID];
+
+        $mode = $this->app['repo']->determineLiveOrTestModeForEntity($paymentId, 'payment');
+
+        $this->app['config']->set('database.default', $mode);
+
+        $netbanking = $this->app['repo']->netbanking->findByPaymentIdAndAction(
+            $paymentId,
+            \RZP\Gateway\Base\Action::AUTHORIZE
+        );
+
+        if ($netbanking === null)
+        {
+            throw new Exception\BadRequestValidationFailureException(
+                'Failed to find requisite payment id: ' . $paymentId);
+        }
+
+        $publicPaymentId = $netbanking->getPublicPaymentId();
+
+        $payment = $this->repo->payment->findOrFailPublic($paymentId);
+
+        $keys = $this->repo->key->getKeysForMerchant($payment->getMerchantId());
+
+        $publicKey = $keys->first()->getPublicKey($mode);
+
+        $url = $this->route->getPublicCallbackUrlWithHash($publicPaymentId, $publicKey);
+
+        $inputMsg = http_build_query($input);
+
+        $url = $url . '?' . $inputMsg;
+
+        return Redirect::to($url);
+    }
+    public function callbackCanara()
+    {
+        $input = Request::all();
+
+        $this->app['trace']->info(
+            TraceCode::NETBANKING_PAYMENT_CALLBACK,
+            [ 'input' => $input ]
+        );
+
+        $paymentId = $input[Canara\ResponseFields::PAYMENT_ID];
 
         $mode = $this->app['repo']->determineLiveOrTestModeForEntity($paymentId, 'payment');
 
