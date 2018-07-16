288ea5a890f4b094d9af73a1d9ab8b2f96b1fa49
Aagosh <you@example.com>
2018-07-16 20:34:12 +0530

    Added relevant test cases

--- tests/Functional/Gateway/Netbanking/Allahabad/NetbankingAllahabadGatewayTest.php
+++ tests/Functional/Gateway/Netbanking/Allahabad/NetbankingAllahabadGatewayTest.php
@@ -13,13 +13,15 @@ class NetbankingAllahabadGatewayTest extends TestCase
 
     public function setUp()
     {
-        $this->testDataFilePath = __DIR__ . '/NetbankingAllahabadGatewayTestData.php';
+        $this->testDataFilePath = __DIR__.'/NetbankingAllahabadGatewayTestData.php';
 
         parent::setUp();
 
         $this->gateway = 'netbanking_allahabad';
 
-        $this->payment = $this->getDefaultNetbankingPaymentArray('ALB');
+        $this->bank = 'ALLA';
+
+        $this->payment = $this->getDefaultNetbankingPaymentArray($this->bank);
 
         $this->setMockGatewayTrue();
 
@@ -28,7 +30,9 @@ class NetbankingAllahabadGatewayTest extends TestCase
 
     public function testPayment()
     {
-        $payment = $this->doAuthPayment($this->payment);
+        $terminal = $this->getLastEntity('terminal', true);
+
+        $this->doAuthAndCapturePayment($this->payment);
 
         $payment = $this->getLastEntity('payment', true);
 
@@ -36,10 +40,118 @@ class NetbankingAllahabadGatewayTest extends TestCase
 
         $gatewayPayment = $this->getLastEntity('netbanking', true);
 
-        // Assert that bank payment id exists and is an integer
-        $this->assertArrayHasKey('bank_payment_id', $gatewayPayment);
+//        $this->assertArraySelectiveEquals(
+//            $this->testData['testPaymentNetbankingEntity'], $payment
+//        );
 
-        $this->assertTrue(filter_var($gatewayPayment['bank_payment_id'],
-                FILTER_VALIDATE_INT) !== false);
+        $this->assertArrayHasKey('bank', $payment);
+
     }
+
+    public function testPaymentVerify()
+    {
+        $payment = $this->doAuthAndCapturePayment($this->payment);
+
+        $verify = $this->verifyPayment($payment['id']);
+
+        assert($verify['payment']['verified'] === 1);
+
+        $gatewayPayment = $this->getLastEntity('netbanking', true);
+
+        $this->assertTestResponse($gatewayPayment, 'testPaymentVerifySuccessEntity');
+    }
+
+    public function testAuthorizeFailed()
+    {
+        $data = $this->testData[__FUNCTION__];
+
+        $this->mockFailedCallbackResponse();
+
+        $this->runRequestResponseFlow(
+            $data,
+            function()
+            {
+                $this->doAuthAndCapturePayment($this->payment);
+            });
+
+        // Assert that we don't save any information into the netbanking entity
+        $gatewayPayment = $this->getLastEntity('netbanking', true);
+        s($gatewayPayment);
+        $this->assertTestResponse($gatewayPayment, 'testPaymentFailedNetbankingEntity');
+    }
+
+    public function testAmountTampering()
+    {
+        $data = $this->testData[__FUNCTION__];
+
+        $this->mockAmountTampering();
+
+        $this->runRequestResponseFlow(
+            $data,
+            function()
+            {
+                $this->doAuthAndCapturePayment($this->payment);
+            });
+
+        $gatewayPayment = $this->getLastEntity('netbanking',true);
+
+        $this->assertTestResponse($gatewayPayment,'testPaymentFailedNetbankingEntity');
+
+    }
+
+    public function testFailedChecksum()
+    {
+        $data = $this->testData[__FUNCTION__];
+
+        $this->mockFailedChecksum();
+
+        $this->runRequestResponseFlow(
+            $data,
+            function()
+            {
+                $this->doAuthAndCapturePayment($this->payment);
+            }
+        );
+
+        $gatewayPayment = $this->getLastEntity('netbanking',true);
+
+        $this->assertTestResponse($gatewayPayment,'testPaymentFailedNetbankingEntity');
+
+    }
+
+    protected function mockFailedCallbackResponse()
+    {
+        $this->mockServerContentFunction(function(& $content, $action = null)
+        {
+            if($action === 'authorize')
+            {
+                $content['PAID'] = "N";
+            }
+        });
+    }
+
+    protected function mockAmountTampering()
+    {
+        $this->mockServerContentFunction(function(& $content, $action = null)
+        {
+            if($action === 'authorize')
+            {
+                $content['AMT'] = 100;
+            }
+        });
+    }
+
+    protected function mockFailedChecksum()
+    {
+        $this->mockServerContentFunction(function(& $content, $action = null)
+        {
+            if($action === 'authorize')
+            {
+                $content['bank_signature'] = 10000000000000000000000;
+            }
+        });
+    }
+
+
+
 }
