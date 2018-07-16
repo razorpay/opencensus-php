c8ec487071b68fb40307e29361e7c28c2af9bb5b
Ankit Agarwal <ankit.agarwal@razorpay.com>
2018-07-16 12:36:10 +0530

    [Shield]: Adding tracer for non 200 response

--- app/Services/ShieldClient.php
+++ app/Services/ShieldClient.php
@@ -283,7 +283,8 @@ class ShieldClient implements ExternalService
                 $options
             );
 
-            return $this->parseAndReturnResponse($response);
+
+            return $this->parseAndReturnResponse($response, $data);
         }
         catch(\Requests_Exception $e)
         {
@@ -299,14 +300,18 @@ class ShieldClient implements ExternalService
         return [];
     }
 
-    protected function parseAndReturnResponse($res): array
+    protected function parseAndReturnResponse($res, array $data)
     {
         $code = $res->status_code;
         $responseArray = json_decode($res->body, true);
 
         if ($code !== 200)
         {
-            $this->trace->error(TraceCode::SHIELD_INTEGRATION_ERROR, ['response' => $responseArray]);
+            $this->trace->error(TraceCode::SHIELD_INTEGRATION_ERROR,
+                    [
+                        'response' => $responseArray,
+                        'request'  => $data,
+                    ]);
         }
 
         return $responseArray;
