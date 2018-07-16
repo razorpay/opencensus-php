3397bacb175e9e5a15ec2c40b9774f7a541d4337
Aagosh <you@example.com>
2018-07-16 20:19:03 +0530

    Added URL File

---
+++ app/Gateway/Netbanking/Allahabad/Url.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace RZP\Gateway\Netbanking\Allahabad;
+
+class Url
+{
+    // TODO: Add live domain later
+    const LIVE_DOMAIN   = '';
+    const TEST_DOMAIN   = 'https://ibuat.allahabadbank.in/servlet/ibs.servlets.IBSThirdPartyPaymentServlet';
+
+    const AUTHORIZE     = '';
+    const VERIFY        = '';
+}
