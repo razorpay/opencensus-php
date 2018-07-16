e0f68b0e4482b03d86b46b04d0a8153feb5726f2
Akshay Sharma <akshay.sharma@razorpay.com>
2018-07-16 17:41:30 +0530

    Code review fixes - formatting, renaming variables and function binding

--- web/js/admin/operations/List.js
+++ web/js/admin/operations/List.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import Form from 'ui/Form';
-import Field, { SelectField, SearchableSelectField } from 'ui/Field';
+import { SearchableSelectField } from 'ui/Field';
 
 import { adminFetch } from 'common/fetch';
 import { snakeToTitleCase } from 'common/util';
@@ -14,22 +14,24 @@ export default class PublicFeaturesList extends Component {
   constructor(props) {
     super();
     this.state = {
-      dashboards: [],
-      isFetching: true,
-      activeDashboardType: '',
-      activeDashboardLabel: '',
-      isFetchingDashboard: false,
-      dashboardData: [],
-      dashboardFields: []
+      reports: [],
+      isLoading: true,
+      activeReportType: '',
+      activeTypeLabel: '',
+      isLoadingData: false,
+      data: [],
+      fields: [],
     };
+
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   componentWillMount() {
     adminFetch(`live/admin/reports/types`).then(response => {
       if (response) {
         this.setState({
-          dashboards: response,
-          isFetching: false,
+          reports: response,
+          isLoading: false,
         });
       } else {
         notifyError(response.data.errors[0]);
@@ -39,9 +41,11 @@ export default class PublicFeaturesList extends Component {
 
   createDynamicFields(data) {
     let fields = [];
+
     if (data[0]) {
       let obj = data[0];
       let keys = Object.keys(obj);
+
       keys.forEach(value =>
         fields.push([
           <span class="capitalize">{snakeToTitleCase(value)}</span>,
@@ -52,26 +56,26 @@ export default class PublicFeaturesList extends Component {
     return fields;
   }
 
-  fetchDashboard(dashboardType) {
-    let activeDashboard = this.state.dashboards.find(
-      el => el['type'] == dashboardType
-    );
-    if (activeDashboard) {
+  fetchDashboard(type) {
+    let activeReport = this.state.reports.find(el => el['type'] === type);
+
+    if (activeReport) {
       this.setState({
-        activeDashboardType: dashboardType,
-        activeDashboardLabel: activeDashboard['label'],
-        isFetchingDashboard: true,
-        dashboardData: [],
-        dashboardFields: [],
+        activeReportType: type,
+        activeTypeLabel: activeReport['label'],
+        isLoadingData: true,
+        data: [],
+        fields: [],
       });
 
-      adminFetch(`live/admin/reports/${dashboardType}`).then(response => {
+      adminFetch(`live/admin/reports/${type}`).then(response => {
         if (response) {
           let newFields = this.createDynamicFields(response);
+
           this.setState({
-            dashboardData: response,
-            dashboardFields: newFields,
-            isFetchingDashboard: false,
+            data: response,
+            fields: newFields,
+            isLoadingData: false,
           });
         } else {
           notifyError(response.data.errors[0]);
@@ -79,57 +83,64 @@ export default class PublicFeaturesList extends Component {
       });
     } else {
       this.setState({
-        activeDashboardType: dashboardType,
-        dashboardData: [],
-        dashboardFields: [],
+        activeReportType: type,
+        data: [],
+        fields: [],
       });
     }
   }
 
-  onSubmit({dashboard}) {
-    this.fetchDashboard(dashboard);
+  onSubmit({ report }) {
+    this.fetchDashboard(report);
   }
 
   render() {
-    let defaultValue = this.state.dashboards[0] ? this.state.dashboards[0]['type'] : '',
-    numberOfRows = this.state.dashboardData.length,
-    resultsStr = numberOfRows == 1 ? `${numberOfRows} result` : `${numberOfRows} results`;
+    let defaultValue = this.state.reports[0]
+        ? this.state.reports[0]['type']
+        : '',
+      numberOfRows = this.state.data.length,
+      resultsStr =
+        numberOfRows === 1
+          ? `${numberOfRows} result`
+          : `${numberOfRows} results`;
 
     return (
       <div class="list-container">
         <div class="box">
           <header>Ops Dashboard</header>
-          {this.state.isFetching ? (
+          {this.state.isLoading ? (
             <div class="spinner center" />
           ) : (
-            <Form class="filters operation-reports" onSubmit={this.onSubmit.bind(this)}>
+            <Form class="filters operation-reports" onSubmit={this.onSubmit}>
               <SearchableSelectField
                 trackBy="value"
                 label="Select a report"
-                name="dashboard"
+                name="report"
                 defaultValue={defaultValue}
-                options={this.state.dashboards.map(item => ({
+                options={this.state.reports.map(item => ({
                   name: item.label,
                   value: item.type,
                 }))}
               />
-              <button class="btn pull-right" disabled={this.state.isFetchingDashboard}>Go</button>
+              <button
+                class="btn pull-right"
+                disabled={this.state.isLoadingData}
+              >
+                Go
+              </button>
             </Form>
           )}
         </div>
 
-        {this.state.activeDashboardType ? (
+        {this.state.activeReportType ? (
           <div class="box">
             <header>
-              {this.state.activeDashboardLabel}({resultsStr})
+              {this.state.activeTypeLabel}({resultsStr})
             </header>
-            {this.state.isFetchingDashboard ? (
+            {this.state.isLoadingData ? (
               <div class="spinner center" />
             ) : (
-              <Table
-                items={this.state.dashboardData}
-                fields={this.state.dashboardFields}
-              />
+              <Table items={this.state.data} fields={this.state.fields} />
             )}
           </div>
         ) : (
