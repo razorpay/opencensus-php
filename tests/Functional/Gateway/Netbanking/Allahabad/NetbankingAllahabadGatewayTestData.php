81b4892e849cafb47931e6e7622c8399fe62854c
Aagosh <you@example.com>
2018-07-16 20:34:54 +0530

    Added test data

--- tests/Functional/Gateway/Netbanking/Allahabad/NetbankingAllahabadGatewayTestData.php
+++ tests/Functional/Gateway/Netbanking/Allahabad/NetbankingAllahabadGatewayTestData.php
@@ -1,7 +1,101 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: aagoshaggarwal
- * Date: 08/07/18
- * Time: 11:33 AM
- */
+
+use RZP\Error\ErrorCode;
+use RZP\Error\PublicErrorCode;
+use RZP\Error\PublicErrorDescription;
+
+return [
+    'testPayment' => [
+        'merchant_id'       => '10000000000000',
+        'amount'            => 50000,
+        'method'            => 'netbanking',
+        'status'            => 'captured',
+        'amount_authorized' => 50000,
+        'amount_refunded'   => 0,
+        'refund_status'     => null,
+        'currency'          => 'INR',
+        'description'       => 'random description',
+        'card_id'           => null,
+        'bank'              => 'ALLA',
+        'error_code'        => null,
+        'error_description' => null,
+        'email'             => 'a@b.com',
+        'contact'           => '+919918899029',
+        'notes'             => [
+            'merchant_order_id' => 'random order id',
+        ],
+        'gateway'           => 'netbanking_allahabad',
+        'signed'            => false,
+        'verified'          => null,
+        'entity'            => 'payment',
+    ],
+
+    'testPaymentNetbankingEntity' => [
+        'bank_payment_id' => 'AB1234',
+        'received'        => true,
+        'bank'            => 'ALLA',
+        'status'          => 'S',
+    ],
+
+    'testPaymentVerifySuccessEntity' => [
+        'received'        => true,
+        'bank'            => 'ALLA',
+        'status'          => RZP\Gateway\Netbanking\Allahabad\Status::YES,
+    ],
+
+    'testAuthorizeFailed' => [
+        'response' => [
+            'content'     => [
+                'error' => [
+                    'code'          => PublicErrorCode::BAD_REQUEST_ERROR,
+                    'description'   => PublicErrorDescription::BAD_REQUEST_PAYMENT_FAILED,
+                ],
+            ],
+            'status_code' => 400,
+        ],
+        'exception' => [
+            'class'               => 'RZP\Exception\GatewayErrorException',
+            'internal_error_code' => ErrorCode::BAD_REQUEST_PAYMENT_FAILED,
+        ],
+    ],
+
+    'testFailedChecksum' => [
+        'response' => [
+            'content'     => [
+                'error' => [
+                    'code'          => PublicErrorCode::BAD_REQUEST_ERROR,
+                    'description'   => PublicErrorDescription::SERVER_ERROR,
+                ],
+            ],
+            'status_code' => 400,
+        ],
+        'exception' => [
+            'class'               => 'RZP\Exception\BadRequestValidationFailureException',
+            'internal_error_code' => ErrorCode::BAD_REQUEST_VALIDATION_FAILURE,
+        ],
+    ],
+
+
+    'testAmountTampering' => [
+        'response' => [
+            'content'     => [
+                'error' => [
+                    'code'          => PublicErrorCode::SERVER_ERROR,
+                    'description'   => PublicErrorDescription::SERVER_ERROR,
+                ],
+            ],
+            'status_code' => 500,
+        ],
+        'exception' => [
+            'class'               => 'RZP\Exception\LogicException',
+            'internal_error_code' => ErrorCode::SERVER_ERROR_AMOUNT_TAMPERED,
+        ],
+    ],
+
+    'testPaymentFailedNetbankingEntity' => [
+        'bank_payment_id' => null,
+        'received'        => false,
+        'bank'            => 'ALLA',
+        'status'          => null
+    ],
+    ];
