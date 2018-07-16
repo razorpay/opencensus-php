e6c0f40a728ce27123ef1fdba7302182718cac5d
Aagosh <you@example.com>
2018-07-16 20:26:34 +0530

    Added Constants File

---
+++ app/Gateway/Netbanking/Allahabad/Constants.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace RZP\Gateway\Netbanking\Allahabad;
+
+class Constants
+{
+    const BANK_ID                   = 'ALB';
+    const USER_LANG_ID              = '001';
+    const USER_TYPE                 = '1';
+    const CG                        = 'Y';
+    const MODE_OF_PAYMENT_AUTH      = 'P';
+    const MODE_OF_PAYMENT_VERIFY    = 'V';
+    const STATFLG                   = 'H';
+}
