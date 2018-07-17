abb391de8ade76d5945698115d471ff67a6cfa54
jitendra-1217 <jitendraojha1217@gmail.com>
2018-07-17 11:48:08 +0530

    [metrics] Removes trace handler and related tests

--- app/Constants/Metric.php
+++ app/Constants/Metric.php
@@ -17,7 +17,6 @@ class Metric
     const ASYNC_JOBS_RECEIVED_TOTAL             = 'async_jobs_received_total';
     const ASYNC_JOBS_PROCESSED_TOTAL            = 'async_jobs_processed_total';
     const ASYNC_JOBS_ERRORS_TOTAL               = 'async_jobs_errors_total';
-    const TRACES_TOTAL                          = 'traces_total';
 
     // Summary type metric names
     // Using '.histogram' as suffix for pattern match to work(refer statsd_mapping.yml) for statsd_exporter
--- tests/Functional/Invoice/InvoiceMetricTest.php
+++ tests/Functional/Invoice/InvoiceMetricTest.php
@@ -128,20 +128,6 @@ class InvoiceMetricTest extends TestCase
                         'type'    => 'query_cache',
                     ],
                 ],
-                // [
-                //     'traces_total',
-                //     1,
-                //     [
-                //         'code'            => 'INVOICE_CREATE_REQUEST',
-                //         'context_code'    => 'INVOICE_CREATE_REQUEST',
-                //         'level'           => 200,
-                //         'level_name'      => 'INFO',
-                //         'channel'         => 'Razorpay API',
-                //         'route'           => 'invoice_create',
-                //         'rzp_mode'        => 'test',
-                //         'rzp_merchant_id' => '10000000000000',
-                //     ],
-                // ],
                 [
                     'async_jobs_received_total',
                     1,
@@ -151,20 +137,6 @@ class InvoiceMetricTest extends TestCase
                         'async_job_name'       => 'RZP_Jobs_EsSync',
                     ],
                 ],
-                // [
-                //     'traces_total',
-                //     1,
-                //     [
-                //         'code'            => 'ES_SYNC_REQUEST',
-                //         'context_code'    => 'ES_SYNC_REQUEST',
-                //         'level'           => 100,
-                //         'level_name'      => 'DEBUG',
-                //         'channel'         => 'Razorpay API',
-                //         'route'           => 'invoice_create',
-                //         'rzp_mode'        => 'test',
-                //         'rzp_merchant_id' => '10000000000000',
-                //     ],
-                // ],
                 [
                     'async_jobs_processed_total',
                     1,
@@ -174,20 +146,6 @@ class InvoiceMetricTest extends TestCase
                         'async_job_name'       => 'RZP_Jobs_EsSync',
                     ],
                 ],
-                // [
-                //     'traces_total',
-                //     1,
-                //     [
-                //         'code'            => 'INVOICE_CREATED',
-                //         'context_code'    => 'INVOICE_CREATED',
-                //         'level'           => 200,
-                //         'level_name'      => 'INFO',
-                //         'channel'         => 'Razorpay API',
-                //         'route'           => 'invoice_create',
-                //         'rzp_mode'        => 'test',
-                //         'rzp_merchant_id' => '10000000000000',
-                //     ],
-                // ],
                 [
                     'http_requests_total',
                     1,
