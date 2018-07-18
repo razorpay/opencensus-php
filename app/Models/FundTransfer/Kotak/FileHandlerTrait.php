34b9b0af98fce033914df9eab7ee5961f3e0dab2
jitendra-1217 <jitendraojha1217@gmail.com>
2018-07-18 16:50:44 +0530

    Traces excel reader config before file parse

--- app/Models/FundTransfer/Kotak/FileHandlerTrait.php
+++ app/Models/FundTransfer/Kotak/FileHandlerTrait.php
@@ -768,6 +768,8 @@ trait FileHandlerTrait
 
         $app['excel.reader']->setSelectedSheetIndices([]);
 
+        $this->traceExcelReaderConfig();
+
         $sheets = $this->parseExcelFile($filePath);
 
         $hasSingleSheet  = (count($sheets) === 1);
@@ -790,6 +792,24 @@ trait FileHandlerTrait
         // }
 
         // return $finalEntries;
+    }
+
+    /**
+     * Traces excel reader configuration, helps with debugging
+     */
+    protected function traceExcelReaderConfig()
+    {
+        $reader = app('excel.reader');
+
+        $config = [
+            'heading'                 => config('excel.import.heading'),
+            'startRow'                => config('excel.import.startRow'),
+            'force_sheets_collection' => config('excel.import.force_sheets_collection'),
+            'sheetsSelected'          => $reader->selectedSheets,
+            'selectedSheetIndices'    => $reader->selectedSheetIndices,
+        ];
+
+        $this->trace()->debug(TraceCode::EXCEL_READER_IMPORT_CONFIG, $config);
     }
 
     protected function getFileLines($file)
