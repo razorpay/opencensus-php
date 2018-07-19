fea27765ed847a0b4ab910a52c9bcc65d4785e11
Aagosh <you@example.com>
2018-07-19 20:09:33 +0530

    Added bank to obscure

--- public/test/obscure.php
+++ public/test/obscure.php
@@ -16,7 +16,8 @@ $callback_url = 'http://'.$baseurl.'/return/callback?key_id='.$key_id;
     <title>Razorpay - Testing page</title>
     <style>
         body {
-            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Ubuntu', 'Cantarell', 'Droid Sans', 'Helvetica Neue', sans-serif;
+        // @codingStandardsIgnoreLine
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Ubuntu', 'Cantarell', 'Droid Sans', 'Helvetica Neue', sans-serif;
         }
         form {
             margin: 20px auto;
@@ -96,6 +97,7 @@ function disableEmptyInputs(form) {
                 <option value="BARB_R">Bank of Baroda - Retail</option>
                 <option value="HDFC">HDFC Bank</option>
                 <option value="CORP">Corporation Bank</option>
+                <option value="ALLA">Allahabad Bank</option>
                 <option value="SBIN">SBI Bank</option>
                 <option value="ICIC">ICICI Bank</option>
                 <option value="AIRP">Airtel Payments Bank</option>
@@ -271,12 +273,14 @@ function disableEmptyInputs(form) {
     <option value="EUR">Euro</option>
     <option value="SGD">Singapore Dollar</option>
 </select>
+    // @codingStandardsIgnoreLine
 <input type="submit" value="Capture" onClick="javascript:document.capture.action = document.capture.action + document.getElementById('capture_id').value +'/capture'; document.capture.submit(); return false;"/>
 </form>
 
 <form name ="refund" method="post" action="//<?=$private_url?>/payments/">
 <input type="text" id="refund_id" placeholder="Enter payment id to refund"/>
 <input type="text" id="amount" name="amount" value="100"/>
+    // @codingStandardsIgnoreLine
 <input type="submit" value="Refund" onClick="javascript:document.refund.action = document.refund.action + document.getElementById('refund_id').value +'/refund'; document.refund.submit(); return false;"/>
 </form>
 </div>
