3e90560c9ab1a9a2c829652f232896d344c2b9e5
Abhinav Venkatesh <abhinav.venkatesh@razorpay.com>
2018-07-20 17:20:25 +0530

    redundant constant removed

--- app/Gateway/Netbanking/Canara/Constants.php
+++ app/Gateway/Netbanking/Canara/Constants.php
@@ -6,7 +6,6 @@ class Constants
 {
     const MODE_OF_TRANSACTION_PURCHASE      = 'PUR';
     const MODE_OF_TRANSACTION_VERIFY        = 'VRF';
-    const INDIAN_CURRENCY                   = 'INR';
     const CLIENT_CODE                       = 'CLIENTCODE';
     const MESSAGE                           = 'This field will contain message description';
     const SUCCESS                           = '0';
