f19367832b38e909349dccb9468084a54b17f40a
Anshul Sahni <anshul.sahni@razorpay.com>
2018-07-23 17:55:09 +0530

    [PARTNER]: added PartnerCredentials detail component for settings

---
+++ web/js/merchant/containers/PartnerDashboard/Settings/PartnerCredentials.js
@@ -0,0 +1,42 @@
+import { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { openModal, closeModal } from 'rzp/modules/modals';
+
+import DetailRow from 'merchant/components/DetailRow';
+
+@connect(null, {
+  openModal,
+  closeModal,
+})
+export default class PartnerCredentials extends Component {
+  handleViewClick = mode => () => {
+    // TODO: write code for handleViewClick
+  };
+
+  render() {
+    return (
+      <div class="list-group details-row-container">
+        {/* live credentials */}
+        <DetailRow
+          label="Live Credentials"
+          value={() => (
+            <button onClick={this.handleViewClick('live')} class="btn-link">
+              View
+            </button>
+          )}
+        />
+
+        {/* test credentials */}
+        <DetailRow
+          label="Test Credentials"
+          value={() => (
+            <button onClick={this.handleViewClick('test')} class="btn-link">
+              View
+            </button>
+          )}
+        />
+      </div>
+    );
+  }
+}
