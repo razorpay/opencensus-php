e61b18c49a3d1f4e347b4823ca24609c3b186b5e
Anshul Sahni <anshul.sahni@razorpay.com>
2018-07-23 18:00:28 +0530

    [PARTNER]: changed UI for settings/index   - rendered WebhookDetails component   - rendered PartnerCredentials component

--- web/js/merchant/containers/PartnerDashboard/Settings/index.js
+++ web/js/merchant/containers/PartnerDashboard/Settings/index.js
@@ -1,5 +1,17 @@
+import PartnerCredentials from './PartnerCredentials';
+import WebhookDetails from './WebhookDetails';
+
 export default () => (
-  <div>
-    <tabbed-container>Settings View</tabbed-container>
+  <div class="content-wrapper content-sm">
+    <div class="panel panel-default">
+      <div class="panel-heading">Webhook</div>
+      <WebhookDetails />
+    </div>
+
+    <div class="panel panel-default">
+      <div class="panel-heading">Partner Credentials</div>
+
+      <PartnerCredentials />
+    </div>
   </div>
 );
