dd7064f5f31ee98418a586f89376b517920e4819
aseemgupta <aseemgupta2625@gmail.com>
2018-07-24 18:02:48 +0530

    Add reversal details view in parent dashboard

--- web/js/merchant/routes.js
+++ web/js/merchant/routes.js
@@ -13,6 +13,7 @@ import VirtualAccountDetails from 'merchant/containers/VirtualAccounts/Details';
 import PlanDetails from 'merchant/containers/Plans/Details';
 import SubscriptionDetails from 'merchant/containers/Subscriptions/Details';
 import TransferDetails from 'merchant/containers/Marketplace/Transfers/Details';
+import ReversalDetails from 'merchant/containers/Marketplace/Reversals/Details';
 import DisputeDetails from 'merchant/containers/Disputes/Details';
 import PaymentLinkBatchDetails from 'merchant/containers/PaymentLinks/BatchDetails';
 
@@ -51,6 +52,7 @@ const entityDetailsMap = {
   '/subscriptions/:id(sub_.+)': { component: SubscriptionDetails },
 
   '/route/transfers/:id': { component: TransferDetails },
+  '/route/reversals/:id': { component: ReversalDetails },
 
   '/disputes/:id(disp_.+)': { component: DisputeDetails },
 };
