75febe375fadc9f21c0e0534a6db01a40153afd8
Anshul Sahni <anshul.sahni@razorpay.com>
2018-07-25 19:41:22 +0530

    [PARTNERS]: refactoring and typo fix in Create Merchant save process   - removed `if (data)` check in then of promise returned from `create` module method   - typo fix - successfull -> successfully   - `closeModal()` called after succefull creation

--- web/js/merchant/containers/PartnerDashboard/SubMerchant/AddMerchant.js
+++ web/js/merchant/containers/PartnerDashboard/SubMerchant/AddMerchant.js
@@ -19,13 +19,12 @@ export default class AddMerchant extends Component {
   addNewMerchant = params => {
     return this.props
       .create(params)
-      .then(data => {
-        if (data) {
-          this.props.showNotification({
-            type: 'success',
-            message: 'Submerchant created successfull',
-          });
-        }
+      .then(() => {
+        this.props.showNotification({
+          type: 'success',
+          message: 'Submerchant created successfully',
+        });
+        this.props.closeModal();
       })
       .catch(({ errors }) => {
         this.props.showNotification({
