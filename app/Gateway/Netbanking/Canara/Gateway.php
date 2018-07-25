4625413ff1a1916f7d2c7fce24d41a7d4afd3978
Abhinav Venkatesh <abhinav.venkatesh@razorpay.com>
2018-07-25 11:47:09 +0530

    added verify amount mismatch check

--- app/Gateway/Netbanking/Canara/Gateway.php
+++ app/Gateway/Netbanking/Canara/Gateway.php
@@ -90,10 +90,35 @@ class Gateway extends Base\Gateway
 
         $this->checkGatewaySuccess($verify);
 
+        $verify->amountMismatch = $this->getVerifyAmountMismatch($verify);
+
+        if ($verify->amountMismatch === true)
+        {
+            throw new Exception\LogicException(
+                'Amount tampering found.',
+                ErrorCode::SERVER_ERROR_AMOUNT_TAMPERED,
+                null,
+                null,
+                [
+                    'callback_response' => $input['gateway'],
+                    'verify_response'   => $verify->verifyResponseContent,
+                    'payment_id'        => $input['payment']['id'],
+                    'gateway'           => $this->gateway
+                ]);
+        }
+
         if ($verify->gatewaySuccess === false)
         {
             throw new Exception\GatewayErrorException(
-                ErrorCode::GATEWAY_ERROR_PAYMENT_VERIFICATION_ERROR);
+                ErrorCode::GATEWAY_ERROR_PAYMENT_VERIFICATION_ERROR,
+                null,
+                null,
+                [
+                    'callback_response' => $input['gateway'],
+                    'verify_response'   => $verify->verifyResponseContent,
+                    'payment_id'        => $input['payment']['id'],
+                    'gateway'           => $this->gateway
+                ]);
         }
     }
 
