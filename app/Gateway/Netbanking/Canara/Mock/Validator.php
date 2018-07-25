1f355521a1254b0dfe8866b55d0dadfe88f2b212
Abhinav Venkatesh <abhinav.venkatesh@razorpay.com>
2018-07-25 12:36:43 +0530

    improved validations

--- app/Gateway/Netbanking/Canara/Mock/Validator.php
+++ app/Gateway/Netbanking/Canara/Mock/Validator.php
@@ -9,16 +9,30 @@ class Validator extends Base\Validator
 {
     protected static $authRules = [
         RequestFields::CLIENT_CODE                    => 'required|alpha_num|max:40',
-        RequestFields::MERCHANT_CODE                  => 'required|string|max:100',
+        RequestFields::MERCHANT_CODE                  => 'required|string',
         RequestFields::CURRENCY                       => 'required|in:INR',
         RequestFields::AMOUNT                         => 'required|numeric|max:999999999999999',
         RequestFields::SERVICE_CHARGE                 => 'required|in:0',
         RequestFields::PAYMENT_ID                     => 'required|alpha_num|size:14',
         RequestFields::SUCCESS_STATIC_FLAG            => 'required|in:N',
         RequestFields::FAILURE_STATIC_FLAG            => 'required|in:N',
-        RequestFields::DATE                           => 'required|', // have to modify
+        RequestFields::DATE                           => 'required|date_format:"d/m/Y\+H:i:s"',
         RequestFields::MODE_OF_TRANSACTION            => 'required|in:PUR',
         RequestFields::CLIENT_ACCOUNT                 => 'sometimes',
     ];
 
+    protected static $verifyRules = [
+        RequestFields::CLIENT_CODE                    => 'required|alpha_num|max:40',
+        RequestFields::MERCHANT_CODE                  => 'required|string',
+        RequestFields::CURRENCY                       => 'required|in:INR',
+        RequestFields::AMOUNT                         => 'required|numeric|max:999999999999999',
+        RequestFields::SERVICE_CHARGE                 => 'required|in:0',
+        RequestFields::PAYMENT_ID                     => 'required|alpha_num|size:14',
+        RequestFields::SUCCESS_STATIC_FLAG            => 'required|in:N',
+        RequestFields::FAILURE_STATIC_FLAG            => 'required|in:N',
+        RequestFields::VER_DATE                       => 'required|date_format:"d/m/Y H:i:s"',
+        RequestFields::MODE_OF_TRANSACTION            => 'required|in:VRF',
+        RequestFields::CLIENT_ACCOUNT                 => 'sometimes',
+        RequestFields::PUR_DATE                       => 'required|date_format:"d/m/Y H:i:s"'
+    ];
 }
