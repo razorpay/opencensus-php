f80d2c02bed169b4aac7288897dc478848681b2f
jitendra-1217 <jitendraojha1217@gmail.com>
2018-07-26 10:50:00 +0530

    Minor view text changes for schindler payment pages

--- resources/views/hostedpage/partials/description.blade.php
+++ resources/views/hostedpage/partials/description.blade.php
@@ -1,5 +1,5 @@
 <?php
-    $intro_note = 'Welcome to the Schindler Group. Now, pay your Schindler service bill in 4 simple steps :';
+    $intro_note = 'Welcome to Schindler. Now, pay your Schindler service bill in 4 simple steps :';
     $instructions = array('Enter the details for the service you availed.', 'Choose the method of payment. ', 'Pay the amount. ', 'Receive online confirmation and get a confirmation email.');
     $end_note =  'In case of any doubts, please reach out to Schindler on';
 ?>
