c9d7064c120c0e8e10c3d13582fc5d745b2fa25e
aseemgupta <aseemgupta2625@gmail.com>
2018-07-27 18:58:30 +0530

    Add cues in transfers list view

--- web/js/merchantLA/containers/Marketplace/Transfers/List.js
+++ web/js/merchantLA/containers/Marketplace/Transfers/List.js
@@ -7,6 +7,67 @@ import TestModeBanner from 'merchantLA/containers/TestModeBanner';
 import ListContainer from 'merchant/containers/ListContainer';
 import { fetchTransfers as fetchAll } from 'merchantLA/modules/collection';
 import { transferId, amount, createdAt } from 'merchantLA/utils/item/pair';
+import { classList } from 'common/util';
+import Amount from 'rzp/ui/Amount';
+import Popover, { PopoverBody } from 'rzp/ui/Popover';
+
+const helperCues = {
+  title: '',
+  value: item => {
+    const hasReversals = item.amount_reversed > 0;
+
+    let notesMsg = 'No Notes';
+    const notesLength = item.notes && Object.keys(item.notes).length;
+
+    if (notesLength) {
+      if (notesLength === 1) {
+        notesMsg = notesLength + ' Note attached';
+      } else {
+        notesMsg = notesLength + ' Notes attached';
+      }
+    }
+
+    return (
+      <div style={{ color: 'green' }}>
+        <span>
+          <i
+            class={classList(
+              'i i-reversal cue',
+              hasReversals ? 'cue--active' : 'cue--inactive'
+            )}
+          />
+          <Popover align="top">
+            <PopoverBody>
+              <div>
+                {hasReversals ? (
+                  <span>
+                    <Amount value={item.amount_reversed} /> amount reversed
+                  </span>
+                ) : (
+                  'No Reversals'
+                )}
+              </div>
+            </PopoverBody>
+          </Popover>
+        </span>
+
+        <span>
+          <i
+            class={classList(
+              'i i-notes cue',
+              notesLength ? 'cue--active' : 'cue--inactive'
+            )}
+          />
+          <Popover align="top">
+            <PopoverBody>
+              <div>{notesMsg}</div>
+            </PopoverBody>
+          </Popover>
+        </span>
+      </div>
+    );
+  },
+};
 
 @connect(state => state.transfers, { fetchAll })
 export default class TransfersListContainer extends ListContainer {
@@ -28,7 +89,7 @@ export default class TransfersListContainer extends ListContainer {
 
               <DataTable
                 title="Transfers"
-                columns={[transferId, amount, createdAt]}
+                columns={[transferId, amount, createdAt, helperCues]}
                 count={this.state.count}
                 skip={this.state.skip}
                 paginate={this.paginate}
