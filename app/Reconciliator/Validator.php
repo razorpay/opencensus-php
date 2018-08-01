0b0efa99cb920647ec8bc7d043494cb09b1688f7
tessie <tessyjoseph1992@gmail.com>
2018-08-01 17:02:19 +0530

    [GP-236] Adds email validation for corp

--- app/Reconciliator/Validator.php
+++ app/Reconciliator/Validator.php
@@ -65,6 +65,7 @@ class Validator
                                                          . "(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)-20[0-9]{2}/",
                                                          "/Refund MIS for 116798_RAZORPAY_[0-9]{2}-[0-9]{2}-20[0-9]{2}/"
                                                      ],
+        RequestProcessor\Base::NETBANKING_CORPORATION  => ['*'],
     ];
 
     const GATEWAY_BODY_REGEX = [
@@ -93,6 +94,7 @@ class Validator
                                                         ."\s*[0-9]{2}-(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)-20[0-9]{2}/",
                                                          "/Please find attached the Refund Report as on\s*[0-9]{2}_[0-9]{2}_20[0-9]{2}/"
                                                      ],
+        RequestProcessor\Base::NETBANKING_CORPORATION  => ['*'],
     ];
 
     const GATEWAY_ATTACHMENT_COUNT = [
