cd9dcf088d63e66b0a956a1f82890eb60aae47ee
Anshul Sahni <anshul.sahni@razorpay.com>
2018-08-06 19:09:24 +0530

    [PARTNER]: modified the UI of webhookdetails in setting dashboard  Webhook details now support both live and test webhook management  Following are the Changes   - instead of WebhookModal using ManageWebhook in modal   - webhook details are being takend from local state instead of global redux state   - passing mode fo ManageWebhook container based on click   - openWebhookModal method supports currying with mode being passed   - instead of rendering webhook details render the option to manage test or live webhook   - removed unused imports

--- web/js/merchant/containers/PartnerDashboard/Settings/WebhookDetails.js
+++ web/js/merchant/containers/PartnerDashboard/Settings/WebhookDetails.js
@@ -1,74 +1,59 @@
-import { Fragment, Component } from 'react';
+import { Component } from 'react';
 import { connect } from 'react-redux';
 
 import { openModal, closeModal } from 'rzp/modules/modals';
 
-import WebhookModal from 'merchant/containers/Webhooks/New';
+import ManageWebhook from './ManageWebhook';
 
-import Time from 'rzp/ui/Time';
 import DetailRow from 'merchant/components/DetailRow';
 
 @connect(
   state => ({
     applicationId: state.applications.partnerApplication.id,
-    webhook: state.webhooks.webhooks[0],
   }),
   { openModal, closeModal }
 )
 export default class WebhookDetails extends Component {
-  openWebhookModal = () => {
+  openWebhookModal = mode => () => {
     this.props.openModal({
       component: (
-        <WebhookModal
-          webhook={this.props.webhook}
+        <ManageWebhook
           onSave={this.props.closeModal}
+          applicationId={this.props.applicationId}
+          mode={mode}
         />
       ),
     });
   };
+
   render() {
-    const webhook = this.props.webhook;
     return (
-      <div class="list-group details-row-container parnter-settings--webhook-details">
-        {webhook ? (
-          <Fragment>
-            {/* Webhook URL */}
-            <DetailRow label="URL" value={() => <code>{webhook.url}</code>} />
+      <div class="list-group details-row-container partner-settings--webhook-details">
+        {/* live webhook */}
+        <DetailRow
+          label="Live Webhook"
+          value={() => (
+            <button
+              onClick={this.openWebhookModal('live')}
+              class="btn btn-link"
+            >
+              Manage
+            </button>
+          )}
+        />
 
-            {/* Created At */}
-            <DetailRow
-              label="Created At"
-              value={() => <Time value={webhook.created_at} format="lll" />}
-            />
-
-            {/* Status for Web hook */}
-            <DetailRow
-              label="Status"
-              value={!!webhook.active ? 'Active' : 'Inactive'}
-            />
-
-            {/* Events enbaled */}
-            <DetailRow
-              label={`${
-                Object.keys(webhook.events).filter(
-                  eventType => !!webhook.events[eventType]
-                ).length
-              } events enabled`}
-              value={() => (
-                <button onClick={this.openWebhookModal} class="btn btn-link">
-                  Edit Webhook
-                </button>
-              )}
-            />
-          </Fragment>
-        ) : (
-          <button
-            onClick={this.openWebhookModal}
-            class="manage-webhook btn btn-default"
-          >
-            Manage Webhook
-          </button>
-        )}
+        {/* test webhook */}
+        <DetailRow
+          label="Test Webhook"
+          value={() => (
+            <button
+              onClick={this.openWebhookModal('test')}
+              class="btn btn-link"
+            >
+              Manage
+            </button>
+          )}
+        />
       </div>
     );
   }
