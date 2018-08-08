4c9bc85a92ef5cd2f74e8778a04e9484155f8695
tessie <tessyjoseph1992@gmail.com>
2018-08-08 13:26:07 +0530

    [GP-209]Adds combined file

---
+++ app/Reconciliator/Bob/CombinedReconciliate.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace RZP\Reconciliator\Bob;
+
+use RZP\Reconciliator\Base;
+
+class CombinedReconciliate extends Base\CombinedReconciliate
+{
+    /**
+     * Column 'Transaction Type' in excel indicates whether
+     * txn is payment or refund
+     *
+     * For payment, value is 'Purchase'
+     * For refund, value is 'Refund'
+     *
+     * @param array $row
+     * @return string|null if invalid transaction type is passed
+     */
+    protected function getReconciliationTypeForRow($row)
+    {
+        $transactionType = strtolower($row[ReconcilationFields::TRANSACTION_TYPE]);
+
+        $reconcilationTypeMapping = [
+            'purchase'  => Base\Reconciliate::PAYMENT,
+            'refund'    => Base\Reconciliate::REFUND,
+        ];
+
+        return (isset($reconcilationTypeMapping[$transactionType]) === true) ? $reconcilationTypeMapping[$transactionType] : null;
+    }
+}
