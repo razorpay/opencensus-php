cc57c163f99e14b27d286553aad348f336ca87be
krantideep95 <kranti.deep@razorpay.com>
2018-08-09 20:57:40 +0530

    update upi_icici verify API (#8957)
    * update upi/icici verify url
    
    * Upi/Icici: update tests for verify
    
    * Upi/Icici/Fields: change payerVA
    
    * rename Upi Icici test files to make them more consistent
    
    * upi/icici: add payer_va to map
    
    * Gateway/Upi/Icici: add updateGatewayPaymentResponse in verify
    
    * Upi/Icici/Mock/server: timestamp -> getTimestamp()
    
    * add test for vpa in testVerifyFailedPayment
    
    * add assertion for vpa in payment entity in testVerifyFailedPayment

--- app/Gateway/Upi/Icici/Gateway.php
+++ app/Gateway/Upi/Icici/Gateway.php
@@ -50,6 +50,7 @@ class Gateway extends Base\Gateway
         Entity::TYPE                      => Entity::TYPE,
         Entity::RECEIVED                  => Entity::RECEIVED,
         Fields::PAYER_VA                  => Entity::VPA,
+        Fields::VERIFY_PAYER_VA           => Entity::VPA,
         Fields::PAYER_NAME                => Entity::NAME,
         Fields::PAYER_MOBILE              => Entity::CONTACT,
         Fields::RESPONSE                  => Entity::STATUS_CODE,
@@ -729,6 +730,8 @@ class Gateway extends Base\Gateway
         $verify->match = ($status === VerifyResult::STATUS_MATCH) ? true : false;
 
         $verify->verifyResponseContent = $this->getMappedAttributes($content);
+
+        $this->updateGatewayPaymentResponse($verify->payment, $content);
 
         return $status;
     }
