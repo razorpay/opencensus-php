30a80d289b9ec9ffe2bb1073f549a9e9b99306b5
Akshay Sharma <akshay.sharma@razorpay.com>
2018-08-10 13:25:51 +0530

    Fixing null attributes issue

--- web/js/admin/adminActions/actionModals/BulkEditMerchants.js
+++ web/js/admin/adminActions/actionModals/BulkEditMerchants.js
@@ -14,43 +14,49 @@ import { ModalContent } from 'component/Modal';
 
 export default function BulkEditMerchants() {
   function onSubmit(body) {
-    if (body.merchant_ids) {
-      Object.keys(body.attributes).forEach(key => {
-        if (!body.attributes[key]) {
-          delete body.attributes[key];
-        }
-      });
-
-      let payload = {
-        url: `live/merchants/bulk`,
-        data: {
-          merchant_ids: splitAndFilter(body.merchant_ids, ','),
-          attributes: body.attributes,
-        },
-      };
-
-      adminPut(payload).then(response => {
-        if (response) {
-          if (response.success) {
-            notifySuccess(
-              `${response.success} merchant(s) have been updated successfully.`
-            );
-          } else {
-            notifyError(`Failed to update the merchants.`);
-          }
-          closeModal();
-          openModal(
-            <ModalContent header="API Response">
-              <div class="code" style={{ width: '650px' }}>
-                {JSON.stringify(response, null, 4)}}
-              </div>
-            </ModalContent>
-          );
-        }
-      });
-    } else {
+    if (!body.merchant_ids) {
       notifyError('Merchant Ids are mandatory.');
+      return;
     }
+
+    if (!body.attributes) {
+      notifyError('Please select at least one field to update.');
+      return;
+    }
+
+    Object.keys(body.attributes).forEach(key => {
+      if (!body.attributes[key]) {
+        delete body.attributes[key];
+      }
+    });
+
+    let payload = {
+      url: `live/merchants/bulk`,
+      data: {
+        merchant_ids: splitAndFilter(body.merchant_ids, ','),
+        attributes: body.attributes,
+      },
+    };
+
+    adminPut(payload).then(response => {
+      if (response) {
+        if (response.success) {
+          notifySuccess(
+            `${response.success} merchant(s) have been updated successfully.`
+          );
+        } else {
+          notifyError(`Failed to update the merchants.`);
+        }
+        closeModal();
+        openModal(
+          <ModalContent header="API Response">
+            <div class="code" style={{ width: '650px' }}>
+              {JSON.stringify(response, null, 4)}}
+            </div>
+          </ModalContent>
+        );
+      }
+    });
   }
 
   return (
