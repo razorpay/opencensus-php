e6c3c6bcf9e5d84fd9a0d7817af14305e4180d8b
Albin George <albinin0002@gmail.com>
2018-08-10 16:39:32 +0530

    [legaldesk] Add statuses

---
+++ app/Gateway/Esigner/Legaldesk/Status.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace RZP\Gateway\Esigner\Legaldesk;
+
+class Status
+{
+    const EMANDATE_CREATE_SUCCESS   = 'success';
+    const EMANDATE_CREATE_FAILED    = 'failed';
+    const EMANDATE_CREATE_CANCELLED = 'cancelled';
+}
