0c79bc0d8390180ff0d60cf83786f39d1e8ccb3d
Vikas Naidu <vck173@gmail.com>
2018-08-14 17:29:05 +0530

    added mid (#9069)

--- app/Models/Payment/Entity.php
+++ app/Models/Payment/Entity.php
@@ -2219,7 +2219,7 @@ class Entity extends Base\PublicEntity
             '10000000000000', '6ZJzxyLFWrGs74', '7LAuMvKMcy7s0f',
             '7thBRSDflu7NHL', '87qTXzFTBLFN7i', '9sOd4xwUKox63N',
             '9fI2f7tNoAmVhu', '6H7N6hlcv29OMG', '8tiqrk8Qpc47l9',
-            '6ZLE5BE57SExGF'
+            '6ZLE5BE57SExGF', 'AaHmTPyOrH1ivc',
         ];
 
         $currentMerchantId = $this->getMerchantId();
