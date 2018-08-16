426145cd850bd8305ced4d12ebcb8d7ae9676643
Vivek <viveklakhera@gmail.com>
2018-08-16 11:11:21 +0530

    Add digio dynamic dba (#9067)

--- app/Gateway/Esigner/Digio/Gateway.php
+++ app/Gateway/Esigner/Digio/Gateway.php
@@ -183,6 +183,7 @@ class Gateway extends Base\Gateway
         $bankCode = $this->getTerminalAccessCode($input);
 
         $mcc = $this->input['terminal']['category'];
+        $serviceProviderName = $input['merchant']->getFilteredDba() ?: $this->getGatewayMerchantId2();
 
         $traceContent = $content = [
             'mandate_request_id'            => $input['payment']['id'],
@@ -194,9 +195,8 @@ class Gateway extends Base\Gateway
             // TODO: Remove sending aadhaar number later
             'aadhaar'                       => $input['token']->getAadhaarNumber(),
             'bank_identifier'               => substr($bankCode, 0, 4),
-
             'management_category'           => CategoryCode::getCategoryCodeFromMcc($mcc),
-            'service_provider_name'         => $this->getGatewayMerchantId2(),
+            'service_provider_name'         => substr($serviceProviderName, 0, 40),
             'service_provider_utility_code' => $this->getGatewayMerchantId(),
             'login_id'                      => $this->getGatewayTerminalId(),
             'customer_account_number'       => $input['token']->getAccountNumber(),
