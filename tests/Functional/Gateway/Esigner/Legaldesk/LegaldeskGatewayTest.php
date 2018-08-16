11d2a0b00512c08381f75b886b45fcc00d288bdd
Albin George <albinin0002@gmail.com>
2018-08-16 14:04:52 +0530

    [legaldesk] Initial test case

---
+++ tests/Functional/Gateway/Esigner/Legaldesk/LegaldeskGatewayTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace RZP\Tests\Functional\Gateway\Esigner\Legaldesk;
+
+use RZP\Constants\Entity;
+use RZP\Models\Feature\Constants;
+use RZP\Tests\Functional\TestCase;
+use RZP\Tests\Functional\Helpers\Payment\PaymentTrait;
+use RZP\Tests\Functional\Fixtures\Entity\TransactionTrait;
+
+class LegaldeskGatewayTest extends TestCase
+{
+    use PaymentTrait;
+    use TransactionTrait;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->fixtures->create('terminal:shared_legaldesk_terminal');
+        $this->fixtures->create(Entity::CUSTOMER);
+
+        $this->fixtures->merchant->enableEmandate();
+        $this->fixtures->merchant->addFeatures([Constants::CHARGE_AT_WILL]);
+
+        $this->gateway = 'esigner_legaldesk';
+    }
+
+    public function testSuccessfulEsignGeneration()
+    {
+        $payment = $this->getEmandatePaymentArray('UTIB', 'aadhaar', 0);
+        $payment['bank_account'] = [
+            'account_number'    => '914010009305862',
+            'ifsc'              => 'UTIB0000123',
+            'name'              => 'Test account',
+        ];
+
+        $order = $this->fixtures->create('order:emandate_order', ['amount' => $payment['amount']]);
+        $payment['order_id'] = $order->getPublicId();
+
+        $this->doAuthPayment($payment);
+    }
+
+    protected function runPaymentCallbackFlowEsignerLegaldesk($response, &$callback = null)
+    {
+        $mock = $this->isGatewayMocked();
+
+        list ($url, $method, $content) = $this->getDataForGatewayRequest($response, $callback);
+
+        if ($mock)
+        {
+            $request = $this->makeFirstGatewayPaymentMockRequest(
+                $url, $method, $content);
+        }
+
+        return $this->submitPaymentCallbackRequest($request);
+    }
+}
