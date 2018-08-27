ad4be846a92fed33acb123141488c805e28e3100
Anshul Sahni <anshul.sahni@razorpay.com>
2018-08-27 14:08:39 +0530

    [FIX][REF]: small fix and refactor in Sidebar/index.js  - Activate your account is hidden for Partner  - using classList for implementing classes in div after inside Link of activation form

--- web/js/merchant/containers/Sidebar/index.js
+++ web/js/merchant/containers/Sidebar/index.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { withRouter, Link } from 'react-router-dom';
 
 import ProgressBar from 'rzp/ui/ProgressBar';
+import { classList } from 'common/util';
 
 import { toggleMobileMenu } from 'merchant/modules/app';
 import MainNavLink from 'merchant/components/MainNavLink';
@@ -136,7 +137,10 @@ export default class Sidebar extends Component {
                 }
 
                 <div class="nav">
-                  <ShowWhen myRole="owner manager admin">
+                  <ShowWhen
+                    myRole="owner manager admin"
+                    additionalCondition={user => !user.isPartner()}
+                  >
                     {(!user.isSubmitted || !config.hasPersonalised) && (
                       <Link
                         className="activation-status-link"
@@ -144,11 +148,12 @@ export default class Sidebar extends Component {
                         onClick={this.onSidebarBannerClick}
                       >
                         <div
-                          className={`activation-status${
+                          className={classList(
+                            'activation-status',
                             user.isSubmitted && !config.hasPersonalised
-                              ? ' not-personalised'
+                              ? 'not-personalised'
                               : ''
-                          }`}
+                          )}
                         >
                           <div className="clearfix">
                             <div className="pull-left">{actionCopy}</div>
