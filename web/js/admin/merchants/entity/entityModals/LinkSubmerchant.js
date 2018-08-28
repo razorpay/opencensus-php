1341637c085253bdfdfbef0302bc5b1d400476b8
Anshul Sahni <anshul.sahni@razorpay.com>
2018-08-28 15:55:42 +0530

    [PARTNER]: submerchants listing in LinkSubmerchant Modal  - LinkSubmerchant modal does not open up till submerchants are fetched

--- web/js/admin/merchants/entity/entityModals/LinkSubmerchant.js
+++ web/js/admin/merchants/entity/entityModals/LinkSubmerchant.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import { Component } from 'react';
 
 import { adminPost } from 'common/fetch';
-import { notifySuccess, notifyError, closeModal } from 'common/modal';
+import { notifySuccess, closeModal } from 'common/modal';
 
 import { ModalContent } from 'component/Modal';
 import Form from 'ui/Form';
 import Field from 'ui/Field';
+import Table from 'ui/Table';
 import AsyncButton from 'ui/AsyncButton';
 
-export default ({ merchantId }) => {
-  const handleSubmit = ({ submerchantId }) => {
+const getSubmerchantFields = unlinkSubmerchant => [
+  ['Merchant Id', item => item.id],
+  ['Merchant Name', item => item.name],
+  [
+    'Actions',
+    item => (
+      <AsyncButton
+        class="link danger"
+        onClick={() => unlinkSubmerchant(item.id)}
+      >
+        Unlink
+      </AsyncButton>
+    ),
+  ],
+];
+
+export default class LinkSbmerchant extends Component {
+  state = {
+    submerchants: this.props.props.merchant.submerchants,
+  };
+
+  handleSubmit = ({ submerchantId }) => {
+    const { merchantId } = this.props;
     return adminPost({
       url: `live_${merchantId}/merchants/${submerchantId}/access_maps`,
       headers: { ['X-Razorpay-Account']: merchantId },
@@ -21,18 +43,40 @@ export default ({ merchantId }) => {
     });
   };
 
-  return (
-    <ModalContent header="Link Sub-Merchant">
-      <Form class="full-span full-elements" style={{ width: '350px' }}>
-        <Field label="Merchant Id" name="submerchantId" />
+  unlinkSubmerchant = submerchantId => {
+    this.props.props.unlinkSubmerchant(submerchantId).then(response => {
+      if (response) {
+        this.setState({
+          submerchants: this.state.submerchants.filter(
+            ({ id }) => id !== submerchantId
+          ),
+        });
+      }
+    });
+  };
 
-        <AsyncButton
-          text="Add"
-          class="btn"
-          pendingClass="small spinner"
-          onSubmit={handleSubmit}
+  render() {
+    return (
+      <ModalContent header="Link Sub-Merchant">
+        <Form class="full-span full-elements" style={{ width: '350px' }}>
+          <Field label="Merchant Id" name="submerchantId" />
+
+          <AsyncButton
+            text="Add"
+            class="btn"
+            pendingClass="small spinner"
+            onSubmit={this.handleSubmit}
+          />
+        </Form>
+
+        <div class="m-t">
+          <strong>Linked Submerchants</strong>
+        </div>
+        <Table
+          items={this.state.submerchants}
+          fields={getSubmerchantFields(this.unlinkSubmerchant)}
         />
-      </Form>
-    </ModalContent>
-  );
-};
+      </ModalContent>
+    );
+  }
+}
