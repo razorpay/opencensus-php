979edfe106ffcd4d1c5ab462fa6dc75d3325a65e
Abhinav Venkatesh <abhinav.venkatesh@razorpay.com>
2018-08-29 13:14:34 +0530

    Add Frequency file

---
+++ app/Gateway/Enach/Rbl/Frequency.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace RZP\Gateway\Enach\Rbl;
+
+class Frequency
+{
+    const ADHOC         = 'Adho';
+    const INTRA_DAY     = 'INDA';
+    const DAILY         = 'DAIL';
+    const WEEKLY        = 'Week';
+    const MONTHLY       = 'MNTH';
+    const BI_MONTHLY    = 'BIMN';
+    const QUARTERLY     = 'QURT';
+    const SEMI_ANNUALLY = 'MIAN';
+    const YEARLY        = 'YEAR';
+}
