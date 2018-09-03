03757a3d1c86673a78d933aae85f820770b3b986
Vikas Naidu <vck173@gmail.com>
2018-09-03 12:03:58 +0530

    set error null (#9242)
    * set error null
    
    * added tests

--- tests/Functional/Refund/RefundTest.php
+++ tests/Functional/Refund/RefundTest.php
@@ -180,7 +180,8 @@ class RefundTest extends TestCase
                 'notes'      => ['a' => 'b'],
                 'receipt'    => '2544325',
             ]);
-        $this->fixtures->base->editEntity('refund', $refund['id'], ['gateway_refunded' => false, 'status' => 'failed']);
+        $this->fixtures->base->editEntity('refund', $refund['id'], ['gateway_refunded' => false, 'status' => 'failed',
+            'error_code' => 'test', 'error_description' => 'test', 'internal_error_code' => 'test']);
 
         $refund = $this->getLastEntity('refund', true);
 
@@ -193,6 +194,9 @@ class RefundTest extends TestCase
 
         $this->assertEquals('abcdProcessed', $refund['reference1']);
         $this->assertEquals('processed', $refund['status']);
+        $this->assertNull($refund['error_code']);
+        $this->assertNull($refund['error_description']);
+        $this->assertNull($refund['internal_error_code']);
     }
 
     public function testRefundEditStatusWithoutReference()
