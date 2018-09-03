a0063139fffb56cd195c05a2ed4c2c6a5b238fc0
Abhinav Venkatesh <abhinav.venkatesh@razorpay.com>
2018-09-03 13:12:48 +0530

    Add Url file

---
+++ app/Gateway/Enach/Rbl/Url.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace RZP\Gateway\Enach\Rbl;
+
+class Url
+{
+    const LIVE_DOMAIN = ''; //TODO
+    const TEST_DOMAIN = 'https://103.14.161.144:8086';
+
+    const NPCIAUTH = '/onmags/sendRequest';
+}
