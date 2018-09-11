90ba86773b02364000f2d6cfec2d50c8ac479964
Patlola <praveen.patlola@razorpay.com>
2018-09-11 07:27:27 +0000

    type hinting dsahboard access

--- app/Models/Merchant/Service.php
+++ app/Models/Merchant/Service.php
@@ -2024,7 +2024,7 @@ class Service extends Base\Service
             $ownerId
         )
         {
-            $enableDashboardAccess = (bool) $input['dashboard_access'] ?? false;
+            $enableDashboardAccess = (bool) ($input['dashboard_access'] ?? false);
 
             unset($input['dashboard_access']);
 
