255be47a9435f31b1ac338c9ae805de5020e1195
krantideep95 <kranti.deep@razorpay.com>
2018-09-12 16:38:17 +0530

    Gateway/AxisMigs/Gateway: add request retry handler for gateway request exception (#9339)

--- app/Gateway/AxisMigs/Gateway.php
+++ app/Gateway/AxisMigs/Gateway.php
@@ -875,7 +875,16 @@ class Gateway extends Base\Gateway
 
         $this->paymentId = $input['payment']['id'];
 
-        $response = $this->postRequest($request);
+        $shouldRetry = function ($e)
+        {
+            return (in_array(get_class($e), [Exception\GatewayRequestException::class], true));
+        };
+
+        $response = $this->retryHandler(
+            [$this, 'postRequest'],
+            [$request],
+            $shouldRetry,
+            2);
 
         return $response;
     }
