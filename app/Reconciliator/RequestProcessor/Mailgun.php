2e74ad87b590ae0777acca940ea42b67e579a626
Vidya Sagar Kushwaha <vidyasagar.kushwaha@razorpay.com>
2018-09-12 10:20:38 +0530

    [recon] adds email validation for CardFss (#9324)
    * [recon] adds email validation for CardFss
    
    * [recon] changed filename constraint to lower case
    
    * [recon] changes filenames for cardFss tests to 'alltransaction'
    
    * [recon] changes test file name to alltransaction
    
    * [recon] minor change

--- app/Reconciliator/RequestProcessor/Mailgun.php
+++ app/Reconciliator/RequestProcessor/Mailgun.php
@@ -29,6 +29,7 @@ class Mailgun extends Base
         self::HITACHI,
         self::OLAMONEY,
         self::UPI_HDFC,
+        self::CARD_FSS_HDFC,
         self::UPI_ICICI,
         self::FREECHARGE,
         self::FIRST_DATA,
