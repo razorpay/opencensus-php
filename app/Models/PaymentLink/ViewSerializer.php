c6764c7159e87b617e0a0b2cee0858e654272c40
jitendra-1217 <jitendraojha1217@gmail.com>
2018-09-17 18:34:43 +0530

    Apspdcl: Sends hosted api to view, temporarily

--- app/Models/PaymentLink/ViewSerializer.php
+++ app/Models/PaymentLink/ViewSerializer.php
@@ -47,13 +47,24 @@ class ViewSerializer extends Base\Core
 
     public function serializeForHosted(): array
     {
-        return [
+        $payload = [
             'key_id'         => $this->getMerchantKeyId(),
             'is_test_mode'   => ($this->mode === Mode::TEST),
             'environment'    => $this->app->environment(),
             E::MERCHANT      => $this->serializeMerchantForHosted(),
             E::PAYMENT_LINK  => $this->serializePaymentLinkForHosted(),
         ];
+
+        // Temporary hack: Specific to Apspdcl flow, adds few endpoints to be used by general view.
+        $checkoutUrl = $this->app['config']->get('app.checkout');
+        $payload += [
+            'endpoints' => [
+                'details' => "{$checkoutUrl}/integration/{$this->mode}/apspdcl/pages/{$this->paymentLink->getPublicId()}/details",
+                'order'   => "{$checkoutUrl}/integration/{$this->mode}/apspdcl/pages/{$this->paymentLink->getPublicId()}/order",
+            ],
+        ];
+
+        return $payload;
     }
 
     public function serializeForInternal(): array
