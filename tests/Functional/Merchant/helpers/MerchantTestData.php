e87c4fae279ddaafb1eb4452ffe85e4df524ab73
thotakartheek004 <thotakartheek004@gmail.com>
2018-09-19 03:54:31 +0530

    Order status paid fix in preferences (#9287)
    * Order status paid fix in preferences
    
    * Minor change in request exception
    
    * PAY-288 Fix for fetch preferences for order in paid status
    
    * Added file
    
    * review changes
    
    * review changes
    
    * Review change

--- tests/Functional/Merchant/helpers/MerchantTestData.php
+++ tests/Functional/Merchant/helpers/MerchantTestData.php
@@ -1501,6 +1501,26 @@ return [
         ],
     ],
 
+
+    'testGetCheckoutPreferencesForPaidOrder' => [
+        'request' => [
+            'url'    => '/preferences',
+            'method' => 'get',
+        ],
+        'response' => [
+            'content' => [
+                'error' => [
+                    'code' => PublicErrorCode::BAD_REQUEST_ERROR,
+                ],
+            ],
+            'status_code' => 400,
+        ],
+        'exception' => [
+            'class' => 'RZP\Exception\BadRequestException',
+            'internal_error_code' => ErrorCode::BAD_REQUEST_PAYMENT_ORDER_ALREADY_PAID
+        ],
+    ],
+
     'testGetCheckoutPreferencesWithSharedMerchantOffer' => [
         'request' => [
             'url'    => '/preferences',
