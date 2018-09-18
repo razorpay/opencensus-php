4f4caf20921db2661df1191addaa3350d40aaee3
Abhinav Venkatesh <abhinav.venkatesh@razorpay.com>
2018-09-18 21:16:19 +0530

    Add request and response fields

---
+++ app/Gateway/Enach/Rbl/RequestFields.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace RZP\Gateway\Enach\Rbl;
+
+class RequestFields
+{
+    const MERCHANT_ID = 'MerchantID';
+    const REQUEST_XML = 'MandateReqDoc';
+    const CHECKSUM    = 'CheckSumVal';
+    const BANK_ID     = 'BankID';
+}
---
+++ app/Gateway/Enach/Rbl/ResponseFields.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace RZP\Gateway\Enach\Rbl;
+
+class ResponseFields
+{
+    const CHECKSUM      = 'CheckSumVal';
+    const RESPONSE_XML  = 'MandateRespDoc';
+    const RESPONSE_TYPE = 'RespType';
+}
