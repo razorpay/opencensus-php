8c162411c9375511270fc959c62eaaf82c81211b
Pratik Kapasi <pratik.kapasi@razorpay.com>
2018-09-19 15:54:53 +0530

    [partners] fixes broken test

--- tests/Functional/Merchant/helpers/PartnerTestData.php
+++ tests/Functional/Merchant/helpers/PartnerTestData.php
@@ -870,6 +870,37 @@ return [
         ],
     ],
 
+    'testFetchPartnerSubmerchantsFilters' => [
+        'request'  => [
+            'url'     => '/submerchants',
+            'method'  => 'GET',
+            'content' => [
+                'name'              => 'random_name_1',
+                'email'             => 'user@example.com',
+                'id'                => '10000000000009',
+                'activation_status' => 'under_review',
+            ],
+        ],
+        'response' => [
+            'content' => [
+                'entity' => 'collection',
+                'count'  => 1,
+                'items'  => [
+                    [
+                        'id'               => 'acc_10000000000009',
+                        'entity'           => 'merchant',
+                        'user'             => [],
+                        'name'             => 'random_name_1',
+                        'details'          => [
+                            'activation_status' => 'under_review',
+                        ],
+                        'dashboard_access' => false,
+                    ],
+                ],
+            ],
+        ],
+    ],
+
 
     'testFetchPartnerSubmerchantsPurePlatformFilters' => [
         'request'  => [
