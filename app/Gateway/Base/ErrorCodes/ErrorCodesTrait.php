c2b2955ecf0f79ba20db449bf3711ebe366e848d
Mukul Acharya <mukulonline.acharya@gmail.com>
2018-09-20 20:18:12 +0530

    GP-383 - added trait

---
+++ app/Gateway/Base/ErrorCodes/ErrorCodesTrait.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace RZP\Gateway\Base\ErrorCodes;
+
+use RZP\Error\ErrorCode;
+
+trait ErrorCodesTrait
+{
+    public static function getInternalCode($content, $errorType)
+    {
+        $errorCode = null;
+
+        $errorFieldsClass = static::getGatewayFieldClass();
+
+        if (isset($errorFieldsClass::$$errorType) === false)
+        {
+            return ErrorCode::GATEWAY_ERROR_UNKNOWN_ERROR;
+        }
+
+        $errorCodeFieldNameList = $errorFieldsClass::getErrorCodeFields();
+
+        foreach ($errorCodeFieldNameList as $index => $fieldName)
+        {
+            if (empty($content[$fieldName]) === true)
+            {
+                continue;
+            }
+
+            $gatewayErrorCode = static::getRelevantGatewayErrorCode($fieldName, $content);
+
+            $internalErrorMap = $errorFieldsClass::$$errorType[$fieldName];
+
+            if (isset(static::$$internalErrorMap[$gatewayErrorCode]) === false)
+            {
+                continue;
+            }
+
+            $errorCode = static::$$internalErrorMap[$gatewayErrorCode];
+            s($errorCode);
+
+            if (empty($errorCode) === false)
+            {
+                break;
+            }
+        }
+
+        if (empty($errorCode) === true)
+        {
+            $errorCode = ErrorCode::GATEWAY_ERROR_UNKNOWN_ERROR;
+        }
+
+        return $errorCode;
+    }
+
+    public static function getCalledClassDirectory()
+    {
+        return substr(get_called_class(), 0, strrpos(get_called_class(), '\\'));
+    }
+
+    public static function getGatewayFieldClass()
+    {
+        $directory = static::getCalledClassDirectory();
+
+        return $directory.'\\'.'ErrorFields'::class;
+    }
+}
