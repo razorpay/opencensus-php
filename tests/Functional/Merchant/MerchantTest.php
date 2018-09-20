9af633e1a23477854f8a4d407ddb54840914c992
pawanrajmurarka <40450445+pawanrajmurarka@users.noreply.github.com>
2018-09-20 14:28:48 +0530

    Yesbank fixes for beneficiary status persist (#9407)

--- tests/Functional/Merchant/MerchantTest.php
+++ tests/Functional/Merchant/MerchantTest.php
@@ -3217,4 +3217,33 @@ class MerchantTest extends TestCase
 
         Mail::assertQueued(BeneficiaryFileMail::class);
     }
+
+    public function testFailedBeneficiaryRegistrationWithYesbank()
+    {
+        $ba = $this->fixtures->create('bank_account');
+
+        $this->fixtures->create('nodal_beneficiary',
+            [
+                'bank_account_id'     => $ba->getId(),
+                'merchant_id'         => $ba->merchant->getId(),
+                'registration_status' => 'registered'
+            ]
+        );
+
+        $this->ba->cronAuth();
+
+        $request = [
+            'url'       => '/merchants/beneficiary/api/yesbank',
+            'method'    => 'post',
+            'content'   => [
+                'duration' => 120
+            ]
+        ];
+
+        $content = $this->makeRequestAndGetContent($request);
+
+        $nodalBeneficiary = $this->getLastEntity('nodal_beneficiary', true);
+
+        $this->assertEquals('registered', $nodalBeneficiary['registration_status']);
+    }
 }
