05f2a618d558afea07f3902d0ec6f054adecb172
pankaj kumar <pankaj.kumar@razorpay.com>
2018-09-24 21:36:42 +0530

    [instant activation] adding sample data for assertion

--- tests/Functional/Merchant/helpers/MerchantActivationTestData.php
+++ tests/Functional/Merchant/helpers/MerchantActivationTestData.php
@@ -9,8 +9,28 @@ return [
             'url'       => '/merchant/activation/business_category_details'
         ],
         'response' => [
-            'content' => [ ],
-            'status_code' => 200,
+            'content' => array (
+                'financial_services' =>
+                    array (
+                        'description' => 'Financial Services',
+                        'subcategories' =>
+                            array (
+                                'mutual_fund' =>
+                                    array (
+                                        'category' => 6211,
+                                        'description' => 'Mutual Fund',
+                                        'category2' => 'Mutual funds',
+                                        'activation_category' => 'greylist',
+                                    ),
+                                'lending' =>
+                                    array (
+                                        'category' => 6012,
+                                        'description' => 'Lending',
+                                        'category2' => 'Lending',
+                                        'activation_category' => 'greylist',
+                                    )
+                    ))),
+            'status_code' => 200
         ]
     ],
 ];
