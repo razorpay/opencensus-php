f066e24cc7218a41e74b1e058b5ffddf9ffd7a4d
Mukul Acharya <mukulonline.acharya@gmail.com>
2018-09-24 14:20:34 +0530

    GP-383 - some missed changes

--- app/Gateway/Base/ErrorCodes/ErrorCodesTrait.php
+++ app/Gateway/Base/ErrorCodes/ErrorCodesTrait.php
@@ -12,7 +12,7 @@ trait ErrorCodesTrait
 
         $errorFieldsClass = static::getGatewayFieldClass();
 
-        if (isset($errorFieldsClass::$$errorType) === false)
+        if (class_exists($errorFieldsClass) === false)
         {
             return ErrorCode::GATEWAY_ERROR_UNKNOWN_ERROR;
         }
