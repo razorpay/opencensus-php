c3273d77793f29ad25905db8300914fc19a27947
Saurav Chowdhury <saurav.chowdhury@razorpay.com>
2018-09-26 12:25:17 +0530

    adds filter for terminal enabled banks

---
+++ app/Models/Terminal/Filters/TerminalBankFilter.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace RZP\Models\Terminal\Filters;
+
+use RZP\Models\Feature;
+use RZP\Trace\TraceCode;
+use RZP\Models\Terminal;
+
+class TerminalBankFilter extends Terminal\Filter
+{
+    public function filter(array $terminals, $verbose = false)
+    {
+        if ($this->input['payment']->isNetbanking() === false)
+        {
+            return;
+        }
+
+        $applicableTerminals = [];
+
+        foreach ($terminals as $terminal)
+        {
+            $enabledBanks = (array) $terminal->getEnabledBanks();
+
+            $bank = $this->input['payment']->getBank();
+
+            if (in_array($bank, $enabledBanks, true) === true)
+            {
+                $applicableTerminals[] = $terminal;
+            }
+        }
+
+        if ($this->input['merchant']->isFeatureEnabled(Feature\Constants::TERMINAL_BANKS_FILTER) === false)
+        {
+            if (count($applicableTerminals) !== count($terminals))
+            {
+                $terminalData = array_pluck($terminals, 'gateway', 'id');
+
+                $applicableTerminalData = array_pluck($applicableTerminals, 'gateway', 'id');
+
+                $this->trace->critical(TraceCode::TERMINAL_BANK_FILTER_DIFF, [
+                    'expected' => $terminalData,
+                    'actual'   => $applicableTerminalData
+                ]);
+            }
+
+            return $terminals;
+        }
+
+        return $applicableTerminals;
+    }
+}
