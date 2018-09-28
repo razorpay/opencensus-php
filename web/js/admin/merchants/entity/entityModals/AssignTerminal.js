8713b477aca51b8159013e78c368d10effa17725
Harman Singh <harman28@gmail.com>
2018-09-28 14:13:43 +0530

    [virtual acc] Adds terminal attrs for VA terminals

--- web/js/admin/merchants/entity/entityModals/AssignTerminal.js
+++ web/js/admin/merchants/entity/entityModals/AssignTerminal.js
@@ -28,7 +28,7 @@ const gatewayMapping = {
   wallet_freecharge: 'Freecharge',
   wallet_jiomoney: 'Jiomoney',
   wallet_sbibuddy: 'SBI Buddy',
-  mobikwik: 'Mobikwik<',
+  mobikwik: 'Mobikwik',
   billdesk: 'Billdesk',
   ebs: 'EBS',
   first_data: 'FirstData',
@@ -51,6 +51,9 @@ const gatewayMapping = {
   wallet_openwallet: 'RZP Open Wallet',
   card_fss: 'Card FSS',
   enach_rbl: 'eNach RBL',
+  bt_yesbank: 'Bank Transfer - Yes Bank',
+  bt_kotak: 'Bank Transfer - Kotak',
+  bt_dashboard: 'Bank Transfer - Dashboard (Test)',
 };
 
 const gatewayAcquirerMapping = {
@@ -263,6 +266,11 @@ export default class TerminalForm extends Component {
 
           <SelectField name="emandate" label="Emandate Allowed" defaultValue="">
             <option value="" />
+            <option value="1">Yes</option>
+            <option value="0">No</option>
+          </SelectField>
+
+          <SelectField name="bank_transfer" label="Bank Transfer" defaultValue="0">
             <option value="1">Yes</option>
             <option value="0">No</option>
           </SelectField>
