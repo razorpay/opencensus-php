72869495e19cda2467de7fc60c1fabe7bf00f093
Abhay P <abhay.prabhakaran@razorpay.com>
2018-10-03 13:09:34 +0530

    adding is_array check in addQueryParamDefault

--- app/Base/RepositoryFetch.php
+++ app/Base/RepositoryFetch.php
@@ -741,6 +741,10 @@ trait RepositoryFetch
         {
             $query->whereNull($attribute);
         }
+        else if (is_array($value))
+        {
+            $query->whereIn($attribute, $value);
+        }
         else
         {
             $query->where($attribute, $value);
