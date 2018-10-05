61b6f3497834e3f287cce23d7384b1ee6c1fdd31
Vikas Naidu <vck173@gmail.com>
2018-10-05 12:40:29 +0530

    [PAY-334] [headless] separate feature for headless (#9489)
    * [PAY-334] [headless] separate feature for headless

--- app/Models/Payment/Processor/HeadlessOtp.php
+++ app/Models/Payment/Processor/HeadlessOtp.php
@@ -42,7 +42,7 @@ trait HeadlessOtp
     {
         if (($payment->isMethodCardOrEmi() === true) and
             ($this->isAuthTypeOtp($payment) === true) and
-            (Payment\Flow::isFeatureBasedFlowEnabled($this->merchant, Payment\Flow::HEADLESS_OTP) === true))
+            ($this->merchant->isFeatureEnabled(Feature\Constants::HEADLESS) === true))
         {
             if ((Payment\Gateway::supportsHeadlessBrowser($payment->getGateway()) === true) and
                 ($payment->card->iinRelation !== null) and
