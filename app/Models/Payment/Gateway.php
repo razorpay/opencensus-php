dee2fc825111283e5379a1d67ab5d37d8c584db7
Albin George <albinin0002@gmail.com>
2018-10-09 18:20:38 +0530

    eSigner routing - Override all and view current configs (#9378)
    * [esigner] Route to get all values for merchant_enach_configs
    
    * [esigner] Create DEFAULT_ESIGNER_GATEWAY to store default gateway
    
    * [esigner] Use "override" to override all other merchant configs
    
    * [esigner] Delete config route
    
    * [esigner] Fix default for getting config value
    
    * [esigner] Backward compatibilty
    With the values existing in cache
    
    * [esigner] Set default value in cache get
    
    * [esigner] Remove changes for backward compatibility
    
    * [esigner] Add merchant enach configs to put validator
    
    * [enach_rbl] Fix config in tests

--- app/Models/Payment/Gateway.php
+++ app/Models/Payment/Gateway.php
@@ -103,6 +103,12 @@ class Gateway
     // Stores raw gateway response in string format.
     const GATEWAY_RESPONSE   = 'gateway_response';
 
+    //
+    // If for a merchant, the esigner gateway is not assigned via config,
+    // the below gateway would be used
+    //
+    const DEFAULT_ESIGNER_GATEWAY = self::ESIGNER_DIGIO;
+
     const GATEWAY_ACQUIRERS = [
         self::AXIS_MIGS    => [self::ACQUIRER_AXIS, self::ACQUIRER_HDFC],
         self::HDFC         => [self::ACQUIRER_HDFC],
