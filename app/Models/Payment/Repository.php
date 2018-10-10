ee6e924c22928337110d1e98e4c73b964c69b3f0
Abhay P <abhay.prabhakaran@razorpay.com>
2018-10-10 17:03:36 +0530

    adding batch id in proxy fetch for payments

--- app/Models/Payment/Repository.php
+++ app/Models/Payment/Repository.php
@@ -57,6 +57,7 @@ class Repository extends Base\Repository
         Entity::BANK_REFERENCE  => 'sometimes|alpha_num|max:22',
         Entity::TRANSFER_ID     => 'filled|public_id|size:18',
         Entity::CAPTURED        => 'sometimes|boolean',
+        Entity::BATCH_ID        => 'sometimes|string|min:14|max:18',
         self::EXPAND . '.*'     => 'filled|string|in:card,emi_plan,disputes,transfer,transfer.recipient_settlement|custom:expand',
     ];
 
