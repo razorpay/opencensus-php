e799364f81ccecc6867d03f8c2cc6ca413af8087
kajol <kajol.nigam@razorpay.com>
2018-10-11 12:45:52 +0530

    [refunds] Remove check for failed to retry refund

--- web/js/admin/entities/entityActions/refund.js
+++ web/js/admin/entities/entityActions/refund.js
@@ -46,7 +46,7 @@ export default ({ entity, mode, updateEntity }) => {
   return (
     <div class="refund-actions">
       <ShowWhen permission="retry_refund_failed">
-        {entity.status === 'failed' && (
+        {entity.status !== 'processed' && (
           <AsyncButton
             class="btn btn-default text-primary"
             pendingClass="btn btn-default text-primary btn-pending"
