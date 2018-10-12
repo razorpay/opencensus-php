db5956b9d20e72b9bca6c582db5bae6319e02747
Patlola <praveen.patlola@razorpay.com>
2018-10-12 10:59:46 +0000

    adding id in the validation fetch rules

--- app/Models/Merchant/Account/Validator.php
+++ app/Models/Merchant/Account/Validator.php
@@ -25,5 +25,6 @@ class Validator extends Merchant\Validator
         Entity::EMAIL => 'sometimes|email',
         Fetch::SKIP   => 'sometimes|integer',
         Fetch::COUNT  => 'sometimes|integer',
+        Fetch::ID     => 'sometimes|alpha_num|size:14',
     ];
 }
