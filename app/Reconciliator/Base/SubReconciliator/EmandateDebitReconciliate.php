7c6f1abcf752041bd2553269a59ff42bb057e705
Albin George <albinin0002@gmail.com>
2018-10-12 17:10:38 +0530

    [recon] Emandate debit - Override processReconciliationRow

--- app/Reconciliator/Base/SubReconciliator/EmandateDebitReconciliate.php
+++ app/Reconciliator/Base/SubReconciliator/EmandateDebitReconciliate.php
@@ -109,4 +109,52 @@ class EmandateDebitReconciliate extends PaymentReconciliate
     {
         return true;
     }
+
+    protected function processReconciliationRow($row, $rowDetails, $paymentId)
+    {
+        // Setting reconciled attribute before pre Reconciled check to check for duplicate row
+        $this->reconciled = $this->checkIfAlreadyReconciled($this->payment);
+
+        // Increment the total count for the summary
+        $this->setSummaryCount(self::TOTAL_SUMMARY, $paymentId);
+
+        $this->runPreReconciledAtCheckRecon($rowDetails);
+
+        if ($this->reconciled === true)
+        {
+            $this->handleAlreadyReconciled($paymentId);
+
+            //
+            // Record gateway fee and service tax for reconciled payments
+            //
+            $this->recordMissingGatewayFeeAndServiceTax($rowDetails);
+        }
+        else
+        {
+            $validate = $this->validatePaymentDetails($row);
+
+            if ($validate === true)
+            {
+                $persistSuccess = $this->persistReconciliationData($rowDetails);
+
+                if ($persistSuccess === false)
+                {
+                    // Increment the failure count for the summary.
+                    $this->setSummaryCount(self::FAILURES_SUMMARY, $paymentId);
+                }
+            }
+            else
+            {
+                // Increment the failure count for the summary.
+                $this->setSummaryCount(self::FAILURES_SUMMARY, $paymentId);
+            }
+        }
+
+        //
+        // Payment can be updated from setPaymentAcquirerData before validation or
+        // from markGatewayCapturedAsTrue after validation, for both cases we are
+        // saving payment entity here from single location to save update queries
+        //
+        $this->repo->saveOrFail($this->payment);
+    }
 }
