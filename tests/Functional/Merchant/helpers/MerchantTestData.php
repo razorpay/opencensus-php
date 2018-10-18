96a1f35a5830faf10cbf9bb4a9018102bad0289e
Pratik Kapasi <pratik.kapasi@razorpay.com>
2018-10-18 16:19:53 +0530

    [ia] removes direct tests. updates other tests with the route in use

--- tests/Functional/Merchant/helpers/MerchantTestData.php
+++ tests/Functional/Merchant/helpers/MerchantTestData.php
@@ -1048,42 +1048,6 @@ return [
         ],
     ],
 
-    'testActivateMerchantWithoutBankAccount' => [
-        'request' => [
-            'content' => [],
-            'url' => '/merchants/1cXSLlUU8V9sXl/activate',
-            'method' => 'POST',
-        ],
-        'response' => [
-            'content' => [
-                'error' => [
-                    'code' => PublicErrorCode::BAD_REQUEST_ERROR,
-                    'description' => PublicErrorDescription::BAD_REQUEST_MERCHANT_NO_BANK_ACCOUNT_FOUND,
-                ],
-            ],
-            'status_code' => 400,
-        ],
-        'exception' => [
-            'class' => RZP\Exception\BadRequestException::class,
-            'internal_error_code' => ErrorCode::BAD_REQUEST_MERCHANT_NO_BANK_ACCOUNT_FOUND,
-        ]
-    ],
-
-    'testActivateMerchant' => [
-        'request' => [
-            'content' => [],
-            'url' => '/merchants/1cXSLlUU8V9sXl/activate',
-            'method' => 'POST',
-        ],
-        'response' => [
-            'content' => [
-                'entity' => 'merchant',
-                'activated' => true,
-                'live' => true,
-            ],
-        ],
-    ],
-
     'testMerchantEnableLive' => [
         'request' => [
             'content' => [],
