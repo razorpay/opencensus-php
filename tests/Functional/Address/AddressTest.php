bace7ce2bfbf4b70f514187b605da218ba85e838
Pratik Kapasi <pratik.kapasi@razorpay.com>
2018-10-22 17:58:10 +0530

    Revert "[ia] skip tests"
    This reverts commit 909db2de6266dc74cfd3be86ac44fb1fbfbaf88c.

--- tests/Functional/Address/AddressTest.php
+++ tests/Functional/Address/AddressTest.php
@@ -13,8 +13,6 @@ class AddressTest extends TestCase
 
     public function setUp()
     {
-        sd("beta");
-
         $this->testDataFilePath = __DIR__ . '/Helpers/AddressTestData.php';
 
         parent::setUp();
