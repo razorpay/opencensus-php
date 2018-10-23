a64ccec0b2333eb79ccc8bba0e499f7a03284eed
Mukul Acharya <mukulonline.acharya@gmail.com>
2018-10-23 19:28:09 +0530

    GP-446 - added none as default

--- app/Gateway/Base/Metric.php
+++ app/Gateway/Base/Metric.php
@@ -342,21 +342,21 @@ class Metric
 
         return [
             Metric::DIMENSION_GATEWAY              => $gateway,
-            Metric::DIMENSION_PAYMENT_METHOD       => '',
+            Metric::DIMENSION_PAYMENT_METHOD       => 'none',
             Metric::DIMENSION_ACTION               => $action,
-            Metric::DIMENSION_CARD_TYPE            => '',
-            Metric::DIMENSION_CARD_NETWORK         => '',
-            Metric::DIMENSION_CARD_COUNTRY         => '',
-            Metric::DIMENSION_PAYMENT_RECURRING    => '',
-            Metric::DIMENSION_INSTRUMENT_TYPE      => '',
-            Metric::DIMENSION_TPV                  => '',
-            Metric::DIMENSION_ISSUER               => '',
+            Metric::DIMENSION_CARD_TYPE            => 'none',
+            Metric::DIMENSION_CARD_NETWORK         => 'none',
+            Metric::DIMENSION_CARD_COUNTRY         => 'none',
+            Metric::DIMENSION_PAYMENT_RECURRING    => 'none',
+            Metric::DIMENSION_INSTRUMENT_TYPE      => 'none',
+            Metric::DIMENSION_TPV                  => 'none',
+            Metric::DIMENSION_ISSUER               => 'none',
             Metric::DIMENSION_UPI_PSP              => $upiPsp,
-            Metric::DIMENSION_CARD_INTERNATIONAL   => '',
-            Metric::DIMENSION_BHARAT_QR            => '',
-            Metric::DIMENSION_AUTH_TYPE            => '',
-            Metric::DIMENSION_TERMINAL_ID          => '',
-            Metric::DIMENSION_MERCHANT_CATEGORY    => ''
+            Metric::DIMENSION_CARD_INTERNATIONAL   => 'none',
+            Metric::DIMENSION_BHARAT_QR            => 'none',
+            Metric::DIMENSION_AUTH_TYPE            => 'none',
+            Metric::DIMENSION_TERMINAL_ID          => 'none',
+            Metric::DIMENSION_MERCHANT_CATEGORY    => 'none'
         ];
     }
 }
