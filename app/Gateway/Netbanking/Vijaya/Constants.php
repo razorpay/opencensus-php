4c26f22362f191d043cdf543da186432a5b9401a
Abhinav Venkatesh <abhinav.venkatesh@razorpay.com>
2018-10-24 15:16:03 +0530

    Add constants

---
+++ app/Gateway/Netbanking/Vijaya/Constants.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace RZP\Gateway\Netbanking\Vijaya;
+
+class Constants
+{
+    const MERCHANT_CONSTANT = '000000000010';
+    const ITEM_CODE         = 'Razorpay';
+    const INDIAN_CURRENCY   = 'INR';
+
+}
