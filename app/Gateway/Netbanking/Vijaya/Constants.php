0511872fbffa532fe7cfdad2bc09e84b46c9a49e
Abhinav Venkatesh <abhinav.venkatesh@razorpay.com>
2018-10-24 16:06:19 +0530

    Add payment and verify mode in constants

--- app/Gateway/Netbanking/Vijaya/Constants.php
+++ app/Gateway/Netbanking/Vijaya/Constants.php
@@ -7,5 +7,6 @@ class Constants
     const MERCHANT_CONSTANT = '000000000010';
     const ITEM_CODE         = 'Razorpay';
     const INDIAN_CURRENCY   = 'INR';
-
+    const PAYMENT_MODE      = 'P';
+    const VERIFY_MODE       = 'V';
 }
