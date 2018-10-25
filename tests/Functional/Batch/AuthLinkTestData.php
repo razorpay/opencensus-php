8bdf062cc660f6e6539dfd514453274b1b0774a7
Abhay P <abhay.prabhakaran@razorpay.com>
2018-10-25 14:42:27 +0530

    test cases

---
+++ tests/Functional/Batch/AuthLinkTestData.php
@@ -0,0 +1,145 @@
+<?php
+
+use RZP\Error\ErrorCode;
+use RZP\Models\Batch\Header;
+use RZP\Error\PublicErrorCode;
+
+return [
+
+    'testCreateBatchOfAuthLinks' => [
+        'request'  => [
+            'url'     => '/batches',
+            'method'  => 'post',
+            'content' => [
+                'type' => 'auth_link',
+            ],
+        ],
+        'response' => [
+            'content' => [
+                'entity'           => 'batch',
+                'type'             => 'auth_link',
+                'status'           => 'created',
+                'total_count'      => 4,
+                'success_count'    => 0,
+                'failure_count'    => 0,
+                'attempts'         => 0,
+                'amount'           => 0,
+                'processed_amount' => 0,
+                'processed_at'     => null,
+            ],
+        ],
+    ],
+
+    'testBatchFileValidation' => [
+        'request'  => [
+            'url'     => '/batches/validate',
+            'method'  => 'post',
+            'content' => [
+                'type' => 'auth_link',
+            ],
+        ],
+        'response' => [
+            'content' => [
+                'processable_count' => 4,
+                'error_count'       => 0,
+                'parsed_entries'    => [
+                    [
+                        Header::AUTH_LINK_CUSTOMER_NAME   => 'test',
+                        Header::AUTH_LINK_CUSTOMER_EMAIL  => 'test@test.test',
+                        Header::AUTH_LINK_CUSTOMER_PHONE  => '9999998888',
+                        Header::AUTH_LINK_AMOUNT_IN_PAISE => 0,
+                        Header::AUTH_LINK_CURRENCY        => "INR",
+                        Header::AUTH_LINK_METHOD          => 'emandate',
+                        Header::AUTH_LINK_TOKEN_EXPIRE_BY => '20-10-2018',
+                        Header::AUTH_LINK_MAX_AMOUNT      => "100000",
+                        Header::AUTH_LINK_EXPIRE_BY       => '20-10-2018',
+                        Header::AUTH_LINK_AUTH_TYPE       => 'netbanking',
+                        Header::AUTH_LINK_BANK            => "HDFC",
+                        Header::AUTH_LINK_NAME_ON_ACCOUNT => "Test",
+                        Header::AUTH_LINK_IFSC            => "HDFC0001233",
+                        Header::AUTH_LINK_ACCOUNT_NUMBER  => "1233100023891",
+                        Header::AUTH_LINK_ACCOUNT_TYPE    => "savings",
+                        Header::AUTH_LINK_RECEIPT         => '#1',
+                        Header::AUTH_LINK_DESCRIPTION     => 'test auth link',
+
+                    ],
+                    [
+                        Header::AUTH_LINK_CUSTOMER_NAME   => 'test',
+                        Header::AUTH_LINK_CUSTOMER_EMAIL  => 'test@test.test',
+                        Header::AUTH_LINK_CUSTOMER_PHONE  => '9999998888',
+                        Header::AUTH_LINK_AMOUNT_IN_PAISE => 1000,
+                        Header::AUTH_LINK_CURRENCY        => "INR",
+                        Header::AUTH_LINK_METHOD          => 'emandate',
+                        Header::AUTH_LINK_TOKEN_EXPIRE_BY => null,
+                        Header::AUTH_LINK_MAX_AMOUNT      => "100000",
+                        Header::AUTH_LINK_EXPIRE_BY       => null,
+                        Header::AUTH_LINK_AUTH_TYPE       => null,
+                        Header::AUTH_LINK_BANK            => "HDFC",
+                        Header::AUTH_LINK_NAME_ON_ACCOUNT => "Test",
+                        Header::AUTH_LINK_IFSC            => "HDFC0001233",
+                        Header::AUTH_LINK_ACCOUNT_NUMBER  => "1233100023891",
+                        Header::AUTH_LINK_ACCOUNT_TYPE    => "savings",
+                        Header::AUTH_LINK_RECEIPT         => '#2',
+                        Header::AUTH_LINK_DESCRIPTION     => 'test auth link',
+                    ],
+                    [
+                        Header::AUTH_LINK_CUSTOMER_NAME   => 'test',
+                        Header::AUTH_LINK_CUSTOMER_EMAIL  => 'test@test.test',
+                        Header::AUTH_LINK_CUSTOMER_PHONE  => '9999998888',
+                        Header::AUTH_LINK_AMOUNT_IN_PAISE => 1000,
+                        Header::AUTH_LINK_CURRENCY        => "INR",
+                        Header::AUTH_LINK_METHOD          => 'card',
+                        Header::AUTH_LINK_TOKEN_EXPIRE_BY => null,
+                        Header::AUTH_LINK_MAX_AMOUNT      => "100000",
+                        Header::AUTH_LINK_EXPIRE_BY       => null,
+                        Header::AUTH_LINK_AUTH_TYPE       => null,
+                        Header::AUTH_LINK_BANK            => "HDFC",
+                        Header::AUTH_LINK_NAME_ON_ACCOUNT => "Test",
+                        Header::AUTH_LINK_IFSC            => "HDFC0001233",
+                        Header::AUTH_LINK_ACCOUNT_NUMBER  => "1233100023891",
+                        Header::AUTH_LINK_ACCOUNT_TYPE    => "savings",
+                        Header::AUTH_LINK_RECEIPT         => '#3',
+                        Header::AUTH_LINK_DESCRIPTION     => 'test auth link',
+                    ],
+                ],
+            ],
+        ],
+    ],
+
+    'testCheckAuthLinkBatchStatus' => [
+        'request'  => [
+            'url'     => '/batches',
+            'method'  => 'post',
+            'content' => [
+                'type' => 'auth_link',
+            ],
+        ],
+        'response' => [
+            'content' => [],
+        ],
+    ],
+
+    'testValidateBatchWithInvalidHeaders' => [
+        'request'   => [
+            'url'     => '/batches/validate',
+            'method'  => 'post',
+            'content' => [
+                'type' => 'auth_link',
+            ],
+        ],
+        'response'  => [
+            'content'     => [
+                'error' => [
+                    'code'        => PublicErrorCode::BAD_REQUEST_ERROR,
+                    'description' => 'The uploaded file has invalid headers',
+                ],
+            ],
+            'status_code' => 400,
+        ],
+        'exception' => [
+            'class'               => 'RZP\Exception\BadRequestException',
+            'internal_error_code' => ErrorCode::BAD_REQUEST_BATCH_FILE_INVALID_HEADERS,
+        ],
+    ],
+
+];
---
+++ tests/Functional/SubscriptionRegistration/SubscriptionRegistrationTest.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace RZP\Tests\Functional\SubscriptionRegistration;
+
+use Mail;
+use Queue;
+
+use RZP\Constants\Entity as E;
+use RZP\Tests\Traits\TestsMetrics;
+use RZP\Tests\Functional\TestCase;
+use RZP\Tests\Functional\Helpers\MocksDnsTrait;
+use RZP\Tests\Functional\Helpers\DbEntityFetchTrait;
+use RZP\Tests\Functional\Helpers\Payment\PaymentTrait;
+use RZP\Tests\Unit\Models\Invoice\Traits\CreatesInvoice;
+
+/**
+ * @group dns-sensitive
+ */
+class SubscriptionRegistrationTest extends TestCase
+{
+    use TestsMetrics;
+    use PaymentTrait;
+    use MocksDnsTrait;
+    use CreatesInvoice;
+    use DbEntityFetchTrait;
+
+    const TEST_INV_ID = 'inv_1000000invoice';
+
+    public function setUp()
+    {
+        $this->testDataFilePath = __DIR__ . '/Helpers/SubscriptionRegistrationTestData.php';
+
+        parent::setUp();
+
+        $this->ba->proxyAuth();
+
+        $this->setupMockDns();
+    }
+
+    public function testCreateAuthLinkWithoutMandate()
+    {
+        $this->startTest();
+    }
+
+    public function testCreateAuthLinkWithCardMandate()
+    {
+        $this->startTest();
+
+        $subr = $this->getLastEntity('subscription_registration', true);
+
+        $this->assertEquals($subr['method'], "card");
+
+        $order = $this->getLastEntity('order', true);
+
+        $this->assertEquals($order['method'], null);
+    }
+
+    public function testCreateAuthLinkWithBankMandate()
+    {
+        $this->startTest();
+
+        $subr = $this->getLastEntity('subscription_registration', true);
+
+        $this->assertEquals($subr['method'], "emandate");
+
+        $order = $this->getLastEntity('order', true);
+
+        $this->assertEquals($order['method'], "emandate");
+    }
+
+    public function testCreateAuthLinkWithBankAccount()
+    {
+        $this->startTest();
+
+        $order = $this->getLastEntity('order', true);
+
+        $bankAccount = $this->getLastEntity('bank_account', true);
+
+        $subr = $this->getLastEntity('subscription_registration', true);
+
+        $this->assertEquals($order['bank'], "HDFC");
+
+        $this->assertEquals($order['method'], "emandate");
+
+        $this->assertEquals($bankAccount['ifsc_code'], "HDFC0001233");
+
+        $this->assertEquals($subr['method'], "emandate");
+    }
+
+    public function testCreateAuthLinkWithIncompleteBankData()
+    {
+        $this->startTest();
+    }
+
+    public function testFetchAuthLinks()
+    {
+        $subrAttributes = ['method' => 'emandate'];
+
+        $subr = $this->fixtures->create('subscription_registration', $subrAttributes);
+
+        $order = $this->fixtures->create("order");
+
+        $invoiceAtrributes = [
+            'entity_id'   => $subr->getId(),
+            'entity_type' => 'subscription_registration',
+            'order_id'    => $order->getId()
+        ];
+
+        $invoice = $this->fixtures->create("invoice", $invoiceAtrributes);
+
+        $response = $this->startTest();
+
+        $this->assertArrayHasKey(E::SUBSCRIPTION_REGISTRATION, $response);
+
+        $this->assertEquals($response[E::SUBSCRIPTION_REGISTRATION]['method'], "emandate");
+    }
+
+    public function testFetchAuthLinksWithMandateAndBankAttributes()
+    {
+        $bank = $this->fixtures->create("bank_account");
+
+        $subrAttributes = [
+            'method'      => 'emandate',
+            'entity_id'   => $bank->getId(),
+            'entity_type' => E::BANK_ACCOUNT
+        ];
+
+        $subr = $this->fixtures->create('subscription_registration', $subrAttributes);
+
+        $orderAtributes = ['bank' => 'HDFC'];
+
+        $order = $this->fixtures->create("order", $orderAtributes);
+
+        $invoiceAtrributes = [
+            'entity_id'   => $subr->getId(),
+            'entity_type' => 'subscription_registration',
+            'order_id'    => $order->getId()
+        ];
+
+        $invoice = $this->fixtures->create("invoice", $invoiceAtrributes);
+
+        $response = $this->startTest();
+
+        $this->assertArrayHasKey(E::SUBSCRIPTION_REGISTRATION, $response);
+
+        $this->assertArrayHasKey(E::BANK_ACCOUNT, $response[E::SUBSCRIPTION_REGISTRATION]);
+
+        $this->assertEquals($response[E::SUBSCRIPTION_REGISTRATION]['method'], "emandate");
+
+        $this->assertEquals($response[E::SUBSCRIPTION_REGISTRATION][E::BANK_ACCOUNT]['bank_name'], "HDFC");
+
+        $this->assertEquals($response[E::SUBSCRIPTION_REGISTRATION][E::BANK_ACCOUNT]['ifsc'], "RZPB0000000");
+    }
+
+    public function testCreateAuthLinkWithCardAndZeroAmount()
+    {
+        $this->startTest();
+    }
+
+    public function testCreateAuthLinkWithBankAndNonZeroAmount()
+    {
+        $this->startTest();
+    }
+}
