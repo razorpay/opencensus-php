2bb447986768f6bbd5b3b417eddb03609037c6b1
Anshul Sahni <anshul.sahni@razorpay.com>
2018-10-26 00:02:46 +0530

    [EMANDATE]: feature check enabled on emandate routes  - isChargeAtWill getter method added User model  - Subscriptions/Batch upload is accessible to only people with hosted_emandate false feature  - route regex added in SUBSCRIPTION_ROUTES_REGEX

--- web/js/merchant/containers/Subscriptions/Index.js
+++ web/js/merchant/containers/Subscriptions/Index.js
@@ -11,7 +11,7 @@ import * as ModalActions from 'rzp/modules/modals';
 import SubscriptionsList from 'merchant/containers/Subscriptions/List';
 import PlansList from 'merchant/containers/Plans/List';
 import ActivationBanner from 'merchant/components/ActivationBanner';
-import AddOnsList from 'merchant/containers/AddOns/List';
+import ShowWhen, { ShowWhenRoute } from 'merchant/components/ShowWhen';
 
 import HostedEmanadateBatches from 'merchant/containers/Subscriptions/Batch/List';
 
@@ -115,21 +115,22 @@ export default class SubscriptionsController extends Component {
               Subscriptions
             </NavLink>
             <NavLink to="/plans">Plans</NavLink>
-            <NavLink exact to="/subscriptions/batchuploads">
-              Batch Upload
-            </NavLink>
-            {/* <NavLink to="/addons">Add Ons</NavLink> */}
+            <ShowWhen additionalCondition={user => user.isChargeAtWillEnabled}>
+              <NavLink exact to="/subscriptions/batchuploads">
+                Batch Upload
+              </NavLink>
+            </ShowWhen>
           </header>
           <TestModeBanner />
           <content>
             <Switch>
-              <Route
+              <ShowWhenRoute
                 path="/subscriptions/batchuploads"
                 component={HostedEmanadateBatches}
+                additionalCondition={user => user.isChargeAtWillEnabled}
               />
               <Route path="/subscriptions" component={SubscriptionsList} />
               <Route path="/plans" component={PlansList} />
-              {/* <Route path="/addons" component={AddOnsList} /> */}
             </Switch>
           </content>
         </tabbed-container>
