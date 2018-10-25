87e27ee8ba8e43660d8fc280d7726cb535a8a00c
Kranti Deep <kranti.deep@razorpay.com>
2018-10-25 07:14:10 +0530

    Hdfc/Gateway: allow retry refunds if authRespCode is P

--- app/Gateway/Hdfc/AuthRespCode.php
+++ app/Gateway/Hdfc/AuthRespCode.php
@@ -15,6 +15,7 @@ class AuthRespCode
     const X = 'X';
     const Y = 'Y';
     const Z = 'Z';
+    const P = 'P';
 
     public static $authRespCodeDescription = [
         self::D => 'Total Amount limit set for the terminal for transactions has been crossed',
@@ -25,6 +26,7 @@ class AuthRespCode
         self::I => 'Maximum credit amount set for per card for rolling 24 hrs has been crossed',
         self::J => 'Maximum transaction set for per card for rolling 24 hrs has been crossed',
         self::K => 'Amount Less than Minimum Amount configured',
+        self::P => '(HDFC internal DB issue)',
         self::X => 'BIN is added as negative BIN in PG',
         self::Y => 'Card is present in negative card list and will be decline in future also unless it is not removed manually.',
         self::Z => 'Card is present in decline card database, will be declined for short time',
@@ -32,7 +34,7 @@ class AuthRespCode
 
     public static function shouldRetryRefund($authRespCode)
     {
-        $retryAuthRespCodes = [self::D, self::E, self::F, self::G, self::H, self::I, self::J];
+        $retryAuthRespCodes = [self::D, self::E, self::F, self::G, self::H, self::I, self::J, self::P];
 
         return (in_array($authRespCode, $retryAuthRespCodes, true) === true);
     }
