fa5c190aca0e098980434db2988893494d0d9789
Abhinav Venkatesh <abhinav.venkatesh@razorpay.com>
2018-10-25 21:26:49 +0530

    Change constructor to use only config and not mode

--- app/Gateway/Netbanking/Canara/AESCrypto.php
+++ app/Gateway/Netbanking/Canara/AESCrypto.php
@@ -7,21 +7,11 @@ use RZP\Constants\Mode;
 
 class AESCrypto extends Base\AESCrypto
 {
-    public function __construct($mode, $config)
+    public function __construct($config)
     {
-        if ($mode == Mode::TEST or $config === null)
-        {
-            $masterKey = $config['test_master_key'];
+        $masterKey = $config['key'];
 
-            $iv = $config['test_IV'];
-        }
-        else
-        {
-            //TODO : Find out if this should be accessed directly from terminal check getSecret()
-            $masterKey = $config['live_master_key'];
-
-            $iv = $config['live_IV'];
-        }
+        $iv = $config['IV'];
 
         parent::__construct(Constants::MODE_CBC, $masterKey, $iv);
     }
