f720703ff229fecc3935979480a6ae87b4b8795e
Anshul Sahni <anshul.sahni@razorpay.com>
2018-10-26 14:33:21 +0530

    [TOKEN]: added action method for chargin token

--- web/js/merchant/models/Token.js
+++ web/js/merchant/models/Token.js
@@ -2,4 +2,16 @@ import GenericEntity from './GenericEntity';
 
 export default class Token extends GenericEntity {
   resourceUrl = 'subscription_registration/tokens';
+
+  chargeToken(data) {
+    return this.makeGenericAjaxCall({
+      url: `${this.resourceUrl}/${this.id}/charge`,
+      method: 'post',
+      data,
+    }).then(response => {
+      if (response.success) {
+        return response.data;
+      }
+    });
+  }
 }
--- web/js/merchant/modules/token.js
+++ web/js/merchant/modules/token.js
@@ -4,15 +4,16 @@ import { makeEntityReducer } from 'rzp/modules/entity';
 
 const TOKEN_FETCH = 'TOKEN_FETCH';
 const DELETE_TOKEN = 'TOKEN_DELETE';
+const TOKEN_CHARGE = 'TOKEN_CHARGE';
 
 export const fetchToken = id => ({
   type: TOKEN_FETCH,
   payload: new Token().fetch(id),
 });
 
-export const chargeToken = data => ({
-  type: 'CHARGE_TOKEN',
-  payload: new Token().chargeToken(data),
+export const chargeToken = ({ id, ...data }) => ({
+  type: TOKEN_CHARGE,
+  payload: new Token({ id }).chargeToken(data),
 });
 
 export const deleteToken = id => ({
