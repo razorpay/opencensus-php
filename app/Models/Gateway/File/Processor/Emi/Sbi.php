44e72e99d96929f9abb3553ca1719983803f2cbf
Vivek <viveklakhera@gmail.com>
2018-10-26 14:41:14 +0530

    Update app/Models/Gateway/File/Processor/Emi/Sbi.php
    Co-Authored-By: krantideep95 <kranti.deep@razorpay.com>

--- app/Models/Gateway/File/Processor/Emi/Sbi.php
+++ app/Models/Gateway/File/Processor/Emi/Sbi.php
@@ -87,7 +87,7 @@ class Sbi extends Base
             throw new GatewayFileException(
                 ErrorCode::SERVER_ERROR_GATEWAY_FILE_ERROR_GENERATING_FILE, [
                 'id'        => $this->gatewayFile->getId(),
-            ],
+                ],
                 $e);
         }
     }
