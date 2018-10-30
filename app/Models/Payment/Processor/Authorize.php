f39c461c049e7204da395485015d96b857ed3c8c
Abhay P <abhay.prabhakaran@razorpay.com>
2018-10-30 14:01:43 +0530

    adding queue running fix

--- app/Models/Payment/Processor/Authorize.php
+++ app/Models/Payment/Processor/Authorize.php
@@ -1019,6 +1019,24 @@ trait Authorize
 
     protected function verifyFeatureForRecurring(Merchant\Entity $merchant, Payment\Entity $payment)
     {
+        //
+        // When we are charging tokens via batch using sqs, auth type won't be set.
+        // Adding this condition to verify recurring feature enabled for batch charge tokens
+        //
+        if ($this->app->runningInQueue() === true)
+        {
+            $this->trace->info(
+                TraceCode::PAYMENT_RECURRING_VIA_BATCH,
+                [
+                    'payment'   => $payment,
+                    'merchant'  => $merchant->toArrayPublic(),
+                ]);
+
+            $this->verifyRecurringEnabledForMerchant($merchant);
+
+            return;
+        }
+
         $authType = $this->app['basicauth']->getAuthType();
 
         switch ($authType)
