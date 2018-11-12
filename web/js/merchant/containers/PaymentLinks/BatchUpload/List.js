cd4882e548e0a5a79dbb4fd3f438995c51453f02
Malligairajan G <malligairajan.g@razorpay.com>
2018-11-12 18:47:31 +0530

    [Batch] Increase PL file size to 10 MB

--- web/js/merchant/containers/PaymentLinks/BatchUpload/List.js
+++ web/js/merchant/containers/PaymentLinks/BatchUpload/List.js
@@ -92,7 +92,7 @@ export default class BatchListContainer extends Component {
         batchFormInitialValues={batchFormInitialValues}
         batchType="payment_link"
         maxRows={50000}
-        maxFileSize={5242880}
+        maxFileSize={10485760}
         gaEvents={gaEvents}
         createBatch={this.props.createBatch}
         validateBatch={this.props.validateBatch}
