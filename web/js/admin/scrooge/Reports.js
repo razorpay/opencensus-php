81e14a7f04c9a943fb25552fa122d56b054dcf56
Akshay Sharma <akshay.sharma@razorpay.com>
2018-11-20 15:17:07 +0530

    Adding Total refunds, Internal errors and Gateway errors columns [Admin]

--- web/js/admin/scrooge/Reports.js
+++ web/js/admin/scrooge/Reports.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
-import { PageTable } from 'ui/Table';
+import Table, { PageTable } from 'ui/Table';
 import Collection from 'model/collection';
 import { adminPost } from 'common/fetch';
 import { Link } from 'react-router-dom';
+import { openModal } from 'common/modal';
+import { ModalContent } from 'component/Modal';
+import { snakeToTitleCase } from 'common/util';
+import { pluralize } from 'rzp/utils/rzp-utils';
 
 function refundLink(item, when) {
   let count = item.aging[when].count;
@@ -14,10 +18,46 @@ function refundLink(item, when) {
           item.method
         }&from=${item.aging[when].from}&to=${item.aging[when].to}`}
       >
-        {count} Refund
-        {count === 1 ? '' : 's'}
+        {count} {pluralize('Refund', count)}
       </Link>
     )
+  );
+}
+
+const errorFields = [
+  ['Name', item => item.error_name],
+  ['Count', item => item.count],
+];
+
+function RefundErrorsModal(data) {
+  let sortedErrors = data.errors.sort((a, b) => b.count - a.count);
+
+  return (
+    <ModalContent
+      class="refund-errors-modal"
+      header={snakeToTitleCase(data.title)}
+    >
+      <Table items={sortedErrors} fields={errorFields} />
+    </ModalContent>
+  );
+}
+
+function refundErrors(item, key) {
+  let count = item[key].length;
+  let viewAllClick = () => {
+    openModal(<RefundErrorsModal errors={item[key]} title={key} />);
+  };
+
+  return (
+    <div>
+      {count > 0 ? (
+        <a class="link" onClick={viewAllClick}>
+          {count} {pluralize('Error', count)}
+        </a>
+      ) : (
+        '-'
+      )}
+    </div>
   );
 }
 
@@ -43,7 +83,7 @@ export default class RefundsList extends Component {
   render() {
     const collection = this.collection;
     return (
-      <div class="list-container">
+      <div class="list-container refund-reports">
         <div class="box">
           <header>Failed Refunds</header>
         </div>
@@ -56,9 +96,12 @@ export default class RefundsList extends Component {
 const fields = [
   ['Gateway', item => item.gateway],
   ['Method', item => item.method],
+  ['Total', item => refundLink(item, 'total')],
   ['Today', item => refundLink(item, 'today')],
   ['Yesterday', item => refundLink(item, 'yesterday')],
   ['Last 7 Days', item => refundLink(item, 'last_7days')],
   ['This Month', item => refundLink(item, 'current_month')],
   ['Last Month', item => refundLink(item, 'last_month')],
+  ['Internal Errors', item => refundErrors(item, 'internal_errors')],
+  ['Gateway Errors', item => refundErrors(item, 'gateway_errors')],
 ];
