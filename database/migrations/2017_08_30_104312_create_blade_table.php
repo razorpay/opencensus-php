2ea87573cd646594ad17462e76a0c7279040297b
savp-RP <antony.vincent@razorpay.com>
2018-11-21 19:09:51 +0530

    GP-686 adding URL column into blade with alter sql and table creation edited

--- database/migrations/2017_08_30_104312_create_blade_table.php
+++ database/migrations/2017_08_30_104312_create_blade_table.php
@@ -46,6 +46,13 @@ class CreateBladeTable extends Migration
             $table->char(Mpi::STATUS, 1)
                   ->nullable();
 
+            /**
+             * Max length of URL is 2083 characters. Providing some buffer for URL encoding, if applicable
+             */
+            $table->string(Mpi::URL, 3000)
+                ->nullable()
+                ->default(null);
+
             $table->char(Mpi::XID, 40)
                   ->nullable();
 
