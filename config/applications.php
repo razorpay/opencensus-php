2b47b4aaaa8b0e233dd01308fef80f04463fdb78
Malligairajan G <malligairajan.g@razorpay.com>
2018-11-21 17:42:26 +0530

    Change the node elector for prod and stage

--- config/applications.php
+++ config/applications.php
@@ -226,6 +226,7 @@ return [
         'namespace'         => env('KUBERNETES_NAMESPACE'),
         'iam_role'          => env('KUBERNETES_IAM_ROLE') ?: env('APP_ENV')."-api",
         'image_path'        => env('KUBERNETES_IMAGE_PATH'),
+        'node_selector'     => env('KUBERNETES_NODE_SELECTOR'),
         'git_commit_hash'   => env('GIT_COMMIT_HASH', false),
         'app_mode'          => env('APP_MODE'),
         'app_env'           => env('APP_ENV'),
