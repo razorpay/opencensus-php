8bfe5a6c99733c58ab0a43a184dcf244ab6fae7e
Abhinav Venkatesh <abhinav.venkatesh@razorpay.com>
2018-11-22 13:05:01 +0530

    Add response fields

--- app/Gateway/Netbanking/Pnb/ResponseFields.php
+++ app/Gateway/Netbanking/Pnb/ResponseFields.php
@@ -4,25 +4,19 @@ namespace RZP\Gateway\Netbanking\Pnb;
 
 class ResponseFields
 {
-    // payment response fields
-    const CHALLAN_NUMBER      = 'cin';
-    const BANK_TRANSACTION_ID = 'banktransid';
-    const BANK_PAYMENT_DATE   = 'bankdate';
-    const BANK_AMOUNT_PAID    = 'bankamount';
-    const BANK_PAYMENT_STATUS = 'bankstatus';
-    const ITEM_CODE           = 'ITC';
+    const API_KEY        = 'api_key';
+    const ENCRYPTED_DATA = 'encrypted_data';
 
-    // verify response fields
-    const CHALLAN_NUMBER_VERIFY      = 'CIN';
-    const BANK_TRANSACTION_ID_VERIFY = 'BankTransID';
-    const BANK_PAYMENT_DATE_VERIFY   = 'BankDate';
-    const BANK_AMOUNT_PAID_VERIFY    = 'BankAmount';
-    const BANK_PAYMENT_STATUS_VERIFY = 'BankStatus';
+    //fields after decrypting
+    const PAYMENT_ID      = 'order_id';
+    const RESPONSE_CODE   = 'response_code';
+    const BANK_PAYMENT_ID = 'transaction_id';
+    const RESPONSE_DESC   = 'response_message';
+    const ERROR_DESC      = 'error_desc';
+    const DESCRPTION      = 'description';
+    const AMOUNT          = 'amount';
+    const CHECKSUM        = 'hash';
 
-    // optional
-    const STATUS_DESCRIPTON   = 'statusdesc';
-
-    // encryption
-    const CHECKSUM            = 'checksum';
-    const ENCDATA             = 'encdata';
+    //verify
+    const BANK_CODE = 'bank_code';
 }
