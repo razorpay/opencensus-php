31c01d144b1fdab1b402536be5a1c1353bd5a4dc
Abhinav Venkatesh <abhinav.venkatesh@razorpay.com>
2018-11-22 13:07:14 +0530

    Add validator for mock server

--- app/Gateway/Netbanking/Pnb/Mock/Validator.php
+++ app/Gateway/Netbanking/Pnb/Mock/Validator.php
@@ -8,29 +8,37 @@ use RZP\Gateway\Netbanking\Pnb\RequestFields;
 class Validator extends Base\Validator
 {
     protected static $authRules = [
-        RequestFields::ENCDATA         => 'required|string',
+        RequestFields::API_KEY        => 'required|string',
+        RequestFields::ENCRYPTED_DATA => 'required|string',
     ];
 
     protected static $authorizeRules = [
-        RequestFields::CHALLAN_NUMBER  => 'required|string|size:14',
-        RequestFields::MERCHANT_DATE   => 'required|string',
-        RequestFields::MERCHANT_AMOUNT => 'required|string',
-        RequestFields::RETURN_URL      => 'required|url',
-        RequestFields::ITEM_CODE       => 'required|string|size:14',
-        RequestFields::CHECKSUM        => 'required|string',
-        RequestFields::USER_NAME       => 'required|string',
-        RequestFields::EMAIL           => 'required|email',
-        RequestFields::REMARK          => 'required|string',
-        RequestFields::PHONE_NUMBER    => 'required|string',
-        RequestFields::ADDRESS         => 'required|string',
+        RequestFields::API_KEY        => 'required|string',
+        RequestFields::PAYMENT_ID     => 'required|string|size:14',
+        RequestFields::MODE           => 'required|string|in:LIVE,TEST',
+        RequestFields::AMOUNT         => 'required|numeric',
+        RequestFields::RETURN_URL     => 'required|url',
+        RequestFields::CURRENCY       => 'required|string|in:INR',
+        RequestFields::CHECKSUM       => 'required|string',
+        RequestFields::DESCRIPTION    => 'required|string',
+        RequestFields::EMAIL          => 'required|email',
+        RequestFields::NAME           => 'required|string',
+        RequestFields::PHONE          => 'required|string',
+        RequestFields::ADDRESS_LINE_1 => 'sometimes|string',
+        RequestFields::ADDRESS_LINE_2 => 'sometimes|string',
+        RequestFields::CITY           => 'required|string',
+        RequestFields::STATE          => 'sometimes|string',
+        RequestFields::COUNTRY        => 'required|string',
+        RequestFields::ZIP_CODE       => 'required|string',
+        RequestFields::BANK_CODE      => 'required|string|in:PNBN,PNBM',
     ];
 
     protected static $verifyRules = [
-        RequestFields::CHALLAN_NUMBER  => 'required|string|size:14',
-        RequestFields::MERCHANT_DATE   => 'required|string',
-        RequestFields::MERCHANT_AMOUNT => 'required|string',
-        RequestFields::RETURN_URL      => 'required|url',
-        RequestFields::ITEM_CODE       => 'required|string|size:14',
+        RequestFields::API_KEY         => 'required|string',
+        RequestFields::PAYMENT_ID      => 'sometimes|string|size:14',
         RequestFields::CHECKSUM        => 'required|string',
+        RequestFields::BANK_CODE       => 'sometimes|string|in:PNBN,PNBM',
+        RequestFields::BANK_PAYMENT_ID => 'sometimes|string',
+        RequestFields::RESPONSE_CODE   => 'sometimes|numeric'
     ];
 }
