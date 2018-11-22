ab1db00aabb2fb43936bf3e8d81fc458db3aa0b8
Abhinav Venkatesh <abhinav.venkatesh@razorpay.com>
2018-11-22 14:24:30 +0530

    Add canara terminal to fixtures

--- app/Models/Gateway/File/Processor/Refund/Canara.php
+++ app/Models/Gateway/File/Processor/Refund/Canara.php
@@ -29,7 +29,6 @@ class Canara extends Base
     const GATEWAY                = Payment\Gateway::NETBANKING_CANARA;
     const GATEWAY_CODE           = IFSC::CNRB;
     const PAYMENT_TYPE_ATTRIBUTE = Payment\Entity::BANK;
-    const BANK_CODE              = 'CNRB';
     const HEADERS                = RefundFileFields::COLUMN_HEADERS;
 
     //protected $config;
