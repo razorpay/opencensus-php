6c5b5ce1f087c90bab7f02ae5b4e54afa0c11944
Anshul Sahni <anshul.sahni@razorpay.com>
2018-11-28 18:35:11 +0530

    [REPORT]: implement listing and view for self serve report config

--- web/js/admin/merchants/entity/MerchantReportConfig/List.js
+++ web/js/admin/merchants/entity/MerchantReportConfig/List.js
@@ -1,11 +1,50 @@
 import { Component } from 'react';
 
 import { openModal } from 'common/modal';
+import { adminFetch } from 'common/fetch';
+
 import { ModalContent } from 'component/Modal';
+import { PageTable } from 'ui/Table';
+
+import Collection from 'model/collection';
 
 import CreateEntity from './Create';
+import Entity from './Entity';
+
+const getFields = ({ handleViewClick }) => [
+  ['ID', item => item.id],
+  ['Name', item => item.name],
+  ['Description', item => item.description],
+  ['No. Of Columns', item => item.template.output_fields.length],
+  [
+    'Actions',
+    item => (
+      <>
+        <button class="btn" onClick={handleViewClick(item)}>
+          View
+        </button>
+      </>
+    ),
+  ],
+];
 
 export default class MerchantReportConfigList extends Component {
+  componentWillMount() {
+    this.collection = new Collection({
+      fetchFn: fetchConfigList.bind(this, this.props.match.params.id),
+    });
+  }
+
+  handleViewClick = item => () => {
+    openModal(
+      <div style={{ width: '750px' }}>
+        <ModalContent header="Report Config">
+          <Entity config={item} />
+        </ModalContent>
+      </div>
+    );
+  };
+
   addNew = () => {
     openModal(
       <div style={{ width: '750px' }}>
@@ -15,15 +54,32 @@ export default class MerchantReportConfigList extends Component {
       </div>
     );
   };
+
   render() {
     return (
-      <>
-        <h1>Merchant Report Configs</h1>
-
-        <button class="button" onClick={this.addNew}>
-          Add New
-        </button>
-      </>
+      <div class="list-container">
+        <div class="box">
+          <header>
+            Merchant Report Configs
+            <button class="button pull-right" onClick={this.addNew}>
+              Add New
+            </button>
+          </header>
+        </div>
+        <PageTable
+          model={this.collection}
+          animateRow={false}
+          fields={getFields({ handleViewClick: this.handleViewClick })}
+        />
+      </div>
     );
   }
+}
+
+function fetchConfigList(merchantId) {
+  return adminFetch(`live_${merchantId}/reporting/configs`).then(response => {
+    if (response) {
+      return response;
+    }
+  });
 }
