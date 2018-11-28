46b29c32c03fb4fce263f72741579c21d3c42ad3
Abhinav Venkatesh <abhinav.venkatesh@razorpay.com>
2018-11-28 23:08:35 +0530

    Change status from string to int

--- app/Gateway/Netbanking/Pnb/Status.php
+++ app/Gateway/Netbanking/Pnb/Status.php
@@ -5,5 +5,5 @@ namespace RZP\Gateway\Netbanking\Pnb;
 class Status
 {
     // Transaction status codes. This is defined by us. Bank sends code, 0 -> success
-    const SUCCESS = '0';
+    const SUCCESS = 0;
 }
