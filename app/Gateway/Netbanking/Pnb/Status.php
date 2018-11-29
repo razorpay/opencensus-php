c0127a355d31da42f4709fd2e6126fb2d305d9fd
Abhinav Venkatesh <abhinav.venkatesh@razorpay.com>
2018-11-29 18:11:44 +0530

    Define status check function

--- app/Gateway/Netbanking/Pnb/Status.php
+++ app/Gateway/Netbanking/Pnb/Status.php
@@ -5,5 +5,10 @@ namespace RZP\Gateway\Netbanking\Pnb;
 class Status
 {
     // Transaction status codes. This is defined by us. Bank sends code, 0 -> success
-    const SUCCESS = 0;
+    const SUCCESS = '0';
+
+    public static function isSuccess($status)
+    {
+        return (string) $status === self::SUCCESS;
+    }
 }
