d25b7799e2a0ef0f197764479e0c87979eb7bf86
Amit Mahbubani <amit.mahbubani@razorpay.com>
2018-11-30 10:33:10 +0530

    [bene_account] Table migration

--- database/migrations/2018_11_29_220246_create_beneficiary_accounts.php
+++ database/migrations/2018_11_29_220246_create_beneficiary_accounts.php
@@ -5,8 +5,9 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 use RZP\Constants\Table;
-use RZP\Models\Beneficiary\Entity as Beneficiary;
 use RZP\Models\Merchant\Entity as Merchant;
+use RZP\Models\Beneficiary\Entity as Beneficiary;
+use RZP\Models\Beneficiary\Account\Entity as BeneficiaryAccount;
 
 class CreateBeneficiaryAccounts extends Migration
 {
@@ -19,7 +20,44 @@ class CreateBeneficiaryAccounts extends Migration
     {
         Schema::create(Table::BENEFICIARY_ACCOUNT, function(Blueprint $table)
         {
-            
+            $table->engine = 'InnoDB';
+
+            $table->char(BeneficiaryAccount::ID, 14)
+                  ->primary();
+
+            $table->char(BeneficiaryAccount::MERCHANT_ID, 14);
+
+            $table->char(BeneficiaryAccount::BENEFICIARY_ID, 14);
+
+            $table->char(BeneficiaryAccount::ACCOUNT_TYPE, 255);
+
+            $table->char(BeneficiaryAccount::ACCOUNT_ID, 14);
+
+            $table->tinyInteger(BeneficiaryAccount::ACTIVE)
+                  ->default(1);
+
+            $table->integer(BeneficiaryAccount::CREATED_AT);
+
+            $table->integer(BeneficiaryAccount::UPDATED_AT);
+
+            $table->integer(BeneficiaryAccount::DELETED_AT)
+                  ->nullable();
+
+            $table->index([BeneficiaryAccount::ACCOUNT_ID, BeneficiaryAccount::ACCOUNT_TYPE]);
+
+            $table->index([BeneficiaryAccount::MERCHANT_ID, BeneficiaryAccount::CREATED_AT]);
+
+            $table->index(BeneficiaryAccount::DELETED_AT);
+
+            $table->foreign(BeneficiaryAccount::BENEFICIARY_ID)
+                  ->references(Beneficiary::ID)
+                  ->on(Table::BENEFICIARY)
+                  ->on_delete('restrict');
+
+            $table->foreign(BeneficiaryAccount::MERCHANT_ID)
+                  ->references(Merchant::ID)
+                  ->on(Table::MERCHANT)
+                  ->on_delete('restrict');
         });
     }
 
@@ -32,7 +70,9 @@ class CreateBeneficiaryAccounts extends Migration
     {
         Schema::table(Table::BENEFICIARY_ACCOUNT, function($table)
         {
-            $table->dropForeign(Table::BENEFICIARY_ACCOUNT . '_' . Beneficiary::MERCHANT_ID . '_foreign');
+            $table->dropForeign(Table::BENEFICIARY_ACCOUNT . '_' . BeneficiaryAccount::BENEFICIARY_ID . '_foreign');
+
+            $table->dropForeign(Table::BENEFICIARY_ACCOUNT . '_' . BeneficiaryAccount::MERCHANT_ID . '_foreign');
         });
 
         Schema::drop(Table::BENEFICIARY_ACCOUNT);
