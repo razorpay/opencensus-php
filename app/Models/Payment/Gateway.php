80a68fdea93138162c1f3d6e336c34ffda002434
neerajkanuri <neerajkanuri@gmail.com>
2018-11-30 15:24:10 +0530

    Fss card scrooge changes (#9952)
    * add card_fss to scrooge
    
    * adding mock failure
    
    * fix card fss recon test
    
    * review changes
    
    * changing errorcode for capture failure
    
    * [scrooge] Handles string response
    
    * changing gateway_response - since content is already a string
    
    * golive timestamp changes

--- app/Models/Payment/Gateway.php
+++ app/Models/Payment/Gateway.php
@@ -470,12 +470,15 @@ class Gateway
         Payment\Gateway::FIRST_DATA     => [
             self::GO_LIVE_TIMESTAMP => 1537966190
         ],
+        Payment\Gateway::CARD_FSS       => [
+            self::GO_LIVE_TIMESTAMP => 1543573854
+        ],
         Payment\Gateway::CYBERSOURCE => [
             self::GO_LIVE_TIMESTAMP => 1542649738
         ],
         Payment\Gateway::UPI_MINDGATE   => [
             self::GO_LIVE_TIMESTAMP => 1540826221
-        ]
+        ],
     ];
 
     /**
