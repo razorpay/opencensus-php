14af90346bdc37a6883049fb58e4a0d1d1738f82
Amit Mahbubani <amit.mahbubani@razorpay.com>
2018-11-30 15:56:17 +0530

    [beneacc] Add Bene Acc Type validation

---
+++ app/Models/Beneficiary/Account/Type.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace RZP\Models\Beneficiary\Account;
+
+use RZP\Constants\Entity;
+use RZP\Exception\BadRequestValidationFailureException;
+
+class Type
+{
+    const VPA          = Entity::VPA;
+    const BANK_ACCOUNT = Entity::BANK_ACCOUNT;
+
+    public static function isValid(string $type): bool
+    {
+        $key = __CLASS__ . '::' . strtoupper($type);
+
+        return ((defined($key) === true) and (constant($key) === $type));
+    }
+
+    public static function validateType(string $type)
+    {
+        if (self::isValid($type) === false)
+        {
+            throw new BadRequestValidationFailureException('Not a valid account type: ' . $type);
+        }
+    }
+}
