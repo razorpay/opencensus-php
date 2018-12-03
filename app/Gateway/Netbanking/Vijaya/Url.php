eaf95c195534bb77258324dacb3bdaab13cc9b86
Abhinav Venkatesh <abhinav.venkatesh@razorpay.com>
2018-12-03 17:04:23 +0530

    Fix Url for verify

--- app/Gateway/Netbanking/Vijaya/Url.php
+++ app/Gateway/Netbanking/Vijaya/Url.php
@@ -7,8 +7,7 @@ class Url
     const LIVE_DOMAIN = '';
     const TEST_DOMAIN = 'http://219.65.65.171:9081';
 
-    const AUTHORIZE      = '/NASApp/BANA623WAR/BANKAWAY?Action.ShoppingMall.Login.Init=Y&BankId=029&MD=P&USER_LANG_ID=001&UserType=1&AppType=corporate';
+    const AUTHORIZE = '/NASApp/BANA623WAR/BANKAWAY?Action.ShoppingMall.Login.Init=Y&BankId=029&MD=P&USER_LANG_ID=001&UserType=1&AppType=corporate';
 
-    const VERIFY_BID_PRESENT  = '/NASApp/BANA623WAR/BANKAWAY?Action.ShoppingMall.Login.Init=Y&BankId=029&MD=V&CRN=INR&CG=Y&USER_LANG_ID=001&UserType=1&AppType=corporate';
-    const VERIFY_BID_ABSENT = '/EBankingWeb/BANA623WAR/BANKAWAY?Action.ShoppingMall.Login.Init=Y&BankId=029&CRN=INR&MD=V&CG=Y&USER_LANG_ID=001&UserType=1&AppType=corporate';
+    const VERIFY = '/NASApp/BANA623WAR/BANKAWAY?Action.ShoppingMall.Login.Init=Y&BankId=029&MD=V&CRN=INR&CG=Y&USER_LANG_ID=001&UserType=1&AppType=corporate';
 }
