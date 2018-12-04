5037d3051c4a216b70089195d54dfd0f9b9a2d66
Patlola <praveen.patlola@razorpay.com>
2018-12-04 12:42:57 +0000

    removed global request usage in api url

--- app/Http/ApiUrl.php
+++ app/Http/ApiUrl.php
@@ -71,7 +71,7 @@ class ApiUrl
         // Fallback for origin is referrer.
         // OSWAP suggests to use referrer if origin header is not present.
         // https://www.owasp.org/index.php/Cross-Site_Request_Forgery_(CSRF)_Prevention_Cheat_Sheet#Identifying_Source_Origin_.28via_Origin.2FReferer_header.29
-        $originDomain = \Request::server('HTTP_ORIGIN') ?? \Request::server('HTTP_REFERER');
+        $originDomain = Request::server('HTTP_ORIGIN') ?? Request::server('HTTP_REFERER');
 
         return $originDomain;
     }
