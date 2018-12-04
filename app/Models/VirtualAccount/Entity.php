25ea7a1e2b0da1fe2f8e60bad14703aeac6641c2
jitendra-1217 <jitendraojha1217@gmail.com>
2018-12-04 16:10:25 +0530

    Balance: Adds HasBalance trait

--- app/Models/VirtualAccount/Entity.php
+++ app/Models/VirtualAccount/Entity.php
@@ -8,9 +8,9 @@ use RZP\Models\Customer;
 use RZP\Models\Merchant;
 use RZP\Models\BankAccount;
 use RZP\Models\BankTransfer;
-use RZP\Models\Merchant\Balance;
 use RZP\Constants\Entity as Constants;
 use RZP\Models\Base\Traits\NotesTrait;
+use RZP\Models\Base\Traits\HasBalance;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
@@ -22,9 +22,8 @@ class Entity extends Base\PublicEntity
 {
     use SoftDeletes;
     use NotesTrait;
+    use HasBalance;
 
-    const ID                   = 'id';
-    const MERCHANT_ID          = 'merchant_id';
     const STATUS               = 'status';
     const NAME                 = 'name';
     const DESCRIPTOR           = 'descriptor';
@@ -140,11 +139,6 @@ class Entity extends Base\PublicEntity
     public function entity()
     {
         return $this->morphTo();
-    }
-
-    public function balance()
-    {
-        return $this->belongsTo(Balance\Entity::class);
     }
 
     // ----------------------- Modifiers ---------------------------------------
@@ -266,26 +260,6 @@ class Entity extends Base\PublicEntity
     public function getReceiverBuilder()
     {
         return new Receiver($this);
-    }
-
-    /**
-     * Returns balance type, handles old entities which have balance_id not
-     * filled yet by defaulting to type 'primary'.
-     * @return string
-     */
-    public function getBalanceType(): string
-    {
-        return optional($this->balance)->getType() ?: Balance\Type::PRIMARY;
-    }
-
-    public function isBalanceTypePrimary(): bool
-    {
-        return $this->getBalanceType() === Balance\Type::PRIMARY;
-    }
-
-    public function isBalanceTypeBanking(): bool
-    {
-        return $this->getBalanceType() === Balance\Type::BANKING;
     }
 
     // ----------------------- Setters -----------------------------------------
