4ffd07e6edfdd197a20950955448958bdbc235bd
Abhinav Venkatesh <abhinav.venkatesh@razorpay.com>
2018-12-04 20:03:28 +0530

    Implement claim and refunds for Vijaya

---
+++ tests/Functional/Gateway/File/NetbankingVijayaCombinedFileTest.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace RZP\Tests\Functional\Gateway\File;
+
+use Mail;
+use Carbon\Carbon;
+
+use RZP\Constants\Timezone;
+use RZP\Models\Gateway\File;
+use RZP\Tests\Functional\TestCase;
+use RZP\Mail\Gateway\DailyFile as DailyFileMail;
+use RZP\Tests\Functional\Helpers\Payment\PaymentTrait;
+
+class NetbankingVijayaCombinedFileTest extends TestCase
+{
+    use PaymentTrait;
+
+    protected $terminal;
+
+    protected $bank = 'VIJB';
+
+    public function setUp()
+    {
+        $this->testDataFilePath = __DIR__ . '/helpers/NetbankingVijayaCombinedFileTestData.php';
+
+        parent::setUp();
+
+        $this->terminal = $this->fixtures->create('terminal:shared_netbanking_vijaya_terminal');
+    }
+
+    public function testNetbankingVijayaCombinedFile()
+    {
+        Mail::fake();
+
+        $payment = $this->getDefaultNetbankingPaymentArray($this->bank);
+
+        $payment = $this->doAuthAndCapturePayment($payment);
+
+        $transaction = $this->getLastEntity('transaction', true);
+
+        $this->fixtures->edit('transaction', $transaction['id'], [
+            'reconciled_at' => Carbon::tomorrow(Timezone::IST)->addHours(8)->timestamp
+        ]);
+
+        $refund = $this->refundPayment($payment['id']);
+
+        $payment = $this->getDefaultNetbankingPaymentArray($this->bank);
+
+        $this->doAuthAndCapturePayment($payment);
+
+        $this->ba->adminAuth();
+
+        $content = $this->startTest();
+
+        $content = $content['items'][0];
+
+        $this->assertNotNull($content[File\Entity::FILE_GENERATED_AT]);
+        $this->assertNotNull(File\Entity::SENT_AT);
+        $this->assertNull($content[File\Entity::FAILED_AT]);
+        $this->assertNull($content[File\Entity::ACKNOWLEDGED_AT]);
+
+        $files = $this->getEntities('file_store', [
+            'count' => 2
+        ], true);
+
+        $expectedFilesContent = [
+            'entity' => 'collection',
+            'count' => 2,
+            'items' => [
+                [
+                    'type' => 'vijaya_netbanking_claim',
+                ],
+                [
+                    'type' => 'vijaya_netbanking_refund',
+                ],
+            ],
+        ];
+
+        $this->assertArraySelectiveEquals($expectedFilesContent, $files);
+
+        Mail::assertSent(DailyFileMail::class, function ($mail)
+        {
+            $date = Carbon::today(Timezone::IST)->format('d-m-Y');
+
+            $testData = [
+                'subject' => 'Vijaya Netbanking claims and refund files for '.$date,
+                'amount' => [
+                    'claims'  => '500.00',
+                    'refunds' => '500.00',
+                    'total'   => '0.00'
+                ],
+                'count' => [
+                    'claims'  => 1,
+                    'refunds' => 1,
+                    'total'   => 2
+                ],
+            ];
+
+            $this->assertArraySelectiveEquals($testData, $mail->viewData);
+
+            //$this->checkRefundsFile($mail->viewData['refundsFile']);
+
+            //$this->checkClaimsFile($mail->viewData['claimsFile']);
+
+            $this->assertCount(2, $mail->attachments);
+
+            return true;
+        });
+    }
+
+
+    /*protected function checkRefundsFile(array $refundFileData)
+    {
+        $refundsFileContents = file($refundFileData['url']);
+
+        $this->assertCount(2, $refundsFileContents);
+    }
+
+    protected function checkClaimsFile(array $claimsFileData)
+    {
+        $claimsFileContents = file($claimsFileData['url']);
+
+        $claimsFileLine1 = explode('|', $claimsFileContents[1]);
+
+        $this->assertCount(4, $claimsFileLine1);
+    }*/
+}
