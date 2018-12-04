b9e24171679f8dbef7c1077093a5597b0e8a1363
Abhay P <abhay.prabhakaran@razorpay.com>
2018-12-04 16:58:38 +0530

    hosted page checks

--- app/Models/Plan/Subscription/Service.php
+++ app/Models/Plan/Subscription/Service.php
@@ -361,6 +361,8 @@ class Service extends Base\Service
 
         $subscription = $this->repo->subscription->findByPublicId($subscriptionId);
 
+        $subscription->getValidator()->validateSubscriptionViewable();
+
         return (new ViewDataSerializer($subscription))->serializeForHosted();
     }
 
