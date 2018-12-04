2e0366e3ef5d6ece2a9732b72db01f1f811cb29e
Amit Mahbubani <amit.mahbubani@razorpay.com>
2018-12-05 00:31:02 +0530

    [bbpricing] Pricing migration changes

--- database/migrations/2014_07_22_114552_create_pricing.php
+++ database/migrations/2014_07_22_114552_create_pricing.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 use RZP\Constants\Table;
+use RZP\Constants\Product;
 use RZP\Models\Pricing\Entity as Pricing;
 
 class CreatePricing extends Migration
@@ -25,6 +26,9 @@ class CreatePricing extends Migration
             $table->char(Pricing::PLAN_ID, Pricing::ID_LENGTH);
 
             $table->string(Pricing::PLAN_NAME);
+
+            $table->string(Pricing::PRODUCT, 255)
+                  ->default(Product::PRIMARY);
 
             $table->string(Pricing::FEATURE, 20);
 
