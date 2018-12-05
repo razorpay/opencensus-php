4e3baad7c7e41a4d009649b013e2e83e7ee7cb25
Abhinav Venkatesh <abhinav.venkatesh@razorpay.com>
2018-12-05 19:15:42 +0530

    Remove merchant constant

--- app/Gateway/Netbanking/Vijaya/Constants.php
+++ app/Gateway/Netbanking/Vijaya/Constants.php
@@ -4,7 +4,6 @@ namespace RZP\Gateway\Netbanking\Vijaya;
 
 class Constants
 {
-    const MERCHANT_CONSTANT = '000000000010';
     const ITEM_CODE         = 'Razorpay';
     const INDIAN_CURRENCY   = 'INR';
     const PAYMENT_MODE      = 'P';
