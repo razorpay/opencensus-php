3801ecc2cf97d3c71835e4216d3b86b04f37e066
abhinavv21 <31855138+abhinavv21@users.noreply.github.com>
2018-12-07 00:42:35 +0530

    Fix librexml usage (#10127)
    * Fix user error handling of librexml
    
    * Enable skipped tests

--- tests/Functional/Gateway/Mpi/Blade/BladeGatewayTest.php
+++ tests/Functional/Gateway/Mpi/Blade/BladeGatewayTest.php
@@ -181,8 +181,6 @@ class BladeGatewayTest extends TestCase
 
     public function testSignatureMissingFromValidPARes()
     {
-        $this->markTestSkipped('Temp: Contact @vivek to fix');
-
         $this->mockSignatureNotFound();
 
         $this->runRequestResponseFlow(
@@ -203,8 +201,6 @@ class BladeGatewayTest extends TestCase
 
     public function testBlankMessage()
     {
-        $this->markTestSkipped('Temp: Contact @vivek to fix');
-
         $this->runRequestResponseFlow(
             $data = $this->testData['testBlankMessage'],
             function()
@@ -283,8 +279,6 @@ class BladeGatewayTest extends TestCase
 
     public function testInvalidIReqCode()
     {
-        $this->markTestSkipped('Temp: Contact @vivek to fix');
-
         $response = $this->getInvalidIreqCode();
 
         $this->mockIReqCode($response);
