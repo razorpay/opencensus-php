eac3f4704495c9e866e1aa9e3bdec7f553b558ba
Abhinav Venkatesh <abhinav.venkatesh@razorpay.com>
2018-12-06 12:48:48 +0530

    [admintest] Update entity count

--- tests/Functional/Admin/AdminTest.php
+++ tests/Functional/Admin/AdminTest.php
@@ -806,7 +806,7 @@ class AdminTest extends TestCase
 
         $this->assertCount(10, $result['fields']);
 
-        $this->assertCount(168, $result['entities']);
+        $this->assertCount(169, $result['entities']);
     }
 
     public function testFetchSoftDeletedEntityForAdmin()
