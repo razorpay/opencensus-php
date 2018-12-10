499ffb8c6de06e5f1fca5855f640a46eaa84d350
Abhinav Venkatesh <abhinav.venkatesh@razorpay.com>
2018-12-11 01:47:19 +0530

    [terminal validator] Fix syntax error

--- app/Models/Terminal/Validator.php
+++ app/Models/Terminal/Validator.php
@@ -562,6 +562,7 @@ class Validator extends Base\Validator
     protected static $netbankingSbiTerminalRules = [
         Entity::GATEWAY                     => 'required|in:' . Gateway::NETBANKING_SBI,
         Entity::GATEWAY_MERCHANT_ID         => 'required|string',
+    ];
 
     protected static $netbankingAllahabadTerminalRules = [
         Entity::GATEWAY                     => 'required|in:' . Gateway::NETBANKING_ALLAHABAD,
