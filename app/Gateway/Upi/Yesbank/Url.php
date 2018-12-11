30ae1dd592f24c04f86a614eef13433aa69d09e6
ambar maini <ambar.maini@razorpay.com>
2018-12-11 19:58:21 +0530

    updated live url

--- app/Gateway/Upi/Yesbank/Url.php
+++ app/Gateway/Upi/Yesbank/Url.php
@@ -4,7 +4,7 @@ namespace RZP\Gateway\Upi\Yesbank;
 
 class Url
 {
-    const LIVE_DOMAIN          = 'https://sky.yesbank.in:444/app/upi/';
+    const LIVE_DOMAIN          = 'https://sky.yesbank.in:444/app/live/upi/';
     const TEST_DOMAIN          = 'https://uatsky.yesbank.in:444/app/uat/upi/';
 
     const PAYOUT               = 'mePayServerReqImps';
