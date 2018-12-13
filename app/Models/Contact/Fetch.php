e4cefeab5091b75d72d654e5cdefcef47a75848e
Vikas Naidu <vck173@gmail.com>
2018-12-13 18:45:31 +0530

    added fetch constants

--- app/Models/Contact/Fetch.php
+++ app/Models/Contact/Fetch.php
@@ -15,17 +15,24 @@ class Fetch extends Base\Fetch
     const RULES = [
         self::DEFAULTS => [
             Entity::EMAIL => 'sometimes|email',
+            Entity::NAME    => 'sometimes|string|max:50',
+            Entity::CONTACT => 'sometimes|contact_syntax',
         ],
     ];
 
     const ACCESSES = [
         self::DEFAULTS => [
+            Entity::ID,
+            Entity::NAME,
             Entity::EMAIL,
-        ],
+            Entity::CONTACT,
     ];
 
     const ES_FIELDS = [
-        //
+        Entity::ID,
+        Entity::NAME,
+        Entity::EMAIL,
+        Entity::CONTACT,
     ];
 
     const COMMON_FIELDS = [
