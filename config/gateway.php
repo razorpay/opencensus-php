60e8a8e64ca935f452ca477762eac54f8b4422e6
Abhinav Venkatesh <abhinav.venkatesh@razorpay.com>
2018-12-13 14:09:24 +0530

    [gateway config] Add Todo

--- config/gateway.php
+++ config/gateway.php
@@ -451,6 +451,7 @@ return [
     'enach_rbl' => [
         'test_merchant_id'                          => env('NPCI_EMANDATE_TEST_MERCHANT_ID'),
         'test_emandate_private_key'                 => env('NPCI_EMANDATE_TEST_PRIVATE_KEY'),
+        //TODO : Verify this please
         'test_emandate_npci_creditor_account'       => env('NPCI_EMANDATE_TEST_CREDITOR_ACCOUNT'),
         'test_emandate_npci_sponser_ifsc'           => env('NPCI_EMANDATE_TEST_SPONSER_IFSC'),
         //TODO : Add these creds
