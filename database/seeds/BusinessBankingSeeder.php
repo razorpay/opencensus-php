ec219b35cee83f7d1220e7fbcd06ad25c25ec1d6
Amit Mahbubani <amit.mahbubani@razorpay.com>
2018-12-14 08:52:07 +0530

    contacts: add DB seeds

---
+++ database/seeds/BusinessBankingSeeder.php
@@ -0,0 +1,63 @@
+<?php
+
+use RZP\Constants\Table;
+use Illuminate\Database\Seeder;
+
+class BusinessBankingSeeder extends Seeder
+{
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Eloquent::unguard();
+
+        $this->seed();
+    }
+
+    private function seed()
+    {
+        $name = DB::connection()->getName();
+
+        DB::transaction(function() use ($name) {
+            DB::table(Table::CONTACT)->insert(
+                [
+                    'id'          => 'BXV5GAmaJEcGr1',
+                    'name'        => 'Contact 1',
+                    'email'       => 'contact1@test.com',
+                    'contact'     => '9123456789',
+                    'type'        => 'employee',
+                    'merchant_id' => '10000000000000',
+                    'created_at'  => time(),
+                    'updated_at'  => time(),
+                    'deleted_at'  => null,
+                ],
+                [
+                    'id'          => 'BXV5GAmaJEcGr2',
+                    'name'        => 'Contact 2',
+                    'email'       => 'contact2@test.com',
+                    'contact'     => '9123456782',
+                    'type'        => 'customer',
+                    'merchant_id' => '10000000000000',
+                    'created_at'  => time(),
+                    'updated_at'  => time(),
+                    'deleted_at'  => null,
+                ],
+                [
+                    'id'          => 'BXV5GAmaJEcGr3',
+                    'name'        => 'Contact 3',
+                    'email'       => 'contact3@test.com',
+                    'contact'     => '9123456783',
+                    'type'        => 'self',
+                    'merchant_id' => '10000000000000',
+                    'created_at'  => time(),
+                    'updated_at'  => time(),
+                    'deleted_at'  => null,
+                ]
+            );
+        });
+    }
+}
