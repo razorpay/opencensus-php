8dd0690b42c898215a88f9bcc0a514f951744091
Amit Mahbubani <amit.mahbubani@razorpay.com>
2018-12-14 08:42:00 +0530

    contacts: add type enum + validator

---
+++ app/Models/Contact/Type.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace RZP\Models\Contact;
+
+use RZP\Exception\BadRequestValidationFailureException;
+
+/**
+ * Class Type
+ *
+ * @package RZP\Models\Contact
+ */
+final class Type
+{
+    const CUSTOMER = 'customer';
+    const EMPLOYEE = 'employee';
+    const VENDOR   = 'vendor';
+    const SELF     = 'self';
+
+    public static function isValid(string $type): bool
+    {
+        $key = __CLASS__ . '::' . strtoupper($type);
+
+        return ((defined($key) === true) and (constant($key) === $type));
+    }
+
+    public static function validateType(string $type)
+    {
+        if (self::isValid($type) === false)
+        {
+            throw new BadRequestValidationFailureException('Not a valid contact type: ' . $type);
+        }
+    }
+}
