056a0ca20e7e5ddbbe77276986b773a5e97b2c74
Amit Mahbubani <amit.mahbubani@razorpay.com>
2018-12-16 16:51:55 +0530

    contacts: review changes

--- database/seeds/BusinessBankingSeeder.php
+++ database/seeds/BusinessBankingSeeder.php
@@ -5,7 +5,6 @@ use Illuminate\Database\Seeder;
 
 class BusinessBankingSeeder extends Seeder
 {
-
     /**
      * Run the database seeds.
      *
@@ -15,55 +14,54 @@ class BusinessBankingSeeder extends Seeder
     {
         Eloquent::unguard();
 
-        $this->seed();
+        DB::transaction(function()
+        {
+            $this->seedContacts();
+        });
     }
 
-    private function seed()
+    private function seedContacts()
     {
-        $name = DB::connection()->getName();
-
-        // Contacts
-        DB::transaction(function() use ($name) {
-            DB::table(Table::CONTACT)->insert(
+        DB::table(Table::CONTACT)->insert(
+            [
                 [
-                    [
-                        'id'          => 'BXV5GAmaJEcGr1',
-                        'name'        => 'Contact 1',
-                        'email'       => 'contact1@test.com',
-                        'contact'     => '9123456789',
-                        'type'        => 'employee',
-                        'notes'       => '{}',
-                        'merchant_id' => '10000000000000',
-                        'created_at'  => time(),
-                        'updated_at'  => time(),
-                        'deleted_at'  => null,
-                    ],
-                    [
-                        'id'          => 'BXV5GAmaJEcGr2',
-                        'name'        => 'Contact 2',
-                        'email'       => 'contact2@test.com',
-                        'contact'     => '9123456782',
-                        'type'        => 'customer',
-                        'notes'       => '{}',
-                        'merchant_id' => '10000000000000',
-                        'created_at'  => time(),
-                        'updated_at'  => time(),
-                        'deleted_at'  => null,
-                    ],
-                    [
-                        'id'          => 'BXV5GAmaJEcGr3',
-                        'name'        => 'Contact 3',
-                        'email'       => 'contact3@test.com',
-                        'contact'     => '9123456783',
-                        'type'        => 'self',
-                        'notes'       => '{}',
-                        'merchant_id' => '10000000000000',
-                        'created_at'  => time(),
-                        'updated_at'  => time(),
-                        'deleted_at'  => null,
-                    ],
-                ]
-            );
-        });
+                    'id'          => 'BXV5GAmaJEcGr1',
+                    'name'        => 'Contact 1',
+                    'email'       => 'contact1@test.com',
+                    'contact'     => '9123456789',
+                    'type'        => 'employee',
+                    'notes'       => '{}',
+                    'merchant_id' => '10000000000000',
+                    'created_at'  => time(),
+                    'updated_at'  => time(),
+                    'deleted_at'  => null,
+                ],
+                [
+                    'id'          => 'BXV5GAmaJEcGr2',
+                    'name'        => 'Contact 2',
+                    'email'       => 'contact2@test.com',
+                    'contact'     => '9123456782',
+                    'type'        => 'customer',
+                    'notes'       => '{}',
+                    'merchant_id' => '10000000000000',
+                    'created_at'  => time(),
+                    'updated_at'  => time(),
+                    'deleted_at'  => null,
+                ],
+                [
+                    'id'          => 'BXV5GAmaJEcGr3',
+                    'name'        => 'Contact 3',
+                    'email'       => 'contact3@test.com',
+                    'contact'     => '9123456783',
+                    'type'        => 'self',
+                    'notes'       => '{}',
+                    'merchant_id' => '10000000000000',
+                    'created_at'  => time(),
+                    'updated_at'  => time(),
+                    'deleted_at'  => null,
+                ],
+            ]
+        );
+
     }
 }
