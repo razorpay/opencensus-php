345567d5042e0e1389def3ee97e8a126a44b2e9a
Amit Mahbubani <amit.mahbubani@razorpay.com>
2018-12-17 22:08:50 +0530

    vpa: Add fund_account->vpa creation

--- app/Models/Vpa/Core.php
+++ app/Models/Vpa/Core.php
@@ -7,5 +7,19 @@ use RZP\Models\Merchant;
 
 class Core extends Base\Core
 {
+    public function createForBankingSource(array $input, Base\PublicEntity $source): Entity
+    {
+        $vpa = (new Entity)->build($input);
 
+        /** @var Merchant\Entity $merchant */
+        $merchant = $source->merchant;
+
+        $vpa->merchant()->associate($merchant);
+
+        $vpa->entity()->associate($source);
+
+        $this->repo->saveOrFail($vpa);
+
+        return $vpa;
+    }
 }
