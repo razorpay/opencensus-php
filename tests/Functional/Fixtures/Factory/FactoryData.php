55cbdf3f2d0e6365f8743db91f68444fdc0014bf
Amit Mahbubani <amit.mahbubani@razorpay.com>
2018-12-21 00:15:50 +0530

    contacts: ref_id tests

--- tests/Functional/Fixtures/Factory/FactoryData.php
+++ tests/Functional/Fixtures/Factory/FactoryData.php
@@ -4,8 +4,9 @@ namespace RZP\Tests\Functional\Fixtures\Factory;
 
 use Config;
 use Eloquent;
-use RZP\Models;
 use Carbon\Carbon;
+
+use RZP\Models;
 use RZP\Models\Contact;
 use RZP\Constants\Timezone;
 use RZP\Models\Settlement\Channel;
@@ -948,15 +949,16 @@ final class FactoryData
         ]);
 
         $factory(\RZP\Models\Contact\Entity::class, [
-            'id'          => $faker->uniqueid,
-            'name'        => $faker->word,
-            'email'       => $faker->email,
-            'contact'     => '9123456789',
-            'type'        => $faker->randomElement(Contact\Type::getAll()),
-            'notes'       => null,
-            'merchant_id' => '10000000000000',
-            'created_at'  => $faker->timestamp,
-            'updated_at'  => $faker->timestamp,
+            'id'           => $faker->uniqueid,
+            'name'         => $faker->word,
+            'email'        => $faker->email,
+            'contact'      => '9123456789',
+            'type'         => $faker->randomElement(Contact\Type::getAll()),
+            'reference_id' => $faker->uniqueid,
+            'notes'        => null,
+            'merchant_id'  => '10000000000000',
+            'created_at'   => $faker->timestamp,
+            'updated_at'   => $faker->timestamp,
         ]);
 
         $factory(\RZP\Models\FundAccount\Entity::class, [
