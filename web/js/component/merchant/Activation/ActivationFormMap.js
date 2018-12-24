0f41793b99129391026e1bd14b1a17ec5f0dca52
rchaubey <dev.rohit.chaubey@gmail.com>
2018-12-24 16:03:23 +0530

    hides `CompanyPan` field when businessType is `Proprietor` in activation

--- web/js/component/merchant/Activation/ActivationFormMap.js
+++ web/js/component/merchant/Activation/ActivationFormMap.js
@@ -304,7 +304,7 @@ const registrationDetails = [
     info:
       'Mandatory for Companies. PAN details should be of the mentioned business only.',
     validator: validatePANCard,
-    _when: excludeFor_Indiv,
+    _when: excludeFor_CompanyPan,
   },
   [
     {
@@ -642,6 +642,14 @@ function excludeFor_Indiv(activation) {
     activation.state.dirty.business_type || activation.props.data.business_type;
 
   return [INDIVIDUAL].indexOf(Number(currentBusinessType)) === -1;
+}
+
+function excludeFor_CompanyPan(activation) {
+  const currentBusinessType =
+    activation.state.dirty.business_type || activation.props.data.business_type;
+  return (
+    [INDIVIDUAL, PROPRIETORSHIP].indexOf(Number(currentBusinessType)) === -1
+  );
 }
 
 function requiredForNGO(activation) {
