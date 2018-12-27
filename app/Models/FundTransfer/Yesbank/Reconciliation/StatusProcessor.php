7dc049a0497a5589c622fc5dddab62b5db4a0698
Prashanth YV <yvprashant@gmail.com>
2018-12-27 19:19:03 +0530

    [fta] adds 99 in failure codes temporarily (#10369)

--- app/Models/FundTransfer/Yesbank/Reconciliation/StatusProcessor.php
+++ app/Models/FundTransfer/Yesbank/Reconciliation/StatusProcessor.php
@@ -51,14 +51,48 @@ class StatusProcessor extends BaseRowProcessor
 
         $banking = $this->row->isOfBanking();
 
-        $response = (new StatusRequest($banking))->init()
-                                                 ->setEntity($this->row)
-                                                 ->makeRequest($gateway);
+        $makeRequest = $this->shouldMakeStatusRequestCall($gateway);
+
+        $statusRequestProcessor = (new StatusRequest($banking))->init()
+                                                               ->setEntity($this->row);
+
+        if ($makeRequest === true)
+        {
+            $response = $statusRequestProcessor->makeRequest($gateway);
+        }
+        else
+        {
+            $response = $statusRequestProcessor->getResponseDataFromFta($gateway);
+        }
 
         if (empty($response) === false)
         {
             $this->setParsedData($response);
         }
+    }
+
+    protected function shouldMakeStatusRequestCall(bool $gateway): bool
+    {
+        //
+        // We should not make status call only for VPA payouts since
+        // Yesbank's Status API call does not work correctly.
+        //
+        if ($gateway === false)
+        {
+            return true;
+        }
+
+        $fta = $this->row;
+
+        $bankCode = $fta->getBankStatusCode();
+
+        $successStatuses = GatewayStatus::getSuccessfulStatus();
+        $failureStatuses = GatewayStatus::getFailureStatus();
+
+        // TODO: Fix this later properly. Use status_code
+        // to figure out whether to retry or not.
+
+        return true;
     }
 
     /**
