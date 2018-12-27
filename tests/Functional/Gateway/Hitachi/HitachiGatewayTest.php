ad1e060c43bfd5c5f5beb1463fc2eb751f5ad92c
vishalashank <vishal.ashank@razorpay.com>
2018-12-27 17:35:48 +0530

    [hitachi_timeout_bug] entity created for case of hitachi server timeout [GP-752]

--- tests/Functional/Gateway/Hitachi/HitachiGatewayTest.php
+++ tests/Functional/Gateway/Hitachi/HitachiGatewayTest.php
@@ -525,8 +525,30 @@ class HitachiGatewayTest extends TestCase
         $this->refundPayment($payment['id']);
 
         $refund = $this->getLastEntity('refund', true);
-
+s($refund);
         $this->refundReverseFailureTestHelper($payment);
+    }
+
+    public function testRefundTimeoutFailure()
+    {
+        $this->doAuthAndCapturePayment($this->payment);
+
+        $payment = $this->getLastEntity('payment', true);
+
+        $this->mockFailureResponseCode();
+//        $this->mockServerContentFunction(function (& $content)
+//        {
+//            throw new Exception\GatewayErrorException(ErrorCode::GATEWAY_ERROR_REQUEST_TIMEOUT);
+//        });
+
+        $this->refundPayment($payment['id']);
+
+        $refund = $this->getLastEntity('refund', true);
+
+        $hitachi = $this->getLastEntity('hitachi', true);
+        s($hitachi);
+        s($refund);
+        $this->assertEquals(explode('_', $refund['id'])[1], $hitachi['refund_id']);
     }
 
     public function testPaymentReverse()
@@ -967,7 +989,7 @@ class HitachiGatewayTest extends TestCase
                 'expiry_year'  => '21',
                 'cvv'          => 123,
                 'name'         => 'Test Card',
-                'international'=> true
+                'international' => true
             ]
         ]);
         $txn = $this->getEntities('transaction', [], true);
