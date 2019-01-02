06b2f0b1fb51278efc52f52c0583c02b710368de
rchaubey <dev.rohit.chaubey@gmail.com>
2019-01-03 02:22:42 +0530

    animate activation banners for visibility

--- web/js/merchant/components/Announcement/index.js
+++ web/js/merchant/components/Announcement/index.js
@@ -6,6 +6,10 @@ import LocalStorageService from 'rzp/utils/localStorage';
 import { classList } from 'common/util';
 
 export default class Announcement extends Component {
+  static defaultProps = {
+    onMount: () => {},
+  };
+
   constructor(props) {
     super(props);
 
@@ -29,12 +33,17 @@ export default class Announcement extends Component {
     }
   }
 
+  componentDidMount() {
+    this.props.onMount();
+  }
+
   render() {
     const {
       className,
       handleClose,
       canBeClosed,
       bannerKey,
+      onMount,
       ...props
     } = this.props;
 
--- web/js/merchant/components/Announcements/Instant/index.js
+++ web/js/merchant/components/Announcements/Instant/index.js
@@ -2,15 +2,34 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 import { activationDuration } from 'common/data';
+import LocalStorageService from 'rzp/utils/localStorage';
+
 import Announcement from 'merchant/components/Announcement';
 
 export default class InstantActivationAnnouncements extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      shouldAnimate: false,
+    };
   }
+
+  handleAnimation = () => {
+    const { user } = this.props;
+
+    //animate only the first time
+    // if(!LocalStorageService.getItem(`announcement-banner-${user.activation_status}-${user.current}`)) {
+    setTimeout(() => {
+      this.setState({
+        shouldAnimate: true,
+      });
+    }, 1000);
+    // }
+  };
 
   render() {
     const { user, mode, payments } = this.props;
+    const { shouldAnimate } = this.state;
 
     let theme = 'warning',
       title,
@@ -58,10 +77,12 @@ export default class InstantActivationAnnouncements extends Component {
       <Announcement
         title={title}
         theme={theme}
+        className={shouldAnimate && 'animate'}
         bannerKey={`announcement-banner-${user.activation_status}-${
           user.current
         }`}
         canBeClosed={user.isAccepted}
+        onMount={this.handleAnimation}
       >
         {content}
       </Announcement>
