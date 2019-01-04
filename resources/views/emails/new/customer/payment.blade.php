82c91dccc96ab3fbac163a8920d188e7c34ae384
Anshul Sahni <anshul_sahni@live.com>
2019-01-04 16:58:58 +0530

    Added new mail template

---
+++ resources/views/emails/new/customer/payment.blade.php
@@ -0,0 +1,440 @@
+
+    <!doctype html>
+    <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+      <head>
+        <title>
+          {{ $merchant['billing_label'] }}
+        </title>
+        <!--[if !mso]><!-- -->
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <!--<![endif]-->
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style type="text/css">
+          #outlook a { padding:0; }
+          .ReadMsgBody { width:100%; }
+          .ExternalClass { width:100%; }
+          .ExternalClass * { line-height:100%; }
+          body { margin:0;padding:0;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%; }
+          table, td { border-collapse:collapse;mso-table-lspace:0pt;mso-table-rspace:0pt; }
+          img { border:0;height:auto;line-height:100%; outline:none;text-decoration:none;-ms-interpolation-mode:bicubic; }
+          p { display:block;margin:13px 0; }
+        </style>
+        <!--[if !mso]><!-->
+        <style type="text/css">
+          @media only screen and (max-width:480px) {
+            @-ms-viewport { width:320px; }
+            @viewport { width:320px; }
+          }
+        </style>
+        <!--<![endif]-->
+        <!--[if mso]>
+        <xml>
+        <o:OfficeDocumentSettings>
+          <o:AllowPNG/>
+          <o:PixelsPerInch>96</o:PixelsPerInch>
+        </o:OfficeDocumentSettings>
+        </xml>
+        <![endif]-->
+        <!--[if lte mso 11]>
+        <style type="text/css">
+          .outlook-group-fix { width:100% !important; }
+        </style>
+        <![endif]-->
+
+      <!--[if !mso]><!-->
+        <link href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700" rel="stylesheet" type="text/css">
+        <style type="text/css">
+          @import url(https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700);
+        </style>
+      <!--<![endif]-->
+
+
+
+    <style type="text/css">
+      @media only screen and (min-width:480px) {
+        .mj-column-per-100 { width:100% !important; max-width: 100%; }
+      }
+    </style>
+
+
+        <style type="text/css">
+
+
+        </style>
+
+
+      </head>
+      <body style="background-color:#F0F0F0;">
+
+
+      <div style="background-color:#F0F0F0;">
+
+
+      <!--[if mso | IE]>
+      <table
+         align="center" border="0" cellpadding="0" cellspacing="0" class="header-outlook" style="width:600px;" width="600"
+      >
+        <tr>
+          <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
+      <![endif]-->
+
+
+      <div class="header" style="box-sizing: border-box; padding-top: 16px; background: {{ $merchant['brand_color'] ?? '#528FF0'}}; background-color: {{ $merchant['brand_color'] ?? '#528FF0'}}; Margin: 0px auto; max-width: 100%;">
+
+        <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="background:{{ $merchant['brand_color'] ?? '#528FF0'}};background-color:{{ $merchant['brand_color'] ?? '#528FF0'}};width:100%;">
+          <tbody>
+            <tr>
+              <td style="direction:ltr;font-size:0px;padding:0px;text-align:center;vertical-align:top;">
+                <!--[if mso | IE]>
+                  <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+
+        <tr>
+
+            <td
+               class="" style="vertical-align:top;width:600px;"
+            >
+          <![endif]-->
+
+      <div class="mj-column-per-100 outlook-group-fix" style="font-size:13px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;">
+
+      <table border="0" cellpadding="0" cellspacing="0" role="presentation" width="100%">
+        <tbody>
+          <tr>
+            <td style="vertical-align:top;padding:0px;">
+
+      <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="" width="100%">
+
+            <tr>
+              <td align="left" style="font-size:0px;padding:0px;word-break:break-word;">
+
+      <div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;line-height:1;text-align:left;color:#000000;">
+        <div class="content branding" style="width: 460px; margin-left: auto; margin-right: auto; padding-bottom: 16px;"><div class="brand-content" style="width: fit-content; margin: 0 auto;"><div class="brand-logo" style="display: table-cell; vertical-align: middle; height: 38px; width: 38px;"><img src="https://razorpay.com/favicon.png?v=2" style="height: 100%; width: 100%;"></div><div class="brand-name" style="display: table-cell; vertical-align: middle; font-size: 16px; line-height: 1.5;">{{ $merchant['billing_label'] }}</div></div></div><div class="content title" style="width: 460px; margin-left: auto; margin-right: auto; background-color: #FFFFFF; padding-top: 12px;"><div class="title-content" style="width: fit-content; margin: 0 auto;"><div class="amount" style="display: inline-block;"><span class="rupees" style="font-size: 24px; line-height: 1.5; color: #0D2366;"> {{$payment['amount_spread'][0]}} {{$payment['amount_spread'][1]}}</span><span class="paise" style="font-size: 16px; line-height: 1.5; color: #515978;">.{{$payment['amount_spread'][2]}}</span></div></div></div>
+      </div>
+
+              </td>
+            </tr>
+
+      </table>
+
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      </div>
+
+          <!--[if mso | IE]>
+            </td>
+
+        </tr>
+
+                  </table>
+                <![endif]-->
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+      </div>
+
+
+      <!--[if mso | IE]>
+          </td>
+        </tr>
+      </table>
+
+      <table
+         align="center" border="0" cellpadding="0" cellspacing="0" class="card-outlook title-outlook" style="width:600px;" width="600"
+      >
+        <tr>
+          <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
+      <![endif]-->
+
+
+      <div class="card title" style="box-sizing: border-box; background-color: #FFFFFF; width: 460px; padding-bottom: 16px; Margin: 0px auto; max-width: 85%;">
+
+        <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;">
+          <tbody>
+            <tr>
+              <td style="direction:ltr;font-size:0px;padding:0px;text-align:center;vertical-align:top;">
+                <!--[if mso | IE]>
+                  <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+
+        <tr>
+
+            <td
+               class="" style="vertical-align:top;width:600px;"
+            >
+          <![endif]-->
+
+      <div class="mj-column-per-100 outlook-group-fix" style="font-size:13px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;">
+
+      <table border="0" cellpadding="0" cellspacing="0" role="presentation" width="100%">
+        <tbody>
+          <tr>
+            <td style="vertical-align:top;padding:0px;">
+
+      <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="" width="100%">
+
+            <tr>
+              <td align="left" style="font-size:0px;padding:0px;word-break:break-word;">
+
+      <div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;line-height:1;text-align:left;color:#000000;">
+        <div class="center-align" style="text-align: center;"><p class="para para-banner para-light" style="margin: 0; font-size: 16px; line-height: 1.5; color: #7B8199;">Paid Successfully</p></div>
+      </div>
+
+              </td>
+            </tr>
+
+      </table>
+
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      </div>
+
+          <!--[if mso | IE]>
+            </td>
+
+        </tr>
+
+                  </table>
+                <![endif]-->
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+      </div>
+
+
+      <!--[if mso | IE]>
+          </td>
+        </tr>
+      </table>
+
+      <table
+         align="center" border="0" cellpadding="0" cellspacing="0" class="card-outlook highlight-outlook informative-outlook" style="width:600px;" width="600"
+      >
+        <tr>
+          <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
+      <![endif]-->
+
+
+      <div class="card highlight informative" style="box-sizing: border-box; background-color: #FFFFFF; width: 460px; border-top: 2px solid #528FF0; padding: 20px 16px; Margin: 0px auto; max-width: 85%; margin-top: 8px;">
+
+        <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;">
+          <tbody>
+            <tr>
+              <td style="direction:ltr;font-size:0px;padding:0px;text-align:center;vertical-align:top;">
+                <!--[if mso | IE]>
+                  <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+
+        <tr>
+
+            <td
+               class="" style="vertical-align:top;width:600px;"
+            >
+          <![endif]-->
+
+      <div class="mj-column-per-100 outlook-group-fix" style="font-size:13px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;">
+
+      <table border="0" cellpadding="0" cellspacing="0" role="presentation" width="100%">
+        <tbody>
+          <tr>
+            <td style="vertical-align:top;padding:0px;">
+
+      <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="" width="100%">
+
+            <tr>
+              <td align="left" style="font-size:0px;padding:0px;word-break:break-word;">
+
+      <div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;line-height:1;text-align:left;color:#000000;">
+        <div class="information-row" style="font-size: 14px; line-height: 1.5; width: 100%; box-sizing: border-box; padding-left: 40px; margin-bottom: 20px;"><div class="label" style="color: #7B8199; display: inline-block; width: calc(50% - 40px);">Payment Id</div><div class="value" style="display: inline-block; width: 50%;">{{$payment['public_id']}}</div></div><div class="information-row" style="font-size: 14px; line-height: 1.5; width: 100%; box-sizing: border-box; padding-left: 40px; margin-bottom: 20px;"><div class="label" style="color: #7B8199; display: inline-block; width: calc(50% - 40px);">Paid On</div><div class="value" style="display: inline-block; width: 50%;">{{$payment['created_at_formatted']}}</div></div><div class="information-row" style="font-size: 14px; line-height: 1.5; width: 100%; box-sizing: border-box; padding-left: 40px; margin-bottom: 20px;"><div class="label" style="color: #7B8199; display: inline-block; width: calc(50% - 40px);">Method</div><div class="value" style="display: inline-block; width: 50%;">{{$payment['method'][0]}}</div></div><div class="information-row" style="font-size: 14px; line-height: 1.5; width: 100%; box-sizing: border-box; padding-left: 40px;"><div class="label" style="color: #7B8199; display: inline-block; width: calc(50% - 40px);">Mobile Number</div><div class="value" style="display: inline-block; width: 50%;">{{ $customer['phone'] }}</div></div>
+      </div>
+
+              </td>
+            </tr>
+
+      </table>
+
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      </div>
+
+          <!--[if mso | IE]>
+            </td>
+
+        </tr>
+
+                  </table>
+                <![endif]-->
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+      </div>
+
+
+      <!--[if mso | IE]>
+          </td>
+        </tr>
+      </table>
+
+      <table
+         align="center" border="0" cellpadding="0" cellspacing="0" class="card-outlook highlight-outlook descriptive-outlook" style="width:600px;" width="600"
+      >
+        <tr>
+          <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
+      <![endif]-->
+
+
+      <div class="card highlight descriptive" style="box-sizing: border-box; background-color: #FFFFFF; width: 460px; border-top: 2px solid #528FF0; padding: 20px 16px; Margin: 0px auto; max-width: 85%; margin-top: 8px;">
+
+        <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;">
+          <tbody>
+            <tr>
+              <td style="direction:ltr;font-size:0px;padding:0px;text-align:center;vertical-align:top;">
+                <!--[if mso | IE]>
+                  <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+
+        <tr>
+
+            <td
+               class="" style="vertical-align:top;width:600px;"
+            >
+          <![endif]-->
+
+      <div class="mj-column-per-100 outlook-group-fix" style="font-size:13px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;">
+
+      <table border="0" cellpadding="0" cellspacing="0" role="presentation" width="100%">
+        <tbody>
+          <tr>
+            <td style="vertical-align:top;padding:0px;">
+
+      <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="" width="100%">
+
+            <tr>
+              <td align="left" style="font-size:0px;padding:0px;word-break:break-word;">
+
+      <div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;line-height:1;text-align:left;color:#000000;">
+        <div class="description" style="text-align: center; color: #515978; font-size: 14px; line-height: 1.5;">We are a payment gateway and only facilitate Netbrahma with online payments. We request you to contact Netbrahma for service related queries</div>
+      </div>
+
+              </td>
+            </tr>
+
+      </table>
+
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      </div>
+
+          <!--[if mso | IE]>
+            </td>
+
+        </tr>
+
+                  </table>
+                <![endif]-->
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+      </div>
+
+
+      <!--[if mso | IE]>
+          </td>
+        </tr>
+      </table>
+
+      <table
+         align="center" border="0" cellpadding="0" cellspacing="0" class="footer-outlook" style="width:600px;" width="600"
+      >
+        <tr>
+          <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
+      <![endif]-->
+
+
+      <div class="footer" style="width: 300px; Margin: 0px auto; max-width: 600px; margin-top: 8px; margin-bottom: 8px;">
+
+        <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;">
+          <tbody>
+            <tr>
+              <td style="direction:ltr;font-size:0px;padding:0px;text-align:center;vertical-align:top;">
+                <!--[if mso | IE]>
+                  <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+
+        <tr>
+
+            <td
+               class="" style="vertical-align:top;width:600px;"
+            >
+          <![endif]-->
+
+      <div class="mj-column-per-100 outlook-group-fix" style="font-size:13px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;">
+
+      <table border="0" cellpadding="0" cellspacing="0" role="presentation" width="100%">
+        <tbody>
+          <tr>
+            <td style="vertical-align:top;padding:0px;">
+
+      <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="" width="100%">
+
+            <tr>
+              <td align="left" style="font-size:0px;padding:0px;word-break:break-word;">
+
+      <div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;line-height:1;text-align:left;color:#000000;">
+        <div class="footer-text" style="font-size: 12px; line-height: 1.5; color: #7B8199; text-align: center; padding: 8px 0;">If you want to dispute this payment go through our&nbsp;<a class="link" href="https://dashboard.razorpay.com" target="_blank" style="text-decoration: none; color: #528FF0;">Customer Dispute Guide.</a></div>
+      </div>
+
+              </td>
+            </tr>
+
+      </table>
+
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      </div>
+
+          <!--[if mso | IE]>
+            </td>
+
+        </tr>
+
+                  </table>
+                <![endif]-->
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+      </div>
+
+
+      <!--[if mso | IE]>
+          </td>
+        </tr>
+      </table>
+      <![endif]-->
+
+
+      </div>
+
+      </body>
+    </html>
