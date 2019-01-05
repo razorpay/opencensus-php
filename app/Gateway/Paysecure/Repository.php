0df901c31311a787f8dd6f5811d9ade0e7c43f46
Albin George <albinin0002@gmail.com>
2019-01-05 15:46:20 +0530

    [paysecure] Add app fetch rules

--- app/Gateway/Paysecure/Repository.php
+++ app/Gateway/Paysecure/Repository.php
@@ -7,4 +7,10 @@ use RZP\Gateway\Base;
 class Repository extends Base\Repository
 {
     protected $entity = 'paysecure';
+
+    protected $appFetchParamRules = [
+        Entity::PAYMENT_ID             => 'sometimes|string|size:14',
+        Entity::GATEWAY_TRANSACTION_ID => 'sometimes',
+        Entity::RRN                    => 'sometimes',
+    ];
 }
