7046f1e761c51a33c1001371d517ffb40d216d47
Vivek Kumar <viveklakhera@gmail.com>
2019-01-07 13:04:50 +0530

    Add encrypt type check for BIN disablement

--- app/Gateway/Mpi/Blade/Gateway.php
+++ app/Gateway/Mpi/Blade/Gateway.php
@@ -114,13 +114,17 @@ class Gateway extends Base\Gateway
             case Base\Enrolled::Y:
                 if ($this->isIvrPayment($input) === true)
                 {
-                    if (empty($response[VERes::MESSAGE][VERes::VERES][VERes::EXTENSION][VERes::IVR_AUTH_DATA]) === true)
+                    if ((empty($response[VERes::MESSAGE][VERes::VERES][VERes::EXTENSION][VERes::IVR_AUTH_DATA]) === true) or
+                        (empty($response[VERes::MESSAGE][VERes::VERES][VERes::EXTENSION][VERes::IVR_AUTH_DATA_ENCRYPT_TYPE]) === false))
                     {
                         throw new Exception\GatewayErrorException(
                             ErrorCode::GATEWAY_ERROR_AUTHENTICATION_NOT_AVAILABLE,
                             null,
                             null,
-                            [],
+                            [
+                                'iin'    => $input['card']['iin'],
+                                'issuer' => $input['card']['issuer']
+                            ],
                             null,
                             BaseGateway\Action::AUTHENTICATE);
                     }
