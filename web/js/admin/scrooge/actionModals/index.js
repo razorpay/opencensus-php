6bb77f6d84da1d89697ef8ccf63f0211d4c119ac
Dheeraj Avvari <dj.mech.iitkgp@gmail.com>
2019-01-10 15:15:52 +0530

    Actions tab on Scrooge dashboard (#3072)
    * Actions tab on Scrooge dashboard
    
    * Common navbar and ui cleanup
    
    * Review changes
    
    * Fixing permission for update status

---
+++ web/js/admin/scrooge/actionModals/index.js
@@ -0,0 +1,1 @@
+export BulkUpdateStatus from './BulkUpdateStatus';
---
+++ web/js/admin/scrooge/constants.js
@@ -0,0 +1,7 @@
+export const updateStatusEvents = [
+  'file_init_event',
+  'file_sent_event',
+  'processed_event',
+  'refund_redo_event',
+  'onhold_event',
+];
