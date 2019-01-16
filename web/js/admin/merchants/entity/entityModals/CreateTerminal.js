538801c385e501b2872a5b1e5a2bc9b93dbde2be
dhrubesh <dhrubesh97@gmail.com>
2019-01-16 13:51:37 +0530

    added post req onsubmit

--- web/js/admin/merchants/entity/entityModals/CreateTerminal.js
+++ web/js/admin/merchants/entity/entityModals/CreateTerminal.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { ModalContent } from 'component/Modal';
+import { getDetailsViewMap } from '../entity-resources';
 
 import { isOrgHDFC } from 'admin/user';
 
@@ -15,72 +16,50 @@ import MultiSelectField from 'ui/MultiSelectField';
 import CurrencyData from './currency.json';
 
 export default class TerminalForm extends Component {
-  state = { pricingPlans: {} };
-
+  getMcc = () => {
+    const detailsMap = getDetailsViewMap(this.props.props);
+    console.log(detailsMap);
+    let mcc = detailsMap.find(o => o.label === 'MCC');
+    // let methods = detailsMap.find(o=>o.label==="Methods");
+    // console.log(methods.children());
+    console.log(mcc);
+    return mcc.value;
+  };
   // Creates terminal
   handleCreate = body => {
-    let file;
-
-    if (
-      body['gateway_terminal_password'] &&
-      body['gateway_terminal_password_confirmation'] !==
-        body['gateway_terminal_password']
-    ) {
-      notifyError('Password and confirmation password entered do not match');
-    } else {
-      delete body['gateway_terminal_password_confirmation'];
-    }
-
-    if (body.type) {
-      let temp = {};
-      body.type.split(',').forEach(elem => {
-        temp[elem] = '1';
-      });
-      body.type = temp;
-    }
-
-    if (body.file) {
-      file = body.file[0];
-    }
-
-    if (
-      body.gateway === 'first_data' &&
-      (file && file.type !== 'application/x-pkcs12')
-    ) {
-      notifyError(JSON.stringify('Invalid certificate file'));
-
-      return;
-    }
-
-    if (file) {
-      body.gateway_client_certificate = file;
-      delete body.file;
-    }
-
-    let mode = body.mode;
-    delete body.mode;
-
-    if (body.terminal_mode) {
-      body.mode = body.terminal_mode; // Terminal mode is sent as mode. And mode(test/live) is just for api url.
-    }
-
-    delete body.terminal_mode;
-
-    return adminFormUpload2(
-      body,
-      `/admin/api/${mode}/merchants/${this.props.merchantId}/terminals`
-    )
+    // var data, gateway_input,terminal;
+    console.log(body);
+    const {
+      pg_merchant_id,
+      gateway,
+      mid,
+      tid,
+      mcc,
+      currency_code,
+      trans_mode,
+    } = body;
+    var data = {
+      gateway: gateway,
+      gateway_input: {
+        mid: mid,
+        tid: tid,
+        mcc: mcc,
+        currency_code: currency_code,
+        trans_mode: trans_mode,
+      },
+      terminal: {
+        pg_merchant_id: pg_merchant_id,
+      },
+    };
+    return adminPost({
+      url: `merchants/${pg_merchant_id}/terminals/onboard`,
+      data: data,
+    })
       .then(response => {
+        console.log('response', response);
         if (response.data.success) {
-          notifySuccess('Terminal assigned successfully.');
+          notifySuccess('Terminal created successfully.');
           closeModal();
-
-          // We're displaying only live terminal on right side of merchant details, so update only for live mode
-          if (mode === 'live') {
-            this.props.props.updateTerminal(response.data.data);
-          }
-        } else {
-          response.data.errors.map(error => notifyError(error));
         }
       })
       .catch(err => {
@@ -94,23 +73,45 @@ export default class TerminalForm extends Component {
     console.log(this.props.props);
     console.log(this.props.props.Model);
     console.log(this.props.props.model);
+    // const detailsMap = getDetailsViewMap(this.props.props);
+    // console.log(detailsMap);
+    // let mcc = detailsMap.find(o=>o.label==="MCC");
+    // console.log(mcc);
     const { merchantId } = this.props;
     return (
       <ModalContent header={'Create Terminal'}>
         <Form class="entity-container" style={{ width: '600px' }}>
-          <Field label="Terminal id" defaultValue={merchantId} disabled />
+          <Field
+            label="Merchant id"
+            name="pg_merchant_id"
+            defaultValue={merchantId}
+            disabled
+          />
           <SelectField name="mode" label="Mode" defaultValue="live">
             <option value="test">Test</option>
             <option value="live">Live</option>
           </SelectField>
+          <SelectField
+            name="trans_mode"
+            label="Transaction mode"
+            defaultValue="CARDS"
+          >
+            <option value="CARDS">CARDS</option>
+            <option value="UPI">UPI</option>
+            <option value="BharatQR">BharatQR</option>
+          </SelectField>
           <SelectField name="gateway" label="Gateway" defaultValue="hitachi">
             <option value="hitachi">Hitachi</option>
           </SelectField>
-          <Field label="Terminal Category" name="category" />{' '}
-          {/* Prefil this */}
-          <Field label="Gateway Merchant Id" name="gateway_merchant_id" />
-          <Field label="Gateway Terminal Id" name="gateway_terminal_id" />
-          <SelectField name="currency" label="Currency" defaultValue="INR">
+          <Field
+            label="Terminal Category"
+            name="category"
+            defaultValue={this.getMcc()}
+            disabled
+          />
+          <Field label="Gateway Merchant Id" name="mid" />
+          <Field label="Gateway Terminal Id" name="tid" />
+          <SelectField name="currency_code" label="Currency" defaultValue="INR">
             <option value="" />
             {CurrencyData.data.map(({ code }) => (
               <option value={code}>{code}</option>
