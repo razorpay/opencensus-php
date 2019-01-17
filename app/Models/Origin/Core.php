4dbea9d8c38dc7ac12abec44d91742bf3dea3a3d
Pratik Kapasi <pratik.kapasi@razorpay.com>
2019-01-17 16:35:04 +0530

    [partners] associates using origin relation, moves origin fn to origin core

--- app/Models/Origin/Core.php
+++ app/Models/Origin/Core.php
@@ -2,20 +2,85 @@
 
 namespace RZP\Models\Origin;
 
+use Razorpay\OAuth\Application as OAuthApp;
+
 use RZP\Models\Base;
+use RZP\Trace\TraceCode;
 
 class Core extends Base\Core
 {
-    public function create(array $input, Base\PublicEntity $entity): Entity
+    public function create(Base\PublicEntity $entity, $originEntity, array $input = []): Entity
     {
         $origin = new Entity;
 
         $origin->build($input);
+
+        $origin->origin()->associate($originEntity);
 
         $origin->entity()->associate($entity);
 
         $this->repo->saveOrFail($origin);
 
         return $origin;
+    }
+
+    public function setOriginDetails(Base\PublicEntity $entity)
+    {
+        try
+        {
+            list($originType, $originId) = app('basicauth')->getOriginDetailsFromAuth();
+
+            $originEntity = $this->fetchOriginEntity($originType, $originId);
+
+            //
+            // Non null origin details are returned only for public auth, partner auth and bearer auth.
+            // Return if null values are returned.
+            //
+            if (empty($originEntity) === true)
+            {
+                return;
+            }
+
+            $origin = $this->create($entity, $originEntity);
+
+            return $origin;
+        }
+        catch (\Throwable $e)
+        {
+            // The payment should not be blocked even if the origin cannot be created. Log an error and proceed.
+            $this->trace->error(TraceCode::ORIGIN_SET_FAILED, [
+                'message'           => $e->getMessage(),
+                Entity::ENTITY_TYPE => $entity->getEntity(),
+                Entity::ENTITY_ID   => $entity->getId(),
+            ]);
+
+            return;
+        }
+    }
+
+    protected function fetchOriginEntity(string $originType, string $originId)
+    {
+        if (empty($originId) === true)
+        {
+            return null;
+        }
+
+        $originEntity = null;
+
+        switch ($originType)
+        {
+            case Constants::MERCHANT:
+                $originEntity = $this->repo->merchant->find($originId);
+                break;
+
+            case Constants::APPLICATION:
+                $originEntity = (new OAuthApp\Repository)->find($originId);
+                break;
+
+            default:
+                break;
+        }
+
+        return $originEntity;
     }
 }
--- app/Models/Payment/Processor/Processor.php
+++ app/Models/Payment/Processor/Processor.php
@@ -251,7 +251,8 @@ class Processor
 
             $paymentArray = $this->authorize($payment, $input, $gatewayInput);
 
-            $this->setOriginDetails($payment);
+            // Creates an origin entity for the payment based on the auth used to initiate the payment.
+            (new Origin\Core)->setOriginDetails($payment);
 
             return $paymentArray;
         }
@@ -267,45 +268,6 @@ class Processor
             (new Payment\Metric)->pushExceptionMetrics($e, Metric::PAYMENT_PROCESS_FAILED, $dimensions);
 
             throw $e;
-        }
-    }
-
-    /**
-     * Creates an origin entity for the payment based on the auth used to initiate the payment.
-     *
-     * @param Payment\Entity $payment
-     */
-    public function setOriginDetails(Payment\Entity $payment)
-    {
-        try
-        {
-            list($originType, $originId) = app('basicauth')->getOriginDetailsFromAuth();
-
-            //
-            // Non null origin details are returned only for public auth, partner auth and bearer auth.
-            // Return if null values are returned.
-            //
-            if ((empty($originType) === true) or (empty($originId) === true))
-            {
-                return;
-            }
-
-            $input = [
-                Origin\Entity::ORIGIN_ID   => $originId,
-                Origin\Entity::ORIGIN_TYPE => $originType,
-            ];
-
-            (new Origin\Core)->create($input, $payment);
-        }
-        catch (\Throwable $e)
-        {
-            // The payment should not be blocked even if the origin cannot be created. Log an error and proceed.
-            $this->trace->error(TraceCode::ORIGIN_SET_FAILED, [
-                'message'    => $e->getMessage(),
-                'payment_id' => $payment->getId(),
-            ]);
-
-            return;
         }
     }
 
