ceaf07d1e191ed921b4623ce6b4a78449086088b
Pratik Kapasi <pratik.kapasi@razorpay.com>
2019-01-18 12:06:45 +0530

    [partners] adds commissions entity & constants classes

---
+++ app/Models/Partner/Commission/Constants.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace RZP\Models\Partner\Commission;
+
+use RZP\Models\Base;
+
+class Constants
+{
+    const PAYMENT = 'payment';
+
+    /**
+     * List of entities for which the commission can be rolled out.
+     *
+     * The entities defined here must the following relations/functions defined -
+     *      $sourceEntity->merchant;
+     *      $sourceEntity->origin;
+     *      $sourceEntity->getCurrency();
+     *
+     * @var array
+     */
+    public static $sourceEntities = [
+        self::PAYMENT,
+    ];
+
+    /**
+     * @return array
+     */
+    public static function getSourceEntities(): array
+    {
+        return self::$sourceEntities;
+    }
+
+    public static function isValidCommissionSource(Base\PublicEntity $entity): bool
+    {
+        $entityType = $entity->getEntity();
+
+        if (in_array($entityType, self::getSourceEntities(), true) === false)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
