b1c70a965cf05300b05cde52b973a9708ae90f0b
Sumeeti Lalwani <sumeeti.lalwani@razorpay.com>
2019-01-29 23:36:49 +0530

    Allow aggrepay partner app to have app features (#10708)

--- tests/Functional/Helpers/Payment/PaymentTrait.php
+++ tests/Functional/Helpers/Payment/PaymentTrait.php
@@ -454,6 +454,35 @@ trait PaymentTrait
         return $content;
     }
 
+    protected function doS2sUpiPaymentPartner($client, $submerchantId, $payment = null, $server = null)
+    {
+        $server = [
+            'HTTP_X-Razorpay-Account' => $submerchantId,
+        ];
+
+        if ($payment === null)
+        {
+            $payment = $this->getDefaultPaymentArray();
+        }
+
+        $request = [
+            'method'  => 'POST',
+            'url'     => '/payments/create/upi',
+            'content' => $payment
+        ];
+
+        if (isset($server))
+        {
+            $request['server'] = $server;
+        }
+
+        $this->ba->privateAuth('rzp_test_partner_' . $client->getId(), $client->getSecret());
+
+        $content = $this->makeRequestAndGetContent($request);
+
+        return $content;
+    }
+
     protected function doS2SPrivateAuthAndCapturePayment($payment = null)
     {
         $paymentAuth = $this->doS2SPrivateAuthPayment($payment);
@@ -1987,6 +2016,37 @@ trait PaymentTrait
         ];
 
         $this->ba->publicAuth('rzp_test_partner_' . $clientId);
+        $content = $this->makeRequestAndGetContent($request);
+
+        return $content;
+    }
+
+    /**
+     * @param $payment
+     * @param $client
+     * @param $submerchantId
+     *
+     * @return bool|mixed|string
+     */
+    protected function doS2SPartnerAuthPayment($payment, $client, $submerchantId)
+    {
+        $server = [
+            'HTTP_X-Razorpay-Account' => $submerchantId,
+        ];
+
+        if ($payment === null)
+        {
+            $payment = $this->getDefaultPaymentArray();
+        }
+
+        $request = [
+            'method'  => 'POST',
+            'url'     => '/payments/create/redirect',
+            'content' => $payment,
+            'server'  => $server,
+        ];
+
+        $this->ba->privateAuth('rzp_test_partner_' . $client->getId(), $client->getSecret());
         $content = $this->makeRequestAndGetContent($request);
 
         return $content;
