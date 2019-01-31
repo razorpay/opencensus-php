3c04af61282dfc30f9cb6f8409537340664a8f48
Albin George <albinin0002@gmail.com>
2019-01-31 18:36:26 +0530

    [paysecure] Stop initiating multiple callbacks from handler JS function

--- resources/views/gateway/paysecurePinpadForm.blade.php
+++ resources/views/gateway/paysecurePinpadForm.blade.php
@@ -9,13 +9,29 @@
 </script>
 <script language="javascript" type="text/javascript">
 
+    redirected = false;
     //reads the response back from PaySecure
     function accu_FunctionResponse(strResponse) {
+
         // We receive only the response code in this case, which gets forwarded to the callback URL in the input
         // If the response code is ACCU999, it simply means that the PINPad lock was opened by the user, hence we
         // can ignore that trigger.
         if (strResponse != 'ACCU999' && strResponse != 'ISSUER999' && strResponse != "") {
-            location.href = "{{$data['callbackUrl']}}" + "?AccuResponseCode=" + strResponse;
+
+            // Sometimes, this function gets invoked from their library with the value "ACCU000" after getting invoked with
+            // the value "ISSUER000". Because of this, the callback request which was initiated with the value "ISSUER000"
+            // gets cancelled and another callback request with value "ACCU000" is initiated. This is handled on the api side.
+            // i.e, if we receive either ISSUER000 or ACCU000, the api marks the payment as success. However, to clearly identify
+            // that the callback is from the iFrame flow, we're blocking the callback redirection from Js after we received
+            // the first status response.
+
+            // We do not make a check to see if the code is ACCU000, because in case in the future if they send ACCU000 only
+            // then the callback requests would get blocked.
+            if (redirected == false) {
+                location.href = "{{$data['callbackUrl']}}" + "?AccuResponseCode=" + strResponse;
+            }
+
+            redirected = true;
         }
     }
 
