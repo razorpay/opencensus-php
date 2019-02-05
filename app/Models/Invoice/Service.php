f4027b4f93e17d2e67920b809fed017eb693b8ba
AbhinavArora95 <abhinavarora95@gmail.com>
2019-02-05 14:13:37 +0530

    Invoice service fetching invoices for a subscription id

--- app/Models/Invoice/Service.php
+++ app/Models/Invoice/Service.php
@@ -48,6 +48,15 @@ class Service extends Base\Service
         return $invoice->toArrayPublic();
     }
 
+    public function fetchInvoicesForSubscriptionId(string $subscriptionId, array $input): array
+    {
+        $id = \RZP\Models\Plan\Subscription\Entity::verifyIdAndStripSign($subscriptionId);
+
+        $invoices = $this->repo->invoice->fetchInvoicesForSubscriptionId($id, $input);
+
+        return $invoices->toArrayPublic();
+    }
+
     public function fetchMultiple(array $input): array
     {
         // Appends USER_ID in query input if userId available in headers via
