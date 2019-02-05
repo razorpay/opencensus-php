e985a8296771632c0e591c1cbde21fc2c8bacf73
Aagosh10 <aagosh1138@gmail.com>
2019-02-05 10:12:30 +0530

    Add paytm terminal rules (#10770)
    * Add paytm terminal rules
    
    * Add type in terminal rules
    
    * Add paytm in edit terminal gateways
    
    Using admin powers

--- app/Models/Terminal/Validator.php
+++ app/Models/Terminal/Validator.php
@@ -85,6 +85,7 @@ class Validator extends Base\Validator
         Payment\Gateway::NETBANKING_VIJAYA,
         Payment\Gateway::EMI_SBI,
         Payment\Gateway::WALLET_OLAMONEY,
+        Payment\Gateway::PAYTM,
     ];
 
     protected static $createValidators = [
@@ -640,6 +641,22 @@ class Validator extends Base\Validator
         Entity::EXPECTED                   => 'sometimes_if:type.bharat_qr,1|boolean',
         Entity::GATEWAY_ACCESS_CODE        => 'sometimes|string|in:proxy,app',
         Entity::ACCOUNT_NUMBER             => 'sometimes|string|max:50',
+    ];
+
+    protected static $paytmTerminalRules = [
+        Entity::GATEWAY                    => 'required|in:paytm',
+        Entity::GATEWAY_TERMINAL_ID        => 'required',
+        Entity::GATEWAY_ACCESS_CODE        => 'required',
+        Entity::GATEWAY_MERCHANT_ID        => 'required',
+        Entity::TYPE                       => 'sometimes',
+    ];
+
+    protected static $paytmEditTerminalRules = [
+        Entity::GATEWAY                    => 'required|in:paytm',
+        Entity::GATEWAY_TERMINAL_ID        => 'sometimes',
+        Entity::GATEWAY_ACCESS_CODE        => 'sometimes',
+        Entity::GATEWAY_MERCHANT_ID        => 'sometimes',
+        Entity::TYPE                       => 'sometimes',
     ];
 
     protected static $enachRblTerminalRules = [
