e918da609d0ce50970b0a03bcc8c95adb28aec1d
jitendra-1217 <jitendraojha1217@gmail.com>
2019-02-06 12:49:30 +0530

    Batch: Removes old tests for payout, fixes row status

--- app/Models/Batch/Processor/Payout.php
+++ app/Models/Batch/Processor/Payout.php
@@ -41,7 +41,8 @@ class Payout extends Base
 
             $payout = $this->processEntryForPayoutForFundAccount($entry, $fundAccount);
 
-            $entry[Batch\Header::STATUS]    = Batch\Status::PROCESSED;
+            // Each row's status for this batch type must be either processed or failed.
+            $entry[Batch\Header::STATUS]    = 'processed';
             $entry[Batch\Header::PAYOUT_ID] = $payout->getPublicId();
         });
     }
@@ -67,5 +68,22 @@ class Payout extends Base
         $input = Batch\Helpers\Payout::getPayoutInput($entry, $fundAccount, $this->merchant);
 
         return $this->payoutCore->createPayoutToFundAccount($input, $this->merchant);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function postProcessEntries(array & $entries)
+    {
+        parent::postProcessEntries($entries);
+
+        // Each row's status for this batch type must be either processed or failed.
+        foreach ($entries as & $entry)
+        {
+            if ($entry[Batch\Header::STATUS] === Batch\Status::FAILURE)
+            {
+                $entry[Batch\Header::STATUS] = 'failed';
+            }
+        }
     }
 }
