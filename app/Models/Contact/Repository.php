e8f10afe40b1eb76964ffd182fd2027f29d9ce2c
jitendra-1217 <jitendraojha1217@gmail.com>
2019-02-06 22:57:44 +0530

    Batch: Fixes contact existence query conditions

--- app/Models/Contact/Repository.php
+++ app/Models/Contact/Repository.php
@@ -27,6 +27,17 @@ class Repository extends Base\Repository
      */
     public function getContactWithSimilarDetails(array $input, Merchant\Entity $merchant)
     {
+        //
+        // If all of following attributes are empty then do not continue with query.
+        // This is because other attributes do not tell if it is similar i.e. multiple people can have same name etc.
+        //
+        if ((empty($input[Entity::CONTACT]) === true) and
+            (empty($input[Entity::EMAIL]) === true) and
+            (empty($input[Entity::REFERENCE_ID]) === true))
+        {
+            return;
+        }
+
         return $this->newQuery()
                     ->where(Entity::CONTACT, $input[Entity::CONTACT] ?? null)
                     ->where(Entity::EMAIL, $input[Entity::EMAIL] ?? null)
