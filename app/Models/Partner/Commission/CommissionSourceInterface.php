2dae1f7db2f2edb6bcf5ccd4e6ef13537527a067
Pratik Kapasi <pratik.kapasi@razorpay.com>
2019-02-12 18:44:02 +0530

    [commissions] adds comments and phpdocs

--- app/Models/Partner/Commission/CommissionSourceInterface.php
+++ app/Models/Partner/Commission/CommissionSourceInterface.php
@@ -2,13 +2,38 @@
 
 namespace RZP\Models\Partner\Commission;
 
+/**
+ * An entity for which a commission entity can be created, must implement this interface
+ *
+ * Interface CommissionSourceInterface
+ *
+ * @package RZP\Models\Partner\Commission
+ */
 interface CommissionSourceInterface
 {
+    /**
+     * Returns the name of the entity - payment, refund, transfer etc
+     *
+     * @return mixed
+     */
     public function getEntity();
 
+    /**
+     * The entity must have a merchant relation defined
+     *
+     * @return mixed
+     */
     public function merchant();
 
+    /**
+     * @return mixed
+     */
     public function getCurrency();
 
+    /**
+     * The entity must have a transaction relation defined
+     *
+     * @return mixed
+     */
     public function transaction();
 }
