0007ca3eb70416b1c8b701d6ded7462f6aee42b0
Akash Sharma <akashsharma@live.in>
2019-02-13 18:35:59 +0530

    Capture verify (#10487)
    * Adds capture verify implementation

--- app/Models/Payment/Gateway.php
+++ app/Models/Payment/Gateway.php
@@ -965,6 +965,10 @@ class Gateway
         self::NETBANKING_VIJAYA,
     ];
 
+    public static $captureVerifyEnabled = [
+        self::HITACHI,
+    ];
+
     /**
      * List of gateways that support recurring payments
      *
