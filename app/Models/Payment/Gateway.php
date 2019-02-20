c5700d5b25dbe5b7c31aa32f68e98ee6920766a4
neerajkanuri <neerajkanuri@gmail.com>
2019-02-20 19:36:49 +0530

    [refunds]: exposing refunds public status to specific merchants (#10911)

--- app/Models/Payment/Gateway.php
+++ app/Models/Payment/Gateway.php
@@ -538,7 +538,7 @@ class Gateway
      *
      * @var array
      */
-    public static $scroogeMerchants = [
+    public static $refundsPublicStatusMerchants = [
         '9DZkE60krEG4wq',
         '9ncOh0EZ8sC9z9',
         '9hefgkvGhT18Q9',
@@ -1405,9 +1405,9 @@ class Gateway
         return array_keys(self::$scroogeGateways);
     }
 
-    public static function getScroogeMerchants(): array
+    public static function getRefundsPublicStatusMerchants(): array
     {
-        return self::$scroogeMerchants;
+        return self::$refundsPublicStatusMerchants;
     }
 
     public static function isIssuerSupportedForPinAuthType($issuer, $gateway, $acquirer)
@@ -1434,12 +1434,6 @@ class Gateway
      */
     public static function isScroogeGatewayAndMerchant(string $gateway = null, string $merchantId = null): bool
     {
-        if (empty($merchantId) === false)
-        {
-            return ((in_array($gateway, self::getScroogeGateways(), true) === true) and
-                (in_array($merchantId, self::getScroogeMerchants(), true) === true));
-        }
-
         return (in_array($gateway, self::getScroogeGateways(), true) === true);
     }
 
@@ -1458,6 +1452,18 @@ class Gateway
             isset(self::$scroogeGateways[$gateway][self::GO_LIVE_TIMESTAMP]) and
             $timestamp > self::$scroogeGateways[$gateway][self::GO_LIVE_TIMESTAMP]
         );
+    }
+
+    /**
+     * This function checks if a given merchant is to be shown refund's Public status.
+     *
+     * @param $merchantId
+     * @return bool
+     *
+     */
+    public static function isRefundsPublicStatusMerchant(string $merchantId = null): bool
+    {
+        return (in_array($merchantId, self::getRefundsPublicStatusMerchants(), true) === true);
     }
 
     /**
