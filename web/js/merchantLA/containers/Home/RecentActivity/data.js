3428e1d2730b531746ed89232f9118803a437559
rchaubey <dev.rohit.chaubey@gmail.com>
2019-02-22 17:27:24 +0530

    added currency support in recent activity payment list

--- web/js/merchantLA/containers/Home/RecentActivity/data.js
+++ web/js/merchantLA/containers/Home/RecentActivity/data.js
@@ -12,8 +12,8 @@ const commonMeta = {
   columns: [
     {
       recordKey: 'amount',
-      transfomer: (value, record, tabName, displayCompact) => {
-        const component = <Amount value={value} />;
+      transfomer: (value, record, tabName, displayCompact, currency) => {
+        const component = <Amount value={value} currency={currency} />;
 
         return displayCompact ? (
           <Link to={`/${tabName}/${record.id}`}>{component}</Link>
