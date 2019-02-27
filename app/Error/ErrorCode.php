de085b67f1585e1295b8a6160bb785d4a16cf341
Sumeeti Lalwani <sumeeti.lalwani@razorpay.com>
2019-02-27 17:43:04 +0530

    Block card refunds by merchant when feature disable_card_refunds is a… (#10946)
    * block card refunds by merchant when feature disable_card_refunds is assigned
    
    * error msg minor modification

--- app/Error/ErrorCode.php
+++ app/Error/ErrorCode.php
@@ -614,6 +614,7 @@ class ErrorCode
 
     const BAD_REQUEST_REFUND_FAILED                                                 = 'BAD_REQUEST_REFUND_FAILED';
     const BAD_REQUEST_REFUND_NOT_ALLOWED                                            = 'BAD_REQUEST_REFUND_NOT_ALLOWED';
+    const BAD_REQUEST_CARD_REFUND_NOT_ALLOWED                                       = 'BAD_REQUEST_CARD_REFUND_NOT_ALLOWED';
     const BAD_REQUEST_REFUND_NOT_ENOUGH_BALANCE                                     = 'BAD_REQUEST_REFUND_NOT_ENOUGH_BALANCE';
     const BAD_REQUEST_REFUND_NOT_ENOUGH_CREDITS                                     = 'BAD_REQUEST_REFUND_NOT_ENOUGH_CREDITS';
     const BAD_REQUEST_REFUND_INVALID_STATE_TO_PROCESSED                             = 'BAD_REQUEST_REFUND_INVALID_STATE_TO_PROCESSED';
--- app/Error/PublicErrorDescription.php
+++ app/Error/PublicErrorDescription.php
@@ -190,6 +190,7 @@ class PublicErrorDescription
     const BAD_REQUEST_PAYMENT_ORDER_ALREADY_PAID                                = 'Payment already done for this order.';
     const BAD_REQUEST_REFUND_FAILED                                             = 'Refund failed';
     const BAD_REQUEST_REFUND_NOT_ALLOWED                                        = 'Refunds cannot be created on your account.';
+    const BAD_REQUEST_CARD_REFUND_NOT_ALLOWED                                   = 'Refunds cannot be created on your account for card payments';
     const BAD_REQUEST_PAYMENT_ALREADY_REFUNDED                                  = 'Refund failed';
     const BAD_REQUEST_REFUND_NOT_ENOUGH_BALANCE                                 = 'Your account does not have enough balance to carry out the refund operation. You can add funds to your account from your Razorpay dashboard or capture new payments.';
     const BAD_REQUEST_REFUND_NOT_ENOUGH_CREDITS                                 = 'Your account does not have enough credits to carry out the refund operation.';
