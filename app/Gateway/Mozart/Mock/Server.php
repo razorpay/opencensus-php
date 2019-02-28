9299d16fa3b400f1386657ea94e7ea3751be58a1
vishalashank <vishal.ashank@razorpay.com>
2019-02-28 15:51:59 +0530

    added otp fail case

--- app/Gateway/Mozart/Mock/Server.php
+++ app/Gateway/Mozart/Mock/Server.php
@@ -1,13 +1,10 @@
 <?php
 
-namespace RZP\Gateway\Cybersource\Mock;
+namespace RZP\Gateway\Mozart\Mock;
 
 use Str;
 use RZP\App;
-use Carbon\Carbon;
 use RZP\Gateway\Base;
-use RZP\Gateway\Cybersource;
-use RZP\Gateway\Cybersource\Fields as F;
 
 class Server extends Base\Mock\Server
 {
@@ -61,24 +58,58 @@ class Server extends Base\Mock\Server
         $input = json_decode($input, true);
         $entities = $input['entities'];
 
-        $response = [
-            'data' =>
-                [
-                    'Errordescription' => 'TRANSACTION PERFORMED SUCCESSFULLY',
-                    'Key' => $entities['terminal']['gateway_secure_secret'],
-                    'MobileNo' => '2376',
-                    'RequestID' => 'RZP190219162906768',
-                    'Responsecode' => '0',
-                    'status' => 'created',
-                    'OrderNo' => '104',
-                    'DealID' => 'CS905114097404',
-                    '_raw' => '',
-                ],
-            'error'             => null,
-            'success'           => true,
-            'mozart_id'         => '',
-            'external_trace_id' => '',
-        ];
+        $this->verifyOtpAttempts($entities['payment']);
+
+        $otp = $entities['gateway']['redirect']['otp'];
+        switch($otp)
+        {
+            case 111111:
+                $response = [
+                    'data' =>
+                        [
+                            'Errordescription' => 'TRANSACTION PERFORMED SUCCESSFULLY',
+                            'Key' => $entities['terminal']['gateway_secure_secret'],
+                            'MobileNo' => '2376',
+                            'RequestID' => 'RZP190219162906768',
+                            'Responsecode' => '0',
+                            'status' => 'created',
+                            'OrderNo' => '104',
+                            'DealID' => 'CS905114097404',
+                            '_raw' => '',
+                        ],
+                    'error'             => null,
+                    'success'           => true,
+                    'mozart_id'         => '',
+                    'external_trace_id' => '',
+                ];
+                break;
+            default:
+                $response = [
+                    'data' =>
+                        [
+                            'Errordescription' => 'Transaction Status : Failed [L3].  Reason : INVALID OR EXPIRED OTP',
+                            'Key' => $entities['terminal']['gateway_secure_secret'],
+                            'MobileNo' => '2376',
+                            'RequestID' => 'RZP190219162906768',
+                            'Responsecode' => 'L3',
+                            'status' => 'creation_failed',
+                            'OrderNo' => '104',
+                            'DealID' => '905909104900',
+                            '_raw' => '',
+                        ],
+                    'error'             => [
+                        'description' => 'Transaction Status : Failed [L3].  Reason : INVALID OR EXPIRED OTP',
+                        'gateway_error_code' => 'L3',
+                        'gateway_error_description' => 'Transaction Status : Failed [L3].  Reason : INVALID OR EXPIRED OTP',
+                        'gateway_status_code' => 200,
+                        'internal_error_code' => 'BAD_REQUEST_PAYMENT_OTP_INCORRECT_OR_EXPIRED',
+
+                    ],
+                    'success'           => false,
+                    'mozart_id'         => '',
+                    'external_trace_id' => '',
+                ];
+        }
 
         $this->content($response, 'pay_verify');
 
