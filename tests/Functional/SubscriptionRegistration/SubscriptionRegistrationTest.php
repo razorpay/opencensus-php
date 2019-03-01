e31b293bf0086f562134d00c01374eb067176302
Abhay P <abhay.prabhakaran@razorpay.com>
2019-03-01 14:54:32 +0530

    test cases for hdfc type mandate auth links

--- tests/Functional/SubscriptionRegistration/SubscriptionRegistrationTest.php
+++ tests/Functional/SubscriptionRegistration/SubscriptionRegistrationTest.php
@@ -277,6 +277,27 @@ class SubscriptionRegistrationTest extends TestCase
         $this->assertEquals($payment->getNotesJson(), $invoice->getNotesJson());
     }
 
+    public function testFutureTokenConfirmedEmandateLinks()
+    {
+        $this->startTest();
+
+        $order = $this->getDbLastEntity('order');
+
+        $payment = $this->setupHdfcEmandateAndGetPaymentRequest();
+
+        $payment['order_id'] = $order->getPublicId();
+
+        $this->doAuthPayment($payment);
+
+        $payment = $this->getDbLastEntity("payment");
+
+        $order = $this->getDbEntityById('order', $order->getPublicId());
+
+        $this->assertEquals($payment->getStatus(), "authorized");
+
+        $this->assertEquals($order->getStatus(), "attempted");
+    }
+
     public function testPayAuthLink()
     {
         $this->startTest();
@@ -364,6 +385,40 @@ class SubscriptionRegistrationTest extends TestCase
             'account_number'    => '123123123',
             'name'              => 'test name',
             'ifsc'              => 'UTIB0002766'
+        ];
+
+        $expireBy = Carbon::now(Timezone::IST)->addDays(10)->getTimestamp();
+
+        $payment['recurring_token'] = [
+            'max_amount' => 3000,
+            'expire_by' => $expireBy,
+        ];
+
+        return $payment;
+    }
+
+    protected function setupHdfcEmandateAndGetPaymentRequest()
+    {
+        $this->mockCardVault();
+
+        $this->fixtures->create('terminal:shared_emandate_hdfc_terminal');
+
+        $this->fixtures->create('terminal:disable_default_hdfc_terminal');
+
+        $this->fixtures->merchant->addFeatures(['charge_at_will']);
+
+        $this->fixtures->merchant->enableEmandate();
+
+        $payment = $this->getEmandateNetbankingRecurringPaymentArray("HDFC", "0");
+
+        $order = $this->fixtures->create('order:emandate_order', ['amount' => $payment['amount']]);
+
+        $payment['order_id'] = $order->getPublicId();
+
+        $payment['bank_account'] = [
+            'account_number'    => '123123123',
+            'name'              => 'test name',
+            'ifsc'              => 'HDFC0001233'
         ];
 
         $expireBy = Carbon::now(Timezone::IST)->addDays(10)->getTimestamp();
