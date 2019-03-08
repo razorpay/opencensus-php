9811c65ff0e329e584a039737c9dc3f422b4656a
Anshul Sahni <anshul.sahni@razorpay.com>
2019-03-08 14:14:35 +0530

    [PARNTER]: modules, model and list items added for Commisions

--- web/js/rzp/ui/item/pair.js
+++ web/js/rzp/ui/item/pair.js
@@ -59,6 +59,7 @@ export const tokenId = { title: 'Token Id', value: id.token };
 export const creditId = { title: 'Credit Id', value: id.credit };
 export const submerchant = { title: 'Merchant Name', value: id.submerchant };
 export const submerchantId = { title: 'Merchant ID', value: id.submerchantId };
+export const commissionId = { title: 'Commission ID', value: id.commission };
 
 export const mapValues = values => title => {
   return { title, value: item => values[item.id] };
