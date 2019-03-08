d7ef28ac4d6fe5cfde900bd25c391fd390dfd13d
Ratan Phayade <ratanphayade@users.noreply.github.com>
2019-03-08 11:56:42 +0530

    [recon] yb upi utr for failed case fixed (#11079)

--- app/Models/FundTransfer/Yesbank/Request/Status.php
+++ app/Models/FundTransfer/Yesbank/Request/Status.php
@@ -162,7 +162,10 @@ class Status extends Base
         //
 
         $ftaId = $response[Constants::UPI_REQUEST_REFERENCE_NUMBER] ?? null;
+
         $utr = $response[Constants::UPI_UNIQUE_RESPONSE_NUMBER] ?? null;
+        $utr = (strtolower($utr) !== 'na')? $utr : null;
+
         $bankReferenceNumber = $response[Constants::UPI_BANK_REFERENCE_NUMBER] ?? null;
 
         $statusCode = $response[Constants::UPI_STATUS_CODE] ?? null;
