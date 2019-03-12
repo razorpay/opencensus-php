eb98310ad366fcb2331333bf7c9258750a9b1e27
Abhay P <abhay.prabhakaran@razorpay.com>
2019-03-12 17:02:11 +0530

    webhook account id fix

--- app/Models/Merchant/Webhook/Core.php
+++ app/Models/Merchant/Webhook/Core.php
@@ -5,6 +5,7 @@ namespace RZP\Models\Merchant\Webhook;
 use RZP\Jobs;
 use RZP\Models;
 use RZP\Exception;
+use Carbon\Carbon;
 use RZP\Models\Base;
 use RZP\Models\Merchant;
 use RZP\Models\Merchant\Webhook;
@@ -85,11 +86,15 @@ class Core extends Base\Core
         String $event,
         Webhook\Entity $webhook) : array
     {
+        $signedAccountId = Merchant\Account\Entity::getSignedId($merchant->getId());
+
         $attributes = [
             Models\Event\Entity::EVENT      => $event,
-            Models\Event\Entity::ACCOUNT_ID => $merchant->getId(),
+            Models\Event\Entity::ACCOUNT_ID => $signedAccountId,
             Models\Event\Entity::CONTAINS   => array_keys($payload),
+            Models\Event\Entity::CREATED_AT => Carbon::now()->getTimestamp(),
         ];
+
         $event = new Models\Event\Entity($attributes);
 
         $event->setPayload($payload);
