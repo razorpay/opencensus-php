653a1635034ed156132ee5e8737007616b1f034e
Albin George <albinin0002@gmail.com>
2019-03-12 11:06:26 +0530

    [sbi_emi] Hex encryption key before sending it over email

--- app/Models/Gateway/File/Processor/Emi/Sbi.php
+++ app/Models/Gateway/File/Processor/Emi/Sbi.php
@@ -64,10 +64,10 @@ class Sbi extends Base
     {
         if ($this->app->environment(Environment::TESTING))
         {
-            return self::TEST_ENCRYPTION_KEY;
+            return bin2hex(self::TEST_ENCRYPTION_KEY);
         }
 
-        return openssl_random_pseudo_bytes(256);
+        return bin2hex(openssl_random_pseudo_bytes(256));
     }
 
     /**
@@ -99,7 +99,7 @@ class Sbi extends Base
                         Service::ENCRYPTION_TYPE,
                         [
                             'mode'   => Service::ENCRYPTION_MODE,
-                            'secret' => $data['password'],
+                            'secret' => hex2bin($data['password']),
                         ])
                     ->type(static::FILE_TYPE)
                     ->entity($this->gatewayFile)
