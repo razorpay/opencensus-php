aca3819b338d3b27e2352fea497574adb5b283f9
Vikas Naidu <vck173@gmail.com>
2019-03-14 17:19:09 +0530

    [Cybersource] added authentication terminals and gateway rules (#11174)

--- app/Models/Terminal/AuthenticationTerminals.php
+++ app/Models/Terminal/AuthenticationTerminals.php
@@ -133,5 +133,26 @@ class AuthenticationTerminals
             self::AUTH_TYPE                 => AuthType::_3DS,
             self::GATEWAY_AUTH_TYPE         => AuthType::_3DS,
         ],
+        [
+            self::MERCHANT_ID               => Account::SHARED_ACCOUNT,
+            self::GATEWAY                   => Gateway::CYBERSOURCE,
+            self::AUTHENTICATION_GATEWAY    => Gateway::MPI_BLADE,
+            self::AUTH_TYPE                 => AuthType::IVR,
+            self::GATEWAY_AUTH_TYPE         => AuthType::OTP,
+        ],
+        [
+            self::MERCHANT_ID               => Account::SHARED_ACCOUNT,
+            self::GATEWAY                   => Gateway::CYBERSOURCE,
+            self::AUTHENTICATION_GATEWAY    => Gateway::MPI_ENSTAGE,
+            self::AUTH_TYPE                 => AuthType::OTP,
+            self::GATEWAY_AUTH_TYPE         => AuthType::OTP,
+        ],
+        [
+            self::MERCHANT_ID               => Account::SHARED_ACCOUNT,
+            self::GATEWAY                   => Gateway::CYBERSOURCE,
+            self::AUTHENTICATION_GATEWAY    => Gateway::MPI_BLADE,
+            self::AUTH_TYPE                 => AuthType::HEADLESS_OTP,
+            self::GATEWAY_AUTH_TYPE         => AuthType::_3DS,
+        ],
     ];
 }
