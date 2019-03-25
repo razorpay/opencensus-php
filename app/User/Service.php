ce6c195667cc48d72326e1d0087d3da791598287
Patlola <praveen.patlola@razorpay.com>
2019-03-25 09:32:59 +0000

    remove unwanted razorx calls form dashboard user

--- app/User/Service.php
+++ app/User/Service.php
@@ -413,11 +413,9 @@ class Service extends Base\Service
                     }
 
                     $data['experiments']['subscription_link'] = $merchantService->getTreatment('subscription_link');
-                    $data['experiments']['mjml_based_mailers'] = $merchantService->getTreatment('mjml_based_mailers');
                     $data['experiments']['coupons'] = $merchantService->getTreatment('coupons');
                     $data['experiments']['is_announcement'] = $merchantService->getTreatment('is_announcement');
                     $data['experiments']['is_banner'] = $merchantService->getTreatment('is_banner');
-                    $data['experiments']['paymentpagesv2'] = $merchantService->getTreatment('paymentpagesv2');
                     $data['experiments']['capital_announcement'] = $merchantService->getTreatment('capital_announcement');
                     $data['experiments']['capital_banner'] = $merchantService->getTreatment('capital_banner');
                     $data['experiments']['subscriptions_live'] = $merchantService->getTreatment('subscriptions_live');
