5c91106a83c96838cabab9248c1cb746b16d9bb6
Abhay P <abhay.prabhakaran@razorpay.com>
2019-03-27 16:51:41 +0530

    refactoring little bit

--- app/Modules/Subscriptions/Factory.php
+++ app/Modules/Subscriptions/Factory.php
@@ -18,29 +18,26 @@ class Factory
 
     protected static function shouldUseExternalService(): bool
     {
-        $merchantFeatureCheck = false;
+
+        if (Route::currentRouteName() === 'payment_create_subscriptions')
+        {
+            // Used for subscription service charges, eg. test_charge, manual_charge, cron, etc.
+           return true;
+        }
 
         $merchant = app('basicauth')->getMerchant();
 
         if ($merchant !== null)
         {
-            if (Route::currentRouteName() === 'payment_create_subscriptions')
-            {
-                // Used for subscription service charges, eg. test_charge, manual_charge, cron, etc.
-                $merchantFeatureCheck = true;
-            }
-            else
-            {
                 // Used for subscription authentication payment
                 $response = app()->razorx->getTreatment($merchant->getId(), 'auth_flow_redirect_to_subserv', app()['rzp.mode']);
 
                 if (strtolower($response) === 'on')
                 {
-                    $merchantFeatureCheck = true;
+                    return true;
                 }
-            }
         }
 
-        return ($merchantFeatureCheck === true);
+        return false;
     }
 }
