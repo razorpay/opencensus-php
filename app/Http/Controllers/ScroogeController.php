1ca36255a88b8bb9fa5e2ee6c210b8abd3e28274
neerajkanuri <neerajkanuri@gmail.com>
2019-04-08 16:35:38 +0530

    [scrooge][dashboard]: sending error message from scrooge in case of 4… (#11447)
    > LGTM. Please close this with @ratanphayade / @kajolnigam also. Thanks!
    
    Done

--- app/Http/Controllers/ScroogeController.php
+++ app/Http/Controllers/ScroogeController.php
@@ -3,6 +3,8 @@
 namespace RZP\Http\Controllers;
 
 use ApiResponse;
+use RZP\Error\Error;
+use RZP\Error\ErrorCode;
 
 class ScroogeController extends Controller
 {
@@ -58,6 +60,16 @@ class ScroogeController extends Controller
     public function downloadGatewayRefundsFile()
     {
         $response = $this->app['scrooge']->downloadGatewayRefundsFile($this->input);
+
+        if ($response['code'] === 400)
+        {
+            $publicErrorMessage = json_decode(json_encode($response['body']), true)['public_error']['message']
+                ?? 'service request failed';
+
+            $error = new Error(ErrorCode::BAD_REQUEST_SCROOGE_DASHBOARD_ERROR, $publicErrorMessage);
+
+            return ApiResponse::generateErrorResponse($error);
+        }
 
         return ApiResponse::json($response['body'], $response['code']);
     }
