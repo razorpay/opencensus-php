dbaa5f25c99c029ca9e482084ac63824819825bf
Abhinav Venkatesh <abhinav.venkatesh@razorpay.com>
2019-04-10 15:59:44 +0530

    [gateway] Hardcode zeta Url

--- app/Gateway/Mozart/Gateway.php
+++ app/Gateway/Mozart/Gateway.php
@@ -342,8 +342,13 @@ class Gateway extends Base\Gateway
 
     protected function getMozartRequest($content, $gateway)
     {
-        // temporary change
-        $baseUrl = 'https://zeta-mozart.stage.razorpay.in/';
+        $baseUrl = $this->app['config']->get('applications.mozart.url');
+
+        // temporary change given to external bank for testing
+        if ($gateway === Payment\Gateway::NETBANKING_SIB)
+        {
+            $baseUrl = 'https://zeta-mozart.stage.razorpay.in/';
+        }
 
         $url =  $baseUrl . 'payments/' . $gateway . '/v1/' . $this->action;
 
