84837a4a66b88fbf08c0cd47c0b35f7d9d5ed8c9
Anshul Sahni <anshul.sahni@razorpay.com>
2019-04-10 14:05:46 +0530

    [COMPONENTS]: Styling improvement in FeeBreakup
    	- `+` sign between two components

--- web/js/rzp/ui/FeeBreakup.js
+++ web/js/rzp/ui/FeeBreakup.js
@@ -2,7 +2,12 @@ export default function(props) {
   const children = props.children;
   return (
     <div class="FeeBreakup">
-      {children.map(child => <div class="FeeBreakup--item">{child}</div>)}
+      {children.map((child, index) => (
+        <React.Fragment key={index}>
+          {index > 1 ? <div class="FeeBreakup--add-symbol">+</div> : null}
+          <div class="FeeBreakup--item">{child}</div>
+        </React.Fragment>
+      ))}
     </div>
   );
 }
