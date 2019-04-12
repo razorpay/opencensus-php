0616aafc94050d77c0688fa9e24f4cfe7838c9ca
Abhay P <abhay.prabhakaran@razorpay.com>
2019-04-12 16:16:21 +0530

    adding the flag to all rbl ids

--- app/Models/Invoice/ViewDataSerializer.php
+++ app/Models/Invoice/ViewDataSerializer.php
@@ -111,6 +111,7 @@ class ViewDataSerializer extends Base\Core
                 $customLabels = [
                     'receipt_number'           => 'CREDIT CARD NUMBER',
                     'first_payment_min_amount' => 'MINIMUM AMOUNT DUE',
+                    'hide_issued_to' => true,
                 ];
 
                 break;
@@ -122,6 +123,7 @@ class ViewDataSerializer extends Base\Core
                 $customLabels = [
                     'receipt_number'           => 'LOAN ACCOUNT NUMBER',
                     'first_payment_min_amount' => 'EMI AMOUNT',
+                    'hide_issued_to' => true,
                 ];
 
                 break;
