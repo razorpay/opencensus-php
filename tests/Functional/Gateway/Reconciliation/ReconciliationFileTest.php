7f1236bf61a53cf6aa485b5595abe76a5d6d72ff
Vidya Sagar Kushwaha <vidyasagar.kushwaha@razorpay.com>
2019-04-16 20:08:07 +0530

    [recon] Minor fix in upi_icici and hitachi recon (#11537)
    * [recon] Hitachi: now using transaction_type to decide recon_type
    
    * [recon] Hitachi : refactor and test case fix

--- tests/Functional/Gateway/Reconciliation/ReconciliationFileTest.php
+++ tests/Functional/Gateway/Reconciliation/ReconciliationFileTest.php
@@ -1160,6 +1160,7 @@ class ReconciliationFileTest extends TestCase
         $facade = $this->overrideHitachiPayment($payment, $forceOverride);
 
         $facade['message_type'] = '0220';
+        $facade['transaction_type'] = '20';
         $facade[HitachiRefundRecon::COLUMN_REFUND_ID] = $payment['refund_id'];
 
         return $facade;
