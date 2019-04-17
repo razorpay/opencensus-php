770b639343fd6544906f69d8f45a241f01f281e1
Vikas Naidu <vck173@gmail.com>
2019-04-17 14:22:37 +0530

    Remove save all cards razorx call (#11541)

--- app/Models/Payment/Processor/Authorize.php
+++ app/Models/Payment/Processor/Authorize.php
@@ -4434,16 +4434,6 @@ trait Authorize
             $cardInput[Card\Entity::VAULT] = Card\Vault::RZP_VAULT;
         }
 
-        if ($vault === false)
-        {
-            $response = $this->app->razorx->getTreatment($merchant->getId(), 'save_all_cards', $this->mode);
-
-            if (strtolower($response) === 'on')
-            {
-              $cardInput[Card\Entity::VAULT] = Card\Vault::RZP_ENCRYPTION;
-            }
-        }
-
         $cardCore = new Card\Core;
 
         $cardData = $cardCore->createAndReturnWithSensitiveData($cardInput, $merchant);
