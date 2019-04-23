b51fe8fc45f541e5745fd7feb73e06a0cc7baeb8
Anshul Sahni <anshul.sahni@razorpay.com>
2019-04-23 12:59:23 +0530

    [COMPONENTS]: Changesin FeeBreakup style and added .font-lg
    	- Added .font-lg in bootstrap-ext.styl
    	- Added three types in <FeeBreakup/>

--- web/js/rzp/ui/FeeBreakup.js
+++ web/js/rzp/ui/FeeBreakup.js
@@ -1,7 +1,9 @@
-export default function(props) {
+import { classList } from 'common/util';
+
+export default function({ type = 'primary', ...props }) {
   const children = props.children;
   return (
-    <div class="FeeBreakup">
+    <div class={classList('FeeBreakup', type)}>
       {children.map((child, index) => (
         <React.Fragment key={index}>
           {index > 1 ? <div class="FeeBreakup--add-symbol">+</div> : null}
