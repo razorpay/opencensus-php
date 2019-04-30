ee5c7b541e109609e6e9fec12a0eaf1d21eddf26
Vikas Naidu <vck173@gmail.com>
2019-04-30 16:02:47 +0530

    [Redirect] fail the payment if its in failed state (#11311)

--- app/Models/Payment/Processor/Authorize.php
+++ app/Models/Payment/Processor/Authorize.php
@@ -5334,6 +5334,13 @@ trait Authorize
                     return $this->processPaymentCallbackSecondTime($payment);
                 }
 
+                if (($payment->isCreated() === false) or
+                    ($payment->hasTerminal() === true))
+                {
+                    throw new Exception\BadRequestException(
+                        ErrorCode::BAD_REQUEST_PAYMENT_ALREADY_PROCESSED);
+                }
+
                 $this->repo->saveOrFail($payment);
 
                 $gatewayInput = $inputDetails['gateway_input'];
