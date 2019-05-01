5af45a8e74936c25611c69e257dae3722bea4ef7
dvvavinash <dvv.avinash@gmail.com>
2019-05-01 12:49:00 +0530

    removing redux devtool middleware

--- web/js/merchantLA/store.js
+++ web/js/merchantLA/store.js
@@ -2,11 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import apiAsyncMiddleware from 'rzp/middlewares/apiAsyncMiddleware';
 import reducers from 'merchantLA/reducers';
 
-const store = createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(apiAsyncMiddleware)
-);
+const store = createStore(reducers, applyMiddleware(apiAsyncMiddleware));
 
 export default store;
 
