1bb1c7b23611f5604dfa312b9211a7596a108922
Abhinav Venkatesh <abhinav.venkatesh@razorpay.com>
2019-05-07 23:32:32 +0530

    Add test env variables

--- config/gateway.php
+++ config/gateway.php
@@ -717,13 +717,9 @@ return [
     ],
 
     'mozart' => [
-        'upi_airtel_test_hash_secret' => env('UPI_AIRTEL_TEST_HASH_SECRET'),
+        'upi_airtel_test_hash_secret'     => env('UPI_AIRTEL_TEST_HASH_SECRET'),
+        'netbanking_sib_test_hash_secret' => env('NETBANKING_SIB_TEST_HASH_SECRET'),
         ],
-
-    'netbanking_sib' => [
-        'test_merchant_id'  => env('NETBANKING_SIB_TEST_MERCHANT_ID'),
-        'test_hash_secret'  => env('NETBANKING_SIB_TEST_HASH_SECRET'),
-    ],
 
     'sharp' => [
     ],
