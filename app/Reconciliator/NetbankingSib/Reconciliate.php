29ea89913665d617a7c4d23a516b9f17328fbbbc
Abhinav Venkatesh <abhinav.venkatesh@razorpay.com>
2019-05-12 21:20:16 +0530

    Add reconciliate for SIB

---
+++ app/Reconciliator/NetbankingSib/Reconciliate.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace RZP\Reconciliator\NetbankingSib;
+
+use RZP\Reconciliator\Base;
+
+class Reconciliate extends Base\Reconciliate
+{
+    protected $columnHeaders = [
+        ReconFields::TRANSACTION_DATE,
+        ReconFields::PAYMENT_ID,
+        ReconFields::PAYMENT_AMOUNT,
+        ReconFields::BANK_REFERENCE_NUMBER
+    ];
+
+    public function getColumnHeadersForType($type)
+    {
+        return $this->columnHeaders;
+    }
+
+    public function getDelimiter()
+    {
+        return '|';
+    }
+
+    protected function getTypeName($fileName)
+    {
+        return self::PAYMENT;
+    }
+}
