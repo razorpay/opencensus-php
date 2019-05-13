04cb7010d119c64725365bde0d4549edfc5e9920
dvvavinash <dvv.avinash@gmail.com>
2019-05-13 10:24:07 +0530

    fixing is_live propertry call issue

--- web/js/merchant/containers/App.js
+++ web/js/merchant/containers/App.js
@@ -304,13 +304,6 @@ export default class App extends Component {
     } else {
       LocalStorageService.setItem(this.modeToken, mode);
       location.reload();
-
-      window.trackHubs({
-        name: 'update_property',
-        data: {
-          is_live: true,
-        },
-      });
     }
   };
 
