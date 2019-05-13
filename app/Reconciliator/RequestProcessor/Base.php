24d6492e9235633a13b80255e42031f266baeee6
Abhinav Venkatesh <abhinav.venkatesh@razorpay.com>
2019-05-13 12:49:39 +0530

    Define SIB nb in recon request processor

--- app/Reconciliator/RequestProcessor/Base.php
+++ app/Reconciliator/RequestProcessor/Base.php
@@ -62,6 +62,7 @@ class Base extends Core
     const NETBANKING_CANARA      = 'NetbankingCanara';
     const NETBANKING_FEDERAL     = 'NetbankingFederal';
     const NETBANKING_CORPORATION = 'NetbankingCorporation';
+    const NETBANKING_SIB         = 'NetbankingSib';
     const NETBANKING_RBL         = 'NetbankingRbl';
     const NETBANKING_CSB         = 'NetbankingCsb';
     const NETBANKING_IDFC        = 'NetbankingIdfc';
@@ -113,6 +114,7 @@ class Base extends Core
         self::NETBANKING_AXIS        => ['ibanking@axisbank.com'],
         self::NETBANKING_ICICI       => ['ubpshelp@icicibank.com'],
         self::NETBANKING_FEDERAL     => ['fednetrm@federalbank.co.in'],
+        self::NETBANKING_SIB         => [],
         self::NETBANKING_RBL         => ['internetbanking@rblbank.com'],
         self::NETBANKING_EQUITAS     => [],
         self::NETBANKING_CANARA      => [],
@@ -169,6 +171,7 @@ class Base extends Core
         Gateway::NETBANKING_AXIS        => self::NETBANKING_AXIS,
         Gateway::NETBANKING_IDFC        => self::NETBANKING_IDFC,
         Gateway::NETBANKING_FEDERAL     => self::NETBANKING_FEDERAL,
+        Gateway::NETBANKING_SIB         => self::NETBANKING_SIB,
         Gateway::NETBANKING_EQUITAS     => self::NETBANKING_EQUITAS,
         Gateway::NETBANKING_BOB         => self::NETBANKING_BOB,
         Gateway::NETBANKING_VIJAYA      => self::NETBANKING_VIJAYA,
