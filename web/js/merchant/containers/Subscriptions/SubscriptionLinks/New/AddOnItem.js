235846e9710460a5f6ad9ad4562403ff7f2ee4a1
dvvavinash <dvv.avinash@gmail.com>
2019-05-22 19:32:24 +0530

    Fix for international items

--- web/js/merchant/containers/Subscriptions/SubscriptionLinks/New/AddOnItem.js
+++ web/js/merchant/containers/Subscriptions/SubscriptionLinks/New/AddOnItem.js
@@ -25,6 +25,7 @@ export default class AddOnItem extends Component {
           closeModal={closeModal}
           onSave={closeModal}
           currency={currency}
+          type="addon"
         />
       ),
     });
