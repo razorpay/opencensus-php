90cccd1edea27385e6fc4630f4d7d2a79ed410a6
dvvavinash <dvv.avinash@gmail.com>
2019-05-22 20:36:13 +0530

    Minor fix for subscription international

--- web/js/merchant/containers/Subscriptions/SubscriptionLinks/New/Review.js
+++ web/js/merchant/containers/Subscriptions/SubscriptionLinks/New/Review.js
@@ -20,7 +20,10 @@ export default function NewSubscriptionLinkReview({
       0
     );
   const subscriptioAmount = planAmount * planQuantity;
-  const minAuthAmount = (props.getCurrencyList[currency] || {}).min_auth_value;
+  const minAuthAmount =
+    currency === 'INR'
+      ? 500
+      : (props.getCurrencyList[currency] || {}).min_auth_value;
   const authorizationAmount = getAuthorizationAmount(
     subscriptioAmount,
     addOnAmount,
