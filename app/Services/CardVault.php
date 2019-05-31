80f274a8cbf50ab9b5f468a05b1aa1b29627c1b6
Vikas Naidu <vck173@gmail.com>
2019-05-31 14:47:17 +0530

    [Vault] cache vault response to avoid multiple requests (#12006)

--- app/Services/CardVault.php
+++ app/Services/CardVault.php
@@ -32,6 +32,8 @@ class CardVault
 
     protected $request;
 
+    protected $cardNumberToToken = [];
+
     public function __construct($app)
     {
         $this->trace = $app['trace'];
@@ -61,6 +63,11 @@ class CardVault
             $payload[self::SCHEME] = $input[self::SCHEME];
         }
 
+        if (empty($this->cardNumberToToken[$payload[self::SECRET]]) === false)
+        {
+            return $this->cardNumberToToken[$payload[self::SECRET]];
+        }
+
         $response = $this->sendRequest('tokenize', 'post', $payload);
 
         if (empty($response[self::TOKEN]) === true)
@@ -68,6 +75,8 @@ class CardVault
             throw new Exception\RuntimeException(
                 'card vault request failed', ['data' => $response]);
         }
+
+        $this->cardNumberToToken[$payload[self::SECRET]] = $response[self::TOKEN];
 
         return $response[self::TOKEN];
     }
