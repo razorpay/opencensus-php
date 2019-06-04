8df53c49f98f99b7766142346fc808ed325064e8
Pratik Kapasi <pratik.kapasi@razorpay.com>
2019-06-04 17:38:52 +0530

    [emails] updates email content

--- resources/views/emails/merchant/instant_activation.blade.php
+++ resources/views/emails/merchant/instant_activation.blade.php
@@ -11,32 +11,49 @@
 <div>
     <p>Congratulations! Your Razorpay account has been activated.</p>
 
-    <p>You can now <b>start accepting online payments using Razorpay</b> using a variety of payment methods in any of
-        the following ways</p>
+    <p>You can now <b>start accepting online payments using Razorpay</b> using a variety of payment methods in any of the following ways</p>
 
 
     <dl>
         <dt style="padding: 0.5%;"><b>Accept Payments on your website</b></dt>
 
-        <dd>Integrate Razorpay onto your website. You can use the API keys from your dashboard. Want to know how to integrate?</dd>
-        <dd><a href="https://razorpay.com/docs/" target="_blank">Guide to go live</a></dd>
+        <dd>Refer to our <a href="https://razorpay.com/docs/payment-gateway/integrations-guide/" target="_blank">Integration Guide</a> to integrate Razorpay onto your website.</dd>
     </dl>
 
 
     <dl>
         <dt style="padding: 0.5%"><b>Accept Payments without integration</b></dt>
 
-        <dd>In case you do not have a website or an app, you can start receiving payments right away using {{{$merchant['org']['business_name']}}} products</dd>
-        <dd><a href="https://{{{$merchant['org']['hostname']}}}/#/app/dashboard?products" target="_blank">View products</a></dd>
+        <dd>In case you do not have a website or an app and do not want to integrate, you can use one of our other products to start receiving payments right away.
+            Refer to our <a href="https://razorpay.com/docs/" target="_blank">Product Document</a> for more information.</dd>
 
     </dl>
 
     <dd>Please note:<i> You will only be able to receive settlements for payments collected using the
-            Razorpay payment gateway to your linked bank account after completing the KYC verification successfully.</i>
+            Razorpay payment gateway to your linked bank account after your KYC is successfully verified.</i>
     </dd>
 
-    <dd>For any further queries or clarifications, feel free to reach out to us by visiting - </dd>
-    <dd><a href="https://razorpay.com/support" target="_blank">https://razorpay.com/support</a></dd>
+
+    <dl>
+        <dt style="padding: 0.5%;"><b>Recommended Reading for you to get started and get to know us better</b></dt>
+        <dd>
+            <ul>
+                <li>
+                    <a href="https://razorpay.com/docs/payment-gateway/getting-started-guide/">Payment Flow</a>
+                </li>
+                <li>
+                    <a href="https://razorpay.com/docs/payment-gateway/orders/">Auto Capture</a>
+                </li>
+                <li>
+                    <a href="https://razorpay.com/docs/payment-gateway/dashboard-guide/">Dashboard Guide</a>
+                </li>
+            </ul>
+        </dd>
+    </dl>
+
+
+    <dd>For any further queries or clarifications, feel free to reach out to our </dd>
+    <dd><a href="https://razorpay.com/support" target="_blank">Support Team</a></dd>
 
 </div>
 
