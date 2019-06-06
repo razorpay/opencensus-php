ffae414daf5e6f72ee426be6a5a405e4868c9ecf
ratanphyade <ratanphayade@gmail.com>
2019-06-07 00:18:10 +0530

    fts: transfer request ot send preferred mode

--- app/Services/FTS/Constants.php
+++ app/Services/FTS/Constants.php
@@ -60,6 +60,8 @@ final class Constants
 
     const ACCOUNT_TYPE                  = 'account_type';
 
+    const PREFERRED_MODE                = 'preferred_mode';
+
     const STATUS_CREATED                = 'created';
 
     const ACCOUNT_NUMBER                = 'account_number';
