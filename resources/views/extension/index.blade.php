023a65cd3b3b2479ed1cf89ef275993aa6636215
aseemgupta <aseemgupta2625@gmail.com>
2019-06-06 15:46:11 +0530

    [Browser Ext]: Refactor hotjar script inclusion as partial

--- resources/views/extension/index.blade.php
+++ resources/views/extension/index.blade.php
@@ -36,84 +36,8 @@
   <script src="https://cdn.razorpay.com/static/analytics/bundle.js" defer onload="initAnalytics()"></script>
   <script src="http://127.0.0.1:7999/static/extension/app.js" async defer onload="renderApp()"></script>
 
-  <script type="text/javascript">
-    var origOpen = XMLHttpRequest.prototype.open;
-    var origSend = XMLHttpRequest.prototype.send;
-    XMLHttpRequest.prototype.open = function (a, b, c, d, e) {
-        if (b.indexOf('hotjar') >= 0) {
-            this.send = function (s) {
-                try {
-                    var x = JSON.parse(s);
-
-                    // Add MID, email, and contact to the message.
-                    if (x['response'] && typeof x['response']['message'] !== 'undefined') {
-                        if (x['response']['message'] === null) {
-                            x['response']['message'] = '';
-                        }
-
-                        if (typeof x['response']['message'] === 'string') {
-                            x['response']['message'] += '\n\nMID: ' + rzp_user.id;
-                            x['response']['message'] += '\nEmail: ' + ((rzp_user.user && rzp_user.user.email) || rzp_user.email);
-                            x['response']['message'] += '\nContact: ' + ((rzp_user.user && rzp_user.user.contact_mobile) || rzp_user.contact_mobile);
-                            x['response']['message'] += '\nName: ' + rzp_user.business_name;
-                        }
-                    }
-
-                    try {
-                        s = JSON.stringify(x);
-                    } catch (stringifyErr) {}
-
-                    if (x['action'] && (x['action'] === 'create_poll_response' || x['action'] === 'update_poll_response')) {
-                        if (x['response_content']) {
-                            if (typeof x['response_content'] === 'string') {
-                                try {
-                                    var rc = JSON.parse(x['response_content']);
-                                    if (rc['answers'] && rc['answers'].length) {
-                                        var d = {
-                                            mid: 'test', // TODO: Get window.rzp_user.current using JWT Token
-                                            feedback: null,
-                                            rating: null
-                                        };
-                                        var a = rc['answers'][0];
-                                        if (a['question'] === 'How would you rate the new dashboard home page?') {
-                                            d.rating = parseInt(a.answer);
-                                        }
-                                        if (rc['answers'].length > 1) {
-                                            a = rc['answers'][1];
-                                            if (a['question'] === 'Please suggest how we can make it better.') {
-                                            d.feedback = a.answer;
-                                            }
-                                        }
-                                        var xhr = new XMLHttpRequest();
-                                        xhr.open('POST', 'https://hooks.zapier.com/hooks/catch/1088429/zk9ygu/', true);
-                                        xhr.send(JSON.stringify(d));
-                                    }
-                                } catch (e) {}
-                            }
-                        }
-                    }
-                } catch (e) {}
-                origSend.apply(this, [s]);
-            }
-        }
-        origOpen.apply(this, [a, b, c, d, e]);
-    }
-  </script>
-
-
+  @include('partials/xhr_overwrite')
 
   <!-- Hotjar Tracking Code for dashboard.razorpay.com -->
-  <script>
-    if (true || location.hostname === 'dashboard.razorpay.com') {
-      (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:575141,hjsv:5};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.onload=window.hjLoaded&&window.hjLoaded();
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-      })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
-    }
-  </script>
+  @include('partials/hotjar')
 </body>
--- resources/views/merchant/index.blade.php
+++ resources/views/merchant/index.blade.php
@@ -38,18 +38,7 @@
 
 <!-- Hotjar Tracking Code for dashboard.razorpay.com -->
 @if(env('APP_ENV') === 'production')
-  <script>
-    if (location.hostname === 'dashboard.razorpay.com') {
-      (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:575141,hjsv:5};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-      })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
-    }
-  </script>
+  @include('partials/hotjar')
 @endif
 
 @if ($isConfirmed and $isPreSignupComplete)
