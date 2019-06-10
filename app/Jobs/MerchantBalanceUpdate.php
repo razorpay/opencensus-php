104bda21eddcc1704720f7e39fbd847298dd0d3f
Vikas Naidu <vck173@gmail.com>
2019-06-10 15:27:51 +0530

    [SQS] added merchant balance update jon

---
+++ app/Jobs/MerchantBalanceUpdate.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace RZP\Jobs;
+
+use RZP\Trace\TraceCode;
+use Razorpay\Trace\Logger as Trace;
+use RZP\Models\Payment\Service as PaymentService;
+
+class MerchantBalanceUpdate extends Job
+{
+    const RELEASE_WAIT_SECS    = 300;
+
+    /**
+     * @var string
+     */
+    protected $queueConfigKey = 'merchant_balance_update';
+
+    /**
+     * @var array
+     */
+    protected $input;
+
+
+    public function __construct(array $input)
+    {
+        parent::__construct($input['mode']);
+
+        $this->input = $input;
+    }
+
+    /**
+     * Process queue request
+     */
+    public function handle()
+    {
+        try
+        {
+            parent::handle();
+           $this->trace->info(
+                TraceCode::MERCHANT_BALANCE_UPDATE_REQUEST,
+                [
+                    'input'       => $this->input,
+                ]
+            );
+
+            $this->trace->info(
+                TraceCode::MERCHANT_BALANCE_UPDATE_SUCCESSFULL,[
+                    'input'          => $this->input
+                ]);
+
+            $this->delete();
+        }
+        catch (\Throwable $e)
+        {
+            $this->trace->traceException($e,
+                Trace::ERROR,
+                TraceCode::MERCHANT_BALANCE_UPDATE_FAILURE,
+                $this->input);
+
+            $this->release(self::RELEASE_WAIT_SECS);
+        }
+    }
+}
--- app/Trace/TraceCode.php
+++ app/Trace/TraceCode.php
@@ -1863,11 +1863,17 @@ class TraceCode extends \Razorpay\Trace\TraceCode
     const CAPTURE_VERIFY_ONHOLD_FINISH_ACTION                   = 'CAPTURE_VERIFY_ONHOLD_FINISH_ACTION';
     const CAPTURE_VERIFY_ONHOLD_ACTION                          = 'CAPTURE_VERIFY_ONHOLD_ACTION';
 
+
     // Mozart Service
     const MOZART_SERVICE_UNEXPECTED_RESPONSE                    = 'MOZART_SERVICE_UNEXPECTED_RESPONSE';
     const MOZART_SERVICE_REQUEST_FAILED                         = 'MOZART_SERVICE_REQUEST_FAILED';
     const MOZART_SERVICE_RESPONSE                               = 'MOZART_SERVICE_RESPONSE';
     const MOZART_SERVICE_REQUEST                                = 'MOZART_SERVICE_REQUEST';
+
+    const MERCHANT_BALANCE_UPDATE_INIT                          = 'MERCHANT_BALANCE_UPDATE_INIT';
+    const MERCHANT_BALANCE_UPDATE_REQUEST                       = 'MERCHANT_BALANCE_UPDATE_REQUEST';
+    const MERCHANT_BALANCE_UPDATE_SUCCESSFULL                   = 'MERCHANT_BALANCE_UPDATE_SUCCESSFULL';
+    const MERCHANT_BALANCE_UPDATE_FAILURE                       = 'MERCHANT_BALANCE_UPDATE_FAILURE';
 
     protected static $messages = [
         self::PAYMENT_NEW_REQUEST                                   => 'Request for new payment received',
