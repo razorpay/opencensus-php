2a5f5054e655787c232aab7bce18a9cb8ee8a223
Akash Sharma <akashsharma@live.in>
2019-06-10 12:42:17 +0530

    For cardless take mcc code as input instead of net category (#12091)

--- tests/Functional/Batch/FlexMoneyBulkTerminalTestData.php
+++ tests/Functional/Batch/FlexMoneyBulkTerminalTestData.php
@@ -43,14 +43,14 @@ return [
                         Header::FLEXMONEY_GATEWAY_MERCHANT_ID  => '123',
                         Header::FLEXMONEY_GATEWAY_MERCHANT_ID2 => '123',
                         Header::FLEXMONEY_TERMINAL_PASSWORD    => 'password',
-                        Header::FLEXMONEY_CATEGORY             => 'ecommerce'
+                        Header::FLEXMONEY_CATEGORY             => '1234'
                     ],
                     [
                         Header::FLEXMONEY_MERCHANT_ID          => '100000Razorpay',
                         Header::FLEXMONEY_GATEWAY_MERCHANT_ID  => '321',
                         Header::FLEXMONEY_GATEWAY_MERCHANT_ID2 => '321',
                         Header::FLEXMONEY_TERMINAL_PASSWORD    => 'password',
-                        Header::FLEXMONEY_CATEGORY             => 'ecommerce'
+                        Header::FLEXMONEY_CATEGORY             => '1234'
                     ]
                 ],
             ],
