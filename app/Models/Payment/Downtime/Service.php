7d347834ee4f07f96ca20cb011df4500c4d42a01
Akash Sharma <akashsharma@live.in>
2019-06-11 16:48:11 +0530

    Fix import for downtime trigger (#12113)

--- app/Models/Payment/Downtime/Service.php
+++ app/Models/Payment/Downtime/Service.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 
 use RZP\Models\Base;
 use RZP\Trace\TraceCode;
+use RZP\Listeners\ApiEventSubscriber;
 use RZP\Constants\Entity as EntityConstants;
 use RZP\Models\Merchant\Webhook\Event;
 
