4797d96bb858e1854d6c6ac422730f68fe5e778f
Shashank Agarwal <shashank.agarwal@razorpay.com>
2019-06-11 18:18:02 +0530

    Added test case

--- tests/Functional/Payment/PaymentCreateTest.php
+++ tests/Functional/Payment/PaymentCreateTest.php
@@ -1129,4 +1129,28 @@ class PaymentCreateTest extends TestCase
 
         $this->runRequestResponseFlow($this->testData[__FUNCTION__]);
     }
+
+    public function testForRuPayPaymentOnHitachiTerminalModePurchase()
+    {
+        $this->sharedTerminal = $this->fixtures->create('terminal:shared_hitachi_terminal');
+        $this->fixtures->merchant->enableMethod('10000000000000', 'card');
+        $this->fixtures->iin->create([
+            'iin' => '555555',
+            'country' => 'IN',
+            'network' => 'RuPay',
+        ]);
+        $this->fixtures->terminal->edit(
+            \RZP\Models\Terminal\Shared::HITACHI_TERMINAL,
+            [
+                'mode' => 2,
+            ]
+        );
+        $payment = $this->getDefaultPaymentArray();
+        $payment['card']['number'] = '555555555555558';
+        $payment['amount'] = 1000000;
+        $content = $this->doAuthPayment($payment);
+        $this->assertArrayHasKey('razorpay_payment_id', $content);
+        $paymentObj = $this->getLastEntity('payment', true);
+        $this->assertNull($paymentObj['gateway_captured'] );
+    }
 }
