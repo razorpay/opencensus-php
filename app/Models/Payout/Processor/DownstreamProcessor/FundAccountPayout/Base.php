b7902b12f3c1b681e661d7700b7bad293a1c5c1b
Prashanth Y.V <prashanth.yv@razorpay.com>
2019-06-12 15:20:21 +0530

    [payouts] adds a note for queued payouts channel

--- app/Models/Payout/Processor/DownstreamProcessor/FundAccountPayout/Base.php
+++ app/Models/Payout/Processor/DownstreamProcessor/FundAccountPayout/Base.php
@@ -9,6 +9,16 @@ class Base extends DSBase
 {
     protected function setChannel(Entity $payout)
     {
+        //
+        // NOTE (for queued only): When the payout is being queued,
+        // the payout might have channel A set. Once we start processing,
+        // it's possible that we are selecting channel B (based on what
+        // DR module returns at that point of time).
+        // Hence, irrespective of the channel set at the time of queued,
+        // the channel that will be actually used is of when the queued
+        // payout is being processed.
+        //
+
         $channel = snake_case(class_basename(get_called_class()));
 
         $payout->setChannel($channel);
