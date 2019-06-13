bc9fd2553874260bf48a3416aff9eef5b8f81bb7
shivanshgaur <shivanshgaur19@gmail.com>
2019-06-13 16:56:07 +0530

    add email templates

---
+++ resources/views/emails/banking_account/notify_status_cancelled.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0" style="line-height: 1.5;width: 100% !important;margin: 0;padding: 0; font-size: 15px;">
+<center>
+    <table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" style="max-width: 600px">
+        <tbody>
+        <tr>
+            <td colspan="3">
+                <h2>Dear {{$contact_name}},</h2>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="3">
+                <p>Your request for RazorpayX Current Account is cancelled.</p>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="3">
+                <p>This does not impact your existing RazorpayX setup. You can continue to use RazorpayX for your business.</p>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="3">
+                <p>We are in the process of adding more banks on RazorpayX. We look forward to having you on RazorpayX current account set up soon.</p>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="3">
+                <p>For any assistance, contact RazorpayX support by replying back to this email and our Onboarding Hero will be happy to help you.</p>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="3">
+                <p>
+                    Team RazorpayX
+                    <br/>
+                    Xperience the future of banking
+                </p>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+</center>
+</body>
+</html>
---
+++ resources/views/emails/banking_account/notify_status_processed.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0" style="line-height: 1.5;width: 100% !important;margin: 0;padding: 0; font-size: 15px;">
+<center>
+    <table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" style="max-width: 600px">
+        <tbody>
+        <tr>
+            <td colspan="3">
+                <h2>Dear {{$contact_name}},</h2>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="3">
+                <p>Hurray! Your RazorpayX Current Account has been processed.</p>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="3">
+                <p>You are just one step away from activating your current account on RazorpayX.</p>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="3">
+                <p><b>Important:</b> To activate the current account on RazorpayX, please submit account credentials on RazorpayX dashboard. Refer the steps in the attached document to obtain account credentials from the bank.</p>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="3">
+                <p>For any assistance, contact RazorpayX support by replying back to this email and our Onboarding Hero will be happy to help you.</p>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="3">
+                <p>
+                    Team RazorpayX
+                    <br/>
+                    Xperience the future of banking
+                </p>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+</center>
+</body>
+</html>
