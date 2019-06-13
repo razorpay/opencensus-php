5af9cd772d59bf5392b704a77a2c48cfb838aa44
Rohit Keshwani <rohitkeshwani07@gmail.com>
2019-06-13 14:34:30 +0530

    Added Metrics for fund account validation

--- app/Models/FundAccount/Validation/Metric.php
+++ app/Models/FundAccount/Validation/Metric.php
@@ -4,6 +4,8 @@ namespace RZP\Models\FundAccount\Validation;
 
 use App;
 
+use RZP\Error\Error;
+use RZP\Exception;
 use RZP\Models\Base;
 
 class Metric extends Base\Core
@@ -14,11 +16,50 @@ class Metric extends Base\Core
     const FUND_ACCOUNT_VALIDATION_CREATED           = 'fund_account_validation_created';
     const FUND_ACCOUNT_VALIDATION_FAILED            = 'fund_account_validation_failed';
 
+    const LABEL_TRACE_CODE                      = 'code';
+    const LABEL_TRACE_FIELD                     = 'field';
+    const LABEL_TRACE_SOURCE                    = 'source';
+    const LABEL_TRACE_EXCEPTION_CLASS           = 'exception_class';
+
     public function pushCreatedMetrics()
     {
+        $this->trace->count(self::FUND_ACCOUNT_VALIDATION_CREATED);
+    }
+
+    protected function getDefaultExceptionDimensions(\Throwable $e): array
+    {
+        $errorAttributes = [];
+
+        if ($e instanceof Exception\BaseException)
+        {
+            if (($e->getError() !== null) and ($e->getError() instanceof Error))
+            {
+                $errorAttributes = $e->getError()->getAttributes();
+            }
+        }
+        else
+        {
+            $errorAttributes = [
+                Metric::LABEL_TRACE_CODE         => $e->getCode(),
+            ];
+        }
+
+        $dimensions = [
+            Metric::LABEL_TRACE_CODE                => array_get($errorAttributes, Error::INTERNAL_ERROR_CODE),
+            Metric::LABEL_TRACE_FIELD               => array_get($errorAttributes, Error::FIELD),
+            Metric::LABEL_TRACE_SOURCE              => array_get($errorAttributes, Error::ERROR_CLASS),
+            Metric::LABEL_TRACE_EXCEPTION_CLASS     => get_class($e),
+        ];
+
+        return $dimensions;
     }
 
     public function pushExceptionMetrics(\Throwable $e, string $metricName, array $extraDimensions = [])
     {
+        $dimensions = $this->getDefaultExceptionDimensions($e);
+
+        $dimensions = array_merge($dimensions, $extraDimensions);
+
+        $this->trace->count($metricName, $dimensions);
     }
 }
