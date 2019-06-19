756a14981513b51825127a4afdeaeb0c7afbf212
shivanshgaur <shivanshgaur19@gmail.com>
2019-06-19 14:24:39 +0530

    change route

--- app/Http/Route.php
+++ app/Http/Route.php
@@ -1105,7 +1105,7 @@ final class Route
         'governor_rule_chain_execute'             => ['post',     '{source}/rule_engine/execute/rule_chain/{namespace}',       'GovernorController@executeChains'                          ],
 
         'banking_account_create'                  => ['post',     'banking_accounts',                                          'BankingAccountController@create'                           ],
-        'banking_serviceable_pincodes'            => ['post',     'banking/serviceability/{channel}/pincodes',                 'BankingAccountController@postServiceablePincodes'          ],
+        'banking_serviceable_pincodes'            => ['post',     'banking_account/serviceability/{channel}/pincodes',         'BankingAccountController@postServiceablePincodes'          ],
         'banking_accounts_list'                   => ['get',      'banking_accounts',                                          'BankingAccountController@list'                             ],
         'banking_account_update'                  => ['patch',    'banking_account/{id}',                                      'BankingAccountController@update'                           ],
 
