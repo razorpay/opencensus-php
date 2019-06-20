923e10f8fab6305fcb3696f5e7a17d2a302a7f06
shivanshgaur <shivanshgaur19@gmail.com>
2019-06-20 13:50:16 +0530

    add tests

--- tests/Functional/BankingAccount/Bank/Rbl/BankingAccountTest.php
+++ tests/Functional/BankingAccount/Bank/Rbl/BankingAccountTest.php
@@ -1,12 +1,15 @@
 <?php
 
+use RZP\Models\BankingAccount;
 use RZP\Tests\Functional\TestCase;
 use RZP\Models\BankingAccount\Entity;
+use RZP\Mail\BankingAccount\NotifyStatusUpdate;
 use RZP\Tests\Functional\RequestResponseFlowTrait;
 
 class BankingAccountTest extends TestCase
 {
     use RequestResponseFlowTrait;
+    use RZP\Tests\Functional\Helpers\DbEntityFetchTrait;
 
     public function setUp()
     {
@@ -24,17 +27,51 @@ class BankingAccountTest extends TestCase
 
     public function testCreateBankingAccount()
     {
+        Mail::fake();
+
         $this->startTest();
+
+        Mail::assertQueued(NotifyStatusUpdate::class, function ($mail)
+        {
+            $this->assertArrayHasKey('contact_name', $mail->viewData);
+
+            $this->assertArrayHasKey('contact_email', $mail->viewData);
+
+            $this->assertArrayHasKey('status', $mail->viewData);
+
+            $this->assertEquals('emails.banking_account.notify_status_created', $mail->view);
+
+            return $mail->hasTo('test@razorpay.com');
+        });
     }
 
     public function testCreateBankingAccountWithUnserviceablePincode()
     {
+        Mail::fake();
+
         $this->startTest();
+
+        Mail::assertQueued(NotifyStatusUpdate::class, function ($mail)
+        {
+            $this->assertArrayHasKey('contact_name', $mail->viewData);
+
+            $this->assertArrayHasKey('contact_email', $mail->viewData);
+
+            $this->assertArrayHasKey('status', $mail->viewData);
+
+            $this->assertEquals('emails.banking_account.notify_status_created', $mail->view);
+
+            return $mail->hasTo('test@razorpay.com');
+        });
     }
 
     public function testCreateBankingAccountWithInvalidBank()
     {
         $data = $this->testData[__FUNCTION__];
+
+        Mail::fake();
+
+        Mail::assertNotQueued(NotifyStatusUpdate::class);
 
         $this->runRequestResponseFlow($data, function()
         {
@@ -70,5 +107,222 @@ class BankingAccountTest extends TestCase
         $response = $this->makeRequestAndGetContent($request);
 
         return $response;
+    }
+
+    public function testNotificationOnBankingAccountProcessedFromProcessing()
+    {
+        $merchant = $this->activatedMerchant();
+
+        $this->ba->proxyAuth('rzp_test_' . $merchant['id']);
+
+        $bankingAccount = $this->createBankingAccount();
+
+        $ba = $this->getDbLastEntity('banking_account');
+
+        $ba->setStatus(BankingAccount\Status::PROCESSING);
+
+        $ba->save();
+
+        $dataToReplace = [
+            'request'  => [
+                'url'     => '/banking_account/' . $bankingAccount['id'],
+                'method'  => 'PATCH',
+            ],
+            'response' => [
+                'content' => [
+                    'merchant_id' => $merchant['id'],
+                ],
+            ],
+        ];
+
+        Mail::fake();
+
+        $this->ba->adminAuth();
+
+        $this->startTest($dataToReplace);
+
+        Mail::assertQueued(NotifyStatusUpdate::class, function ($mail)
+        {
+            $this->assertArrayHasKey('contact_name', $mail->viewData);
+
+            $this->assertArrayHasKey('contact_email', $mail->viewData);
+
+            $this->assertArrayHasKey('status', $mail->viewData);
+
+            $this->assertEquals('emails.banking_account.notify_status_processed', $mail->view);
+
+            return true;
+        });
+    }
+
+    public function testNotificationOnBankingAccountCancelledFromProcessing()
+    {
+        $merchant = $this->activatedMerchant();
+
+        $this->ba->proxyAuth('rzp_test_' . $merchant['id']);
+
+        $bankingAccount = $this->createBankingAccount();
+
+        $ba = $this->getDbLastEntity('banking_account');
+
+        $ba->setStatus(BankingAccount\Status::PROCESSING);
+
+        $ba->save();
+
+        $dataToReplace = [
+            'request'  => [
+                'url'     => '/banking_account/' . $bankingAccount['id'],
+                'method'  => 'PATCH',
+            ],
+            'response' => [
+                'content' => [
+                    'merchant_id' => $merchant['id'],
+                ],
+            ],
+        ];
+
+        Mail::fake();
+
+        $this->ba->adminAuth();
+
+        $this->startTest($dataToReplace);
+
+        Mail::assertQueued(NotifyStatusUpdate::class, function ($mail)
+        {
+            $this->assertArrayHasKey('contact_name', $mail->viewData);
+
+            $this->assertArrayHasKey('contact_email', $mail->viewData);
+
+            $this->assertArrayHasKey('status', $mail->viewData);
+
+            $this->assertEquals('emails.banking_account.notify_status_cancelled', $mail->view);
+
+            return true;
+        });
+    }
+
+    public function testNotificationOnBankingAccountCancelledFromInitiated()
+    {
+        $merchant = $this->activatedMerchant();
+
+        $this->ba->proxyAuth('rzp_test_' . $merchant['id']);
+
+        $bankingAccount = $this->createBankingAccount();
+
+        $ba = $this->getDbLastEntity('banking_account');
+
+        $ba->setStatus(BankingAccount\Status::INITIATED);
+
+        $ba->save();
+
+        $dataToReplace = [
+            'request'  => [
+                'url'     => '/banking_account/' . $bankingAccount['id'],
+                'method'  => 'PATCH',
+            ],
+            'response' => [
+                'content' => [
+                    'merchant_id' => $merchant['id'],
+                ],
+            ],
+        ];
+
+        Mail::fake();
+
+        $this->ba->adminAuth();
+
+        $this->startTest($dataToReplace);
+
+        Mail::assertNotQueued(NotifyStatusUpdate::class);
+    }
+
+    public function testNotificationOnBankingAccountProcessingFromInitiated()
+    {
+        $merchant = $this->activatedMerchant();
+
+        $this->ba->proxyAuth('rzp_test_' . $merchant['id']);
+
+        $bankingAccount = $this->createBankingAccount();
+
+        $ba = $this->getDbLastEntity('banking_account');
+
+        $ba->setStatus(BankingAccount\Status::INITIATED);
+
+        $ba->save();
+
+        $dataToReplace = [
+            'request'  => [
+                'url'     => '/banking_account/' . $bankingAccount['id'],
+                'method'  => 'PATCH',
+            ],
+            'response' => [
+                'content' => [
+                    'merchant_id' => $merchant['id'],
+                ],
+            ],
+        ];
+
+        Mail::fake();
+
+        $this->ba->adminAuth();
+
+        $this->startTest($dataToReplace);
+
+        Mail::assertQueued(NotifyStatusUpdate::class, function ($mail)
+        {
+            $this->assertArrayHasKey('contact_name', $mail->viewData);
+
+            $this->assertArrayHasKey('contact_email', $mail->viewData);
+
+            $this->assertArrayHasKey('status', $mail->viewData);
+
+            $this->assertEquals('emails.banking_account.notify_status_processing', $mail->view);
+
+            return true;
+        });
+    }
+
+    public function testNotificationOnBankingAccountCreatedFromCreated()
+    {
+        $merchant = $this->activatedMerchant();
+
+        $this->ba->proxyAuth('rzp_test_' . $merchant['id']);
+
+        $bankingAccount = $this->createBankingAccount();
+
+        $ba = $this->getDbLastEntity('banking_account');
+
+        $ba->setStatus(BankingAccount\Status::INITIATED);
+
+        $ba->save();
+
+        $dataToReplace = [
+            'request'  => [
+                'url'     => '/banking_account/' . $bankingAccount['id'],
+                'method'  => 'PATCH',
+            ],
+            'response' => [
+                'content' => [
+                    'merchant_id' => $merchant['id'],
+                ],
+            ],
+        ];
+
+        Mail::fake();
+
+        $this->ba->adminAuth();
+
+        $this->startTest($dataToReplace);
+
+        Mail::assertNotQueued(NotifyStatusUpdate::class);
+    }
+
+    protected function activatedMerchant()
+    {
+        $attribute = ['activation_status' => 'activated'];
+
+        $merchantDetail = $this->fixtures->create('merchant_detail', $attribute);
+
+        return $merchantDetail->merchant;
     }
 }
--- tests/Functional/BankingAccount/Bank/Rbl/Helpers/BankingAccountTestData.php
+++ tests/Functional/BankingAccount/Bank/Rbl/Helpers/BankingAccountTestData.php
@@ -1,6 +1,7 @@
 <?php
 
 use RZP\Error\ErrorCode;
+use RZP\Models\BankingAccount;
 use RZP\Error\PublicErrorCode;
 
 return [
@@ -69,6 +70,96 @@ return [
         'exception' => [
             'class'               => 'RZP\Exception\BadRequestValidationFailureException',
             'internal_error_code' => ErrorCode::BAD_REQUEST_VALIDATION_FAILURE,
+        ],
+    ],
+
+    'testNotificationOnBankingAccountProcessedFromProcessing' => [
+        'request'  => [
+            'url'     => '/banking_account',
+            'method'  => 'PATCH',
+            'content' => [
+                BankingAccount\Entity::STATUS => BankingAccount\Status::PROCESSED,
+                BankingAccount\Entity::BANK_INTERNAL_STATUS => BankingAccount\RblStatus::CLOSED
+            ],
+        ],
+        'response' => [
+            'content' => [
+                'merchant_id' => '10000000000000',
+                'channel'     => 'rbl',
+                BankingAccount\Entity::STATUS => BankingAccount\Status::PROCESSED,
+            ],
+        ],
+    ],
+
+    'testNotificationOnBankingAccountCancelledFromProcessing' => [
+        'request'  => [
+            'url'     => '/banking_account',
+            'method'  => 'PATCH',
+            'content' => [
+                BankingAccount\Entity::STATUS => BankingAccount\Status::CANCELLED,
+                BankingAccount\Entity::BANK_INTERNAL_STATUS => BankingAccount\RblStatus::CANCELLED
+            ],
+        ],
+        'response' => [
+            'content' => [
+                'merchant_id' => '10000000000000',
+                'channel'     => 'rbl',
+                BankingAccount\Entity::STATUS => BankingAccount\Status::CANCELLED,
+            ],
+        ],
+    ],
+
+    'testNotificationOnBankingAccountCancelledFromInitiated' => [
+        'request'  => [
+            'url'     => '/banking_account',
+            'method'  => 'PATCH',
+            'content' => [
+                BankingAccount\Entity::STATUS => BankingAccount\Status::CANCELLED,
+                BankingAccount\Entity::BANK_INTERNAL_STATUS => BankingAccount\RblStatus::CANCELLED
+            ],
+        ],
+        'response' => [
+            'content' => [
+                'merchant_id' => '10000000000000',
+                'channel'     => 'rbl',
+                BankingAccount\Entity::STATUS => BankingAccount\Status::CANCELLED,
+            ],
+        ],
+    ],
+
+    'testNotificationOnBankingAccountProcessingFromInitiated' => [
+        'request'  => [
+            'url'     => '/banking_account',
+            'method'  => 'PATCH',
+            'content' => [
+                BankingAccount\Entity::STATUS => BankingAccount\Status::PROCESSING,
+                BankingAccount\Entity::BANK_INTERNAL_STATUS => BankingAccount\RblStatus::OPEN
+            ],
+        ],
+        'response' => [
+            'content' => [
+                'merchant_id' => '10000000000000',
+                'channel'     => 'rbl',
+                BankingAccount\Entity::STATUS => BankingAccount\Status::PROCESSING,
+            ],
+        ],
+    ],
+
+    'testNotificationOnBankingAccountCreatedFromCreated' => [
+        'request'  => [
+            'url'     => '/banking_account',
+            'method'  => 'PATCH',
+            'content' => [
+                BankingAccount\Entity::STATUS => BankingAccount\Status::INITIATED,
+//                BankingAccount\Entity::BANK_INTERNAL_STATUS => BankingAccount\RblStatus::CLOSED
+            ],
+        ],
+        'response' => [
+            'content' => [
+                'merchant_id' => '10000000000000',
+                'channel'     => 'rbl',
+                BankingAccount\Entity::STATUS => BankingAccount\Status::INITIATED,
+            ],
         ],
     ],
 ];
