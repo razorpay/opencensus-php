c8d6d06380a99f38de6532e2df5f0c1895c26d27
Albin George <albinin0002@gmail.com>
2019-06-21 12:41:57 +0530

    [paysecure] Add message type to createCardEntityFromSavedToken

--- app/Models/Payment/Processor/Authorize.php
+++ app/Models/Payment/Processor/Authorize.php
@@ -4833,13 +4833,16 @@ trait Authorize
 
         $card = $cardCore->createDuplicateCard($savedCard, $this->merchant);
 
+        $iin = $this->app['repo']->iin->find($card['iin']);
+
         $this->payment->card()->associate($card);
 
         return array_merge(
             $card->toArray(),
             [
-                'number' => $cardNumber,
-                'cvv' => $cvv
+                'number'       => $cardNumber,
+                'cvv'          => $cvv,
+                'message_type' => $iin['message_type'],
             ]);
     }
 
