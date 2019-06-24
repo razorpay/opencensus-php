93e88ff20e42ff9f56e2d0bf60c286535cba86e5
digi0ps <sriru1998@gmail.com>
2019-06-24 19:15:15 +0530

    [Browser Ext] Redirect to dashboard onclick.

--- web/entry/extension-entry.js
+++ web/entry/extension-entry.js
@@ -1,6 +1,10 @@
 (function(d, t) {
   var email = window['rzp_user'].email;
 
+  function continueToDashboard() {
+    location.reload();
+  }
+
   function injectPopup() {
     // Container
     var popup = d.createElement('div');
@@ -26,20 +30,23 @@
     text.innerHTML =
       'Click the Razorpay Icon <img src="https://razorpay.com/favicon.png" class="ExtensionPopup-Icon" /> in the top right to load the Chrome Extension';
 
-    var showText = d.createElement('div');
-    showText.setAttribute('class', 'ExtensionPopup-Text--small');
-    showText.innerText = 'or continue to the dashboard.';
+    var continueText = d.createElement('div');
+    continueText.setAttribute('class', 'ExtensionPopup-Text--small');
+    continueText.innerText = 'or continue to the dashboard.';
+    continueText.addEventListener('click', continueToDashboard);
 
     // Put everything together
     popup.appendChild(header);
     popup.appendChild(loggedInText);
     popup.appendChild(text);
-    popup.appendChild(showText);
+    popup.appendChild(continueText);
     t.appendChild(popup);
   }
 
   // Remove splash
   d.getElementById('splash').remove();
-
+  // Remove Key
+  localStorage.removeItem('referrer');
+  // Inject popup
   injectPopup();
 })(document, document.body);
