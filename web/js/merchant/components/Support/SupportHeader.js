1e216660ef358f1e5c20183178825048aa7f3b81
aseemgupta <aseemgupta2625@gmail.com>
2019-07-08 16:33:13 +0530

    [Support]: Show helper tooltip for accessing Dashboard guide

--- web/js/merchant/components/Support/SupportHeader.js
+++ web/js/merchant/components/Support/SupportHeader.js
@@ -1,8 +1,35 @@
 import { Component } from 'react';
+import Popover, { PopoverBody } from 'rzp/ui/Popover';
 
+import LocalStorageService from 'rzp/utils/localStorage';
 import { classList } from 'common/util';
 
 export default class SupportHeader extends Component {
+  constructor(props) {
+    super(props);
+
+    const hasInteractedBefore = LocalStorageService.getItem(
+      'show-support-helper'
+    );
+    this.state = {
+      showHelpTooltip: !hasInteractedBefore,
+    };
+  }
+
+  componentDidUpdate(prevProps) {
+    if (
+      this.state.showHelpTooltip &&
+      prevProps.isOpened !== this.props.isOpened &&
+      this.props.isOpened
+    ) {
+      LocalStorageService.setItem('show-support-helper', 'true');
+
+      this.setState({
+        showHelpTooltip: false,
+      });
+    }
+  }
+
   render() {
     const { notifyCount = 0, isOpened, onToggle } = this.props;
 
@@ -11,13 +38,27 @@ export default class SupportHeader extends Component {
         class={classList('support-launcher', isOpened && 'active')}
         onClick={onToggle}
       >
-        {notifyCount ? <span class="notify-icon">{notifyCount}</span> : null}
-        <div class="open-icon">
-          <i class="i i-headset" />
-        </div>
-        <div class="close-icon">
-          <i class="i i-close " />
-        </div>
+        <span className="help-content">
+          {notifyCount ? <span class="notify-icon">{notifyCount}</span> : null}
+          <div class="open-icon">
+            <i class="i i-headset" />
+          </div>
+          <div class="close-icon">
+            <i class="i i-close " />
+          </div>
+
+          <Popover
+            align="left"
+            theme="dark"
+            persistent={this.state.showHelpTooltip}
+          >
+            <PopoverBody>
+              <div>
+                To know how to use the Dashboard, read the Dashboard Guide
+              </div>
+            </PopoverBody>
+          </Popover>
+        </span>
       </div>
     );
   }
