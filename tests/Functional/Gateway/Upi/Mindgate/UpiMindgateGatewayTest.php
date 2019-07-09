3a84dd3d79f694354dcc6b4c4d9fc73894a12804
akshaygupta475 <akshay.gupta@razorpay.com>
2019-07-09 14:08:37 +0530

    added upi_transaction_id check in test cases

--- tests/Functional/Gateway/Upi/Mindgate/UpiMindgateGatewayTest.php
+++ tests/Functional/Gateway/Upi/Mindgate/UpiMindgateGatewayTest.php
@@ -102,12 +102,7 @@ class UpiMindgateGatewayTest extends TestCase
         $upiEntity = $this->getLastEntity('upi', true);
         $this->assertNotNull($upiEntity['npci_reference_id']);
         $this->assertNotNull($payment['acquirer_data']['rrn']);
-
-        // checking upi_transaction_id coming for mindgate gateway
-        if ($payment['gateway'] === "upi_mindgate")
-        {
-            $this->assertNotNull($payment['acquirer_data']['upi_transaction_id']);
-        }
+        $this->assertNotNull($payment['acquirer_data']['upi_transaction_id']);
 
         $this->assertEquals($payment['reference16'], $upiEntity['npci_reference_id']);
         $this->assertNotNull($upiEntity['gateway_payment_id']);
