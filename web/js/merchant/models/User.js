c6eb29cba137380467d1e9428d6f4f8e5d8db2b4
dvvavinash <dvv.avinash@gmail.com>
2019-07-11 15:05:55 +0530

    Remove test international

--- web/js/merchant/models/User.js
+++ web/js/merchant/models/User.js
@@ -331,7 +331,6 @@ export default class User {
   }
 
   get isInttCurrenciesEnabled() {
-    return true; // For testing purpose
     return (
       !!this.international && this.getExpStatus('international_currencies')
     );
