6023096e1937d118fc20b7987e9ba23a1ddfa076
Patlola Praveen Kumar Reddy <praveen.patlola@razorpay.com>
2019-07-16 14:44:42 +0530

    Payment transfer transaction retry on deadlock found (#12415)
    * payment transfer transaction retry on deadlock error
    
    * transaction attempts to 2

--- app/Models/Payment/Processor/Processor.php
+++ app/Models/Payment/Processor/Processor.php
@@ -1193,9 +1193,20 @@ class Processor
 
         $validator->validateInput('transfer', $input);
 
+        $merchantId = $payment->getMerchantId();
+
+        $result = app('razorx')->getTreatment($merchantId, 'transfer_deadlock_retry', $this->mode);
+
+        $deadLockRetryAttempts = 1;
+
+        if (strtolower($result) === 'on')
+        {
+            $deadLockRetryAttempts = 2;
+        }
+
         return $this->mutex->acquireAndRelease(
             $payment->getId(),
-            function() use ($payment, $input)
+            function() use ($payment, $input, $deadLockRetryAttempts)
             {
                 $this->repo->reload($payment);
 
@@ -1211,7 +1222,7 @@ class Processor
                         ['transfer_ids' => $transfers->getIds()]);
 
                     return $transfers;
-                });
+                }, $deadLockRetryAttempts);
             });
     }
 
@@ -1715,7 +1726,6 @@ class Processor
 
             $gatewayDowntimeError = true;
 
-
             throw $ex;
         }
         finally
@@ -1754,7 +1764,6 @@ class Processor
             Action::AUTHORIZE,
             Action::CALLBACK
         ];
-
 
         return in_array($action, $gatewayDowntimeActions, true);
     }
