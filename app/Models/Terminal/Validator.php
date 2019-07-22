9a49119e5875f2dc37852760906d9c7657397a1c
abhinavv21 <31855138+abhinavv21@users.noreply.github.com>
2019-07-22 17:14:10 +0530

    [terminal] Add Cub nb edit terminal rules (#12522)
    * [terminal] Add edit rules for cub terminal

--- app/Models/Terminal/Validator.php
+++ app/Models/Terminal/Validator.php
@@ -86,6 +86,7 @@ class Validator extends Base\Validator
         Payment\Gateway::NETBANKING_VIJAYA,
         Payment\Gateway::NETBANKING_YESB,
         Payment\Gateway::NETBANKING_FEDERAL,
+        Payment\Gateway::NETBANKING_CUB,
         Payment\Gateway::EMI_SBI,
         Payment\Gateway::WALLET_OLAMONEY,
         Payment\Gateway::PAYTM,
@@ -516,6 +517,16 @@ class Validator extends Base\Validator
         Entity::GATEWAY_TERMINAL_PASSWORD  => 'required|string',
         Entity::GATEWAY_SECURE_SECRET2     => 'required|string',
         Entity::GATEWAY_TERMINAL_PASSWORD2 => 'required|string',
+    ];
+
+    protected static $netbankingCubEditTerminalRules = [
+        Entity::GATEWAY                    => 'sometimes|in:netbanking_cub',
+        Entity::GATEWAY_MERCHANT_ID        => 'sometimes|string',
+        Entity::GATEWAY_ACCESS_CODE        => 'sometimes|string',
+        Entity::GATEWAY_SECURE_SECRET      => 'sometimes|string',
+        Entity::GATEWAY_TERMINAL_PASSWORD  => 'sometimes|string',
+        Entity::GATEWAY_SECURE_SECRET2     => 'sometimes|string',
+        Entity::GATEWAY_TERMINAL_PASSWORD2 => 'sometimes|string',
     ];
 
     protected static $netbankingVijayaTerminalRules = [
