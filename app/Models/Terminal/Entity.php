2ffbe0c9513a435f631b26e47a121f18e8c0be7a
Rohit Keshwani <rohitkeshwani07@gmail.com>
2019-07-22 16:55:00 +0530

    Added support of more billdesk banks

--- app/Models/Terminal/Entity.php
+++ app/Models/Terminal/Entity.php
@@ -934,7 +934,11 @@ class Entity extends Base\PublicEntity
 
         $supportedBanks = Netbanking::getSupportedBanksForGateway($gateway, $corporate, $tpv);
 
-        $this->setAttribute(self::ENABLED_BANKS, $supportedBanks);
+        $disabledBanks = Netbanking::getDefaultDisabledBanksForGateway($gateway, $corporate, $tpv);
+
+        $enabledBanks = array_diff($supportedBanks, $disabledBanks);
+
+        $this->setAttribute(self::ENABLED_BANKS, $enabledBanks);
     }
 
     public function edit(array $input = [], $operation = 'edit')
