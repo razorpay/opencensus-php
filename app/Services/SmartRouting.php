3dd526f936cf7e267dab988f7a92e5d66b8d9810
akshaygupta475 <akshay.gupta@razorpay.com>
2019-07-24 18:28:03 +0530

    added request in case of failure

--- app/Services/SmartRouting.php
+++ app/Services/SmartRouting.php
@@ -158,6 +158,7 @@ class SmartRouting
             $this->trace->error(
                 TraceCode::SMART_ROUTING_SERVICE_ERROR,
                 [
+                    'request'  => $request,
                     'response' => $e->getMessage(),
                     'action'   => $action,
                     'data'     => $data,
