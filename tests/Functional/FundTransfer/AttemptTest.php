b01f9b07d6fab5194e8d6a353d6601afe6d11dac
PAWAN RAJ MURARKA <pawan.murarka@razorpay.com>
2019-07-25 14:40:28 +0530

    Updated cron flow for yesbank transfers and skipped unused tests

--- tests/Functional/FundTransfer/AttemptTest.php
+++ tests/Functional/FundTransfer/AttemptTest.php
@@ -213,12 +213,18 @@ class AttemptTest extends TestCase
 
     public function testPayoutFileCreationYesbankImps()
     {
+        // Yesbank using api based channels
+        $this->markTestSkipped();
+
         $this->createDataAndAssertInitiateTransferSuccess(
             Channel::YESBANK, 2, Attempt\Type::PAYOUT);
     }
 
     public function testPayoutFileCreationYesbankRtgsSuccess()
     {
+        // Yesbank using api based channels
+        $this->markTestSkipped();
+
         $now = Carbon::create(2018, 8, 14, 10, 0, 0, Timezone::IST);
 
         Carbon::setTestNow($now);
