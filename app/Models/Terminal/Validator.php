6f382fce35e1d93cf6260541e2c8589f869132eb
vivekagarwal1984 <vivek.agarwal@razorpay.com>
2019-07-29 17:21:10 +0530

    Update axis migs edit rules

--- app/Models/Terminal/Validator.php
+++ app/Models/Terminal/Validator.php
@@ -289,7 +289,7 @@ class Validator extends Base\Validator
         Entity::CARD                       => 'sometimes|boolean|in:1',
         Entity::TYPE                       => 'sometimes|array',
         Entity::INTERNATIONAL              => 'sometimes|boolean',
-        Entity::CAPABILITY                 => 'sometimes|boolean',
+        Entity::CAPABILITY                 => 'sometimes|in:0,2',
     ];
 
     protected static $isgEditTerminalRules = [
