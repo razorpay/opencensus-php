f7d51c6505964c278d57de3205419300c9e72344
dvvavinash <dvv.avinash@gmail.com>
2019-07-30 19:04:10 +0530

    Fix for seller_app experiment

--- web/js/merchant/models/User.js
+++ web/js/merchant/models/User.js
@@ -347,9 +347,9 @@ export default class User {
   get toShowExtraFieldsInPP() {
     return this.getExpStatus('show_extra_fields_in_pp');
   }
-  
+
   get isEnhancedEPOSEnabled() {
-    return this.isFeatureEnabled('sellerapp_plus');
+    return this.getExpStatus('sellerapp_plus');
   }
 }
 
