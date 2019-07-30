cebbf011aec1915b6062ae9a51bd0657bd8c2cfb
akshaygupta475 <akshay.gupta@razorpay.com>
2019-07-30 17:33:54 +0530

    removed test cases related to downtime sorting and failed terminals

--- tests/Functional/Payment/helpers/GatewayDowntimeSorterTestData.php
+++ tests/Functional/Payment/helpers/GatewayDowntimeSorterTestData.php
@@ -8,62 +8,11 @@ return [
         'source'      => 'other',
     ],
 
-    'hdfcVisaDowntimeData' => [
-        'gateway'     => 'hdfc',
-        'reason_code' => 'LOW_SUCCESS_RATE',
-        'network'     => 'VISA',
-        'issuer'      => 'HDFC',
-    ],
-
     'migsAllIssuerDowntimeData' => [
         'gateway'     => 'axis_migs',
         'reason_code' => 'LOW_SUCCESS_RATE',
         'network'     => 'ALL',
         'issuer'      => 'ALL',
-    ],
-
-    'hdfcAllNetworkAllIssuerDowntimeData' => [
-        'gateway'     => 'hdfc',
-        'reason_code' => 'LOW_SUCCESS_RATE',
-        'network'     => 'ALL',
-        'issuer'      => 'ALL',
-    ],
-
-    'hdfcAllNetworkAllIssuerAllAcquirerDowntimeData' => [
-        'gateway'     => 'hdfc',
-        'reason_code' => 'LOW_SUCCESS_RATE',
-        'network'     => 'ALL',
-        'issuer'      => 'ALL',
-        'acquirer'    => 'ALL',
-    ],
-
-    'hdfcAllNetworkAllIssuerHdfcAcquirerDowntimeData' => [
-        'gateway'     => 'hdfc',
-        'reason_code' => 'LOW_SUCCESS_RATE',
-        'network'     => 'ALL',
-        'issuer'      => 'ALL',
-        'acquirer'    => 'hdfc',
-    ],
-
-    'allGatewayAllIssuerAllNetworkHdfcAcquirerData' => [
-        'gateway'     => 'ALL',
-        'reason_code' => 'LOW_SUCCESS_RATE',
-        'network'     => 'ALL',
-        'issuer'      => 'ALL',
-        'acquirer'    => 'hdfc',
-    ],
-
-    'hdfcMastercardNetworkData' => [
-        'gateway'     => 'hdfc',
-        'reason_code' => 'LOW_SUCCESS_RATE',
-        'network'     => 'MC',
-        'issuer'      => 'ALL',
-    ],
-
-    'hdfcUnkownIssuerNetworkData' => [
-        'gateway'     => 'hdfc',
-        'reason_code' => 'ISSUER_DOWN',
-        'source'      => 'STATUSCAKE',
     ],
 
     'allGatewayIssuerHdfcNetworkVisaData' => [
@@ -80,45 +29,10 @@ return [
         'issuer'      => 'HDFC',
     ],
 
-    'hdfcNetworkAllIssuerHdfc' => [
-        'gateway'     => 'hdfc',
-        'reason_code' => 'ISSUER_DOWN',
-        'network'     => 'ALL',
-        'issuer'      => 'HDFC',
-    ],
-
     'cybersourceDowntimeData' => [
         'gateway'     => 'cybersource',
         'reason_code' => 'LOW_SUCCESS_RATE',
         'network'     => 'ALL',
         'issuer'      => 'ALL',
-    ],
-
-    'testDowntimeSortingForVajraUpiWebHook' => [
-        'request' => [
-            'content' => [
-                'evalMatches' => [],
-                'message'  => '',
-                'ruleId'   => 242,
-                'ruleName' => 'Total Success  Rate is less then 80',
-                'ruleUrl'  => 'https://vajra.razorpay.com/d/XmyC-WYmz/prod-payments-success-rate?fullscreen=true&edit=true&tab=alert&panelId=2&orgId=1',
-                'state'    => 'alerting',
-                'title'    => '[Alerting] Total Success  Rate is less then 80',
-            ],
-            'method' => 'POST',
-            'url' => '/gateway/downtimes/webhook/vajra'
-        ],
-        'response' => [
-            'content' => [
-                [
-                    'scheduled'   => false,
-                    'partial'     => false,
-                    'source'      => 'VAJRA',
-                    'reason_code' => 'LOW_SUCCESS_RATE',
-                    'method'      => 'upi',
-                    'gateway'     => 'upi_icici',
-                ]
-            ]
-        ],
     ],
 ];
