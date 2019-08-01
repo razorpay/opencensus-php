1b79f38e4a2333476d5b71c220d65c3ea300558d
Badrinath <51152241+badrimuralim@users.noreply.github.com>
2019-08-01 12:48:14 +0530

    [recon] netbanking refund automatic recon (#12508)
    * [recon] netbanking recon automatic recon
    
    * [recon] add functions for recon
    
    * use refund/core
    
    * optimise imports
    
    * reconcile after sending mail.
    
    * use one function
    
    * fix formatting
    
    * change flow for kotak netbanking refund test
    
    * [recon] federal netbanking test
    
    * [recon] include obc, csb in reconciliation.
    
    * [recon] revert kotak and federal test cases
    
    * [recon] add new testcases for kotak
    
    * [recon] new test case for netbanking federal
    
    * [recon] testcases for hdfc

--- app/Models/Payment/Refund/Service.php
+++ app/Models/Payment/Refund/Service.php
@@ -44,10 +44,13 @@ class Service extends Base\Service
     const SCROOGE_TAGGING_LIVE_TIMESTAMP = 1552646209;
 
     protected $mutex;
+    protected $core;
 
     public function __construct()
     {
         parent::__construct();
+
+        $this->core = new Refund\Core;
 
         $this->mutex = $this->app['api.mutex'];
     }
@@ -250,6 +253,8 @@ class Service extends Base\Service
         $gateway = $terminal->getGateway();
 
         $file = $this->app['gateway']->call($gateway, Payment\Action::GENERATE_REFUNDS, $input, $this->mode);
+
+        $this->core->reconcileNetbankingRefunds($data);
 
         return ['file' => $file, 'count' => $count];
     }
--- tests/Functional/Gateway/File/helpers/NebtankingFederalCombinedFileTestData.php
+++ tests/Functional/Gateway/File/helpers/NebtankingFederalCombinedFileTestData.php
@@ -35,5 +35,26 @@ return [
                 ],
             ]
         ],
-    ]
+    ],
+
+    'testGenerateFederalRefundFile' => [
+        'request' => [
+            'content' => [
+                'bank' => "FDRL",
+                'method' => "netbanking",
+                'mode' => "test",
+                'from'=> Carbon::today(Timezone::IST)->getTimestamp(),
+                'to'  => Carbon::tomorrow(Timezone::IST)->getTimestamp()
+            ],
+            'url' => '/refunds/excel',
+            'method' => 'POST'
+        ],
+        'response' => [
+            'content'=> [
+                'netbanking_federal' => [
+                    'refunds' => storage_path('files/filestore/FBK_REFUND_'.Carbon::today(Timezone::IST)->format('d_m_Y').'.txt'),
+                ],
+            ],
+        ],
+    ],
 ];
