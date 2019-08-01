a7b1936f9efd7189a0d3f5661d4b5822f660b889
kajolnigam <42341725+kajolnigam@users.noreply.github.com>
2019-08-01 12:07:21 +0530

    [queue_migration] moves bank transfer to new queue (#12658)

--- config/queue.php
+++ config/queue.php
@@ -199,6 +199,10 @@ return [
         'test'       => env('AWS_PAYMENT_BATCH_QUEUE'),
         'live'       => env('AWS_PAYMENT_BATCH_QUEUE'),
     ],
+    'bank_transfer_batch' => [
+        'test'       => env('AWS_PAYMENT_BATCH_QUEUE'),
+        'live'       => env('AWS_PAYMENT_BATCH_QUEUE'),
+    ],
     'capture' => [
         'test'       => env('AWS_CAPTURE_TEST_QUEUE'),
         'live'       => env('AWS_CAPTURE_LIVE_QUEUE'),
