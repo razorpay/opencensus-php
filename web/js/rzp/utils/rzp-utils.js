89ac62086cb5f6ec2513310b5c07094364497d55
dvvavinash <dvv.avinash@gmail.com>
2019-08-06 13:50:54 +0530

    Fix for xss attack in PowerSelect Component

--- web/js/rzp/utils/rzp-utils.js
+++ web/js/rzp/utils/rzp-utils.js
@@ -911,3 +911,10 @@ export function setNativeValue(element, value) {
     valueSetter.call(element, value);
   }
 }
+
+export const sanitizeHTML = str => {
+  const temp = document.createElement('div');
+  temp.textContent = str;
+
+  return temp.innerHTML;
+};
