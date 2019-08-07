4cfaaef7f83ee714c2c759c1ad817d9fb1dd39de
Vidya Sagar Kushwaha <vidyasagar.kushwaha@razorpay.com>
2019-08-07 17:11:55 +0530

    [recon] Hitachi : create unexpected BQR payment via Recon flow (#12664)
    * [recon] Hitachi : create unexpected BQR payment via Recon flow
    
    * [recon] adds dummy card_network in callback data
    
    * [recon] reverts Mode change related changes
    
    * [recon] Derive card network from card bin
    
    * [recon] adds testcase, Overwrites the MODE to test in determineAndSetModeForQr()
    
    * [recon] uses dashboard Auth to skip checksum validation
    
    * [recon] fixed mode issue,now uses live mode, like other similar tests
    
    * [recon] uses static variable isRecon flag to skip checksum validation
    
    * [recon] addresses comments

--- app/Reconciliator/Base/Reconciliate.php
+++ app/Reconciliator/Base/Reconciliate.php
@@ -104,6 +104,14 @@ class Reconciliate extends Base\Core
 
     protected $gateway;
 
+    /**
+     * This variable indicates if we are in reconciliation code flow.
+     * Currently this flag is being used to skip checksum validation
+     * while creating Hitachi Unexpected payment via recon.
+     * @var bool
+     */
+    public static $isReconRunning = false;
+
     public function __construct(string $gateway = null)
     {
         parent::__construct();
@@ -170,6 +178,8 @@ class Reconciliate extends Base\Core
 
         foreach ($allFilesContents as $fileContents)
         {
+            self::$isReconRunning = true;
+
             $extraDetails = $fileContents[Orchestrator::EXTRA_DETAILS];
 
             $reconciliationType = $this->getReconciliationType($fileContents[Orchestrator::EXTRA_DETAILS]);
@@ -204,6 +214,8 @@ class Reconciliate extends Base\Core
             {
                 // Create the output file
                 $this->generateReconOutputFile($batchProcessor, $extraDetails);
+
+                self::$isReconRunning = false;
             }
         }
 
