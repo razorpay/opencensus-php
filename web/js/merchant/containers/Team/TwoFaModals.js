f2585c0b106a18c7c451db32b4f7b3cab2651878
Vikram Singh <vikram.singh@razorpay.com>
2019-08-07 19:14:40 +0530

    WIP 2FA

--- web/js/merchant/containers/Team/TwoFaModals.js
+++ web/js/merchant/containers/Team/TwoFaModals.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ModalHeader from 'rzp/ui/ModalHeader';
 import { OtpInput } from 'merchant/components/OtpInput';
+import { connect } from 'react-redux';
+import { Field, reduxForm, formValueSelector } from 'redux-form';
+import InputField from 'rzp/ui/Forms/InputField';
+import { required, phone } from 'rzp/utils/validators';
+import AsyncButton from 'react-async-button';
 
 const VerifyMobileNumber = ({
   mobile,
   closeModal,
-  onOtpEnter,
   onConfirm,
+  wrong,
+  onOtpEnter,
   changeMobile,
 }) => {
   return (
@@ -19,12 +25,7 @@ const VerifyMobileNumber = ({
         </p>
         <p>OTP will expire in 5mins. </p>
 
-        <OtpInput
-          onComplete={otp => {
-            onOtpEnter(otp);
-          }}
-          wrong={false}
-        />
+        <OtpInput onComplete={otp => {}} wrong={wrong} />
         <p>Didn’t receive an SMS? Sending.</p>
         <div class="Modal__actions">
           <button class="btn btn-primary btn-block" onClick={onConfirm}>
@@ -68,5 +69,186 @@ const MissingNumbers = ({ count, closeModal }) => {
     </div>
   );
 };
+const EnableAgreement = ({ closeModal, onAgree }) => {
+  return (
+    <div class="2fa-modal">
+      <ModalHeader
+        title="Setting up 2-step verification"
+        onCloseClick={closeModal}
+      />
+      <div class="modal-body">
+        <p>
+          To enforce 2-step verification to your team, your account needs to be
+          2-step enabled
+        </p>
+        <div class="Modal__actions no-side-padding">
+          <button class="btn btn-primary btn-block" onClick={onAgree}>
+            Agree and proceed
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+const DisableAgreement = ({ closeModal, onAgree }) => {
+  return (
+    <div class="2fa-modal">
+      <ModalHeader
+        title="Disable 2-step verification"
+        onCloseClick={closeModal}
+      />
+      <div class="modal-body">
+        <p>
+          Are you sure you want to disable 2-step verification to all your team
+          members?
+        </p>
+        <div
+          class="Modal__actions no-side-padding"
+          style={{ display: 'flex', justifyContent: 'space-between' }}
+        >
+          <button
+            class="btn btn-default"
+            onClick={closeModal}
+            style={{ width: '131px' }}
+          >
+            No, Don’t!
+          </button>
+          <button
+            class="btn btn-primary"
+            onClick={onAgree}
+            style={{ width: '131px' }}
+          >
+            Yes, disable it
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export { VerifyMobileNumber, MissingNumbers };
+@reduxForm({
+  form: 'askPhone',
+  initialValues: {
+    phone: '',
+  },
+})
+class AskMobileNumber extends Component {
+  handleSubmit = () => {};
+  render() {
+    const { closeModal } = this.props;
+    return (
+      <div class="2fa-modal">
+        <ModalHeader
+          title="Setting up 2-step verification"
+          onCloseClick={closeModal}
+        />
+        <div class="modal-body">
+          <p>
+            Let's setup a mobile number where you will receive an SMS with OTP
+            everytime you log in.
+          </p>
+          <form
+            onSubmit={this.handleSubmit(this.save)}
+            style={{ marginBottom: '35px' }}
+          >
+            <div class="form-group">
+              <label>Enter your phone number </label>
+              <Field
+                name="contact_mobile"
+                component={InputField}
+                class="form-control"
+                placeholder="Phone Number"
+                validate={[
+                  required(),
+                  phone('Invalid Mobile'),
+                  value => {
+                    if (value === this.props.user.user.contact_mobile) {
+                      return "You can't invite yourself";
+                    }
+                  },
+                ]}
+              />
+            </div>
+            <div class="form-group">
+              <AsyncButton
+                class="btn btn-primary btn-block"
+                text="Send OTP"
+                pendingText="Sending OTP..."
+                onClick={this.handleSubmit(this.save)}
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+@reduxForm({
+  form: 'askPhone',
+  initialValues: {
+    phone: '',
+  },
+})
+class PasswordVerification extends Component {
+  handleSubmit = f => {
+    console.log(f);
+    this.props.onSuccess();
+  };
+  render() {
+    const { closeModal, title, email } = this.props;
+
+    return (
+      <div class="2fa-modal">
+        <ModalHeader title={title} onCloseClick={closeModal} />
+        <div class="modal-body">
+          <p>
+            To confirm please enter the password for <strong>{email}</strong>
+          </p>
+          <form
+            onSubmit={() => this.handleSubmit(this.save)}
+            style={{ marginBottom: '35px' }}
+          >
+            <div class="form-group">
+              <Field
+                name="password"
+                component={InputField}
+                class="form-control"
+                placeholder="Password"
+                validate={[required()]}
+                autoFocus={true}
+              />
+            </div>
+            <div
+              class="Modal__actions no-side-padding"
+              style={{ display: 'flex', justifyContent: 'space-between' }}
+            >
+              <button
+                class="btn btn-default"
+                onClick={closeModal}
+                style={{ width: '131px' }}
+              >
+                Cancel
+              </button>
+              <AsyncButton
+                style={{ width: '131px' }}
+                class="btn btn-primary btn-block"
+                text="Confirm"
+                pendingText="Please Wait..."
+                onClick={() => this.handleSubmit(this.save)}
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export {
+  VerifyMobileNumber,
+  MissingNumbers,
+  EnableAgreement,
+  DisableAgreement,
+  PasswordVerification,
+  AskMobileNumber,
+};
