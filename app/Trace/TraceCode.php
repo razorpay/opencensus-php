b76bf7853a6db0c5585db684fd0c2c1cf1bae74a
krantideep95 <kranti.deep@razorpay.com>
2019-08-08 18:58:03 +0530

    Revert "Revert "remove paysecure specific card cache methods"" (#12729)
    * Revert "Revert "remove paysecure specific card cache methods (#12711)" (#12727)"
    
    This reverts commit 276ff6918ef1fecf172a6c077a8c3ba96490eff3.
    
    * Card/Entity: set vault_token in toArray()
    
    * trace if card vault token is missing
    
    * replace strings with const
    
    * used paysecure key for cache
    
    * using getter for network code
    
    * use network_code attribute for checking network

--- app/Trace/TraceCode.php
+++ app/Trace/TraceCode.php
@@ -1074,6 +1074,7 @@ class TraceCode extends \Razorpay\Trace\TraceCode
     const CARD_VAULT_RESPONSE                                   = 'CARD_VAULT_RESPONSE';
     const CARD_VAULT_RETRY                                      = 'CARD_VAULT_RETRY';
     const CARD_VAULT_REQUEST_FAILED                             = 'CARD_VAULT_REQUEST_FAILED';
+    const CARD_VAULT_TOKEN_MISSING                              = 'CARD_VAULT_TOKEN_MISSING';
     const CORE_PAYMENT_SERVICE_REQUEST                          = 'CORE_PAYMENT_SERVICE_REQUEST';
     const CORE_PAYMENT_SERVICE_RESPONSE                         = 'CORE_PAYMENT_SERVICE_RESPONSE';
     const CORE_PAYMENT_SERVICE_RETRY                            = 'CORE_PAYMENT_SERVICE_RETRY';
