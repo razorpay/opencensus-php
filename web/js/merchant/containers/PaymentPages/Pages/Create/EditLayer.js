36fd0c3bc3e45abdbff94ecaa4df148c6104302d
aseemgupta <aseemgupta2625@gmail.com>
2019-08-13 16:44:11 +0530

    [PaymentpagesV3]: Add display field for UDF

--- web/js/merchant/containers/PaymentPages/Pages/Create/EditLayer.js
+++ web/js/merchant/containers/PaymentPages/Pages/Create/EditLayer.js
@@ -1,11 +1,12 @@
 import { Info } from 'component/Input';
 import { classList } from 'common/util';
 
-export default ({ children, infoTxt, className, onClick, ...rest }) => (
+export default ({ children, infoTxt, className, onClick, setRef, ...rest }) => (
   <div
     {...rest}
     onClick={onClick}
     class={classList('wysiwyg-edit-layer', className)}
+    ref={setRef}
   >
     {children}
     {infoTxt && <Info text={infoTxt} />}
