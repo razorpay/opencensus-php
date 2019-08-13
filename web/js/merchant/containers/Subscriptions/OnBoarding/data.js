58f18783b225d2e7294de8bfd5ffce17e3920a36
dvvavinash <dvv.avinash@gmail.com>
2019-08-13 11:29:47 +0530

    Add OnBoarding in subscriptions

---
+++ web/js/merchant/containers/Subscriptions/OnBoarding/data.js
@@ -0,0 +1,47 @@
+// Features
+export const FEATURES_DATA = [
+  {
+    icon: '/dist/css/assets/feature_onboarding/2.svg',
+    title: 'Subscription Links',
+    desc:
+      'Share unique links and onboard users for your Subscription packages instantly! No API integration necessary.',
+  },
+  {
+    icon: '/dist/css/assets/feature_onboarding/3.svg',
+    title: 'Multi-currency Support',
+    desc:
+      'Accept recurring payments from customers in India and abroad via Razorpay Subscriptions. 100 currencies including supported!',
+  },
+  {
+    icon: '/dist/css/assets/feature_onboarding/5.svg',
+    title: 'Multiple payment modes',
+    desc: `Offer your customers a variety of payment methods. All payment modes offering recurring are compliant with RBI's regulations.`,
+  },
+];
+
+// Landing
+export const PROS = [
+  <span>
+    <b>8</b>% Reduction in churn
+  </span>,
+  <span>
+    <b>24</b>% Lower collection costs
+  </span>,
+  <span>
+    Upto <b>1.7</b>x increase in CLTV
+  </span>,
+];
+
+// Features Links
+export const FEATURES_LINKS = [
+  {
+    label: 'Know more',
+    url: 'https://razorpay.com/docs/subscriptions/',
+    ga: 'Know More CTA',
+  },
+  {
+    label: 'View API Docs',
+    url: 'https://razorpay.com/docs/subscriptions/',
+    ga: 'View API Docs',
+  },
+];
