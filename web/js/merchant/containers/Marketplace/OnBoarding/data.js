817c1ed49ece1d569ddc0be471e9588444fb6ace
dvvavinash <dvv.avinash@gmail.com>
2019-08-13 15:07:15 +0530

    [Route Onboarding]: Add OnBoarding and  QuickGuide components into Subscriptions

---
+++ web/js/merchant/containers/Marketplace/OnBoarding/data.js
@@ -0,0 +1,32 @@
+// Features
+export const FEATURES_DATA = [
+  {
+    icon: 'https://razorpay.com/assets/route/2.svg',
+    title: 'Linked Accounts',
+    desc:
+      'Easily onboard your vendors, sellers, service providers etc. as linked accounts without the hassles of any physical paperwork.',
+  },
+  {
+    icon: 'https://razorpay.com/assets/route/3.svg',
+    title: 'Transfers',
+    desc:
+      'Create as many transfers as you need to various linked accounts and automate the process through powerful Transfer APIs.',
+  },
+  {
+    icon: 'https://razorpay.com/assets/route/4.svg',
+    title: 'Settlements',
+    desc: 'Control the entire fund movements with flexible settlement plans.',
+  },
+];
+
+// Features Links
+export const FEATURES_LINKS = [
+  {
+    label: 'Know more',
+    url: 'https://razorpay.com/docs/route/',
+  },
+  {
+    label: 'View API Docs',
+    url: 'https://razorpay.com/docs/route/',
+  },
+];
---
+++ web/js/merchant/containers/Marketplace/QuickGuide/index.js
@@ -0,0 +1,113 @@
+import { connect } from 'react-redux';
+
+import { PossibleStatuses, RZPFeatures } from 'rzp/utils/constants';
+
+import Step from 'merchant/components/StepGuide/Step';
+import QuickGuide, {
+  getQuickGuideIsClosed,
+} from 'merchant/components/QuickGuide';
+import QuickStepGuide, {
+  Title,
+  CloseBtn as QuickGuideCloseBtn,
+} from 'merchant/components/QuickGuide/QuickStepGuide';
+
+import { getQuickGuideData } from './data';
+
+const { done, locked, active, loading } = PossibleStatuses;
+
+@connect(state => ({
+  user: state.session.user,
+  mode: state.session.mode,
+}))
+@QuickGuide({
+  feature: RZPFeatures.ROUTE,
+  data_points: ['transfers', 'accounts'],
+  dataTransformer: (key, state) => {
+    if (key === 'accounts') {
+      return {
+        ...state.accounts,
+        items: state.accounts.accounts,
+      };
+    }
+
+    return state[key];
+  },
+})
+export default class MarketPlaceQuickGuide extends React.Component {
+  getCloseBtn = isCompleted => {
+    return (
+      <QuickGuideCloseBtn
+        isCompleted={isCompleted}
+        onClick={this.props.onClickClose}
+      />
+    );
+  };
+
+  render() {
+    const { accountsStatus, transfersStatus } = getStatus(this.props);
+
+    const CloseBtn = this.getCloseBtn(transfersStatus === done);
+
+    return (
+      <QuickStepGuide class="Route" title={QuickGuideTitle} closeBtn={CloseBtn}>
+        <Step
+          status={accountsStatus}
+          {...getQuickGuideData.LinkedAccount(accountsStatus)}
+        />
+
+        <Step
+          status={transfersStatus}
+          {...getQuickGuideData.Transfers(transfersStatus)}
+        />
+      </QuickStepGuide>
+    );
+  }
+}
+
+const QuickGuideTitle = <Title />;
+
+export const getRouteQuickGuideIsClosed = props => {
+  let isClosed = getQuickGuideIsClosed(props, RZPFeatures.ROUTE);
+
+  // Check if transfers non created state count is more then or equal to 2
+  if (isClosed || props.transfers.items.length <= 2) {
+    return isClosed;
+  }
+
+  return true;
+};
+
+const getStatus = ({ accounts, transfers }) => {
+  if (!transfers.items.length && !accounts.items.length && accounts.loading) {
+    return {
+      accountsStatus: loading,
+      transfersStatus: loading,
+    };
+  }
+
+  const accountsStatus = transfers.items.length
+    ? done
+    : accounts.items.length ? done : active;
+
+  if (!transfers.items.length && transfers.loading) {
+    return {
+      accountsStatus: accountsStatus,
+      transfersStatus: loading,
+    };
+  }
+
+  let transfersStatus = locked;
+
+  if (accountsStatus === done) {
+    if (transfers.items.length) {
+      transfersStatus = done;
+    } else {
+      transfersStatus = active;
+    }
+  }
+
+  return {
+    accountsStatus,
+    transfersStatus,
+  };
+};
