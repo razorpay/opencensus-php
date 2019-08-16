48ff0864759f31562529a43d4b782678b35062ca
ratanphyade <ratanphayade@gmail.com>
2019-08-17 03:18:01 +0530

    bucket: merchant preference filter added

---
+++ app/Models/Settlement/bucket/Preference.php
@@ -0,0 +1,295 @@
+<?php
+
+namespace RZP\Models\Settlement\Bucket;
+
+use Cache;
+use Carbon\Carbon;
+
+use RZP\Models\Base;
+use RZP\Models\Feature;
+use RZP\Models\Merchant;
+use RZP\Constants\Timezone;
+use RZP\Models\Settlement\Holidays;
+
+class Preference extends Base\Core
+{
+    /**
+     * Will give the timestamp for next bucket for the given timestamp
+     * if the hour anchor is given, then it'll make sure that this is added in the hour bucket
+     *
+     * @param     $timestamp
+     * @param int $hour
+     * @return int
+     */
+    public static function getNextBucket($timestamp, int $hour = 0): int
+    {
+        $timestamp = Carbon::createFromTimestamp($timestamp, Timezone::IST);
+
+        $timestamp->subSeconds($timestamp->second)
+                  ->subMinutes($timestamp->minute)
+                  ->addHours(1);
+
+        // TODO: add hour anchor
+        // If no hour anchor is given then used the calculated timestamp
+        if ($hour === 0)
+        {
+            return $timestamp->getTimestamp();
+        }
+
+        // Add hour anchor (next eligible hour)
+        // if anchor is present then it should go only on working days
+        if ($timestamp->hour > $hour)
+        {
+            $timestamp = Holidays::getNextWorkingDay($timestamp);
+
+        }
+
+        $timestamp = $timestamp->setDateTime($timestamp->year, $timestamp->month, $timestamp->day, $hour, 0);
+
+        return $timestamp->getTimestamp();
+    }
+    /**
+     * get suitable bucket timestamp for early settlement.
+     * if early settlement is not enabled for the merchant then it'll return false
+     * so rest of the precess will execute as expected
+     *
+     * @param string $merchantId
+     * @param        $settlementTime
+     * @return array
+     */
+    public function getEarlySettlementBucketIfApplicable(string $merchantId, $settlementTime): array
+    {
+        // fetch ES feature list for the given merchant
+        $featureList = $this->getSettlementFeatureList($merchantId);
+
+        // If ES feature is enabled to the merchant then,
+        // add the merchant to ES bucket based on the settlement time given
+        if (empty($featureList) === true)
+        {
+            return [false, 0];
+        }
+
+        // this is a mandatory flag to enable early settlement
+        if (in_array(Feature\Constants::ES_AUTOMATIC, $featureList, true) === false)
+        {
+            return [false, 0];
+        }
+
+        $timestamp = $this->getEarlySettlementBucket($merchantId, $featureList, $settlementTime);
+
+        return [true, $timestamp];
+    }
+
+    /**
+     * adds merchant to settlement bucket if merchant has special preferences
+     *
+     * @param string          $merchantId
+     * @param                 $settlementTime
+     * @return array
+     */
+    public function getMerchantSpecificBucket(string $merchantId, $settlementTime): array
+    {
+        $merchant = $this->repo->merchant->find($merchantId);
+
+        $settlementTime = Carbon::createFromTimestamp($settlementTime, Timezone::IST);
+
+        $data = $this->getDSPMerchantBucket($merchant, $settlementTime);
+
+        if ($data[0] === true)
+        {
+            return $data;
+        }
+
+        $data = $this->getMutualFundBucket($merchant, $settlementTime);
+
+        if ($data[0] === true)
+        {
+            return $data;
+        }
+
+        $data = $this->getKarvySettlementBucket($merchant, $settlementTime);
+
+        if ($data[0] === true)
+        {
+            return $data;
+        }
+
+        return [false, 0];
+    }
+
+    /**
+     * fetches settlement related feature on given merchant ID
+     * It'll also cache the response for 300 sec for frequent uses
+     *
+     * @param string $merchantId
+     * @return array
+     */
+    protected function getSettlementFeatureList(string $merchantId)
+    {
+        // fetch the data and cache it for future use
+        $featureList = $this->repo
+                            ->feature
+                            ->findMerchantWithFeatures(
+                                $merchantId,
+                                [
+                                    Feature\Constants::ES_AUTOMATIC,
+                                    Feature\Constants::ES_AUTOMATIC_THREE_PM,
+                                ]);
+
+        return $featureList;
+    }
+
+    /**
+     * If merchant has early settlement enabled them find the best bucket
+     * and add merchant to that bucket
+     *
+     * @param string $merchantId
+     * @param array  $featureList
+     * @param        $settlementTime
+     * @return int
+     */
+    protected function getEarlySettlementBucket(string $merchantId, array $featureList, $settlementTime): int
+    {
+        $settlementTime = Carbon::createFromTimestamp($settlementTime, Timezone::IST);
+
+        $now = Carbon::now(Timezone::IST)->getTimestamp();
+
+        $settlementHour = $settlementTime->hour;
+
+        // 9AM or 5PM bucket (which ever is nearest)
+        $hour = (($settlementHour <= Constants::NINE_AM) or ($settlementHour > Constants::FIVE_PM)) ?
+            Constants::NINE_AM : Constants::FIVE_PM;
+
+        // Nearest 3PM bucket
+        if (in_array(Feature\Constants::ES_AUTOMATIC_THREE_PM, $featureList, true) === true)
+        {
+            if ($hour === Constants::NINE_AM)
+            {
+                $hour = (($settlementHour <= Constants::NINE_AM) or ($settlementHour > Constants::THREE_PM)) ?
+                    Constants::NINE_AM : Constants::THREE_PM;
+            }
+            else
+            {
+                $hour = (($settlementHour <= Constants::THREE_PM) or ($settlementHour > Constants::FIVE_PM)) ?
+                    Constants::THREE_PM : Constants::FIVE_PM;
+            }
+        }
+
+        return self::getNextBucket($now, $hour);
+    }
+
+    /**
+     * check karvy merchant preference based if applicable
+     * karvy wants only 2 settlements (1 and 3 PM)
+     *
+     * @param Merchant\Entity $merchant
+     * @param Carbon          $settlementTime
+     * @return array
+     */
+    protected function getKarvySettlementBucket(Merchant\Entity $merchant, Carbon $settlementTime): array
+    {
+        if ($merchant->getParentId() !== Merchant\Preferences::MID_KARVY)
+        {
+            return [false, 0];
+        }
+
+        $hour = 0;
+
+        if (($settlementTime->hour < Constants::ONE_PM) or ($settlementTime->hour >= Constants::THREE_PM))
+        {
+            $hour = Constants::ONE_PM;
+        }
+
+        $timestamp = self::getNextBucket($settlementTime->getTimestamp(), $hour);
+
+        return [true, $timestamp];
+    }
+
+    /**
+     * Mutual fund merchant preferences
+     * This has multiple requirements like
+     * 1. Only one settlement per day (@ 1 PM)
+     * 2. Only two settlements per day (@ 1 and 2 PM)
+     *
+     * @param Merchant\Entity $merchant
+     * @param Carbon          $settlementTime
+     * @return array
+     */
+    protected function getMutualFundBucket(Merchant\Entity $merchant, Carbon $settlementTime): array
+    {
+        // Mutual Fund Marketplace Merchant ids
+        $merchantIds = [
+            Merchant\Preferences::MID_GOALWISE_TPV,
+            Merchant\Preferences::MID_GOALWISE_NON_TPV,
+            Merchant\Preferences::MID_WEALTHAPP,
+            Merchant\Preferences::MID_WEALTHY,
+            Merchant\Preferences::MID_PAISABAZAAR,
+        ];
+
+        $parentMerchantId = $merchant->getParentId();
+
+        if (($merchant->isLinkedAccount() === false) or
+            (in_array($parentMerchantId, $merchantIds, true) === false))
+        {
+            return [false, 0];
+        }
+
+        //
+        // Maps the mids that want to receive only 1 settlement per day.
+        // They need all transactions till 1 pm to be settled in the 1 pm cycle.
+        //
+        $oneSetlAt1PmMids = [
+            Merchant\Preferences::MID_WEALTHY,
+            Merchant\Preferences::MID_PAISABAZAAR,
+        ];
+
+        // matched merchant will have only one settlement per day (@ 1 PM)
+        if (in_array($parentMerchantId, $oneSetlAt1PmMids, true) === true)
+        {
+            $hour = Constants::ONE_PM;
+        }
+        else
+        {
+            // These merchants will have two settlements per day (@ 1 and 2 PM)
+            $hour = Constants::TWO_PM;
+
+            if (($settlementTime->hour < Constants::ONE_PM) or ($settlementTime->hour >= Constants::TWO_PM))
+            {
+                $hour = Constants::ONE_PM;
+            }
+        }
+
+        $timestamp = self::getNextBucket($settlementTime->getTimestamp(), $hour);
+
+        return [true, $timestamp];
+    }
+
+    /**
+     * DSP merchant preference
+     * this merchant wants settlement only between 10AM - 3PM
+     *
+     * @param Merchant\Entity $merchant
+     * @param Carbon          $settlementTime
+     * @return array
+     */
+    protected function getDSPMerchantBucket(Merchant\Entity $merchant, Carbon $settlementTime): array
+    {
+        $merchantId = $merchant->getId();
+
+        if ($merchantId === Constants::MERCHANT_DSP)
+        {
+            $hour = 0;
+
+            if (($settlementTime->hour >= Constants::THREE_PM) or ($settlementTime->hour < Constants::TEN_AM))
+            {
+                $hour = Constants::TEN_AM;
+            }
+
+            $timestamp = self::getNextBucket($settlementTime->getTimestamp(), $hour);
+
+            return [true, $timestamp];
+        }
+
+        return [false, 0];
+    }
+}
