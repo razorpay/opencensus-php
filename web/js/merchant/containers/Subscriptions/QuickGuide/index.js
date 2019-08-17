df433f99050f9bf651bcdb96b2d250636894d6aa
dvvavinash <dvv.avinash@gmail.com>
2019-08-18 03:56:02 +0530

    Rebase with subscriptions-onboarding-final

--- web/js/merchant/containers/Subscriptions/QuickGuide/index.js
+++ web/js/merchant/containers/Subscriptions/QuickGuide/index.js
@@ -6,7 +6,10 @@ import {
 
 import Step from 'merchant/components/StepGuide/Step';
 import QuickGuide, {
+<<<<<<< HEAD
   setQuickGuideIsClosedInLocalStorage,
+=======
+>>>>>>> Rebase with subscriptions-onboarding-final
   getQuickGuideIsClosedFromLocalStorage,
 } from 'merchant/components/QuickGuide';
 import QuickStepGuide, {
@@ -75,6 +78,7 @@ export default class SubscriptionQuickGuide extends React.Component {
 const Title = <QuickGuideTitle />;
 
 export const getSubscriptionQuickGuideIsClosed = props => {
+<<<<<<< HEAD
   if (
     props.subscriptionProductOnBoarding &&
     props.subscriptionProductOnBoarding.isQuickGuideOpen
@@ -82,6 +86,8 @@ export const getSubscriptionQuickGuideIsClosed = props => {
     return false;
   }
 
+=======
+>>>>>>> Rebase with subscriptions-onboarding-final
   if (props.subscriptions.loading) return true;
 
   let isClosed = getQuickGuideIsClosedFromLocalStorage(
@@ -110,7 +116,10 @@ export const getSubscriptionQuickGuideIsClosed = props => {
     if (createCount >= 2) {
       isClosed = true;
 
+<<<<<<< HEAD
       setQuickGuideIsClosedInLocalStorage(RZPFeatures.SUBSCRIPTIONS, true);
+=======
+>>>>>>> Rebase with subscriptions-onboarding-final
       return false;
     }
   });
