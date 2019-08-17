6d07795b26bf4002ca51df13ecfabf5138f31ec1
ratanphyade <ratanphayade@gmail.com>
2019-08-18 00:31:56 +0530

    settlement: mark complete feature added on bucket

--- app/Models/Settlement/Bucket/Repository.php
+++ app/Models/Settlement/Bucket/Repository.php
@@ -14,4 +14,12 @@ class Repository extends Base\Repository
                     ->where(Entity::BUCKET_TIMESTAMP, $bucketTimestamp)
                     ->get();
     }
+
+    public function markAsComplete(string $merchantId, $timestamp)
+    {
+        return $this->newQuery()
+                    ->where(Entity::BUCKET_TIMESTAMP, '<', $timestamp)
+                    ->where(Entity::MERCHANT_ID, $merchantId)
+                    ->update([Entity::COMPLETED => 1]);
+    }
 }
