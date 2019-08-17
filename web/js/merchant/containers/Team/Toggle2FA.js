fb199e49541a712de0639848467d5df70617f644
Anshul Sahni <anshul.sahni@razorpay.com>
2019-08-17 17:18:27 +0530

    [TEAM]: Refactor 2FAToggle component
    	- Remove dead code from Team page

--- web/js/merchant/containers/Team/Toggle2FA.js
+++ web/js/merchant/containers/Team/Toggle2FA.js
@@ -1,5 +1,8 @@
 import { Component } from 'react';
 import { connect } from 'react-redux';
+
+import { classList } from 'common/util';
+
 import { updateFeatures } from 'merchant/modules/config';
 import { showNotification } from 'rzp/modules/notifications';
 import { toggle2FaEnforcement, updateSelfContact } from 'merchant/modules/team';
@@ -16,22 +19,15 @@ import {
   PasswordVerification,
 } from 'merchant/containers/Team/TwoFaModals';
 
-@connect(
-  state => {
-    return {
-      session: state.session,
-    };
-  },
-  {
-    updateFeatures,
-    showNotification,
-    openModal,
-    closeModal,
-    toggle2FaEnforcement,
-    updateSelfContact,
-    updateSession,
-  }
-)
+@connect(state => ({ user: state.session.user }), {
+  updateFeatures,
+  showNotification,
+  openModal,
+  closeModal,
+  toggle2FaEnforcement,
+  updateSelfContact,
+  updateSession,
+})
 export default class Toggle2FA extends Component {
   showModal = component => {
     //Close anyother open modal
@@ -43,18 +39,12 @@ export default class Toggle2FA extends Component {
   };
   //Set the sate in redux store to reflect the new changes
   update2FAState = flag => {
-    const {
-      user: { user: currentUser },
-      mode,
-    } = this.props.session;
+    const { user: currentUser } = this.props.user;
     const user = new User({
       ...this.props.session.user,
-      user: { ...currentUser, second_factor_auth_enforced: flag },
+      user: { ...currentUser, second_factor_auth: flag },
     });
-    this.props.updateSession({
-      user,
-      mode,
-    });
+    this.props.updateSession({ user });
   };
   toggle2FA = flag => {
     //promise that should be resolved to true when all the steps are completed
@@ -71,7 +61,7 @@ export default class Toggle2FA extends Component {
         <PasswordVerification
           title={title}
           closeModal={abort}
-          email={this.props.session.user.user.email}
+          email={this.props.user.user.email}
           onConfirm={password =>
             this.props.toggle2FaEnforcement(flag ? 1 : 0, password).then(
               res => {
@@ -149,11 +139,8 @@ export default class Toggle2FA extends Component {
     return new Promise(resolve => {
       actionCompleted = resolve;
       if (flag) {
-        let {
-          user: {
-            user: { second_factor_auth_setup },
-          },
-        } = this.props.session;
+        const { user: { second_factor_auth_setup } } = this.props.user;
+
         const afterAgreement =
           //Check if user has mobile number verified for setup to continue, if yes skip mobile number verification & move to password verification
           second_factor_auth_setup === true ? verifyPassword : verifyMobile;
@@ -169,21 +156,16 @@ export default class Toggle2FA extends Component {
   };
 
   render() {
-    let {
-      user: {
-        user: { second_factor_auth_enforced },
-      },
-    } = this.props.session;
+    const { user: { second_factor_auth } } = this.props.user;
     return (
       <div class="panel panel-default">
         <div class="panel-heading">
           <span class="title">
-            <i class="i i-phonelink-lock"></i> &nbsp; 2-Step verification to the
-            team
+            <i class="i i-phonelink-lock" /> 2-Step verification to the team
           </span>
           <span class="toggler-btn">
             <SwitchField
-              defaultChecked={!!second_factor_auth_enforced}
+              defaultChecked={second_factor_auth}
               onChange={(flag, cb) =>
                 this.toggle2FA(flag).then(completed => {
                   completed && this.update2FAState(flag);
@@ -192,11 +174,14 @@ export default class Toggle2FA extends Component {
               }
               type="prime"
             />
-            {!!second_factor_auth_enforced ? (
-              <b class="text-primary">Enabled</b>
-            ) : (
-              <b className="text-faded">Disabled</b>
-            )}
+            <strong
+              class={classList(
+                'm-l',
+                second_factor_auth ? 'text-primary' : 'text-faded'
+              )}
+            >
+              {second_factor_auth ? 'Enabled' : 'Disabled'}
+            </strong>
           </span>
         </div>
 
