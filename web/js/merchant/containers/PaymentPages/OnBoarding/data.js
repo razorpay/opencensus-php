2a2c0f763e19d14358a7c581fc7cae889140cad1
dvvavinash <dvv.avinash@gmail.com>
2019-08-18 18:22:47 +0530

    [PaymentPages Onboarding]: Update onboarding slide styles

--- web/js/merchant/containers/PaymentPages/OnBoarding/data.js
+++ web/js/merchant/containers/PaymentPages/OnBoarding/data.js
@@ -1,19 +1,19 @@
 // Features
 export const FEATURES_DATA = [
   {
-    icon: '/dist/css/assets/feature_onboarding/2.svg',
+    icon: '/dist/css/assets/product_onboarding/2.svg',
     title: 'No Coding Required',
     desc:
       'Your business can go online with zero integration and tech efforts. We build and operate for you.',
   },
   {
-    icon: '/dist/css/assets/feature_onboarding/10.svg',
+    icon: '/dist/css/assets/product_onboarding/6.svg',
     title: 'Custom Branded Page',
     desc:
       'Customize the look and feel of your payment pages to reflect your brand colours, for seamless customer experience.',
   },
   {
-    icon: '/dist/css/assets/feature_onboarding/8.svg',
+    icon: '/dist/css/assets/product_onboarding/7.svg',
     title: 'Memorable URLS',
     desc:
       'Look professional with a custom URL that is as easy to remember and is an extension of your business.',
