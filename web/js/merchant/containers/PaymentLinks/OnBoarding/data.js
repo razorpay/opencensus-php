c1a1624285d860b6aac5db6815b2c81a2d60f71c
dvvavinash <dvv.avinash@gmail.com>
2019-08-18 16:59:23 +0530

    [PaymentLinks Onboarding]: Add OnBoarding, QUickGuide

---
+++ web/js/merchant/containers/PaymentLinks/OnBoarding/data.js
@@ -0,0 +1,29 @@
+// Features
+export const FEATURES_DATA = [
+  {
+    icon: '/dist/css/assets/feature_onboarding/2.svg',
+    title: 'No Coding Required',
+    desc:
+      'Your business can go online with zero integration and tech efforts. We build and operate for you.',
+  },
+  {
+    icon: '/dist/css/assets/feature_onboarding/10.svg',
+    title: 'Custom Branded Page',
+    desc:
+      'Customize the look and feel of your payment pages to reflect your brand colours, for seamless customer experience.',
+  },
+  {
+    icon: '/dist/css/assets/feature_onboarding/8.svg',
+    title: 'Memorable URLS',
+    desc:
+      'Look professional with a custom URL that is as easy to remember and is an extension of your business.',
+  },
+];
+
+// Features Links
+export const FEATURES_LINKS = [
+  {
+    label: 'Know more',
+    url: 'https://razorpay.com/docs/payment-pages/',
+  },
+];
---
+++ web/js/merchant/containers/PaymentLinks/OnBoarding/index.js
@@ -0,0 +1,106 @@
+import { RZPFeatures } from 'rzp/utils/constants';
+
+import Slider, { SliderDots } from 'component/Slider';
+
+import Landing from 'merchant/components/OnBoarding/Screens/Landing';
+import Features from 'merchant/components/OnBoarding/Screens/Features';
+import OnBoarding, {
+  FeatureEnableSliderButton,
+  OnBoardingWrapper,
+  SkipAndGetStartedButton,
+  isAllowedResetBoarding,
+  setOnBoardingDataInLocalState,
+} from 'merchant/components/OnBoarding';
+
+import { FEATURES_DATA, FEATURES_LINKS } from './data';
+
+@OnBoarding({
+  feature: RZPFeatures.PP,
+})
+export default class PaymentPagesOnBoarding extends React.Component {
+  getNextBtnProp = sliderProps => () => {
+    return (
+      <FeatureEnableSliderButton
+        isLocalEnabler
+        feature={RZPFeatures.PP}
+        onClick={this.props.closeOnboarding}
+        page={sliderProps.active}
+      />
+    );
+  };
+
+  render() {
+    const { active, onSlideChange } = this.props;
+
+    return (
+      <OnBoardingWrapper class="PaymentPages">
+        <Slider active={active} onSlideChange={onSlideChange}>
+          {sliderProps => (
+            <Landing
+              {...sliderProps}
+              title="Payment Pages"
+              imageUrl="https://razorpay.com/assets/paymentpages/hero-main.svg"
+              desc="Create custom-branded, hosted Payment Pages in a few clicks to accept payments online. Your business can go online with zero integration and tech efforts."
+            />
+          )}
+
+          {sliderProps => (
+            <Features
+              {...sliderProps}
+              title="What makes Payment Pages great?"
+              nextBtn={this.getNextBtnProp(sliderProps)}
+              featureLinks={FEATURES_LINKS}
+              features={FEATURES_DATA}
+            />
+          )}
+
+          {sliderProps => (
+            <SliderDots {...sliderProps}>
+              <SkipAndGetStartedButton onClick={this.props.closeOnboarding} />
+            </SliderDots>
+          )}
+        </Slider>
+      </OnBoardingWrapper>
+    );
+  }
+}
+
+export function getIsAllowedPaymentPagesOnBoarding({
+  mode,
+  merchantId,
+  paymentPages,
+  loading,
+}) {
+  if (paymentPages.length || loading) {
+    return false;
+  }
+
+  return isAllowedResetBoarding({
+    mode,
+    merchantId,
+    feature: RZPFeatures.PP,
+  });
+}
+
+export function getIsPaymentPagesEnabled({ user, paymentPages, loading }) {
+  if (user.isPaymentPagesEnabled) {
+    return true;
+  }
+
+  if (paymentPages.length) {
+    setOnBoardingDataInLocalState({
+      feature: RZPFeatures.PP,
+      data: {
+        isEnabled: true,
+      },
+    });
+
+    return true;
+  }
+
+  if (loading) {
+    return true;
+  }
+
+  return false;
+}
