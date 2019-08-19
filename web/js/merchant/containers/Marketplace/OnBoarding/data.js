960c207deca4bb3788295580fba369d86aec4381
dvvavinash <dvv.avinash@gmail.com>
2019-08-19 21:35:54 +0530

    [Route Onboarding]: Update data OnBoarding

--- web/js/merchant/containers/Marketplace/OnBoarding/data.js
+++ web/js/merchant/containers/Marketplace/OnBoarding/data.js
@@ -1,19 +1,19 @@
 // Features
 export const FEATURES_DATA = [
   {
-    icon: '/dist/css/assets/feature_onboarding/2.svg',
+    icon: '/dist/css/assets/product_onboarding/2.svg',
     title: 'Linked Accounts',
     desc:
       'Easily onboard your vendors, sellers, service providers etc. as linked accounts without the hassles of any physical paperwork.',
   },
   {
-    icon: '/dist/css/assets/feature_onboarding/10.svg',
+    icon: '/dist/css/assets/product_onboarding/10.svg',
     title: 'Transfers',
     desc:
       'Create as many transfers as you need to various linked accounts and automate the process through powerful Transfer APIs.',
   },
   {
-    icon: '/dist/css/assets/feature_onboarding/8.svg',
+    icon: '/dist/css/assets/product_onboarding/8.svg',
     title: 'Settlements',
     desc: 'Control the entire fund movements with flexible settlement plans.',
   },
@@ -27,6 +27,6 @@ export const FEATURES_LINKS = [
   },
   {
     label: 'View API Docs',
-    url: 'https://razorpay.com/docs/route/',
+    url: 'https://razorpay.com/docs/api/route/',
   },
 ];
