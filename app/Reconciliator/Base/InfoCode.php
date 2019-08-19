bafbda96e46574037e77fe6552231aac91162c21
Vidya Sagar Kushwaha <vidyasagar.kushwaha@razorpay.com>
2019-08-19 17:04:57 +0530

    [recon] adds recon row skipped trace in combined Reconciliate (#12853)
    * [recon] adds recon row skipped trace in combined Reconciliate
    
    * [recon] uses array skippedRows

--- app/Reconciliator/Base/InfoCode.php
+++ app/Reconciliator/Base/InfoCode.php
@@ -126,6 +126,8 @@ class InfoCode
 
     const RECON_SKIP_INVALID_BLANK_FILE                 = 'RECON_SKIP_INVALID_BLANK_FILE';
 
+    const RECON_SKIPPED_ROWS                            = 'RECON_SKIPPED_ROWS';
+
     const CARD_TYPE_UNKNOWN                             = 'CARD_TYPE_UNKNOWN';
 
     const CARD_TYPE_MISMATCH                            = 'CARD_TYPE_MISMATCH';
