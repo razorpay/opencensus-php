ca72beb22326b2cd710a726c89c3cc41ea7d73b7
Aakash  Raina <aakashraina9@gmail.com>
2019-08-21 12:05:53 +0530

    Revert "Revert "Payment instant refunds""

---
+++ web/js/merchant/components/Refunds/RefundTimeline.js
@@ -0,0 +1,79 @@
+import Time from 'rzp/ui/Time';
+import { RefundStatusLabel } from 'merchant/components/StatusLabel';
+
+function ShowTime({ time }) {
+  return <Time value={time} format="DD MMM YYYY, hh:mm:ss a" />;
+}
+
+export default class RefundStatusTimeline extends React.Component {
+  getMilestones = refund => {
+    const mileStones = [];
+    mileStones.push({
+      status: 'processing',
+      mode: `(${refund.speed_processed} refund)`,
+      timeStamp: refund.created_at,
+    });
+
+    if (refund.speed_change_time) {
+      mileStones.push(
+        {
+          text: 'Refund mode updated to Normal',
+          timeStamp: refund.speed_change_time,
+        },
+        {
+          status: 'processing',
+          mode: `(${refund.speed_processed} refund)`,
+          timeStamp: refund.speed_change_time,
+        }
+      );
+    }
+
+    if (refund.public_status === 'processed') {
+      mileStones.push({
+        status: 'processed',
+        mode: `(${refund.speed_processed} refund)`,
+        timeStamp:
+          refund.speed_processed === 'normal'
+            ? refund.speed_change_time
+              ? refund.speed_change_time
+              : refund.created_at
+            : refund.processed_at,
+      });
+    }
+
+    return mileStones.reverse();
+  };
+
+  render() {
+    let mileStones = this.getMilestones(this.props.refund);
+
+    return (
+      <ul class="refund-timeline">
+        {mileStones.map((item, idx) => {
+          if (item.status) {
+            return (
+              <li>
+                <div class="refund-timeline-status">
+                  <RefundStatusLabel status={item.status} />
+                </div>
+                <p class="refund-timeline-mode">{item.mode}</p>
+                <p class="refund-timeline-timestamp">
+                  <ShowTime time={item.timeStamp} />
+                </p>
+              </li>
+            );
+          } else {
+            return (
+              <li>
+                <p class="refund-timeline-text">{item.text}</p>
+                <p class="refund-timeline-timestamp">
+                  <ShowTime time={item.timeStamp} />
+                </p>
+              </li>
+            );
+          }
+        })}
+      </ul>
+    );
+  }
+}
