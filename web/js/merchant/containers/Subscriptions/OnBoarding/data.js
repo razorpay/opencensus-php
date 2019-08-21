ac20812675e8aae9052fe2c1853b9808291fa5f3
dvvavinash <dvv.avinash@gmail.com>
2019-08-21 18:09:24 +0530

    [Subscriptions Onboarding]: Update image file names of Features slides

--- web/js/merchant/containers/Subscriptions/OnBoarding/data.js
+++ web/js/merchant/containers/Subscriptions/OnBoarding/data.js
@@ -1,19 +1,19 @@
 // Features
 export const FEATURES_DATA = [
   {
-    icon: '/dist/css/assets/product_onboarding/2.svg',
+    icon: '/dist/css/assets/product_onboarding/without_coding.svg',
     title: 'Subscription Links',
     desc:
       'Share unique links and onboard users for your Subscription packages instantly! No API integration necessary.',
   },
   {
-    icon: '/dist/css/assets/product_onboarding/3.svg',
+    icon: '/dist/css/assets/product_onboarding/partial_payments.svg',
     title: 'Multi-currency Support',
     desc:
       'Accept recurring payments from customers in India and abroad via Razorpay Subscriptions. 100 currencies including supported!',
   },
   {
-    icon: '/dist/css/assets/product_onboarding/5.svg',
+    icon: '/dist/css/assets/product_onboarding/alter_native_payment_option.svg',
     title: 'Multiple payment modes',
     desc: `Offer your customers a variety of payment methods. All payment modes offering recurring are compliant with RBI's regulations.`,
   },
