e1522a38fe1cc33658d867b52f8b4f6f672ad98a
dvvavinash <dvv.avinash@gmail.com>
2019-08-21 19:06:56 +0530

    [PaymentPages Onboarding]: Update image urls in Features Slide

--- web/js/merchant/containers/PaymentPages/OnBoarding/data.js
+++ web/js/merchant/containers/PaymentPages/OnBoarding/data.js
@@ -1,19 +1,19 @@
 // Features
 export const FEATURES_DATA = [
   {
-    icon: '/dist/css/assets/product_onboarding/2.svg',
+    icon: '/dist/css/assets/product_onboarding/without_coding.svg',
     title: 'No Coding Required',
     desc:
       'Your business can go online with zero integration and tech efforts. We build and operate for you.',
   },
   {
-    icon: '/dist/css/assets/product_onboarding/6.svg',
+    icon: '/dist/css/assets/product_onboarding/custom_brand.svg',
     title: 'Custom Branded Page',
     desc:
       'Customize the look and feel of your payment pages to reflect your brand colours, for seamless customer experience.',
   },
   {
-    icon: '/dist/css/assets/product_onboarding/7.svg',
+    icon: '/dist/css/assets/product_onboarding/easy_refunds.svg',
     title: 'Memorable URLS',
     desc:
       'Look professional with a custom URL that is as easy to remember and is an extension of your business.',
