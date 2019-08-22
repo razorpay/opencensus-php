9ff5fb48039173987d9b6c95a2e2c4df2df664eb
Badrimurali <badrinath.m@razorpay.com>
2019-08-22 16:22:44 +0530

    [recon] comment testing bucketconfig for testing

--- app/Models/FileStore/Storage/Base/Bucket.php
+++ app/Models/FileStore/Storage/Base/Bucket.php
@@ -5,6 +5,7 @@ namespace RZP\Models\FileStore\Storage\Base;
 use RZP\Constants\Mode;
 use RZP\Models\FileStore\Type;
 use RZP\Models\Merchant\Detail\Entity as MerchantDetail;
+use RZP\Trace\TraceCode;
 
 class Bucket
 {
@@ -28,15 +29,14 @@ class Bucket
             if (in_array($type, $types))
             {
                 $bucketConfigName = $bucketConfig;
-
                 break;
             }
         }
 
-        if (($env !== 'production') and ($env !== 'testing'))
-        {
-            $bucketConfigName = static::TEST_BUCKET_NAME;
-        }
+//        if (($env !== 'production') and ($env !== 'testing'))
+//        {
+//            $bucketConfigName = static::TEST_BUCKET_NAME;
+//        }
 
         return $bucketConfigName;
     }
