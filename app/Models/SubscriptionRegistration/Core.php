1f3ade60d08bd78ff5c03f96149f55b6a3b8cc19
Baljeet Kumar <baljeet.kumar@razorpay.com>
2019-08-26 11:19:37 +0530

    Fix recurring status issue and fetch invoice from repo (#12953)
    * fixed the recurring status issue and use this->repo for fetching invoice related to token.registration
    
    * [SubscriptionRegistration] fetching merchant from entity

--- app/Models/SubscriptionRegistration/Core.php
+++ app/Models/SubscriptionRegistration/Core.php
@@ -194,7 +194,7 @@ class Core extends Base\Core
                 $subr->setStatus(Status::COMPLETED);
             }
         }
-        else if ($token->getRecurringStatus() === Customer\Token\RecurringStatus::CONFIRMED)
+        else if ($token->getRecurringStatus() === Customer\Token\RecurringStatus::REJECTED)
         {
             $subr->setStatus(Status::COMPLETED);
         }
@@ -334,8 +334,8 @@ class Core extends Base\Core
     {
         $token = $tokenRegistration->token;
 
-        $invoice = (new Invoice\Repository)->findByMerchantAndTokenRegistration(
-            $this->merchant,
+        $invoice = $this->repo->invoice->findByMerchantAndTokenRegistration(
+            $tokenRegistration->merchant,
             $tokenRegistration
         );
 
