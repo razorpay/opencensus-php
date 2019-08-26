f49aeb9eb5f394b37266e7109fc4ef41483a8c92
dvvavinash <dvv.avinash@gmail.com>
2019-08-27 00:09:24 +0530

    [PaymentLinks Onboarding]: Update payment link docs url

--- web/js/merchant/containers/PaymentLinks/OnBoarding/data.js
+++ web/js/merchant/containers/PaymentLinks/OnBoarding/data.js
@@ -23,7 +23,7 @@ export const FEATURES_DATA = [
 export const FEATURES_LINKS = [
   {
     label: 'Know more',
-    url: 'https://razorpay.com/docs/payment-pages/',
+    url: 'https://razorpay.com/docs/payment-links/',
   },
   {
     label: 'View API Docs',
