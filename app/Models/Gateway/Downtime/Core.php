3e16518fde7897588fac31ebbf102e4b14a20904
Shivam Yuvraj <shivamyuvraj1@gmail.com>
2019-08-28 20:22:39 +0530

    modified fetch downtime query (#13027)

--- app/Models/Gateway/Downtime/Core.php
+++ app/Models/Gateway/Downtime/Core.php
@@ -358,6 +358,7 @@ class Core extends Base\Core
             Entity::GATEWAY => $gateways,
             Entity::PARTIAL => false,
             Entity::BEGIN   => $now,
+            Entity::END     => $now,
         ];
 
         switch ($payment->getMethod())
