9fe0026eb64b371f2c6aa87a0cbdcfe80e79fab3
dvvavinash <dvv.avinash@gmail.com>
2019-08-29 15:55:09 +0530

    [PaymentPages Onboarding]: Add Onboarding and QuickGuide

---
+++ web/js/merchant/containers/PaymentPages/OnBoarding/data.js
@@ -0,0 +1,29 @@
+// Features
+export const FEATURES_DATA = [
+  {
+    icon: '/dist/css/assets/product_onboarding/without_coding.svg',
+    title: 'No Coding Required',
+    desc:
+      'Your business can go online with zero integration and tech efforts. We build and operate for you.',
+  },
+  {
+    icon: '/dist/css/assets/product_onboarding/custom_brand.svg',
+    title: 'Custom Branded Page',
+    desc:
+      'Customize the look and feel of your payment pages to reflect your brand colours, for seamless customer experience.',
+  },
+  {
+    icon: '/dist/css/assets/product_onboarding/easy_refunds.svg',
+    title: 'Memorable URLS',
+    desc:
+      'Look professional with a custom URL that is as easy to remember and is an extension of your business.',
+  },
+];
+
+// Features Links
+export const FEATURES_LINKS = [
+  {
+    label: 'Know more',
+    url: 'https://razorpay.com/docs/payment-pages/',
+  },
+];
---
+++ web/js/merchant/containers/PaymentPages/QuickGuide/index.js
@@ -0,0 +1,132 @@
+import { connect } from 'react-redux';
+
+import { PossibleStatuses, RZPFeatures } from 'rzp/utils/constants';
+
+import QuickGuide, {
+  setQuickGuideIsClosedInLocalStorage,
+  getQuickGuideIsClosedFromLocalStorage,
+} from 'merchant/components/QuickGuide';
+import QuickStepGuide, {
+  QuickGuideStep,
+  QuickGuideTitle,
+  QuickGuideCloseBtn,
+} from 'merchant/components/QuickGuide/QuickStepGuide';
+
+import { getQuickGuideData } from './data';
+
+const { done, locked, active, loading } = PossibleStatuses;
+
+@connect(state => ({
+  user: state.session.user,
+  mode: state.session.mode,
+  invoices: state.invoices,
+}))
+@QuickGuide({
+  feature: RZPFeatures.PP,
+  data_points: ['paymentPages'],
+  dataTransformer: (key, state) => {
+    return {
+      ...state.invoices,
+      items: state.invoices.paymentPages,
+    };
+  },
+})
+export default class PaymentPagesQuickGuide extends React.Component {
+  getCloseBtn = isCompleted => {
+    return (
+      <QuickGuideCloseBtn
+        isCompleted={isCompleted}
+        onClick={this.props.onClickClose}
+      />
+    );
+  };
+
+  render() {
+    const { paymentPageStatus, paymentReceiveStatus } = getStatus(this.props);
+
+    const CloseBtn = this.getCloseBtn(paymentReceiveStatus === done);
+
+    let activeStep = 0;
+
+    if (paymentReceiveStatus === done) {
+      activeStep = 1;
+    }
+
+    return (
+      <QuickStepGuide
+        activeStep={activeStep}
+        class="PaymetPages"
+        title={Title}
+        closeBtn={CloseBtn}
+      >
+        <QuickGuideStep
+          status={paymentPageStatus}
+          step="PaymentPage"
+          feature={RZPFeatures.PP}
+          {...getQuickGuideData.PaymentPage(paymentPageStatus)}
+        />
+
+        <QuickGuideStep
+          status={paymentReceiveStatus}
+          step="PaymentReceive"
+          feature={RZPFeatures.PP}
+          {...getQuickGuideData.ReceivePayments(paymentReceiveStatus)}
+        />
+      </QuickStepGuide>
+    );
+  }
+}
+
+const Title = <QuickGuideTitle />;
+
+export const getPaymentPageQuickGuideIsClosed = props => {
+  let isClosed = getQuickGuideIsClosedFromLocalStorage(RZPFeatures.PP);
+
+  // Check if transfers non created state count is more then or equal to 2
+  if (isClosed || props.paymentPages.length <= 2) {
+    return isClosed;
+  }
+
+  props.paymentPages.forEach(page => {
+    if (page.times_paid) {
+      setQuickGuideIsClosedInLocalStorage(RZPFeatures.PP, true);
+
+      return false;
+    }
+  });
+
+  return true;
+};
+
+const getStatus = ({ paymentPages, invoices }) => {
+  let paymentPageStatus = loading,
+    paymentReceiveStatus = loading;
+
+  if (invoices.loading) {
+    return {
+      paymentPageStatus,
+      paymentReceiveStatus,
+    };
+  }
+
+  if (paymentPages.items.length) {
+    paymentPageStatus = done;
+    paymentReceiveStatus = active;
+
+    paymentPages.items.forEach(page => {
+      if (page.times_paid) {
+        paymentReceiveStatus = done;
+
+        return false;
+      }
+    });
+  } else {
+    paymentPageStatus = active;
+    paymentReceiveStatus = locked;
+  }
+
+  return {
+    paymentPageStatus,
+    paymentReceiveStatus,
+  };
+};
