cfb50e5ebe8accc6c547ef950a3d1142d62a91e0
aseemgupta <aseemgupta2625@gmail.com>
2019-08-30 20:16:51 +0530

    [PaymentpagesV3]: Fix amount field matching logic

--- web/js/merchant/containers/PaymentPages/Pages/Create/Form/Amount_Fields/V3.js
+++ web/js/merchant/containers/PaymentPages/Pages/Create/Form/Amount_Fields/V3.js
@@ -17,16 +17,20 @@ export function mapFieldToAmountFieldType(amountField) {
   let amountFieldType = null;
 
   // Fixed price
-  if (amountField.mandatory) {
-    amountFieldType = getAmountFieldTypes()[0]; // FIELD_TYPES.fixed_price,
-  } else if (amountField.hasOwnProperty('min_purchase')) {
+  if (
+    amountField.hasOwnProperty('min_purchase') &&
+    amountField.min_purchase !== null
+  ) {
     // Note: Assumed that other kind of fields shouldn't have min_purchase key, or else we'll have to check non-null values
     amountFieldType = getAmountFieldTypes()[3]; // FIELD_TYPES.multiple_purchase
   } else if (amountField.item.amount) {
     amountFieldType = getAmountFieldTypes()[1]; // FIELD_TYPES.fixed_price_optional
-  } else {
+  } else if (!amountField.item.amount) {
     // Note: All other fields are by default dynamic_price, or else can check for min_amount
     amountFieldType = getAmountFieldTypes()[2]; // FIELD_TYPES.dynamic_price
+  } else {
+    // Note: amountField.mandatory is not valid check for fixed_price field because dynamic_price and multiple_purchase items can also have mandatory = true
+    amountFieldType = getAmountFieldTypes()[0]; // FIELD_TYPES.fixed_price,
   }
 
   return amountFieldType && amountFieldType.key;
