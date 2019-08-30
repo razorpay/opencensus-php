e31ceb508adc18b56eec9a85b4c3ae4eb66977a2
Vikram Singh <vikram.singh@razorpay.com>
2019-08-30 18:33:34 +0530

    Adds utm parameters, read from analytics library

--- web/js/merchant/containers/App.js
+++ web/js/merchant/containers/App.js
@@ -53,7 +53,15 @@ import track from 'react-tracking';
 @track(
   ({ user, mode }) => {
     const u = { email: user.user.email, id: user.user.id, mid: user.current };
-    return window.rzpQ.component('Home', { user: u, mode: mode });
+    let utm = null;
+    if (undefined !== analytics) {
+      utm = analytics.utils.getLandingParams();
+    }
+    return window.rzpQ.component('Home', {
+      user: u,
+      utm_params: utm,
+      mode: mode,
+    });
   },
   {
     dispatch: data => {
