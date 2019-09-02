201c3b49dbe4ee92cdec4a43908f80bdd216c50d
ambar maini <ambar.maini@razorpay.com>
2019-09-02 23:05:22 +0530

    adds migration file

---
+++ database/migrations/2019_08_29_090706_create_banking_account_state.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+use RZP\Constants\Table;
+use RZP\Models\BankingAccount\State\Entity;
+
+class CreateBankingAccountState extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(Table::BANKING_ACCOUNT_STATE, function (Blueprint $table)
+        {
+            $table->char(Entity::ID, Entity::ID_LENGTH)
+                  ->primary();
+
+            $table->char(Entity::ADMIN_ID, Entity::ID_LENGTH)
+                  ->nullable();
+
+            $table->char(Entity::MERCHANT_ID, Entity::ID_LENGTH)
+                  ->nullable();
+
+            $table->char(Entity::BANKING_ACCOUNT_ID, Entity::ID_LENGTH);
+
+            $table->char(Entity::STATUS, 255);
+
+            $table->char(Entity::BANK_STATUS, 255)
+                  ->nullable();
+
+            $table->integer(Entity::CREATED_AT);
+
+            $table->integer(Entity::UPDATED_AT);
+
+            $table->index(Entity::STATUS);
+
+            $table->index(Entity::BANKING_ACCOUNT_ID);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
