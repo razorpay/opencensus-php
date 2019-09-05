9efd26315830de66e3f3103afe5f6b3d205eb737
aseemgupta <aseemgupta2625@gmail.com>
2019-09-05 16:07:00 +0530

    [PaymentpagesV3]: Add files for PaymentsList in V3 details view

---
+++ web/js/merchant/containers/PaymentPages/Pages/Entity/V3/PaymentsList/index.js
@@ -0,0 +1,52 @@
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { fetchPayments as fetchAll } from 'merchant/modules/collection';
+
+import ListContainer from 'merchant/containers/ListContainer';
+import PaymentsListFilter from './PaymentsListFilter';
+
+import {
+  paymentId,
+  amount,
+  customer,
+  createdAtShort,
+  status,
+} from 'rzp/ui/item/pair';
+
+import EntityTable from 'merchant/components/EntityTable';
+
+const PaymentsTable = props => {
+  let paymentColumns = [paymentId, amount, customer, createdAtShort, status];
+
+  return <EntityTable title="Payments" columns={paymentColumns} {...props} />;
+};
+
+@withRouter
+@connect(state => state.payments, { fetchAll })
+export default class PaymentsList extends ListContainer {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { children, ...restProps } = this.props;
+
+    return (
+      <div class="content-wrapper">
+        {children}
+
+        <PaymentsListFilter
+          form="paymentListFilter"
+          count={this.state.count}
+          onSubmit={this.search}
+        />
+        <PaymentsTable
+          count={this.state.count}
+          skip={this.state.skip}
+          paginate={this.paginate}
+          {...restProps}
+        />
+      </div>
+    );
+  }
+}
