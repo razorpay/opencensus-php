144523be96a14678ee6840389b830025ae55afce
Umesh Malhotra <umesh.malhotra@razorpay.com>
2019-09-08 15:47:07 +0530

    Fixes bug - only activated terminals should be enabled, fixes error c… (#13052)
    * Fixes bug - only activated terminals should be enabled, fixes error codes
    
    * Refactors- coding standards
    
    * Adds review suggestion changes
    
    * Adds review suggestion changes
    
    * Removes unused import
    
    * Adds new url

--- tests/Functional/Merchant/TerminalTest.php
+++ tests/Functional/Merchant/TerminalTest.php
@@ -13,9 +13,7 @@ use RZP\Tests\Functional\TestCase;
 use RZP\Tests\Functional\Helpers\Payment\PaymentTrait;
 use \RZP\Models\Terminal\Shared;
 use RZP\Exception;
-use RZP\Tests\Functional\Fixtures\Entity\Terminal as TerminalFixture;
 use RZP\Tests\Functional\Partner\PartnerTrait;
-use RZP\Models\Feature\Constants as FeatureConstants;
 
 class TerminalTest extends TestCase
 {
@@ -1238,127 +1236,5 @@ class TerminalTest extends TestCase
 
         // Adding below assert to check if the org is being associated to terminal (via merchant) properly
         $this->assertEquals('100000razorpay', $terminal['org_id']);
-    }
-
-    public function testEnableTerminal()
-    {
-        $subMerchantId = $this->setUpPartnerAuthAndGetSubMerchantId();
-
-        $this->fixtures->merchant->addFeatures(FeatureConstants::TERMINAL_ONBOARDING);
-
-        $terminal = $this->fixtures->create('terminal', [
-            'enabled' => false,
-            'merchant_id' => $subMerchantId,
-            'mc_mpan' => '1234567890123456',
-            'visa_mpan' => '9876543210123456',
-            'rupay_mpan' => '1234123412341234',
-            'notes'     => 'some notes'
-        ]);
-
-        $url = '/terminals/'.$terminal['id'].'/enable';
-
-        $this->testData[__FUNCTION__]['request']['url'] = $url;
-
-        $this->startTest();
-    }
-
-    public function testDisableTerminal()
-    {
-        $subMerchantId = $this->setUpPartnerAuthAndGetSubMerchantId();
-
-        $this->fixtures->merchant->addFeatures(FeatureConstants::TERMINAL_ONBOARDING);
-
-        $terminal = $this->fixtures->create('terminal', [
-            'enabled' => true,
-            'merchant_id' => $subMerchantId,
-            'mc_mpan' => '1234567890123456',
-            'visa_mpan' => '9876543210123456',
-            'rupay_mpan' => '1234123412341234',
-            'notes'     => 'some notes'
-        ]);
-
-        $url = '/terminals/'.$terminal['id'].'/disable';
-
-        $this->testData[__FUNCTION__]['request']['url'] = $url;
-
-        $this->startTest();
-    }
-
-    public function testSubMerchantsShouldNotBeAbleToDisableTerminals()
-    {
-        $this->ba->privateAuth();
-
-        $terminal = $this->fixtures->create('terminal', [
-            'enabled' => true,
-            'merchant_id' => '10000000000000',
-            'mc_mpan' => '1234567890123456',
-            'visa_mpan' => '9876543210123456',
-            'rupay_mpan' => '1234123412341234',
-            'notes'     => 'some notes'
-        ]);
-
-        $url = '/terminals/'.$terminal['id'].'/disable';
-
-        $this->testData[__FUNCTION__]['request']['url'] = $url;
-
-        $this->startTest();
-    }
-
-    public function testFetchTerminals()
-    {
-        $subMerchantId = $this->setUpPartnerAuthAndGetSubMerchantId();
-
-        $this->fixtures->merchant->addFeatures(FeatureConstants::TERMINAL_ONBOARDING);
-
-        $url = '/terminals';
-
-        $this->testData[__FUNCTION__]['request']['url'] = $url;
-
-        $terminal1 = (new TerminalFixture)->createBharatQrTerminal();
-
-        $terminal1['merchant_id'] = $subMerchantId;
-
-        $terminal1->save();
-
-        $terminal2 = (new TerminalFixture)->createBharatQrIsgTerminal();
-
-        $terminal2['merchant_id'] = $subMerchantId;
-
-        $terminal2->save();
-
-        $this->startTest();
-    }
-
-    public function testPartnerWithouTerminalControlFeatureShouldNotBeAbleToFetchTerminals()
-    {
-        $this->setUpPartnerAuthAndGetSubMerchantId();
-
-        $url = '/terminals';
-
-        $this->testData[__FUNCTION__]['request']['url'] = $url;
-
-        $this->startTest();
-    }
-
-    public function testSubMerchantsShouldNotBeAbleToFetchTerminals()
-    {
-        $this->ba->privateAuth();
-
-        $this->testData[__FUNCTION__]['request']['url'] = '/terminals';
-
-        $this->startTest();
-    }
-
-    public function testTerminalOnboardingCreateTerminal()
-    {
-        $subMerchantId = $this->setUpPartnerAuthAndGetSubMerchantId();
-
-        $this->fixtures->merchant->addFeatures(FeatureConstants::TERMINAL_ONBOARDING);
-
-        $url = '/terminals';
-
-        $this->testData[__FUNCTION__]['request']['url'] = $url;
-
-        $this->startTest();
     }
 }
