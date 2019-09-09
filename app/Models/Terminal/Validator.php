09ed998ce69317f258d7219f2c7d0ea0ca018121
Amogh <Amogh.Kokari@razorpay.com>
2019-09-09 17:49:47 +0530

    Paypal Auto-capture (#13126)
    * add auth capture mode validator to terminal

--- app/Models/Terminal/Validator.php
+++ app/Models/Terminal/Validator.php
@@ -1156,13 +1156,14 @@ class Validator extends Base\Validator
         $isNonCardNonMockGateway = ((Gateway::isMethodSupported(Payment\Method::CARD, $gateway)) and
                                     (in_array($gateway, $nonCardPurchaseExceptions, true)));
 
-        // Migs, Amex, OpenWallet, CardlessEmi terminals are always in auth-capture mode
+        // Migs, Amex, OpenWallet, CardlessEmi, PayPal terminals are always in auth-capture mode
         //
         $authCaptureOnly = [
             Gateway::AXIS_MIGS,
             Gateway::AMEX,
             Gateway::WALLET_OPENWALLET,
             Gateway::CARDLESS_EMI,
+            Gateway::WALLET_PAYPAL,
         ];
 
         $isAuthCaptureOnlyGateway = (in_array($gateway, $authCaptureOnly, true));
