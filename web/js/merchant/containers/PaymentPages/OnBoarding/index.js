637636c7a090f40768302ce595fed0e0bdded3f1
dvvavinash <dvv.avinash@gmail.com>
2019-09-09 19:17:06 +0530

    [Onboarding]: Update content for Invoice, PL, PP

--- web/js/merchant/containers/PaymentPages/OnBoarding/index.js
+++ web/js/merchant/containers/PaymentPages/OnBoarding/index.js
@@ -73,7 +73,7 @@ export default class PaymentPagesOnBoarding extends React.Component {
               title="Payment Pages"
               feature={RZPFeatures.PP}
               imageUrl="https://razorpay.com/assets/paymentpages/hero-main.svg"
-              desc="Create custom-branded, hosted Payment Pages in a few clicks to accept payments online. Your business can go online with zero integration and tech efforts."
+              desc="Build a custom, branded payment page for your business in under 10 minutes and start accepting international and domestic payments with zero integration and tech efforts."
             />
           )}
 
