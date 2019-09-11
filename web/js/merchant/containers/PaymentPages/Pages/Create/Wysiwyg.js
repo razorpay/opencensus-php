f595e38915eaa7fecf216f5426bf4a4a7946407e
aseemgupta <aseemgupta2625@gmail.com>
2019-09-11 18:20:09 +0530

    [TEMP]: Load  wysiwyg script from beta

--- web/js/merchant/containers/PaymentPages/Pages/Create/Wysiwyg.js
+++ web/js/merchant/containers/PaymentPages/Pages/Create/Wysiwyg.js
@@ -160,7 +160,8 @@ export default class PaymentPagesWysiwyg extends React.PureComponent {
       });
     };
 
-    script.src = 'https://cdn.razorpay.com/static/hosted/wysiwyg.js';
+    // script.src = 'https://cdn.razorpay.com/static/hosted/wysiwyg.js';
+    script.src = 'https://betacdn.razorpay.com/static/hosted/wysiwyg.js'; // TODO: Only for Beta testing
 
     document.head.appendChild(script);
 
