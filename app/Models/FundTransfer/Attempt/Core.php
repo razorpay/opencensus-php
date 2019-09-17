b47d678b9ea023b13261f07a41a19c93d67e7604
shivanshgaur <shivanshgaur19@gmail.com>
2019-09-18 00:46:14 +0530

    remove source_type

--- app/Models/FundTransfer/Attempt/Core.php
+++ app/Models/FundTransfer/Attempt/Core.php
@@ -717,10 +717,11 @@ class Core extends Base\Core
             );
 
             $slackData = [
-                'headLine'  => 'fta source processing failed',
-                'fta_id'    => $ftaData['fta_id'],
-                'status'    => $ftaData['fta_status'],
-                'source_id' => $ftaData['source_id'],
+                'headLine'    => 'fta source processing failed',
+                'fta_id'      => $ftaData['fta_id'],
+                'status'      => $ftaData['fta_status'],
+                'source_id'   => $ftaData['source_id'],
+                'error'       => $e->getMessage(),
             ];
 
             $alerts = new Alerts();
