580e2628fccfc96ea196ab85e3d811b58a0f00a7
dvvavinash <dvv.avinash@gmail.com>
2019-09-17 17:29:49 +0530

    Add hotjar tags for onboarding

--- web/js/merchant/components/QuickGuide/index.js
+++ web/js/merchant/components/QuickGuide/index.js
@@ -95,6 +95,12 @@ export default params => {
     }
 
     componentWillReceiveProps(nextProps) {
+      if (nextProps[DATA_POINTS[DATA_POINTS.length - 1]].items.length) {
+        if (typeof window.hj === 'function') {
+          window.hj('tagRecording', [`${FEATURE}_onboarding_completed`]);
+        }
+      }
+
       if (!this.props.currentOnboarding.isTour) return;
 
       let isLoading = false;
