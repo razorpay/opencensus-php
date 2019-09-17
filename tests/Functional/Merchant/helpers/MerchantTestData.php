b52b69a8c212973d60750e924c07c333b121eb7d
Shivam Yuvraj <shivamyuvraj1@gmail.com>
2019-09-17 13:47:41 +0530

    [PAY-1793] action api change for email event (#13281)

--- tests/Functional/Merchant/helpers/MerchantTestData.php
+++ tests/Functional/Merchant/helpers/MerchantTestData.php
@@ -3567,6 +3567,38 @@ return [
         ]
     ],
 
+    'testMerchantEditReceiptEmailEventCapture' => [
+        'request' => [
+            'content' => [
+                'action' => 'set_receipt_email_event_captured'
+            ],
+            'url' => '/merchants/%s/action',
+            'method' => 'PUT',
+        ],
+        'response' => [
+            'content' => [
+                'entity'    => 'merchant',
+                'receipt_email_trigger_event' => 'captured',
+            ],
+        ]
+    ],
+
+    'testMerchantEditReceiptEmailEventAuthorized' => [
+        'request' => [
+            'content' => [
+                'action' => 'set_receipt_email_event_authorized'
+            ],
+            'url' => '/merchants/%s/action',
+            'method' => 'PUT',
+        ],
+        'response' => [
+            'content' => [
+                'entity'    => 'merchant',
+                'receipt_email_trigger_event' => 'authorized',
+            ],
+        ]
+    ],
+
     'testMerchantArchiveWithNoMerchantDetails' => [
         'request' => [
             'content' => [
