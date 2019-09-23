b3225d27812aa88ed622359cb27fa6ef7b03d2be
Abhijit <abhijith.lal@razorpay.com>
2019-09-23 18:50:00 +0530

    [individual] - Update to Default Department/Volume for Business Type Changed in L1 form (#13342)

--- app/Models/Merchant/Detail/Core.php
+++ app/Models/Merchant/Detail/Core.php
@@ -186,6 +186,31 @@ class Core extends Base\Core
     }
 
     /**
+     * This function is used for Not Registered Onboarding flow where there is need to set Default Volume/Department
+     * in MerchantDetails table. The reason for doing so is if Business type is changed from Non registered
+     * to some other business type, then need to skip pre signup form from Dashboard login.
+     *
+     * @param Entity          $merchantDetails
+     * @param Merchant\Entity $merchant
+     *
+     * @throws \RZP\Exception\BadRequestValidationFailureException
+     */
+    public function updateToDefaultDepartmentVolumeIfApplicable(Entity $merchantDetails, Merchant\Entity $merchant)
+    {
+        if ($merchantDetails->isDirty((Entity::BUSINESS_TYPE)) === true)
+        {
+            $merchantBusinessType = BusinessType::getKeyFromIndex($merchant->merchantDetail[Entity::BUSINESS_TYPE]);
+
+            if (BusinessType::isUnregisteredBusiness($merchantBusinessType))
+            {
+                $merchantDetails->setAttribute(Entity::TRANSACTION_VOLUME, Department::getDefaultDepartment());
+
+                $merchantDetails->setAttribute(Entity::DEPARTMENT, TransactionVolume::getDefaultVolume());
+            }
+        }
+    }
+
+    /**
      * Saves the instant activation details and also instantly activates the merchant based on the business details.
      *
      * @param array           $input
