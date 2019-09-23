1bec03cb259594a26420bcfa7d1242fc7e294a34
guptaankit015 <ankit.gupta@razorpay.com>
2019-09-23 19:28:21 +0530

    fix

--- database/migrations/2014_03_21_204875_create_terminals.php
+++ database/migrations/2014_03_21_204875_create_terminals.php
@@ -162,7 +162,7 @@ class CreateTerminals extends Migration
             $table->tinyInteger(Terminal::EXPECTED)
                   ->default(0);
 
-            $table->string(Terminal::CURRENCY, 255)
+            $table->string(Terminal::CURRENCY, 512)
                   ->default(Terminal::DEFAULT_CURRENCY);
 
             $table->string(Terminal::NETWORK_CATEGORY)
