d51451963be3fa063d2ffdaf722a144a6ebdcbd0
guptaankit015 <ankit.gupta@razorpay.com>
2019-09-24 14:43:49 +0530

    array validations added

--- app/Models/Terminal/Validator.php
+++ app/Models/Terminal/Validator.php
@@ -57,7 +57,7 @@ class Validator extends Base\Validator
         Entity::EMI_SUBVENTION              => 'sometimes|in:customer,merchant',
         Entity::GATEWAY_ACQUIRER            => 'sometimes|string|max:30',
         Entity::NETWORK_CATEGORY            => 'required_if:netbanking,1|string|max:30',
-        Entity::CURRENCY                    => 'sometimes',
+        Entity::CURRENCY                    => 'sometimes|array|max:20',
         Entity::ACCOUNT_NUMBER              => 'sometimes|string|max:50',
         Entity::IFSC_CODE                   => 'sometimes|string|size:11',
         Entity::CARDLESS_EMI                => 'sometimes|boolean',
@@ -178,7 +178,7 @@ class Validator extends Base\Validator
         Entity::GATEWAY_RECON_PASSWORD     => 'sometimes|alpha_num',
         Entity::EMI_SUBVENTION             => 'sometimes|in:customer,merchant',
         Entity::TYPE                       => 'sometimes|array',
-        Entity::CURRENCY                   => 'sometimes',
+        Entity::CURRENCY                   => 'sometimes|array|max:1',
         Entity::CAPABILITY                 => 'sometimes|in:0,2',
     ];
 
@@ -188,7 +188,7 @@ class Validator extends Base\Validator
         Entity::GATEWAY_TERMINAL_ID        => 'required|string|max:8',
         Entity::TYPE                       => 'sometimes|array',
         Entity::INTERNATIONAL              => 'sometimes|boolean',
-        Entity::CURRENCY                   => 'sometimes',
+        Entity::CURRENCY                   => 'sometimes|array|max:1',
         Entity::MC_MPAN                    => 'required_if:type.bharat_qr,1|string|size:16',
         Entity::VISA_MPAN                  => 'required_if:type.bharat_qr,1|string|size:16',
         Entity::RUPAY_MPAN                 => 'required_if:type.bharat_qr,1|string|size:16',
@@ -254,7 +254,7 @@ class Validator extends Base\Validator
         Entity::EMI                        => 'sometimes|boolean',
         Entity::EMI_DURATION               => 'required_only_if:emi,1|integer|in:3,6,9,12',
         Entity::EMI_SUBVENTION             => 'sometimes|in:customer,merchant',
-        Entity::CURRENCY                   => 'sometimes',
+        Entity::CURRENCY                   => 'sometimes|array|max:1',
     ];
 
     protected static $amexTerminalRules = [
@@ -500,7 +500,7 @@ class Validator extends Base\Validator
         Entity::GATEWAY_TERMINAL_PASSWORD2              => 'required|string',
         Entity::GATEWAY_MERCHANT_ID                     => 'required|string',
         Entity::GATEWAY_TERMINAL_PASSWORD               => 'required|string',
-        Entity::CURRENCY                                => 'sometimes',
+        Entity::CURRENCY                                => 'sometimes|array',
         Entity::INTERNATIONAL                           => 'sometimes|boolean',
         Entity::MODE                                    => 'sometimes',
         Entity::TYPE                                    => 'required|array',
@@ -512,7 +512,7 @@ class Validator extends Base\Validator
         Entity::GATEWAY_TERMINAL_PASSWORD2              => 'sometimes|string',
         Entity::GATEWAY_MERCHANT_ID                     => 'sometimes|string',
         Entity::GATEWAY_TERMINAL_PASSWORD               => 'sometimes|string',
-        Entity::CURRENCY                                => 'sometimes',
+        Entity::CURRENCY                                => 'sometimes|array',
         Entity::INTERNATIONAL                           => 'sometimes|boolean',
         Entity::MODE                                    => 'sometimes',
         Entity::TYPE                                    => 'sometimes|array',
