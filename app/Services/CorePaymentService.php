482a016c7bcd60946d0b72ae6eee3ba508098460
Sujata Yadav <sujata@razorpay.com>
2019-09-25 19:05:23 +0530

    Upi expiry time to float (#13444)

--- app/Services/CorePaymentService.php
+++ app/Services/CorePaymentService.php
@@ -100,6 +100,13 @@ class CorePaymentService
             $input[self::GATEWAY]['features']['tpv'] = $input[Entity::MERCHANT]->isTPVRequired();
         }
 
+        if (empty($input[Entity::UPI]) === false &&
+            empty($input['upi']['expiry_time']) === false)
+        {
+            $input['upi']['expiry_time'] = (float)$input['upi']['expiry_time'];
+        }
+
+
         $content = [
             self::ACTION  => $action,
             self::GATEWAY => $gateway,
