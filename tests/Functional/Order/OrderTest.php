838dd4c40d30039c1af870db0e648cfedce176ee
Baljeet Kumar <baljeet.kumar@razorpay.com>
2019-09-25 12:57:32 +0530

    Use max token amount from order (#13407)
    * Get max amount from tokenRegistration
    
    * use tokenregistration max amount as default value
    
    * added payment create test case for validating token max amount
    
    * add token max_amount test to orders tests
    
    * fix typo in test cases
    
    * use order->id instead of getPublicId()
    
    * use signed order id in payment request
    
    * passed order id as is
    
    * added payment_capture = 1 for emandate payment
    
    * set payment_capture true in the order creation of the test case
    
    * add mock for card vault and add emandate feature
    
    * fix the max_Amount check in token

--- tests/Functional/Order/OrderTest.php
+++ tests/Functional/Order/OrderTest.php
@@ -243,6 +243,35 @@ class OrderTest extends TestCase
         return $order;
     }
 
+    public function testEmandateRegistrationOrderWithTokenMaxAmount()
+    {
+        $this->mockCardVault();
+        $this->fixtures->create('terminal:shared_emandate_axis_terminal');
+        $this->fixtures->merchant->addFeatures(['charge_at_will']);
+
+        $this->fixtures->merchant->enableEmandate();
+
+        $order = $this->startTest();
+
+        $payment = $this->getEmandateNetbankingRecurringPaymentArray('UTIB', 0);
+
+        $payment['order_id'] = $order['id'];
+
+        $payment['bank_account'] = [
+            'account_number'    => '123123123',
+            'name'              => 'test name',
+            'ifsc'              => 'UTIB0002766'
+        ];
+
+        $this->doAuthPayment($payment);
+
+        $paymentEntity = $this->getLastEntity('payment', true);
+
+        $token = $this->getEntityById('token', $paymentEntity['token_id'], true);
+
+        $this->assertEquals(2500, $token['max_amount']);
+    }
+
     public function testEmandateRegistrationOrderWithZeroRupeeAndTokenWithFirstAmount()
     {
         $order = $this->startTest();
