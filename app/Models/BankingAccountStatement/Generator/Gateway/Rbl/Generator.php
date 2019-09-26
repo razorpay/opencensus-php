ea342fc41fc19474bbcfcf010e4cdb215bfc1be7
Anubhav Shrivastava <anubhav.shrivastava@razorpay.com>
2019-09-26 17:16:06 +0530

    updating query for fetching account statements

--- app/Models/BankingAccountStatement/Generator/Gateway/Rbl/Generator.php
+++ app/Models/BankingAccountStatement/Generator/Gateway/Rbl/Generator.php
@@ -37,9 +37,9 @@ abstract class Generator extends Base
 
         $allBankAccountTransactions = $this->repo
                                            ->statement
-                                           ->fetch(['balance_id' => $bankingAccount->getBalanceId()],
-                                                    $bankingAccount->getMerchantId())
-                                           ->sortBy('created_at');
+                                           ->getStatementsWithInRange($bankingAccount->getMerchantId(),
+                                                                      $this->fromDate,
+                                                                      $this->toDate);
 
         list($statementSummary, $transactions) = $this->getAccountSummaryAndTransactions($allBankAccountTransactions);
 
