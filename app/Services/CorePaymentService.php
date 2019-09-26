3edfb2397d0c2e219a1bb16e5a86b9707a0aaf59
Shivam Yuvraj <shivamyuvraj1@gmail.com>
2019-09-26 16:52:27 +0530

    unset pii data before logging (#13467)

--- app/Services/CorePaymentService.php
+++ app/Services/CorePaymentService.php
@@ -9,6 +9,7 @@ use RZP\Exception;
 use RZP\Models\Payment;
 use RZP\Constants\Entity;
 use RZP\Models\Terminal;
+use RZP\Models\Merchant;
 use RZP\Trace\TraceCode;
 use RZP\Error\ErrorCode;
 use RZP\Error\ErrorClass;
@@ -260,6 +261,11 @@ class CorePaymentService
         unset($request['content'][self::INPUT][Entity::TERMINAL][Terminal\Entity::GATEWAY_TERMINAL_PASSWORD2]);
         unset($request['content'][self::INPUT][Entity::TERMINAL][Terminal\Entity::GATEWAY_SECURE_SECRET]);
         unset($request['content'][self::INPUT][Entity::TERMINAL][Terminal\Entity::GATEWAY_SECURE_SECRET2]);
+        unset($request['content'][self::INPUT][Entity::MERCHANT][Merchant\Entity::NAME]);
+        unset($request['content'][self::INPUT][Entity::MERCHANT][Merchant\Entity::EMAIL]);
+        unset($request['content'][self::INPUT][Entity::MERCHANT][Merchant\Entity::WEBSITE]);
+        unset($request['content'][self::INPUT][Entity::MERCHANT][Merchant\Entity::DISPLAY_NAME]);
+        unset($request['content'][self::INPUT][Entity::MERCHANT][Merchant\Entity::MERCHANT_DETAIL]);
 
         $this->trace->info(TraceCode::CORE_PAYMENT_SERVICE_REQUEST, $request);
     }
