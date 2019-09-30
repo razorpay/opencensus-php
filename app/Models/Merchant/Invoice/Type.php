fd2ab4acfcae6288557a25c09793e3b882943887
neerajkanuri <neeraj.kanuri@razorpay.com>
2019-09-30 21:09:43 +0530

    [instant_refunds] : handling postpaid transactions (#13425)
    * [instant_refunds] : handling postpaid transactions
    
    * handling refund credits
    
    * Handling fee reversal for postpaid models
    
    * fix invoices and tests
    
    * Skipping fee deduction from refund credits
    
    * balance check and handling refund credits
    
    * adding assertion for balance check
    
    * [merchant_invoice] : adding instant refund test

--- app/Models/Merchant/Invoice/Type.php
+++ app/Models/Merchant/Invoice/Type.php
@@ -16,14 +16,8 @@ class Type
     // Commission on All Validations
     const VALIDATION            = 'validation';
 
-    // Fee on Instant Refund <= INR 1,000
-    const REFUND_LTE_1K         = 'refund_lte_1k';
-
-    // Fee on Instant Refund > INR 1,000 & <= INR 10,000
-    const REFUND_GT_1K_LTE_10K  = 'refund_gt_1k_lte_10k';
-
-    // Fee on Instant Refund > INR 10,000
-    const REFUND_GT_10K         = 'refund_gt_10k';
+    // Fee on Instant Refunds
+    const INSTANT_REFUNDS       = 'instant_refunds';
 
     // This is kept to support older invoice
     const NON_CARD              = 'non_card';
@@ -40,10 +34,8 @@ class Type
         self::ADJUSTMENT    => 997158,
         self::VALIDATION    => 997158,
         // This is kept to support older invoice
-        self::NON_CARD      => 997158,
-        self::REFUND_LTE_1K         => 997158,
-        self::REFUND_GT_1K_LTE_10K  => 997158,
-        self::REFUND_GT_10K         => 997158,
+        self::NON_CARD        => 997158,
+        self::INSTANT_REFUNDS => 997158,
     ];
 
     const DEFAULT_DESCRIPTION = 'Commission';
@@ -52,9 +44,7 @@ class Type
     const CARD_GT_2K_DESCRIPTION            = 'Commission on Card Payments > INR 2,000';
     const OTHERS_DESCRIPTION                = 'Commission on All Methods Except Cards';
     const VALIDATION_DESCRIPTION            = 'Commission on All Validations';
-    const REFUND_LTE_1K_DESCRIPTION         = 'Fee on Instant Refund <= INR 1,000';
-    const REFUND_GT_1K_LTE_10K_DESCRIPTION  = 'Fee on Instant Refund > INR 1,000 & <= INR 10,000';
-    const REFUND_GT_10K_DESCRIPTION         = 'Fee on Instant Refund > INR 10,000';
+    const INSTANT_REFUNDS_DESCRIPTION       = 'Fee on Instant Refunds';
 
     protected static $typeToDescriptionMap = [
         self::CARD_LTE_2K   => self::CARD_LTE_2K_DESCRIPTION,
@@ -62,10 +52,8 @@ class Type
         self::OTHERS        => self::OTHERS_DESCRIPTION,
         self::VALIDATION    => self::VALIDATION_DESCRIPTION,
         // This is kept to support older invoice
-        self::NON_CARD              => self::OTHERS_DESCRIPTION,
-        self::REFUND_LTE_1K         => self::REFUND_LTE_1K_DESCRIPTION,
-        self::REFUND_GT_1K_LTE_10K  => self::REFUND_GT_1K_LTE_10K_DESCRIPTION,
-        self::REFUND_GT_10K         => self::REFUND_GT_10K_DESCRIPTION,
+        self::NON_CARD        => self::OTHERS_DESCRIPTION,
+        self::INSTANT_REFUNDS => self::INSTANT_REFUNDS_DESCRIPTION,
     ];
 
     public static function getAllTypes(): array
@@ -73,9 +61,7 @@ class Type
         return [
             self::CARD_LTE_2K,
             self::CARD_GT_2K,
-            self::REFUND_LTE_1K,
-            self::REFUND_GT_1K_LTE_10K,
-            self::REFUND_GT_10K,
+            self::INSTANT_REFUNDS,
             self::OTHERS,
             self::VALIDATION,
         ];
