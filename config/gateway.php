bbe95574cea3855842b7048261c7d733c1a6e502
abhinavv21 <31855138+abhinavv21@users.noreply.github.com>
2019-10-03 18:22:34 +0530

    [gateway] Enable force-auth for Sbi nb and add cred fetch changes (#13534)
    * [payment gateway] Enable force auth for sbi nb
    
    * [gateway] Implement force auth
    
    * [gateway] Move creds from terminal to credstash

--- config/gateway.php
+++ config/gateway.php
@@ -764,6 +764,7 @@ return [
         'test_merchant_id_recurring'  => env('NETBANKING_SBI_TEST_MERCHANT_ID_RECURRING'),
         'test_hash_secret'            => env('NETBANKING_SBI_TEST_HASH_SECRET'),
         'test_hash_secret_recurring'  => env('NETBANKING_SBI_TEST_HASH_SECRET_RECURRING'),
+        'live_hash_secret'            => env('NETBANKING_SBI_LIVE_HASH_SECRET'),
         'iv'                          => env('NETBANKING_SBI_IV'),
         'emandate_corporate_id'       => env('EMANDATE_SBI_CORPORATE_ID'),
     ],
