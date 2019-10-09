aacb16e00ef55227f3cc4f6096dfd72ab583c784
Devansh <devansh.dwivedi@razorpay.com>
2019-10-09 09:48:52 +0530

    Update Desktop.js

--- web/js/merchant/containers/Home/Desktop.js
+++ web/js/merchant/containers/Home/Desktop.js
@@ -18,7 +18,7 @@ import RecentActivity from 'merchant/containers/Home/RecentActivity';
 import GenericPanel, { PanelBody } from 'merchant/components/Home/GenericPanel';
 import Announcement from 'merchant/components/Announcements/Instant';
 import CapitalAnnouncement from 'merchant/components/Announcements/Capital';
-import EarlyScheduledAnnouncement from 'merchant/components/Announcements/ScheduledSettlements';
+//import EarlyScheduledAnnouncement from 'merchant/components/Announcements/ScheduledSettlements';
 import PersonaliseBanner from 'merchant/components/Announcements/PersonaliseAccount';
 import Button from 'component/Button';
 import OndemandModal from 'merchant/containers/Settlements/OndemandModal';
