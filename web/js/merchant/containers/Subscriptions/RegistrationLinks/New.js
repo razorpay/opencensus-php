1ecb3dd6c46f2c823774cb63798b7e124abb1221
dvvavinash <dvv.avinash@gmail.com>
2019-10-10 15:38:34 +0530

    [NACH]: Fix for [ME-3156]  - Add auth_type as manual for request

--- web/js/merchant/containers/Subscriptions/RegistrationLinks/New.js
+++ web/js/merchant/containers/Subscriptions/RegistrationLinks/New.js
@@ -263,6 +263,7 @@ export default class CreateNewRegistrationLinkContainer extends React.Component 
 
     if (this.isNACHPayment) {
       payload.subscription_registration.bank_account = bankAccountDetails;
+      payload.subscription_registration.auth_type = 'manual';
     }
 
     if (this.isEmandatePayment || this.isNACHPayment) {
