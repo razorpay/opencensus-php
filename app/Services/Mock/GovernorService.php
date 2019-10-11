b0dd895a62e7768e50a20f14e9ad1b1aebbb9d60
parryd <paridhi.yadav@razorpay.com>
2019-10-11 16:41:39 +0530

    fix

--- app/Services/Mock/GovernorService.php
+++ app/Services/Mock/GovernorService.php
@@ -16,7 +16,7 @@ class GovernorService extends BaseGovernorService
             'response_code' => 400,
         ];
     }
-    public function sendRequestV1(string $method, string $path, string $content, array $headers)
+    public function sendRequestV1(string $method, string $path, string $content)
     {
         return [
             [
