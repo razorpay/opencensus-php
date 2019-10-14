a6a628d4e89bfd44f382cd30d1d7546f30c40e6e
Prashanth Y.V <prashanth.yv@razorpay.com>
2019-10-14 14:13:53 +0530

    minor code improvements

--- app/Models/Payout/Processor/DownstreamProcessor/DownstreamProcessor.php
+++ app/Models/Payout/Processor/DownstreamProcessor/DownstreamProcessor.php
@@ -62,7 +62,17 @@ class DownstreamProcessor
         return $this->payout->balance->getAccountType() ?? AccountType::SHARED;
     }
 
-    protected function getChannelForFundTransfer()
+    /**
+     * TODO: Currently there is no proper way to decide the channel through
+     * which the payout should be routed in case of shared accounts.
+     * Till the time we achieve this by Dynamic routing, we are doing
+     * a hack of using config key to store the MIDs for which
+     * channel for processing the payout should be CITI and ICICI.
+     * The precedence between ICICI and CITI is ICICI.
+     *
+     * @return string
+     */
+    protected function getChannelForFundTransfer(): string
     {
         $merchant = $this->payout->merchant;
 
@@ -79,21 +89,6 @@ class DownstreamProcessor
         return $this->payout->balance->getChannel() ?? Channel::YESBANK;
     }
 
-    // ToDo: Currently there is no proper way to decide the channel through
-    // which the payout should be routed in case of shared accounts.
-    // Till the time we achieve this by Dynamic routing, we are doing
-    // a hack of using config key to store the Mids for which
-    // channel for processing the payout should be CITI and ICICI.
-    // The precendence between ICICI and CITI is ICICI.
-    protected function checkIfChannelShouldBeCiti(Merchant $merchant): bool
-    {
-        $mid = $merchant->getId();
-
-        $citiMids = (new AdminService())->getConfigKey(['key' => ConfigKey::CITI_CHANNEL_PAYOUT_MIDS]);
-
-        return (in_array($mid, $citiMids, true));
-    }
-
     protected function checkIfChannelShouldBeIcici(Merchant $merchant): bool
     {
         $mid = $merchant->getId();
@@ -101,5 +96,14 @@ class DownstreamProcessor
         $iciciMids = (new AdminService())->getConfigKey(['key' => ConfigKey::ICICI_CHANNEL_PAYOUT_MIDS]);
 
         return (in_array($mid, $iciciMids, true));
+    }
+
+    protected function checkIfChannelShouldBeCiti(Merchant $merchant): bool
+    {
+        $mid = $merchant->getId();
+
+        $citiMids = (new AdminService())->getConfigKey(['key' => ConfigKey::CITI_CHANNEL_PAYOUT_MIDS]);
+
+        return (in_array($mid, $citiMids, true));
     }
 }
