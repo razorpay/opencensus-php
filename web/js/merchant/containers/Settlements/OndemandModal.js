ab49844141511123f7a6672eac03efdf79f3d91f
genu <genu.sheela@gmail.com>
2019-10-14 19:16:46 +0530

    changes closeOrigin

--- web/js/merchant/containers/Settlements/OndemandModal.js
+++ web/js/merchant/containers/Settlements/OndemandModal.js
@@ -600,7 +600,7 @@ export default class OndemandModal extends Component {
               </div>
             )
           ) : (
-            <ModalCloseReasons closeOrigin="Early Settlement" />
+            <ModalCloseReasons closeOrigin="OnDemand" />
           )}
         </React.Fragment>
       </div>
