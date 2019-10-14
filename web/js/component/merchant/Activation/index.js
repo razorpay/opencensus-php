f54aa1683fa78d4abf7036bca311d74706cc5fb0
Zameer Haque <zameer.haque@razorpay.com>
2019-10-14 16:31:51 +0530

    Fix files deleted due to rebase

--- web/js/component/merchant/Activation/index.js
+++ web/js/component/merchant/Activation/index.js
@@ -761,11 +761,8 @@ export default class ActivationWizard extends React.Component {
       submitted,
       international,
       poi_verification_status,
-<<<<<<< HEAD
       promoter_pan_name,
       promoter_pan,
-=======
->>>>>>> Fix files deleted due to rebase
     } = data;
 
     // Updating % activation_progress (side bar) and other important activation fields
@@ -777,11 +774,8 @@ export default class ActivationWizard extends React.Component {
       activation_flow,
       international,
       poi_verification_status,
-<<<<<<< HEAD
       promoter_pan,
       promoter_pan_name,
-=======
->>>>>>> Fix files deleted due to rebase
       submitted: +submitted,
     }));
 
