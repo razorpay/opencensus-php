7cbc53fbf3b677c963729ba7b70963df49dbc1a5
Saimadhav Heblikar <sahutd@users.noreply.github.com>
2019-10-16 20:48:21 +0530

    [adhoc] dont throw server error if optional postal_code is not sent (#13678)
    * dont throw server error if optional postal_code is not sent
    
    * update test
    
    * 1) changed validator 2) added cast
    
    * removed zipcode string validator

--- app/Models/Address/Validator.php
+++ app/Models/Address/Validator.php
@@ -22,10 +22,10 @@ class Validator extends Base\Validator
 
     protected static $createForPaymentRules = [
         Entity::TYPE    => 'required|custom',
-        Entity::LINE1   => 'required|string|between:10,255',
+        Entity::LINE1   => 'required|string|between:1,255',
         Entity::LINE2   => 'sometimes|string|between:5,255',
         Entity::CITY    => 'required|string|between:2,32',
-        Entity::ZIPCODE => 'sometimes|string|between:2,10',
+        Entity::ZIPCODE => 'sometimes|between:2,10',
         Entity::STATE   => 'sometimes|string|between:2,32',
         Entity::COUNTRY => 'required|string|between:2,64|custom',
         Entity::PRIMARY => 'sometimes|in:0,1',
