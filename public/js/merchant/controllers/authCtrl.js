11279c982a0c9db1a966bce7e9a6683f5f59e1eb
Vikram <vikram.singh@razorpay.com>
2019-10-18 19:34:22 +0530

    Removed debugger statement

--- public/js/merchant/controllers/authCtrl.js
+++ public/js/merchant/controllers/authCtrl.js
@@ -91,7 +91,6 @@ app
       // signup state container
       var email = $location.search().email;
       var role = $location.search().r;
-      debugger;
       try {
         email = atob(decodeURIComponent(email));
       } catch (e) {
@@ -190,7 +189,7 @@ app
         // disable signup submission before captcha in prod
         submissionDisabled: $location.host() === 'dashboard.razorpay.com',
       };
-
+      console.log($scope.signup);
       // wait for recaptcha response
       $scope.$watch('signup.data.captcha', function(newVal) {
         if (newVal && newVal.length !== 0) {
