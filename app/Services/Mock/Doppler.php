fa9d8d7beac64d8140bcbc143214f9451460b049
parryd <paridhi.yadav@razorpay.com>
2019-10-23 12:53:38 +0530

    fix

--- app/Services/Mock/Doppler.php
+++ app/Services/Mock/Doppler.php
@@ -7,7 +7,7 @@ use RZP\Services\Doppler as BaseDoppler;
 
 class Doppler extends BaseDoppler
 {
-    public function sendFeedback(Payment\Entity $payment, string $paymentStatus, string $errorCode, string $internalErrorCode)
+    public function sendFeedback(Payment\Entity $payment, string $paymentStatus, string $errorCode = null, string $internalErrorCode = null)
     {
 
     }
