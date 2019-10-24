575d0314f1389e7686c8c402508caa35eca7d6cd
Akshay Goyal <akshay.goyal@razorpay.com>
2019-10-24 14:10:27 +0530

    Fix FactoryData for banking_Account_details

--- tests/Functional/Fixtures/Factory/FactoryData.php
+++ tests/Functional/Fixtures/Factory/FactoryData.php
@@ -1139,6 +1139,7 @@ final class FactoryData
         $factory(\RZP\Models\BankingAccount\Detail\Entity::class, [
             'id'                 => 1,
             'banking_account_id' => '01234567890123',
+            'merchant_id'        => '10000000000000',
             'gateway_key'        => 'client_id',
             'gateway_value'      => '123',
             'updated_at'         => $faker->timestamp,
