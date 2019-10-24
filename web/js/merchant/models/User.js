9d5699812510a0d559ad33427d507ac9a31ca16f
dvvavinash <dvv.avinash@gmail.com>
2019-10-24 17:28:22 +0530

    Put payment link batch upload under razor x for sellar_app

--- web/js/merchant/models/User.js
+++ web/js/merchant/models/User.js
@@ -400,6 +400,10 @@ export default class User {
       : this.isAllowedEdit('team');
   }
 
+  get isPaymentLinkBatchEnabledForSellerAppRole() {
+    return this.getExpStatus('sellerapp_PL_batch_upload');
+  }
+
   // No experiment of disable-edit-<moduleName> => Module is not restricted
   isViewRestrictedByRazorX(moduleName) {
     // Eg: disable-view-reports (if corresponding experiment is "on", it can't be viewed by those merchants)
