ff5aa23bbb5f5b023e8379a60897657fbde775bd
Rohit Keshwani <rohitkeshwani07@users.noreply.github.com>
2019-10-24 13:15:29 +0530

    Update tests/Functional/Payment/PaymentCreateTest.php
    Co-Authored-By: krantideep95 <kranti.deep@razorpay.com>

--- tests/Functional/Payment/PaymentCreateTest.php
+++ tests/Functional/Payment/PaymentCreateTest.php
@@ -1462,7 +1462,7 @@ class PaymentCreateTest extends TestCase
         $gateway->shouldReceive('call')
             ->with(Mockery::type('string'), Mockery::type('string'), Mockery::type('array'),
                 Mockery::type('string'), Mockery::type('RZP\Models\Terminal\Entity'))->andReturnUsing
-            (function ($gateway,$action,$input,$mode)
+            (function ($gateway, $action, $input, $mode)
             {
                 throw new Exception\BadRequestException(ErrorCode::BAD_REQUEST_PAYMENT_BLOCKED_DUE_TO_FRAUD);
             });
