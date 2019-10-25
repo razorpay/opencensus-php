864abfee52a339b307b326b54acee15e9947430a
Chintan Acharya <chintan.acharya@razorpay.com>
2019-10-25 15:38:19 +0530

    Add auth selection section

--- resources/views/emandate/form.blade.php
+++ resources/views/emandate/form.blade.php
@@ -115,11 +115,7 @@
         padding: 0px 12px;
       }
 
-      #section1 {
-        position: relative;
-      }
-
-      #section2 {
+      #section1, #section2, #section3 {
         position: relative;
       }
 
@@ -475,10 +471,11 @@
           @include('emandate.commonFields')
         </div>
       </main>
-      <main class="hidden">
+      <main>
         <div class="heading">Please select Authentication method:</div>
         <div class="accordion-container">
-          <div id="section1" class={{ in_array('netbanking', $data['request']['content']['bank_details']['auth_types']) ? '' : 'disabled'}}>
+        @if (in_array('netbanking', $data['request']['content']['bank_details']['auth_types']))
+          <div id="section1">
             <label class="accordion-heading pickable" for="content1">
               <svg width="24px" height="17px" viewBox="0 0 24 17" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"> <title>Rectangle 88</title> <desc>Created with Sketch.</desc> <defs> <linearGradient x1="0%" y1="0%" x2="100%" y2="100%" id="linearGradient-1"> <stop stop-color="#EA3A44" offset="0%"></stop> <stop stop-color="#B70611" offset="100%"></stop> </linearGradient> </defs> <g id="Flow-1--Testing" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Both-HDFC--Aadhar" transform="translate(-28.000000, -502.000000)" fill="url(#linearGradient-1)"> <path d="M28,518.26087 L52,518.26087 L52,519 L28,519 L28,518.26087 Z M29.4117647,516.782609 L50.5882353,516.782609 L50.5882353,517.521739 L29.4117647,517.521739 L29.4117647,516.782609 Z M32.5351235,509.391304 L34.0468314,509.391304 L34.0468314,515.304348 L32.5351235,515.304348 L32.5351235,509.391304 Z M37.0702471,509.391304 L38.5819549,509.391304 L38.5819549,515.304348 L37.0702471,515.304348 L37.0702471,509.391304 Z M41.6053706,509.391304 L43.1170784,509.391304 L43.1170784,515.304348 L41.6053706,515.304348 L41.6053706,509.391304 Z M46.1404941,509.391304 L47.652202,509.391304 L47.652202,515.304348 L46.1404941,515.304348 L46.1404941,509.391304 Z M29.4117647,507.173913 L50.5882353,507.173913 L50.5882353,507.913043 L29.4117647,507.913043 L29.4117647,507.173913 Z M29.4117647,505.464674 L40,502 L50.5882353,505.464674 L50.5882353,506.157609 L29.4117647,506.157609 L29.4117647,505.464674 Z" id="Rectangle-88"></path> </g> </g> </svg>
               <span class="title">
@@ -487,13 +484,12 @@
               </span>
             </label>
             <input type="radio" id="content1" name="auth_type" value="netbanking" hidden checked>
-            @if (in_array('netbanking', $data['request']['content']['bank_details']['auth_types']))
-                <span class="arrow"></span>
-            @endif
+            <span class="arrow"></span>
           </div>
-
-          <div class="separate"></div>
-          <div id="section2" class={{ in_array('aadhaar', $data['request']['content']['bank_details']['auth_types']) ? '' : 'disabled'}}>
+        @endif
+        @if (in_array('aadhaar', $data['request']['content']['bank_details']['auth_types']))
+          <div class="separate hidden"></div>
+          <div id="section2" class="hidden">
             <label class="accordion-heading pickable" for="content2">
               <svg width="24px" height="16px" viewBox="0 0 24 16" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"> <title>Group 11</title> <desc>Created with Sketch.</desc> <defs> <linearGradient x1="0%" y1="54.6514554%" x2="103.027754%" y2="39.6782836%" id="linearGradient-1"> <stop stop-color="#F53742" offset="0%"></stop> <stop stop-color="#CB0D1A" offset="98.6427774%"></stop> </linearGradient> <linearGradient x1="0%" y1="0%" x2="100%" y2="100%" id="linearGradient-2"> <stop stop-color="#EA3A44" offset="0%"></stop> <stop stop-color="#B70611" offset="100%"></stop> </linearGradient> </defs> <g id="Flow-1--Testing" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Group-11"> <g id="Group-2"> <rect id="cards-base" stroke="url(#linearGradient-1)" stroke-width="0.639" x="0.902833333" y="0.930611111" width="22.361" height="14.6943333" rx="1.27777778"></rect> <polygon id="Rectangle-24" fill="url(#linearGradient-2)" points="2.66666667 3.66666667 20.9966666 3.66666667 20.9966666 5 2.66666667 5"></polygon> <polygon id="Rectangle-24" fill="url(#linearGradient-2)" points="2.66666667 8 7.33333333 8 7.33333333 12.6666667 2.66666667 12.6666667"></polygon> <polygon id="Rectangle-24" fill="url(#linearGradient-2)" points="8.66666667 8.33333333 18 8.33333333 18 9.66666667 8.66666667 9.66666667"></polygon> <polygon id="Rectangle-24" fill="url(#linearGradient-2)" points="8.66666667 11 20.9966666 11 20.9966666 12.3333333 8.66666667 12.3333333"></polygon> </g> </g> </g> </svg>
               <span class="title">
@@ -554,6 +550,21 @@
                 @endif
             </div>
           </div>
+        @endif
+        @if (in_array('debitcard', $data['request']['content']['bank_details']['auth_types']))
+          <div class="separate"></div>
+          <div id="section3">
+            <label class="accordion-heading pickable" for="content3">
+              <svg width="24px" height="17px" viewBox="0 0 24 17" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"> <title>Rectangle 88</title> <desc>Created with Sketch.</desc> <defs> <linearGradient x1="0%" y1="0%" x2="100%" y2="100%" id="linearGradient-1"> <stop stop-color="#EA3A44" offset="0%"></stop> <stop stop-color="#B70611" offset="100%"></stop> </linearGradient> </defs> <g id="Flow-1--Testing" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Both-HDFC--Aadhar" transform="translate(-28.000000, -502.000000)" fill="url(#linearGradient-1)"> <path d="M28,518.26087 L52,518.26087 L52,519 L28,519 L28,518.26087 Z M29.4117647,516.782609 L50.5882353,516.782609 L50.5882353,517.521739 L29.4117647,517.521739 L29.4117647,516.782609 Z M32.5351235,509.391304 L34.0468314,509.391304 L34.0468314,515.304348 L32.5351235,515.304348 L32.5351235,509.391304 Z M37.0702471,509.391304 L38.5819549,509.391304 L38.5819549,515.304348 L37.0702471,515.304348 L37.0702471,509.391304 Z M41.6053706,509.391304 L43.1170784,509.391304 L43.1170784,515.304348 L41.6053706,515.304348 L41.6053706,509.391304 Z M46.1404941,509.391304 L47.652202,509.391304 L47.652202,515.304348 L46.1404941,515.304348 L46.1404941,509.391304 Z M29.4117647,507.173913 L50.5882353,507.173913 L50.5882353,507.913043 L29.4117647,507.913043 L29.4117647,507.173913 Z M29.4117647,505.464674 L40,502 L50.5882353,505.464674 L50.5882353,506.157609 L29.4117647,506.157609 L29.4117647,505.464674 Z" id="Rectangle-88"></path> </g> </g> </svg>
+              <span class="title">
+                Debit Card
+                <div class="sub-title">Via Debit Card details</div>
+              </span>
+            </label>
+            <input type="radio" id="content3" name="auth_type" value="debitcard" hidden checked>
+            <span class="arrow"></span>
+          </div>
+        @endif
         </div>
       </main>
       <div class="action">
@@ -629,6 +640,23 @@
 
     var nbMthd = data['request']['content']['bank_details']['auth_types'].indexOf('netbanking');
     var adrMthd = data['request']['content']['bank_details']['auth_types'].indexOf('aadhaar');
+
+    {{-- If auth_type was previously passed, i.e. it is present in the input, select it. --}}
+    var preselectedAuthType = data['request']['content']['input']['auth_type'];
+    var isPreselectedAuthAvailable = data['request']['content']['bank_details']['auth_types'].indexOf(preselectedAuthType) > -1;
+
+    var netbankingRadio = document.querySelector('input[type=radio][name=auth_type][value=netbanking]');
+    var debitcardRadio = document.querySelector('input[type=radio][name=auth_type][value=debitcard]');
+
+    if (isPreselectedAuthAvailable) {
+      if (preselectedAuthType === 'netbanking') {
+        debitcardRadio && (debitcardRadio.checked = false);
+        netbankingRadio && (netbankingRadio.checked = true);
+      } else if (preselectedAuthType === 'debitcard') {
+        netbankingRadio && (netbankingRadio.checked = false);
+        debitcardRadio && (debitcardRadio.checked = true);
+      }
+    }
 
     if (nbMthd === -1) {
       document.querySelector('#section1 label + input').setAttribute('disabled', true);
