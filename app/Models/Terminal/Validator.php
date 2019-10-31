a12a456629045239439a08e6c2ada275d71d9a86
Akash Sharma <akashsharma@live.in>
2019-10-31 14:47:03 +0530

    Make network_category for rule filter optional in validator (#13848)

--- app/Models/Terminal/Validator.php
+++ app/Models/Terminal/Validator.php
@@ -863,6 +863,7 @@ class Validator extends Base\Validator
         Entity::TYPE                        => 'sometimes|array',
         Entity::MODE                        => 'sometimes|integer|in:2,3',
         Entity::CATEGORY                    => 'sometimes|string|numeric|digits:4',
+        Entity::NETWORK_CATEGORY            => 'sometimes|string|max:30',
     ];
 
     protected static $cardFssEditTerminalRules = [
