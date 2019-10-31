a3cecddaace324025da83fa18700cc1b0a98bc23
ratanphyade <ratanphayade@gmail.com>
2019-10-31 13:59:11 +0530

    [recon] skip failed recon to unblock others

--- app/Models/FundTransfer/Attempt/BulkRecon.php
+++ app/Models/FundTransfer/Attempt/BulkRecon.php
@@ -141,17 +141,26 @@ class BulkRecon extends Base\Core
 
                     foreach ($ftas as $fta)
                     {
-                        $reconDetails = (new $entityProcessor($fta))->process();
+                        try
+                        {
+                            $reconDetails = (new $entityProcessor($fta))->process();
 
-                        $this->allReconciledRows[] = $reconDetails;
+                            $this->allReconciledRows[] = $reconDetails;
 
-                        $entity = $reconDetails['entity'];
+                            $entity = $reconDetails['entity'];
 
-                        $this->updateBatchFundTransferStats($entity);
+                            $this->updateBatchFundTransferStats($entity);
 
-                        $this->updateCriticalErrorsSummary($fta);
+                            $this->updateCriticalErrorsSummary($fta);
 
-                        $this->dispatchForStatusCheck($fta);
+                            $this->dispatchForStatusCheck($fta);
+                        }
+                        catch (\Throwable $e)
+                        {
+                            $this->trace->error(TraceCode::FTA_RECON_FAILED, [
+                                'fta_id' => $fta->getId(),
+                            ]);
+                        }
                     }
                 }
 
