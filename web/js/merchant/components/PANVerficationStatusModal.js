cb7ef6dbe7085365727076a4463a5bc5e3db76da
Zameer Haque <zameer.haque@razorpay.com>
2019-11-03 16:21:10 +0530

    add showNotification action in mapDispatch

--- web/js/merchant/components/PANVerficationStatusModal.js
+++ web/js/merchant/components/PANVerficationStatusModal.js
@@ -1,22 +1,58 @@
 import React from 'react';
 import { ModalMask, Modal } from 'component/Modal';
 
-const PANVerficationStatusModal = ({ user }) => {
+const PAN = 'PAN';
+const PAN_FAILURE = 'PAN_FAILURE'; // timeout in PAN verification
+const PAN_SUCCESS = 'PAN_SUCCESS'; // timeout in PAN verification
+
+const VERIFICATION_STATUS = {
+  failed: PAN_FAILURE,
+  verified: PAN_SUCCESS,
+};
+
+const MODAL_CONTENT = {
+  PAN_FAILURE: {
+    title: 'PAN Under Review',
+    subtitle: 'We are trying to review your PAN',
+    body: () => (
+      <p>
+        This seems to be taking longer than usual. You can explore the dashboard
+        while we review your PAN Details.
+      </p>
+    ),
+    background: 'pending',
+  },
+  PAN_SUCCESS: {
+    title: 'Account Activated',
+    subtitle: 'PAN successfully verified',
+    body: () => (
+      <>
+        <p>
+          You can start accepting payment now by integrating with a website/app
+          or using other products.
+        </p>
+        <p>You can accept upto ₹10,000 per transaction. know more</p>
+      </>
+    ),
+    background: 'success',
+  },
+};
+
+const PANVerficationStatusModal = ({ user, onClose }) => {
+  console.log(user);
+  console.log(user.poi_verification_status);
+  const content =
+    MODAL_CONTENT[VERIFICATION_STATUS[user.poi_verification_status]];
+  console.log('content', content);
   return (
     <ModalMask>
-      <Modal className="pan-status-modal">
-        <div className="modal-header">
-          <h1>PAN Under Review</h1>
-          <p>PAN successfully verified</p>
+      <Modal className="pan-status-modal" onClose={onClose}>
+        <div className={`modal-header ${content.background}`}>
+          <h1>{content.title}</h1>
+          <p>{content.subtitle}</p>
         </div>
         <div className="modal-body">
-          <div className="modal-description">
-            <p>
-              You can start accepting payment now by integrating with a
-              website/app or using other products.
-            </p>
-            <p>You can accept upto ₹10,000 per transaction. know more</p>
-          </div>
+          <div className="modal-description">{content.body()}</div>
           <button className="btn btn-primary">Go to Dashboard</button>
         </div>
       </Modal>
