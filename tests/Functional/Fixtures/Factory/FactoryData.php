b09f6ee9e019b44e82372a3bf5ed82cde0f16a82
Akshata S Shetty <akshata.shetty@razorpay.com>
2019-11-04 13:23:38 +0530

    Update_Func_Configs_2 updated all occureances of rzp-test-bucket in API

--- tests/Functional/Fixtures/Factory/FactoryData.php
+++ tests/Functional/Fixtures/Factory/FactoryData.php
@@ -879,7 +879,7 @@ final class FactoryData
             'name'        => 'xyz.xlsx',
             'store'       => 's3',
             'location'    => 'xyz',
-            'bucket'      => 'rzp-test-bucket',
+            'bucket'      => 'rzp-1018-nonprod-test-bucket',
             'region'      => 'us-east-1',
             'created_at'  => $faker->timestamp,
             'updated_at'  => $faker->timestamp,
