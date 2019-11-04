46027ed95d4f226fa67ee412745d833d425d396b
Zameer Haque <zameer.haque@razorpay.com>
2019-11-04 12:03:33 +0530

    Remove code for throwing error on invalid pan from ActivationUtils

--- web/js/component/merchant/Activation/ActivationUtils.js
+++ web/js/component/merchant/Activation/ActivationUtils.js
@@ -33,17 +33,6 @@ function L1FormSuccess(props) {
     if (poi_verification_status == 'verified') {
       props.showPANStatusModal();
     }
-    // else if (poi_verification_status == 'incorrect_details') {
-    //   const error = {
-    //     errors: ['Incorrect PAN Details Provided'],
-    //   };
-    //   throw error;
-    // } else if (poi_verification_status == 'not_matched') {
-    //   const error = {
-    //     errors: ['Provided details does not match any records.'],
-    //   };
-    //   throw error;
-    // }
   } else {
     const {
       isWhitelistFlow,
