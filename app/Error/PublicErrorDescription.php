a56611069faf74504735a6cea765955bc2bfa349
Subhrajit Makur <Avyukth@users.noreply.github.com>
2019-11-05 12:56:19 +0530

    bulk feature remove modification Done (#13437)
    Bulk Feature Remove and Add

--- app/Error/PublicErrorDescription.php
+++ app/Error/PublicErrorDescription.php
@@ -570,6 +570,7 @@ class PublicErrorDescription
     const BAD_REQUEST_MERCHANT_UNEDITABLE_FEATURE                               = 'You cannot change the value of this feature';
     const BAD_REQUEST_MERCHANT_FEATURE_UNEDITABLE_IN_LIVE                       = 'You cannot enable/disable features in live mode';
     const BAD_REQUEST_MERCHANT_FEATURE_ALREADY_ASSIGNED                         = 'The given feature is already assigned to the merchant';
+    const BAD_REQUEST_MERCHANT_NOT_EXIST                                        = 'The given merchant ID is not present';
     const BAD_REQUEST_MERCHANT_FEATURE_NOT_EXIST                                = 'The given feature do not exist for above merchants. Rest are processed';
     const BAD_REQUEST_MERCHANT_FEATURE_ONBOARDING_STATUS_INVALID                = 'The product onboarding status provided is invalid';
     const BAD_REQUEST_MERCHANT_FEATURE_ACTIVATION_FORM_ALREADY_SUBMITTED        = 'The product activation form is already submitted.';
