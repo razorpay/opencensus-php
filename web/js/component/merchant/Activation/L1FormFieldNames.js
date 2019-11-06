9ed2c93e5a46438f64024dfeeb1583ddcfaa6a49
Zameer Haque <zameer.haque@razorpay.com>
2019-11-06 19:16:29 +0530

    Remove L1FormMap duplicate files and use L1ForFieldNames for making L1 Request

---
+++ web/js/component/merchant/Activation/L1FormFieldNames.js
@@ -0,0 +1,24 @@
+import AddressFields from 'merchant/containers/Activation/AddressFieldsMap';
+
+const L1_BUSINESS_FIELD_NAMES = [
+  'business_dba',
+  'business_category',
+  'business_model',
+  'business_subcategory',
+  'promoter_pan',
+  'promoter_pan_name',
+  'business_type',
+  'business_name',
+  'business_website',
+];
+const ADDRESS_FIELD_NAMES = AddressFields.map(
+  field =>
+    Array.isArray(field)
+      ? field.map(nestedField => nestedField.name)
+      : field.name
+)
+  .reduce((prevField, curField) => prevField.concat(curField), [])
+  .filter(field => !!field);
+const L1_FORM_FIELD_NAMES = L1_BUSINESS_FIELD_NAMES.concat(ADDRESS_FIELD_NAMES);
+
+export default L1_FORM_FIELD_NAMES;
