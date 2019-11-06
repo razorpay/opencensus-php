17b29a8b5b4e0e0c93a31da15d2c8a54a66ef58d
Shridhar Bavannawar <shridhar.b@razorpay.com>
2019-11-06 13:00:46 +0530

    show link only for partner_intent user

--- web/js/merchant/containers/PartnerDashboard/Onboarding/partnerOnbr.js
+++ web/js/merchant/containers/PartnerDashboard/Onboarding/partnerOnbr.js
@@ -84,7 +84,7 @@ export default class BaseScreen extends React.Component {
               onRoleSelect={this.onRoleSelect}
               role={this.state.role}
               abort={this.props.closeModal}
-              isExistingUser={this.props.user.merchant_partner_intent}
+              isExistingUser={this.props.user.isPartnerIntent()}
             />
           )}
           {sliderProps => (
