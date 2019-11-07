bab08db441df0baa9d01711fe84099ec776dded5
akshaygupta475 <akshay.gupta@razorpay.com>
2019-11-07 12:02:59 +0530

    adding load sorter to router's sorters list

--- app/Models/Terminal/Selector.php
+++ app/Models/Terminal/Selector.php
@@ -88,6 +88,9 @@ class Selector extends Base\Core
      */
     protected static $smartRoutingSorters = [
 
+        // Boost a gateway terminals based on load distribution of probabilities
+        Sorters\TerminalLoadSorter::class,
+
         // Sorting based on merchant category
         Sorters\MerchantSorter::class,
 
