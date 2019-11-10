8ae2b4bf4e431d040161d196970f41b2a774f886
krantideep95 <kranti.deep@razorpay.com>
2019-11-10 20:34:29 +0530

    fixes mozart request input; assert in tests (#13952)

--- tests/Functional/D2cBureauDetails/D2cBureauDetailsTest.php
+++ tests/Functional/D2cBureauDetails/D2cBureauDetailsTest.php
@@ -2,6 +2,7 @@
 
 namespace RZP\Tests\Functional\D2cBureauDetails;
 
+use RZP\Services\Mock\Mozart;
 use RZP\Models\D2cBureauDetail;
 use RZP\Models\Feature\Constants;
 use RZP\Tests\Functional\TestCase;
@@ -72,6 +73,59 @@ class D2cBureauDetailsTest extends TestCase
         $response = $this->makeRequestAndGetContent($this->testData['testPostCreate']['request']);
 
         $this->testData[__FUNCTION__]['request']['url'] = strtr($this->testData[__FUNCTION__]['request']['url'], ['{id}' => $response['id'],]);
+
+        $mozartServiceMock = $this->getMockBuilder(Mozart::class)
+                                  ->setConstructorArgs([$this->app])
+                                  ->setMethods(['sendMozartRequest'])
+                                  ->getMock();
+
+        $mozartServiceMock->method('sendMozartRequest')
+                          ->will($this->returnCallback(
+                                function ($namespace, $gateway, $action, $input, $version, $useMozartMappedInternalErrorCode)
+                                {
+                                    $this->assertArraySelectiveEquals([
+                                            'first_name'    => 'testhello',
+                                            'address'       => 'Adress',
+                                            'city'          => 'city',
+                                    ], $input['d2c_bureau_details']);
+
+                                    return [
+                                        'success'   => true,
+                                        'data'      => [
+                                            'score'         => '752',
+                                            'report'        => [
+                                                'active_accounts'                           => '1',
+                                                'closed_accounts'                           => '1',
+                                                'count_of_accounts'                         => '2',
+                                                'secured_account_outstanding_balance'       => '152000',
+                                                'total_outstanding_balance'                 => '152000',
+                                                'un_secured_account_outstanding_balance'    => '0'
+                                            ],
+                                            '_raw'          => 'garbage',
+                                            'raw_report'    => [
+                                                'INProfileResponse' => [
+                                                    'CAIS_Account'  => [
+                                                        'CAIS_Account_DETAILS' => [
+                                                            'AccountHoldertypeCode' => '1',
+                                                            'Account_Number'        => 'XXXXXXXX0304',
+                                                            'Account_Status'        => '11',
+                                                            'Account_Type'          => '51',
+                                                            'Amount_Past_Due'       => '501',
+                                                            'CAIS_Account_History'  => [
+                                                                'Asset_Classification'  => '?',
+                                                                'Days_Past_Due'         => '14',
+                                                                'Month'                 => '08',
+                                                                'Year'                  => '2019'
+                                                            ],
+                                                        ],
+                                                    ]
+                                                ]
+                                            ]
+                                        ]
+                                    ];
+                                }));
+
+        $this->app->instance('mozart', $mozartServiceMock);
 
         $this->startTest();
 
