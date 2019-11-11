357190c31b48e26dde5ce8568255350ed06c1dd9
shivanshgaur <shivansh.gaur@razorpay.com>
2019-11-11 23:26:24 +0530

    sla for payouts irrespective of mode (#13986)

--- app/Jobs/FundTransfer.php
+++ app/Jobs/FundTransfer.php
@@ -272,9 +272,7 @@ class FundTransfer extends Job
 
         if ((empty($sla) === false) and
             ($fta->getSourceType() === Attempt\Type::PAYOUT) and
-            (((int) $sla) <= $duration) and
-            (($fta->getMode() === FTA\Mode::IMPS) or
-                ($fta->getMode() === FTA\Mode::IFT)))
+            (((int) $sla) <= $duration))
         {
             $this->trace->info(
                 TraceCode::FTA_SLA_EXPIRED,
