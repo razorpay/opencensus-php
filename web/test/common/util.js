dd369785bd9d26dbbc3662b94ddf678d90a86435
aseemgupta <aseemgupta2625@gmail.com>
2019-11-11 19:29:10 +0530

    [Dashboard Maintenance]: Fix import in tests

--- web/test/common/util.js
+++ web/test/common/util.js
@@ -1,9 +1,9 @@
 require('it-each')();
 const expect = require('chai').expect;
 
-import { stringToObj } from 'common/util';
+import { stringToObj } from 'rzp/utils/rzp-utils';
 
-describe('common/util: doStringToObj', () => {
+describe('rzp/utils/rzp-utils: doStringToObj', () => {
   describe('dot-notation', () => {
     it('should insert value in passed object with passed path', () => {
       const argumentSet = ['path.to.key', 'value', {}];
