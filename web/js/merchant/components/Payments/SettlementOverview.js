b6c31196662fa172e4267239aedf2b82216481a0
AakashRaina <aakashraina9@gmail.com>
2019-11-11 15:46:55 +0530

    Adds new query parameters for settlement details

--- web/js/merchant/components/Payments/SettlementOverview.js
+++ web/js/merchant/components/Payments/SettlementOverview.js
@@ -24,10 +24,11 @@ function SettlementOverview(props) {
         <span>GST(18%) Fee</span>
         <span>40 INR</span>
       </div>
-      <div class="settlement-payment-info">
+      // TODO: Add when design is ready //
+      {/* <div class="settlement-payment-info">
         <i class="i i-info-circle" />
         <span>View your payment methods</span>
-      </div>
+      </div> */}
     </div>
   );
 }
