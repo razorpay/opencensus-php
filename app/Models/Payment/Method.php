839a0caccb03d7819c76d7c89e59651e9870d2bd
gagan-chawara <53043164+gagan-chawara@users.noreply.github.com>
2019-11-11 20:53:36 +0530

    Update ES payment methods (#13991)

--- app/Models/Payment/Method.php
+++ app/Models/Payment/Method.php
@@ -32,11 +32,9 @@ class Method
         self::PAYLATER      => 'Pay Later',
     ];
 
-    protected static $esAutomaticMethods = [
-        self::CARD          => 'Card',
-        self::NETBANKING    => 'Net Banking',
-        self::UPI           => 'UPI',
-        self::EMI           => 'EMI',
+    protected static $nonEsAutomaticMethods = [
+        self::EMANDATE      => 'E-Mandate',
+        self::BANK_TRANSFER => 'Bank Transfer',
     ];
 
     public static $bankMethods = [
@@ -75,9 +73,9 @@ class Method
         return array_keys(self::$methods);
     }
 
-    public static function getEsPaymentMethods()
+    public static function getNonEsPaymentMethods()
     {
-        return array_keys(self::$esAutomaticMethods);
+        return array_keys(self::$nonEsAutomaticMethods);
     }
 
     public static function isValid($method)
@@ -87,7 +85,8 @@ class Method
 
     public static function isValidEsMethod($method)
     {
-        return in_array($method, self::getEsPaymentMethods(), true);
+        return ((in_array($method, self::getNonEsPaymentMethods(), true) === false) and
+                (in_array($method, self::getAllPaymentMethods(), true) === true));
     }
 
     public static function validateMethod($method)
