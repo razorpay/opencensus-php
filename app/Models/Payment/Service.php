a52c233a02b1ee68cf419810462b23e0027ecc70
Jayesh Pawar <jayesh.pawar@razorpay.com>
2019-11-12 18:05:40 +0530

    [vpa_validate] patching merchant id called from jobs

--- app/Models/Payment/Service.php
+++ app/Models/Payment/Service.php
@@ -1761,7 +1761,16 @@ class Service extends Base\Service
      */
     public function validateVpa($input)
     {
-        $data = $this->getNewProcessor()->validateVpa($input);
+        $merchant = $this->merchant;
+
+        // If the caller is from Jobs, merchant id is set to null
+        // This is quick fix to solve the problem
+        if ($merchant === null and isset($input['merchant_id']))
+        {
+            $merchant = $this->repo->merchant->find($input['merchant_id']);
+        }
+
+        $data = $this->getNewProcessor($merchant)->validateVpa($input);
 
         return $data;
     }
