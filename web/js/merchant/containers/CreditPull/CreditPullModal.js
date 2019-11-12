8a7d28742c4d8b3d3bef6abc7c8c44603ca9ecf7
Devansh <devansh.dwivedi@razorpay.com>
2019-11-12 10:54:41 +0530

    Adding user update call on dashboard.

--- web/js/merchant/containers/CreditPull/CreditPullModal.js
+++ web/js/merchant/containers/CreditPull/CreditPullModal.js
@@ -27,6 +27,8 @@ import {
 import CreditPullClose from './CreditPullClose';
 import CreditPullSuccess from './CreditPullSuccess';
 import ajax from 'merchant/utils/ajax';
+import User from 'merchant/models/User';
+import { updateSession } from 'merchant/modules/session';
 
 const validate = values => {
   const errors = {};
@@ -51,6 +53,7 @@ const validate = values => {
     ...MerchantActions,
     ...ModalActions,
     bMerchantReducer,
+    updateSession,
   }
 )
 @reduxForm({
@@ -188,55 +191,10 @@ export default class CreditPullModal extends Component {
               mobile,
               tokenStuff,
               merchantId
-            )
-              .then(({ data: { report, score, max_loan_amount } }) => {
-                clearTimeout(this.timer);
-                this.openReportScreen(report, score, max_loan_amount);
-              })
-              .catch(errorResponse => {
-                let {
-                  error_wrong_otp,
-                  error_wrong_phone,
-                  error_wrong_merchant,
-                  error_max_attempts,
-                  error_otp_required,
-                  error_otp_length,
-                } = this.errorMessages;
-                const error = (errorResponse.errors || [])[0];
-                let gaPayload = {
-                  eventAction: `Error`,
-                  eventLabel: `${error} ? ${error} : "Some unexpected error occurred"`,
-                };
-                this.fireGAEvent(gaPayload);
-                if (
-                  error === error_wrong_otp ||
-                  error === error_otp_required ||
-                  error === error_otp_length
-                ) {
-                  throw errorResponse;
-                } else if (error === error_wrong_phone) {
-                  this.props.showNotification({
-                    type: 'error',
-                    message: error,
-                    hidePrevious: true,
-                  });
-                } else if (
-                  error === error_wrong_merchant ||
-                  error === error_max_attempts
-                ) {
-                  clearTimeout(this.timer);
-                  this.props.closeModal();
-                  this.openErrorScreen(error);
-                } else {
-                  clearTimeout(this.timer);
-                  this.props.showNotification({
-                    type: 'error',
-                    message: 'Some unexpected error occurred',
-                    hidePrevious: true,
-                  });
-                  this.props.closeModal();
-                }
-              });
+            ).then(([{ report, score, max_loan_amount }, userResponse]) => {
+              this.props.updateSession({ user: userResponse.data });
+              this.openReportScreen(report, score, max_loan_amount);
+            });
           }}
           onResend={() => {
             return this.sendReqForOtp(mobile, tokenStuff).then(() => {
@@ -292,7 +250,57 @@ export default class CreditPullModal extends Component {
       },
       {},
       '/merchant/api'
-    );
+    )
+      .then(({ data }) => {
+        clearTimeout(this.timer);
+        let updatedUser = new User(this.props.user);
+        return Promise.all([data, updatedUser.fetch()]);
+      })
+      .catch(errorResponse => {
+        this.handleOTPError(errorResponse);
+      });
+  };
+
+  handleOTPError = errorResponse => {
+    let {
+      error_wrong_otp,
+      error_wrong_phone,
+      error_wrong_merchant,
+      error_max_attempts,
+      error_otp_required,
+      error_otp_length,
+    } = this.errorMessages;
+    const error = (errorResponse.errors || [])[0];
+    let gaPayload = {
+      eventAction: `Error`,
+      eventLabel: `${error} ? ${error} : "Some unexpected error occurred"`,
+    };
+    this.fireGAEvent(gaPayload);
+    if (
+      error === error_wrong_otp ||
+      error === error_otp_required ||
+      error === error_otp_length
+    ) {
+      throw errorResponse;
+    } else if (error === error_wrong_phone) {
+      this.props.showNotification({
+        type: 'error',
+        message: error,
+        hidePrevious: true,
+      });
+    } else if (error === error_wrong_merchant || error === error_max_attempts) {
+      clearTimeout(this.timer);
+      this.props.closeModal();
+      this.openErrorScreen(error);
+    } else {
+      clearTimeout(this.timer);
+      this.props.showNotification({
+        type: 'error',
+        message: 'Some unexpected error occurred',
+        hidePrevious: true,
+      });
+      this.props.closeModal();
+    }
   };
 
   initialAlign = () => {
