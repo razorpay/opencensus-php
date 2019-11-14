a786a7e7069f69d1f40b12498891e5538c8ca123
singlashubham957 <53046322+singlashubham957@users.noreply.github.com>
2019-11-14 21:02:16 +0530

    [scrooge] Wallet Airtelmoney  integration (#13817)
    * changes for integration of airtelmoney in scrooge
    
    * removed commented code
    
    * Update app/Gateway/Wallet/Airtelmoney/Gateway.php
    
    minor change
    
    Co-Authored-By: Nikhilesh Chamarthi <nikhilesh.chamarthi@razorpay.com>
    
    * only formatting
    
    * minor changes
    
    * removed unnecessary code
    
    * formatting changes

--- app/Models/Payment/Gateway.php
+++ app/Models/Payment/Gateway.php
@@ -627,6 +627,7 @@ class Gateway
         Payment\Gateway::ATOM,
         Payment\Gateway::UPI_AIRTEL,
         Payment\Gateway::CARDLESS_EMI,
+        Payment\Gateway::WALLET_AIRTELMONEY,
         Payment\Gateway::WALLET_PAYZAPP,
     ];
 
