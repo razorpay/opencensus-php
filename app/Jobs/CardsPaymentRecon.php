f42285b1f12478d9a59ee8abe9b2b8fdaf79c035
Amit Mahbubani <amit.mahbubani@razorpay.com>
2019-11-19 04:06:25 +0530

    drain CardsPaymentRecon

---
+++ app/Jobs/CardsPaymentRecon.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace RZP\Jobs;
+
+class CardsPaymentRecon extends Job
+{
+    public function __construct(array $data)
+    {
+        parent::__construct();
+    }
+
+    /**
+     * @throws \Throwable
+     */
+    public function handle()
+    {
+        // nothing :|
+    }
+}
