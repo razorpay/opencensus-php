4d3d3646875a6a62d1dfe79d9cd40c860a31f3bc
Soutreya Kundu <soutreya.kundu@razorpay.com>
2019-11-19 12:23:35 +0530

    Updated auth type for route.

--- app/Http/Route.php
+++ app/Http/Route.php
@@ -1149,7 +1149,7 @@ final class Route
         'subscription_registration_list_links'     => ['get',      'subscription_registration/auth_links',           'SubscriptionRegistrationController@listAuthLinks'                  ],
         'subscription_registration_create_links'   => ['post',     'subscription_registration/auth_links',           'SubscriptionRegistrationController@createAuthLink'                 ],
         'subscription_registration_fetch_link'     => ['get',      'subscription_registration/auth_links/{id}',      'SubscriptionRegistrationController@fetchAuthLink'                  ],
-        'subscription_registration_resend_link'    => ['post',     'subscription_registration/auth_links/{id}/notify/{medium}',      'SubscriptionRegistrationController@sendNotification'    ],
+        'subscription_registration_resend_link'    => ['post',     'subscription_registration/auth_links/{id}/notify/{medium}',     'SubscriptionRegistrationController@sendNotification'       ],
         'subscription_registration_fetch_link_internal' => ['get', 'subscription_registration/auth_links/{id}/internal', 'SubscriptionRegistrationController@fetchAuthLinkInternal'      ],
         'subscription_registration_fetch_token'    => ['get',      'subscription_registration/tokens/{id}',          'SubscriptionRegistrationController@fetchToken'                     ],
         'subscription_registration_delete_token'   => ['delete',   'subscription_registration/tokens/{id}',          'SubscriptionRegistrationController@deleteToken'                    ],
@@ -1817,6 +1817,7 @@ final class Route
         'invoice_remove_line_item',
         'invoice_send_notification_private',
         'invoice_cancel',
+        'subscription_registration_resend_link',
     ];
 
     // The below routes can be used with partner credentials without X-Razorpay-Account header,
@@ -2016,7 +2017,6 @@ final class Route
         'subscription_registration_list_links',
         'subscription_registration_create_links',
         'subscription_registration_fetch_link',
-        'subscription_registration_resend_link',
         'subscription_registration_fetch_token',
         'subscription_registration_delete_token',
         'subscription_registration_charge_token',
