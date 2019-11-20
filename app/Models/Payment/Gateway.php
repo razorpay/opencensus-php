3ab5fdd8ac16ae0b0ffae524b15f691c6c829dbf
singlashubham957 <53046322+singlashubham957@users.noreply.github.com>
2019-11-20 14:43:43 +0530

    Added scb in scrooge gateways (#14059)

--- app/Models/Payment/Gateway.php
+++ app/Models/Payment/Gateway.php
@@ -629,6 +629,7 @@ class Gateway
         Payment\Gateway::CARDLESS_EMI,
         Payment\Gateway::WALLET_AIRTELMONEY,
         Payment\Gateway::WALLET_PAYZAPP,
+        Payment\Gateway::NETBANKING_SCB,
         Payment\Gateway::WALLET_AMAZONPAY,
         Payment\Gateway::WALLET_OPENWALLET,
     ];
