66c62f7215395775f67779b971e92f6cc4732c14
aseemgupta <aseemgupta2625@gmail.com>
2019-11-21 18:31:04 +0530

    [Tests]: Fix path changes as per refactor

--- web/test/common/utils/rzp-utils.js
+++ web/test/common/utils/rzp-utils.js
@@ -1,7 +1,39 @@
 require('it-each')();
 const expect = require('chai').expect;
+import { isFunction, stringToObj } from 'common/utils/rzp-utils';
 
-import { stringToObj } from 'common/utils/rzp-utils';
+describe('common/utils/rzp-utils Fn: isFunction', function() {
+  const falseValues = [
+    2,
+    2.3,
+    true,
+    false,
+    0,
+    '0',
+    [],
+    {},
+    '',
+    null,
+    undefined,
+  ];
+
+  it('should be function', function() {
+    const value = function() {};
+    const result = isFunction(value);
+
+    expect(result).to.eql(true);
+  });
+
+  it.each(falseValues, 'all values should not be function.', function(
+    value,
+    next
+  ) {
+    const result = isFunction(value);
+    expect(result).to.eql(false);
+
+    next();
+  });
+});
 
 describe('common/utils/rzp-utils: doStringToObj', () => {
   describe('dot-notation', () => {
