7880c59e229c52d3a33f301e3eca960f712e677d
dvvavinash <dvv.avinash@gmail.com>
2019-11-22 01:58:37 +0530

    [Reminders]: Remove change the location on cancel

--- web/js/merchant/components/Reminders/Settings/index.js
+++ web/js/merchant/components/Reminders/Settings/index.js
@@ -191,29 +191,25 @@ export default class ReminderSettings extends React.Component {
   };
 
   handleRouteChange = location => {
-    this.context
-      .confirm({
-        header: 'Discard unsaved changes?',
-        message:
-          'You have made changes to the reminder schedule.  All changes will be lost.',
-        affirmativeLabel: 'Discard',
-        abortLabel: 'Cancel',
-        action: () => {
-          this.setState(
-            {
-              settings: {
-                ...this.state.__stashed_settings__,
-              },
+    this.context.confirm({
+      header: 'Discard unsaved changes?',
+      message:
+        'You have made changes to the reminder schedule.  All changes will be lost.',
+      affirmativeLabel: 'Discard',
+      abortLabel: 'Cancel',
+      action: () => {
+        this.setState(
+          {
+            settings: {
+              ...this.state.__stashed_settings__,
             },
-            () => {
-              this.props.history.push(location.pathname);
-            }
-          );
-        },
-      })
-      .catch(() => {
-        this.props.history.push(this.currLocation);
-      });
+          },
+          () => {
+            this.props.history.push(location.pathname);
+          }
+        );
+      },
+    });
 
     return false;
   };
