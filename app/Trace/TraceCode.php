f9fd87d457273c3fd9a773337433fe3415dac9b4
pawanrajmurarka <40450445+pawanrajmurarka@users.noreply.github.com>
2019-11-21 15:04:27 +0530

    Skip processing if updated fta status is not created (#14151)
    * Skip processing if updated fta status is not created
    
    * Changed entity reload to cloned attempt

--- app/Trace/TraceCode.php
+++ app/Trace/TraceCode.php
@@ -344,6 +344,7 @@ class TraceCode extends \Razorpay\Trace\TraceCode
     const NODAL_HEALTH_CHECK_REQUEST                            = 'NODAL_HEALTH_CHECK_REQUEST';
     const NODAL_HEALTH_CHECK_RESPONSE                           = 'NODAL_HEALTH_CHECK_RESPONSE';
     const NODAL_REQUEST_FAILED                                  = 'NODAL_REQUEST_FAILED';
+    const NODAL_TRANSFER_REQUEST_DUPLICATE                      = 'NODAL_TRANSFER_REQUEST_DUPLICATE';
     const NODAL_TRANSFER_STATUS_UPDATE_FAILED                   = 'NODAL_TRANSFER_STATUS_UPDATE_FAILED';
     const FTA_SOURCE_PROCESSING_FAILED                          = 'FTA_SOURCE_PROCESSING_FAILED';
     const FTA_SOURCE_PROCESSING_DATA                            = 'FTA_SOURCE_PROCESSING_DATA';
