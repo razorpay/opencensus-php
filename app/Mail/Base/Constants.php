3ab7baf49ba7c51db9a2fc70aca42c76770626fc
hemanth132 <45421294+hemanth132@users.noreply.github.com>
2019-11-25 19:13:43 +0530

    send email when partner gets onboarded (#13984)
    * [PARTNER]:Added mjml email templates for partner onboarding

--- app/Mail/Base/Constants.php
+++ app/Mail/Base/Constants.php
@@ -38,6 +38,9 @@ class Constants
     const RAZORPAY_X              = 'razorpay_x';
     const CAPITAL_SUPPORT         = 'capital_support';
     const NACH                    = 'nach';
+    const PARTNER_ON_BOARDING     = 'partner_on_boarding';
+
+    const PARTNER_ON_BOARDING_REPLY = 'partner_on_boarding_reply';
 
     const MAIL_ADDRESSES = [
         self::SUPPORT                 => 'support@razorpay.com',
@@ -71,8 +74,10 @@ class Constants
         self::LINKED_ACCOUNT_REVERSAL => 'refunds@razorpay.com',
         self::GATEWAY_POD             => 'pod.gateway@razorpay.com',
         self::CAPITAL_SUPPORT         => 'capital.support@razorpay.com',
-        self::NACH                    => 'nach@razorpay.com'
+        self::NACH                    => 'nach@razorpay.com',
+        self::PARTNER_ON_BOARDING     => 'partnercommunication@razorpay.com',
 
+        self::PARTNER_ON_BOARDING_REPLY => 'kzgpFWFVZU@razorpay.com',
     ];
 
     const HEADERS = [
@@ -99,7 +104,8 @@ class Constants
         self::MERCHANT_ONBOARDING     => 'support@razorpay.com',
         self::LINKED_ACCOUNT_REVERSAL => 'Linked Account Refunds File',
         self::RAZORPAY_X              => 'RazorpayX',
-        self::CAPITAL_SUPPORT         => 'Razorpay Capital Team',
+        self::PARTNER_ON_BOARDING     => 'Razorpay Partner Program',
+        self::CAPITAL_SUPPORT         => 'Razorpay Capital',
         self::NACH                    => 'Razorpay Nach',
     ];
 }
