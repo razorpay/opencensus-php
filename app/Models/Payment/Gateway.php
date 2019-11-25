2b7af7da4bb51a00f3645b206553ed0c5fd3fe9d
Shivam Yuvraj <shivamyuvraj1@gmail.com>
2019-11-25 14:47:08 +0530

    [PON-59] bug fixes for axis bqr (#14104)

--- app/Models/Payment/Gateway.php
+++ app/Models/Payment/Gateway.php
@@ -171,6 +171,7 @@ class Gateway
         self::UPI_HULK     => [self::ACQUIRER_HDFC],
         self::CARDLESS_EMI => [CardlessEmi::ZESTMONEY, CardlessEmi::EARLYSALARY, CardlessEmi::FLEXMONEY],
         self::PAYLATER     => [PayLater::EPAYLATER],
+        self::WORLDLINE    => [self::ACQUIRER_AXIS],
         self::MPGS         => [self::ACQUIRER_HDFC, self::ACQUIRER_AXIS, self::ACQUIRER_AMEX],
     ];
 
--- tests/Functional/Merchant/helpers/TerminalData.php
+++ tests/Functional/Merchant/helpers/TerminalData.php
@@ -2180,18 +2180,20 @@ return [
             'content' => [
                 'merchant_id'               => '10000000000000',
                 'gateway'                   => 'worldline',
+                'expected'                  => '1',
+                'card'                      => '1',
                 'gateway_merchant_id'       => '037122003842039',
                 'gateway_terminal_id'       => '70374018',
                 'gateway_acquirer'          => 'axis',
-                'card'                      => 1,
                 'gateway_terminal_password' => '9900991100',
                 'mc_mpan'                   => '5122600004774122',
                 'visa_mpan'                 => '4604901004774122',
                 'rupay_mpan'                => '6100020004774141',
                 'vpa'                       => 'MAB.037122003842039@AXISBANK',
                 'type'                      => [
-                    'non_recurring' => '1',
-                    'bharat_qr' => '1',
+                    'non_recurring'                 => '1',
+                    'bharat_qr'                     => '1',
+                    'direct_settlement_with_refund' => '1'
                 ],
             ],
             'method' => 'POST'
@@ -2392,8 +2394,7 @@ return [
                     'mc_mpan'       =>  '1234567880123456',
                     'rupay_mpan'    =>  '1234567890123457',
                     'visa_mpan'     =>  '1234567890123456'
-                ]
-
+                ],
             ]
         ]
     ],
