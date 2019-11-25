4e0977745c3138d12f057a4c04f54047fd07e35c
Albin George <albinin0002@gmail.com>
2019-11-25 16:55:32 +0530

    [bfl] Implement amount check, gateway transaction id, etc

--- app/Reconciliator/BajajFinserv/SubReconciliator/PaymentReconciliate.php
+++ app/Reconciliator/BajajFinserv/SubReconciliator/PaymentReconciliate.php
@@ -2,12 +2,69 @@
 
 namespace RZP\Reconciliator\BajajFinserv\SubReconciliator;
 
-use RZP\Reconciliator\Base;;
+use RZP\Trace\TraceCode;
+use RZP\Reconciliator\Base;
+use RZP\Models\Base\PublicEntity;
 
 class PaymentReconciliate extends Base\SubReconciliator\PaymentReconciliate
 {
+    const COLUMN_AMOUNT = 'amount_financed_rs';
+
+    const COLUMN_GATEWAY_TRANSACTION_ID = 'rrn';
+
     protected function getPaymentId(array $row)
     {
         return $row['asset_serial_numberimei'] ?? null;
+    }
+
+    protected function getReconPaymentAmount(array $row)
+    {
+        return Base\SubReconciliator\Helper::getIntegerFormattedAmount($row[self::COLUMN_AMOUNT]) ?? null;
+    }
+
+    protected function getGatewayTransactionId(array $row)
+    {
+        if (isset($row[self::COLUMN_GATEWAY_TRANSACTION_ID]) === false)
+        {
+            return null;
+        }
+
+        return trim($row[self::COLUMN_GATEWAY_TRANSACTION_ID]);
+    }
+
+    protected function setGatewayTransactionId(string $gatewayTransactionId, PublicEntity $gatewayPayment)
+    {
+        $body = $gatewayPayment->getData();
+
+        $body['DealID'] = $gatewayTransactionId;
+
+        $attributes['raw'] = json_encode($body);
+
+        $gatewayPayment->fill($attributes);
+    }
+
+    protected function validatePaymentAmountEqualsReconAmount(array $row)
+    {
+        $convertCurrency = $this->payment->getConvertCurrency();
+
+        $paymentAmount = ($convertCurrency === true) ? $this->payment->getBaseAmount() : $this->payment->getAmount();
+
+        if ($paymentAmount !== $this->getReconPaymentAmount($row))
+        {
+            $this->messenger->raiseReconAlert(
+                [
+                    'trace_code'        => TraceCode::RECON_INFO_ALERT,
+                    'info_code'         => Base\InfoCode::AMOUNT_MISMATCH,
+                    'payment_id'        => $this->payment->getId(),
+                    'expected_amount'   => $paymentAmount,
+                    'recon_amount'      => $this->getReconPaymentAmount($row),
+                    'currency'          => $this->payment->getCurrency(),
+                    'gateway'           => $this->gateway
+                ]);
+
+            return false;
+        }
+
+        return true;
     }
 }
