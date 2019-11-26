7965affaf7920a8d4526a605b09b890222e2912d
Subhrajit Makur <Avyukth@users.noreply.github.com>
2019-11-26 13:17:20 +0530

    small fix (#14225)
    Entity quantity changed

--- tests/Functional/Admin/AdminTest.php
+++ tests/Functional/Admin/AdminTest.php
@@ -806,7 +806,7 @@ class AdminTest extends TestCase
 
         $this->assertCount(11, $result['fields']);
 
-        $this->assertCount(238, $result['entities']);
+        $this->assertCount(239, $result['entities']);
     }
 
     public function testFetchSoftDeletedEntityForAdmin()
