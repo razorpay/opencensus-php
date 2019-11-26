a95e6a5b15fc9cdfcf4bddd3bffdc298a2617b0e
Vishal Ashank <45755515+vishalashank@users.noreply.github.com>
2019-11-26 15:43:42 +0530

    Disable metric push for api_payment_captured_v1_bucket (#14226)

--- app/Models/Payment/Processor/Capture.php
+++ app/Models/Payment/Processor/Capture.php
@@ -437,7 +437,9 @@ trait Capture
 
         $this->notifyPaymentCaptured();
 
-        (new Payment\Metric)->pushCapturedMetrics($this->payment);
+        // temporarily disabling metric push for "api_payment_captured_v1_bucket"
+        //
+        //(new Payment\Metric)->pushCapturedMetrics($this->payment);
     }
 
     protected function callAndHandleCaptureOnGateway(array $data)
