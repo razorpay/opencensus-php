8f66686b54be519e30c63e17da1e4359c2b3cbc5
dvvavinash <dvv.avinash@gmail.com>
2019-11-27 13:47:36 +0530

    [Reminders]: Add false as a default argument

--- web/js/merchant/containers/Reminders/PaymentLinksSettings.js
+++ web/js/merchant/containers/Reminders/PaymentLinksSettings.js
@@ -183,10 +183,10 @@ export default class PaymentLinksSettings extends React.Component {
   }
 }
 
-function serializeConfig(item, disabled) {
+function serializeConfig(item, disabled = false) {
   return {
     label: item.title,
     value: item.id,
-    disabled: !!disabled,
+    disabled,
   };
 }
