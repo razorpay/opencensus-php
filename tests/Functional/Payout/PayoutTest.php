bb3da3e25e5d300b59dfa8d106680a3bb83db6c7
Debojyoti Chakraborty <debojyoti.chakraborty@razorpay.com>
2019-11-27 16:05:24 +0530

    adding dedupe test cases for bulk payout and fund_account

--- tests/Functional/Payout/PayoutTest.php
+++ tests/Functional/Payout/PayoutTest.php
@@ -1636,6 +1636,38 @@ class PayoutTest extends TestCase
         $this->startTest();
     }
 
+    public function testBulkPayoutWithSameContact()
+    {
+        $this->ba->batchAuth();
+
+        $headers = [
+            'HTTP_X_Batch_Id'    => 'C0zv9I46W4wiOq',
+        ];
+
+        // append headers
+        $this->testData[__FUNCTION__]['request']['server'] = $headers;
+
+        $response = $this->startTest();
+
+        $this->assertEquals($response['items'][0]['fund_account']['contact_id'], $response['items'][1]['fund_account']['contact_id']);
+    }
+
+    public function testBulkPayoutWithSameFundAccount()
+    {
+        $this->ba->batchAuth();
+
+        $headers = [
+            'HTTP_X_Batch_Id'    => 'C0zv9I46W4wiOq',
+        ];
+
+        // append headers
+        $this->testData[__FUNCTION__]['request']['server'] = $headers;
+
+        $response = $this->startTest();
+
+        $this->assertEquals($response['items'][0]['fund_account']['id'], $response['items'][1]['fund_account']['id']);
+    }
+
     public function testBulkPayoutWithSameIdempotencyandBatchId()
     {
         $this->ba->batchAuth();
