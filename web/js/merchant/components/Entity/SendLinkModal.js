dc3b64f4bffba8d63af8909fca7012455195416b
dvvavinash <dvv.avinash@gmail.com>
2019-11-29 15:19:34 +0530

    [Auth Link]: Hide prop details in DOM if not available

--- web/js/merchant/components/Entity/SendLinkModal.js
+++ web/js/merchant/components/Entity/SendLinkModal.js
@@ -17,6 +17,29 @@ import Alert from 'common/ui/Forms/Alert';
   }
 )
 export default class extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      _dirty: {
+        email: '1',
+        phone: '1',
+      },
+    };
+  }
+
+  onChange = e => {
+    this.setState({
+      _dirty: {
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
+
+  onSubmit = () => {
+    this.props.onSubmit(this.state._dirty);
+  };
+
   render() {
     const {
       isTestMode,
@@ -31,19 +54,32 @@ export default class extends React.PureComponent {
     return (
       <div class="SendLink--Modal">
         <ModalHeader title="Send Link" onCloseClick={closeModal} />
+
         <div class="modal-body">
           {description && <p>{description}</p>}
 
-          <Form class="full-span" onSubmit={this.props.onSubmit}>
-            <Input.Check name="email" defaultValue={'1'} fieldLabel={email} />
+          <Form class="full-span" onChange={this.onChange}>
+            {email && (
+              <Input.Check
+                name="email"
+                defaultValue={this.state._dirty.email}
+                fieldLabel={email}
+              />
+            )}
 
-            <Input.Check name="phone" defaultValue={'1'} fieldLabel={phone} />
+            {phone && (
+              <Input.Check
+                name="phone"
+                defaultValue={this.state._dirty.phone}
+                fieldLabel={phone}
+              />
+            )}
 
             {children}
 
             {isTestMode && (
               <Alert
-                class="m-b"
+                class="alert-sm"
                 type="warning"
                 message={testModeMessage}
                 showDismiss={false}
@@ -54,7 +90,7 @@ export default class extends React.PureComponent {
               type="submit"
               pendingState="Sending..."
               class="btn-block"
-              onClick={this.props.onSubmit}
+              onClick={this.onSubmit}
             >
               Send Link
             </AsyncBtn.Primary>
