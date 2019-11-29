fbaec35c4fbb7f4d4c3b2aa56eb0fc8b814da48d
KailasRazorpay <kailas.sheregar@razorpay.com>
2019-11-29 12:52:29 +0530

    Added filters for permission in worflows get

--- app/Http/Controllers/WorkflowController.php
+++ app/Http/Controllers/WorkflowController.php
@@ -92,12 +92,14 @@ class WorkflowController extends Controller
     {
         $input = Request::all();
         $permission = Request::get('permission');
+        $limit = Request::get('count');
+        $offset = Request::get('skip');
 
         $orgId = $this->ba
                       ->getAdmin()
                       ->getPublicOrgId();
 
-        $data = $this->service()->fetchMultiple($orgId, $input, $permission);
+        $data = $this->service()->fetchMultiple($orgId, $input, $permission ?? null, $limit ?? 10, $offset ?? 0);
 
         return ApiResponse::json($data);
     }
--- app/Models/Workflow/Service.php
+++ app/Models/Workflow/Service.php
@@ -38,11 +38,11 @@ class Service extends Base\Service
         return $response;
     }
 
-    public function fetchMultiple(string $orgId, array $input, $permission = null)
+    public function fetchMultiple(string $orgId, array $input, $permission = null, $limit = 10, $offset = 0)
     {
         Org\Entity::verifyIdAndStripSign($orgId);
 
-        $workflows = $this->repo->workflow->findByOrgId($orgId, $permission);
+        $workflows = $this->repo->workflow->findByOrgId($orgId, $permission, $limit, $offset);
 
         return $workflows->toArrayPublic();
     }
