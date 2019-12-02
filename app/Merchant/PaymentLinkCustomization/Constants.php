820bfe40fb3640e029495c47aba7eaee590bd900
dvvavinash <dvv.avinash@gmail.com>
2019-12-02 16:39:56 +0530

    [Payment Link]: Remove test purpose code

--- app/Merchant/PaymentLinkCustomization/Constants.php
+++ app/Merchant/PaymentLinkCustomization/Constants.php
@@ -4,7 +4,7 @@ namespace App\Merchant\PaymentLinkCustomization;
 
 class Constants
 {
-    const   TEST_MID = 'C0uw3CXseZPwmJ',
+    const   TEST_MID = '10000000000000',
             BHARTI_AXA = 'D2BsrUJVg04abr';
 
     const DEFAULT_EXPIRY = [
