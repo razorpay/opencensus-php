4984617a6b637d0a67968538b2cc61dca58d9b4e
dvvavinash <dvv.avinash@gmail.com>
2019-12-03 23:06:52 +0530

    [Auth Link]: Add analytics library

--- resources/views/invoice/auth_link.blade.php
+++ resources/views/invoice/auth_link.blade.php
@@ -24,4 +24,5 @@
 </script>
 <script type="text/javascript" src="https://checkout.razorpay.com/v1/checkout.js"></script>
 <script type="text/javascript" src="{{env('AWS_CF_CDN_URL')}}/static/auth_link/bundle.js"></script>
+<script type="text/javascript" src='https://cdn.razorpay.com/static/analytics/bundle.js' async></script>
 </html>
