4fadde7e7a6e22171f8f33719d6ea99ea1cf871a
Sai Neeraj Kanuri <neeraj.kanuri@razorpay.com>
2019-12-09 14:34:28 +0530

    [scrooge] : onboarding netbanking_allahabad

--- app/Models/Payment/Gateway.php
+++ app/Models/Payment/Gateway.php
@@ -641,6 +641,7 @@ class Gateway
         Payment\Gateway::NETBANKING_OBC,
         Payment\Gateway::NETBANKING_CANARA,
         Payment\Gateway::NETBANKING_CORPORATION,
+        Payment\Gateway::NETBANKING_ALLAHABAD,
     ];
 
     public static $scroogeFileBasedRefundGatewaysWithTimestamps = [
@@ -648,6 +649,7 @@ class Gateway
         Payment\Gateway::NETBANKING_OBC         => 1575484200,
         Payment\Gateway::NETBANKING_CANARA      => 1575484200,
         Payment\Gateway::NETBANKING_CORPORATION => 1575484200,
+        Payment\Gateway::NETBANKING_ALLAHABAD   => 1575484200,
     ];
 
     public static $channels = [
