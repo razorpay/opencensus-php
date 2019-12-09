99a81497837f7e5edc69093cb3ea10fd0cf32e55
Abhinav Venkatesh <abhinav.venkatesh@razorpay.com>
2019-12-09 17:46:03 +0530

    [payment processor] Add nbplus interface in processor

---
+++ app/Models/Payment/Processor/NbPlusService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace RZP\Models\Payment\Processor;
+
+use RZP\Exception;
+use RZP\Error\ErrorCode;
+use RZP\Gateway\Base\Action;
+
+trait NbPlusService
+{
+    public function callNbPlusServiceAction($payment, $gateway, $action, $gatewayData)
+    {
+        $response = $this->app['nbplus.payments']->action($gateway, $action, $gatewayData);
+
+        $this->handleNbPlusResponse($payment, $response);
+
+        // If action is verify we get verify trace data
+        if ($action === Action::VERIFY)
+        {
+            return $response;
+        }
+
+        return $response['data'];
+    }
+
+    protected function handleNbPlusResponse($payment, $response)
+    {
+        if (empty($response) === true)
+        {
+            throw new Exception\GatewayErrorException(
+                ErrorCode::BAD_REQUEST_PAYMENT_FAILED
+            );
+        }
+
+        $response = $this->app['nbplus.payments']->checkForErrors($response);
+    }
+}
