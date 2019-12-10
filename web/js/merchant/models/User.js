99d301e0a0fffe864689102e415850ef8563465a
aseemgupta <aseemgupta2625@gmail.com>
2019-12-10 19:05:57 +0530

    [Paymentpages]: Remove data handling of old payment pages

--- web/js/merchant/models/User.js
+++ web/js/merchant/models/User.js
@@ -413,11 +413,6 @@ export default class User {
     return this.getExpStatus('sellerapp_plus');
   }
 
-  // Payment pages multiple line items
-  get isPPMLIEnabled() {
-    return this.getExpStatus('paymentpages_mli');
-  }
-
   get isMobileHotjarSurveyEnabled() {
     return this.getExpStatus('mobile_hotjar_survey');
   }
