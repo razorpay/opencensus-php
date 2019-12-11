63cd87e3bade7269d7549d050fe3d0daa42051c9
thotakartheek004 <thotakartheek004@gmail.com>
2019-12-11 16:37:30 +0530

    Update Service.php

--- app/Models/Gateway/Terminal/Service.php
+++ app/Models/Gateway/Terminal/Service.php
@@ -181,7 +181,7 @@ class Service extends Base\Service
     protected function shouldCreateTerminal(bool $checkFeatureEnabled, $merchantId)
     {
         $isFunc = $this->app->environment(Environment::FUNC);
-                                                                   
+        
         if($isFunc == true){
             return false;                                                          
         }
