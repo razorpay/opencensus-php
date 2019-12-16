4a7dc57f92f27dd969adb65ac04282d11cb5223c
krishnaacc123 <krishnaacc123@gmail.com>
2019-12-16 16:57:07 +0530

    add import for function

--- web/js/merchant/containers/App.js
+++ web/js/merchant/containers/App.js
@@ -24,6 +24,7 @@ import { fetchFeaturesAjax } from 'merchant/reducers/config';
 import AddGST from 'merchant/views/Account/Profile/components/AddGST';
 import { fetchGST } from 'merchant/reducers/profile';
 import { fetchConfig } from 'merchant/reducers/config';
+import { fireAnalyticsEvents } from 'common/utils/googleAnalytics';
 import {
   resizeWindow,
   updateMerchantLiveTransactionFlag,
