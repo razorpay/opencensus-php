9ddf05aebae66313f4ffe1ecf51b2f9d6b6032e5
Albin George <albinin0002@gmail.com>
2019-12-16 12:04:23 +0530

    [paysecure] Don't send verify call if we don't have gateway trans id (#14503)
    * [paysecure] Don't send transaction status if trans id is empty
    
    * [paysecure] Add test case for not hitting trans status on gateway
    
    * [paysecure] Send payment verify exception if trans id is missing

--- tests/Functional/Gateway/Paysecure/PaysecureGatewayTest.php
+++ tests/Functional/Gateway/Paysecure/PaysecureGatewayTest.php
@@ -813,7 +813,7 @@ class PaysecureGatewayTest extends TestCase
             $this->doAuthPayment($this->payment);
         });
 
-        $data = $this->testData['testVerifyFailedPayment'];
+        $data = $this->testData['testVerifyAuthTimeoutPayment'];
 
         $payment = $this->getDbLastEntity('payment');
 
@@ -836,7 +836,7 @@ class PaysecureGatewayTest extends TestCase
         );
     }
 
-    public function testVerifyFailedPayment()
+    public function testVerifyAuthTimeoutPayment()
     {
         $this->mockServerContentFunction(
             function (&$content, $action = null)
@@ -880,6 +880,66 @@ class PaysecureGatewayTest extends TestCase
         $paysecure = $this->getDbLastEntityToArray('paysecure');
 
         $this->assertNotNull($paysecure['apprcode']);
+    }
+
+    public function testVerifyMissingCallbackPayment()
+    {
+        $this->mockServerContentFunction(
+            function (&$content, $action = null)
+            {
+                if ($action === 'initiate2')
+                {
+                    throw new GatewayTimeoutException('Timed out');
+                }
+            }
+        );
+
+        $data = $this->testData['testAuthorizeFailed'];
+
+        $this->runRequestResponseFlow($data, function()
+        {
+            $this->doAuthPayment($this->payment);
+        });
+
+        $this->mockServerContentFunction(
+            function (&$content, $action = null)
+            {
+                if ($action === 'transaction_status')
+                {
+                    // This is to ensure that the transaction status api is NOT hit on the gateway
+                    // If this exception is thrown, the "verified" field of payment would be failed
+                    throw new \Exception('Error from gateway');
+                }
+            }
+        );
+
+        $data = $this->testData[__FUNCTION__];
+
+        $this->runRequestResponseFlow(
+            $data,
+            function()
+            {
+                $payment = $this->getDbLastEntity('payment');
+
+                $this->verifyPayment($payment->getPublicId());
+            });
+
+        $payment = $this->getDbLastEntityToArray('payment');
+
+        $this->assertArraySelectiveEquals(
+            [
+                'method'        => 'card',
+                'gateway'       => $this->paymentEntityGateway,
+                'amount'        => 50000,
+                'status'        => 'failed',
+                'verified'      => null,
+            ],
+            $payment
+        );
+
+        $paysecure = $this->getDbLastEntityToArray('paysecure');
+
+        $this->assertNull($paysecure['apprcode']);
     }
 
     protected function assertSuccess($authResponse, $flow)
