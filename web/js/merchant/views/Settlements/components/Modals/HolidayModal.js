2051601675578fb4e0872aea7ba0d090924c4655
AakashRaina <aakashraina9@gmail.com>
2019-12-16 17:01:04 +0530

    Refactors holiday list

--- web/js/merchant/views/Settlements/components/Modals/HolidayModal.js
+++ web/js/merchant/views/Settlements/components/Modals/HolidayModal.js
@@ -1,30 +1,15 @@
 import { Component } from 'react';
 import { connect } from 'react-redux';
-import Alert from 'common/ui/Forms/Alert';
 import ModalHeader from 'common/ui/ModalHeader';
-import SettlementBreakupTable from 'merchant/views/Settlements/components/BreakupTable';
+import HolidaysTable from 'merchant/views/Settlements/components/HolidayTable';
 import { fetchBreakupDetails } from 'merchant/reducers/settlements/details';
 import * as ModalActions from 'merchant_common/reducers/modals';
 
-@connect(state => state.settlement.breakupDetails, {
+@connect(state => state.settlement, {
   fetchBreakupDetails,
   ...ModalActions,
 })
 export default class HolidayModal extends Component {
-  componentWillMount() {
-    this.props.fetchBreakupDetails({
-      id: this.props.settlementId,
-    });
-  }
-
-  componentDidMount() {
-    this.props.onMount && this.props.onMount(this.props.settlementId);
-  }
-
-  componentWillUnmount() {
-    this.props.onUnmount && this.props.onUnmount(this.props.settlementId);
-  }
-
   render() {
     const date = new Date();
     const year = date.getUTCFullYear().toString();
@@ -34,34 +19,10 @@ export default class HolidayModal extends Component {
           title={`Holidays List`}
           onCloseClick={this.props.closeModal}
         />
-
-        <div class="modal-body holiday">
-          <div class="table-responsive table">
-            <table class="holiday-table">
-              <thead>
-                <tr>
-                  <th>Date</th>
-                  <th>Description</th>
-                </tr>
-              </thead>
-              <tbody>
-                {this.props.data.data[year].map((d, i) => {
-                  return (
-                    <tr key={i}>
-                      <td class="holiday-td">{d.date}</td>
-                      <td class="holiday-td">{d.description}</td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
-          <div class="Modal__actions text-right">
-            <button class="btn btn-default" onClick={this.props.closeModal}>
-              Close
-            </button>
-          </div>
-        </div>
+        <HolidaysTable
+          items={this.props.holidayList.data[year]}
+          loading={false}
+        />
       </div>
     );
   }
