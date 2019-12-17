a8f0c6876985d4a3e65454278e5a582c49a13bbc
Anubhav Shrivastava <anubhav.shrivastava@razorpay.com>
2019-12-17 11:08:20 +0530

    removing $publicAuth

--- app/Models/PayoutLink/Entity.php
+++ app/Models/PayoutLink/Entity.php
@@ -113,16 +113,6 @@ class Entity extends Base\PublicEntity
         self::CANCELLED_AT
     ];
 
-    protected $publicAuth = [
-        self::ID,
-        self::STATUS,
-        self::AMOUNT,
-        self::CURRENCY,
-        self::DESCRIPTION,
-        self::RECEIPT,
-        self::CANCELLED_AT
-    ];
-
     protected $dates = [
         self::CREATED_AT,
         self::UPDATED_AT,
