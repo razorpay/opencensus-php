3be83d6676c8af10603416f4ce92406cec4284e4
Anubhav Shrivastava <anubhav.shrivastava@razorpay.com>
2019-12-17 11:12:55 +0530

    review comments

--- app/Error/PublicErrorDescription.php
+++ app/Error/PublicErrorDescription.php
@@ -794,7 +794,7 @@ class PublicErrorDescription
     const BAD_REQUEST_PAYOUT_MODE_NOT_SUPPORTED                                 = 'Mode is unsupported';
 
     // payout links
-    const BAD_REQUEST_CANNOT_GENERATE_OTP_WITHOUT_PHONE_AND_EMAIL              = 'Attached contact does not have either email or phone number';
+    const BAD_REQUEST_CANNOT_GENERATE_OTP_WITHOUT_PHONE_AND_EMAIL              = 'Attached contact neither has email or phone number';
     const BAD_REQUEST_CUSTOMER_OTP_DELIVERY_FAILED                             = 'Error in SMS/EMAIL delivery for OTP';
     const BAD_REQUEST_CUSTOMER_OTP_GENERATION_FAILED                           = 'Otp generation failed';
 
--- app/Models/PayoutLink/Validator.php
+++ app/Models/PayoutLink/Validator.php
@@ -42,7 +42,7 @@ class Validator extends Base\Validator
     ];
 
     protected static $verifyOtpRules = [
-        Entity::OTP     => 'required|string|max:6',
+        Entity::OTP     => 'required|string|min:4|max:6',
         Entity::CONTEXT => 'sometimes|string|min:5|max:10'
     ];
 }
