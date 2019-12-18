7af65005a33f0f817013f13458f0c298811ceb3c
akshaygupta475 <akshay.gupta@razorpay.com>
2019-12-18 18:59:30 +0530

    added rule create/update and delete inside transaction

--- app/Models/Gateway/Rule/Service.php
+++ app/Models/Gateway/Rule/Service.php
@@ -30,17 +30,19 @@ class Service extends Base\Service
 
         $rule = $this->repo->gateway_rule->findOrFailPublic($id);
 
-        $this->repo->deleteOrFail($rule);
+        $this->repo->transaction(function () use ($rule, $id)
+        {
+            try
+            {
+                $this->repo->deleteOrFail($rule);
 
-        // try catch added temporarily
-        try
-        {
-            $this->app->smartRouting->deleteGatewayRule($id, $rule->getGroup());
-        }
-        catch (\Throwable $e)
-        {
-            $this->trace->traceException($e, Trace::ERROR, TraceCode::SMART_ROUTING_SERVICE_ERROR);
-        }
+                $this->app->smartRouting->deleteGatewayRule($id, $rule->getGroup());
+            }
+            catch (\Throwable $e)
+            {
+                $this->trace->traceException($e,Trace::ERROR, TraceCode::SMART_ROUTING_SERVICE_ERROR);
+            }
+        });
 
         return $rule->toArrayDeleted();
     }
