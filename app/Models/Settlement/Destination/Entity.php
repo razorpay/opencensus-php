4b7e4494bef1400646db985edd3e60da735f0f02
ratanphayade <ratanphayade@gmail.com>
2019-12-20 14:04:05 +0530

    destination: settlement entity defination added

---
+++ app/Models/Settlement/Destination/Entity.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace RZP\Models\Settlement\Destination;
+
+use RZP\Models\Base;
+use RZP\Constants\Entity as EntityConstant;
+
+class Entity extends Base\PublicEntity
+{
+    const ID                 = 'id';
+    const SETTLEMENT_ID      = 'settlement_id';
+    const DESTINATION_TYPE   = 'destination_type';
+    const DESTINATION_ID     = 'destination_id';
+
+    protected $entity = EntityConstant::SETTLEMENT_DESTINATION;
+
+    protected $fillable = [
+        self::MERCHANT_ID,
+        self::SETTLEMENT_ID,
+        self::DESTINATION_TYPE,
+        self::DESTINATION_ID,
+    ];
+
+    protected $visible = [
+        self::ID,
+        self::MERCHANT_ID,
+        self::SETTLEMENT_ID,
+        self::DESTINATION_TYPE,
+        self::DESTINATION_ID,
+    ];
+
+    protected $public = [
+        self::ID,
+        self::MERCHANT_ID,
+        self::SETTLEMENT_ID,
+        self::DESTINATION_TYPE,
+        self::DESTINATION_ID,
+    ];
+
+    public function settlement()
+    {
+        return $this->belongsTo(
+            'RZP\Models\Settlement\Entity',
+            self::SETTLEMENT_ID);
+    }
+
+    public function destination()
+    {
+        return $this->morphTo('destination', self::DESTINATION_TYPE, self::DESTINATION_ID);
+    }
+}
