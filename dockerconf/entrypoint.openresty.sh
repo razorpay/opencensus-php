41acd601a432be15959a487cddbadaf104c2b685
srinidhis <srinidhi.s@razorpay.com>
2019-12-23 16:48:16 +0530

    typo fix

--- dockerconf/entrypoint.openresty.sh
+++ dockerconf/entrypoint.openresty.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-alohomora cast --region ap-south-1 --env "$APP_MODE" --app api "/usr/local/openresty/ngnix/sites/api.openresty.conf.j2"
+alohomora cast --region ap-south-1 --env "$APP_MODE" --app api "/usr/local/openresty/nginx/sites/api.openresty.conf.j2"
 
 #removing as sites only need valid files
-rm /usr/local/openresty/ngnix/sites/api.openresty.conf.j2
+rm /usr/local/openresty/nginx/sites/api.openresty.conf.j2
 
 nginx -g "daemon off;"
