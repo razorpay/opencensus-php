67faac953043e0fcd173abfa9bbedbad14a1055e
Albin George <albinin0002@gmail.com>
2019-12-23 17:38:49 +0530

    [fss_sbi] Add recon files

---
+++ app/Reconciliator/CardFssSbi/SubReconciliator/PaymentReconciliate.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace RZP\Reconciliator\CardFssSbi\SubReconciliator;
+
+use RZP\Reconciliator\Base;
+
+class PaymentReconciliate extends Base\SubReconciliator\PaymentReconciliate
+{
+
+}
