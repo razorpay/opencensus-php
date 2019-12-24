8d3345651f90cb08ec4f4e91f063494f58b9395a
Anubhav Shrivastava <anubhav.shrivastava@razorpay.com>
2019-12-24 10:15:39 +0530

    adding view file

---
+++ resources/views/payout_link/customer_hosted.blade.php
@@ -0,0 +1,41 @@
+<html>
+
+<link rel="stylesheet" type="text/css" href="https://x.razorpay.in/dist/payoutlinks.css">
+
+<body>
+    <div id="app"></div>
+    <script>
+        window.data={
+            primary_color_light: '#6CB845',
+            primary_color_dark: '#6aab3d',
+            primary_color: '#6CB845',
+            primary_light_color: {
+                r: 108,
+                g: 184,
+                b: 69,
+                o: 0.1,
+            },
+            logo:
+                'https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/072015/spotify_2015.png',
+            client: 'Zoomcar',
+            amount: {
+                rupee: '989',
+                paisa: '52',
+            },
+            userDetails: {
+                name: 'Aryanum Subramanium',
+                maskedPhone: '98* ** ***10',
+                maskedEmail: 'ar****@****.com',
+                description:
+                    'Some description that will be given in by the business. Should allow only a maximum number of characters here.',
+                referenceId: '1467890',
+            },
+            apiHost: 'http://bad88495.ngrok.io/v1/',
+            // apiHost: 'http://api.razorpay.in/',
+            payoutLinkId: 'poutlk_Dteuw6gFD6YmUT',
+        };
+
+    </script>
+    <script src="https://x.razorpay.in/dist/payoutlinks.js" defer></script>
+</body>
+</html>
