dcf8bbf52b1075b2be47cae730d1c79ad5e325af
abhinavv21 <31855138+abhinavv21@users.noreply.github.com>
2019-12-26 22:10:32 +0530

    [gateway] Add Enach intermediate diplay page (#14431)

--- app/Models/Feature/Constants.php
+++ app/Models/Feature/Constants.php
@@ -135,6 +135,7 @@ class Constants
     const EXPOSE_FA_VALIDATION_UTR        = 'expose_fa_validation_utr';
     const VALIDATE_MERCHANT_DOMAIN        = 'validate_merchant_domain';
     const GOOGLE_PAY_CARDS                = 'google_pay_cards';
+    const ENACH_INTERMEDIATE              = 'enach_intermediate';
     const SAVE_VPA                        = 'save_vpa';
 
     const PARTNER_ACTIVATE_MERCHANT       = 'partner_activate_merchant';
@@ -457,6 +458,7 @@ class Constants
         self::SKIP_WEBSITE_INTERNAT           => true,
         self::SHOW_CREDIT_SCORE               => true,
         self::AGGREGATOR_OAUTH_CLIENT         => true,
+        self::ENACH_INTERMEDIATE              => true,
         self::SAVE_VPA                        => true,
     ];
 
