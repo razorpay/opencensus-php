e0bb61c5cabe934922aa2c2beaddc4cce5ca4054
Anubhav Shrivastava <anubhav.shrivastava@razorpay.com>
2019-12-27 12:38:38 +0530

    updating payout_link fixture factory

--- tests/Functional/Fixtures/Factory/FactoryData.php
+++ tests/Functional/Fixtures/Factory/FactoryData.php
@@ -1205,6 +1205,7 @@ final class FactoryData
             'user_id'              => null,
             'currency'             => 'INR',
             'description'          => 'This is a test payout',
+            'purpose'              => 'refund',
             'receipt'              => 'Test Payout Receipt',
             'notes'                => null,
             'short_url'            => 'http=>//76594130.ngrok.io/i/mGs4ehe',
