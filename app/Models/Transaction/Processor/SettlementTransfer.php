b88727f14002aebbe6d49559e537f1e367151fc8
sagaraglawe <52568732+sagaraglawe@users.noreply.github.com>
2020-01-02 12:57:55 +0530

    Aggregate settlement schedule final (#14725)
    * transfer: settlement transfer create interface exposed
    
    * transaction: adds transaction create for settlement processor
    
    * refactor: adds backward compatability for settlement with transfer
    
    * [aggregate][settlement]:adding the schedule for settlement transfer
    
    * [aggregate][settlement]:self review
    
    * settlement_schedule_testcase
    
    * settlement_transfer_testcase_added
    
    * removed settlement_tranfer from payment method
    
    * fixed test
    
    * nit changes
    
    * nit changes
    
    * removed timstamp from method params
    
    Co-authored-by: Ratan Phayade <ratanphayade@users.noreply.github.com>
    Co-authored-by: Gyanendra Pratap Singh <gyan.pratap2695@gmail.com>

--- app/Models/Transaction/Processor/SettlementTransfer.php
+++ app/Models/Transaction/Processor/SettlementTransfer.php
@@ -5,6 +5,10 @@ namespace RZP\Models\Transaction\Processor;
 use Carbon\Carbon;
 
 use RZP\Constants\Timezone;
+use RZP\Models\Settlement\Holidays;
+use RZP\Models\Schedule\Task as ScheduleTask;
+use RZP\Models\Schedule\Library as ScheduleLibrary;
+
 
 class SettlementTransfer extends Base
 {
@@ -28,8 +32,9 @@ class SettlementTransfer extends Base
 
     public function updateTransaction()
     {
-        // TODO: calculate the settledAt from schedule
-        $settledAt = Carbon::now(Timezone::IST)->getTimestamp();
+        $merchantId = $this->source->getMerchantId();
+        
+        $settledAt = $this->getSettledAtTimestampForSettlementTransfer($merchantId);
 
         $this->txn->setSettledAt($settledAt);
         $this->txn->setGatewayFee(0);
@@ -43,5 +48,46 @@ class SettlementTransfer extends Base
         $this->merchantBalance = $this->source->balance;
 
         $this->repo->balance->lockForUpdateAndReload($this->merchantBalance);
+    }
+
+    public function getSettledAtTimestampForSettlementTransfer($merchantId, $international = false)
+    {
+        $merchant =  $this->repo->merchant->findOrFailPublic($merchantId);
+
+        $returnTime = null;
+
+        $scheduleTask = (new ScheduleTask\Core)->getSettlementTransferScheduleForMerchant($merchant);
+
+        $currentTimestamp = Carbon::now(Timezone::IST)->getTimestamp();
+
+        // use schedule from pivot schedule_task if defined and use next run from there
+        if ($scheduleTask !== null)
+        {
+            $schedule = $scheduleTask->schedule;
+
+            $nextRunAt = $scheduleTask->getNextRunAt();
+
+            $returnTime = ScheduleLibrary::getNextApplicableTime(
+                                                                $currentTimestamp,
+                                                                $schedule,
+                                                                $nextRunAt);
+        }
+        else
+        {
+            //if the merchant do not have the schedule assigned for the Settlement Transfer transactions
+            //then by default we are setting these at T+0 , 4pm for that Settlement Transfer to settle
+            $settled_at = Carbon::createFromTimestamp($currentTimestamp, Timezone::IST);
+
+            if($settled_at->hour >= Constants::FOUR_PM)
+            {
+                $settled_at = Holidays::getNextWorkingDay($settled_at);
+            }
+
+            $returnTime = $settled_at->startOfDay()
+                                     ->addHours(Constants::FOUR_PM)
+                                     ->getTimestamp();
+        }
+
+        return $returnTime;
     }
 }
