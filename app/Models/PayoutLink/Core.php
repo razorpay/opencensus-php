97df4a0304458f24d49bb12e14d66ef0d8cf97a1
Anubhav Shrivastava <anubhav.shrivastava@razorpay.com>
2020-01-03 15:53:12 +0530

    mask phone bug

--- app/Models/PayoutLink/Core.php
+++ app/Models/PayoutLink/Core.php
@@ -461,8 +461,7 @@ class Core extends Base\Core
                                          Trace::ERROR,
                                          TraceCode::INVALID_EMAIL_CANNOT_MASK,
                                          [
-                                             'email'      => $email,
-                                             'contact_id' => $contact->getId()
+                                             'email'      => $email
                                          ]
             );
         }
@@ -479,9 +478,9 @@ class Core extends Base\Core
 
         $phoneLen = strlen($phone);
 
-        return substr($phoneLen, 0, 2) .
+        return substr($phone, 0, 2) .
                str_repeat('*', $phoneLen - 4) .
-               substr($phone, $phoneLen - 2, $phoneLen - 1);
+               substr($phone, $phoneLen - 2, 2);
 
     }
 
