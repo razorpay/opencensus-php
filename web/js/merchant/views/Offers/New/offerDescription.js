4c083ca0814e54c1cc8b075d400506da2326fccb
Gourav <github@gourav.info>
2020-01-07 12:15:29 +0530

    [Offers CE-1064] Deferred renamed to cashback

--- web/js/merchant/views/Offers/New/offerDescription.js
+++ web/js/merchant/views/Offers/New/offerDescription.js
@@ -47,7 +47,7 @@ export default ({
         options={[
           { label: 'Please select', name: '' },
           { label: 'Instant', name: 'instant' },
-          { label: 'Deferred', name: 'deferred' },
+          { label: 'Cashback', name: 'deferred' },
           { label: 'Already Discounted', name: 'already_discounted' },
         ]}
         onChange={getFormOnChangeHandler()}
