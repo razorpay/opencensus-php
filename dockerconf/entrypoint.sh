4417f03519224d028a6c856e484a431d07adaa25
guptaankit015 <ankit.gupta@razorpay.com>
2020-01-07 15:37:47 +0530

    nbplus service url fix

--- dockerconf/entrypoint.sh
+++ dockerconf/entrypoint.sh
@@ -53,8 +53,8 @@ configure_dark(){
     echo "CORE_PAYMENT_SERVICE_TEST_URL=\"https://cps-dark-test.razorpay.com/v1/\"" >> ./environment/.env.production
     echo "CARD_PAYMENT_SERVICE_LIVE_URL=\"https://payments-card-dark.razorpay.com/v1/\"" >> ./environment/.env.production
     echo "CARD_PAYMENT_SERVICE_TEST_URL=\"https://payments-card-test-dark.razorpay.com/v1/\"" >> ./environment/.env.production
-    echo "NBPLUS_PAYMENT_SERVICE_LIVE_URL=\"https://payments-nbplus-live-dark.razorpay.com/v1/\"" >> ./environment/.env.production
-    echo "NBPLUS_PAYMENT_SERVICE_TEST_URL=\"https://payments-nbplus-test-dark.razorpay.com/v1/\"" >> ./environment/.env.production
+    echo "NBPLUS_PAYMENT_SERVICE_LIVE_URL=\"https://payments-nbplus-live-dark.razorpay.com/\"" >> ./environment/.env.production
+    echo "NBPLUS_PAYMENT_SERVICE_TEST_URL=\"https://payments-nbplus-test-dark.razorpay.com/\"" >> ./environment/.env.production
 }
 
 run_migration_job(){
