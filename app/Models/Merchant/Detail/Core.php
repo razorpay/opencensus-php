0c0b53d962991a31c1f2c619f2914930be38df4d
RISHABH ACHARYA <rishabh.acharya@razorpay.com>
2020-01-08 19:26:20 +0530

    Revert "update domain while updating website through edit merchant flow (admin dashboard) (#14812)" (#14832)
    This reverts commit 5d43251987e54a1a72e8f3b033e4a1815ea7d7fd.

--- app/Models/Merchant/Detail/Core.php
+++ app/Models/Merchant/Detail/Core.php
@@ -617,12 +617,9 @@ class Core extends Base\Core
             $data[Entity::TRANSACTION_REPORT_EMAIL] = implode(',', $input[Entity::TRANSACTION_REPORT_EMAIL]);
         }
 
-        if ((isset($input[Merchant\Entity::WEBSITE]) === true) and
-            ($merchant->getWebsite() !== $input[Merchant\Entity::WEBSITE]))
+        if (isset($input[Merchant\Entity::WEBSITE]) === true)
         {
             $data[Entity::BUSINESS_WEBSITE] = $input[Merchant\Entity::WEBSITE];
-
-            (new Merchant\Core())->updateWhitelistedDomain($merchant, $input);
         }
 
         if (empty($data) === false)
