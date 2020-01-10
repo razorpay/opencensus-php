0d3738a7716ec014435d7fdf9c913cecd8552a41
Kritika Khandelwal <51109002+khandelwalkritika@users.noreply.github.com>
2020-01-10 13:52:55 +0530

    Revert "Merge branch 'PAYAPPS-464-remove_razorx'" (#14869)
    This reverts commit 6c2899545cadcaf8a0cf589bd7bea243007ec1b7, reversing
    changes made to 79915b3a9db0430baacdccfd7c77f7262bafa8b8.

--- app/Models/Transfer/Core.php
+++ app/Models/Transfer/Core.php
@@ -111,6 +111,11 @@ class Core extends Base\Core
     {
         $transfers = new Base\Collection();
 
+        if ($this->isTransfersForOrderEnabled() === false)
+        {
+            return $transfers;
+        }
+
         foreach ($transferInput as $input)
         {
             $input[Entity::STATUS] = Status::CREATED;
@@ -475,6 +480,11 @@ class Core extends Base\Core
 
     public function validateTransfersInput(int $orderAmount, array $transfers)
     {
+        if ($this->isTransfersForOrderEnabled() === false)
+        {
+            return;
+        }
+
         $this->verifyFeatureAllowed(Feature\Constants::MARKETPLACE, $this->merchant);
 
         $this->validateMerchantForTransfer($this->merchant);
@@ -665,5 +675,15 @@ class Core extends Base\Core
         ];
 
         $this->app['events']->fire('api.transfer.processed', $eventPayload);
+    }
+
+    protected function isTransfersForOrderEnabled(): bool
+    {
+        $variant = $this->app->razorx->getTreatment($this->merchant->getId(),
+                                                    Merchant\RazorxTreatment::TRANSFERS_VIA_ORDER,
+                                                    $this->mode
+        );
+
+        return (strtolower($variant) === 'on');
     }
 }
