9ba0a24e0d91b5cc930b7a269336d5d0baadae26
Gaurav Kumar <53046360+w1p30ut@users.noreply.github.com>
2020-01-16 17:09:01 +0530

    [recon] Autoreconcile netbanking refunds  (#14871)
    * [recon] Autoreconcile netbanking refunds and its testcase

--- app/Models/Payment/Gateway.php
+++ app/Models/Payment/Gateway.php
@@ -1616,6 +1616,9 @@ class Gateway
         IFSC::CBIN => Gateway::NETBANKING_CBI,
         Netbanking::PUNB_R => Gateway::NETBANKING_PNB,
         Netbanking::BARB_R => Gateway::NETBANKING_BOB,
+        IFSC::CIUB => Gateway::NETBANKING_CUB,
+        IFSC::SIBL => Gateway::NETBANKING_SIB,
+        IFSC::YESB => Gateway::NETBANKING_YESB,
     ];
 
 
