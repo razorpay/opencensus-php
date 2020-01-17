16e88b9c2b0a7ffc63e69803982722f392565caa
Ambar <ambar.maini@razorpay.com>
2020-01-17 18:57:56 +0530

    remove payout link webhooks

--- app/Models/Merchant/Webhook/Event.php
+++ app/Models/Merchant/Webhook/Event.php
@@ -286,9 +286,7 @@ class Event
         self::PAYOUT_LINK_ISSUED                => 61,
         self::PAYOUT_LINK_PROCESSED             => 62,
         self::PAYOUT_LINK_PROCESSING            => 63,
-        self::PAYOUT_LINK_CANCELLED             => 64,
-        self::PAYOUT_LINK_ATTEMPTED             => 65,
-        self::PAYOUT_REJECTED                   => 66,
+        self::PAYOUT_REJECTED                   => 64,
     ];
 
     /**
@@ -355,8 +353,6 @@ class Event
         self::PAYOUT_LINK_ISSUED                => [Product::BANKING],
         self::PAYOUT_LINK_PROCESSED             => [Product::BANKING],
         self::PAYOUT_LINK_PROCESSING            => [Product::BANKING],
-        self::PAYOUT_LINK_CANCELLED             => [Product::BANKING],
-        self::PAYOUT_LINK_ATTEMPTED             => [Product::BANKING],
         self::PAYOUT_UPDATED                    => [Product::PRIMARY, Product::BANKING],
         self::PAYOUT_REJECTED                   => [Product::PRIMARY, Product::BANKING],
     ];
