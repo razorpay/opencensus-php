b6dbbe08287710b0d8440521def45eed9175e6ae
Ambar <ambar.maini@razorpay.com>
2020-01-17 20:08:17 +0530

    adding back the constants

--- app/Models/Merchant/Webhook/Event.php
+++ app/Models/Merchant/Webhook/Event.php
@@ -54,7 +54,7 @@ class Event
     const PAYOUT_QUEUED                     = 'payout.queued';
     const PAYOUT_INITIATED                  = 'payout.initiated';
     const PAYOUT_UPDATED                    = 'payout.updated';
-//    const PAYOUT_REJECTED                   = 'payout.rejected';
+    const PAYOUT_REJECTED                   = 'payout.rejected';
     const REFUND_SPEED_CHANGED              = 'refund.speed_changed';
     const REFUND_PROCESSED                  = 'refund.processed';
     const REFUND_FAILED                     = 'refund.failed';
@@ -78,7 +78,7 @@ class Event
     const PAYOUT_LINK_ISSUED                = 'payout_link.issued';
     const PAYOUT_LINK_PROCESSING            = 'payout_link.processing';
     const PAYOUT_LINK_ATTEMPTED             = 'payout_link.attempted';
-//    const PAYOUT_LINK_CANCELLED             = 'payout_link.cancelled';
+    const PAYOUT_LINK_CANCELLED             = 'payout_link.cancelled';
     const PAYOUT_LINK_PROCESSED             = 'payout_link.processed';
 
     protected static $events = [
@@ -192,7 +192,7 @@ class Event
         self::PAYOUT_QUEUED,
         self::PAYOUT_INITIATED,
         self::PAYOUT_UPDATED,
-//        self::PAYOUT_REJECTED,
+        self::PAYOUT_REJECTED,
         self::REFUND_SPEED_CHANGED,
         self::REFUND_PROCESSED,
         self::REFUND_FAILED,
@@ -216,7 +216,7 @@ class Event
         self::PAYOUT_LINK_ISSUED,
         self::PAYOUT_LINK_PROCESSED,
         self::PAYOUT_LINK_PROCESSING,
-//        self::PAYOUT_LINK_CANCELLED,
+        self::PAYOUT_LINK_CANCELLED,
         self::PAYOUT_LINK_ATTEMPTED
     ];
 
@@ -419,10 +419,10 @@ class Event
         self::PAYOUT_LINK_ISSUED                => Entity::PAYOUT_LINK,
         self::PAYOUT_LINK_PROCESSED             => Entity::PAYOUT_LINK,
         self::PAYOUT_LINK_PROCESSING            => Entity::PAYOUT_LINK,
-//        self::PAYOUT_LINK_CANCELLED             => Entity::PAYOUT_LINK,
+        self::PAYOUT_LINK_CANCELLED             => Entity::PAYOUT_LINK,
         self::PAYOUT_LINK_ATTEMPTED             => Entity::PAYOUT_LINK,
         self::PAYOUT_UPDATED                    => Entity::PAYOUT,
-//        self::PAYOUT_REJECTED                   => Entity::PAYOUT,
+        self::PAYOUT_REJECTED                   => Entity::PAYOUT,
     ];
 
     public static $eventsToFeatureMap = [
@@ -455,7 +455,7 @@ class Event
         self::TERMINAL_ACTIVATED                => Feature\Constants::TERMINAL_ONBOARDING,
         self::TERMINAL_FAILED                   => Feature\Constants::TERMINAL_ONBOARDING,
         self::PAYOUT_UPDATED                    => Feature\Constants::PAYOUT,
-//        self::PAYOUT_REJECTED                   => Feature\Constants::PAYOUT,
+        self::PAYOUT_REJECTED                   => Feature\Constants::PAYOUT,
     ];
 
     /**
