41962cc966c471bb6fbb1ebbbe62af6ce7fba8ac
gagan-chawara <53043164+gagan-chawara@users.noreply.github.com>
2020-01-20 13:43:02 +0530

    Prevent finance role from enabling es_automatic on merchant. (#14924)
    * Prevent finance role from enabling es_automatic on merchant.
    
    * Move out unnecessary functions from DB transaction
    
    * Format code
    
    * format code

--- app/Models/Merchant/Service.php
+++ app/Models/Merchant/Service.php
@@ -1821,45 +1821,45 @@ class Service extends Base\Service
 
     public function enableScheduledEs(): array
     {
-        $this->repo->transactionOnLiveAndTest(function ()
+        $userRole = $this->repo
+                         ->merchant
+                         ->getMerchantUserMapping(
+                            $this->merchant->getId(),
+                            $this->user->getId(),
+                            null,
+                            Product::PRIMARY)
+                         ->pivot
+                         ->role;
+
+        if (($userRole !== User\Role::ADMIN) and ($userRole !== User\Role::OWNER))
         {
-            $userRole = $this->repo
-                             ->merchant
-                             ->getMerchantUserMapping(
-                                 $this->merchant->getId(),
-                                 $this->user->getId(),
-                                 null,
-                                 Product::PRIMARY)
-                             ->pivot
-                             ->role;
+            throw new Exception\BadRequestException(ErrorCode::BAD_REQUEST_MERCHANT_USER_ACTION_NOT_SUPPORTED,
+                                                    'role',
+                                                    $userRole);
+        }
 
-            if (($userRole !== User\Role::ADMIN) and ($userRole !== User\Role::OWNER) and ($userRole !== User\Role::FINANCE))
-            {
-                throw new Exception\BadRequestException(ErrorCode::BAD_REQUEST_MERCHANT_USER_ACTION_NOT_SUPPORTED,
-                                                        'role',
-                                                        $userRole);
-            }
+        $this->getScheduledEarlySettlementPricingForMerchant();
 
-            $this->getScheduledEarlySettlementPricingForMerchant();
+        $schedule = (new Schedule\Repository)->getScheduleByPeriodIntervalAnchorHourDelayAndType(
+                                                Schedule\Period::HOURLY,
+                                                1,
+                                                null,
+                                                0,
+                                                0,
+                                                ScheduleTask\Type::SETTLEMENT);
 
-            $scheduledTasks = (new ScheduleTask\Core)->getMerchantSettlementScheduleTasks($this->merchant, false);
+        if ($schedule === null)
+        {
+            throw new Exception\LogicException(
+                'Schedule for Scheduled Automatic settlement was not found.',
+                ErrorCode::BAD_REQUEST_UNKNOWN_SCHEDULE
+            );
+        }
 
-            $schedule = (new Schedule\Repository)->getScheduleByPeriodIntervalAnchorHourDelayAndType(
-                                                    Schedule\Period::HOURLY,
-                                                    1,
-                                                    null,
-                                                    0,
-                                                    0,
-                                                    ScheduleTask\Type::SETTLEMENT);
+        $scheduledTasks = (new ScheduleTask\Core)->getMerchantSettlementScheduleTasks($this->merchant, false);
 
-            if ($schedule === null)
-            {
-                throw new Exception\LogicException(
-                    'Schedule for Scheduled Automatic settlement was not found.',
-                    ErrorCode::BAD_REQUEST_UNKNOWN_SCHEDULE
-                );
-            }
-
+        $this->repo->transactionOnLiveAndTest(function () use($schedule, $scheduledTasks)
+        {
             foreach ($scheduledTasks as $scheduledTask)
             {
                 $input = [
