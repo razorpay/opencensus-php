86039ecbf73c264997368edb15ba2a18fa4e9ac1
dvv.avinash <dvv.avinash@gmail.com>
2020-01-22 00:56:43 +0530

    [Auth Link]: Move analytics events to auth_link bundle

--- resources/views/invoice/auth_link.blade.php
+++ resources/views/invoice/auth_link.blade.php
@@ -19,72 +19,9 @@
         config: {
             api: "{{env('APP_URL')}}/"
         }
-    }
-
-    function noop() {}
-
-    window.rzpQ = {
-        interaction: noop,
-        push: noop,
-        now: function() {
-            return window.rzpQ;
-        },
-        defineEventModifiers:noop,
-        authLink: function() {
-            return window.rzpQ;
-        }
     };
-
-    function initAnalytics() {
-        if (!window.analytics || window.location.hostname.indexOf('razorpay.com') < 0) {
-            return;
-        }
-
-        window.analytics.init(['ga', 'lj'], {
-            lj: data.is_test_mode
-                ? '96df432a283745908a06f711acd9e5eb' // 'feb51cc8168711ea8d71362b9e155667' Please add this key when data analytics issue fixed
-                : '96df432a283745908a06f711acd9e5eb'
-        });
-
-        if (window.analytics.createQ) {
-            window.rzpQ = window.analytics.createQ({ pollFreq: 500 });
-        }
-
-        window.rzpQ.defineEventModifiers({
-            'authLink':[
-                {
-                    propertyName:'event_type',
-                    value:'charge_at_will'
-                },
-                {
-                    propertyName:'event_group',
-                    value:'charge_at_will_hosted_page'
-                },
-                {
-                    propertyName: 'page_id',
-                    value: data.invoice.id
-                }
-            ]
-        });
-
-        window.rzpQ.push(
-            window.rzpQ
-            .now()
-            .authLink()
-            .interaction('auth_link.payment.landed')
-        );
-    }
-
-    window.trackLink = function() {
-        window.rzpQ.push(
-            window.rzpQ
-            .now()
-            .authLink()
-            .interaction('auth_link.payment.redirect', { type: 'footer' })
-        );
-    }
 </script>
+<script type="text/javascript" src='https://cdn.razorpay.com/static/analytics/bundle.js' defer></script>
 <script type="text/javascript" src="https://checkout.razorpay.com/v1/checkout.js"></script>
-<script type="text/javascript" src="{{env('AWS_CF_CDN_URL')}}/static/auth_link/bundle.js"></script>
-<script type="text/javascript" onload="initAnalytics()" src='https://cdn.razorpay.com/static/analytics/bundle.js'></script>
+<script type="text/javascript" src="{{env('AWS_CF_CDN_URL')}}/static/auth_link/bundle.js" defer></script>
 </html>
