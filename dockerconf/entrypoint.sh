743245990606b1ecf9705c18157bb9596dde2ce3
srinidhis <srinidhi.s@razorpay.com>
2020-01-22 15:39:49 +0530

    removed httpd conf

--- dockerconf/entrypoint.sh
+++ dockerconf/entrypoint.sh
@@ -27,8 +27,6 @@ configure(){
   $ALOHOMORA_BIN cast --region ap-south-1 --env "$APP_MODE" --app api "environment/.env.vault.j2" "environment/env.php.j2" "dockerconf/api.apache.conf.j2"
   echo "copying apache config"
   cp dockerconf/api.apache.conf /etc/apache2/conf.d/api.conf
-  cp dockerconf/httpd.conf /etc/apache2/httpd.conf
-  
   ## Enable newrelic only for prod and perf
   if [[ "${APP_MODE}" == "prod" ]] || [[ "${APP_MODE}" == "perf" ]]; then
     $ALOHOMORA_BIN cast --region ap-south-1 --env "$APP_MODE" --app api "dockerconf/newrelic.ini.j2"
