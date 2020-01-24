850622d4dd5f0adca3c6075531bb653c7671fd1d
hemanth132 <45421294+hemanth132@users.noreply.github.com>
2020-01-24 09:51:34 +0530

    add support for external ids in merchant and legal entity (#14981)

--- app/Models/Merchant/Core.php
+++ app/Models/Merchant/Core.php
@@ -15,6 +15,7 @@ use RZP\Models\Base;
 use RZP\Models\User;
 use RZP\Jobs\EsSync;
 use RZP\Models\Batch;
+use RZP\Models\Partner;
 use RZP\Models\Pricing;
 use RZP\Constants\Mode;
 use RZP\Models\Feature;
@@ -177,6 +178,15 @@ class Core extends Base\Core
     {
         $aggregatorMerchant->getValidator()->validateSubMerchantInput($input, $linkedAccount);
 
+        // validate that external id passed is unique for that partner
+        if (empty($input[Entity::EXTERNAL_ID]) === false)
+        {
+            (new Partner\Core)->validateExternalIdForPartnerSubmerchant($aggregatorMerchant, $input[Entity::EXTERNAL_ID]);
+        }
+
+        $legalEntity           = null;
+        $externalLegalEntityId = null;
+
         $input['email'] = $input['email'] ?? $aggregatorMerchant->getEmail();
 
         if ($accountEntity === true)
@@ -186,6 +196,19 @@ class Core extends Base\Core
         else
         {
             $entity = new Entity;
+
+            if (empty($input[Entity::LEGAL_ENTITY_ID]) === false)
+            {
+                $legalEntity = $this->repo->legal_entity->findOrFailPublic($input[Entity::LEGAL_ENTITY_ID]);
+
+                unset($input[Entity::LEGAL_ENTITY_ID]);
+            }
+            else if (empty($input[Entity::LEGAL_EXTERNAL_ID]) === false)
+            {
+                $externalLegalEntityId = $input[Entity::LEGAL_EXTERNAL_ID];
+
+                unset($input[Entity::LEGAL_EXTERNAL_ID]);
+            }
         }
 
         $subMerchant = $entity->build($input);
@@ -221,13 +244,25 @@ class Core extends Base\Core
             $subMerchant->org()->associate($org);
         }
 
+        if (empty($legalEntity) === false)
+        {
+            $subMerchant->legalEntity()->associate($legalEntity);
+        }
+
         $this->repo->saveOrFail($subMerchant);
 
         $this->addMerchantSupportingEntities($subMerchant, $aggregatorMerchant);
 
         $this->syncHeimdallRelatedEntities($subMerchant, $input);
 
-        $this->upsertLegalEntity($subMerchant, []);
+        $legalEntityInput = [];
+
+        if (empty($externalLegalEntityId) === false)
+        {
+            $legalEntityInput[LegalEntity\Entity::EXTERNAL_ID] = $externalLegalEntityId;
+        }
+
+        $this->upsertLegalEntity($subMerchant, $legalEntityInput);
 
         return $subMerchant;
     }
