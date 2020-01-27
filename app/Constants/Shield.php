d6625d42d71b50542876e450a96f55e6e018641b
bluc <sourabhd2003@gmail.com>
2020-01-27 21:41:01 +0530

    Pass payment product to shield

--- app/Constants/Shield.php
+++ app/Constants/Shield.php
@@ -70,6 +70,18 @@ final class Shield
     const INVOICE_TYPE                 = 'invoice_type';
     const INVOICE_ENTITY_TYPE          = 'invoice_entity_type';
     const INTEGRATION                  = 'integration';
+    const PAYMENT_PRODUCT              = 'payment_product';
+
+    const PRODUCT_PAYMENT_GATEWAY       = 'payment_gateway';
+    const PRODUCT_PAYMENT_LINKS         = 'payment_links';
+    const PRODUCT_PAYMENT_INVOICES      = 'payment_invoices';
+    const PRODUCT_PAYMENT_EPOS          = 'payment_epos';
+    const PRODUCT_PAYMENT_SUBSCRIPTIONS = 'payment_subscriptions';
+    const PRODUCT_PAYMENT_PAGES         = 'payment_pages';
+    const PRODUCT_PAYMENT_ROUTE         = 'payment_route';
+    const PRODUCT_PAYMENT_SMART_COLLECT = 'payment_smart_collect';
+    const PRODUCT_PAYMENT_CAW           = 'payment_charge_at_will';
+
 
     // Response constants
     const ACTION_KEY              = 'action';
