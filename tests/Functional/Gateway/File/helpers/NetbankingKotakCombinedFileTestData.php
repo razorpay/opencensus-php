53340082c266557dcbe18def01d21b741858a139
Sai Neeraj Kanuri <neeraj.kanuri@razorpay.com>
2020-01-29 13:11:24 +0530

    [gateway_file][refund][kotak] : adding refund out of expected date range checks

--- tests/Functional/Gateway/File/helpers/NetbankingKotakCombinedFileTestData.php
+++ tests/Functional/Gateway/File/helpers/NetbankingKotakCombinedFileTestData.php
@@ -74,6 +74,76 @@ return [
         ],
     ],
 
+    'testGenerateKotakCombinedFileForNonTpvRefundsOutOfRange' => [
+        'request' => [
+            'content' => [
+                'type'     => 'combined',
+                'targets'  => ['kotak'],
+                'sub_type' => 'non_tpv',
+                'begin'    => Carbon::today(Timezone::IST)->getTimestamp(),
+                'end'      => Carbon::tomorrow(Timezone::IST)->getTimestamp()
+            ],
+            'url' => '/gateway/files',
+            'method' => 'POST'
+        ],
+        'response' => [
+            'content' => [
+                'entity' => 'collection',
+                'count' => 1,
+                'admin' => true,
+                'items' => [
+                    [
+                        'status'              => 'failed',
+                        'scheduled'           => true,
+                        'partially_processed' => false,
+                        'attempts'            => 1,
+                        'sender'              => 'refunds@razorpay.com',
+                        'type'                => 'combined',
+                        'sub_type'            => 'non_tpv',
+                        'target'              => 'kotak',
+                        'entity'              => 'gateway_file',
+                        'admin'               => true
+                    ],
+                ],
+            ],
+        ],
+    ],
+
+    'testGenerateKotakCombinedFileForTpvRefundsOutOfRange' => [
+        'request' => [
+            'content' => [
+                'type'     => 'combined',
+                'targets'  => ['kotak'],
+                'sub_type' => 'tpv',
+                'begin'    => Carbon::today(Timezone::IST)->timestamp,
+                'end'      => Carbon::tomorrow(Timezone::IST)->timestamp,
+            ],
+            'url' => '/gateway/files',
+            'method' => 'POST'
+        ],
+        'response' => [
+            'content' => [
+                'entity' => 'collection',
+                'count' => 1,
+                'admin' => true,
+                'items' => [
+                    [
+                        'status'              => 'failed',
+                        'scheduled'           => true,
+                        'partially_processed' => false,
+                        'attempts'            => 1,
+                        'sender'              => 'refunds@razorpay.com',
+                        'type'                => 'combined',
+                        'sub_type'            => 'tpv',
+                        'target'              => 'kotak',
+                        'entity'              => 'gateway_file',
+                        'admin'               => true
+                    ],
+                ],
+            ],
+        ],
+    ],
+
     'testGenerateTpvKotakRefundFile' => [
         'request' => [
             'content' => [
