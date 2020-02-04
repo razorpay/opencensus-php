28baedfbabf5416b79bc9797a034cb3c589e7c13
Arjun Tomer <arjun.tomer@razorpay.com>
2020-02-04 14:29:01 +0530

    changes to fix test case

--- tests/Functional/Payment/VerifyTest.php
+++ tests/Functional/Payment/VerifyTest.php
@@ -1372,7 +1372,7 @@ class VerifyTest extends TestCase
 
     protected function setupRedisMock($paymentArray = [])
     {
-        $redisMock = $this->getMockBuilder(Redis::class)->setMethods(['set', 'get', 'setex', 'client'])
+        $redisMock = $this->getMockBuilder(Redis::class)->setMethods(['set', 'get', 'setex', 'client', 'exists'])
                           ->getMock();
 
         Redis::shouldReceive('connection')
@@ -1392,9 +1392,6 @@ class VerifyTest extends TestCase
 
         Redis::shouldReceive('expire')
             ->andReturn(true);
-
-        Redis::shouldReceive('exists')
-            ->andReturn(0);
 
         $redisMock->method('set')->will($this->returnCallback(function ($resourceId, $requestId) use ($paymentArray)
         {
@@ -1424,6 +1421,8 @@ class VerifyTest extends TestCase
                 ->andReturn("control");
 
         $redisMock->method('get')->will($this->returnValue(''));
+
+        $redisMock->method('exists')->will($this->returnValue(0));
     }
 
     protected function setupRedisMockForBlockedGateway($paymentArray = [])
