c07d8ab42cff3a182493953ed9016d8d8285d2b9
Albin George <albinin0002@gmail.com>
2020-02-09 22:04:54 +0530

    TEMP COMMIT

--- public/test/obscure.php
+++ public/test/obscure.php
@@ -4,8 +4,8 @@
 
 $baseurl = $_SERVER['HTTP_HOST'] . '/v1';
 
-$key_id = $_GET['key'] ?? 'rzp_test_1DP5mmOlF5G5ag';
-$secret = 'thisissupersecret';
+$key_id = $_GET['key'] ?? 'rzp_test_NcTYsQW03hJokQ';
+$secret = 'm3aFDSxdKNuKyOffGlWNkJSq';
 
 $public_url = $baseurl;
 $private_url = $key_id.':'.$secret.'@'.$baseurl;
@@ -273,7 +273,7 @@ function disableEmptyInputs(form) {
     </tr>
     <tr>
         <td colspan="40"><b>Card No: </b> </td>
-        <td><input type="text" name="card[number]" value="6084851000013575" size="25" autocomplete="cc-number"></td>
+        <td><input type="text" name="card[number]" value="4160211000148214" size="25" autocomplete="cc-number"></td>
     </tr>
     <tr>
         <td colspan="40">CVV:</td>
