6abe514968293b685de33c7f3ba70551c3d2ee0b
Arjun Tomer <arjun.tomer@razorpay.com>
2020-02-11 11:31:34 +0530

    comment changes

--- app/Services/Stork.php
+++ app/Services/Stork.php
@@ -105,7 +105,7 @@ class Stork
             [],
             // Options and authentication for requests.
             [
-                'timeout' => 0.35, // Minimum possible value is 1 second.
+                'timeout' => 0.35, // Minimum possible value is 0.35 second.
                 'auth' => [$config['auth'][$mode]['user'], $config['auth'][$mode]['pass']],
             ]);
     }
