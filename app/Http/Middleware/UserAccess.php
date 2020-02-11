874d59f971d7b469e8a1697fac4e3774fb6516b6
Jayesh Pawar <jayesh.pawar@razorpay.com>
2020-02-11 12:55:25 +0530

    Revert "[acl_x] optimised permission check call to O(1); using php-ds library"
    This reverts commit 913909c6e982c03ef9dd255952a2174304cbcc4a.

--- app/Http/Middleware/UserAccess.php
+++ app/Http/Middleware/UserAccess.php
@@ -247,6 +247,8 @@ class UserAccess
 
         $userRole = $this->getUserRole();
 
+        $userRolePermissions = $this->getBankingRolePermissions($userRole);
+
         $routePermission = $this->getRoutePermission($route);
 
         // Allow route to all roles having wildcard permission
@@ -256,7 +258,7 @@ class UserAccess
         }
 
         // If role doesn't have route permission then deny otherwise allow
-        if ($this->userRolePermissionsMap->isInvalidPermission($userRole, $routePermission))
+        if (in_array($routePermission, $userRolePermissions, true) === false)
         {
             throw new BadRequestException(ErrorCode::BAD_REQUEST_UNAUTHORIZED);
         }
@@ -275,6 +277,24 @@ class UserAccess
         }
 
         return $userRole;
+    }
+
+    /**
+     * @param $role
+     *
+     * @return mixed|null
+     * @throws BadRequestException
+     */
+    private function getBankingRolePermissions($role)
+    {
+        $rolePermissions = $this->userRolePermissionsMap->getRolePermissions($role);
+
+        if ($rolePermissions === null)
+        {
+            throw new BadRequestException(ErrorCode::BAD_REQUEST_UNAUTHORIZED_USER_PERMISSIONS_MISSING);
+        }
+
+        return $rolePermissions;
     }
 
     /**
