951f39edf26df63177183ecbd7612b0f1aa60d9c
ranjithkotian <ranjith.kotian@razorpay.com>
2020-02-14 15:36:58 +0530

    adding auth link roles to list

--- web/js/common/ui/item/pair.js
+++ web/js/common/ui/item/pair.js
@@ -3,12 +3,13 @@ import * as id from './id';
 import { getAmount, getTime } from 'common/ui/item';
 import { makeIdLink } from 'common/ui/item/id';
 import { getIntervalCycle, subString } from 'common/utils/rzp-utils';
-import { roles, agentRole, RBLRoles } from 'merchant/helpers/data';
+import { roles, agentRole, RBLRoles, AuthLinkRoles } from 'merchant/helpers/data';
 
 const allRoles = {
   ...roles,
   ...agentRole,
   ...RBLRoles,
+  ...AuthLinkRoles,
 };
 import { RefundStatusLabel } from 'merchant/components/StatusLabel';
 
