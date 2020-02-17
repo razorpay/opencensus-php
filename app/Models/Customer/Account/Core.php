f17ec5f3f4ffcb06c850666ee848e74d43361f7e
kumar <jayesh.kumar@razorpay.com>
2020-02-17 18:29:47 +0530

    Renaming variable for SBB-177

--- app/Models/Customer/Account/Core.php
+++ app/Models/Customer/Account/Core.php
@@ -109,11 +109,11 @@ class Core extends Base\Core
      */
     protected function create(array $input, Merchant\Entity $merchant, $failOnDuplicate = true)
     {
-        $secureTrace = $input;
+        $inputTrace = $input;
 
-        unset($secureTrace['name'], $secureTrace['email'], $secureTrace['contact']);
+        unset($inputTrace['name'], $inputTrace['email'], $inputTrace['contact']);
 
-        $this->trace->info(TraceCode::CUSTOMER_CREATE, $secureTrace);
+        $this->trace->info(TraceCode::CUSTOMER_CREATE, $inputTrace);
 
         $customer = (new Customer\Entity)->build($input);
 
