ff900639a5e5910aa58f74df66550b3a5b768020
Rohit Keshwani <rohitkeshwani07@gmail.com>
2020-02-17 13:01:26 +0530

    payment authorization event added

--- app/Models/Payment/Processor/Processor.php
+++ app/Models/Payment/Processor/Processor.php
@@ -1836,7 +1836,7 @@ class Processor
 
         (new Payment\Metric)->pushFailedMetrics($payment);
 
-        $this->eventPaymentFailed();
+        $this->eventPaymentFailed($exception);
 
         if ($this->merchant->isFeatureEnabled(Feature::PAYMENT_FAILURE_EMAIL) === true)
         {
@@ -1954,13 +1954,15 @@ class Processor
         $payment->setTwoFactorAuth($twoFactorAuth);
     }
 
-    protected function eventPaymentFailed()
+    protected function eventPaymentFailed($exception)
     {
         $eventPayload = [
             ApiEventSubscriber::MAIN => $this->payment
         ];
 
         $this->app['events']->fire('api.payment.failed', $eventPayload);
+
+        $this->app['diag']->trackPaymentEvent(EventCode::PAYMENT_AUTHORIZATION_FAILED, $this->payment, $exception);
     }
 
     protected function setPaymentError(Exception\BaseException $e, $traceCode)
