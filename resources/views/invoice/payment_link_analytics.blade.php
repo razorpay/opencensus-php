9b9c7ad154176aab4af563ae9979f20469613ed6
dvv.avinash <dvv.avinash@gmail.com>
2020-02-18 16:15:27 +0530

    [Payment Link]: Remove es6 syntax

--- resources/views/invoice/payment_link_analytics.blade.php
+++ resources/views/invoice/payment_link_analytics.blade.php
@@ -1,16 +1,14 @@
 <script>
   // Lamberjack analytics events
   function pushToRzpQ(event, event_options) {
-    var isProdWebsite = window.location.hostname.indexOf('razorpay.com') >= 0;
-
     var eventOptions = event_options || {};
-    eventOptions.mode = isProdWebsite ? 'live' : 'test';
+    eventOptions.mode = data.is_test_mode ? 'test' : 'live';
 
     window.rzpQ.push(
       window.rzpQ
       .now()
       .paymentLink()
-      .interaction(event, event_options)
+      .interaction(event, eventOptions)
     );
   }
 
