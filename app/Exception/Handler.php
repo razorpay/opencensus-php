ad93b8dfef0b1bc33615353bf0ba87ea5e5525c5
KailasRazorpay <kailas.sheregar@razorpay.com>
2020-02-21 19:25:48 +0530

    Obscures email and password in exception stack trace

--- app/Exception/Handler.php
+++ app/Exception/Handler.php
@@ -313,6 +313,8 @@ class Handler extends ExceptionHandler
          */
         $stack = explode("\n", $exception->getTraceAsString());
 
+        $stack = $this->hideSensitiveInformationFromStack($stack);
+
         if ($level === 0)
         {
             // Only trace 30 stack function calls if it's a zero level exception
@@ -531,5 +533,46 @@ class Handler extends ExceptionHandler
     {
         return ((config('app.debug') === true) or
                 ($this->ba->isDebugApp() === true));
+    }
+
+    protected function hideSensitiveInformationFromStack(array $stackArr)
+    {
+        $hideParams = [
+            [
+                'regex'     => '/(.*getUserByEmailAndVerifyPassword\(\')(.*)(\'\)$)/i',
+                'replace'   => '******\', \'******',
+            ]
+        ];
+
+        $obscuredStack = [];
+
+        foreach ($stackArr as $stackLine)
+        {
+            foreach ($hideParams as $key => $patternArr)
+            {
+                try
+                {
+                    $regex = $patternArr['regex'];
+                    $replace = $patternArr['replace'];
+
+                    $found = preg_match($regex, $stackLine, $matches);
+
+                    if ($found === 1)
+                    {
+                        $obscuredStack[] = $matches[1] . $replace . $matches[3];
+                    }
+                    else
+                    {
+                        $obscuredStack[] = $stackLine;
+                    }
+                }
+                catch (\Throwable $e)
+                {
+                    return $stackArr;
+                }
+            }
+        }
+
+        return $obscuredStack;
     }
 }
