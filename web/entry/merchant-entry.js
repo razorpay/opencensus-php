ffd28d9771f6ec490562586077688e5b2117ee68
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2020-02-25 16:06:58 +0530

    [FIX] add babel polyfill as package

--- web/entry/merchant-entry.js
+++ web/entry/merchant-entry.js
@@ -1,24 +1,24 @@
 (function() {
-  const base = Array.prototype.slice
+  var base = Array.prototype.slice
     .call(document.querySelectorAll('script[src]'), -1)[0]
     .src.replace(/[^\/]+$/, '');
 
-  const appendLink = function(src) {
-    const link = document.createElement('link');
+  var appendLink = function(src) {
+    var link = document.createElement('link');
     link.rel = 'stylesheet';
     link.href = src;
     document.documentElement.appendChild(link);
   };
 
   if (localStorage.referrer === 'chrome-extension') {
-    appendLink(`${base  }css/extension-popup.css`);
-    document.write(`<script src="${base}extension-entry.js"></script>`);
+    appendLink(base + 'css/extension-popup.css');
+    document.write('<script src="' + base + 'extension-entry.js"></script>');
     return;
   }
 
-  document.write(`<script src="${base}vendor_m.js"></script>`);
-  document.write(`<script src="${base}merchant.js"></script>`);
-  appendLink(`${base}css/merchant.css`);
+  document.write('<script src="' + base + 'vendor_m.js"></script>');
+  document.write('<script src="' + base + 'merchant.js"></script>');
+  appendLink(base + 'css/merchant.css');
   appendLink(
     'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
   );
--- web/entry/merchantLA-entry.js
+++ web/entry/merchantLA-entry.js
@@ -1,18 +1,18 @@
 (function() {
-  const base = Array.prototype.slice
+  var base = Array.prototype.slice
     .call(document.querySelectorAll('script[src]'), -1)[0]
     .src.replace(/[^\/]+$/, '');
 
-  const appendLink = function(src) {
-    const link = document.createElement('link');
+  var appendLink = function(src) {
+    var link = document.createElement('link');
     link.rel = 'stylesheet';
     link.href = src;
     document.documentElement.appendChild(link);
   };
 
-  document.write(`<script src="${base}vendor_m.js"></script>`);
-  document.write(`<script src="${base}merchantLA.js"></script>`);
-  appendLink(`${base}css/merchant-la.css`);
+  document.write('<script src="' + base + 'vendor_m.js"></script>');
+  document.write('<script src="' + base + 'merchantLA.js"></script>');
+  appendLink(base + 'css/merchant-la.css');
   appendLink(
     'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
   );
