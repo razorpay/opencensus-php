871e4c1b7d24320cff1a416c8aaa7c75a957be1f
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2020-02-25 14:12:26 +0530

    [FIX] add babel polyfill as package

--- web/entry/merchant-entry.js
+++ web/entry/merchant-entry.js
@@ -1,29 +1,24 @@
 (function() {
-  var base = Array.prototype.slice
+  const base = Array.prototype.slice
     .call(document.querySelectorAll('script[src]'), -1)[0]
     .src.replace(/[^\/]+$/, '');
 
-  var appendLink = function(src) {
-    var link = document.createElement('link');
+  const appendLink = function(src) {
+    const link = document.createElement('link');
     link.rel = 'stylesheet';
     link.href = src;
     document.documentElement.appendChild(link);
   };
 
   if (localStorage.referrer === 'chrome-extension') {
-    appendLink(base + 'css/extension-popup.css');
-    document.write('<script src="' + base + 'extension-entry.js"></script>');
+    appendLink(`${base  }css/extension-popup.css`);
+    document.write(`<script src="${base}extension-entry.js"></script>`);
     return;
   }
 
-  // polyfills for ie 10/11
-  document.write(
-    '<script src="https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.26.0/polyfill.min.js"></script>'
-  );
-
-  document.write('<script src="' + base + 'vendor_m.js"></script>');
-  document.write('<script src="' + base + 'merchant.js"></script>');
-  appendLink(base + 'css/merchant.css');
+  document.write(`<script src="${base}vendor_m.js"></script>`);
+  document.write(`<script src="${base}merchant.js"></script>`);
+  appendLink(`${base}css/merchant.css`);
   appendLink(
     'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
   );
