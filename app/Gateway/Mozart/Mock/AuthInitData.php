17bd37d2ff264bc522644f0a316e76a6ac8980d0
Albin George <albinin0002@gmail.com>
2020-03-02 22:51:34 +0530

    [hdfc_debit_emi] Rename mozart actions

--- app/Gateway/Mozart/Mock/AuthInitData.php
+++ app/Gateway/Mozart/Mock/AuthInitData.php
@@ -8,39 +8,6 @@ class AuthInitData extends Base\Mock\Server
 {
     use Base\Mock\GatewayTrait;
 
-    public function hdfc_debit_emi($entities)
-    {
-        return [
-            'data' =>
-                [
-                    'status'                     => 'OTP_sent',
-                    'BankReferenceNo'            => 'abc123456',
-                    'MerchantReferenceNo'        => $entities['payment']['id'],
-                    'AuthenticationErrorCode'    => '0000',
-                    'AuthenticationErrorMessage' => '',
-                    'EligibilityStatus'          => 'Yes',
-                    'Token'                      => '123456',
-                    '_raw'                       => '',
-                ],
-            'next' => [
-                'redirect' => [
-                    'content' => [
-                        'type' => 'otp',
-                        'next' => [
-                            'submit_otp',
-                        ]
-                    ],
-                    'method' => 'post',
-                    'url' => $entities['otpSubmitUrl'],
-                ]
-            ],
-            'error'             => null,
-            'success'           => true,
-            'mozart_id'         => '',
-            'external_trace_id' => '',
-        ];
-    }
-
     public function upi_mindgate($entities)
     {
         $response = [
