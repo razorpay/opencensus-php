52ac11a4da8b290dfbafc6c686c64a4768128ef9
Junaid Anwer <anwerj@users.noreply.github.com>
2020-03-05 14:56:23 +0530

    [P2P] Merchant reassign customer|With Admin (#14968)

--- app/Models/P2p/Device/Validator.php
+++ app/Models/P2p/Device/Validator.php
@@ -22,6 +22,7 @@ class Validator extends Base\Validator
     protected static $deregisterSuccessRules;
     protected static $updateWithActionRules;
     protected static $restoreDeviceRules;
+    protected static $reassignCustomerRules;
 
     public function rules()
     {
@@ -190,6 +191,16 @@ class Validator extends Base\Validator
             Vpa\Entity::DEFAULT     => 'sometimes|string|regex:/vpa_(\.*){14}/',
             'deleted'               => 'sometimes|array',
             'deleted.*'             => 'sometimes|string|regex:/vpa_(\.*){14}/',
+        ]);
+
+        return $rules;
+    }
+
+    public function makeReassignCustomerRules()
+    {
+        $rules = $this->makeRules([
+            Entity::CUSTOMER_ID     => 'required|string|regex:/cust_(\.*){14}/',
+            'forced'                => 'sometimes|boolean|in:0,1',
         ]);
 
         return $rules;
