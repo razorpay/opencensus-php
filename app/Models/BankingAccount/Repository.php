f3aa7e9d2097960a7b153657b1fee55fef4bf08d
roguexray007 <kunal.sikri@razorpay.com>
2020-03-07 18:05:08 +0530

    display balance on dashboard for CA

--- app/Models/BankingAccount/Repository.php
+++ app/Models/BankingAccount/Repository.php
@@ -74,7 +74,6 @@ class Repository extends Base\Repository
     public function getBankingAccountsWithBalance($merchantId)
     {
         return $this->newQuery()
-                    ->with(['balance:id,balance,currency'])
                     ->where(Entity::MERCHANT_ID, $merchantId)
                     ->get();
     }
