983fb1599edb3058c74b06dbb0e93541d607a088
Saimadhav Heblikar <saimadhavheblikar@gmail.com>
2020-03-15 14:53:32 +0530

    change the request library, set timeout to 100ms, add trace

--- app/Services/TerminalsService.php
+++ app/Services/TerminalsService.php
@@ -2,12 +2,12 @@
 
 namespace RZP\Services;
 
-use Requests;
 
 use RZP\Error\ErrorCode;
 use RZP\Trace\TraceCode;
 use RZP\Models\Terminal;
 use RZP\Models\Merchant;
+use RZP\Http\Request\Requests;
 use RZP\Exception\IntegrationException;
 
 class TerminalsService
@@ -22,6 +22,7 @@ class TerminalsService
 
     protected $baseUrl;
 
+    const TIMEOUT           = 0.1; // 100 milliseconds
 
     const URL               = 'url';
     const CONTENT           = 'content';
@@ -265,7 +266,10 @@ class TerminalsService
         ];
 
         return [
-            'auth' => $auth
+            'auth'            => $auth,
+            'timeout'         => self::TIMEOUT,
+            'connect_timeout' => self::TIMEOUT,
+            'show_trace'      => true,
         ];
     }
 
