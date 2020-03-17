3f76fb713f08589a374a17608322449492b3e7d0
akshaygupta475 <akshay.gupta@razorpay.com>
2020-03-17 12:33:03 +0530

    razorX always true

--- app/Models/Terminal/Selector.php
+++ app/Models/Terminal/Selector.php
@@ -785,6 +785,8 @@ class Selector extends Base\Core
 
     protected function shouldHitRoutingService(string $feature, string $paymentId = null)
     {
+        return true;
+
         $isProduction = $this->app->environment(Environment::PRODUCTION);
 
         if ($isProduction === false)
