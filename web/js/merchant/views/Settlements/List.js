7dd8da10f7dadd97374ccb04dc7bffc377d40e5f
Zameer Haque <20766601+haquezameer@users.noreply.github.com>
2020-03-23 11:16:57 +0530

    [ANNOUNCEMENT] Remove Yes Bank settlements hold banner (#4481)

--- web/js/merchant/views/Settlements/List.js
+++ web/js/merchant/views/Settlements/List.js
@@ -41,8 +41,6 @@ import SettlementSchedule from 'merchant/views/Settlements/components/Settlement
 import SettlementDetail from 'merchant/views/Settlements/components/SettlementDetail';
 import Time from 'common/ui/Time';
 
-import YesBankAnnouncementSettlements from 'merchant/components/Announcements/YesBankAnnouncements/YesBankAnnouncementSettlements';
-
 @withRouter
 @connect(
   state => ({
@@ -247,8 +245,6 @@ export default class SettlementsListContainer extends ListContainer {
 
     return (
       <React.Fragment>
-        {user.isYesBankMerchant && <YesBankAnnouncementSettlements />}
-
         {/* instant settlements banner */}
         {user.isISBannerEnabled && (
           <EarlySettlementsAnnouncement userId={user.current} />
