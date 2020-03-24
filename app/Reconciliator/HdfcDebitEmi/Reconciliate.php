496fd17e092e00985c15e3032f7893979b721f5c
Albin George <albinin0002@gmail.com>
2020-03-24 20:16:47 +0530

    [hdfc_debit_emi] Accept password protected zip files

--- app/Reconciliator/HdfcDebitEmi/Reconciliate.php
+++ app/Reconciliator/HdfcDebitEmi/Reconciliate.php
@@ -2,18 +2,34 @@
 
 namespace RZP\Reconciliator\HdfcDebitEmi;
 
+use Carbon\Carbon;
+use RZP\Constants\Timezone;
 use RZP\Reconciliator\Base;
 use RZP\Reconciliator\FileProcessor;
 
 class Reconciliate extends Base\Reconciliate
 {
+    // Override this from base, since we need to know the file name instead of the sheet name
+    protected function getFileName(array $extraDetails): string
+    {
+        return $extraDetails[FileProcessor::FILE_DETAILS][FileProcessor::FILE_NAME];
+    }
+
     protected function getTypeName($fileName)
     {
-        return self::COMBINED;
+        if (strpos($fileName, 'merchantreconcilationreport') === 0)
+        {
+            return self::COMBINED;
+        }
     }
 
     public function getFileType(string $mimeType): string
     {
         return FileProcessor::EXCEL;
+    }
+
+    public function getReconPassword($fileDetails)
+    {
+        return Carbon::now(Timezone::IST)->format('dmY');
     }
 }
