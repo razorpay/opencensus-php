1d5ba959fe7a9a35f1bdf9294b795878e8485354
Akshay Goyal <akshay.g174@gmail.com>
2020-03-24 13:12:47 +0530

    Hide PII data in contact create an update (#16006)

--- app/Models/Contact/Core.php
+++ app/Models/Contact/Core.php
@@ -24,7 +24,9 @@ class Core extends Base\Core
         bool $createDuplicate = false,
         bool $allowRZPFeesContactCreation = false): Entity
     {
-        $this->trace->info(TraceCode::CONTACT_CREATE_REQUEST, ['input' => $input]);
+        $traceData = $this->unsetPersonalIdentifiableInformation($input);
+
+        $this->trace->info(TraceCode::CONTACT_CREATE_REQUEST, ['input' => $traceData]);
 
         (new Validator)->validateInput('create', $input);
 
@@ -93,11 +95,13 @@ class Core extends Base\Core
 
     public function update(Entity $contact, array $input): Entity
     {
+        $traceData = $this->unsetPersonalIdentifiableInformation($input);
+
         $this->trace->info(
             TraceCode::CONTACT_UPDATE_REQUEST,
             [
                 'id'    => $contact->getId(),
-                'input' => $input,
+                'input' => $traceData,
             ]);
 
         (new Validator)->validateInput('edit', $input);
@@ -188,5 +192,20 @@ class Core extends Base\Core
         $contact = $this->create($contactData, $merchant, null, true, true);
 
         return $contact;
+    }
+
+    protected function unsetPersonalIdentifiableInformation(array $input): array
+    {
+        if (empty($input[Entity::CONTACT]) === false)
+        {
+            $input[Entity::CONTACT] = str_repeat('*', strlen($input[Entity::CONTACT]));
+        }
+
+        if (empty($input[Entity::EMAIL]) === false)
+        {
+            $input[Entity::EMAIL] = str_repeat('*', strlen($input[Entity::EMAIL]));
+        }
+
+        return $input;
     }
 }
