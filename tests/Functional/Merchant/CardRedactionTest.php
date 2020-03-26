7f26c484044ffa8f00f8b07f7b90abd07a417036
roguexray007 <kunal.sikri@razorpay.com>
2020-03-26 16:23:45 +0530

    add more test cases

--- tests/Functional/Merchant/CardRedactionTest.php
+++ tests/Functional/Merchant/CardRedactionTest.php
@@ -5,18 +5,37 @@ namespace Functional\Merchant;
 use Illuminate\Routing\Router;
 use RZP\Trace\ApiTraceProcessor;
 use RZP\Tests\Functional\TestCase;
+use RZP\Tests\Functional\RequestResponseFlowTrait;
 use RZP\Tests\Functional\Helpers\DbEntityFetchTrait;
 
 class CardRedactionTest extends TestCase
 {
     use DbEntityFetchTrait;
+    use RequestResponseFlowTrait;
 
     public function setUp()
     {
         parent::setUp();
     }
 
-    protected function mockRouter()
+    protected function setRegexViaRedis($regex)
+    {
+        $this->ba->adminAuth();
+
+        $request = [
+            'method'  => 'PUT',
+            'url'     => '/config/keys',
+            'content' => [
+                "config:credit_card_regex_for_redacting" => $regex
+            ]
+        ];
+
+        $response = $this->makeRequestAndGetContent($request);
+
+        return $response;
+    }
+
+    protected function mockRouter($route)
     {
         $routerMock = $this->getMockBuilder(Router::class)
                            ->setConstructorArgs([$this->app['events'],$this->app])
@@ -24,7 +43,7 @@ class CardRedactionTest extends TestCase
                            ->getMock();
 
         $routerMock->method('currentRouteName')
-                   ->willReturn('payout_create');
+                   ->willReturn($route);
 
         $this->app->instance('router', $routerMock);
     }
@@ -34,7 +53,7 @@ class CardRedactionTest extends TestCase
         /** @var ApiTraceProcessor $trace */
         $trace = new ApiTraceProcessor($this->app);
 
-        $this->mockRouter();
+        $this->mockRouter('payout_create');
 
         $record = [
             'context' => [
@@ -58,7 +77,7 @@ class CardRedactionTest extends TestCase
         /** @var ApiTraceProcessor $trace */
         $trace = new ApiTraceProcessor($this->app);
 
-        $this->mockRouter();
+        $this->mockRouter('payout_create');
 
         $record = [
             'context' => [
@@ -68,6 +87,7 @@ class CardRedactionTest extends TestCase
                 'account_number4' => '2700612396041683',
                 'account_number5' => '2720650392744411',
                 'account_number6' => '2719705517784880',
+                'account_number7' => '271970551778',
             ]
         ];
 
@@ -81,6 +101,7 @@ class CardRedactionTest extends TestCase
                 'account_number4' => "CARD_NUMBER_SCRUBBED(16)",
                 'account_number5' => "CARD_NUMBER_SCRUBBED(16)",
                 'account_number6' => "CARD_NUMBER_SCRUBBED(16)",
+                'account_number7' => "271970551778",
             ]
         ];
 
@@ -92,7 +113,7 @@ class CardRedactionTest extends TestCase
         /** @var ApiTraceProcessor $trace */
         $trace = new ApiTraceProcessor($this->app);
 
-        $this->mockRouter();
+        $this->mockRouter('payout_create');
 
         $record = [
             'context' => [
@@ -118,7 +139,7 @@ class CardRedactionTest extends TestCase
         /** @var ApiTraceProcessor $trace */
         $trace = new ApiTraceProcessor($this->app);
 
-        $this->mockRouter();
+        $this->mockRouter('payout_create');
 
         $record = [
             'context' => [
@@ -142,7 +163,7 @@ class CardRedactionTest extends TestCase
         /** @var ApiTraceProcessor $trace */
         $trace = new ApiTraceProcessor($this->app);
 
-        $this->mockRouter();
+        $this->mockRouter('payout_create');
 
         $record = [
             'context' => [
@@ -166,7 +187,7 @@ class CardRedactionTest extends TestCase
         /** @var ApiTraceProcessor $trace */
         $trace = new ApiTraceProcessor($this->app);
 
-        $this->mockRouter();
+        $this->mockRouter('payout_create');
 
         $record = [
             'context' => [
@@ -194,7 +215,7 @@ class CardRedactionTest extends TestCase
         /** @var ApiTraceProcessor $trace */
         $trace = new ApiTraceProcessor($this->app);
 
-        $this->mockRouter();
+        $this->mockRouter('payout_create');
 
         $record = [
             'context' => [
@@ -211,6 +232,120 @@ class CardRedactionTest extends TestCase
                 'account_number'  => "CARD_NUMBER_SCRUBBED(15)",
                 'account_number2' => "CARD_NUMBER_SCRUBBED(15)",
                 'account_number3' => "CARD_NUMBER_SCRUBBED(16)",
+            ]
+        ];
+
+        $this->assertArraySelectiveEquals($expectedResponse, $updatedRecord);
+    }
+
+    public function testVisaCardRedactionForNonBankingRoute()
+    {
+        /** @var ApiTraceProcessor $trace */
+        $trace = new ApiTraceProcessor($this->app);
+
+        $this->mockRouter('checkout');
+
+        $record = [
+            'context' => [
+                'account_number' => '4012888888881881',
+            ]
+        ];
+
+        $updatedRecord =  $trace($record);
+
+        $expectedResponse = [
+            'context' => [
+                'account_number' => "4012888888881881"
+            ]
+        ];
+
+        $this->assertArraySelectiveEquals($expectedResponse, $updatedRecord);
+    }
+
+    public function testRedactionViaRegexFromRedisWithRegexBeingDifferentThanCCNumberSeries()
+    {
+        /** @var ApiTraceProcessor $trace */
+        $trace = new ApiTraceProcessor($this->app);
+
+        // regex is for amex card
+        $regex = "/^3[47][0-9]{13}$/";
+
+        $response = $this->setRegexViaRedis($regex);
+
+        $record = [
+            'context' => [
+                'account_number' => '4012888888881881',  // visa card
+            ]
+        ];
+
+        $this->mockRouter('payout_create');
+
+        $updatedRecord =  $trace($record);
+
+        $expectedResponse = [
+            'context' => [
+                'account_number' => "4012888888881881"
+            ]
+        ];
+
+        $this->assertArraySelectiveEquals($expectedResponse, $updatedRecord);
+    }
+
+    public function testRedactionViaRegexFromRedisWithRegexBeingSameAsCCNumberSeries()
+    {
+        /** @var ApiTraceProcessor $trace */
+        $trace = new ApiTraceProcessor($this->app);
+
+        // regex is for amex card
+        $regex = "/^3[47][0-9]{13}$/";
+
+        $response = $this->setRegexViaRedis($regex);
+
+        $record = [
+            'context' => [
+                'account_number' => '371449635398431',  // amex card
+            ]
+        ];
+
+        $this->mockRouter('payout_create');
+
+        $updatedRecord =  $trace($record);
+
+        $expectedResponse = [
+            'context' => [
+                'account_number' => "CARD_NUMBER_SCRUBBED(15)"
+            ]
+        ];
+
+        $this->assertArraySelectiveEquals($expectedResponse, $updatedRecord);
+    }
+
+    public function testRedactionForNormalStrings()
+    {
+        /** @var ApiTraceProcessor $trace */
+        $trace = new ApiTraceProcessor($this->app);
+
+        $record = [
+            'context' => [
+                'account_number'  => '1234567891011',
+                'account_number2' => 'hehehehwwkwk',
+                'account_number3' => 'normalString',
+                'account_number4' => '9834728',
+                'visa card'       => '4012888888881881',
+            ]
+        ];
+
+        $this->mockRouter('payout_create');
+
+        $updatedRecord =  $trace($record);
+
+        $expectedResponse = [
+            'context' => [
+                'account_number'  => '1234567891011',
+                'account_number2' => 'hehehehwwkwk',
+                'account_number3' => 'normalString',
+                'account_number4' => '9834728',
+                'visa card'       => 'CARD_NUMBER_SCRUBBED(16)',
             ]
         ];
 
