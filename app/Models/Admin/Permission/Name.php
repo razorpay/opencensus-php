315b1abd6c5a49f2920b7b30057a1374af28c267
Jayesh Pawar <jayesh.pawar@razorpay.com>
2020-03-30 15:43:29 +0530

    fixing acl for owner role

--- app/Models/Admin/Permission/Name.php
+++ app/Models/Admin/Permission/Name.php
@@ -345,6 +345,9 @@ class Name
     const UPDATE_TEST_MERCHANT_BALANCE        = 'update_test_merchant_balance';
     const VIEW_VIRTUAL_ACCOUNT                = 'view_virtual_account';
     const EDIT_MERCHANT_INTERNATIONAL         = 'edit_merchant_international';
+    const UPLOAD_MERCHANT_DOCUMENT            = 'upload_merchant_document';
+    const DELETE_MERCHANT_DOCUMENT            = 'delete_merchant_document';
+    const UPDATE_MERCHANT_BANK_ACCOUNT_STATUS = 'merchant_bank_account_change_status';
 
     public static $actionMap = [
         Merchant\Action::ARCHIVE                            => self::EDIT_MERCHANT_ARCHIVE,
