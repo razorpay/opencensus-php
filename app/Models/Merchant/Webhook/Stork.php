1f7c6fe42a121485598622476ac4bfa303b05625
ArjunTomer <arjun.tomer@razorpay.com>
2020-03-31 17:40:42 +0530

    Revert "unset log params" (#16167)

--- app/Models/Merchant/Webhook/Stork.php
+++ app/Models/Merchant/Webhook/Stork.php
@@ -116,20 +116,7 @@ class Stork
      */
     public function processEvent(Event\Entity $event, string $mode)
     {
-        $eventTrace = $event->toArrayPublic();
-
-        if (is_array($eventTrace) === true)
-        {
-            unset($eventTrace['payload']['payment']['entity']['notes']['email'],
-                $eventTrace['payload']['payment']['entity']['notes']['phone'],
-                $eventTrace['payload']['payment']['entity']['notes']['address'],
-                $eventTrace['payload']['payment']['entity']['notes']['card_no'],
-                $eventTrace['payload']['payment']['entity']['notes']['exp'],
-                $eventTrace['payload']['payment']['entity']['notes']['cvv'],
-                $eventTrace['payload']['payment']['entity']['notes']['name_on_card']);
-        }
-
-        $this->trace->info(TraceCode::STORK_DISPATCH_EVENT_REQUEST, $eventTrace);
+        $this->trace->info(TraceCode::STORK_DISPATCH_EVENT_REQUEST, $event->toArrayPublic());
 
         $this->service->init($mode);
 
