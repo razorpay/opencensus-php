409b8b27337e3dab716c911cd710a0a9b2cf1963
Junaid Anwer <anwerj@users.noreply.github.com>
2020-03-31 12:26:51 +0530

    [Upi] Enable top PSPs for UPI Metric (#16149)

---
+++ app/Gateway/Upi/Base/ProviderPsp.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace RZP\Gateway\Upi\Base;
+
+class ProviderPsp
+{
+    const BHIM          = 'bhim';
+    const GOOGLE_PAY    = 'google_pay';
+    const PAYTM         = 'paytm';
+    const PHONEPE       = 'phonepe';
+    const WHATSAPP      = 'whatsapp';
+}
