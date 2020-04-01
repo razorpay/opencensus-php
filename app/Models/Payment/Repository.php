4bbdf4c41120c3d786256f153939254cfc447162
Rohit Keshwani <rohitkeshwani07@gmail.com>
2020-04-01 12:36:40 +0530

    small fix in query

--- app/Models/Payment/Repository.php
+++ app/Models/Payment/Repository.php
@@ -1859,7 +1859,7 @@ class Repository extends Base\Repository
         }
         else if ($key == DowntimeDetection::NETWORK)
         {
-            $query = $query->where(Card\Entity::NETWORK_CODE, $value);
+            $query = $query->where(Card\Entity::NETWORK, $value);
         }
         else
         {
