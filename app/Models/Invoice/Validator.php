7fc3a5c3d00584f1aa22f6a51d0644a35ca282a5
Abhay <abhayp@users.noreply.github.com>
2020-04-14 16:35:00 +0000

    skipping mutex if batch present (#16384)

--- app/Models/Invoice/Validator.php
+++ app/Models/Invoice/Validator.php
@@ -577,17 +577,20 @@ class Validator extends Base\Validator
 
             $merchant = $invoice->merchant;
 
-            $mutex =  App::getFacadeRoot()['api.mutex'];
-
-            $mutexAcquired = $mutex->acquire($merchant->getId()."-".$receipt, self::RECEIPT_MUTEX_TIMEOUT);
-
-            if ($mutexAcquired === false)
+            if ($invoice->hasBatch() === false)
             {
-                throw new BadRequestException(
-                    ErrorCode::BAD_REQUEST_INVOICE_RECEIPT_ANOTHER_OPERATION_IN_PROGRESS,
-                    null,
-                    ['resource' => $merchant->getId()."-".$receipt]
-                );
+                $mutex =  App::getFacadeRoot()['api.mutex'];
+
+                $mutexAcquired = $mutex->acquire($merchant->getId()."-".$receipt, self::RECEIPT_MUTEX_TIMEOUT);
+
+                if ($mutexAcquired === false)
+                {
+                    throw new BadRequestException(
+                        ErrorCode::BAD_REQUEST_INVOICE_RECEIPT_ANOTHER_OPERATION_IN_PROGRESS,
+                        null,
+                        ['resource' => $merchant->getId()."-".$receipt]
+                    );
+                }
             }
 
             $isDuplicateReceipt = app('repo')->invoice->isDuplicateReceipt($this->entity, $receipt);
