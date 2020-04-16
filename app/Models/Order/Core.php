365968bab69a8c9decf67818f6ca614a37042942
Jayesh Kumar <jayesh.kumar@razorpay.com>
2020-04-16 16:06:42 +0530

    receipt removed from input trace (#16425)

--- app/Models/Order/Core.php
+++ app/Models/Order/Core.php
@@ -27,7 +27,8 @@ class Core extends Base\Core
     {
         $inputTrace = $input;
 
-        unset($inputTrace['bank_account']['account_number'], $inputTrace['bank_account']['name'], $inputTrace['notes']);
+        unset($inputTrace['bank_account']['account_number'], $inputTrace['bank_account']['name'],
+                $inputTrace['notes'], $inputTrace['receipt'], $inputTrace['cardnumber']);
 
         $this->trace->info(
             TraceCode::ORDER_CREATE_REQUEST,
