4133a417b7fd95f01eae22cfc84720acc9efea6a
neerajkanuri <neeraj.kanuri@razorpay.com>
2020-04-20 13:16:17 +0530

    [pricing][refund] : logging refund pricing rules during selection with razorx check (#16478)

--- app/Models/Merchant/RazorxTreatment.php
+++ app/Models/Merchant/RazorxTreatment.php
@@ -109,4 +109,7 @@ final class RazorxTreatment
 
     // Check whether payout to amex cards is supported for a merchant ot not.
     const PAYOUT_TO_AMEX_CARDS = 'payout_to_amex_cards';
+
+    // Check whether refund pricing rules should be logged for merchant
+    const LOG_REFUND_PRICING_RULES = 'log_refund_pricing_rules';
 }
