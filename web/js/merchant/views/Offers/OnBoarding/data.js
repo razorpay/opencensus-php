fedb762b7f17fcbb3dce46e06f103b0858a47f68
Saransh Gupta <saranshgupta1995@gmail.com>
2020-04-27 13:05:14 +0530

    [Offers Onboarding] fix typos (#4686)

--- web/js/merchant/views/Offers/OnBoarding/data.js
+++ web/js/merchant/views/Offers/OnBoarding/data.js
@@ -3,11 +3,11 @@ export const FEATURES_DATA = [
   {
     icon: '/dist/css/assets/product_onboarding/offers_payment_methods.svg',
     title: 'Offers Across All Payment Methods',
-    desc: `Run offers across all payment methods or for speciic banks, card networks and wallets.`,
+    desc: `Run offers across all payment methods or for specific banks, card networks and wallets.`,
   },
   {
     icon: '/dist/css/assets/product_onboarding/offers_discount_type.svg',
-    title: 'Costomise Discount Type',
+    title: 'Customise Discount Type',
     desc:
       'Give your customers flat discounts or percentage discounts depending on your promotional scheme.',
   },
