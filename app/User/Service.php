b807ed89dbb5c3957d6620ed00c8d3623c816739
Akshay Goyal <akshay.g174@gmail.com>
2020-04-28 12:10:50 +0530

    Remove unused token from users API body (#4668)

--- app/User/Service.php
+++ app/User/Service.php
@@ -463,8 +463,6 @@ class Service extends Base\Service
 
         $data['user'] = $userDetails;
 
-        $data['_token'] = \Request::getSession()->token();
-
         $activated = false;
 
         // Default values in case no merchant is associated
