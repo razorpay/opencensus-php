55e99a947a9a4c319edbe98e4ee8c5e6567b0bb3
Rohit Keshwani <rohitkeshwani07@users.noreply.github.com>
2020-05-06 09:32:52 +0530

    fixed test (#16739)

--- tests/Functional/Admin/AdminTest.php
+++ tests/Functional/Admin/AdminTest.php
@@ -806,7 +806,7 @@ class AdminTest extends TestCase
 
         $this->assertCount(11, $result['fields']);
 
-        $this->assertCount(267, $result['entities']);
+        $this->assertCount(268, $result['entities']);
     }
 
     public function testFetchSoftDeletedEntityForAdmin()
