03063e1dc1d0a685ff6c813b014d90f21fdbf465
Aseem Gupta <aseem2625@users.noreply.github.com>
2020-05-06 23:58:28 +0530

    [PaymentPages]: Change fetch fn of payment details (#4731)

--- web/js/merchant/views/PaymentPages/PaymentPages/Details/PaymentsList/index.js
+++ web/js/merchant/views/PaymentPages/PaymentPages/Details/PaymentsList/index.js
@@ -29,7 +29,7 @@ export default class PaymentsList extends ListContainer {
   }
 
   // Hook to modify fetchAll of ListContainer
-  fetchList = params => {
+  fetchEntityList = params => {
     return this.props.fetchAll({
       ...params,
       payment_link_id: this.props.paymentPageId,
