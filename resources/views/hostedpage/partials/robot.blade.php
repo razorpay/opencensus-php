411f01b9035bf357d2076c00cc57c279b4ceebd6
Abhay <abhayp@users.noreply.github.com>
2020-05-19 10:47:56 +0000

    adding nofollow if customer details present (#16928)

--- resources/views/hostedpage/partials/robot.blade.php
+++ resources/views/hostedpage/partials/robot.blade.php
@@ -1,5 +1,5 @@
 @if(isset($no_track) === true and $no_track === true)
-    <meta name="robots" content="noindex">
+    <meta name="robots" content="noindex,nofollow">
 @else
     <meta name="robots" content="index, follow">
 @endif
