e19357ec937c64575b8f2603f2efe273330ffd20
Nikhilesh Chamarthi <nikhilesh.chamarthi@razorpay.com>
2020-06-02 16:50:54 +0530

    [FTS] Configuring M2P for gateway pvt (#16580)

--- app/Models/Admin/Validator.php
+++ app/Models/Admin/Validator.php
@@ -218,7 +218,7 @@ class Validator extends Base\Validator
     ];
 
     protected static $mozartGatewayPvtRules = [
-        'gateway'            => 'required|string|in:citi,icici,yesbank_upi,yesbank,icici_imps,rbl',
+        'gateway'            => 'required|string|in:citi,icici,yesbank_upi,yesbank,icici_imps,rbl,m2p_ct',
         'action'             => 'required|string|in:gateway_auth,transfer_init,transfer_status,beneficiary_verify,beneficiary_register,registration,account_balance',
         'namespace'          => 'required|string',
         'payload'            => 'required|array',
