4b87c5f505e230dd74c622d2f2e94537034dcf6d
Rakshitwalia16 <rakshit.walia@razorpay.com>
2020-06-04 16:25:47 +0530

    bvt env support for skip form validation (#17225)

--- app/Models/Merchant/Detail/Validator.php
+++ app/Models/Merchant/Detail/Validator.php
@@ -361,7 +361,7 @@ class Validator extends Base\Validator
 
     public function isValidAutomationEnv(): bool
     {
-        return ($this->env === 'func' or $this->env === 'automation');
+        return ($this->env === 'func' or $this->env === 'automation' or $this->env === 'bvt' );
     }
 
     public function validateBankDetailsVerificationStatus($attribute, $value)
