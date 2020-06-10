807b930c7dd3f4973d19c3460ead175043481e9e
gagan-chawara <53043164+gagan-chawara@users.noreply.github.com>
2020-06-10 14:25:32 +0530

    U/ufh admin get url (#17279)
    * fetch merchant_id from header in ufh_get_file_signed_url_admin route
    
    * take merchant_id from params instead of headers
    
    * use ufhService to get signed URL

--- app/Http/Controllers/UfhController.php
+++ app/Http/Controllers/UfhController.php
@@ -17,7 +17,20 @@ class UfhController extends Controller
 
     public function getSignedUrl(string $fileId)
     {
-        $response = $this->ufhClient()->getSignedUrl($fileId, []);
+        $input = Request::all();
+        $app = $this->app;
+        $ufhServiceMock = $app['config']->get('applications.ufh.mock');
+        $merchantId = isset($input['merchant_id']) == true ? $input['merchant_id'] : null;
+
+        if ($ufhServiceMock === true)
+        {
+            $ufhService = new MockUfhService($app, $merchantId);
+        }
+        else
+        {
+            $ufhService = new UfhService($app, $merchantId);
+        }
+        $response = $ufhService->getSignedUrl($fileId, []);
 
         return ApiResponse::json($response);
     }
