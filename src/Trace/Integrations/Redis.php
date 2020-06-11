a0454656b55a4fde05a70003da80685d2d9ebae8
Devi A S L <devi.a@razorpay.com>
2020-06-11 16:39:11 +0530

    add Redis integration

---
+++ src/Trace/Integrations/Redis.php
@@ -0,0 +1,88 @@
+<?php
+// from https://github.com/census-instrumentation/opencensus-php/pull/236/files
+// https://github.com/nrk/predis
+/**
+ * Copyright 2019 OpenCensus Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace OpenCensus\Trace\Integrations;
+
+use OpenCensus\Trace\Span;
+
+/**
+ * This class handles instrumenting Redis requests using the opencensus extension.
+ *
+ * Example:
+ * ```
+ * use OpenCensus\Trace\Integrations\Redis;
+ *
+ * Redis::load();
+ * ```
+ */
+
+class Redis implements IntegrationInterface
+{
+    /**
+     * Static method to add instrumentation to redis requests
+     */
+    public static function load()
+    {
+        if (!extension_loaded('opencensus')) {
+            trigger_error('opencensus extension required to load Redis integrations.', E_USER_WARNING);
+        }
+
+        opencensus_trace_method('Predis\Client', '__construct', [static::class, 'handleConstruct']);
+
+        opencensus_trace_method('Predis\Client', 'set', [static::class, 'handleCall']);
+
+        opencensus_trace_method('Predis\Client', 'get', [static::class, 'handleCall']);
+
+        opencensus_trace_method('Predis\Client', 'flushDB');
+
+    }
+
+    /**
+     * Trace Construct Options
+     *
+     * @param $predis
+     * @param  $params
+     * @return array
+     */
+    public static function handleConstruct($predis, $params)
+    {
+        return [
+            'attributes' => [
+                'host' => $params['host'],
+                'port' => $params['port']
+            ],
+            'kind' => Span::KIND_CLIENT
+        ];
+    }
+
+    /**
+     * Trace Set / Get Operations
+     *
+     * @param $predis
+     * @param  $method
+     * @return array
+     */
+    public static function handleCall($predis, $method)
+    {
+        return [
+            'attributes' => ['method' => $method],
+            'kind' => Span::KIND_CLIENT
+        ];
+    }
+}
