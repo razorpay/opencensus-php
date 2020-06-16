e03bb9bd1180b6615ab41c6d8cbcbb7ea86f5a91
Nikhilesh Chamarthi <nikhilesh.chamarthi@razorpay.com>
2020-06-16 10:38:33 +0530

    [FTS] Adding m2p channel in source account creation (#17355)

--- app/Services/FTS/Validator.php
+++ app/Services/FTS/Validator.php
@@ -13,7 +13,7 @@ class Validator extends Base\Validator
         'type'             => 'filled|string|required|in:bank_account,banking_account',
         'config'           => 'required|array',
         'product'          => 'filled|string|required|in:payout,refund,payout_refund,settlement,penny_testing,ca_payout,es_on_demand',
-        'channel'          => 'filled|string|required|in:yesbank,icici,citi',
+        'channel'          => 'filled|string|required|in:yesbank,icici,citi,m2p',
         'credentials'      => 'required|array',
         'mozartIdentifier' => 'filled|string|required|in:V1,V2',
         'sourceAccountType'=> 'filled|string|required|in:current,nodal'
