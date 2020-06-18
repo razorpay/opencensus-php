83356e4deb816fd2c42a46820fc4665a14cd6055
Krishna Kumar <krishnaacc123@gmail.com>
2020-06-18 11:46:33 +0530

    [Email]: change email template for otp based confirmation of signup (#17382)
    * [Email]: change email template for otp based confirmation of signup
    
    * [Email]: update email otp template

--- resources/views/emails/mjml/merchant/user/email_confirmation_via_link.blade.php
+++ resources/views/emails/mjml/merchant/user/email_confirmation_via_link.blade.php
@@ -173,7 +173,7 @@
               <td align="left" style="font-size:0px;padding:0px;word-break:break-word;">
 
       <div style="font-family:Trebuchet MS;font-size:13px;line-height:1;text-align:left;color:#000000;">
-        <div class="card title" style="width: 85%; width: calc(46000% - 211600px); max-width: 460px; min-width: 308px; margin-left: auto; margin-right: auto; box-sizing: border-box; padding-left: 16px; padding-right: 16px; border-radius: 2px; background-color: #FFFFFF; padding-bottom: 16px; border-top-left-radius: 0; border-top-right-radius: 0;"><div class="center-align font-size-normal font-color-primary" style="text-align: center; font-size: 16px; line-height: 1.5; color: #0D2366;">Verify your email</div></div>
+        <div class="card title" style="width: 85%; width: calc(46000% - 211600px); max-width: 460px; min-width: 308px; margin-left: auto; margin-right: auto; box-sizing: border-box; padding-left: 16px; padding-right: 16px; border-radius: 2px; background-color: #FFFFFF; padding-bottom: 16px; border-top-left-radius: 0; border-top-right-radius: 0;"><div class="center-align font-size-normal font-color-primary" style="text-align: center; font-size: 16px; line-height: 1.5; color: #0D2366;">Verify Your Email Address</div></div>
       </div>
 
               </td>
