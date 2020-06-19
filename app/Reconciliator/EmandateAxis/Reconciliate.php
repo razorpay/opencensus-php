e24a20548e44ab936654c359c34a3b3d0573296e
Mahesh Sakore <61787726+rzpmsakore@users.noreply.github.com>
2020-06-19 12:19:18 +0530

    Create Exclude file extension list for EmandateAxis (#17532)
    * added exclude list

--- app/Reconciliator/EmandateAxis/Reconciliate.php
+++ app/Reconciliator/EmandateAxis/Reconciliate.php
@@ -3,11 +3,22 @@
 namespace RZP\Reconciliator\EmandateAxis;
 
 use RZP\Reconciliator\Base;
+use RZP\Reconciliator\FileProcessor;
 
 class Reconciliate extends Base\Reconciliate
 {
     public function getTypeName($fileName)
     {
         return self::EMANDATE_DEBIT;
+    }
+
+    public function inExcludeList(array $fileDetails, array $inputDetails = [])
+    {
+        if (strpos($fileDetails[FileProcessor::EXTENSION], 'xls') !== false)
+        {
+            return false;
+        }
+
+        return true;
     }
 }
