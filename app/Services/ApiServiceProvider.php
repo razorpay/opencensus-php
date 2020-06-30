837aa3739997d646cf1bfdfe885637ebb5c3c6ba
Shashank agarwal <shashank.agarwal@razorpay.com>
2020-06-30 13:33:52 +0530

    Revert "shifted the mutex redis to redislabs (#17568)" (#17576)
    This reverts commit 935febf2ae6dfa75a6b43c4780abdf1929fb5c1f.

--- app/Services/ApiServiceProvider.php
+++ app/Services/ApiServiceProvider.php
@@ -669,7 +669,7 @@ class ApiServiceProvider extends BaseServiceProvider
 
             $mutex = new Mutex($app);
 
-            $mutex->setRedisClient(Redis::Connection());
+            $mutex->setRedisClient($this->app['redisdualwrite']);
 
             return $mutex;
         });
