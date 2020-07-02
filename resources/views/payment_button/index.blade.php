a58879b041fd92f068d0af775ef8c7804d8bc4e1
Aseem Gupta <aseem2625@users.noreply.github.com>
2020-07-02 19:22:43 +0530

    [PaymentButton]: Take cdn base from env variable (#17742)
    * [PaymentButton]: Take cdn base from env variable
    
    * [PaymentButton]: Take cdn base from env variable

--- resources/views/payment_button/index.blade.php
+++ resources/views/payment_button/index.blade.php
@@ -20,13 +20,7 @@
 
     <link
         rel="stylesheet"
-        href="https://betacdn.razorpay.com/static/widget/payment-form.css"
-    />
-    <!-- TODO: Lazy load fonts -->
-    <link
-        href="https://fonts.googleapis.com/css?family=Lato:300,400,700"
-        rel="stylesheet"
-        type="text/css"
+        href="{{env('AWS_CF_CDN_URL')}}/static/widget/payment-form.css"
     />
 
     @if (isset($data['environment']))
@@ -61,7 +55,7 @@
 
     <script
         type="text/javascript"
-        src="https://betacdn.razorpay.com/static/widget/payment-form.js"
+        src="{{env('AWS_CF_CDN_URL')}}/static/widget/payment-form.js"
         onload="loadRazorpayPaymentForm()"
         defer
     ></script>
