99bd0c030811f062c42e750c54363881a2808002
giliyarrazorpay <58775167+giliyarrazorpay@users.noreply.github.com>
2020-07-22 12:55:57 +0530

    razorx feature flag for microservice switch and service inactive state (#5120)

--- app/User/Service.php
+++ app/User/Service.php
@@ -1087,6 +1087,8 @@ class Service extends Base\Service
             'enable_payment_buttons',
             'enable_subscription_buttons',
             'batch_scheduling_options',
+            'rx_payout_links_inactive',
+            'rx_payout_links_ms',
         ];
 
         $experimentsResults = $merchantService->getBulkTreatment($features);
