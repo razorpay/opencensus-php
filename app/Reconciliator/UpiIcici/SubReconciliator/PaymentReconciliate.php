1f52c8f391deed05cfca30c9777ebb13341ea1b0
Gaurav Kumar <53046360+w1p30ut@users.noreply.github.com>
2020-07-31 11:42:39 +0530

    [recon] Using createfromformat as parse by default reads in mdy (#18257)

--- app/Reconciliator/UpiIcici/SubReconciliator/PaymentReconciliate.php
+++ app/Reconciliator/UpiIcici/SubReconciliator/PaymentReconciliate.php
@@ -202,7 +202,7 @@ class PaymentReconciliate extends Base\SubReconciliator\PaymentReconciliate
         $dateTime = $row[self::DATE] . ' ' . $row[self::TIME];
 
         // Format date to required format .i.e 20200703202700
-        $formattedDate = Carbon::parse($dateTime)->format('YdmHis');
+        $formattedDate = Carbon::createFromFormat('d/m/Y H:i A', $dateTime)->format('YdmHis');
 
         $callbackData[UpiIciciFields::TXN_INIT_DATE] = $formattedDate;
         $callbackData[UpiIciciFields::TXN_COMPLETION_DATE] = $formattedDate;
