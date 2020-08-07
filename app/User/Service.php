25e5254160f32991e7de60f2af9e0a9fe7e550da
Sumit Gupta <sumit.gupta@razorpay.com>
2020-08-07 12:24:54 +0530

    add feature flag for payout links onboarding revamp + feature flag for tax-payments app - payout flows (#5235)
    * add feature flag for payout links onboarding revamp
    
    * Update Service.php
    
     added rx_tax_payments_payout razorx feature flag  to service.php
    
    * add rx as prefix for RazorpayX experiments
    
    * Follow snake casing convention
    
    Co-authored-by: prabhasjoshi <54536909+prabhasjoshi@users.noreply.github.com>

--- app/User/Service.php
+++ app/User/Service.php
@@ -1090,6 +1090,8 @@ class Service extends Base\Service
             'rx_payout_links_ms',
             'upi_subscription',
             'rx_bulk_approvals',
+            'rx_payout_links_onboarding_revamp',
+            'rx_tax_payments_payout',
         ];
 
         $experimentsResults = $merchantService->getBulkTreatment($features);
