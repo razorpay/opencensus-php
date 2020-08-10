ae4c0a873e712ab98938de61662bf930ec91a965
Nick Isaacs <nick.isaacs@razorpay.com>
2020-08-10 10:32:50 +0530

    [RX-3099] Add the taxpayments enabled flag to user response (#5203)
    * add the taxpayments enabled flag to user response
    
    * check if settings exists
    
    * use boolval to check flag
    
    * use json_decode instead of boolval

--- app/User/Service.php
+++ app/User/Service.php
@@ -1187,6 +1187,19 @@ class Service extends Base\Service
             {
                 $data['banking_details']['is_live_payout_created'] = false;
             }
+
+            // check if tax payments is enabled
+            try {
+                if ($data['activation_status'] === 'activated')
+                {
+                    $isTaxPaymentsEnabled = $merchantService->isXTaxPaymentsEnabled();
+                    $data['banking_details']['is_tax_payments_enabled'] = $isTaxPaymentsEnabled;
+                }
+            }
+            catch (\Razorpay\Api\Errors\Error $e)
+            {
+                $data['banking_details']['is_tax_payments_enabled'] = false;
+            }
         }
 
         return $data;
