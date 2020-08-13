5c82c0c08744405b56dc8fa18b41e8c246c13422
Kritika Khandelwal <51109002+khandelwalkritika@users.noreply.github.com>
2020-08-13 19:44:23 +0530

    [PAYAPP-2539] Disabling UPI Icici recon for upi transfer (#18471)

--- app/Reconciliator/RequestProcessor/Base.php
+++ app/Reconciliator/RequestProcessor/Base.php
@@ -170,7 +170,7 @@ class Base extends Core
         self::JIOMONEY               => [],
         self::EBS                    => [],
         self::FIRST_DATA             => ['customer.care@icici.mailserv.in'],
-        self::UPI_ICICI              => ['eazypay@icicibank.com'],
+        self::UPI_ICICI              => [],
         self::VIRTUAL_ACC_KOTAK      => ['kmb.reports@kotak.com'],
         self::VIRTUAL_ACC_YESBANK    => ['ereport@yesbank.in'],
         self::VIRTUAL_ACC_RBL        => [],
