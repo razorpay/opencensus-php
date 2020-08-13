7eb37c06b9560e235a49e5059b83ccdeea67d616
Aseem Gupta <aseem2625@users.noreply.github.com>
2020-08-13 10:32:46 +0530

    [Reports]: Temp fix, Allowing using old reports for now (#5277)

--- web/js/merchant/reducers.js
+++ web/js/merchant/reducers.js
@@ -92,6 +92,7 @@ import {
 } from 'merchant/reducers/subscriptions';
 import { plansReducer, planReducer } from 'merchant/reducers/plans';
 import { addOnsReducer } from 'merchant/reducers/addons';
+import { reportsReducer } from 'merchant/reducers/reports';
 import LoanApplicationReducer from 'merchant/reducers/capital';
 
 import wysiwygReducer from 'merchant/reducers/wysiwyg';
@@ -157,6 +158,7 @@ export default combineReducers({
   virtualaccount: virtualAccountReducer,
   states: statesReducer,
   taxes: taxesReducer,
+  reports: reportsReducer,
   merchantReports: merchantReportsReducer,
   partnerReports: partnerReportsReducer,
   submerchants: submerchantsReducer,
