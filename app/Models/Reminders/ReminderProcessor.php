96c9175b1b3cca602aeddd62e47b340b4514f2f1
Gyanendra Pratap Singh <gyanendra.singh@razorpay.com>
2020-08-20 13:09:04 +0530

    RSR-1423 settlement reminder processor (#18569)
    * settlement reminder processor
    
    * nit change
    
    * single namespace for settlements

--- app/Models/Reminders/ReminderProcessor.php
+++ app/Models/Reminders/ReminderProcessor.php
@@ -29,12 +29,14 @@ abstract class ReminderProcessor
     const NEGATIVE_BALANCE          = 'negative_balance';
     const TERMINAL_CREATED_WEBHOOK  = 'terminal_created_webhook';
     const UPI_AUTO_RECURRING        = 'upi_auto_recurring';
+    const SETTLEMENTS               = 'settlements';
 
     const REMINDERS_API_NAMESPACE_PROCESSORS = [
         self::PAYMENT_LINK              => 'InvoiceReminderProcessor',
         self::NEGATIVE_BALANCE          => 'NegativeBalanceReminderProcessor',
         self::TERMINAL_CREATED_WEBHOOK  => 'TerminalCreatedWebhookReminderProcessor',
         self::UPI_AUTO_RECURRING        => 'UpiAutoRecurringReminderProcessor',
+        self::SETTLEMENTS               => 'SettlementReminderProcessor',
     ];
 
     public function __construct()
---
+++ app/Models/Reminders/SettlementReminderProcessor.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace RZP\Models\Reminders;
+
+use RZP\Trace\TraceCode;
+
+class SettlementReminderProcessor extends ReminderProcessor
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function process(string $entity, string $namespace, string $id, array $data)
+    {
+        $this->trace->info(TraceCode::SETTLEMENT_REMINDER_CALLBACK,
+            [
+                'entity'    => $entity,
+                'entity_id' => $id,
+                'namespace' => $namespace,
+                'data'      => $data,
+            ]
+        );
+
+        return app('settlements_reminder')->executionReminder($data);
+    }
+}
