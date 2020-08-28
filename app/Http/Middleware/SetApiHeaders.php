72bed13fe00904d2dc647c12809bd6c292891db2
Patlola Praveen Kumar Reddy <praveen.patlola@razorpay.com>
2020-08-28 15:36:46 +0530

    Revert "refactor: added logs to debug signup related issues" (#5435)
    This reverts commit c3d6fa0a6b4583ffe2845ad49e1a9e040b5c6365.

--- app/Http/Middleware/SetApiHeaders.php
+++ app/Http/Middleware/SetApiHeaders.php
@@ -6,7 +6,6 @@ use Session;
 use Closure;
 use App\Http\ApiUrl;
 use App\Http\Headers;
-use App\Trace\TraceCode;
 use Illuminate\Contracts\Auth\Guard;
 use Razorpay\Api\Request as ApiRequest;
 
@@ -56,13 +55,9 @@ class SetApiHeaders {
             Headers::CSRF_TOKEN => $csrfToken . ',' . $timeStamp,
         ];
 
-        app('trace')->info(TraceCode::USER_LOGIN_CREDS, ['session_id' => Session::getId()]);
-
         $response = $next($request);
 
         $response->withHeaders($csrfTokenHeader);
-
-        app('trace')->info(TraceCode::USER_LOGIN_CREDS, ['session_id' => Session::getId()]);
 
         return $response;
 	}
