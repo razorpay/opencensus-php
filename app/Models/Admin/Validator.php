14f0fe411f8501af4ba33d947aeed9b2f25d5846
rajatsingh555 <63587399+rajatsingh555@users.noreply.github.com>
2020-09-01 11:36:05 +0530

    Added axis to gateway_pvt (#18715)

--- app/Models/Admin/Validator.php
+++ app/Models/Admin/Validator.php
@@ -237,7 +237,7 @@ class Validator extends Base\Validator
     ];
 
     protected static $mozartGatewayPvtRules = [
-        'gateway'            => 'required|string|in:citi,icici,yesbank_upi,yesbank,icici_imps,rbl,m2p',
+        'gateway'            => 'required|string|in:citi,icici,yesbank_upi,yesbank,icici_imps,rbl,m2p,axis',
         'action'             => 'required|string|in:gateway_auth,transfer_init,transfer_status,beneficiary_verify,beneficiary_register,registration,account_balance',
         'namespace'          => 'required|string',
         'payload'            => 'required|array',
