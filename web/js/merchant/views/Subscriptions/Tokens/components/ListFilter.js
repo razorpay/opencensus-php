2136a42e7a31c2610a8bf6f5bf52e2190de8bf31
Avinash Devarakonda <dvv.avinash@gmail.com>
2020-09-02 15:11:05 +0530

    feat(Tokens)(PAYAPP-2564): add filters for tokens listing page (#5370)
    * feat(Tokens): add filters for tokens listing page
    
    * fix: rename payment filter id

--- web/js/merchant/views/Subscriptions/Tokens/components/ListFilter.js
+++ web/js/merchant/views/Subscriptions/Tokens/components/ListFilter.js
@@ -2,11 +2,21 @@ import { Field } from 'redux-form';
 
 import ListFilter from 'merchant/components/ListFilter';
 
-export default props => (
+export default (props) => (
   <ListFilter {...props}>
     <div class="form-group list-filter-item">
       <label>Token Id</label>
       <Field name="id" component="input" class="form-control input-sm" />
+    </div>
+
+    <div class="form-group list-filter-item">
+      <label>Payment Id</label>
+      <Field name="payment_id" component="input" class="form-control input-sm" />
+    </div>
+
+    <div class="form-group list-filter-item">
+      <label>Customer Contact</label>
+      <Field name="customer_contact" component="input" class="form-control input-sm" />
     </div>
 
     <div class="form-group list-filter-item count">
