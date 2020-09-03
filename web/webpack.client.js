37e12e31054860ac89f639d3695d08655d19fe5b
Prashant Chaudhary <prashant.chaudhary@razorpay.com>
2020-09-03 18:58:42 +0530

    fix(build): fixed minified entry file bug (#5499)

--- web/webpack.client.js
+++ web/webpack.client.js
@@ -116,7 +116,7 @@ module.exports = ({ config, project }) => {
             js : ${JSON.stringify(htmlWebpackPlugin.files.js)},
             css : ${JSON.stringify(htmlWebpackPlugin.files.css)}
           };
-          var __VERSION__ = ${htmlWebpackPlugin.options.version}
+          var __VERSION__ = ${htmlWebpackPlugin.options.version};
           ${require(`./entry/${project}-entry`)()}})()`;
       },
     }),
