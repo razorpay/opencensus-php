f3575578b3fb6c58af5eebe723f5d9e23d9ac962
NIDHI TANDON <nidhi-tandon@users.noreply.github.com>
2020-09-04 11:24:35 +0530

    feat(login): remove datalake event for incognito sessions (#5460)

--- public/js/merchant/controllers/authCtrl.js
+++ public/js/merchant/controllers/authCtrl.js
@@ -964,31 +964,6 @@ app
       function hideSpinner() {
         $('.loading-animation').removeClass('active');
       }
-
-      // @TODO: Remove this after launching google oauth
-      /**
-       * Function to fire datalake events
-       * This metrics is required to know how many users logs in via incognito
-       * As google oauth doesnt work in incognito, its decision metrics
-       * for the launch
-       */
-      function fireIncognitoEvent() {
-        if ('storage' in navigator && 'estimate' in navigator.storage) {
-          navigator.storage.estimate().then(function (res) {
-            if (res.quota < 1200000000) {
-              window.rzpQ.push(
-                window.rzpQ.now().onbr().success('login.incognito_session', {
-                  source: 'sign_in',
-                  sessionId: window.session_id,
-                  emailId: $scope.login.data.email,
-                  mode: $scope.eventsMode,
-                }),
-              );
-            }
-          });
-        }
-      }
-
       $scope.goToDashboard = function (data) {
         fireDLSuccessEvents('login.login', {
           source: 'sign_in',
