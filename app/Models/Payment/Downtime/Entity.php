923b419a6077db94a97f9b855f51b0b9e1c6e992
Paridhi Yadav <51924754+parryd@users.noreply.github.com>
2020-09-16 11:24:41 +0530

    setting vpa handle to ALL for overall upi downtime (#19029)

--- app/Models/Payment/Downtime/Entity.php
+++ app/Models/Payment/Downtime/Entity.php
@@ -159,7 +159,7 @@ class Entity extends Base\PublicEntity
                     $instrument[self::PSP] = $psp;
 
                 }
-                else if( empty($vpaHandle) === false && $vpaHandle != self::ALL)
+                else if( empty($vpaHandle) === false)
                 {
                     $instrument[self::VPA_HANDLE] = $vpaHandle;
                 }
