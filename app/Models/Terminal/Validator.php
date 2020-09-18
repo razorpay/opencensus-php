1b5545a4c412ab4cdfe6e64050fe86d3c97f0079
Shivam Yuvraj <shivam.yuvraj@razorpay.com>
2020-09-18 18:48:17 +0530

    made mode editable (#19101)

--- app/Models/Terminal/Validator.php
+++ app/Models/Terminal/Validator.php
@@ -467,6 +467,7 @@ class Validator extends Base\Validator
         Entity::IFSC_CODE                  => 'sometimes|string|size:11',
         Entity::STATUS                     => 'sometimes|string|in:deactivated,activated',
         Entity::PROCURER                   => 'sometimes|string|in:razorpay,merchant',
+        Entity::MODE                       => 'sometimes|in:2,3',
     ];
 
     protected static $mpgsEditTerminalRules = [
