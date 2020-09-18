a72e4065e0f4442174d35b1ea63cd2253161374a
Avinash Devarakonda <dvv.avinash@gmail.com>
2020-09-18 14:50:31 +0530

    Fix for wrong text in Batch Modal (#5609)

--- web/js/merchant/views/Subscriptions/Batch/components/RegistrationLinkBatchCreationForm.js
+++ web/js/merchant/views/Subscriptions/Batch/components/RegistrationLinkBatchCreationForm.js
@@ -5,25 +5,17 @@ import CheckBoxField from 'common/ui/Forms/CheckboxField';
 export default () => (
   <div>
     <h5 class="send-link-head">
-      <strong>SEND PAYMENT LINKS</strong>
+      <strong>SEND REGISTRATION LINKS</strong>
     </h5>
     <div class="form-group send-links-form">
       <div class="checkbox rzpCheckbox next m-r">
-        <Field
-          name="config.sms_notify"
-          id="sms_notify"
-          component={CheckBoxField}
-        />
+        <Field name="config.sms_notify" id="sms_notify" component={CheckBoxField} />
         <label for="sms_notify" class="icon i-check">
           Send SMS
         </label>
       </div>
       <div class="checkbox rzpCheckbox next m-r">
-        <Field
-          name="config.email_notify"
-          id="email_notify"
-          component={CheckBoxField}
-        />
+        <Field name="config.email_notify" id="email_notify" component={CheckBoxField} />
         <label for="email_notify" class="icon i-check">
           Send Email
         </label>
@@ -31,7 +23,7 @@ export default () => (
     </div>
     <p>
       <i class="i i-info-circle m-r" />
-      Payment Links with SMS and Email will be sent once the batch is created.
+      Registration Links with SMS and Email will be sent once the batch is created.
     </p>
   </div>
 );
