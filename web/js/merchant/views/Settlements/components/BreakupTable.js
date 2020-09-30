4bb5b57ce632742e08f6d06427ec9694fa5d9bef
Aakash  Raina <aakashraina9@gmail.com>
2020-09-30 16:46:08 +0530

    [Adhoc]Settlement breakup changes (#5712)
    * [Settlements] Minor refactor
    
    * [Settlements]Updated eslint rules
    
    * [Settlements] Refactors breakup table
    
    * [Settlements] Fixes amount issue for fee & tax
    
    * [Eslint] Reverts changes
    
    * [Settlements] Minor code review changes

--- web/js/merchant/views/Settlements/components/BreakupTable.js
+++ web/js/merchant/views/Settlements/components/BreakupTable.js
@@ -1,38 +1,80 @@
-import { titleCase } from 'common/utils/rzp-utils';
+import React from 'react';
 import Amount from 'common/ui/Amount';
 import TableBody from 'common/ui/TableBody';
+import Spinner from 'common/ui/Spinner';
 
-const Breakup = ({ breakup }) => {
+import { useCallback } from 'react';
+import { titleCase } from 'common/utils/rzp-utils';
+
+const isResponseNew = (obj) => {
+  delete obj.amountInINR;
+  delete obj.resourceUrl;
+  delete obj.resourceIdField;
+
+  let newResponse = false;
+
+  if ('tax' in obj && 'fee' in obj) newResponse = true;
+  else newResponse = false;
+
+  return {
+    columnNames: Object.keys(obj),
+    newResponse,
+  };
+};
+
+const Breakup = ({ breakup, newResponse }) => {
   return (
     <tr>
       <td>{titleCase(breakup.component)}</td>
       <td>
-        <Amount value={breakup.amount} currency={'INR'} />
+        <Amount value={breakup.amount} currency="INR" />
       </td>
       <td>{breakup.count}</td>
       <td>{titleCase(breakup.type)}</td>
+      {newResponse && (
+        <td>
+          <Amount value={breakup.fee} currency="INR" />
+        </td>
+      )}
+      {newResponse && (
+        <td>
+          <Amount value={breakup.tax} currency="INR" />
+        </td>
+      )}
     </tr>
   );
 };
 
-export default ({ items, loading }) => {
+const BreakupTable = ({ items, loading }) => {
+  // Table columns will be dynamic now, so adding check on items
+  if (items.length === 0) {
+    return (
+      <div class="page-spinner-container">
+        <Spinner />
+      </div>
+    );
+  }
+
+  const { newResponse, columnNames } = useCallback(isResponseNew(items[0]), [items]);
+
   return (
     <div class="table-reponsive">
       <table class="table table-hover">
         <thead>
           <tr>
-            <th>Component</th>
-            <th>Amount</th>
-            <th>Count</th>
-            <th>Type</th>
+            {columnNames.map((column, idx) => {
+              return <th key={idx}>{titleCase(column)}</th>;
+            })}
           </tr>
         </thead>
         <TableBody colSpan={4} isLoading={loading} rows={items}>
           {items.map((breakup, index) => (
-            <Breakup key={`breakup_${index}`} breakup={breakup} />
+            <Breakup key={`breakup_${index}`} breakup={breakup} newResponse={newResponse} />
           ))}
         </TableBody>
       </table>
     </div>
   );
 };
+
+export default BreakupTable;
