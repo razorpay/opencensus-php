75dcf62478e311fc98ae2ade51da224c8f48873b
likhitAgrawal27 <57517271+likhitAgrawal27@users.noreply.github.com>
2020-10-07 13:00:53 +0530

    Update ErrorCodeMapping.php (#19291)

--- app/Models/Payout/ErrorCodeMapping.php
+++ app/Models/Payout/ErrorCodeMapping.php
@@ -160,6 +160,7 @@ class ErrorCodeMapping
         'NPCI_TIMEOUT_FAILURE'                  => 'Timeout between NPCI and beneficiary bank. Please retry after 30 min.',
         'PBANK_VALIDATION_ERROR'                => 'Invalid Beneficiary details',
         'VERIFICATION_FAILED'                   => 'Payout failed. Contact support for help.',
+        'BBANK_GATEWAY_THROTTLED_FAILURE'       => 'Beneficiary bank is offline. Reinitiate transfer after 30 min.',
     ];
 
     public static function getErrorMessageFromBankResponseCode(Entity $payout, string $bankStatusCode = null)
