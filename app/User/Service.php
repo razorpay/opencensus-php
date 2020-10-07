6df25a33e8e2021136d37d9f2ceb20f67e23ff3d
rohitjha96 <67622107+rohitjha96@users.noreply.github.com>
2020-10-07 12:04:57 +0530

    skip workflow flag added (#5761)

--- app/User/Service.php
+++ app/User/Service.php
@@ -1362,6 +1362,7 @@ class Service extends Base\Service
             'rx_accounting_payouts_ask_clientID',
             'rx_ca_self_serve_flow',
             'enable_pl_batch_upload',
+            'skip_workflow_payout_specific_feature',
         ];
 
         $experimentsResults = $merchantService->getBulkTreatment($features);
