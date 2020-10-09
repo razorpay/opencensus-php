14f540817a10b333335e4934eedff110760d7cdd
Abhijit <abhijith.lal@razorpay.com>
2020-10-09 11:16:01 +0530

    [Submerchant/Invite] - increase the limit to 1 lakh (#19369)

--- app/Models/Batch/Limit.php
+++ app/Models/Batch/Limit.php
@@ -60,7 +60,7 @@ class Limit
         Type::TERMINAL_CREATION                     => 5000,
         Type::MERCHANT_ACTIVATION                   => 5000,
         Type::CAPTURE_SETTING                       => 1100000,
-        Type::PARTNER_SUBMERCHANT_INVITE            => 50000,
+        Type::PARTNER_SUBMERCHANT_INVITE            => 5000,
         Type::SUBMERCHANT_LINK                      => 50000,
         Type::NACH_MIGRATION                        => 500100,
     ];
--- app/Models/Partner/RateLimitBatch.php
+++ app/Models/Partner/RateLimitBatch.php
@@ -16,7 +16,7 @@ class RateLimitBatch extends Base\Core
 
     const RATE_LIMIT_TTL = 86400; // 24 hours
 
-    const THRESHOLD_RATE_LIMIT_COUNT = 10000;
+    const THRESHOLD_RATE_LIMIT_COUNT = 100000;
 
 
     public function __construct()
