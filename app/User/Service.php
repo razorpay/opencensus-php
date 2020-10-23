22f3b18c23045f88766fd0ef0f73811eda1cff46
Nick Isaacs <nick.isaacs@razorpay.com>
2020-10-23 13:03:30 +0530

    remove rx_vp_reporting flag (#5895)

--- app/User/Service.php
+++ app/User/Service.php
@@ -1364,7 +1364,6 @@ class Service extends Base\Service
             'rx_accounting_payouts_active',
             'rx_accounting_payouts_ask_clientID',
             'rx_ca_self_serve_flow',
-            'rx_vendor_payments_reporting',
             'enable_pl_batch_upload',
             'skip_workflow_payout_specific_feature',
             'low_balance_alert_frequency_30M',
