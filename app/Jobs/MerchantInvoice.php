f3232f097d381b08c45ee031c049a6d17f5e9c41
sagaraglawe <52568732+sagaraglawe@users.noreply.github.com>
2020-11-02 02:24:37 +0530

    [mi]:increase queue job time out to 45 minutes (#19694)

--- app/Jobs/MerchantInvoice.php
+++ app/Jobs/MerchantInvoice.php
@@ -15,11 +15,11 @@ class MerchantInvoice extends Job
 
     protected $year;
 
-    public $timeout = 1800;
+    public $timeout = 2700;
 
     const MERCHANT_INVOICE_MUTEX_RESOURCE = 'MERCHANT_INVOICE_CREATE_%s_%s_%s';
 
-    const MUTEX_LOCK_TIMEOUT = 1800;
+    const MUTEX_LOCK_TIMEOUT = 2700;
 
     public function __construct(
         string $merchantId,
