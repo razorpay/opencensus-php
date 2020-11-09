4dd45592221945ad59db69324acd97371dcd2149
Aakash  Raina <aakashraina9@gmail.com>
2020-11-09 14:42:48 +0530

    [Settlements] Fixes size for settlements breakup modal (#6062)

--- web/js/merchant/views/Settlements/List.js
+++ web/js/merchant/views/Settlements/List.js
@@ -169,6 +169,7 @@ export default class SettlementsListContainer extends ListContainer {
           onUnmount={this.settlementBreakupOnUnmount}
         />
       ),
+      size: 'large',
     });
   };
 
