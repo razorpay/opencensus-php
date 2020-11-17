c1dc43033d5583d5c54903bd0ef1c2b54f4a71b7
Apurva ShivKumar Nimodiya <apurva.shivkumar@razorpay.com>
2020-11-17 12:19:41 +0530

    test case completed

--- tests/Functional/FundAccounts/FundAccountValidationTest.php
+++ tests/Functional/FundAccounts/FundAccountValidationTest.php
@@ -38,6 +38,7 @@ class FundAccountValidationTest extends TestCase
     {
         $this->testDataFilePath = __DIR__ . '/helpers/FundAccountValidationTestData.php';
 
+
         parent::setUp();
 
         $this->fixtures->merchant->editEntity('merchant', '10000000000000', ['fee_model' => 'postpaid']);
@@ -321,8 +322,92 @@ class FundAccountValidationTest extends TestCase
         $this->createValidationWithFundAccountEntity();
     }
 
+
+    public function testWebhookFiringFundAccountValidationCompletedWithStork()
+    {
+        $testData = $this->testData['testFiringOfWebhookOnFAVCompletionWithStork'];
+
+        $this->enableRazorXTreatmentForStork();
+
+        $this->mockServiceStorkRequest(
+            function ($path, $payload) use ($testData)
+            {
+                $this->assertEquals('rx-test', $payload['event']['service']);
+                $this->assertEquals('fund_account.validation.completed', $payload['event']['name']);
+                $this->assertEquals('merchant', $payload['event']['owner_type']);
+                $this->assertEquals('10000000000000', $payload['event']['owner_id']);
+                $this->assertArraySelectiveEquals($testData, json_decode($payload['event']['payload'], true));
+
+                return new \Requests_Response();
+            })->once();
+
+        $this->testFundAccValidationWithAccountNumberAndBankAccount();
+
+        $fav = $this->getDbLastEntity('fund_account_validation');
+        $fta = $this->getDbLastEntity('fund_transfer_attempt');
+
+        $payoutId = $fav->getId();
+        s($payoutId);
+
+        $this->fixtures->edit(
+            'fund_transfer_attempt',
+            $fta->getId(),
+            [
+                'utr'    => null,
+                'is_fts' => 1,
+            ]);
+
+//        $this->fixtures->edit(
+//            'payout',
+//            $payout->getId(),
+//            [
+//                'status' => Payout\Status::PROCESSED,
+//                'utr'    => null,
+//            ]);
+
+        $testData = $this->testData['testFiringOfWebhookOnFAVCompletionWithStork'];
+
+        $this->expectWebhookEventWithContents('fund_account.validation.completed', $testData);
+
+        $this->ba->appAuth();
+
+        $request = [
+            'method'  => 'POST',
+            'url'     =>  '/update_fts_fund_transfer',
+            'content' => [
+                'bank_processed_time' => '2019-12-04 15:51:21',
+                'bank_status_code'    => 'SUCCESS',
+                'extra_info'          => [
+                    'beneficiary_name' => 'Amit M',
+                    'cms_ref_no'       => 'd10ce8e4167f11eab1750a0047330000',
+                    'internal_error'   => false
+                ],
+                'failure_reason'      => '',
+                'fund_transfer_id'    => 1236890,
+                'mode'                => 'IMPS',
+                'narration'           => 'Kissht FastCash Disbursal',
+                'remarks'             => 'Check the status by calling getStatus API.',
+                'source_id'           => $payoutId,
+                'source_type'         => 'fund_account_validation',
+                'status'              => 'PROCESSED',
+                'utr'                 => '933815233814'
+            ],
+        ];
+
+        s($this->makeRequestAndGetContent($request));
+
+        $payout = $this->getDbEntityById('fund_account_validation', $payoutId);
+       // $fta = $this->getDbEntityById('fund_transfer_attempt', $fta->getId());
+
+//       $this->assertEquals('933815233814', $payout->getUtr());
+//        $this->assertEquals('933815233814', $fta->);
+
+
+    }
+
     public function testWebhookFundAccountValidationCompletedWithStork()
     {
+
         $testData = $this->testData['testFiringOfWebhookOnFAVCompletionWithStork'];
 
         $this->enableRazorXTreatmentForStork();
@@ -377,7 +462,7 @@ class FundAccountValidationTest extends TestCase
 
         $this->testData[__FUNCTION__]['request']['content']['fund_account']['id'] =  $fundAccountResponse['id'];
 
-        $this->startTest();
+        s($this->startTest());
 
         // get database entities
         $balance = $this->getLastEntity('balance', true);
