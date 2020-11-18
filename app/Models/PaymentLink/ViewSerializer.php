d776db91077a3fe065ec2b0915c28050890a7fc9
Abhay <abhayp@users.noreply.github.com>
2020-11-18 22:49:30 +0530

    branding logo change (#19959)
    * branding logo change
    
    * alignment
    
    * adding org in template data

--- app/Models/PaymentLink/ViewSerializer.php
+++ app/Models/PaymentLink/ViewSerializer.php
@@ -238,7 +238,7 @@ class ViewSerializer extends Base\Core
 
                 $branding['show_rzp_logo'] = false;
 
-                $branding['branding_logo'] = 'https://cdn.razorpay.com/static/assets/hostedpages/axis_logo.png';
+                $branding['branding_logo'] = 'https://cdn.razorpay.com/static/assets/hostedpages/axis_logo.svg';
 
                 break;
         }
