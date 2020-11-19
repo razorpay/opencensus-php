7a178e580ca4d5d2acc56a89b90b8c039902e986
Parth <71763920+parth-p-ui@users.noreply.github.com>
2020-11-19 10:33:02 +0530

    tooltip added for test mode (#6033)
    * tooltip added for test mode
    
    * css important removed

--- web/js/merchant/components/HeaderNav/SwitchMode.js
+++ web/js/merchant/components/HeaderNav/SwitchMode.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import Dropdown, { DropdownTrigger, DropdownContent } from 'common/ui/Dropdown';
+import Popover, { PopoverBody } from 'common/ui/Popover';
 import { classList } from 'common/utils/rzp-utils';
 
 const SwitchMode = ({ mode, modeFormatted, onSwitchMode }) => {
   return (
     <Dropdown>
       <DropdownTrigger class="dropdown-toggle switch-modes-toggle">
-        <span
-          class={`ModeIndicator ${
-            mode === 'test' ? 'ModeIndicator--test' : 'ModeIndicator--live'
-          }`}
-        />{' '}
+        {mode === 'test' ? (
+          <small class="help-content">
+            <i class="i i-info-circle ModeIndicator--test" />
+            <Popover align="bottom" theme="dark" className="test-mode-popover">
+              <PopoverBody>
+                <div>All the transactions that are done in Test mode are</div>
+                <div>sample transactions and there will not be any real money</div>
+                <div>debited/credited in your account.</div>
+              </PopoverBody>
+            </Popover>
+          </small>
+        ) : (
+          <span class="ModeIndicator ModeIndicator--live" />
+        )}{' '}
         {modeFormatted} Mode <span class="caret" />
       </DropdownTrigger>
       <DropdownContent>
@@ -20,7 +30,17 @@ const SwitchMode = ({ mode, modeFormatted, onSwitchMode }) => {
               onClick={() => onSwitchMode('test')}
               class={classList(mode === 'test' && 'selected')}
             >
-              <span class="ModeIndicator ModeIndicator--test" /> Test Mode
+              <small class="help-content">
+                <i class="i i-info-circle ModeIndicator--test" />
+                <Popover align="bottom" theme="dark" className="test-mode-popover">
+                  <PopoverBody>
+                    <div>All the transactions that are done in Test mode are</div>
+                    <div>sample transactions and there will not be any real money</div>
+                    <div>debited/credited in your account.</div>
+                  </PopoverBody>
+                </Popover>
+              </small>{' '}
+              Test Mode
             </a>
           </li>
           <li>
