07a7d4aed1ec2cc92125127b0f78d9904fa54c28
Shivam Yuvraj <shivam.yuvraj@razorpay.com>
2020-11-26 18:34:31 +0530

    tpv editable for atom (#20146)

--- app/Models/Terminal/Validator.php
+++ app/Models/Terminal/Validator.php
@@ -438,6 +438,9 @@ class Validator extends Base\Validator
         Entity::ACCOUNT_NUMBER              => 'sometimes|string|max:50',
         Entity::GATEWAY_SECURE_SECRET2      => 'sometimes|string',
         Entity::PROCURER                    => 'sometimes|string|in:razorpay,merchant',
+        Entity::TPV                         => 'sometimes|in:0,1,2',
+        Entity::STATUS                      => 'sometimes|string|in:deactivated,activated',
+
     ];
 
     protected static $bajajfinservEditTerminalRules = [
