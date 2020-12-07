e6cd2a9148117bf4f38b3ef91d279a53cca74541
Soutreya Kundu <42569541+Soutreya@users.noreply.github.com>
2020-12-07 16:14:25 +0530

    Revert "Added test LA for webhook. (#19652)" (#19912)
    This reverts commit 4bcaad8a284f7f1eadff4a87f0547c88a2f95d4c.

--- app/Models/Merchant/Preferences.php
+++ app/Models/Merchant/Preferences.php
@@ -140,7 +140,6 @@ class Preferences
     const MID_STASHFIN              = 'Ao42qLIgNsuREt';
     const MID_VOCATIONAL_EDU        = 'F5NF8QNN7XAJ2w';
     const MID_BSE                   = 'FlaHVYQCGKbK2t';
-    const MID_ASHISH                = 'E7qAq7CV6MQSkx'; // Test
 
     const MID_CLIX_CAPITAL            = 'AxEq4Z2U8Gd8vH';
     const MID_CLIX_CAPITAL_SERVICES   = 'Bkeuzp5jlMNhzD';
@@ -354,7 +353,6 @@ class Preferences
 
     const TRANSFER_SETTLED_WEBHOOK_MIDS = [
         self::MID_BSE,
-        self::MID_ASHISH,
     ];
 
     const NO_MERCHANT_INVOICE_PARENT_MIDS =  [
