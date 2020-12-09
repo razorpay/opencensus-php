d794209ddf57214e7b42af8de4c3445b71a3097f
Varun Kumar K <36691576+varunkumark1997@users.noreply.github.com>
2020-12-09 10:58:44 +0530

    emandate Banklist update dec/2020 (#20323)
    * update supported banks for emandate and esign
    
    * update bank count
    
    * add to zerorupeeemandate
    
    * update zerorupee list
    
    * reorder change

--- tests/Functional/Gateway/Enach/Rbl/EnachRblGatewayTest.php
+++ tests/Functional/Gateway/Enach/Rbl/EnachRblGatewayTest.php
@@ -1272,7 +1272,7 @@ class EnachRblGatewayTest extends TestCase
             }
         }
 
-        $this->assertEquals(30, $count);
+        $this->assertEquals(36, $count);
 
         $this->assertArrayNotHasKey(IFSC::UTBI, $banks);
     }
