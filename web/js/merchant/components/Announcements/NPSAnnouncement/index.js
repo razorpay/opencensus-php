35a7c414f7b0e0d164f723fa7bcd76d3324dc001
Zameer Haque <20766601+haquezameer@users.noreply.github.com>
2020-12-10 14:54:51 +0530

    feat(NPSAnnouncementBanner): update links for PG_1m, PL, PP and disable other surveys for dec (#6339)

--- web/js/merchant/components/Announcements/NPSAnnouncement/index.js
+++ web/js/merchant/components/Announcements/NPSAnnouncement/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import AnnouncementBanner from 'merchant/components/Announcements/AnnouncementBanner';
 
 const SURVEY_LINKS = {
-  PL: 'https://razorpay.typeform.com/to/CdVDgkfA',
-  PP: 'https://razorpay.typeform.com/to/ebwzvYC1',
-  PG_1m: 'https://razorpay.typeform.com/to/JZZhgPLq',
-  PG_6m: 'https://razorpay.typeform.com/to/RvgxGRGM',
-  PG_12m: 'https://razorpay.typeform.com/to/mZcTfb8R',
-  other_products: 'https://razorpay.typeform.com/to/suSnn21l',
+  PL: 'https://razorpay.typeform.com/to/LOb8O1a1',
+  PP: 'https://razorpay.typeform.com/to/rC2nqalJ',
+  PG_1m: 'https://razorpay.typeform.com/to/FtXjOpIi',
+  // PG_6m: 'https://razorpay.typeform.com/to/RvgxGRGM',
+  // PG_12m: 'https://razorpay.typeform.com/to/mZcTfb8R',
+  // other_products: 'https://razorpay.typeform.com/to/suSnn21l',
 };
 
 const getLink = (user) => {
@@ -23,17 +23,17 @@ const getLink = (user) => {
     return { link: SURVEY_LINKS['PG_1m'], cohort: 'pg_1m' };
   }
 
-  if (user.isFeatureEnabled('nps_survey_pg_6m')) {
-    return { link: SURVEY_LINKS['PG_6m'], cohort: 'pg_6m' };
-  }
+  // if (user.isFeatureEnabled('nps_survey_pg_6m')) {
+  //   return { link: SURVEY_LINKS['PG_6m'], cohort: 'pg_6m' };
+  // }
 
-  if (user.isFeatureEnabled('nps_survey_pg_12m')) {
-    return { link: SURVEY_LINKS['PG_12m'], cohort: 'pg_12m' };
-  }
+  // if (user.isFeatureEnabled('nps_survey_pg_12m')) {
+  //   return { link: SURVEY_LINKS['PG_12m'], cohort: 'pg_12m' };
+  // }
 
-  if (user.isFeatureEnabled('nps_survey_other_products')) {
-    return { link: SURVEY_LINKS['other_products'], cohort: 'other_products' };
-  }
+  // if (user.isFeatureEnabled('nps_survey_other_products')) {
+  //   return { link: SURVEY_LINKS['other_products'], cohort: 'other_products' };
+  // }
 
   return null;
 };
@@ -49,7 +49,7 @@ const NPSAnnouncement = ({ user }) => {
     <AnnouncementBanner
       title="Your Feedback Matters"
       theme="success"
-      bannerKey={`nps-banner-nov-20-${survey.cohort}-${user.current}`}
+      bannerKey={`nps-banner-dec-20-${survey.cohort}-${user.current}`}
       canBeClosed={true}
     >
       Hello! Request you to fill in this quick feedback survey about your experience with Razorpay.
