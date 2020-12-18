72a8af39ce7b818968b76a38ba3423619f919c0a
Sandesh Damkondwar <sandeshdamkondwar@gmail.com>
2020-12-18 11:35:15 +0530

    [CARE - 22] chore: disabled call support option (#6412)
    * chore: disabled call support option
    
    * chore: changed working hours

--- web/js/merchant/components/Support/components/SupportBody.js
+++ web/js/merchant/components/Support/components/SupportBody.js
@@ -157,7 +157,7 @@ export default class SupportBody extends Component {
                 }`}
                 onClick={() => handleClick('chat')}
               >
-                Chat with us <small class="help-content">(9am-5pm, working days)</small>
+                Chat with us <small class="help-content">(9am-6pm, working days)</small>
                 {notifyCount > 0 && <span class="notify-icon m-l">{notifyCount}</span>}
                 <small class="help-block">
                   {shouldDisable && notifyCount < 1
@@ -172,7 +172,7 @@ export default class SupportBody extends Component {
               class={`support-item p-all call ${shouldDisable ? 'disabled' : ''}`}
               onClick={() => handleClick('call')}
             >
-              Call Support <small class="help-content">(9am-5pm, working days)</small>
+              Call Support <small class="help-content">(9am-6pm, working days)</small>
               <small class="help-block">
                 {shouldDisable ? 'Currently unavailable' : 'For queries and help on the dashboard'}
               </small>
