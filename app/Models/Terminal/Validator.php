39232b2fe511b0d9d11e1a6d77434b19cfa0996d
Gauravsingh <52907566+gauravsingh16@users.noreply.github.com>
2021-01-05 17:13:34 +0530

    added terminal type for edit (#20693)

--- app/Models/Terminal/Validator.php
+++ app/Models/Terminal/Validator.php
@@ -1202,8 +1202,8 @@ class Validator extends Base\Validator
         Entity::GATEWAY_SECURE_SECRET       => 'sometimes|string',
         Entity::TPV                         => 'sometimes|in:0,1,2',
         Entity::PROCURER                    => 'sometimes|string|in:razorpay,merchant',
-        Entity::TPV                         => 'sometimes|in:0,1,2',
         Entity::CORPORATE                   => 'sometimes|int|in:0,1,2',
+        Entity::TYPE                        => 'sometimes|array',
     ];
 
     protected static $netbankingAllahabadTerminalRules = [
