4b81bf3ce24ce6443d0a13fa36234018df23343f
Mahesh Sakore <61787726+rzpmsakore@users.noreply.github.com>
2021-01-05 17:15:07 +0530

    email and subject change (#20692)

--- app/Reconciliator/RequestProcessor/Base.php
+++ app/Reconciliator/RequestProcessor/Base.php
@@ -208,7 +208,7 @@ class Base extends Core
         self::PAYPAL                 => [],
         self::VAS_AXIS               => [],
         self::GETSIMPL               => [],
-        self::BAJAJFINSERV           => ['remiecftransactions@bizsupportc.com'],
+        self::BAJAJFINSERV           => ['remiecftransactions@bizsupportc.com', 'ashwani.verma1@bajajfinserv.in'],
         self::YES_BANK               => ['yesacquirer@insolutionsglobal.com'],
         self::HDFC_DEBIT_EMI         => ['emailintimation@hdfcbank.com'],
         self::UPI_JUSPAY             => ['crs.upimerchantsettlement@axisbank.com'],
--- app/Reconciliator/Validator.php
+++ app/Reconciliator/Validator.php
@@ -95,7 +95,7 @@ class Validator extends Base\Core
         RequestProcessor\Base::CARDLESS_EMI_FLEXMONEY => ["/^Flexmoney Recon and Refund files/"],
         RequestProcessor\Base::PHONEPE            => [".*/Settlement Report/"],
         RequestProcessor\Base::NETBANKING_KVB     => ["/Recon file [0-9]{2}.[0-9]{2}.20[0-9]{2}/"],
-        RequestProcessor\Base::BAJAJFINSERV       => ["/Payment MIS RAZORPAY SOFTWARE PRIVATE LIMITED_ [0-9]{1,2} (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) 20[0-9]{2}/"],
+        RequestProcessor\Base::BAJAJFINSERV       => ["/Payment MIS_Razorpay Software_ [0-9]{1,2} (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) 20[0-9]{2}/"],
         RequestProcessor\Base::YES_BANK           => ["/Yes Bank_ MPR [0-9]{2}-[0-9]{2}-20[0-9]{2}/"],
         RequestProcessor\Base::HDFC_DEBIT_EMI     => ["/^DCEMI Reconciliation & Payment Summary Report/"],
         RequestProcessor\Base::UPI_JUSPAY         => ["/BAJAJ TXN DETAILS/"],
