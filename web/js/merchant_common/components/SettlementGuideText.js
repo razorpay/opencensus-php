a00c302a2c2df26dcf020b9ecfaf1839f6dd012c
adewaskar <aditya.dewaskar@razorpay.com>
2021-01-21 11:06:28 +0530

    Merchant LA settlements issue (#6669)
    * added fix with safety check
    
    * added fix with safety check

--- web/js/merchant_common/components/SettlementGuideText.js
+++ web/js/merchant_common/components/SettlementGuideText.js
@@ -8,7 +8,7 @@ const SettlementGuideText = ({ user }) => {
       <div className="col-md-6 col-md-offset-3 col-sm-12 text-center">
         <div>The amount that gets settled to your bank account will show up here.</div>
         <div>
-          {(user && user.isOrgAllowedFunctionality('external_links')) ? (
+          {(user && user.isOrgAllowedFunctionality && user.isOrgAllowedFunctionality('external_links')) ? (
             <a
               className="btn-link"
               target="_blank"
