98c0eb99d380447f88f4d685aab1b20531ebbadc
Gyanendra Pratap Singh <gyanendra.singh@razorpay.com>
2021-01-28 23:13:31 +0530

    removing box from b2c pdf (#21050)

--- resources/views/merchant/pg_invoice/invoice.blade.php
+++ resources/views/merchant/pg_invoice/invoice.blade.php
@@ -102,11 +102,11 @@
             <li>To get GST input, please make sure that you have updated your GSTIN in your Razorpay Dashboard.</li>
             <li>All the Invoice, Debit &amp; Credit note values are inclusive of GST.</li>
 
+            @if (isset($einvoice_data['callout_message']))
             <div class="text-left" style="border:1px solid black;padding:5px;margin-bottom:10px;font-weight:bold;">
-                @if (isset($einvoice_data['callout_message']))
                     NOTE: {{{$einvoice_data['callout_message']}}}
-                @endif
             </div>
+            @endif
         </ol>
     </div>
 
