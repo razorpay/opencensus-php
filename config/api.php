ce2576f69343575f71540b8a4d68ff24e3aff858
shubhamiitmandi123 <70131730+shubhamiitmandi123@users.noreply.github.com>
2021-02-02 10:42:41 +0530

    setting API calls timeout to 10 minutes (#6749)

--- config/api.php
+++ config/api.php
@@ -13,5 +13,5 @@ return array(
     'auth_guest_pass'       =>  env('API_GUEST_AUTH_PASS'),
     'auth_internal_pass'    =>  env('API_INTERNAL_AUTH_PASS'),
     'mock'                  =>  env('API_MOCK'),
-    'request_timeout'       =>  3600, 
+    'request_timeout'       =>  600, // 10 minutes
 );
