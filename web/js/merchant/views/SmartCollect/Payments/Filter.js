cc736fe018f15ee27a54502cfa00624e834c4e2c
Avinash Devarakonda <venkata.avinash@razorpay.com>
2021-02-10 14:55:02 +0530

    fix: add missing filter (#6837)

--- web/js/merchant/views/SmartCollect/Payments/Filter.js
+++ web/js/merchant/views/SmartCollect/Payments/Filter.js
@@ -52,6 +52,16 @@ export default ({ showBatchIdFilter, ...props }) => {
       </div>
 
       <div class="form-group list-filter-item">
+        <label>Bank Reference Number</label>
+        <Field
+          name="va_transaction_id"
+          component="input"
+          class="form-control input-sm"
+          onBlur={props.onEleBlur('va_transaction_id')}
+        />
+      </div>
+
+      <div class="form-group list-filter-item">
         <label>Notes</label>
         <Field
           name="notes"
