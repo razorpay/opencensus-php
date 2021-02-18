8fe558fcdf5b96b7ccbbbae0b48ae92588703f65
Sourabh Basu Datta <bluc@users.noreply.github.com>
2021-02-18 19:12:58 +0530

    Update automated dispute merchant disabled email template (#21425)
    * Update automated dispute merchant disabled email template
    
    * Minor update

--- resources/views/partials/dispute/customer/merchant_disabled.blade.php
+++ resources/views/partials/dispute/customer/merchant_disabled.blade.php
@@ -1,9 +1,9 @@
 <p class="para-normal font-size-medium" style="font-size: 14px; line-height: 1.5; color: #515978; margin-bottom: 17px;">
-    We are sorry to hear that you have not received the services for the transaction. The business in question is no longer associated with us and unfortunately, there are not enough funds in the account of {{$merchant['name']}} for us to proceed with refund of your payment. We are working with our internal team to recover funds but this process may take time.
+    We are sorry to hear that you have not received the services for the transaction. Our team has taken corrective measures against the business {{$merchant['name']}}.
 </p>
 
 <p class="para-normal font-size-medium" style="font-size: 14px; line-height: 1.5; color: #515978; margin: 20px 0;">
-    Kindly file a chargeback through your bank so that the amount can be refunded by the bank as they have the authority to do that even if the merchant does not have an amount in his Merchant Account.Thank you for your understanding.
+    As we are a Payment Gateway facilitating payments, the authority to refund lies with the bank. You would need to reach out to your bank to file a dispute.
 </p>
 
 <p class="para-normal font-size-medium" style="font-size: 14px; line-height: 1.5; color: #515978; margin: 20px 0;">
