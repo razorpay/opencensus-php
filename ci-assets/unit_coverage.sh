db1e3c0fe15fea5b1dbb7fe2f1dfe22ab863e951
Premkumar <prem.svmm@gmail.com>
2021-02-19 19:59:00 +0530

    [Smart collect]: Unit Coverage check for smart collect (#21397)
    * [Smart collect]: Unit Coverage check for smart collect
    
    * [Smart collect]: added smart collect yml file
    
    * [Smart collect]: updated sha tag
    
    * [Smart collect]: updated permission for sh file
    
    * [Smart collect]: added driver
    
    * [Smart collect]: added version
    
    * [Smart collect]: vpa prefix test only
    
    * [Smart collect]: updated dir
    
    * [Smart collect]: updated dir to correct one
    
    * [Smart collect]: add condition to check dir
    
    * [Smart collect]: remove docker pull
    
    * [Smart collect]: move file to workspace
    
    * [Smart collect]: move to root dir
    
    * [Smart collect]: added condition to move file
    
    * [Smart collect]: refer coverage file directly in dir
    
    * [Smart collect]: push to sonar from pwd
    
    * [Smart collect]: added commit id to version
    
    * [Smart collect]: removed sonar push from job
    
    * [Smart collect]: run all test and push to sonarqube
    
    * [Smart collect]: cat clover file
    
    * [Smart collect]: add report file from root dir
    
    * [Smart collect]: run all smart collect tests
    
    * [Smart collect]: uncomment the commented out changes
    
    * [Smart collect]: remove unwanted changes in yml
    
    * [Smart collect]: remove changes not required
    
    * [Smart collect]: run on push
    
    * [Smart collect]: update yml
    
    * [Smart collect]: update status name for workflow
    
    * [Smart collect]: change type to deployment
    
    * [Smart collect]: remove temp code
    
    * [Smart collect]: update env name
    
    * excluded Smart collect related directory from Application TS
    
    * Added source to VA dir
    
    * [Smart collect]: update bash file and yml
    
    * [Smart collect]: update source dir
    
    * Revert "excluded Smart collect related directory from Application TS"
    
    This reverts commit 27770468eede14ff3c9e3effb7dc700d49309609.
    
    * [Smart collect]: fetch value from dir
    
    * [Smart collect]: update yml file
    
    * [Smart collect]: update dir path
    
    * [Smart collect]: update github status update
    
    * [Smart collect]: uncommented the unit tcs
    
    Co-authored-by: ritesh-commits <68008524+ritesh-commits@users.noreply.github.com>

---
+++ ci-assets/unit_coverage.sh
@@ -0,0 +1,56 @@
+#version
+phpunit=7.3.0
+phpcodecoverage=6.1.4
+
+function init_setup
+{
+    pecl install xdebug-2.9.0 && echo 'zend_extension=/usr/lib/php7/modules/xdebug.so' >> /etc/php7/php.ini
+    mkdir -p /opt/razorpay/certs/first_data
+    touch /etc/php7/conf.d/assertion.ini
+    echo "zend.assertions=1" >> /etc/php7/conf.d/assertion.ini
+    echo "assert.exception=1" >> /etc/php7/conf.d/assertion.ini
+    sed -i 's/max_execution_time.*/max_execution_time=120/' /etc/php7/php.ini
+    sed -i 's/memory_limit.*/memory_limit=-1/' /etc/php7/php.ini
+    composer config --global github-oauth.github.com $GIT_TOKEN
+    composer global require hirak/prestissimo
+    composer remove --dev orchestra/testbench phpunit/phpunit-mock-objects --no-interaction
+    composer require --dev phpunit/phpunit:"$phpunit" phpunit/php-code-coverage:"$phpcodecoverage" codedungeon/phpunit-result-printer:^0.15.5 symfony/phpunit-bridge  --update-with-dependencies
+    composer require --dev pcov/clobber
+    composer dump-autoload -og
+}
+
+function run_tests
+{
+    php vendor/phpunit/phpunit/phpunit -d memory_limit=-1 --testsuite "$TEST_SUITE_NAME" --printer="Codedungeon\PHPUnitPrettyResultPrinter\Printer" --debug --verbose  --coverage-clover clover.xml
+}
+
+function push_to_sonar
+{
+    file="clover.xml"
+    if [ -f "$file" ];then
+        dir=$(pwd)
+        echo "--------Downloading dependencies----------"
+        wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.23-r3/glibc-2.23-r3.apk
+        apk --allow-untrusted --force add glibc-2.23-r3.apk
+        apk update && apk add nodejs && apk del gnu-libiconv
+        wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-3.3.0.1492-linux.zip
+        unzip sonar-scanner-cli-3.3.0.1492-linux.zip
+        rm sonar-scanner-cli-3.3.0.1492-linux.zip
+        PATH=$PATH:$(pwd)/sonar-scanner-3.3.0.1492-linux/bin
+        export PATH
+        echo "-------------Pushing report to sonar--------"
+        sonar-scanner -X \
+                -Dsonar.host.url="$SONAR_HOST" \
+                -Dsonar.projectKey="$SONAR_PROJECT_ID" \
+                -Dsonar.projectName="$SONAR_PROJECT_ID" \
+                -Dsonar.projectVersion="$GIT_COMMIT_ID" \
+                -Dsonar.login="$SONAR_TOKEN" \
+                -Dsonar.sources="$SOURCE_DIR" \
+                -Dsonar.php.coverage.reportPaths="$dir"/"$file"
+    fi
+}
+
+init_setup
+run_tests
+push_to_sonar
+exit $?
