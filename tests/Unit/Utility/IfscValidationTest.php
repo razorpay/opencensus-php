841ca99abf8dc68341f93e6d242b717ce035f489
Junaid Anwer <anwerj@users.noreply.github.com>
2021-02-21 21:42:08 +0530

    [IFSC] Updating to version 1.5.11 (#21463)

---
+++ tests/Unit/Utility/IfscValidationTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace RZP\Tests\Unit\Utility;
+
+use RZP\Exception;
+use RZP\Tests\TestCase;
+use Razorpay\IFSC\IFSC;
+use RZP\Models\BankAccount;
+use RZP\Models\BankAccount\OldNewIfscMapping;
+
+class IfscValidationTest extends TestCase
+{
+    public function testValidationOnMappedIfsc()
+    {
+        $result = [
+            'total'     => count(OldNewIfscMapping::$oldToNewIfscMapping),
+            'valid'     => 0,
+        ];
+
+        foreach (OldNewIfscMapping::$oldToNewIfscMapping as $mapped)
+        {
+            if (IFSC::validate($mapped) === true)
+            {
+                $result['valid']++;
+            }
+        }
+
+        $this->assertArraySubset([
+            'total' => 20911,
+            'valid' => 20868,
+        ], $result);
+    }
+}
