fa5c9e792fcc81f991a8cf907084d54455641026
Varun Kumar K <36691576+varunkumark1997@users.noreply.github.com>
2021-03-01 10:56:32 +0530

    [NBPLUS-495] increase the batch limit to 20k for axis debit (#21598)
    * increase the batch limit to 20k for axis debit
    
    * increase the batch limit to 20k for axis debit

--- app/Models/Batch/Limit.php
+++ app/Models/Batch/Limit.php
@@ -30,7 +30,7 @@ class Limit
         'emandate_register_enach_npci_netbanking'   => 10000,
         'emandate_register_sbi'                     => 10000,
         'emandate_debit_hdfc'                       => 10000,
-        'emandate_debit_axis'                       => 10000,
+        'emandate_debit_axis'                       => 20000,
         'emandate_debit_enach_rbl'                  => 10000,
         'emandate_debit_enach_npci_netbanking'      => 100000,
         'emandate_debit_enach_nb_icici'             => 100000,
