13a3f2f9d82c48b3f49a026117ec547886b655de
Anshu Raj <anshu.raj@razorpay.com>
2021-03-02 10:23:46 +0530

    Updates messaging post typeform submission (#6994)

--- web/js/merchant/views/Settings/Configuration/components/InternationalConfigComponents/RequestSubmittedModal.js
+++ web/js/merchant/views/Settings/Configuration/components/InternationalConfigComponents/RequestSubmittedModal.js
@@ -7,11 +7,11 @@ const RequestSubmittedModal = ({ closeModal }) => (
     <ModalHeader title="Request Submitted" onCloseClick={closeModal} />
     <div class="modal-body">
       <div>
-        We have received your request for enabling international payments for
-        Payment Pages, Payment Links & Invoices.
+        We have received your request for enabling international payments for Payment Pages, Payment
+        Links & Invoices.
       </div>
       <br />
-      <div>We would get back to you shortly with an update on your request</div>
+      <div>We will get back to you within 5-7 working days with an update on your request</div>
       <div class="Modal__actions text-right">
         <Button.Primary onClick={closeModal}>Got it</Button.Primary>
       </div>
