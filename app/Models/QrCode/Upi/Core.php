8a3b21fae5d18c0f7d49130a75259706c40629c0
ritesh-commits <68008524+ritesh-commits@users.noreply.github.com>
2021-03-09 16:18:08 +0530

    Vajra metrics for UPI QR (#21644)
    * Added slack notification for failed payment and pushed payment metrics for upi qr on vajra
    
    * Update Core.php
    
    * Update Core.php
    
    * Removed slack notification
    
    * Code refactor
    
    * Removed Duplicate callback tracing
    
    * Update Processor.php
    
    * Update app/Models/QrCode/Upi/Core.php
    
    Co-authored-by: Kritika Khandelwal <51109002+khandelwalkritika@users.noreply.github.com>
    
    Co-authored-by: Kritika Khandelwal <51109002+khandelwalkritika@users.noreply.github.com>
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Models/QrCode/Upi/Core.php
+++ app/Models/QrCode/Upi/Core.php
@@ -3,10 +3,11 @@
 namespace RZP\Models\QrCode\Upi;
 
 use RZP\Models\Base;
+use RZP\Services\Mutex;
 use RZP\Models\Terminal;
 use RZP\Error\ErrorCode;
-use RZP\Services\Mutex;
 use RZP\Trace\TraceCode;
+use RZP\Models\VirtualAccount;
 use Razorpay\Trace\Logger as Trace;
 
 class Core extends Base\Core
@@ -25,31 +26,54 @@ class Core extends Base\Core
     {
         // We can build the entity with data['upi_qr'],
         // Upi Qr Entities are always expected by definition
-        $upiQr = (new Entity);
+        $errorMessage = null;
 
-        $processor = new Processor($input, $referenceId, $data, $terminal);
+        try
+        {
+            $upiQr = (new Entity);
 
-        $this->mutex->acquireAndRelease($referenceId,
-            function() use ($processor, $upiQr)
-            {
-                $upiQr = $processor->process($upiQr);
+            $processor = new Processor($input, $referenceId, $data, $terminal);
 
-                // This will be null in case it's a duplicate notification
-                return $upiQr;
-            },
-            // Avg response time of this whole route is about 300ms,
-            // so 10x of that should be quite safe
-            $ttl = 30,
-            $errorCode = ErrorCode::BAD_REQUEST_VIRTUAL_ACCOUNT_OPERATION_IN_PROGRESS,
-            // A process will generally not need to do multiple retries at
-            // all, since the retry times are adequate for the previous
-            // process to complete. Still setting to 3 for freak occurrences.
-            $retryCount = 3,
-            // 2x and 4x of avg response time for this entire route
-            // (not just the process within the lock)
-            $minRetryDelay = 600,
-            $maxRetryDelay = 1200);
+            $this->mutex->acquireAndRelease($referenceId,
+                function() use ($processor, $upiQr) 
+                {
+                    $upiQr = $processor->process($upiQr);
 
-        return true;
+                    // This will be null in case it's a duplicate notification
+                    return $upiQr;
+                },
+                // Avg response time of this whole route is about 300ms,
+                // so 10x of that should be quite safe
+                                            $ttl = 30,
+                                            $errorCode = ErrorCode::BAD_REQUEST_VIRTUAL_ACCOUNT_OPERATION_IN_PROGRESS,
+                // A process will generally not need to do multiple retries at
+                // all, since the retry times are adequate for the previous
+                // process to complete. Still setting to 3 for freak occurrences.
+                                            $retryCount = 3,
+                // 2x and 4x of avg response time for this entire route
+                // (not just the process within the lock)
+                                            $minRetryDelay = 600,
+                                            $maxRetryDelay = 1200);
+
+            $valid = true;
+        }
+        catch (\Throwable $ex)
+        {
+            $this->trace->traceException(
+                $ex, Trace::CRITICAL, TraceCode::UPI_QR_PAYMENT_PROCESSING_FAILED, $input);
+
+            $errorMessage = $ex->getMessage();
+
+            $valid = false;
+        }
+        finally
+        {
+            // is expected is always true here, as the callback reaches this point only if it was expected, otherwise
+            // the GatewayController would forward to create unexpected payment
+            (new VirtualAccount\Metric())->pushPaymentMetrics('upi_qr', true, $valid,
+                                                              $terminal->getGateway(), $errorMessage);
+        }
+
+        return $valid;
     }
 }
--- app/Trace/TraceCode.php
+++ app/Trace/TraceCode.php
@@ -1341,6 +1341,8 @@ class TraceCode extends \Razorpay\Trace\TraceCode
     const BHARAT_QR_UNEXPECTED_PAYMENT                          = 'BHARAT_QR_UNEXPECTED_PAYMENT';
     const BHARAT_QR_PAYMENT_VALIDATE_REQUEST                    = 'BHARAT_QR_PAYMENT_VALIDATE_REQUEST';
 
+    const UPI_QR_PAYMENT_PROCESSING_FAILED                      = 'UPI_QR_PAYMENT_PROCESSING_FAILED';
+
     const UPI_TRANSFER_SAVE_REQUEST                             = 'UPI_TRANSFER_SAVE_REQUEST';
     const UPI_TRANSFER_REQUEST_SAVED                            = 'UPI_TRANSFER_REQUEST_SAVED';
     const UPI_TRANSFER_SAVE_REQUEST_FAILED                      = 'UPI_TRANSFER_SAVE_REQUEST_FAILED';
