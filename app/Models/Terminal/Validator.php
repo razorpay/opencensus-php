6d3c29e3caf95c333e960608487cb2f61890b7d5
Vikas Naidu <vck173@gmail.com>
2021-03-11 13:15:41 +0530

    [Terminals] hdfc terminal to accept alpha num in terminal id (#21713)
    * [Terminals] hdfc terminal to accept alpha num in terminal id
    
    * changed validation for hdfc terminal
    
    * Minor fix
    
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>
    Co-authored-by: Shivam Yuvraj <shivamyuvraj1@gmail.com>

--- app/Models/Terminal/Validator.php
+++ app/Models/Terminal/Validator.php
@@ -248,7 +248,7 @@ class Validator extends Base\Validator
     protected static $hdfcTerminalRules = [
         Entity::GATEWAY                    => 'required|in:hdfc',
         Entity::GATEWAY_MERCHANT_ID        => 'required|integer|digits_between:4,8',
-        Entity::GATEWAY_TERMINAL_ID        => 'required|integer|digits:8',
+        Entity::GATEWAY_TERMINAL_ID        => 'required|alpha_num|size:8',
         Entity::GATEWAY_TERMINAL_PASSWORD  => 'required|string|max:15',
         Entity::EMI                        => 'sometimes|boolean',
         Entity::INTERNATIONAL              => 'sometimes|boolean',
