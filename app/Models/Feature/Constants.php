c5bb5078e23ca7c74224ad6603ca92caacaef2eb
krantideep95 <kranti.deep@razorpay.com>
2021-03-30 17:48:35 +0530

    change feature const to CAPITAL_CARDS_COLLECTIONS (#22226)

--- app/Models/Feature/Constants.php
+++ app/Models/Feature/Constants.php
@@ -169,7 +169,7 @@ class Constants
 
     const GSTIN_SELF_SERVE                = 'gstin_self_serve';
 
-    const CAPITAL_CARDS_AUTO_COLLECTIONS = 'capital_cards_auto_collections';
+    const CAPITAL_CARDS_COLLECTIONS       = 'capital_cards_collections';
     /**
       Prevents user to switch to test mode from live mode
     */
@@ -912,7 +912,7 @@ class Constants
         self::ALTERNATE_PAYOUT_FR             => true,
         self::DISABLE_X_AMAZONPAY             => true,
         self::CAW_RECURRING_CHARGE_AXIS       => true,
-        self::CAPITAL_CARDS_AUTO_COLLECTIONS  => true,
+        self::CAPITAL_CARDS_COLLECTIONS       => true,
     ];
 
     // Entity type constants
