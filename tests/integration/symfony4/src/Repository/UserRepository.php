6ab168ebfdcef98c06d540981d7658afcc3f3fe6
Sanket Rathi <Sanket.Rathi@razorpay.com>
2021-04-05 18:02:16 +0530

    fixed error

--- tests/integration/symfony4/src/Repository/UserRepository.php
+++ tests/integration/symfony4/src/Repository/UserRepository.php
@@ -19,7 +19,7 @@ namespace App\Repository;
 
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method User|null find($id, $lockMode = null, $lockVersion = null)
