6eb957eb4f57f2ecc6282bb12dc405d818765e67
Sanket Rathi <Sanket.Rathi@razorpay.com>
2021-04-06 17:47:54 +0530

    reverted

--- tests/integration/guzzle6/test.sh
+++ tests/integration/guzzle6/test.sh
@@ -18,10 +18,9 @@ set -e
 pushd $(dirname ${BASH_SOURCE[0]})
 source ../setup_test_repo.sh
 
-#sed -i "s|dev-master|dev-${BRANCH}|" composer.json
-#sed -i "s|https://github.com/census-instrumentation/opencensus-php|${REPO}|" composer.json
-#composer install -n --prefer-dist
-composer require --update-with-all-dependencies guzzlehttp/guzzle:~6.0
+sed -i "s|dev-master|dev-${BRANCH}|" composer.json
+sed -i "s|https://github.com/census-instrumentation/opencensus-php|${REPO}|" composer.json
+composer install -n --prefer-dist
 
 vendor/bin/phpunit --verbose
 
