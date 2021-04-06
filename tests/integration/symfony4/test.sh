0ba6d10c556cd5443b58aaef3c53cc983cd7bf9f
Sanket Rathi <Sanket.Rathi@razorpay.com>
2021-04-06 10:26:04 +0530

    added allow-no-migration

--- tests/integration/symfony4/test.sh
+++ tests/integration/symfony4/test.sh
@@ -41,7 +41,7 @@ else
     composer dumpautoload
 fi
 
-bin/console doctrine:migrations:migrate -n
+bin/console doctrine:migrations:migrate -n --allow-no-migration
 
 echo "Running PHP server at ${TEST_HOST}:${TEST_PORT}"
 php -S ${TEST_HOST}:${TEST_PORT} -t public &
