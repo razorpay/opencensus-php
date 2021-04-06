bd273d41e6afd57108bb59ed53e2ca9d3e2602a0
Sanket Rathi <Sanket.Rathi@razorpay.com>
2021-04-06 15:27:13 +0530

    made changes

--- tests/integration/laravel/test.sh
+++ tests/integration/laravel/test.sh
@@ -24,7 +24,8 @@ cp -R app config routes tests phpunit.xml.dist laravel
 pushd laravel
 
 composer config repositories.opencensus git ${REPO}
-composer require --update-with-all-dependencies opencensus/opencensus:dev-${BRANCH}
+composer require opencensus/opencensus:dev-${BRANCH}
+composer require --dev ramsey/uuid:4.1.1
 composer require --dev guzzlehttp/guzzle:~6.0
 
 php artisan migrate
