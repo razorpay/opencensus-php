e6e94027eac5fc372d750a25930d5c23e0d3353c
Sanket Rathi <Sanket.Rathi@razorpay.com>
2021-04-09 11:26:31 +0530

    updated

--- tests/integration/symfony4/test.sh
+++ tests/integration/symfony4/test.sh
@@ -29,7 +29,7 @@ composer config repositories.opencensus git ${REPO}
 composer require --no-interaction opencensus/opencensus:dev-${BRANCH}
 composer require --no-interaction --dev phpunit/phpunit:^7.0 guzzlehttp/guzzle:~6.0
 
-bin/console doctrine:migrations:migrate -n
+bin/console doctrine:migrations:migrate -n --allow-no-migration
 
 echo "Running PHP server at ${TEST_HOST}:${TEST_PORT}"
 php -S ${TEST_HOST}:${TEST_PORT} -t public &
