00cbb3320a7deebbc9d39bf47f92fba9de6c2c7b
Abhijit <abhijith.lal@razorpay.com>
2021-04-15 15:54:57 +0530

    Revert "set expire sessions at hard limit for admin/merchant dashboard (#7256)" (#7299)
    This reverts commit 53956f4b3ae08305752c6756218097de1d56be8d.

--- app/Session/CustomCacheBasedSessionHandler.php
+++ app/Session/CustomCacheBasedSessionHandler.php
@@ -29,11 +29,8 @@ class CustomCacheBasedSessionHandler extends \Illuminate\Session\CacheBasedSessi
     public function write($sessionId, $data)
     {
         $connection = $this->cache->connection();
-        $sessionKey = $this->sessionNamespace.':'.$sessionId;
 
-        $ttlForSession = $connection->ttl($sessionKey);
-
-        $responses = $connection->transaction(function ($tx) use ($sessionId, $data, $ttlForSession)
+        $responses = $connection->transaction(function ($tx) use ($sessionId, $data)
         {
             $lifetime = $this->minutes * 60;
 
@@ -45,32 +42,27 @@ class CustomCacheBasedSessionHandler extends \Illuminate\Session\CacheBasedSessi
 
             $tx->hmset($sessionKey, $data);
 
-            // add session and expire for the session only if the key is not set.
-            if ($ttlForSession == -2)
+            $tx->expire($sessionKey, $lifetime);
+
+            // Write to admins:adminID:sessions = [ Sid1, Sid2, Sid3 ]
+            if (isset($data['admin_id']))
             {
-                $tx->expire($sessionKey, $lifetime);
+                $adminKey = $this->getAdminSessionKey($data['admin_id']);
 
-                // Write to admins:adminID:sessions = [ Sid1, Sid2, Sid3 ]
-                if (isset($data['admin_id']))
-                {
-                    $adminKey = $this->getAdminSessionKey($data['admin_id']);
+                $tx->sadd($adminKey, $sessionId);
 
-                    $tx->sadd($adminKey, $sessionId);
-
-                    $tx->expire($adminKey, $lifetime);
-                }
-
-                // Write to users:userID:sessions = [ Sid1, Sid2, Sid3 ]
-                if (isset($data['user_id']))
-                {
-                    $userKey = $this->getUserSessionKey($data['user_id']);
-
-                    $tx->sadd($userKey, $sessionId);
-
-                    $tx->expire($userKey, $lifetime);
-                }
+                $tx->expire($adminKey, $lifetime);
             }
 
+            // Write to users:userID:sessions = [ Sid1, Sid2, Sid3 ]
+            if (isset($data['user_id']))
+            {
+                $userKey = $this->getUserSessionKey($data['user_id']);
+
+                $tx->sadd($userKey, $sessionId);
+
+                $tx->expire($userKey, $lifetime);
+            }
         });
 
         return $responses;
