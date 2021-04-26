e1e5fc34c310afd0b003c67a247b79b485162b22
krantideep95 <kranti.deep@razorpay.com>
2021-04-26 15:24:17 +0530

    increase settlement_ondemand_feature_config batch limit to 50000 (#22649)

--- app/Models/Batch/Limit.php
+++ app/Models/Batch/Limit.php
@@ -70,6 +70,7 @@ class Limit
         Type::NACH_MIGRATION                        => 500100,
         Type::LINKED_ACCOUNT_CREATE                 => 50000,
         Type::PAYOUT_LINK_BULK                      => 50000,
+        Type::SETTLEMENT_ONDEMAND_FEATURE_CONFIG    => 50000,
     ];
 
     /**
