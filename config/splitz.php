e9b3a6ddf499ec5728f9df6298146138fe8adfdb
Siddhant Agarwal <sidag95@users.noreply.github.com>
2021-04-29 10:58:39 +0530

    chore(APR20-Reten1-Dashboard Announcement): remove APR20-Reten1-Dashboard announcement (#7392)

--- config/splitz.php
+++ config/splitz.php
@@ -7,7 +7,6 @@ return [
 
         //project nitro campaign experiments
         'ANNOUNCEMENT_NITRO_HYDERABAD_SPLITZ' => env('ANNOUNCEMENT_NITRO_HYDERABAD_SPLITZ',''),
-        'ANNOUNCEMENT_RETENTION1_APRIL2021_SPLITZ' => env('ANNOUNCEMENT_RETENTION1_APRIL2021_SPLITZ',''),
 
         // developer csat survey experiment
         'ANNOUNCEMENT_DX_CSAT_APRIL2021_SPLITZ' => env('ANNOUNCEMENT_DX_CSAT_APRIL2021_SPLITZ', ''),
