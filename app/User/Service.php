dcc2f39b9865b35d041adf5fd567a9ae71c10fd2
udaysharma-dev <63637087+udaysharma-dev@users.noreply.github.com>
2021-05-04 11:31:46 +0530

    adding experiment for razorpay-x mobile app annpuncement (#7425)

--- app/User/Service.php
+++ app/User/Service.php
@@ -1606,7 +1606,8 @@ class Service extends Base\Service
             'pure_platform_signup',
             'rx_icici_ca_onboarding',
             'optimizer_bin_number_op_in',
-            'show_csat_survey'
+            'show_csat_survey',
+            'rx_mobile_app_announcement'
         ];
 
         $experimentsResults = $merchantService->getBulkTreatment($features);
