0c563febe12496ba969877d3f5f20af88de44a88
srinidhis05 <srinidhi.s@razorpay.com>
2021-05-17 14:35:50 +0530

    Entrypoint fix (#7522)
    * fix: entrypoint variable not found fix
    
    * feat: entrypoint if condition fix

--- dockerconf/entrypoint.sh
+++ dockerconf/entrypoint.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 
 echo "$(date) Cast config for environments"
 # casting only env.php.j2 for devserve env as the secrets are injected via kube secrets
-# DEV_SERVE variable is to be passed as true
-if [[ -n "${DEV_SERVE:-}" && "${DEV_SERVE}" == "true" ]]; then
+# DEV_SERVE variable is set
+if [[ -n "${DEV_SERVE:-}" ]]; then
   alohomora cast --region ap-south-1 --env $APP_MODE --app dashboard "environment/env.php.j2"
 else
   alohomora cast --region ap-south-1 --env $APP_MODE --app dashboard "environment/.env.vault.j2" "environment/env.php.j2"
