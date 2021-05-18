7a5e895b9171f70b8122634a4161edd2d4e2e3d8
sesudhanshu <sudhanshu.srivastva@razorpay.com>
2021-05-18 12:24:03 +0530

    workflow dependency check (#22913)
    * workflow dependency check
    
    * Fix Syntax Error
    
    * Set Env for workflow status
    
    * Updating workflow
    
    * Updating workflow
    
    * Updating workflow
    
    * Updating workflow
    
    * Adding Comments
    
    * Removing Comments
    
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

---
+++ .github/actions/getWorkflowStatus.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+get_workflow_status_value() {
+  URI="https://api.github.com"
+  API_HEADER="Accept: application/vnd.github.v3+json"
+  AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
+  branch_name=${BRANCH}
+  workflow_file=${WORKFLOW}
+  workflow_status=""
+  while [ "$workflow_status" != "completed" ]
+  do
+    workflow_details=$(curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" "${URI}/repos/${GITHUB_REPOSITORY}/actions/workflows/$workflow_file/runs?branch=$branch_name&per_page=1" | jq --raw-output '{conclusion: .workflow_runs[0].conclusion, status: .workflow_runs[0].status}')
+    workflow_status=$(echo "$workflow_details" | jq --raw-output '.status')
+    workflow_conclusion=$(echo "$workflow_details" | jq --raw-output '.conclusion')
+    sleep 3m
+  done
+}
+
+get_workflow_status_value
+echo $workflow_conclusion
