5aa438616d26aac879877987fbdbd38073b303fe
YADAV GAURAV <53484152+gauravcom@users.noreply.github.com>
2021-05-28 09:09:53 +0530

    Added attributes- Edit term rules (#22919)
    * Added category and n/w category - term rule
    
    * Added more attributes to be edited
    
    * Updated edit term rules
    
    * PR comments changes
    
    Co-authored-by: gauravcom <“yadav.gaurav@razorpay.com”>
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Models/Terminal/Validator.php
+++ app/Models/Terminal/Validator.php
@@ -252,7 +252,9 @@ class Validator extends Base\Validator
         Entity::TYPE . '.direct_settlement_with_refund' => 'required|in:1',
         Entity::PROCURER                                => 'sometimes|string|in:razorpay,merchant',
         Entity::MODE                                    => 'sometimes|in:2',
-        Entity::UPI                                     => 'sometimes|boolean|in:1',
+        Entity::UPI                                     => 'sometimes|boolean|in:0,1',
+        Entity::NETWORK_CATEGORY                        => 'sometimes|string|max:30',
+        Entity::CATEGORY                                => 'sometimes|string|numeric|digits:4',
         Entity::STATUS                                  => 'sometimes|in:pending,activated,deactivated,failed'
     ];
 
@@ -600,6 +602,10 @@ class Validator extends Base\Validator
         Entity::ENABLED                    => 'sometimes|in:0,1',
         Entity::UPI                        => 'sometimes|boolean|in:0,1',
         Entity::TYPE                       => 'sometimes|array',
+        Entity::NETBANKING                 => 'sometimes|boolean|in:0,1',
+        Entity::CARD                       => 'sometimes|boolean|in:0,1',
+        Entity::NETWORK_CATEGORY           => 'sometimes|string|max:30',
+        Entity::CATEGORY                   => 'sometimes|string|numeric|digits:4',
     ];
 
     protected static $ccavenueEditTerminalRules = [
