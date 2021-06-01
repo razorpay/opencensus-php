628c5d948dcf651533cd675295ab419ad2c9d268
srinidhis05 <srinidhi.s@razorpay.com>
2021-06-01 16:54:08 +0530

    fix: changes header propogation key to rzpctx instead of uberctx (#7659)

--- app/Http/Headers.php
+++ app/Http/Headers.php
@@ -9,7 +9,7 @@ class Headers
     const JWT_TOKEN  = 'X-JWT-TOKEN';
 
     const OAUTH_SOURCE = 'X-OAUTH-SOURCE';
-    // The uberctx-dev-serve-user header is passed to upstream service for routing to
+    // The rzpctx-dev-serve-user header is passed to upstream service for routing to
     // devserve environment if applicable.
-    const DEV_SERVE_USER  = 'uberctx-dev-serve-user';
+    const DEV_SERVE_USER  = 'rzpctx-dev-serve-user';
 }
