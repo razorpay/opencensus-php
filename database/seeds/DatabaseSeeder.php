46090ec04856825eff8b423511bfdd8c5962e40f
Soutreya Kundu <42569541+Soutreya@users.noreply.github.com>
2021-06-08 23:00:30 +0530

    Added marketplace to seeder. (#23310)
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -232,6 +232,17 @@ class DatabaseSeeder extends Seeder
                 )
             );
 
+            DB::table(Table::FEATURE)->insert(
+                array(
+                    'id'            => 'feature_606060',
+                    'name'          => 'marketplace',
+                    'entity_id'     => '10000000000000',
+                    'entity_type'   => 'merchant',
+                    'created_at'    => $currentTime,
+                    'updated_at'    => $currentTime
+                )
+            );
+
             DB::table(Table::MERCHANT_DETAIL)->insert(
                 array(
                     'merchant_id'   => Account::NODAL_ACCOUNT,
