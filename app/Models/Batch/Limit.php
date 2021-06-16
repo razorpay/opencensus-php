b2bf0371dfe88bb73081141e6b797836835388df
parthanand31-sudo <66663796+parthanand31-sudo@users.noreply.github.com>
2021-06-16 17:23:51 +0530

    Changing value of emandate_debit_hdfc to 50000 (#23465)
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Models/Batch/Limit.php
+++ app/Models/Batch/Limit.php
@@ -30,7 +30,7 @@ class Limit
         'emandate_register_enach_npci_netbanking'   => 10000,
         'emandate_register_sbi'                     => 10000,
         'nach_register_nach_citi'                   => 25000,
-        'emandate_debit_hdfc'                       => 10000,
+        'emandate_debit_hdfc'                       => 50000,
         'emandate_debit_axis'                       => 20000,
         'emandate_debit_enach_rbl'                  => 10000,
         'emandate_debit_enach_npci_netbanking'      => 100000,
