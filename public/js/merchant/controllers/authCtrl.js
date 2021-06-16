62b2370346af85a6db426cc78839e4d50802b16f
Prem Kumar G <79976639+Prem-RZP@users.noreply.github.com>
2021-06-16 11:32:32 +0530

    Icici brand (#7779)
    * styles: auth-container changes
    
    * styles: auth-container style changes
    
    * styles: auth-container css changes
    
    * fixes: org custom_code changes

--- public/js/merchant/controllers/authCtrl.js
+++ public/js/merchant/controllers/authCtrl.js
@@ -123,7 +123,7 @@ app
         $scope.isOrgRZP = $scope.organization.custom_code === 'rzp';
         $scope.isOrgHDFC = $scope.organization.custom_code === 'hdfc';
         $scope.isOrgAXIS = $scope.organization.custom_code === 'axis';
-        $scope.isOrgICICI = $scope.organization.custom_code === 'icici';
+        $scope.isOrgICICI = $scope.organization.custom_code === 'icic';
         $scope.second_factor_auth_mode =
           OTP_AUTH_MODE[$scope.organization.second_factor_auth_mode] || 'phone number/email';
       });
