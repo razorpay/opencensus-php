6777ca37a91003520f81d8745c421f84aaf98304
Pratush Pandita <76031763+pratushpandita@users.noreply.github.com>
2021-06-17 13:37:04 +0530

    upi sbi public key rotation changes with experiment (#23525)
    * upi sbi public key rotation changes with experiment
    
    * adding mode arg
    
    * changing razorx feature name
    
    * adding UPI SBI gateway check outside
    
    * [UPI] SBI RazorX experiment|minor changes and testcase
    
    Co-authored-by: Junaid Anwer <junaid.anwer@razorpay.com>
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Gateway/Mozart/Gateway.php
+++ app/Gateway/Mozart/Gateway.php
@@ -34,6 +34,8 @@ class Gateway extends Base\Gateway
 
     const CACHE_KEY    = 'gateway:cache_key_%s';
     const BAJAJ_FINSERV_REST_API =  'bajaj_finserv_rest_api';
+    const GATEWAY_SBI_PUBLIC_KEY_RAZORX_PREFIX = 'gateway_sbi_public_key';
+
     protected $map = [
         'data'      => Entity::RAW,
     ];
@@ -1597,7 +1599,20 @@ class Gateway extends Base\Gateway
                 return $url;
             }
         }
+        if($gateway === Payment\Gateway::UPI_SBI)
+        {
+            $variant = $this->app->razorx->getTreatment(
+                $this->request->getTaskId(),
+                self::GATEWAY_SBI_PUBLIC_KEY_RAZORX_PREFIX,
+                $mode);
 
+            if (strtolower($variant) === 'v2')
+            {
+                $url = $baseUrl . $prefix . '/' . $gateway . '/v2/' . $this->action;
+
+                return $url;
+            }
+        }
         $isBajajFinserv = $this->isBajajFinservGateway($input);
 
         if($isBajajFinserv === true)
--- tests/Functional/Gateway/Upi/Sbi/UpiSbiGatewayTest.php
+++ tests/Functional/Gateway/Upi/Sbi/UpiSbiGatewayTest.php
@@ -11,8 +11,10 @@ use RZP\Models\Payment;
 use RZP\Error\ErrorCode;
 use RZP\Models\FileStore;
 use RZP\Constants\Entity;
+use RZP\Gateway\Upi\Mozart;
 use RZP\Constants\Timezone;
 use RZP\Models\Gateway\File;
+use RZP\Services\RazorXClient;
 use RZP\Models\Payment\Method;
 use RZP\Models\Payment\Refund;
 use RZP\Gateway\Upi\Base\Type;
@@ -114,6 +116,16 @@ class UpiSbiGatewayTest extends TestCase
         $this->assertNotNull($upiEntity[Upi::GATEWAY_DATA]);
         $this->assertEquals('123456789012',$upiEntity[Upi::NPCI_REFERENCE_ID]);
         $this->assertEquals('7971807546', $upiEntity[Upi::GATEWAY_DATA]['addInfo2']);
+    }
+
+    public function testPaymentWithRazorxValueV2()
+    {
+        $this->mockRazorx(Mozart\Gateway::GATEWAY_SBI_PUBLIC_KEY_RAZORX_PREFIX, 'v2', $called);
+
+        $this->testPayment();
+
+        // Make sure a call was made to RazorX
+        $this->assertTrue($called);
     }
 
     public function testIntentPayment()
@@ -1338,5 +1350,28 @@ class UpiSbiGatewayTest extends TestCase
     protected function getDecryptedContent(string $json)
     {
         return $this->mockServer()->decrypt(json_decode($json, true)['resp']);
+    }
+
+    protected function mockRazorx($param, $value, &$called)
+    {
+        $razorxMock = $this->getMockBuilder(RazorXClient::class)
+            ->setConstructorArgs([$this->app])
+            ->setMethods(['getTreatment'])
+            ->getMock();
+
+        $this->app->instance('razorx', $razorxMock);
+
+        $this->app->razorx->method('getTreatment')
+            ->will($this->returnCallback(
+                function ($mid, $feature, $mode) use($param, $value, &$called)
+                {
+                    if ($feature === $param)
+                    {
+                        $called = true;
+                        return $value;
+                    }
+
+                    return 'control';
+                }));
     }
 }
