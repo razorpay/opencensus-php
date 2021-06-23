d6d05eb214d107c637ca215608de993f6eba59db
Nilesh Kevlani <nilesh.kevlani@razorpay.com>
2021-06-23 23:46:00 +0530

    Increase TTL for redis key to 72 hours (#23707)

--- app/Models/Dispute/Core.php
+++ app/Models/Dispute/Core.php
@@ -30,8 +30,8 @@ class Core extends Base\Core
 {
     use FileHandlerTrait;
 
-    // 24 hours = 24*60*60
-    const REFUND_PROCESS_REDIS_TTL         = 86400;
+    // 72 hours = 72*60*60
+    const REFUND_PROCESS_REDIS_TTL         = 259200;
     const REFUND_PROCESS_REDIS_KEY         = 'dispute_refund_process_%s';
 
     const DEBIT_ADJUSTMENT_DESCRIPTION  = 'Debit disputed amount V2';
