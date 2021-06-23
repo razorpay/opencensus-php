cb801db5ecc13d579187107291b0a8cfa7b7d7bc
Kartik Agarwal <36539216+iamkagarwal@users.noreply.github.com>
2021-06-23 14:58:26 +0530

    add error codes (#23668)
    * add error codes
    
    * add error code
    
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Gateway/Netbanking/Hdfc/ErrorCode.php
+++ app/Gateway/Netbanking/Hdfc/ErrorCode.php
@@ -25,6 +25,13 @@ class ErrorCode
     const ACCOUNT_INVALID                       = 'No Valid account to complete this transaction.';
     const OTP_INCORRECT                         = 'OTP entered was incorrect. Please try again.';
     const UNAUTHORIZED                          = 'You are not authorized to do this transaction. Please contact the Bank.';
+    const UNABLE_TO_FETCH_STATUS                = 'Unfortunately, we could not receive the status of this transaction. We request you to kindly check your Account Balance/ Statement to confirm the transaction status. If the amount is not debited from (or credited to) your account, kindly re-initiate this ';
+    const FACILITY_UNAVAILABLE                  = 'We apologize, but this facility is temporarily unavailable. Please try again later.';
+    const AMOUNT_EXCEEDED                       = 'Amount exceeded the authorized merchant limit, Please contact the concerned website.';
+    const SECURE_ACCESS_NOT_ENABLED             = 'Secure Access not enabled. Please log into Netbanking to complete the Secure Access registration.';
+    const UNABLE_TO_CARRY_INSTRUCTION           = 'We are unable to carry out your instruction. Please contact the bank.';
+    const ACCOUNT_NOT_ACTIVATED                 = 'Your account has not been activated for NetBanking. Please contact the Bank.';
+    const DUPLICATE_TRANSACTION                 = 'Sorry, this is a duplicate transaction. A transaction with the same details has already been processed.';
 
     protected static $errorMap = [
         self::TRANSFER_TERMINATED_BY_USER         => Error\ErrorCode::BAD_REQUEST_PAYMENT_NETBANKING_CANCELLED_BY_USER,
@@ -50,6 +57,13 @@ class ErrorCode
         self::ACCOUNT_INVALID                     => Error\ErrorCode::BAD_REQUEST_PAYMENT_INVALID_ACCOUNT,
         self::OTP_INCORRECT                       => Error\ErrorCode::BAD_REQUEST_PAYMENT_OTP_INCORRECT,
         self::UNAUTHORIZED                        => Error\ErrorCode::BAD_REQUEST_PAYMENT_FAILED,
+        self::UNABLE_TO_FETCH_STATUS              => Error\ErrorCode::BAD_REQUEST_PAYMENT_FAILED,
+        self::FACILITY_UNAVAILABLE                => Error\ErrorCode::BAD_REQUEST_PAYMENT_BANK_SYSTEM_ERROR,
+        self::AMOUNT_EXCEEDED                     => Error\ErrorCode::GATEWAY_ERROR_TERMINAL_MAX_TRANSACTION_LIMIT_REACHED,
+        self::SECURE_ACCESS_NOT_ENABLED           => Error\ErrorCode::BAD_REQUEST_NETBANKING_USER_NOT_REGISTERED,
+        self::UNABLE_TO_CARRY_INSTRUCTION         => Error\ErrorCode::BAD_REQUEST_PAYMENT_FAILED,
+        self::ACCOUNT_NOT_ACTIVATED               => Error\ErrorCode::BAD_REQUEST_NETBANKING_USER_NOT_REGISTERED,
+        self::DUPLICATE_TRANSACTION               => Error\ErrorCode::GATEWAY_ERROR_PAYMENT_DUPLICATE_REQUEST,
     ];
 
     public static function getApiErrorCode(string $error): string
