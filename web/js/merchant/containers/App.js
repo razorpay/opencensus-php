02d931fa8d5fff172fce92ed28b997270958faed
Neeraj Kumar Singh <neeraj.singh@razorpay.com>
2021-06-25 14:08:25 +0530

    Seg add mobile (#7871)
    * added phone to segment identify call
    
    * added country code
    
    * fixed format
    
    * merge recent master

--- web/js/merchant/containers/App.js
+++ web/js/merchant/containers/App.js
@@ -183,7 +183,7 @@ export default class App extends Component {
             kycStatus,
             merchantId: user.current,
             businessCategory: user.businessCategory,
-            phone: user.contact_mobile,
+            phone: '+91'+user.contact_mobile,
             ...dataFromAPI,
           });
 
