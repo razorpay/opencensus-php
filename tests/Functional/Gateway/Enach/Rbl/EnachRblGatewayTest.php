b42893a35b52583db3639c3aa17b127040f9f528
shahbaz-razorpay <70690063+shahbaz-razorpay@users.noreply.github.com>
2021-06-28 13:03:20 +0530

    added new banks in aadhaar emandate list (#23765)
    * added new banks in aadhaar emandate list
    
    * added to zero rupee banks
    
    * added to old banks to zero rupee banks
    
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- tests/Functional/Gateway/Enach/Rbl/EnachRblGatewayTest.php
+++ tests/Functional/Gateway/Enach/Rbl/EnachRblGatewayTest.php
@@ -1279,7 +1279,7 @@ class EnachRblGatewayTest extends TestCase
             }
         }
 
-        $this->assertEquals(41, $count);
+        $this->assertEquals(44, $count);
 
         $this->assertArrayNotHasKey(IFSC::UTBI, $banks);
     }
