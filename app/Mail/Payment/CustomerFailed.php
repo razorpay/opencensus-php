c3cd14bd68c36e60e12f77078c938d0a22a12eda
shubhamiitmandi123 <70131730+shubhamiitmandi123@users.noreply.github.com>
2021-07-08 17:02:13 +0530

    support email is changed to support url for zerodha in Customer Email (#24016)
    * support email is changed to support url for zerodha
    
    * minor change
    
    * minor change
    
    * changing for authorized email as well
    
    * Revert "changing for authorized email as well"
    
    This reverts commit a13b9f0e38d0c2c6b7ea99f3158b85a13b61ed02.
    
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Mail/Payment/CustomerFailed.php
+++ app/Mail/Payment/CustomerFailed.php
@@ -7,6 +7,19 @@ use RZP\Mail\Base\Constants;
 
 class CustomerFailed extends Base
 {
+
+    const ZERODHA_MIDS = [
+        'EQ8AzfZip2meDu',
+        '4jrfbTLsua1pWJ',
+        '9VIi8FakOk1SiV',
+        '5zJACbxPORFLk8',
+        'F02iHSCplfL5m7',
+        'Eh54Q1B6HQKbS3',
+        'E2Mw08X9tYN35Z',
+        '5XBrWzODBkDPmi',
+        '4sR6aB3rYxH3Sv',
+    ];
+
     protected function addSubject()
     {
         $label = $this->data['merchant']['billing_label'] ?? $this->data['payment']['amount'];
@@ -51,4 +64,20 @@ class CustomerFailed extends Base
         return $this;
     }
 
+    protected function addMailData()
+    {
+       $data = $this->data;
+
+       if ((isset($data['merchant']['id']) === true) and
+           (isset($data['merchant']['support_details']) === true) and
+           (isset($data['merchant']['support_details']['email']) === true) and
+           (in_array($data['merchant']['id'], self::ZERODHA_MIDS) === true))
+       {
+           $data['merchant']['support_details']['email'] = 'support.zerodha.com';
+       }
+
+        $this->with($data);
+
+        return $this;
+    }
 }
