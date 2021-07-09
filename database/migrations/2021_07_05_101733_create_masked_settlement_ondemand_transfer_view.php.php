29563b97fbd4e7d81d9b54151549b6490704c3b1
prathyu1729 <prathyu1729@gmail.com>
2021-07-10 01:04:26 +0530

    ondemand tables views created (#23948)
    * ondemand tables views created
    
    * typo fix
    
    * view name change
    
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

---
+++ database/migrations/2021_07_05_101733_create_masked_settlement_ondemand_transfer_view.php.php
@@ -0,0 +1,50 @@
+<?php
+
+use RZP\Constants\Table;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMaskedSettlementOndemandTransferView extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $columns = [
+            'id',
+            'payout_id',
+            'mode',
+            'amount',
+            'status',
+            'processed_at',
+            'reversed_at',
+            'last_attempt_at',
+            'attempts',
+            'created_at',
+            'updated_at',
+            'deleted_at',
+        ];
+
+        $columnStr = implode(',', $columns);
+
+        $view = DB::getConfig('view_db') . '.masked_settlement_ondemand_transfer_view';
+
+        $statement = 'CREATE ALGORITHM=MERGE VIEW ' . wrap_db_table($view) . ' AS
+                        SELECT ' . $columnStr .
+            ' FROM ' . Table::SETTLEMENT_ONDEMAND_TRANSFER;
+
+        DB::statement($statement);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('DROP VIEW IF EXISTS masked_settlement_ondemand_transfer_view');
+    }
+}
---
+++ database/migrations/2021_07_05_102510_create_masked_settlement_ondemand_attempts_view.php.php
@@ -0,0 +1,46 @@
+<?php
+
+use RZP\Constants\Table;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMaskedSettlementOndemandAttemptsView extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $columns = [
+            'id',
+            'settlement_ondemand_transfer_id',
+            'payout_id',
+            'status',
+            'failure_reason',
+            'created_at',
+            'updated_at',
+            'deleted_at'
+        ];
+
+        $columnStr = implode(',', $columns);
+
+        $view = DB::getConfig('view_db') . '.masked_settlement_ondemand_attempts_view';
+
+        $statement = 'CREATE ALGORITHM=MERGE VIEW ' . wrap_db_table($view) . ' AS
+                        SELECT ' . $columnStr .
+            ' FROM ' . Table::SETTLEMENT_ONDEMAND_ATTEMPT;
+
+        DB::statement($statement);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('DROP VIEW IF EXISTS masked_settlement_ondemand_attempts_view');
+    }
+}
