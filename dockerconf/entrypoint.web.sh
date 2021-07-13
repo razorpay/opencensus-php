250d12e16b3932f2138db47ce7e7f7f2eca2209d
srinidhis05 <srinidhi.s@razorpay.com>
2021-07-13 21:56:45 +0530

    adds devspace yaml file and priting of logs to stdout for devstack (#23760)
    * adds devspace yaml file and priting of logs to stdout for devstack
    
    * Update devspace.yaml
    
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- dockerconf/entrypoint.web.sh
+++ dockerconf/entrypoint.web.sh
@@ -53,6 +53,8 @@ configure(){
     mkdir -p /var/log/apache/
     chown 0775 /var/log/apache/
     alohomora cast --region ap-south-1 --env "$APP_MODE" --app api "environment/env.php.j2" "dockerconf/api.apache.conf.j2"
+     # log the output into stdout as php monolog has a bug in logging
+    tail -F storage/logs/$HOSTNAME-trace-$(date +%Y-%m-%d).log &
  else
     alohomora cast --region ap-south-1 --env "$APP_MODE" --app api "environment/.env.vault.j2" "environment/env.php.j2" "dockerconf/api.apache.conf.j2"
   fi
