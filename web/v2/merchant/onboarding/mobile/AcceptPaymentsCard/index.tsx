ebaa2626973615b9302bf8168a1e5939cd107dd7
Abdul Shafi Ashraf <37157426+abdul00a@users.noreply.github.com>
2021-07-14 10:12:20 +0530

    fix: sentry error (#8026)
    * fix: sentry error
    
    * address PR comments

--- web/v2/merchant/onboarding/mobile/AcceptPaymentsCard/index.tsx
+++ web/v2/merchant/onboarding/mobile/AcceptPaymentsCard/index.tsx
@@ -256,7 +256,9 @@ const AcceptPaymentsCard: React.FC = () => {
     {
       retry: false,
       staleTime: Infinity,
-      onError: (err: any) => snackbar.error(err?.response?.errors[0]),
+      onError: (err: any) => {
+        if (err?.response?.errors) snackbar.error(err.response.errors[0]);
+      },
     },
   );
   const { status: websiteWorkflowQueryStatus, data: isWebsiteInWorkflow } = useQuery(
@@ -265,7 +267,9 @@ const AcceptPaymentsCard: React.FC = () => {
     {
       retry: false,
       staleTime: Infinity,
-      onError: (err: any) => snackbar.error(err?.response?.errors[0]),
+      onError: (err: any) => {
+        if (err?.response?.errors) snackbar.error(err.response.errors[0]);
+      },
     },
   );
   const {
