b685d6aacf4511d81edccefdba449a882abd3a26
ambar93 <ambar.maini@razorpay.com>
2021-07-28 00:01:58 +0530

    Bump up ifsc version to 1.6 (#24360)
    * bump up ifsc version to 1.6
    
    * fix test case
    
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- tests/Unit/Utility/IfscValidationTest.php
+++ tests/Unit/Utility/IfscValidationTest.php
@@ -27,7 +27,7 @@ class IfscValidationTest extends TestCase
 
         $this->assertArraySubset([
             'total' => 20911,
-            'valid' => 20884,
+            'valid' => 20881,
         ], $result);
     }
 }
