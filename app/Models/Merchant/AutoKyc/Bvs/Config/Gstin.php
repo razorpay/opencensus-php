2aeb95b05b847f50a8633dc54f99d777ac96e027
vasanthikakarla <84132254+vasanthikakarla@users.noreply.github.com>
2021-08-04 18:33:48 +0530

    bvs rule change for GSTIN

--- app/Models/Merchant/AutoKyc/Bvs/Config/Gstin.php
+++ app/Models/Merchant/AutoKyc/Bvs/Config/Gstin.php
@@ -14,67 +14,77 @@ class Gstin extends BaseConfig
         ],
     ];
 
-    protected $rule_v2 = [
-        'version'    => 'v2',
-        'rules_list' => [
-            '0' => [
-                'rule_type' => 'string_comparison_rule',
-                'rule_def'  =>
-                    [
-                        'or' => [
-                            [
-                                'fuzzy_suzzy' => [
-                                    [
-                                        'var' => 'artefact.details.legal_name.value',
-                                    ],
-                                    [
-                                        'var' => 'enrichments.online_provider.details.legal_name.value',
-                                    ],
-                                    81,
-                                ],
-                            ],
-                            [
-                                'fuzzy_wuzzy' => [
-                                    [
-                                        'var' => 'artefact.details.trade_name.value',
-                                    ],
-                                    [
-                                        'var' => 'enrichments.online_provider.details.trade_name.value',
-                                    ],
-                                    81,
-                                    [
-                                        "private limited",
-                                        "limited liability partnership",
-                                        "pvt",
-                                        "ltd",
-                                        "."
-                                    ]
-                                ]
-                            ],
-                        ],
-                    ],
-            ],
-            '1' => [
-                'rule_type' => 'array_comparison_rule',
-                'rule_def'  => [
-                    "some" => [
+    protected $rule_v2    = [
+        "version"    => "v3",
+        "rules_list" => [
+            "0" => [
+                "rule_type" => "string_comparison_rule",
+                "rule_def"  => [
+                    "or" => [
                         [
-                            'var' => 'enrichments.online_provider.details.signatory_names',
-                        ],
-                        [
-                            'fuzzy_suzzy' => [
+                            "fuzzy_suzzy" => [
                                 [
-                                    'var' => 'artefact.details.legal_name.value',
+                                    "var" => "artefact.details.legal_name.value"
                                 ],
                                 [
-                                    'var' => "each_array_element",
+                                    "var" => "enrichments.online_provider.details.legal_name.value"
+                                ],
+                                81
+                            ]
+                        ],
+                        [
+                            "fuzzy_wuzzy" => [
+                                [
+                                    "var" => "artefact.details.trade_name.value"
+                                ],
+                                [
+                                    "var" => "enrichments.online_provider.details.trade_name.value"
                                 ],
                                 81,
-                            ],
+                                [
+                                    "private limited",
+                                    "limited liability partnership",
+                                    "pvt",
+                                    "ltd",
+                                    "."
+                                ]
+                            ]
+                        ],
+                        [
+                            "fuzzy_suzzy" => [
+                                [
+                                    "var" => "artefact.details.trade_name.value"
+                                ],
+                                [
+                                    "var" => "enrichments.online_provider.details.legal_name.value"
+                                ],
+                                81
+                            ]
                         ]
                     ]
-                ],
+                ]
+            ],
+            "1" => [
+                "rule_type" => "array_comparison_rule",
+                "rule_def"  => [
+                    "some" => [
+                        [
+                            "var" => "enrichments.online_provider.details.signatory_names"
+                        ],
+                        [
+                            "fuzzy_wuzzy" => [
+                                [
+                                    "var" => "each_array_element"
+                                ],
+                                [
+                                    "var" => "artefact.details.legal_name.value"
+                                ],
+                                70
+                            ]
+                        ]
+                    ]
+                ]
             ]
-        ],
+        ]
     ];
 }
