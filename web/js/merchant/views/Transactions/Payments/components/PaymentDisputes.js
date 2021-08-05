b3273845291d8c1b70a376ada6878a593d479f9b
Anshu Raj <anshu.raj@razorpay.com>
2021-08-05 13:05:41 +0530

    feat:[TXNRISK-435] show open disputes by default in payment detail view (#8173)
    * Added payment dispute banner
    
    * open content toggler if any dispute is open
    
    * using stylus syntax

--- web/js/merchant/views/Transactions/Payments/components/PaymentDisputes.js
+++ web/js/merchant/views/Transactions/Payments/components/PaymentDisputes.js
@@ -3,25 +3,39 @@ import React from 'react';
 import Amount from 'common/ui/Amount';
 import ContentToggler from 'common/ui/Toggler/ContentToggler';
 import Definition from 'common/ui/Definition';
-import EntityDetailRow from 'merchant/components/EntityDetailRow';
-import { dispute as Id } from 'common/ui/item/id';
-import { titleCase } from 'common/utils/rzp-utils';
+import Time from 'common/ui/Time';
+import { dispute as disputeId } from 'common/ui/item/id';
 import { DisputeStatusLabel as StatusLabel } from 'merchant/components/StatusLabel';
 
-export default ({ disputes }) => {
+const PaymentDisputes = ({ disputes }) => {
+  const showContent = Boolean(disputes?.filter((d) => d.status === 'open').length);
   return (
-    <ContentToggler>
+    <ContentToggler show={showContent}>
       <span>{disputes.length} Disputes raised</span>
-      {disputes.map(dispute => (
-        <Definition allowEmptyTitle={true} key={dispute.id} customClass="m-t">
-          {Id(dispute)}
-          <div>
-            {titleCase(dispute.phase)},{' '}
-            <Amount value={dispute.amount} currency={dispute.currency} />
+      {disputes.map((dispute) => (
+        <Definition allowEmptyTitle={true} key={dispute.id} customClass="m-t payment-dispute">
+          {disputeId(dispute)}
+          <div class="m-t">
+            <Amount value={dispute.amount} currency={dispute.currency} className="p-r m-r" />
+            <StatusLabel status={dispute.status} />
           </div>
-          <StatusLabel status={dispute.status} />
+          {dispute.status === 'open' ? (
+            <div class="alert alert-warning banner">
+              <p>
+                Your customer has raised a {dispute.phase} on this payment. To avoid losing the
+                dispute&nbsp;
+                <u>
+                  respond before <Time value={dispute.respond_by} format="ll" />
+                </u>
+              </p>
+            </div>
+          ) : (
+            ''
+          )}
         </Definition>
       ))}
     </ContentToggler>
   );
 };
+
+export default PaymentDisputes;
