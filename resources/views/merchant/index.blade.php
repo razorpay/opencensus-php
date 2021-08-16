81c7565f234a91c042ace831cb236a22c34bf22b
Prashant Chaudhary <prashant.chaudhary@razorpay.com>
2021-08-16 10:26:56 +0530

    Revert "Revert "fix(signup): Open the signup module on website even if user is logged-in (#8242)"" (#8249)
    This reverts commit 522b33c0a5dcc49ff06e788b467240f212cbc425.

--- resources/views/merchant/index.blade.php
+++ resources/views/merchant/index.blade.php
@@ -123,7 +123,13 @@ _dcs.account = '9421167';
 <!-- Blank interface init before loading the project entry file -->
 @include('partials/rzpq-interface')
 
-@if ($isConfirmed and $isPreSignupComplete)
+@if ($newAuthFlow === true and app('request')->input('auth_source') === 'website')
+  <script type="text/javascript">
+      window.session_id = "{!! $session_id !!}"
+      window.isAuthPage = true;
+  </script>
+  <script src="{{$cdnDashboardUrl}}/dist/newAuth-entry.js"></script>
+@elseif ($isConfirmed and $isPreSignupComplete)
   <script type="text/javascript">
     window.rzp_user = {!! $user !!};
     window.rzp_org = {!! $org !!};
