8832c3fddfed2b8a7d988a7c4970f1e0b13f6aa2
Pullak Barik <39293478+TipsByPullak@users.noreply.github.com>
2021-09-02 19:34:58 +0530

    Add a missing semicolon in FtsStatusUpdateRetryConsumer (#24978)
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Services/Kafka/Consumers/FtsStatusUpdateRetryConsumer/Consumer.php
+++ app/Services/Kafka/Consumers/FtsStatusUpdateRetryConsumer/Consumer.php
@@ -85,7 +85,7 @@ class Consumer extends BaseConsumer
         $this->trace->info(TraceCode::KAFKA_FTS_STATUS_UPDATE_RETRY_CONSUMER_SLACK_NOTIFICATION_SENT, [
             "operation" => $operation,
             "payload"   => $this->getPayloadWithSensitiveDetailsMasked($payload),
-        ])
+        ]);
 
         (new SlackNotification)->send($operation, $payload, null, 1, 'rx_ca_rbl_alerts');
     }
