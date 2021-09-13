247d82255ffab5d6c73cdaebc9c4aa07b7abafde
premkumars459 <55206728+premkumars459@users.noreply.github.com>
2021-09-13 21:36:55 +0530

    change razorx name (#25138)
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Models/Merchant/RazorxTreatment.php
+++ app/Models/Merchant/RazorxTreatment.php
@@ -321,7 +321,7 @@ final class RazorxTreatment
     //Experiment to allow mtu coupon code application
     const MTU_COUPON_CODE = 'mtu_coupon_code';
 
-    const IGNORE_INDEX_IN_TRANSACTIONS_FETCH = 'ignore_index_in_transactions_fetch';
+    const IGNORE_INDEX_IN_TRANSACTIONS_FETCH = 'ignore_index_in_transactions_fetch_2';
 
     const BAS_FETCH_RE_ARCH = 'bas_fetch_re_arch';
 
