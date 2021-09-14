6fc4ca8777eeff8fc853f3e91243ecca4d04cfe8
Tamil Selvan V <tamilselvan.v@razorpay.com>
2021-09-14 19:21:52 +0530

    increased upi icici request timeout to 30 (#25148)
    * increased upi icici request timeout to 30
    
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Gateway/Upi/Icici/Gateway.php
+++ app/Gateway/Upi/Icici/Gateway.php
@@ -45,7 +45,7 @@ class Gateway extends Base\Gateway
      * Default request timeout duration in seconds.
      * @var  integer
      */
-    const TIMEOUT = 20;
+    const TIMEOUT = 30;
 
     protected $gateway = 'upi_icici';
 
