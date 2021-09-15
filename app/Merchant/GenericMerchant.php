a761b052dee7c414cd6b9922db84dbe4abda021c
zahlekhan <zahle.khan@razorpay.com>
2021-09-15 19:07:45 +0530

    add 2nd demo merchant id to test accounts (#8492)
    * add 2nd demo merchant id to test accounts
    
    * rearrange TEST_MERCHANT_IDS

--- app/Merchant/GenericMerchant.php
+++ app/Merchant/GenericMerchant.php
@@ -15,7 +15,9 @@ class GenericMerchant
         '10000000000000',
         '100DemoAccount',
         'Hrw2ujXW6LGEk7', // Demo banking user merchant account - Beta
-        'HpoQK4cqkegpKO'  // Demo banking user merchant account - Prod
+        'HxkEbyIrQQsBbf', // Demo banking user merchant account - Beta
+        'HpoQK4cqkegpKO',  // Demo banking user merchant account - Prod
+        'HxhLvOJVj2h3MT'  // Demo banking user 2 merchant account - Prod
     ];
 
     /**
