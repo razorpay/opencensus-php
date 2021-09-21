6d753a45227673d84839182d9b4bc8f10aa5fb4b
prasanth08Kumar99 <87698061+prasanth08Kumar99@users.noreply.github.com>
2021-09-21 17:05:35 +0530

    Implemented basic functional logic (#25212)
    * Implemented basic functional logic
    
    * With changes after review
    
    * Unit testcases added with only VISA and gatewaycode 81
    
    * Addressed all possible testcases and added single line gaps
    
    * missed to add single line gap
    
    * removed unnecessary if conditions and used tree import
    
    * Changes made a/c to updated tech specs when entry not found
    
    * Changed network of VISA to RZP when code not found in map
    
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Models/Dispute/Reason/Service.php
+++ app/Models/Dispute/Reason/Service.php
@@ -3,6 +3,8 @@
 namespace RZP\Models\Dispute\Reason;
 
 use RZP\Models\Base;
+use RZP\Models\Card\NetworkName;
+use RZP\Models\Dispute\Reason\Constants as ReasonConstants;
 
 class Service extends Base\Service
 {
@@ -30,5 +32,51 @@ class Service extends Base\Service
         return $this->repo
             ->dispute_reason
             ->getReasonByNetworkAndGatewayCode($network, $gatewayCode);
+    }
+
+    public function getReasonAndNetworkCode(string $reasonCode, string $network)
+    {
+        $code = $reasonCode;
+
+        $newCode = '';
+
+        $reason = [];
+
+        if ($network == NetworkName::VISA)
+        {
+            if (array_key_exists($reasonCode, ReasonConstants::VISA_REASON_CODE_VS_CURRENT_REASON_CODE))
+            {
+                $newCode = ReasonConstants::VISA_REASON_CODE_VS_CURRENT_REASON_CODE[$reasonCode];
+
+                $code = $newCode;
+            }
+            else
+            {
+                $network = 'RZP';
+
+                $code = 'RZP00';
+            }
+        }
+
+        try
+        {
+            $disputeReason = $this->getReasonByNetworkAndGatewayCode($network, $code);
+
+            $reason['network_code'] = $network . '-' . $code;
+
+            $reason['reason_code'] = $disputeReason->getCode();
+
+            return $reason;
+        }
+        catch (\Exception $e)
+        {
+            $disputeReason = $this->getReasonByNetworkAndGatewayCode('RZP', 'RZP00');
+
+            $reason['network_code'] = 'RZP-RZP00';
+
+            $reason['reason_code'] = $disputeReason->getCode();
+
+            return $reason;
+        }
     }
 }
--- tests/Unit/Models/Dispute/CoreTest.php
+++ tests/Unit/Models/Dispute/CoreTest.php
@@ -3,13 +3,39 @@
 namespace RZP\Tests\Unit\Models\Dispute;
 
 use Carbon\Carbon;
+use RZP\Constants\Table;
 use RZP\Models\Dispute\Entity;
 use RZP\Models\Dispute\Constants;
+use Illuminate\Support\Facades\DB;
 use RZP\Tests\Functional\TestCase;
+use RZP\Models\Dispute\Reason\Service as DisputeReasonService;
 use RZP\Models\Merchant\FreshdeskTicket\Constants as FreshdeskConstants;
 
 class CoreTest extends TestCase
 {
+    public function testGetReasonAndNetworkCode()
+    {
+        $reasons = $this->getReasonsForGetReasonAndNetworkCode();
+
+        foreach ($reasons as $reason)
+        {
+            DB::table(Table::DISPUTE_REASON)->insert($reason);
+        }
+
+        $tests = $this->getTestsForGetReasonAndNetworkCode();
+
+        foreach ($tests as $test)
+        {
+            $response = DisputeReasonService::getNewInstance()->getReasonAndNetworkCode(
+                $test['input_reason_code'], $test['network']
+            );
+
+            $this->assertEquals($test['expected_network_code'], $response['network_code']);
+
+            $this->assertEquals('this is the code', $response['reason_code']);
+        }
+    }
+
     public function testCustomerDisputeFDInstanceRoute()
     {
         $payment = $this->fixtures->create('payment:captured');
@@ -59,5 +85,76 @@ class CoreTest extends TestCase
         $fdInstance = $getFdInstanceMethod->invokeArgs($disputeCore, [$dispute]);
 
         $this->assertEquals($fdInstance, FreshdeskConstants::URL);
+    }
+
+    private function getReasonsForGetReasonAndNetworkCode()
+    {
+        $reasons = [
+            [
+                'id'                  => str_random(14),
+                'network'             => 'Visa',
+                'gateway_code'        => '81',
+                'gateway_description' => '',
+                'code'                => 'this is the code',
+                'description'         => '',
+                'created_at'          => time(),
+                'updated_at'          => time(),
+            ],
+            [
+                'id'                  => str_random(14),
+                'network'             => 'RuPay',
+                'gateway_code'        => '123',
+                'gateway_description' => '',
+                'code'                => 'this is the code',
+                'description'         => '',
+                'created_at'          => time(),
+                'updated_at'          => time(),
+            ],
+            [
+                'id'                  => str_random(14),
+                'network'             => 'RZP',
+                'gateway_code'        => 'RZP00',
+                'gateway_description' => '',
+                'code'                => 'this is the code',
+                'description'         => '',
+                'created_at'          => time(),
+                'updated_at'          => time(),
+            ]
+        ];
+
+        return $reasons;
+    }
+
+    private function getTestsForGetReasonAndNetworkCode()
+    {
+        $tests = [
+            [
+                'network'                => 'Visa',
+                'input_reason_code'      => '10.3',
+                'expected_network_code'  => 'Visa-81',
+            ],
+            [
+                'network'                => 'Visa',
+                'input_reason_code'      => '12.7',
+                'expected_network_code'  => 'RZP-RZP00',
+            ],
+            [
+                'network'                => 'MasterCard',
+                'input_reason_code'      => '123',
+                'expected_network_code'  => 'RZP-RZP00',
+            ],
+            [
+                'network'                => 'RuPay',
+                'input_reason_code'      => '123',
+                'expected_network_code'  => 'RuPay-123',
+            ],
+            [
+                'network'                => 'Visa',
+                'input_reason_code'      => '13.3',
+                'expected_network_code'  => 'RZP-RZP00',
+            ]
+        ];
+
+        return $tests;
     }
 }
