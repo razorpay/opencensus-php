63ee7ae3f1b2585de4d33951bdaaeae4017b9273
Prashant Yadav <knowprashantyadav@gmail.com>
2021-09-28 11:26:11 +0530

    (ROUTING-1162) Atom UI change - Added textual instructions rather than image (#8591)
    * Atom UI change - Added textual instructions rather than image
    
    * removed fragement

--- web/js/merchant/views/Navigator/components/Provider/HowToGetDetails.js
+++ web/js/merchant/views/Navigator/components/Provider/HowToGetDetails.js
@@ -3,11 +3,22 @@ import { gatewayDetailsMapping } from '../util';
 
 export const HowToGetDetails = ({ providers, selectedProvider, closeModal }) => {
   const gatewayName = providers[selectedProvider]['Gateway Name'].data_value;
+
   return (
     <div className="gateway-details-desc--how-to-modal">
       <ModalHeader onCloseClick={closeModal} title={`Where do I find ${gatewayName} details?`} />
       <div className="modal-body">
-        <img src={gatewayDetailsMapping[selectedProvider].dashboardImg} />
+        {selectedProvider === 'atom' ? (
+          <div className="atom-block-wrapper">
+            <p className="atom-block-title">Have a registered Atom Business Account</p>
+            <p className="atom-block-desc">
+              For API keys details, you need to refer to the excel sheet shared by Atom on your
+              registered email Id.
+            </p>
+          </div>
+        ) : (
+          <img src={gatewayDetailsMapping[selectedProvider].dashboardImg} />
+        )}
         <div className="acc-info-msg">
           <i className="i i-info-outline" />
           Don’t have a registered {gatewayName} Business Account, register on{' '}
