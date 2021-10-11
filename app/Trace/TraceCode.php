cb00f5a6bac0cd12b84930a2b7d314a79f86a4c6
Aniket Agarwalla <44133710+ani37@users.noreply.github.com>
2021-10-12 00:16:48 +0530

    function to get payment id from rrn and arn (#25399)
    * function to get payment id from rrn and arn
    
    * fixed suggested changes
    
    * fixed extra space
    
    * added suggested changes
    
    * fixed extra space
    
    * added suggested changes
    
    * fixed suggested issues
    
    * minor correction
    
    * added suggested changes
    
    * formated
    
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Trace/TraceCode.php
+++ app/Trace/TraceCode.php
@@ -4686,6 +4686,8 @@ class TraceCode extends \Razorpay\Trace\TraceCode
     const STUCK_PAYOUTS_NOTIFY_EMAIL_FAILURE                      = 'STUCK_PAYOUTS_NOTIFY_EMAIL_FAILURE';
 
     const DRUID_REQUEST_FAILURE                                   = 'DRUID_REQUEST_FAILURE';
+    const PAYMENT_DATA_NOT_FOUND_ON_DRUID                         = 'PAYMENT_DATA_NOT_FOUND_ON_DRUID';
+
     const DATALAKE_PRESTO_QUERY                                   = 'DATALAKE_PRESTO_QUERY';
     const DATALAKE_PRESTO_REQUEST_FAILURE                         = 'DATALAKE_PRESTO_REQUEST_FAILURE';
 
