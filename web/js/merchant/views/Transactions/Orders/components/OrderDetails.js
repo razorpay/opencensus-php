6cbc84a12f7c3ac185a6925a400e927e160c9976
Ritesh Ganjewala <42371840+InvincibleZeal@users.noreply.github.com>
2021-10-12 15:20:07 +0530

    bugfix(sentry): check typeof order notes in order details (#8723)

--- web/js/merchant/views/Transactions/Orders/components/OrderDetails.js
+++ web/js/merchant/views/Transactions/Orders/components/OrderDetails.js
@@ -84,7 +84,7 @@ export default (props) => {
               )}
 
               <EntityDetailRow label="Notes">
-                {Object.keys(order.notes).length
+                {!(order.notes instanceof Array)
                   ? Object.keys(order.notes).map((key, index) => (
                       <Definition key={index} customClass="notes">
                         {key}
