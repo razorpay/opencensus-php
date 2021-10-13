ce0dfccb1f06fc26ade90b7bfb66c0ffb203b755
varchasva-tiwari <varchasva.tiwari@razorpay.com>
2021-10-13 11:16:06 +0530

    Added public message for TXN_REJECTED_BENE_BANK_RETRIABLE error code (#25631)
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Models/Payout/ErrorCodeMapping.php
+++ app/Models/Payout/ErrorCodeMapping.php
@@ -170,7 +170,8 @@ class ErrorCodeMapping
         'MERCHANT_INSUFFICIENT_FUND'            => 'Payout failed due to insufficient funds in the bank account.',
         'FTS_BANK_TXN_LIMIT_CROSSED'            => 'Payout failed due to extended NEFT window limit exhausted. Please try again in the next NEFT window.',
         'MERCHANT_FROZEN_ACCOUNT'               => 'Payout failed as the debit account is under freeze by the partner bank.',
-        'PBANK_VALIDATION_ERROR_PENDING'        => 'Temporary Issue at Partner bank. Reinitiate transfer after 30 min.'
+        'PBANK_VALIDATION_ERROR_PENDING'        => 'Temporary Issue at Partner bank. Reinitiate transfer after 30 min.',
+        'TXN_REJECTED_BENE_BANK_RETRIABLE'      => 'Transaction not permitted to beneficiary account.'
     ];
 
     public static $alternateFailureReasonMapping = [
