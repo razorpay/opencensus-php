916d97ffc1069fa7abe5f9a71a61678a3a01aad3
Shivam Anand Murmu <35562703+Rozamo@users.noreply.github.com>
2021-11-09 14:17:54 +0530

    feat: added banking service url to window (#8893)
    * feat: added banking service url to window
    
    * feat: added banking_service_url in prod

--- resources/views/partials/environment.blade.php
+++ resources/views/partials/environment.blade.php
@@ -7,6 +7,7 @@
     window.SHIELD_STAGE = "{{ env('SHIELD_STAGE') }}";
     window.cdnDashboardUrl = "{{ config('app.cdn_dashboard_url') }}";
     window.cdnBaseUrl = "{{ config('app.cdn_base_url') }}";
+    window.bankingServiceUrl = "{{ config('app.banking_service_url') }}";
     window.OAUTH_CLIENT_ID = "{{ env('MERCHANT_OAUTH_CLIENT_ID') }}";
     window.INVISIBLE_CAPTCHA_SITE_KEY = "{{ env('INVISIBLE_CAPTCHA_SITE_KEY') }}";
     window.CHECKBOX_CAPTCHA_SITE_KEY = "{{ env('CHECKBOX_CAPTCHA_SITE_KEY') }}";
