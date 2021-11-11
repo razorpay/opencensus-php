6f9bbb6e53d0f084b794d3f16e3e0a89de850789
parthanand31-sudo <66663796+parthanand31-sudo@users.noreply.github.com>
2021-11-11 19:44:26 +0530

    [cardless emi]adding error code module branch name (#26289)
    * adding error code module branch name
    
    * fix
    
    * reverting branch name from api
    
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- tests/Functional/Gateway/CardlessEmi/CardlessEmiGatewayTestData.php
+++ tests/Functional/Gateway/CardlessEmi/CardlessEmiGatewayTestData.php
@@ -291,7 +291,7 @@ return [
             'content'     => [
                 'error' => [
                     'code'        => PublicErrorCode::BAD_REQUEST_ERROR,
-                    'description' => PublicErrorDescription::BAD_REQUEST_PAYMENT_FAILED,
+                    'description' => "Your payment didn't go through due to a temporary issue. Any debited amount will be refunded in 4-5 business days.",
                 ],
             ],
             'status_code' => 400,
@@ -355,7 +355,7 @@ return [
             'content'     => [
                 'error' => [
                     'code'        => PublicErrorCode::GATEWAY_ERROR,
-                    'description' => PublicErrorDescription::GATEWAY_ERROR,
+                    'description' => "Your payment didn't go through due to a temporary issue. Any debited amount will be refunded in 4-5 business days.",
                 ],
             ],
             'status_code' => 502,
