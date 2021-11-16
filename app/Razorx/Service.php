1b0adaffe4ee5691222aa8b4974c249b45e069ec
Burhanuddin Udaipurwala <udaipurwalaburhanuddin@gmail.com>
2021-11-16 17:05:42 +0530

    feat: add zoho cashflow feature flag (#8752)
    * feat: add zoho cashflow feature flag
    
    * feat: add zoho cashflow experiment

--- app/Razorx/Service.php
+++ app/Razorx/Service.php
@@ -198,6 +198,7 @@ class Service extends Base\Service
             'rx_ca_self_serve_flow_neo',
             'rx_non_self_serve_ca_flow',
             'add_on_card_onboarding',
+            'zoho_cashflow',
             'dashboard_super_checkout',
             'lite_onboarding',
             'stores',
