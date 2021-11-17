f0584d338de822750cb4d58eae2e21759bb8b426
shahbaz-razorpay <70690063+shahbaz-razorpay@users.noreply.github.com>
2021-11-18 04:07:27 +0530

    fix bug in transaction create for walnut (#26446)

--- app/Models/Transaction/Processor/Payment.php
+++ app/Models/Transaction/Processor/Payment.php
@@ -353,7 +353,7 @@ class Payment extends Base
             case ($this->isVasMerchantWithDirectSettlement()):
             case ($this->txn->isPostpaid() === true):
             case ($this->txn->getCreditType() === Transaction\CreditType::FEE):
-            case (($this->source->isCardlessEmiWalnut369()) and ($this->merchant->isFeatureEnabled(Feature\Constants::SOURCED_BY_WALNUT369) === true)):
+            case (($this->source->isCardlessEmiWalnut369()) and ($this->source->merchant->isFeatureEnabled(Feature\Constants::SOURCED_BY_WALNUT369) === true)):
             case ($this->txn->getCreditType() === Transaction\CreditType::AMOUNT):
                 $netAmount = $amount;
                 break;
@@ -447,7 +447,7 @@ class Payment extends Base
         /* For the walnut369 sourced merchant, we don't apply our pricing on the payment and instead settle the amount
          * based on the subvention/mdr received in the payment which is used to create discount entity
          * */
-        if (($payment->isCardlessEmiWalnut369() === true) and ($this->merchant->isFeatureEnabled(Feature\Constants::SOURCED_BY_WALNUT369) === true))
+        if (($payment->isCardlessEmiWalnut369() === true) and ($payment->merchant->isFeatureEnabled(Feature\Constants::SOURCED_BY_WALNUT369) === true))
         {
             $discount = $this->repo->discount->fetchForPayment($payment);
 
