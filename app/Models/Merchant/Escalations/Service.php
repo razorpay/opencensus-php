5b1b608b97fe6228aed72c002f68623af65c9547
snehagupta01 <43209043+snehagupta01@users.noreply.github.com>
2021-11-18 19:11:18 +0530

    -mtu notfn (#26454)
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Models/Merchant/Escalations/Service.php
+++ app/Models/Merchant/Escalations/Service.php
@@ -57,16 +57,16 @@ class Service extends Base\Service
                 'error' => $e->getMessage()
             ]);
         }
-        try
-        {
-            $core->sendNotificationsToCouponCodeEligibleMerchant($input);
-        }
-        catch (\Exception $e)
-        {
-            $this->trace->info(TraceCode::COUPON_CODE_ELIGIBLE_MERCHANT_NOT_MTU_NOTIFICATION_FAILED, [
-                'type' => 'sendCouponCodeEligibleMerchantNotMTUNotification', 'error' => $e->getMessage()
-            ]);
-        }
+//        try
+//        {
+//            $core->sendNotificationsToCouponCodeEligibleMerchant($input);
+//        }
+//        catch (\Exception $e)
+//        {
+//            $this->trace->info(TraceCode::COUPON_CODE_ELIGIBLE_MERCHANT_NOT_MTU_NOTIFICATION_FAILED, [
+//                'type' => 'sendCouponCodeEligibleMerchantNotMTUNotification', 'error' => $e->getMessage()
+//            ]);
+//        }
         try
         {
             $core->pushTransactionDetailsToSegmentCron();
