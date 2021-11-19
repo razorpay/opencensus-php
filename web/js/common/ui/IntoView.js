39403449860b8b223f1722705db59020f58b55a6
Ritesh Ganjewala <42371840+InvincibleZeal@users.noreply.github.com>
2021-11-19 13:22:15 +0530

    feat(ui/intoview): support an array of routes for IntoView component (#9009)
    * feat(ui/intoview): support route/array of routes in IntoView component
    
    * doc(intoview): add examples for intoview component
    
    * doc(intoview): corrections in intoview doc
    
    * feat(intoview): update scroll logic

--- web/js/common/ui/IntoView.js
+++ web/js/common/ui/IntoView.js
@@ -2,19 +2,35 @@ import React, { useRef, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 import scrollTo from 'common/utils/scrollTo';
 
+/**
+ * Component to scroll the `children` component into view
+ *
+ * @param {React.Component} children - Component to scroll into view
+ * @param {(string|string[])} hashedWith - scroll if `hashedWith` route(s) is present in the url
+ * @return {React.Component} Component
+ * @example <IntoView hashedWith={"route"}><ChildComponent /></IntoView>
+ * @example <IntoView hashedWith={["route1", "route2"]}><ChildComponent /></IntoView>
+ */
 function IntoView({ children, location, hashedWith }) {
   const showView = useRef(null);
 
   useEffect(() => {
-    if (location.pathname && location.pathname.includes(hashedWith) && showView.current) {
-      const {offsetTop}=showView.current
-      if(offsetTop){
-        scrollTo({endPos:offsetTop});
+    if (!location.pathname) return;
+
+    const urlSegments = location.pathname.split('/');
+
+    const hashPresent = Array.isArray(hashedWith)
+      ? hashedWith.some((hash) => urlSegments.includes(hash))
+      : urlSegments.includes(hashedWith);
+
+    if (hashPresent && showView.current) {
+      const { offsetTop } = showView.current;
+      if (offsetTop) {
+        scrollTo({ endPos: offsetTop });
       }
-      
     }
-  },[]);
- 
+  }, []);
+
   return (
     <div className="scroll-into-view" ref={showView}>
       {children}
