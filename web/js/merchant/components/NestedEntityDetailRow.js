83e6f48f9c15a1cd8147ec3f2657c330a0e4e772
Suhas R. More <suhas.more@razorpay.com>
2021-11-24 11:31:41 +0530

    Quick fix token details failure (#9078)
    * Fixed(CAW): Fixes issue, token details failing to load
    
    * Fix: removed console log statement

--- web/js/merchant/components/NestedEntityDetailRow.js
+++ web/js/merchant/components/NestedEntityDetailRow.js
@@ -4,11 +4,11 @@ import NestedEntityDetailToggler from 'common/ui/Toggler/NestedEntityDetailToggl
 export default ({ label, value = {} }) => {
   const placeholder = '--';
 
-  if (Object.keys(value).length) {
+  if (value && Object.keys(value).length) {
     return (
       <NestedEntityDetailToggler label={label} show={false}>
         <div class="table-responsive">
-          {Object.keys(value).map(key => (
+          {Object.keys(value).map((key) => (
             <div key={key} class="pair-list-item">
               <div class="item-label">{key}</div>
               <div class="items-value">{value[key] || placeholder}</div>
