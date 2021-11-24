15a07de53745bdc88e939091adffd48ee6a1e814
Neeraj Kumar Singh <neeraj.singh@razorpay.com>
2021-11-24 15:40:43 +0530

    refactor: added slack integration video and slack listing on App store (#9085)

--- web/js/merchant/views/PartnerAppStore/data/content/slack.js
+++ web/js/merchant/views/PartnerAppStore/data/content/slack.js
@@ -2,6 +2,7 @@ function Data(brandColor) {
   const brandStyles = {
     brandBorderLeft: { borderLeft: `8px solid ${brandColor}` },
   };
+  const howToVideoUrl = 'https://www.youtube.com/embed/fCncEZiN1xc';
 
   return (
     <main>
@@ -17,55 +18,16 @@ function Data(brandColor) {
       </div>
       <div className="how-to-use-razorpay">
         <article>
-          <section>
-            <h2 className="how-to-use-heading" style={brandStyles.brandBorderLeft}>
-              How to use the Razorpay app on Slack?
-            </h2>
-            <p style={brandStyles.brandBorderLeft}>
-              You will need an activated{' '}
-              <a href="https://dashboard.razorpay.com">Razorpay account</a> and a Slack account for
-              this integration to work.
-            </p>
-          </section>
+          <h2 className="how-to-use-heading" style={brandStyles.brandBorderLeft}>
+            How to use the Razorpay app on Slack?
+          </h2>
+          <p style={brandStyles.brandBorderLeft}>
+            You will need an activated <a href="https://dashboard.razorpay.com">Razorpay account</a>{' '}
+            and a Slack account for this integration to work.
+          </p>
           <section className="how-to-use-video">
-            <p>Here is how you can use this app with four simple steps</p>
-            <ul>
-              <li>
-                Click on{' '}
-                <a
-                  href="https://rzp-labs-slack.razorpay.com/slack-connect"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  ‘Get Started’
-                </a>{' '}
-                and log in to your Slack account
-              </li>
-              <li>
-                If you are admin of the Slack workspace you wish to install, then click authorise in
-                next step, else request your admin to authorize
-                <br />
-                <br />
-                Once authorized, Razorpay will be added to your workspace.
-              </li>
-              <li>
-                Type <b>“/razorpay connect” </b> in any channel and send. As a response you will see
-                a welcome message with the <b>“Connect to Razorpay”</b> button. Click on the same
-                and follow the steps as it is.
-                <br />
-                <br />
-                You will need owner access to a Razorpay account to make this step work. Once done,
-                you should see a success message from Razorpay
-              </li>
-              <li>
-                Once connected, type <b>‘/razorpay subscribe payment_downtimes’</b> in a channel
-                where you wish to get notifications.
-              </li>
-            </ul>
-            <p>
-              That’s it! Your Razorpay payment downtime updates on slack are configured. Now sit
-              back and relax. When there is downtime next time we will alert you!
-            </p>
+            <p>Watch this quick video tutorial to start using Razorpay with Slack.</p>
+            <iframe width="560" height="315" src={howToVideoUrl} frameBorder="0" allowFullScreen />
           </section>
           <section className="queries-section">
             <p>
