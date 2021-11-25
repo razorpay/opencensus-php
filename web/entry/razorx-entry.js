c315b1c6ea4dd3aba1c1201b233d56d5cafac42f
Reet Srivastava <55198441+reetsrivastava@users.noreply.github.com>
2021-11-25 11:36:02 +0530

    [DAS-1355] Replace document.write with appendChild (#9088)
    * perf replaced document.write with appendChild
    
    * fix linting errors
    
    * refactor removed comments, added merchant-entry files to eslintignore
    
    * Adding EOL eslintignore

--- web/entry/razorx-entry.js
+++ web/entry/razorx-entry.js
@@ -1,25 +1,32 @@
 function RazorxEntry() {
   function executeJS() {
-    var cdnDashboardUrl = window.cdnDashboardUrl || '';
-    var base = Array.prototype.slice
-      .call(document.querySelectorAll('script[src]'), -1)[0]
-      .src.replace(/[^\/]+$/, '');
+    const cdnDashboardUrl = window.cdnDashboardUrl || '';
 
-    var appendLink = function (src) {
-      var link = document.createElement('link');
+    const base = Array.prototype.slice
+      .call(document.querySelectorAll('script[src]'), -1)[0]
+      .src.replace(/[^/]+$/, '');
+
+    const appendLink = (src) => {
+      const link = document.createElement('link');
       link.rel = 'stylesheet';
       link.href = src;
       document.documentElement.appendChild(link);
+    };
+
+    const appendScript = (src) => {
+      const s = document.createElement('script');
+      s.src = src;
+      document.documentElement.appendChild(s);
     };
 
     appendLink(
       'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
     );
 
-    websiteAssets.js.forEach(function (src) {
-      document.write('<script src="' + cdnDashboardUrl + src + '"></script>');
+    websiteAssets.js.forEach((src) => {
+      appendScript(cdnDashboardUrl + src);
     });
-    websiteAssets.css.forEach(function (src) {
+    websiteAssets.css.forEach((src) => {
       appendLink(cdnDashboardUrl + src);
     });
   }
