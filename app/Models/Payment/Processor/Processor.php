2b7d343c0fda75e0e801e206290ee14f1719b3d2
saikiransaindla2 <51969015+saikiransaindla2@users.noreply.github.com>
2021-11-29 16:38:38 +0530

    Added hosted in allowed libraries for tokenisation consent (#26667)
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Models/Payment/Processor/Processor.php
+++ app/Models/Payment/Processor/Processor.php
@@ -707,7 +707,7 @@ class Processor
             $paymentMethod = $input[Payment\Entity::METHOD] ?? '';
             $paymentMethodsUsingCards = [Payment\Entity::CARD, Payment\Entity::EMI];
             $library = $input['_']['library'] ?? '';
-            $allowedLibraries = [Payment\Analytics\Metadata::CHECKOUTJS];
+            $allowedLibraries = [Payment\Analytics\Metadata::CHECKOUTJS, Payment\Analytics\Metadata::HOSTED];
 
             $this->trace->info(
                 TraceCode::TOKENISATION_CONSENT_LOG,
