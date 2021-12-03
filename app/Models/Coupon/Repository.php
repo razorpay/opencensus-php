f82208173d0c877956960bd126f57d81d8542a70
vasanthikakarla <84132254+vasanthikakarla@users.noreply.github.com>
2021-12-03 18:07:40 +0530

    fix test case (#26820)

--- app/Models/Coupon/Repository.php
+++ app/Models/Coupon/Repository.php
@@ -40,7 +40,7 @@ class Repository extends Base\Repository
     {
         $allowedMerchantIds = [Merchant\Account::SHARED_ACCOUNT, $merchantId];
 
-        return $this->newQueryWithConnection($this->getSlaveConnection('live'))
+        return $this->newQuery()
                     ->where(Entity::CODE, '=', $code)
                     ->whereIn(Entity::MERCHANT_ID, $allowedMerchantIds)
                     ->with(['source'])
