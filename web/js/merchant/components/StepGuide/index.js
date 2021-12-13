44389fd794e352685bf3a657059f85fda3b3a9af
snehasish das gupta <snehasish.dasgupta@razorpay.com>
2021-12-13 17:04:29 +0530

    fix(DAS-1470): step indicator support added for stepGuide Component (#9297)

--- web/js/merchant/components/StepGuide/index.js
+++ web/js/merchant/components/StepGuide/index.js
@@ -11,11 +11,23 @@ export default class StepGuide extends React.Component {
 
     this.state = {
       activeStep: props.activeStep || 0,
+      showLeftIndicator: false,
+      showRightIndicator: true,
     };
   }
 
-  setActiveStep = (activeStep = 0) => () => {
+  /* Method to get the step number based on which indicator is clicked */
+  handleSlideIndicator = (slideLeft) => {
+    const { activeStep } = this.state;
+    const stepNumber = slideLeft ? activeStep - 1 : activeStep + 1;
+    this.setActiveStep(stepNumber);
+  };
+  /* Method to update the state Regarding the active Step */
+  setActiveStep = (activeStep = 0) => {
+    const { children } = this.props;
     this.setState({
+      showLeftIndicator: !(activeStep === 0),
+      showRightIndicator: !(activeStep === children?.length - 1),
       activeStep,
     });
   };
@@ -30,7 +42,7 @@ export default class StepGuide extends React.Component {
 
   render() {
     const { className, title, children, closeBtn, isMobileResolution, org } = this.props;
-    const { activeStep } = this.state;
+    const { activeStep, showLeftIndicator, showRightIndicator } = this.state;
 
     const CloseBtn = closeBtn && (closeBtn || <i class="i i-close" />);
 
@@ -43,6 +55,18 @@ export default class StepGuide extends React.Component {
           org.custom_code === 'axis' && 'no-img',
         )}
       >
+        {showLeftIndicator && (
+          <i
+            class="StepGuide-indicator i i-chevron-left"
+            onClick={() => this.handleSlideIndicator(true)}
+          />
+        )}
+        {showRightIndicator && (
+          <i
+            class="StepGuide-indicator i i-chevron-right"
+            onClick={() => this.handleSlideIndicator(false)}
+          />
+        )}
         {title && <div class="StepGuide--Title">{title}</div>}
 
         <div class="StepGuide--Steps">{isMobileResolution ? children[activeStep] : children}</div>
@@ -55,7 +79,7 @@ export default class StepGuide extends React.Component {
               <div
                 key={stepNum}
                 className={classList('Switcher-switch', activeStep === stepNum && 'active')}
-                onClick={this.setActiveStep(stepNum)}
+                onClick={() => this.setActiveStep(stepNum)}
               />
             ))}
           </div>
