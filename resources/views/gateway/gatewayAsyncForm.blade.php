126d4d739658f485930afc5c7381b8cdc5617b5d
Manish Chauhan <86999815+manish-chauhanrzp@users.noreply.github.com>
2021-12-13 21:07:01 +0530

    key_id removed in cancel url and  redirect callback url  (#27020)
    * key id removed
    
    * key_id removed
    
    * key id removed
    
    * Key is will be used if present
    
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- resources/views/gateway/gatewayAsyncForm.blade.php
+++ resources/views/gateway/gatewayAsyncForm.blade.php
@@ -168,8 +168,12 @@
     var request_url = data.request.url;
     var key_id = '{{ App::getFacadeRoot()['basicauth']->getPublicKey() }}';
     var payment_base = '{{$data["api"]}}/v1/payments/' + data.payment_id;
-    var cancel_url = payment_base + '/cancel?key_id='+key_id;
-    var callback_url = payment_base + '/redirect_callback?key_id='+key_id;
+    var query_param = '';
+    if (key_id != '') {
+        query_param = '?key_id=' + key_id;
+    }
+    var cancel_url = payment_base + '/cancel'+query_param;
+    var callback_url = payment_base + '/redirect_callback'+query_param;
 
     var $ =  function (id) {
       return document.getElementById(id);
