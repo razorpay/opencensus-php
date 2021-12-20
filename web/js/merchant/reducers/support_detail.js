51bbaf21f6615d9f7ee3dcd44a8e0779727279e2
snehasish das gupta <snehasish.dasgupta@razorpay.com>
2021-12-20 19:44:23 +0530

    refac(DAS-1524):  (#9336)
    * refac(DAS-1524): added the UI support for the API response success for no data in DB for support Details
    
    * refac(DAS-1524): eslint fixes
    
    * refac(DAS-1524): eslint fixes

--- web/js/merchant/reducers/support_detail.js
+++ web/js/merchant/reducers/support_detail.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-cycle
 import { merchantFetch } from 'merchant/utils/ajax';
 import { set, merge } from 'common/utils/immutable';
 
@@ -5,7 +6,7 @@ import { set, merge } from 'common/utils/immutable';
 const FETCH_MERCHANT_SUPPORT_DETAIL = 'FETCH_MERCHANT_SUPPORT_DETAIL';
 const ADD_MERCHANT_SUPPORT_DETAIL = 'CREATE_MERCHANT_SUPPORT_DETAIL';
 
-let initialState = {
+const initialState = {
   merchantSupportDetail: {
     loading: true,
     data: {},
@@ -27,19 +28,24 @@ export const createSupportDetail = (data) => {
     payload: merchantFetch({
       url: 'proxy/merchants/supportdetails',
       method: 'put',
-      data: data,
+      data,
     }),
   };
 };
 
 //reducers
-export default function (state = initialState, action) {
+export default (state = initialState, action) => {
   switch (action.type) {
     case `${FETCH_MERCHANT_SUPPORT_DETAIL}::SUCCESS`:
     case `${ADD_MERCHANT_SUPPORT_DETAIL}::SUCCESS`:
       return merge(state, {
         merchantSupportDetail: {
-          data: action.payload.data,
+          /* 
+            Added this alternative support to update the state with initial state value or whatever is previously there 
+            cause if there is not support detail entry in the DB for merchant instead of throwing error from now BE will
+            send NULL in data object or the response so to accommodate the rest of the code making data a {} (blank object)
+          */
+          data: action.payload.data || state.merchantSupportDetail.data,
           loading: false,
           error: null,
         },
@@ -56,4 +62,4 @@ export default function (state = initialState, action) {
     default:
       return state;
   }
-}
+};
