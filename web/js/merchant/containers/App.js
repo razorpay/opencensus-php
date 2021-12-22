9cf15169882a356fbe3f7dc22f3f832d45809b4d
ajaybrahmarzp <83278548+ajaybrahmarzp@users.noreply.github.com>
2021-12-22 17:49:19 +0530

    fix sentry errors (#9385)

--- web/js/merchant/containers/App.js
+++ web/js/merchant/containers/App.js
@@ -305,19 +305,19 @@ class App extends Component {
       });
 
     if (
-      !user.activation_form_milestone &&
-      !user.activated &&
-      this.props.user.isActivationFormFullView
+      !user?.activation_form_milestone &&
+      !user?.activated &&
+      this.props.user?.isActivationFormFullView
     ) {
       if (isMobileDevice()) {
         this.props.history.push('/onboarding/steps');
       } else {
         const firstStepToken = 'onboarding_first_step';
-        removeItem(`${firstStepToken}--${user.current}`);
+        removeItem(`${firstStepToken}--${user?.current}`);
         this.props.history.push('/kyc');
       }
     } else if (
-      this.props.user.isActivationFormFullView &&
+      this.props.user?.isActivationFormFullView &&
       this.props.location.pathname === '/activation' &&
       !isMobileDevice()
     ) {
