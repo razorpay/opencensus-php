0497fabdf1bc42812d8cf18b195934856cbf291a
sindhu777 <sindhu.kesaboina@razorpay.com>
2021-12-24 09:24:06 +0530

    add rupay in valid vault values (#27319)
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Models/Card/Validator.php
+++ app/Models/Card/Validator.php
@@ -14,7 +14,7 @@ class Validator extends Base\Validator
         Entity::EXPIRY_YEAR        => 'required|integer|digits:4|non_past_year',
         Entity::CVV                => 'sometimes|numeric|digits_between:3,4|nullable',
         Entity::NAME               => 'sometimes|regex:(^[a-zA-Z.\- 0-9\']+$)|max:100',
-        Entity::VAULT              => 'sometimes|string|in:tokenex,rzpvault,rzpencryption,mastercard,visa',
+        Entity::VAULT              => 'sometimes|string|in:tokenex,rzpvault,rzpencryption,mastercard,visa,rupay',
         Entity::INTERNATIONAL      => 'sometimes',
         Entity::IS_CVV_OPTIONAL    => 'sometimes|boolean',
         Entity::IS_TOKENIZED_CARD  => 'sometimes|boolean',
