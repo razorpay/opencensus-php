59a8ea393b403798c9730a98c319370f0dc5a733
Jatin Rathee <81687260+jatinrathee@users.noreply.github.com>
2022-01-11 13:13:26 +0530

    Ondemand Settlements enable now fix (#9482)

--- web/js/merchant/views/Settlements/InstantSettlements/InstantSettlements/SettlementMessage/index.js
+++ web/js/merchant/views/Settlements/InstantSettlements/InstantSettlements/SettlementMessage/index.js
@@ -14,7 +14,7 @@ const SettlementMessageContainer = ({ user, holidayList, openModal, balance }) =
   const isBankingDay = !isWeekend && !upcomingBankHoliday;
 
   function getPayoutMessageData() {
-    if (!user.isAutomaticSettlementEnabled && !user.isAutomaticSettlementRestricted) {
+    if (!user.isAutomaticSettlementEnabled && !user.isOndemandSettlementsRestricted) {
       return messageFactory.getEnableScheduledSettlements();
     } else if (isBankingDay && balance < 100) {
       return messageFactory.getSettlementWillBeSkipped();
