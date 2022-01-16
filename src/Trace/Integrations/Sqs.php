43a21c5ed7a32caad82108ee72441676acb1da07
sr <shivam.rai@razorpay.com>
2022-01-17 00:09:58 +0530

    WIP: sqs

--- src/Trace/Integrations/Sqs.php
+++ src/Trace/Integrations/Sqs.php
@@ -11,23 +11,30 @@ class Sqs implements IntegrationInterface
             trigger_error('opencensus extension required to load Sqs integrations.', E_USER_WARNING);
         }
 
-        opencensus_trace_method('Aws\Sqs\SqsClient', 'execute', function ($params) {
-            return [
-                'attributes' => ['queueURL' => $params[0]['QueueUrl'],
-                    'delaySeconds' => $params[0]['DelaySeconds']],
-                'kind' => 'client',
-                'name' => 'Sqs sendMessage',
-            ];
-        });
+        opencensus_trace_method('Aws\Sqs\SqsClient', 'execute', [static::class, 'handleExecuteCommand']);
 
-        opencensus_trace_method('Aws\Sqs\SqsClient', 'executeAsync', function ($params) {
-            return [
-                'attributes' => ['queueURL' => $params[0]['QueueUrl'],
-                    'delaySeconds' => $params[0]['DelaySeconds']],
-                'kind' => 'client',
-                'name' => 'Sqs sendMessage',
-            ];
-        });
+    }
+    static function handleExecuteCommand($sqs, $command)
+    {
 
+        self::debug_to_console($sqs);
+        self::debug_to_console($command);
+        var_dump($sqs, $command);
+//        return [
+//            'attributes' => ['queueURL' => $params[0]['QueueUrl'],
+//                'delaySeconds' => $params[0]['DelaySeconds']],
+//            'kind' => 'client',
+//            'name' => 'Sqs sendMessage',
+//        ];
+
+
+    }
+
+    static function debug_to_console($data) {
+        $output = $data;
+        if (is_array($output))
+            $output = implode(',', $output);
+
+        echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
     }
 }
