9225c25a238a93411021460a98519d26316e2187
snehagupta01 <43209043+snehagupta01@users.noreply.github.com>
2022-01-18 15:05:35 +0530

    logs (#27819)

--- app/Jobs/UpdateMerchantContext.php
+++ app/Jobs/UpdateMerchantContext.php
@@ -109,6 +109,7 @@ class UpdateMerchantContext extends Job
 
         $this->trace->info(TraceCode::UPDATE_MERCHANT_CONTEXT_JOB,[
             "merchant_id"   => $merchant->getId(),
+            "business_type" => $merchantDetail->getBusinessType(),
             "bvs_validation_id" => $this->validationId,
             "CAN_UPDATE_MERCHANT_CONTEXT"=>$canUpdateMerchantContext,
             "POA_VERIFICATION_STATUS"=>$merchantDetail->getAttribute(Entity::POA_VERIFICATION_STATUS),
