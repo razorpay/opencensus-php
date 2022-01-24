15641b56dc484062a0d4800dab7060d53ad2257a
Nanda Kishor Jeripothula <jeripothula.kishor@razorpay.com>
2022-01-24 23:46:42 +0530

    rollback: rollback the change of url in captch-checkout test page (#27939)

--- public/test/layout.php
+++ public/test/layout.php
@@ -187,7 +187,7 @@ $('#keys').onclick = t.onkeydown = e => {
     var source = 'checkout.razorpay.com';
 
     if (getSearchParams().branch) {
-      source = `https://betacdn.np.razorpay.in/checkout/builds/branch-builds/${getSearchParams().branch}`
+      source = `betacdn.np.razorpay.in/checkout/builds/branch-builds/${getSearchParams().branch}`
     } else if (isStage) {
       source = 'checkout.stage.razorpay.in';
     } else if (isFunc) {
