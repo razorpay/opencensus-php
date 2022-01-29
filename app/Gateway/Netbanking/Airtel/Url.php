8b59231152267f73cdf08c90dd60ff6687ce7d65
shahbaz-razorpay <70690063+shahbaz-razorpay@users.noreply.github.com>
2022-01-29 15:50:42 +0530

    changed urls (#28023)

--- app/Gateway/Netbanking/Airtel/Url.php
+++ app/Gateway/Netbanking/Airtel/Url.php
@@ -4,8 +4,8 @@ namespace RZP\Gateway\Netbanking\Airtel;
 
 class Url
 {
-    const LIVE_DOMAIN   = 'https://ecom.airtelbank.com/ecom/v2';
-    const TEST_DOMAIN   = 'https://sit.airtelmoney.in/ecom/v2';
+    const LIVE_DOMAIN   = 'https://ecom.airtelbank.com/payment/ecom/v2';
+    const TEST_DOMAIN   = 'https://apbuat.airtelbank.com:5050/ecom/v2';
 
     const AUTHORIZE     = '/initiatePayment';
     const VERIFY        = '/inquiry';
