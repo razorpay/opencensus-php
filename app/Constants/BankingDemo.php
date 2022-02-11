6b2b6912598bed9274d662618320bbc970a96b16
zahlekhan <zahle.khan@razorpay.com>
2022-02-11 20:04:13 +0530

    reduce duration to 1 day (#28336)

--- app/Constants/BankingDemo.php
+++ app/Constants/BankingDemo.php
@@ -5,7 +5,7 @@ namespace RZP\Constants;
 class BankingDemo
 {
     // All data in X Demo account will be hidden/deleted after this duration
-    const MAX_TIME_DURATION = 1296000; // 15 days
+    const MAX_TIME_DURATION = 86400; // 1 day(s)
 
     const BANK_ACCOUNT      = '2323230091169046';
 }
