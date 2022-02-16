999021c869078fb0224d0347641f0d60518d3c0c
sanket <sanket.rathi@razorpay.com>
2022-02-16 16:12:42 +0530

    updated timeout (#28402)

--- app/Http/Controllers/InstrumentRequestController.php
+++ app/Http/Controllers/InstrumentRequestController.php
@@ -272,7 +272,7 @@ class InstrumentRequestController extends BaseController
             [],
             \Requests::GET,
             'v2/merchant_instrument_status?merchant_id=' . $merchant->getId(),
-            ['timeout' => 1.5],
+            ['timeout' => 2.5],
             $this->getMerchantHeadersForInstrumentRequest());
 
         return ApiResponse::json($response);
