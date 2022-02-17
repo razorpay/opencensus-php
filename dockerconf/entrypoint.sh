a59959a740b07fc0090b7147303c31ca459a5b7f
Vinita Nirmal <73991784+vinita-razor@users.noreply.github.com>
2022-02-17 13:51:10 +0530

    increased the input var count (#9759)

--- dockerconf/entrypoint.sh
+++ dockerconf/entrypoint.sh
@@ -33,7 +33,7 @@ fi
 echo "$(date) Copy dashboard vhost"
 cp dockerconf/nginx.conf /etc/nginx/conf.d/default.conf
 
-echo "setting max_input_vars to 2000"
+echo "setting max_input_vars to 5000"
 sed -ie "s/; max_input_vars =.*/max_input_vars = 2000/g" /etc/php7/php.ini
 
 export PATH=$PATH:/app/
