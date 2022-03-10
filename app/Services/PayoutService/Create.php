018972c986688fd360abacc2b1bc82c7715002aa
akshatshukla-175 <97943220+akshatshukla-175@users.noreply.github.com>
2022-03-10 12:26:10 +0530

    updating mode as optional parameter for payouts service (#28739)
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Services/PayoutService/Create.php
+++ app/Services/PayoutService/Create.php
@@ -64,7 +64,7 @@ class Create extends Base
             Payout\Entity::PURPOSE               => $input[Payout\Entity::PURPOSE],
             Payout\Entity::AMOUNT                => (int) $input[Payout\Entity::AMOUNT],
             Payout\Entity::CURRENCY              => $input[Payout\Entity::CURRENCY],
-            Payout\Entity::MODE                  => $input[Payout\Entity::MODE],
+            Payout\Entity::MODE                  => $input[Payout\Entity::MODE] ?? null,
             Payout\Entity::QUEUE_IF_LOW_BALANCE  => (boolean) ($input[Payout\Entity::QUEUE_IF_LOW_BALANCE] ?? false),
             Payout\Entity::ACCOUNT_NUMBER        => (string) $input[Payout\Entity::ACCOUNT_NUMBER],
             Payout\Entity::REFERENCE_ID          => $input[Payout\Entity::REFERENCE_ID] ?? null,
