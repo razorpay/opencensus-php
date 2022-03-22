751401af3e182f62d5baf903195c57c4d2105182
Arya Shrivastava <88476223+ashrivas8@users.noreply.github.com>
2022-03-22 20:52:08 +0530

    chore: reorder tabs (#9959)

--- web/js/merchant/views/MagicCheckout/MagicCheckoutRoutes.js
+++ web/js/merchant/views/MagicCheckout/MagicCheckoutRoutes.js
@@ -3,12 +3,22 @@ import BulkAddressUpload from 'merchant/views/MagicCheckout/BulkAddressUpload';
 import OrderStatusTab from 'merchant/views/MagicCheckout/OrderStatusTab';
 import MagicSettings from 'merchant/views/MagicCheckout/MagicSettings';
 
+/**
+ * Order of tabs:
+ * 1. Settings
+ * 2. Shipping Services
+ * 3. Address Upload
+ * 4. Delivery Status upload
+ *
+ * Note: when adding a new tab, confirm the ordering with product first.
+ */
+
 const routes = [
   {
-    tabName: 'Address',
-    path: '/magic/address',
-    condition: (_user) => _user.isBulkAddressUploadEnabled,
-    Component: BulkAddressUpload,
+    tabName: 'Magic Settings',
+    path: '/magic/settings',
+    condition: (_user) => _user.isMagicSettingsEnabled,
+    Component: MagicSettings,
   },
   {
     tabName: 'Shipping Services',
@@ -17,10 +27,10 @@ const routes = [
     Component: ShippingAccount,
   },
   {
-    tabName: 'Magic Settings',
-    path: '/magic/settings',
-    condition: (_user) => _user.isMagicSettingsEnabled,
-    Component: MagicSettings,
+    tabName: 'Address',
+    path: '/magic/address',
+    condition: (_user) => _user.isBulkAddressUploadEnabled,
+    Component: BulkAddressUpload,
   },
   {
     tabName: 'Upload Delivery Status',
