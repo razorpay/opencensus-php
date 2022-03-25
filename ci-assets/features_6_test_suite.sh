960532c1d8f0a8b78e66b45e89c4d476cfe21d75
shubhdeep128 <97942927+shubhdeep128@users.noreply.github.com>
2022-03-25 09:01:46 +0530

    Added Ut Metrics (#28943)
    * Added Ut Metrics
    
    * Modified Tsst Scripts to Push UT Metrics to DB
    
    * Fixed Bash Error
    
    * Added Log Output
    
    * Changed Failure Calculation
    
    * Added Test Result Caching
    
    * Fixed Install Error
    
    * Added JQ Parsing Logic
    
    * Added UT Summary Push
    
    * Fixed Permission Error
    
    * Added Escape sequence to queries
    
    * Added Shebang Line
    
    * Fixed Text Formatting in Insert Query
    
    * Fixed JSON Formatting in insert query
    
    * Testing JSON Formatting
    
    * Testing JSON Formatting Again
    
    * Fixed JSON Escaping
    
    * Testing Backslash
    
    * Reformatted Code
    
    * Changed Double Quote Escape
    
    * Testing
    
    * Testing again
    
    * Fixed Query Formatting
    
    * Testing Workflow Step for Metrics Push
    
    * Fixed Permission error
    
    * Refactored Metrics Push
    
    * Test empty arrays
    
    * Fixed Test Time Retrieval
    
    * Testing Metrics Path Change
    
    * Fixed Metrics Path
    
    * Changed Test Case Name to match PHP Cache names
    
    * Testing Conversion to Single Step
    
    * Converted UT Metrics Push to Single Step
    
    * Cleaned Up Tests
    
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- ci-assets/features_6_test_suite.sh
+++ ci-assets/features_6_test_suite.sh
@@ -1,1 +1,10 @@
-php vendor/phpunit/phpunit/phpunit -d memory_limit=2048M --testsuite "Feature-6 Test Suite" --printer="Codedungeon\PHPUnitPrettyResultPrinter\Printer"
+#!/bin/bash
+log_file_name="test-output.log"
+export START_TIME=$(date +%s)
+echo "start-time: ${START_TIME}"
+php vendor/phpunit/phpunit/phpunit -d memory_limit=2048M --testsuite "Feature-6 Test Suite" --printer="Codedungeon\PHPUnitPrettyResultPrinter\Printer" >> $log_file_name
+export END_TIME=$(date +%s)
+echo "end-time: ${END_TIME}"
+cat $log_file_name
+echo "${START_TIME},${END_TIME}" >> utMetrics.csv
+
