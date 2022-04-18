67013d4aefe5097176bf7c4294f275c3ca63c6fb
shubhdeep128 <97942927+shubhdeep128@users.noreply.github.com>
2022-04-18 19:30:04 +0530

    Added UT Metrics Push (#29293)
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- ci-assets/gateway_2_test_suite.sh
+++ ci-assets/gateway_2_test_suite.sh
@@ -1,1 +1,11 @@
-php vendor/phpunit/phpunit/phpunit -d memory_limit=2048M --testsuite "Gateway-2 Test Suite" --printer="Codedungeon\PHPUnitPrettyResultPrinter\Printer"
+#!/bin/bash
+TEST_SUITE_STATUS=0
+log_file_name="test-output.log"
+export START_TIME=$(date +%s)
+echo "start-time: ${START_TIME}"
+php vendor/phpunit/phpunit/phpunit -d memory_limit=2048M --testsuite "Gateway-2 Test Suite" --printer="Codedungeon\PHPUnitPrettyResultPrinter\Printer" >> $log_file_name
+export TEST_SUITE_STATUS=$((TEST_SUITE_STATUS + $?))
+export END_TIME=$(date +%s)
+echo "end-time: ${END_TIME}"
+cat $log_file_name
+echo "${START_TIME},${END_TIME},${TEST_SUITE_STATUS}" >> utMetrics.csv
