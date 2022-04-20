485cdba6b36134739738b86dfcf86316bdfeef76
Enigma <52568191+k-shrestha@users.noreply.github.com>
2022-04-21 00:39:34 +0530

    Hotfix Batch s3 bucket migration region correction (#29639)
    * changes for bucket fix
    
    * type fix
    
    Co-authored-by: kalpit.jain <kalpit.jain@razorpay.com>

--- config/filestore.php
+++ config/filestore.php
@@ -86,7 +86,7 @@ return [
         ],
         'non_migrated_batch_bucket_config' => [
             'name'   => env('AWS_S3_NON_MIGRATED_BATCH_BUCKET'),
-            'region' => env('AWS_BUCKET_REGION', 'ap-south-1'),
+            'region' => env('AWS_S3_NON_MIGRATED_BATCH_REGION', 'ap-south-1'),
         ],
     ],
 
