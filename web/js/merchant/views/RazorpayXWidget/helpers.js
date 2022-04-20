a5072b5e2ab969b8b4f6c6dc2acee8a5136b480f
PranjalMaithani <75153839+PranjalMaithani@users.noreply.github.com>
2022-04-20 12:30:48 +0530

    X widget kyc property fix (#10082)
    * testing-iframe-x
    
    * Revert "testing-iframe-x"
    
    This reverts commit f393d4cdc7ca5264232140f73c476d657699e3cb.
    
    * fix: vA KYC analytics property

--- web/js/merchant/views/RazorpayXWidget/helpers.js
+++ web/js/merchant/views/RazorpayXWidget/helpers.js
@@ -1,13 +1,13 @@
 export const getKycAnalyticsProperties = () => {
   const {
     user: { signup_via_email },
-    merchant: { activated: vaKycActivated },
+    activation_status: vaActivationStatus,
     ca_activation_status,
     activated: pgKycActivated,
   } = window.rzp_user;
 
   return {
-    VA_KYC_Activated: !!vaKycActivated,
+    VA_KYC_Activated: vaActivationStatus === 'activated',
     CA_Activated: ca_activation_status === 'activated',
     PG_KYC_Activated: !!pgKycActivated,
     signupMethod: signup_via_email ? 'email' : 'mobile',
