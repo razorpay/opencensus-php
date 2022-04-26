e3ffca5c75df92d48da27e2c3fb245aa0a26674e
parteek-3108 <71115539+parteek-3108@users.noreply.github.com>
2022-04-26 21:28:38 +0530

    Scbl Emi Changes (#27206)
    * format changed
    
    * fields added
    
    * final changes done'
    
    * added the changes in row ordering
    
    * mask part removed from card number and conditionals field removed
    
    * Comments acknowledged
    
    * Comment added
    
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Models/Gateway/File/Processor/Emi/Scbl.php
+++ app/Models/Gateway/File/Processor/Emi/Scbl.php
@@ -2,7 +2,9 @@
 
 namespace RZP\Models\Gateway\File\Processor\Emi;
 
+use App;
 use RZP\Models\Bank\IFSC;
+use RZP\Services;
 use RZP\Models\FileStore;
 
 class Scbl extends Base
@@ -16,6 +18,8 @@ class Scbl extends Base
     {
         $formattedData = [];
 
+        $rrn = $this->getRrnNumber($data['items']);
+
         foreach ($data['items'] as $emiPayment)
         {
             $emiTenure = $emiPayment->emiPlan['duration'];
@@ -27,14 +31,20 @@ class Scbl extends Base
             $emiRate = $emiPayment->emiPlan['rate'];
 
             $formattedData[] = [
-                'CARD_NUMBER'                  => $this->getCardNumber($emiPayment->card),
-                'MID'                          => $emiPayment->getId(),
-                'MERCHANT_NAME'                => $merchant->getDbaName(),
-                'TRXN_AMOUNT'                  => $this->getFormattedAmount($emiPayment->getAmount()),
-                'TRXN_DATE'                    => $this->getFormattedDate($emiPayment->getCaptureTimestamp()),
-                'SETTLEMENT_DATE'              => $this->getFormattedDate($txn->getSettledAt()),
-                'AUTH_CODE'                    => $this->getAuthCode($emiPayment),
-                'TENOR'                        => $emiTenure,
+                'Card Number'                  => $emiPayment->card->getLast4(),
+                'Transaction Amount'           => $this->getFormattedAmount($emiPayment->getAmount()),
+                'Transaction Date'             => $this->getFormattedDate($emiPayment->getCaptureTimestamp()),
+                'Auth Code'                    => $this->getAuthCode($emiPayment),
+                'Merchant Name'                => $merchant->getDbaName(),
+                'RRN'                          => $rrn[$emiPayment->getId()]['rrn'] ?? '',
+                'ARN'                          => '',
+                'Settlement Date'              => $this->getFormattedDate($txn->getSettledAt()),
+                'MID'                          => '',
+                'TID'                          => '',
+                'Card Hash'                    => '',
+                'EMI ID'                       => $emiPayment->getId(),
+                'Tenure'                       => $emiTenure,
+                // Below fields are optional
                 'REDUCING_INTEREST_RATE_P_A'   => $this->getFormattedEmiRate($emiRate),
                 'PROCESSING_FEE'               => '',
                 'FORCLOSURE_FEE'               => '',
@@ -49,11 +59,34 @@ class Scbl extends Base
 
     protected function getFormattedAmount($amount)
     {
-        return number_format($amount / 100, 2);
+        return number_format($amount / 100, 2,'.', '');
     }
 
     protected function getFormattedEmiRate($emiRate)
     {
         return rtrim(number_format($emiRate / 10000, 4), '0');
+    }
+
+    protected function getRrnNumber($data)
+    {
+        $CPS_PARAMS = [
+            Services\CardPaymentService::RRN
+        ];
+
+        $paymentIds = array();
+
+        foreach ($data as $payment)
+        {
+            array_push($paymentIds, $payment->id);
+        }
+
+        $request = [
+            'fields'        => $CPS_PARAMS,
+            'payment_ids'   => $paymentIds,
+        ];
+
+        $response = App::getFacadeRoot()['card.payments']->fetchAuthorizationData($request);
+
+        return $response;
     }
 }
