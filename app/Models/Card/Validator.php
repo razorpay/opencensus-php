a02ad1d3116a7ed5694d20696fc9b2b0ad8d3dc9
ankit <30381705+ankitpal11@users.noreply.github.com>
2022-05-01 18:30:13 +0530

    last_4 fix (#29828)

--- app/Models/Card/Validator.php
+++ app/Models/Card/Validator.php
@@ -23,7 +23,7 @@ class Validator extends Base\Validator
         Entity::TOKEN_PROVIDER     => 'sometimes|string',
         Entity::TOKEN_EXPIRY_MONTH => 'sometimes|integer|digits_between:1,2|max:12|min:0',
         Entity::TOKEN_EXPIRY_YEAR  => 'sometimes|integer|digits:4|non_past_year',
-        Entity::LAST4              => 'sometimes|integer|digits:4'
+        Entity::LAST4              => 'sometimes|numeric|digits:4'
     ];
 
     protected static $createCpsRequestRules = [
