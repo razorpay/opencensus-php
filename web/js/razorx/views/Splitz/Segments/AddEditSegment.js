e2438997c9ad4abe07f3d6aa3d2cd435eeb0a8a2
Varsha Singh <varsha.singh@razorpay.com>
2022-05-02 15:15:55 +0530

    feat: add name checking validation to 'Create Segment' form (#10043)
    * feat: add name checking validation to 'Create Segment' form
    
    * fix: change state name to `matchingSegmentName`
    
    * refactor: convert class component into a functional component
    
    * refactor: make changes according to PR review
    
    * refactor: use useRef for variable
    
    * refactor: replace useState with useReducer
    
    * refactor: make changes according to PR review
    
    * refactor: make changes according to PR review
    
    * refactor: make changes according to PR review
    
    * refactor: remove unused import
    
    * refactor(r): remove unnecessary variable
    
    * refactor(remove unused variable): remove unused variable

--- web/js/razorx/views/Splitz/Segments/AddEditSegment.js
+++ web/js/razorx/views/Splitz/Segments/AddEditSegment.js
@@ -1,75 +1,46 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
 import { ModalContent } from 'common/new-ui/Modal';
 import adminFetch from 'razorx/helpers/admin-fetch';
 import { closeModal, notifySuccess, notifyError } from 'razorx/components/Modal';
 import Form from 'razorx/components/ui/Form';
 import Field, { TextAreaField, FileField, SelectField } from 'razorx/components/ui/Field';
 import { splitzFetch } from 'razorx/helpers/fetch';
+import { CRON_EXPRESSION_GENERATOR, DYNAMIC_SEGMENTS_DOC } from './constants';
 
 const CSV = 'CSV';
 const SQL = 'SQL';
+const header = 'Create Segment';
 
-@withRouter
-export default class AddEditSegment extends React.Component {
-  state = {
-    isSaving: false,
-    fileId: null,
-    segmentType: CSV,
-  };
+const typeDropdownProperties = [CSV, SQL].map((item) => (
+  <option key={item} value={item}>
+    {item}
+  </option>
+));
 
-  onSubmit = (form) => {
-    const isInvalid = this.isInvalid(form);
+function reducer(state, action) {
+  const { type, payload } = action;
+  return { ...state, [type]: payload };
+}
 
-    if (!!isInvalid) {
-      notifyError(isInvalid);
-      return;
-    }
+const initialState = {
+  isSaving: false,
+  fileId: null,
+  segmentType: CSV,
+  matchingSegmentName: null,
+};
 
-    const type = this.state.segmentType;
+export default function AddEditSegment(props) {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-    const payload = {
-      segment: {
-        name: form.name,
-        description: form.description,
-        falsePositivityRate: '0.000001',
-      },
-    };
+  const { isSaving, fileId, segmentType, matchingSegmentName } = state;
 
-    if (type === SQL) {
-      payload.segment.cron_expression = form.cronExpression;
-      payload.segment.sql_query = form.sqlQuery;
-    } else {
-      payload.segment.inputFileID = this.state.fileId;
-    }
-    payload.segment.source_type = this.state.segmentType;
-
-    const segmentUrl = 'segment.v1.SegmentAPI/Create';
-    const successMsg = `Segment is successfully created`;
-
-    this.setState({ isSaving: true });
-
-    splitzFetch({ url: segmentUrl, data: payload })
-      .then((response) => {
-        this.setState({ isSaving: false });
-        notifySuccess(successMsg);
-        closeModal();
-        this.props.collection.fetch();
-        this.props.history.push(`/splitz/segments/${response.items.id}`);
-      })
-      .catch((err) => {
-        this.setState({ isSaving: false });
-        notifyError(err);
-      });
-  };
-
-  isInvalid(form) {
-    if (this.state.segmentType === CSV && !this.state.fileId) {
+  function validateForm(form) {
+    if (segmentType === CSV && !fileId) {
       return 'Input file should be uploaded';
     }
 
-    if (this.state.segmentType === SQL) {
+    if (segmentType === SQL) {
       if (!form.cronExpression || !form.sqlQuery) {
         return 'Cron Expression and SQL Query must not be empty';
       }
@@ -84,10 +55,57 @@ export default class AddEditSegment extends React.Component {
         return "SQL Query must be in the format, 'SELECT column_name FROM ...'";
       }
     }
+
+    if (matchingSegmentName !== null) {
+      return 'A segment with this name already exists.';
+    }
+
     return false;
   }
 
-  handleFileChange = (event) => {
+  const onSubmit = (form) => {
+    const invalid = validateForm(form);
+
+    if (!!invalid) {
+      notifyError(invalid);
+      return;
+    }
+
+    const payload = {
+      segment: {
+        name: form.name,
+        description: form.description,
+        falsePositivityRate: '0.000001',
+      },
+    };
+
+    if (segmentType === SQL) {
+      payload.segment.cron_expression = form.cronExpression;
+      payload.segment.sql_query = form.sqlQuery;
+    } else {
+      payload.segment.inputFileID = fileId;
+    }
+    payload.segment.source_type = segmentType;
+
+    const segmentUrl = 'segment.v1.SegmentAPI/Create';
+    const successMsg = `Segment is successfully created`;
+
+    dispatch({ type: 'isSaving', payload: true });
+
+    splitzFetch({ url: segmentUrl, data: payload })
+      .then(() => {
+        dispatch({ type: 'isSaving', payload: false });
+        notifySuccess(successMsg);
+        closeModal();
+        props.collection.fetch();
+      })
+      .catch((err) => {
+        dispatch({ type: 'isSaving', payload: false });
+        notifyError(err);
+      });
+  };
+
+  const handleFileChange = (event) => {
     event.preventDefault();
     const file = event.currentTarget.files[0];
 
@@ -100,7 +118,7 @@ export default class AddEditSegment extends React.Component {
     bodyFormData.append('body', `name=${file.name}&type=splitz_segment&entity=`);
     bodyFormData.append('file', file);
 
-    this.setState({ isSaving: true });
+    dispatch({ type: 'isSaving', payload: true });
 
     adminFetch({
       url: '/makeapicall/admin-ufh/file/upload',
@@ -109,122 +127,123 @@ export default class AddEditSegment extends React.Component {
       headers: { 'Content-Type': 'multipart/form-data' },
     })
       .then((res) => {
-        this.setState({ fileId: res.file_id, isSaving: false });
+        dispatch({ type: 'fileId', payload: res.file_id });
       })
       .catch((err) => {
         console.error('File Upload Error: ', err);
-        this.setState({ fileId: null, isSaving: false });
+        dispatch({ type: 'fileId', payload: null });
+      })
+      .finally(() => {
+        dispatch({ type: 'isSaving', payload: false });
       });
   };
 
-  render() {
-    const { isSaving } = this.state;
-    const header = 'Create Segment';
-    const isLoading = isSaving;
-    const typeDropdownProperties = [
-      {
-        label: CSV,
-        value: CSV,
+  const checkForExistingSegmentName = (e) => {
+    splitzFetch({
+      url: 'segment.v1.SegmentAPI/GetByName',
+      data: {
+        segmentName: e.target.value,
       },
-      {
-        label: SQL,
-        value: SQL,
-      },
-    ].map((op, i) => (
-      <option key={i} value={op.value}>
-        {op.label}
-      </option>
-    ));
+    })
+      .then((res) => {
+        dispatch({ type: 'matchingSegmentName', payload: res?.items?.name });
+      })
+      .catch(() => {
+        dispatch({ type: 'matchingSegmentName', payload: null });
+      });
+  };
 
-    return (
-      <ModalContent class="modal-features modal-json-edit" header={header}>
-        <Form
-          onSubmit={this.onSubmit}
-          class="full-span full-elements"
-          style={{ opacity: isLoading ? 0.5 : 1 }}
-        >
-          <React.Fragment>
-            {isLoading && <div className="spinner center" />}
-            <Field
-              type="text"
-              label="Name"
-              name="name"
-              placeholder="Segment Name"
-              defaultValue=""
-              required
+  const changeSegmentType = (e) => {
+    const type = e.target.value;
+    dispatch({ type: 'segmentType', payload: type });
+  };
+
+  const formOpacity = `form-submit-opacity-${isSaving ? 'low' : 'high'}`;
+
+  return (
+    <ModalContent className="modal-features modal-json-edit" header={header}>
+      <Form onSubmit={onSubmit} className={`full-span full-elements ${formOpacity}`}>
+        <React.Fragment>
+          {isSaving && <div className="spinner center" />}
+          <Field
+            type="text"
+            label="Name"
+            name="name"
+            placeholder="Segment Name"
+            defaultValue=""
+            required
+            onChange={checkForExistingSegmentName}
+          />
+          {matchingSegmentName && (
+            <span className="error-message">This segment name already exists.</span>
+          )}
+          <TextAreaField
+            label="Description"
+            name="description"
+            placeholder="Segment Description"
+            defaultValue=""
+            required
+          />
+          <SelectField
+            name="type"
+            label="Type"
+            defaultValue="CSV"
+            required
+            onChange={changeSegmentType}
+          >
+            {typeDropdownProperties}
+          </SelectField>
+          {segmentType === SQL && (
+            <div>
+              <TextAreaField label="SQL Query" name="sqlQuery" placeholder="SQL Query" required />
+              <Field type="text" label="Cron Expression" name="cronExpression" required />
+              <p>
+                Use{' '}
+                <a
+                  href={CRON_EXPRESSION_GENERATOR}
+                  target="_blank"
+                  className="form-links"
+                  rel="noreferrer noopener"
+                >
+                  this link
+                </a>{' '}
+                to generate cron expression.{' '}
+                <a
+                  href={DYNAMIC_SEGMENTS_DOC}
+                  target="_blank"
+                  className="form-links"
+                  rel="noreferrer noopener"
+                >
+                  Click here
+                </a>{' '}
+                to see recommendations and more.
+              </p>
+            </div>
+          )}
+          {segmentType === CSV && (
+            <FileField
+              label="Input File"
+              id="inputFile"
+              name="inputFile"
+              required={segmentType === CSV}
+              onChange={handleFileChange}
+              className="form-margin"
             />
-            <TextAreaField
-              label="Description"
-              name="description"
-              placeholder="Segment Description"
-              defaultValue=""
-              required
-            />
-            <SelectField
-              name="type"
-              label="Type"
-              defaultValue="CSV"
-              required
-              onChange={(evt) => {
-                const type = evt.target.value;
-                this.setState({ segmentType: type });
-              }}
-            >
-              {typeDropdownProperties}
-            </SelectField>
-            {this.state.segmentType === SQL && (
-              <div>
-                <TextAreaField label="SQL Query" name="sqlQuery" placeholder="SQL Query" required />
-                <Field type="text" label="Cron Expression" name="cronExpression" required />
-                <p>
-                  Use{' '}
-                  <a
-                    href="https://www.freeformatter.com/cron-expression-generator-quartz.html"
-                    target="_blank"
-                    style={{ color: 'blue' }}
-                    rel="noreferrer noopener"
-                  >
-                    this link
-                  </a>{' '}
-                  to generate cron expression.{' '}
-                  <a
-                    href="https://docs.google.com/document/d/176KNlWrlDofaOGegwAUC9bCYlZ6F4b9FvSRHi7BcDDs"
-                    target="_blank"
-                    style={{ color: 'blue' }}
-                    rel="noreferrer noopener"
-                  >
-                    Click here
-                  </a>{' '}
-                  to see recommendations and more.
-                </p>
-              </div>
-            )}
-            {this.state.segmentType === CSV && (
-              <FileField
-                label="Input File"
-                id="inputFile"
-                name="inputFile"
-                required={this.state.segmentType === CSV}
-                onChange={this.handleFileChange}
-              />
-            )}
-            <div style={{ marginTop: 24 }} />
-          </React.Fragment>
-          <div className="footer">
-            <button className="btn btn--primary">
-              Create Segment
-              <span className="spin-btn" />
-            </button>
-          </div>
-        </Form>
-      </ModalContent>
-    );
-  }
+          )}
+        </React.Fragment>
+        <div className="footer">
+          <button className="btn btn--primary">
+            Create Segment
+            <span className="spin-btn" />
+          </button>
+        </div>
+      </Form>
+    </ModalContent>
+  );
 }
 
 AddEditSegment.defaultProps = {};
 
 AddEditSegment.propTypes = {
   collection: PropTypes.object.isRequired,
-  history: PropTypes.object.isRequired,
 };
---
+++ web/js/razorx/views/Splitz/Segments/constants.js
@@ -0,0 +1,5 @@
+export const DYNAMIC_SEGMENTS_DOC =
+  'https://docs.google.com/document/d/176KNlWrlDofaOGegwAUC9bCYlZ6F4b9FvSRHi7BcDDs';
+
+export const CRON_EXPRESSION_GENERATOR =
+  'https://www.freeformatter.com/cron-expression-generator-quartz.html';
