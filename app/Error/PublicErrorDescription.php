8b6486be4a67059dd40fb0d2361bb7daedf07cb0
bytesid19 <41238462+bytesid19@users.noreply.github.com>
2022-05-09 21:49:57 +0530

    Changed error description for code-BAD_REQUEST_MERCHANT_EDIT_OPERATION_IN_PROGRESS (#29925)
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Error/PublicErrorDescription.php
+++ app/Error/PublicErrorDescription.php
@@ -1089,7 +1089,7 @@ class PublicErrorDescription
 
     const BAD_REQUEST_AMAZONPAY_PAYOUT_NOT_ALLOWED_ON_DIRECT_ACCOUNT            = 'Payouts to amazonpay wallet are not allowed on current accounts';
 
-    const BAD_REQUEST_MERCHANT_EDIT_OPERATION_IN_PROGRESS                       = 'The information that you have filled could not be processed. Please try again later';
+    const BAD_REQUEST_MERCHANT_EDIT_OPERATION_IN_PROGRESS                       = 'Your details could not be saved as another request is in progress. Please try again in sometime.';
 
     //Partner Activation error descriptions
     const BAD_REQUEST_PARTNER_ACTIVATION_OPERATION_IN_PROGRESS                      = 'Partner activation is in progress';
