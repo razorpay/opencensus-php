3f87b637feb34a1c919b5a7fa6fdeced6475b205
Suhas R. More <suhas.more@razorpay.com>
2022-06-23 10:52:46 +0530

    Fix: Removing card token status hard coding (#10428)

--- web/js/merchant/views/Subscriptions/Tokens/List.js
+++ web/js/merchant/views/Subscriptions/Tokens/List.js
@@ -106,5 +106,5 @@ export default class TokensListContainer extends ListContainer {
 }
 
 export function getTokenStatus(token) {
-  return token.method === 'card' ? 'confirmed' : token?.recurring_details?.status ?? '';
+  return token?.recurring_details?.status ?? '';
 }
