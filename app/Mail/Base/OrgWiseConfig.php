80fbd46679a26aa47fa70a7b4bada39b97afb3a5
Raghav Tayal <raghavtayal2@gmail.com>
2022-07-04 19:47:56 +0530

    added axis easy pay for disabled mail (#31118)
    * added axis easy pay for disabled mail
    
    * added other orgs
    
    * remove duplicate org test
    
    * addressed comments
    
    * addressed comments
    
    * modified test
    
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Mail/Base/OrgWiseConfig.php
+++ app/Mail/Base/OrgWiseConfig.php
@@ -11,13 +11,161 @@ use RZP\Models\Feature\Constants as Features;
 class OrgWiseConfig
 {
     const HDFC  = 'hdfc';
-
+    const AXIS_EASY_PAY = 'UTIB';
+    const AXIS_BANK_BQR_TEAM = 'axisbank';
+    const AXIS_BANK_LTD = 'axis';
+    const HDFC_COLLECT_NOW ='HDFC';
+    const HDFC_BANK_GIG =  'HDFC GIG';
+    const ICICI_BANK = 'icic';
+    const SOUTH_INDIAN_BANK = 'SIBL';
+    const HSBC = 'HSBC';
+    const KOTAK = 'KKBK';
+    const YES_BANK = 'YESB';
+    const JK_BANK_IPG = 'jkb';
+    const BANK_OF_BARODA = 'bob';
+    const TJSB_BANK = 'tjsb';
     /**
      * If a mailer does not have entry here the it will be sent.
      * If there is an entry then it will be sent if the merchant
      * does not have the blocking feature for that mailer if the
      * restriction applies to his org.
      */
+    const captureOrgList = [
+        self::HDFC,
+        self::AXIS_EASY_PAY,
+        self::AXIS_BANK_BQR_TEAM,
+        self::AXIS_BANK_LTD,
+        self::HDFC_COLLECT_NOW,
+        self::HDFC_BANK_GIG,
+        self::ICICI_BANK,
+        self::SOUTH_INDIAN_BANK,
+        self::HSBC,
+        self::KOTAK,
+        self::YES_BANK,
+        self::JK_BANK_IPG,
+        self::BANK_OF_BARODA,
+        self::TJSB_BANK
+    ];
+
+    const authorisedOrgList = [
+        self::HDFC,
+        self::AXIS_EASY_PAY,
+        self::AXIS_BANK_BQR_TEAM,
+        self::AXIS_BANK_LTD,
+        self::HDFC_COLLECT_NOW,
+        self::HDFC_BANK_GIG,
+        self::ICICI_BANK,
+        self::SOUTH_INDIAN_BANK,
+        self::HSBC,
+        self::KOTAK,
+        self::YES_BANK,
+        self::JK_BANK_IPG,
+        self::BANK_OF_BARODA,
+        self::TJSB_BANK];
+
+    const failedOrgList = [
+        self::HDFC,
+        self::AXIS_EASY_PAY,
+        self::AXIS_BANK_BQR_TEAM,
+        self::AXIS_BANK_LTD,
+        self::HDFC_COLLECT_NOW,
+        self::HDFC_BANK_GIG,
+        self::ICICI_BANK,
+        self::SOUTH_INDIAN_BANK,
+        self::HSBC,
+        self::KOTAK,
+        self::YES_BANK,
+        self::JK_BANK_IPG,
+        self::BANK_OF_BARODA,
+        self::TJSB_BANK
+    ];
+
+    const failedToAuthorisedOrgList = [
+        self::HDFC,
+        self::AXIS_EASY_PAY,
+        self::AXIS_BANK_BQR_TEAM,
+        self::AXIS_BANK_LTD,
+        self::HDFC_COLLECT_NOW,
+        self::HDFC_BANK_GIG,
+        self::ICICI_BANK,
+        self::SOUTH_INDIAN_BANK,
+        self::HSBC,
+        self::KOTAK,
+        self::YES_BANK,
+        self::JK_BANK_IPG,
+        self::BANK_OF_BARODA,
+        self::TJSB_BANK
+    ];
+
+    const refundOrgList = [
+        self::HDFC,
+        self::AXIS_EASY_PAY,
+        self::AXIS_BANK_BQR_TEAM,
+        self::AXIS_BANK_LTD,
+        self::HDFC_COLLECT_NOW,
+        self::HDFC_BANK_GIG,
+        self::ICICI_BANK,
+        self::SOUTH_INDIAN_BANK,
+        self::HSBC,
+        self::KOTAK,
+        self::YES_BANK,
+        self::JK_BANK_IPG,
+        self::BANK_OF_BARODA,
+        self::TJSB_BANK
+    ];
+
+    const authorisedReminderOrgList = [
+        self::HDFC,
+        self::AXIS_EASY_PAY,
+        self::AXIS_BANK_BQR_TEAM,
+        self::AXIS_BANK_LTD,
+        self::HDFC_COLLECT_NOW,
+        self::HDFC_BANK_GIG,
+        self::ICICI_BANK,
+        self::SOUTH_INDIAN_BANK,
+        self::HSBC,
+        self::KOTAK,
+        self::YES_BANK,
+        self::JK_BANK_IPG,
+        self::BANK_OF_BARODA,
+        self::TJSB_BANK
+    ];
+
+    const dailyReportOrgList = [
+        self::HDFC,
+        self::AXIS_EASY_PAY,
+        self::AXIS_BANK_BQR_TEAM,
+        self::AXIS_BANK_LTD,
+        self::HDFC_COLLECT_NOW,
+        self::HDFC_BANK_GIG,
+        self::ICICI_BANK,
+        self::SOUTH_INDIAN_BANK,
+        self::HSBC,
+        self::KOTAK,
+        self::YES_BANK,
+        self::JK_BANK_IPG,
+        self::BANK_OF_BARODA,
+        self::TJSB_BANK
+    ];
+
+    const customerFailedOrgList = [
+        self::HDFC,
+        self::AXIS_EASY_PAY,
+        self::AXIS_BANK_BQR_TEAM,
+        self::AXIS_BANK_LTD,
+        self::HDFC_COLLECT_NOW,
+        self::HDFC_BANK_GIG,
+        self::ICICI_BANK,
+        self::SOUTH_INDIAN_BANK,
+        self::HSBC,
+        self::KOTAK,
+        self::YES_BANK,
+        self::JK_BANK_IPG,
+        self::BANK_OF_BARODA,
+        self::TJSB_BANK
+    ];
+
+
     const CONFIG = [
         \Rzp\Mail\User\AccountVerification::class            => [
             Features::SELF_KYC_DISABLED => [self::HDFC]
@@ -41,28 +189,28 @@ class OrgWiseConfig
             Features::SELF_KYC_DISABLED => [self::HDFC]
         ],
         \RZP\Mail\Payment\Captured::class                    => [
-            Features::PAYMENT_MAILS_DISABLED => [self::HDFC]
+            Features::PAYMENT_MAILS_DISABLED => self::captureOrgList
         ],
         \RZP\Mail\Payment\Authorized::class                  => [
-            Features::PAYMENT_MAILS_DISABLED => [self::HDFC]
+            Features::PAYMENT_MAILS_DISABLED => self::authorisedOrgList
         ],
         \RZP\Mail\Payment\Failed::class                      => [
-            Features::PAYMENT_MAILS_DISABLED => [self::HDFC]
+            Features::PAYMENT_MAILS_DISABLED => self::failedOrgList
         ],
         \RZP\Mail\Payment\FailedToAuthorized::class          => [
-            Features::PAYMENT_MAILS_DISABLED => [self::HDFC]
+            Features::PAYMENT_MAILS_DISABLED => self::failedToAuthorisedOrgList
         ],
         \RZP\Mail\Payment\Refunded::class                    => [
-            Features::PAYMENT_MAILS_DISABLED => [self::HDFC]
+            Features::PAYMENT_MAILS_DISABLED => self::refundOrgList
         ],
         \RZP\Mail\Merchant\AuthorizedPaymentsReminder::class => [
-            Features::PAYMENT_MAILS_DISABLED => [self::HDFC]
+            Features::PAYMENT_MAILS_DISABLED => self::authorisedReminderOrgList
         ],
         \RZP\Mail\Merchant\DailyReport::class                => [
-            Features::PAYMENT_MAILS_DISABLED => [self::HDFC]
+            Features::PAYMENT_MAILS_DISABLED => self::dailyReportOrgList
         ],
         \RZP\Mail\Payment\CustomerFailed::class              => [
-            Features::PAYMENT_MAILS_DISABLED => [self::HDFC]
+            Features::PAYMENT_MAILS_DISABLED => self::customerFailedOrgList
         ]
     ];
 
--- tests/Unit/Models/Mailer/OrgWiseConfigTest.php
+++ tests/Unit/Models/Mailer/OrgWiseConfigTest.php
@@ -8,9 +8,23 @@ use RZP\Tests\Functional\TestCase;
 
 class OrgWiseConfigTest extends TestCase
 {
-    const HDFC_ORG_CODE = 'hdfc';
 
     const TEST_ORG_CODE = 'dummy';
+    const HDFC  = 'hdfc';
+    const AXIS_EASY_PAY = 'UTIB';
+    const AXIS_BANK_BQR_TEAM = 'axisbank';
+    const AXIS_BANK_LTD = 'axis';
+    const HDFC_COLLECT_NOW ='HDFC';
+    const HDFC_BANK_GIG =  'HDFC GIG';
+    const ICICI_BANK = 'icic';
+    const SOUTH_INDIAN_BANK = 'SIBL';
+    const HSBC = 'HSBC';
+    const KOTAK = 'KKBK';
+    const YES_BANK = 'YESB';
+    const JK_BANK_IPG = 'jkb';
+    const BANK_OF_BARODA = 'bob';
+    const TJSB_BANK = 'tjsb';
+
 
     /**
      * Org with code self::TEST_ORG_CODE has config present and has
@@ -19,26 +33,48 @@ class OrgWiseConfigTest extends TestCase
      * Org with code self::TEST_ORG_CODE_2 does not have an entry
      * in the config array at all.
      */
-    public function testEmailConfigDummyOrg()
+    public function emailEnabledForOrgTest($merchant, $customCode)
     {
-        $merchant = Merchant\Entity::find('10000000000000');
-
-        $this->fixtures->merchant->addFeatures('payment_mails_disabled');
-
         $sendPaymentCaptureMail = OrgWiseConfig::getEmailEnabledForOrg(
-            self::HDFC_ORG_CODE,
+            $customCode,
             \RZP\Mail\Payment\Captured::class,
             $merchant);
 
         $sendPaymentAuthorizeMail = OrgWiseConfig::getEmailEnabledForOrg(
-            self::HDFC_ORG_CODE,
+            $customCode,
             \RZP\Mail\Payment\Authorized::class,
             $merchant);
 
         $sendPaymentRefundMail = OrgWiseConfig::getEmailEnabledForOrg(
-            self::HDFC_ORG_CODE,
+            $customCode,
             \RZP\Mail\Payment\Authorized::class,
             $merchant);
+
+        $sendPaymentFailedMail = OrgWiseConfig::getEmailEnabledForOrg(
+            $customCode,
+            \RZP\Mail\Payment\Failed::class,
+            $merchant);
+
+        $sendPaymentFailedToAuthorisedMail = OrgWiseConfig::getEmailEnabledForOrg(
+            $customCode,
+            \RZP\Mail\Payment\FailedToAuthorized::class,
+            $merchant);
+
+        $sendPaymentFailedToAuthorisedPaymentReminder = OrgWiseConfig::getEmailEnabledForOrg(
+            $customCode,
+            \RZP\Mail\Merchant\AuthorizedPaymentsReminder::class,
+            $merchant);
+
+        $sendDailyReport = OrgWiseConfig::getEmailEnabledForOrg(
+            $customCode,
+            \RZP\Mail\Merchant\DailyReport::class,
+            $merchant);
+
+        $sendCustomerFailed = OrgWiseConfig::getEmailEnabledForOrg(
+            $customCode,
+            \RZP\Mail\Payment\CustomerFailed::class,
+            $merchant);
+
 
         $sendPaymentRefundMailNoConfigOrg = OrgWiseConfig::getEmailEnabledForOrg(
             self::TEST_ORG_CODE,
@@ -51,6 +87,29 @@ class OrgWiseConfigTest extends TestCase
 
         $this->assertFalse($sendPaymentRefundMail);
 
+        $this->assertFalse($sendPaymentFailedMail);
+
+        $this->assertFalse($sendPaymentFailedToAuthorisedMail);
+
+        $this->assertFalse($sendPaymentFailedToAuthorisedPaymentReminder);
+
+        $this->assertFalse($sendDailyReport);
+
+
         $this->assertTrue($sendPaymentRefundMailNoConfigOrg);
+    }
+
+    public function testEmailConfigDummyForOrgs()
+    {
+        $merchant = Merchant\Entity::find('10000000000000');
+
+        $this->fixtures->merchant->addFeatures('payment_mails_disabled');
+
+       $org_custom_code = [self::HDFC, self::AXIS_EASY_PAY, self::AXIS_BANK_BQR_TEAM, self::AXIS_BANK_LTD, self::HDFC_COLLECT_NOW, self::HDFC_BANK_GIG, self::ICICI_BANK, self::SOUTH_INDIAN_BANK, self::HSBC,self::KOTAK, self::YES_BANK, self::JK_BANK_IPG, self::BANK_OF_BARODA, self::TJSB_BANK];
+
+        foreach ($org_custom_code as $value)
+        {
+            $this->emailEnabledForOrgTest($merchant, $value);
+        }
     }
 }
