fdd05fd9bbcb8a6a1f0d4720d009f7aaee0e71f8
Amjad Ali <amjd@users.noreply.github.com>
2022-07-07 19:07:00 +0530

    Change BAS request timeout to 30 seconds (#31204)
    * Change BAS request timeout to 30 seconds
    
    * Change BAS request timeout to 30 seconds

--- config/applications.php
+++ config/applications.php
@@ -1218,7 +1218,7 @@ return [
     'banking_account_service' => [
         'url'                      => env('APP_BANKING_ACCOUNT_SERVICE_URL'),
         'secret'                   => env('APP_BANKING_ACCOUNT_SERVICE_SECRET'),
-        'timeout'                  => env('APP_BANKING_ACCOUNT_SERVICE_TIMEOUT_SECS', 15),
+        'timeout'                  => env('APP_BANKING_ACCOUNT_SERVICE_TIMEOUT_SECS', 30),
         'mock'                     => env('MOCK_BANKING_ACCOUNT_SERVICE', false),
         'rbl_leads_sf_time_filter' => env('RBL_LEADS_SF_TIME_FILTER', 1631903400),
     ],
