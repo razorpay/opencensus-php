e2422a810a1588ae77f1ef7435396df5951d49d7
Jagadeesh_Ravada <jagadeeshrsn@gmail.com>
2022-07-13 06:31:07 +0530

    Disabling tokenised flow changes in subsequent payments (#31287)
    * Disabling tokenised flow changes in subsequent payments
    
    * dummy commit
    
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Services/CardPaymentService.php
+++ app/Services/CardPaymentService.php
@@ -245,50 +245,54 @@ class CardPaymentService
             unset($input['payment']['billing_address']);
         }
 
-        if (!empty($input['token']) and ($input['payment']['recurring'] === true) and ($input['payment']['recurring_type'] === 'auto'))
-        {
+        //ToDo: Fix this issue of checking for tokenised card and initial payment fetch
 
-            if (!empty($input['token']['card']) and ($input['token']['card']['network'] === 'Visa'))
+        if (false) {
+            if (!empty($input['token']) and ($input['payment']['recurring'] === true) and ($input['payment']['recurring_type'] === 'auto'))
             {
-                $initialPayment = (new Payment\Repository)->fetchInitialPaymentIdForToken($input['token']['id'], $input['merchant']['id']);
 
-                $paymentId = $initialPayment->getId();
-
-                $request = [
-                    'fields'        => ['network_transaction_id'],
-                    'payment_ids'   => [$paymentId],
-                ];
-
-                $input['payment']['network_transaction_id'] = '039217544591994';
-
-                try
+                if (!empty($input['token']['card']) and ($input['token']['card']['network'] === 'Visa'))
                 {
-                    $response = $this->app['card.payments']->fetchAuthorizationData($request);
+                    $initialPayment = (new Payment\Repository)->fetchInitialPaymentIdForToken($input['token']['id'], $input['merchant']['id']);
 
-                    $this->trace->info(
-                        TraceCode::HITACHI_DATA_CPS_REQUEST_RESPONSE,
-                        [
-                            'info_code'     => InfoCode::CPS_RESPONSE_AUTHORIZATION_DATA,
-                            'response'      => $response,
-                        ]);
+                    $paymentId = $initialPayment->getId();
 
-                    if ($response[$paymentId]['network_transaction_id'] !== "")
+                    $request = [
+                        'fields'        => ['network_transaction_id'],
+                        'payment_ids'   => [$paymentId],
+                    ];
+
+                    $input['payment']['network_transaction_id'] = '039217544591994';
+
+                    try
                     {
-                        $input['payment']['network_transaction_id'] = $response[$paymentId]['network_transaction_id'];
+                        $response = $this->app['card.payments']->fetchAuthorizationData($request);
+
+                        $this->trace->info(
+                            TraceCode::HITACHI_DATA_CPS_REQUEST_RESPONSE,
+                            [
+                                'info_code'     => InfoCode::CPS_RESPONSE_AUTHORIZATION_DATA,
+                                'response'      => $response,
+                            ]);
+
+                        if ($response[$paymentId]['network_transaction_id'] !== "")
+                        {
+                            $input['payment']['network_transaction_id'] = $response[$paymentId]['network_transaction_id'];
+                        }
                     }
-                }
-                catch (\Exception $ex)
-                {
-                    $this->trace->info(
-                        TraceCode::HITACHI_DATA_CPS_REQUEST_RESPONSE,
-                        [
-                            'info_code'            => InfoCode::CPS_PAYMENT_AUTH_DATA_ABSENT,
-                            'initial_payment_id'   => $paymentId,
-                        ]);
+                    catch (\Exception $ex)
+                    {
+                        $this->trace->info(
+                            TraceCode::HITACHI_DATA_CPS_REQUEST_RESPONSE,
+                            [
+                                'info_code'            => InfoCode::CPS_PAYMENT_AUTH_DATA_ABSENT,
+                                'initial_payment_id'   => $paymentId,
+                            ]);
+                    }
+
                 }
 
             }
-
         }
 
         $content = [
