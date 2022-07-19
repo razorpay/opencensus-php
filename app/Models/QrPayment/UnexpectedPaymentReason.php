8349a5b62051b0c101ff624f0993874bc8319c74
ritesh-commits <68008524+ritesh-commits@users.noreply.github.com>
2022-07-19 18:53:33 +0530

    Add merchant not live unexpected reason (#31327)
    * Update UnexpectedPaymentReason.php
    
    * Update UnexpectedPaymentReason.php
    
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Models/QrPayment/UnexpectedPaymentReason.php
+++ app/Models/QrPayment/UnexpectedPaymentReason.php
@@ -6,28 +6,31 @@ use RZP\Error\PublicErrorDescription;
 
 class UnexpectedPaymentReason
 {
-    const QR_PAYMENT_AMOUNT_MISMATCH    = 'Actual payment amount does not match expected payment amount';
+    const QR_PAYMENT_AMOUNT_MISMATCH = 'Actual payment amount does not match expected payment amount';
 
-    const QR_PAYMENT_ON_CLOSED_QR_CODE  = 'Payment made on closed QR code';
+    const QR_PAYMENT_ON_CLOSED_QR_CODE = 'Payment made on closed QR code';
 
-    const QR_PAYMENT_QR_NOT_FOUND       = 'Payment made on invalid QR code';
+    const QR_PAYMENT_QR_NOT_FOUND = 'Payment made on invalid QR code';
 
     const QR_CODE_PAYMENT_FAILED_FEE_OR_TAX_TAMPERED = 'Payment failed because fees or tax was tampered';
 
-    const QR_CODE_PAYMENT_FAILED_UPI_NOT_ENABLED     = 'UPI transactions are not enabled for the merchant';
+    const QR_CODE_PAYMENT_FAILED_UPI_NOT_ENABLED = 'UPI transactions are not enabled for the merchant';
 
-    const QR_CODE_CUTOFF_TIME_EXCEEDED               = 'The payment transaction time exceeds the cutoff limit';
+    const QR_CODE_CUTOFF_TIME_EXCEEDED = 'The payment transaction time exceeds the cutoff limit';
 
-    const QR_CODE_MISSING_ORDER_ID                   = 'Payment processing failed due to missing order id';
+    const QR_CODE_MISSING_ORDER_ID = 'Payment processing failed due to missing order id';
+
+    const BAD_REQUEST_MERCHANT_NOT_LIVE_ACTION_DENIED = 'There is a temporary block placed on the account currently because of which new payment operations are put on hold. If you are seeing this message unexpectedly, please contact the site admin regarding the issue.';
 
     protected static $toCreateUnexpected = [
         self::QR_CODE_PAYMENT_FAILED_FEE_OR_TAX_TAMPERED,
         self::QR_CODE_PAYMENT_FAILED_UPI_NOT_ENABLED,
         self::QR_CODE_MISSING_ORDER_ID,
+        self::BAD_REQUEST_MERCHANT_NOT_LIVE_ACTION_DENIED,
         PublicErrorDescription::BAD_REQUEST_PAYMENT_ORDER_ALREADY_PAID,
     ];
 
-    public static function shouldCreateUnexpectedPayment(string $message) : bool
+    public static function shouldCreateUnexpectedPayment(string $message): bool
     {
         return in_array($message, self::$toCreateUnexpected);
     }
