2c23bc3f79186e1489d72ae35656a0f06cabe0f2
Nikita <54991379+Nikita3102@users.noreply.github.com>
2022-07-26 00:30:22 +0530

    adding rupay_sihub as a mandatehub for iins (#31446)
    * adding rupay_sihub as a mandatehub for iins
    
    * update mandatehub.php
    
    * update mandatehub.php
    
    * dummy commit
    
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Models/Card/IIN/MandateHub.php
+++ app/Models/Card/IIN/MandateHub.php
@@ -6,10 +6,12 @@ class MandateHub
 {
     const MANDATE_HQ      = 'mandate_hq';
     const BILLDESK_SIHUB  = 'billdesk_sihub';
+    const RUPAY_SIHUB     = 'rupay_sihub';
 
     public static $mandate_hubs = [
         self::MANDATE_HQ       => 1,
         self::BILLDESK_SIHUB   => 2,
+        self::RUPAY_SIHUB      => 4,
     ];
 
     public static function getEnabledMandateHubs($hex)
@@ -47,3 +49,4 @@ class MandateHub
         return array_keys(self::$mandate_hubs);
     }
 }
+
