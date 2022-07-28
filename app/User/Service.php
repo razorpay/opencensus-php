328e8e29e6f0ed131b4eae32a72bdf9ae77fa62b
piyushRazorpay955 <96859558+piyushRazorpay955@users.noreply.github.com>
2022-07-28 14:08:13 +0530

    session whitelisted error code (#10573)

--- app/User/Service.php
+++ app/User/Service.php
@@ -635,7 +635,7 @@ class Service extends Base\Service
                 $userId = $error[Constants::INTERNAL]['user_details']['user_id'] ??  "";
 
                 if (empty($userId) === false and
-                    (in_array($error[Constants::INTERNAL_ERROR_CODE], Constants::SESSION_BLACKLISTED_ERROR_CODES, true) === false))
+                    (in_array($error[Constants::INTERNAL_ERROR_CODE], Constants::SESSION_WHITELISTED_ERROR_CODES, true) === true))
                 {
                     Session::set('user_id', $userId);
                     Session::set('logged_in_via', $logged_in_via);
@@ -737,7 +737,7 @@ class Service extends Base\Service
                 $userId = $error[Constants::INTERNAL][Constants::USER_DETAILS][Constants::USER_ID] ??  "";
 
                 if (empty($userId) === false and
-                    (in_array($error[Constants::INTERNAL_ERROR_CODE], Constants::SESSION_BLACKLISTED_ERROR_CODES, true) === false))
+                    (in_array($error[Constants::INTERNAL_ERROR_CODE], Constants::SESSION_WHITELISTED_ERROR_CODES, true) === true))
                 {
                     Session::set(Constants::USER_ID, $userId);
                     Session::set('logged_in_via', $logged_in_via);
