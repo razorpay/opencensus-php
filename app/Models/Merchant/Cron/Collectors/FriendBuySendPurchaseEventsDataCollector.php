9e8141825e21fd640cc8a954aa989717790c7710
vasanthikakarla <84132254+vasanthikakarla@users.noreply.github.com>
2022-08-04 00:11:47 +0530

    amount fix (#31810)

--- app/Models/Merchant/Cron/Collectors/FriendBuySendPurchaseEventsDataCollector.php
+++ app/Models/Merchant/Cron/Collectors/FriendBuySendPurchaseEventsDataCollector.php
@@ -29,7 +29,7 @@ class FriendBuySendPurchaseEventsDataCollector extends DbDataCollector
 
         foreach ($merchantIdChunks as $merchantIdChunk) {
             // filter merchants who have crossed settlements above threshold
-            $query = "select sum(base_amount) amount,merchant_id from payments_v1 where merchant_id in (%s) group by merchant_id having amount>%s limit %s";
+            $query = "select sum(base_amount) amount,merchant_id from payments_v1 where merchant_id in (%s) group by merchant_id having amount>=%s limit %s";
 
             $query = sprintf($query, "'" . implode("','", $merchantIdChunk) . "'", env(M2MConstants::M2M_REFERRAL_MIN_TRANSACTION_AMOUNT), count($merchantIdChunk) + 1);
 
