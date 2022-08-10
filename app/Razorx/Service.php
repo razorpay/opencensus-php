b106b886bba5873b5c2d20e67a025b2f79b874b0
Tarun Gupta <31823611+tarungupta9@users.noreply.github.com>
2022-08-10 15:17:41 +0530

    feat: cards nach experiment added (#10701)

--- app/Razorx/Service.php
+++ app/Razorx/Service.php
@@ -273,6 +273,7 @@ class Service extends Base\Service
             'capital_founders_card',
             'capital_last_day_repayment',
             'capital_loc_x_migration_reroute',
+            'capital_cards_nach_payment',
             'rx_vendor_balances_rollout'
         ];
 
