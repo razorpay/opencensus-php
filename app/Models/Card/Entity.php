c4adb0dfd6aa63c286803d988b794c1412d5f144
ankit <30381705+ankitpal11@users.noreply.github.com>
2022-08-11 11:21:53 +0530

    sending token_iin in card array (#31965)
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Models/Card/Entity.php
+++ app/Models/Card/Entity.php
@@ -1129,7 +1129,7 @@ class Entity extends Base\PublicEntity
 
         if ($data[Card\Entity::TRIVIA] === '1')
         {
-            $data['iin']          = empty($data['token_iin']) ? $data['iin'] : substr($data['token_iin'],0,6);
+            $data['token_iin']    = empty($data['token_iin']) ? $data['iin'] : substr($data['token_iin'],0,9);
             $data['expiry_month'] = empty($data['token_expiry_month']) ? $data['expiry_month'] : intval($data['token_expiry_month']);
             $data['expiry_year']  = empty($data['token_expiry_year']) ? $data['expiry_year'] : intval($data ['token_expiry_year']);
             $data['last4']        = empty($data['token_last4']) ? $data['last4'] : $data ['token_last4'];
