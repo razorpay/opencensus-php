139dcaa75e91824e52c6cab1c4e26815131ead44
Sandesh Damkondwar <sandeshdamkondwar@gmail.com>
2022-08-12 16:24:00 +0530

    Revert "chore: allowing only certain domain to capture because of quota issues (#10676)" (#10723)
    This reverts commit 4d731f3e56d2288756e057fbb8a035faa4461e75.

--- web/js/common/utils/observability.js
+++ web/js/common/utils/observability.js
@@ -25,7 +25,7 @@ export function initSentry(appName) {
           return event;
         },
         browserTracing: {
-          tracingOrigins: ['cdn.razorpay.com', 'dashboard.razorpay.com'],
+          tracingOrigins: ['dashboard.razorpay.com', /^\//],
         },
         tracesSampleRate: 0.05,
       });
