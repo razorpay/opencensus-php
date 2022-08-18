c89e0a19162b5c506730a5faa8462efc935a833b
Akshat Jain <32021987+iamakshatjain@users.noreply.github.com>
2022-08-18 10:25:41 +0530

    chore(capital): add feature flag for business rewards (#10747)

--- app/Razorx/Service.php
+++ app/Razorx/Service.php
@@ -271,6 +271,7 @@ class Service extends Base\Service
             'capital_cards_statement',
             'international_apm_onboarding',
             'capital_founders_card',
+            'capital_business_rewards',
             'capital_last_day_repayment',
             'capital_loc_x_migration_reroute',
             'capital_cards_nach_payment',
