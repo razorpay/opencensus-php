42226cbc51b32955adb48d1b525fa91bdb3bdd03
mkharshita <35831970+mkharshita@users.noreply.github.com>
2022-08-29 13:24:06 +0530

    Bugfix changing walnut to axio (#32241)
    * Change walnut to axio
    
    * debug
    
    * change the codition for checking
    
    * review changes
    
    * review changes
    
    * change walnut to axio for all cases
    
    * remove imports
    
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Models/Payment/Processor/CardlessEmi.php
+++ app/Models/Payment/Processor/CardlessEmi.php
@@ -19,6 +19,7 @@ class CardlessEmi
     const FLEXMONEY    = 'flexmoney';
     const WALNUT369    = 'walnut369';
     const SEZZLE       = 'sezzle';
+    const AXIO         = 'Axio';
 
     const HDFC = 'hdfc';
     const KKBK = 'kkbk';
@@ -50,7 +51,7 @@ class CardlessEmi
         self::EARLYSALARY  => 'EarlySalary',
         self::ZESTMONEY    => 'ZestMoney',
         self::FLEXMONEY    => 'FlexMoney',
-        self::WALNUT369    => 'Walnut369',
+        self::WALNUT369    => 'Axio',
         self::SEZZLE       => 'Sezzle',
     ];
 
