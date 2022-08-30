75c326397baa601e28afd955c9bf1ece259b6e13
13shivam <shivam.rai@razorpay.com>
2022-08-30 12:17:06 +0530

    guzzle updat

--- tests/integration/laravel/test.sh
+++ tests/integration/laravel/test.sh
@@ -25,7 +25,7 @@ pushd laravel
 
 composer config repositories.opencensus git ${REPO}
 composer require opencensus/opencensus:dev-${BRANCH} --with-all-dependencies
-composer require --dev guzzlehttp/guzzle:~6.0
+composer require --dev guzzlehttp/guzzle:~7.0
 
 php artisan migrate
 vendor/bin/phpunit
