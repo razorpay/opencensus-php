0396fe3307cdab52e66c034345d11dd1de4bfa1a
rajashreekhandelia <rajashree.khandelia@razorpay.com>
2022-08-30 23:43:57 +0530

    Bugfix/read pp in scalability config (#32379)
    * pp added in app scalability product config
    
    * changed new product
    
    * adding descrp
    
    * change description
    
    Co-authored-by: ani37 <aniket.agarwalla@razorpay.com>
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Models/Merchant/Validator.php
+++ app/Models/Merchant/Validator.php
@@ -409,7 +409,7 @@ class Validator extends Base\Validator
 
     // app scalability validation
     protected static $appScalabilityChangeFtuxRules = [
-        constants::PRODUCT       => 'required|string|in:payment_link,payment_gateway',
+        constants::PRODUCT       => 'required|string|in:payment_link,payment_gateway,payment_pages',
         constants::FTUX_COMPLETE => 'required|bool',
         constants::INTRODUCING   => 'sometimes|bool',
     ];
