9d51b85efab84e5a95561b043647bee983e8c74f
achalshah84 <82373629+achalshah84@users.noreply.github.com>
2022-09-12 11:04:51 +0530

    Changed team name for oauth routes (#10901)

--- app/Http/RouteTeamMap.php
+++ app/Http/RouteTeamMap.php
@@ -8,9 +8,10 @@ class RouteTeamMap
     /**
      * Follows the notation <BU>_<TeamName>
      */
-    const TEAM_PAYMENTS_DASHBOARD = 'payments_dashboard';
-    const TEAM_PAYMENTS_GROWTH    = 'payments_growth';
-    const TEAM_UNKNOWN            = 'unknown_unknown';
+    const TEAM_PAYMENTS_DASHBOARD       = 'payments_dashboard';
+    const TEAM_PAYMENTS_GROWTH          = 'payments_growth';
+    const TEAM_RAZORPAYX_ONBOARDING     = 'razorpayx_onboarding';
+    const TEAM_UNKNOWN                  = 'unknown_unknown';
 
     /**
      * @param $route
@@ -31,26 +32,26 @@ class RouteTeamMap
     protected static $routeTeamMap = [
         'status'                    => [self::TEAM_PAYMENTS_DASHBOARD],
         'user'                      => [self::TEAM_PAYMENTS_DASHBOARD],
-        'oauth_user'                => [self::TEAM_PAYMENTS_DASHBOARD],
+        'oauth_user'                => [self::TEAM_RAZORPAYX_ONBOARDING],
         'merchant'                  => [self::TEAM_PAYMENTS_DASHBOARD],
-        'oauth_merchant'            => [self::TEAM_PAYMENTS_DASHBOARD],
+        'oauth_merchant'            => [self::TEAM_RAZORPAYX_ONBOARDING],
         'extension_user_logout'     => [self::TEAM_PAYMENTS_DASHBOARD],
         'get_org'                   => [self::TEAM_PAYMENTS_DASHBOARD],
-        'oauth_get_org'             => [self::TEAM_PAYMENTS_DASHBOARD],
+        'oauth_get_org'             => [self::TEAM_RAZORPAYX_ONBOARDING],
         'user_keep_alive'           => [self::TEAM_PAYMENTS_DASHBOARD],
         'user_logout'               => [self::TEAM_PAYMENTS_DASHBOARD],
-        'oauth_user_logout'         => [self::TEAM_PAYMENTS_DASHBOARD],
+        'oauth_user_logout'         => [self::TEAM_RAZORPAYX_ONBOARDING],
         'user_details'              => [self::TEAM_PAYMENTS_DASHBOARD],
-        'oauth_user_details'        => [self::TEAM_PAYMENTS_DASHBOARD],
+        'oauth_user_details'        => [self::TEAM_RAZORPAYX_ONBOARDING],
         'get_user_details'          => [self::TEAM_PAYMENTS_DASHBOARD],
         'remove_user'               => [self::TEAM_PAYMENTS_DASHBOARD],
         'tnc'                       => [self::TEAM_PAYMENTS_GROWTH],
         'user_pre_signup'           => [self::TEAM_PAYMENTS_GROWTH],
-        'oauth_user_pre_signup'     => [self::TEAM_PAYMENTS_GROWTH],
+        'oauth_user_pre_signup'     => [self::TEAM_RAZORPAYX_ONBOARDING],
         'user_business_types'       => [self::TEAM_PAYMENTS_GROWTH],
         'user_register_unbounce'    => [self::TEAM_PAYMENTS_GROWTH],
         'merchant_details'          => [self::TEAM_PAYMENTS_GROWTH],
-        'oauth_merchant_details'    => [self::TEAM_PAYMENTS_GROWTH],
+        'oauth_merchant_details'    => [self::TEAM_RAZORPAYX_ONBOARDING],
         'get_keys'                  => [self::TEAM_PAYMENTS_GROWTH],
         'keys_setup'                => [self::TEAM_PAYMENTS_GROWTH],
         'post_activation'           => [self::TEAM_PAYMENTS_GROWTH],
