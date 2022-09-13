4fee746643b3d3dbb4a821174a176e6505113b55
snehasish das gupta <snehasish.dasgupta@razorpay.com>
2022-09-13 18:57:41 +0530

    fix(redirection): stoping redirection to easy dashboard for case of referalCode (#10921)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -215,7 +215,7 @@ class UserController extends Controller
     {
         $uri = trim(\Request::getRequestUri(), '/');
 
-        if (preg_match('/(\br=partner\b)|(\bauth_source\b)|(\breferral_code\b)|(\bcoupon_code\b)/', $uri))
+        if (preg_match('/(\br=partner\b)|(\bauth_source\b)|(\breferral_code\b)|(\breferralCode\b)|(\bcoupon_code\b)/', $uri))
         {
             return true;
         }
