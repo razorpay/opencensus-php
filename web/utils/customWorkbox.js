893b777cec6cf75d9620dab878249c0db99eba00
Sandesh Damkondwar <sandeshdamkondwar@gmail.com>
2022-09-16 10:59:03 +0530

    Revert "[SSAB-663] chore: caching third party assets on stale while revalidate (#10941)" (#10955)
    This reverts commit 228c073ff688900a21bda5767d40ce9af848e30b.

--- web/utils/customWorkbox.js
+++ web/utils/customWorkbox.js
@@ -6,34 +6,8 @@ import { ExpirationPlugin } from 'workbox-expiration';
 import { CacheableResponsePlugin } from 'workbox-cacheable-response';
 import { BroadcastUpdatePlugin } from 'workbox-broadcast-update';
 
-const RAZORPAY_CDN_ASSETS = /https:\/\/cdn\.razorpay\.com\/dashboard\/dist\/(js|css)?\/.*\.(js|css)?$/;
+const ENABLED_CDN_ASSETS = /https:\/\/cdn\.razorpay\.com\/dashboard\/dist\/(js|css)?\/.*\.(js|css)?$/;
 const ENABLED_FONTS = /\.(woff|woff2)?$/;
-const THIRD_PARTY_DOMAINS = [
-  // for other razorpay assets like holdidays, static
-  'https://cdn.razorpay.com',
-
-  // other third party domains
-  'https://wchat.freshchat.com',
-  'https://static.hotjar.com',
-  'https://js.refiner.io',
-  'https://dynamic.criteo.com',
-  'https://www.redditstatic.com',
-  'https://static.ads-twitter.com',
-  'https://bat.bing.com',
-  'https://js.hs-scripts.com',
-  'https://js.usemessages.com',
-  'https://js.hs-analytics.net',
-  'https://js.hsadspixel.net',
-  'https://js.hs-banner.com',
-  'https://js.hsleadflows.net',
-  'https://www.clarity.ms',
-  'https://d2r1yp2w7bby2u.cloudfront.net',
-  'https://in.wzrkt.com',
-  'https://cdn.yellowmessenger.com',
-  'https://js-na1.hs-scripts.com',
-  'https://code.jquery.com',
-  'https://www.youtube.com',
-];
 
 clientsClaim();
 self.skipWaiting();
@@ -41,7 +15,7 @@ self.skipWaiting();
 precacheAndRoute(self.__WB_MANIFEST);
 
 registerRoute(
-  ({ url: { href = '' } }) => RAZORPAY_CDN_ASSETS.test(href),
+  ({ url: { href = '' } }) => ENABLED_CDN_ASSETS.test(href),
   new CacheFirst({
     cacheName: 'static-chunks',
     plugins: [
@@ -51,26 +25,16 @@ registerRoute(
       }),
       new ExpirationPlugin({
         maxAgeSeconds: 60 * 60 * 24 * 30,
-        purgeOnQuotaError: true,
       }),
     ],
   }),
 );
 
 registerRoute(
-  ({ url: { origin }, request }) => {
-    return THIRD_PARTY_DOMAINS.includes(origin) && request.destination === 'script';
-  },
+  new RegExp('https://cdn\\.razorpay\\.com.*/dist/(merchant-entry|merchantLA-entry)?\\.*\\.(js)?$'),
   new StaleWhileRevalidate({
-    cacheName: 'static-assets-thirdparty',
-    plugins: [
-      new BroadcastUpdatePlugin(),
-      new ExpirationPlugin({
-        maxAgeSeconds: 60 * 60 * 24 * 30,
-        maxEntries: 50,
-        purgeOnQuotaError: true,
-      }),
-    ],
+    cacheName: 'static-assets',
+    plugins: [new BroadcastUpdatePlugin()],
   }),
 );
 
@@ -87,8 +51,6 @@ registerRoute(
       }),
       new ExpirationPlugin({
         maxAgeSeconds: 60 * 60 * 24 * 30,
-        maxEntries: 10,
-        purgeOnQuotaError: true,
       }),
     ],
   }),
