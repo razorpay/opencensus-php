6739c413cce7788fe268b7e3d96354f52ed1def4
Praveen Kumar <praveen.k@razorpay.com>
2022-09-17 20:32:08 +0530

    Add netbanking_kvb rearch fetch condition (#32815)
    * add kvb rearch fetch condition
    
    * add kvb rearch fetch condition
    
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Models/Gateway/File/Processor/Claim/Kvb.php
+++ app/Models/Gateway/File/Processor/Claim/Kvb.php
@@ -65,7 +65,8 @@ class Kvb extends NetbankingBase
 
     protected function fetchBankPaymentId($data)
     {
-        if ($data['payment']['cps_route'] === Payment\Entity::NB_PLUS_SERVICE)
+        if (($data['payment']['cps_route'] === Payment\Entity::NB_PLUS_SERVICE) or
+            ($data['payment']['cps_route'] === Payment\Entity::NB_PLUS_SERVICE_PAYMENTS))
         {
             return $data['gateway'][Netbanking::BANK_TRANSACTION_ID]; // payment through nbplus service
         }
@@ -75,7 +76,8 @@ class Kvb extends NetbankingBase
 
     protected function fetchBankAccountNumber($data)
     {
-        if ($data['payment']['cps_route'] === Payment\Entity::NB_PLUS_SERVICE)
+        if (($data['payment']['cps_route'] === Payment\Entity::NB_PLUS_SERVICE) or
+            ($data['payment']['cps_route'] === Payment\Entity::NB_PLUS_SERVICE_PAYMENTS))
         {
             return $data['gateway'][Netbanking::BANK_ACCOUNT_NUMBER]; // payment through nbplus service
         }
