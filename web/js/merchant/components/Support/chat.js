3cb87adbcb4c3d1a38a4325a3527f36875c78591
Mohit Agrawal <61590976+mohitagrawal1305@users.noreply.github.com>
2022-09-19 13:00:17 +0530

    feat: updated chat script (#10978)

--- web/js/merchant/components/Support/chat.js
+++ web/js/merchant/components/Support/chat.js
@@ -8,6 +8,8 @@ const CLOSING_TEXTS = [
   'Thank you for sharing your experience.',
   'We will be closing the conversation as you seem to be away. Please feel free to connect back in case you need any further assistance. We will be happy to help! Thank you for chatting with Razorpay.',
   'We are closing the conversation as you have been inactive for more than 5 minutes.',
+  'Thank you so much for using “Razorpay” chat service. We hope we will hear from you soon! Have a good day.',
+  'Sure! We are restarting the conversation',
 ];
 
 const initFreshchat = (data) => {
