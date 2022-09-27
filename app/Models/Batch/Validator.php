c43e9db5f17d03a755537340d0a5588a52b2508d
Atul Prakash <88471306+decentfellow@users.noreply.github.com>
2022-09-28 01:03:56 +0530

    recurring charge bulk create validation rules (#33100)
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Models/Batch/Validator.php
+++ app/Models/Batch/Validator.php
@@ -229,6 +229,14 @@ class Validator extends Base\Validator
         Entity::SCHEDULE        => 'sometimes|numeric',
     ];
 
+    protected static $recurringChargeBulkCreateRules = [
+        Entity::TYPE            => 'required|in:recurring_charge_bulk',
+        Entity::FILE            => 'required_without:file_id|file|max:60720' . self::DEFAULT_MIME_RULE, // 60MB
+        Entity::NAME            => 'filled|string|max:255',
+        Entity::FILE_ID         => 'required_without:file|public_id',
+        Entity::SCHEDULE        => 'sometimes|numeric',
+    ];
+
     protected static $recurringChargeAxisCreateRules = [
         Entity::TYPE            => 'required|in:recurring_charge_axis',
         Entity::FILE            => 'required_without:file_id|file|max:60720' . self::DEFAULT_MIME_RULE, // 60MB
