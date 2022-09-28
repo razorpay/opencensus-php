5aea69a8c5356644892c80edb2dfc5f1f46a45c6
Raajitha <87758012+raajithapotala@users.noreply.github.com>
2022-09-28 22:08:50 +0530

    fix (#33124)

--- app/Models/Customer/Token/Core.php
+++ app/Models/Customer/Token/Core.php
@@ -1988,7 +1988,7 @@ class Core extends Base\Core
             ],
             "tokenised"    => $isTokenized,
             "merchant"     => [
-                "id"       => (isset($this->merchant) === true) ? null : $this->merchant->getId(),
+                "id"       => (isset($this->merchant) === false) ? null : $this->merchant->getId(),
             ],
             "internal_service_request" => $internalServiceRequest,
         ];
