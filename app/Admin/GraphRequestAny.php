17e1f02add7d3d6b71b458521957260144c602fd
Shubham kumar <51047960+shubham25kr07@users.noreply.github.com>
2022-10-03 17:03:02 +0530

    Add uid mid graphql (#11064)
    * refactor: added x-dashboard uid and mid for graphql
    
    * refactor: revert package.json file
    
    * refactor: "added $this->trace instead of Trace"
    
    * refactor: "added $this->trace instead of Trace"
    
    * fix: added null of no-header is present
    
    * fix: added null of no-header is present

--- app/Admin/GraphRequestAny.php
+++ app/Admin/GraphRequestAny.php
@@ -259,6 +259,12 @@ class GraphRequestAny
 
             $this->headers['X-Dashboard-Merchant-Id'] = app('request.ctx')->getMerchantId();
         }
+
+        $this->trace->info(TraceCode::GRAPH_REQUEST_X_DASHBOARD_UID_MID, [
+            'x-dashboard-merchant-id' => $this->headers['X-Dashboard-Merchant-Id'] ?? null,
+            'x-dashboard-user-id'    => $this->headers['X-Dashboard-User-Id'] ?? null,
+        ]);
+
     }
 
     private function appendMerchantHeaderIfValid($user)
