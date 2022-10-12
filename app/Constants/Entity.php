78c32a21d2dbb095d3476e7de69ba4a6a6f2b6ad
Manthan Surkar <42006277+thesmallstar@users.noreply.github.com>
2022-10-12 07:22:54 +0530

    Remove MERCHANT_BUSINESS_DETAIL from ASV entities (#33429)
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Constants/Entity.php
+++ app/Constants/Entity.php
@@ -839,8 +839,7 @@ class Entity
         self::STAKEHOLDER,
         self::MERCHANT_DOCUMENT,
         self::MERCHANT_EMAIL,
-        self::MERCHANT_WEBSITE,
-        self::MERCHANT_BUSINESS_DETAIL
+        self::MERCHANT_WEBSITE
     ];
 
     /**
