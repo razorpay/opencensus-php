5359a7491f80e921e4e85ba9e3213a4f6d849ef1
Anant Mishra <anant.mishra@razorpay.com>
2022-10-21 15:10:24 +0530

    fix("exclude invitation from redirecting to easy onboarding"): exclude invitation from redirection (#11221)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -214,7 +214,7 @@ class UserController extends Controller
     {
         $uri = trim(\Request::getRequestUri(), '/');
 
-        if (preg_match('/(\br=partner\b)|(\bauth_source\b)|(\breferral_code\b)|(\bcoupon_code\b)|(\bmerchant_invitation\b)/', $uri))
+        if (preg_match('/(\br=partner\b)|(\bauth_source\b)|(\breferral_code\b)|(\bcoupon_code\b)|(\bmerchant_invitation\b)|(\binvitation\b)/', $uri))
         {
             return true;
         }
