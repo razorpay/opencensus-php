e9df7493462e66d5ad85ec72f193ddbac461477c
Manthan Surkar <42006277+thesmallstar@users.noreply.github.com>
2022-10-28 03:21:25 +0530

    Add Merchant Business Detail to ASV entity (#33773)

--- app/Constants/Entity.php
+++ app/Constants/Entity.php
@@ -840,7 +840,8 @@ class Entity
         self::STAKEHOLDER,
         self::MERCHANT_DOCUMENT,
         self::MERCHANT_EMAIL,
-        self::MERCHANT_WEBSITE
+        self::MERCHANT_WEBSITE,
+        self::MERCHANT_BUSINESS_DETAIL
     ];
 
     /**
