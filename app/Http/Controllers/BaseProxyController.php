26273b5de08b514d06d51817249c612d77bcc550
Shubham choudhary <70131730+shubhamiitmandi123@users.noreply.github.com>
2022-11-02 11:09:07 +0530

    Revert "changing cmma default url to devstack and adding devstack header (#33777)" (#33848)
    This reverts commit be6f0f1d212ae5ebfa0e10e07e41ef20dca0cdac.

--- app/Http/Controllers/BaseProxyController.php
+++ app/Http/Controllers/BaseProxyController.php
@@ -15,7 +15,6 @@ use Psr\Http\Message\RequestInterface;
 use RZP\Http\Controllers\Processors\PostProcessor;
 use RZP\Http\Controllers\Processors\PreProcessor;
 use RZP\Http\Request\Requests;
-use RZP\Http\RequestHeader;
 use RZP\Trace\TraceCode;
 
 abstract class BaseProxyController extends Controller
@@ -279,10 +278,6 @@ abstract class BaseProxyController extends Controller
 
         $arrHeaders = new ArrayHeaders($headers);
         $headers    = $arrHeaders->toArray();
-
-        $headers = array_merge($headers, [
-            RequestHeader::DEV_SERVE_USER =>  Request::header(RequestHeader::DEV_SERVE_USER)
-        ]);
 
         $baseUrl = $this->getBaseUrl();
         $url     = $baseUrl . '/' . $path;
