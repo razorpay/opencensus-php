23cfed786a24217dafcc9632ee6b11e9f7cd2902
Ishan Batra <104422121+Ishan-Batra@users.noreply.github.com>
2022-11-08 23:10:13 +0530

    Master transaction metrics (#33950)
    * Metrics for Transactions banking
    
    * Metrics for Transactions banking
    
    * Review comments
    
    * Review comments
    
    * Review comments
    
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

---
+++ app/Models/Transaction/Metric.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace RZP\Models\Transaction;
+
+/**
+ * List of metrics in Transaction/ module
+ */
+final class Metric
+{
+    // Counters
+    const TRANSACTION_VA_REQUEST_TOTAL                            = 'transaction_va_request_total';
+    const TRANSACTION_CA_REQUEST_TOTAL                            = 'transaction_ca_request_total';
+    const TRANSACTION_LEGACY_REQUEST_TOTAL                        = 'transaction_legacy_request_total';
+    // Histogram
+    const TRANSACTION_VA_REQUEST_LATENCY_MILLISECONDS             = 'transaction_va_request_latency_ms';
+    const TRANSACTION_CA_REQUEST_LATENCY_MILLISECONDS             = 'transaction_ca_request_latency_ms';
+    const TRANSACTION_LEGACY_REQUEST_LATENCY_MILLISECONDS         = 'transaction_legacy_request_latency_ms';
+}
--- app/Models/Transaction/Statement/Service.php
+++ app/Models/Transaction/Statement/Service.php
@@ -5,10 +5,12 @@ namespace RZP\Models\Transaction\Statement;
 use RZP\Constants\Mode;
 use RZP\Trace\TraceCode;
 use RZP\Models\Merchant;
+use RZP\Constants\Metric;
 use RZP\Models\Transaction;
 use RZP\Base\ConnectionType;
 use RZP\Models\Feature\Constants;
 use RZP\Models\Base\PublicCollection;
+use RZP\Models\Transaction\Metric as TxnMetric;
 
 /**
  * Class Service
@@ -120,7 +122,8 @@ class Service extends Transaction\Service
 
     /*
      * TODO: VendorPayment Internal Auth Also Uses this method -->
-     *        switching merchants between these feature flags could lead to a potential data miss, and should be handled carefully later.
+     *       switching merchants between these feature flags could lead to a potential data miss,
+     *       and should be handled carefully later.
      */
     // Temporary route for forcing the index for X Dashboard Requests for Account statement
     public function fetchMultipleForBanking($input)
@@ -130,11 +133,17 @@ class Service extends Transaction\Service
 
         $balance = $merchantValidator->validateAndTranslateAccountNumberForBanking($input);
 
+        $dimension = $this->getDimensions();
+
         // Route request to ledger statement if ledger read feature is enabled
         if (($this->merchant->isFeatureEnabled(Constants::LEDGER_REVERSE_SHADOW) === true) and
             ($this->isExperimentEnabled(Merchant\RazorxTreatment::RX_REARCH_TIDB_EXPERIMENT) === true) and
             ($balance->isAccountTypeShared() === true))
         {
+            $startTime = millitime();
+
+            $this->trace->count(TxnMetric::TRANSACTION_VA_REQUEST_TOTAL, $dimension);
+
             $ledger = $this->repo->ledger_statement->fetch($input,
                                                            $this->merchant->getId(),
                                                            ConnectionType::RX_DATA_WAREHOUSE_MERCHANT);
@@ -153,6 +162,11 @@ class Service extends Transaction\Service
                 ]
             );
 
+            $this->trace->histogram(
+                TxnMetric::TRANSACTION_VA_REQUEST_LATENCY_MILLISECONDS,
+                millitime() - $startTime,
+                $dimension);
+
             return $response;
         }
 
@@ -160,6 +174,10 @@ class Service extends Transaction\Service
         if (($this->isExperimentEnabled(Merchant\RazorxTreatment::RX_DA_ACC_STMT_EXPERIMENT) === true) and
             ($balance->isAccountTypeDirect() === true))
         {
+            $startTime = millitime();
+
+            $this->trace->count(TxnMetric::TRANSACTION_CA_REQUEST_TOTAL, $dimension);
+
             $this->trace->info(
                 TraceCode::DRIVING_ACCOUNT_STATEMENT_FOR_DA_VIA_BAS,
                 [
@@ -185,8 +203,18 @@ class Service extends Transaction\Service
                 ]
             );
 
+            $this->trace->histogram(
+                TxnMetric::TRANSACTION_CA_REQUEST_LATENCY_MILLISECONDS,
+                millitime() - $startTime,
+                $dimension);
+
             return $response;
         }
+
+        // metrics for legacy
+        $startTime = millitime();
+
+        $this->trace->count(TxnMetric::TRANSACTION_LEGACY_REQUEST_TOTAL, $dimension);
 
         /** @var PublicCollection $transactions */
         $transactions = $this->repo->statement->setBaseQueryAndFetchForBanking($input,
@@ -208,6 +236,11 @@ class Service extends Transaction\Service
             ]
         );
 
+        $this->trace->histogram(
+            TxnMetric::TRANSACTION_LEGACY_REQUEST_LATENCY_MILLISECONDS,
+            millitime() - $startTime,
+            $dimension);
+
         return $response;
     }
 
@@ -219,5 +252,18 @@ class Service extends Transaction\Service
             $experiment, $app['basicauth']->getMode() ?? Mode::LIVE);
 
         return ($variant === 'on');
+    }
+
+    /**
+     * @return array
+     */
+    private function getDimensions(): array
+    {
+        $dimensions = [];
+
+        $dimensions[Metric::LABEL_RZP_INTERNAL_APP_NAME] = app('request.ctx')->getInternalAppName()
+                                                           ?? Metric::LABEL_NONE_VALUE;
+
+        return $dimensions;
     }
 }
