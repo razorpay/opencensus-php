157e2ba3c5178c4518942978a14cba34c954a258
Gaurav Singhal <42150487+Gauravsinghal09@users.noreply.github.com>
2022-11-14 19:42:38 +0530

    echo workflow logs (#34041)
    * echo workflow logs
    
    * build: removes workflow status logs
    
    * reverts changes
    
    * adds dummy echo
    
    * adds logs in github workflows
    
    * exp to print log
    
    * adds log
    
    * update curl flags
    
    * echoing the output to a file and printing it
    
    * changes the way curl response is being parsed
    
    * build: dumps build image workflow logs to a txt file and creates a new github action to print the txt file
    
    * adds log workflow to pr_approved_workflow.yml & modifies the logic to dump logs to file
    
    * minor fix
    
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- .github/actions/getWorkflowStatus.sh
+++ .github/actions/getWorkflowStatus.sh
@@ -5,12 +5,24 @@ get_workflow_status_value() {
   AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
   branch_name=${BRANCH}
   workflow_file=${WORKFLOW}
-  workflow_details=$(curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" "${URI}/repos/${GITHUB_REPOSITORY}/actions/workflows/$workflow_file/runs?branch=$branch_name&per_page=1" | jq --raw-output '{conclusion: .workflow_runs[0].conclusion, status: .workflow_runs[0].status}')
+  workflow_response=$(curl -sSD build_image_workflow_response_header.txt -H "${AUTH_HEADER}" -H "${API_HEADER}" "${URI}/repos/${GITHUB_REPOSITORY}/actions/workflows/$workflow_file/runs?branch=$branch_name&per_page=1" | jq '.')
+
+  echo "$workflow_response" > build_image_workflow_response_body.txt
+  cat build_image_workflow_response_header.txt >> build_image_workflow_log.txt
+  cat build_image_workflow_response_body.txt >> build_image_workflow_log.txt
+
+  workflow_details=$(echo "$workflow_response" | jq --raw-output '{conclusion: .workflow_runs[0].conclusion, status: .workflow_runs[0].status}')
   workflow_status=$(echo "$workflow_details" | jq --raw-output '.status')
   while [ "$workflow_status" != "completed" ]
   do
     sleep 3m
-    workflow_details=$(curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" "${URI}/repos/${GITHUB_REPOSITORY}/actions/workflows/$workflow_file/runs?branch=$branch_name&per_page=1" | jq --raw-output '{conclusion: .workflow_runs[0].conclusion, status: .workflow_runs[0].status}')
+    workflow_response=$(curl -sSD build_image_workflow_response_header.txt -H "${AUTH_HEADER}" -H "${API_HEADER}" "${URI}/repos/${GITHUB_REPOSITORY}/actions/workflows/$workflow_file/runs?branch=$branch_name&per_page=1" | jq '.')
+
+    echo "$workflow_response" > build_image_workflow_response_body.txt
+    cat build_image_workflow_response_header.txt >> build_image_workflow_log.txt
+    cat build_image_workflow_response_body.txt >> build_image_workflow_log.txt
+
+    workflow_details=$(echo "$workflow_response" | jq --raw-output '{conclusion: .workflow_runs[0].conclusion, status: .workflow_runs[0].status}')
     workflow_status=$(echo "$workflow_details" | jq --raw-output '.status')
   done
   workflow_conclusion=$(echo "$workflow_details" | jq --raw-output '.conclusion')
