4320312441ff63cd596897df01b20e07ca7f9a22
Ridhima16 <50755164+Ridhima16@users.noreply.github.com>
2022-11-15 16:01:06 +0530

    adds jira check for Migration and Archival PRs as well (#34107)
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- .github/actions/jiraStatusCheck.sh
+++ .github/actions/jiraStatusCheck.sh
@@ -13,36 +13,24 @@ then
   exit 0
 fi
 
-if [ "${ARCHIVAL_LABEL}" = "true" ]
-then
-  echo "This PR contains archival label. Jira check is not needed here."
-  exit 0
-fi
-
 if [ "${PAYMENTS_BU_FLAG}" = "true" ]
 then
-  if [ "${NON_MIGRATION_FLAG}" = "true" ]
+  if [ "${JIRA_ISSUE_ID}" = "" ]
   then
-    if [ "${JIRA_ISSUE_ID}" = "" ]
-    then
-      echo "Jira Id is not present for Non-Migration PR."
-      exit 1
-    else
-      jira_issue_status=$(curl -s "${BASE_URL}/rest/api/2/issue/${JIRA_ISSUE_ID}?fields=status" --user "${USER_EMAIL}":"${API_TOKEN}" | jq .fields.status.name)
-      echo "Server response is <$jira_issue_status>"
-      jira_issue_status_in_lower_case="$(echo "$jira_issue_status" | awk '{print tolower($0)}')"
-      if [ $jira_issue_status_in_lower_case == "\"approved\"" ]
-      then
-        echo "Jira issue is approved."
-        exit 0
-      else
-        echo "Jira issue is not approved."
-        exit 1
-      fi
-    fi
+    echo "Jira Id is not present for PR."
+    exit 1
   else
-    echo "This is a not a Non-Migration PR. Jira check is not needed here."
-    exit 0
+    jira_issue_status=$(curl -s "${BASE_URL}/rest/api/2/issue/${JIRA_ISSUE_ID}?fields=status" --user "${USER_EMAIL}":"${API_TOKEN}" | jq .fields.status.name)
+    echo "Server response is <$jira_issue_status>"
+    jira_issue_status_in_lower_case="$(echo "$jira_issue_status" | awk '{print tolower($0)}')"
+    if [ $jira_issue_status_in_lower_case == "\"approved\"" ]
+    then
+      echo "Jira issue is approved."
+      exit 0
+    else
+      echo "Jira issue is not approved."
+      exit 1
+    fi
   fi
 else
   echo "PR is not for Payments BU. Jira check is not needed here."
