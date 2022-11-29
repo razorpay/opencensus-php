44ff1aff70afd90e88b5e765daa0422ce89621b3
ChetanGN <54831964+ChetanGN@users.noreply.github.com>
2022-11-30 03:50:52 +0530

    Reverted checkout info and added shopify order id (#34314)
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Models/Merchant/OneClickCheckout/Shopify/Service.php
+++ app/Models/Merchant/OneClickCheckout/Shopify/Service.php
@@ -47,10 +47,6 @@ class Service extends Base\Service
         'JCdhfzRcU0ymaX',   //LAMRIM LLP
     ];
 
-    const addCheckoutInfoMids = [
-        'G4ysrlrxfPzRqk',  //Nicobar Design
-    ];
-
     const farziEnabledMids = [
         'ChdCdGm7TvuVk6',   //boAt
     ];
@@ -494,8 +490,6 @@ class Service extends Base\Service
 
         $countryCode = $orderArray['customer_details']['shipping_address']['country'];
 
-        $merchantId = $this->merchant->getId();
-
         // NOTE: promotions is not set if the 1ccResetAPI call fails, until CX team fixes it
         // keep the null check here
         $response = [
@@ -503,22 +497,13 @@ class Service extends Base\Service
             'promotions'       => $orderArray['promotions'] ?? [],
             'shipping_fee'     => $orderArray['shipping_fee'],
             'order_id'         => $shopifyOrder['order']['name'],
+            'id'               => $shopifyOrder['order']['id'],
             'total_tax'        => $shopifyOrder['order']['total_tax'],
             'payment_method'   => $payment['method'],
             'payment_currency' => $payment['currency'],
             'payment_id'       => $paymentId,
             'shipping_country' => Constants\Country::getCountryNameByCode($countryCode) ?? $countryCode
         ];
-
-        if (isset($merchantId) === true and in_array($merchantId, self::addCheckoutInfoMids) === true)
-        {
-            $checkoutId = $order['notes']['storefront_id'];
-
-            $checkout = (new Checkout)->getCheckoutFromAdminApi($checkoutId);
-
-            $response['checkout_id']    = isset($checkout['name']) ? str_replace('#', '', $checkout['name']) : '';
-            $response['checkout_token'] = isset($checkout['token']) ? $checkout['token'] : '';
-        }
 
         // NOTE: Logging the response to debug an issue where the FE is not receiving data
         // for analytics
