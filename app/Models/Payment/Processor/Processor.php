fe666cbab21e809144df259ec05509b521b56351
shubhdeep128 <97942927+shubhdeep128@users.noreply.github.com>
2022-12-01 23:38:20 +0530

    [Barricade] Fix VAS Field in payload (#34593)
    * [Barricade] Fix VAS Field in payload
    
    * Add settled by
    
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Models/Payment/Processor/Processor.php
+++ app/Models/Payment/Processor/Processor.php
@@ -4141,7 +4141,8 @@ class Processor
             'dcc_mark_up_percent'=>$dcc_mark_up_percent,
             'fee_bearer' => $payment->getFeeBearer(),
             'fee' => $payment->getFee(),
-            'is_hdfc_vas' => $payment->isHdfcVasDSCustomerFeeBearerSurcharge(),
+            'is_vas_merchant' => $payment->isVasMerchantWithDirectSettlement(),
+            'settled_by' => $payment->getSettledBy(),
         ];
         $terminal = $payment->terminal;
 
