ba606cd85a6a11bfbda8ce9553ff06e9e94cc009
udayraj-rzp <103119590+udayraj-rzp@users.noreply.github.com>
2022-12-09 17:28:23 +0530

    fix: jira issue in quotes to enable spaces in status (#34794)

--- .github/actions/jiraStatusCheck.sh
+++ .github/actions/jiraStatusCheck.sh
@@ -23,7 +23,7 @@ then
     jira_issue_status=$(curl -s "${BASE_URL}/rest/api/2/issue/${JIRA_ISSUE_ID}?fields=status" --user "${USER_EMAIL}":"${API_TOKEN}" | jq .fields.status.name)
     echo "Server response is <$jira_issue_status>"
     jira_issue_status_in_lower_case="$(echo "$jira_issue_status" | awk '{print tolower($0)}')"
-    if [ $jira_issue_status_in_lower_case == "\"approved\"" ]
+    if [ "$jira_issue_status_in_lower_case" == "\"approved\"" ]
     then
       echo "Jira issue is approved."
       exit 0
