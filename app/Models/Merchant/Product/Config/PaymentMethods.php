3b3a8948db910d0291cb4205c4dd9849dee409a2
Bhavya Y <bhavya.y@razorpay.com>
2022-12-14 09:18:37 +0530

    Increasing timeout for terminal get request to reduce errors (#34866)

--- app/Models/Merchant/Product/Config/PaymentMethods.php
+++ app/Models/Merchant/Product/Config/PaymentMethods.php
@@ -47,7 +47,7 @@ class PaymentMethods
             \Requests::GET,
 
             'v2/merchant_instrument_status?merchant_id=' . $merchant->getId(),
-            ['timeout' => 3], //Increasing timeout to 3sec for now
+            ['timeout' => 4], //Increasing timeout to 4sec for now
             $this->getMerchantHeadersForInstrumentRequest());
 
 //        $response[Util\Constants::FEATURES] = $this->getFeatures();
