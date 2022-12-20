9924afb6a759712610f4b09d4b41dd084f185dbb
shivam586 <55330279+shivam586@users.noreply.github.com>
2022-12-20 09:29:19 +0530

    update experiment name from capital to small (#35009)

--- app/Models/Merchant/RazorxTreatment.php
+++ app/Models/Merchant/RazorxTreatment.php
@@ -454,7 +454,7 @@ final class RazorxTreatment
     const BVS_CREATE_VALIDATION_METADATA = 'BVS_CREATE_VALIDATION_METADATA';
 
     // Experiment to change the fetch logic for showing statements on x dashboard
-    const RAZORX_FLAG_TO_ENHANCE_FETCH_LOGIC = 'RAZORX_FLAG_TO_ENHANCE_FETCH_LOGIC';
+    const RAZORX_FLAG_TO_ENHANCE_FETCH_LOGIC = 'razorx_flag_to_enhance_fetch_logic';
 
     // Experiment to send manual verification data to BVS
     const BVS_MANUAL_VERIFICATION_DATA = 'BVS_MANUAL_VERIFICATION_DATA';
