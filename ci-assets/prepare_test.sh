57a45cb71e39e026c0a33b50bf3dd78f5c049b5c
mitashadongre <119572647+mitashadongre@users.noreply.github.com>
2023-01-03 09:43:38 +0530

    Updated EMM branch to master (#35323)
    * changed module branch to master
    
    * added master branch
    
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- ci-assets/prepare_test.sh
+++ ci-assets/prepare_test.sh
@@ -19,5 +19,5 @@ git init --quiet
 git config core.sparseCheckout true
 cp ../error_modules .git/info/sparse-checkout
 git remote add origin https://$GIT_TOKEN@github.com/razorpay/error-mapping-module
-git fetch origin 8314_emm_bfl --quiet
-git checkout origin/8314_emm_bfl --quiet
+git fetch origin master --quiet
+git checkout origin/master --quiet
