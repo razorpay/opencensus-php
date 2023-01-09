ac43e78e35ff715985eb040ee0c69899bc8b0417
AnkitaBagale <69760792+AnkitaBagale@users.noreply.github.com>
2023-01-09 16:51:22 +0530

    chore: removed razorX flag (#11641)

--- app/Razorx/Service.php
+++ app/Razorx/Service.php
@@ -186,7 +186,6 @@ class Service extends Base\Service
             'rx_icici_auto_kyc',
             'updated_lite_onboarding',
             'show_multiple_vas_on_x',
-            'rx_undo_payout_feature',
             'rx_command_palette',
             'rx_tally_accrual',
             'gstin_sync',
