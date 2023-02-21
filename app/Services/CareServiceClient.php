6851949212b71ec5127c800950971622e53279c8
Danish Kamaal <danish.kamal@razorpay.com>
2023-02-22 00:08:45 +0530

    SC-193: Increase timeout to 5 mins for csm changed cron (#36415)

--- app/Services/CareServiceClient.php
+++ app/Services/CareServiceClient.php
@@ -34,13 +34,16 @@ class CareServiceClient
     const CREATED_AT    = 'created_at';
 
     const MERCHANT_POPULAR_PRODUCTS_CRON = 'MerchantPopularProductsCron';
+    const MERCHANT_NOTIFY_CSM_CHANGED_CRON = 'MerchantNotifyCsmChangedCron';
 
     const ROUTES_URL_MAP  = [
         self::MERCHANT_POPULAR_PRODUCTS_CRON => 'twirp/rzp.care.merchantNavigation.v1.MerchantNavigationService/PostMerchantPopularProducts',   // 5mins
+        self::MERCHANT_NOTIFY_CSM_CHANGED_CRON => 'twirp/rzp.care.csm.v1.CsmService/NotifyMerchantsWithCsmChange',
     ];
 
     const PATH_TIMEOUT_MAP  = [
         self::MERCHANT_POPULAR_PRODUCTS_CRON => 300,   // 5mins
+        self::MERCHANT_NOTIFY_CSM_CHANGED_CRON => 300,
     ];
 
     public function __construct($app = null)
