07fc4cad5218b901a964eea50c3159d15bc28704
Pankaj Ahuja <9896838+pankaj-a@users.noreply.github.com>
2023-02-24 14:26:57 +0530

    Bvs list documents proxy endpoint (#36342)
    * Adds proxy endpoint for BVS consent and kyc document list
    
    * Fix url path for bvs document list api
    
    ---------
    
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Http/Controllers/BvsAdminProxyController.php
+++ app/Http/Controllers/BvsAdminProxyController.php
@@ -8,20 +8,28 @@ class BvsAdminProxyController extends BaseProxyController
 {
     const LIST_VALIDATIONS_FOR_MERCHANT = 'ListValidationsForMerchant';
     const GET_VALIDATION_WITH_DETAILS = 'GetValidationWithDetails';
+    const GET_CONSENT_DOCUMENTS_BY_OWNER_ID = 'GetConsentDocumentsByOwnerId';
+    const LIST_KYC_DOCUMENTS = 'ListDocuments';
 
     const ROUTES_URL_MAP    = [
         self::LIST_VALIDATIONS_FOR_MERCHANT => "/twirp\/platform.bvs.ownerdetails.v1.VerificiationLifecycleAPI\/ListValidations/",
-        self::GET_VALIDATION_WITH_DETAILS => "/twirp\/platform.bvs.validation.v2.ValidationAPI\/GetValidationWithDetails/"
+        self::GET_VALIDATION_WITH_DETAILS => "/twirp\/platform.bvs.validation.v2.ValidationAPI\/GetValidationWithDetails/",
+        self::GET_CONSENT_DOCUMENTS_BY_OWNER_ID => "/twirp\/platform.bvs.consentdocumentmanager.v2.ConsentDocumentManagerAPI\/GetConsentDocumentsByOwnerId/",
+        self::LIST_KYC_DOCUMENTS => "/twirp\/platform.bvs.kycdocumentmanager.v1.KYCDocumentManagerAPI\/ListDocuments/"
     ];
 
     const ADMIN_ROUTES = [
         self::LIST_VALIDATIONS_FOR_MERCHANT,
-        self::GET_VALIDATION_WITH_DETAILS
+        self::GET_VALIDATION_WITH_DETAILS,
+        self::GET_CONSENT_DOCUMENTS_BY_OWNER_ID,
+        self::LIST_KYC_DOCUMENTS
     ];
 
     const ADMIN_ROUTES_VS_PERMISSION   = [
         self::LIST_VALIDATIONS_FOR_MERCHANT   => Name::VIEW_ALL_ENTITY,
         self::GET_VALIDATION_WITH_DETAILS   => Name::VIEW_ACTIVATION_FORM,
+        self::GET_CONSENT_DOCUMENTS_BY_OWNER_ID => Name::VIEW_ALL_ENTITY,
+        self::LIST_KYC_DOCUMENTS => Name::VIEW_ALL_ENTITY,
     ];
 
     public function __construct()
