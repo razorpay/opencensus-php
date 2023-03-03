83c8cfe851ef4ac129e14e746f8fe7b2db98aca6
nikhil-rzp <99319888+nikhil-rzp@users.noreply.github.com>
2023-03-03 11:46:18 +0530

    add mode in queue key (#36597)

--- config/queue.php
+++ config/queue.php
@@ -613,11 +613,20 @@ return [
 
     'fulfillment_event_update' => env('AWS_ORDER_STATUS_UPDATE_QUEUE'),
 
-    'merchant_based_balance_update_v1' => env('AWS_MERCHANT_BASED_BAL_UPDATE_V1'),
+    'merchant_based_balance_update_v1' => [
+        'live'       => env('AWS_MERCHANT_BASED_BAL_UPDATE_V1'),
+        'test'       => env('AWS_MERCHANT_BASED_BAL_UPDATE_V1'),
+    ],
 
-    'merchant_based_balance_update_v2' => env('AWS_MERCHANT_BASED_BAL_UPDATE_V2'),
+    'merchant_based_balance_update_v2' => [
+        'live'       => env('AWS_MERCHANT_BASED_BAL_UPDATE_V2'),
+        'test'       => env('AWS_MERCHANT_BASED_BAL_UPDATE_V2'),
+    ],
 
-    'merchant_based_balance_update_v3' => env('AWS_MERCHANT_BASED_BAL_UPDATE_V3'),
+    'merchant_based_balance_update_v3' => [
+        'live'       => env('AWS_MERCHANT_BASED_BAL_UPDATE_V3'),
+        'test'       => env('AWS_MERCHANT_BASED_BAL_UPDATE_V3'),
+    ],
 
     /*
     |--------------------------------------------------------------------------
