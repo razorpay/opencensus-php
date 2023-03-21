962b971c8d316ddfa7fad082026da56a42b42410
sofiya02 <84763889+sofiya02@users.noreply.github.com>
2023-03-21 11:53:40 +0530

    DCS feature migration along with feature name modification (#36188)
    DCS feature migration for shopify reports

--- app/Services/Dcs/Features/Constants.php
+++ app/Services/Dcs/Features/Constants.php
@@ -68,6 +68,7 @@ class Constants
     const MerchantActivationByPartnerEnabled = 'merchant_activation_by_partner_enabled';
     const OverridingSubmerchantConfigEnabled = 'overriding_submerchant_config_enabled';
     const AdditionalFieldsHdfcOnboarding = 'additional_fields_hdfc_onboarding';
+    const ShopifyPaymentsReport = 'shopify_payments_report';
     const NoDocOnboardingEnabled = 'no_doc_onboarding_enabled';
     const OnboardedViaV2ApiEnabled = 'onboarded_via_v2_api_enabled';
     const BlockSendingOnboardingSms = 'block_sending_onboarding_sms';
@@ -135,6 +136,7 @@ class Constants
         self::MerchantActivationByPartnerEnabled => "rzp/platform/partner/onboarding/Features",
         self::OverridingSubmerchantConfigEnabled => "rzp/platform/partner/configuration/Features",
         self::AdditionalFieldsHdfcOnboarding => "rzp/pg/org/dashboard/admin/Features",
+        self::ShopifyPaymentsReport => "rzp/pg/merchant/report/Features",
         self::NoDocOnboardingEnabled => 'rzp/pg/merchant/onboarding/PartnershipFeatures',
         self::OnboardedViaV2ApiEnabled => 'rzp/pg/merchant/onboarding/PartnershipFeatures',
         self::BlockSendingOnboardingSms => 'rzp/pg/merchant/communication/PartnershipFeatures',
@@ -204,6 +206,7 @@ class Constants
         APIFeaturesConstants::INSTANT_ACTIVATION_V2_API                     => self::SubmerchantInstantActivationViaV2ApiEnabled,
         APIFeaturesConstants::PARTNER_ACTIVATE_MERCHANT                     => self::MerchantActivationByPartnerEnabled,
         APIFeaturesConstants::OVERRIDE_SUB_CONFIG                           => self::OverridingSubmerchantConfigEnabled,
+        APIFeaturesConstants::RAZORPAY_SECURE_MERCHANT => self::ShopifyPaymentsReport,
         APIFeaturesConstants::NO_DOC_ONBOARDING                             => self::NoDocOnboardingEnabled,
         APIFeaturesConstants::CREATE_SOURCE_V2                              => self::OnboardedViaV2ApiEnabled,
         APIFeaturesConstants::BLOCK_ONBOARDING_SMS                          => self::BlockSendingOnboardingSms,
