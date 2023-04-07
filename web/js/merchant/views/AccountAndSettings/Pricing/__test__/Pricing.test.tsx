083f76f1922c22dd5a3264502f438d2426eec2f7
Shivam Anand Murmu <35562703+Rozamo@users.noreply.github.com>
2023-04-07 13:29:54 +0530

    fix: remove useLocation and use createMemoryHistory for location creation (#12278)

--- web/js/merchant/views/AccountAndSettings/Pricing/__test__/Pricing.test.tsx
+++ web/js/merchant/views/AccountAndSettings/Pricing/__test__/Pricing.test.tsx
@@ -1,37 +1,36 @@
 import React from 'react';
 import { render, userEvent, screen, waitFor } from 'test-utils';
 import Pricing from 'merchant/views/AccountAndSettings/Pricing';
-import { createLocation } from 'history';
+import { createMemoryHistory } from 'history';
 import { ROUTES_INFO } from 'merchant/views/AccountAndSettings/typings/routes';
 
-let location;
+jest.mock(
+  'merchant/views/AccountAndSettings/Pricing/components/PricingPlans',
+  () => (): JSX.Element => {
+    return <div>Pricing Plans Component</div>;
+  },
+);
 
 describe('Tests for the Pricing page', () => {
-  beforeAll(() => {
-    location = createLocation(ROUTES_INFO.PRICING_PLANS);
-    location.push = jest.fn();
-  });
+  const renderApp = () => {
+    const history = createMemoryHistory({ initialEntries: [ROUTES_INFO.PRICING_PLANS] });
 
-  test('Location should change to pricing plans url when clicking the Pricing Plans tab', () => {
-    render(<Pricing location={location} />);
-
-    userEvent.click(screen.getByTestId('pricing-plan-link'));
-
-    expect(location.pathname).toBe(ROUTES_INFO.PRICING_PLANS);
-  });
-
-  test('Should show the Pricing Plans tab when in corresponding location', async () => {
-    jest.mock(
-      'merchant/views/AccountAndSettings/Pricing/components/PricingPlans',
-      () => (): JSX.Element => {
-        return <div>Pricing Plans Component</div>;
-      },
-    );
-
-    render(<Pricing location={location} />, {
+    return render(<Pricing location={history.location} />, {
       path: ROUTES_INFO.PRICING_PLANS,
       historyOptions: { initialEntries: [ROUTES_INFO.PRICING_PLANS] },
     });
+  };
+
+  test('Location should change to pricing plans url when clicking the Pricing Plans tab', () => {
+    const { history } = renderApp();
+
+    userEvent.click(screen.getByTestId('pricing-plan-link'));
+
+    expect(history?.location?.pathname).toBe(ROUTES_INFO.PRICING_PLANS);
+  });
+
+  test('Should show the Pricing Plans tab when in corresponding location', async () => {
+    renderApp();
 
     await waitFor(() => {
       expect(screen.getByText('Pricing Plans Component')).toBeInTheDocument();
