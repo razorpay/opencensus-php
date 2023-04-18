e272ba9101744b961260281b5e22d7822b4cb04d
Sumit Dhanania <sumitdhanania94@gmail.com>
2023-04-18 14:42:50 +0530

    test: added E2E case for loading partner dashboard (#12338)

---
+++ web/e2e/suites/partner-dashboard.spec.js
@@ -0,0 +1,18 @@
+const { test, expect } = require('@playwright/test');
+const { StorageStatePath } = require('../utils/constants');
+
+test.describe.parallel('Test partner dashboard landing page @flow=home', () => {
+  test.use({
+    storageState: StorageStatePath.EMAIL_TEST_LOGIN_STATE,
+  });
+  test('should load the partner dashboard @priority=critical', async ({ page }) => {
+    await page.goto('/app/dashboard');
+
+    await page.locator('text="Partner"').click();
+
+    const partnerDashboardWelcomeText = page.locator(
+      'text=Welcome to Partner dashboard, PlayWright Test Account!',
+    );
+    await expect(partnerDashboardWelcomeText).toBeVisible();
+  });
+});
