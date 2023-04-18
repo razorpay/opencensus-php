76b435f99c8839055c3bd575f77cd02e4a4419b6
santoshrohannarava <88091269+santoshrohannarava@users.noreply.github.com>
2023-04-18 21:03:47 +0530

    Changes to use new sms templates for Settlements (#37375)
    * Update Events.php
    
    * dummy commit
    
    ---------
    
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Notifications/Settlement/Events.php
+++ app/Notifications/Settlement/Events.php
@@ -9,12 +9,13 @@ class Events
     const FAILED        = 'FAILED';
 
     const SMS_TEMPLATES = [
-        self::PROCESSED     => 'sms.settlements.processed',
-        self::FAILED        => 'sms.settlements.failed',
+        self::PROCESSED     => 'sms.settlements.processed_new',
+        self::FAILED        => 'sms.settlements.failed_new',
     ];
 
     const WHATSAPP_TEMPLATES = [
         self::PROCESSED => 'A settlement of {amount} for your Razorpay account {merchant_id} for {date} has been deposited into your bank account ending with {bank_account_id}, Settlement Id is {settlement_id} and UTR is {utr}. Please login into your Razorpay dashboard for additional details at {url}.',
         self::FAILED    => 'We regret to inform you that the settlement for Razorpay merchant account {merchant_id} for {date} failed to be processed to your account: {bank_account_id} because of this error {failure_reason}, Please login into your Razorpay account and update your bank account details at {url}.',
     ];
+    
 }
