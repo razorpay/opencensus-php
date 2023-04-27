73f18cb0f00854aba59ac37d27d5b9da7ccb31b7
Anirban Mandal <mandaline97@gmail.com>
2023-04-27 08:42:45 +0530

    feat: csp for admin route (#12324)

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -268,7 +268,7 @@ Route::group(['middleware' => ['web']], function () {
 
     Route::get('razorx/{all?}', 'AdminController@getIndex')->name('razorx_catchall')->where(['all' => '.*']);
     Route::get('admin/capital-los/{all?}', 'AdminController@getIndex')->name('capital_catchall')->where(['all' => '.*']);
-    Route::get('admin/{all}', 'AdminController@getIndex')->name('admin_catchall')->where(['all' => '.*']);
+    Route::get('admin/{all}', 'AdminController@getIndex')->name('admin_catchall')->where(['all' => '.*'])->middleware(['set_x_frame', 'set_csp_header']);
 });
 
 Route::group(['middleware'  => 'graph'], function()
