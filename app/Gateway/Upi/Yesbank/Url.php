32c26686c1bc98c58d1feb86b4970f322edd6e02
Rahul Gupta <104202591+rahulg983355@users.noreply.github.com>
2023-05-05 10:28:45 +0530

    updated the YB URL (#37626)
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Gateway/Upi/Yesbank/Url.php
+++ app/Gateway/Upi/Yesbank/Url.php
@@ -4,8 +4,8 @@ namespace RZP\Gateway\Upi\Yesbank;
 
 class Url
 {
-    const LIVE_DOMAIN          = 'https://sky.yesbank.in:444/app/live/upi/';
-    const TEST_DOMAIN          = 'https://uatsky.yesbank.in:444/app/uat/upi/';
+    const LIVE_DOMAIN          = 'https://skyway.yesbank.in:443/app/live/upi/';
+    const TEST_DOMAIN          = 'https://uatskyway.yesbank.in:443/app/uat/upi/';
 
     const PAYOUT               = 'mePayServerReqImp';
     const PAYOUT_VERIFY        = 'meTransStatusQuery';
