3229aa1c4b93cd55d76ba345c086a8980c274bac
shubhamtyagii13 <84892433+shubhamtyagii13@users.noreply.github.com>
2023-05-31 17:14:41 +0530

    added $isThreeDecimalCurrencySupported flag in getAllCurrency function (#38161)
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -47,7 +47,16 @@ class CurrencyController extends Controller
      */
     public function getAllCurrency()
     {
-        $data = (new Currency\Core)->getSupportedCurrenciesDetails();
+        /*
+         * Added isThreeDecimalCurrencySupported to true, to return three decimal currencies
+         * as well in response of supported currency in "currency_fetch_all_proxy" endpoint.
+         * We have a feature flag 'SHAADI_COM_NEW_CURRENCY' restrictions for merchants to return data in DCC flow in @getSupportedCurrenciesDetails function.
+         * This is required as we will be supporting n exponent currencies at frontend in merchant dashboard.
+         * Note: this flag will be removed once we remove the feature flag dependency.
+         */
+
+        $isThreeDecimalCurrencySupported = true;
+        $data = (new Currency\Core)->getSupportedCurrenciesDetails($isThreeDecimalCurrencySupported);
 
         return ApiResponse::json($data);
     }
