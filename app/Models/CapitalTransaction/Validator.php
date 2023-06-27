20f9808e2491214e9e5933b65a3f69926285a2e8
Apoorv Agrawal <44508548+apoorv679@users.noreply.github.com>
2023-06-27 18:43:15 +0530

    add new capital txn type: `interest_waiver` (#38706)
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Models/CapitalTransaction/Validator.php
+++ app/Models/CapitalTransaction/Validator.php
@@ -7,7 +7,7 @@ class Validator
     public static $createTransactionInput = [
         Entity::ID          => 'required|alpha_num|size:14',
         Entity::AMOUNT      => 'required|int',
-        Entity::TYPE        => 'required|string|in:repayment_breakup,installment,charge',
+        Entity::TYPE        => 'required|string|in:repayment_breakup,installment,charge,interest_waiver',
         Entity::CURRENCY    => 'required|string|in:INR',
         Entity::MERCHANT_ID => 'required|alpha_num|size:14',
         Entity::BALANCE_ID  => 'required|alpha_num|size:14',
