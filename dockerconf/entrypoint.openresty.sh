809993f09d7968fc321bc39abd561e20daacd983
Manjunatha Shetty H <manjunatha.h@razorpay.com>
2023-06-30 20:40:05 +0530

    feat: removing openresty from CI (#38427)
    * feat: removing openresty from CI
    
    * merge fixes
    
    * fixed module check script
    
    ---------
    
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- dockerconf/entrypoint.openresty.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-alohomora cast --region ap-south-1 --env "$APP_MODE" --app api "/usr/local/openresty/nginx/sites/api.openresty.conf.j2"
-
-#removing as sites only need valid files
-rm /usr/local/openresty/nginx/sites/api.openresty.conf.j2
-
-nginx -g "daemon off;"
