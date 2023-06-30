caebea7ae2924b92db02cbb9a9d126a91e96e422
prem-razorpay <116806144+prem-razorpay@users.noreply.github.com>
2023-06-30 18:45:22 +0530

    segregating ut for rx and razorpay Test Suite (#38180)
    * segregating ut for rx and razorpay Test Suite
    
    * segregating ut for rx and razorpay Test Suite
    
    * segregating ut for rx and razorpay Test Suite
    
    * segregating ut for rx and razorpay Test Suite
    
    * segregating ut for rx and razorpay Test Suite
    
    * fix: splitz mock not enabled by default
    
    * changing redis timeout
    
    * changing redis host and port v2 in workflow
    
    ---------
    
    Co-authored-by: Zahle Khan <zahle.khan@razorpay.com>
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

---
+++ ci-assets/razorpayx_test-2_suite.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+TEST_SUITE_STATUS=0
+log_file_name="test-output.log"
+export START_TIME=$(date +%s)
+echo "start-time: ${START_TIME}"
+SUITE_NAME="RazorpayX Test-2 Suite" php vendor/phpunit/phpunit/phpunit -d memory_limit=4096M --testsuite "RazorpayX Test-2 Suite" >> $log_file_name
+export TEST_SUITE_STATUS=$((TEST_SUITE_STATUS + $?))
+export END_TIME=$(date +%s)
+echo "end-time: ${END_TIME}"
+cat $log_file_name
+echo "${START_TIME},${END_TIME},${TEST_SUITE_STATUS}" >> utMetrics.csv
+exit $TEST_SUITE_STATUS
