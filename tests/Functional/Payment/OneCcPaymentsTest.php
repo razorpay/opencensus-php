3f5e534dedae83aa241010ed80cee9385a2119ee
Gaurav Singhal <42150487+Gauravsinghal09@users.noreply.github.com>
2023-08-08 10:57:23 +0530

    adds magic FTs suite (#39517)
    * adds magic FTs sutie
    
    * refactor
    
    * merging master
    
    * segregate magic FTs
    
    * segregate magic FTs
    
    * update syntax
    
    ---------
    
    Co-authored-by: prem <prem.dhan@razorpay.com>
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

---
+++ tests/Functional/Payment/OneCcPaymentsTest.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace Functional\Payment;
+
+use RZP\Tests\Traits\MocksSplitz;
+use RZP\Tests\Functional\TestCase;
+use Illuminate\Support\Facades\App;
+use RZP\Exception\BadRequestException;
+use RZP\Tests\Functional\OAuth\OAuthTrait;
+use RZP\Tests\Functional\Partner\PartnerTrait;
+use RZP\Tests\Functional\Helpers\TerminalTrait;
+use RZP\Tests\Functional\Invoice\InvoiceTestTrait;
+use RZP\Tests\Functional\Helpers\PaymentsUpiTrait;
+use RZP\Models\Feature\Constants as FeatureConstants;
+use RZP\Tests\Functional\Helpers\DbEntityFetchTrait;
+use RZP\Tests\Functional\Helpers\Payment\PaymentTrait;
+use RZP\Tests\Functional\Helpers\Heimdall\HeimdallTrait;
+use Razorpay\Edge\Passport\Tests\GeneratesTestPassportJwts;
+
+class OneCcPaymentsTest extends TestCase
+{
+    use OAuthTrait;
+    use MocksSplitz;
+    use PartnerTrait;
+    use PaymentTrait;
+    use DbEntityFetchTrait;
+    use InvoiceTestTrait;
+    use TerminalTrait;
+    use HeimdallTrait;
+    use GeneratesTestPassportJwts;
+    use PaymentsUpiTrait;
+
+    protected function setUp(): void
+    {
+        $this->testDataFilePath = __DIR__.'/helpers/PaymentCreateTestData.php';
+
+        parent::setUp();
+
+        $this->ba->publicAuth();
+
+        $this->payment = $this->getDefaultPaymentArray();
+
+        $this->sharedTerminal = $this->fixtures->create('terminal:shared_sharp_terminal');
+        $this->mandateHqTerminal = $this->fixtures->create('terminal:shared_mandate_hq_terminal');
+        $this->fixtures->create('terminal:disable_default_hdfc_terminal');
+    }
+
+    protected function getOrderMetaValue()
+    {
+        $app = App::getFacadeRoot();
+        $shipping_address = [
+            'line1'         => 'some line one',
+            'line2'         => 'some line two',
+            'city'          => 'Bangalore',
+            'state'         => 'Karnataka',
+            'zipcode'       => '560001',
+            'country'       => 'in',
+            'type'          => 'shipping_address',
+            'primary'       => true
+        ];
+        $billing_address = [
+            'line1'         => 'some line one',
+            'line2'         => 'some line two',
+            'city'          => 'Bangalore',
+            'state'         => 'Karnataka',
+            'zipcode'       => '560001',
+            'country'       => 'in',
+            'type'          => 'billing_address',
+            'primary'       => true
+        ];
+        $customer = [
+            'contact'           =>'+9191111111111',
+            'email'             =>'john.doe@razorpay.com',
+            'shipping_address'  =>$shipping_address,
+            'billing_address'   =>$billing_address
+
+        ];
+        return [
+            'cod_fee'           => 100000,
+            'net_price'         => 1100000,
+            'sub_total'         => 1100000,
+            'shipping_fee'      => 10000,
+            'customer_details'  => $app['encrypter']->encrypt($customer),
+            'line_items_total'  => 1000000,
+        ];
+    }
+
+
+    public function test1CCOrderPaymentsWithCustomerDeatils(){
+        $this->fixtures->merchant->addFeatures(FeatureConstants::ONE_CLICK_CHECKOUT);
+        $order = $this->fixtures->order->create(['receipt' => 'receipt']);
+        $this->fixtures->create('order_meta',
+            [
+                'order_id' => $order->getId(),
+                'value'    => $this->getOrderMetaValue(),
+                'type'     => 'one_click_checkout',
+            ]);
+        $this->ba->publicAuth();
+
+        $testData = $this->testData[__FUNCTION__];
+        $payment = $this->getDefaultPaymentArray();
+        $payment["order_id"] = 'order_'.$order->getId();
+        $payment["amount"] = $order->getAmount();
+        $testData['request']['content'] = $payment;
+
+        $response = $this->makeRequestParent($testData['request']);
+
+        $this->processAndAssertStatusCode($testData, $response);
+        $this->processAndAssertResponseData($testData, $response);
+    }
+
+    public function test1CCOrderPaymentsWithoutCustomerDeatils()
+    {
+        $this->fixtures->merchant->addFeatures(FeatureConstants::ONE_CLICK_CHECKOUT);
+        $order = $this->fixtures->order->create(['receipt' => 'receipt']);
+        $this->fixtures->create('order_meta',
+            [
+                'order_id' => $order->getId(),
+                'value' => [
+                    'line_items_total' => $order->getAmount(),
+                    "cod_fee" => 0,
+                    "shipping_fee" => 0,
+                ],
+                'type' => 'one_click_checkout',
+            ]);
+        $this->ba->publicAuth();
+        $testData = $this->testData[__FUNCTION__];
+        $payment = $this->getDefaultPaymentArray();
+        $payment["order_id"] = 'order_' . $order->getId();
+        $payment["amount"] = $order->getAmount();
+        $testData['request']['content'] = $payment;
+
+        $this->expectException(BadRequestException::class);
+        $this->expectExceptionMessage('Something went wrong, please try again after sometime.');
+
+        $response = $this->makeRequestParent($testData['request']);
+
+        $this->processAndAssertStatusCode($testData, $response);
+        $this->processAndAssertResponseData($testData, $response);
+    }
+}
