86b42fe1a31d17d82e818ec799f89831121df763
sameeksha2879 <103935648+sameeksha2879@users.noreply.github.com>
2023-08-14 10:59:10 +0530

    remove banks from esign aadhaar list (#39649)
    * removed psib from aadhaar list
    
    * removed esaf,finf,sury from esign emandate
    
    * fixed test case
    
    ---------
    
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- tests/Functional/Gateway/Enach/Rbl/EnachRblGatewayTest.php
+++ tests/Functional/Gateway/Enach/Rbl/EnachRblGatewayTest.php
@@ -1307,7 +1307,7 @@ class EnachRblGatewayTest extends TestCase
             }
         }
 
-        $this->assertEquals(378, $count);
+        $this->assertEquals(374, $count);
 
         $this->assertArrayNotHasKey(IFSC::UTBI, $banks);
     }
