18de32fa85df67ffacfca00d857171b85afcd4c5
Mohammad Arif <87689341+md-arif-rzp@users.noreply.github.com>
2023-09-05 11:34:13 +0530

    Bugfix internal notes validator update (#40018)
    * avoid emoji's for internal notes
    
    * dummy commit
    
    * dummy commit
    
    * updating regex
    
    * updating regex
    
    ---------
    
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Models/Merchant/Detail/Validator.php
+++ app/Models/Merchant/Detail/Validator.php
@@ -265,7 +265,7 @@ class Validator extends Base\Validator
         Entity::CLARIFICATION_MODE                       => 'sometimes|max:15',
         Entity::ISSUE_FIELDS                             => 'sometimes|string',
         Entity::ISSUE_FIELDS_REASON                      => 'sometimes|string',
-        Entity::INTERNAL_NOTES                           => 'sometimes|string',
+        Entity::INTERNAL_NOTES                           => 'sometimes|string|regex:/^[\w\d\s"!@#$=%^&*(\/_,:;.), -\]]+$/',
         Entity::ACTIVATION_FLOW                          => 'sometimes|custom',
         Entity::INTERNATIONAL_ACTIVATION_FLOW            => 'sometimes|custom',
         Entity::CUSTOM_FIELDS                            => 'filled|array',
