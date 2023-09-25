6b905e0a820420c085fa3e7c24639931ae45590b
AnshulJS <anshul.nautiyal@razorpay.com>
2023-09-25 13:56:31 +0530

    feat: add mid on checkout payment in pricing bundle for Desktop & Mobile (#13397)
    * feat: add mid on checkout payment in pricing bundle
    
    * feat: destructure
    
    * feat: add mercahnt id in notes object
    
    * feat: add safe check for object destrucutre

--- web/js/common/ui/PricingSubscription/PricingBundleCommon.tsx
+++ web/js/common/ui/PricingSubscription/PricingBundleCommon.tsx
@@ -345,9 +345,13 @@ const handleCheckoutPayment =
         const { subscription = {} } = response;
         if (type === PAYMENT_TYPE.PG || !type) {
           // TODO: remove `!type` when add this feature in Mobile
+          const { merchant_id, account_key, payment_subscription_id } = subscription || {};
           const options = {
-            key: subscription?.account_key,
-            subscription_id: subscription?.payment_subscription_id,
+            notes: {
+              merchant_id,
+            },
+            key: account_key,
+            subscription_id: payment_subscription_id,
             name: `Razorpay Pricing Package`,
             description: '18% GST included',
             image: rzpLogo,
