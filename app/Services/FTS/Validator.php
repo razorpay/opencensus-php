fff91496aa7a6a5f0792183c9b846ed48077051a
varchasva-tiwari <varchasva.tiwari@razorpay.com>
2023-09-25 18:57:19 +0530

    Add IDFC channel for FTS SA creation (#40419)
    * Add IDFC channel for FTS SA creation
    
    * Added v5 for bulk

--- app/Services/FTS/Validator.php
+++ app/Services/FTS/Validator.php
@@ -13,10 +13,10 @@ class Validator extends Base\Validator
       'type'                        => 'filled|string|required|in:bank_account,banking_account',
       'config'                      => 'required|array',
       'product'                     => 'filled|string|required|in:payout,refund,payout_refund,settlement,penny_testing,ca_payout,es_on_demand',
-      'channel'                     => 'filled|string|required|in:yesbank,icici,citi,m2p,axis,rbl,amazon_pay',
+      'channel'                     => 'filled|string|required|in:yesbank,icici,citi,m2p,axis,rbl,amazon_pay,idfc',
       'credentials'                 => 'required|array',
       'fund_account_id'             => 'sometimes|integer',
-      'mozartIdentifier'            => 'filled|string|required|in:V1,V2,V3,V4',
+      'mozartIdentifier'            => 'filled|string|required|in:V1,V2,V3,V4,V5',
       'sourceAccountType'           => 'filled|string|required|in:current,nodal',
       'sourceAccountTypeIdentifier' => 'sometimes|string|in:pool,direct',
     ];
