fb03a29fa4e947d400e988145b3f221a0bbe2cb8
Rajeev Rai <rajeev.rai@razorpay.com>
2023-09-26 12:54:12 +0530

    Breaking PHP validation step (#40428)
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- ci-assets/validate_php_syntax.sh
+++ ci-assets/validate_php_syntax.sh
@@ -1,1 +1,9 @@
-find -path ./vendor -prune -o -iname '*.php'|xargs -n1 php -l
+#!/bin/sh
+
+app="app"
+
+if [ "$1" = "$app" ]; then
+  find . -type f -iname "*.php" | grep "./app/" | xargs -n1 php -l
+else
+  find . -type f -iname "*.php" | grep -v "./app/" | xargs -n1 php -l
+fi
