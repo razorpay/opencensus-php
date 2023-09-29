bb7edd6217c38ec42f5624677da3cfd06bc38e7f
Shubham kumar <51047960+shubham25kr07@users.noreply.github.com>
2023-09-29 17:42:19 +0530

    added traces to debug payment count mismatch issue (#40496)
    * added traces to debug payment count mismatch issue
    
    * changed to array get
    
    * renamed trace code
    
    ---------
    
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Models/PaymentLink/Core.php
+++ app/Models/PaymentLink/Core.php
@@ -1593,7 +1593,12 @@ class Core extends Base\Core
         {
             $existingComputedSettings = $entity->getComputedSettings()->toArray();
         }
-
+    
+        $this->trace->info(TraceCode::PAYMENT_LINK_UPDATE_PAYMENT_CAPTURE_DEBUG, [
+            'count'                            => $count,
+            'existing_captured_payments_count' => array_get($existingComputedSettings, Entity::CAPTURED_PAYMENTS_COUNT, 0)
+        ]);
+        
         $existingComputedSettings[Entity::CAPTURED_PAYMENTS_COUNT] = $count;
 
         $entity->getComputedSettingsAccessor()->upsert($existingComputedSettings)->save();
