856abc96f4b4ae3735198494294f348c3607f45f
ShivamKSRazorpay <103921926+ShivamKSRazorpay@users.noreply.github.com>
2023-11-14 15:23:09 +0530

    fix: [SSAB-4287] sentry issue - add Array some polyfill (#13646)
    * fix: add Array some polyfill
    
    * docs: add ref link
    
    * chore: fix eslint issues
    
    * chore: fix lint issue

--- web/js/common/utils/polyfills.js
+++ web/js/common/utils/polyfills.js
@@ -1,22 +1,25 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/startsWith
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
 if (!String.prototype.startsWith) {
-  String.prototype.startsWith = function(searchString, position) {
+  // eslint-disable-next-line func-names, no-extend-native
+  String.prototype.startsWith = function (searchString, position) {
     return this.substr(position || 0, searchString.length) === searchString;
   };
 
-  String.prototype.endsWith = function(searchStr, Position) {
+  // eslint-disable-next-line func-names, no-extend-native
+  String.prototype.endsWith = function (searchStr, Position) {
     if (!(Position < this.length)) Position = this.length;
+    // eslint-disable-next-line no-bitwise
     else Position |= 0; // round position
-    return (
-      this.substr(Position - searchStr.length, searchStr.length) === searchStr
-    );
+    return this.substr(Position - searchStr.length, searchStr.length) === searchStr;
   };
 }
 
 // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
 if (!String.prototype.endsWith) {
-  String.prototype.endsWith = function(searchString, position) {
+  // eslint-disable-next-line func-names, no-extend-native
+  String.prototype.endsWith = function (searchString, position) {
+    // eslint-disable-next-line no-var
     var subjectString = this.toString();
     if (
       typeof position !== 'number' ||
@@ -27,13 +30,15 @@ if (!String.prototype.endsWith) {
       position = subjectString.length;
     }
     position -= searchString.length;
+    // eslint-disable-next-line no-var, vars-on-top
     var lastIndex = subjectString.lastIndexOf(searchString, position);
     return lastIndex !== -1 && lastIndex === position;
   };
 }
 
 if (!String.prototype.includes) {
-  String.prototype.includes = function(search, start) {
+  // eslint-disable-next-line func-names, no-extend-native
+  String.prototype.includes = function (search, start) {
     if (typeof start !== 'number') {
       start = 0;
     }
@@ -48,22 +53,28 @@ if (!String.prototype.includes) {
 // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/findIndex
 
 if (!Array.prototype.findIndex) {
+  // eslint-disable-next-line no-extend-native
   Object.defineProperty(Array.prototype, 'findIndex', {
-    value: function(predicate) {
+    // eslint-disable-next-line func-names
+    value: function (predicate) {
+      // eslint-disable-next-line strict
       'use strict';
       if (this == null) {
-        throw new TypeError(
-          'Array.prototype.findIndex called on null or undefined'
-        );
+        throw new TypeError('Array.prototype.findIndex called on null or undefined');
       }
       if (typeof predicate !== 'function') {
         throw new TypeError('predicate must be a function');
       }
+      // eslint-disable-next-line no-var, vars-on-top
       var list = Object(this);
+      // eslint-disable-next-line no-var, vars-on-top, no-bitwise
       var length = list.length >>> 0;
+      // eslint-disable-next-line no-var, vars-on-top, prefer-rest-params
       var thisArg = arguments[1];
+      // eslint-disable-next-line no-var, vars-on-top
       var value;
 
+      // eslint-disable-next-line no-var, vars-on-top
       for (var i = 0; i < length; i++) {
         value = list[i];
         if (predicate.call(thisArg, value, i, list)) {
@@ -78,14 +89,30 @@ if (!Array.prototype.findIndex) {
   });
 }
 
+// https://medium.com/@imranshaikh_55168/javascript-array-polyfills-how-to-create-polyfills-in-js-a731072e5090
+if (!Array.prototype.some) {
+  // eslint-disable-next-line func-names, no-extend-native
+  Array.prototype.some = function (callback) {
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        return true;
+      }
+    }
+    return false;
+  };
+}
+
 // https://developer.mozilla.org/en-US/docs/Web/API/Element/closest#Polyfill
 if (window.Element && !Element.prototype.closest) {
-  Element.prototype.closest = function(s) {
+  // eslint-disable-next-line func-names, no-extend-native
+  Element.prototype.closest = function (s) {
+    // eslint-disable-next-line no-var, one-var
     var matches = (this.document || this.ownerDocument).querySelectorAll(s),
       i,
       el = this;
     do {
       i = matches.length;
+      // eslint-disable-next-line no-empty
       while (--i >= 0 && matches.item(i) !== el) {}
     } while (i < 0 && (el = el.parentElement));
     return el;
@@ -93,15 +120,12 @@ if (window.Element && !Element.prototype.closest) {
 }
 
 if (typeof window.CustomEvent !== 'function') {
+  // eslint-disable-next-line no-inner-declarations
   function CustomEvent(event, params) {
     params = params || { bubbles: false, cancelable: false, detail: undefined };
+    // eslint-disable-next-line no-var, vars-on-top
     var evt = document.createEvent('CustomEvent');
-    evt.initCustomEvent(
-      event,
-      params.bubbles,
-      params.cancelable,
-      params.detail
-    );
+    evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
     return evt;
   }
 
