907d8498b665cc365e9b9604f16d86386250f8eb
srikar <51258352+srikar-komanduri@users.noreply.github.com>
2023-11-14 18:41:20 +0530

    Send files in bvt payload (#41268)
    * Revert "Revert "Start sending files changed in BVT payload (#41193)" (#41257)"
    
    This reverts commit b6d7557527efdae49010ff10798c8faa4420777b.
    
    * Fixed the mistake by sending array instead of string
    
    * Remove testing related changes
    
    * Add condition if files changed are empty
    
    ---------
    
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- .github/actions/triggerDevstack.sh
+++ .github/actions/triggerDevstack.sh
@@ -7,6 +7,7 @@ run_devstack() {
   roastPRCommit=${ROAST_PR_COMMIT}
   WEBHOOK_TRIGGER=${WEBHOOK_TRIGGER}
   PIPELINE_ID="51ab409e-1ce1-4c59-ae13-f702c02a9c4a"
+  FILES_CHANGED=${FILES_CHANGED}
 
   statusCode=$(curl -c /tmp/cookies -o -s -w "%{http_code}" --location --request GET 'https://deploy-api.razorpay.com/login' \
     --header "Authorization: Bearer ${GIT_TOKEN}")
@@ -56,11 +57,16 @@ run_devstack() {
   fi
   echo "Triggering webhook for BVT testing execution for :" + "$commitId"
   echo "Triggering webhook for BVT testing execution for Roast PR :" + "$roastPRCommit"
+  echo "files = " + $FILES_CHANGED
+  if [ -z "$FILES_CHANGED" ]; then
+      echo "Files changed are empty. setting it to null"
+      FILES_CHANGED=null
+  fi
   curl -X POST \
     -u github-actions:"$SPINNAKER_PASSWORD" \
     https://deploy-github-actions.razorpay.com/webhooks/webhook/"$WEBHOOK_TRIGGER" \
     -H "content-type: application/json" \
-    -d "{\"review\":{\"state\":\"approved\", \"skip_roast\":\"$skipDevstack\", \"roast_commit_id\":\"$roastPRCommit\"},\"pull_request\":{\"head\":{ \"sha\":\"$commitId\"},\"number\":\"$PRNumber\",\"state\":\"approved\"}}"
+    -d "{\"review\":{\"state\":\"approved\", \"skip_roast\":\"$skipDevstack\", \"roast_commit_id\":\"$roastPRCommit\"},\"pull_request\":{\"head\":{ \"sha\":\"$commitId\"},\"number\":\"$PRNumber\",\"state\":\"approved\"},\"files\":$FILES_CHANGED}"
 }
 
 run_devstack
