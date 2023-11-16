74c390d59e9ca90df6c5bf84e83fbb8340f50eed
nitin meena <40690661+nitml@users.noreply.github.com>
2023-11-16 16:08:04 +0530

    DCS and API feature merging response fix revert (#41324)
    DCS and API feature merging response fix revert

--- app/Models/Feature/Repository.php
+++ app/Models/Feature/Repository.php
@@ -50,18 +50,8 @@ class Repository extends Base\Repository
 
         $dcs = $this->app['dcs'];
         $dcsResponse = $dcs->getDcsEnabledFeatures($entityType, $entityId, $mode);
-        return $this->mergeUniqueArrays($apiResponse, $dcsResponse);
-    }
 
-    private function mergeUniqueArrays($arr1, $arr2){
-        foreach ($arr2 as $element)
-        {
-            if (!in_array($element, $arr1))
-            {
-                $arr1[] = $element;
-            }
-        }
-        return $arr1;
+        return $apiResponse->concat($dcsResponse)->unique(Entity::NAME);
     }
 
     public function findByEntityTypeEntityIdAndNameOrFail(string $entityType, string $entityId, string $featureName)
