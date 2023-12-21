533e64db256e068d10e0775277eb12c6ca0c76e5
Smile Gupta <smile.gupta@razorpay.com>
2023-12-21 14:42:38 +0530

    [MCX-3214] fix(magic checkout): fixed the issue for persistence in case of native checkout (#13863)
    * fix: fixed the issue for presistance in case of native checkout
    
    * feat: removed the comments and optimised the peice more
    
    * feat: added the fallback fir existing merchants
    
    * feat: removed the debugger and bracket issue
    
    * chors: removed break in default

--- web/js/merchant/views/MagicCheckout/MagicSettings/containers/native/ShippingWrapper.js
+++ web/js/merchant/views/MagicCheckout/MagicSettings/containers/native/ShippingWrapper.js
@@ -1,18 +1,41 @@
 import { useState } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { NATIVE_SHIPPING_VIEWS } from 'merchant/views/MagicCheckout/MagicSettings/constants';
 import NativeShippingTab from 'merchant/views/MagicCheckout/MagicSettings/components/native/ShippingTab';
 import NativeShippingAPI from 'merchant/views/MagicCheckout/MagicSettings/containers/native/ShippingAPI';
 import ShipRocketSettings from 'merchant/views/MagicCheckout/ShippingServices';
 import { SHIPPING_PARTNERS } from 'merchant/views/MagicCheckout/ShippingServices/constants';
 import SuspenseWithLoader from 'common/new-ui/SuspenseWithLoader';
+import { updateMagicSettings } from 'merchant/reducers/magicCheckout/magicSettings/actions';
 
-const NativeShippingTabWrapper = ({ settings }) => {
+const NativeShippingTabWrapper = ({ settings, updateSettings }) => {
   let defaultView = NATIVE_SHIPPING_VIEWS.PROVIDER_SELECTION;
-  if (settings.shipping_info) {
+
+  switch (settings.shipping_source) {
+    case NATIVE_SHIPPING_VIEWS.API:
+      defaultView = NATIVE_SHIPPING_VIEWS.API;
+      break;
+    case NATIVE_SHIPPING_VIEWS.SHIPROCKET:
+      defaultView = NATIVE_SHIPPING_VIEWS.SHIPROCKET;
+      break;
+    default:
+  }
+
+  // this is fallback for the exisiting native merchants
+  if (!settings?.shipping_source && settings.shipping_info) {
     defaultView = NATIVE_SHIPPING_VIEWS.API;
   }
+
   const [view, setView] = useState(defaultView);
+
+  const onSave = (shipping_source) => {
+    setView(shipping_source);
+    const payload = {
+      shipping_source,
+    };
+    updateSettings(payload, false);
+  };
 
   const switchToProviderSelector = () => setView(NATIVE_SHIPPING_VIEWS.PROVIDER_SELECTION);
   let Component;
@@ -20,7 +43,7 @@ const NativeShippingTabWrapper = ({ settings }) => {
     return (
       <NativeShippingTab
         onNext={(view) => {
-          setView(view);
+          onSave(view);
         }}
       />
     );
@@ -59,4 +82,12 @@ const mapStateToProps = (state) => ({
   settings: state.magic_settings,
 });
 
-export default connect(mapStateToProps, null)(NativeShippingTabWrapper);
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators(
+    {
+      updateSettings: updateMagicSettings,
+    },
+    dispatch,
+  );
+
+export default connect(mapStateToProps, mapDispatchToProps)(NativeShippingTabWrapper);
