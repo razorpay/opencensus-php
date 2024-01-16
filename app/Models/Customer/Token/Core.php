f7d6927ae8d29a6fb4064bb9610238f8182ae0ff
parthanand31-sudo <66663796+parthanand31-sudo@users.noreply.github.com>
2024-01-16 11:17:47 +0530

    [SI-14567] added a check for rupay where arn is mandatory (#42194)
    * added a check for rupay where arn is mandatory
    
    * updated to empty condition
    
    * added logs
    
    * removed loggers
    
    * removed loggers
    
    * added logs
    
    ---------
    
    Co-authored-by: Nishant Passari <nishant.passari@razorpay.com>

--- app/Models/Customer/Token/Core.php
+++ app/Models/Customer/Token/Core.php
@@ -1990,10 +1990,25 @@ class Core extends Base\Core
 
         (new Validator)->validateInput(Validator::CREATE_NETWORK_CARD, $input[Entity::CARD]);
 
+        $this->trace->info(
+            TraceCode::CARD_NOT_ELIGIBLE_FOR_TOKENISATION_TEST,
+            [
+                'input'=> $input
+            ]
+        );
         if ($this->isNetworkRuPay($input[Entity::CARD]))
         {
             (new Validator)->validateInput(Validator::CREATE_NETWORK_TOKEN_RUPAY, $input);
+
+            if (isset($input['via_push_provisioning']) === false)
+            {
+                if (empty($input[Token\Entity::AUTHENTICATION]['authentication_reference_number']) === true)
+                {
+                    throw new Exception\BadRequestException(ErrorCode::BAD_REQUEST_CARD_NOT_ELIGIBLE_FOR_TOKENISATION);
+                }
+            }
         }
+
         else if (empty($input[Token\Entity::AUTHENTICATION]) === false)
         {
             (new Validator)->validateInput(Validator::CREATE_NETWORK_TOKEN_AUTHENTICAION_DATA, $input[Token\Entity::AUTHENTICATION]);
