b236ad51a84ac846dfd05404eb622dc141a25787
Pankaj Ahuja <9896838+pankaj-a@users.noreply.github.com>
2024-02-20 06:03:40 +0530

    Add internal_payment_pricing for charge_collections_internal application (#42669)
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Http/Route.php
+++ app/Http/Route.php
@@ -17225,6 +17225,7 @@ class Route
 
         'charge_collections_internal' => [
             'cc_create_transaction',
+            'internal_payment_pricing',
         ],
 
         'affordability' => [
