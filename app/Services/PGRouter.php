217c81bcf6cddd2610ba76acfc2844325cf4329a
vishwa-razor <vishwanath.sharma@razorpay.com>
2024-03-22 13:26:26 +0530

    fix

--- app/Services/PGRouter.php
+++ app/Services/PGRouter.php
@@ -172,10 +172,10 @@ class PGRouter
 
         $this->mode = (isset($app['rzp.mode']) === true) ? $app['rzp.mode'] : Mode::LIVE;
 
-        if ($this->mode == Mode::TEST) {
-            $this->config = $app['config']->get('applications.pg_router_test');
-        } else {
+        if ($this->mode == Mode::LIVE) {
             $this->config = $app['config']->get('applications.pg_router');
+        } else {
+            $this->config = $app['config']->get('applications.pg_router_test');
         }
 
         $this->baseUrl = $this->config['url'];
