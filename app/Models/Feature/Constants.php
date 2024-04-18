590054047684ac5a4407b1f12db8390d7d8cf4ae
Vinamra Jain <vinamra.jain@razorpay.com>
2024-04-18 22:48:50 +0530

    Support feature flag for line_items (#43553)

--- app/Models/Feature/Constants.php
+++ app/Models/Feature/Constants.php
@@ -76,6 +76,7 @@ class Constants
     const FSS_RISK_UDF                    = 'fss_risk_udf';
     const RULE_FILTER                     = 'rule_filter';
     const TPV                             = 'tpv';
+    const ENABLE_LINE_ITEMS               = 'enable_line_items';
     const IRCTC_REPORT                    = 'irctc_report';
     const DISABLE_MAESTRO                 = 'disable_maestro';
     const DISABLE_RUPAY                   = 'disable_rupay';
@@ -2925,6 +2926,7 @@ class Constants
         self::RewardPoints                           => true,
         self::SHOW_INTL_RISK_ANALYTICS_DASHBOARD          => true,
         self::EXTERNAL_PA_VAS                        =>  true,
+        self::ENABLE_LINE_ITEMS                      => true,
     ];
 
     // Entity type constants
