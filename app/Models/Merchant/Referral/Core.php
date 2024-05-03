8c98fbc9581c77bf43803a9fcd6c05275d44b45b
Swati Kanchan <swati.kanchan@razorpay.com>
2024-05-03 17:12:14 +0530

    PRTS-3513 Adding easy-onboarding flag for POS referral links (#43834)
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Models/Merchant/Referral/Core.php
+++ app/Models/Merchant/Referral/Core.php
@@ -373,6 +373,7 @@ class Core extends Base\Core
         {
             // setting easy type for easy redirection for on-boarding
             $productConfig[Product::PRIMARY]["params"][self::EASY_ONBOARDING_TYPE_PARAM] = "1";
+            $productConfig[Product::POS]["params"][self::EASY_ONBOARDING_TYPE_PARAM] = "1";
         }
 
     }
