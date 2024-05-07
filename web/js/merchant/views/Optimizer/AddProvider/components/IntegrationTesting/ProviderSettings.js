e7deb3ae63c5329f0229b18597f059dee98d3780
Parth <71763920+parth-p-ui@users.noreply.github.com>
2024-05-07 20:16:14 +0530

    [OP-1023] feat: provider settings screen component (#14658)
    * feat: add provider settings screen
    
    * test: add wallets and sodexo UTs

---
+++ web/js/merchant/views/Optimizer/AddProvider/components/IntegrationTesting/ProviderSettings.js
@@ -0,0 +1,151 @@
+import React, { useState } from 'react';
+import {
+  Box,
+  Heading,
+  Text,
+  Switch,
+  Divider,
+  CheckboxGroup,
+  Checkbox,
+} from '@razorpay/blade/components';
+
+import { METHODS_MAP } from 'merchant/views/Navigator/constants';
+
+export const ProviderSettings = ({
+  providerName,
+  gatewayMetaData,
+  methods,
+  setMethods,
+  integrationType,
+}) => {
+  const methodsList = gatewayMetaData?.['Payment Methods']?.data_value || [];
+  // Move wallet method at the end
+  if (methodsList.includes('wallet')) {
+    methodsList.splice(methodsList.indexOf('wallet'), 1);
+    methodsList.push('wallet');
+  }
+  // Add sodexo method for PayU gateway
+  if (gatewayMetaData?.Sodexo && !methodsList.includes('sodexo') && integrationType === 's2s') {
+    methodsList.splice(methodsList.indexOf('card') + 1, 0, 'sodexo');
+  }
+  const wallets = gatewayMetaData?.['Payment Methods']?.meta_data?.wallet_metadata?.wallets || [];
+  const wallets1List = wallets?.slice(
+    0,
+    wallets?.length % 2 === 0 ? wallets?.length / 2 : wallets?.length / 2 + 1,
+  );
+  const wallets2List = wallets?.slice(
+    wallets?.length % 2 === 0 ? wallets?.length / 2 : wallets?.length / 2 + 1,
+  );
+
+  // To show wallets in 2 different checkboxgroups in 2 columns
+  const [wallets1, setWallets1] = useState(wallets1List);
+  const [wallets2, setWallets2] = useState(wallets2List);
+
+  const showWallets = !!methods.wallet;
+
+  const changeMethods = ({ isChecked, value }) => {
+    let wallet_metadata = {};
+    if (value === 'wallet' && isChecked) {
+      setWallets1(wallets1List);
+      setWallets2(wallets2List);
+      wallet_metadata = {
+        wallets,
+      };
+    } else if (value === 'wallet' && !isChecked) {
+      setWallets1([]);
+      setWallets2([]);
+      wallet_metadata = {};
+    } else if (value !== 'wallet') {
+      wallet_metadata = methods?.wallet_metadata;
+    }
+    setMethods({
+      ...methods,
+      [value]: isChecked,
+      wallet_metadata,
+    });
+  };
+
+  const changeWallets = ({ name, values }) => {
+    let newWallets = [];
+    if (name === 'wallets1') {
+      setWallets1(values);
+      newWallets = [...wallets2, ...values];
+    } else if (name === 'wallets2') {
+      setWallets2(values);
+      newWallets = [...wallets1, ...values];
+    }
+    setMethods({
+      ...methods,
+      wallet_metadata: {
+        wallets: newWallets,
+      },
+    });
+  };
+
+  return (
+    <Box display="flex" flexDirection="column" padding="spacing.8">
+      <Heading size="medium">Provider Settings</Heading>
+      <Text as="p" marginTop="spacing.5" size="small" color="surface.text.gray.normal">
+        You can take a final decision about which methods to enable and disable for {providerName}
+      </Text>
+      <Box
+        display="flex"
+        flexDirection="column"
+        gap="spacing.5"
+        padding={['spacing.4', 'spacing.6']}
+        marginTop="spacing.7"
+        borderColor="surface.border.gray.subtle"
+        borderWidth="thin"
+        borderRadius="small"
+        width="650px"
+      >
+        <Box display="flex" flexDirection="column" gap="spacing.4">
+          {methodsList?.map((method, index) => (
+            <>
+              <Box display="flex" gap="spacing.4">
+                <Box display="flex" gap="spacing.2" alignItems="center" width="200px">
+                  <Text color="surface.text.gray.subtle">{METHODS_MAP[method]}</Text>
+                </Box>
+                <Box display="flex" gap="spacing.2">
+                  <Switch
+                    accessibilityLabel={METHODS_MAP[method]}
+                    size="medium"
+                    name={method}
+                    value={method}
+                    isChecked={methods[method]}
+                    onChange={changeMethods}
+                    isDisabled={method === 'sodexo' && !methods.card}
+                  />
+                </Box>
+              </Box>
+              {index < methodsList.length - 1 && <Divider />}
+            </>
+          ))}
+          {showWallets && (
+            <Box display="flex" gap="spacing.4">
+              <Box display="flex" gap="spacing.2" width="200px" />
+              <Box display="flex" flexDirection="column" gap="spacing.5">
+                <Box display="flex" gap="spacing.7">
+                  <CheckboxGroup name="wallets1" onChange={changeWallets} defaultValue={wallets1}>
+                    {wallets1List?.map((wallet) => (
+                      <Checkbox key={wallet} value={wallet}>
+                        {wallet}
+                      </Checkbox>
+                    ))}
+                  </CheckboxGroup>
+                  <CheckboxGroup name="wallets2" onChange={changeWallets} defaultValue={wallets2}>
+                    {wallets2List?.map((wallet) => (
+                      <Checkbox key={wallet} value={wallet}>
+                        {wallet}
+                      </Checkbox>
+                    ))}
+                  </CheckboxGroup>
+                </Box>
+              </Box>
+            </Box>
+          )}
+        </Box>
+      </Box>
+    </Box>
+  );
+};
---
+++ web/js/merchant/views/Optimizer/AddProvider/components/IntegrationTesting/__test__/ProviderSettings.test.js
@@ -0,0 +1,219 @@
+import React from 'react';
+import { BladeProvider } from '@razorpay/blade/components';
+import { bladeTheme } from '@razorpay/blade/tokens';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { ProviderSettings } from 'merchant/views/Optimizer/AddProvider/components/IntegrationTesting/ProviderSettings';
+
+describe('Optimizer IntegrationTesting ProviderSettings', () => {
+  const mockProps = {
+    providerName: 'payu test',
+    gatewayMetaData: {
+      'Gateway Name': {
+        data_type: 'string',
+        data_value: 'PayU',
+        terminals_key: '',
+      },
+      Key: {
+        data_type: 'string',
+        data_value: 'payu key',
+        min_length: 6,
+        terminals_key: '',
+      },
+      'Payment Methods': {
+        data_type: 'array',
+        data_value: ['card', 'upi', 'netbanking', 'emi', 'wallet', 'emandate'],
+        terminals_key: '',
+        meta_data: {
+          wallet_metadata: {
+            wallets: [
+              'itzcash',
+              'airtelmoney',
+              'freecharge',
+              'oxigen',
+              'payzapp',
+              'amexeasyclick',
+              'olamoney',
+              'paycash',
+              'jiomoney',
+              'citibankrewards',
+              'amazonpay',
+              'paytm',
+              'phonepe',
+            ],
+          },
+        },
+      },
+      Recurring: {
+        data_type: 'bool',
+        data_value: 'recurring',
+        terminals_key: '',
+      },
+      Salt: {
+        data_type: 'string',
+        data_value: 'payu salt',
+        min_length: 8,
+        terminals_key: '',
+      },
+      Sodexo: {
+        data_type: 'bool',
+        data_value: 'sodexo',
+        terminals_key: '',
+      },
+      optimizer_seamless_disabled: {
+        data_type: 'bool',
+        data_value: 'optimizer_seamless_disabled',
+        terminals_key: '',
+      },
+    },
+    methods: {},
+    setMethods: jest.fn(),
+    integrationType: 'instant',
+  };
+
+  const App = (props) => {
+    return (
+      <BladeProvider themeTokens={bladeTheme}>
+        <ProviderSettings {...props} />
+      </BladeProvider>
+    );
+  };
+
+  it('should render ProviderSettings without any errors', () => {
+    expect(() => render(<App {...mockProps} />)).not.toThrowError();
+  });
+
+  it('should render all the methods', () => {
+    render(<App {...mockProps} />);
+    expect(screen.getByText('Provider Settings')).toBeInTheDocument();
+    expect(
+      screen.getByText(
+        'You can take a final decision about which methods to enable and disable for payu test',
+      ),
+    ).toBeInTheDocument();
+    ['Card', 'UPI', 'Netbanking', 'EMI', 'E-Mandate', 'Wallet'].forEach((method) => {
+      expect(screen.getByRole('switch', { name: method })).toBeInTheDocument();
+    });
+  });
+
+  it('should render all the wallets', () => {
+    const wallets = mockProps.gatewayMetaData['Payment Methods'].meta_data.wallet_metadata.wallets;
+    const props = {
+      ...mockProps,
+      methods: {
+        wallet: true,
+        wallet_metadata: {
+          wallets,
+        },
+      },
+    };
+    render(<App {...props} />);
+    const walletSwitch = screen.getByRole('switch', { name: 'Wallet' });
+    expect(walletSwitch).toBeInTheDocument();
+    expect(walletSwitch).toBeChecked();
+    wallets.forEach((wallet) => {
+      const walletCheckbox = screen.getByRole('checkbox', { name: wallet });
+      expect(walletCheckbox).toBeInTheDocument();
+      expect(walletCheckbox).toBeChecked();
+    });
+  });
+
+  it('should call setMethods on change of methods', async () => {
+    render(<App {...mockProps} />);
+    const cardSwitch = screen.getByRole('switch', { name: 'Card' });
+    expect(cardSwitch).toBeInTheDocument();
+    expect(cardSwitch).not.toBeChecked();
+    await userEvent.click(cardSwitch);
+    expect(mockProps.setMethods).toHaveBeenCalledWith({
+      ...mockProps.methods,
+      card: true,
+    });
+    const walletSwitch = screen.getByRole('switch', { name: 'Wallet' });
+    expect(walletSwitch).toBeInTheDocument();
+    expect(walletSwitch).not.toBeChecked();
+    await userEvent.click(walletSwitch);
+    expect(mockProps.setMethods).toHaveBeenCalledWith({
+      ...mockProps.methods,
+      wallet: true,
+      wallet_metadata: {
+        wallets: mockProps.gatewayMetaData['Payment Methods'].meta_data.wallet_metadata.wallets,
+      },
+    });
+  });
+
+  it('should call setMethods on change of wallet method', async () => {
+    const wallets = mockProps.gatewayMetaData['Payment Methods'].meta_data.wallet_metadata.wallets;
+    const props = {
+      ...mockProps,
+      methods: {
+        wallet: true,
+        wallet_metadata: {
+          wallets,
+        },
+      },
+    };
+    render(<App {...props} />);
+    const walletSwitch = screen.getByRole('switch', { name: 'Wallet' });
+    expect(walletSwitch).toBeInTheDocument();
+    expect(walletSwitch).toBeChecked();
+    await userEvent.click(walletSwitch);
+    expect(mockProps.setMethods).toHaveBeenCalledWith({
+      ...mockProps.methods,
+      wallet: false,
+      wallet_metadata: {},
+    });
+  });
+
+  it('should call setMethods on change of wallets', async () => {
+    const wallets = mockProps.gatewayMetaData['Payment Methods'].meta_data.wallet_metadata.wallets;
+    const props = {
+      ...mockProps,
+      methods: {
+        wallet: true,
+        wallet_metadata: {
+          wallets,
+        },
+      },
+    };
+    render(<App {...props} />);
+    const wallet = 'phonepe';
+    const walletCheckbox = screen.getByRole('checkbox', { name: wallet });
+    expect(walletCheckbox).toBeInTheDocument();
+    expect(walletCheckbox).toBeChecked();
+    await userEvent.click(walletCheckbox);
+    expect(mockProps.setMethods).toHaveBeenCalledWith({
+      ...mockProps.methods,
+      wallet: true,
+      wallet_metadata: {
+        wallets: props.methods.wallet_metadata.wallets.filter((w) => w !== wallet),
+      },
+    });
+  });
+
+  it('should render sodexo switch disabled for s2s integration when card is not selected', () => {
+    const props = {
+      ...mockProps,
+      integrationType: 's2s',
+    };
+    render(<App {...props} />);
+    const sodexoSwitch = screen.getByRole('switch', { name: 'Sodexo' });
+    expect(sodexoSwitch).toBeInTheDocument();
+    expect(sodexoSwitch).toBeDisabled();
+  });
+
+  it('should render sodexo switch for s2s integration when card is selected', () => {
+    const props = {
+      ...mockProps,
+      integrationType: 's2s',
+      methods: {
+        card: true,
+      },
+    };
+    render(<App {...props} />);
+    const sodexoSwitch = screen.getByRole('switch', { name: 'Sodexo' });
+    expect(sodexoSwitch).toBeInTheDocument();
+    expect(sodexoSwitch).not.toBeDisabled();
+    expect(sodexoSwitch).not.toBeChecked();
+  });
+});
