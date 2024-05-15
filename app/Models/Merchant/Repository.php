aab0bcc8466e9b9960d45db0dce49ea643595236
kumarpankaj18 <42538137+kumarpankaj18@users.noreply.github.com>
2024-05-15 22:20:44 +0530

    Migrate shared account find or fail (#44011)
    Co-authored-by: Piyush Verma <piyush.verma@razorpay.com>
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Models/Merchant/Repository.php
+++ app/Models/Merchant/Repository.php
@@ -439,9 +439,7 @@ class Repository extends Base\Repository
     {
         if ($this->sharedMerchant === null)
         {
-            $this->sharedMerchant = $this->newQuery()
-                                         ->where(Entity::ID, '=', Account::SHARED_ACCOUNT)
-                                         ->firstOrFail();
+            $this->sharedMerchant = $this->findOrFail(Account::SHARED_ACCOUNT);
         }
 
         return $this->sharedMerchant;
