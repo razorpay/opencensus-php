861563128a7d4b3fa9eb60fd534f0b00980ea7a6
Divyansh Singh <divyansh.singh@razorpay.com>
2024-05-17 10:05:36 +0530

    feat: add validator for phone number in PL [ROW-64] (#14604)
    * [feat]: add validator for phone number for PL
    
    * [fix]: remove empty contact no validation
    
    * [fix]: fix failing UT to pass validation check

--- web/js/merchant/views/PaymentLinks/PaymentLinks/CreateV2/components/Fields/ContactDetails.js
+++ web/js/merchant/views/PaymentLinks/PaymentLinks/CreateV2/components/Fields/ContactDetails.js
@@ -1,7 +1,13 @@
+import { isValidPhoneNumber } from '@razorpay/i18nify-js/phoneNumber';
 import Input from 'common/new-ui/Input';
 import track from 'merchant/views/PaymentLinks/PaymentLinks/CreateV2/track';
 
 const ContactDetails = (props) => {
+  const validateContact = (val) => {
+    if (!val || isValidPhoneNumber(val)) return undefined;
+    return 'Invalid contact number.';
+  };
+
   return (
     <Input.Group
       class="InputGroup--inline InputGroup--vTop customer-details hidden-xs"
@@ -27,6 +33,7 @@ const ContactDetails = (props) => {
           addonBefore={<i class="i i-phone-outline" />}
           defaultValue={props.defaultContactNumber}
           onBlur={track.lj.fields.contact}
+          validator={(val) => validateContact(val)}
         />
       </div>
     </Input.Group>
