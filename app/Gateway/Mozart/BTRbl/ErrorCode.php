5d076df985abab13a12bd46fa2b2bda4bf1a0c6a
Ankit <129067869+aksrazor@users.noreply.github.com>
2024-05-30 09:50:39 +0530

    [ NBPLUS-2031 ] have added new error code and made condition as or (#44167)
    * have added new error code and made condition or
    
    * have added error
    
    ---------
    
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Gateway/Mozart/BTRbl/ErrorCode.php
+++ app/Gateway/Mozart/BTRbl/ErrorCode.php
@@ -8,13 +8,13 @@ class ErrorCode
      * ErrorStatus :  ER001
      * Enter a proper JSON Format Request
      */
-    const  ER001 = 'ER001';
+    const ER001 = 'ER001';
 
     /*
      * ErrorCode : ERR_REF010
      * ErrorReason : Enter proper data types and constraints
      */
-    const ER002  = 'ER002';
+    const ER002 = 'ER002';
 
     /*
      * ErrorCode : ER006
@@ -23,15 +23,24 @@ class ErrorCode
     const ER006 = 'ER006';
 
     /*
-    * ErrorCode : ER018
-    * ErrorReason : Enter proper request field length and check ESB  database
-    */
+     * ErrorCode : ER018
+     * ErrorReason : Enter proper request field length and check ESB  database
+     */
     const ER018 = 'ER018';
 
     const ERROR_CODE = [
-         self::ER001 => 'ER001',
-         self::ER002 => 'ER002',
-         self::ER006 => 'ER006',
-         self::ER018 => 'ER018'
+        self::ER001 => 'ER001',
+        self::ER002 => 'ER002',
+        self::ER006 => 'ER006',
+        self::ER018 => 'ER018',
+        self::ERR005 => 'ERR005',
+        self::ERR003 => 'ERR003',
+        self::ERR004 => 'ERR004',
+        self::ERR006 => 'ERR006',
+        self::ERR007 => 'ERR007',
+        self::ERR009 => 'ERR009',
+        self::ERR000 => 'ERR000',
+        self::ERRO01 => 'ERRO01',
+
     ];
 }
