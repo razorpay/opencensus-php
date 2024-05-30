7ec6e8bc4095196dc05c0d35c1c1c313666dcbf2
Satvik padhiyar <40052349+satvikpadhiyar@users.noreply.github.com>
2024-05-30 11:00:25 +0530

    added route for GET_MCC_CATEGORIZATION_DETAILS_BY_ACCOUNT_ID (#44189)

--- app/Http/Controllers/BvsAdminProxyController.php
+++ app/Http/Controllers/BvsAdminProxyController.php
@@ -11,6 +11,7 @@ class BvsAdminProxyController extends BaseProxyController
     const GET_CONSENT_DOCUMENTS_BY_OWNER_ID = 'GetConsentDocumentsByOwnerId';
     const LIST_KYC_DOCUMENTS = 'ListDocuments';
     const GET_WEBSITE_POLICY_VERIFICATIONS_BY_ACCOUNT_ID = 'GetWebsiteVerificationsByAccountID';
+    const GET_MCC_CATEGORIZATION_DETAILS_BY_ACCOUNT_ID ='GetMccDetailsByAccountId';
 
 
     const ROUTES_URL_MAP    = [
@@ -18,7 +19,8 @@ class BvsAdminProxyController extends BaseProxyController
         self::GET_VALIDATION_WITH_DETAILS => "/twirp\/platform.bvs.validation.v2.ValidationAPI\/GetValidationWithDetails/",
         self::GET_CONSENT_DOCUMENTS_BY_OWNER_ID => "/twirp\/platform.bvs.consentdocumentmanager.v2.ConsentDocumentManagerAPI\/GetConsentDocumentsByOwnerId/",
         self::LIST_KYC_DOCUMENTS => "/twirp\/platform.bvs.kycdocumentmanager.v1.KYCDocumentManagerAPI\/ListDocuments/",
-        self::GET_WEBSITE_POLICY_VERIFICATIONS_BY_ACCOUNT_ID => "/twirp\/platform.bvs.websiteverification.v1.WebsiteVerificationAPI\/GetWebsiteVerificationByAccountId/"
+        self::GET_WEBSITE_POLICY_VERIFICATIONS_BY_ACCOUNT_ID => "/twirp\/platform.bvs.websiteverification.v1.WebsiteVerificationAPI\/GetWebsiteVerificationByAccountId/",
+        self::GET_MCC_CATEGORIZATION_DETAILS_BY_ACCOUNT_ID=>"/twirp\/platform.bvs.mcccategorization.v1.MccCategorizationAPI\/GetMccDetailsByAccountId/"
     ];
 
     const ADMIN_ROUTES = [
@@ -27,7 +29,8 @@ class BvsAdminProxyController extends BaseProxyController
         self::GET_CONSENT_DOCUMENTS_BY_OWNER_ID,
         self::LIST_KYC_DOCUMENTS,
         self::GET_CONSENT_DOCUMENTS_BY_OWNER_ID,
-        self::GET_WEBSITE_POLICY_VERIFICATIONS_BY_ACCOUNT_ID
+        self::GET_WEBSITE_POLICY_VERIFICATIONS_BY_ACCOUNT_ID,
+        self::GET_MCC_CATEGORIZATION_DETAILS_BY_ACCOUNT_ID
     ];
 
     const ADMIN_ROUTES_VS_PERMISSION   = [
@@ -35,7 +38,8 @@ class BvsAdminProxyController extends BaseProxyController
         self::GET_VALIDATION_WITH_DETAILS   => Name::VIEW_ACTIVATION_FORM,
         self::GET_CONSENT_DOCUMENTS_BY_OWNER_ID => Name::VIEW_ALL_ENTITY,
         self::LIST_KYC_DOCUMENTS => Name::VIEW_ALL_ENTITY,
-        self::GET_WEBSITE_POLICY_VERIFICATIONS_BY_ACCOUNT_ID => Name::VIEW_ALL_ENTITY
+        self::GET_WEBSITE_POLICY_VERIFICATIONS_BY_ACCOUNT_ID => Name::VIEW_ALL_ENTITY,
+        self::GET_MCC_CATEGORIZATION_DETAILS_BY_ACCOUNT_ID =>Name::VIEW_ALL_ENTITY
     ];
 
     public function __construct()
