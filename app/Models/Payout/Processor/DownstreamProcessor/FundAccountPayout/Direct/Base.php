baa3bdabdda1c0ff6fc79f9fc6a2e75e140ef2e4
sarthak2707 <35216274+sarthak2707@users.noreply.github.com>
2024-06-04 02:28:36 +0530

    Adding deduct credits method for ca ps payouts (#44241)
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Models/Payout/Processor/DownstreamProcessor/FundAccountPayout/Direct/Base.php
+++ app/Models/Payout/Processor/DownstreamProcessor/FundAccountPayout/Direct/Base.php
@@ -290,4 +290,20 @@ class Base extends FundAccountPayout\Base
             $payout->setFeeType(CreditType::REWARD_FEE);
         }
     }
+
+    public function adjustFeeAndTaxesIfCreditsAvailable($payout)
+    {
+        $fees = $payout->getFees();
+
+        $tax = $payout->getTax();
+
+        if ($payout->merchant->isFeatureEnabled(Feature\Constants::HIGH_TPS_COMPOSITE_PAYOUT) === false)
+        {
+            $this->adjustMerchantFeesThroughRewardFeeCreditsForPayout($payout, $fees, $tax);
+        }
+
+        $payout->setFees($fees);
+
+        $payout->setTax($tax);
+    }
 }
