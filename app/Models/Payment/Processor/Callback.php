0ccab777ed56a1ebb82e79284790c00dbc2d1940
Adlina Mesri <115601956+adlinamesri@users.noreply.github.com>
2024-06-10 16:35:27 +0800

    remove experiment condition (#44312)
    Co-authored-by: adlina mesri <adlina@curlec.com>
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Models/Payment/Processor/Callback.php
+++ app/Models/Payment/Processor/Callback.php
@@ -1151,17 +1151,8 @@ trait Callback
         }
         else
         {
-            //for touchngo wallet auto payment is done via s2s, it didnt required TOPUP action. need to set the status to failed if the status is insufficient fund.
-            $recurringIdVariant = (new MerchantCore())->isRazorxExperimentEnable($this->merchant->getId(), 'tng_auto_debit');
 
-            $this->trace->info(TraceCode::RAZORX_EXPERIMENT_RESULT,
-                [
-                    'merchant_id' => $this->merchant->getId(),
-                    '$recurringIdVariant' => $recurringIdVariant,
-                    'internalError' => $e->getError()->getInternalErrorCode()
-                ]);
-
-            if (($recurringIdVariant === true) and $this->payment->isGateway(Payment\Gateway::TNGD) === true && $this->payment->isWalletAutoRecurring() && $e->getError()->getInternalErrorCode() === ErrorCode::BAD_REQUEST_PAYMENT_WALLET_INSUFFICIENT_BALANCE)
+            if ( $this->payment->isGateway(Payment\Gateway::TNGD) === true && $this->payment->isWalletAutoRecurring() && $e->getError()->getInternalErrorCode() === ErrorCode::BAD_REQUEST_PAYMENT_WALLET_INSUFFICIENT_BALANCE)
             {
                 $this->updatePaymentAuthFailed($e);
             }
