f11992d212c8c9e4b0a51d62d613b8da23df8ee9
Vinayaka Bhat <46439003+vbhat869@users.noreply.github.com>
2024-06-11 13:33:31 +0530

    fix: styling fix for Partner settings page (#14835)

--- web/js/merchant/views/PartnerDashboard/PartnerAccountsSettings/index.tsx
+++ web/js/merchant/views/PartnerDashboard/PartnerAccountsSettings/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Box } from '@razorpay/blade/components';
+import { Box, Card, CardBody } from '@razorpay/blade/components';
 
 import SupportTickets from 'merchant/views/AccountAndSettings/BusinessSettings/Tabs/SupportTickets';
 
 const PartnerAccountsSettings = (): JSX.Element => {
   return (
-    <Box>
-      <SupportTickets />
-    </Box>
+    <Card padding="spacing.3" elevation="none">
+      <CardBody>
+        <Box>
+          <SupportTickets />
+        </Box>
+      </CardBody>
+    </Card>
   );
 };
 
