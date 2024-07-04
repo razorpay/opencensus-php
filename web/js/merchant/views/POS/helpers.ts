48eb748c0ffac4b7c37b12ac16242cbfbf48e47e
Amitabh <108728818+amitabh-blade@users.noreply.github.com>
2024-07-04 12:32:10 +0530

    [pos]: blocked pos tab for sales assisted onboarding (#14940)

--- web/js/merchant/views/POS/helpers.ts
+++ web/js/merchant/views/POS/helpers.ts
@@ -56,11 +56,15 @@ export const isPosExperimentEnabled = ({
       ? allowedFlows.includes(user?.pos_activation_flow ?? '')
       : true;
 
+  const isBlockedForSalesAssisted =
+    user.user?.signup_campaign === 'assisted_onboarding' && !user?.pos_activation_status;
+
   return (
     isExperimentEnabled(abExperiments?.pos_onboarding) &&
     !!user.is_pgos_merchant &&
     isWhitelistedForPos &&
-    !isUnregisteredMerchant
+    !isUnregisteredMerchant &&
+    !isBlockedForSalesAssisted
   );
 };
 
