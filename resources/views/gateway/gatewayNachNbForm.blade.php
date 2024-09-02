e7b9e1e2b19772fd5246120dc90463bc3cd28cab
Vivek Jha <jhavive@users.noreply.github.com>
2024-09-03 00:37:06 +0530

    [RSUBS-1450]changing the cancel mandate change (#45503)
    Co-authored-by: darshparekh <101813324+darshparekh@users.noreply.github.com>

--- resources/views/gateway/gatewayNachNbForm.blade.php
+++ resources/views/gateway/gatewayNachNbForm.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.nach')
 @section('content')
   <form name="form1" action="{{$data['request']['url']}}" method="post" style="margin: 40px 0 0; line-height: 46px; text-align: center;" onsubmit="disableSubmitButton()">
-    <span onclick="cancelPayment(this)" style="color: #528FF0; cursor: pointer;">Cancel Payment</span>
+    <span onclick="cancelPayment(this)" style="color: #528FF0; cursor: pointer;">Cancel</span>
     @foreach ($data['request']['content'] as $key => $value)
       <input type="hidden" name="{{$key}}" value="{{$value}}">
     @endforeach
