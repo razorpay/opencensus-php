f5cb79648f114c50b5d415a8df95110f47d18302
Gurpreet Singh Walia <101316048+gurpreetwalia@users.noreply.github.com>
2024-10-04 19:07:18 +0530

    AFD-1950 Reverting SaveOrFail Changes (#46226)

--- app/Models/Merchant/Methods/Repository.php
+++ app/Models/Merchant/Methods/Repository.php
@@ -355,12 +355,4 @@ class Repository extends Base\Repository
         return $methods;
     }
 
-    public function saveOrFail($entity, array $options = array()){
-        $this->trace->info(TraceCode::METHODS_WRITE, [
-            'data' => $entity,
-            'options' => $options,
-        ]);
-        parent::saveOrFail($entity, $options);
-    }
-
 }
