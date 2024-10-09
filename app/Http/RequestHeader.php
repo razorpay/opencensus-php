cbf1b0a5c941bbd57522711c8f8fbf7c8f57f676
Vinita Nirmal <73991784+vinita-razor@users.noreply.github.com>
2024-10-09 14:45:41 +0530

    [CDT-48] CMMA-1291 pass case type in header (#46005)
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Http/RequestHeader.php
+++ app/Http/RequestHeader.php
@@ -122,6 +122,8 @@ class RequestHeader
 
     const X_RAZORPAY_REQUEST_ID          = 'X-Razorpay-Request-Id';
 
+    const X_CASE_TYPE                    = 'X-Case-type';
+
     const X_MOBILE_OAUTH                 = 'x-mobile-oauth';
 
     const ACCEPT_VERSION                 = 'Accept-Version';
