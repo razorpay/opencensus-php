a6463564618bb5b73c07e7e8f27c7a4290f74a36
Gaurang Patel <94369640+zorro-30@users.noreply.github.com>
2024-10-17 09:05:36 +0530

    [CARD-4629] || reverting the code change (#46720)
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Models/Payment/Processor/Authorize.php
+++ app/Models/Payment/Processor/Authorize.php
@@ -11979,17 +11979,15 @@ trait Authorize
             {
                 $payment->setGatewayCaptured(true);
 
+                [$txn, $feesSplit] = (new Transaction\Core)->createFromPaymentAuthorized($payment);
+
+                $this->repo->saveOrFail($txn);
+
                 $merchant = $this->repo->merchant->findOrFailPublic($payment->getMerchantId());
 
                 if ($merchant->isFeatureEnabled(Feature\Constants::PG_LEDGER_REVERSE_SHADOW) === true)
                 {
                     (new ReverseShadowPaymentsCore())->createLedgerEntryForGatewayCaptureReverseShadow($payment, $txn->getId());
-                }
-                else
-                {
-                    [$txn, $feesSplit] = (new Transaction\Core)->createFromPaymentAuthorized($payment);
-
-                    $this->repo->saveOrFail($txn);
                 }
 
                 // Also sets the transaction association with the payment.
