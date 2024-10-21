0ae917661e4498aeca4616ac984c5bb21a2b8a6e
Amrendra Singh <amrendra.singh@razorpay.com>
2024-10-21 17:42:24 +0530

    [CBMS-141] - Update max amount Intl Bank transfers (#46818)
    Co-authored-by: aadarshbaid <94033020+aadarshbaid@users.noreply.github.com>
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Models/Merchant/Entity.php
+++ app/Models/Merchant/Entity.php
@@ -740,7 +740,7 @@ class Entity extends Base\PublicEntity
     // Increase txn limit for B2B intl_bank_transfer payments
     // Higher limit is now Rs 8.2L base amount
     // https://razorpay.slack.com/archives/C01LK94TC69/p1708595682462969?thread_ts=1708496760.889379&cid=C01LK94TC69
-    const MAX_PAYMENT_AMOUNT_DEFAULT_INTL_BANK_TRANSFER = 82000000;
+    const MAX_PAYMENT_AMOUNT_DEFAULT_INTL_BANK_TRANSFER = 247500000;
 
     public function isCustomOrgUpiQrEnabled(): bool
     {
