948821f11f103a230d8c93a5be2f90670c6dc5a4
Rahul Makhija <86592481+rahul-rzp@users.noreply.github.com>
2024-10-24 14:56:45 +0530

    Fix/payment schedule fallback [PID-123] (#15680)
    * pg router testing
    
    * add fallback for empty state
    
    * skip failing e2e
    
    ---------
    
    Co-authored-by: Abhishek Kumar <k.abhishek@razorpay.com>

--- web/js/merchant/views/Settlements/components/PaymentSchedule.js
+++ web/js/merchant/views/Settlements/components/PaymentSchedule.js
@@ -19,53 +19,51 @@ const PaymentSchedule = (props) => {
     setShowOtherMethods(!showOtherMethods);
   };
 
-  return (
-    schedules[`${paymentType}:default`] && (
-      <div className="payment-schedule-container">
-        <div className="default-cycle schedule-row">
-          <div className="section capitalize">{paymentType} Payments</div>
-          <div className="section capitalize text-right">
-            <strong>{schedules[`${paymentType}:default`]}</strong>
-          </div>
+  return schedules[`${paymentType}:default`] ? (
+    <div className="payment-schedule-container">
+      <div className="default-cycle schedule-row">
+        <div className="section capitalize">{paymentType} Payments</div>
+        <div className="section capitalize text-right">
+          <strong>{schedules[`${paymentType}:default`]}</strong>
         </div>
-        {otherMethods?.length > 0 && (
-          <div className="other-methods-container">
-            <span className="pr-5">
-              <strong>Note:</strong> Some {paymentType} payment methods have a different schedule.
-            </span>
-            <span className="btn-link" onClick={toggleOtherMethods}>
-              <span className="text-no-wrap">{showOtherMethods ? 'Hide' : 'View'} schedules</span>
-              <i className={`i i-arrow-${showOtherMethods ? 'up' : 'down'}`} />
-            </span>
-            {showOtherMethods && (
-              <div className="mt-12">
-                <div className="schedule-row other-methods-heading mb-6">
-                  <div className="section">Payment method</div>
-                  <div className="section text-right">Settlement schedule</div>
-                </div>
-                {otherMethods.map((method, index) => {
-                  return (
-                    <div
-                      className={`${
-                        index !== otherMethods.length - 1 ? 'mb-6 ' : ''
-                      }schedule-row other-methods-row`}
-                      key={`${paymentType}:${method}`}
-                      aria-label="methods"
-                    >
-                      <div className="section capitalize">{method}</div>
-                      <div className="section capitalize text-right">
-                        <strong>{schedules[`${paymentType}:${method}`]}</strong>
-                      </div>
-                    </div>
-                  );
-                })}
-              </div>
-            )}
-          </div>
-        )}
       </div>
-    )
-  );
+      {otherMethods?.length > 0 && (
+        <div className="other-methods-container">
+          <span className="pr-5">
+            <strong>Note:</strong> Some {paymentType} payment methods have a different schedule.
+          </span>
+          <span className="btn-link" onClick={toggleOtherMethods}>
+            <span className="text-no-wrap">{showOtherMethods ? 'Hide' : 'View'} schedules</span>
+            <i className={`i i-arrow-${showOtherMethods ? 'up' : 'down'}`} />
+          </span>
+          {showOtherMethods && (
+            <div className="mt-12">
+              <div className="schedule-row other-methods-heading mb-6">
+                <div className="section">Payment method</div>
+                <div className="section text-right">Settlement schedule</div>
+              </div>
+              {otherMethods.map((method, index) => {
+                return (
+                  <div
+                    className={`${
+                      index !== otherMethods.length - 1 ? 'mb-6 ' : ''
+                    }schedule-row other-methods-row`}
+                    key={`${paymentType}:${method}`}
+                    aria-label="methods"
+                  >
+                    <div className="section capitalize">{method}</div>
+                    <div className="section capitalize text-right">
+                      <strong>{schedules[`${paymentType}:${method}`]}</strong>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  ) : null;
 };
 
 export default PaymentSchedule;
