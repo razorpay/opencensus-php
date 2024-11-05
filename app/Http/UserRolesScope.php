e1a977058b7282aa9ef92ad5e99cf71df04710a3
Vinita Nirmal <73991784+vinita-razor@users.noreply.github.com>
2024-11-05 11:16:43 +0530

    [AFD-2036] merchant_activation_save remove ADMIN_READONLY role (#47266)

--- app/Http/UserRolesScope.php
+++ app/Http/UserRolesScope.php
@@ -511,7 +511,7 @@ class UserRolesScope
             'merchant_activation_business_categories'       => [Role::ADMIN, Role::OWNER, Role::MANAGER, Role::SELLERAPP,Role::FINANCE, Role::PARTNER,Role::RAZORPAY_SALES, Role::ADMIN_READONLY],
             'merchant_activation_business_categories_v2'    => [Role::ADMIN, Role::OWNER, Role::MANAGER, Role::SELLERAPP,Role::FINANCE, Role::PARTNER, Role::PARTNER_AGENT,Role::RAZORPAY_SALES, Role::ADMIN_READONLY],
             'merchant_activation_gst_details'               => [Role::OWNER, Role::MANAGER, Role::ADMIN, Role::FINANCE, Role::PARTNER, Role::PARTNER_AGENT,Role::RAZORPAY_SALES, Role::ADMIN_READONLY],
-            'merchant_activation_save'                      => [Role::OWNER, Role::MANAGER, Role::ADMIN, Role::FINANCE, Role::PARTNER, Role::PARTNER_AGENT,Role::RAZORPAY_SALES, Role::ADMIN_READONLY],
+            'merchant_activation_save'                      => [Role::OWNER, Role::MANAGER, Role::ADMIN, Role::FINANCE, Role::PARTNER, Role::PARTNER_AGENT,Role::RAZORPAY_SALES],
             'merchant_activation_otp_send'                  => [Role::OWNER, Role::MANAGER, Role::ADMIN, Role::FINANCE],
             'merchant_coupons_apply'                        => [Role::OWNER, Role::ADMIN],
             'merchant_checkout_details'                     => [Role::OWNER, Role::MANAGER, Role::ADMIN, Role::FINANCE, Role::ADMIN_READONLY],
