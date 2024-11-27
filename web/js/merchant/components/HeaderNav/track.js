a738514c0bd8bf19818358f9692d78c19a6181e6
Vishwesh Bajpai <63144193+vishweshbajpai@users.noreply.github.com>
2024-11-27 21:52:05 +0530

    fix: add screen in analytics call (#15847)

--- web/js/merchant/components/HeaderNav/track.js
+++ web/js/merchant/components/HeaderNav/track.js
@@ -1,3 +1,4 @@
+import { ANALYTICS } from 'common/constant';
 import { analyticsTrack } from 'common/utils/analytics';
 import { getCommonAnalyticsProperties } from 'common/utils/rzp-utils';
 
@@ -5,7 +6,7 @@ export const createNewAccountTrack = () => {
   analyticsTrack({
     objectName: 'Create A New Account Button',
     actionName: 'Clicked',
-    screen: '',
+    screen: ANALYTICS.SCREEN.DASHBOARD,
     properties: {
       version: 'v2',
       session_id: window.session_id,
