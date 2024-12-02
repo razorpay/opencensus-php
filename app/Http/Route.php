5bbaff9cccde662d6bc7c97b2ad71ca16ef581b9
apoorvkrishna2022 <109572721+apoorvkrishna2022@users.noreply.github.com>
2024-12-02 21:51:56 +0530

    [RAY-344] | adding fetch user details internal route in razorassist (#48319)

--- app/Http/Route.php
+++ app/Http/Route.php
@@ -18486,6 +18486,7 @@ class Route
         'credcase' => ['expire_keys'],
 
         'razorassist' => [
+            'merchant_fetch_internal_users',
             'email_user_status_for_email_update',
             'merchant_edit_email_self_serve',
             'fetch_users_internal',
