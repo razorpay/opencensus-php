7b60ef0057362d402db7cb88507466e29de409e3
anudeep-chakilam <97085771+anudeep-chakilam@users.noreply.github.com>
2024-12-06 13:46:17 +0530

    [RTO-1234] Fix: Add tidb namespace in insurance query (#48556)

--- app/Models/Insurance/Repository.php
+++ app/Models/Insurance/Repository.php
@@ -11,12 +11,17 @@ class Repository extends Base\Repository
 
     public function fetchInsurance(string $paymentId, string $orderId)
     {
+        $insuranceTidbNamespace = $this->app->runningUnitTests() ? '' : 'prod_checkout_service.';
+
+        $insuranceTable = $insuranceTidbNamespace . $this->getTableName();
+
         $insuranceRepo = $this->repo->insurance;
 
         $insuranceInsuredEntityIdColumn = $insuranceRepo->dbColumn(Entity::INSURED_ENTITY_ID);
         $insuranceInsuredEntityTypeColumn = $insuranceRepo->dbColumn(Entity::INSURED_ENTITY_TYPE);
 
-        $q = $this->newQueryWithConnection($this->getConnectionFromType(ConnectionType::DATA_WAREHOUSE_MERCHANT));
+        $q = $this->newQueryWithConnection($this->getConnectionFromType(ConnectionType::DATA_WAREHOUSE_MERCHANT))
+            ->from($insuranceTable);
 
         if (!empty($orderId))
         {
