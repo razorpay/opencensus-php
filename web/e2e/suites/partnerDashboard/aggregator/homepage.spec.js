5c285cbb78d838276e585508d37096396780eec1
Harsh Karande <harshskarande@gmail.com>
2024-12-07 14:02:35 +0530

    chore(PID): add owners entry for skipped tests [PID-1588] (#15816)
    * skip test for payment links
    
    * skip pos tests
    
    * unskip previous test
    
    * add owners entry for skipped tests
    
    * on level with master

--- web/e2e/suites/partnerDashboard/aggregator/homepage.spec.js
+++ web/e2e/suites/partnerDashboard/aggregator/homepage.spec.js
@@ -6,26 +6,24 @@ import { waitForSelectorToBeVisible } from 'utils/common';
 const { test } = require('utils/base');
 
 // Aggregator Partner Tests
-test.describe.parallel(
-  'Test Aggregator Partner Dashboard landing page @flow=partner-homepage @project=partner-dashboard',
-  () => {
-    test.use({
-      storageState: getStorageStatePath(BASE_PATH).AGGREGATOR_PARTNER_TEST_LOGIN_STATE,
+test.describe
+  .parallel('Test Aggregator Partner Dashboard landing page @flow=partner-homepage @project=partner-dashboard', () => {
+  test.use({
+    storageState: getStorageStatePath(BASE_PATH).AGGREGATOR_PARTNER_TEST_LOGIN_STATE,
+  });
+  test.beforeEach(async ({ page }) => {
+    await loadPartnerDashboardHomePage(
+      page,
+      'AGGREGATOR_PARTNER',
+      CONTENT_SELECTORS.HOME_PAGE.AGGREGATOR_WELCOME_TEXT,
+    );
+  });
+  test.skip('should load the Aggregator Partner Dashboard @priority=critical', async ({ page }) => {
+    await page.locator(CTA_SELECTORS.HOME_PAGE.REFER_NEW_CLIENT).click();
+    await waitForSelectorToBeVisible({
+      page,
+      selector: CONTENT_SELECTORS.SELECT_PRODUCT.LEGACY_HEADER,
     });
-    test.beforeEach(async ({ page }) => {
-      await loadPartnerDashboardHomePage(
-        page,
-        'AGGREGATOR_PARTNER',
-        CONTENT_SELECTORS.HOME_PAGE.AGGREGATOR_WELCOME_TEXT,
-      );
-    });
-    test('should load the Aggregator Partner Dashboard @priority=critical', async ({ page }) => {
-      await page.locator(CTA_SELECTORS.HOME_PAGE.REFER_NEW_CLIENT).click();
-      await waitForSelectorToBeVisible({
-        page,
-        selector: CONTENT_SELECTORS.SELECT_PRODUCT.LEGACY_HEADER,
-      });
-      // TODO v2: add more checks separately
-    });
-  },
-);
+    // TODO v2: add more checks separately
+  });
+});
--- web/e2e/suites/partnerDashboard/platform/homepage.spec.js
+++ web/e2e/suites/partnerDashboard/platform/homepage.spec.js
@@ -6,26 +6,24 @@ import { waitForSelectorToBeVisible } from 'utils/common';
 const { test, expect } = require('utils/base');
 
 // Platform Partner Tests
-test.describe.parallel(
-  'Test Platform Partner Dashboard landing page @flow=partner-homepage @project=partner-dashboard',
-  () => {
-    test.use({
-      storageState: getStorageStatePath(BASE_PATH).PLATFORM_PARTNER_TEST_LOGIN_STATE,
+test.describe
+  .parallel('Test Platform Partner Dashboard landing page @flow=partner-homepage @project=partner-dashboard', () => {
+  test.use({
+    storageState: getStorageStatePath(BASE_PATH).PLATFORM_PARTNER_TEST_LOGIN_STATE,
+  });
+  test.beforeEach(async ({ page }) => {
+    await loadPartnerDashboardHomePage(
+      page,
+      'PLATFORM_PARTNER',
+      CONTENT_SELECTORS.HOME_PAGE.PLATFORM_PARTNER_WELCOME_TEXT,
+    );
+  });
+  test.skip('should load the Platform Partner Dashboard @priority=critical', async ({ page }) => {
+    await waitForSelectorToBeVisible({
+      page,
+      selector: CONTENT_SELECTORS.HOME_PAGE.START_YOUR_JOURNEY,
     });
-    test.beforeEach(async ({ page }) => {
-      await loadPartnerDashboardHomePage(
-        page,
-        'PLATFORM_PARTNER',
-        CONTENT_SELECTORS.HOME_PAGE.PLATFORM_PARTNER_WELCOME_TEXT,
-      );
-    });
-    test('should load the Platform Partner Dashboard @priority=critical', async ({ page }) => {
-      await waitForSelectorToBeVisible({
-        page,
-        selector: CONTENT_SELECTORS.HOME_PAGE.START_YOUR_JOURNEY,
-      });
-      expect(page.locator(CTA_SELECTORS.HOME_PAGE.REFER_NEW_CLIENT)).not.toBeVisible();
-      // TODO v2: add more checks separately
-    });
-  },
-);
+    expect(page.locator(CTA_SELECTORS.HOME_PAGE.REFER_NEW_CLIENT)).not.toBeVisible();
+    // TODO v2: add more checks separately
+  });
+});
