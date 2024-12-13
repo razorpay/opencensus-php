7932a724a1beb4cd3b38e1c674da0ed89f808ce5
ayushiidubey <ayushi.dubey.1299@gmail.com>
2024-12-13 14:19:30 +0530

    [OMAQ-781] Migrate merchant's needs_clarification status notifications to pgos  (#47616)
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- config/app.php
+++ config/app.php
@@ -513,7 +513,10 @@ return array(
 
     'rejected_communications_from_api_exp_id' => env('SEND_MERCHANT_NOTIFICATIONS_FROM_API_EXPERIMENT_ID'),
 
+    'needs_clarification_communications_from_api_exp_id' => env('SEND_NC_MERCHANT_NOTIFICATIONS_FROM_API_EXPERIMENT_ID'),
+
     'cac_blacklist_exp_id' => env('CAC_BLACKLIST_EXP_ID'),
+
 
     'nocode' => [
         'cache' => [
