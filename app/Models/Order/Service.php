e4bd3a7b0a8665e72da9244640ae39c82601463c
Puneeth S <puneeth.s@razorpay.com>
2024-12-16 23:24:43 +0530

    PCORE-4812 | API Decomp Fetch parity producer (#48685)
    Co-authored-by: satyap07 <satyanand.prasad@razorpay.com>
    Co-authored-by: bhumika-rzp <bhumika.srivastava@razorpay.com>
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>
    Co-authored-by: anusha-musthaphak <anusha.musthaphak@razorpay.com>

--- app/Models/Order/Service.php
+++ app/Models/Order/Service.php
@@ -33,6 +33,7 @@ use RZP\Models\Payment\Processor\Netbanking;
 use RZP\Services\Segment\EventCode as SegmentEvent;
 use RZP\Services\Segment\Constants as SegmentConstants;
 use RZP\Models\Order\Product\Constants as ProductConstants;
+use RZP\Jobs\OrderPaymentsParity as OrderPaymentsParityJob;
 
 class Service extends Base\Service
 {
@@ -792,13 +793,44 @@ class Service extends Base\Service
 
             $res = $apiPayments->merge($rearchPayments);
 
-            return $res->toArrayPublic();
+            $response = $res->toArrayPublic();
+
+            $this->pushPaymentsOrderForParity($response, ["order_id" => $orderId]);
+
+            return $response;
 
         }
 
         $payments = $this->repo->payment->fetch($input, $this->merchant->getId(), ConnectionType::DATA_WAREHOUSE_MERCHANT);
 
-        return $payments->toArrayPublic();
+        $response = $payments->toArrayPublic();
+
+        $this->pushPaymentsOrderForParity($response, $input);
+
+        return $response;
+    }
+
+    public function pushPaymentsOrderForParity($payments, $input)
+    {
+        try
+        {
+            $microtime = microtime(true);
+
+            // Convert seconds to milliseconds
+            $milliseconds = round($microtime * 1000);
+
+            OrderPaymentsParityJob::dispatch($this->mode, $input, $payments, $milliseconds);
+        }
+        catch (\Throwable $ex)
+        {
+            $this->trace->traceException(
+                $ex,
+                500,
+                TraceCode::ORDER_PAYMENTS_PARITY_EXCEPTION,
+                [
+                    "message" => $ex->getMessage()
+                ]);
+        }
     }
 
     public function fetchLineItemsFor(string $id): array
