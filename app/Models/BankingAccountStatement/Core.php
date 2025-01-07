31e3d4d6a2d5eff289d8ac1241596fa7f8777597
Rajesh-Manchikanti <manchikanti.rajesh@razorpay.com>
2025-01-07 06:12:38 +0530

    [XPAT-2173] - Timeout Increase for BAS Fetch (#49691)
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Models/BankingAccountStatement/Core.php
+++ app/Models/BankingAccountStatement/Core.php
@@ -419,7 +419,7 @@ class Core extends Base\Core
 
                     $basDetailEntity->balance->updateLastFetchedAt();
                 },
-                300,
+                600,
                 ErrorCode::BAD_REQUEST_ANOTHER_BANKING_ACCOUNT_STATEMENT_FETCH_IN_PROGRESS
             );
         }
