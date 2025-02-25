d6c026e2f686a2573038bffd9aebc7b16c5d38fb
Pranav Gupta <pronav@users.noreply.github.com>
2025-02-25 11:57:45 +0530

    update checkout api url (#15810)

--- web/js/merchant/views/Capital/CashAdvance/utils.js
+++ web/js/merchant/views/Capital/CashAdvance/utils.js
@@ -273,10 +273,8 @@ export const waitUntil = (condition, cb) => {
 };
 
 export const devStackCheckoutConfig = {
-  api: `${DEV_BASE_URL}/api/`,
-  frameApi: `${DEV_BASE_URL}/api/`,
-  frame: 'https://api-cc.func.razorpay.in/test/checkout.html',
-  js: 'http://checkout.pronav.in/dist/',
+  // which API server to use. comment out this prop to use prod as default (api.razorpay.com)
+  api: new URL(DEV_BASE_URL).origin,
 };
 
 const isProd = isProductionEnv();
