eb6389806519cb5145e53b77619661532d732013
Joel Jaimon <joel.jaimon99@gmail.com>
2025-03-03 11:19:27 +0530

    feat(CEXP-0): CODEOWNERs Revert (#16690)
    * update: CODEOWNERs

--- apps/digital-bills/src/app/App.tsx
+++ apps/digital-bills/src/app/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Box } from '@razorpay/blade/components';
 import DigitalBillsRouter from '@apps/digital-bills/src/bootstrap/Route/DigitalBillsRouter';
 
+// Main App Component
 const App = (): React.ReactElement => {
   return (
     <Box padding="spacing.5">
