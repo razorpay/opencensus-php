f420e8858535170857d6905620386c94fd95d692
Vinil Vasani <35430471+vinilvasani@users.noreply.github.com>
2025-03-10 19:50:41 +0530

    fix(onenav): ecosystem downtime fix in mobile onenav (#16789)
    * fix(onenav): enabling ecosystem downtime and announcements for mobile
    
    * fix(onenav): hiding for connected mobile home

--- web/js/merchant/components/NavigationLayout/TopNavigation/TopNavigation.tsx
+++ web/js/merchant/components/NavigationLayout/TopNavigation/TopNavigation.tsx
@@ -277,6 +277,49 @@ const TopNavigation = ({
     navigate(ONE_NAV_MOBILE_PATH);
   };
 
+  const AnnouncementsAndEcosystemDowntimeWrapper = () => {
+    return (
+      <>
+        {showEcosystemDowntimeButton && (
+          <EcosystemDowntimes
+            mode={mode}
+            showMobileNav={showMobileNav}
+            isRTUXHomepage={false}
+            isConnectedNavigation={true}
+          />
+        )}
+
+        {showAnnouncementButton && (
+          <GrowthAssetEB FallbackComponent={ErrorFallbackComponent}>
+            {user.isWhatsNewLazyEnabled ? (
+              <NotificationIcon showMobileNav={showMobileNav} {...commonProps} />
+            ) : (
+              <ShowWhen
+                additionalCondition={
+                  () => !org.features.includes('disable_announcements') // If the org features array include "disable_announcements" then we hide "Announcement Tab".
+                }
+              >
+                <SuspenseWithLoader type="default">
+                  <WhatsNew showMobileNav={showMobileNav} {...commonProps} />
+                </SuspenseWithLoader>
+              </ShowWhen>
+            )}
+          </GrowthAssetEB>
+        )}
+      </>
+    );
+  };
+
+  // Reusable component for mobile navigation actions
+  const MobileNavActions = () => {
+    return (
+      <Box display="flex" alignItems="center" gap="spacing.3">
+        <AnnouncementsAndEcosystemDowntimeWrapper />
+        <ProfileDropdown showMobileNav={showMobileNav} mode={mode} {...commonProps} />
+      </Box>
+    );
+  };
+
   if (renderFullPageView) return null; // hide Top Nav
 
   const isConnectedMobileHome = currentPath == ONE_NAV_MOBILE_PATH;
@@ -319,12 +362,11 @@ const TopNavigation = ({
             </Box>
 
             <Box>
-              {/* TODO: Will add after initial release */}
               {/* <Heading textAlign="center" size="small" weight="semibold">
                 {selectedProduct.product.title}
               </Heading> */}
             </Box>
-            <ProfileDropdown showMobileNav={showMobileNav} mode={mode} {...commonProps} />
+            <MobileNavActions />
           </TopNav>
           <Box paddingX={'spacing.3'} backgroundColor="surface.background.gray.intense">
             <Divider />
@@ -434,34 +476,7 @@ const TopNavigation = ({
         >
           {shouldShowSearch && <UniversalSearch isConnectedNavigation={true} />}
 
-          {/* TODO: Check how to ecosystem downtime on mobile post initlal release */}
-          {showEcosystemDowntimeButton && (
-            <EcosystemDowntimes
-              mode={mode}
-              showMobileNav={showMobileNav}
-              isRTUXHomepage={false}
-              isConnectedNavigation={true}
-            />
-          )}
-
-          {/* TODO: Check how to handle announcement on mobile post initlal release */}
-          {showAnnouncementButton && (
-            <GrowthAssetEB FallbackComponent={ErrorFallbackComponent}>
-              {user.isWhatsNewLazyEnabled ? (
-                <NotificationIcon showMobileNav={showMobileNav} {...commonProps} />
-              ) : (
-                <ShowWhen
-                  additionalCondition={
-                    () => !org.features.includes('disable_announcements') // If the org features array include "disable_announcements" then we hide "Announcement Tab".
-                  }
-                >
-                  <SuspenseWithLoader type="default">
-                    <WhatsNew showMobileNav={showMobileNav} {...commonProps} />
-                  </SuspenseWithLoader>
-                </ShowWhen>
-              )}
-            </GrowthAssetEB>
-          )}
+          <AnnouncementsAndEcosystemDowntimeWrapper />
           <ProfileDropdown showMobileNav={showMobileNav} mode={mode} {...commonProps} />
         </Box>
       </TopNavActions>
