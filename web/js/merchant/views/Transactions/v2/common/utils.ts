efb7892fb2b056db2dbeba9c3a67161c1270edf8
Abhishek Kumar <abhieshekumar@gmail.com>
2025-03-27 16:23:06 +0530

    Dummy PR (#16924)

--- web/js/merchant/views/Transactions/v2/common/utils.ts
+++ web/js/merchant/views/Transactions/v2/common/utils.ts
@@ -185,23 +185,23 @@ export const getDefaultSearchByValueAndOption = ({
 
 export const onSearch =
   (history: RouteComponentProps['history']) =>
-  (args: Record<string, unknown>): void => {
-    args = encodeSensitiveFields(args);
-    const { pathname, hash, state } = history.location;
-    history.replace({
-      pathname,
-      hash,
-      search: stringifyQueryParams(args),
-      state,
-    });
-  };
+    (args: Record<string, unknown>): void => {
+      args = encodeSensitiveFields(args);
+      const { pathname, hash, state } = history.location;
+      history.replace({
+        pathname,
+        hash,
+        search: stringifyQueryParams(args),
+        state,
+      });
+    };
 
 export const onPaginate =
   (paginate: Paginate) =>
-  (params: Record<string, unknown>): void => {
-    const searchParams = qs.parse(window.location.search);
-    paginate({ ...searchParams, ...params });
-  };
+    (params: Record<string, unknown>): void => {
+      const searchParams = qs.parse(window.location.search);
+      paginate({ ...searchParams, ...params });
+    };
 
 export const getCreatedOnTime = ({ created_at }: { created_at: number }): string => {
   const format = getDateFormat(created_at);
@@ -304,7 +304,7 @@ export const getCountryTaxDefinition = ({ countryCode = '' }: { countryCode: str
 };
 
 export const shouldHideAnalytics = (user: User, mode: Environments): boolean => {
-  // Hide analytics overview for JnK Omni merchant.
+  // Hide analytics overview for JnK Omni merchant
   if (user.isJnKOmniEnabled) {
     return true;
   }
