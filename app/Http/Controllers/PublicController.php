dc07b5a2c370447539e7e20751c75b960e3cafc6
rohande1329 <rohan.de@razorpay.com>
2025-03-27 20:14:21 +0530

    [AFD-3462] | Changed Merchant to Org for FF functionality  (#47183)
    Co-authored-by: rzpcibot <64553331+rzpcibot@users.noreply.github.com>

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -284,7 +284,8 @@ class PublicController extends Controller
             }
             $merchantID = $order['merchant_id'];
             $merchant = $this->repo->merchant->findOrFail($merchantID);
-            $isCheckout2FeatureEnabled = $merchant->isFeatureEnabled(Feature\Constants::HDFC_CHECKOUT_2);
+            $isCheckout2FeatureEnabled = ($merchant->org->isFeatureEnabled(Feature\Constants::HDFC_CHECKOUT_2) OR
+                ($merchant->isFeatureEnabled(Feature\Constants::HDFC_CHECKOUT_2)));
             $app = \App::getFacadeRoot();
 
             $app['trace']->info(TraceCode::RENDER_CHECKOUT_HOSTED, [
@@ -292,16 +293,10 @@ class PublicController extends Controller
                 'orderID' => $orderID,
                 'merchantID' => $merchantID,
                 'order' => $order,
-                'isCheckout2FeatureEnabled' => $isCheckout2FeatureEnabled
+                'isCheckout2FeatureEnabled' => $isCheckout2FeatureEnabled,
             ]);
 
-            // check the experiment
-            $variant = $this->app['razorx']->getTreatment($merchantID,
-                RazorxTreatment::HDFC_CHECKOUT_2, $mode);
-            if (strtolower($variant) === 'on')
-            {
-                return $isCheckout2FeatureEnabled;
-            }
+            return $isCheckout2FeatureEnabled;
         } catch (\Exception $e)
         {
             $this->app['trace']->traceException(
@@ -325,7 +320,6 @@ class PublicController extends Controller
                 'script'       => $script,
                 'urls'         => "{}"
             ];
-
         return View::make('public.embedded', $options);
     }
     public function renderCheckoutHosted()
