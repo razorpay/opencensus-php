e11ef63cfc67c8fab1ea0199d8c2a68d8b084b4b
satanick1987 <157377171+satanick1987@users.noreply.github.com>
2025-04-10 16:28:19 +0530

    BP-4633: Skip validation for Invite Merchant Flow behind permission (#53435)

--- app/Models/Admin/AdminLead/Service.php
+++ app/Models/Admin/AdminLead/Service.php
@@ -27,14 +27,9 @@ class Service extends Base\Service
 
         $merchantType = $this->getMerchantType($input);
 
-        $properties = [
-            'id'            => $orgId,
-            'experiment_id' => $this->app['config']->get('app.banking_redirection_enabled'),
-        ];
+        $orgId = Org\Entity::verifyIdAndSilentlyStripSign($orgId);
 
-        $isExperimentEnabled =  (new MerchantCore())->isSplitzExperimentEnable($properties, 'enable');
-
-        if($isExperimentEnabled === false){
+        if ((new Org\Service)->isRequiredPermissionEnabledforOrg($orgId, Permission\Name::CUSTOM_INVITE_MERCHANT_FLOW) !== true) {
             $this->validateInvitation($orgId, $input);
         }
 
@@ -134,63 +129,63 @@ class Service extends Base\Service
     {
         $invitations = $this->repo->admin_lead->fetchByOrgId($orgId);
 
-        if ($this->customInvitationFlowEnabled($orgId) === true) 
+        if ($this->customInvitationFlowEnabled($orgId) === true)
         {
             return $this->getCustomInvitations($invitations, $orgId);
         }
-        
+
 
         return $invitations->toArrayPublic();
     }
 
     protected function customInvitationFlowEnabled($orgId)
-    {   
+    {
 
         $orgId = Org\Entity::verifyIdAndSilentlyStripSign($orgId);
 
         $permissionEnabled = (new Org\Service)->isRequiredPermissionEnabledforOrg($orgId, Permission\Name::CUSTOM_INVITE_MERCHANT_FLOW);
-        
+
         return $permissionEnabled === true;
     }
 
     protected function getCustomInvitations($invitations, $orgId)
-    {   
+    {
 
-        if (empty($invitations) === true) 
+        if (empty($invitations) === true)
         {
             return [];
         }
 
         $org = $this->repo->org->find($orgId);
 
-        if (empty($org) === true) 
+        if (empty($org) === true)
         {
             return $invitations->toArrayPublic();
         }
 
         $host_name = $org->getPrimaryHostName();
-       
+
         $userEmails = $invitations->pluck('email')->toArray();
         $users = $this->repo->user->getMultipleUsersByEmails($userEmails);
-        
+
         $usersMap = [];
-        foreach ($users as $user) 
+        foreach ($users as $user)
         {
             $usersMap[$user['email']] = $user;
         }
-        
+
         $result = $invitations->toArray();
 
-        foreach ($result as $key => $invitation) 
+        foreach ($result as $key => $invitation)
         {
 
             // check if host name is present
             if (empty($host_name) === false && empty($invitation['token']) === false) {
                 $result[$key]['form_data']['invite_url'] = 'https://' . $host_name .'/#/access/signup?merchant_invitation=' . $invitation['token'];
             }
-                
+
             $email = $invitation['email'] ?? "";
-            
+
             $user = $usersMap[$email];
             if (empty($user) === false && empty($user['contact_mobile']) === false) {
                 $result[$key]['form_data']['contact_mobile'] = $user['contact_mobile'];
