e4eb1e2f1d1e4059b4f7491dfa50188a920a93ff
Rahul Makhija <86592481+rahul-rzp@users.noreply.github.com>
2025-04-15 22:23:55 +0530

    feat: enhance affected projects action with debugging logs (#16928)

--- web/js/merchant/playwright.config.js
+++ web/js/merchant/playwright.config.js
@@ -1,6 +1,5 @@
 const { withDashboardCore, DASHBOARD_FEDERATED_MODULES } = require('@libs/shared-core');
 
-// dummy
 module.exports = withDashboardCore({
   playwrightOptions: {
     moduleName: DASHBOARD_FEDERATED_MODULES.PAYMENTS_DASHBOARD,
