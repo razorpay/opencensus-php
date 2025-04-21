0fc47eb10a09f1ff97553409aced24b7d1ef2116
Swapnil <swapnil.kr@razorpay.com>
2025-04-21 18:37:05 +0530

    feat(onenav)[CEXP-903]: Fixed: Horizontal scroll on dashboard (#17048)
    * Fixed: Horizontal scroll on dashboard
    
    * Resolved comments

--- web/js/merchant/containers/Home/RTUX/styles.ts
+++ web/js/merchant/containers/Home/RTUX/styles.ts
@@ -1,7 +1,10 @@
 import styled from 'styled-components';
 
 export const ResponsiveWrapper = styled.div`
-  @media (min-width: 1465px) {
+  /*
+    Set min-width to 1540px(8px as buffer) to prevent horizontal scroll.
+  */
+  @media (min-width: 1540px) {
     width: 1220px;
     margin: 0 auto;
   }
