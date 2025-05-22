ad26bc34f8e0c7e581b05ed950bacc9b3ce601af
shridharbavannawar <shridhar.b@razorpay.com>
2025-05-22 09:47:24 +0530

    block events sent to clevertap (#17245)

--- libs/shared-utils/src/common/utils/commonAnalytics.ts
+++ libs/shared-utils/src/common/utils/commonAnalytics.ts
@@ -378,7 +378,7 @@ export const analyticsTrack = ({
       },
       {
         integrations: {
-          CleverTap: toCleverTap,
+          //CleverTap: toCleverTap,
           [Facebook]: toFacebook,
         },
       },
