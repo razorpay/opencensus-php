19bb7cb1971c845f86e63e1f25394fd98df3e8db
Neeraj-RZP <neeraj.sati@razorpay.com>
2025-05-26 13:24:29 +0530

    feat: test mode banner in FTUX (#17240)
    * feat: test mode setup
    
    * feat: test mode check

--- web/js/merchant/containers/Home/FTUX/index.tsx
+++ web/js/merchant/containers/Home/FTUX/index.tsx
@@ -1,7 +1,9 @@
 import React, { Suspense } from 'react';
-import { Box } from '@razorpay/blade/components';
+import { Alert, Box, DotIcon } from '@razorpay/blade/components';
 import lazyLoader from 'merchant/routes/LazyLoader';
 import LayoutLoader from './Loader';
+import { isMobileDevice } from '@libs/shared-utils';
+import { useStore } from '@federated/apps/shell/commonStore';
 import { useTwoFactorVerificationContext } from 'common/ui/TwoFactorVerification/TwoFactorVerificationContext';
 import { closeModal as close2faModal } from 'merchant_common/reducers/modals';
 
@@ -14,6 +16,8 @@ const FTUXLanding = lazyLoader(
 
 const FTUXHomepage = (): JSX.Element => {
   const { criticalFlow } = useTwoFactorVerificationContext();
+  const { mode } = useStore((state) => state.session);
+  const isMobile = isMobileDevice();
 
   const triggerTwoFaAuth = ({
     enforceVerifyOtp,
@@ -34,11 +38,24 @@ const FTUXHomepage = (): JSX.Element => {
     });
 
   return (
-    <Box display="flex" justifyContent="center">
-      <Box maxWidth="920px" width="100%">
-        <Suspense fallback={<LayoutLoader />}>
-          <FTUXLanding triggerTwoFaAuth={triggerTwoFaAuth} />
-        </Suspense>
+    <Box>
+      {mode === 'test' && (
+        <Alert
+          description="You are in Test mode - you can test payments and features here. Switch to Live Mode (bottom left) to accept real transactions."
+          icon={DotIcon}
+          isFullWidth
+          color="notice"
+          isDismissible={false}
+          marginX={isMobile ? '-8px' : '-16px'}
+          marginTop={isMobile ? '-8px' : '-16px'}
+        />
+      )}
+      <Box display="flex" justifyContent="center">
+        <Box maxWidth="920px" width="100%" paddingTop="spacing.2">
+          <Suspense fallback={<LayoutLoader />}>
+            <FTUXLanding triggerTwoFaAuth={triggerTwoFaAuth} />
+          </Suspense>
+        </Box>
       </Box>
     </Box>
   );
