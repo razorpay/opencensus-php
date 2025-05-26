a318ff301c8aa2e0cd8cc5a8f2501d2301f21e5d
Priyanshu Agarwal <priyanshu.agarwal@razorpay.com>
2025-05-26 06:21:44 +0530

    updated data accessor method

--- app/Models/User/Service.php
+++ app/Models/User/Service.php
@@ -4921,73 +4921,70 @@ class Service extends Base\Service
        }
    }
 
-   public function userServiceDataAccessor($input)
-   {
-       $responseData = [];
+    public function userServiceDataAccessor($input)
+    {
+        $responseData = [];
 
-       $this->trace->info(
-           TraceCode::USER_SERVICE_DATA_ACCESSOR,
-               [
-                   'input' => $input
-               ]
-       );
+        $this->trace->info(
+            TraceCode::USER_SERVICE_DATA_ACCESSOR,
+            [
+                'input' => $input
+            ]
+        );
 
-       $action = $input['action'] ?? 'default';
+        $action = $input['action'] ?? 'default';
 
-       switch ($action) {
-           case 'create_merchant':
-               $responseData = $this->merchantService->create($input['payload']['merchant_input_data'],
-                   $input['payload']['merchant_detail_input_data'],
-                   $input['payload']['create_merchant_metadata']
-               );
-               break;
+        switch ($action) {
+            case 'create_merchant':
+                $merchantInputData = $input['payload']['merchant_input_data'] ?? [];
+                $merchantDetailInputData = $input['payload']['merchant_detail_input_data'] ?? [];
+                $createMerchantMetadata = $input['payload']['create_merchant_metadata'] ?? [];
+                $responseData = $this->merchantService->create($merchantInputData,
+                    $merchantDetailInputData,
+                    $createMerchantMetadata
+                );
+                break;
 
-           case 'fetch_permission':
-               $responseData = $this->core->userPermissions($input['merchant']);
-               break;
+            case 'product_switch':
+                $this->productSwitch($input);
+                $responseData = ['success' => true];
+                break;
 
-           case 'product_switch':
-               $this->productSwitch($input);
-               $responseData = ['success' => true];
-               break;
+            case 'actor_info':
+                $responseData = Adapter\Base::getActorInfo();
+                break;
 
-           case 'actor_info':
-               $responseData = Adapter\Base::getActorInfo();
-               break;
+            case 'fetch_role_names_from_authz':
+                $responseData = $this->fetchAuthzRoleNames($input);
+                break;
 
-           case 'fetch_role_names_from_authz':
-               $responseData = $this->fetchAuthzRoleNames($input);
-               break;
+            case 'fetch_role_name':
+                $responseData = $this->repo->roles->fetchRoleName($input['role_name']);
+                break;
 
-           case 'fetch_role_name':
-               $responseData = $this->repo->roles->fetchRoleName($input['role_name']);
-               break;
+            case 'fetch_settings':
+                $responseData = $this->core->fetchUserSettings($input['user_id']);
+                break;
 
-           case 'fetch_methods':
-               $merchantEntity = $this->repo->merchant->findOrFailPublic($input['merchant_id']);
-               $responseData = $merchantEntity->getMethods();
-               break;
+            case 'fetch_additional_data':
+                $merchantEntity = $this->repo->merchant->findOrFailPublic($input['merchant_id']);
+                $responseData = [
+                    'permissions' =>  $this->core->userPermissions($input['merchant']),
+                    'attributes' => $this->core->fetchMerchantAttribute($input['merchant_id']),
+                    'methods' => $merchantEntity->getMethods()
+                ];
 
-           case 'fetch_attributes':
-               $responseData = $this->core->fetchMerchantAttribute($input['merchant_id']);
-               break;
+                $responseData += $this->core->fetchBankingBalanceData($input['merchant_id']);
+                break;
 
-           case 'fetch_balance':
-               $responseData = $this->core->fetchBankingBalanceData($input['merchant_id']);
-               break;
+            default:
+                break;
+        }
 
-           case 'fetch_settings':
-               $responseData = $this->core->fetchUserSettings($input['user_id']);
-               break;
-
-           default:
-               break;
-       }
-
-       return [
-           'response' => $responseData
-       ];
-   }
+        return [
+            'response' => $responseData
+        ];
+    }
 
    public function fetchAuthzRoleNames($input)
    {
