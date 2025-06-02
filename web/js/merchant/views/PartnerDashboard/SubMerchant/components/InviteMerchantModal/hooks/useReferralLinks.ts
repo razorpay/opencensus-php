d82748930b5ba1575f9987da06c7820f8227a93d
Navin kumar <1995navinkumar@gmail.com>
2025-06-02 13:13:45 +0530

    Revert "fix: disable merchant referral for pure platform partners (#17255)" (#17282)
    This reverts commit e27ec982b340fed75e1bc87c630be04dcd858009.

--- web/js/merchant/views/PartnerDashboard/SubMerchant/components/InviteMerchantModal/hooks/useReferralLinks.ts
+++ web/js/merchant/views/PartnerDashboard/SubMerchant/components/InviteMerchantModal/hooks/useReferralLinks.ts
@@ -1,6 +1,4 @@
 import { UseQueryResult, useQuery } from '@tanstack/react-query';
-import { useStore } from '@federated/apps/shell/commonStore';
-import { PARTNER_TYPE } from '@dashboards/payments/views/PartnerDashboard/constants';
 
 import { ShowNotificationType } from 'common/typings';
 import { merchantFetch } from 'merchant/utils/ajax';
@@ -33,10 +31,6 @@ const useReferralLinks = ({
 }: {
   showNotification: ShowNotificationType;
 }): UseQueryResult<ReferralData> => {
-  const isPurePlatformPartner = useStore(
-    (state) => state.session.user.partner_type === PARTNER_TYPE.PURE_PLATFORM,
-  );
-
   return useQuery({
     queryKey: ['fetch-referrals'],
     queryFn: async (): Promise<ReferralData> => {
@@ -48,7 +42,7 @@ const useReferralLinks = ({
       });
       return data.referrals;
     },
-    enabled: !isPurePlatformPartner,
+    enabled: true,
     retry: false,
     refetchOnWindowFocus: false,
     refetchOnMount: false,
