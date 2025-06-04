f6744a1cbdb68c957065eca1c47ef09d84f16485
shridharbavannawar <shridhar.b@razorpay.com>
2025-06-04 19:37:49 +0530

    update e2e resources (#17330)

--- libs/shared-core/src/plugins/withDashboardCore/core/withDashboardPlaywright/infra/getSelfPayload.ts
+++ libs/shared-core/src/plugins/withDashboardCore/core/withDashboardPlaywright/infra/getSelfPayload.ts
@@ -9,9 +9,14 @@ export const getSelfPayload = async ({ repoName, selfCommit, affectedProjects }:
   const payload = {
     name: repoName,
     chart_values: {
-      web_requests_memory: '350Mi',
-      web_requests_cpu: '500m',
-      replicas: 3,
+      dashboard_shell_requests_memory: '400Mi',
+      dashboard_shell_requests_cpu: '400m',
+      dashboard_shell_limits_memory: '500Mi',
+      dashboard_shell_replicas: 3,
+      dashboard_requests_memory: '400Mi',
+      dashboard_requests_cpu: '400m',
+      dashboard_limits_memory: '500Mi',
+      dashboard_replicas: 3,
       dashboard: dashboardChartValues,
     },
   };
