350a3798120e00df4e14fcece78dd6c0320a74fb
Parth Sethi <49690593+victorakaps@users.noreply.github.com>
2025-06-17 19:39:51 +0530

    Feat/e2e move to base new (#17422)
    * test on CI
    
    * test on CI
    
    * test on CI
    
    * test on CI
    
    * test on CI
    
    * test on CI
    
    * skip e2e failure
    
    * test on CI
    
    * empty commit
    
    * empty commit
    
    * empty commit
    
    * empty commit
    
    ---------
    
    Co-authored-by: harshcut <harshskarande@gmail.com>

--- libs/shared-core/src/plugins/withDashboardCore/core/withDashboardPlaywright/constants/base-dependencies.playwright.ts
+++ libs/shared-core/src/plugins/withDashboardCore/core/withDashboardPlaywright/constants/base-dependencies.playwright.ts
@@ -17,6 +17,15 @@ export const BASE_DEPENDENCIES: BASE_DEPENDENCIES_TYPE = [
   { namespace: 'gimli', service_name: 'gimli-base' },
   { namespace: 'reminders', service_name: 'reminders-base' },
   { namespace: 'scrooge', service_name: 'scrooge-web-base' },
+  { namespace: 'ui-config-service', service_name: 'ui-config-service-base' },
+  { namespace: 'care', service_name: 'care-web-base' },
+  { namespace: 'partnerships', service_name: 'partnerships-live-base' },
+  { namespace: 'no-code-apps', service_name: 'no-code-apps-base' },
+  { namespace: 'splitz', service_name: 'splitz-base' },
+  { namespace: 'subscriptions', service_name: 'subscriptions-web-base' },
+  { namespace: 'terminals', service_name: 'terminals-live-base' },
+  { namespace: 'pg-router', service_name: 'pg-router-base' },
+  { namespace: 'api', service_name: 'api-web-base' },
 ];
 
 export type { BASE_DEPENDENCIES_TYPE };
