9647f776908c9fee8b06df89938279207dd3f4ad
adlina mesri <adlina.mesri@razorpay.com>
2025-06-18 13:22:54 +0800

    revert unrelated changes

--- app/Models/Payment/Processor/Processor.php
+++ app/Models/Payment/Processor/Processor.php
@@ -8083,7 +8083,7 @@ class Processor
                     $customerId = $input[Payment\Entity::CUSTOMER_ID];
 
                     Customer\Entity::verifyIdAndStripSign($customerId);
-
+                    
                     $token = (new Customer\Token\Core)->getByTokenIdAndCustomerId($tokenId, $customerId);
 
                     if ($this->isOptimizerMigratedToken($token))
