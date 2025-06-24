a2f722b9f2245fd99e86717644d978199145a3e7
Rahul Makhija <86592481+rahul-rzp@users.noreply.github.com>
2025-06-24 10:15:59 +0530

    fix(canary builds): make hotfix/ branch name check case insensitive (#17472)
    * add change
    
    * add logs and fix
    
    * use run_canary_build only for shell builds like other builds
    
    * add dummy commit

--- web/js/merchant/playwright.config.js
+++ web/js/merchant/playwright.config.js
@@ -1,6 +1,6 @@
 const { withDashboardCore, DASHBOARD_FEDERATED_MODULES } = require('@libs/shared-core');
 
-// dummy 2
+// dummy
 module.exports = withDashboardCore({
   playwrightOptions: {
     moduleName: DASHBOARD_FEDERATED_MODULES.PAYMENTS_DASHBOARD,
