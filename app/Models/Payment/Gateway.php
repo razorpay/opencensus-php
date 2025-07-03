2050823830141cc88929299a03a07f5b05e1a39a
Sumant Yerawar <sumantrajendra.y@razorpay.com>
2025-07-03 23:04:22 +0530

    emandate cug bank fix (#56922)

--- app/Models/Payment/Gateway.php
+++ app/Models/Payment/Gateway.php
@@ -6517,10 +6517,7 @@ class Gateway
                 self::TNGD,
             ],
             Method::EMANDATE => [
-                self::ENACH_NPCI_NETBANKING,
-                self::NETBANKING_AXIS,
-                self::NETBANKING_SBI,
-                self::NETBANKING_HDFC
+                self::ENACH_NPCI_NETBANKING
             ]
         ];
 
